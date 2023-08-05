9913979d573c9f043030f504873ee6d17905ca0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:15 +0000

    Add package NearPy/0.2/NearPy-0.2.tar.gz

---
+++ packages/NearPy/NearPy-0.2.tar.gz/NearPy-0.2/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='NearPy',
+    version='0.2',
+    author='Ole Krause-Sparmann',
+    author_email='ole@pixelogik.de',
+    packages=[
+        'nearpy',
+        'nearpy.distances',
+        'nearpy.experiments',
+        'nearpy.filters',
+        'nearpy.hashes',
+        'nearpy.hashes.permutation',
+        'nearpy.storage',
+        'nearpy.utils'
+    ],
+    url='http://pypi.python.org/pypi/NearPy/',
+    license='LICENSE.txt',
+    description='Framework for fast approximated nearest neighbour search.',
+    long_description=open('README.txt').read(),
+    install_requires=[
+        "numpy",
+        "scipy",
+        "redis",
+        "bitarray"
+    ],
+)
