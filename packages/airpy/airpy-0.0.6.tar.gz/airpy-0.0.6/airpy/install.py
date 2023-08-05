86de6ffec09d751a7c25caa8db9849e0f6c6e6fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:52 +0000

    Add package airpy/0.0.6/airpy-0.0.6.tar.gz

---
+++ packages/airpy/airpy-0.0.6.tar.gz/airpy-0.0.6/airpy/install.py
@@ -0,0 +1,35 @@
+import os
+import requests
+import zipfile
+import click
+import io
+import airpy
+import json
+
+from airpy import utils
+
+def install_metadata(name):
+	directory = airpy.data_directory + '/' + name
+	metadata_response = requests.get('https://pypi.python.org/pypi/'+ name + '/json')
+	if metadata_response.status_code == 200:
+		metadata = metadata_response.json()
+		with open(directory + '/' + name + '_airpy.json', 'w') as outfile:
+			json.dump(metadata, outfile, sort_keys = True, indent = 4, ensure_ascii = False)
+		return True
+	else:
+		return False
+
+def airinstall(name):
+	if utils.is_doc_installed(name):
+		click.echo('Docs for ' + name + ' is already installed.')
+	else:
+		response = requests.get('https://readthedocs.org/projects/' + name + '/downloads/htmlzip/latest/')
+		if response.status_code == 200:
+			directory = airpy.data_directory + '/' + name
+			os.makedirs(directory)
+			z = zipfile.ZipFile(io.BytesIO(response.content), 'a')
+			z.extractall(directory)
+			install_metadata(name)
+		else:
+			click.echo('There are no docs for Python module named ' + '\'' + name + '\'')
+			return False
---
+++ packages/airpy/airpy-0.0.6.tar.gz/airpy-0.0.6/airpy/utils.py
@@ -0,0 +1,19 @@
+import airpy
+import os
+import json
+
+def is_doc_installed(name):
+	directory = airpy.data_directory + '/' + name
+	if os.path.exists(directory):
+		return True
+	else:
+		return False
+
+def package_info(name, parameter):
+	directory = airpy.data_directory + '/' + name
+	try:
+		with open(directory + '/' + name + '_airpy.json') as json_file:
+			data = json.load(json_file)
+		return data['info'][parameter]
+	except FileNotFoundError:
+		return 'No Description'
