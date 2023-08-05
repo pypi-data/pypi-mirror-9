010dc9e7764b33409527870e3e8462c7d81aea3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:19 +0000

    Add package Flask-Slither/0.5.2/Flask-Slither-0.5.2.tar.gz

---
+++ packages/Flask-Slither/Flask-Slither-0.5.2.tar.gz/Flask-Slither-0.5.2/setup.py
@@ -0,0 +1,42 @@
+"""
+Flask-Slither
+-------------
+
+Slither is a thin library that sits between a mongodb and RESTful
+API endpoints.
+"""
+from setuptools import setup
+
+
+setup(
+    name='Flask-Slither',
+    version='0.5.2',
+    url='http://github.com/gevious/flask_slither',
+    license='MIT',
+    author='Nico Gevers',
+    author_email='ingevious@gmail.com',
+    description='A small library between MongoDB and JSON API endpoints',
+    long_description=__doc__,
+    packages=['flask_slither'],
+    test_suite='nose.collector',
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    install_requires=[
+        'Flask==0.10.1',
+        'pymongo==2.6',
+    ],
+    tests_require=[
+        'Flask-Testing==0.4.1',
+        'nose==1.3.3',
+    ],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
