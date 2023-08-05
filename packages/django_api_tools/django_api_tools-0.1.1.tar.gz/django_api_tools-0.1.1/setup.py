69cf918e8a10bdf1d8807ed5d3a3485c5d8e6be0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:17 +0000

    Add package django_api_tools/0.1.1/django_api_tools-0.1.1.tar.gz

---
+++ packages/django_api_tools/django_api_tools-0.1.1.tar.gz/django_api_tools-0.1.1/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+__author__ = 'szpytfire'
+
+setup(
+  name = 'django_api_tools',
+  packages = ['django_api_tools'],
+  version = '0.1.1',
+  description = 'Django API add-on is a mini-framework which allows developers to run RESTful APIs alongside websites using Forms/Templates.',
+  author = 'Tom Szpytman',
+  author_email = 'mail@tomszpytman.com',
+  url = 'https://github.com/szpytfire/django-api-tools',
+  download_url = 'https://github.com/szpytfire/django-api-tools/tarball/0.1.1',
+  keywords = ['django', 'api', 'rest'],
+  classifiers = [],
+)
