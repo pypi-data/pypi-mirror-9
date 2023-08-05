cf93bf9090dc96b32ac773dca55caa1c6a6b173f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:35 +0000

    Add package iugu-python/0.9.4/iugu-python-0.9.4.tar.gz

---
+++ packages/iugu-python/iugu-python-0.9.4.tar.gz/iugu-python-0.9.4/lib/iugu/version.py
@@ -0,0 +1,7 @@
+__version__ = "0.9.4"
+
+#------------------------------------------------------------------------------
+# CHANGELOG:
+# ... oldest changes
+# 2014-07-07 v0.9.3 - Removed requirement of config.py variables (token, account)
+# 2015-02-22 v0.9.4 - Supported changes from API 
