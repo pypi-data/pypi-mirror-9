d986f382933ee0411561386fee8624c772f4391c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:20 +0000

    Add package pyramid_transactional_celery/0.1.1/pyramid_transactional_celery-0.1.1-py2.py3-none-any.whl

---
+++ packages/pyramid_transactional_celery/pyramid_transactional_celery-0.1.1-py2.py3-none-any.whl/pyramid_transactional_celery/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+__author__ = 'Christopher Petrilli'
+__email__ = 'petrilli@amber.org'
+__version__ = '0.1.0'
+
+from .transactional_task import (
+    TransactionalTask,
+    task_tm,
+)
