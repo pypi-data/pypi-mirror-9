e743f4837f3b13557ddc9f3d3ecad6f9f9c310ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:58 +0000

    Add package bespin/0.4.9.7/bespin-0.4.9.7.tar.gz

---
+++ packages/bespin/bespin-0.4.9.7.tar.gz/bespin-0.4.9.7/bespin/option_spec/artifact_objs.py
@@ -0,0 +1,158 @@
+from bespin.errors import BadOption, MissingFile, InvalidArtifact
+from bespin.processes import command_output
+from bespin import helpers as hp
+
+from input_algorithms.spec_base import NotSpecified
+from input_algorithms.dictobj import dictobj
+
+import logging
+import shutil
+import os
+
+log = logging.getLogger("bespin.option_spec.artifact_objs")
+
+class ArtifactCollection(dictobj):
+    fields = ['artifacts']
+
+    def __iter__(self):
+        return iter(self.artifacts)
+
+    def items(self):
+        return self.artifacts.items()
+
+    def clean_old_artifacts(self, s3, environment, dry_run=True):
+        # Iterate over each artifact we need to clean
+        for key, artifact in self.artifacts.items():
+            log.info("Cleaning old artifacts\tartifact=%s", key)
+            # Get contents of bucket
+            artifact_path = os.path.dirname(artifact.upload_to.format(**environment))
+            artifact_keys = list(s3.list_keys_from_s3_path(artifact_path))
+
+            # Get all the time stamps and determine the files to delete
+            sorted_keys = sorted(artifact_keys, key=lambda x: x.last_modified)
+            keys_to_del = sorted_keys[:-artifact.history_length]
+
+            # Iterate through all the artifacts deleting any ones flagged for deletion
+            for artifact_key in keys_to_del:
+                log.info("Deleting artifact %s ", artifact_key.name)
+                if dry_run:
+                    log.info("DRYRUN: Would delete key")
+                else:
+                    artifact_key.delete()
+
+class Artifact(dictobj):
+    fields = {
+          "paths": "Paths to copy from disk into the artifact"
+        , "files": """
+              Any files to add into the artifact
+
+              For example::
+
+                files:
+                  - content: "{__stack__.vars.version}"
+                    path: /artifacts/app/VERSION.txt
+          """
+        , "commands": "Commands that need to be run to generate content for the artifact"
+        , "upload_to": "S3 path to upload the artifact to"
+        , "history_length": """
+              The number of artifacts to keep in s3
+
+              .. note:: These only get purged if the stack has ``artifact_retention_after_deployment`` set
+                to true or if the ``clean_old_artifacts`` task is run
+          """
+        , "compression_type": "The compression to use on the artifact"
+        }
+
+class ArtifactPath(dictobj):
+    fields = ["host_path", "artifact_path"]
+
+    def add_to_tar(self, tar, environment=None):
+        """Add everything in this ArtifactPath to the tar"""
+        if environment is None:
+            environment = {}
+
+        for full_path, tar_path in self.files(environment):
+            print(tar_path)
+            tar.add(full_path, tar_path)
+
+    def files(self, environment, prefix_path=None):
+        """Iterate over the files in our host_path and yield (full_path, tar_path)"""
+        host_path = self.host_path
+        prefix_path = "/" if prefix_path is None else prefix_path
+        while host_path and host_path.startswith("/"):
+            host_path = host_path[1:]
+
+        host_path = os.path.abspath(os.path.join(prefix_path, host_path.format(**environment)))
+        artifact_path = os.path.abspath(self.artifact_path.format(**environment))
+
+        if not os.path.exists(host_path):
+            raise MissingFile("Expected to be able to copy in a path", path=host_path, artifact_path=artifact_path)
+
+        if os.path.isfile(host_path):
+            yield host_path, artifact_path
+            return
+
+        for root, dirs, files in os.walk(self.host_path):
+            for f in files:
+                file_full_path = os.path.abspath(os.path.join(root, f))
+                file_tar_path = file_full_path.replace(os.path.normpath(self.host_path), self.artifact_path, 1)
+                yield file_full_path, file_tar_path
+
+class ArtifactFile(dictobj):
+    fields = ["content", "path"]
+
+    def add_to_tar(self, tar, environment=None):
+        """Add this file to the tar"""
+        if environment is None:
+            environment = {}
+
+        with hp.a_temp_file() as f:
+            f.write(self.content.format(**environment).encode('utf-8'))
+            f.close()
+            print(self.path)
+            tar.add(f.name, self.path)
+
+class ArtifactCommand(dictobj):
+    fields = ["copy", "modify", "command", "add_into_tar"]
+
+    def add_to_tar(self, tar, environment=None):
+        if environment is None:
+            environment = {}
+
+        with hp.a_temp_directory() as command_root:
+            self.do_copy(command_root, environment)
+            self.do_modify(command_root, environment)
+            self.do_command(command_root, environment)
+            self.do_copy_into_tar(command_root, environment, tar)
+
+    def do_copy_into_tar(self, into, environment, tar):
+        for path in self.add_into_tar:
+            for full_path, tar_path in path.files(environment, prefix_path=into):
+                print(tar_path)
+                tar.add(full_path, tar_path)
+
+    def do_command(self, root, environment):
+        command_output(self.command.format(**environment), cwd=root, timeout=600, verbose=True)
+
+    def do_modify(self, into, environment):
+        for key, options in self.modify.items():
+            path = os.path.join(into, key)
+            if not os.path.exists(path):
+                raise MissingFile("Expected a file to modify", path=path)
+
+            if "append" in options:
+                with open(path, "a") as fle:
+                    fle.write("\n")
+                    for append in options["append"]:
+                        fle.write("{0}\n".format(append.format(**environment)))
+
+    def do_copy(self, into, environment):
+        for path in self.copy:
+            for full_path, copy_path in path.files(environment):
+                while copy_path.startswith("/"):
+                    copy_path = copy_path[1:]
+                copy_path = os.path.join(into, copy_path)
+                if not os.path.exists(os.path.dirname(copy_path)):
+                    os.makedirs(os.path.dirname(copy_path))
+                shutil.copy(full_path, copy_path)
+
---
+++ packages/bespin/bespin-0.4.9.7.tar.gz/bespin-0.4.9.7/bespin/option_spec/bespin_specs.py
@@ -0,0 +1,226 @@
+"""
+Here we define the yaml specification for Besin options, task options and stack
+options.
+
+The specifications are responsible for sanitation, validation and normalisation.
+"""
+
+from input_algorithms.spec_base import (
+      formatted, defaulted, any_spec, dictionary_spec, dictof, listof, required, delayed
+    , string_spec, overridden, boolean, file_spec, optional_spec, integer_spec, or_spec, container_spec
+    , valid_string_spec, create_spec, string_choice_spec, Spec
+    )
+
+from bespin.option_spec import task_objs, stack_objs, stack_specs, artifact_objs, imports, deployment_check
+from bespin.formatter import MergedOptionStringFormatter
+from bespin.option_spec.bespin_obj import Bespin
+from bespin.helpers import memoized_property
+from bespin.errors import BadFile
+
+from input_algorithms.spec_base import NotSpecified
+from input_algorithms.dictobj import dictobj
+from input_algorithms import validators
+
+import json
+import yaml
+import six
+import os
+
+class valid_params(Spec):
+    filetype = NotImplemented
+    params_spec = NotImplemented
+
+    def setup(self, default):
+        self.dflt = default
+
+    def normalise_either(self, meta, val):
+        if isinstance(val, six.string_types) or val is NotSpecified:
+            val = formatted(defaulted(string_spec(), self.dflt), formatter=MergedOptionStringFormatter).normalise(meta, val)
+            if os.path.exists(val):
+                try:
+                    with open(val) as fle:
+                        val = self.filetype.load(fle)
+                except (ValueError, TypeError) as error:
+                    raise BadFile(error, filename=val, meta=meta)
+                self.params_spec().normalise(meta, val)
+        else:
+            self.params_spec().normalise(meta, val)
+
+        return val
+
+class valid_params_json(valid_params):
+    filetype = json
+    params_spec = lambda k: stack_specs.params_json_spec()
+
+class valid_params_yaml(valid_params):
+    filetype = yaml
+    params_spec = lambda k: stack_specs.params_yaml_spec()
+
+class valid_stack_json(valid_params):
+    filetype = json
+    params_spec = lambda k: stack_specs.stack_json_spec()
+
+class Environment(dictobj):
+    fields = ["account_id", "vars", "region"]
+
+class other_options(dictobj):
+    fields = ["run", "create", "build"]
+
+class BespinSpec(object):
+    """Knows about bespin specific configuration"""
+
+    @memoized_property
+    def task_name_spec(self):
+        """Just needs to be ascii"""
+        return valid_string_spec(
+              validators.no_whitespace()
+            , validators.regexed("^[a-zA-Z][a-zA-Z0-9-_\.]*$")
+            )
+
+    def tasks_spec(self, available_actions, default_action="run"):
+        """Tasks for a particular stack"""
+        return dictof(
+              self.task_name_spec
+            , create_spec(task_objs.Task
+                , action = defaulted(string_choice_spec(available_actions, "No such task"), default_action)
+                , options = dictionary_spec()
+                , overrides = dictionary_spec()
+                , description = string_spec()
+                )
+            )
+
+    @memoized_property
+    def environments_spec(self):
+        """Spec for each environment options"""
+        return dictof(
+              string_spec()
+            , create_spec(Environment
+                , account_id = required(or_spec(string_spec(), valid_string_spec(validators.regexed("\d+"))))
+                , region = defaulted(string_spec(), "ap-southeast-2")
+                , vars = dictionary_spec()
+                )
+            )
+
+    @memoized_property
+    def stack_spec(self):
+        """Spec for each stack"""
+        return create_spec(stack_objs.Stack
+            , validators.deprecated_key("url_checker", "Use ``confirm_deployment.url_checker1``")
+            , validators.deprecated_key("deploys_s3_path", "Use ``confirm_deployment.deploys_s3_path``")
+            , validators.deprecated_key("sns_confirmation", "Use ``confirm_deployment.sns_confirmation``")
+            , validators.deprecated_key("autoscaling_group_id", "Use ``auto_scaling_group_name``")
+
+            , bespin = any_spec()
+
+            , name = formatted(defaulted(string_spec(), "{_key_name_1}"), formatter=MergedOptionStringFormatter)
+            , key_name = formatted(overridden("{_key_name_1}"), formatter=MergedOptionStringFormatter)
+            , stack_name = formatted(defaulted(string_spec(), "{_key_name_1}"), formatter=MergedOptionStringFormatter)
+            , environment = formatted(overridden("{environment}"), formatter=MergedOptionStringFormatter)
+
+            , env = listof(stack_specs.env_spec(), expect=stack_objs.Environment)
+            , build_env = listof(stack_specs.env_spec(), expect=stack_objs.Environment)
+            , stack_name_env = listof(stack_specs.env_spec(), expect=stack_objs.Environment)
+
+            , tags = dictionary_spec()
+
+            , stack_json = valid_stack_json(default="{config_root}/{_key_name_1}.json")
+
+            , params_json = valid_params_json(default="{config_root}/{environment}/{_key_name_1}-params.json")
+            , params_yaml = valid_params_yaml(default="{config_root}/{environment}/{_key_name_1}-params.yaml")
+
+            , build_first = listof(formatted(string_spec(), formatter=MergedOptionStringFormatter))
+            , build_after = listof(formatted(string_spec(), formatter=MergedOptionStringFormatter))
+            , ignore_deps = defaulted(boolean(), False)
+
+            , vars = dictof(string_spec(), stack_specs.var_spec())
+
+            , skip_update_if_equivalent = listof(stack_specs.skipper_spec())
+
+            , suspend_actions = defaulted(boolean(), False)
+            , auto_scaling_group_name = optional_spec(formatted(string_spec(), formatter=MergedOptionStringFormatter))
+
+            , artifact_retention_after_deployment = defaulted(boolean(), False)
+
+            , command = optional_spec(string_spec())
+
+            , instance_count_limit = defaulted(integer_spec(), 10)
+
+            , artifacts = container_spec(artifact_objs.ArtifactCollection, dictof(string_spec(), create_spec(artifact_objs.Artifact
+                , compression_type = string_choice_spec(["gz", "xz"])
+                , history_length = integer_spec()
+                , upload_to = formatted(string_spec(), formatter=MergedOptionStringFormatter)
+                , commands = listof(stack_specs.artifact_command_spec(), expect=artifact_objs.ArtifactCommand)
+                , paths = listof(stack_specs.artifact_path_spec(), expect=artifact_objs.ArtifactPath)
+                , files = listof(create_spec(artifact_objs.ArtifactFile
+                    , content = formatted(string_spec(), formatter=MergedOptionStringFormatter)
+                    , path = formatted(string_spec(), formatter=MergedOptionStringFormatter)
+                    ))
+                )))
+
+            , ssh = optional_spec(create_spec(stack_objs.SSH
+                , validators.deprecated_key("autoscaling_group_id", "Use ``auto_scaling_group_name``")
+
+                , user = required(formatted(string_spec(), formatter=MergedOptionStringFormatter))
+                , bastion = optional_spec(formatted(string_spec(), formatter=MergedOptionStringFormatter))
+                , bastion_key_location = optional_spec(formatted(string_spec(), formatter=MergedOptionStringFormatter))
+                , instance_key_location = optional_spec(formatted(string_spec(), formatter=MergedOptionStringFormatter))
+
+                , address = optional_spec(formatted(string_spec(), formatter=MergedOptionStringFormatter))
+                , instance = optional_spec(formatted(string_spec(), formatter=MergedOptionStringFormatter))
+                , auto_scaling_group_name = optional_spec(formatted(string_spec(), formatter=MergedOptionStringFormatter))
+
+                , bastion_key_path = formatted(defaulted(string_spec(), "{config_root}/{environment}/bastion_ssh_key.pem"), formatter=MergedOptionStringFormatter)
+                , instance_key_path = formatted(defaulted(string_spec(), "{config_root}/{environment}/ssh_key.pem"), formatter=MergedOptionStringFormatter)
+                ))
+
+            , confirm_deployment = optional_spec(create_spec(deployment_check.ConfirmDeployment
+                , deploys_s3_path = optional_spec(listof(stack_specs.s3_address()))
+                , zero_instances_is_ok = defaulted(boolean(), False)
+                , auto_scaling_group_name = required(formatted(string_spec(), formatter=MergedOptionStringFormatter))
+
+                , url_checker = optional_spec(create_spec(deployment_check.UrlChecker
+                    , check_url = required(formatted(string_spec(), formatter=MergedOptionStringFormatter))
+                    , endpoint = required(delayed(stack_specs.var_spec()))
+                    , expect = required(formatted(string_spec(), formatter=MergedOptionStringFormatter))
+                    , timeout_after = defaulted(integer_spec(), 600)
+                    ))
+
+                , sns_confirmation = optional_spec(create_spec(deployment_check.SNSConfirmation
+                    , validators.deprecated_key("auto_scaling_group_id", "Use ``confirm_deployment.auto_scaling_group_name``")
+                    , validators.deprecated_key("env", "Use ``stack.<stack>.env`` instead``")
+
+                    , timeout = defaulted(integer_spec(), 300)
+                    , version_message = required(formatted(string_spec(), formatter=MergedOptionStringFormatter))
+                    , deployment_queue = required(formatted(string_spec(), formatter=MergedOptionStringFormatter))
+                    ))
+                ))
+            )
+
+    @memoized_property
+    def bespin_spec(self):
+        """Spec for bespin options"""
+        formatted_string = formatted(string_spec(), MergedOptionStringFormatter, expected_type=six.string_types)
+        formatted_boolean = formatted(boolean(), MergedOptionStringFormatter, expected_type=bool)
+
+        return create_spec(Bespin
+            , validators.deprecated_key("region", "Please use ``environments.<env>.region``")
+
+            , config = file_spec()
+            , configuration = any_spec()
+
+            , assume_role = optional_spec(string_spec())
+
+            , dry_run = defaulted(boolean(), False)
+            , flat = defaulted(boolean(), False)
+            , environment = optional_spec(string_spec())
+
+            , extra = defaulted(formatted_string, "")
+            , no_assume_role = defaulted(formatted_boolean, False)
+
+            , chosen_task = defaulted(formatted_string, "list_tasks")
+            , chosen_stack = defaulted(formatted_string, "")
+            , chosen_artifact = defaulted(formatted_string, "")
+
+            , extra_imports = listof(imports.import_spec())
+            )
+
