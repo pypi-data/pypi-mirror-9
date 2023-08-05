48e0c58c00f5ec76b8facc7ca2f7827cd2d70497
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:56 +0000

    Add package eventbrite/3.0.1/eventbrite-3.0.1.tar.gz

---
+++ packages/eventbrite/eventbrite-3.0.1.tar.gz/eventbrite-3.0.1/eventbrite/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+__author__ = 'Daniel Greenfeld'
+__email__ = 'danny@eventbrite.com'
+
+from ._version import __version__
+
+
+from .client import Eventbrite
+from .utils import EVENTBRITE_API_URL
