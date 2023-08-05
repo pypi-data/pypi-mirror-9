856b5cd94ad8b22c05ebab0d34b168067bf02167
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:37 +0000

    Add package xxhash/0.3.2/xxhash-0.3.2.zip

---
+++ packages/xxhash/xxhash-0.3.2.zip/xxhash-0.3.2/setup.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+
+
+from setuptools import setup, Extension
+import os
+
+VERSION = "0.3.2"
+XXHASH_VERSION = "r39"
+
+if os.name == 'posix':
+    extra_compile_args = [
+        "-std=c99",
+        "-O3",
+        "-Wall",
+        "-W",
+        "-Wundef",
+        # ref: http://bugs.python.org/issue21121
+        "-Wno-error=declaration-after-statement",
+    ]
+else:
+    extra_compile_args = None
+
+define_macros = [
+    ('VERSION', VERSION),
+    ('XXHASH_VERSION', XXHASH_VERSION)
+]
+
+setup(
+    name='xxhash',
+    version=VERSION,
+    description="Python binding for xxHash",
+    long_description=open('README.rst', 'r').read(),
+    author='Yue Du',
+    author_email='ifduyue@gmail.com',
+    url='https://github.com/ifduyue/python-xxhash',
+    ext_modules=[
+        Extension('xxhash', [
+            'python-xxhash.c',
+            'xxhash/xxhash.c',
+        ],
+        extra_compile_args=extra_compile_args,
+        define_macros=define_macros)
+    ],
+    setup_requires=["nose>=1.3.0"],
+    test_suite='nose.collector',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: BSD License',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: Implementation :: CPython',
+    ],
+)
