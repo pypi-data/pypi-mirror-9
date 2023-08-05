85273eaa19efad9d6c68fbe709be654543e85ef6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:55 +0000

    Add package fencepy/0.4.3/fencepy-0.4.3.tar.gz

---
+++ packages/fencepy/fencepy-0.4.3.tar.gz/fencepy-0.4.3/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup
+import sys
+
+pkgversion = '0.4.3'
+
+setup(
+    name='fencepy',
+    version=pkgversion,
+    description='Standardized fencing off of python virtual environments on a per-project basis',
+    author='Adam Kaufman',
+    author_email='kaufman.blue@gmail.com',
+    url='https://github.com/ajk8/fencepy',
+    download_url='https://github.com/ajk8/fencepy/tarball/' + pkgversion,
+    license='MIT',
+    packages=['fencepy'],
+    entry_points={'console_scripts': ['fencepy=fencepy:fence',
+                                      'fencepy-%s.%s=fencepy:fence' % sys.version_info[:2]]},
+    test_suite='tests',
+    install_requires=[
+        'virtualenv>=1.11'
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Software Development'
+    ],
+    keywords='virtualenv development'
+)
