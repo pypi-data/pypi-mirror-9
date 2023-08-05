d4be182c23c52019b76ed7c7536cadfb9746f22f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:38 +0000

    Add package privacyIDEA/2.0/privacyIDEA-2.0.tar.gz

---
+++ packages/privacyIDEA/privacyIDEA-2.0.tar.gz/privacyIDEA-2.0/migrations/versions/2551ee982544_.py
@@ -0,0 +1,28 @@
+"""Add column Type to the TokenInfo
+
+Revision ID: 2551ee982544
+Revises: 4f32a4e1bf33
+Create Date: 2015-02-02 18:05:02.480354
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '2551ee982544'
+down_revision = '4f32a4e1bf33'
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    try:
+        op.add_column('tokeninfo', sa.Column('Type', sa.Unicode(length=100), nullable=True))
+    except Exception as exx:
+        print "Count not add the column 'Type' to table tokeninfo"
+        print (exx)
+    ### end Alembic commands ###
+
+
+def downgrade():
+    op.drop_column('tokeninfo', 'Type')
+    ### end Alembic commands ###
