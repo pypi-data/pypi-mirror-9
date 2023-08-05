d6bda236ec98d5e310bc6a2d2461dc43b0ba2ea0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:41 +0000

    Add package django-tethys_datasets/0.5.1/django-tethys_datasets-0.5.1.tar.gz

---
+++ packages/django-tethys_datasets/django-tethys_datasets-0.5.1.tar.gz/django-tethys_datasets-0.5.1/tethys_datasets/admin.py
@@ -0,0 +1,41 @@
+from django.contrib import admin
+from .models import DatasetService, SpatialDatasetService
+from django.forms import ModelForm, PasswordInput
+
+
+class DatasetServiceForm(ModelForm):
+    class Meta:
+        model = DatasetService
+        fields = ('name', 'engine', 'endpoint', 'apikey', 'username', 'password')
+        widgets = {
+            'password': PasswordInput(),
+        }
+
+
+class SpatialDatasetServiceForm(ModelForm):
+    class Meta:
+        model = SpatialDatasetService
+        fields = ('name', 'engine', 'endpoint', 'apikey', 'username', 'password')
+        widgets = {
+            'password': PasswordInput(),
+        }
+
+
+class DatasetServiceAdmin(admin.ModelAdmin):
+    """
+    Admin model for Web Processing Service Model
+    """
+    form = DatasetServiceForm
+    fields = ('name', 'engine', 'endpoint', 'apikey', 'username', 'password')
+
+
+class SpatialDatasetServiceAdmin(admin.ModelAdmin):
+    """
+    Admin model for Spatial Dataset Service Model
+    """
+    form = SpatialDatasetServiceForm
+    fields = ('name', 'engine', 'endpoint', 'apikey', 'username', 'password')
+
+
+admin.site.register(DatasetService, DatasetServiceAdmin)
+admin.site.register(SpatialDatasetService, SpatialDatasetServiceAdmin)
