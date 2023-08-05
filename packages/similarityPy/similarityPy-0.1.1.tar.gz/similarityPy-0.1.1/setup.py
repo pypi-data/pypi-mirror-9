e9cd28a9e5ef23c5e6110e15ed142e014a57ad34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:49 +0000

    Add package similarityPy/0.1.1/similarityPy-0.1.1.tar.gz

---
+++ packages/similarityPy/similarityPy-0.1.1.tar.gz/similarityPy-0.1.1/setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+
+from setuptools import find_packages
+
+
+setup(
+    name='similarityPy',
+    version='0.1.1',
+    packages=find_packages(),
+    description='Similarity Algorithm (Data Mining) implementation in Python',
+    long_description=open('README.md').read(),
+    install_requires=[
+        "enum >= 0.4.4",
+    ],
+    author='Cenk Bircanoglu',
+    author_email='cenk.bircanoglu@gmail.com',
+    url='https://github.com/cenkbircanoglu/similarityPy',  # use the URL to the github repo
+    download_url='https://github.com/cenkbircanoglu/similarityPy/tarball/v0.1.0',  # I'll explain this in a second
+    keywords=['data mining', 'distance', 'similarity', 'measure', 'python', 'statistic'],  # arbitrary keywords
+    classifiers=[],
+    include_package_data=True,
+    zip_safe=False,
+    license='MIT',
+    platforms=['any'],
+)
