3c7eda0178423b1965152ed709812ab1283e5fc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:11 +0000

    Add package valclust/0.1.0/valclust-0.1.2.tar.gz

---
+++ packages/valclust/valclust-0.1.2.tar.gz/valclust-0.1.2/valclust/ExternalValidity/comp_clusterings.py
@@ -0,0 +1,74 @@
+# Vahid Mirjalili
+# Clustering Validation and Analysis
+
+import numpy as np
+import pyprind
+import sys
+from ..cluster import Cluster
+import editdist
+
+
+class CompareCluster(Cluster):
+
+
+    def __init__(self, X, y):
+        self.set_data(X, y)
+
+    def clusterPurity(self, cinx, g):
+	""" Compute the purity of an individual cluster cinx 
+	with respect to a given clustering solution 
+	g (which could be the ground-truth partitioning).
+	"""
+	cmembers = self._get_members(cinx)
+	memg = g[cmembers]
+	sdict = {}
+	for k in memg:
+	    if k in sdict.keys():
+	        sdict[k] += 1
+	    else:
+		sdict[k] = 1
+
+	largest = np.max(sdict.values())
+	return (largest / float(memg.shape[0]))
+
+
+    def totalPurity(self, g, singleton=-1):
+        """ Compute the total purity measure w.r.t. g partitioning
+		singleton is the indicator for singletons.
+	"""
+	res, n = 0.0, 0
+	for k in np.unique(self.y):
+	   if (k != singleton):
+	       res += self.clusterPurity(k, g)
+	       n += 1
+	return (res/n)
+
+    def normalizedMutualInfo(self, g):
+	""" Compute Normalized Mutual Information (NMI) 
+		w.r.t. ground truth partitioning.
+	"""
+	n = self.n
+
+	y_uniq = self.unique()
+
+	gc = Cluster(X=None, y=g)
+	g_uniq = gc.unique()
+
+	y_csize = self.cluster_sizes()
+	g_csize = gc.cluster_sizes()
+
+
+	sval = 0.0
+	for i in y_uniq:
+	    imembers = self._get_members(i)
+
+	    isize = y_csize[y_csize[:,0]==i,1]
+	    gsub = g[imembers]
+
+	    for j in np.unique(gsub):
+		size_ij = np.sum(gsub == j)
+		jsize = g_csize[g_csize[:,0]==j,1]
+		print("%d %d \t %d %d --> %d  %f" %(i, isize, j, jsize, size_ij, size_ij * n/float(isize * jsize)))
+		sval += size_ij/float(n) * np.log(size_ij * n/float(isize * jsize))
+
+	return(2.0 * sval / (self.entropy() + gc.entropy()))
