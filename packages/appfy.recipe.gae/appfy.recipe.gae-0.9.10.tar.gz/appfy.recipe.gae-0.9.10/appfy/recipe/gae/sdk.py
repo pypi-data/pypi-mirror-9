290c8021bc6093e92517f4e7604f279e602f418a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:46 +0000

    Add package appfy.recipe.gae/0.9.10/appfy.recipe.gae-0.9.10.tar.gz

---
+++ packages/appfy.recipe.gae/appfy.recipe.gae-0.9.10.tar.gz/appfy.recipe.gae-0.9.10/appfy/recipe/gae/sdk.py
@@ -0,0 +1,104 @@
+# -*- coding: utf-8 -*-
+"""
+appfy.recipe.gae:sdk
+--------------------
+
+Downloads and installs the App Engine SDK in the buildout directory.
+
+Options
+~~~~~~~
+
+:url: URL to the App Engine SDK file. Default is to download the latest version
+    from storage.googleapis.com.
+:destination: Destination of the extracted SDK. Default is the parts directory.
+:clear-destination: If `true`, deletes the destination dir before
+    extracting the download. Default is `true`.
+
+Example
+~~~~~~~
+
+::
+
+  [gae_sdk]
+  # Dowloads and extracts the App Engine SDK.
+  recipe = appfy.recipe.gae:sdk
+  url = http://googleappengine.googlecode.com/files/google_appengine_1.3.5.zip
+  destination = ${buildout:parts-directory}
+  hash-name = false
+  clear-destination = true
+"""
+from distutils import version
+import json
+import logging
+import os
+import re
+import urllib2
+
+from appfy.recipe import download
+
+
+class HeadRequest(urllib2.Request):
+    def get_method(self):
+        return "HEAD"
+
+
+class SDKCouldNotBeFound(Exception):
+    pass
+
+
+class Recipe(download.Recipe):
+
+    # Eg. featured/google_appengine_1.9.14.zip
+    PYTHON_SDK_RE = re.compile(
+        r'featured/google_appengine_(\d+\.\d+\.\d+).zip'
+    )
+    URL = ("https://www.googleapis.com/storage/"
+           "v1/b/appengine-sdks/o?prefix=featured")
+
+    def __init__(self, buildout, name, options):
+        self.logger = logging.getLogger(name)
+
+        parts_dir = os.path.abspath(buildout['buildout']['parts-directory'])
+        options.setdefault('destination', parts_dir)
+        options.setdefault('clear-destination', 'true')
+
+        super(Recipe, self).__init__(buildout, name, options)
+
+    def install(self):
+        if not self.option_url:
+            self.option_url = self.find_latest_sdk_url()
+        self.logger.info('Using SDK version found at %s', self.option_url)
+        return super(Recipe, self).install()
+
+    def find_latest_sdk_url(self):
+        def version_key(sdk):
+            version_string = self.PYTHON_SDK_RE.match(sdk['name']).group(1)
+            return version.StrictVersion(version_string)
+
+        raw_bucket_list = urllib2.urlopen(self.URL).read()
+        bucket_list = json.loads(raw_bucket_list)
+
+        all_sdks = bucket_list['items']
+        python_sdks = [
+            sdk for sdk in all_sdks if self.PYTHON_SDK_RE.match(sdk['name'])
+        ]
+
+        # 1.9.14 > 1.9.13 so we need reverse order
+        python_sdks.sort(key=version_key, reverse=True)
+
+        # Newest listed versions are not immediately available to download.
+        # Check over HEAD.
+        for sdk in python_sdks:
+            url = str(sdk['mediaLink'])
+            try:
+                request = HeadRequest(url)
+                urllib2.urlopen(request)
+            except urllib2.HTTPError as e:
+                # 403, 401 - not yet published, try next one
+                if e.code not in (401, 403):
+                    raise
+            else:
+                return url
+        raise SDKCouldNotBeFound(
+            'Could not find a usable SDK version automatically'
+        )
---
+++ packages/appfy.recipe.gae/appfy.recipe.gae-0.9.10.tar.gz/appfy.recipe.gae-0.9.10/setup.py
@@ -0,0 +1,98 @@
+"""
+appfy.recipe.gae
+================
+
+.. image:: https://travis-ci.org/prmtl/appfy.recipe.gae.png?branch=master
+   :target: https://travis-ci.org/prmtl/appfy.recipe.gae
+
+`appfy.recipe.gae` provides a series of
+`zc.buildout <http://pypi.python.org/pypi/zc.buildout>`_
+recipes to help with `Google App Engine <http://code.google.com/appengine/>`_
+development. It is inspired by
+`rod.recipe.appengine <http://pypi.python.org/pypi/rod.recipe.appengine>`_,
+but using a different layout and with extended functionalities. It is also
+split in different recipes. Currently `appfy.recipe.gae` has 3 recipes:
+
+:appfy.recipe.gae\:app_lib: Downloads libraries from PyPi and installs in
+    the app directory.
+:appfy.recipe.gae\:sdk: Downloads and installs the App Engine SDK.
+:appfy.recipe.gae\:tools: Installs a python executable and several SDK
+    scripts in the buildout directory: appcfg, bulkload_client, bulkloader,
+    dev_appserver and remote_api_shell. It also allows to set default values
+    to start the dev_appserver.
+
+Source code and issue tracker can be found at
+`https://github.com/prmtl/appfy.recipe.gae
+<https://github.com/prmtl/appfy.recipe.gae>`_.
+
+For an example of how appfy makes distribution of App Engine apps easy and
+nice, see `Moe installation instructions <http://www.tipfy.org/wiki/moe/>`_.
+"""
+import os
+
+import setuptools
+
+
+def get_readme():
+    base = os.path.abspath(os.path.realpath(os.path.dirname(__file__)))
+    files = [
+        os.path.join(base, 'README.rst'),
+        os.path.join(base, 'CHANGES.txt'),
+    ]
+    content = []
+    for filename in files:
+        f = open(filename, 'r')
+        content.append(f.read().strip())
+        f.close()
+
+    return '\n\n\n'.join(content)
+
+
+tests_require = (
+    'hacking',
+    'nose',
+)
+
+
+setuptools.setup(
+    name='appfy.recipe.gae',
+    version='0.9.10',
+    author='Rodrigo Moraes',
+    author_email='rodrigo.moraes@gmail.com',
+    maintainer='Sebastian Kalinowski',
+    maintainer_email='sebastian@kalinowski.eu',
+    description='Buildout recipes for App Engine development.',
+    long_description=get_readme(),
+    license='Apache Software License',
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'setuptools',
+        'zc.buildout',
+        'zc.recipe.egg >= 2.0.0'
+    ],
+    entry_points={
+        'zc.buildout': [
+            'default = appfy.recipe.gae.tools:Recipe',
+            'tools = appfy.recipe.gae.tools:Recipe',
+            'sdk = appfy.recipe.gae.sdk:Recipe',
+            'app_lib = appfy.recipe.gae.app_lib:Recipe',
+        ],
+    },
+    zip_safe=False,
+    keywords=('buildout recipe google app engine appengine gae zc.buildout '
+              'appfy tipfy'),
+    url='https://github.com/prmtl/appfy.recipe.gae',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Framework :: Buildout',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Topic :: Software Development :: Build Tools',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    tests_require=tests_require,
+    extras_require={
+        'test': tests_require,
+    },
+    test_suite='nose.collector',
+)
