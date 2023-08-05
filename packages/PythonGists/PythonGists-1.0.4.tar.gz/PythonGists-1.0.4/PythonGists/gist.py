7ad1db1589108d4ec5d2952c77f6907eb2e10d1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:10 +0000

    Add package PythonGists/1.0.4/PythonGists-1.0.4.tar.gz

---
+++ packages/PythonGists/PythonGists-1.0.4.tar.gz/PythonGists-1.0.4/PythonGists/gist.py
@@ -0,0 +1,64 @@
+import requests
+
+try:
+	import simplejson as json
+except ImportError:
+	import json
+try:
+	from .access import main as login 
+
+except:
+	from access import main as login 
+
+API_URL='https://api.github.com'
+
+class GitHubGist(object):
+	def __init__(self,username,password):
+		self.accessToken=login(username,password)
+	
+	@staticmethod
+	def Gist(description,content,name,token=None):
+		public=True
+		url=API_URL+'/gists'
+
+		if token is None:
+			authtoken=None
+		else:
+			authtoken=token
+		token='token {0}'.format(authtoken)
+		data=json.dumps({"description":description,"public":public,"files":{name:{"content":content}}})
+
+		if authtoken is None:
+			r=requests.post(url,data=data)
+		else:
+			r=requests.post(url,headers={'Authorization':token},data=data)
+		
+		uniqueID=r.json()['url']
+		gistLink="http://gist.github.com/{0}".format(uniqueID.split('/')[-1])
+		return gistLink
+	@staticmethod
+	def GistFromFile(description,file):
+		with open(file,'r') as f:
+			content=f.read()
+		return GitHubGist.Gist(description,content,file)
+	def createGist(self,description,content,name):
+		return self.Gist(description,content,name,self.accessToken)
+	def createGistFromFile(self,description,file):
+		with open(file,'r') as f:
+			content=f.read()
+		return self.Gist(description,content,file,self.accessToken)
+	@staticmethod
+	def getGistsLinks(username):
+		url='{0}/users/{1}/gists'.format(API_URL,username)
+		data=requests.get(url).json()
+		return [a['url'] for a in data]
+	@staticmethod
+	def getGistsData(username):
+		url='{0}/users/{1}/gists'.format(API_URL,username)
+		data=requests.get(url).json()
+		return data
+		
+if __name__=='__main__':
+	
+	link = GitHubGist.GistFromFile('something','__init__.py')
+	print(link)
