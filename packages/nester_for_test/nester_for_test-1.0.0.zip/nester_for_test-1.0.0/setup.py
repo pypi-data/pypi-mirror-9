024606f98e3961b8adfd3e1c42bfa0e220827a98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:31 +0000

    Add package nester_for_test/1.0.0/nester_for_test-1.0.0.zip

---
+++ packages/nester_for_test/nester_for_test-1.0.0.zip/nester_for_test-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup (
+    name = 'nester_for_test',
+    version = '1.0.0',
+    py_modules = 'nester',
+    author='Fellow',
+    author_email='chao87644@gmail.com',
+    description='a simple printer of nested list'
+
+)
