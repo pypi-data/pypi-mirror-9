b1e0dee638112e9668bd947f1f814cdb44974984
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:03 +0000

    Add package django-db-file-storage/0.2.6/django-db-file-storage-0.2.6.tar.gz

---
+++ packages/django-db-file-storage/django-db-file-storage-0.2.6.tar.gz/django-db-file-storage-0.2.6/db_file_storage/storage.py
@@ -0,0 +1,104 @@
+# -*- coding: utf-8 -*-
+
+# python imports
+from cStringIO import StringIO
+from urllib import urlencode
+
+# django imports
+from django.core.files.storage import Storage
+from django.core.urlresolvers import reverse
+"""
+    As of Django 1.7, the utilities in django.db.models.loading are
+      deprecated (to be removed in 1.9) in favor of the the new
+      application loading system. Check here:
+      https://github.com/django/django/blob/1.7/django/db/models/loading.py
+"""
+from django import VERSION as DJ_VERSION
+if DJ_VERSION[0] <= 1 and DJ_VERSION[1] <= 6:
+    from django.db.models.loading import get_model
+else:
+    from django.apps import apps
+    get_model = apps.get_model
+
+
+def _get_model_class(model_class_path):
+    app_label, model_name = model_class_path.rsplit('.', 1)
+    return get_model(app_label, model_name)
+
+
+def _get_encoded_bytes_from_file(file):
+    file.seek(0)
+    return file.read().encode('base64')
+
+
+def _get_file_from_encoded_bytes(encoded_bytes):
+    file_buffer = str(encoded_bytes).decode('base64')
+    return StringIO(file_buffer)
+
+
+def _get_unique_filename(model_class, filename_field, filename):
+    final_name = filename
+
+    if ('.' in filename.rsplit('/', 1)[-1]):
+        stem, extension = final_name.rsplit('.', 1)
+    else:
+        stem, extension = (final_name, '')
+
+    append_to_name = 1
+    while model_class.objects.filter(**{filename_field: final_name}).exists():
+        final_name = '%s_(%s)%s' % (
+            stem,
+            append_to_name,
+            ('.%s' % extension) if extension else ''
+        )
+        append_to_name += 1
+    return final_name
+
+
+class DatabaseFileStorage(Storage):
+    def _open(self, name, mode='rb'):
+        (model_class_path, content_field, filename_field,
+            mimetype_field, filename) = name.split('/')
+        model_class = _get_model_class(model_class_path)
+        model_instance = model_class.objects.only(
+            content_field, mimetype_field
+        ).get(**{filename_field: name})
+        encoded_bytes = getattr(model_instance, content_field)
+
+        return {
+            'filename': filename,
+            'file': _get_file_from_encoded_bytes(encoded_bytes),
+            'mimetype': getattr(model_instance, mimetype_field)
+        }
+
+    def _save(self, name, content):
+        (model_class_path, content_field, filename_field,
+            mimetype_field, filename) = name.split('/')
+        model_class = _get_model_class(model_class_path)
+        new_filename = _get_unique_filename(model_class, filename_field, name)
+        encoded_bytes = _get_encoded_bytes_from_file(content)
+        mimetype = content.file.content_type
+        model_class.objects.create(**{
+            content_field: encoded_bytes,
+            filename_field: new_filename,
+            mimetype_field: mimetype,
+        })
+        return new_filename
+
+    def delete(self, name):
+        (model_class_path, content_field, filename_field,
+            mimetype_field, filename) = name.split('/')
+        model_class = _get_model_class(model_class_path)
+        model_class.objects.filter(**{filename_field: name}).delete()
+
+    def exists(self, name):
+        (model_class_path, content_field, filename_field,
+            mimetype_field, filename) = name.split('/')
+        model_class = _get_model_class(model_class_path)
+        return model_class.objects.filter(
+            **{filename_field: filename}
+        ).exists()
+
+    def url(self, name):
+        _url = reverse('db_file_storage.download_file')
+        return _url + '?' + urlencode({'name': name})
---
+++ packages/django-db-file-storage/django-db-file-storage-0.2.6.tar.gz/django-db-file-storage-0.2.6/setup.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+
+setup(
+    name = 'django-db-file-storage',
+    version = '0.2.6',
+    author = 'Victor Oliveira da Silva',
+    author_email = 'victor_o_silva@hotmail.com',
+    packages = ['db_file_storage', 'db_file_storage.test'],
+    url = 'http://pypi.python.org/pypi/django-db-file-storage/',
+    license = 'Creative Commons Attribution-Noncommercial-Share Alike license',
+    description = "Custom FILE_STORAGE for Django. Saves model's filefields in your database instead of your file system.",
+    long_description = open('README.txt').read(),
+    install_requires=[
+        "Django",
+    ],
+)
