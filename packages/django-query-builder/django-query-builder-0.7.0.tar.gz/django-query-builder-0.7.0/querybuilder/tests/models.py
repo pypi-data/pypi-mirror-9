67bb0b36f599fa590848b6e2a122fc422a664150
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:55 +0000

    Add package django-query-builder/0.7.0/django-query-builder-0.7.0.tar.gz

---
+++ packages/django-query-builder/django-query-builder-0.7.0.tar.gz/django-query-builder-0.7.0/querybuilder/tests/models.py
@@ -0,0 +1,37 @@
+from django.db import models
+from jsonfield import JSONField
+
+
+class User(models.Model):
+    """
+    User model
+    """
+    email = models.CharField(max_length=256)
+
+
+class Account(models.Model):
+    """
+    Account model
+    """
+    user = models.OneToOneField(User)
+    first_name = models.CharField(max_length=64)
+    last_name = models.CharField(max_length=64)
+
+
+class Order(models.Model):
+    """
+    Order model
+    """
+    account = models.ForeignKey(Account)
+    revenue = models.FloatField(null=True)
+    margin = models.FloatField()
+    margin_percent = models.FloatField()
+    time = models.DateTimeField()
+
+
+class MetricRecord(models.Model):
+    """
+    Example metric model
+    """
+    other_value = models.FloatField(default=0)
+    data = JSONField()
---
+++ packages/django-query-builder/django-query-builder-0.7.0.tar.gz/django-query-builder-0.7.0/setup.py
@@ -0,0 +1,58 @@
+# import multiprocessing to avoid this bug (http://bugs.python.org/issue15881#msg170215)
+import multiprocessing
+assert multiprocessing
+import re
+from setuptools import setup, find_packages
+
+
+def get_version():
+    """
+    Extracts the version number from the version.py file.
+    """
+    VERSION_FILE = 'querybuilder/version.py'
+    mo = re.search(r'^__version__ = [\'"]([^\'"]*)[\'"]', open(VERSION_FILE, 'rt').read(), re.M)
+    if mo:
+        return mo.group(1)
+    else:
+        raise RuntimeError('Unable to find version string in {0}.'.format(VERSION_FILE))
+
+
+setup(
+    name='django-query-builder',
+    version=get_version(),
+    description='Build complex nested queries',
+    long_description=open('README.rst').read(),
+    url='https://github.com/ambitioninc/django-query-builder',
+    author='Wes Okes',
+    author_email='wes.okes@gmail.com',
+    keywords='django, database, query',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Framework :: Django',
+        'Development Status :: 5 - Production/Stable',
+    ],
+    license='MIT',
+    install_requires=[
+        'django>=1.6',
+        'pytz>=2012h',
+        'fleming>=0.3',
+        'six',
+    ],
+    tests_require=[
+        'psycopg2',
+        'django-nose',
+        'mock==1.0.1',
+        'south',
+        'django-dynamic-fixture',
+        'jsonfield==0.9.20',
+    ],
+    test_suite='run_tests.run_tests',
+    include_package_data=True,
+)
