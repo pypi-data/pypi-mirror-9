a2cfc2d00f36946f72e4f71f27afea152a64aad8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:07 +0000

    Add package isochrones/0.5-beta/isochrones-0.5-beta.tar.gz

---
+++ packages/isochrones/isochrones-0.5-beta.tar.gz/isochrones-0.5-beta/isochrones/__init__.py
@@ -0,0 +1,9 @@
+__version__ = '0.5-beta'
+
+try:
+    __ISOCHRONES_SETUP__
+except NameError:
+    __ISOCHRONES_SETUP__ = False
+
+if not __ISOCHRONES_SETUP__:
+    __all__ = ['dartmouth','basti','padova']
---
+++ packages/isochrones/isochrones-0.5-beta.tar.gz/isochrones-0.5-beta/setup.py
@@ -0,0 +1,48 @@
+from setuptools import setup, find_packages
+import os,sys
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+# Hackishly inject a constant into builtins to enable importing of the
+# package before the library is built.
+import sys
+if sys.version_info[0] < 3:
+    import __builtin__ as builtins
+else:
+    import builtins
+builtins.__ISOCHRONES_SETUP__ = True
+import isochrones
+version = isochrones.__version__
+
+# Publish the library to PyPI.
+if "publish" in sys.argv[-1]:
+    os.system("python setup.py sdist upload")
+    sys.exit()
+
+# Push a new tag to GitHub.
+if "tag" in sys.argv:
+    os.system("git tag -a {0} -m 'version {0}'".format(version))
+    os.system("git push --tags")
+    sys.exit()
+
+setup(name = "isochrones",
+    version = version,
+    description = "Defines objects for interpolating stellar model grids.",
+    long_description = readme(),
+    author = "Timothy D. Morton",
+    author_email = "tim.morton@gmail.com",
+    url = "https://github.com/timothydmorton/isochrones",
+    packages = find_packages(),
+    package_data = {'':['data/*']},
+    classifiers=[
+      'Development Status :: 3 - Alpha',
+      'Intended Audience :: Science/Research',
+      'Operating System :: OS Independent',
+      'Programming Language :: Python',
+      'Topic :: Scientific/Engineering :: Astronomy'
+      ],
+    install_requires=['plotutils','pandas>=0.14','astropy>=0.3','emcee>=2.0'],
+    zip_safe=False
+) 
