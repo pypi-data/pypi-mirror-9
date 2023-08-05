efcde20be8444c185a5511724a42f72fc723e170
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:00 +0000

    Add package caelum/0.1.21.17/caelum-0.1.21.17.tar.gz

---
+++ packages/caelum/caelum-0.1.21.17.tar.gz/caelum-0.1.21.17/tests/unit.py
@@ -0,0 +1,24 @@
+import unittest
+class BaseEERETest(unittest.TestCase):
+    def runTest(self):
+        from caelum import eere
+        SCODE = '063800'
+        annual_ghi = sum([float(i['GHI (W/m^2)']) for i in eere.EPWdata(SCODE)])
+        SCODE = '724666'
+        annual_ghi2 = sum([float(i['GHI (W/m^2)']) for i in eere.EPWdata(SCODE)])
+        print annual_ghi2
+        self.assertAlmostEquals(annual_ghi, 985289)
+
+class eereDesign(unittest.TestCase):
+    def runTest(self):
+        from caelum import eere
+        SCODE = '063800'
+        self.assertAlmostEquals(eere.minimum(SCODE),-10.2)
+        self.assertAlmostEquals(eere.twopercent(SCODE),25.4)
+        SCODE = '724666'
+        self.assertAlmostEquals(eere.minimum(SCODE),-22.9)
+        self.assertAlmostEquals(eere.twopercent(SCODE),30.2)
+
+if __name__ == '__main__':
+    suite = unittest.TestLoader().loadTestsFromTestCase(BaseEERETest)
+    unittest.TextTestRunner(verbosity=2).run(suite)
