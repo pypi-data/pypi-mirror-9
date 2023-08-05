5d9897702802134c5f67e21a95a96e9d733a48da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:52 +0000

    Add package grace/0.3.13/grace-0.3.13.tar.gz

---
+++ packages/grace/grace-0.3.13.tar.gz/grace-0.3.13/grace/doc.py
@@ -0,0 +1,59 @@
+from error import CreateFolderError, FileNotWritableError, RemoveFolderError
+import pyjsdoc
+import os
+from shutil import rmtree
+
+
+class Doc(object):
+    def __init__(self, config):
+        self._cwd = os.getcwd()
+        self._config = config
+
+        if 'doc_path' not in self._config:
+            self._doc_path = None
+        else:
+            self._doc_path = os.path.join(self._config['doc_path'], self._config['name'], 'JSDoc')
+
+        self._lib_path = os.path.join(self._cwd, 'src', 'lib')
+
+    def run(self, with_libs=False):
+        source = os.path.join(self._cwd, 'src', 'javascript')
+        dest = os.path.join(self._cwd, 'build', 'JSDocs')
+
+        if not os.path.exists(self._config['build_path']):
+            try:
+                os.makedirs(self._config['build_path'])
+            except:
+                raise CreateFolderError('Could not create the project folder.')
+
+        if os.path.exists(dest):
+            try:
+                rmtree(dest)
+            except:
+                raise RemoveFolderError('Could not remove the JSDoc folder.')
+
+        if with_libs:
+            jsdoc = pyjsdoc.CodeBaseDoc([source, self._lib_path])
+        else:
+            jsdoc = pyjsdoc.CodeBaseDoc([source])
+
+        try:
+            os.makedirs(dest)
+        except:
+            raise CreateFolderError('Could not create the doc folder in your build directory.')
+
+        try:
+            jsdoc.save_docs(output_dir=dest)
+        except:
+            raise FileNotWritableError('Could not write the docs to into the build directory.')
+
+        if self._doc_path is not None:
+            try:
+                os.makedirs(self._doc_path)
+            except:
+                raise CreateFolderError('Could not create the doc folder at "' + self._doc_path + '".')
+
+            try:
+                jsdoc.save_docs(output_dir=self._doc_path)
+            except:
+                raise FileNotWritableError('Could not write the docs to target directory (' + self._doc_path + ').')
