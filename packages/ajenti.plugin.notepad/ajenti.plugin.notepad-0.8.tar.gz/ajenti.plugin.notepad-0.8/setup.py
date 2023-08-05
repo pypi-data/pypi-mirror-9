3e7ec95b73ef8308b827e484c360643117f03efd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:11 +0000

    Add package ajenti.plugin.notepad/0.8/ajenti.plugin.notepad-0.8.tar.gz

---
+++ packages/ajenti.plugin.notepad/ajenti.plugin.notepad-0.8.tar.gz/ajenti.plugin.notepad-0.8/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+import os
+
+__requires = filter(None, open('requirements.txt').read().splitlines())
+
+setup(
+    name='ajenti.plugin.notepad',
+    version='0.8',
+    install_requires=__requires,
+    description='Notepad',
+    long_description='A Notepad plugin for Ajenti panel',
+    author='Ajenti project',
+    author_email='e@ajenti.org',
+    url='http://ajenti.org',
+    packages=find_packages(),
+    include_package_data=True,
+)
