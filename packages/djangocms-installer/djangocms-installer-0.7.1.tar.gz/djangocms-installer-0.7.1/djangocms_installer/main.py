caf9dfcbe8ba2e0d3af02838c2b924ebbbd39746
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:55 +0000

    Add package djangocms-installer/0.7.1/djangocms-installer-0.7.1.tar.gz

---
+++ packages/djangocms-installer/djangocms-installer-0.7.1.tar.gz/djangocms-installer-0.7.1/djangocms_installer/main.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+from distutils.util import strtobool
+import logging
+import os
+import shutil
+import six
+import sys
+
+
+from . import config, install, django
+
+
+def execute():
+    # Log info and above to console
+    logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.INFO)
+
+    config_data = config.parse(sys.argv[1:])
+    try:
+        if config_data.plugins:
+            config.show_plugins()
+        elif config_data.dump_reqs:
+            config.show_requirements(config_data)
+        else:
+            if not config_data.no_deps:
+                if config_data.requirements_file:
+                    install.requirements(config_data.requirements_file, True)
+                else:
+                    install.requirements(config_data.requirements)
+            install.check_install(config_data)
+            django.create_project(config_data)
+            django.patch_settings(config_data)
+            django.copy_files(config_data)
+            if not config_data.no_sync:
+                django.setup_database(config_data)
+            if config_data.starting_page:
+                django.load_starting_page(config_data)
+            if config_data.aldryn:
+                sys.stdout.write("Project created!\n")
+                sys.stdout.write("aldryn boilerplate requires action before you can actually run "
+                                 "the project.\n"
+                                 "See documentation at http://aldryn-boilerplate.readthedocs.org/"
+                                 "for more information.\n")
+            else:
+                sys.stdout.write("All done!\n")
+                sys.stdout.write("Get into '%s' directory and type 'python manage.py runserver' "
+                                 "to start your project\n" % os.path.abspath(config_data.project_directory))
+    except Exception as e:
+        # Clean up your own mess
+        if os.path.exists(config_data.project_directory):
+            choice = 'N'
+            if config_data.noinput is False:
+                sys.stdout.write("Failure occurred. Do you want to cleanup by removing %s? "
+                                 "[Y/N] " % os.path.abspath(config_data.project_directory))
+                choice = raw_input().lower()
+            else:
+                sys.stdout.write("Failure occurred.\n")
+            if strtobool(choice) or config_data.noinput:
+                sys.stdout.write("Removing everything under %s\n"
+                                 "" % os.path.abspath(config_data.project_directory))
+                shutil.rmtree(config_data.project_directory, True)
+        if six.PY3:
+            tb = sys.exc_info()[2]
+            raise EnvironmentError("%s\nDocumentation available at "
+                                   "http://djangocms-installer.rtfd.org\n" % e).with_traceback(tb)
+        else:
+            raise
