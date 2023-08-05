4ec7d90441522a5a4f3579ee08b33265952a6a8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:17 +0000

    Add package django-picker/0.2.1/django-picker-0.2.1.zip

---
+++ packages/django-picker/django-picker-0.2.1.zip/django-picker-0.2.1/picker/templatetags/pickertags.py
@@ -0,0 +1,40 @@
+from __future__ import unicode_literals
+
+from django.utils.html import escapejs, format_html
+from django.template.loader import get_template
+from django.template.context import Context
+from django.template import Library
+from django.conf import settings
+from picker import settings as picker_settings
+
+register = Library()
+
+
+@register.simple_tag
+def load_js():
+    if 'django.contrib.staticfiles' in settings.INSTALLED_APPS:
+        from django.contrib.staticfiles.templatetags.staticfiles import static
+    else:
+        from django.templatetags.static import static
+
+    js_html = ''
+    for js in picker_settings.PICKER_JS:
+        file_path = static('picker/%s' % js)
+        scripts = '<script type="text/javascript" src="%s"></script>' % file_path
+        js_html += format_html(scripts)
+    return js_html
+
+
+@register.simple_tag
+def load_css():
+    if 'django.contrib.staticfiles' in settings.INSTALLED_APPS:
+        from django.contrib.staticfiles.templatetags.staticfiles import static
+    else:
+        from django.templatetags.static import static
+
+    css_html = ''
+    for css in picker_settings.PICKER_CSS:
+        file_path = static('picker/%s' % css)
+        scripts = '<link rel="stylesheet" type="text/css" href="%s">' % file_path
+        css_html += format_html(scripts)
+    return css_html
---
+++ packages/django-picker/django-picker-0.2.1.zip/django-picker-0.2.1/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# from distutils.core import setup
+import os
+import sys
+from setuptools import setup, find_packages
+from distutils.sysconfig import get_python_lib
+
+version = '0.2.1'
+
+setup(
+    name='django-picker',
+    version=version,
+    author=u'ShinZ Natkid',
+    author_email='shinznatkid@gmail.com',
+    packages=find_packages(),
+    url='http://github.com/shinznatkid/picker',
+    license='BSD',
+    include_package_data=True,
+    description='Put javascript, css framework into your project.',
+    long_description=open('README.md').read(),
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python :: 2.7',
+    ]
+)
