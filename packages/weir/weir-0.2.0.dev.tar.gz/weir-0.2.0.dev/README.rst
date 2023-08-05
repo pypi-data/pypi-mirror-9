3fcf071ed9ed31cf158ea09ed59816dec0e2f153
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:21 +0000

    Add package weir/0.2.0.dev/weir-0.2.0.dev.tar.gz

---
+++ packages/weir/weir-0.2.0.dev.tar.gz/weir-0.2.0.dev/README.rst
@@ -0,0 +1,29 @@
+Weir
+====
+Weir is a Python API for ZFS.
+
+Motivation / rationale
+----------------------
+(combine with description?)
+
+- goals:
+
+  - api based on libzfs, but object-oriented, pythonic?
+
+  - remote operation
+
+Installation
+------------
+Requires Python 2.7.
+
+To install Weir, simply:
+
+``$ pip install weir``
+
+Usage
+-----
+...
+
+License
+-------
+Licensed under the Common Development and Distribution License (CDDL).
---
+++ packages/weir/weir-0.2.0.dev.tar.gz/weir-0.2.0.dev/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='weir',
+    version='0.2.0.dev',
+    description='A Python API for ZFS',
+    long_description=long_description,
+
+    url='https://bitbucket.org/sjdrake/weir',
+    author='Stephen Drake',
+    author_email='steve@synergyconsultingnz.com',
+
+    license='Common Development and Distribution License (CDDL)',
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+    ],
+    keywords='zfs',
+
+    packages=['weir'],
+)
