c4eefe3b966b0561d8fb53617db91f93bf93fbd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:49 +0000

    Add package docker-harpoon/0.5.3.5/docker-harpoon-0.5.3.5.tar.gz

---
+++ packages/docker-harpoon/docker-harpoon-0.5.3.5.tar.gz/docker-harpoon-0.5.3.5/harpoon/executor.py
@@ -0,0 +1,264 @@
+"""
+This is where the mainline sits and is responsible for setting up the logging,
+the argument parsing and for starting up Harpoon.
+"""
+
+from __future__ import print_function
+
+from harpoon.errors import BadOption, BadDockerConnection
+from harpoon.overview import Overview
+
+from rainbow_logging_handler import RainbowLoggingHandler
+from input_algorithms.spec_base import NotSpecified
+from docker.client import Client as DockerClient
+from delfick_error import DelfickError
+import requests
+import argparse
+import logging
+import docker
+import ssl
+import sys
+import os
+
+log = logging.getLogger("harpoon.executor")
+
+def setup_logging(verbose=False, silent=False, debug=False):
+    log = logging.getLogger("")
+    handler = RainbowLoggingHandler(sys.stderr)
+    handler._column_color['%(asctime)s'] = ('cyan', None, False)
+    handler._column_color['%(levelname)-7s'] = ('green', None, False)
+    handler._column_color['%(message)s'][logging.INFO] = ('blue', None, False)
+    handler.setFormatter(logging.Formatter("%(asctime)s %(levelname)-7s %(name)-15s %(message)s"))
+    log.addHandler(handler)
+    log.setLevel([logging.INFO, logging.DEBUG][verbose or debug])
+    if silent:
+        log.setLevel(logging.ERROR)
+
+    logging.getLogger("requests").setLevel([logging.CRITICAL, logging.ERROR][verbose or debug])
+    return handler
+
+class CliParser(object):
+    """Knows what argv looks like"""
+    def parse_args(self, argv=None):
+        """Split the args into <args> -- <extra_args> and run <args> through our argparse.ArgumentParser"""
+        if argv is None:
+            argv = sys.argv[1:]
+
+        argv = list(argv)
+        args = []
+        extras = None
+        default_task = NotSpecified
+        default_image = NotSpecified
+
+        if argv:
+            if not argv[0].startswith("-"):
+                default_task = argv[0]
+                argv.pop(0)
+
+            if argv and not argv[0].startswith("-"):
+                default_image = argv[0]
+                argv.pop(0)
+
+        while argv:
+            nxt = argv.pop(0)
+            if extras is not None:
+                extras.append(nxt)
+            elif nxt == "--":
+                extras = []
+            else:
+                args.append(nxt)
+
+        other_args = ""
+        if extras:
+            other_args = " ".join(extras)
+
+        parser = self.make_parser(default_task=default_task, default_image=default_image)
+        args = parser.parse_args(args)
+        if default_task is not NotSpecified and args.harpoon_chosen_task != default_task:
+            raise BadOption("Please don't specify task as a positional argument and as a --task option", positional=default_task, kwarg=args.task)
+        if default_image is not NotSpecified and args.harpoon_chosen_image != default_image:
+            raise BadOption("Please don't specify image as a positional argument and as a --image option", positional=default_image, kwargs=args.image)
+
+        return args, other_args
+
+    def make_parser(self, default_task=NotSpecified, default_image=NotSpecified):
+        parser = argparse.ArgumentParser(description="Opinionated layer around docker")
+
+        logging = parser.add_mutually_exclusive_group()
+        logging.add_argument("--verbose"
+            , help = "Enable debug logging"
+            , action = "store_true"
+            )
+
+        logging.add_argument("--silent"
+            , help = "Only log errors"
+            , action = "store_true"
+            )
+
+        logging.add_argument("--debug"
+            , help = "Debug logs"
+            , action = "store_true"
+            )
+
+        opts = {}
+        if os.path.exists("./harpoon.yml"):
+            opts["default"] = "./harpoon.yml"
+            opts["required"] = False
+        else:
+            opts["required"] = True
+
+        if "HARPOON_CONFIG" in os.environ:
+            opts["default"] = os.environ["HARPOON_CONFIG"]
+            del opts["required"]
+        parser.add_argument("--harpoon-config"
+            , help = "The config file specifying what harpoon should care about"
+            , type = argparse.FileType("r")
+            , **opts
+            )
+
+        extra = {"default": "list_tasks"}
+        if default_task is not NotSpecified:
+            extra["default"] = default_task
+        parser.add_argument("--task"
+            , help = "The task to run"
+            , dest = "harpoon_chosen_task"
+            , **extra
+            )
+
+        parser.add_argument("--non-interactive"
+            , help = "Make this non interactive"
+            , dest = "harpoon_interactive"
+            , action = "store_false"
+            )
+
+        extra = {"default": ""}
+        if default_image is not NotSpecified:
+            extra["default"] = default_image
+        parser.add_argument("--image"
+            , help = "Specify a particular image"
+            , dest = "harpoon_chosen_image"
+            , **extra
+            )
+
+        command = parser.add_mutually_exclusive_group()
+
+        command.add_argument("--command"
+            , help = "Specify a command to run for tasks that need one"
+            )
+
+        command.add_argument("--bash"
+            , help = "Specify a command that will be ran as /bin/bash -c '<command>'"
+            )
+
+        parser.add_argument("--silent-build"
+            , help = "Make the build process quiet"
+            , dest = "harpoon_silent_build"
+            , action = "store_true"
+            )
+
+        parser.add_argument("--keep-replaced"
+            , help = "Don't delete images that have their tag stolen by a new image"
+            , dest = "harpoon_keep_replaced"
+            , action = "store_true"
+            )
+
+        parser.add_argument("--no-intervention"
+            , help = "Don't ask to intervene broken builds"
+            , dest = "harpoon_no_intervention"
+            , action = "store_true"
+            )
+
+        parser.add_argument("--env"
+            , help = "Environment option to start the container with"
+            , dest = "extra_env"
+            , action = "append"
+            )
+
+        parser.add_argument("--port"
+            , help = "Specify a port to publish in the running container you make"
+            , dest = "extra_ports"
+            , action = "append"
+            )
+
+        parser.add_argument("--flat"
+            , help = "Used with the show command"
+            , dest = "harpoon_flat"
+            , action = "store_true"
+            )
+
+        parser.add_argument("--ignore-missing"
+            , help = "Used by the pull commands to ignore if an image doesn't exist"
+            , dest = "harpoon_ignore_missing"
+            , action = "store_true"
+            )
+
+        return parser
+
+    def interpret_args(self, argv, no_docker=False):
+        """Parse argv, do some transformation and return cli_args suitable for Overview"""
+        args, extra = CliParser().parse_args(argv)
+
+        cli_args = {"harpoon": {}}
+        for key, val in sorted(vars(args).items()):
+            if key.startswith("harpoon_"):
+                cli_args["harpoon"][key[8:]] = val
+            else:
+                cli_args[key] = val
+        cli_args["harpoon"]["extra"] = extra
+
+        if not no_docker:
+            cli_args["harpoon"]["docker_context"] = docker_context()
+
+        for key in ('bash', 'command'):
+            if cli_args[key] is None:
+                cli_args[key] = NotSpecified
+
+        return args, cli_args
+
+def docker_context():
+    """Make a docker context"""
+    host = os.environ.get('DOCKER_HOST')
+    cert_path = os.environ.get('DOCKER_CERT_PATH')
+    tls_verify = os.environ.get('DOCKER_TLS_VERIFY')
+
+    options = {"timeout": 60}
+    if host:
+        options['base_url'] = (host.replace('tcp://', 'https://') if tls_verify else host)
+
+    if tls_verify and cert_path:
+        options['tls'] = docker.tls.TLSConfig(
+              verify = True
+            , ca_cert = os.path.join(cert_path, 'ca.pem')
+            , client_cert = (os.path.join(cert_path, 'cert.pem'), os.path.join(cert_path, 'key.pem'))
+            , ssl_version = ssl.PROTOCOL_TLSv1
+            , assert_hostname = False
+            )
+
+    client = DockerClient(**options)
+    try:
+        info = client.info()
+        log.info("Connected to docker daemon\tdriver=%s\tkernel=%s", info["Driver"], info["KernelVersion"])
+    except (requests.exceptions.ConnectionError, requests.exceptions.Timeout) as error:
+        raise BadDockerConnection(base_url=options['base_url'], error=error)
+    return client
+
+def main(argv=None):
+    try:
+        args, cli_args = CliParser().interpret_args(argv)
+        handler = setup_logging(verbose=args.verbose, silent=args.silent, debug=args.debug)
+        Overview(configuration_file=args.harpoon_config.name, logging_handler=handler).start(cli_args)
+    except DelfickError as error:
+        print("")
+        print("!" * 80)
+        print("Something went wrong! -- {0}".format(error.__class__.__name__))
+        print("\t{0}".format(error))
+        if CliParser().parse_args(argv)[0].debug:
+            raise
+        sys.exit(1)
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        pass
+
---
+++ packages/docker-harpoon/docker-harpoon-0.5.3.5.tar.gz/docker-harpoon-0.5.3.5/harpoon/option_spec/harpoon_specs.py
@@ -0,0 +1,209 @@
+"""
+Here we define the yaml specification for Harpoon options, task options and image
+options.
+
+The specifications are responsible for sanitation, validation and normalisation.
+"""
+
+from input_algorithms.spec_base import (
+      create_spec, defaulted, string_choice_spec
+    , dictionary_spec, string_spec, valid_string_spec
+    , listof, optional_spec, or_spec, any_spec
+    , directory_spec, filename_spec, file_spec
+    , boolean, required, formatted, overridden
+    , integer_spec, dictof, dict_from_bool_spec
+    , container_spec
+    )
+
+from harpoon.option_spec.command_specs import command_spec
+from harpoon.formatter import MergedOptionStringFormatter
+from harpoon.option_spec.command_objs import Commands
+from harpoon.helpers import memoized_property
+from harpoon.option_spec import task_objs
+
+from input_algorithms.dictobj import dictobj
+from input_algorithms import validators
+
+import time
+import six
+
+class Harpoon(dictobj):
+    fields = [
+          "config", "chosen_image", "chosen_task", "flat", "silent_build"
+        , "extra", "no_intervention", "ignore_missing", "keep_replaced"
+        , "interactive", "do_push", "only_pushable", "docker_context"
+        ]
+
+class other_options(dictobj):
+    fields = ["run", "create", "build"]
+
+class HarpoonSpec(object):
+    """Knows about harpoon specific configuration"""
+
+    @memoized_property
+    def image_name_spec(self):
+        """
+        Image names are constrained by what docker wants
+
+        And by the fact that option_merge means we can't have keys with dots in them.
+        Otherwise if we have something like "ubuntu14.04" as an image, then when we do
+        {images.ubuntu14.04.image_name} it'll look for config["images"]["ubuntu14"]["04"]["image_name"]
+        instead of config["images"]["ubuntu14.04"]["image_name"] which is unlikely to be the
+        desired result.
+        """
+        return valid_string_spec(
+              validators.no_whitespace()
+            , validators.regexed("^[a-zA-Z][a-zA-Z0-9-_\.]*$")
+            )
+
+    @memoized_property
+    def task_name_spec(self):
+        """Just needs to be ascii"""
+        return valid_string_spec(
+              validators.no_whitespace()
+            , validators.regexed("^[a-zA-Z][a-zA-Z0-9-_\.]*$")
+            )
+
+    @memoized_property
+    def container_name_spec(self):
+        """Just needs to be ascii"""
+        return valid_string_spec(
+              validators.no_whitespace()
+            , validators.regexed("^[a-zA-Z][a-zA-Z0-9-_\.]*$")
+            )
+
+    def tasks_spec(self, available_actions, default_action="run"):
+        """Tasks for a particular image"""
+        return dictof(
+              self.task_name_spec
+            , create_spec(task_objs.Task, validators.deprecated_key("spec", "Use ``action`` and ``options`` instead (note that ``action`` defaults to run)")
+                , action = defaulted(string_choice_spec(available_actions, "No such task"), default_action)
+                , options = dictionary_spec()
+                , overrides = dictionary_spec()
+                , description = string_spec()
+                )
+            )
+
+    @memoized_property
+    def image_spec(self):
+        """Spec for each image"""
+        from harpoon.option_spec import image_specs as specs
+        from harpoon.option_spec import image_objs
+        return create_spec(image_objs.Image
+            # Change the context options
+            , validators.deprecated_key("exclude_context", "Use ``context.exclude``")
+            , validators.deprecated_key("use_git_timestamps", "Use ``context.use_git_timestamps``")
+            , validators.deprecated_key("respect_gitignore", "Use ``context.use_gitignore``")
+            , validators.deprecated_key("parent_dir", "Use ``context.parent_dir``")
+
+            # Changed how volumes_from works
+            , validators.deprecated_key("volumes_from", "Use ``volumes.share_with``")
+
+            # Deprecated link
+            , validators.deprecated_key("link", "Use ``links``")
+
+            # Harpoon options
+            , harpoon = any_spec()
+
+            # default the name to the key of the image
+            , name = formatted(defaulted(string_spec(), "{_key_name_1}"), formatter=MergedOptionStringFormatter)
+            , key_name = formatted(overridden("{_key_name_1}"), formatter=MergedOptionStringFormatter)
+            , image_name = optional_spec(string_spec())
+            , image_index = defaulted(string_spec(), "")
+            , container_name = optional_spec(string_spec())
+            , image_name_prefix = defaulted(string_spec(), "")
+
+            , user = defaulted(string_spec(), None)
+            , mtime = defaulted(any_spec(), time.time())
+            , configuration = any_spec()
+
+            # The spec itself
+            , bash = optional_spec(formatted(string_spec(), formatter=MergedOptionStringFormatter))
+            , command = optional_spec(formatted(string_spec(), formatter=MergedOptionStringFormatter))
+            , commands = required(container_spec(Commands, listof(command_spec())))
+            , links = listof(specs.link_spec(), expect=image_objs.Link)
+
+            , context = dict_from_bool_spec(lambda meta, val: {"enabled": val}
+                , create_spec(image_objs.Context
+                    , include = listof(string_spec())
+                    , exclude = listof(string_spec())
+                    , enabled = defaulted(boolean(), True)
+
+                    , parent_dir = directory_spec(formatted(defaulted(string_spec(), "{config_root}"), formatter=MergedOptionStringFormatter))
+                    , use_gitignore = defaulted(boolean(), False)
+                    , use_git_timestamps = defaulted(or_spec(boolean(), listof(string_spec())), False)
+                    )
+                )
+
+            , lxc_conf = defaulted(filename_spec(), None)
+
+            , volumes = create_spec(image_objs.Volumes
+                , mount = listof(specs.mount_spec(), expect=image_objs.Mount)
+                , share_with = listof(formatted(string_spec(), MergedOptionStringFormatter, expected_type=image_objs.Image))
+                )
+
+            , dependency_options = dictof(self.image_name_spec
+                , create_spec(image_objs.DependencyOptions
+                  , attached = defaulted(boolean(), False)
+                  )
+                )
+
+            , env = listof(specs.env_spec(), expect=image_objs.Environment)
+            , ports = listof(specs.port_spec(), expect=image_objs.Port)
+
+            , other_options = create_spec(other_options
+                , run = dictionary_spec()
+                , build = dictionary_spec()
+                , create = dictionary_spec()
+                )
+
+            , network = create_spec(image_objs.Network
+                , dns = defaulted(listof(string_spec()), None)
+                , mode = defaulted(string_spec(), None)
+                , hostname = defaulted(string_spec(), None)
+                , domainname = defaulted(string_spec(), None)
+                , disabled = defaulted(boolean(), False)
+                , dns_search = defaulted(listof(string_spec()), None)
+                , extra_hosts = optional_spec(listof(string_spec(), None))
+                , publish_all_ports = defaulted(boolean(), False)
+                )
+
+            , cpu = create_spec(image_objs.Cpu
+                , cap_add = defaulted(boolean(), None)
+                , cpuset = defaulted(listof(string_spec()), None)
+                , cap_drop = defaulted(boolean(), None)
+                , mem_limit = defaulted(integer_spec(), 0)
+                , cpu_shares = defaulted(integer_spec(), None)
+                , memswap_limit = defaulted(integer_spec(), 0)
+                )
+
+            , devices = defaulted(listof(string_spec()), None)
+            , privileged = defaulted(boolean(), False)
+            , restart_policy = defaulted(string_spec(), None)
+            )
+
+    @memoized_property
+    def harpoon_spec(self):
+        """Spec for harpoon options"""
+        formatted_string = formatted(string_spec(), MergedOptionStringFormatter, expected_type=six.string_types)
+        formatted_boolean = formatted(boolean(), MergedOptionStringFormatter, expected_type=bool)
+
+        return create_spec(Harpoon
+            , config = file_spec()
+
+            , extra = defaulted(formatted_string, "")
+            , chosen_task = defaulted(formatted_string, "list_tasks")
+            , chosen_image = defaulted(formatted_string, "")
+
+            , flat = defaulted(formatted_boolean, False)
+            , interactive = defaulted(formatted_boolean, True)
+            , silent_build = defaulted(formatted_boolean, False)
+            , keep_replaced = defaulted(formatted_boolean, False)
+            , ignore_missing = defaulted(formatted_boolean, False)
+            , no_intervention = defaulted(formatted_boolean, False)
+
+            , do_push = defaulted(formatted_boolean, False)
+            , only_pushable = defaulted(formatted_boolean, False)
+            , docker_context = any_spec()
+            )
+
