a9138cd40d50de658995743b621b25e3339f9eb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:50 +0000

    Add package chatpy/0.2.2/chatpy-0.2.2-py2.7.egg

---
+++ packages/chatpy/chatpy-0.2.2-py2.7.egg/chatpy/__init__.py
@@ -0,0 +1,25 @@
+# chatpy
+# Copyright 2013-2015 aqn
+# the original source code is written by Joshua Roesslein (Tweepy)
+# See LICENSE for details.
+
+"""
+Chatwork API library
+"""
+__version__ = '0.2.2'
+__license__ = 'MIT'
+
+from chatpy.models import Status, ModelFactory
+from chatpy.error import ChatpyError
+from chatpy.api import API
+from chatpy.cache import Cache, MemoryCache, FileCache
+from chatpy.auth import TokenAuthHandler
+
+# Global, unauthenticated instance of API
+api = API()
+
+
+def debug(level=1):
+    from six.moves import http_client
+    http_client.HTTPConnection.debuglevel = level
+
