c5efc0c6896745168fccc95ebcd6fa8a9e909ac2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:13 +0000

    Add package td-client/0.1.4/td-client-0.1.4.tar.gz

---
+++ packages/td-client/td-client-0.1.4.tar.gz/td-client-0.1.4/tdclient/partial_delete_api.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+from __future__ import unicode_literals
+
+try:
+    from urllib.parse import quote as urlquote # >=3.0
+except ImportError:
+    from urllib import quote as urlquote
+
+class PartialDeleteAPI(object):
+    ####
+    ## Partial delete API
+    ##
+
+    def partial_delete(self, db, table, to, _from, params={}):
+        """
+        TODO: add docstring
+        """
+        params["to"] = str(to)
+        params["from"] = str(_from)
+        with self.post("/v3/table/partialdelete/%s/%s" % (urlquote(str(db)), urlquote(str(table))), params) as res:
+            code, body = res.status, res.read()
+            if code != 200:
+                self.raise_error("Partial delete failed", res, body)
+            js = self.checked_json(body, ["job_id"])
+            return str(js["job_id"])
