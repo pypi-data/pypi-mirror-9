fe96f00ae09cdc7d20ec92e994a5c6cf5edbef29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:49 +0000

    Add package ocflib/4/ocflib-4.tar.gz

---
+++ packages/ocflib/ocflib-4.tar.gz/ocflib-4/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+VERSION = 4
+
+setup(
+    name='ocflib',
+    version=VERSION,
+    author="Open Computing Facility",
+    author_email='help@ocf.berkeley.edu',
+    description="libraries for account and server management",
+    url='https://www.ocf.berkeley.edu',
+    packages=find_packages(),
+    install_requires=(
+        'paramiko',
+        'cracklib',
+        'pexpect',
+        'dnspython3'
+    )
+)
