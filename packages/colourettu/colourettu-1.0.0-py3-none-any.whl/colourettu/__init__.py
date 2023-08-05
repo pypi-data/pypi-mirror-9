4f0d8f93265c72739edb8f591a8c54738362df12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:01 +0000

    Add package colourettu/1.0.0/colourettu-1.0.0-py3-none-any.whl

---
+++ packages/colourettu/colourettu-1.0.0-py3-none-any.whl/colourettu/__init__.py
@@ -0,0 +1,12 @@
+from __future__ import absolute_import
+
+from ._colour import colour
+from ._colour import _luminance as luminance
+from ._colour import _contrast as contrast
+
+from ._colour import _A_contrast as A_contrast
+from ._colour import _AA_contrast as AA_contrast
+from ._colour import _AAA_contrast as AAA_contrast
+
+
+__version__ = '1.0.0'
