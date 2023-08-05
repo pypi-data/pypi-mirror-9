18a9ca36c7e1dfd510881cb1d7b909f0b2335a76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:23 +0000

    Add package rainbowstream/1.2.6/rainbowstream-1.2.6.tar.gz

---
+++ packages/rainbowstream/rainbowstream-1.2.6.tar.gz/rainbowstream-1.2.6/setup.py
@@ -0,0 +1,60 @@
+from setuptools import setup, find_packages
+import os
+import os.path
+
+# Bumped version
+version = '1.2.6'
+
+# Require
+install_requires = [
+    "python-dateutil",
+    "arrow",
+    "requests",
+    "pyfiglet",
+    "twitter",
+    "Pillow",
+    "PySocks"
+]
+
+# Copy default config if not exists
+default = os.path.expanduser("~") + os.sep + '.rainbow_config.json'
+if not os.path.isfile(default):
+    cmd = 'cp rainbowstream/colorset/config ' + default
+    os.system(cmd)
+    cmd = 'chmod 777 ' + default
+    os.system(cmd)
+
+# Setup
+setup(name='rainbowstream',
+      version=version,
+      description="A smart and nice Twitter client on terminal.",
+      long_description=open("./README.rst", "r").read(),
+      classifiers=[
+          "Development Status :: 5 - Production/Stable",
+          "Environment :: Console",
+          "Intended Audience :: End Users/Desktop",
+          "Natural Language :: English",
+          "Operating System :: OS Independent",
+          "Programming Language :: Python :: 2.7",
+          "Programming Language :: Python :: 3.2",
+          "Programming Language :: Python :: 3.3",
+          "Programming Language :: Python :: 3.4",
+          "Topic :: Internet :: WWW/HTTP :: Dynamic Content :: CGI Tools/Libraries",
+          "Topic :: Utilities",
+          "License :: OSI Approved :: MIT License",
+      ],
+      keywords='twitter, command-line tools, stream API',
+      author='Vu Nhat Minh',
+      author_email='nhatminh179@gmail.com',
+      url='http://www.rainbowstream.org/',
+      license='MIT License',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=True,
+      install_requires=install_requires,
+      entry_points="""
+      # -*- Entry points: -*-
+      [console_scripts]
+      rainbowstream=rainbowstream.rainbow:fly
+      """,
+      )
