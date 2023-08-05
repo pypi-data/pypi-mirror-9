b7f555855e9a45d9e1b0b5fc2dcae1462de8bf45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:35 +0000

    Add package dsmsfilepackager/0.1.8/dsmsfilepackager-0.1.8.tar.gz

---
+++ packages/dsmsfilepackager/dsmsfilepackager-0.1.8.tar.gz/dsmsfilepackager-0.1.8/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='dsmsfilepackager',
+    version='0.1.8',
+    author='Chris Horsley',
+    author_email='chris.horsley@csirtfoundry.com',
+    packages=['dsmsfilepackager'],
+    scripts=[],
+    url='http://bitbucket.org/dsms/dsmsfilepackager/',
+    license='LICENSE.txt',
+    description='',
+    long_description=open('README').read(),
+    install_requires=[
+        "sftpsyncer==0.1.7",
+        "python-magic"
+    ],
+)
