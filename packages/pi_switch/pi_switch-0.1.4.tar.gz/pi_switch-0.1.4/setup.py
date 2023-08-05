6c4697fb4cceb26663035ea86e7ec952e1bb9025
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:14 +0000

    Add package pi_switch/0.1.4/pi_switch-0.1.4.tar.gz

---
+++ packages/pi_switch/pi_switch-0.1.4.tar.gz/pi_switch-0.1.4/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+from distutils.extension import Extension
+
+setup(
+    name="pi_switch",
+    packages = [ "pi_switch" ],
+    version = "0.1.4",
+    description = "A library for the raspberry pi to control remote power outlet sockets.",
+    author = "Alexander Rueedlinger",
+    author_email = "a.rueedlinger@gmail.com",
+    url = "https://github.com/lexruee/pi-switch-python",
+    download_url = "https://github.com/lexruee/pi-switch-python/tarball/0.1",
+    keywords = ['switch', 'outlet sockets', 'control', 'remote', 'raspberry pi'],
+    classifiers = [
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)"
+    ],
+    ext_modules = [
+        Extension("pi_switch_wrapper",
+                    sources = [
+                        "pi_switch/wrapper/pi_switch.cpp"
+                    ],
+                    libraries = ["boost_python", "wiringPi"])
+    ],
+    py_modules=["pi_switch"],
+    install_requires = [],
+    )
