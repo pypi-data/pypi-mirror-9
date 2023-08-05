309f89a719f10df0856a9785835b4c055535d3a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:33 +0000

    Add package walrus/0.1.3/walrus-0.1.3.tar.gz

---
+++ packages/walrus/walrus-0.1.3.tar.gz/walrus-0.1.3/walrus/__init__.py
@@ -0,0 +1,25 @@
+"""
+Lightweight Python utilities for working with Redis.
+"""
+
+__author__ = 'Charles Leifer'
+__license__ = 'MIT'
+__version__ = '0.1.3'
+
+#               ___
+#            .-9 9 `\
+#          =(:(::)=  ;
+#            ||||     \
+#            ||||      `-.
+#           ,\|\|         `,
+#          /                \
+#         ;                  `'---.,
+#         |                         `\
+#         ;                     /     |
+#         \                    |      /
+#  jgs     )           \  __,.--\    /
+#       .-' \,..._\     \`   .-'  .-'
+#      `-=``      `:    |   /-/-/`
+#                   `.__/
+
+from walrus.tusk import *
