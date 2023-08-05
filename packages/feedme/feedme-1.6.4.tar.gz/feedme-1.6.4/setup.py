a048402d2944697cf1d832fae52305316a41815e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:26 +0000

    Add package feedme/1.6.4/feedme-1.6.4.tar.gz

---
+++ packages/feedme/feedme-1.6.4.tar.gz/feedme-1.6.4/setup.py
@@ -0,0 +1,34 @@
+import os
+from setuptools import setup
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='feedme',
+    version='1.6.4',
+    author='dotkom',
+    author_email='dotkom@online.ntnu.no',
+    packages=['feedme'],
+    license='BSD License',  # example license
+    description='Food ordering management system',
+    long_description='A food ordering management system for Django.\n\n\
+    This project was initially started to help \'dotKom\' with organizing the ordering of food for their work nights.\n\n\
+    Check out the github repo for installation instructions.',
+    url='https://github.com/dotKom/feedme',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+    include_package_data=True,
+)
