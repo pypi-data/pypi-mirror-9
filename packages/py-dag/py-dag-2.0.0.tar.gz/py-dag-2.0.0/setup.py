afbb6901a6e3e0c6d7ac255e209153d06b3167e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:52 +0000

    Add package py-dag/2.0.0/py-dag-2.0.0.tar.gz

---
+++ packages/py-dag/py-dag-2.0.0.tar.gz/py-dag-2.0.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='py-dag',
+      version='2.0.0',
+      description='Directed acyclic graph implementation',
+      url='https://github.com/thieman/py-dag',
+      author='Travis Thieman',
+      author_email='travis.thieman@gmail.com',
+      license='MIT',
+      packages=['dag'],
+      test_suite='nose.collector',
+      tests_require=['nose'],
+      zip_safe=False)
