52acd58ef0345f6cec34b9c34c606848213dc476
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:49 +0000

    Add package DateRanger/0.2.0.dev/DateRanger-0.2.0.dev.tar.gz

---
+++ packages/DateRanger/DateRanger-0.2.0.dev.tar.gz/DateRanger-0.2.0.dev/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+
+setup(
+    name='DateRanger',
+    url='https://github.com/spitfire-sidra/DateRanger',
+    version='0.2.0.dev',
+    author='Amo Chen',
+    author_email='spitfire.sidra@gmail.com',
+    packages=['DateRanger',],
+    license='License.txt',
+    description='Useful bussiness date ranges',
+    long_description=open('README.rst').read(),
+)
