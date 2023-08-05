36275cbc5b33e4155a390acccd5623f796b52a22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:43 +0000

    Add package autotorrent/1.2.0/autotorrent-1.2.0.tar.gz

---
+++ packages/autotorrent/autotorrent-1.2.0.tar.gz/autotorrent-1.2.0/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+def read_description():
+    import os
+    path = os.path.join(os.path.dirname(__file__), 'README.rst')
+    try:
+        with open(path) as f:
+            return f.read()
+    except:
+        return 'No description found'
+
+setup(
+    name='autotorrent',
+    version='1.2.0',
+    description='AutoTorrent allows easy cross-seeding',
+    long_description=read_description(),
+    author='Anders Jensen',
+    author_email='johndoee+autotorrent@tidalstream.org',
+    maintainer='John Doee',
+    url='https://github.com/JohnDoee/autotorrent',
+    packages=['autotorrent'],
+    install_requires=['BitTorrent-bencode'],
+    license='BSD',
+    entry_points={ 'console_scripts': [
+        'autotorrent = autotorrent.cmd:commandline_handler',
+    ]},
+)
