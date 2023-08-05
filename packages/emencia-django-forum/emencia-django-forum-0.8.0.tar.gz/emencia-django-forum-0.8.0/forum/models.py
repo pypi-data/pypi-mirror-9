e5a71e04c4f917a219aef1709d4a27b2c7aa3deb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:22 +0000

    Add package emencia-django-forum/0.8.0/emencia-django-forum-0.8.0.tar.gz

---
+++ packages/emencia-django-forum/emencia-django-forum-0.8.0.tar.gz/emencia-django-forum-0.8.0/forum/models.py
@@ -0,0 +1,209 @@
+# -*- coding: utf-8 -*-
+import math
+
+from django.conf import settings
+from django.contrib.auth.models import User
+from django.db import models
+import django.dispatch
+from django.utils.translation import ugettext_lazy as _
+from django.utils.timezone import now as tz_now
+
+from forum.utils.imports import safe_import_module
+
+class Category(models.Model):
+    """
+    Category
+    """
+    created = models.DateTimeField(_("created"), auto_now_add=True)
+    slug = models.SlugField(_('slug'), unique=True, max_length=50)
+    order = models.SmallIntegerField(_('order'))
+    title = models.CharField(_("title"), blank=False, max_length=255, unique=True)
+    description = models.TextField(_("description"), blank=True)
+    visible = models.BooleanField(_('visible'), default=True, help_text=_("Unvisible category won't be visible nor its threads."))
+
+    def __unicode__(self):
+        return self.title
+
+    @models.permalink
+    def get_absolute_url(self):
+        return ('forum:category-details', [self.slug])
+
+    def get_last_thread(self):
+        """
+        Return the last modified thread 
+        """
+        cache_key = '_get_last_thread_cache'
+        if not hasattr(self, cache_key):
+            item = None
+            res = self.thread_set.filter(visible=True).order_by('-modified')[0:1]
+            if len(res)>0:
+                item = res[0]
+            setattr(self, cache_key, item)
+        return getattr(self, cache_key)
+    
+    class Meta:
+        verbose_name = _("Category")
+        verbose_name_plural = _("Categories")
+        permissions = (
+            ('moderate', 'Moderate category'),
+        )
+
+
+class Thread(models.Model):
+    """
+    Thread
+    """
+    created = models.DateTimeField(_("created"), editable=False, null=True, blank=True)
+    modified = models.DateTimeField(_("modified"), editable=False, null=True, blank=True, help_text=_("This only filled when a message is added."))
+    author = models.ForeignKey(User, verbose_name=_("author"), editable=False, blank=False)
+    category = models.ForeignKey(Category, verbose_name=_("category"))
+    subject = models.CharField(_("subject"), max_length=150)
+    closed = models.BooleanField(_("closed"), default=False)
+    sticky = models.BooleanField(_("sticky"), default=False, help_text=_("Sticky thread will be on top of thread list."))
+    announce = models.BooleanField(_("announce"), default=False, help_text=_("Announce thread can be displayed out of the forum"))
+    visible = models.BooleanField(_('visible'), default=True, help_text=_("Unvisible threads won't be visible nor its messages."))
+
+    def __unicode__(self):
+        return self.subject
+
+    @models.permalink
+    def get_absolute_url(self):
+        return ('forum:thread-details', [self.category.slug, self.id])
+
+    def get_first_post(self):
+        """
+        Retourne le premier post en date, celui créé lors de la création du fil
+        """
+        cache_key = '_get_starter_cache'
+        if not hasattr(self, cache_key):
+            item = None
+            res = self.post_set.all().order_by('created')[0:1]
+            if len(res)>0:
+                item = res[0]
+            setattr(self, cache_key, item)
+        return getattr(self, cache_key)
+
+    def get_last_post(self):
+        """
+        Retourne le dernier post en date
+        """
+        cache_key = '_get_last_poster_cache'
+        if not hasattr(self, cache_key):
+            item = None
+            res = self.post_set.all().order_by('-created')[0:1]
+            if len(res)>0:
+                item = res[0]
+            setattr(self, cache_key, item)
+        return getattr(self, cache_key)
+    
+    def save(self, *args, **kwargs):
+        """
+        Fill 'created' and 'modified' attributes on first create
+        """
+        if self.created is None:
+            self.created = tz_now()
+        
+        if self.modified is None:
+            self.modified = self.created
+            
+        super(Thread, self).save(*args, **kwargs)
+    
+    class Meta:
+        verbose_name = _("Thread")
+        verbose_name_plural = _("Threads")
+        permissions = (
+            ('moderate', 'Moderate thread'),
+        )
+
+
+class ThreadWatch(models.Model):
+    """
+    Thread watch
+    """
+    owner = models.ForeignKey(User, verbose_name=_("owner"), blank=False)
+    thread = models.ForeignKey(Thread, verbose_name=_("thread"), blank=False)
+
+    def __unicode__(self):
+        return _("Thread watch : {0}").format(self.thread.subject)
+    
+    class Meta:
+        verbose_name = _("Thread watch")
+        verbose_name_plural = _("Thread watchs")
+
+
+class Post(models.Model):
+    """
+    Thread message
+    """
+    author = models.ForeignKey(User, verbose_name=_("author"), editable=False, blank=False)
+    thread = models.ForeignKey(Thread, verbose_name=_("thread"))
+    created = models.DateTimeField(_("created"), editable=False, blank=True, null=True)
+    modified = models.DateTimeField(_("modified"), editable=False, blank=True, null=True)
+    text = models.TextField(_('message'))
+
+    def __unicode__(self):
+        return _("{0}: message #{1}").format(self.thread.subject, self.id)
+
+    def get_absolute_url(self):
+        return u"{0}{1}".format(self.thread.get_absolute_url(), self.get_paginated_urlargs())
+    
+    def get_paginated_urlargs(self):
+        """
+        Return url arguments to retrieve the Post in a paginated list
+        """
+        position = self.get_paginated_position()
+        
+        if not position:
+            return '#forum-post-{0}'.format(self.id)
+        
+        return '?page={0}#forum-post-{1}'.format(position, self.id)
+    
+    def get_paginated_position(self):
+        """
+        Return the Post position in the paginated list
+        """
+        # If Post list is not paginated
+        if not settings.FORUM_THREAD_DETAIL_PAGINATE:
+            return 0
+        
+        count = Post.objects.filter(thread=self.thread_id, created__lt=self.created).count() + 1
+        
+        return int(math.ceil(count / float(settings.FORUM_THREAD_DETAIL_PAGINATE)))
+    
+    def save(self, *args, **kwargs):
+        """
+        Fill 'created' and 'modified' attributes on first create and allways update 
+        the thread's 'modified' attribute
+        """
+        edited = not(self.created is None)
+        
+        if self.created is None:
+            self.created = tz_now()
+        
+        # Update de la date de modif. du message
+        if self.modified is None:
+            self.modified = self.created
+        else:
+            self.modified = tz_now()
+        
+        super(Post, self).save(*args, **kwargs)
+        
+        # Update de la date de modif. du thread lors de la création du message
+        if not edited:
+            self.thread.modified = self.created
+            self.thread.save()
+
+    class Meta:
+        verbose_name = _("Message")
+        verbose_name_plural = _("Messages")
+
+
+# Declaring signals
+new_message_posted_signal = django.dispatch.Signal(providing_args=["post_instance","threadwatchs"])
+
+# Trying to import signal receiver callable
+message_post_receiver = safe_import_module(settings.FORUM_NEW_POST_SIGNAL)
+
+# Connecting signal to the receiver if any
+if message_post_receiver:
+    new_message_posted_signal.connect(message_post_receiver, dispatch_uid="forum.post.new_post_watcher")
---
+++ packages/emencia-django-forum/emencia-django-forum-0.8.0.tar.gz/emencia-django-forum-0.8.0/forum/views/thread.py
@@ -0,0 +1,175 @@
+# -*- coding: utf-8 -*-
+"""
+Thread views
+"""
+from django.conf import settings
+from django.core.urlresolvers import reverse
+from django.shortcuts import get_object_or_404
+from django.views import generic
+from django.views.generic.edit import FormMixin
+
+from braces.views import LoginRequiredMixin, UserFormKwargsMixin
+
+from forum.utils.views import SimpleListView, ListAppendView
+from forum.mixins import ModeratorRequiredMixin, ModeratorCheckMixin, ThreadQuerysetFiltersMixin
+from forum.models import Category, Thread, Post
+
+from forum.forms.post import PostCreateForm
+from forum.forms.thread import ThreadCreateForm, ThreadEditForm
+
+
+class LastThreadViews(LoginRequiredMixin, ThreadQuerysetFiltersMixin, SimpleListView):
+    """
+    Last modified thread view
+    """
+    template_name = 'forum/thread/lasts.html'
+    queryset = Thread.objects.all()
+    paginate_by = settings.FORUM_LAST_THREAD_PAGINATE
+
+
+class ThreadDetailsView(LoginRequiredMixin, UserFormKwargsMixin, ListAppendView):
+    """
+    Thread list view with a form to post a new message
+    """
+    model = Post
+    form_class = PostCreateForm
+    template_name = 'forum/thread/details.html'
+    paginate_by = settings.FORUM_THREAD_DETAIL_PAGINATE
+    context_object_name = 'object_list'
+    
+    def get_queryset(self, *args, **kwargs):
+        self.category_instance = get_object_or_404(Category, slug=self.kwargs['category_slug'], visible=True)
+        self.thread_instance = get_object_or_404(Thread, category=self.category_instance, pk=self.kwargs['thread_id'], visible=True)
+        self.queryset = self.thread_instance.post_set.select_related().order_by('created')
+        
+        # Mark the form as locked for non-admin users on a closed thread
+        # TODO: Unlock this also for moderators
+        if not self.request.user.is_staff:
+            self.locked_form = self.thread_instance.closed
+        
+        return super(ThreadDetailsView, self).get_queryset(*args, **kwargs)
+        
+    def get_context_data(self, **kwargs):
+        context = super(ThreadDetailsView, self).get_context_data(**kwargs)
+        context.update({
+            'FORUM_OWNER_MESSAGE_CAN_EDIT': settings.FORUM_OWNER_MESSAGE_CAN_EDIT,
+            'FORUM_TEXT_FIELD_JS_TEMPLATE': settings.FORUM_TEXT_FIELD_JS_TEMPLATE,
+            'FORUM_TEXT_MARKUP_RENDER_TEMPLATE': settings.FORUM_TEXT_MARKUP_RENDER_TEMPLATE,
+            'FORUM_AUTHOR_VCARD_TEMPLATE': settings.FORUM_AUTHOR_VCARD_TEMPLATE,
+            'category_instance': self.category_instance,
+            'thread_instance': self.thread_instance,
+        })
+        return context
+
+    def get_form_kwargs(self):
+        kwargs = super(ThreadDetailsView, self).get_form_kwargs()
+        kwargs.update({'thread': self.thread_instance})
+        return kwargs
+
+    def get_success_url(self):
+        return self.object.get_absolute_url()
+    
+    def get(self, *args, **kwargs):
+        resp = super(ThreadDetailsView, self).get(*args, **kwargs)
+        
+        # Choix de suivi d'un fil
+        watched = (self.thread_instance.threadwatch_set.filter(owner=self.request.user).count() > 0)
+        if "watch" in self.request.GET and not watched:
+            threadwatch_instance = self.thread_instance.threadwatch_set.create(owner=self.request.user)
+            watched = True
+        elif "unwatch" in self.request.GET and watched:
+            threadwatch_instance = self.thread_instance.threadwatch_set.get(owner=self.request.user)
+            threadwatch_instance.delete()
+            watched = False
+        # Push flag in request user object
+        self.request.user.watcher = watched
+        
+        return resp
+
+
+class ThreadCreateView(LoginRequiredMixin, UserFormKwargsMixin, ModeratorCheckMixin, generic.CreateView):
+    """
+    Thread create view is available for anyone but moderators have more options than non-moderator users
+    """
+    model = Thread
+    form_class = ThreadCreateForm
+    template_name = 'forum/thread/form.html'
+    raise_exception = True
+
+    def get_category(self):
+        return get_object_or_404(Category, slug=self.kwargs['category_slug'], visible=True)
+    
+    def get(self, *args, **kwargs):
+        self.category_instance = self.get_category()
+        return super(ThreadCreateView, self).get(*args, **kwargs)
+    
+    def post(self, *args, **kwargs):
+        self.category_instance = self.get_category()
+        return super(ThreadCreateView, self).post(*args, **kwargs)
+
+    def get_form_kwargs(self):
+        kwargs = super(ThreadCreateView, self).get_form_kwargs()
+        # check moderator perms
+        if self.has_moderator_permissions(self.request):
+            kwargs.update({'for_moderator': True})
+        return kwargs
+    
+    def get_context_data(self, **kwargs):
+        context = super(ThreadCreateView, self).get_context_data(**kwargs)
+        context.update({
+            'FORUM_TEXT_FIELD_JS_TEMPLATE': settings.FORUM_TEXT_FIELD_JS_TEMPLATE,
+            'FORUM_TEXT_MARKUP_RENDER_TEMPLATE': settings.FORUM_TEXT_MARKUP_RENDER_TEMPLATE,
+            'category_instance': self.category_instance,
+        })
+        return context
+
+    def form_valid(self, form):
+        form.category_instance = self.category_instance
+        return super(ThreadCreateView, self).form_valid(form)
+
+    def get_success_url(self):
+        return self.object.get_absolute_url()
+
+
+class ThreadEditView(ModeratorRequiredMixin, UserFormKwargsMixin, generic.UpdateView):
+    """
+    Thread edit view
+    
+    Restricted to moderators only
+    """
+    model = Thread
+    form_class = ThreadEditForm
+    template_name = 'forum/thread/form.html'
+    context_object_name = "thread_instance"
+    raise_exception = True
+    
+    def get_object(self, *args, **kwargs):
+        return get_object_or_404(Thread, category__slug=self.kwargs['category_slug'], pk=self.kwargs['thread_id'])
+        
+    def get_context_data(self, **kwargs):
+        context = super(ThreadEditView, self).get_context_data(**kwargs)
+        context.update({
+            'category_instance': self.object.category,
+        })
+        return context
+
+    def get_form_kwargs(self):
+        kwargs = super(ThreadEditView, self).get_form_kwargs()
+        # don't check anything, if the user is granted to access this view, it 
+        # have the rights to have the full moderator form
+        kwargs.update({'for_moderator': True})
+        return kwargs
+
+    #def check_permissions(self, request):
+        #"""
+        #Check if user have global or per object moderator permissions, first on category instance, 
+        #if not then on thread instance
+        #"""
+        #thread_instance = self.get_object()
+        #return self.check_moderator_permissions(request, thread_instance.category, thread_instance)
+
+    def get_success_url(self):
+        if self.object.visible:
+            return self.object.get_absolute_url()
+        else:
+            return reverse('gestus:category-details', args=(self.object.category.slug,))
