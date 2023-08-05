615fc93c1d71a9a867c5374180474e9be6795aec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:45 +0000

    Add package ark/0.8.2/ark-0.8.2.tar.gz

---
+++ packages/ark/ark-0.8.2.tar.gz/ark-0.8.2/readme.md
@@ -0,0 +1,46 @@
+
+Ark
+===
+
+Ark is a static website generator in Python. It transforms a directory of text files written in [Syntex][] or [Markdown][] into a self-contained website that can be viewed locally or served remotely.
+
+[Syntex]: https://github.com/dmulholland/syntex
+[Markdown]: http://daringfireball.net/projects/markdown/
+
+See the project's [documentation](http://docs.com) for further details.
+
+
+Installation
+------------
+
+Install directly from the Python Package Index using `pip`:
+
+    $ pip install ark
+
+Ark requires Python 3.2 or later.
+
+
+Dependencies
+------------
+
+The following dependencies are installed automatically by `pip`:
+
+* [Click][]
+* [Ibis][]
+* [Markdown][MD]
+* [Pygments][]
+* [PyYAML][]
+* [Syntex][]
+
+[MD]: https://pythonhosted.org/Markdown/
+[PyYAML]: http://pyyaml.org/
+[Pygments]: http://pygments.org/
+[Syntex]: http://github.com/dmulholland/syntex
+[Ibis]: http://github.com/dmulholland/ibis
+[Click]: http://click.pocoo.org/
+
+
+License
+-------
+
+This work has been placed in the public domain.
---
+++ packages/ark/ark-0.8.2.tar.gz/ark-0.8.2/setup.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+"""
+Ark
+===
+
+Ark is a static website generator in Python 3. It transforms a directory
+of text files written in Syntex or Markdown into a self-contained website
+that can be viewed locally or served remotely.
+
+Ark is under active development and is not yet ready for production use.
+
+"""
+
+import os
+import re
+import io
+
+from setuptools import setup, find_packages
+
+
+filepath = os.path.join(os.path.dirname(__file__), 'ark', 'meta.py')
+with io.open(filepath, encoding='utf-8') as metafile:
+    regex = r'''^__([a-z]+)__ = ["'](.*)["']'''
+    meta = dict(re.findall(regex, metafile.read(), flags=re.MULTILINE))
+
+
+setup(
+    name = 'ark',
+    version = meta['version'],
+    packages =  find_packages(),
+    include_package_data = True,
+    entry_points = {
+        'console_scripts': [
+            'ark = ark.cli:cli',
+        ],
+    },
+    install_requires = [
+        'Click',
+        'Markdown',
+        'Pygments',
+        'PyYAML',
+        'syntex',
+        'ibis',
+    ],
+    author = 'Darren Mulholland',
+    license = 'Public Domain',
+    description = 'Static website generator.',
+    long_description = __doc__,
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+        'License :: Public Domain',
+        'Topic :: Text Processing :: Markup :: HTML',
+    ],
+)
