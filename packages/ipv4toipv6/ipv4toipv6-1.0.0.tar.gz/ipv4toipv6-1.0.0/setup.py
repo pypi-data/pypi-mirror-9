9c5e24ff29e079b4f407c61b3a270e05565934dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:31 +0000

    Add package ipv4toipv6/1.0.0/ipv4toipv6-1.0.0.tar.gz

---
+++ packages/ipv4toipv6/ipv4toipv6-1.0.0.tar.gz/ipv4toipv6-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+	name = 'ipv4toipv6',
+	version = '1.0.0',
+	py_modules = ['toipv6'],
+	author = 'Ewurama Minka',
+	url = '',
+	description = 'A simple application to convert ip addresses from version 4 to version 6',
+)
---
+++ packages/ipv4toipv6/ipv4toipv6-1.0.0.tar.gz/ipv4toipv6-1.0.0/toipv6.py
@@ -0,0 +1,17 @@
+import socket
+from binascii import hexlify
+
+//while True:
+ //ipv4 = raw_input("Enter ipv4 address to convert")
+ def conv(ipv4):
+  ipv6 = socket.inet_aton(ipv4)
+  //print '\n\n the ipv6 address is ' , hexlify(ipv6), '\n\n'
+
+ //conv(ipv4)
+
+ //ans = raw_input("Do want to enter another ipv4 address? y/n      ")
+ //if ans == 'n':
+  //print "thanks for usind ipv6 calculater"
+ // break
+
+
