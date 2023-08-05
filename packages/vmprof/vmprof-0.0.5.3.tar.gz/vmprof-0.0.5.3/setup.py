e03ffdc95102bdc1ee8d85d456d08e0feeb82773
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:30 +0000

    Add package vmprof/0.0.5.3/vmprof-0.0.5.3.tar.gz

---
+++ packages/vmprof/vmprof-0.0.5.3.tar.gz/vmprof-0.0.5.3/setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup, find_packages, Extension
+import os, sys
+
+j = os.path.join
+
+libdwarf = j(j(j(os.path.dirname(os.path.abspath(__file__)), 'src'),
+             'hotpatch'), 'libdwarf.a')
+
+
+if hasattr(sys, 'pypy_translation_info'):
+    ext_modules = [] # built-in
+else:
+    ext_modules = [Extension('_vmprof',
+                           sources=[
+                               'src/_vmprof.c',
+                               'src/hotpatch/tramp.c',
+                               'src/hotpatch/elf.c',
+                               'src/hotpatch/x86_gen.c',
+                               'src/hotpatch/util.c',
+                               'src/vmprof.c',
+                               'src/get_custom_offset.c',
+                               ],
+                            libraries=['elf', 'unwind'],
+                            extra_link_args=['%s' % libdwarf])]
+
+
+setup(
+    name='vmprof',
+    author='vmprof team',
+    author_email='sebastian.pawlus@gmail.com',
+    version="0.0.5.3",
+    packages=find_packages(),
+    description="Python's vmprof client",
+    install_requires=[
+    ],
+    classifiers=[
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+    ],
+    zip_safe=False,
+    include_package_data=True,
+    ext_modules=ext_modules,
+)
