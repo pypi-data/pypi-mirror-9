1225ce0bdb27d0573159845d678be1dd83430174
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:53 +0000

    Add package provoke/0.2.1/provoke-0.2.1-py2.6.egg

---
+++ packages/provoke/provoke-0.2.1-py2.6.egg/provoke/example/client.py
@@ -0,0 +1,11 @@
+
+import sys
+
+from provoke.app import WorkerApplication
+
+app = WorkerApplication()
+app.declare_task('do_work')
+
+args = sys.argv[1:] or ['Hello', 'World!']
+res = app.tasks.do_work.apply_async(args, send_result=True)
+print 'Received result:', res.get()
