2e9bdf7a5b59837b53381b10f07df2ab5b9be9d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:44 +0000

    Add package NeXpy/0.5.3/NeXpy-0.5.3.tar.gz

---
+++ packages/NeXpy/NeXpy-0.5.3.tar.gz/NeXpy-0.5.3/src/nexpy/nexpygui.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python 
+# -*- coding: utf-8 -*-
+
+#-----------------------------------------------------------------------------
+# Copyright (c) 2013-2014, NeXpy Development Team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file COPYING, distributed with this software.
+#-----------------------------------------------------------------------------
+
+
+def main():
+
+    # MUST define the QT_API before importing matplotlib
+    import os, sys
+    os.environ['QT_API'] = 'pyside'
+    
+    import matplotlib
+    matplotlib.use('Qt4Agg')
+
+    sys.path.insert(0, os.path.abspath(os.path.join('..')))
+    from nexpy.gui.consoleapp import main
+    main()
+
+
+if __name__ == '__main__':
+    main()
