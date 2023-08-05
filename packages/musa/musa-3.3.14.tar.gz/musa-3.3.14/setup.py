e9eeed03f5695369791b8fccc8d82bd3a40071bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:52 +0000

    Add package musa/3.3.14/musa-3.3.14.tar.gz

---
+++ packages/musa/musa-3.3.14.tar.gz/musa-3.3.14/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import os,glob
+from setuptools import setup,find_packages
+
+VERSION='3.3.14'
+
+setup(
+    name = 'musa',
+    version = VERSION,
+    license = 'PSF',
+    author = 'Ilkka Tuohela',
+    author_email = 'hile@iki.fi',
+    description = 'Module for music tagging and library management',
+    keywords = 'music library tag management',
+    url = 'http://tuohela.net/packages/musa',
+    packages = ( 'musa', ),
+    package_data = { '': [ '*.md', '*.txt' ] },
+    scripts = glob.glob('bin/*'),
+    install_requires = ( 
+        'configobj', 
+        'soundforest>=3.3.3', 
+    ),
+)
+
