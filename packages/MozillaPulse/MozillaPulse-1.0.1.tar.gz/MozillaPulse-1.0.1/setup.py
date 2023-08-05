ae3110f83a64cac5d8d72291db99afb9791b68c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:39 +0000

    Add package MozillaPulse/1.0.1/MozillaPulse-1.0.1.tar.gz

---
+++ packages/MozillaPulse/MozillaPulse-1.0.1.tar.gz/MozillaPulse-1.0.1/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+
+from setuptools import setup
+
+setup(name='MozillaPulse',
+      version='1.0.1',
+      description='Helper library for interacting with the Mozilla Pulse ' +
+                  'message system at pulse.mozilla.org',
+      url='http://pulse.mozilla.org',
+      author='Mark Cote',
+      author_email='mcote@mozilla.com',
+      license='MPL 2.0',
+      packages=['mozillapulse', 'mozillapulse.messages'],
+      install_requires=['kombu', 'pytz'],
+      test_suite='test.unittests',
+)
