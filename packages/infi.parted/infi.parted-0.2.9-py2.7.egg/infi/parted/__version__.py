e41d98800fb7c4cafe3b7fc824077e82f23427de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:21 +0000

    Add package infi.parted/0.2.9/infi.parted-0.2.9-py2.7.egg

---
+++ packages/infi.parted/infi.parted-0.2.9-py2.7.egg/infi/parted/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.2.9"
+__git_commiter_name__ = "Guy Rozendorn"
+__git_commiter_email__ = "guy@rzn.co.il"
+__git_branch__ = '(detached from v0.2.9)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = 'c65473d00498ca7e3bc10f33720aa01f6e760fa8'
+__git_head_subject__ = 'HIP-1433 fixing the blkid regex'
+__git_head_message__ = ''
+__git_dirty_diff__ = ''
