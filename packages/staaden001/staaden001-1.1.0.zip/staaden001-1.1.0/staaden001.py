bb317fdb7f9152b9e2960b15b0c7f2c0808c3592
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:04 +0000

    Add package staaden001/1.1.0/staaden001-1.1.0.zip

---
+++ packages/staaden001/staaden001-1.1.0.zip/staaden001-1.1.0/staaden001.py
@@ -0,0 +1,14 @@
+"""  Module nester     Written by Becky Staaden     1/20/2015   """
+
+
+
+
+def print_lol(the_list, level):
+             
+    for each_item in the_list:
+        if isinstance(each_item, list):
+            print_lol(each_item, level+1)
+        else:
+            for tab_stop in range(level):
+                print("\t", end='')
+            print(each_item)
