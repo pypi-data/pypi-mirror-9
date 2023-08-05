558590f40bd64f1538ae75e565d81c72a252d9e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:22 +0000

    Add package emanual/0.2/emanual-0.2.tar.gz

---
+++ packages/emanual/emanual-0.2.tar.gz/emanual-0.2/setup.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# coding=utf-8
+"""
+EManual-CLI
+-----------
+
+Command Line Interface for EManual
+"""
+from setuptools import setup, find_packages
+import emanual
+import codecs
+
+setup(
+    name='emanual',
+    version=emanual.__version__,
+    description=emanual.__doc__.strip(),
+    long_description=__doc__,
+    url='https://github.com/EManual/EManual-CLI',
+    author=emanual.__author__,
+    author_email='tonjayin@gmail.name',
+    license='MIT',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'Click',
+        'path.py'
+    ],
+    entry_points={
+        'console_scripts' : [
+            'emanual=emanual.cli:main',
+        ]
+    },
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: MIT License',
+    ],
+    keywords='emanual cli',
+)
+
+
+
