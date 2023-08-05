06430d6329bf75a3db7b84f281a88d3af5c689b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:52 +0000

    Add package ScalaFunctional/0.0.2/ScalaFunctional-0.0.2.tar.gz

---
+++ packages/ScalaFunctional/ScalaFunctional-0.0.2.tar.gz/ScalaFunctional-0.0.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ScalaFunctional',
+    description="Scala functional programming style in Python",
+    url="https://github.com/EntilZha/ScalaFunctional",
+    author="Pedro Rodriguez",
+    author_email="pedro@snowgeek.org",
+    license="MIT",
+    keywords="functional scala",
+    packages=find_packages(exclude=['contrib', 'docs', 'tests*', 'test']),
+    version="0.0.2",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 2.7"
+    ]
+)
