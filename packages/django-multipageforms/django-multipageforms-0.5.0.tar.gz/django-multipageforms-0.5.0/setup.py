83bc7f836805f42279d0ca33e23afc6b3a3ba1ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:45 +0000

    Add package django-multipageforms/0.5.0/django-multipageforms-0.5.0.tar.gz

---
+++ packages/django-multipageforms/django-multipageforms-0.5.0.tar.gz/django-multipageforms-0.5.0/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+from setuptools import find_packages
+
+README_FILE = open('README.rst')
+try:
+    long_description = README_FILE.read()
+finally:
+    README_FILE.close()
+
+setup(name='django-multipageforms',
+        version='0.5.0',
+        packages=['multipageforms'],
+        package_dir = {'': 'src',},
+        include_package_data=True,
+        zip_safe=False,
+        platforms=['any'],
+        description='Form wizard is dead, long live multipageforms',
+        author_email='kaleissin@gmail.com',
+        author='kaleissin',
+        long_description=long_description,
+        classifiers=[
+                'Development Status :: 4 - Beta',
+                'Environment :: Web Environment',
+                'Framework :: Django',
+                'Intended Audience :: Developers',
+                'License :: OSI Approved :: MIT License',
+                'Operating System :: OS Independent',
+                'Programming Language :: Python',
+                'Programming Language :: Python :: 2',
+                'Programming Language :: Python :: 2.7',
+                'Topic :: Software Development :: Libraries :: Application Frameworks',
+                'Topic :: Software Development :: Libraries :: Python Modules',
+        ]
+)
