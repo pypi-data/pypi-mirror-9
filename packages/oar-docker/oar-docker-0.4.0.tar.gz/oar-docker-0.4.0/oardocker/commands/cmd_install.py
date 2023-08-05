5095d8c5dd15933815e5b409ec3bee43c1d3a0ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:32 +0000

    Add package oar-docker/0.4.0/oar-docker-0.4.0.tar.gz

---
+++ packages/oar-docker/oar-docker-0.4.0.tar.gz/oar-docker-0.4.0/oardocker/commands/cmd_install.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from __future__ import with_statement, absolute_import, unicode_literals
+
+import click
+from ..actions import install
+from ..context import pass_context, on_started, on_finished
+
+
+@click.command('install')
+@click.argument('src')
+@pass_context
+@on_finished(lambda ctx: ctx.state.dump())
+@on_finished("clean")
+@on_started("stop")
+@on_started(lambda ctx: ctx.assert_valid_env())
+def cli(ctx, src):
+    """Install and configure OAR from src"""
+    install(ctx, src,
+            needed_tag="base",
+            tag="latest",
+            parent_cmd="build")
