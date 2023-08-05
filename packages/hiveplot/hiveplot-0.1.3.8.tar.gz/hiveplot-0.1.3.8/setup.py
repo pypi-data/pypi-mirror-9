4e5c2fbb01f677c4c458979d6f35fc0bb7b751ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:06 +0000

    Add package hiveplot/0.1.3.8/hiveplot-0.1.3.8.tar.gz

---
+++ packages/hiveplot/hiveplot-0.1.3.8.tar.gz/hiveplot-0.1.3.8/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup  # Always prefer setuptools over distutils
+
+setup(name='hiveplot',
+      version='0.1.3.8',
+      py_modules=['hiveplot'],
+      url='https://github.com/ericmjl/hiveplot',
+      author='Eric J. Ma',
+      email='ericmajinglong@gmail.com',
+      maintainer='Eric J. Ma',
+      maintainer_email='ericmajinglong@gmail.com'
+      )
