6a15e980f34168b919788a0d433063a53f85093e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:06 +0000

    Add package cymem/1.11/cymem-1.11.tar.gz

---
+++ packages/cymem/cymem-1.11.tar.gz/cymem-1.11/cymem/cymem.pxd
@@ -0,0 +1,11 @@
+cdef class Pool:
+    cdef readonly size_t size
+    cdef readonly dict addresses
+
+    cdef void* alloc(self, size_t number, size_t size) except NULL
+    cdef void free(self, void* addr) except *
+    cdef void* realloc(self, void* addr, size_t n) except NULL
+
+
+cdef class Address:
+    cdef void* ptr
---
+++ packages/cymem/cymem-1.11.tar.gz/cymem-1.11/setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+try:
+    from Cython.Build import cythonize
+    from Cython.Distutils import Extension
+    exts = cythonize([Extension("cymem.cymem", ["cymem/cymem.pyx"])])
+except ImportError:
+    from distutils.extension import Extension
+    exts = [Extension("cymem.cymem", ["cymem/cymem.c"])]
+
+
+import sys
+import os
+from os.path import splitext
+
+
+setup(
+    ext_modules=exts,
+
+    name="cymem",
+    packages=["cymem"],
+    version="1.11",
+    author="Matthew Honnibal",
+    author_email="honnibal@gmail.com",
+    url="http://github.com/syllog1sm/cymem",
+    package_data={"cymem": ["*.pxd", "*.pyx", "*.c"]},
+    description="""Manage calls to calloc/free through Cython""",
+    classifiers=[
+                'Environment :: Console',
+                'Operating System :: OS Independent',
+                'Intended Audience :: Science/Research',
+                'Programming Language :: Cython',
+                'Topic :: Scientific/Engineering'],
+)
