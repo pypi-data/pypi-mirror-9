955d74d5860d471e0ec600d5edcfe204a7f30385
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:49 +0000

    Add package setuptools-yaml/0.2/setuptools-yaml-0.2.tar.gz

---
+++ packages/setuptools-yaml/setuptools-yaml-0.2.tar.gz/setuptools-yaml-0.2/setuptools_yaml.py
@@ -0,0 +1,34 @@
+import inspect
+import os
+import logging
+
+import yaml
+
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+def metadata_yaml(dist, attr, value):
+    logger.debug(
+        'metadata_yaml: '
+        'dist = %r; attr = %r; value = %r',
+        dist, attr, value)
+    frame = _get_code_object()
+    setup_py_path = inspect.getsourcefile(frame)
+    yaml_filename = os.path.join(os.path.dirname(setup_py_path), value)
+    logger.debug('yaml_filename = %r', yaml_filename)
+    metadata = yaml.load(file(yaml_filename, 'r'))
+    for keyname in metadata.keys():
+        if keyname in dist.metadata.__dict__.keys():
+            setattr(dist.metadata, keyname, metadata[keyname])
+
+
+def _get_code_object():
+    frame = inspect.currentframe()
+
+    while frame:
+        code = frame.f_back.f_code
+        if code.co_filename.endswith('setup.py'):
+            return code
+        frame = frame.f_back
