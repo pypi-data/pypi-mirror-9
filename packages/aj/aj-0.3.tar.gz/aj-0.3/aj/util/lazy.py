f91573e0bda8daf8aee54e5c679019f53ae0b1a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:58 +0000

    Add package aj/0.3/aj-0.3.tar.gz

---
+++ packages/aj/aj-0.3.tar.gz/aj-0.3/aj/util/lazy.py
@@ -0,0 +1,29 @@
+import logging
+
+
+class LazyModule (object):
+    def __init__(self, module, object=None):
+        self._module = module
+        self._object = object
+        self._loaded = False
+
+    def __load(self):
+        logging.debug('Lazy-loading module %s' % self._module)
+        target = __import__(self._module, fromlist=[''])
+        if self._object:
+            target = getattr(target, self._object)
+
+        for k in dir(target):
+            try:
+                self.__dict__[k] = getattr(target, k)
+            except AttributeError:
+                pass
+
+        self._loaded = True
+
+    def __getattr__(self, attr):
+        if not self._loaded:
+            self.__load()
+        return self.__dict__[attr]
+
+
