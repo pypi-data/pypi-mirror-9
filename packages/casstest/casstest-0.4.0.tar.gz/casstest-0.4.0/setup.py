4ef275d1f5b2b7a1a9e17430177c019cb04f0322
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:00 +0000

    Add package casstest/0.4.0/casstest-0.4.0.tar.gz

---
+++ packages/casstest/casstest-0.4.0.tar.gz/casstest-0.4.0/setup.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+readme = open('README.rst').read()
+history = open('HISTORY.rst').read().replace('.. :changelog:', '')
+
+requirements = [
+    'cassandra-driver==2.1.3'
+]
+
+test_requirements = [
+]
+
+setup(
+    name='casstest',
+    version='0.4.0',
+    description='Just a quick dockerfile test for my entertainment',
+    long_description=readme + '\n\n' + history,
+    author='Chris Goller',
+    author_email='goller@gmail.com',
+    url='https://github.com/goller/casstest',
+    packages=[
+        'casstest',
+    ],
+    package_dir={'casstest':
+                 'casstest'},
+    entry_points={'console_scripts': ['run-cass-test = casstest.casstest:main']},
+    include_package_data=True,
+    install_requires=requirements,
+    license="BSD",
+    zip_safe=False,
+    keywords='casstest',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
+    test_suite='tests',
+    tests_require=test_requirements
+)
---
+++ packages/casstest/casstest-0.4.0.tar.gz/casstest-0.4.0/tests/test_casstest.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+test_casstest
+----------------------------------
+
+Tests for `casstest` module.
+"""
+
+import unittest
+
+from casstest import casstest
+
+
+class TestCasstest(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_something(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
