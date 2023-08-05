ab0a1c6defcd59f13aeb0854f8822fd7a1423a3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:48 +0000

    Add package bespin/0.2.9/bespin-0.2.9.tar.gz

---
+++ packages/bespin/bespin-0.2.9.tar.gz/bespin-0.2.9/bespin/amazon/s3.py
@@ -0,0 +1,106 @@
+from bespin.errors import BadS3Bucket
+
+from six.moves.urllib.parse import urlparse
+from contextlib import contextmanager
+from collections import namedtuple
+from six import StringIO
+from filechunkio import FileChunkIO
+import humanize
+import logging
+import boto
+import math
+import sys
+import os
+
+log = logging.getLogger("bespin.amazon.s3")
+
+S3Location = namedtuple("S3Location", ["bucket", "key", "full"])
+
+def s3_location(value):
+    """Return us an s3 location record type"""
+    info = urlparse(value)
+    if info.scheme != "s3":
+        raise ValueError("S3 location must be a valid s3 url\tgot={0}".format(value))
+
+    bucket = info.netloc
+    if not bucket:
+        raise ValueError("S3 location must be a valid s3 url\tgot={0}".format(value))
+
+    key = info.path
+    return S3Location(bucket, key, value)
+
+@contextmanager
+def a_multipart_upload(bucket, key):
+    mp = None
+    try:
+        mp = bucket.initiate_multipart_upload(key)
+        yield mp
+    except:
+        if mp:
+            mp.cancel_upload()
+        raise
+    else:
+        if mp:
+            mp.complete_upload()
+
+def get_bucket(credentials, bucket_name):
+    try:
+        return credentials.s3.get_bucket(bucket_name)
+    except boto.exception.S3ResponseError as error:
+        if error.status in (404, 403):
+            raise BadS3Bucket("Bucket either doesn't exist or isn't available to you", name=bucket_name)
+        else:
+            raise
+
+def list_keys_from_s3_path(credentials, query_path):
+    query = s3_location(query_path)
+    bucket = get_bucket(credentials, query.bucket)
+    return bucket.list(prefix="rca-contract")
+
+def delete_key_from_s3(credentials, key, dry_run):
+    if dry_run:
+        print("Would delete {0}".format(key.name))
+    else:
+        print("Deleted {0}".format(key.name))
+        key.delete()
+
+def upload_file_to_s3(credentials, source_filename, destination_path):
+    source_file = open(source_filename, 'rb')
+    destination_file = s3_location(destination_path)
+
+    source = os.path.abspath(source_file.name)
+    source_size = os.stat(source).st_size
+    log.info("Uploading from %s (%s) to %s", source, humanize.naturalsize(source_size), destination_file.full)
+
+    bucket = get_bucket(credentials, destination_file.bucket)
+
+    chunk = 5242881
+    chunk_count = 0
+
+    offset = 0
+    offsets = []
+    while offset < source_size:
+        offsets.append(offset)
+        if source_size - offset < chunk:
+            break
+        offset += chunk
+    offsets.append(source_size+1)
+    log.info("Uploading %s chunks", len(offsets) - 1)
+
+    try:
+        with a_multipart_upload(bucket, destination_file.key) as mp:
+            for i in range(0, len(offsets)-1):
+                first, last = offsets[i], offsets[i+1]-1
+                bytes_size = last - first
+
+                with FileChunkIO(source, 'r', offset=first, bytes=bytes_size) as fp:
+                    log.info("Uploading chunk %s (%s)", i+1, humanize.naturalsize(bytes_size))
+                    mp.upload_part_from_file(fp, part_num=i + 1)
+    except boto.exception.S3ResponseError as error:
+        if error.status is 403:
+            log.error("Seems you are unable to edit this location :(")
+            sys.exit(1)
+        else:
+            raise
+
+    log.info("Finished uploading")
---
+++ packages/bespin/bespin-0.2.9.tar.gz/bespin-0.2.9/bespin/tasks.py
@@ -0,0 +1,88 @@
+"""
+The functionality itself for each task.
+
+Each task is specified with the ``a_task`` decorator and indicates whether it's
+necessary to provide the task with the object containing all the stacks and/or
+one specific stack object.
+"""
+
+from bespin.actions.builder import Builder
+import itertools
+import logging
+
+log = logging.getLogger("bespin.tasks")
+
+available_tasks = {}
+class a_task(object):
+    """Records a task in the ``available_tasks`` dictionary"""
+    def __init__(self, needs_stack=False, needs_stacks=False, needs_credentials=False):
+        self.needs_stack = needs_stack
+        self.needs_stacks = needs_stack or needs_stacks
+        self.needs_credentials = needs_credentials
+
+    def __call__(self, func):
+        available_tasks[func.__name__] = func
+        func.needs_stack = self.needs_stack
+        func.needs_stacks = self.needs_stacks
+        func.needs_credentials = self.needs_credentials
+        return func
+
+@a_task()
+def list_tasks(overview, configuration, **kwargs):
+    """List the available_tasks"""
+    print("Available tasks to choose from are:")
+    print("Use the --task option to choose one")
+    print("")
+    keygetter = lambda item: item[1].label
+    tasks = sorted(overview.find_tasks().items(), key=keygetter)
+    for label, items in itertools.groupby(tasks, keygetter):
+        print("--- {0}".format(label))
+        print("----{0}".format("-" * len(label)))
+        sorted_tasks = sorted(list(items), key=lambda item: len(item[0]))
+        max_length = max(len(name) for name, _ in sorted_tasks)
+        for key, task in sorted_tasks:
+            print("\t{0}{1} :-: {2}".format(" " * (max_length-len(key)), key, task.description or ""))
+        print("")
+
+@a_task(needs_stacks=True)
+def show(overview, configuration, stacks, **kwargs):
+    """Show what stacks we have"""
+    flat = configuration.get("bespin.flat", False)
+    only_pushable = configuration.get("bespin.only_pushable", False)
+
+    for index, layer in enumerate(Builder().layered(stacks, only_pushable=only_pushable)):
+        if flat:
+            for _, stack in layer:
+                print(stack.stack_name)
+        else:
+            print("Layer {0}".format(index))
+            for _, stack in layer:
+                print("    {0}".format(stack.display_line()))
+            print("")
+
+@a_task(needs_stack=True, needs_credentials=True)
+def deploy(overview, configuration, stacks, stack, credentials, **kwargs):
+    """Deploy a particular stack"""
+    Builder().deploy_stack(stack, stacks, credentials)
+
+@a_task(needs_stack=True, needs_credentials=True)
+def publish_artifacts(overview, configuration, stacks, stack, credentials, **kwargs):
+    """Build and publish an artifact"""
+    Builder().publish_artifacts(stack, credentials)
+
+@a_task(needs_stack=True, needs_credentials=True)
+def clean_old_artifacts(overview, configuration, stacks, stack, credentials, **kwargs):
+    """Cleanup old artifacts"""
+    Builder().clean_old_artifacts(stack, credentials)
+
+@a_task(needs_stack=True, needs_credentials=True)
+def confirm_deployment(overview, configuration, stacks, stack, credentials, **kwargs):
+    """Confirm deployment via SNS notification for each instance"""
+    log.info("All stacks have been confirmed to be deployed!")
+
+@a_task(needs_stack=True)
+def sanity_check(overview, configuration, stacks, stack, **kwargs):
+    """Sanity check a stack and it's dependencies"""
+    Builder().sanity_check(stack, stacks)
+    log.info("All the stacks are sane!")
+
