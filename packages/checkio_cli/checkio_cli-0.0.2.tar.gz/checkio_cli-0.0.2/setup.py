ccc274016caff59d80811eed580f38698c03073f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:22 +0000

    Add package checkio_cli/0.0.2/checkio_cli-0.0.2.tar.gz

---
+++ packages/checkio_cli/checkio_cli-0.0.2.tar.gz/checkio_cli-0.0.2/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='checkio_cli',
+    version='0.0.2',
+    description='CheckiO common line interface for run and debug mission',
+    author='CheckiO',
+    author_email='igor@checkio.org',
+    url='https://github.com/CheckiO/checkio-cli',
+    download_url='https://github.com/CheckiO/checkio-cli/tarball/0.1',
+    # entry_points={
+    #     'console_scripts': ['checkio-cli = checkio_cli.cli:main'],
+    # },
+    # install_requires=[
+    #     'GitPython==0.3.5',
+    #     'docker-py==0.7.0',
+    #     'tornado==4.0.2',
+    #     'coloredlogs',
+    #     'future',
+    # ],
+)
