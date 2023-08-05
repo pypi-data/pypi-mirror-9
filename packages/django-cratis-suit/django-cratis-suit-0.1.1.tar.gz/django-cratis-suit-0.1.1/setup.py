ab05d8fb92e45b67cf347d070e38fd9ec2f25c4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:53 +0000

    Add package django-cratis-suit/0.1.1/django-cratis-suit-0.1.1.tar.gz

---
+++ packages/django-cratis-suit/django-cratis-suit-0.1.1.tar.gz/django-cratis-suit-0.1.1/setup.py
@@ -0,0 +1,20 @@
+import os
+from setuptools import setup, find_packages
+# from tests import PyTest
+
+setup(
+    name='django-cratis-suit',
+    version='0.1.1',
+    packages=find_packages(),
+
+    url='',
+    license='MIT License',
+    author='Alex Rudakov',
+    author_email='ribozz@gmail.com',
+    description='Django-suit integration',
+    long_description='',
+    install_requires=[
+        'django-suit',
+    ]
+)
+
