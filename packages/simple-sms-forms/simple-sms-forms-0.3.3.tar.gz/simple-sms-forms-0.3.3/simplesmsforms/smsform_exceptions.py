a9b36ab60635fa657ad7fa9e657f3d53ff97e1fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:09 +0000

    Add package simple-sms-forms/0.3.3/simple-sms-forms-0.3.3.tar.gz

---
+++ packages/simple-sms-forms/simple-sms-forms-0.3.3.tar.gz/simple-sms-forms-0.3.3/simplesmsforms/smsform_exceptions.py
@@ -0,0 +1,32 @@
+class SMSFieldException(Exception):
+
+    def __init__(self, field):
+        self.field = field
+
+    def __str__(self):
+        return repr(self.field)
+
+
+class MissingRequiredFieldException(SMSFieldException):
+
+    def __str__(self):
+        return "The {required_field} is missing but is required.".format(
+            required_field=self.field
+        )
+
+
+class InvalidDateException(SMSFieldException):
+    pass
+
+
+class ChoiceException(SMSFieldException):
+    pass
+
+class DuplicateFieldsException(SMSFieldException):
+    def __str__(self):
+        return "Duplicate field detected: {fields}".format(
+            fields=self.field)
+
+class UnrecognizedDataFoundException(SMSFieldException):
+    def __str__(self):
+        return "Unrecognized or duplicate data found: '{field}'. Please remove it before submitting the SMS".format(field=self.field)
