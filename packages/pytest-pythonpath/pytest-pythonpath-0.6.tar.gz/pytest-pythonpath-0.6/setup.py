fa7fc96c600e2cf71ac130ec7e588e9ca0027b19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:31 +0000

    Add package pytest-pythonpath/0.6/pytest-pythonpath-0.6.tar.gz

---
+++ packages/pytest-pythonpath/pytest-pythonpath-0.6.tar.gz/pytest-pythonpath-0.6/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+description = 'pytest plugin for adding to the PYTHONPATH from command line or configs.'
+try:
+    long_description = open("README.md").read()
+except:
+    long_description = description
+
+setup(
+    name='pytest-pythonpath',
+    description=description,
+    long_description=long_description,
+    version='0.6',
+    author='Eric Palakovich Carr',
+    author_email='carreric@gmail.com',
+    url='https://github.com/bigsassy/pytest-pythonpath',
+    py_modules=['pytest_pythonpath'],
+    entry_points={'pytest11': ['pythonpath = pytest_pythonpath']},
+    install_requires=['pytest>=2.5.2']
+)
