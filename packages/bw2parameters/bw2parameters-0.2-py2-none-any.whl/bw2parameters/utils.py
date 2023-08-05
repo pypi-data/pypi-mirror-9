3020bb5cbdecee67498bbaa5c1cb46f49fdcdf78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:08 +0000

    Add package bw2parameters/0.2/bw2parameters-0.2-py2-none-any.whl

---
+++ packages/bw2parameters/bw2parameters-0.2-py2-none-any.whl/bw2parameters/utils.py
@@ -0,0 +1,25 @@
+import asteval
+
+
+def _get_existing_symbols():
+    interpreter = asteval.Interpreter()
+    return set(interpreter.symtable)
+
+EXISTING_SYMBOLS = _get_existing_symbols()
+
+
+def get_symbols(expression):
+    interpreter = asteval.Interpreter()
+    nf = asteval.NameFinder()
+    nf.generic_visit(interpreter.parse(expression))
+    return set(nf.names).difference(EXISTING_SYMBOLS)
+
+try:
+    # Python 2
+    basestring
+    def isstr(s):
+        return isinstance(s, basestring)
+except NameError:
+    # Python 3
+    def isstr(s):
+        return isinstance(s, str)
