13de569146dfd115dc84384e96d458f8fa1b1b18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:11 +0000

    Add package giftman_nester/1.0.0/giftman_nester-1.0.0.tar.gz

---
+++ packages/giftman_nester/giftman_nester-1.0.0.tar.gz/giftman_nester-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+		name = 'giftman_nester',
+		version = '1.0.0',
+		py_modules = ['giftman_nester'],
+		author = 'giftman',
+		author_email = '121552591@qq.com',
+		url = 'www.baidu.com',
+		description = 'A S',
+     )
