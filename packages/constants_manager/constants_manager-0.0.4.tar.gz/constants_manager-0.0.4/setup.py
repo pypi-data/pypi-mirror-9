3c0e783abcd41865a7177e738380e7fc2d0dd663
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:59 +0000

    Add package constants_manager/0.0.4/constants_manager-0.0.4.tar.gz

---
+++ packages/constants_manager/constants_manager-0.0.4.tar.gz/constants_manager-0.0.4/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+import sys
+
+setup(name='constants_manager',
+      version='0.0.4',
+      description='a library of managing constants.',
+      long_description="""This library is help you to use managing constants. Especially when you creating web application.""",
+      author='Kenichi Masuda',
+      author_email='masuken@gmail.com',
+      url='https://github.com/masudaK/constants_manager',
+      packages=find_packages(),
+      zip_safe=(sys.version>="2.5"),
+      license='GNU Lesser General Public License v3 or later (LGPLv3+)',
+      keywords='',
+      platforms='Linux',
+      classifiers=['Topic :: Software Development :: Libraries :: Python Modules',
+                   'License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)',
+                   'Programming Language :: Python :: 3'
+                   ]
+      )
+
