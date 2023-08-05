65d201b04736299a65f585285a10b6607b359d38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:36 +0000

    Add package mixcloud/0.0.1/mixcloud-0.0.1.tar.gz

---
+++ packages/mixcloud/mixcloud-0.0.1.tar.gz/mixcloud-0.0.1/setup.py
@@ -0,0 +1,30 @@
+from pip.req import parse_requirements
+import setuptools
+
+reqs = [str(ir.req) for ir in parse_requirements('requirements.txt')]
+
+with open('README.rst') as f:
+    readme = f.read()
+
+with open('HISTORY.rst') as f:
+    history = f.read()
+
+setuptools.setup(name='mixcloud',
+                 version='0.0.1',
+                 author='Etienne Millon',
+                 author_email='me@emillon.org',
+                 url="https://github.com/emillon/mixcloud",
+                 license='BSD',
+                 packages=['mixcloud'],
+                 install_requires=reqs,
+                 description='Bindings for the mixcloud.com API',
+                 long_description=readme + '\n\n' + history,
+                 classifiers=[
+                     'Development Status :: 3 - Alpha',
+                     'Intended Audience :: Developers',
+                     'License :: OSI Approved :: BSD License',
+                     'Operating System :: OS Independent',
+                     'Programming Language :: Python :: 2',
+                     'Programming Language :: Python :: 3',
+                     ],
+                 )
