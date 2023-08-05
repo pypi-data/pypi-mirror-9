3b76ebb035e284a960eb7851624066a0ec9e0691
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:55 +0000

    Add package sphinx-kr-theme/0.2.1/sphinx_kr_theme-0.2.1-py2.py3-none-any.whl

---
+++ packages/sphinx-kr-theme/sphinx_kr_theme-0.2.1-py2.py3-none-any.whl/sphinx_kr_theme/__init__.py
@@ -0,0 +1,31 @@
+"""
+    The kr-sphinx-themes wrapper.
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    Usage::
+
+        # put in your conf.py
+
+        import sphinx_kr_theme
+
+        html_theme = 'kr'
+        html_theme_path = [sphinx_kr_theme.get_html_theme_path()]
+"""
+
+import pkg_resources
+
+
+__version__ = '0.2.1'
+
+
+def get_html_theme_path():
+    """Gets the html theme's path.
+
+    :returns: the absolute path of theme location.
+    """
+    return pkg_resources.resource_filename(__package__, '')
+
+
+def get_path():
+    # the alias of ``get_html_theme_path``
+    return get_html_theme_path()
