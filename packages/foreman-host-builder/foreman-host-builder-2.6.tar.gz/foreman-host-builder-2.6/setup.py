9e1cbd32f0800480c4d4a0a1a47712f8f961272d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:38 +0000

    Add package foreman-host-builder/2.6/foreman-host-builder-2.6.tar.gz

---
+++ packages/foreman-host-builder/foreman-host-builder-2.6.tar.gz/foreman-host-builder-2.6/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+try:
+    from setuptools import setup
+except ImportError:
+    print 'Please install setuptools before running this script:'
+    print ' sudo pip install setuptools'
+    print ' sudo apt-get install python-setuptools'
+    print ' sudo yum install python-setuptools'
+    exit(1)
+
+setup(name='foreman-host-builder',
+      version='2.6',
+      description='Build hosts in Foreman from a template file',
+      author='Xavier Naveira',
+      author_email='xnaveira@gmail.com',
+      url='https://github.com/xnaveira/foreman-host-builder',
+      install_requires=['python-foreman','fabric','ConfigParser','pyyaml'],
+      scripts=['foreman-host-builder.py'],
+      packages=['fhb'],
+     )
