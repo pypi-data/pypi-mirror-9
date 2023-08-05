359899f600a08b6ce1f1d45b11f8b35d92ac5c86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:25 +0000

    Add package avwx/0.1.1/avwx-0.1.1.tar.gz

---
+++ packages/avwx/avwx-0.1.1.tar.gz/avwx-0.1.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='avwx',
+    version='0.1.1',
+    description="A tool to grab aviation-related weather",
+    url="https://github.com/NicholasMerrill/avwx",
+    author="Nick Merrill",
+    author_email="public@nickmerrill.co",
+    license='MIT',
+    packages=['avwx'],
+    install_requires=[
+        'python-dateutil',
+    ],
+    zip_safe=False,
+)
+
