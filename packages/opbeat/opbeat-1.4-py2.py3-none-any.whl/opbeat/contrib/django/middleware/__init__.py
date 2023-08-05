73766cc8299b62753e6754ad9acbf48115e55643
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:12 +0000

    Add package opbeat/1.4/opbeat-1.4-py2.py3-none-any.whl

---
+++ packages/opbeat/opbeat-1.4-py2.py3-none-any.whl/opbeat/contrib/django/middleware/__init__.py
@@ -0,0 +1,60 @@
+"""
+opbeat.contrib.django.middleware
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+:copyright: (c) 2011-2012 Opbeat
+
+Large portions are
+:copyright: (c) 2010 by the Sentry Team, see AUTHORS for more details.
+:license: BSD, see LICENSE for more details.
+"""
+
+from __future__ import absolute_import
+from django.conf import settings
+from opbeat.contrib.django.models import client
+import threading
+import logging
+
+def _is_ignorable_404(uri):
+    """
+    Returns True if the given request *shouldn't* notify the site managers.
+    """
+    urls = getattr(settings, 'IGNORABLE_404_URLS', ())
+    return any(pattern.search(uri) for pattern in urls)
+
+
+class Opbeat404CatchMiddleware(object):
+    def process_response(self, request, response):
+        if response.status_code != 404 or _is_ignorable_404(request.get_full_path()):
+            return response
+        data = client.get_data_from_request(request)
+        data.update({
+            'level': logging.INFO,
+            'logger': 'http404',
+        })
+        result = client.capture('Message', param_message={'message':'Page Not Found: %s','params':[request.build_absolute_uri()]}, data=data)
+        request.opbeat = {
+            'app_id': data.get('app_id', client.app_id),
+            'id': client.get_ident(result),
+        }
+        return response
+
+
+class OpbeatResponseErrorIdMiddleware(object):
+    """
+    Appends the X-Opbeat-ID response header for referencing a message within
+    the Opbeat datastore.
+    """
+    def process_response(self, request, response):
+        if not getattr(request, 'opbeat', None):
+            return response
+        response['X-Opbeat-ID'] = request.opbeat['id']
+        return response
+
+
+class OpbeatLogMiddleware(object):
+    # Create a threadlocal variable to store the session in for logging
+    thread = threading.local()
+
+    def process_request(self, request):
+        self.thread.request = request
---
+++ packages/opbeat/opbeat-1.4-py2.py3-none-any.whl/tests/events/tests.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+from mock import Mock
+from opbeat.utils.compat import TestCase
+from opbeat.events import Message
+
+
+class MessageTest(TestCase):
+
+    def test_to_string(self):
+        unformatted_message = 'My message from %s about %s'
+        client = Mock()
+        message = Message(client)
+        message.logger = Mock()
+        data = {
+            'param_message': {
+                'message': unformatted_message,
+            }
+        }
+
+        self.assertEqual(message.to_string(data), unformatted_message)
+
+        data['param_message']['params'] = (1, 2)
+        self.assertEqual(message.to_string(data),
+                         unformatted_message % (1, 2))
