0ed425ba5d944e99593f7e11684fba4b377f5206
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:28 +0000

    Add package persistentdicts/1.2.2/persistentdicts-1.2.2.tar.gz

---
+++ packages/persistentdicts/persistentdicts-1.2.2.tar.gz/persistentdicts-1.2.2/setup.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+"""
+dict-like interfaces for various databases (cassandra, kyotocabinet, sqlite)
+"""
+import setuptools
+
+DOWNLOAD = "https://github.com/chmduquesne/persistentdicts/archive/%s.tar.gz"
+
+# We use semantic versioning http://semver.org/.
+#
+# Summary: Given a version number MAJOR.MINOR.PATCH, we increment the:
+#
+#     MAJOR for incompatible API changes,
+#     MINOR for added functionality in a backwards-compatible manner
+#     PATCH for backwards-compatible bug fixes.
+VERSION = open("version").read().strip()
+
+PACKAGE_NAME = "persistentdicts"
+DESCRIPTION = __doc__
+GIT_REF = VERSION
+
+
+# We also have travis-ci generate a version file that contains the most
+# recent tag, followed by dev and the number of revisions since that tag
+# (in respect with https://www.python.org/dev/peps/pep-0440/), and we
+# maintain a bleeding-edge version.
+if "dev" in VERSION:
+    PACKAGE_NAME = "persistentdicts-dev"
+    DESCRIPTION = "development version of persistentdicts"
+    GIT_REF = "master"
+
+setuptools.setup(
+    name=PACKAGE_NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=open("README.rst").read(),
+    author="Christophe-Marie Duquesne",
+    author_email="chmd@chmd.fr",
+    url="https://github.com/chmduquesne/persistentdicts",
+    download_url=DOWNLOAD % (GIT_REF),
+    keywords=["database", "interface", "adapter"],
+    packages=["persistentdicts"],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        ],
+    install_requires=[
+        "kyotocabinet >= 1.9",
+        "cassandra-driver >= 2.1.3"
+        ]
+)
