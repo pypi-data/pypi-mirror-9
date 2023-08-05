59c0a7a241147b94a6c4e97098785ea29f33d7ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:24 +0000

    Add package asyncnoti/1.0.5/asyncnoti-1.0.5.tar.gz

---
+++ packages/asyncnoti/asyncnoti-1.0.5.tar.gz/asyncnoti-1.0.5/asyncnoti/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+from .asyncnoti import Asyncnoti
+
+__all__ = [
+    'Asyncnoti',
+]
---
+++ packages/asyncnoti/asyncnoti-1.0.5.tar.gz/asyncnoti-1.0.5/setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+
+setup(
+    name='asyncnoti',
+    packages=['asyncnoti'],
+    version='1.0.5',
+    description='Library to interact with the Asyncnoti API',
+    url='https://github.com/asyncnoti/asyncnoti-python',
+    download_url = 'https://github.com/asyncnoti/asyncnoti-python/tarball/1.0.4',
+    author='Asyncnoti',
+    author_email='admin@asyncnoti.com',
+    keywords=['asyncnoti', 'websockets', 'realtime'],
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Topic :: Internet :: WWW/HTTP",
+    ],
+    license='MIT',
+    install_requires=['six'],
+    tests_require=['nose', 'mock', 'coverage'],
+    test_suite='asyncnoti_tests',
+)
