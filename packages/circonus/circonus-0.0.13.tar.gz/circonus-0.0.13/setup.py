0a391fb4b9691719f8cea12993542b1fd7f93d17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:36 +0000

    Add package circonus/0.0.13/circonus-0.0.13.tar.gz

---
+++ packages/circonus/circonus-0.0.13.tar.gz/circonus-0.0.13/setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+from setuptools import find_packages, setup
+
+import codecs
+
+import circonus
+
+
+with codecs.open("README.rst", "r", "utf-8") as f:
+    readme = f.read()
+with codecs.open("HISTORY.rst", "r", "utf-8") as f:
+    history = f.read()
+
+setup(
+    name="circonus",
+    version=circonus.__version__,
+    description="Interact with the Circonus REST API.",
+    long_description=readme + "\n\n" + history,
+    author="Monetate Inc.",
+    author_email="kmolendyke@monetate.com",
+    url="https://github.com/monetate/circonus",
+    license="MIT",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 2.7"
+    ],
+    keywords="circonus monitoring analytics",
+    packages=find_packages(),
+    install_requires=["colour", "requests"],
+    setup_requires=["colour"]
+)
