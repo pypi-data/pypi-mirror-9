0a2ef8de3287172051c956ee6ba844c13930d9af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:26 +0000

    Add package pompei/0.1.01/pompei-0.1.01.tar.gz

---
+++ packages/pompei/pompei-0.1.01.tar.gz/pompei-0.1.01/pompei/__main__.py
@@ -0,0 +1,20 @@
+#! /usr/bin/env python
+
+"""
+For the command line interface. Not finished.
+
+Pompei, photo mosaic creations with python.
+
+Usage:
+  python pompei [options]
+  
+Options:
+  --moviefile=<filename>
+  --folder=<foldername>    
+  --movie_fps=<fps>      
+  --movieframe=<filename> <time>
+  --signature=<sx> <sy>
+  --resulution=<rx> <ry>
+  --help -h              Show this screen.
+  
+"""
