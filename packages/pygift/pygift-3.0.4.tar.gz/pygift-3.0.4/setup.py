c5a61b214134e114924c92a870b17bbc22917dc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:33 +0000

    Add package pygift/3.0.4/pygift-3.0.4.tar.gz

---
+++ packages/pygift/pygift-3.0.4.tar.gz/pygift-3.0.4/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+
+setup(
+    name='pygift',
+    version='3.0.4',
+    author='Denis Orlikhin',
+    author_email='qbikk@ya.ru',
+    url='https://github.com/overplumbum/pygift',
+    scripts=['pygift'],
+    license='apache-2.0',
+)
