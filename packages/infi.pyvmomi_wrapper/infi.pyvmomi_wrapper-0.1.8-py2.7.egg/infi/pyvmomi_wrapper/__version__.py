2d9f5cd1586300892b9a7b7e317c8a36e45710c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:42 +0000

    Add package infi.pyvmomi_wrapper/0.1.8/infi.pyvmomi_wrapper-0.1.8-py2.7.egg

---
+++ packages/infi.pyvmomi_wrapper/infi.pyvmomi_wrapper-0.1.8-py2.7.egg/infi/pyvmomi_wrapper/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.1.8"
+__git_commiter_name__ = "Guy Rozendorn"
+__git_commiter_email__ = "guy@rzn.co.il"
+__git_branch__ = '(detached from v0.1.8)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = 'edb000d472e2e4eef9bab406698a8e50d602c567'
+__git_head_subject__ = 'Finished release v0.1.8.'
+__git_head_message__ = '* release/v0.1.8:\n  remove unused traversal spec\n  fix VirtualApp traversal in VirtualMachinePropertyCollector and refactor traversal spec creation\n  update homepage'
+__git_dirty_diff__ = ''
