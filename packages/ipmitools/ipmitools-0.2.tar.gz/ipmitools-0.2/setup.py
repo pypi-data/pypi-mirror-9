7c36d55c52c0c757664f9eb3fd0e3b78dbe26337
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:56 +0000

    Add package ipmitools/0.2/ipmitools-0.2.tar.gz

---
+++ packages/ipmitools/ipmitools-0.2.tar.gz/ipmitools-0.2/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+      name='ipmitools',
+      packages=['ipmi'],
+      version='0.2',
+      description='Run ipmitool commands on consoles',
+      author='Raghu Udiyar',
+      author_email='raghusiddarth@gmail.com',
+      url='https://github.com/raags/ipmitool',
+      download_url = 'https://github.com/raags/ipmitool/tarball/0.2',
+      install_requires=['pexpect', 'argparse'],
+      entry_points = {
+        'console_scripts': [
+        'ipmitool.py=ipmi.ipmicli:main'
+        ],
+      },
+     )
