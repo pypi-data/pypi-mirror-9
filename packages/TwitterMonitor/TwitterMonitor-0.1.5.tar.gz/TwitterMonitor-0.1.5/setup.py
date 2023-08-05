432ede1fb0209765183bb66945f0d8a131315dd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:05 +0000

    Add package TwitterMonitor/0.1.5/TwitterMonitor-0.1.5.tar.gz

---
+++ packages/TwitterMonitor/TwitterMonitor-0.1.5.tar.gz/TwitterMonitor-0.1.5/setup.py
@@ -0,0 +1,47 @@
+from setuptools import setup
+from twitter_monitor.common import version
+import os
+
+# Compile a list of packages available
+packages = []
+for dirpath, dirnames, filenames in os.walk("twitter_monitor"):
+    if "__pycache__" in dirpath:
+        continue
+
+    packages.append(dirpath.replace("/", "."))
+
+packages = set(packages)
+
+long_description = ""
+try:
+    with file("docs/about.rst") as f:
+        long_description = f.read()
+except Exception, e:
+    pass
+
+setup(
+    name="TwitterMonitor",
+    version=version,
+    author="Alisson R. Perez",
+    author_email="alissonperez@gmail.com",
+    url="https://github.com/alissonperez/TwitterMonitor",
+    packages=packages,
+    classifiers=[
+        "Programming Language :: Python :: 2 :: Only",
+        "Development Status :: 2 - Pre-Alpha",
+        "Operating System :: MacOS",
+        "Operating System :: Unix",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+        "Intended Audience :: Information Technology",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: System :: Monitoring",
+        "Topic :: Utilities",
+    ],
+    install_requires=[
+        "tweepy>=2.3",
+    ],
+    description="Small library to create monitoring routines with Twitter DM. (Python 2.x)",
+    long_description=long_description
+)
