3be56a34e22119da81645133fe9f79b5a30017a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:20 +0000

    Add package evalidate/0.4/evalidate-0.4.tar.gz

---
+++ packages/evalidate/evalidate-0.4.tar.gz/evalidate-0.4/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='evalidate',
+      version='0.4',
+      description='Eval code validator',
+      url='https://bitbucket.org/yaroslaff/evalidate',
+      author='Yaroslav Polyakov',
+      author_email='xenon@sysattack.com',
+      license='MIT',
+      packages=['evalidate'],
+      zip_safe=False)
+
