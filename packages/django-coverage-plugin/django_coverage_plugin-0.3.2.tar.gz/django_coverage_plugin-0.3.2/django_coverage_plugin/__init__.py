063154de1ceefc62d3484873e000dbf0e150f82f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:43 +0000

    Add package django-coverage-plugin/0.3.2/django_coverage_plugin-0.3.2.tar.gz

---
+++ packages/django-coverage-plugin/django_coverage_plugin-0.3.2.tar.gz/django_coverage_plugin-0.3.2/django_coverage_plugin/__init__.py
@@ -0,0 +1,4 @@
+from .plugin import Plugin
+
+# silence pyflakes
+Plugin
---
+++ packages/django-coverage-plugin/django_coverage_plugin-0.3.2.tar.gz/django_coverage_plugin-0.3.2/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='django_coverage_plugin',
+    version='0.3.2',
+    description='Django template coverage.py plugin',
+    author='Ned Batchelder',
+    author_email='ned@nedbatchelder.com',
+    url='https://github.com/nedbat/django_coverage_plugin',
+    packages=['django_coverage_plugin'],
+    install_requires=[
+        # If you change this, update tox.ini and requirements.txt also.
+        'Django >= 1.4',
+        'coverage >= 4.0a4',
+        'six >= 1.4.0',
+    ],
+)
