dc8a8c03bb8aa137058dcc457d33cf32670dc685
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:51 +0000

    Add package ninarow/0.0.1/ninarow-0.0.1.tar.gz

---
+++ packages/ninarow/ninarow-0.0.1.tar.gz/ninarow-0.0.1/ninarow/gui.py
@@ -0,0 +1,283 @@
+import Tkinter as Tk
+import logging
+from ninarow import logic
+import Queue
+import time
+import threading
+
+LOGGER = logging.getLogger("ninarow GUI")
+LOGGER.addHandler(logging.StreamHandler())
+LOGGER.setLevel(logging.DEBUG)
+
+
+def show_pre_game_menu(old, root):
+    if old:
+        old.destroy()
+    frame = PreGameMenu(root)
+    frame.pack(side="top", fill="both", expand="true", padx=4, pady=4)
+
+
+def start_game(old, root, logic_board):
+    old.message_queue.put("stop")
+    old.destroy()
+    game_board = GameBoard(root, logic_board)
+    game_board.pack(side="top", fill="both", expand="true", padx=4, pady=4)
+
+
+class IntSelectionWidget(Tk.Frame):
+    def __init__(self, parent, label="", start_val=0, min_val=None, max_val=None, max_error_message="maximal value reached",
+                 min_error_message="minimal value reached", on_error=None):
+        Tk.Frame.__init__(self, parent)
+        self.error_function = on_error
+        self.max_error_message = max_error_message
+        self.min_error_message = min_error_message
+        self.min_val = min_val
+        self.max_val = max_val
+        self.top_label = Tk.Label(master=self, text=label, bg="light cyan")
+        self.spin_box = Tk.Spinbox(self, from_=min_val, to=max_val, command=self.value_changed)
+        self.spin_box.delete(0, Tk.END)
+        self.spin_box.insert(0, start_val)
+        # track values to respond to increase/decrease
+        self.old_val = start_val
+        self.top_label.pack()
+        self.spin_box.pack()
+
+    def get(self):
+        return int(self.spin_box.get())
+
+    def value_changed(self):
+        if self.old_val == self.spin_box.get():
+            if int(self.old_val) == self.min_val:
+                self.__error(self.min_error_message)
+            else:
+                self.__error(self.max_error_message)
+        self.old_val = self.spin_box.get()
+
+    def __error(self, message):
+        if self.error_function:
+            self.error_function(message)
+
+
+class PreGameMenu(Tk.Frame):
+
+    def __init__(self, parent):
+
+        self.parent = parent
+
+        self.message_rate = 2
+        #gracefully stop thread
+        # self.protocol("WM_DELETE_WINDOW", op"))
+
+        Tk.Frame.__init__(self, parent)
+
+        # run a thread to iterate the queue of messages and display them for a short time
+        self.message = Tk.StringVar()
+        self.message_label = Tk.Label(master=self, textvariable=self.message, fg="red")
+        self.message_queue = Queue.Queue(2)
+        self.messenger_thread = threading.Thread(target=self.messenger)
+        self.messenger_thread.start()
+
+        self.players_widget = IntSelectionWidget(
+            self,
+            label="Number of players",
+            start_val=2,
+            min_val=2,
+            max_val=6,
+            max_error_message="Maximal players -> 6",
+            min_error_message="Minimal players -> 2",
+            on_error=self.display_message
+        )
+        self.players_widget.pack()
+
+        self.rows_widget = IntSelectionWidget(
+            self,
+            label="Number of rows",
+            start_val=6,
+            min_val=3,
+            max_val=15,
+            max_error_message="Maximal rows -> 15",
+            min_error_message="Minimal rows -> 3",
+            on_error=self.display_message
+        )
+        self.rows_widget.pack()
+
+        self.columns_widget = IntSelectionWidget(
+            self,
+            label="Number of columns",
+            start_val=7,
+            min_val=3,
+            max_val=15,
+            max_error_message="Maximal columns -> 15",
+            min_error_message="Minimal columns -> 3",
+            on_error=self.display_message
+        )
+        self.columns_widget.pack()
+
+        self.goal_widget = IntSelectionWidget(
+            self,
+            label="Sequence goal",
+            start_val=4,
+            min_val=3,
+            max_val=10,
+            max_error_message="Maximal goal is 10",
+            min_error_message="Minimal goal is 3",
+            on_error=self.display_message
+        )
+        self.goal_widget.pack()
+
+        self.start_button = Tk.Button(master=self, text="Start game!", command=threading.Thread(target=self.start_game).start)
+        self.start_button.pack()
+        self.message_label.pack()
+
+    def start_game(self):
+        self.message_rate = 0
+        self.message_queue.put("stop")
+        start_game(
+            self,
+            self.parent,
+            logic.Board(
+                self.players_widget.get(),
+                rows=self.rows_widget.get(),
+                columns=self.columns_widget.get(),
+                goal=self.goal_widget.get())
+        )
+    def destroy(self):
+        self.message_queue.put("stop")
+        Tk.Frame.destroy(self)
+
+    def display_message(self, message):
+        if self.message_queue.full():
+            # block for one element
+            self.message.set("STOP CLICKING STUFF!")
+        else:
+            self.message_queue.put(message)
+
+    def messenger(self):
+        while True:
+            message = self.message_queue.get()
+            if not message == "stop":
+                self.message.set(message)
+                time.sleep(self.message_rate)
+                self.message.set("")
+            else:
+                # stop thread
+                LOGGER.debug("thread stopped")
+                break
+
+
+class GameBoard(Tk.Frame):
+
+    def __init__(self, parent, board):
+        self.parent = parent
+
+        self.board = board
+        self.rows, self.columns = board.get_size()
+        self.cell_size = 40
+        canvas_width = self.columns * self.cell_size
+        canvas_height = self.rows * self.cell_size
+
+        Tk.Frame.__init__(self, parent)
+        self.player_indicator = Tk.StringVar(self)
+        self.player_indicator.set("%s players starts." % self.board.current_player.get_color())
+        self.player_indicator_label = Tk.Label(self, background="bisque", textvariable=self.player_indicator)
+        self.player_indicator_label.pack()
+        self.canvas = Tk.Canvas(self, borderwidth=0, highlightthickness=0,
+                                width=canvas_width, height=canvas_height, background="bisque")
+        self.canvas.pack(side="top", fill="both", expand=True, padx=2, pady=2)
+
+        self.canvas.bind("<Configure>", self.resize_event)
+        self.canvas.bind("<Button-1>", self.put_one_event)
+
+        self.undo_button = Tk.Button(self, fg="red", bg="black", command=self.undo)
+        self.undo_button.pack()
+
+        self.win_button = None
+
+    def undo(self):
+        self.board.undo()
+        self.refresh()
+
+    def resize_event(self, event):
+        x_size = int((event.width-1) / self.columns)
+        y_size = int((event.height-1) / self.rows)
+        self.cell_size = min(x_size, y_size)
+        self.refresh()
+
+    def put_one(self, column, player):
+        try:
+            self.board.put_one(column, player)
+        except logic.NotYourTurnError as e:
+            raise e
+        except logic.LocationTakenError as e:
+            raise e
+        self.refresh()
+
+    def refresh(self):
+        self.configure(bg=self.board.current_player.get_color())
+        self.canvas.delete("pieces")
+        for row in range(self.rows):
+            for col in range(self.columns):
+                x1 = (col * self.cell_size)
+                y1 = (row * self.cell_size)
+                x2 = x1 + self.cell_size
+                y2 = y1 + self.cell_size
+                color = (self.board.get_piece(row, col) and self.board.get_piece(row, col).owner.get_color()) or "white"
+                self.canvas.create_oval(x1, y1, x2, y2, outline="black", fill=color, tags="pieces")
+        self.canvas.tag_raise("pieces")
+        LOGGER.debug("redrew canvas")
+
+    def get_normalized_coords(self, event):
+        return event.x / self.cell_size, event.y / self.cell_size
+
+    def put_one_event(self, event):
+        player = self.board.current_player
+        column = self.get_normalized_coords(event)[0]
+        self.player_indicator.set("%s player plays column %s" % (player.get_color(), str(column)))
+        self.put_one(column, player)
+        if self.board.board_won():
+            self.announce_winner(self.board.board_won())
+            self.player_indicator.set("%s player won the game!" % self.board.get_winner())
+
+    def restart_game(self):
+        show_pre_game_menu(self, self.parent)
+
+    def announce_winner(self, player):
+        self.win_button = Tk.Button(
+            self, text="Player %s wins!" % str(player), command=self.restart_game
+        )
+        self.win_button.pack()
+
+
+# image comes from the silk icon set which is under a Creative Commons
+# license. For more information see http://www.famfamfam.com/lab/icons/silk/
+imagedata = '''
+    R0lGODlhEAAQAOeSAKx7Fqx8F61/G62CILCJKriIHM+HALKNMNCIANKKANOMALuRK7WOVLWPV9eR
+    ANiSANuXAN2ZAN6aAN+bAOCcAOKeANCjKOShANKnK+imAOyrAN6qSNaxPfCwAOKyJOKyJvKyANW0
+    R/S1APW2APW3APa4APe5APm7APm8APq8AO28Ke29LO2/LO2/L+7BM+7BNO6+Re7CMu7BOe7DNPHA
+    P+/FOO/FO+jGS+/FQO/GO/DHPOjBdfDIPPDJQPDISPDKQPDKRPDIUPHLQ/HLRerMV/HMR/LNSOvH
+    fvLOS/rNP/LPTvLOVe/LdfPRUfPRU/PSU/LPaPPTVPPUVfTUVvLPe/LScPTWWfTXW/TXXPTXX/XY
+    Xu/SkvXZYPfVdfXaY/TYcfXaZPXaZvbWfvTYe/XbbvHWl/bdaPbeavvadffea/bebvffbfbdfPvb
+    e/fgb/Pam/fgcvfgePTbnfbcl/bfivfjdvfjePbemfjelPXeoPjkePbfmvffnvbfofjlgffjkvfh
+    nvjio/nnhvfjovjmlvzlmvrmpvrrmfzpp/zqq/vqr/zssvvvp/vvqfvvuPvvuvvwvfzzwP//////
+    ////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////yH+FUNyZWF0ZWQgd2l0aCBU
+    aGUgR0lNUAAh+QQBCgD/ACwAAAAAEAAQAAAIzAD/CRxIsKDBfydMlBhxcGAKNIkgPTLUpcPBJIUa
+    +VEThswfPDQKokB0yE4aMFiiOPnCJ8PAE20Y6VnTQMsUBkWAjKFyQaCJRYLcmOFipYmRHzV89Kkg
+    kESkOme8XHmCREiOGC/2TBAowhGcAyGkKBnCwwKAFnciCAShKA4RAhyK9MAQwIMMOQ8EdhBDKMuN
+    BQMEFPigAsoRBQM1BGLjRIiOGSxWBCmToCCMOXSW2HCBo8qWDQcvMMkzCNCbHQga/qMgAYIDBQZU
+    yxYYEAA7
+'''
+
+
+def main():
+    root = Tk.Tk()
+    root.protocol("WM_DELETE_WINDOW", root.destroy)
+    show_pre_game_menu(None, root)
+    root.mainloop()
+
+if "__main__" == __name__:
+    main()
---
+++ packages/ninarow/ninarow-0.0.1.tar.gz/ninarow-0.0.1/ninarow/logic.py
@@ -0,0 +1,242 @@
+__author__ = 'reuts'
+import logging
+import os
+import unittest
+from random import choice
+
+LOGGER = logging.getLogger("ninarow")
+LOGGER.addHandler(logging.StreamHandler())
+LOGGER.setLevel(logging.DEBUG)
+
+
+class Piece(object):
+
+    def __init__(self, x, y, owner):
+        self.owner = owner
+        self.location = (x, y,)
+
+    def __str__(self):
+        return "[Piece at %s taken by %s]" % (self.location, self.owner)
+
+
+class LocationTakenError(Exception):
+    pass
+
+
+class NotYourTurnError(Exception):
+    pass
+
+
+class Board(object):
+
+    def __init__(self, number_of_players, rows=6, columns=7, goal=4):
+        self.moves = list()
+        self._rows = rows
+        self._columns = columns
+        self._goal = goal
+
+        self._board = [[None for _ in range(columns)] for _ in range(rows)]
+
+        self._players = []
+        for player in range(1, number_of_players+1):
+            self._players.append(Player(player))
+
+        self.current_player = choice(self._players)
+
+        LOGGER.debug("%d X %x board initialized: %s" % (rows, columns, str(self._board)))
+
+    def get_size(self):
+        return self._rows, self._columns
+
+    def __win_by_row_from_location(self, row, col, player, left):
+        try:
+
+            if left == 0:
+                return True
+
+            piece = self._board[row][col]
+            if not piece:
+                return False
+
+            return piece.owner == player and self.__win_by_row_from_location(row+1, col, player, left-1)
+
+        except IndexError:
+            return False
+
+    def __win_by_col_from_location(self, row, col, player, left):
+        try:
+
+            if left == 0:
+                return True
+
+            piece = self._board[row][col]
+
+            if not piece:
+                return False
+
+            return piece.owner == player and self.__win_by_col_from_location(row, col+1, player, left-1)
+
+        except IndexError:
+            return False
+
+    def __win_from_location(self, row, col, player, left):
+        return self.__win_by_col_from_location(
+            row, col, player, left
+        ) or self.__win_by_row_from_location(
+            row, col, player, left
+        )
+
+    def board_won(self):
+        for row in range(self._rows):
+            for col in range(self._columns):
+                piece = self._board[row][col]
+                if not piece:
+                    continue
+                player = piece.owner
+                if player.id == -1:
+                    continue
+                if self.__win_from_location(row, col, player, self._goal):
+                    return player
+
+    def move_turn_to_next_player(self):
+        next_player_index = (self._players.index(self.current_player) + 1) % len(self._players)
+        self.current_player = self._players[next_player_index]
+
+    def put_one(self, _column, _player):
+        if self.current_player != _player:
+            raise NotYourTurnError("This is player %s's turn! Not player %s's." % (self.current_player, _player))
+        if self.board_won():
+            raise BoardWonError("Board already won by player: %s" % self.board_won())
+        for row in reversed(range(self._rows)):
+            _piece = self._board[row][_column]
+
+            LOGGER.debug("Trying to put %s in %d, %d - %s" % (_player, row, _column, _piece))
+            if not _piece:
+                self._board[row][_column] = Piece(row, _column, _player)
+                self.move_turn_to_next_player()
+                self.moves.append((self.current_player, row, _column,))
+                return row, _column
+
+        raise LocationTakenError()
+
+    def undo(self):
+        if len(self.moves) > 0:
+            player, row, column = self.moves.pop()
+            print row, column
+            self._board[row][column] = None
+            self.move_turn_to_next_player()
+
+    def get_piece(self, x, y):
+        return self._board[x][y]
+
+    def get_players(self):
+        return self._players
+
+    def __str__(self):
+        return os.linesep.join(map(str, [map(lambda x: (x and x.owner.id) or -1, self._board[row]) for row in range(self._rows)]))
+
+
+class Player(object):
+
+    def __init__(self, _id):
+        self.id = _id
+
+    def put_one(self, _board, _location):
+        LOGGER.debug("Player %s trying to put a piece in %s" % (self, _location))
+        return _board.put_one(_location, self)
+
+    def get_color(self):
+        return {
+            -1: "white",
+            1: "blue",
+            2: "yellow",
+            3: "green",
+            4: "red",
+            5: "purple",
+            6: "orange"
+        }[self.id]
+
+    def __str__(self):
+        return "Player: %d" % self.id
+
+
+class BoardWonError(Exception):
+    pass
+
+
+class TestNInARow(unittest.TestCase):
+
+    def setUp(self):
+        self._board = Board(2)
+        self.starting_player, self.second_player = self._board._players
+        if not self.starting_player == self._board.current_player:
+            # switch in case we got the turns reversed
+            self.second_player = self.starting_player
+            self.starting_player = self._board.current_player
+
+        print "starting player : %s" % self.starting_player
+        print "second player : %s" % self.second_player
+
+    def test_simple_put(self):
+        self.starting_player.put_one(self._board, 0)
+        assert self._board._board[-1][0].owner is self.starting_player, self._board._board[-1][0].owner
+
+    def test_put_over(self):
+        self.starting_player.put_one(self._board, 0)
+        self.second_player.put_one(self._board, 1)
+        self.starting_player.put_one(self._board, 0)
+        assert self._board._board[-2][0].owner is self.starting_player, self._board._board[-2][0].owner
+        assert self._board._board[-1][1].owner is self.second_player, self._board._board[-1][1].owner
+
+    def test_vertical_win_with_two_players(self):
+        self.starting_player.put_one(self._board, 0)
+        self.second_player.put_one(self._board, 1)
+        self.starting_player.put_one(self._board, 0)
+        self.second_player.put_one(self._board, 1)
+        self.starting_player.put_one(self._board, 0)
+        self.second_player.put_one(self._board, 1)
+        self.starting_player.put_one(self._board, 0)
+        self.assertRaises(BoardWonError, self.second_player.put_one, self._board, 0)
+
+    def test_double_play_simple(self):
+        self.starting_player.put_one(self._board, 0)
+        self.assertRaises(NotYourTurnError, self.starting_player.put_one, self._board, 0)
+
+    def test_simple_horizontal_win_2_players(self):
+        self.starting_player.put_one(self._board, 0)
+        self.second_player.put_one(self._board, 0)
+        self.starting_player.put_one(self._board, 1)
+        self.second_player.put_one(self._board, 1)
+        self.starting_player.put_one(self._board, 2)
+        self.second_player.put_one(self._board, 2)
+        self.starting_player.put_one(self._board, 3)
+        self.assertRaises(BoardWonError, self.second_player.put_one, self._board, 3)
+
+    def test_horizontal_win_edge(self):
+        self.starting_player.put_one(self._board, 6)
+        self.second_player.put_one(self._board, 6)
+        self.starting_player.put_one(self._board, 5)
+        self.second_player.put_one(self._board, 5)
+        self.starting_player.put_one(self._board, 4)
+        self.second_player.put_one(self._board, 4)
+        self.starting_player.put_one(self._board, 3)
+        print str(self._board)
+        self.assertRaises(BoardWonError, self.second_player.put_one, self._board, 3)
+
+    def test_vertical_win_edge(self):
+        self.starting_player.put_one(self._board, 1)
+        self.second_player.put_one(self._board, 1)
+        self.starting_player.put_one(self._board, 1)
+        self.second_player.put_one(self._board, 2)
+        self.starting_player.put_one(self._board, 1)
+        self.second_player.put_one(self._board, 3)
+        self.starting_player.put_one(self._board, 1)
+        self.second_player.put_one(self._board, 4)
+        self.starting_player.put_one(self._board, 1)
+        self.assertRaises(BoardWonError, self.second_player.put_one, self._board, 3)
+
+if "__main__" == __name__:
+    print "Starting"
+    board = Board(2)
+    player1 = Player(1)
+    player1.put_one(board, 1)
