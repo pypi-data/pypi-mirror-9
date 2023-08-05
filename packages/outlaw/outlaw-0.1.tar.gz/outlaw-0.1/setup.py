c47988d61e583a3e60428081cbb1bf00125c009a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:52 +0000

    Add package outlaw/0.1/outlaw-0.1.tar.gz

---
+++ packages/outlaw/outlaw-0.1.tar.gz/outlaw-0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'outlaw',
+  packages = ['outlaw'], # this must be the same as the name above
+  version = '0.1',
+  description = 'Outlaw.py',
+  author = 'Josh Price',
+  author_email = 'josh@joshprice.net',
+  url = 'https://github.com/jprice/outlaw.py', # use the URL to the github repo
+  download_url = 'https://github.com/jprice/outlaw.py/tarball/0.1', # I'll explain this in a second
+  keywords = ['testing', 'logging', 'example'], # arbitrary keywords
+  classifiers = []
+)
