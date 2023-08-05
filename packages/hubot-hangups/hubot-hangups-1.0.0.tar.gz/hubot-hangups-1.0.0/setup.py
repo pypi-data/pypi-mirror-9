1d5c404c034375e79456ab44fbf8a5d5842893ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:42 +0000

    Add package hubot-hangups/1.0.0/hubot-hangups-1.0.0.tar.gz

---
+++ packages/hubot-hangups/hubot-hangups-1.0.0.tar.gz/hubot-hangups-1.0.0/setup.py
@@ -0,0 +1,66 @@
+from setuptools import setup, find_packages  # Always prefer setuptools over distutils
+from codecs import open  # To use a consistent encoding
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the relevant file
+with open(path.join(here, 'DESCRIPTION.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='hubot-hangups',
+
+    # Versions should comply with PEP440.  For a discussion on single-sourcing
+    # the version across setup.py and the project code, see
+    # https://packaging.python.org/en/latest/single_source_version.html
+    version='1.0.0',
+
+    description='A hubot adapter for Google Hangouts',
+    long_description=long_description,
+
+    # The project's main homepage.
+    url='https://github.com/groupby/hubot-hangups',
+
+    # Author details
+    author='Eric Hacke',
+    author_email='eric.hacke@groupbyinc.com',
+
+    # Choose your license
+    license='MIT',
+
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        'Development Status :: 4 - Beta',
+
+        'Intended Audience :: Developers',
+        'Topic :: Communications :: Chat',
+
+        'License :: OSI Approved :: MIT License',
+
+        # Supported python versions:
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
+
+    keywords='hubot adapter google hangouts hangups',
+
+    # You can just specify the packages manually here if your project is
+    # simple. Or you can use find_packages().
+    packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
+
+    # List run-time dependencies here.  These will be installed by pip when your
+    # project is installed. For an analysis of "install_requires" vs pip's
+    # requirements files see:
+    # https://packaging.python.org/en/latest/requirements.html
+    install_requires=['hangups==0.2.4', 'requests==2.2.1', 'cherrypy']#,
+
+    # To provide executable scripts, use entry points in preference to the
+    # "scripts" keyword. Entry points provide cross-platform support and allow
+    # pip to create the appropriate form of executable for the target platform.
+    #entry_points={
+    #    'console_scripts': [
+    #        'sample=sample:main',
+    #    ],
+    #},
+)
