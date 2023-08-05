e88116778d089a35e5130d2fe22e1dd8de4d677d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:18 +0000

    Add package simplio/0.1/simplio-0.1.tar.gz

---
+++ packages/simplio/simplio-0.1.tar.gz/simplio-0.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+
+setup(
+    name='simplio',
+    version='0.1',
+    description='Simplest-case command-line input/output',
+    long_description=(
+        'Simplio is a Python function decorator that applies an input file '
+        'object and an output file object as arguments to the decorated '
+        'function. It determines this based on STDIN or the presence of '
+        'command-line arguments.'),
+    url='https://github.com/josephl/simplio',
+    author='Joseph Lee',
+    author_email='joe.lee.three.thousand@gmail.com',
+    license='MIT',
+    keywords='input output file io',
+)
