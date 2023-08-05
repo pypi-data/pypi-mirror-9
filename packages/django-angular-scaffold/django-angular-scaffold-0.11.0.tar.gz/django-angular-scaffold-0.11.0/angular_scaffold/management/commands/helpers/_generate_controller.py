a057fc4a40e54757e7dc6e99572f2e512256b9a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:43 +0000

    Add package django-angular-scaffold/0.11.0/django-angular-scaffold-0.11.0.tar.gz

---
+++ packages/django-angular-scaffold/django-angular-scaffold-0.11.0.tar.gz/django-angular-scaffold-0.11.0/angular_scaffold/management/commands/helpers/_generate_controller.py
@@ -0,0 +1,14 @@
+import os
+from string import Template
+
+_template = Template("""app.controller("${name}Controller", function ($$scope, $$location, $$log) {
+    'use strict';
+    $$log.debug("${name} Controller Initialized");
+});""")
+
+
+def generate_controller(directory, name):
+    controller = os.path.join("app", "controllers", name.lower() + "Controller.js")
+    title = name.title()
+    with open(os.path.join(directory, 'assets', controller), 'w') as f:
+        f.write(_template.substitute(name=title))
