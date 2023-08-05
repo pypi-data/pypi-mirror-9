4632504122c9273cdb9f90a077fbb6fe64475520
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:41 +0000

    Add package pynhost/0.2.2/pynhost-0.2.2.tar.gz

---
+++ packages/pynhost/pynhost-0.2.2.tar.gz/pynhost-0.2.2/pynhost/grammarbase.py
@@ -0,0 +1,33 @@
+import logging
+from pynhost import ruleparser
+
+class GrammarBase:
+    def __init__(self):
+        self.rules = []
+        self.recording_macros = {}
+        self.mapping = {}
+        self.dictionary = {}
+
+    def _is_loaded(self):
+        return True
+
+    def _begin_recording_macro(self, rule_name):
+        logging.info("Started recording macro '{}' for grammar {}".format(rule_name, self))
+        self.recording_macros[rule_name] = []
+
+    def _finish_recording_macro(self, rule_name):
+        logging.info("Finished recording macro '{}' for grammar {}".format(rule_name, self))
+        rule = ruleparser.Rule(rule_name, self.recording_macros[rule_name][:-1], self)
+        self.rules.append(rule)
+        del self.recording_macros[rule_name]
+
+    def _run_command(self, command):
+        pass
+
+    def _load_rule(self, rule, actions):
+        pass
+
+def set_rules(grammar):
+    for rule_text, actions in grammar.mapping.items():
+        rule = ruleparser.Rule(rule_text, actions, grammar, dictionary=grammar.dictionary)
+        grammar.rules.append(rule)
