8b2a1d82b0db7f0144a3dc164bfd4410755ccc98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:22 +0000

    Add package Flask-Mandrill/0.2/Flask-Mandrill-0.2.tar.gz

---
+++ packages/Flask-Mandrill/Flask-Mandrill-0.2.tar.gz/Flask-Mandrill-0.2/HISTORY.rst
@@ -0,0 +1,20 @@
+.. :changelog:
+
+Release History
+---------------
+
+
+0.2 (2015-01-15)
+++++++++++++++++
+
+- Adds support for sending template emails.
+
+0.1.1 (2013-10-25)
+++++++++++++++++++
+
+- Added config option MANDRILL_DEFAULT_FROM.
+
+0.1.0 (2012-10-25)
+++++++++++++++++++
+
+- First release.
---
+++ packages/Flask-Mandrill/Flask-Mandrill-0.2.tar.gz/Flask-Mandrill-0.2/setup.py
@@ -0,0 +1,64 @@
+"""
+Flask-Mandrill
+==============
+
+A Flask Extension to remove some of the boiler plate encountered when
+sending emails with `Mandrill <http://www.mandrill.com/>`_
+
+Installation
+````````````
+
+.. code:: bash
+
+    $ pip install flask-mandrill
+
+
+Usage
+`````
+
+.. code:: python
+
+        from flask import Flask
+        from flask.ext.mandrill import Mandrill
+
+        app = Flask(__name__)
+        app.config['MANDRILL_API_KEY'] = 'your api key'
+        mandrill = Mandrill(app)
+        mandrill.send_email(
+            from_email='someone@yourdomain.com',
+            to=[{'email': 'someoneelse@someotherdomain.com'}],
+            text='Hello World'
+        )
+"""
+
+from setuptools import setup
+
+
+setup(
+    name='Flask-Mandrill',
+    version='0.2',
+    url='http://github.com/volker48/flask-mandrill',
+    license='MIT',
+    author='Marcus McCurdy',
+    author_email='marcus.mccurdy@gmail.com',
+    description='Adds Mandrill support to Flask applications',
+    long_description=__doc__ + '\n\n' +
+                     open('HISTORY.rst').read(),
+    py_modules=['flask_mandrill'],
+    zip_safe=False,
+    platforms='any',
+    install_requires=[
+        'Flask',
+        'requests'
+        ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+        ]
+)
