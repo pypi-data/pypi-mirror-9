957c43284a7aca75051db475f71bbdfa9738803d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:37 +0000

    Add package trytond_nereid_catalog_tree/3.2.1.0/trytond_nereid_catalog_tree-3.2.1.0.tar.gz

---
+++ packages/trytond_nereid_catalog_tree/trytond_nereid_catalog_tree-3.2.1.0.tar.gz/trytond_nereid_catalog_tree-3.2.1.0/setup.py
@@ -0,0 +1,146 @@
+#!/usr/bin/env python
+# This file is part of Tryton.  The COPYRIGHT file at the top level of
+# this repository contains the full copyright notices and license terms.
+import sys
+import re
+import os
+import time
+import unittest
+import ConfigParser
+from setuptools import setup, Command
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+class SQLiteTest(Command):
+    """
+    Run the tests on SQLite
+    """
+    description = "Run tests on SQLite"
+
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        from trytond.config import CONFIG
+        CONFIG['db_type'] = 'sqlite'
+        os.environ['DB_NAME'] = ':memory:'
+
+        from tests import suite
+        test_result = unittest.TextTestRunner(verbosity=3).run(suite())
+
+        if test_result.wasSuccessful():
+            sys.exit(0)
+        sys.exit(-1)
+
+
+class PostgresTest(Command):
+    """
+    Run the tests on Postgres.
+    """
+    description = "Run tests on Postgresql"
+
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        from trytond.config import CONFIG
+        CONFIG['db_type'] = 'postgresql'
+        CONFIG['db_host'] = 'localhost'
+        CONFIG['db_port'] = 5432
+        CONFIG['db_user'] = 'postgres'
+        CONFIG['db_password'] = 'test'
+
+        os.environ['DB_NAME'] = 'test_' + str(int(time.time()))
+
+        from tests import suite
+        test_result = unittest.TextTestRunner(verbosity=3).run(suite())
+
+        if test_result.wasSuccessful():
+            sys.exit(0)
+        sys.exit(-1)
+
+
+config = ConfigParser.ConfigParser()
+config.readfp(open('tryton.cfg'))
+info = dict(config.items('tryton'))
+for key in ('depends', 'extras_depend', 'xml'):
+    if key in info:
+        info[key] = info[key].strip().splitlines()
+major_version, minor_version, _ = info.get('version', '0.0.1').split('.', 2)
+major_version = int(major_version)
+minor_version = int(minor_version)
+
+requires = [
+    'simplejson',
+    'trytond_nereid>=3.2.0.5,<3.3',
+]
+for dep in info.get('depends', []):
+    if not re.match(r'(ir|res|webdav)(\W|$)', dep):
+        requires.append(
+            'trytond_%s >= %s.%s, < %s.%s' % (
+                dep, major_version, minor_version, major_version,
+                minor_version + 1
+            )
+        )
+requires.append(
+    'trytond >= %s.%s, < %s.%s' % (
+        major_version, minor_version, major_version, minor_version + 1
+    ),
+)
+
+setup(
+    name='trytond_nereid_catalog_tree',
+    version=info.get('version', '0.0.1'),
+    description="Nereid Catalog Tree",
+    author="Openlabs Technologies & consulting (P) Limited",
+    author_email='info@openlabs.co.in',
+    url='http://www.openlabs.co.in',
+    package_dir={'trytond.modules.nereid_catalog_tree': '.'},
+    packages=[
+        'trytond.modules.nereid_catalog_tree',
+        'trytond.modules.nereid_catalog_tree.tests',
+    ],
+    package_data={
+        'trytond.modules.nereid_catalog_tree': info.get('xml', [])
+        + info.get('translation', [])
+        + ['tryton.cfg', 'locale/*.po', 'tests/*.rst', '*.odt']
+        + ['view/*.xml']
+    },
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Plugins',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Tryton',
+        'Topic :: Office/Business',
+    ],
+    license='GPL-3',
+    install_requires=requires,
+    zip_safe=False,
+    entry_points="""
+    [trytond.modules]
+    nereid_catalog_tree = trytond.modules.nereid_catalog_tree
+    """,
+    test_suite='tests',
+    test_loader='trytond.test_loader:Loader',
+    cmdclass={
+        'test': SQLiteTest,
+        'test_on_postgres': PostgresTest,
+    },
+)
