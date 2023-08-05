1476e73adbda3ba7fb427b38d7ea017402949ff4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:54 +0000

    Add package Flask-QiniuStorage/0.9.3/Flask-QiniuStorage-0.9.3.zip

---
+++ packages/Flask-QiniuStorage/Flask-QiniuStorage-0.9.3.zip/Flask-QiniuStorage-0.9.3/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+
+setup(
+    name='Flask-QiniuStorage',
+    version='0.9.3',
+    url='https://github.com/csuzhangxc/Flask-QiniuStorage',
+    license='MIT',
+    author='Zhang Xuecheng',
+    author_email='csuzhangxc@gmail.com',
+    description='Qiniu Storage for Flask',
+    long_description='Qiniu Storage for Flask. Please visit: https://github.com/csuzhangxc/Flask-QiniuStorage',
+    py_modules=['flask_qiniustorage'],
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    keywords='qiniu for flask',
+    package_data={'': ['LICENSE']},
+    install_requires=[
+        'setuptools',
+        'Flask',
+        'qiniu'
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
