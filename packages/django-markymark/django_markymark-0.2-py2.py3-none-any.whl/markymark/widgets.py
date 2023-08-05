d34bbb9bc7bccdb28c825f6f1c6a8dd242cb8569
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:23 +0000

    Add package django-markymark/0.2/django_markymark-0.2-py2.py3-none-any.whl

---
+++ packages/django-markymark/django_markymark-0.2-py2.py3-none-any.whl/markymark/widgets.py
@@ -0,0 +1,13 @@
+import floppyforms.__future__ as forms
+
+from markymark import conf
+
+
+class MarkdownTextarea(forms.Textarea):
+    class Media:
+        css = {'all': conf.MARKYMARK_CSS}
+        js = conf.MARKYMARK_JS
+
+    def __init__(self, *args, **kwargs):
+        super(MarkdownTextarea, self).__init__(*args, **kwargs)
+        self.attrs['data-provide'] = 'init-markdown'
