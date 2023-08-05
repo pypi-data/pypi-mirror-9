ea776667dcfa676b4ce5e621c90cfaee81fff80b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:28 +0000

    Add package scription/0.73.01/scription-0.73.01.zip

---
+++ packages/scription/scription-0.73.01.zip/scription-0.73.01/setup.py
@@ -0,0 +1,32 @@
+from distutils.core import setup
+
+fh = open('README')
+description = fh.read()
+fh.close()
+
+setup( name='scription',
+       version= '0.73.01',
+       license='BSD License',
+       description='simple script parameter parser',
+       long_description=description,
+       url='https://bitbucket.org/stoneleaf/scription',
+       packages=['scription'],
+       package_data={'scription':['CHANGES', 'LICENSE', 'README']},
+       author='Ethan Furman',
+       author_email='ethan@stoneleaf.us',
+       classifiers=[
+            'Development Status :: 4 - Beta',
+            'Intended Audience :: Developers',
+            'Intended Audience :: End Users/Desktop',
+            'Intended Audience :: System Administrators',
+            'License :: OSI Approved :: BSD License',
+            'Programming Language :: Python',
+            'Topic :: Database',
+            'Programming Language :: Python :: 2.4',
+            'Programming Language :: Python :: 2.5',
+            'Programming Language :: Python :: 2.6',
+            'Programming Language :: Python :: 2.7',
+            'Programming Language :: Python :: 3',
+            ],
+    )
+
