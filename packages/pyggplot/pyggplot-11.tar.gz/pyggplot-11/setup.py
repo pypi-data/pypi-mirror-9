bad32e67e614d6a857e7fda61b97da72fde81f04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:49 +0000

    Add package pyggplot/11/pyggplot-11.tar.gz

---
+++ packages/pyggplot/pyggplot-11.tar.gz/pyggplot-11/setup.py
@@ -0,0 +1,22 @@
+try:
+    from setuptools import setup
+except:
+    from distutils.core import setup
+
+setup(
+    name='pyggplot',
+    version='11',
+    packages=['pyggplot',],
+    license='BSD',
+    #url='http://code.google.com/p/pydataframe/',
+    author='Florian Finkernagel',
+    description = "A pythonic wrapper around R's ggplot",
+    author_email='finkernagel@coonabibba.de',
+    long_description=open('README.md').read(),
+    package_data={'': ['LICENSE.txt', 'README.md']},
+    include_package_data=True,    install_requires=[
+        'pandas>=0.15',
+        'rpy2',
+        'ordereddict',
+        ]
+)
