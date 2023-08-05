313126818e5ac4af2a1a190a17c22f050e6a1726
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:03 +0000

    Add package gxargparse/0.1.8/gxargparse-0.1.8.tar.gz

---
+++ packages/gxargparse/gxargparse-0.1.8.tar.gz/gxargparse-0.1.8/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+import sys, os
+
+if sys.argv[-1] == 'publish':
+    os.system("python setup.py sdist bdist_wheel upload")
+    sys.exit()
+
+
+setup(name="gxargparse",
+        version='0.1.8',
+        description='Galaxy drop-in replacement for argparse',
+        author='Eric Rasche',
+        author_email='rasche.eric@yandex.ru',
+        requires=['galaxyxml'],
+        url='https://github.com/erasche/gxargparse',
+        packages=["argparse", "gxargparse"],
+        entry_points={
+            'console_scripts': [
+                    'gxargparse_check_path = gxargparse.check_path:main',
+                ]
+            },
+        classifiers=[
+            'Development Status :: 3 - Alpha',
+            'Operating System :: OS Independent',
+            'Intended Audience :: Developers',
+            'Environment :: Console',
+            'License :: OSI Approved :: Apache Software License',
+            ],
+        )
