4e9e6f2bea927aad3c2e566cb486ff7f1237abfb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:30 +0000

    Add package PyLinkbot/2.0.5/PyLinkbot-2.0.5-cp34-cp34m-macosx_10_6_intel.whl

---
+++ packages/PyLinkbot/PyLinkbot-2.0.5-cp34-cp34m-macosx_10_6_intel.whl/linkbot/demo/getJointAngles.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import linkbot
+import time
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print ("Usage: {0} <Linkbot Serial ID>".format(sys.argv[0]))
+        quit()
+    serialID = sys.argv[1]
+    myLinkbot = linkbot.Linkbot(serialID)
+    myLinkbot.connect()
+
+    print(myLinkbot.getJointAngles())
+    for i in range(1,4):
+        print(myLinkbot.getJointAngle(i))
+
