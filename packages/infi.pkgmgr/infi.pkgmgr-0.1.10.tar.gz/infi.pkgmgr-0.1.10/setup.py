5b996079ce14e56288138c43567626c1b781143f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:03 +0000

    Add package infi.pkgmgr/0.1.10/infi.pkgmgr-0.1.10.tar.gz

---
+++ packages/infi.pkgmgr/infi.pkgmgr-0.1.10.tar.gz/infi.pkgmgr-0.1.10/setup.py
@@ -0,0 +1,52 @@
+
+SETUP_INFO = dict(
+    name = 'infi.pkgmgr',
+    version = '0.1.10',
+    author = 'Guy Rozendorn',
+    author_email = 'guy@rzn.co.il',
+
+    url = 'https://github.com/Infinidat/infi.pkgmgr',
+    license = 'PSF',
+    description = """A simple wrapper for some Linux package managers.""",
+    long_description = """A simple wrapper for some Linux package managers. Currently supports Ubuntu (apt-get) and Red Hat (yum)""",
+
+    # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers = [
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: Python Software Foundation License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+
+    install_requires = ['infi.execute',
+'infi.pyutils',
+'infi.run_as_root',
+'munch',
+'setuptools'],
+    namespace_packages = ['infi'],
+
+    package_dir = {'': 'src'},
+    package_data = {'': []},
+    include_package_data = True,
+    zip_safe = False,
+
+    entry_points = dict(
+        console_scripts = [],
+        gui_scripts = [],
+        ),
+)
+
+if SETUP_INFO['url'] is None:
+    _ = SETUP_INFO.pop('url')
+
+def setup():
+    from setuptools import setup as _setup
+    from setuptools import find_packages
+    SETUP_INFO['packages'] = find_packages('src')
+    _setup(**SETUP_INFO)
+
+if __name__ == '__main__':
+    setup()
+
