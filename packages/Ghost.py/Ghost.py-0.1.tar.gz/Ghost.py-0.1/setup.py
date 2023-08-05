5bf161a13c3b32cdb74a5ead67c2647d11406654
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:40 +0000

    Add package Ghost.py/0.1/Ghost.py-0.1.tar.gz

---
+++ packages/Ghost.py/Ghost.py-0.1.tar.gz/Ghost.py-0.1/setup.py
@@ -0,0 +1,35 @@
+"""
+Ghost.py
+--------
+
+Webkit based webclient.
+
+"""
+from setuptools import setup, find_packages
+from ghost import __version__
+
+
+setup(
+    name='Ghost.py',
+    version=__version__,
+    url='https://github.com/jeanphix/Ghost.py',
+    license='mit',
+    author='Jean-Philippe Serafin',
+    author_email='serafinjp@gmail.com',
+    description='Webkit based webclient.',
+    long_description=__doc__,
+    data_files=[('ghost', ['README.rst',])],
+    package_dir={'ghost': 'ghost'},
+    zip_safe=False,
+    platforms='any',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+)
