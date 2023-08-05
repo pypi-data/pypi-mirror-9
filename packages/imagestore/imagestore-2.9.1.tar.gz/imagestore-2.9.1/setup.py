dcab91f464991d74846f2417da67324e9d5d1b88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:31 +0000

    Add package imagestore/2.9.1/imagestore-2.9.1.tar.gz

---
+++ packages/imagestore/imagestore-2.9.1.tar.gz/imagestore-2.9.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='imagestore',
+    version='2.9.1',
+    packages=find_packages(),
+    install_requires=[
+        'django',
+        'sorl-thumbnail',
+        'django-autocomplete-light',
+        'django-tagging',
+        'swapper'
+    ],
+    author='Pavel Zhukov',
+    author_email='gelios@gmail.com',
+    description='Gallery solution for django projects',
+    long_description=open('README.rst').read(),
+    license='GPL',
+    keywords='django gallery',
+    url='https://github.com/hovel/imagestore',
+    include_package_data=True
+)
