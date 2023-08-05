3158d946814afac8b791ca32b0af6b1005dc7241
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:41 +0000

    Add package redisext/1.0.0/redisext-1.0.0.tar.gz

---
+++ packages/redisext/redisext-1.0.0.tar.gz/redisext-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+setup(
+    name = 'redisext',
+    packages = ['redisext'],
+    version = '1.0.0',
+    description = 'Data models for Redis',
+    author = 'Andrey Gubarev',
+    author_email = 'mylokin@me.com',
+    url = 'https://github.com/mylokin/redisext',
+    keywords = ['redis', 'orm', 'models'],
+    classifiers = [],
+)
