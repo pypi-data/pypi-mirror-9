cae5f117748f32faf2d41e10cca26bf170856f3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:03 +0000

    Add package pypore/0.0.2.dev20150129185922/pypore-0.0.2.dev20150129185922.tar.gz

---
+++ packages/pypore/pypore-0.0.2.dev20150129185922.tar.gz/pypore-0.0.2.dev20150129185922/src/pypore/__init__.py
@@ -0,0 +1,9 @@
+"""
+Created by `@parkin`_ 8/6/13
+
+pypore is a nanopore translocation analysis package.
+
+"""
+
+from pypore.version import version as __version__
+
---
+++ packages/pypore/pypore-0.0.2.dev20150129185922.tar.gz/pypore-0.0.2.dev20150129185922/src/pypore/version.py
@@ -0,0 +1,10 @@
+
+# THIS FILE IS GENERATED FROM SETUP.PY
+short_version = '0.0.2'
+version = '0.0.2'
+full_version = '0.0.2.dev20150129185922'
+git_revision = 'c6406088364fbf03f95685632503eb43562dc788'
+release = False
+
+if not release:
+    version = full_version
