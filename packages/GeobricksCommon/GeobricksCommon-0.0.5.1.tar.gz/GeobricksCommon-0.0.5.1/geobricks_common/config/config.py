58236fa29694987e917bdbaa419e78debc37bab3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:52 +0000

    Add package GeobricksCommon/0.0.5.1/GeobricksCommon-0.0.5.1.tar.gz

---
+++ packages/GeobricksCommon/GeobricksCommon-0.0.5.1.tar.gz/GeobricksCommon-0.0.5.1/geobricks_common/config/config.py
@@ -0,0 +1,36 @@
+import logging
+
+
+config = {
+
+    "settings": {
+        # Logging configurations
+        "logging": {
+            "level": logging.INFO,
+            "format": "%(asctime)s | %(levelname)-8s | %(name)-20s | Line: %(lineno)-5d | %(message)s",
+            "datefmt": "%d-%m-%Y | %H:%M:%s"
+        },
+
+        # Folders
+        "folders": {
+            "tmp": "/tmp",
+            "geoserver_datadir": "../../test_data/geoserver_data_dir/",
+            "storage": "../../test_data/storage/",
+            # this is used by the filesystem to get the (published) layers in the file system
+            "workspace_layer_separator": ":"
+        },
+
+        # Email configurations (for now uses gmail as default client)
+        "email": {
+            "user":  "user",
+            "password": "password"
+        },
+
+        "metadata" : {
+            "url_get_metadata_uid": "http://fenix.fao.org/d3s_dev/msd/resources/metadata/uid/<uid>",
+        }
+    }
+
+}
+
+
