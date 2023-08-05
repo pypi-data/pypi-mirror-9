cd5173035493ffa0e5f07368f6f52911bdd85bae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:50 +0000

    Add package pydoctor/0.6b2/pydoctor-0.6b2.tar.gz

---
+++ packages/pydoctor/pydoctor-0.6b2.tar.gz/pydoctor-0.6b2/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+from distutils.core import setup
+
+setup(
+    name='pydoctor',
+    version='0.6b2',
+    author='Michael Hudson-Doyle',
+    author_email='micahel@gmail.com',
+    url='http://launchpad.net/pydoctor',
+    description='API doc generator.',
+    license='MIT/X11',
+    packages=[
+        'pydoctor',
+        'pydoctor.templatewriter',
+        'pydoctor.templatewriter.pages',
+        ],
+    package_data={
+        'pydoctor': [
+            'templates/*',
+            ],
+        },
+    scripts=[
+        'bin/pydoctor',
+        ],
+    install_requires=["Twisted", "epydoc"],
+    )
