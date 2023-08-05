e4c5769d06bbf6f9306ad32ad3b2efad1c45d11e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:26 +0000

    Add package mailgunlog/0.0.2/mailgunlog-0.0.2.tar.gz

---
+++ packages/mailgunlog/mailgunlog-0.0.2.tar.gz/mailgunlog-0.0.2/setup.py
@@ -0,0 +1,39 @@
+import setuptools
+
+setuptools.setup(name='mailgunlog',
+                 version='0.0.2',
+                 description='Mailgun Log',
+                 long_description='Python Package to retrieve Mailgun logs for a given domain.',
+                 author='Getup Cloud',
+                 author_email='mateus.caruccio@getupcloud.com',
+                 url='https://github.com/getupcloud/python-mailgunlog',
+                 packages=['mailgunlog'],
+                 install_requires=['requests>=2.0.0'],
+                 license='Apache Software License',
+                 entry_points={
+                     'console_scripts': [
+                         'mailgunlog = mailgunlog.mailgunlog:main'
+                     ],
+                 },
+                 keywords='mailgun log email mail',
+                 classifiers=[
+                     # How mature is this project? Common values are
+                     #   3 - Alpha
+                     #   4 - Beta
+                     #   5 - Production/Stable
+                     'Development Status :: 4 - Beta',
+
+                     # Indicate who your project is intended for
+                     'Intended Audience :: Developers',
+                     'Topic :: Software Development :: Build Tools',
+
+                     # Pick your license as you wish (should match "license" above)
+                      'License :: OSI Approved :: Apache Software License',
+
+                     # Specify the Python versions you support here. In particular, ensure
+                     # that you indicate whether you support Python 2, Python 3 or both.
+                     'Programming Language :: Python :: 2',
+                     'Programming Language :: Python :: 2.6',
+                     'Programming Language :: Python :: 2.7',
+                 ]
+)
