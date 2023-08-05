382b1bd9d01fd7c23c4537cf4574fe781ce37cf6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:10 +0000

    Add package WebPocket/0.1.3b0/WebPocket-0.1.3b0.tar.gz

---
+++ packages/WebPocket/WebPocket-0.1.3b0.tar.gz/WebPocket-0.1.3b0/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(name='WebPocket',
+      version='0.1.3b',
+      description='Actor-model based WebSocket client in Python',
+      long_description='Actor-model based WebSocket client in Python',
+      author='xhs',
+      author_email='chef@dark.kitchen',
+      url='https://github.com/xhs/WebPocket/',
+      packages=['webpocket'],
+      platforms=["any"],
+      license='BSD',
+      install_requires=['Twisted', 'service_identity'])
---
+++ packages/WebPocket/WebPocket-0.1.3b0.tar.gz/WebPocket-0.1.3b0/webpocket/framer.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+
+from struct import pack
+from frame import *
+from exc import *
+
+class Framer(object):
+  def __init__(self, actors, payload_limit=0):
+    self._actors = actors
+    self._buffer = b''
+    self._payload_limit = payload_limit
+
+  def send_text(self, text):
+    limit = self._payload_limit
+    if limit > 0 and limit < len(text):
+      text = self._send_fragment(0, OPCODE_TEXT, text, limit)
+      while limit < len(text):
+        text = self._send_fragment(0, OPCODE_CONTINUE, text, limit)
+      self._send_fragment(1, OPCODE_CONTINUE, text, limit)
+    else:
+      data = Frame(opcode=OPCODE_TEXT, payload=text).encode()
+      self._actors['streamer'].send(data)
+
+  def send_binary(self, binary):
+    limit = self._payload_limit
+    if limit > 0 and limit < len(binary):
+      binary = self._send_fragment(0, OPCODE_BINARY, binary, limit)
+      while limit < len(binary):
+        binary = self._send_fragment(0, OPCODE_CONTINUE, binary, limit)
+      self._send_fragment(1, OPCODE_CONTINUE, binary, limit)
+    else:
+      data = Frame(opcode=OPCODE_BINARY, payload=binary).encode()
+      self._actors['streamer'].send(data)
+
+  def _send_fragment(self, fin, opcode, payload, limit):
+    data = Frame(fin=fin, opcode=opcode, payload=payload[:limit]).encode()
+    self._actors['streamer'].send(data)
+    return payload[limit:]
+
+  def feed(self, data):
+    self._buffer += data
+    while len(self._buffer) > 0:
+      try:
+        frame, left_data = Frame().decode(self._buffer)
+        self._buffer = left_data
+        self._actors['messager'].feed(frame)
+      except InsufficientBytesException:
+        break
+
+  def send_pong(self):
+    data = Frame(opcode=OPCODE_PONG).encode()
+    self._actors['streamer'].send(data)
+
+  def send_close(self, code, reason):
+    payload = pack('!H', code) + reason
+    data = Frame(opcode=OPCODE_CLOSE, payload=payload).encode()
+    self._actors['streamer'].send(data)
+
+if __name__ == '__main__':
+  class DummyMessager(object):
+    def feed(self, frame):
+      frame.inspect()
+
+  messager = DummyMessager()
+  framer = Framer({'messager': messager})
+
+  data = Frame(payload='foobar').encode()
+  framer.feed(data[:3])
+  framer.feed(data[3:])
