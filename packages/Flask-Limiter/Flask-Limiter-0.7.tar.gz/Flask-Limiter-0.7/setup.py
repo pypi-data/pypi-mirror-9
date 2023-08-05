0ec2f59078093dd0c9e40649010e5f6d149f1863
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:46 +0000

    Add package Flask-Limiter/0.7/Flask-Limiter-0.7.tar.gz

---
+++ packages/Flask-Limiter/Flask-Limiter-0.7.tar.gz/Flask-Limiter-0.7/setup.py
@@ -0,0 +1,39 @@
+"""
+setup.py for Flask-Limiter
+
+
+"""
+__author__ = "Ali-Akber Saifee"
+__email__ = "ali@indydevs.org"
+__copyright__ = "Copyright 2014, Ali-Akber Saifee"
+
+from setuptools import setup, find_packages
+import os
+
+this_dir = os.path.abspath(os.path.dirname(__file__))
+REQUIREMENTS = filter(None, open(
+    os.path.join(this_dir, 'requirements', 'main.txt')).read().splitlines())
+
+import versioneer
+
+versioneer.versionfile_source = "flask_limiter/_version.py"
+versioneer.versionfile_build = "flask_limiter/version.py"
+versioneer.tag_prefix = ""
+versioneer.parentdir_prefix = "flask_limiter-"
+
+setup(
+    name='Flask-Limiter',
+    author=__author__,
+    author_email=__email__,
+    license=open("LICENSE.txt").read(),
+    url="https://flask-limiter.readthedocs.org",
+    zip_safe=False,
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    install_requires=REQUIREMENTS,
+    classifiers=[k for k in open('CLASSIFIERS').read().split('\n') if k],
+    description='Rate limiting for flask applications',
+    long_description=open('README.rst').read() + open('HISTORY.rst').read(),
+    packages=find_packages(exclude=["tests*"]),
+)
+
