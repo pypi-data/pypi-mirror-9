43202e93904344319281d762853c3ef704e09657
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:59 +0000

    Add package tellme/0.1.3/tellme-0.1.3.tar.gz

---
+++ packages/tellme/tellme-0.1.3.tar.gz/tellme-0.1.3/setup.py
@@ -0,0 +1,42 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import io
+from setuptools import setup, find_packages
+
+
+dependencies = [
+    'PyYAML>=3.11',
+    'dataset>=0.5.5'
+]
+
+# with io.open('README.md', mode='r+t', encoding='utf-8') as stream:
+#     readme = stream.read()
+
+setup(
+    name='tellme',
+    description='A toolkit for generating user-facing reports from things happening in code.',
+    long_description='A toolkit for generating user-facing reports from things happening in code.',
+    version='0.1.3',
+    author='Open Knowledge Foundation',
+    author_email='info@okfn.org',
+    url='http://okfn.org',
+    license='MIT',
+    packages=find_packages(exclude=['tests']),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=dependencies,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+)
