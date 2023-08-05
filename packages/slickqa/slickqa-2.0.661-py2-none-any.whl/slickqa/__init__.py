c5470cd65de0fab4857837081ab9db44f82a4a43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:47 +0000

    Add package slickqa/2.0.661/slickqa-2.0.661-py2-none-any.whl

---
+++ packages/slickqa/slickqa-2.0.661-py2-none-any.whl/slickqa/__init__.py
@@ -0,0 +1,21 @@
+"""
+slick.py is a library for talking to slick (http://code.google.com/p/slickqa).
+
+Copyright 2013 AccessData Group, LLC.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+"""
+from .data import *
+from .connection import SlickConnection, SlickCommunicationError
+from .queries import *
+from .slickqa import *
---
+++ packages/slickqa/slickqa-2.0.661-py2-none-any.whl/slickqa/micromodels/__init__.py
@@ -0,0 +1,6 @@
+from .models import Model
+from .fields import BaseField, CharField, StringField, IntegerField, FloatField,\
+                    BooleanField, DateTimeField, DateField, TimeField,\
+                    ModelField, ModelCollectionField, FieldCollectionField
+
+__version__ = '0.5.1'
