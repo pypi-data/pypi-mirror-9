e987300e1a90f04ef95277422a72fa0fa1a3f737
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:53 +0000

    Add package celerybeat-mongo/0.0.8/celerybeat-mongo-0.0.8.tar.gz

---
+++ packages/celerybeat-mongo/celerybeat-mongo-0.0.8.tar.gz/celerybeat-mongo-0.0.8/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+setup(
+    name = "celerybeat-mongo",
+    description = "A Celery Beat Scheduler that uses MongoDB to store both schedule definitions and status information",
+    version = "0.0.8",
+    license = "Apache License, Version 2.0",
+    author = "Zakir Durumeric",
+    author_email = "zakird@gmail.com",
+    maintainer = "Zakir Durumeric",
+    maintainer_email = "zakird@gmail.com",
+
+    keywords = "python celery beat mongo",
+
+    packages = [
+        "celerybeatmongo"
+    ],
+
+    install_requires=[
+        'setuptools',
+        'pymongo',
+        'mongoengine',
+        'celery',
+    ]
+
+)
