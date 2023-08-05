ceaaa37dc5af86d349cd5cf2c0d4d7c4e09d8375
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:25 +0000

    Add package ldclient-py/0.14.1/ldclient-py-0.14.1.tar.gz

---
+++ packages/ldclient-py/ldclient-py-0.14.1.tar.gz/ldclient-py-0.14.1/setup.py
@@ -0,0 +1,32 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+from pip.req import parse_requirements
+import uuid
+
+
+# parse_requirements() returns generator of pip.req.InstallRequirement objects
+install_reqs = parse_requirements('requirements.txt', session=uuid.uuid1())
+
+# reqs is a list of requirement
+# e.g. ['django==1.5.1', 'mezzanine==1.4.6']
+reqs = [str(ir.req) for ir in install_reqs]
+
+setup(
+    name='ldclient-py',
+    version='0.14.1',
+    author='Catamorphic Co.',
+    author_email='team@catamorphic.com',
+    packages=['ldclient'],
+    url='https://github.com/launchdarkly/python-client',
+    description='LaunchDarkly SDK for Python',
+    long_description='LaunchDarkly SDK for Python',
+    install_requires=reqs,
+    classifiers=[
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2 :: Only',
+    ]
+)
