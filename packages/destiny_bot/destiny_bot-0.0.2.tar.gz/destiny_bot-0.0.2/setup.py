d8d0903e428b29f07267d5ffff023fef96a64726
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:50 +0000

    Add package destiny_bot/0.0.2/destiny_bot-0.0.2.tar.gz

---
+++ packages/destiny_bot/destiny_bot-0.0.2.tar.gz/destiny_bot-0.0.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='destiny_bot',
+    version='0.0.2',
+    description='Set of commands that scrape destinytracker.com for info.',
+    author='Nolan Brubaker',
+    author_email='palendae@gmail.com',
+    packages=find_packages(),
+    py_modules=['destiny_bot', 'scraper'],
+    include_package_data=True,
+    zip_safe=True,
+    entry_points = dict(
+        helga_plugins=[
+            'nightfall = destiny_bot:nightfall',
+            'heroic = destiny_bot:heroic',
+        ]
+    )
+)
