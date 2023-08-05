5d942504ad8151d03897f608915c1286ae1f7e3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:05 +0000

    Add package staaden001/1.2.0/staaden001-1.2.0.zip

---
+++ packages/staaden001/staaden001-1.2.0.zip/staaden001-1.2.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup (
+    name = 'staaden001',
+    version = '1.2.0',
+    py_modules = ['staaden001'],
+    author = 'bstaaden',
+    author_email = 'beckystaaden@gmail.com',
+    url = 'fe80::219b:9284:2e2f:9970%13',
+    description = 'A simple printer of nested list',
+    )
