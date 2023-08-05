bdb827ad7e1431a0d56792dfa1464c44958f8ff9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:33 +0000

    Add package planetfilter/0.2.1/planetfilter-0.2.1.tar.gz

---
+++ packages/planetfilter/planetfilter-0.2.1.tar.gz/planetfilter-0.2.1/setup.py
@@ -0,0 +1,30 @@
+from distutils.core import setup
+
+setup(
+    name = 'planetfilter',
+    version = '0.2.1',
+    description = 'Filter for blog aggregators',
+    author = 'Francois Marier',
+    author_email = 'francois@fmarier.org',
+    url = 'https://launchpad.net/planetfilter',
+    scripts = ['planetfilter'],
+    keywords = ['rss', 'atom', 'blogs', 'planet'],
+    license = 'AGPL-3.0+',
+    requires = ['defusedxml'],
+    classifiers = [
+        "Programming Language :: Python",
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "Intended Audience :: End Users/Desktop",
+        "License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        ],
+    long_description = """\
+PlanetFilter uses a blacklist to filter a blog aggregator feed. It
+allows anyone to subscribe to popular blog aggregators without being
+overwhelmed by the noise.
+
+.. _project page: https://launchpad.net/planetfilter
+"""
+    )
