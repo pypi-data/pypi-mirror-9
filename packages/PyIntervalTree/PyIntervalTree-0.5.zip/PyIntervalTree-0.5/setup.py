b65413a68ee752405f0bb2c701ba84670baf219d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:11 +0000

    Add package PyIntervalTree/0.5/PyIntervalTree-0.5.zip

---
+++ packages/PyIntervalTree/PyIntervalTree-0.5.zip/PyIntervalTree-0.5/setup.py
@@ -0,0 +1,55 @@
+'''
+PyIntervalTree: A mutable, self-balancing interval tree. Queries may be by point, by range overlap, or by range envelopment.
+
+Note that "python setup.py test" invokes pytest on the package. With appropriately
+configured setup.cfg, this will check both xxx_test modules and docstrings.
+
+Copyright 2014, Chaim-Leib Halbert et al.
+Most recent fork and modifications by Konstantin Tretyakov
+
+Licensed under LGPL.
+'''
+import sys
+from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
+
+# This is a plug-in for setuptools that will invoke py.test
+# when you run python setup.py test
+class PyTest(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        import pytest  # import here, because outside the required eggs aren't loaded yet
+        sys.exit(pytest.main(self.test_args))
+
+
+version = '0.5'
+setup(
+    name = 'PyIntervalTree',
+    version = version,
+    description = 'Mutable, self-balancing interval tree',
+    long_description=open("README.rst").read(),
+    classifiers=[ # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+        'Development Status :: 7 - Inactive',
+        'Programming Language :: Python',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+        'Topic :: Software Development :: Libraries',
+    ],
+    keywords="interval-tree data-structure intervals tree", # Separate with spaces
+    author = 'Chaim-Leib Halbert, Konstantin Tretyakov',
+    author_email = 'kt@ut.ee',
+    url='https://github.com/konstantint/PyIntervalTree',
+    license="LGPL",
+    packages=find_packages(exclude=['examples', 'tests']),
+    include_package_data=True,
+    zip_safe=True,
+    install_requires=['sortedcontainers'],    
+    tests_require=['pytest'],
+    cmdclass={'test': PyTest},
+    entry_points={}
+)
