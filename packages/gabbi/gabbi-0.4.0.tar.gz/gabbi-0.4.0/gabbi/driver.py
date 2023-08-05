41867670f86050e5aff04da6ae26de5833ac0822
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:20 +0000

    Add package gabbi/0.4.0/gabbi-0.4.0.tar.gz

---
+++ packages/gabbi/gabbi-0.4.0.tar.gz/gabbi-0.4.0/gabbi/driver.py
@@ -0,0 +1,171 @@
+# Copyright 2014, 2015 Red Hat
+#
+# Authors: Chris Dent <chdent@redhat.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+"""Generate HTTP tests from YAML files
+
+Each HTTP request is its own TestCase and can be requested to be run in
+isolation from other tests. If it is member of a sequence of requests,
+prior requests will be run.
+
+A sequence is represented by an ordered list in a single YAML file.
+
+Each sequence becomes a TestSuite.
+
+An entire directory of YAML files is a TestSuite of TestSuites.
+"""
+
+import glob
+import inspect
+import os
+from unittest import suite
+import uuid
+
+import httplib2
+import wsgi_intercept
+from wsgi_intercept import httplib2_intercept
+import yaml
+
+from .suite import GabbiSuite
+from .case import HTTPTestCase
+
+
+# Empty test from which all others inherit
+BASE_TEST = {
+    'name': '',
+    'desc': '',
+    'ssl': False,
+    'redirects': False,
+    'method': 'GET',
+    'url': '',
+    'status': '200',
+    'request_headers': {},
+    'response_headers': {},
+    'response_strings': None,
+    'response_json_paths': None,
+    'data': '',
+}
+
+
+class TestBuilder(type):
+    """Metaclass to munge a dynamically created test.
+    """
+
+    required_attributes = {'has_run': False}
+
+    def __new__(mcs, name, bases, attributes):
+        attributes.update(mcs.required_attributes)
+        return type.__new__(mcs, name, bases, attributes)
+
+
+def build_tests(path, loader, host=None, port=8001, intercept=None,
+                test_loader_name=None, fixture_module=None):
+    """Read YAML files from a directory to create tests.
+
+    Each YAML file represents an ordered sequence of HTTP requests.
+    """
+    top_suite = suite.TestSuite()
+
+    if test_loader_name is None:
+        test_loader_name = inspect.stack()[1]
+        test_loader_name = os.path.splitext(os.path.basename(
+            test_loader_name[1]))[0]
+
+    if intercept:
+        host = install_intercept(intercept, port)
+
+    yaml_file_glob = '%s/*.yaml' % path
+
+    # Return an empty suite if we have no host to access, either via
+    # a real host or an intercept
+    if host or intercept:
+        for test_file in glob.iglob(yaml_file_glob):
+            test_yaml = load_yaml(test_file)
+            test_name = '%s_%s' % (test_loader_name,
+                                   os.path.splitext(
+                                       os.path.basename(test_file))[0])
+            file_suite = test_suite_from_yaml(loader, test_name, test_yaml,
+                                              path, host, port, fixture_module)
+            top_suite.addTest(file_suite)
+    return top_suite
+
+
+def factory(wsgi_app):
+    """Satisfy a bad API."""
+    return wsgi_app
+
+
+def install_intercept(wsgi_callable, port):
+    """Install a wsgi-intercept on a random hostname."""
+    hostname = str(uuid.uuid4())
+    httplib2_intercept.install()
+    wsgi_intercept.add_wsgi_intercept(hostname, port, factory(wsgi_callable))
+    return hostname
+
+
+def load_yaml(yaml_file):
+    """Read and parse any YAML file. Let exceptions flow where they may."""
+    with open(yaml_file) as source:
+        return yaml.safe_load(source.read())
+
+
+def test_suite_from_yaml(loader, test_base_name, test_yaml, test_directory,
+                         host, port, fixture_module):
+    """Generate a TestSuite from YAML data."""
+
+    file_suite = GabbiSuite()
+    test_data = test_yaml['tests']
+    fixtures = test_yaml.get('fixtures', None)
+
+    # Set defaults from BASE_TESTS then update those defaults
+    # with any defaults set in the YAML file.
+    base_test_data = dict(BASE_TEST)
+    base_test_data.update(test_yaml.get('defaults', {}))
+
+    # Establish any fixture classes.
+    fixture_classes = []
+    if fixtures and fixture_module:
+        for fixture_class in fixtures:
+            fixture_classes.append(getattr(fixture_module, fixture_class))
+
+    prior_test = None
+    for test_datum in test_data:
+        test = dict(base_test_data)
+        test.update(test_datum)
+
+        test_name = '%s_%s' % (test_base_name,
+                               test['name'].lower().replace(' ', '_'))
+
+        if set(test.keys()) != set(BASE_TEST.keys()):
+            raise AssertionError('Invalid test keys used in test: %s'
+                                 % test_name)
+
+        # Use metaclasses to build a class of the necessary type
+        # with relevant arguments.
+        klass = TestBuilder(test_name, (HTTPTestCase,),
+                            {'test_data': test,
+                             'test_directory': test_directory,
+                             'fixtures': fixture_classes,
+                             'http': httplib2.Http(),
+                             'host': host,
+                             'port': port,
+                             'prior': prior_test})
+
+        tests = loader.loadTestsFromTestCase(klass)
+        this_test = tests._tests[0]
+        file_suite.addTest(this_test)
+        prior_test = this_test
+
+    return file_suite
---
+++ packages/gabbi/gabbi-0.4.0.tar.gz/gabbi-0.4.0/gabbi/fixture.py
@@ -0,0 +1,88 @@
+# Copyright 2014, 2015 Red Hat
+#
+# Authors: Chris Dent <chdent@redhat.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+"""Manage fixtures for gabbi at the test suite level."""
+
+import sys
+from contextlib import contextmanager
+
+import six
+
+
+class GabbiFixtureError(Exception):
+    """Generic exception for GabbiFixture."""
+    pass
+
+
+class GabbiFixture(object):
+    """A context manager that operates as a fixture.
+
+    Subclasses must implement start_fixture and stop_fixture, each of which
+    contain the logic for stopping and starting whatever the fixture is.
+    What a fixture is is left as an exercise for the implementor.
+
+    These context managers will be nested so any actually work needs to
+    happen in `start_fixture` and `stop_fixture` and not in ``__init__``.
+    Otherwise exception handling will not work properly.
+    """
+
+    def __enter__(self):
+        self.start_fixture()
+
+    def __exit__(self, type, value, traceback):
+        self.stop_fixture()
+
+    def start_fixture(self):
+        """Implement the actual workings of starting the fixture here."""
+        pass
+
+    def stop_fixture(self):
+        """Implement the actual workings of stopping the fixture here."""
+        pass
+
+
+@contextmanager
+def nest(fixtures):
+    """Nest a series of fixtures.
+
+    This is duplicated from ``nested`` in the stdlib, which has been
+    deprecated because of issues with how exceptions are difficult to
+    handle during ``__init__``. Gabbi needs to nest an unknown number
+    of fixtures dynamically, so the ``with`` syntax that replaces
+    ``nested`` will not work.
+    """
+    vars = []
+    exits = []
+    exc = (None, None, None)
+    try:
+        for fixture in fixtures:
+            enter = fixture.__enter__
+            exit = fixture.__exit__
+            vars.append(enter())
+            exits.append(exit)
+        yield vars
+    except:
+        exc = sys.exc_info()
+    finally:
+        while exits:
+            exit = exits.pop()
+            try:
+                if exit(*exc):
+                    exc = (None, None, None)
+            except:
+                exc = sys.exc_info()
+        if exc != (None, None, None):
+            six.reraise(exc[0], exc[1], exc[2])
