d910828da352c4882ebfa04592cb198c9c9e6136
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:42 +0000

    Add package docker-harpoon/0.5.3.4/docker-harpoon-0.5.3.4.tar.gz

---
+++ packages/docker-harpoon/docker-harpoon-0.5.3.4.tar.gz/docker-harpoon-0.5.3.4/setup.py
@@ -0,0 +1,49 @@
+from setuptools import setup, find_packages
+from harpoon import VERSION
+
+setup(
+      name = "docker-harpoon"
+    , version = VERSION
+    , packages = ['harpoon'] + ['harpoon.%s' % pkg for pkg in find_packages('harpoon')]
+    , include_package_data = True
+
+    , install_requires =
+      [ "delfick_error==1.6.1"
+      , "option_merge==0.9.1"
+      , "input_algorithms==0.4.3"
+
+      , "docker-py==0.7.0"
+      #, "dockerpty==0.3.2"
+
+      , "six"
+      , "glob2"
+      , "dulwich"
+      , "humanize"
+
+      , "pyYaml==3.10"
+      , "rainbow_logging_handler==2.2.2"
+      ]
+
+    , extras_require =
+      { "tests":
+        [ "noseOfYeti>=1.5.0"
+        , "nose"
+        , "mock"
+        ]
+      }
+
+    , entry_points =
+      { 'console_scripts' :
+        [ 'harpoon = harpoon.executor:main'
+        ]
+      }
+
+    # metadata for upload to PyPI
+    , url = "https://github.com/realestate-com-au/harpoon"
+    , author = "Stephen Moore"
+    , author_email = "stephen.moore@rea-group.com"
+    , description = "Opinionated wrapper around docker"
+    , license = "MIT"
+    , keywords = "docker"
+    )
+
