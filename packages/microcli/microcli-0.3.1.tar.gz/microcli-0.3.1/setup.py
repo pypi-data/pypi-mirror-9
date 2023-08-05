ef0b7ba631b8c674287b72a3b065f12dcf1973b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:48 +0000

    Add package microcli/0.3.1/microcli-0.3.1.tar.gz

---
+++ packages/microcli/microcli-0.3.1.tar.gz/microcli-0.3.1/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name="microcli",
+      version="0.3.1",
+      description="Extremely lightweight CLI lib for python",
+      long_description="Extremely lightweight CLI lib for python",
+      author="Peter Neumark",
+      author_email="neumark.peter@gmail.com",
+      url="https://github.com/neumark/microcli",
+      download_url="https://github.com/neumark/microcli",
+      py_modules=["microcli"],
+      classifiers=[
+          "Intended Audience :: Developers",
+          "License :: OSI Approved :: Apache Software License",
+          "Operating System :: OS Independent",
+          "Programming Language :: Python",
+          "Topic :: Software Development :: Libraries :: Python Modules",
+          "Programming Language :: Python",
+          "Programming Language :: Python :: 2",
+          "Programming Language :: Python :: 2.6",
+          "Programming Language :: Python :: 2.7",
+          "Programming Language :: Python :: 3",
+          "Programming Language :: Python :: 3.3",
+          "Programming Language :: Python :: 3.4"
+          ])
