999d9127f443c18d0c2e01501958e7ef9114fee6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:12 +0000

    Add package django-summernote/0.6.1/django-summernote-0.6.1.tar.gz

---
+++ packages/django-summernote/django-summernote-0.6.1.tar.gz/django-summernote-0.6.1/setup.py
@@ -0,0 +1,51 @@
+import os
+from setuptools import setup, find_packages
+from django_summernote import version, PROJECT
+
+
+MODULE_NAME = 'django_summernote'
+PACKAGE_DATA = list()
+CLASSIFIERS = [
+    'Development Status :: 4 - Beta',
+    'Environment :: Web Environment',
+    'Framework :: Django',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python',
+    'Topic :: Internet :: WWW/HTTP',
+    'Topic :: Software Development :: Libraries :: Python Modules',
+    'Topic :: Utilities',
+]
+
+for directory in ['static', 'templates']:
+    for root, dirs, files in os.walk(os.path.join(MODULE_NAME, directory)):
+        for filename in files:
+            PACKAGE_DATA.append("%s/%s" % (root[len(MODULE_NAME) + 1:],
+                                           filename))
+
+
+setup(
+    name=PROJECT,
+    version=version,
+    packages=find_packages(),
+    package_data={'': PACKAGE_DATA, },
+    zip_safe=False,
+
+    author='Park Hyunwoo',
+    author_email='ez.amiryo' '@' 'gmail.com',
+    maintainer='Park Hyunwoo',
+    maintainer_email='ez.amiryo' '@' 'gmail.com',
+    url='http://github.com/summernote/django-summernote',
+
+    description='Summernote plugin for Django',
+    classifiers=CLASSIFIERS,
+
+    install_requires=['django', ],
+    test_suite='runtests.runtests',
+    tests_require=['django-dummy-plug', ],
+)
