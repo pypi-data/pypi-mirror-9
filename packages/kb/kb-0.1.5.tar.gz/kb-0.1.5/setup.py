a1e14a31ef214edd52479e945e5de1da37e9afd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:27 +0000

    Add package kb/0.1.5/kb-0.1.5.tar.gz

---
+++ packages/kb/kb-0.1.5.tar.gz/kb-0.1.5/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    author="WillemJan Faber",
+    author_email=["WillemJan.faber@kb.nl", "lab@kb.nl"],
+    name='kb',
+    url="https://github.com/KBNLresearch/KB-python-API",
+    description='Access to National Library of the Netherlands datasets',
+    version='0.1.5',
+    packages=['kb.nl.api', 'kb.nl.helpers', 'kb', 'kb.nl', 'kb.nl.collections'],
+    license='GNU General Public License',
+    install_requires=['lxml>=2.3', 'requests'],
+    classifiers=['Development Status :: 3 - Alpha',
+                 'Intended Audience :: Developers',
+                 'Intended Audience :: Science/Research',
+                 'Programming Language :: Python :: 2.7',
+                 'Programming Language :: Python :: 3',
+                 'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+                 'Natural Language :: Dutch',
+                 'Topic :: Sociology :: History']
+)
