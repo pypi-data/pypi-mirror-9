341aa99d6e87aae3e11185eb7a8b8a0d52163d1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:38 +0000

    Add package ez/1.4.9/ez-1.4.9.tar.gz

---
+++ packages/ez/ez-1.4.9.tar.gz/ez-1.4.9/ez/__init__.py
@@ -0,0 +1,8 @@
+from easyshell import *
+from easyshell import __doc__ as doc1
+from pyg import Mail, mail, AddEvent, addevent, Sheet
+from pyg import Mail2, mail2, AddEvent2, addevent2, Sheet2
+from pyg import __doc__ as doc2
+from version import __version__
+
+__doc__ = doc1 + '\n\n\n\n' + doc2
