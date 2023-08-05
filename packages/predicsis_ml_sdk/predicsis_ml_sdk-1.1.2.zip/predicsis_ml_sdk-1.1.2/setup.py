0521f60445e67abfc5d3880667190d2fec8fe32e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:26 +0000

    Add package predicsis_ml_sdk/1.1.2/predicsis_ml_sdk-1.1.2.zip

---
+++ packages/predicsis_ml_sdk/predicsis_ml_sdk-1.1.2.zip/predicsis_ml_sdk-1.1.2/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(
+    name='predicsis_ml_sdk',
+    version='1.1.2',
+    author='Michal K. Szczerbak',
+    author_email='michal.szczerbak@predicsis.com',
+    packages=['predicsis', 'predicsis.tests'],
+    scripts=[],
+    url='http://predicsis.com',
+    license='LICENSE.txt',
+    description='PredicSis REST API Python bindings.',
+    long_description=open('README.txt').read(),
+    install_requires=[
+        "requests >= 2.5.0",
+    ],
+)
