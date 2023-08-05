ca5fcc3d9e95e610266e365f79a918c2acd89081
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:10 +0000

    Add package trireme/1.0.0/trireme-1.0.0.tar.gz

---
+++ packages/trireme/trireme-1.0.0.tar.gz/trireme-1.0.0/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages  # Always prefer setuptools over distutils
+
+setup(
+    name='trireme',
+    version='1.0.0',
+
+    description='Migration tool providing support for Apache Cassandra, DataStax Enterprise Cassandra, & DataStax '
+                'Enterprise Solr.',
+
+    url='https://github.com/o19s/trireme',
+    download_url='https://github.com/o19s/trireme/tarball/1.0.0',
+
+    author='Christopher Bradford',
+    author_email='cbradford@opensourceconnections.com',
+
+    license='BSD',
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+
+        'Programming Language :: Python :: 3',
+    ],
+
+    keywords='cassandra solr dse dsc migrations development',
+
+    packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
+
+    install_requires=['lz4', 'blist', 'cassandra-driver', 'requests', 'invoke']
+)
