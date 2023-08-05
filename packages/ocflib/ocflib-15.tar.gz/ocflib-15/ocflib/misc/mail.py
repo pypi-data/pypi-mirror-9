dc91e9b0ad2455668dbe86bb134fa3f668705800
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:17 +0000

    Add package ocflib/15/ocflib-15.tar.gz

---
+++ packages/ocflib/ocflib-15.tar.gz/ocflib-15/ocflib/misc/mail.py
@@ -0,0 +1,48 @@
+"""Email handling and sending"""
+
+import email.mime.text
+import subprocess
+from email.utils import parseaddr
+
+import ocflib.account.validators
+import ocflib.constants as constants
+import ocflib.misc.validators as validators
+
+
+def email_for_user(username):
+    """Return email for a user.
+
+    Currently, just appends @ocf.berkeley.edu, but could eventually do
+    something more complicated."""
+
+    ocflib.account.validators.validate_username(username, check_exists=True)
+    return "{}@ocf.berkeley.edu".format(username)
+
+
+def send_mail_user(user, subject, body, sender=constants.MAIL_FROM):
+    """Send a plan-text mail message to a user."""
+    send_mail(email_for_user(user), subject, body, sender=sender)
+
+
+def send_mail(to, subject, body, sender=constants.MAIL_FROM):
+    """Send a plain-text mail message.
+
+    `body` should be a string with newlines, wrapped at about 80 characters."""
+
+    if not validators.valid_email(parseaddr(sender)[1]):
+        raise ValueError("Invalid sender address.")
+
+    if not validators.valid_email(parseaddr(to)[1]):
+        raise ValueError("Invalid recipient address.")
+
+    msg = email.mime.text.MIMEText(body)
+
+    msg['Subject'] = subject
+    msg['From'] = sender
+    msg['To'] = to
+
+    # we send the message via sendmail, since we may one day prohibit traffic
+    # to port 25 that doesn't go via the system mailserver
+    p = subprocess.Popen((constants.SENDMAIL_PATH, '-t', '-oi'),
+                         stdin=subprocess.PIPE)
+    p.communicate(msg.as_string().encode('utf8'))
