424c6de38d91b5d969cf134b0dbe695a947b52be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:03 +0000

    Add package carlcm/0.1.7/carlcm-0.1.7.tar.gz

---
+++ packages/carlcm/carlcm-0.1.7.tar.gz/carlcm-0.1.7/setup.py
@@ -0,0 +1,29 @@
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from distutils.core import setup, find_packages
+
+version = '0.1.7'
+
+config = {
+    'description': 'CarlCM',
+    'author': 'Carl J Mackey',
+    'url': 'https://github.com/cjmackey/carlcm',
+    'author_email': 'carljmackey@gmail.com',
+    'version': version,
+    'install_requires': ['nose', 'mock', 'coverage',
+                         'jinja2', 'pyfakefs', 'python-consul'],
+    'packages': find_packages(),
+    'scripts': ['bin/carlcm-counselor'],
+    'name': 'carlcm',
+    'classifiers': [
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Topic :: System :: Installation/Setup',
+    ],
+}
+
+setup(**config)
