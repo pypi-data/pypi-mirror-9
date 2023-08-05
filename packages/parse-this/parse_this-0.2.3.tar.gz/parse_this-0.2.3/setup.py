10f0f623e87996546b5266117e307f142c2aba83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:36 +0000

    Add package parse-this/0.2.3/parse_this-0.2.3.tar.gz

---
+++ packages/parse-this/parse_this-0.2.3.tar.gz/parse_this-0.2.3/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+import os
+from setuptools import setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst'), "r") as readme_file:
+    readme = readme_file.read()
+
+setup(
+    name = "parse_this",
+    version = "0.2.3",
+    description = "Makes it easy to create a command line interface for any function, method or classmethod..",
+    long_description = readme,
+    py_modules = ["parse_this", "parse_this_test"],
+    author = "Bertrand Vidal",
+    author_email = "vidal.bertrand@gmail.com",
+    download_url = "https://pypi.python.org/pypi/parse_this",
+    url = "https://github.com/bertrandvidal/parse_this",
+    classifiers = [
+        "Intended Audience :: Developers",
+        "Intended Audience :: Information Technology",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+    ],
+    setup_requires = [
+        "nose",
+    ],
+)
