e0aa47b8e2c2ef75e6dd216fa50041c9c4668e4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:30 +0000

    Add package malss/0.4.6/malss-0.4.6.tar.gz

---
+++ packages/malss/malss-0.4.6.tar.gz/malss-0.4.6/malss/algorithm.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+
+class Algorithm(object):
+    def __init__(self, estimator, parameters, name):
+        self.estimator = estimator
+        self.parameters = parameters
+        self.best_score = None
+        self.best_params = None
+        self.is_best_algorithm = False
+        self.grid_scores = None
+        self.classification_report = None
+        self.name = name
