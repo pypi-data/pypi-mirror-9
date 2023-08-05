0def0c883f5f8bc29356c8018a5ba051135669a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:23 +0000

    Add package reprounzip-docker/0.6/reprounzip-docker-0.6.tar.gz

---
+++ packages/reprounzip-docker/reprounzip-docker-0.6.tar.gz/reprounzip-docker-0.6/setup.py
@@ -0,0 +1,36 @@
+import os
+from setuptools import setup
+
+
+# pip workaround
+os.chdir(os.path.abspath(os.path.dirname(__file__)))
+
+
+with open('README.rst') as fp:
+    description = fp.read()
+setup(name='reprounzip-docker',
+      version='0.6',
+      packages=['reprounzip', 'reprounzip.unpackers'],
+      entry_points={
+          'reprounzip.unpackers': [
+              'docker = reprounzip.unpackers.docker:setup']},
+      namespace_packages=['reprounzip', 'reprounzip.unpackers'],
+      install_requires=[
+          'reprounzip>=0.6',
+          'rpaths>=0.8'],
+      description="Allows the ReproZip unpacker to create Docker containers",
+      author="Remi Rampin, Fernando Chirigati, Dennis Shasha, Juliana Freire",
+      author_email='reprozip-users@vgc.poly.edu',
+      maintainer="Remi Rampin",
+      maintainer_email='remirampin@gmail.com',
+      url='http://vida-nyu.github.io/reprozip/',
+      long_description=description,
+      license='BSD',
+      keywords=['reprozip', 'reprounzip', 'reproducibility', 'provenance',
+                'vida', 'nyu', 'docker'],
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Intended Audience :: Science/Research',
+          'License :: OSI Approved :: BSD License',
+          'Topic :: Scientific/Engineering',
+          'Topic :: System :: Archiving'])
