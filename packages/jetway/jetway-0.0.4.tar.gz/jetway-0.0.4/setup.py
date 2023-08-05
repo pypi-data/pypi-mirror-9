6202d7867099dc6b3729eb2a829383ae95aece34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:37 +0000

    Add package jetway/0.0.4/jetway-0.0.4.tar.gz

---
+++ packages/jetway/jetway-0.0.4.tar.gz/jetway-0.0.4/setup.py
@@ -0,0 +1,41 @@
+from setuptools import find_packages
+from setuptools import setup
+from pip import req
+
+_install_requirements = req.parse_requirements('requirements.txt')
+
+
+setup(
+    name='jetway',
+    version=open('VERSION').read().strip(),
+    description=(
+        'Client library for the Jetway static site staging service.'
+    ),
+    url='https://github.com/grow/jetway-client',
+    license='MIT',
+    author='Grow SDK Authors',
+    author_email='hello@grow.io',
+    include_package_data=True,
+    install_requires=[str(ir.req) for ir in _install_requirements],
+    packages=find_packages(),
+    keywords=[
+        'grow',
+        'cms',
+        'static site generator',
+        's3',
+        'google cloud storage',
+        'content management'
+    ],
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ])
