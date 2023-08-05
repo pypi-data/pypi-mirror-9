21eb2ce70cb15e16df1d6dba8748303fd3924e41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:41 +0000

    Add package ZODB/4.1.0/ZODB-4.1.0.tar.gz

---
+++ packages/ZODB/ZODB-4.1.0.tar.gz/ZODB-4.1.0/CHANGES.rst
@@ -0,0 +1,132 @@
+================
+ Change History
+================
+
+4.1.0 (2015-01-11)
+==================
+
+- Fix registration of custom logging level names ("BLATHER", "TRACE).
+
+  We have been registering them in the wrong order since 2004.  Before
+  Python 3.4, the stdlib ``logging`` module masked the error by registering
+  them in *both* directions.
+
+- Add support for Python 3.4.
+
+4.0.1 (2014-07-13)
+==================
+
+- Fix POSKeyError during transaction.commit when after savepoint.rollback.
+  see https://github.com/zopefoundation/ZODB/issues/16
+
+- Ensure that the pickler used in PyPy always has a ``persistent_id``
+  attribute (``inst_persistent_id`` is not present on the pure-Python
+  pickler). (PR #17)
+
+- Provide better error reporting when trying to load an object on a
+  closed connection.
+
+4.0.0 (2013-08-18)
+==================
+
+Finally released.
+
+4.0.0b3 (2013-06-11)
+====================
+
+- Switch to using non-backward-compatible pickles (protocol 3, without
+  storing bytes as strings) under Python 3.  Updated the magic number
+  for file-storage files under Python3 to indicate the incompatibility.
+
+- Fixed: A ``UnicodeDecodeError`` could happen for non-ASCII OIDs
+  when using bushy blob layout.
+
+4.0.0b2 (2013-05-14)
+====================
+
+- Extended the filename renormalizer used for blob doctests to support
+  the filenames used by ZEO in non-shared mode.
+
+- Added ``url`` parameter to ``setup()`` (PyPI says it is required).
+
+4.0.0b1 (2013-05-10)
+=====================
+
+- Skipped non-unit tests in ``setup.py test``.  Use the buildout to run tests
+  requiring "layer" support.
+
+- Included the filename in the exception message to support debugging in case
+  ``loadBlob`` does not find the file.
+
+- Added support for Python 3.2 / 3.3.
+
+.. note::
+
+   ZODB 4.0.x is supported on Python 3.x for *new* applications only.
+   Due to changes in the standard library's pickle support, the Python3
+   support does **not** provide forward- or backward-compatibility
+   at the data level with Python2.  A future version of ZODB may add
+   such support.
+
+   Applications which need migrate data from Python2 to Python3 should
+   plan to script this migration using separte databases, e.g. via a
+   "dump-and-reload" approach, or by providing explicit fix-ups of the
+   pickled values as transactions are copied between storages.
+
+
+4.0.0a4 (2012-12-17)
+=====================
+
+- Enforced usage of bytes for ``_p_serial`` of persistent objects (fixes
+  compatibility with recent persistent releases).
+
+4.0.0a3 (2012-12-01)
+=====================
+
+- Fixed: An elaborate test for trvial logic corrupted module state in a
+        way that made other tests fail spuriously.
+
+4.0.0a2 (2012-11-13)
+=====================
+
+Bugs Fixed
+----------
+
+- An unneeded left-over setting in setup.py caused installation with
+  pip to fail.
+
+4.0.0a1 (2012-11-07)
+=====================
+
+New Features
+------------
+
+- The ``persistent`` and ``BTrees`` packages are now released as separate
+  distributions, on which ZODB now depends.
+
+- ZODB no longer depends on zope.event.  It now uses ZODB.event, which
+  uses zope.event if it is installed.  You can override
+  ZODB.event.notify to provide your own event handling, although
+  zope.event is recommended.
+
+- BTrees allowed object keys with insane comparison. (Comparison
+  inherited from object, which compares based on in-process address.)
+  Now BTrees raise TypeError if an attempt is made to save a key with
+  comparison inherited from object. (This doesn't apply to old-style
+  class instances.)
+
+Bugs Fixed
+----------
+
+- Ensured that the export file and index file created by ``repozo`` share
+  the same timestamp.
+
+  https://bugs.launchpad.net/zodb/+bug/993350
+
+- Pinned the ``transaction`` and ``manuel`` dependencies to Python 2.5-
+  compatible versions when installing under Python 2.5.
+
+
+.. note::
+   Please see ``doc/HISTORY.txt`` for changelog entries for older versions
+   of ZODB.
---
+++ packages/ZODB/ZODB-4.1.0.tar.gz/ZODB-4.1.0/setup.py
@@ -0,0 +1,172 @@
+##############################################################################
+#
+# Copyright (c) 2002, 2003 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Zope Object Database: object database and persistence
+
+The Zope Object Database provides an object-oriented database for
+Python that provides a high-degree of transparency. Applications can
+take advantage of object database features with few, if any, changes
+to application logic.  ZODB includes features such as a plugable storage
+interface, rich transaction support, and undo.
+"""
+
+VERSION = "4.1.0"
+
+import os
+import sys
+from setuptools import setup, find_packages
+
+if sys.version_info < (2, 6):
+    print("This version of ZODB requires Python 2.6 or higher")
+    sys.exit(0)
+
+if (3,) < sys.version_info < (3, 2):
+    print("This version of ZODB requires Python 3.2 or higher")
+    sys.exit(0)
+
+PY3 = sys.version_info >= (3,)
+
+# The (non-obvious!) choices for the Trove Development Status line:
+# Development Status :: 5 - Production/Stable
+# Development Status :: 4 - Beta
+# Development Status :: 3 - Alpha
+
+classifiers = """\
+Development Status :: 4 - Beta
+Intended Audience :: Developers
+License :: OSI Approved :: Zope Public License
+Programming Language :: Python
+Programming Language :: Python :: 2
+Programming Language :: Python :: 2.6
+Programming Language :: Python :: 2.7
+Programming Language :: Python :: 3
+Programming Language :: Python :: 3.2
+Programming Language :: Python :: 3.3
+Programming Language :: Python :: 3.4
+Programming Language :: Python :: Implementation :: CPython
+Topic :: Database
+Topic :: Software Development :: Libraries :: Python Modules
+Operating System :: Microsoft :: Windows
+Operating System :: Unix
+Framework :: ZODB
+"""
+
+def _modname(path, base, name=''):
+    if path == base:
+        return name
+    dirname, basename = os.path.split(path)
+    return _modname(dirname, base, basename + '.' + name)
+
+def _flatten(suite, predicate=lambda *x: True):
+    from unittest import TestCase
+    for suite_or_case in suite:
+        if predicate(suite_or_case):
+            if isinstance(suite_or_case, TestCase):
+                yield suite_or_case
+            else:
+                for x in _flatten(suite_or_case):
+                    yield x
+
+def _no_layer(suite_or_case):
+    return getattr(suite_or_case, 'layer', None) is None
+
+def _unittests_only(suite, mod_suite):
+    for case in _flatten(mod_suite, _no_layer):
+        suite.addTest(case)
+
+def alltests():
+    import logging
+    import pkg_resources
+    import unittest
+
+    # Something wacked in setting recursion limit when running setup test
+    import ZODB.FileStorage.tests
+    del ZODB.FileStorage.tests._save_index
+
+    class NullHandler(logging.Handler):
+        level = 50
+
+        def emit(self, record):
+            pass
+
+    logging.getLogger().addHandler(NullHandler())
+
+    suite = unittest.TestSuite()
+    base = pkg_resources.working_set.find(
+        pkg_resources.Requirement.parse('ZODB')).location
+    for dirpath, dirnames, filenames in os.walk(base):
+        if os.path.basename(dirpath) == 'tests':
+            for filename in filenames:
+                if filename.endswith('.py') and filename.startswith('test'):
+                    mod = __import__(
+                        _modname(dirpath, base, os.path.splitext(filename)[0]),
+                        {}, {}, ['*'])
+                    _unittests_only(suite, mod.test_suite())
+        elif 'tests.py' in filenames:
+            mod = __import__(_modname(dirpath, base, 'tests'), {}, {}, ['*'])
+            _unittests_only(suite, mod.test_suite())
+    return suite
+
+doclines = __doc__.split("\n")
+
+def read_file(*path):
+    base_dir = os.path.dirname(__file__)
+    file_path = (base_dir, ) + tuple(path)
+    with open(os.path.join(*file_path), 'rb') as file:
+        return file.read()
+
+long_description = str(
+    ("\n".join(doclines[2:]) + "\n\n" +
+     ".. contents::\n\n" +
+     read_file("README.rst").decode('latin-1')  + "\n\n" +
+     read_file("CHANGES.rst").decode('latin-1')))
+
+tests_require = ['zope.testing', 'manuel']
+
+setup(name="ZODB",
+      version=VERSION,
+      setup_requires=['persistent'],
+      maintainer="Zope Foundation and Contributors",
+      maintainer_email="zodb-dev@zope.org",
+      packages = find_packages('src'),
+      package_dir = {'': 'src'},
+      url = 'http://www.zodb.org/',
+      license = "ZPL 2.1",
+      platforms = ["any"],
+      description = doclines[0],
+      classifiers = filter(None, classifiers.split("\n")),
+      long_description = long_description,
+      test_suite="__main__.alltests", # to support "setup.py test"
+      tests_require = tests_require,
+      extras_require = dict(test=tests_require),
+      install_requires = [
+        'persistent',
+        'BTrees',
+        'ZConfig',
+        'transaction >= 1.4.1' if PY3 else 'transaction',
+        'six',
+        'zc.lockfile',
+        'zdaemon >= 4.0.0a1',
+        'zope.interface',
+        ] + (['zodbpickle >= 0.2'] if PY3 else []),
+      zip_safe = False,
+      entry_points = """
+      [console_scripts]
+      fsdump = ZODB.FileStorage.fsdump:main
+      fsoids = ZODB.scripts.fsoids:main
+      fsrefs = ZODB.scripts.fsrefs:main
+      fstail = ZODB.scripts.fstail:Main
+      repozo = ZODB.scripts.repozo:main
+      """,
+      include_package_data = True,
+      )
