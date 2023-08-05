ee4e36be0125646e9d8992b7635669a64c67ef43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:45 +0000

    Add package djproxy/2.0.0/djproxy-2.0.0.tar.gz

---
+++ packages/djproxy/djproxy-2.0.0.tar.gz/djproxy-2.0.0/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+from djproxy import __version__, __author__
+
+# Multiprocessing is needed to execute `python setup.py tests` without any
+# errors in some installations.
+try:
+    import multiprocessing
+    multiprocessing  # resolve unused import pep8 violation by 'using' it
+except ImportError:
+    pass
+
+setup(
+    name="djproxy",
+    version=__version__,
+    url='https://github.com/thomasw/djproxy',
+    download_url='https://github.com/thomasw/djproxy/releases',
+    author=__author__,
+    author_email='thomas.welfley+djproxy@gmail.com',
+    description='djproxy is a simple reverse proxy class-based generic view '
+                'for Django apps. If your application depends on proxies '
+                'provided by a web server in production, djproxy can be used '
+                'to duplicate that functionality during local development.',
+    packages=find_packages(),
+    tests_require=[
+        'mock==1.0.1', 'nose==1.3.0', 'pinocchio==0.3.1', 'pyflakes==0.7.3',
+        'unittest2==0.5.1', 'requests>=1.0.0', 'django>=1.4'],
+    install_requires=['requests>=1.0.0', 'django>=1.4'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Software Development :: Libraries',
+    ],
+    test_suite='nose.collector'
+)
---
+++ packages/djproxy/djproxy-2.0.0.tar.gz/djproxy-2.0.0/tests/response_construction_tests.py
@@ -0,0 +1,43 @@
+from django.test.client import RequestFactory
+from mock import MagicMock, Mock, call
+from unittest2 import TestCase
+
+from helpers import RequestPatchMixin, ResponsePatchMixin
+from test_views import TestProxy
+
+
+class ResponseConstructionTest(TestCase, RequestPatchMixin,
+                               ResponsePatchMixin):
+    def setUp(self):
+        self.proxy = TestProxy.as_view()
+        self.browser_request = RequestFactory().get('/')
+
+        self.proxy_stub = Mock(
+            content='upstream content', headers={
+                'Fake-Header': '123',
+                'Content-Encoding': 'gzip'
+            }, status_code=200)
+        self.patch_request(self.proxy_stub)
+        self.patch_response(MagicMock())
+
+        self.proxy(self.browser_request)
+
+    def tearDown(self):
+        self.stop_patching_request()
+        self.stop_patching_response()
+
+
+class HttpProxyContentPassThrough(ResponseConstructionTest):
+    def test_creates_response_object_with_proxied_content_and_status(self):
+        self.response_mock.assert_called_once_with(
+            'upstream content', status=200)
+
+
+class HttpProxyHeaderPassThrough(ResponseConstructionTest):
+    def test_sets_upstream_headers_on_response_object(self):
+        self.response_stub.__setitem__.assert_any_call('Fake-Header', '123')
+
+    def test_doesnt_set_ignored_upstream_headers_on_response_obj(self):
+        self.assertNotIn(
+            call('Content-Encoding', 'gzip'),
+            self.response_stub.__setitem__.mock_calls)
