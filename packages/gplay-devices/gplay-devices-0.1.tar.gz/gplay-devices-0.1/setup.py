e31520ebbec4e911d3bbd05213bd63c607e2f342
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:46 +0000

    Add package gplay-devices/0.1/gplay-devices-0.1.tar.gz

---
+++ packages/gplay-devices/gplay-devices-0.1.tar.gz/gplay-devices-0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='gplay-devices',
+    version='0.1',
+    py_modules=['gplay-devices'],
+
+    author='Ronuk Raval',
+    author_email='ronuk.raval@gmail.com',
+    description='List of supported devices on Google Play',
+    license='MIT',
+    keywords='google-play supported-devices',
+    url='https://github.com/EncircleInc/gplay-devices',
+)
