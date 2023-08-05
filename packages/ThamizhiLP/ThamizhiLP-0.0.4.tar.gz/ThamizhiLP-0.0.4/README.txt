7b1ec5d1196e85007963c4261ee371411587f613
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:18 +0000

    Add package ThamizhiLP/0.0.4/ThamizhiLP-0.0.4.tar.gz

---
+++ packages/ThamizhiLP/ThamizhiLP-0.0.4.tar.gz/ThamizhiLP-0.0.4/README.txt
@@ -0,0 +1,2 @@
+Thamizhi  Language Processor - Docs yet to be created. 
+Mail to iamsarves@gmail.com
---
+++ packages/ThamizhiLP/ThamizhiLP-0.0.4.tar.gz/ThamizhiLP-0.0.4/setup.py
@@ -0,0 +1,27 @@
+# chardet's setup.py
+from distutils.core import setup
+setup(
+    name = "ThamizhiLP",
+    packages = ["ThamizhiLP"],
+    version = "0.0.4",
+    description = "Thamizhi Language Processor",
+    author = "Kengatharaiyer Sarveswaran",
+    author_email = "iamsarves@gmail.com",
+    url = "https://www.iamsarves.com",
+    download_url = "https://github.com/sarves/thamizhi",
+    keywords = ["Thamil", "Tamil", "NLP"],
+    classifiers = [
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Development Status :: 4 - Beta",
+        "Environment :: Other Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Text Processing :: Linguistic",
+        ],
+    long_description = """\
+Thamizhi Language Processing. Mail iamsarves@gmail.com for more information
+"""
+)
