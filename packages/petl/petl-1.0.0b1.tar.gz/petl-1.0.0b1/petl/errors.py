b9575587c2fc7ac5c7811980f7b60de38c77ff8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:37 +0000

    Add package petl/1.0.0b1/petl-1.0.0b1.tar.gz

---
+++ packages/petl/petl-1.0.0b1.tar.gz/petl-1.0.0b1/petl/errors.py
@@ -0,0 +1,19 @@
+from __future__ import absolute_import, print_function, division
+
+
+class DuplicateKeyError(Exception):
+
+    def __init__(self, key):
+        self.key = key
+
+    def __str__(self):
+        return 'duplicate key: %r' % self.key
+
+
+class FieldSelectionError(Exception):
+
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return 'selection is not a field or valid field index: %r' % self.value
