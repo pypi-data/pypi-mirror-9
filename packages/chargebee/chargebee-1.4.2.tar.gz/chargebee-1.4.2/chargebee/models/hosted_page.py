72b86fbd14a83e49e98f37f2d5be7b5bf8b97631
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:06 +0000

    Add package chargebee/1.4.2/chargebee-1.4.2.tar.gz

---
+++ packages/chargebee/chargebee-1.4.2.tar.gz/chargebee-1.4.2/chargebee/models/hosted_page.py
@@ -0,0 +1,44 @@
+import json
+from chargebee.model import Model
+from chargebee import request
+from chargebee import APIError
+
+class HostedPage(Model):
+
+    fields = ["id", "type", "url", "state", "failure_reason", "pass_thru_content", "embed", \
+    "created_at", "expires_at"]
+
+    @property
+    def content(self):
+        from chargebee import Content
+        if 'content' in self.values:
+            return Content(self.values['content'])
+        return None
+
+    @staticmethod
+    def checkout_new(params, env=None):
+        return request.send('post', request.uri_path("hosted_pages","checkout_new"), params, env)
+
+    @staticmethod
+    def checkout_existing(params, env=None):
+        return request.send('post', request.uri_path("hosted_pages","checkout_existing"), params, env)
+
+    @staticmethod
+    def update_card(params, env=None):
+        return request.send('post', request.uri_path("hosted_pages","update_card"), params, env)
+
+    @staticmethod
+    def update_payment_method(params, env=None):
+        return request.send('post', request.uri_path("hosted_pages","update_payment_method"), params, env)
+
+    @staticmethod
+    def checkout_onetime_charge(params, env=None):
+        return request.send('post', request.uri_path("hosted_pages","checkout_onetime_charge"), params, env)
+
+    @staticmethod
+    def checkout_onetime_addons(params, env=None):
+        return request.send('post', request.uri_path("hosted_pages","checkout_onetime_addons"), params, env)
+
+    @staticmethod
+    def retrieve(id, env=None):
+        return request.send('get', request.uri_path("hosted_pages",id), None, env)
---
+++ packages/chargebee/chargebee-1.4.2.tar.gz/chargebee-1.4.2/chargebee/models/transaction.py
@@ -0,0 +1,38 @@
+import json
+from chargebee.model import Model
+from chargebee import request
+from chargebee import APIError
+
+class Transaction(Model):
+    class LinkedInvoice(Model):
+      fields = ["invoice_id", "applied_amount", "invoice_date", "invoice_amount"]
+      pass
+
+    fields = ["id", "customer_id", "subscription_id", "payment_method", "reference_number", \
+    "gateway", "description", "type", "date", "amount", "id_at_gateway", "status", "error_code", \
+    "error_text", "voided_at", "void_description", "masked_card_number", "refunded_txn_id", "linked_invoices"]
+
+
+    @staticmethod
+    def list(params=None, env=None):
+        return request.send('get', request.uri_path("transactions"), params, env)
+
+    @staticmethod
+    def transactions_for_customer(id, params=None, env=None):
+        return request.send('get', request.uri_path("customers",id,"transactions"), params, env)
+
+    @staticmethod
+    def transactions_for_subscription(id, params=None, env=None):
+        return request.send('get', request.uri_path("subscriptions",id,"transactions"), params, env)
+
+    @staticmethod
+    def transactions_for_invoice(id, params=None, env=None):
+        return request.send('get', request.uri_path("invoices",id,"transactions"), params, env)
+
+    @staticmethod
+    def retrieve(id, env=None):
+        return request.send('get', request.uri_path("transactions",id), None, env)
+
+    @staticmethod
+    def record_payment(id, params, env=None):
+        return request.send('post', request.uri_path("invoices",id,"record_payment"), params, env)
