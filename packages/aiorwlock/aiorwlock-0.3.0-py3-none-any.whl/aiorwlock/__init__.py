397d07ce48f3e08d908c4fed239fc03e855ffd85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:29 +0000

    Add package aiorwlock/0.3.0/aiorwlock-0.3.0-py3-none-any.whl

---
+++ packages/aiorwlock/aiorwlock-0.3.0-py3-none-any.whl/aiorwlock/__init__.py
@@ -0,0 +1,260 @@
+import asyncio
+import collections
+
+__version__ = '0.3.0'
+__all__ = ['RWLock']
+
+
+class _ContextManager:
+    """Context manager.
+
+    This enables the following idiom for acquiring and releasing a
+    lock around a block:
+
+        with (yield from lock):
+            <block>
+
+    while failing loudly when accidentally using:
+
+        with lock:
+            <block>
+    """
+
+    def __init__(self, lock):
+        self._lock = lock
+
+    def __enter__(self):
+        # We have no use for the "as ..."  clause in the with
+        # statement for locks.
+        return None
+
+    def __exit__(self, *args):
+        try:
+            self._lock.release()
+        finally:
+            self._lock = None  # Crudely prevent reuse.
+
+
+# implementation based on:
+# http://bugs.python.org/issue8800
+
+# The internal lock object managing the RWLock state.
+class _RWLockCore:
+
+    def __init__(self, fast, loop):
+        self._do_yield = not fast
+        self._loop = loop or asyncio.get_event_loop()
+
+        self._read_waiters = collections.deque()
+        self._write_waiters = collections.deque()
+        self._state = 0  # positive is shared count, negative exclusive count
+        self._owning = []  # tasks will be few, so a list is not inefficient
+
+    @property
+    def read_locked(self):
+        return self._state > 0
+
+    @property
+    def write_locked(self):
+        return self._state < 0
+
+    # Acquire the lock in read mode.
+    @asyncio.coroutine
+    def acquire_read(self):
+        me = asyncio.Task.current_task(loop=self._loop)
+
+        if me in self._owning:
+            self._state += 1
+            self._owning.append(me)
+            if self._do_yield:
+                yield from asyncio.sleep(0.0, loop=self._loop)
+            return True
+
+        if not self._write_waiters and self._state >= 0:
+            self._state += 1
+            self._owning.append(me)
+            if self._do_yield:
+                yield from asyncio.sleep(0.0, loop=self._loop)
+            return True
+
+        fut = asyncio.Future(loop=self._loop)
+        self._read_waiters.append(fut)
+        try:
+            yield from fut
+            self._state += 1
+            self._owning.append(me)
+            return True
+        finally:
+            self._read_waiters.remove(fut)
+
+    # Acquire the lock in write mode.  A 'waiting' count is maintain ed,
+    # ensurring that 'readers' will yield to writers.
+    @asyncio.coroutine
+    def acquire_write(self):
+        me = asyncio.Task.current_task(loop=self._loop)
+
+        if me in self._owning:
+            if self._state > 0:
+                raise RuntimeError("cannot upgrade RWLock from read to write")
+            self._state -= 1
+            self._owning.append(me)
+            if self._do_yield:
+                yield from asyncio.sleep(0.0, loop=self._loop)
+            return True
+
+        if self._state == 0:
+            self._state -= 1
+            self._owning.append(me)
+            if self._do_yield:
+                yield from asyncio.sleep(0.0, loop=self._loop)
+            return True
+
+        fut = asyncio.Future(loop=self._loop)
+        self._write_waiters.append(fut)
+        try:
+            yield from fut
+            self._state -= 1
+            self._owning.append(me)
+            return True
+        finally:
+            self._write_waiters.remove(fut)
+
+    def release(self):
+        me = asyncio.Task.current_task(loop=self._loop)
+        try:
+            self._owning.remove(me)
+        except ValueError:
+            raise RuntimeError("cannot release an un-acquired lock")
+        if self._state > 0:
+            self._state -= 1
+        else:
+            self._state += 1
+        if self._state == 0:
+            if self._write_waiters:
+                self._write_waiters[0].set_result(None)
+            elif self._read_waiters:
+                self._read_waiters[0].set_result(None)
+
+
+# Lock objects to access the _RWLockCore in reader or writer mode
+class _ReaderLock:
+
+    def __init__(self, lock):
+        self._lock = lock
+
+    @property
+    def locked(self):
+        return self._lock.read_locked
+
+    @asyncio.coroutine
+    def acquire(self):
+        yield from self._lock.acquire_read()
+
+    def release(self):
+        self._lock.release()
+
+    def __repr__(self):
+        status = 'locked' if self._lock._state > 0 else 'unlocked'
+        return "<ReaderLock: [{}]>".format(status)
+
+    def __enter__(self):
+        raise RuntimeError(
+            '"yield from" should be used as context manager expression')
+
+    def __exit__(self, *args):
+        # This must exist because __enter__ exists, even though that
+        # always raises; that's how the with-statement works.
+        pass  # pragma: no cover
+
+    def __iter__(self):
+        # This is not a coroutine.  It is meant to enable the idiom:
+        #
+        #     with (yield from lock):
+        #         <block>
+        #
+        # as an alternative to:
+        #
+        #     yield from lock.acquire()
+        #     try:
+        #         <block>
+        #     finally:
+        #         lock.release()
+        yield from self.acquire()
+        return _ContextManager(self)
+
+
+class _WriterLock:
+
+    def __init__(self, lock):
+        self._lock = lock
+
+    @property
+    def locked(self):
+        return self._lock.write_locked
+
+    @asyncio.coroutine
+    def acquire(self):
+        yield from self._lock.acquire_write()
+
+    def release(self):
+        self._lock.release()
+
+    def __repr__(self):
+        status = 'locked' if self._lock._state < 0 else 'unlocked'
+        return "<WriterLock: [{}]>".format(status)
+
+    def __enter__(self):
+        raise RuntimeError(
+            '"yield from" should be used as context manager expression')
+
+    def __exit__(self, *args):
+        # This must exist because __enter__ exists, even though that
+        # always raises; that's how the with-statement works.
+        pass  # pragma: no cover
+
+    def __iter__(self):
+        # This is not a coroutine.  It is meant to enable the idiom:
+        #
+        #     with (yield from lock):
+        #         <block>
+        #
+        # as an alternative to:
+        #
+        #     yield from lock.acquire()
+        #     try:
+        #         <block>
+        #     finally:
+        #         lock.release()
+        yield from self.acquire()
+        return _ContextManager(self)
+
+
+class RWLock:
+    # Doc shamelessly ripped off from Java
+    """A RWLock maintains a pair of associated locks, one for read-only
+    operations and one for writing. The read lock may be held simultaneously
+    by multiple reader tasks, so long as there are no writers. The write
+    lock is exclusive.
+    """
+
+    core = _RWLockCore
+
+    def __init__(self, *, fast=False, loop=None):
+        self._loop = loop or asyncio.get_event_loop()
+        core = self.core(fast, self._loop)
+        self._reader_lock = _ReaderLock(core)
+        self._writer_lock = _WriterLock(core)
+
+    @property
+    def reader_lock(self):
+        """The lock used for read, or shared, access"""
+        return self._reader_lock
+
+    @property
+    def writer_lock(self):
+        """The lock used for write, or exclusive, access"""
+        return self._writer_lock
+
+    def __repr__(self):
+        return '<RWLock: {} {}>'.format(self.reader_lock.__repr__(),
+                                        self.writer_lock.__repr__())
---
+++ packages/aiorwlock/aiorwlock-0.3.0-py3-none-any.whl/tests/test_rwlock.py
@@ -0,0 +1,453 @@
+import asyncio
+from functools import wraps
+
+import unittest
+from unittest import mock
+
+from aiorwlock import RWLock
+
+
+def run_until_complete(fun):
+    if not asyncio.iscoroutinefunction(fun):
+        fun = asyncio.coroutine(fun)
+
+    @wraps(fun)
+    def wrapper(test, *args, **kw):
+        loop = test.loop
+        ret = loop.run_until_complete(
+            asyncio.wait_for(fun(test, *args, **kw), 15, loop=loop))
+        return ret
+    return wrapper
+
+
+class Bunch(object):
+    """A bunch of Tasks. Port python threading tests"""
+
+    def __init__(self, f, n, wait_before_exit=False, loop=None):
+        """
+        Construct a bunch of `n` tasks running the same function `f`.
+        If `wait_before_exit` is True, the tasks won't terminate until
+        do_finish() is called.
+        """
+        self._loop = loop or asyncio.get_event_loop()
+        self.f = f
+        self.n = n
+        self.started = []
+        self.finished = []
+        self._can_exit = not wait_before_exit
+
+        self._futures = []
+
+        @asyncio.coroutine
+        def task():
+            tid = asyncio.Task.current_task(loop=self._loop)
+            self.started.append(tid)
+            try:
+                yield from f()
+            finally:
+                self.finished.append(tid)
+                while not self._can_exit:
+                    yield from asyncio.sleep(0.01, loop=self._loop)
+
+        for i in range(n):
+            t = asyncio.Task(task(), loop=self._loop)
+            self._futures.append(t)
+
+    @asyncio.coroutine
+    def wait_for_finished(self):
+        yield from asyncio.gather(*self._futures, loop=self._loop)
+
+    def do_finish(self):
+        self._can_exit = True
+
+
+def _wait(loop=None):
+    _loop = loop or asyncio.get_event_loop()
+    yield from asyncio.sleep(0.01, loop=_loop)
+
+
+class TestRWLockReader(unittest.TestCase):
+
+    def setUp(self):
+        self.loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(None)
+
+    def tearDown(self):
+        self.loop.close()
+        del self.loop
+
+    def get_reader_lock(self, loop=None):
+        return RWLock(loop=loop).reader_lock
+
+    def get_writer_lock(self, loop=None):
+        return RWLock(loop=loop).writer_lock
+
+    def test_ctor_loop(self):
+        loop = mock.Mock()
+        rwlock = self.get_reader_lock(loop=loop)
+        self.assertIs(rwlock._lock._loop, loop)
+
+        rwlock = RWLock(loop=self.loop).reader_lock
+        self.assertIs(rwlock._lock._loop, self.loop)
+
+    def test_ctor_noloop(self):
+        asyncio.set_event_loop(self.loop)
+        rwlock = self.get_reader_lock(loop=None)
+        self.assertIs(rwlock._lock._loop, self.loop)
+
+    @run_until_complete
+    def test_repr(self):
+        rwlock = RWLock(loop=self.loop)
+        self.assertTrue('RWLock' in rwlock.__repr__())
+        self.assertTrue('WriterLock: [unlocked' in rwlock.__repr__())
+        self.assertTrue('ReaderLock: [unlocked' in rwlock.__repr__())
+
+        # reader lock __repr__
+        yield from rwlock.reader_lock.acquire()
+        self.assertTrue('ReaderLock: [locked]' in rwlock.__repr__())
+        rwlock.reader_lock.release()
+        self.assertTrue('ReaderLock: [unlocked]' in rwlock.__repr__())
+
+        # writer lock __repr__
+        yield from rwlock.writer_lock.acquire()
+        self.assertTrue('WriterLock: [locked]' in rwlock.__repr__())
+        rwlock.writer_lock.release()
+        self.assertTrue('WriterLock: [unlocked]' in rwlock.__repr__())
+
+    @run_until_complete
+    def test_release_unlocked(self):
+        rwlock = RWLock(loop=self.loop)
+        with self.assertRaises(RuntimeError):
+            rwlock.reader_lock.release()
+
+    @run_until_complete
+    def test_many_readers(self):
+        rwlock = RWLock(loop=self.loop)
+        N = 5
+        locked = []
+        nlocked = []
+
+        @asyncio.coroutine
+        def f():
+            yield from rwlock.reader_lock.acquire()
+            try:
+                locked.append(1)
+                yield from _wait(loop=self.loop)
+                nlocked.append(len(locked))
+                yield from _wait(loop=self.loop)
+                locked.pop(-1)
+            finally:
+                rwlock.writer_lock.release()
+
+        yield from Bunch(f, N, loop=self.loop).wait_for_finished()
+        self.assertTrue(max(nlocked) > 1)
+
+    @run_until_complete
+    def test_reader_recursion(self):
+
+        rwlock = RWLock(loop=self.loop)
+        N = 5
+        locked = []
+        nlocked = []
+
+        @asyncio.coroutine
+        def f():
+            yield from rwlock.reader_lock.acquire()
+            try:
+                yield from rwlock.reader_lock.acquire()
+                try:
+                    locked.append(1)
+                    yield from _wait(loop=self.loop)
+                    nlocked.append(len(locked))
+                    yield from _wait(loop=self.loop)
+                    locked.pop(-1)
+                finally:
+                    rwlock.reader_lock.release()
+            finally:
+                rwlock.reader_lock.release()
+
+        yield from Bunch(f, N, loop=self.loop).wait_for_finished()
+        self.assertTrue(max(nlocked) > 1)
+
+    @run_until_complete
+    def test_writer_recursion(self):
+        rwlock = RWLock(loop=self.loop)
+        N = 5
+        locked = []
+        nlocked = []
+
+        @asyncio.coroutine
+        def f():
+            yield from rwlock.writer_lock.acquire()
+            try:
+                yield from rwlock.writer_lock.acquire()
+                try:
+                    locked.append(1)
+                    yield from _wait(loop=self.loop)
+                    nlocked.append(len(locked))
+                    yield from _wait(loop=self.loop)
+                    locked.pop(-1)
+                finally:
+                    rwlock.reader_lock.release()
+            finally:
+                rwlock.reader_lock.release()
+
+        yield from Bunch(f, N, loop=self.loop).wait_for_finished()
+        self.assertEqual(max(nlocked), 1)
+
+    @run_until_complete
+    def test_writer_then_reader_recursion(self):
+        rwlock = RWLock(loop=self.loop)
+        N = 5
+        locked = []
+        nlocked = []
+
+        @asyncio.coroutine
+        def f():
+            yield from rwlock.writer_lock.acquire()
+            try:
+                yield from rwlock.reader_lock.acquire()
+                try:
+                    locked.append(1)
+                    yield from _wait(loop=self.loop)
+                    nlocked.append(len(locked))
+                    yield from _wait(loop=self.loop)
+                    locked.pop(-1)
+                finally:
+                    rwlock.reader_lock.release()
+            finally:
+                rwlock.writer_lock.release()
+
+        yield from Bunch(f, N, loop=self.loop).wait_for_finished()
+        self.assertEqual(max(nlocked), 1)
+
+    @run_until_complete
+    def test_writer_recursion_fail(self):
+        rwlock = RWLock(loop=self.loop)
+        N = 5
+        locked = []
+
+        @asyncio.coroutine
+        def f():
+            yield from rwlock.reader_lock.acquire()
+            try:
+                with self.assertRaises(RuntimeError):
+                    yield from rwlock.writer_lock.acquire()
+                locked.append(1)
+            finally:
+                rwlock.reader_lock.release()
+
+        yield from Bunch(f, N, loop=self.loop).wait_for_finished()
+        self.assertEqual(len(locked), N)
+
+    @run_until_complete
+    def test_readers_writers(self):
+        rwlock = RWLock(loop=self.loop)
+        N = 5
+        rlocked = []
+        wlocked = []
+        nlocked = []
+
+        @asyncio.coroutine
+        def r():
+            yield from rwlock.reader_lock.acquire()
+            try:
+                rlocked.append(1)
+                yield from _wait(loop=self.loop)
+                nlocked.append((len(rlocked), len(wlocked)))
+                yield from _wait(loop=self.loop)
+                rlocked.pop(-1)
+            finally:
+                rwlock.reader_lock.release()
+
+        @asyncio.coroutine
+        def w():
+            yield from rwlock.writer_lock.acquire()
+            try:
+                wlocked.append(1)
+                yield from _wait(loop=self.loop)
+                nlocked.append((len(rlocked), len(wlocked)))
+                yield from _wait(loop=self.loop)
+                wlocked.pop(-1)
+            finally:
+                rwlock.writer_lock.release()
+
+        b1 = Bunch(r, N, loop=self.loop)
+        b2 = Bunch(w, N, loop=self.loop)
+
+        yield from asyncio.sleep(0.0001, loop=self.loop)
+
+        yield from b1.wait_for_finished()
+        yield from b2.wait_for_finished()
+
+        r, w, = zip(*nlocked)
+
+        self.assertTrue(max(r) > 1)
+        self.assertEqual(max(w), 1)
+
+        for r, w in nlocked:
+            if w:
+                self.assertEqual(r, 0)
+            if r:
+                self.assertEqual(w, 0)
+
+    @run_until_complete
+    def test_writer_success(self):
+        # Verify that a writer can get access
+        rwlock = RWLock(loop=self.loop)
+        N = 5
+        reads = 0
+        writes = 0
+
+        @asyncio.coroutine
+        def r():
+            # read until we achive write successes
+            nonlocal reads, writes
+            while writes < 2:
+                # print("current pre-reads", reads)
+                yield from rwlock.reader_lock.acquire()
+                try:
+                    reads += 1
+                    # print("current reads", reads)
+                finally:
+                    rwlock.reader_lock.release()
+
+        @asyncio.coroutine
+        def w():
+            nonlocal reads, writes
+            while reads == 0:
+                yield from _wait(loop=self.loop)
+
+            for i in range(2):
+                yield from _wait(loop=self.loop)
+
+                # print("current pre-writes", reads)
+                yield from rwlock.writer_lock.acquire()
+                try:
+                    writes += 1
+                    # print("current writes", reads)
+                finally:
+                    rwlock.writer_lock.release()
+
+        b1 = Bunch(r, N, loop=self.loop)
+        b2 = Bunch(w, 1, loop=self.loop)
+
+        yield from b1.wait_for_finished()
+        yield from b2.wait_for_finished()
+        self.assertEqual(writes, 2)
+        # uncomment this to view performance
+        # print('>>>>>>>>>>>', writes, reads)
+
+    @run_until_complete
+    def test_writer_success_fast(self):
+        # Verify that a writer can get access
+        rwlock = RWLock(loop=self.loop, fast=True)
+        N = 5
+        reads = 0
+        writes = 0
+
+        @asyncio.coroutine
+        def r():
+            # read until we achive write successes
+            nonlocal reads, writes
+            while writes < 2:
+                # print("current pre-reads", reads)
+                yield from rwlock.reader_lock.acquire()
+                try:
+                    reads += 1
+                    # print("current reads", reads)
+                    yield from asyncio.sleep(0, loop=self.loop)
+                finally:
+                    rwlock.reader_lock.release()
+
+        @asyncio.coroutine
+        def w():
+            nonlocal reads, writes
+            while reads == 0:
+                yield from _wait(loop=self.loop)
+
+            for i in range(2):
+                yield from _wait(loop=self.loop)
+
+                # print("current pre-writes", reads)
+                yield from rwlock.writer_lock.acquire()
+                try:
+                    writes += 1
+                    # print("current writes", reads)
+                finally:
+                    rwlock.writer_lock.release()
+
+        b1 = Bunch(r, N, loop=self.loop)
+        b2 = Bunch(w, 1, loop=self.loop)
+
+        yield from b1.wait_for_finished()
+        yield from b2.wait_for_finished()
+        self.assertEqual(writes, 2)
+        # uncomment this to view performance
+        # print('>>>>>>>>>>>', writes, reads)
+
+    @run_until_complete
+    def test_writer_success_with_statement(self):
+        # Verify that a writer can get access
+        rwlock = RWLock(loop=self.loop)
+        N = 5
+        reads = 0
+        writes = 0
+
+        @asyncio.coroutine
+        def r():
+            # read until we achive write successes
+            nonlocal reads, writes
+            while writes < 2:
+                # print("current pre-reads", reads)
+                with (yield from rwlock.reader_lock):
+                    reads += 1
+                    # print("current reads", reads)
+
+        @asyncio.coroutine
+        def w():
+            nonlocal reads, writes
+            while reads == 0:
+                yield from _wait(loop=self.loop)
+
+            for i in range(2):
+                yield from _wait(loop=self.loop)
+
+                # print("current pre-writes", reads)
+                with (yield from rwlock.writer_lock):
+                    writes += 1
+
+        b1 = Bunch(r, N, loop=self.loop)
+        b2 = Bunch(w, 1, loop=self.loop)
+
+        yield from b1.wait_for_finished()
+        yield from b2.wait_for_finished()
+        self.assertEqual(writes, 2)
+        # uncomment this to view performance
+        # print('>>>>>>>>>>>', writes, reads)
+
+    def test_raise_error_on_with_for_reader_lock(self):
+        rwlock = RWLock(loop=self.loop)
+        with self.assertRaises(RuntimeError):
+            with rwlock.reader_lock:
+                pass
+
+    def test_raise_error_on_with_for_writer_lock(self):
+        rwlock = RWLock(loop=self.loop)
+        with self.assertRaises(RuntimeError):
+            with rwlock.writer_lock:
+                pass
+
+    @run_until_complete
+    def test_read_locked(self):
+        rwlock = RWLock(loop=self.loop)
+        self.assertFalse(rwlock.reader_lock.locked)
+        with (yield from rwlock.reader_lock):
+            self.assertTrue(rwlock.reader_lock.locked)
+
+    @run_until_complete
+    def test_write_locked(self):
+        rwlock = RWLock(loop=self.loop)
+        self.assertFalse(rwlock.writer_lock.locked)
+        with (yield from rwlock.writer_lock):
+            self.assertTrue(rwlock.writer_lock.locked)
