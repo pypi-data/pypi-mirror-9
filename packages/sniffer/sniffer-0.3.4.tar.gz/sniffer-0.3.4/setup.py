6f66a4497b7f2803995c910f24cbd133d8ba46b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:29 +0000

    Add package sniffer/0.3.4/sniffer-0.3.4.tar.gz

---
+++ packages/sniffer/sniffer-0.3.4.tar.gz/sniffer-0.3.4/setup.py
@@ -0,0 +1,44 @@
+from setuptools import setup, find_packages
+
+# will fail to install via pip/easy_install
+#from sniffer.metadata import __version__, __author__, __author_email__
+
+__version__, __author__, __author_email__ = "0.3.4", "Jeff Hui", "jeff@jeffhui.net"
+
+setup(
+    name='sniffer',
+    version=__version__,
+    description='An automatic test runner. Supports nose out of the box.',
+    long_description=open('README.rst').read(),
+    author=__author__,
+    author_email=__author_email__,
+    url='http://github.com/jeffh/sniffer/',
+    install_requires=[
+        'colorama',
+        'python-termstyle',
+        'nose',
+    ],
+    extras_require = {
+        'Growl': ['gntp==0.7'],
+        'LibNotify': ['py-notify==0.3.1'],
+        'OSX': ['MacFSEvents==0.2.8'],
+        #'Windows': ['pywin'], # not part of PYPI
+        'Linux': ['pyinotify==0.9.0'],
+    },
+    entry_points = {
+        'console_scripts': ['sniffer = sniffer:main'],
+    },
+    packages=find_packages(),
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Testing',
+        'Topic :: Software Development :: Libraries',
+    ],
+)
