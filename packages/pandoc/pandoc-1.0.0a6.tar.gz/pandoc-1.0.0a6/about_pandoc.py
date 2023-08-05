975aa9234231b390ca16bd7fabdc450927ccfea5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:54 +0000

    Add package pandoc/1.0.0a6/pandoc-1.0.0a6.tar.gz

---
+++ packages/pandoc/pandoc-1.0.0a6.tar.gz/pandoc-1.0.0a6/about_pandoc.py
@@ -0,0 +1,14 @@
+# coding: utf-8
+
+metadata = dict(
+  __name__    = "pandoc",
+  __version__ = "1.0.0-alpha.6", 
+  __license__ = "MIT License",
+  __author__  = u"Sébastien Boisgérault <Sebastien.Boisgerault@mines-paristech.fr>",
+  __url__     = "https://github.com/boisgera/pandoc",
+  __summary__ = "Pandoc Documents for Python",
+)
+
+globals().update(metadata)
+__all__ = metadata.keys()
+
