e9a827eec4de5abac4ef9194fd1027224d0db3c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:46 +0000

    Add package django-shuffler/0.02/django-shuffler-0.02.tar.gz

---
+++ packages/django-shuffler/django-shuffler-0.02.tar.gz/django-shuffler-0.02/setup.py
@@ -0,0 +1,12 @@
+# coding: utf-8
+from distutils.core import setup
+
+setup(
+    name='django-shuffler',
+    version='0.02',
+    description='Mixing DB data for faking personal data.',
+    author='Telminov Sergey',
+    url='https://github.com/telminov/shuffler',
+    packages=['shuffler',],
+    license='The MIT License',
+)
