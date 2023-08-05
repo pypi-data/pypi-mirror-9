df5cbb586b489bd62d421d7168028a7de6bbc63e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:41 +0000

    Add package infi.os-info/0.1.4/infi.os_info-0.1.4-py2.7.egg

---
+++ packages/infi.os-info/infi.os_info-0.1.4-py2.7.egg/infi/os_info/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.1.4"
+__git_commiter_name__ = "Guy Rozendorn"
+__git_commiter_email__ = "guy@rzn.co.il"
+__git_branch__ = '(detached from v0.1.4)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = 'cd31486f90076003dc6f5181129041a0dec9b344'
+__git_head_subject__ = 'Finished release v0.1.4.'
+__git_head_message__ = '* release/v0.1.4:\n  HOSTDEV-1949 fixing previous commit\n  HOSTDEV-1949 removing the git hash from the version string'
+__git_dirty_diff__ = ''
