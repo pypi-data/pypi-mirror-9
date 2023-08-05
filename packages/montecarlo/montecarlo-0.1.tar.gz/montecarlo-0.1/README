f851bbe65a3b633b2ceafc0da1cb28c159540c9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:16 +0000

    Add package montecarlo/0.1/montecarlo-0.1.tar.gz

---
+++ packages/montecarlo/montecarlo-0.1.tar.gz/montecarlo-0.1/README
@@ -0,0 +1,2 @@
+# pymontecarlo
+A small Python wrapper to quickly create simple Monte Carlo simulations.
---
+++ packages/montecarlo/montecarlo-0.1.tar.gz/montecarlo-0.1/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+  name = 'montecarlo',
+  packages = ['montecarlo'], # this must be the same as the name above
+  version = '0.1',
+  description = 'A small Python library for creating simple Monte Carlo simulations.',
+  author = 'Christopher Su',
+  author_email = 'chris+gh@christopher.su',
+  url = 'https://github.com/csu/pymontecarlo', # use the URL to the github repo
+  download_url = 'https://github.com/csu/pymontecarlo/tarball/0.1', # I'll explain this in a second
+)
