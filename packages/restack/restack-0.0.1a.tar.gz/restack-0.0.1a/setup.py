d333c4f58584581bb7be4f7a9e81313c23ee83e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:29 +0000

    Add package restack/0.0.1a/restack-0.0.1a.tar.gz

---
+++ packages/restack/restack-0.0.1a.tar.gz/restack-0.0.1a/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+
+setup(
+    name="restack",
+    version="0.0.1a",
+    author="Dave Jeffrey",
+    author_email="mail@davidjeffrey.co.uk",
+    description="A wrapper for working with Restack, the API for IoT communication",
+    license="BSD",
+    keywords="restack IoT",
+    url="https://github.com/lepoetemaudit/restack",
+    packages=['restack'],
+    long_description="For usage, see the homepage of this package on GitHub: https://github.com/lepoetemaudit/restack",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: BSD License",
+    ],
+)
