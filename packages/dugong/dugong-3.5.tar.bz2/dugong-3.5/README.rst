a19c7d5cb8e96e35eb1c8bd748a2e07f9e339118
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:47 +0000

    Add package dugong/3.5/dugong-3.5.tar.bz2

---
+++ packages/dugong/dugong-3.5.tar.bz2/dugong-3.5/README.rst
@@ -0,0 +1,78 @@
+==========================
+ The Python Dugong Module
+==========================
+
+.. default-role:: code
+
+.. start-intro
+
+The Python Dugong module provides an API for communicating with HTTP
+1.1 servers. It is an alternative to the standard library's
+`http.client` (formerly *httplib*) module. In contrast to
+`http.client`, Dugong:
+
+- allows you to send multiple requests right after each other without
+  having to read the responses first.
+
+- supports waiting for 100-continue before sending the request body.
+
+- raises an exception instead of silently delivering partial data if the
+  connection is closed before all data has been received.
+
+- raises one specific exception (`ConnectionClosed`) if the connection
+  has been closed (while `http.client` connection may raise any of
+  `BrokenPipeError`, `~http.client.BadStatusLine`,
+  `ConnectionAbortedError`, `ConnectionResetError`,
+  `~http.client.IncompleteRead` or simply return ``''`` on read)
+
+- supports non-blocking, asynchronous operation and is compatible with
+  the asyncio_ module.
+
+- can in most cases distinguish between an unavailable DNS server and
+  an unresolvable hostname.
+
+- is not compatible with old HTTP 0.9 or 1.0 servers.
+
+All request and response headers are represented as `str`, but must be
+encodable in latin1. Request and response body must be `bytes-like
+objects`_ or binary streams.
+
+Dugong requires Python 3.3 or newer.
+
+.. _`bytes-like objects`: http://docs.python.org/3/glossary.html#term-bytes-like-object
+
+Installation
+============
+
+As usual: download the tarball from PyPi_, extract it, and run ::
+
+  # python3 setup.py install [--user]
+
+To run the self-tests, install `py.test`_ and run ::
+
+  # py.test-3 test/
+
+
+.. _PyPi: https://pypi.python.org/pypi/dugong/#downloads
+
+Getting Help
+============
+
+The documentation can be `read online`__ and is also included in the
+*doc/html* directory of the dugong tarball.
+
+Please report any bugs on the `issue tracker`_. For discussion and
+questions, please subscribe to the `dugong mailing list`_.
+
+Development Status
+==================
+
+The Dugong API is not yet stable and may change from one release to
+the other.
+
+.. __: http://pythonhosted.org/dugong/
+.. _dugong mailing list: https://groups.google.com/d/forum/python-dugong
+.. _issue tracker: https://bitbucket.org/nikratio/python-dugong/issues
+.. _py.test: http://www.pytest.org/
+.. _asyncio: http://docs.python.org/3.4/library/asyncio.html
+
---
+++ packages/dugong/dugong-3.5.tar.bz2/dugong-3.5/setup.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python3
+
+import sys
+import os.path
+
+try:
+    import setuptools
+except ImportError:
+    raise SystemExit('Setuptools/distribute package not found. Please install from '
+                     'https://pypi.python.org/pypi/distribute')
+
+if sys.version_info < (3,3):
+    raise SystemExit('Python version is %d.%d.%d, but Dugong requires 3.3 or newer'
+                     % sys.version_info[:3])
+
+basedir = os.path.abspath(os.path.dirname(sys.argv[0]))
+
+def main():
+    try:
+        from sphinx.application import Sphinx #pylint: disable-msg=W0612
+    except ImportError:
+        pass
+    else:
+        fix_docutils()
+
+    with open(os.path.join(basedir, 'README.rst'), 'r') as fh:
+        long_desc = fh.read()
+    import dugong
+
+    setuptools.setup(
+          name='dugong',
+          zip_safe=True,
+          long_description=long_desc,
+          version=dugong.__version__,
+          description=('A HTTP 1.1 client module supporting asynchronous IO, pipelining '
+                       'and `Expect: 100-continue`. Designed for RESTful protocols.'),
+          author='Nikolaus Rath',
+          author_email='Nikolaus@rath.org',
+          license='PSF',
+          keywords=['http'],
+          package_dir={'': '.'},
+          packages=setuptools.find_packages(),
+          url='https://bitbucket.org/nikratio/python-dugong',
+          classifiers=['Programming Language :: Python :: 3',
+                       'Intended Audience :: Developers',
+                       'License :: OSI Approved :: Python Software Foundation License',
+                       'Topic :: Internet :: WWW/HTTP',
+                       'Topic :: Software Development :: Libraries :: Python Modules' ],
+          provides=['dugong'],
+          command_options={ 'sdist': { 'formats': ('setup.py', 'bztar') } ,
+                            'build_sphinx': {'version': ('setup.py', dugong.__version__),
+                                             'release': ('setup.py', dugong.__version__) }},
+     )
+
+
+def fix_docutils():
+    '''Work around https://bitbucket.org/birkenfeld/sphinx/issue/1154/'''
+
+    import docutils.parsers
+    from docutils.parsers import rst
+    old_getclass = docutils.parsers.get_parser_class
+
+    # Check if bug is there
+    try:
+        old_getclass('rst')
+    except AttributeError:
+        pass
+    else:
+        return
+
+    def get_parser_class(parser_name):
+        """Return the Parser class from the `parser_name` module."""
+        if parser_name in ('rst', 'restructuredtext'):
+            return rst.Parser
+        else:
+            return old_getclass(parser_name)
+    docutils.parsers.get_parser_class = get_parser_class
+
+    assert docutils.parsers.get_parser_class('rst') is rst.Parser
+
+if __name__ == '__main__':
+    main()
