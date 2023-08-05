939d593c475e320d12459a6d7d6789f77a4960f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:21 +0000

    Add package django-constance-trbs/0.7.4/django-constance-trbs-0.7.4.tar.gz

---
+++ packages/django-constance-trbs/django-constance-trbs-0.7.4.tar.gz/django-constance-trbs-0.7.4/constance/settings.py
@@ -0,0 +1,28 @@
+import os
+from constance.utils import import_module_attr, parse_config
+
+settings = import_module_attr(
+    os.getenv('CONSTANCE_SETTINGS_MODULE', 'django.conf.settings')
+)
+
+READONLY = getattr(settings, 'CONSTANCE_READONLY', False)
+
+REDIS_PREFIX = getattr(settings, 'CONSTANCE_REDIS_PREFIX', getattr(settings, 'CONSTANCE_PREFIX', 'constance:'))
+
+BACKEND = getattr(settings, 'CONSTANCE_BACKEND', 'constance.backends.redisd.RedisBackend')
+
+CONFIG = parse_config(getattr(settings, 'CONSTANCE_CONFIG', {}))
+
+CONNECTION_CLASS = getattr(settings, 'CONSTANCE_REDIS_CONNECTION_CLASS', getattr(settings, 'CONSTANCE_CONNECTION_CLASS', None))
+
+REDIS_CONNECTION = getattr(settings, 'CONSTANCE_REDIS_CONNECTION', getattr(settings, 'CONSTANCE_CONNECTION', {}))
+
+DATABASE_CACHE_BACKEND = getattr(settings, 'CONSTANCE_DATABASE_CACHE_BACKEND', None)
+
+DATABASE_PREFIX = getattr(settings, 'CONSTANCE_DATABASE_PREFIX', '')
+
+SUPERUSER_ONLY = getattr(settings, 'CONSTANCE_SUPERUSER_ONLY', getattr(settings, 'CONSTANCE_ACCESS_SUPERUSER_ONLY', True))
+
+CACHE_TIMEOUT = getattr(settings, 'CONSTANCE_CACHE_TIMEOUT', 1)
+
+CONFIG_CLASS = getattr(settings, 'CONSTANCE_CONFIG_CLASS', 'constance.config.Config')
---
+++ packages/django-constance-trbs/django-constance-trbs-0.7.4.tar.gz/django-constance-trbs-0.7.4/setup.py
@@ -0,0 +1,53 @@
+import os
+from setuptools import setup, find_packages
+
+# work around to prevent http://bugs.python.org/issue15881 from showing up
+try:
+    import multiprocessing  # NOQA
+except ImportError:
+    pass
+
+try:
+    f = open(os.path.join(os.path.dirname(__file__), 'README.rst'))
+    long_description = f.read().strip()
+    f.close()
+except IOError:
+    long_description = None
+
+setup(
+    name='django-constance-trbs',
+    version='0.7.4',
+    url="http://github.com/trbs/django-constance-trbs",
+    description='Django live settings with pluggable backends, including Redis.',
+    long_description=long_description,
+    author='Forked from Comoga Django Team',
+    author_email='trbs@trbs.net',
+    maintainer='Jannis Leidel',
+    maintainer_email='jannis@leidel.info',
+    license='BSD',
+    keywords='django libraries settings redis'.split(),
+    platforms='any',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Utilities',
+    ],
+    packages=find_packages(exclude=['tests']),
+    include_package_data=True,
+    test_suite='tests.runtests.main',
+    tests_require=['django>=1.6', 'django-picklefield', 'redis'],
+    install_requires=['six'],
+    zip_safe=False,
+    extras_require={
+        'database': ['django-picklefield'],
+        'redis': ['redis'],
+    }
+)
