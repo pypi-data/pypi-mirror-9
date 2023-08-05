dd6e0316eca390b1915da3163f47938adbf8d64d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:27 +0000

    Add package cish/0.2.1/cish-0.2.1.zip

---
+++ packages/cish/cish-0.2.1.zip/cish-0.2.1/cish/pyenv.py
@@ -0,0 +1,232 @@
+# Copyright (c) 2014, Stefan C. Mueller
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice,
+# this list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+# this list of conditions and the following disclaimer in the documentation
+# and/or other materials provided with the distribution.
+#
+# 3. Neither the name of the copyright holder nor the names of its contributors
+# may be used to endorse or promote products derived from this software without
+# specific prior written permission.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+import os.path
+import sys
+import shutil
+import subprocess
+import json
+
+class PyEnv(object):
+    """
+    Represents a python environment.
+    An environment consists of a python interpeter, python library paths and utilities such as `pip`.
+    Currenty we are only concerned about the paths were we find the interpeter and the utilities.
+    """
+
+    def __init__(self, search_paths):
+        """
+        Manually configure an enviroment.
+        You might want to consider using the factory methods provided in this module.
+        """
+        self.search_paths = search_paths
+        self.exec_patterns = ['{name}' ,'{name}.exe']
+        self.python_patterns = ['{name}', 'w{name}.exe' ,'{name}.exe']
+
+
+    def __getattr__(self, name):
+        """
+        Returns a method that invokes an exectuable in this environment.
+        Arguments passed to the method become console line arguments.
+        """
+        executable = self.find_executable(name)
+        
+        def invoker(*args):
+            subprocess.check_call([executable] + list(args))
+        return invoker
+
+
+    def virtualenv(self, path="env", system_side_packages=False):
+        """
+        Creates a new virtual environment and returns the PyEnv for it.
+        
+        :param path: Location of the virtual environment. Can be an
+            absolute path or a path relative to the current directory.
+            Defaults to `"env"`.
+            
+        :param system_side_packages: Should the venv see the packages
+            of the main environment?
+
+        :returns: PyEnv instance for the new environment.
+        """
+        abspath = os.path.abspath(path)
+        
+        if os.path.isdir(abspath):
+            shutil.rmtree(abspath)
+        elif os.path.exists(abspath):
+            os.remove(abspath)
+        
+        parent = os.path.dirname(abspath)
+        if not os.path.exists(parent):
+            os.makedirs(parent)
+        
+        currentdir = os.getcwd()
+        try:
+            os.chdir(parent)
+            
+            virtualenv = self.find_executable("virtualenv")
+            args = [virtualenv, os.path.basename(abspath)]
+            if system_side_packages:
+                args.append("--system-site-packages")
+            subprocess.check_call(args)
+
+            venv = from_virtualenv(abspath)
+            if system_side_packages:
+                venv.search_paths.extend(self.search_paths)
+            return venv
+
+        finally:
+            os.chdir(currentdir)
+
+
+    def find_executable(self, name):
+        """
+        Finds an executable with the given name in this enviroment.
+        
+        :returns: Absolute path to the executable.
+
+        :raises ValueError: if the exectuable could not be found.
+        """
+        
+        patterns = self.python_patterns if name == "python" else self.exec_patterns
+        candidate_names = [ext.format(name=name) for ext in patterns]
+        candidates = [os.path.join(path, name) for path in self.search_paths for name in candidate_names]
+   
+        for candidate in candidates:
+            if os.path.exists(candidate):
+                return candidate
+            
+        raise ValueError("Unable to find {name}. Looked at {candidates}".format(
+            name=repr(name),
+            candidates=", ".join(candidates)
+        ))
+        
+
+def interpeter_pyenv():
+    """
+    Returns the environment in which this script is running.
+    """
+    if not sys.executable:
+        raise ValueError("Interpeter that runs this script cannot be identified.")
+    return from_interpreter(sys.executable)
+
+
+def from_config(*search_paths):
+    """
+    Reads a json file with a `{name:"path/to/python", ...}` content and
+    returns a `dict` with the names and :class:`PyEnv` instances for each
+    entry.
+
+    By default it searches in the following locations (in that order):
+
+    * ./cish.json
+    * ${HOME}/cish.json  (or its windows equivalent)
+    * /etc/cish.json (linux, osx)
+    * C:\cish.json (windows)
+
+    Additional paths can be given as arguments and are searched before
+    falling back to the default paths.
+    """
+
+    filename = "cish.json"
+
+    paths = list(search_paths)
+    paths.append(os.path.abspath(filename))
+    paths.append(os.path.join(os.path.expanduser("~"), filename))
+    if os.name == "nt":
+        paths.append("C:\\" + filename)
+    else:
+        paths.append(os.path.sep + os.path.join("etc", filename))
+
+    for path in paths:
+        if os.path.exists(path):
+            config_file = path
+            break
+    else:
+        raise ValueError("Unable to locate configuration file. Searched in {paths}".format(
+            paths = ", ".join(paths)))
+
+    with open(config_file, 'r') as f:
+        config = json.load(f)
+
+    if not hasattr(config, "iteritems"):
+        raise ValueError("Invalid config file {f}. Must contain a key-value dict " + 
+            "as the top level element.".format(f=config_file))
+
+    return dict((name, from_interpreter(exe)) for name, exe in config.iteritems())
+
+
+def from_interpreter(exe):
+    """
+    Attempts to construct the environment for a given python interpeter by guessing
+    where the paths are relative to it.
+ 
+    :param exe: Path to the python interpeter executable.
+ 
+    :returns: Instance of :class:`PyEnv`
+    """
+    exeabs = os.path.abspath(exe)
+    if not os.path.exists(exeabs):
+        raise ValueError("Python interpreter {exe} does not exist here {exeabs}.".format(exe=exe, exeabs=exeabs))
+    
+    path = os.path.dirname(exeabs)
+    return _from_paths(path, ["Scripts", "scripts"])
+
+
+def from_virtualenv(path):
+    """
+    Attempts to construct the environment from the directory created by `virtualenv`.
+    
+    :param path: Directory containing the virtualenv.
+
+    :returns: Instance of :class:`PyEnv`
+    """
+    return _from_paths(path, ["bin", "Scripts", "scripts"])    
+
+
+def _from_paths(path, subdirs):
+    """
+    Helper method to construct an environment.
+
+    :param path: Base path of the environment.
+
+    :param subdirs: Directory names that might contain the interpeter and utilities.
+
+    :returns: Instance of :class:`PyEnv`
+    """
+    paths = [path] + [os.path.join(path, subdir) for subdir in subdirs]
+    search_paths = []
+    for path in paths:
+        if os.path.isdir(path): 
+            search_paths.append(path)
+    if not search_paths:
+        raise ValueError("Python environment not found. None of the directories exists {dirs}".format(
+            dirs=", ".join(paths)))
+    return PyEnv(search_paths)
+
---
+++ packages/cish/cish-0.2.1.zip/cish-0.2.1/cish/test_pyenv.py
@@ -0,0 +1,165 @@
+# Copyright (c) 2014, Stefan C. Mueller
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice,
+# this list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+# this list of conditions and the following disclaimer in the documentation
+# and/or other materials provided with the distribution.
+#
+# 3. Neither the name of the copyright holder nor the names of its contributors
+# may be used to endorse or promote products derived from this software without
+# specific prior written permission.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+import unittest
+import os.path
+import shutil
+import tempfile
+import subprocess
+import json
+
+from cish import pyenv
+
+class TestPyEnv(unittest.TestCase):
+    """
+    Unit-tests for :class:`pyenv.PyEnv`.
+    """
+
+    def setUp(self):
+        self.cwd = os.getcwd()
+        self.tmpdir = tempfile.mkdtemp()
+    
+    def tearDown(self):
+        os.chdir(self.cwd)
+        shutil.rmtree(self.tmpdir)
+
+    def test_linux_style(self):
+        """
+        Python installed using linux distribution packages
+        usually ends up in '/usr/bin', as do all the utilities.
+        """
+        self.create_files(["bin/python",
+                           "bin/pip",
+                           "bin/virtualenv"])
+        env = pyenv.from_interpreter(self.get_path("bin/python"))
+        self.assertEqual(env.find_executable("pip"), self.get_path("bin/pip"))
+        
+    def test_windows_style(self):
+        """
+        Windows installations have the utilities in a separate directory.
+        """
+        self.create_files(["python.exe",
+                           "Scripts/pip",
+                           "Scripts/virtualenv"])
+        env = pyenv.from_interpreter(self.get_path("python.exe"))
+        self.assertEqual(env.find_executable("pip"), self.get_path("Scripts/pip"))
+        
+    def test_prefer_wpython(self):
+        """
+        Windows installations have the utilities in a separate directory.
+        """
+        self.create_files(["python.exe",
+                           "wpython.exe"])
+        env = pyenv.from_interpreter(self.get_path("python.exe"))
+        self.assertEqual(env.find_executable("python"), self.get_path("wpython.exe"))
+        
+    def test_w_prefix_only_for_python(self):
+        """
+        On windows installations the interpreter might have a `w` prefix,
+        but this is not used for other executables, so we should not 
+        return the wrong one.
+        """
+        self.create_files(["python.exe",
+                           "wpip.exe",
+                           "pip.exe"])
+        env = pyenv.from_interpreter(self.get_path("python.exe"))
+        self.assertEqual(env.find_executable("pip"), self.get_path("pip.exe"))
+       
+    def test_getattr(self):
+        """
+        Test if we can invoke the python interpreter using a method on the environment.
+        """
+        env = pyenv.interpeter_pyenv() 
+        env.python("-c", "pass")
+
+    def test_virtualenv_abs(self):
+        """
+        Tests if we can create a virtual environment giving an absolute path.
+        """
+        env = pyenv.interpeter_pyenv()
+        venv = env.virtualenv(self.get_path("myenv"))
+        self.assertTrue(venv.find_executable("python").startswith(self.tmpdir))
+
+    def test_virtualenv_relative(self):
+        """
+        Tests if we can create a virtual environment giving a relative path.
+        """
+        os.chdir(self.tmpdir)
+        env = pyenv.interpeter_pyenv()
+        venv = env.virtualenv("myenv")
+        self.assertTrue(venv.find_executable("python").startswith(self.tmpdir))
+
+    def test_virtualenv_overwrites(self):
+        """
+        Tests if a virtual env is replacing an existing virtual env.
+        """
+        env = pyenv.interpeter_pyenv()
+        env.virtualenv(self.get_path("myenv"))
+        with open(self.get_path("myenv/helloworld"), 'w') as f:
+            f.write("content")
+        
+        env.virtualenv(self.get_path("myenv"))
+        
+        self.assertFalse(os.path.exists(self.get_path("myenv/helloworld")))
+
+    def test_config(self):
+        """
+        Test if we can load an environment from a configuration file.
+        """
+        self.create_files(["python.exe"])
+
+        cfgfile = self.get_path("config.json")
+        with open(cfgfile, 'w') as f:
+            json.dump({"abc": self.get_path("python.exe")}, f)
+
+        envs = pyenv.from_config(cfgfile)
+        self.assertEqual(self.get_path("python.exe"), envs["abc"].find_executable("python"))
+ 
+    def get_path(self, path):
+        """
+        Returns the absolute path to a file relative to the temporary directory.
+        """
+        return os.path.join(self.tmpdir, path.replace("/", os.sep))
+
+    def create_files(self, files):
+        """
+        Takes a list of files (optionally with relative paths)
+        and creates them in the temporary directory.
+        """
+        files = [f.replace("/", os.sep) for f in files]
+        
+        for relative_file in files:
+            relative_file = relative_file.replace("/", os.sep)
+            relative_path, filename = os.path.split(relative_file)
+            absolute_path = os.path.join(self.tmpdir, relative_path)
+            if not os.path.exists(absolute_path):
+                os.makedirs(absolute_path)
+            absolute_file = os.path.join(absolute_path, filename)
+            with open(absolute_file, 'w') as f:
+                f.write(relative_file)
