486f97c18cd211356c52dfa8f71e722137286605
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:08 +0000

    Add package partlycloudy/0.3/partlycloudy-0.3.tar.gz

---
+++ packages/partlycloudy/partlycloudy-0.3.tar.gz/partlycloudy-0.3/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+
+setup(name = 'partlycloudy',
+      version = '0.3',
+      author = 'Connor Hudson',
+      author_email = 'pypi@tekhne-blog.appspotmail.com',
+      url = 'https://github.com/technoboy10/partly-cloudy',
+      description = 'A ridiculously lightweight Python wrapper for the littleBits Cloud API',
+      license = 'MIT',
+      packages = ['partlycloudy'],
+      install_requires = [
+          'requests == 2.4.0',
+      ],
+      classifiers = [
+        "Programming Language :: Python",
+      ],
+)
