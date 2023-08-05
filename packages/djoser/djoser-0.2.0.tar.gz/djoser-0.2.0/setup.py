2061043ce78cc454d089bc1fd32d2e9cd52d7b2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:11 +0000

    Add package djoser/0.2.0/djoser-0.2.0.tar.gz

---
+++ packages/djoser/djoser-0.2.0.tar.gz/djoser-0.2.0/setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+try:
+    import pypandoc
+    description = pypandoc.convert('README.md', 'rst')
+except (IOError, ImportError):
+    description = open('README.md').read()
+
+REQUIREMENTS = [i.strip() for i in open('requirements.txt').readlines()]
+
+setup(
+    name='djoser',
+    version='0.2.0',
+    packages=['djoser'],
+    license='MIT',
+    author='SUNSCRAPERS',
+    description='REST version of Django authentication system.',
+    author_email='info@sunscrapers.com',
+    long_description=description,
+    install_requires=REQUIREMENTS,
+    include_package_data=True,
+    url='https://github.com/sunscrapers/djoser',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+    ]
+)
