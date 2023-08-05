4a500c4697f70529913b4f1c712aac54aae4d493
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:43 +0000

    Add package Flask-Resize/0.2.3/Flask-Resize-0.2.3.tar.gz

---
+++ packages/Flask-Resize/Flask-Resize-0.2.3.tar.gz/Flask-Resize-0.2.3/setup.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+"""
+Flask-Resize
+------------
+
+Flask extension for resizing images in templates.
+
+See https://github.com/jmagnusson/Flask-Resize for usage.
+
+"""
+from __future__ import print_function
+from pip.req import parse_requirements
+from setuptools import setup, find_packages
+
+
+appname = 'Flask-Resize'
+pkgname = appname.lower().replace('-', '_')
+metadata_relpath = '{}/metadata.py'.format(pkgname)
+
+# Get package metadata. We use exec here instead of importing the
+# package directly, so we can avoid any potential import errors.
+with open(metadata_relpath) as fh:
+    metadata = {}
+    exec(fh.read(), globals(), metadata)
+
+# Temp fix for https://github.com/tweepy/tweepy/issues/533
+import uuid
+requirements = parse_requirements('requirements.txt', session=uuid.uuid1())
+
+setup(
+    name=appname,
+    version=metadata['__version__'],
+    description='Flask extension for resizing images in templates',
+    long_description=__doc__,
+    packages=find_packages(),
+    package_dir={pkgname: pkgname},
+    package_data={
+        pkgname: [
+            'fonts/*.ttf',
+        ],
+    },
+    install_requires=[str(ir.req) for ir in requirements],
+    entry_points={
+        'console_scripts': {},
+    },
+    author='Jacob Magnusson',
+    author_email='m@jacobian.se',
+    url='https://github.com/jmagnusson/Flask-Resize',
+    license='BSD',
+    platforms='any',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
