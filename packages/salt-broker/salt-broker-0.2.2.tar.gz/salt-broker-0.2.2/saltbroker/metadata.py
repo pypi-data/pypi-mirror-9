021660304dab30f54a35307af5d6f17eee23c8b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:28 +0000

    Add package salt-broker/0.2.2/salt-broker-0.2.2.tar.gz

---
+++ packages/salt-broker/salt-broker-0.2.2.tar.gz/salt-broker-0.2.2/saltbroker/metadata.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+NAME = 'salt-broker'
+DESCRIPTION = 'A lightweight proxy for saltstack'
+VERSION = '0.2.2'
+AUTHOR = 'pengyao'
+AUTHOR_EMAIL = 'pengyao@pengyao.org'
+COPYRIGHT = 'Copyright © 2015 ' + AUTHOR
+LICENCE = 'Apache License V2.0'
+URL = 'https://github.com/pengyao/salt-broker'
+
+
+
