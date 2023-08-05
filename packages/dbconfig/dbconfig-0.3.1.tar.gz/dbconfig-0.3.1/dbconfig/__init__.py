97a8691321eee0ea09064d9cd553862d56c67669
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:02 +0000

    Add package dbconfig/0.3.1/dbconfig-0.3.1.tar.gz

---
+++ packages/dbconfig/dbconfig-0.3.1.tar.gz/dbconfig-0.3.1/dbconfig/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+from .models import ConfigFile
+from .utils import ConfigGroup, autodiscover
+from .context import config_context
+
+
+__all__ = ("VERSION", "ConfigFile", "ConfigGroup",
+           "autodiscover", "config_context", )
+
+
+VERSION = (0, 1, 7)
