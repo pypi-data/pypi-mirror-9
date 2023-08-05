41659f9a4a48b6d5b926d69b0123304cc95aa8c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:53 +0000

    Add package pylsdj/2.2.1/pylsdj-2.2.1.tar.gz

---
+++ packages/pylsdj/pylsdj-2.2.1.tar.gz/pylsdj-2.2.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(name='pylsdj',
+      version='2.2.1',
+      description='A utility belt for dealing with LSDJ-related files',
+      url='http://github.com/alexras/pylsdj',
+      author='Alex Rasmussen',
+      author_email='alexras@acm.org',
+      classifiers = [
+          'Development Status :: 4 - Beta',
+          'Environment :: Other Environment',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: MIT License',
+          'Natural Language :: English',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Topic :: Software Development :: Libraries :: Python Modules'],
+      license='MIT',
+      packages=['pylsdj', 'pylsdj.vendor'],
+      requires=['bread'],
+      install_requires=['bread>=1.5.4'],
+      zip_safe=False)
