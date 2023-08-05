31ca431e7d6faa00bfb541494da48960638c4d05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:58 +0000

    Add package timesheet/0.5.0/timesheet-0.5.0.tar.gz

---
+++ packages/timesheet/timesheet-0.5.0.tar.gz/timesheet-0.5.0/timesheet/commands/help.py
@@ -0,0 +1,22 @@
+__author__ = 'vahid'
+
+from timesheet.commands import Command, get_command, get_available_command_names
+from argcomplete.completers import ChoicesCompleter
+
+
+class HelpCommand(Command):
+    name = 'help'
+    description = 'Prints help for given command'
+
+    @classmethod
+    def add_arguments(cls):
+        cls.parser.add_argument('command', nargs='?', help="Command to print help about that").completer = \
+            ChoicesCompleter(get_available_command_names())
+
+    def do_job(self):
+        if self.args.command:
+            command_class = get_command(self.args.command)
+            command_class.help()
+        else:
+            from timesheet.cli import parser
+            parser.print_help()
