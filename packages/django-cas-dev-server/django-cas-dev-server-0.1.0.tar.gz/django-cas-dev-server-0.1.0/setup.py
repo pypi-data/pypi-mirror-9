4bc66bae1fdc0ad9f592a347a717958513f26f68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:11 +0000

    Add package django-cas-dev-server/0.1.0/django-cas-dev-server-0.1.0.tar.gz

---
+++ packages/django-cas-dev-server/django-cas-dev-server-0.1.0.tar.gz/django-cas-dev-server-0.1.0/setup.py
@@ -0,0 +1,35 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='django-cas-dev-server',
+    version='0.1.0',
+    description='Extends the Django development server to include a local CAS server.',
+    url='https://github.com/TaymonB/django-cas-dev-server',
+    author='Taymon A. Beal',
+    author_email='taymonbeal@gmail.com',
+    license='MIT',
+    platforms=['OS Independent'],
+    keywords='development django cas',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Internet :: WWW/HTTP :: HTTP Servers',
+        'Topic :: Software Development',
+    ],
+    packages=find_packages(),
+    install_requires=[
+        'Django>=1.7',
+        'django-environ',
+        'django-mama-cas',
+    ],
+)
