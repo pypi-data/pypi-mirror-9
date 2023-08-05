f24e9e546df629f88c0653365b788946f20991c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:58 +0000

    Add package python-axolotl/0.1.5/python_axolotl-0.1.5-py2.7.egg

---
+++ packages/python-axolotl/python_axolotl-0.1.5-py2.7.egg/axolotl/util/hexutil.py
@@ -0,0 +1,11 @@
+import codecs, sys
+decode_hex = codecs.getdecoder("hex_codec")
+class HexUtil:
+    @staticmethod
+    def decodeHex(hexString):
+        hexString = hexString.encode() if type(hexString) is str else hexString
+        result = decode_hex(hexString)[0]
+        if sys.version_info >= (3,0):
+            result = result.decode('latin-1')
+        return result
+
