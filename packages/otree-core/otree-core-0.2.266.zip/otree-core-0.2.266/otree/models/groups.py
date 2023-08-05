3584c86c9ae89ab410bdfccce032deaf083725d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:31 +0000

    Add package otree-core/0.2.266/otree-core-0.2.266.zip

---
+++ packages/otree-core/otree-core-0.2.266.zip/otree-core-0.2.266/otree/models/groups.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from save_the_change.mixins import SaveTheChange
+
+from otree.db import models
+from otree.common_internal import get_models_module
+
+
+class BaseGroup(SaveTheChange, models.Model):
+    """Base class for all Groupes.
+    """
+
+    _is_missing_players = models.BooleanField(default=False)
+
+    class Meta:
+        abstract = True
+
+    def __unicode__(self):
+        return str(self.pk)
+
+    def get_player_by_id(self, id_in_group):
+        for p in self.get_players():
+            if p.id_in_group == id_in_group:
+                return p
+
+    def get_player_by_role(self, role):
+        for p in self.get_players():
+            if p.role() == role:
+                return p
+
+    def set_players(self, players_list):
+        for i, player in enumerate(players_list, start=1):
+            player.group = self
+            player.id_in_group = i
+            player.save()
+
+    @property
+    def _Constants(self):
+        return get_models_module(self._meta.app_label).Constants
