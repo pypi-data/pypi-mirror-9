28fd6b7ed2bca8e77ef3ac03ce7226cab1157887
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:01 +0000

    Add package sphinxcontrib-newsfeed/0.1.4/sphinxcontrib-newsfeed-0.1.4.tar.gz

---
+++ packages/sphinxcontrib-newsfeed/sphinxcontrib-newsfeed-0.1.4.tar.gz/sphinxcontrib-newsfeed-0.1.4/demo/conf.py
@@ -0,0 +1,12 @@
+
+# Enable `sphinxcontrib-newsfeed` extension.
+extensions = ['sphinxcontrib.newsfeed']
+
+# Standard configuration.
+project = u'Elvënsense'
+master_doc = 'index'
+html_theme = 'sphinxdoc'
+highlight_language = 'rest'
+templates_path = ['_templates']
+exclude_patterns = ['_build', '_templates']
+
---
+++ packages/sphinxcontrib-newsfeed/sphinxcontrib-newsfeed-0.1.4.tar.gz/sphinxcontrib-newsfeed-0.1.4/demo/index.rst
@@ -0,0 +1,14 @@
+Elvënsense News
+===============
+
+This is a small demo for ``sphinxcontrib-newsfeed`` extension.
+What follows is the output of ``feed`` directive.
+
+.. feed::
+   :rss: index.rss
+   :title: Elvënsense News
+   :link: http://example.com/
+
+   release
+   welcome
+
