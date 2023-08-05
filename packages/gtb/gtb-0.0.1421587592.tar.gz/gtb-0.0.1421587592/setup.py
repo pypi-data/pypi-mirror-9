f242017038631a7c2ed72a1dee5df470ab29c832
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:15 +0000

    Add package gtb/0.0.1421587592/gtb-0.0.1421587592.tar.gz

---
+++ packages/gtb/gtb-0.0.1421587592.tar.gz/gtb-0.0.1421587592/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+	description = 'Tools for inspecting running greenlets and getting tracebacks',
+	author = 'Mike Lang',
+	install_requires = ['greenlet'],
+	py_modules = ['gtb'],
+	author_email = 'mikelang3000@gmail.com',
+	version = '0.0.1421587592',
+	long_description = 'Tools for inspecting running greenlets and getting tracebacks',
+	name = 'gtb',
+)
