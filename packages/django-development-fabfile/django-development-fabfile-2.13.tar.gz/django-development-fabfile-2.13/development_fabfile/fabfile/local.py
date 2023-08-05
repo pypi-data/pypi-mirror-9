2c1bdbf48132ad1ac0a565d14cedcfbdb8f7534d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:19 +0000

    Add package django-development-fabfile/2.13/django-development-fabfile-2.13.tar.gz

---
+++ packages/django-development-fabfile/django-development-fabfile-2.13.tar.gz/django-development-fabfile-2.13/development_fabfile/fabfile/local.py
@@ -0,0 +1,318 @@
+"""Fabfile for tasks that only manipulate things on the local machine."""
+import os
+import re
+
+from django.conf import settings
+
+from fabric.api import env, hide, lcd, local
+from fabric.api import settings as fab_settings
+from fabric.colors import green, red
+from fabric.utils import abort, warn, puts
+from fabric.state import env
+
+from .servers import local_machine
+
+
+USER_AND_HOST = '-U {0}'.format(settings.LOCAL_PG_ADMIN_ROLE)
+if getattr(settings, 'LOCAL_PG_USE_LOCALHOST', True):
+    USER_AND_HOST += ' -h localhost'
+
+HOST = ''
+if getattr(settings, 'LOCAL_PG_USE_LOCALHOST', True):
+    HOST = '-h localhost'
+
+
+DB_PASSWORD = settings.DATABASES['default']['PASSWORD']
+
+
+def check():
+    """Runs flake8, check_coverage and test."""
+    flake8()
+    syntax_check()
+    jshint()
+    test()
+    check_coverage()
+
+
+def check_coverage():
+    """Checks if the coverage is 100%."""
+    with lcd(settings.LOCAL_COVERAGE_PATH):
+        total_line = local('grep -n Total index.html', capture=True)
+        match = re.search(r'^(\d+):', total_line)
+        total_line_number = int(match.groups()[0])
+        percentage_line_number = total_line_number + 4
+        percentage_line = local(
+            'awk NR=={0} index.html'.format(percentage_line_number),
+            capture=True)
+        match = re.search(r'<td>(\d.+)%</td>', percentage_line)
+        percentage = float(match.groups()[0])
+    if percentage < 100:
+        abort(red('Coverage is {0}%'.format(percentage)))
+    print(green('Coverage is {0}%'.format(percentage)))
+
+
+def create_db(with_postgis=False):
+    """
+    Creates the local database.
+
+    :param with_postgis: If ``True``, the postgis extension will be installed.
+
+    """
+    local_machine()
+    local('psql {0} -c "CREATE USER {1} WITH PASSWORD \'{2}\'"'.format(
+        USER_AND_HOST, env.db_role, DB_PASSWORD))
+    local('psql {0} -c "CREATE DATABASE {1} ENCODING \'UTF8\'"'.format(
+        USER_AND_HOST, env.db_name))
+    if with_postgis:
+        local('psql {0} {1} -c "CREATE EXTENSION postgis"'.format(
+            USER_AND_HOST, env.db_name))
+    local('psql {0} -c "GRANT ALL PRIVILEGES ON DATABASE {1}'
+          ' to {2}"'.format(USER_AND_HOST, env.db_name, env.db_role))
+    local('psql {0} -c "GRANT ALL PRIVILEGES ON ALL TABLES'
+          ' IN SCHEMA public TO {1}"'.format(
+              USER_AND_HOST, env.db_role))
+
+
+def delete_db():
+    """
+    Deletes all data in the database.
+
+    You need django-extensions in order to use this.
+    However, please note that this is not as thorough as a real database drop.
+
+    """
+    local(' ./manage.py reset_db --router=default --noinput')
+
+
+def export_db(filename=None):
+    """
+    Exports the database.
+
+    Make sure that you have this in your ``~/.pgpass`` file:
+
+    localhost:5433:*:<db_role>:<password>
+
+    Also make sure that the file has ``chmod 0600 .pgpass``.
+
+    Usage::
+
+        fab export_db
+        fab export_db:filename=foobar.dump
+
+    """
+    local_machine()
+    if not filename:
+        filename = settings.DB_DUMP_FILENAME
+    local('pg_dump -c -Fc -O -U {0}{1} -f {2}'.format(
+        env.db_role, HOST, filename))
+
+
+def drop_db():
+    """Drops the local database."""
+    local_machine()
+    with fab_settings(warn_only=True):
+        local('psql {0} -c "DROP DATABASE {1}"'.format(
+            USER_AND_HOST, env.db_name))
+        local('psql {0} -c "DROP USER {1}"'.format(
+            USER_AND_HOST, env.db_role))
+
+
+def jshint():
+    """Runs jshint checks."""
+    with fab_settings(warn_only=True):
+        needs_to_abort = False
+        # because jshint fails with exit code 2, we need to allow this as
+        # a successful exit code in our env
+        if 2 not in env.ok_ret_codes:
+            env.ok_ret_codes.append(2)
+        output = local(
+            'find -name "{}" -print'.format('*.js'),
+            capture=True,
+        )
+        files = output.split()
+        jshint_installed = local('command -v jshint', capture=True)
+        if not jshint_installed.succeeded:
+            warn(red(
+                "To enable an extended check of your js files, please"
+                " install jshint by entering:\n\n    npm install -g jshint"
+            ))
+        else:
+            for file in files:
+                if hasattr(settings, 'JSHINT_CHECK_EXCLUDES'):
+                    excludes = settings.JSHINT_CHECK_EXCLUDES
+                else:
+                    excludes = settings.SYNTAX_CHECK_EXCLUDES
+                if any(s in file for s in excludes):
+                    continue
+                jshint_result = local(
+                    'jshint {0}'.format(file),
+                    capture=True
+                )
+                if jshint_result:
+                    warn(red('JS errors detected in file {0}'.format(
+                        file
+                    )))
+                    puts(jshint_result)
+                    needs_to_abort = True
+        if needs_to_abort:
+            abort(red('There have been errors. Please fix them and run'
+                      ' the check again.'))
+        else:
+            puts(green('jshint found no errors. Very good!'))
+
+def syntax_check():
+    """Runs flake8 against the codebase."""
+    with fab_settings(warn_only=True):
+        for file_type in settings.SYNTAX_CHECK:
+            needs_to_abort = False
+            # because egrep fails with exit code 1, we need to allow this as
+            # a successful exit code in our env
+            if 1 not in env.ok_ret_codes:
+                env.ok_ret_codes.append(1)
+            output = local(
+                'find -name "{}" -print'.format(file_type),
+                capture=True,
+            )
+            files = output.split()
+            for file in files:
+                if any(s in file for s in settings.SYNTAX_CHECK_EXCLUDES):
+                    continue
+                result = local('egrep -i -n "{0}" {1}'.format(
+                    settings.SYNTAX_CHECK[file_type], file), capture=True)
+                if result:
+                    warn(red("Syntax check found in '{0}': {1}".format(
+                        file, result)))
+                    needs_to_abort = True
+            if needs_to_abort:
+                abort(red('There have been errors. Please fix them and run'
+                          ' the check again.'))
+            else:
+                puts(green('Syntax check found no errors. Very good!'))
+
+
+def flake8():
+    """Runs flake8 against the codebase."""
+    return local('flake8 --ignore=E126 --statistics '
+                 '--exclude=submodules,migrations,node_modules '
+                 '--snippets="import ipdb,ipdb.set_trace()" .')
+
+
+def import_db(filename=None):
+    """
+    Imports the database.
+
+    Make sure that you have this in your ``~/.pgpass`` file:
+
+    localhost:5433:*:publishizer_publishizer:publishizer
+
+    Also make sure that the file has ``chmod 0600 .pgpass``.
+
+    Usage::
+
+        fab import_db
+        fab import_db:filename=foobar.dump
+
+    """
+    local_machine()
+    if not filename:
+        filename = settings.DB_DUMP_FILENAME
+    with fab_settings(warn_only=True):
+        local('pg_restore -O -c -U {0}{1} -d {2} {3}'.format(
+            env.db_role, HOST, env.db_name, filename))
+
+
+def import_media(filename=None):
+    """
+    Extracts media dump into your local media root.
+
+    Please note that this might overwrite existing local files.
+
+    Usage::
+
+        fab import_media
+        fab import_media:filename=foobar.tar.gz
+
+    """
+    if not filename:
+        filename = settings.MEDIA_DUMP_FILENAME
+
+    project_root = os.getcwd()
+
+    with fab_settings(hide('everything'), warn_only=True):
+        is_backup_missing = local('test -e "$(echo %s)"' % os.path.join(
+            project_root, filename)).failed
+    if is_backup_missing:
+        abort(red('ERROR: There is no media backup that could be imported in'
+                  ' {0}. We need a file called {1} in that folder.'.format(
+                      project_root, filename)))
+
+    # copy the dump into the media root folder
+    with lcd(project_root):
+        local('cp {0} {1}'.format(filename, settings.MEDIA_ROOT))
+
+    # extract and remove media dump
+    with lcd(settings.MEDIA_ROOT):
+        local('tar -xvf {0}'.format(filename))
+        local('rm -rf {0}'.format(filename))
+
+
+def lessc(responsive=False):
+    """
+    Compiles all less files.
+
+    This is useful if you are using the Twitter Bootstrap Framework.
+
+    """
+    local('lessc {0}/static/css/bootstrap.less'
+          ' {0}/static/css/bootstrap.css'.format(settings.PROJECT_NAME))
+    if responsive:
+        local('lessc {0}/static/css/responsive.less'
+              ' {0}/static/css/bootstrap-responsive.css'.format(
+                  settings.PROJECT_NAME))
+
+
+def rebuild():
+    """
+    Deletes and re-creates your DB. Needs django-extensions and South.
+
+    """
+    drop_db()
+    create_db()
+    local('python2.7 manage.py syncdb --all --noinput')
+    local('python2.7 manage.py migrate --fake')
+
+
+def reset_passwords():
+    """Resets all passwords to `test123`."""
+    local('python2.7 manage.py set_fake_passwords --password=test123')
+
+
+def test(options=None, integration=1, selenium=1, test_settings=None):
+    """
+    Runs manage.py tests.
+
+    Usage::
+
+        fab test
+        fab test:app
+        fab test:app.tests.forms_tests:TestCaseName
+        fab test:integration=0
+        fab test:selenium=0
+
+    """
+    if test_settings is None:
+        test_settings = settings.TEST_SETTINGS_PATH
+    command = ("./manage.py test -v 2 --traceback --failfast" +
+               " --settings={0}".format(test_settings))
+    if int(integration) == 0:
+        command += " --exclude='integration_tests'"
+    if int(selenium) == 0:
+        command += " --exclude='selenium_tests'"
+    if options:
+        command += ' {0}'.format(options)
+    with fab_settings(warn_only=True):
+        result = local(command, capture=False)
+    if result.failed:
+        abort(red('Some tests failed'))
+    else:
+        print green('All tests passed')
---
+++ packages/django-development-fabfile/django-development-fabfile-2.13.tar.gz/django-development-fabfile-2.13/development_fabfile/fabfile/remote.py
@@ -0,0 +1,328 @@
+"""Fab tasks that execute things on a remote server."""
+from django.conf import settings
+
+from fabric.api import cd, env, local, run
+
+from .local import drop_db, create_db, import_db, reset_passwords
+from .utils import require_server, run_workon
+
+
+@require_server
+def run_collectstatic():
+    """
+    Runs `./manage.py collectstatic` on the given server.
+
+    Usage::
+
+        fab <server> run_collectstatic
+
+    """
+    with cd(settings.FAB_SETTING('SERVER_PROJECT_ROOT')):
+        run_workon('python2.7 manage.py collectstatic --noinput')
+
+
+@require_server
+def run_compilemessages():
+    """
+    Executes ./manage.py compilemessages on the server.
+
+    Usage::
+
+        fab <server name> run_compilemessages
+
+    """
+
+    with cd(settings.FAB_SETTING('SERVER_PROJECT_ROOT')):
+        run_workon('python2.7 manage.py compilemessages')
+
+
+@require_server
+def run_deploy_website(restart_apache=False, restart_uwsgi=False,
+                       restart_nginx=False):
+    """
+    Executes all tasks necessary to deploy the website on the given server.
+
+    Usage::
+
+        fab <server> run_deploy_website
+
+    """
+    run_git_pull()
+    run_pip_install()
+    run_rsync_project()
+    run_syncdb()
+    run_collectstatic()
+    if getattr(settings, 'MAKEMESSAGES_ON_DEPLOYMENT', False):
+        run_makemessages()
+    if getattr(settings, 'COMPILEMESSAGES_ON_DEPLOYMENT', False):
+        run_compilemessages()
+    if restart_apache:
+        run_restart_apache()
+    if restart_uwsgi:
+        run_restart_uwsgi()
+    if restart_nginx:
+        run_restart_nginx()
+    else:
+        run_touch_wsgi()
+
+
+@require_server
+def run_download_db(filename=None):
+    """
+    Downloads the database from the server into your local machine.
+
+    In order to import the downloaded database, run ``fab import_db``
+
+    Usage::
+
+        fab prod run_download_db
+        fab prod run_download_db:filename=foobar.dump
+
+    """
+    if not filename:
+        filename = settings.DB_DUMP_FILENAME
+    local('scp {0}@{1}:{2}{3} .'.format(
+        env.user, env.host_string,
+        settings.FAB_SETTING('SERVER_DB_BACKUP_DIR'), filename))
+
+
+@require_server
+def run_download_media(filename=None):
+    """
+    Downloads the media dump from the server into your local machine.
+
+    In order to import the downloaded media dump, run ``fab import_media``
+
+    Usage::
+
+        fab prod run_download_media
+        fab prod run_download_media:filename=foobar.tar.gz
+
+    """
+    if not filename:
+        filename = settings.MEDIA_DUMP_FILENAME
+
+    local('scp {0}@{1}:{2}{3} .'.format(
+        env.user, env.host_string,
+        settings.FAB_SETTING('SERVER_MEDIA_BACKUP_DIR'), filename))
+
+
+@require_server
+def run_export_db(filename=None, db_role=None):
+    """
+    Exports the database on the server.
+
+    Usage::
+
+        fab prod run_export_db
+        fab prod run_export_db:filename=foobar.dump
+        fab prod run_export_db:db_role=foobar1
+
+    """
+    if not filename:
+        filename = settings.DB_DUMP_FILENAME
+    if not db_role:
+        db_role = env.db_role
+    run('pg_dump -c -Fc -O -U {0} -f {1}{2}'.format(
+        db_role, settings.FAB_SETTING('SERVER_DB_BACKUP_DIR'),
+        filename))
+
+
+@require_server
+def run_export_media(filename=None):
+    """
+    Exports the media folder on the server.
+
+    Usage::
+
+        fab prod run_export_media
+        fab prod run_export_media:filename=foobar.tar.gz
+
+    """
+    if not filename:
+        filename = settings.MEDIA_DUMP_FILENAME
+
+    with cd(settings.FAB_SETTING('SERVER_MEDIA_ROOT')):
+        run('rm -rf {0}'.format(filename))
+        run('tar -czf {0} *'.format(filename))
+        run('mv {0} {1}'.format(
+            filename, settings.FAB_SETTING('SERVER_MEDIA_BACKUP_DIR')))
+
+
+@require_server
+def run_git_pull():
+    """
+    Pulls the latest code and updates submodules.
+
+    Usage::
+
+        fab <server> run_git_pull
+
+    """
+    with cd(settings.FAB_SETTING('SERVER_REPO_ROOT')):
+        run('git pull && git submodule init && git submodule update')
+
+
+@require_server
+def import_remote_db():
+    """
+    Downloads the prod db and imports it locally.
+
+    """
+    run_export_db()
+    run_download_db()
+    drop_db()
+    create_db()
+    import_db()
+    reset_passwords()
+
+
+@require_server
+def run_makemessages():
+    """
+    Executes ./manage.py makemessages -s --all on the server.
+
+    Usage::
+
+        fab <server name> run_makemessages
+
+    """
+    with cd(settings.FAB_SETTING('SERVER_PROJECT_ROOT')):
+        run_workon('python2.7 manage.py makemessages -s --all')
+
+
+@require_server
+def run_pip_install(upgrade=0):
+    """
+    Installs the requirement.txt file on the given server.
+
+    Usage::
+
+        fab <server> run_pip_install
+        fab <server> run_pip_install:upgrade=1
+
+    :param upgrade: If set to 1, the command will be executed with the
+      ``--upgrade`` flag.
+
+    """
+    command = 'pip install -r {0}'.format(
+        settings.FAB_SETTING('SERVER_REQUIREMENTS_PATH'))
+    if upgrade:
+        command += ' --upgrade'
+    run_workon(command)
+
+
+@require_server
+def run_restart_apache():
+    """
+    Restarts apache on the given server.
+
+    Usage::
+
+        fab <server> run_restart_apache
+
+    """
+    run('{0}restart'.format(settings.FAB_SETTING('SERVER_APACHE_BIN_DIR')))
+
+
+@require_server
+def run_restart_uwsgi():
+    """
+    Restarts uwsgi on the given server.
+
+    Usage::
+
+        fab <server> run_restart_uwsgi
+
+    """
+
+    with cd(settings.FAB_SETTING('SERVER_LOCAL_ETC_DIR')):
+        run('supervisorctl restart uwsgi')
+
+
+@require_server
+def run_restart_nginx():
+    """
+    Restarts uwsgi on the given server.
+
+    Usage::
+
+        fab <server> run_restart_nginx
+
+    """
+
+    with cd(settings.FAB_SETTING('SERVER_LOCAL_ETC_DIR')):
+        run('supervisorctl restart nginx')
+
+
+@require_server
+def run_rsync_project():
+    """
+    Copies the project from the git repository to it's destination folder.
+
+    This has the nice side effect of rsync deleting all ``.pyc`` files and
+    removing other files that might have been left behind by sys admins messing
+    around on the server.
+
+    Usage::
+
+        fab <server> run_rsync_project
+
+    """
+    excludes = ''
+    for exclude in settings.RSYNC_EXCLUDES:
+        excludes += " --exclude '{0}'".format(exclude)
+    command = "rsync -avz --stats --delete {0} {1} {2}".format(
+        excludes, settings.FAB_SETTING('SERVER_REPO_PROJECT_ROOT'),
+        settings.FAB_SETTING('SERVER_APP_ROOT'))
+    run(command)
+
+
+@require_server
+def run_syncdb():
+    """
+    Runs `./manage.py syncdb --migrate` on the given server.
+
+    Usage::
+
+        fab <server> run_syncdb
+
+    """
+    with cd(settings.FAB_SETTING('SERVER_PROJECT_ROOT')):
+        run_workon('python2.7 manage.py syncdb --migrate --noinput')
+
+
+@require_server
+def run_touch_wsgi():
+    """
+    Runs `touch <path>/wsgi.py` on the given server.
+
+    Usage::
+
+        fab <server> run_touch_wsgi
+
+    """
+    run('touch {0}'.format(settings.FAB_SETTING('SERVER_WSGI_FILE')))
+
+
+@require_server
+def run_upload_db(filename=None):
+    """
+    Uploads your local database to the server.
+
+    You can create a local dump with ``fab export_db`` first.
+
+    In order to import the database on the server you still need to SSH into
+    the server.
+
+    Usage::
+
+        fab prod run_upload_db
+        fab prod run_upload_db:filename=foobar.dump
+
+    """
+    if not filename:
+        filename = settings.DB_DUMP_FILENAME
+    local('scp {0} {1}@{2}:{3}'.format(
+        filename, env.user, env.host_string,
+        settings.FAB_SETTING('SERVER_DB_BACKUP_DIR')))
