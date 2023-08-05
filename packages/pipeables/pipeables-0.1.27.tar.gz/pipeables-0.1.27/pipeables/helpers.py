12f91774e603d75cdcf62d4406db7e6ac969cfcd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:17 +0000

    Add package pipeables/0.1.27/pipeables-0.1.27.tar.gz

---
+++ packages/pipeables/pipeables-0.1.27.tar.gz/pipeables-0.1.27/pipeables/helpers.py
@@ -0,0 +1,19 @@
+from pipeables import queries
+from base import BaseQuery
+
+def getQuery(proj,name):
+    #run the query
+    class runStuff(BaseQuery):
+      """anonymous runner"""
+
+    return runStuff(**queries[proj][name].__dict__)
+
+def getQuery(proj,name,conf):
+    class runStuff(BaseQuery):
+      """anonymous runner"""
+
+    query_dict = queries[proj][querytorun].__dict__.copy()
+    if dbconffile:
+      query_dict['dbconffile'] = dbconffile
+
+    return runStuff(**query_dict)
