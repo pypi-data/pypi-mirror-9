664c6a27d613a5af2813712cedd24305c753f417
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:31 +0000

    Add package dictsearch/1.1.1/dictsearch-1.1.1.tar.gz

---
+++ packages/dictsearch/dictsearch-1.1.1.tar.gz/dictsearch-1.1.1/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+      name = "dictsearch",
+      version = "1.1.1",
+      author = "Tommy Carpenter",
+      author_email = "tommyjcarpenter@gmail.com",
+      url = "https://github.com/tommyjcarpenter/dictsearch",
+      description = "A tool for iterating dictionaries as trees and printing all leaf nodes at some path",
+      license = "MIT License",
+      py_modules = ['dictsearch']
+     )
