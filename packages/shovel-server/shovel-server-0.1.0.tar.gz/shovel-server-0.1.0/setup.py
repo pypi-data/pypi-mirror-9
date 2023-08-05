96721b16261b38f4b906087a234b533bc5fbe982
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:43 +0000

    Add package shovel-server/0.1.0/shovel-server-0.1.0.tar.gz

---
+++ packages/shovel-server/shovel-server-0.1.0.tar.gz/shovel-server-0.1.0/setup.py
@@ -0,0 +1,33 @@
+#! /usr/bin/env python
+import sys
+
+from setuptools import setup
+extra = {
+    'install_requires': ['argparse']
+}
+if sys.version_info >= (3,):
+    extra['use_2to3'] = True
+
+
+setup(name               = 'shovel-server',
+    version              = '0.1.0',
+    description          = 'Shovel Tasks in Server Form',
+    url                  = 'http://github.com/seomoz/shovel-server',
+    author               = 'Dan Lecocq',
+    author_email         = 'dan@moz.com',
+    license              = "MIT License",
+    keywords             = 'tasks, shovel, rake, server',
+    packages             = ['shovel.server'],
+    package_dir          = {'shovel.server': 'shovel'},
+    package_data         = {'shovel.server': [
+        'templates/*.tpl', 'static/css/*']},
+    include_package_data = True,
+    scripts              = ['bin/shovel-server'],
+    classifiers          = [
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent'
+    ],
+    **extra
+)
