e04061e890b226248d89c4736dd41255a01bb493
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:27 +0000

    Add package galaxyxml/0.1.15/galaxyxml-0.1.15.tar.gz

---
+++ packages/galaxyxml/galaxyxml-0.1.15.tar.gz/galaxyxml-0.1.15/galaxyxml/__init__.py
@@ -0,0 +1,58 @@
+from lxml import etree
+
+class GalaxyXML(object):
+
+    def __init__(self):
+        self.root = etree.Element('root')
+
+    def export(self):
+        return etree.tostring(self.root, pretty_print=True)
+
+
+class Util(object):
+
+    @classmethod
+    def coerce(cls, data, kill_lists=False):
+        """Recursive data sanitisation
+        """
+        if isinstance(data, dict):
+            return {k: cls.coerce(v, kill_lists=kill_lists) for k, v in
+                    data.items() if v is not None}
+        elif isinstance(data, list):
+            if kill_lists:
+                return cls.coerce(data[0])
+            else:
+                return [cls.coerce(v, kill_lists=kill_lists) for v in data]
+        else:
+            return cls.coerce_value(data)
+
+    @classmethod
+    def coerce_value(cls, obj):
+        """Make everything a string!
+        """
+        if isinstance(obj, bool):
+            if obj:
+                return "true"
+            else:
+                return "false"
+        elif isinstance(obj, str):
+            return obj
+        else:
+            return str(obj)
+
+    @classmethod
+    def clean_kwargs(cls, params):
+        if 'kwargs' in params:
+            kwargs = params['kwargs']
+            for k in kwargs:
+                params[k] = kwargs[k]
+            del params['kwargs']
+        del params['self']
+
+        # There will be more params, it would be NICE to use a whitelist
+        # instead of a blacklist, but until we have more data let's just
+        # blacklist stuff we see commonly.
+        for blacklist in ('positional',):
+            if blacklist in params:
+                del params[blacklist]
+        return params
