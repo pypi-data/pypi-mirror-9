385f3dbf5a5d78f6d151232f21b666fe10f08c19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:53 +0000

    Add package mester/1.0.0/mester-1.0.0.zip

---
+++ packages/mester/mester-1.0.0.zip/mester-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name            = 'mester',
+    version         = '1.0.0',
+    py_modules      = ['nester'],
+    author          = 'wyw',
+    author_email    = 'wang6087480@126.com',
+    url             = 'http://www.wyw.com',
+    description     = "A simple printer of nester of nested list",
+    )
