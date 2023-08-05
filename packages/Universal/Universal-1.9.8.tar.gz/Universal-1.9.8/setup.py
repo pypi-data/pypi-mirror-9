df4d8b97b455facef4dd2a76e9223299b140bc3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:52 +0000

    Add package Universal/1.9.8/Universal-1.9.8.tar.gz

---
+++ packages/Universal/Universal-1.9.8.tar.gz/Universal-1.9.8/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+        name='Universal',
+        description='Universal Competitive Programming Suite helps you work faster'
+                    ' in programming competitions',
+        version='1.9.8',
+        packages=['universal'],
+        license='GPLv3+',
+        author='Shubham Chaudhary',
+        author_email='me@shubhamchaudhary.in',
+        url='https://github.com/shubhamchaudhary/universal',
+        long_description=open('README.txt').read(),
+)
+
