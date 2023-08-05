5d1a96e26cdaa616318e50156ce48a288af8acdf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:16 +0000

    Add package cabot-alert-email/1.0/cabot-alert-email-1.0.tar.gz

---
+++ packages/cabot-alert-email/cabot-alert-email-1.0.tar.gz/cabot-alert-email-1.0/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='cabot-alert-email',
+      version='1.0',
+      description='An email alert plugin for Cabot by Arachnys',
+      author='Arachnys',
+      author_email='info@arachnys.org',
+      url='http://cabotapp.com',
+      packages=[
+      	'cabot_alert_email'
+      	],
+      download_url= 'https://github.com/bonniejools/cabot-alert-email/tarball/0.1'
+     )
