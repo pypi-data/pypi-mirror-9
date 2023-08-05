0915281971191bfd4b74669dd773efd0043f2fb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:31 +0000

    Add package Barista/0.0.2/Barista-0.0.2.tar.gz

---
+++ packages/Barista/Barista-0.0.2.tar.gz/Barista-0.0.2/setup.py
@@ -0,0 +1,42 @@
+"""
+Barista
+-------
+
+Barista helps you manage the Flask
+"""
+
+from distutils.core import setup
+
+setup(
+    name='Barista',
+    version='0.0.2',
+    url='',
+    license='BSD',
+    author='Mek Karpeles',
+    author_email='michael.karpeles@gmail.com',
+    description='A Flask application creator',
+    long_description=__doc__,
+    packages=[
+        'barista'
+        ],
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    install_requires=[
+        'Flask',
+        'Flask-Routing'
+    ],
+    scripts=[
+        "scripts/barista"
+        ],
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
