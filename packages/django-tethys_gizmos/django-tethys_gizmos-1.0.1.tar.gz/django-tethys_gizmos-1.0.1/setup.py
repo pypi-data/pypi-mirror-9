85bcb450882a86b47985c38eead9ecab47429c22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:03 +0000

    Add package django-tethys_gizmos/1.0.1/django-tethys_gizmos-1.0.1.tar.gz

---
+++ packages/django-tethys_gizmos/django-tethys_gizmos-1.0.1.tar.gz/django-tethys_gizmos-1.0.1/setup.py
@@ -0,0 +1,38 @@
+import os
+from setuptools import setup, find_packages
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+requires = ['django',
+            'requests',]
+
+version = '1.0.1'
+setup(
+    name='django-tethys_gizmos',
+    version=version,
+    packages=find_packages(),
+    include_package_data=True,
+    license='BSD 2-Clause License',
+    description='An app to enable Tethys gizmos in your site.',
+    long_description=README,
+    url='',
+    author='Nathan Swain',
+    author_email='nathan.swain@byu.net',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License', # example license
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+    install_requires = requires,
+)
