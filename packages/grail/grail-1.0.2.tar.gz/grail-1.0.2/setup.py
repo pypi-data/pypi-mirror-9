e4a98327aceea8d58e1dbedc6048d1a811cc7a22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:26 +0000

    Add package grail/1.0.2/grail-1.0.2.tar.gz

---
+++ packages/grail/grail-1.0.2.tar.gz/grail-1.0.2/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+version = '1.0.2'
+
+setup(
+    name='grail',
+    version=version,
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+    ],
+    packages=[
+        'grail',
+    ],
+    description='Grail is a library which allows test script creation based on steps. '
+                'It helps to structure your tests and get rid of additional test documentation for your code.',
+    include_package_data=True,
+    author='Wargaming.NET',
+    author_email='i_khrol@wargaming.net',
+    url='http://www.wargaming.net/',
+    install_requires=[
+        'nose==1.3.3',
+    ],
+)
