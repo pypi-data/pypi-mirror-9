fd27fcd318ecde3a9f4aafa285c0047fb296f73c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:03 +0000

    Add package ssdb.py/0.1.6/ssdb.py-0.1.6.tar.gz

---
+++ packages/ssdb.py/ssdb.py-0.1.6.tar.gz/ssdb.py-0.1.6/setup.py
@@ -0,0 +1,37 @@
+"""
+ssdb.py
+-------
+
+Ssdb Python Client Library.
+
+https://github.com/hit9/ssdb.py
+"""
+
+from setuptools import setup
+
+
+setup(
+    name='ssdb.py',
+    version='0.1.6',
+    author='hit9',
+    author_email='nz2324@126.com',
+    description="Ssdb Python Client Library",
+    long_description=__doc__,
+    license='bsd2',
+    keywords=['ssdb', 'client', 'library'],
+    url='https://github.com/hit9/ssdb.py',
+    py_modules=['ssdb'],
+    install_requires=['spp>=0.0.5'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
