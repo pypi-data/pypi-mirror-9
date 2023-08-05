dd3645fc0ff9bfde20de942ab3941f1a2ba82537
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:22 +0000

    Add package pygal/1.6.2/pygal-1.6.2.tar.gz

---
+++ packages/pygal/pygal-1.6.2.tar.gz/pygal-1.6.2/setup.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# This file is part of pygal
+#
+# A python svg graph plotting library
+# Copyright © 2012-2014 Kozea
+#
+# This library is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Lesser General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option) any
+# later version.
+#
+# This library is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with pygal. If not, see <http://www.gnu.org/licenses/>.
+import os
+import sys
+import re
+
+from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
+
+
+class PyTest(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        # import here, cause outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main(self.test_args)
+        sys.exit(errno)
+
+
+ROOT = os.path.dirname(__file__)
+
+# Explicitly specify the encoding of pygal/__init__.py if we're on py3.
+kwargs = {}
+if sys.version_info[0] == 3:
+    kwargs['encoding'] = 'utf-8'
+
+with open(os.path.join(ROOT, 'pygal', '__init__.py'), **kwargs) as fd:
+    __version__ = re.search("__version__ = '([^']+)'", fd.read()).group(1)
+
+setup(
+    name="pygal",
+    version=__version__,
+    description="A python svg graph plotting library",
+    author="Kozea",
+    url="http://pygal.org/",
+    author_email="florian.mounier@kozea.fr",
+    license="GNU LGPL v3+",
+    platforms="Any",
+    packages=find_packages(),
+    provides=['pygal'],
+    scripts=["pygal_gen.py"],
+    keywords=[
+        "svg", "chart", "graph", "diagram", "plot", "histogram", "kiviat"],
+    tests_require=["pytest", "pyquery", "flask", "cairosvg"],
+    cmdclass={'test': PyTest},
+    package_data={'pygal': ['css/*', 'graph/*.svg']},
+    extras_require={
+        'lxml': ['lxml'],
+        'png': ['cairosvg']
+
+    },
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: End Users/Desktop",
+        "License :: OSI Approved :: "
+        "GNU Lesser General Public License v3 or later (LGPLv3+)",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "Topic :: Multimedia :: Graphics :: Presentation"])
