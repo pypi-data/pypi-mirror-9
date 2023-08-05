4fc3cf0e768267b0f7d7dcf14557b062746ab8dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:40 +0000

    Add package plone.batching/1.0.3/plone.batching-1.0.3.zip

---
+++ packages/plone.batching/plone.batching-1.0.3.zip/plone.batching-1.0.3/CHANGES.rst
@@ -0,0 +1,51 @@
+Changelog
+=========
+
+1.0.3 (2015-02-20)
+------------------
+
+- Fix issue with orphaning
+  [do3cc]
+
+1.0.2 (2014-04-13)
+------------------
+
+- Fix issue where a start >= end will always return last item.
+  https://dev.plone.org/ticket/13880\
+  [thepjot]
+
+- Fix multiple_pages if the length of the sequence is exactly the
+  page length.
+  [gaudenz]
+
+1.0.1 (2014-01-27)
+------------------
+
+- Fix issue with sequences when the reported length was different
+  than len() iteration would return the full unbatched sequence.
+  [alecm]
+
+
+1.0 (2013-05-23)
+----------------
+
+- Use index instead of template attribute in BatchView to be able to customize
+  only the template.
+  [vincentfretin, thomasdesvenain]
+
+- Fixed wrong msgid for 'First item'.
+  [vincentfretin]
+
+
+1.0b1 (2013-01-17)
+------------------
+
+- Nothing changed yet.
+
+
+1.0a1 (2012-04-25)
+------------------
+
+- Factored out Plone batching implementation to seperate egg (PLIP #12235)
+  [tom_gross]
+
---
+++ packages/plone.batching/plone.batching-1.0.3.zip/plone.batching-1.0.3/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+version = '1.0.3'
+
+setup(
+    name='plone.batching',
+    version=version,
+    description="Batching facilities used in Plone.",
+    long_description=open("README.rst").read() + "\n" +
+                     open("CHANGES.rst").read() + "\n" +
+                     open("plone/batching/batching.rst").read() + "\n" +
+                     open("USAGE.rst").read(),
+    classifiers=[
+        'Framework :: Plone :: 4.3',
+        'Framework :: Zope2',
+        'Programming Language :: Python :: 2.6',
+    ],
+    keywords='Plone',
+    author='Plone Foundation',
+    author_email='plone-developers@lists.sourceforge.net',
+    url='http://pypi.python.org/pypi/plone.batching',
+    license='GPL',
+    packages=find_packages(exclude=['ez_setup']),
+    namespace_packages=['plone'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=['setuptools'],
+    entry_points='''
+          [z3c.autoinclude.plugin]
+          target = plone
+      ''',
+)
