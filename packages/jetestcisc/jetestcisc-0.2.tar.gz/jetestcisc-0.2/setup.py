eecd3747f495af23870e82c9a143c9c0ce0a88e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:30 +0000

    Add package jetestcisc/0.2/jetestcisc-0.2.tar.gz

---
+++ packages/jetestcisc/jetestcisc-0.2.tar.gz/jetestcisc-0.2/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+setup(
+  name='jetestcisc',
+  packages=['jetestcisc'],
+  version='0.2',
+  description='A random test lib',
+  author='Jason Edelman',
+  author_email='jedelman8@gmail.com',
+  url='https://github.com/jedelman8/jetestcisc',
+  download_url='https://github.com/jedelman8/jetestcisc/tarball/0.2',
+  install_requires=[
+      'xmltodict==0.9.2',
+  ],
+)
