2082128e5cd89453c47852565e79ac92e9a58078
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:37 +0000

    Add package python-ev3/0.0.2.6/python_ev3-0.0.2.6-py3.4.egg

---
+++ packages/python-ev3/python_ev3-0.0.2.6-py3.4.egg/ev3/lego.py
@@ -0,0 +1,172 @@
+from .ev3dev import LegoSensor, Motor
+
+
+class TouchSensor(LegoSensor):
+
+    def __init__(self, port=-1):
+#Both lego-nxt-touch and lego-ev3-touch support auto
+        LegoSensor.__init__(self, port, name='lego-ev3-touch')
+
+    @property
+    def is_pushed(self):
+        self.mode = 'TOUCH'
+        return bool(self.value0)
+
+class LightSensor(LegoSensor):
+    
+    def __init__(self, port=-1):
+        LegoSensor.__init__(self, port, name='lego-nxt-light')
+    
+    @property
+    def reflect(self):
+        self.mode = 'REFLECT'
+        #Reflected light intensity (0 to 100)
+        return self.value0/(int(self.decimals)*10)
+
+    @property
+    def ambient(self):
+        self.mode = 'AMBIENT'
+        #Ambient light intensity (0 to 100)
+        return self.value0/(int(self.decimals)*10)
+
+class SoundSensor(LegoSensor):
+
+    def __init__(self, port=-1):
+        LegoSensor.__init__(self, port, name='nxt-analog')
+
+    @property
+    def db(self):
+        self.mode = 'ANALOG-0'
+        # Sound pressure level (0 to 1000)
+        return "%.2f" % (self.value0/(int(self.decimals)*10))
+
+    @property
+    def dba(self):
+        self.mode = 'ANALOG-1'
+        # Sound pressure level (0 to 1000)
+        return "%.2f" % (self.value0/(int(self.decimals)*10))
+
+class ColorSensor(LegoSensor):
+    colors = (None, 'black', 'blue', 'green',
+              'yellow', 'red', 'white', 'brown')
+
+    def __init__(self, port=-1):
+        LegoSensor.__init__(self, port, name='ev3-uart-29')
+
+    @property
+    def rgb(self):
+        self.mode = 'RGB-RAW'
+        return self.value0, self.value1, self.value2
+
+    @property
+    def color(self):
+        self.mode = 'COL-COLOR'
+        return self.value0
+
+    @property
+    def reflect(self):
+        self.mode = 'COL-REFLECT'
+        return self.value0
+
+    @property
+    def ambient(self):
+        self.mode = 'COL-AMBIENT'
+        return self.value0
+
+    @property
+    def ref_raw(self):
+        self.mode = 'REF-RAW'
+        return self.value0, self.value1
+
+
+class InfraredSensor(LegoSensor):
+
+    def __init__(self, port=-1):
+       LegoSensor.__init__(self, port, name='ev3-uart-33')
+
+    @property
+    def remote(self):
+        self.mode = 'IR-REMOTE'
+        return self.value0, self.value1, self.value2, self.value3
+
+    @property
+    def remote_bin(self):
+        self.mode = 'IR-REM-A'
+        return self.value0
+
+    @property
+    def prox(self):
+        self.mode = 'IR-PROX'
+        return self.value0
+
+    @property
+    def seek(self):
+        self.mode = 'IR-SEEK'
+        return [(self.value0, self.value1),
+                (self.value2, self.value3),
+                (self.value4, self.value5),
+                (self.value6, self.value7)]
+
+
+class GyroSensor(LegoSensor):
+
+    def __init__(self, port=-1):
+        LegoSensor.__init__(self, port, name='ev3-uart-32')
+
+    @property
+    def ang(self):
+        self.mode = 'GYRO-ANG'
+        return self.value0
+
+    @property
+    def rate(self):
+        self.mode = 'GYRO-RATE'
+        return self.value0
+
+    @property
+    def ang_and_rate(self):
+        self.mode = 'GYRO-G&A'
+        return self.value0, self.value1
+
+
+class UltrasonicSensor(LegoSensor):
+
+    def __init__(self, port=-1):
+        LegoSensor.__init__(self, port, name='ev3-uart-30')
+
+    @property
+    def dist_cm(self):
+        self.mode = 'US-DIST-CM'
+        return self.value0
+
+    @property
+    def dist_in(self):
+        self.mode = 'US-DIST-IN'
+        return self.value0
+
+    @property
+    def listen(self):
+        self.mode = 'US-LISTEN'
+        return bool(self.value0)
+
+    @property
+    def si_cm(self):
+        self.mode_force_flush('US-SI-CM')
+        return self.value0
+
+    @property
+    def si_in(self):
+        self.mode_force_flush('US-SI-IN')
+        return self.value0
+
+
+class LargeMotor(Motor):
+
+    def __init__(self, port=''):
+        Motor.__init__(self, port, _type='tacho')
+
+
+class MediumMotor(Motor):
+
+    def __init__(self, port=''):
+        Motor.__init__(self, port, _type='minitacho')
---
+++ packages/python-ev3/python_ev3-0.0.2.6-py3.4.egg/ev3/mindsensors.py
@@ -0,0 +1,133 @@
+from .ev3dev import I2CS
+
+
+class MindSensorI2CS(I2CS):
+
+    @property
+    def version(self):
+        return self.read_byte_array_as_string(0x00, 8)
+
+    @property
+    def vendor_id(self):
+        return self.read_byte_array_as_string(0x08, 8)
+
+    @property
+    def device_id(self):
+        return self.read_byte_array_as_string(0x10, 8)
+
+
+@I2CS.create_i2c_property(
+    command=(0x41, {'read_only': False}),
+    button_set_1 = 0x42,
+    button_set_2= 0x43,
+    x_left= 0x44,
+    y_left= 0x45,
+    x_right= 0x46,
+    y_right= 0x47,
+    up= 0x4A,
+    right= 0x4B,
+    down= 0x4C,
+    left= 0x4D,
+    l2= 0x4E,
+    r2= 0x4F,
+    l1= 0x50,
+    r1= 0x51,
+    triangle= 0x52,
+    circle= 0x53,
+    cross= 0x54,
+    square= 0x55)
+class PSPNxV4(MindSensorI2CS):
+
+    def __init__(self, port, addr=0x01):
+        I2CS.__init__(self, port, addr)
+        self.command = 0x49
+
+
+def absoluteIMU_property(cls):
+    for prop in ['x_acc', 'y_acc', 'z_acc',
+                             'x_raw_magnetic', 'y_raw_magnetic', 'z_raw_magnetic',
+                             'x_gyro', 'y_gyro', 'z_gyro',
+                             'compass']:
+        def fget(self):
+            return (getattr(self, prop + '_msb') << 8) + getattr(self, prop + '_lsb')
+        setattr(cls, prop, property(fget))
+    return cls
+
+@I2CS.create_i2c_property(
+    command=(0x41, {'read_only':False}),
+    x_tilt= 0x42,
+    y_tilt= 0x43,
+    z_tilt= 0x44,
+    x_acc_lsb= 0x45,
+    x_acc_msb= 0x46,
+    y_acc_lsb= 0x47,
+    y_acc_msb= 0x48,
+    z_acc_lsb= 0x49,
+    z_acc_msb= 0x4A,
+    compass_lsb= 0x4B,
+    compass_msb= 0x4C,
+    x_raw_magnetic_lsb= 0x4D,
+    x_raw_magnetic_msb= 0x4E,
+    y_raw_magnetic_lsb= 0x4F,
+    y_raw_magnetic_msb= 0x50,
+    z_raw_magnetic_lsb= 0x51,
+    z_raw_magnetic_msb= 0x52,
+    x_gyro_lsb= 0x53,
+    x_gyro_msb= 0x54,
+    y_gyro_lsb= 0x55,
+    y_gyro_msb= 0x56,
+    z_gyro_lsb= 0x57,
+    z_gyro_msb= 0x58,
+    gyro_filter= 0x5A)
+@absoluteIMU_property
+class AbsoluteIMU(MindSensorI2CS):
+# Is this too tricky to create property?
+
+    def __init__(self, port, addr=0x11):
+        I2CS.__init__(self, port, addr)
+
+    def compass_cal_start(self):
+        self.command = 0x43
+
+    def compass_cal_end(self):
+        self.command = 0x63
+
+    def acc_2g(self):
+        self.command = 0x31
+
+    def acc_4g(self):
+        self.command = 0x32
+
+    def acc_8g(self):
+        self.command = 0x33
+
+    def acc_16g(self):
+        self.command = 0x34
+
+class MagicWand(MindSensorI2CS):
+
+    val = 0xff
+
+    # port = 1..4 , matching the EV3 input port where the magic wand is connected
+    def __init__(self, port, addr=0x38):
+        MindSensorI2CS.__init__(self, port, addr)
+
+    def put_data(self,v):
+        self.val=v
+        MindSensorI2CS.write_byte(self, v)
+
+    # turns all leds on
+    def led_all_on(self):
+        self.put_data(0x00)
+        
+    # turns all leds off
+    def led_all_off(self):
+        self.put_data(0xff)
+
+    # turns a specific led on. leds are indexed 1..8
+    def led_on(self, num):
+        self.put_data(self.val & (0xff - (1<<(num-1))))
+
+    # turns a specific led off. leds are indexed 1..8       
+    def led_off(self, num):
+        self.put_data(self.val | (1<<(num-1)))
