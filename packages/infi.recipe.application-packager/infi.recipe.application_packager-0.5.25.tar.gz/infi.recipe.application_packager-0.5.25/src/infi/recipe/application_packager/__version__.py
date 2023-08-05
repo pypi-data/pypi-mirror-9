9b15f0393535a891192fbf1bcb1f222d2f9692c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:45 +0000

    Add package infi.recipe.application-packager/0.5.25/infi.recipe.application_packager-0.5.25.tar.gz

---
+++ packages/infi.recipe.application-packager/infi.recipe.application_packager-0.5.25.tar.gz/infi.recipe.application_packager-0.5.25/src/infi/recipe/application_packager/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.5.25"
+__git_commiter_name__ = "Guy Rozendorn"
+__git_commiter_email__ = "guy@rzn.co.il"
+__git_branch__ = '(detached from v0.5.25)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '43252231c7098e4a560fa0e3419ce480ba5cfbbd'
+__git_head_subject__ = 'HOSTDEV-1949 delete_non_production_packages_from_cache_dist deleted foo-1.0-1'
+__git_head_message__ = ''
+__git_dirty_diff__ = ''
