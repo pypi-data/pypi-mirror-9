9f2b66fdd817ae0393a3f26b30e8acd406bc7d15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:23 +0000

    Add package bottle-utils-lazy/0.2/bottle-utils-lazy-0.2.tar.gz

---
+++ packages/bottle-utils-lazy/bottle-utils-lazy-0.2.tar.gz/bottle-utils-lazy-0.2/README.rst
@@ -0,0 +1,11 @@
+=================
+Bottle Utils Lazy
+=================
+
+Bottle Utils is a collection of loosely related packages for developing web
+applications using Bottle_ web framework. This package contains utilities for
+lazy evaluation.
+
+.. _Bottle: http://bottlepy.org/
+
+
---
+++ packages/bottle-utils-lazy/bottle-utils-lazy-0.2.tar.gz/bottle-utils-lazy-0.2/setup.py
@@ -0,0 +1,32 @@
+import os
+from setuptools import setup, find_packages
+
+import bottle_utils
+
+def read(fname):
+    """ Return content of specified file """
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = 'bottle-utils-lazy',
+    version = bottle_utils.__version__,
+    author = 'Outernet Inc',
+    author_email = 'branko@outernet.is',
+    description = ('Lazy evaluation utilities for developing apps with Bottle '
+                   'web framework'),
+    license = 'BSD',
+    keywords = 'bottle utils i18n http lazy csrf ajax',
+    url = 'http://outernet-project.github.io/bottle-utils/',
+    packages=find_packages(),
+    long_description=read('README.rst'),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Topic :: Utilities',
+        'Framework :: Bottle',
+        'Environment :: Web Environment',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
+)
