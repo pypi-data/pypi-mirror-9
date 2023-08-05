f9aba912467528930548376bbc581b860d20cd98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:14 +0000

    Add package keen/0.3.7/keen-0.3.7.tar.gz

---
+++ packages/keen/keen-0.3.7.tar.gz/keen-0.3.7/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name="keen",
+      version="0.3.7",
+      description="Python Client for Keen IO",
+      author="Keen IO",
+      author_email="team@keen.io",
+      url="https://github.com/keenlabs/KeenClient-Python",
+      packages=["keen"],
+      install_requires=["requests", "pycrypto", "Padding"],
+      tests_require=["nose"]
+      )
