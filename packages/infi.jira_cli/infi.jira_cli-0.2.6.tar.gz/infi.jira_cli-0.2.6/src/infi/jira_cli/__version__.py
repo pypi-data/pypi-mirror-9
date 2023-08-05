025e183268fa216352bb63769103d8c0829bfb66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:42 +0000

    Add package infi.jira_cli/0.2.6/infi.jira_cli-0.2.6.tar.gz

---
+++ packages/infi.jira_cli/infi.jira_cli-0.2.6.tar.gz/infi.jira_cli-0.2.6/src/infi/jira_cli/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.2.6"
+__git_commiter_name__ = "Guy Rozendorn"
+__git_commiter_email__ = "guy@rzn.co.il"
+__git_branch__ = '(detached from v0.2.6)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '2c88dcc165471595e52e52b8514d1716f6e0a76b'
+__git_head_subject__ = 'Finished release v0.2.6.'
+__git_head_message__ = '* release/v0.2.6:\n  TRIVIAL refactoring for readability\n  TRIVIAL dry_run\n  HOSTDEV-1940\n  jirelnotes fixes\n  other-versions-included-in-this-releases\n  TRIVIAL also notify clones'
+__git_dirty_diff__ = ''
