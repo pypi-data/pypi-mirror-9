7c9a0a56ba34e24c8c74158e6e68ea5bd6712016
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:05 +0000

    Add package dj_mano/0.2.0/dj_mano-0.2.0.tar.gz

---
+++ packages/dj_mano/dj_mano-0.2.0.tar.gz/dj_mano-0.2.0/dj_mano/__init__.py
@@ -0,0 +1,5 @@
+# vim: fileencoding=utf-8 tw=100 expandtab ts=4 sw=4 :
+
+from .fields import MaterializedAnnotationField, MaterializedAnnotationIntegerField, \
+    MaterializedAnnotationFloatField
+from .utils import hook_it_all
