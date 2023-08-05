a221661ef87a23e3a36363ac587810660ef48c8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:17 +0000

    Add package seizure/1.0.2/seizure-1.0.2.tar.gz

---
+++ packages/seizure/seizure-1.0.2.tar.gz/seizure-1.0.2/seizure/lib/conversion.py
@@ -0,0 +1,50 @@
+import os
+import subprocess
+import tempfile
+
+
+class Converter(object):
+    def __init__(self, ffmpeg_bin):
+        self.ffmpeg = ffmpeg_bin
+
+    def convert(self, paths):
+        save_to = self.rename_extension(self.joined_filename(paths), 'mp4')
+        if os.path.exists(save_to):
+            raise OSError("File {} exists".format(save_to))
+        filelist = self.create_filelist(paths)
+        with tempfile.NamedTemporaryFile() as f:
+            f.write(filelist)
+            f.seek(0)
+            f.flush()
+            print(filelist)
+            self.convert_and_join(f.name, save_to)
+        return save_to
+
+    def create_filelist(self, paths):
+        filelist = ''
+        for f in paths:
+            filelist += 'file \'{}\'\n'.format(os.path.abspath(f))
+        return bytes(filelist, 'UTF-8')
+
+    def joined_filename(self, paths):
+        """
+        Assumes the paths are in the form {str}_{num}.{ext} e.g. path_01.mp4,
+        path_02.mp4 etc. and returns path.mp4. Takes the first filename base.
+        """
+        base, ext = self.remove_digits(paths[0])
+        return base + ext
+
+    def convert_and_join(self, filelist, save_to):
+        subprocess.check_call([self.ffmpeg, '-f', 'concat', '-i', filelist,
+                               '-c', 'copy', save_to])
+
+    @staticmethod
+    def remove_digits(path):
+        base, ext = os.path.splitext(path)
+        last_underscore = base.rindex('_')
+        return base[:last_underscore], ext
+
+    @staticmethod
+    def rename_extension(path, extension):
+        ext = os.path.splitext(path)[1].split('.')[-1]
+        return path.replace(ext, extension)
---
+++ packages/seizure/seizure-1.0.2.tar.gz/seizure-1.0.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='seizure',
+    version='1.0.2',
+    packages=['seizure', 'seizure.lib'],
+    license='',
+    author='Shane Drury',
+    author_email='shane.r.drury@gmail.com',
+    description='Download VODs from Twitch',
+    entry_points={'console_scripts': ['seizure = seizure.scripts:main'], },
+    keywords=['twitch', 'vod', 'download'],
+    url='https://github.com/ShaneDrury/seizure',
+    download_url='https://github.com/ShaneDrury/seizure/tarball/1.0.2',
+    install_requires=['requests', 'progressbar'],
+    )
