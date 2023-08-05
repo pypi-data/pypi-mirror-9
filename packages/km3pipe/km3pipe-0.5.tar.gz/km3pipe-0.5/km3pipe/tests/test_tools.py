f5db58537902cef0fe0cb3e1be1b75b6602737bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:52 +0000

    Add package km3pipe/0.5/km3pipe-0.5.tar.gz

---
+++ packages/km3pipe/km3pipe-0.5.tar.gz/km3pipe-0.5/km3pipe/tests/test_tools.py
@@ -0,0 +1,30 @@
+from __future__ import division, absolute_import, print_function
+
+from km3pipe.testing import *
+from km3pipe.tools import unpack_nfirst, split
+
+
+class TestTools(TestCase):
+
+    def test_unpack_nfirst(self):
+        a_tuple = (1, 2, 3, 4, 5)
+        a, b, c, rest = unpack_nfirst(a_tuple, 3)
+        self.assertEqual(1, a)
+        self.assertEqual(2, b)
+        self.assertEqual(3, c)
+        self.assertTupleEqual((4, 5), rest)
+
+    def test_split_splits_strings(self):
+        string = "1 2 3 4"
+        parts = split(string)
+        self.assertListEqual(['1', '2', '3', '4'], parts)
+
+    def test_split_callback_converts_correctly(self):
+        string = "1 2 3 4"
+        parts = split(string, int)
+        self.assertListEqual([1, 2, 3, 4], parts)
+
+        string = "1.0 2.1 3.2 4.3"
+        parts = split(string, float)
+        self.assertListEqual([1.0, 2.1, 3.2, 4.3], parts)
+
