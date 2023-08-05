8cebebd6a0f7b8f1c155abb8a21f501fb544a45c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:04 +0000

    Add package reactor/0.1.2/reactor-0.1.2.tar.gz

---
+++ packages/reactor/reactor-0.1.2.tar.gz/reactor-0.1.2/setup.py
@@ -0,0 +1,31 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+# load long description
+with open('LONG_DESCRIPTION.rst') as f:
+    long_description = f.read()
+
+
+setup(
+	name='reactor',
+    version='0.1.2',
+    description='Reactor.am python client',
+    long_description=long_description,
+    install_requires=[
+		'unittest2',
+		'mock',
+		'requests',
+	],
+	url='https://reactor.am/',
+	author='Reactor',
+	author_email='hello@reactor.am',
+    packages=['reactor'],
+    classifiers=[
+    	'Development Status :: 5 - Production/Stable',
+    	'Intended Audience :: Customer Service',
+    	'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+    	'Programming Language :: Python :: 2.7',
+    ]
+)
