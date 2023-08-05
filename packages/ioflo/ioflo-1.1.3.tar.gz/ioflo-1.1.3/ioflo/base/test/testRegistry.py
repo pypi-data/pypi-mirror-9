30eb21a0cbe52126f7e2f9714f6f89afc9758a90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:27 +0000

    Add package ioflo/1.1.3/ioflo-1.1.3.tar.gz

---
+++ packages/ioflo/ioflo-1.1.3.tar.gz/ioflo-1.1.3/ioflo/base/test/testRegistry.py
@@ -0,0 +1,19 @@
+
+import ioflo.base.registering as registering
+
+def TestRegistry():
+    """Module self test
+    """
+    x = Registry()
+    print(x.name)
+    y = Registry()
+    print(y.name)
+
+    name = "Hello"
+    if Registry.VerifyName(name):
+        z = Registry(name=name)
+    print(Registry.Names)
+    print(Registry.VerifyName(name))
+
+if __name__ == "__main__":
+    TestRegistry()
