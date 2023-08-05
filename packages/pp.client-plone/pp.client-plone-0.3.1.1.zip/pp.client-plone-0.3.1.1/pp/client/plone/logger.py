7cc30754ab643685b2fabd4491f4f3fcd3807a50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:09 +0000

    Add package pp.client-plone/0.3.1.1/pp.client-plone-0.3.1.1.zip

---
+++ packages/pp.client-plone/pp.client-plone-0.3.1.1.zip/pp.client-plone-0.3.1.1/pp/client/plone/logger.py
@@ -0,0 +1,10 @@
+################################################################
+# pp.client-plone
+# (C) 2013,  ZOPYX Limited, D-72074 Tuebingen, Germany
+################################################################
+
+#from logging import getLogger
+
+#LOG = getLogger('pp.client-plone')
+
+from pp.core.logger import LOG
