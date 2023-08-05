a62885b387153c9cf79b2be1aa30af54558edf9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:45 +0000

    Add package robotframework-appiumlibrary/1.2.4/robotframework-appiumlibrary-1.2.4.tar.gz

---
+++ packages/robotframework-appiumlibrary/robotframework-appiumlibrary-1.2.4.tar.gz/robotframework-appiumlibrary-1.2.4/setup.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+
+import sys
+from os.path import join, dirname
+
+sys.path.append(join(dirname(__file__), 'src'))
+from ez_setup import use_setuptools
+use_setuptools()
+from setuptools import setup
+
+execfile(join(dirname(__file__), 'src', 'AppiumLibrary', 'version.py'))
+
+setup(name         = 'robotframework-appiumlibrary',
+      version      = VERSION,
+      description  = 'app testing library for Robot Framework',
+      long_description = open(join(dirname(__file__), 'README.rst')).read(),
+      author       = 'William Zhang, Xie Lieping',
+      author_email = '<jollychang@gmail.com>, <frankbp@gmail.com>',
+      url          = 'https://github.com/jollychang/robotframework-appiumlibrary',
+      license      = 'Apache License 2.0',
+      keywords     = 'robotframework testing testautomation mobile appium webdriver app',
+      platforms    = 'any',
+      classifiers  = [
+                        "Development Status :: 5 - Production/Stable",
+                        "License :: OSI Approved :: Apache Software License",
+                        "Operating System :: OS Independent",
+                        "Programming Language :: Python",
+                        "Topic :: Software Development :: Testing"
+                     ],
+      install_requires = [
+                            'decorator >= 3.3.2',
+                            'robotframework >= 2.6.0',
+                            'docutils >= 0.8.1',
+                            'Appium-Python-Client >= 0.5',
+                            'mock >= 1.0.1',
+                            'sauceclient >= 0.1.0',
+                            'pytest-cov >= 1.8.1',
+                            'pytest-xdist >= 1.11',
+                            'pytest-pythonpath >= 0.4',
+                         ],
+      py_modules=['ez_setup'],
+      package_dir  = {'' : 'src'},
+      packages     = ['AppiumLibrary','AppiumLibrary.keywords','AppiumLibrary.locators',
+                      'AppiumLibrary.utils'],
+      include_package_data = True,
+      )
