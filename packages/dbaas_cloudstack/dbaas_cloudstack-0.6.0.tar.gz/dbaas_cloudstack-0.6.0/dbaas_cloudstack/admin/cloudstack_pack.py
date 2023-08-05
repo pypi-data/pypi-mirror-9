d255e3cb6f3f848f29b08cae50c4680380de2383
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:54 +0000

    Add package dbaas_cloudstack/0.6.0/dbaas_cloudstack-0.6.0.tar.gz

---
+++ packages/dbaas_cloudstack/dbaas_cloudstack-0.6.0.tar.gz/dbaas_cloudstack-0.6.0/dbaas_cloudstack/admin/cloudstack_pack.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, unicode_literals
+from django.contrib import admin
+
+
+class CloudStackPackAdmin(admin.ModelAdmin):
+    search_fields = ("offering__name", "engine_type__name", )
+    list_display = ("name", "offering","pack_region","pack_environment", "engine_type")
+    save_on_top = True
+
+    def pack_region(self, pack):
+        return pack.region.name
+
+    pack_region.short_description = "Region"
+
+    def pack_environment(self, pack):
+        return pack.environment.name
+
+    pack_environment.short_description = "Environment"
