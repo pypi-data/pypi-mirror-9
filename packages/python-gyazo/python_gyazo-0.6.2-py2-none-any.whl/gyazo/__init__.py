99424df062dcb328e2c5dee3d204f576b10a8d61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:06 +0000

    Add package python-gyazo/0.6.2/python_gyazo-0.6.2-py2-none-any.whl

---
+++ packages/python-gyazo/python_gyazo-0.6.2-py2-none-any.whl/gyazo/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, unicode_literals
+
+from .api import Api
+from .error import GyazoError
+from .image import Image, ImageList
---
+++ packages/python-gyazo/python_gyazo-0.6.2-py2-none-any.whl/gyazo/error.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+class GyazoError(Exception):
+    """Base class for Gyazo errors"""
