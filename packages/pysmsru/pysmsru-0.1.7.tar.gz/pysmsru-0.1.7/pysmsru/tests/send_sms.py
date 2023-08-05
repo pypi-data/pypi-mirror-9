beab2a7b1af52fea45d15fd3c79779bbcd7279e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:29 +0000

    Add package pysmsru/0.1.7/pysmsru-0.1.7.tar.gz

---
+++ packages/pysmsru/pysmsru-0.1.7.tar.gz/pysmsru-0.1.7/pysmsru/tests/send_sms.py
@@ -0,0 +1,25 @@
+# -*- coding: utf8 -*-
+import unittest
+from pysmsru import SMSRU
+
+
+class TestSendSms(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def Xtest_send_one_wrong_sender_name(self):
+        smsru = SMSRU(
+            debug=True,
+            sendername='lukyoung',
+        )
+        smsru.send_one(phone_to='79214050080', message='test')
+
+    def test_send_one(self):
+        smsru = SMSRU(
+            debug=True
+        )
+        smsru.send_one(phone_to='79214050080', message=u'test ыфв')
+
+
+if __name__ == '__main__':
+    unittest.main()
