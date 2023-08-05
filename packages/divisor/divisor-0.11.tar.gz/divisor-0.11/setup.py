1bfc66ece569ba2b889f06c0dde2017530302b20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:28 +0000

    Add package divisor/0.11/divisor-0.11.tar.gz

---
+++ packages/divisor/divisor-0.11.tar.gz/divisor-0.11/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+setup(
+    name = 'divisor',
+    scripts = ['divisor'],
+    version = '0.11',
+    description = 'A simple game about dividing numbers.',
+    author = 'David Besen',
+    author_email = 'dbesen@gmail.com',
+    url = 'https://bitbucket.org/dbesen/divisor',
+    keywords = ['game', 'cli', 'math'],
+    classifiers = [],
+)
