cdf227cac9aadb98f29ab3460bde5fa0336e2bb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:15 +0000

    Add package classtricks/0.0.1421586552/classtricks-0.0.1421586552.tar.gz

---
+++ packages/classtricks/classtricks-0.0.1421586552.tar.gz/classtricks-0.0.1421586552/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+	description = 'A collection of simple tricks and helper functions for working with classes',
+	author = 'Mike Lang',
+	author_email = 'mikelang3000@gmail.com',
+	py_modules = ['classtricks'],
+	version = '0.0.1421586552',
+	long_description = 'A collection of simple tricks and helper functions for working with classes',
+	name = 'classtricks',
+)
