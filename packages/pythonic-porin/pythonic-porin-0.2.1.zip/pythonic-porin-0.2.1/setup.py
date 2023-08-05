948313f59a84c06fce9d7d30e814c840f73f5a44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:46 +0000

    Add package pythonic-porin/0.2.1/pythonic-porin-0.2.1.zip

---
+++ packages/pythonic-porin/pythonic-porin-0.2.1.zip/pythonic-porin-0.2.1/setup.py
@@ -0,0 +1,44 @@
+from distutils.core import setup
+from distutils.extension import Extension
+import numpy as np
+
+try:
+    from Cython.Distutils import build_ext
+except ImportError:
+    use_cython = False
+else:
+    use_cython = True
+
+cmdclass = { }
+
+if use_cython:
+    ext_modules = [
+        Extension("PyPore.cparsers", [ "PyPore/cparsers.pyx" ], include_dirs=[np.get_include()] ),
+        Extension("PyPore.calignment", [ "PyPore/calignment.pyx" ], include_dirs=[np.get_include()] )
+    ]
+    cmdclass.update({ 'build_ext': build_ext })
+else:
+    ext_modules = [
+        Extension("PyPore.cparsers", [ "PyPore/cparsers.c" ], include_dirs=[np.get_include()] ),
+        Extension("PyPore.calignment", [ "PyPore/calignment.c" ], include_dirs=[np.get_include()] )
+    ]
+
+setup(
+    name='pythonic-porin',
+    version='0.2.1',
+    author='Jacob Schreiber',
+    author_email='jmschreiber91@gmail.com',
+    packages=['PyPore'],
+    url='http://pypi.python.org/pypi/pythonic-porin/',
+    license='LICENSE.txt',
+    description='Nanopore Data Analysis package. Provides tools for reading data,\
+        performing event detection, segmentation, visualization, and analysis using\
+        hidden Markov models, and other tools. Designed for the UCSC Nanopore Group.',
+    cmdclass=cmdclass,
+    ext_modules=ext_modules,
+    install_requires=[
+        "cython >= 0.20.1",
+        "numpy >= 1.8.0",
+        "matplotlib >= 1.3.1"
+    ],
+)
