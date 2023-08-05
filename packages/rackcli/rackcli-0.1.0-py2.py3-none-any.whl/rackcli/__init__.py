4acc2d1cb9029fabd7e945bda6e6ab997eb74c32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:22 +0000

    Add package rackcli/0.1.0/rackcli-0.1.0-py2.py3-none-any.whl

---
+++ packages/rackcli/rackcli-0.1.0-py2.py3-none-any.whl/rackcli/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+from pkg_resources import get_distribution
+
+
+__author__ = 'Jesse Noller'
+__email__ = 'jesse.noller@rackspace.com'
+__version__ = get_distribution('rackcli').version
