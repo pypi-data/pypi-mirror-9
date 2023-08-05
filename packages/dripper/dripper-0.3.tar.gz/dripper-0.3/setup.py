a7b958e0576bd730d734af542934767c9b99c9a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:19 +0000

    Add package dripper/0.3/dripper-0.3.tar.gz

---
+++ packages/dripper/dripper-0.3.tar.gz/dripper-0.3/setup.py
@@ -0,0 +1,28 @@
+import os
+
+from setuptools import setup
+
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(here, 'README.rst')) as f:
+    README = f.read()
+with open(os.path.join(here, 'CHANGES.txt')) as f:
+    CHANGES = f.read()
+
+
+setup(
+    name='dripper',
+    version='0.3',
+    license='MIT',
+    packages=['dripper'],
+    install_requires=[],
+    description='Cleaning your messy data.',
+    long_description=README + '\n\n' + CHANGES,
+    author='Hiroki KIYOHARA',
+    author_email='hirokiky@gmail.com',
+    url='https://github.com/hirokiky/dripper',
+    keywords='dict data converter cleaning mapping',
+    include_package_data=True,
+    zip_safe=False,
+)
