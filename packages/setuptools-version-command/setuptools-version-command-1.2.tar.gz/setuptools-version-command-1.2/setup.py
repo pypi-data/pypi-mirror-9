e3af666ebd25cf7b6ff71ed781e947d5dcf77f05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:51 +0000

    Add package setuptools-version-command/1.2/setuptools-version-command-1.2.tar.gz

---
+++ packages/setuptools-version-command/setuptools-version-command-1.2.tar.gz/setuptools-version-command-1.2/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python2.7
+
+from setuptools import setup
+
+description = '''Adds a command to dynamically get the version from the VCS of choice'''
+
+with open('README.txt', 'r') as f:
+    long_description = f.read()
+
+setup(
+    author='Joost Molenaar',
+    author_email='j.j.molenaar@gmail.com',
+    url='https://github.com/j0057/setuptools-version-command',
+    name='setuptools-version-command',
+    version='1.2',
+    description=description,
+    long_description=long_description,
+    packages=['setuptools_version_command'],
+    entry_points={
+        'distutils.setup_keywords': [
+            'version_command_pep440 = setuptools_version_command:set_pep440',
+            'version_command = setuptools_version_command:execute_version_command'
+        ]
+    })
