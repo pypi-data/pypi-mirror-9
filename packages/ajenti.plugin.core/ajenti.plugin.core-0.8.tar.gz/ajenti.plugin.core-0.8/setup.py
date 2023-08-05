1fa9ace79dd3689a8b31c8d2aeaa356d68fd6ff9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:05 +0000

    Add package ajenti.plugin.core/0.8/ajenti.plugin.core-0.8.tar.gz

---
+++ packages/ajenti.plugin.core/ajenti.plugin.core-0.8.tar.gz/ajenti.plugin.core-0.8/setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+import os
+
+__requires = filter(None, open('requirements.txt').read().splitlines())
+
+setup(
+    name='ajenti.plugin.core',
+    version='0.8',
+    install_requires=__requires,
+    description='Ajenti core plugin',
+    author='Ajenti project',
+    author_email='e@ajenti.org',
+    url='http://ajenti.org',
+    packages=find_packages(),
+    include_package_data=True,
+)
