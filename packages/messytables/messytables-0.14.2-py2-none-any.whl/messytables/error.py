19d76504615d502c0e9ec398757063d0b498f44d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:22 +0000

    Add package messytables/0.14.2/messytables-0.14.2-py2-none-any.whl

---
+++ packages/messytables/messytables-0.14.2-py2-none-any.whl/messytables/error.py
@@ -0,0 +1,12 @@
+class MessytablesError(Exception):
+    """A generic error to inherit from"""
+
+
+class ReadError(MessytablesError):
+    '''Error reading the file/stream in terms of the expected format.'''
+    pass
+
+
+class TableError(MessytablesError, LookupError):
+    """Couldn't identify correct table."""
+    pass
