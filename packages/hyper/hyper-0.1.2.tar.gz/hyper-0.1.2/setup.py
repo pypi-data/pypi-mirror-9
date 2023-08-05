973442c655312a3845132e08f738268ce997b078
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:07 +0000

    Add package hyper/0.1.2/hyper-0.1.2.tar.gz

---
+++ packages/hyper/hyper-0.1.2.tar.gz/hyper-0.1.2/setup.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import itertools
+import os
+import re
+import sys
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+# Get the version
+version_regex = r'__version__ = ["\']([^"\']*)["\']'
+with open('hyper/__init__.py', 'r') as f:
+    text = f.read()
+    match = re.search(version_regex, text)
+
+    if match:
+        version = match.group(1)
+    else:
+        raise RuntimeError("No version number found!")
+
+# Stealing this from Kenneth Reitz
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    sys.exit()
+
+py_version = sys.version_info[:2]
+
+def resolve_install_requires():
+    if py_version in [(2,7), (3,3)]:
+        return ['pyOpenSSL>=0.14']
+    return []
+
+packages = ['hyper', 'hyper.http20']
+
+setup(
+    name='hyper',
+    version=version,
+    description='HTTP/2 Client for Python',
+    long_description=open('README.rst').read() + '\n\n' + open('HISTORY.rst').read(),
+    author='Cory Benfield',
+    author_email='cory@lukasa.co.uk',
+    url='http://hyper.rtfd.org',
+    packages=packages,
+    package_data={'': ['LICENSE', 'README.rst', 'CONTRIBUTORS.rst', 'HISTORY.rst', 'NOTICES']},
+    package_dir={'hyper': 'hyper'},
+    include_package_data=True,
+    license='MIT License',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: Implementation :: CPython',
+    ],
+    install_requires=resolve_install_requires(),
+)
