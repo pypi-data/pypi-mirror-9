568f1b481ac8e7ef5052dd079d592ba7276284b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:39 +0000

    Add package dockerup/1.0.10/dockerup-1.0.10.tar.gz

---
+++ packages/dockerup/dockerup-1.0.10.tar.gz/dockerup-1.0.10/dockerup/__init__.py
@@ -0,0 +1,262 @@
+#!/usr/bin/python2.7
+
+import json
+import os
+import shutil
+import logging
+import dockerup.docker
+
+class DockerUp(object):
+
+	"""
+	Service for synchronizing locally running Docker containers with an external
+	configuration file. If available, EC2 user-data is used as the configuration file,
+	otherwise dockerup looks in /etc/dockerup/dockerup.json by default (override with --config).
+
+	This script can be run on-demand or via a cron job.
+
+	Sample config file is shown below:
+
+	{
+		"containers": [
+			{
+				"type": "docker",
+				"name": "historical-app",
+				"image": "barchart/historical-app-alpha",
+				"portMappings": [ 
+					{
+						"containerPort": "8080",
+						"hostPort": "8080"
+					}
+				]
+			},
+			{
+				"type": "docker",
+				"name": "logstash-forwarder",
+				"image": "barchart/logstash-forwarder",
+				"volumes": [
+					{
+						"containerPath": "/var/log/containers",
+						"hostPath": "/var/log/ext",
+						"mode": "ro"
+					}
+				]
+			}
+		]
+	}
+	"""
+
+	def __init__(self, config, cache):
+
+		self.config = config
+		self.cache = cache
+		self.docker = docker.Docker()
+
+		self.log = logging.getLogger(__name__)
+
+	def update(self, container):
+
+		if not 'image' in container:
+			self.log.warn('No image defined for container, skipping')
+			return
+
+		current = self.status(container)
+		updated = self.docker.pull(container['image']) or self.updated(container)
+		status = self.status(container)
+
+		if updated or not current['running']:
+
+			if current['running']:
+				self.stop(current)
+
+			if status['image']:
+				try:
+					self.run(container, status)
+					status = self.status(container)
+				except Exception as e:
+					self.log.error('Could not run container: %s' % e)
+			else:
+				self.log.error('Image not found: %s' % container['image'])
+
+		return status
+
+	def status(self, container):
+
+		image_parts = container['image'].split(':')
+		repository = image_parts[0]
+		tag = 'latest' if len(image_parts) == 1 else image_parts[1]
+
+		image = self.docker.image(repository, tag)
+		container = self.docker.container(image['id']) if image else None
+
+		return {
+			'repository': repository,
+			'tag': tag,
+			'image': image['id'] if image else None,
+			'container': container['id'] if container else None,
+			'running': container['running'] if container else False
+		}
+
+	def updated(self, container):
+		
+		cachefile = '%s/%s.json' % (self.cache, self.__cache_name(container))
+
+		if not os.path.exists(cachefile):
+			return True
+
+		serialized = json.dumps(container)
+
+		with open(cachefile) as local:
+			cached = local.read()
+
+		if serialized != cached:
+			return True
+
+		return False
+
+	def cache_config(self):
+		
+		if 'containers' in self.config:
+
+			for container in self.config['containers']:
+
+				cachefile = '%s/%s.json' % (self.cache, self.__cache_name(container))
+
+				with open(cachefile, 'w') as local:
+					json.dump(container, local)
+
+	def __cache_name(self, container):
+
+		image_clean = container['image'].replace(':', '_').replace('/', '_')
+
+		if 'name' in container:
+			return '%s-%s' % (image_clean, container['name'])
+
+		return image_clean
+
+	def run(self, config, status):
+
+		if 'type' in config and config['type'] != 'docker':
+			return False
+
+		# Set up exported logging directory for apps
+		args = ['-v', '/var/log/ext/%s:/var/log/ext' % status['image']]
+
+		if 'volumes' in config:
+
+			for vol in config['volumes']:
+
+				if not 'containerPath' in vol:
+					self.log.warn('No container mount point specified, skipping volume')
+					continue
+
+				volargs = []
+				if 'hostPath' in vol:
+					volargs.append(vol['hostPath'])
+				volargs.append(vol['containerPath'])
+				if 'mode' in vol:
+					volargs.append(vol['mode'].lower())
+
+				args.append('-v')
+				args.append(':'.join(volargs))
+
+		if 'name' in config:
+			if config['name'].startswith('local-'):
+				self.log.error('Invalid container name, local-* is reserved')
+				return False
+			args.append('--name=%s' % config['name'])
+
+		if 'privileged' in config and config['privileged']:
+			args.append('--privileged')
+
+		if 'network' in config:
+			args.append('--net=%s' % config['network'].lower())
+
+		if 'portMappings' in config:
+			for port in config['portMappings']:
+				args.append('-p')
+				if 'hostPort' in port:
+					args.append('%s:%s' % (port['hostPort'], port['containerPort']))
+				else:
+					args.append(port['containerPort'])
+
+		if 'env' in config:
+			for key, value in config['env'].iteritems():
+				args.append('-e')
+				args.append('%s=%s' % (key, value))
+
+		return self.docker.run(config['image'], args)
+
+	def stop(self, status, remove=True):
+
+		self.docker.stop(status['container'], remove)
+
+		if remove:
+			try:
+				# Remove logs directory
+				shutil.rmtree('/var/log/ext/%s' % status['image'])
+			except Exception as e:
+				self.log.warn('Could not remove logs: %s' % e)
+
+	# Shutdown containers with unrecognized images
+	def shutdown_unknown(self):
+
+		existing = []
+
+		for entry in os.listdir(self.cache):
+
+			if not entry.endswith('.json'):
+				continue
+
+			cachefile = '%s/%s' % (self.cache, entry)
+
+			with open(cachefile) as local:
+				cached = json.load(local)
+
+			status = self.status(cached)
+			
+			if status['container']:
+				existing.append(status['container'])
+
+		# Iterate through running containers and stop them if they don't match a cached config
+		[self.docker.stop(c['id']) for c in self.docker.containers() if c['running'] and not c['id'] in existing]
+
+	# Shutdown leftover containers from old configurations
+	def cleanup(self, running):
+
+		for entry in os.listdir(self.cache):
+
+			if not entry.endswith('.json'):
+				continue
+
+			cachefile = '%s/%s' % (self.cache, entry)
+
+			with open(cachefile) as local:
+				cached = json.load(local)
+
+			status = self.status(cached)
+
+			if status['container'] and not status['container'] in running:
+				self.stop(status)
+
+			os.unlink(cachefile)
+
+	def start(self):
+
+		# Rare occurence, kill containers that have an unknown image tag
+		# Usually due to manual updates, may be required to avoid port binding conflicts
+		self.shutdown_unknown()
+
+		# Process configuration and store running container IDs
+		running = []
+		if 'containers' in self.config:
+			running = [self.update(container)['container'] for container in self.config['containers']]
+
+		# Cleanup containers with no config
+		self.cleanup(running)
+
+		# Cache config for next run
+		self.cache_config()
+
+		# Remove unused containers/images from Docker
+		self.docker.cleanup()
---
+++ packages/dockerup/dockerup-1.0.10.tar.gz/dockerup-1.0.10/dockerup/docker.py
@@ -0,0 +1,176 @@
+import os
+import sys
+import logging
+from dockerup.proc import read_command
+
+class Docker(object):
+
+	def __init__(self):
+
+		self.image_cache = []
+		self.container_cache = []
+
+		self.log = logging.getLogger(__name__)
+
+	def refresh(self):
+		self.image_cache = self.__load_images()
+		self.container_cache = self.__load_containers()
+
+	def image(self, repository=None, tag=None, id=None):
+
+		for image in self.images():
+
+			if repository and repository != image['repository']:
+				continue
+
+			if tag and tag != image['tag']:
+				continue
+
+			if id and id != image['id']:
+				continue
+
+			return image
+
+		return None
+
+	def images(self):
+
+		if not len(self.image_cache):
+			self.image_cache = self.__load_images()
+
+		return self.image_cache
+
+	def container(self, image=None):
+
+		for container in self.containers():
+			if image is None or image == container['image']:
+				return container
+
+		return None
+
+	def containers(self):
+
+		if not len(self.container_cache):
+			self.container_cache = self.__load_containers()
+
+		return self.container_cache
+
+	def pull(self, image):
+
+		try:
+			self.log.debug('Pulling image: %s', image)
+			for line in read_command(['docker', 'pull', image]):
+				if line.startswith('Status: Downloaded newer image'):
+					return True
+		except:
+			# Missing image probably, just return false
+			pass
+
+		return False
+
+	# Run a new container
+	def run(self, image, options=None):
+
+		args = ['docker', 'run', '-d', '--restart=always']
+
+		if options is not None:
+			args.extend(options)
+
+		args.append(image)
+
+		self.log.debug('Running container: %s' % image)
+
+		container = read_command(args).strip()
+
+		self.log.info('Started container: %s' % container)
+
+		return container
+
+	# Start existing container
+	def start(self, container):
+		self.log.debug('Starting container: %s', container)
+		out = read_command(['docker', 'start', container])
+
+	# Stop running container
+	def stop(self, container, remove=True):
+
+		self.log.debug('Stopping container: %s', container)
+		out = read_command(['docker', 'stop', container])
+
+		if remove:
+			self.rm(container)
+
+	# Remove container
+	def rm(self, container):
+
+		self.log.debug('Removing stopped container: %s' % container)
+		out = read_command(['docker', 'rm', container])
+
+	# Remove image
+	def rmi(self, image):
+
+		self.log.debug('Removing image: %s' % image)
+		out = read_command(['docker', 'rmi', image])
+
+	# Cleanup stopped containers and unused images
+	def cleanup(self, images=True):
+
+		# Always refresh state before cleanup
+		self.refresh()
+
+		running = []
+
+		for container in self.containers():
+			if container['running']:
+				running.append(container['image'])
+			else:
+				self.rm(container['id'])
+		
+		if images:
+			for image in self.images():
+				if not image['id'] in running:
+					try:
+						self.rmi(image['id'])
+					except Exception as e:
+						# Probably an intermediary container
+						pass
+
+	"""
+	Private methods
+	"""
+
+	def __load_images(self):
+
+		out = read_command(['docker', 'images', '--no-trunc'], True, '<none>')
+
+		images = []
+
+		return [{
+				'id': line['IMAGE ID'],
+				'repository': line['REPOSITORY'],
+				'tag': line['TAG']
+			} for line in out]
+
+	def __load_containers(self):
+
+		out = read_command(['docker', 'ps', '-a', '--no-trunc'], True)
+
+		containers = []
+		images = self.images()
+
+		for line in out:
+
+			image_id = line['IMAGE']
+
+			if ':' in line['IMAGE']:
+				parts = line['IMAGE'].split(':')
+				match = self.image(parts[0], parts[1])
+				image_id = match['id'] if match else None
+
+			containers.append({
+				'id': line['CONTAINER ID'],
+				'image': image_id,
+				'running': line['STATUS'].startswith('Up ')
+			})
+		
+		return containers
