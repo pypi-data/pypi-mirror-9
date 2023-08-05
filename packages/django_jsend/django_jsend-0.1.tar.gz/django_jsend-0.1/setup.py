cb6282e7c1db012865e998d90c33207afb2ec3e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:35 +0000

    Add package django_jsend/0.1/django_jsend-0.1.tar.gz

---
+++ packages/django_jsend/django_jsend-0.1.tar.gz/django_jsend-0.1/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+	name = 'django_jsend',
+	packages = ['django_jsend'],
+	version = '0.1',
+	description = 'Django view for JSend specification',
+	author = 'Maikel Martens',
+	author_email = 'maikel@martens.me',
+	url = 'https://github.com/krukas/django-jsend',
+	download_url = 'https://github.com/krukas/django-jsend/tarball/0.1',
+	keywords = ['Django', 'view', 'JSend'],
+	classifiers = [],
+)
