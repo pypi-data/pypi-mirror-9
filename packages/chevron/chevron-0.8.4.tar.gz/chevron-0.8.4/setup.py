949c468770b7d410810313d2d5dc1801e7869f8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:26 +0000

    Add package chevron/0.8.4/chevron-0.8.4.tar.gz

---
+++ packages/chevron/chevron-0.8.4.tar.gz/chevron-0.8.4/setup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+try:
+    import pypandoc
+    readme = pypandoc.convert('README.md', 'rest')
+except (ImportError, RuntimeError):
+    print('\n\n!!!\npypandoc not loaded\n!!!\n')
+    readme = ''
+
+
+setup(name='chevron',
+      version='0.8.4',
+      license='MIT',
+
+      description='Mustache templating language renderer',
+      long_description=readme,
+
+      author='noah morrison',
+      author_email='noah@morrison.ph',
+      url='https://github.com/noahmorrison/chevron',
+
+      packages=['chevron'],
+      entry_points={
+          'console_scripts': ['chevron=chevron:cli_main']
+      },
+
+      classifiers=[
+          'Development Status :: 4 - Beta',
+
+          'License :: OSI Approved :: MIT License',
+
+          'Programming Language :: Python :: 2.6',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3.2',
+          'Programming Language :: Python :: 3.3',
+          'Programming Language :: Python :: 3.4',
+
+          'Topic :: Text Processing :: Markup'
+      ]
+      )
