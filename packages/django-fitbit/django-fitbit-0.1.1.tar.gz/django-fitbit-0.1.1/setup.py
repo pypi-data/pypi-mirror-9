3af92c6c1146129540ce64dfc3c5a97a2269d29d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:51 +0000

    Add package django-fitbit/0.1.1/django-fitbit-0.1.1.tar.gz

---
+++ packages/django-fitbit/django-fitbit-0.1.1.tar.gz/django-fitbit-0.1.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+required = [line for line in open('requirements/base.txt').read().split("\n")]
+
+setup(
+    name="django-fitbit",
+    version=__import__("fitapp").__version__,
+    author="orcas",
+    author_email="",
+    packages=find_packages(),
+    install_requires=["setuptools"] + required,
+    include_package_data=True,
+    url="https://github.com/orcasgit/django-fitbit/",
+    license="",
+    description="Django integration for python-fitbit",
+    long_description=open("README.md").read(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        'License :: OSI Approved :: Apache Software License',
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 2 :: Only"
+    ],
+    test_suite="runtests.runtests"
+)
