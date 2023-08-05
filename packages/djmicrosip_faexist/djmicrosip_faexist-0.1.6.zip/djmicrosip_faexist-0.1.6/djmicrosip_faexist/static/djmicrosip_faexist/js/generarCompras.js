7c1937bb1a153db9545705187c54a3f8aff7182c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:43 +0000

    Add package djmicrosip_faexist/0.1.6/djmicrosip_faexist-0.1.6.zip

---
+++ packages/djmicrosip_faexist/djmicrosip_faexist-0.1.6.zip/djmicrosip_faexist-0.1.6/djmicrosip_faexist/static/djmicrosip_faexist/js/generarCompras.js
@@ -0,0 +1,47 @@
+
+$("input[name='select_all']").on("click", sellect_all);
+function sellect_all(){
+  var to_select = this.checked;
+  documentos = $("input[type='checkbox'][name='documento']");
+  documentos.each(function(){
+    this.checked = to_select;
+    mostrar_selecionados();
+  });
+}
+
+function mostrar_selecionados(){
+  var selecionados = $("input[type='checkbox'][name='documento']:checked").length
+
+  if (selecionados > 0) {
+    $("#selected_documents_count").text(" "+selecionados + " documentos selecionados.");
+  }else{
+    $("#selected_documents_count").text("");
+  }
+}
+
+$("input[type='checkbox'][name='documento']").on("click", mostrar_selecionados );
+
+$("#btn_generarFacturas").on("click", function(){
+  if ( $("#btn_generarFacturas").attr("disabled") == "disabled")
+    return false
+  $("#btn_generarFacturas").attr("disabled",'disabled');
+  
+  var documentos_selecionados = $("input[type='checkbox'][name='documento']:checked").map(function() {return this.value;}).get();
+  $.ajax({
+    url:'/djmicrosip_faexist/generar_compras/', 
+    type : 'get', 
+    data:{
+      'documentos_ids': documentos_selecionados,
+    }, 
+    success: function(data){
+      alert(data.msg);
+      location.reload();
+    },
+    error: function() {
+      alert('fallo algo');
+    },
+  });
+});
+
+
+$("#selected_documents_count").text("");
