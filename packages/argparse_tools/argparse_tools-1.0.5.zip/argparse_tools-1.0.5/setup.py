13fe26a87f155404dcd3b5d8c58640b98755934a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:41 +0000

    Add package argparse_tools/1.0.5/argparse_tools-1.0.5.zip

---
+++ packages/argparse_tools/argparse_tools-1.0.5.zip/argparse_tools-1.0.5/setup.py
@@ -0,0 +1,26 @@
+try:
+    from distutils.core import setup
+    from setuptools import find_packages
+except ImportError:
+    print ("Please install Distutils and setuptools"
+           " before installing this package")
+    raise
+
+setup(
+    name='argparse_tools',
+    version='1.0.5',
+    description=(
+        'Share a standardized set of argparse arguments within your codebase'),
+    long_description="Check the project homepage for details",
+    keywords=['argparse', ],
+
+    author='Alex Gaudio',
+    author_email='adgaudio@gmail.com',
+    url='https://github.com/adgaudio/argparse_tools',
+
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires = ['argparse'],
+    tests_require=['nose'],
+    test_suite="nose.main",
+)
