4d833f81517611b3a67557163bf6a3828490b9cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:38 +0000

    Add package clack/1.1.0/clack-1.1.0.tar.gz

---
+++ packages/clack/clack-1.1.0.tar.gz/clack-1.1.0/clack/utils.py
@@ -0,0 +1,8 @@
+"""Utility functions for clack."""
+
+import os.path
+
+
+def local_path(filename):
+    """Return an absolute path to a file in the current directory."""
+    return os.path.join(os.path.dirname(os.path.realpath(__file__)), filename)
