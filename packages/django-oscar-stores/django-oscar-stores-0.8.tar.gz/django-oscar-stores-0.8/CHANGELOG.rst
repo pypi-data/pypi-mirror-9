a728f073c2b2e84daf17399ce413b9b64f565378
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:35 +0000

    Add package django-oscar-stores/0.8/django-oscar-stores-0.8.tar.gz

---
+++ packages/django-oscar-stores/django-oscar-stores-0.8.tar.gz/django-oscar-stores-0.8/CHANGELOG.rst
@@ -0,0 +1,59 @@
+=========
+Changelog
+=========
+
+0.8
+===
+
+* Django 1.7 support
+* Fix localisation bug
+
+0.7
+===
+
+* Load geocode service dynamically
+* Drop support for Oscar 0.5
+
+0.6.1
+=====
+
+* Support Oscar 0.7
+
+0.6
+===
+
+* Support Django 1.6
+* Templates now load assets over HTTPS where appropriate
+
+0.5.1
+=====
+
+A couple of extensions.
+
+* Fix cancel link for store form
+* Allow HTML map box to be more easily customised
+* Allow search results to be distance limited
+
+0.5
+===
+
+* Support Oscar 0.6
+
+* Support Django 0.5
+
+0.4.1
+=====
+
+* Allow opening hours form to pick up new fields.
+
+0.4
+===
+
+* Upgrade to Oscar 0.5.  This involves upgrading the JS to use jQuery 1.9 and
+  adjusting the dashboard templates.
+
+* Improve support for multiple opening hours for a single day.
+
+* Tests now run on Travis.  Test coverage is monitored on coveralls.io
+
+* Allow GeoIP to be disabled with a setting.
---
+++ packages/django-oscar-stores/django-oscar-stores-0.8.tar.gz/django-oscar-stores-0.8/setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+import os
+from setuptools import setup, find_packages
+
+setup(
+    name='django-oscar-stores',
+    version="0.8",
+    url='https://github.com/django-oscar/django-oscar-stores',
+    author="David Winterbottom",
+    author_email="david.winterbottom@gmail.com",
+    description="An extension for Oscar to include stores",
+    long_description=open(
+        os.path.join(os.path.dirname(__file__), 'README.rst')).read(),
+    keywords="django, oscar, e-commerce",
+    license='BSD',
+    platforms=['linux'],
+    packages=find_packages(exclude=["sandbox*", "tests*"]),
+    include_package_data=True,
+    install_requires=[
+        'django-oscar>=0.6',
+        'requests>=1.1',
+    ],
+    # See http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: Unix',
+        'Programming Language :: Python',
+    ])
