7e0677cc704cabaf4ce139a945989eb5d32dcbd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:44 +0000

    Add package pycedict/0.9.1/pycedict-0.9.1.tar.gz

---
+++ packages/pycedict/pycedict-0.9.1.tar.gz/pycedict-0.9.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pycedict',
+      version='0.9.1',
+      description='A library for parsing CEDict and adding tone marks to pinyin',
+      url='https://github.com/jdillworth/pycedict',
+      author='Jeremy Dillworth',
+      author_email='jeremy@winter14.com',
+      license='New BSD',
+      packages=['cedict'],
+      zip_safe=False)
