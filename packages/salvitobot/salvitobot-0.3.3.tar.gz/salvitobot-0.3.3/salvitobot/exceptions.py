436f165c1a822258e231ac5f473de838f398b973
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:09 +0000

    Add package salvitobot/0.3.3/salvitobot-0.3.3.tar.gz

---
+++ packages/salvitobot/salvitobot-0.3.3.tar.gz/salvitobot-0.3.3/salvitobot/exceptions.py
@@ -0,0 +1,18 @@
+class NoCountryError(Exception):
+    pass
+
+
+class ProcedureError(Exception):
+    pass
+
+
+class ToPublishPostError(Exception):
+    pass
+
+
+class WordPressNotConfigured(Exception):
+    pass
+
+
+class NoTwitterToken(Exception):
+    pass
