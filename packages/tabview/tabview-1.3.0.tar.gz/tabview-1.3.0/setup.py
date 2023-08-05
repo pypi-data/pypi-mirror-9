aaa222104ad774a61cb330b271842e38ea1a90bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:45 +0000

    Add package tabview/1.3.0/tabview-1.3.0.tar.gz

---
+++ packages/tabview/tabview-1.3.0.tar.gz/tabview-1.3.0/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+import setuptools
+
+setup(name="tabview",
+      version="1.3.0",
+      description="A curses command-line CSV and list (tabular data) viewer",
+      long_description=open('README.rst').read(),
+      author="Scott Hansen",
+      author_email="firecat4153@gmail.com",
+      url="https://github.com/firecat53/tabview",
+      download_url="https://github.com/firecat53/tabview/tarball/1.3.0",
+      packages=['tabview'],
+      scripts=['bin/tabview'],
+      package_data={'tabview': ['README.rst']},
+      data_files=[('share/doc/tabview',
+                   ['README.rst', 'LICENSE.txt', 'CHANGELOG.rst'])],
+      license="MIT",
+      )
