0af58f5b839b5f286d34ac2e4c70d297166d465d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:10 +0000

    Add package pyfrc/2015.2.6/pyfrc-2015.2.6.tar.gz

---
+++ packages/pyfrc/pyfrc-2015.2.6.tar.gz/pyfrc-2015.2.6/lib/pyfrc/tests/basic.py
@@ -0,0 +1,103 @@
+'''
+    The primary purpose of these tests is to run through your code
+    and make sure that it doesn't crash. If you actually want to test
+    your code, you need to write your own custom tests to tease out
+    the edge cases.
+    
+    To use these, add the following to a python file in your tests directory::
+    
+        from pyfrc.tests import *
+    
+'''
+
+import math
+
+
+def test_autonomous(control, fake_time, robot):
+    '''Runs autonomous mode by itself'''
+    
+    # run autonomous mode for 15 seconds
+    control.set_autonomous(enabled=True)
+    control.run_test(lambda tm: tm < 15)
+    
+    # make sure autonomous mode ran for 15 seconds
+    assert int(fake_time.get()) == 15
+
+
+def test_disabled(control, fake_time, robot):
+    '''Runs disabled mode by itself'''
+    
+    # run disabled mode for 5 seconds
+    control.set_autonomous(enabled=False)
+    control.run_test(lambda tm: tm < 5)
+    
+    # make sure disabled mode ran for 5 seconds
+    assert int(fake_time.get()) == 5
+
+
+def test_operator_control(control, fake_time, robot):
+    '''Runs operator control mode by itself'''
+    
+    # run operator mode for 120 seconds
+    control.set_operator_control(enabled=True)
+    control.run_test(lambda tm: tm < 120)
+    
+    # make sure operator mode ran for 10 seconds
+    assert int(fake_time.get()) == 120
+
+
+def test_practice(control, fake_time, robot):
+    '''Runs through the entire span of a practice match'''
+    
+    class TestController:
+        
+        def __init__(self):
+            self.mode = None
+            
+            self.disabled = 0
+            self.autonomous = 0
+            self.teleop = 0
+            
+            
+        def on_step(self, tm):
+            '''
+                Called on each simulation step. This runs through each mode,
+                and asserts that the robot didn't spend too much time in any
+                particular mode.
+            
+                :param tm: The current robot time
+            '''
+            
+            mode = control.get_mode()
+            if mode == self.mode:
+                return
+            
+            if mode == 'autonomous':
+                self.autonomous += 1
+                assert int(math.floor(fake_time.get())) == 5
+                
+            elif mode == 'teleop':
+                self.teleop += 1
+                assert int(math.floor(fake_time.get())) == 21
+            
+            elif mode == 'disabled':
+                self.disabled += 1
+                
+                if self.disabled == 1:
+                    assert int(math.floor(fake_time.get())) == 0
+                else:
+                    assert int(math.floor(fake_time.get())) == 20
+            else:
+                assert False, "Internal error!"
+            
+            self.mode = mode
+    
+    control.set_practice_match()
+    tc = control.run_test(TestController)
+    
+    assert int(math.floor(fake_time.get())) == 141
+    
+    # If an error occurs here, for some reason a mode got called too many times
+    assert tc.disabled == 2
+    assert tc.autonomous == 1
+    assert tc.teleop == 1
