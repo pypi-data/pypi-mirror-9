bdc9bee4caf8e1aa9b750311c472c8fe3ae4087a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:44 +0000

    Add package beebotte/0.3.1/beebotte-0.3.1.tar.gz

---
+++ packages/beebotte/beebotte-0.3.1.tar.gz/beebotte-0.3.1/setup.py
@@ -0,0 +1,28 @@
+from distutils.core import setup
+
+setup(
+    name='beebotte',
+    version='0.3.1',
+    description='Python library for interfacing with Beebotte',
+    url='http://beebotte.com',
+    download_url='https://pypi.python.org/pypi/beebotte/',
+    author='Beebotte',
+    author_email='contact@beebotte.com',
+    maintainer="Bachar Wehbi",
+    maintainer_email='bwehbi@beebotte.com',
+    packages=['beebotte'],
+    install_requires=[
+        "requests"
+    ],
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Development Status :: 4 - Beta",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Developers",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    keywords='beebotte, api, rest, pubsub, restful, iot, internet of things, device, realtime, connected, websockets',
+    license='MIT',
+)
