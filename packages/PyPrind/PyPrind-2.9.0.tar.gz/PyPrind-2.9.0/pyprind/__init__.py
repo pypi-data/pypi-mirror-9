37580727406ca18df7cbcf444bee31b6b9c3cb50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:09 +0000

    Add package PyPrind/2.9.0/PyPrind-2.9.0.tar.gz

---
+++ packages/PyPrind/PyPrind-2.9.0.tar.gz/PyPrind-2.9.0/pyprind/__init__.py
@@ -0,0 +1,61 @@
+# Sebastian Raschka 01/27/2014
+# PyPrind - Python Progress Indicator module
+
+""" The PyPrind (Python Progress Indicator) module lets you visualize the
+progress of a programming task in Python via a progress bar or a
+percentage indicator.
+Progress bars are visualized via a `ProgBar()` object, and
+alternatively, the progress can be shown as an percentage via the
+`ProgPercent()` object.
+
+Example - Progress Bar
+-------------------------------
+   ```import pyprind
+
+   n = 10000000
+   my_prbar = pyprind.ProgBar(n)
+   for i in range(n):
+       # do some computation
+       my_prbar.update()
+
+   for i in pyprind.prog_bar(range(n)):
+       # do something
+       pass
+   ```
+
+Example - Percentage Indicator
+-------------------------------
+   ```import pyprind
+
+   n = 1000000
+   my_perc = pyprind.ProgPercent(n)
+   for i in range(n):
+       # do some computation
+       my_perc.update()
+
+   for i in pyprind.prog_percent(range(n)):
+       # do something
+       pass
+   ```
+
+"""
+
+from .progbar import ProgBar
+from .progpercent import ProgPercent
+
+
+def generator_factory(mother_class):
+    def generator_progress(iteritem, iterations=None, *args, **kw):
+        if iterations is None:
+            iterations = len(iteritem)
+        assert iterations
+        mbar = mother_class(iterations, *args, **kw)
+        for item in iteritem:
+            yield item
+            mbar.update()
+    return generator_progress
+
+prog_percent = generator_factory(ProgPercent)
+prog_bar = generator_factory(ProgBar)
+
+__version__ = '2.9.0'
---
+++ packages/PyPrind/PyPrind-2.9.0.tar.gz/PyPrind-2.9.0/setup.py
@@ -0,0 +1,53 @@
+from distutils.core import setup
+
+setup(name='PyPrind',
+      version='2.9.0',
+      description='Python Progress Indicator Utility',
+      author='Sebastian Raschka',
+      author_email='se.raschka@gmail.com',
+      url='https://github.com/rasbt/pyprind',
+      packages=['pyprind'],
+      data_files = [('', ['LICENSE.txt']),
+                    ('', ['README.html']),
+                    ('', ['CHANGELOG.txt']),
+                    ('examples', ['examples/ex1_percentage_indicator_stderr.py']),
+                    ('examples', ['examples/ex1_percentage_indicator_stdout.py']),
+                    ('examples', ['examples/ex1_progress_bar_stderr.py']),
+                    ('examples', ['examples/ex1_progress_bar_stdout.py']),
+                    ('examples', ['examples/ex2_percent_indicator_allargs.py']),
+                    ('examples', ['examples/ex2_progressbar_allargs.py']),
+                    ('examples', ['examples/ex3_percentage_indicator_monitor.py']),
+                    ('examples', ['examples/ex3_progress_bar_monitor.py']),
+                   ],
+      requires = ['psutil'],
+      license='GPLv3',
+      platforms='any',
+      classifiers=[
+          'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+          'Development Status :: 5 - Production/Stable',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Environment :: Console',
+      ],
+      long_description="""
+
+The PyPrind (Python Progress Indicator) module provides a progress bar and a percentage indicator
+object that let you track the progress of a loop structure or other iterative computation.
+Typical applications include the processing of large data sets to provide an intuitive estimate
+at runtime about the progress of the computation.
+
+For more details and examples please see the package documentation.
+
+A short video demonstration of the progress tracking can be found on YouTube:
+http://youtu.be/iWjSGe89Pvs
+
+
+Contact
+=============
+
+If you have any questions or comments about PyPrind, please feel free to contact me via
+eMail: se.raschka@gmail.com
+or Twitter: https://twitter.com/rasbt
+
+""",
+    )
