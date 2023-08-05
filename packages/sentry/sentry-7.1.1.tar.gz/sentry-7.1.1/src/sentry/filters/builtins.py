62a60355bed0c211c7850d2b10517eeaf0170c57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:21 +0000

    Add package sentry/7.1.1/sentry-7.1.1.tar.gz

---
+++ packages/sentry/sentry-7.1.1.tar.gz/sentry-7.1.1/src/sentry/filters/builtins.py
@@ -0,0 +1,34 @@
+"""
+sentry.filters.base
+~~~~~~~~~~~~~~~~~~~
+
+:copyright: (c) 2010-2014 by the Sentry Team, see AUTHORS for more details.
+:license: BSD, see LICENSE for more details.
+"""
+from __future__ import absolute_import
+
+from django.utils.datastructures import SortedDict
+from django.utils.translation import ugettext_lazy as _
+
+from sentry.models import GroupStatus
+
+from .base import Filter
+
+__all__ = ('StatusFilter',)
+
+
+STATUS_LEVELS = (
+    (GroupStatus.UNRESOLVED, _('Unresolved')),
+    (GroupStatus.RESOLVED, _('Resolved')),
+    (GroupStatus.MUTED, _('Muted')),
+)
+
+
+class StatusFilter(Filter):
+    label = _('Status')
+    column = 'status'
+    default = '0'
+    choices = SortedDict(STATUS_LEVELS)
+
+    def get_choices(self):
+        return self.choices
