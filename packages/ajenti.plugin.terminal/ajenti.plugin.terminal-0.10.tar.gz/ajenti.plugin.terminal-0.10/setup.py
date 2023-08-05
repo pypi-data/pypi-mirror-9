26e0daa0f4c768dc5815f81c048d5b2fadb67a8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:12 +0000

    Add package ajenti.plugin.terminal/0.10/ajenti.plugin.terminal-0.10.tar.gz

---
+++ packages/ajenti.plugin.terminal/ajenti.plugin.terminal-0.10.tar.gz/ajenti.plugin.terminal-0.10/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+import os
+
+__requires = filter(None, open('requirements.txt').read().splitlines())
+
+setup(
+    name='ajenti.plugin.terminal',
+    version='0.10',
+    install_requires=__requires,
+    description='Terminal',
+    long_description='A Terminal plugin for Ajenti panel',
+    author='Ajenti project',
+    author_email='e@ajenti.org',
+    url='http://ajenti.org',
+    packages=find_packages(),
+    include_package_data=True,
+)
