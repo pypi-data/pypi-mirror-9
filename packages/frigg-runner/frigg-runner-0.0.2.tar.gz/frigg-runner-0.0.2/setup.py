ed16cb2f085aa39d013692f1e894598d61bf9f19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:05 +0000

    Add package frigg-runner/0.0.2/frigg-runner-0.0.2.tar.gz

---
+++ packages/frigg-runner/frigg-runner-0.0.2.tar.gz/frigg-runner-0.0.2/setup.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from distutils.core import setup
+
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read().replace('.. :changelog:', '')
+
+requirements = [
+    'invoke',
+    'pyaml',
+    'frigg-common',
+]
+
+test_requirements = [
+    'pytest',
+    'pytest-cov',
+    'pytest-sugar',
+    'mock'
+]
+
+setup(
+    name='frigg-runner',
+    version='0.0.2',
+    description="Frigg runner execute .frigg.yml file localy.",
+    long_description=readme + '\n\n' + history,
+    author="Eirik Martiniussen Sylliaas",
+    author_email='eirik@sylliaas.no',
+    url='https://github.com/eirsyl/frigg-runner',
+    packages=find_packages(exclude='tests'),
+    package_dir={'frigg-runner':
+                 'frigg-runner'},
+    include_package_data=True,
+    entry_points={
+        "console_scripts": ['frigg = frigg_runner.cli:main']
+    },
+    install_requires=requirements,
+    license="MIT",
+    zip_safe=False,
+    keywords='frigg-runner',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
+    test_suite='tests',
+    tests_require=test_requirements
+)
