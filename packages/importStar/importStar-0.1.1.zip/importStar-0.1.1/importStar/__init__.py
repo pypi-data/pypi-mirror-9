b292da7bd1e475a198db30a10dd8c0b47f61f8ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:57 +0000

    Add package importStar/0.1.1/importStar-0.1.1.zip

---
+++ packages/importStar/importStar-0.1.1.zip/importStar-0.1.1/importStar/__init__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+'''
+importStar - auto modify all __init__.py in a package to import all modules
+'''
+
+__version__ = '0.1.1'
+__author__ = 'Karl Bedrich'
+__email__ = 'karl@bedrich.de'
+__url__ = 'https://github.com/radjkarl/importstar'
+__license__ = 'GPLv3'
+__description__ = __doc__
+__depencies__= [
+	]
+__classifiers__ = [
+		'Intended Audience :: Developers',
+		'Intended Audience :: Science/Research',
+		'Intended Audience :: Other Audience',
+		'License :: OSI Approved :: GNU General Public License (GPL)',
+		'Operating System :: OS Independent',
+		'Programming Language :: Python',
+		'Topic :: Software Development :: Libraries :: Python Modules',
+		]
