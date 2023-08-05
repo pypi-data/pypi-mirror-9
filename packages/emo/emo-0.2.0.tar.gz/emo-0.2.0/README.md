79f2aa694e575aa301564c0f4a93c51e817cf0b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:28 +0000

    Add package emo/0.2.0/emo-0.2.0.tar.gz

---
+++ packages/emo/emo-0.2.0.tar.gz/emo-0.2.0/README.md
@@ -0,0 +1,26 @@
+![emo. command line emoji search.](http://i.imgur.com/7JDtvNi.png)
+# emo
+
+Command line emoji search.
+
+# Installation
+
+    pip install emo
+
+Note: Waiting for a squatter to get off the name before I can publish to pip..
+
+# Usage
+
+    $ emo pray fire
+    🙏 🔥
+
+Emo can also copy straight to the clipboard:
+
+    $ emo tree fire cloud --copy
+    🌴 🔥 ☁
+
+There are also --list and --all arguments if you just want to see what's available.
+
+And that's all it does!
+
+Rich Jones 2014.
---
+++ packages/emo/emo-0.2.0.tar.gz/emo-0.2.0/emo/__init__.py
@@ -0,0 +1,90 @@
+#! /usr/bin/env python
+
+# I hate Python 3.
+from __future__ import print_function
+
+import argparse
+import xerox
+from code import emojiCodeDict
+
+def emoji_search(args):
+
+    words = args['query']
+    to_print = u''
+    
+    for search in words:
+        exact_search = ':' + search + ':'
+        exact = False
+        for key in emojiCodeDict.keys():
+            if exact_search == key:
+                if to_print:
+                    to_print = to_print + ' ' + emojiCodeDict[key]
+                else:
+                    to_print =  emojiCodeDict[key]
+                exact = True
+                break
+        if exact:
+            continue 
+        for key in emojiCodeDict.keys():
+            if search in key: 
+                if to_print:
+                    to_print = to_print + ' ' + emojiCodeDict[key]
+                else:
+                    to_print =  emojiCodeDict[key]
+                break
+
+    print(to_print)
+    if args['copy']:
+        xerox.copy(to_print)
+
+def list_all_emoji(copy=False):
+   
+    all_keys = emojiCodeDict 
+    keys = sorted(all_keys, key=lambda key: all_keys[key])
+    
+    to_copy = '' 
+    for word in keys:
+        print(word + ' ' + emojiCodeDict[word])
+        to_copy = to_copy + word + ' ' + emojiCodeDict[word] + '\n'        
+    if copy:
+        xerox.copy(to_copy)
+
+def dump_all_emoji(copy=False):
+    
+    all_keys = emojiCodeDict 
+    keys = sorted(all_keys, key=lambda key: all_keys[key])
+  
+    to_print = '' 
+    for word in keys:
+        to_print = to_print + ' ' + emojiCodeDict[word] 
+    print(to_print)
+    
+    if copy:
+       xerox.copy(to_print)
+
+def get_parser():
+    parser = argparse.ArgumentParser(description='Command line emoji search.')
+    parser.add_argument('query', metavar='QUERY', type=str, nargs='*',
+            help='the emoji to search for')
+    parser.add_argument('-c','--copy', help='copy result to the clipboard.', default=False, dest='copy', action='store_true')
+    parser.add_argument('-l','--list', help='list all of the available emoji.', default=False, dest='list', action='store_true')
+    parser.add_argument('-a','--all', help='dump all of the available emoji.', default=False, dest='all', action='store_true')
+    return parser
+
+def command_line_runner():
+    parser = get_parser()
+    args = vars(parser.parse_args())
+    if not args['query'] and not args['list'] and not args['all']:
+        parser.print_help()
+        return
+    if ['query']:
+        emoji_search(args)
+    
+    if args['list']:
+        list_all_emoji(args['copy'])
+    
+    if args['all']:
+        dump_all_emoji(args['copy'])
+
+if __name__ == '__main__':
+    command_line_runner()
