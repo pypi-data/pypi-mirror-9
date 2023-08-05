5f6daf9bea02c02a1454d6b9b4923f42ea790b28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:50 +0000

    Add package supersmoother/0.2/supersmoother-0.2.tar.gz

---
+++ packages/supersmoother/supersmoother-0.2.tar.gz/supersmoother-0.2/supersmoother/tests/test_utils.py
@@ -0,0 +1,221 @@
+from .. import utils
+
+import numpy as np
+from numpy.testing import assert_allclose, assert_, assert_equal, assert_raises
+
+
+def test_iterable():
+    x = [1, 2, 3]
+    y = 1
+    assert_(utils.iterable(x))
+    assert_(not utils.iterable(y))
+
+
+def test_multiinterp(rseed=0, N=100):
+    """Test of the multinterp function"""
+    rng = np.random.RandomState(rseed)
+
+    def check_match(k):
+        x = np.sort(rng.rand(k))
+        y = rng.rand(k, N)
+        xquery = rng.rand(N)
+
+        res1 = utils.multinterp(x, y, xquery, slow=False)
+        res2 = utils.multinterp(x, y, xquery, slow=True)
+        print(abs(res1 - res2))
+        assert_allclose(res1, res2)
+
+    for k in 3, 4, 5:
+        yield check_match, k
+
+
+def test_setattr_context():
+    """Test of the setattr_context() function"""
+    class Foo(object):
+        pass
+    f = Foo()
+    f.attr = "abc"
+    with utils.setattr_context(f, attr="123"):
+        assert_equal(f.attr, "123")
+    assert_equal(f.attr, "abc")
+
+    try:
+        with utils.setattr_context(f, attr="123"):
+            raise ValueError()
+    except:
+        pass
+    assert_equal(f.attr, "abc")
+
+
+def test_validate_inputs_nosort(N=10, rseed=0):
+    """Test input validation without sorting"""
+    rng = np.random.RandomState(rseed)
+    t, y = rng.rand(2, N)
+    dy = 1
+
+    t_, y_, dy_ = utils.validate_inputs(t, y, dy)
+    assert_allclose(t, t_)
+    assert_allclose(y, y_)
+    assert_allclose(dy, dy_)
+
+
+def test_validate_inputs_sort(N=10, rseed=0):
+    """Test input validation with sorting"""
+    rng = np.random.RandomState(rseed)
+    t, y = rng.rand(2, N)
+    dy = 1
+
+    t_, y_, dy_ = utils.validate_inputs(t, y, dy, sort_by=t)
+    isort = np.argsort(t)
+    assert_allclose(t[isort], t_)
+    assert_allclose(y[isort], y_)
+    assert_allclose(dy, dy_)
+
+
+def test_validate_inputs_fail():
+    rng = np.random.RandomState(0)
+    t, y = rng.rand(2, 10)
+    dy = 1
+
+    # bad keyword argument
+    assert_raises(ValueError, utils.validate_inputs, t, y, dy,
+                  blah='blah')
+
+    # non-sortable array
+    assert_raises(ValueError, utils.validate_inputs, 1, 1, 1)
+
+    # bad sort array
+    assert_raises(ValueError, utils.validate_inputs, t, y, dy, sort_by=[1])
+
+
+def test_windowed_sum_fixed(N=10, span=5, rseed=0):
+    """Test the windowed sum for a fixed-span"""
+    rng = np.random.RandomState(rseed)
+    data = rng.rand(3, N)
+
+    for subtract_mid in [True, False]:
+        assert_allclose(utils.windowed_sum(*data, span=span,
+                                           subtract_mid=subtract_mid),
+                        utils.windowed_sum(*data, span=span, slow=True,
+                                           subtract_mid=subtract_mid))
+
+
+def test_windowed_sum_variable(N=10, rseed=0):
+    """Test the windowed sum for a variable span"""
+    rng = np.random.RandomState(rseed)
+    span = rng.randint(3, 6, N)
+    data = np.random.random((3, N))
+
+    for subtract_mid in [True, False]:
+        assert_allclose(utils.windowed_sum(*data, span=span,
+                                           subtract_mid=subtract_mid),
+                        utils.windowed_sum(*data, span=span, slow=True,
+                                           subtract_mid=subtract_mid))
+
+
+def test_windowed_sum_bad_kwargs():
+    rng = np.random.RandomState(0)
+    span = rng.randint(3, 6, 10)
+    data = np.random.random((3, 10))
+
+    # no span specified
+    assert_raises(ValueError, utils.windowed_sum, *data)
+
+    # non-positive span
+    assert_raises(ValueError, utils.windowed_sum, *data, span=0)
+
+    # nonsense keyword argument
+    assert_raises(ValueError, utils.windowed_sum, *data, gobbledeygook='yay')
+
+    for subtract_mid in [True, False]:
+        assert_allclose(utils.windowed_sum(*data, span=span,
+                                           subtract_mid=subtract_mid),
+                        utils.windowed_sum(*data, span=span, slow=True,
+                                           subtract_mid=subtract_mid))
+
+
+def make_linear(N=100, err=1E-6, rseed=None):
+    rng = np.random.RandomState(rseed)
+    t = 10 * rng.rand(N)
+    y = t + err * rng.randn(N)
+    return t, y, err
+
+
+def test_perfect_sine(N=100, rseed=0):
+    """Test approximate recovery of a sine wave with no noise"""
+    t = np.linspace(0, 10, 200)
+    y = np.sin(t)
+    dy = 1
+
+    for method in [utils.moving_average_smooth, utils.linear_smooth]:
+        yfit = method(t, y, dy, span=5)
+        assert_allclose(y[3:-3], yfit[3:-3], atol=0.005)
+
+
+def test_constant_data(N=100, rseed=0):
+    """Test linear & moving average smoothers on constant data"""
+    rng = np.random.RandomState(rseed)
+    t = 10 * rng.rand(N)
+    y = 1.0
+    dy = 1.0
+    span = 5
+
+    for method in [utils.moving_average_smooth, utils.linear_smooth]:
+        yfit = method(t, y, dy, span=5)
+        assert_allclose(y, yfit)
+
+
+def test_equal_spaced_linear_data(N=100, rseed=0):
+    """Test linear & moving average smoothers on equally-spaced linear data"""
+    t = np.linspace(0, 10, N)
+    y = 1 + 2 * t
+    dy = 1
+    span = 5
+
+    for method in [utils.moving_average_smooth, utils.linear_smooth]:
+        yfit = method(t, y, dy, span=5)
+        assert_allclose(y[3:-3], yfit[3:-3])
+
+
+def test_random_linear_data(N=100, rseed=0):
+    """Test linear smoother on linear data"""
+    rng = np.random.RandomState(rseed)
+    t = 10 * rng.rand(N)
+    y = 1 + 2 * t
+    dy = 1.0
+    span = 5
+
+    yfit = utils.linear_smooth(t, y, dy, span=5)
+    assert_allclose(y, yfit)
+
+
+def test_t_vs_t_out(N=100, rseed=0):
+    """Test two means of arriving at the same answer"""
+    rng = np.random.RandomState(rseed)
+    t = np.sort(10 * rng.rand(N))
+    y = np.sin(t)
+    dy = 1.0
+    span = 5
+
+    for method in [utils.linear_smooth, utils.moving_average_smooth]:
+        yfit1 = method(t, y, dy, span=5, cv=False)
+        yfit2 = method(t, y, dy, span_out=5, t_out=t, cv=False)
+        assert_allclose(yfit1, yfit2)
+
+
+def test_smooth_assertions():
+    """Test errors and assertions in linear_smooth and moving_average_smooth"""
+    rng = np.random.RandomState(0)
+    t = 10 * rng.rand(10)
+    y = np.sin(t)
+    dy = 1.0
+
+    for method in [utils.linear_smooth, utils.moving_average_smooth]:
+        # No span set
+        assert_raises(ValueError, method, t, y, dy)
+
+        # Both spans set
+        assert_raises(ValueError, method, t, y, dy, span=1, span_out=1)
+
+        # span_out set without t_out
+        assert_raises(ValueError, method, t, y, dy, span_out=1)
---
+++ packages/supersmoother/supersmoother-0.2.tar.gz/supersmoother-0.2/supersmoother/utils.py
@@ -0,0 +1,292 @@
+from __future__ import division, print_function
+
+from contextlib import contextmanager
+
+import numpy as np
+
+
+@contextmanager
+def setattr_context(obj, **kwargs):
+    """
+    Context manager to temporarily change the values of object attributes
+    while executing a function.
+
+    Example
+    -------
+    >>> class Foo: pass
+    >>> f = Foo(); f.attr = 'hello'
+    >>> with setattr_context(f, attr='goodbye'):
+    ...     print(f.attr)
+    goodbye
+    >>> print(f.attr)
+    hello
+    """
+    old_kwargs = dict([(key, getattr(obj, key)) for key in kwargs])
+    [setattr(obj, key, val) for key, val in kwargs.items()]
+    try:
+        yield
+    finally:
+        [setattr(obj, key, val) for key, val in old_kwargs.items()]
+
+
+def iterable(obj):
+    """Utility to check if object is iterable"""
+    try:
+        iter(obj)
+    except:
+        return False
+    else:
+        return True
+
+
+def validate_inputs(*arrays, **kwargs):
+    """Validate input arrays
+
+    This checks that
+    - Arrays are mutually broadcastable
+    - Broadcasted arrays are one-dimensional
+
+    Optionally, arrays are sorted according to the ``sort_by`` argument.
+
+    Parameters
+    ----------
+    *args : ndarrays
+        All non-keyword arguments are arrays which will be validated
+    sort_by : array
+        If specified, sort all inputs by the order given in this array.
+    """
+    arrays = np.broadcast_arrays(*arrays)
+    sort_by = kwargs.pop('sort_by', None)
+
+    if kwargs:
+        raise ValueError("unrecognized arguments: {0}".format(kwargs.keys()))
+
+    if arrays[0].ndim != 1:
+        raise ValueError("Input arrays should be one-dimensional.")
+
+    if sort_by is not None:
+        isort = np.argsort(sort_by)
+        if isort.shape != arrays[0].shape:
+            raise ValueError("sort shape must equal array shape.")
+        arrays = tuple([a[isort] for a in arrays])
+    return arrays
+
+
+def windowed_sum(*arrays, **kwargs):
+    """Compute the windowed sum of the given arrays
+
+    Parameters
+    ----------
+    *args : array_like
+        Each additional argument is an array that will be windowed
+    span : int or array of ints
+        The span to use for the sum at each point
+    subtract_mid : boolean
+        If true, then subtract the middle value from each sum
+    slow : boolean
+        If true, use a slow method to compute the result. This is
+        primarily useful for testing and validation.
+    indices : array
+        the indices of the center of the desired windows. If provided,
+        it must be broadcastable with the span.
+
+    Returns
+    -------
+    arrays : tuple of ndarrays
+        arrays containing the windowed sum of each input array
+    """
+    span = kwargs.pop('span', None)
+    subtract_mid = kwargs.pop('subtract_mid', False)
+    slow = kwargs.pop('slow', False)
+    indices = kwargs.pop('indices', None)
+
+    if kwargs:
+        raise ValueError("Unrecognized keywords: {0}".format(kwargs.keys()))
+
+    if span is None:
+        raise ValueError("Must provide a positive integer span")
+
+    span = np.asarray(span, dtype=int)
+    if not np.all(span > 0):
+        raise ValueError("span values must be positive")
+
+    if indices is not None:
+        span, indices = np.broadcast_arrays(span, indices)
+
+    if slow:
+        # Slow fixed/variable span
+        results = []
+        for array in map(np.asarray, arrays):
+            assert array.ndim == 1
+            span, array = np.broadcast_arrays(span, array)
+            result = (array[max(0, i - s // 2): i - s // 2 + s].sum()
+                      for i, s, in enumerate(span))
+            results.append(np.fromiter(result,
+                                       dtype=array.dtype,
+                                       count=len(array)))
+    elif span.ndim == 0:
+        # Fast fixed-span
+        window = np.ones(span)
+        results = [np.convolve(a, window, mode='same') for a in arrays]
+    else:
+        # Fast variable-span
+        if indices is None:
+            indices = np.arange(len(span))
+        mins = np.asarray(indices) - span // 2
+
+        results = []
+        for a in map(np.asarray, arrays):
+            ranges = np.vstack([np.maximum(0, mins),
+                                np.minimum(len(a), mins + span)]).ravel('F')
+            results.append(np.add.reduceat(np.append(a, 0), ranges)[::2])
+
+    if subtract_mid:
+        results = (r - a for r, a in zip(results, arrays))
+
+    return tuple(results)
+
+
+def moving_average_smooth(t, y, dy, span=None, cv=True,
+                          t_out=None, span_out=None):
+    """Perform a moving-average smooth of the data
+
+    Parameters
+    ----------
+    t, y, dy : array_like
+        time, value, and error in value of the input data
+    span : array_like
+        the integer spans of the data
+    cv : boolean (default=True)
+        if True, treat the problem as a cross-validation, i.e. don't use
+        each point in the evaluation of its own smoothing.
+    t_out : array_like (optional)
+        the output times for the moving averages
+    span_out : array_like (optional)
+        the spans associated with the output times t_out
+
+    Returns
+    -------
+    y_smooth : array_like
+        smoothed y values at each time t (or t_out)
+    """
+    t, y, dy = validate_inputs(t, y, dy, sort_by=t)
+
+    if span_out is not None:
+        if t_out is None:
+            raise ValueError("Must specify t_out when span_out is given")
+        if span is not None:
+            raise ValueError("Must specify only one of span, span_out")
+        span, t_out = np.broadcast_arrays(span_out, t_out)
+        indices = np.searchsorted(t, t_out)
+    elif span is None:
+        raise ValueError("Must specify either span_out or span")
+    else:
+        indices = None
+
+    w = dy ** -2
+    w, yw = windowed_sum(w, y * w, span=span, subtract_mid=cv,
+                         indices=indices)
+
+    if t_out is None or span_out is not None:
+        return yw / w
+    else:
+        i = np.minimum(len(t) - 1, np.searchsorted(t, t_out))
+        return yw[i] / w[i]
+
+
+def linear_smooth(t, y, dy, span=None, cv=True,
+                  t_out=None, span_out=None):
+    """Perform a linear smooth of the data
+
+    Parameters
+    ----------
+    t, y, dy : array_like
+        time, value, and error in value of the input data
+    span : array_like
+        the integer spans of the data
+    cv : boolean (default=True)
+        if True, treat the problem as a cross-validation, i.e. don't use
+        each point in the evaluation of its own smoothing.
+    t_out : array_like (optional)
+        the output times for the moving averages
+    span_out : array_like (optional)
+        the spans associated with the output times t_out
+
+    Returns
+    -------
+    y_smooth : array_like
+        smoothed y values at each time t or t_out
+    """
+    t_input = t
+    t, y, dy = validate_inputs(t, y, dy, sort_by=t)
+
+    if span_out is not None:
+        if t_out is None:
+            raise ValueError("Must specify t_out when span_out is given")
+        if span is not None:
+            raise ValueError("Must specify only one of span, span_out")
+        span, t_out = np.broadcast_arrays(span_out, t_out)
+        indices = np.searchsorted(t, t_out)
+    elif span is None:
+        raise ValueError("Must specify either span_out or span")
+    else:
+        indices = None
+
+    w = dy ** -2
+    w, tw, yw, ttw, tyw = windowed_sum(w, t * w, y * w, t * t * w, t * y * w,
+                                       span=span, indices=indices,
+                                       subtract_mid=cv)
+    denominator = (w * ttw - tw * tw)
+    slope = (tyw * w - tw * yw)
+    intercept = (ttw * yw - tyw * tw)
+
+    if t_out is None:
+        return (slope * t_input + intercept) / denominator
+    elif span_out is not None:
+        return (slope * t_out + intercept) / denominator
+    else:
+        i = np.minimum(len(t) - 1, np.searchsorted(t, t_out))
+        return (slope[i] * t_out + intercept[i]) / denominator[i]
+
+
+def multinterp(x, y, xquery, slow=False):
+    """Multiple linear interpolations
+
+    Parameters
+    ----------
+    x : array_like, shape=(N,)
+        sorted array of x values
+    y : array_like, shape=(N, M)
+        array of y values corresponding to each x value
+    xquery : array_like, shape=(M,)
+        array of query values
+    slow : boolean, default=False
+        if True, use slow method (used mainly for unit testing)
+
+    Returns
+    -------
+    yquery : ndarray, shape=(M,)
+        The interpolated values corresponding to each x query.
+    """
+    x, y, xquery = map(np.asarray, (x, y, xquery))
+    assert x.ndim == 1
+    assert xquery.ndim == 1
+    assert y.shape == x.shape + xquery.shape
+
+    # make sure xmin < xquery < xmax in all cases
+    xquery = np.clip(xquery, x.min(), x.max())
+
+    if slow:
+        from scipy.interpolate import interp1d
+        return np.array([interp1d(x, y)(xq) for xq, y in zip(xquery, y.T)])
+    elif len(x) == 3:
+        # Most common case: use a faster approach
+        yq_lower = y[0] + (xquery - x[0]) * (y[1] - y[0]) / (x[1] - x[0])
+        yq_upper = y[1] + (xquery - x[1]) * (y[2] - y[1]) / (x[2] - x[1])
+        return np.where(xquery < x[1], yq_lower, yq_upper)
+    else:
+        i = np.clip(np.searchsorted(x, xquery, side='right') - 1,
+                    0, len(x) - 2)
+        j = np.arange(len(xquery))
+        return y[i, j] + ((xquery - x[i]) *
+                          (y[i + 1, j] - y[i, j]) / (x[i + 1] - x[i]))
