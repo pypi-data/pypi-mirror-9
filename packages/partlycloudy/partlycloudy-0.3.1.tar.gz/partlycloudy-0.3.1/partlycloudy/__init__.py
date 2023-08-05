7b11ee3a65349e9b35a283526dc101bb3247561a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:08 +0000

    Add package partlycloudy/0.3.1/partlycloudy-0.3.1.tar.gz

---
+++ packages/partlycloudy/partlycloudy-0.3.1.tar.gz/partlycloudy-0.3.1/partlycloudy/__init__.py
@@ -0,0 +1,33 @@
+"""
+Special thanks to @manitou48 and @jasonkuhrt for their help and support
+"""
+
+
+import requests
+import re
+import json
+
+class Bit:
+    def __init__(self, auth_token, device_id):
+        self.token = auth_token
+        self.id = device_id
+        self.headers = {"Authorization": "Bearer " + self.token, "Accept": "application/vnd.littlebits.v2+json"}
+
+    def output(self, pct, dur):
+        r = requests.post("https://api-http.littlebitscloud.cc/devices/" + self.id + "/output", data={"percent": pct, "duration_ms": dur}, headers=self.headers)
+        return r.text
+
+    def raw_stream(self):
+        #Use this for raw streaming input
+        for line in requests.get("https://api-http.littlebitscloud.cc/devices/" + self.id + "/input", headers=self.headers, stream=True).iter_lines():
+            if line:
+                yield json.loads(line[5:])
+
+    def stream(self):
+        #Use this for raw streaming input
+        for line in requests.get("https://api-http.littlebitscloud.cc/devices/" + self.id + "/input", headers=self.headers, stream=True).iter_lines():
+            if line:
+                yield int(json.loads(line[5:])["payload"]["percent"])
+
+    def input(self):
+        return self.stream().next()
