3f098c2217b56157f00289a995fb24dcce8bbf74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:56 +0000

    Add package twentytab-sortable/0.9/twentytab-sortable-0.9.tar.gz

---
+++ packages/twentytab-sortable/twentytab-sortable-0.9.tar.gz/twentytab-sortable-0.9/sortable/__init__.py
@@ -0,0 +1,3 @@
+VERSION = (0, 9)
+__version__ = '.'.join(map(str, VERSION))
+DATE = "2015-02-05"
---
+++ packages/twentytab-sortable/twentytab-sortable-0.9.tar.gz/twentytab-sortable-0.9/sortable/admin.py
@@ -0,0 +1,46 @@
+from django.contrib import admin
+from django.conf import settings
+from django.contrib.admin import TabularInline, StackedInline
+
+
+class PositionAdmin(admin.ModelAdmin):
+    """
+    Abstract admin option class for PositionModel
+    """
+    list_display = ('position', 'pk')
+    list_editable = ('position',)
+    list_display_links = ('pk',)
+    ordering = ('position',)
+    exclude = ('position',)
+
+    class Media:
+        js = (settings.JQUERY_LIB,
+              settings.JQUERYUI_LIB,
+              u'{}sortable/js/admin-list-reorder.js'.format(settings.STATIC_URL),)
+
+    class Meta:
+        abstract = True
+
+
+class SortableStackedInline(StackedInline):
+    """Adds necessary media files to regular Django StackedInline"""
+    extra = 0
+    ordering = ('position',)
+
+    class Media:
+        css = {u"all": (u"{}sortable/css/inline-reorder.css".format(settings.STATIC_URL),)}
+        js = (settings.JQUERY_LIB,
+              settings.JQUERYUI_LIB,
+              u'{}sortable/js/inline-reorder.js'.format(settings.STATIC_URL),)
+
+
+class SortableTabularInline(TabularInline):
+    """Adds necessary media files to regular Django TabularInline"""
+    extra = 0
+    ordering = ('position',)
+
+    class Media:
+        css = {u"all": (u"{}sortable/css/inline-reorder.css".format(settings.STATIC_URL),)}
+        js = (settings.JQUERY_LIB,
+              settings.JQUERYUI_LIB,
+              u'{}sortable/js/inline-reorder.js'.format(settings.STATIC_URL),)
