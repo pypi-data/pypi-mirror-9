68f537fab8e3f5968fa7702878143d7ced2b7ae8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:16 +0000

    Add package csvbar/1.1/csvbar-1.1-py2.py3-none-any.whl

---
+++ packages/csvbar/csvbar-1.1-py2.py3-none-any.whl/csvbar-1.1.dist-info/DESCRIPTION.rst
@@ -0,0 +1,60 @@
+CSV Bar
+=======
+
+Draw bar charts from CSV files in the terminal.
+
+Requires either version 2 or 3 of [Python] (https://www.python.org/), including `pip`.
+
+
+Installing
+----------
+
+Install with Pip: `pip install csvbar`.
+
+
+Usage
+-----
+
+Say you have a CSV file such as:
+
+```csv
+Donor,Value
+Friendly Society,49428.84
+Limited Liability Partnership,96122.93
+Other,233016.58
+Registered Political Party,333058.01
+Unincorporated Association,2656919.98
+Company,3266514.88
+Individual,10191197.0
+Trade Union,42328877.05
+```
+
+You can then:
+
+```bash
+$ csvbar data.csv
+
+Friendly Society               ▌ 49,428.84
+Limited Liability Partnership  ▌ 96,122.93
+Other                          ▌ 233,016.58
+Registered Political Party     ▌ 333,058.01
+Unincorporated Association     ███ 2,656,919.98
+Company                        ███ 3,266,514.88
+Individual                     ████████████ 10,191,197.00
+Trade Union                    ██████████████████████████████████████████████████ 42,328,877.05
+
+```
+
+It also accepts data on STDIN, useful if you're using something like [CSV Kit] (https://github.com/onyxfish/csvkit).
+
+
+Inspiration
+-----------
+
+There are similar tools:
+
+ * [data_hacks] (https://github.com/bitly/data_hacks), by [Bitly] (https://bitly.com/)
+ * [termgraph] (https://github.com/mkaz/termgraph), by [Marcus Kazmierczak] (https://twitter.com/mkaz)
+ * [spark] (https://github.com/holman/spark), by [Zach Holman] (https://twitter.com/holman)
+
+
---
+++ packages/csvbar/csvbar-1.1-py2.py3-none-any.whl/csvbar.py
@@ -0,0 +1,58 @@
+# coding: utf-8
+
+# csvbar -- draw bar graphs from CSV files in the terminal
+# https://github.com/tbij/csvbar
+
+import argparse
+import locale
+import csv
+import sys
+import os
+
+locale.setlocale(locale.LC_ALL, '')
+
+def main():
+    args = arguments()
+    labels, data = read(args['FILE'])
+    display(labels, data, args['width'])
+
+def arguments():
+    parser = argparse.ArgumentParser(description='draw bar graphs from CSV files in the terminal')
+    parser.add_argument('FILE', nargs='?', default='-', help='the CSV file to operate on -- if omitted, will accept input on STDIN')
+    parser.add_argument('--width', type=int, default=50, help='width of graph in characters (default is 50)')
+    return vars(parser.parse_args())
+
+def read(filename):
+    labels = []
+    data = []
+    if not os.path.isfile(filename): sys.exit(filename + ': no such file')
+    file = sys.stdin if filename == '-' else open(filename, 'rb')
+    reader = csv.reader(file)
+    headers = next(reader) # skip these
+    for line in reader:
+        labels.append(line[0])
+        try: data.append(float(line[1]))
+        except ValueError: sys.exit(line[1] + ' is not a number')
+    file.close()
+    return labels, data
+
+def display(labels, data, width):
+    full_tick = '█'
+    half_tick = '▌'
+    total = sum(data)
+    step = max(data) / width
+    spacing = max(len(label) for label in labels)
+    print('')
+    for i in range(len(data)):
+        label = labels[i]
+        count = data[i]
+        padding = ' ' * (spacing - len(label) + 2)
+        bar = half_tick if count < step else int(count / step) * full_tick
+        number = locale.format('%.2f', count, grouping=True)
+        percentage = locale.format('%.2f', count / total * 100, grouping=True)
+        print(label + padding + bar + ' ' + number + ' (' + percentage + '%)')
+    print('')
+    print('Total: ' + locale.format('%.2f', total, grouping=True))
+
+if __name__ == '__main__':
+    main()
