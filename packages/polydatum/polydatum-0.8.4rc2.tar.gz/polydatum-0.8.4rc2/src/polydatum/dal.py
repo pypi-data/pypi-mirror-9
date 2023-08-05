82a0492e3fef9a3f2802b014ee3a3e98cc6324c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:38 +0000

    Add package polydatum/0.8.4rc2/polydatum-0.8.4rc2.tar.gz

---
+++ packages/polydatum/polydatum-0.8.4rc2.tar.gz/polydatum-0.8.4rc2/src/polydatum/dal.py
@@ -0,0 +1,167 @@
+from contextlib import contextmanager
+from .context import DataAccessContext
+from polydatum.errors import AlreadyExistsException
+from polydatum.util import is_generator
+from .resources import ResourceManager
+from .context import _ctx_stack
+
+
+class DataAccessLayer(object):
+    """
+    Gives you access to a DataManager's services.
+    """
+
+    def __init__(self, data_manager):
+        self._services = {}
+        self._data_manager = data_manager
+
+    def register_services(self, **services):
+        """
+        Register Services that can be accessed by this DAL. Upon
+        registration, the service is set up.
+
+        :param **services: Keyword arguments where the key is the name
+          to register the Service as and the value is the Service.
+        """
+        for key, service in services.items():
+            if key in self._services:
+                raise AlreadyExistsException('A Service for {} is already registered.'.format(key))
+
+            self._init_service(key, service)
+        return self
+
+    def replace_service(self, key, service):
+        """
+        Replace a Service with another. Usually this is a bad
+        idea but is often done in testing to replace a Service
+        with a mock version. It's also used for practical
+        reasons if you need to swap out services for different
+        framework implementations (ex: Greenlet version vs
+        threaded)
+
+        :param key: Name of service
+        :param service: Service
+        """
+        return self._init_service(key, service)
+
+    def _init_service(self, key, service):
+        service.setup(self._data_manager)
+        self._services[key] = service
+        return service
+
+    def __getattr__(self, name):
+        assert self._data_manager.get_active_context(), 'A DataAccessContext must be started to access the DAL.'
+        return self._services[name]
+
+    def __getitem__(self, path):
+        """
+        Get a service method by dot notation path. Useful for
+        serializing DAL methods as strings.
+
+        If the first part of the path is "dal", it is ignored.
+
+        Example::
+
+            dal['myservice.get'](my_id)
+        """
+        paths = path.split('.')
+        p = paths.pop(0)
+        if p == 'dal':
+            p = paths.pop(0)
+        loc = self._services[p]
+        while 1:
+            try:
+                p = paths.pop(0)
+            except IndexError:
+                break
+            else:
+                loc = getattr(loc, p)
+        return loc
+
+
+class DataManager(object):
+    """
+    Registry for Services, Resources, and other DAL objects.
+    """
+
+    def __init__(self, resource_manager=None):
+        if not resource_manager:
+            resource_manager = ResourceManager(self)
+
+        self._resource_manager = resource_manager
+        self._dal = DataAccessLayer(self)
+        self._middleware = []
+
+        # TODO Make _ctx_stack only exist on the DataManager
+        self.ctx_stack = _ctx_stack
+
+    def register_context_middleware(self, *middleware):
+        """
+        :param middleware: Middleware in order of execution
+        """
+        for m in middleware:
+            if not is_generator(m):
+                raise Exception('Middleware {} must be a Python generator callable.'.format(m))
+
+        self._middleware.extend(middleware)
+
+    def get_middleware(self, context):
+        """
+        Returns all middleware in order of execution
+
+        :param context: The DataAccessContext. You could override the DataManager
+            and return different middleware based on the context here.
+        """
+        return self._middleware
+
+    def register_resources(self, **resources):
+        """
+        Register Resources with the ResourceManager.
+        """
+        self._resource_manager.register_resources(**resources)
+
+    def replace_resource(self, key, resource):
+        """
+        Replace a Resources on the ResourceManager.
+        """
+        self._resource_manager.replace_resource(key, resource)
+
+    def register_services(self, **services):
+        """
+        Register Services with the DataAccessLayer
+        """
+        self._dal.register_services(**services)
+
+    def replace_service(self, key, service):
+        """
+        Replace a Service on the DataAccessLayer
+        """
+        self._dal.replace_service(key, service)
+
+    def get_resource(self, name):
+        if name in self._resource_manager:
+            return self._resource_manager[name]
+
+    def get_dal(self):
+        return self._dal
+
+    def context(self, meta=None):
+        return DataAccessContext(self, meta=meta)
+
+    def get_active_context(self):
+        """
+        Safely checks if there's a context active
+        and returns it
+        """
+        if self.ctx_stack.top:
+            return self.ctx_stack.top
+
+    @contextmanager
+    def dal(self, meta=None):
+        """
+        Start a new DataAccessContext.
+
+        :returns: DataAccessLayer for this DataManager
+        """
+        with self.context(meta=meta):
+            yield self._dal
---
+++ packages/polydatum/polydatum-0.8.4rc2.tar.gz/polydatum-0.8.4rc2/src/tests/test_services.py
@@ -0,0 +1,160 @@
+from polydatum import DataManager, Service
+from polydatum.resources import Resource
+from uuid import uuid4
+
+
+class MockStoreConnection(object):
+    def __init__(self, store):
+        self._store = store
+
+    def get(self, item_id):
+        return self._store._data.get(item_id, None)
+
+    def delete(self, item_id):
+        if item_id in self._store._data:
+            del self._store._data[item_id]
+            return True
+        return False
+
+    def save(self, record):
+        if not record.get('id', None):
+            record['id'] = str(uuid4())
+
+        self._store._data[record['id']] = record
+        return record
+
+    def close(self):
+        self._store._pool.append(self)
+
+
+class MockStore(object):
+    def __init__(self):
+        self._data = {}
+
+        # Simulate a connection pool
+        self._pool = [
+            MockStoreConnection(self),
+            MockStoreConnection(self),
+            MockStoreConnection(self),
+        ]
+
+    def connect(self):
+        # Get a connection from a pool
+        return self._pool.pop()
+
+
+class MockStoreResource(Resource):
+    def __init__(self, store):
+        super(MockStoreResource, self).__init__()
+        self._store = store
+
+    def __call__(self, context):
+        """
+        Yield a connection from the pool and closed when done.
+        """
+        connection = self._store.connect()
+        try:
+            yield connection
+        finally:
+            connection.close()
+
+
+class UserService(Service):
+    @property
+    def _store(self):
+        return self._ctx.user_db
+
+    def get(self, id):
+        return self._store.get(id)
+
+    def update(self, id, user):
+        user['id'] = id
+        self._store.save(user)
+        return user
+
+    def delete(self, id):
+        self._dal.users.profile.delete(id)
+        return self._store.delete(id)
+
+    def create(self, user):
+        self._store.save(user)
+        return user
+
+
+class UserProfileService(Service):
+    @property
+    def _store(self):
+        return self._ctx.user_profile_db
+
+    def get(self, user_id):
+        return self._store.get(user_id)
+
+    def update(self, user_id, profile):
+        profile['id'] = user_id
+        self._store.save(profile)
+        return profile
+
+    def delete(self, user_id):
+        return self._store.delete(user_id)
+
+
+def get_dam():
+    data_manager = DataManager()
+    data_manager.register_services(
+        users=UserService().register_services(
+            profile=UserProfileService()
+        )
+    )
+    data_manager.register_resources(
+        user_db=MockStoreResource(MockStore()),
+        user_profile_db=MockStoreResource(MockStore()),
+    )
+    return data_manager
+
+
+def test_service():
+    """
+    Verify simple service CRUD
+    """
+    data_manager = get_dam()
+    with data_manager.dal() as dal:
+        user = {
+            'name': str(uuid4())
+        }
+
+        saved_user = dal.users.create(user)
+        assert saved_user['id']
+        assert saved_user['name'] == user['name']
+
+        got_user = dal.users.get(saved_user['id'])
+        assert got_user['id'] == saved_user['id']
+
+        deleted = dal.users.delete(saved_user['id'])
+        assert deleted
+
+
+def test_sub_service():
+    """
+    Verify a sub-service is callable
+    """
+    data_manager = get_dam()
+    with data_manager.dal() as dal:
+        user = {
+            'name': str(uuid4())
+        }
+
+        user = dal.users.create(user)
+        profile = dal.users.profile.get(user['id'])
+        assert not profile
+
+        uuid = str(uuid4())
+        profile = dal.users.profile.update(user['id'], dict(uuid=uuid))
+        assert profile
+        assert profile['id'] == user['id']
+        assert profile['uuid'] == uuid
+
+        deleted = dal.users.delete(user['id'])
+        assert deleted
+
+        profile = dal.users.profile.get(user['id'])
+        assert not profile
