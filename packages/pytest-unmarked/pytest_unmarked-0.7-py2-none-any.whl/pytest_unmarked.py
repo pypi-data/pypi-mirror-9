d6bf13899cb3a410dbd80af71e6f70c7ef855e16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:33 +0000

    Add package pytest-unmarked/0.7/pytest_unmarked-0.7-py2-none-any.whl

---
+++ packages/pytest-unmarked/pytest_unmarked-0.7-py2-none-any.whl/pytest_unmarked.py
@@ -0,0 +1,44 @@
+from _pytest.mark import matchmark
+import py
+
+
+def pytest_addoption(parser):
+    group = parser.getgroup('Unmarked', 'Unmarked')
+    group._addoption('--unmarked',
+                     action="store_true", dest="unmarked", default=False,
+                     help="Run unmarked tests.")
+
+
+def pytest_cmdline_main(config):
+    if config.option.markers:
+        config.do_configure()
+        tw = py.io.TerminalWriter()
+        for line in config.getini("markers"):
+            name, rest = line.split(":", 1)
+            tw.write("@pytest.mark.%s:" % name, bold=True)
+            tw.line(rest)
+            tw.line()
+        config.do_unconfigure()
+        return 0
+pytest_cmdline_main.tryfirst = True
+
+
+def pytest_collection_modifyitems(items, config):
+    matchexpr = config.option.markers
+    print config.getini("markers")
+    print config.option.markers
+    if not matchexpr:
+        return
+
+    remaining = []
+    deselected = []
+    for colitem in items:
+        if matchexpr:
+            if matchmark(colitem, matchexpr):
+                deselected.append(colitem)
+                continue
+        remaining.append(colitem)
+
+    if deselected:
+        config.hook.pytest_deselected(items=deselected)
+        items[:] = remaining
