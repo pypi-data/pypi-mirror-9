65cea8fb82000dd2fd4c7eeb7eb735f17dc210eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:45 +0000

    Add package django-ormcache/1.0/django-ormcache-1.0.tar.gz

---
+++ packages/django-ormcache/django-ormcache-1.0.tar.gz/django-ormcache-1.0/setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+import os
+import sys
+
+from setuptools import setup
+
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py register sdist upload')
+    os.system('python setup.py register bdist_wheel upload')
+    sys.exit()
+
+
+readme_text = open("README.rst", "r").read()
+
+setup(
+    name="django-ormcache",
+    version="1.0",
+    description="ORM cache for Django",
+    license="MIT",
+    keywords="cache django",
+    author="Corey Farwell",
+    author_email="coreyf@rwell.org",
+    maintainer="Corey Farwell",
+    maintainer_email="coreyf@rwell.org",
+    url="https://github.com/educreations/django-ormcache",
+    long_description=readme_text,
+    packages=["ormcache"],
+    package_dir={"ormcache": "ormcache"},
+    install_requires=['Django>=1.6'],
+    classifiers=[
+        'Framework :: Django',
+        "Intended Audience :: Developers",
+        'License :: OSI Approved :: MIT License',
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Software Development',
+    ]
+)
