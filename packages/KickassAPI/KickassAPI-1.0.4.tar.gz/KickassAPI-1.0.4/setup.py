e96f47a36b7eeb04446bd249c7088abdbcb9bf8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:34 +0000

    Add package KickassAPI/1.0.4/KickassAPI-1.0.4.tar.gz

---
+++ packages/KickassAPI/KickassAPI-1.0.4.tar.gz/KickassAPI-1.0.4/setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+URL = 'https://github.com/FEE1DE4D/KickassAPI'
+setup(
+    name='KickassAPI',
+    version='1.0.4',
+    py_modules=['KickassAPI'],
+
+    author='fm4d',
+    author_email='m@fm4d.net',
+
+    url=URL,
+    description='Python API for kickass.to',
+    license='GPLv2+',
+
+    long_description="Documentation can be found here " + URL,
+    
+     install_requires=[
+        'setuptools',
+        'pyquery',
+        'requests',
+    ],
+
+    classifiers=[
+        "License :: OSI Approved :: GNU General Public License v2 or later ("
+        "GPLv2+)",
+
+        "Programming Language :: Python :: 2.7",
+
+        "Development Status :: 3 - Alpha",
+
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Multimedia",
+        "Topic :: Software Development :: Libraries :: Python Modules"
+    ],
+)
