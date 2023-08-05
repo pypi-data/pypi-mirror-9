dce5692aa369cb0ab5b6be01ae499bc73083c923
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:39 +0000

    Add package codetimer/0.1/codetimer-0.1.tar.gz

---
+++ packages/codetimer/codetimer-0.1.tar.gz/codetimer-0.1/setup.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+__author__ = 'Min'
+
+from distutils.core import setup
+
+setup (
+        name               = 'codetimer',
+        version             = '0.1',
+        py_modules      = ['codetimer'],
+        author              = 'Min',
+        author_email     = 'wwwee98@gmail.com',
+        url                    = 'https://www.facebook.com/yingerking',
+        description        = """
+        SimpleCodeTimer !/n
+/n
+Before :/n
+=================== CODE ====================/n
+    def say(what):/n
+        print "say : %s" % what/n
+/n
+    say("ho!!")/n
+/n
+=================== OUTPUT ===================/n
+    say : ho!!/n
+/n
+After :/n
+/n
+=================== CODE ====================/n
+    import codetimer/n
+    @codetimer.methodTimer/n
+    def say(what):/n
+        print "say : %s" % what/n
+/n
+    say("ho!!")/n
+/n
+=================== OUTPUT ===================/n
+    say : ho!!/n
+    SimpleCodeTimer : say method costs 0.000028 seconds !/n
+
+        """,
+        keywords = ['runtime', 'timer', 'codetimer']
+    )
