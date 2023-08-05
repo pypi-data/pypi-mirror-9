342bb9ce9e4699c91df4806c771fedd4c4a8b6f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:34 +0000

    Add package cubi/0.0.8/cubi-0.0.8.tar.gz

---
+++ packages/cubi/cubi-0.0.8.tar.gz/cubi-0.0.8/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'cubi',
+    version = '0.0.8',
+    keywords = ('cube', 'cubi', 'cube-rpc'),
+    description = 'python implementation for Cube RPC protocol',
+    license = 'MIT License',
+    install_requires = ['bp', 'gevent'],
+
+    scripts = ['cubi-simple-server', 'cubi-simple-server-test', 'cubi-another-server', 'cubi-another-server-test'],
+
+    author = 'http://www.liaohuqiu.net',
+    author_email = 'liaohuqiu@gmail.com',
+    url = 'https://github.com/liaohuqiu/cube-rpc-python',
+
+    packages = find_packages(),
+    platforms = 'any',
+)
