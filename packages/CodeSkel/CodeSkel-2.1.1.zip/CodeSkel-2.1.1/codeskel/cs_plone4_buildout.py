95ee1693512389248ad9bde53fcf4eac875ce151
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:12 +0000

    Add package CodeSkel/2.1.1/CodeSkel-2.1.1.zip

---
+++ packages/CodeSkel/CodeSkel-2.1.1.zip/CodeSkel-2.1.1/codeskel/cs_plone4_buildout.py
@@ -0,0 +1,42 @@
+from codeskel.base import BaseTemplate
+from codeskel.base import var
+
+
+class Plone4Buildout(BaseTemplate):
+    _template_dir = 'templates/cs_plone4_buildout'
+    summary = "A buildout for Plone 4 projects"
+    use_local_commands = False
+    required_templates = []
+    use_cheetah = True
+
+    vars = [
+        var('http_port',
+            'HTTP port',
+            default=8080),
+    ]
+
+    def pre(self, command, output_dir, vars):
+        super(BaseTemplate, self).pre(command, output_dir, vars)
+        vars['zeo_port'] = int(vars['http_port']) + 10
+        vars['instance2_port'] = int(vars['http_port']) + 1
+        vars['instance3_port'] = int(vars['http_port']) + 2
+        vars['instance4_port'] = int(vars['http_port']) + 3
+        vars['instance5_port'] = int(vars['http_port']) + 4
+        vars['haproxy_port'] = int(vars['http_port']) + 20
+        vars['varnish_port'] = int(vars['http_port']) + 30
+
+    def post(self, command, output_dir, vars):
+        print "-----------------------------------------------------------"
+        print " Generation finished"
+        print
+        print " See README.txt for details"
+        print "-----------------------------------------------------------"
+        print ""
+        print "              NEXT STEPS "
+        print "-----------------------------------------------------------"
+        print "              $ cd " + output_dir
+        print "              $ ~/buildout.python/bin/virtualenv-2.7 ."
+        print "              $ ./bin/pip install --upgrade setuptools"
+        print "              $ ./bin/python2.7 bootstrap.py"
+        print "              $ ./bin/buildout -vv"
+        print ""
---
+++ packages/CodeSkel/CodeSkel-2.1.1.zip/CodeSkel-2.1.1/setup.py
@@ -0,0 +1,45 @@
+from setuptools import setup, find_packages
+
+version = '2.1.1'
+
+setup(name='CodeSkel',
+      version=version,
+      description="A collection of skeletons for quickstarting CS projects.",
+      long_description=open("README.rst").read() + "\n" +
+                       open('HISTORY.TXT', 'r').read(),
+      classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Framework :: Zope2",
+        "Framework :: Zope3",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='web zope command-line skeleton project',
+      author='Mikel Larreategi',
+      author_email='mlarreategi@codesyntax.com',
+      url='http://github.com/codesyntax/CodeSkel',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+         'ZopeSkel <= 2.99',
+         'zopeskel.dexterity',
+      ],
+      entry_points="""
+      [paste.paster_create_template]
+      bootstrap_theme = codeskel:BootstrapTheme
+      cs_plone3_theme = codeskel:Plone3Theme
+      cs_plone4_buildout = codeskel:Plone4Buildout
+      csdexterity = codeskel:CSDexterity
+      cs_django_project = codeskel:CSDjangoProject
+      cs_django_buildout = codeskel:CSDjangoBuildout
+
+      [zopeskel.zopeskel_sub_template]
+      cs_dexterity_content = codeskel:DexterityContentCS
+      """,
+      )
