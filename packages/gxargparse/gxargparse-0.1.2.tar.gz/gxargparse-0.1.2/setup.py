978d663e1f9ddfd848d7e1d97782fbe89f3fc38b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:47 +0000

    Add package gxargparse/0.1.2/gxargparse-0.1.2.tar.gz

---
+++ packages/gxargparse/gxargparse-0.1.2.tar.gz/gxargparse-0.1.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+
+setup(name="gxargparse",
+        version='0.1.2',
+        description='Galaxy drop-in replacement for argparse',
+        author='Eric Rasche',
+        author_email='rasche.eric@yandex.ru',
+        license='GPL3',
+        requires=['galaxyxml'],
+        url='https://github.com/erasche/gxargparse',
+        packages=["argparse"],
+        classifiers=[
+            'Development Status :: 3 - Alpha',
+            'Operating System :: OS Independent',
+            'Intended Audience :: Developers',
+            'Environment :: Console',
+            ],
+        )
