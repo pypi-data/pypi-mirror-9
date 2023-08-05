f4a2e6fad72b86181848c32b49008db4fab7da58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:12 +0000

    Add package nntoolkit/0.1.27/nntoolkit-0.1.27.tar.gz

---
+++ packages/nntoolkit/nntoolkit-0.1.27.tar.gz/nntoolkit-0.1.27/README.md
@@ -0,0 +1,11 @@
+[![Build Status](https://travis-ci.org/MartinThoma/nntoolkit.svg?branch=master)](https://travis-ci.org/MartinThoma/nntoolkit)
+[![Coverage Status](https://img.shields.io/coveralls/MartinThoma/nntoolkit.svg)](https://coveralls.io/r/MartinThoma/nntoolkit?branch=master)
+[![Documentation Status](http://img.shields.io/badge/docs-latest-brightgreen.svg)](http://pythonhosted.org/nntoolkit)
+[![Code Health](https://landscape.io/github/MartinThoma/nntoolkit/master/landscape.svg)](https://landscape.io/github/MartinThoma/nntoolkit/master)
+
+nntoolkit
+=========
+
+Toolkit to train neural networks
+
+See [Deep Learning Tutorials](http://deeplearning.net/tutorial/).
---
+++ packages/nntoolkit/nntoolkit-0.1.27.tar.gz/nntoolkit-0.1.27/nntoolkit/evaluate.py
@@ -0,0 +1,121 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Evaluate a neural network."""
+
+import numpy
+import json
+import sys
+
+PY3 = sys.version > '3'
+
+if not PY3:
+    from future.builtins import open
+
+# nntoolkit modules
+import nntoolkit.utils as utils
+
+
+def get_parser():
+    """Return the parser object for this script."""
+    from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
+    parser = ArgumentParser(description=__doc__,
+                            formatter_class=ArgumentDefaultsHelpFormatter)
+    parser.add_argument("-m", "--model",
+                        dest="modelfile",
+                        help="where is the model file (.tar)?",
+                        metavar="FILE",
+                        type=lambda x: utils.is_valid_file(parser, x),
+                        required=True)
+    parser.add_argument("-i", "--input",
+                        dest="inputvec",
+                        help="""a file which contains an input vector
+                               [[0.12, 0.312, 1.21 ...]]""",
+                        metavar="FILE",
+                        type=lambda x: utils.is_valid_file(parser, x),
+                        required=True)
+    return parser
+
+
+def show_results(results, n=10, print_results=True):
+    """Show the TOP n results of a classification."""
+    # Print headline
+    s = ""
+    if len(results) == 0:
+        s += "-- No results --"
+    else:
+        s += "{0:18s} {1:7s}\n".format("Class", "Prob")
+        s += "#"*50 + "\n"
+        for entry in results:
+            if n == 0:
+                break
+            else:
+                n -= 1
+            s += "{0:18s} {1:>7.4f}%\n".format(entry['semantics'],
+                                               entry['probability']*100)
+        s += "#"*50
+    if print_results:
+        print(s)
+    return s
+
+
+def get_model_output(model, x):
+    """
+    :param model: A dictionary which represents a model
+    :param x: An input vector
+    :returns: The output vector of the model
+    """
+    if model['type'] == 'mlp':
+        for layer in model['layers']:
+            b, W, activation = layer['b'], layer['W'], layer['activation']
+            x = numpy.dot(x, W)
+            x = activation(x + b)
+        x = x[0]
+    return x
+
+
+def get_results(model_output, output_semantics):
+    results = []
+    for symbolnr, prob in enumerate(model_output):
+        results.append({'symbolnr': symbolnr,
+                        'probability': prob,
+                        'semantics': output_semantics[symbolnr]})
+    results = sorted(results, key=lambda x: x['probability'], reverse=True)
+    return results
+
+
+def main(modelfile, features, print_results=True):
+    """Evaluate the model described in ``modelfile`` with ``inputvec`` as
+       input data.
+
+    :param features: List of floats
+    :param print_results: Print results if True. Always return results.
+    :returns: List of possible answers, reverse-sorted by probability.
+    """
+    model = utils.get_model(modelfile)
+    if not model:
+        return []
+    x = numpy.array([features])
+    model_output = get_model_output(model, x)
+    results = get_results(model_output, model['outputs'])
+
+    if print_results:
+        show_results(results, n=10)
+    return results
+
+
+def main_bash(modelfile, inputvec_file, print_results=True):
+    """Evaluate the model described in ``modelfile`` with ``inputvec_file`` as
+       input data.
+
+    :param inputvec_file: File with json content. The content is a list with
+        one list as element. This list contains floats.
+    :param print_results: Print results if True. Always return results.
+    """
+    features = json.load(open(inputvec_file))
+    return main(modelfile, features, print_results)
+
+
+if __name__ == '__main__':
+    args = get_parser().parse_args()
+    main_bash(args.modelfile, args.inputvec)
