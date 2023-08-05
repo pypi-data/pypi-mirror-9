35a170d1cd6f81337fae00f3dc34a2ef65192323
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:04 +0000

    Add package incuna-auth/2.3.0/incuna-auth-2.3.0.tar.gz

---
+++ packages/incuna-auth/incuna-auth-2.3.0.tar.gz/incuna-auth-2.3.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import find_packages, setup
+
+
+install_requires = ('django-admin-sso', 'django-crispy-forms')
+
+setup(
+    name='incuna-auth',
+    version='2.3.0',
+    url='http://github.com/incuna/incuna-auth',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=install_requires,
+    description='Provides authentication parts.',
+    long_description=open('README.rst').read(),
+    author='Incuna Ltd',
+    author_email='admin@incuna.com',
+)
