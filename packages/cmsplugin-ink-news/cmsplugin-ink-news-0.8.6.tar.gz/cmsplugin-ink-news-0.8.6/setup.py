74c4e759e1715bf0d60f339ac098cd07edb5a8d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:55 +0000

    Add package cmsplugin-ink-news/0.8.6/cmsplugin-ink-news-0.8.6.tar.gz

---
+++ packages/cmsplugin-ink-news/cmsplugin-ink-news-0.8.6.tar.gz/cmsplugin-ink-news-0.8.6/setup.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+#
+# Copyright (C) 2010 Martin Owens
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+import os
+import sys
+
+from distutils.core import setup
+from cmsplugin_news import __version__
+
+# remove MANIFEST. distutils doesn't properly update it when the contents of directories change.
+if os.path.exists('MANIFEST'): os.remove('MANIFEST')
+
+setup(
+    name             = 'cmsplugin-ink-news',
+    version          = __version__,
+    description      = 'The modified news plugin used for inkscape\'s website.',
+    long_description = "See short description.",
+    url              = 'https://code.launchpad.net/~doctormo',
+    author           = 'Martin Owens',
+    author_email     = 'doctormo@gmail.com',
+    platforms        = 'linux',
+    license          = 'AGPLv3',
+    install_requires=[
+        'django-cms>=3.0',
+        'pytz',
+    ],
+    include_package_data = True,
+    packages = [
+      'cmsplugin_news',
+    ],
+)
+
