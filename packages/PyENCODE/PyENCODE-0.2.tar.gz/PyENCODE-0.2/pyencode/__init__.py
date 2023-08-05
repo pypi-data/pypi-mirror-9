e41885a38d7f5a19723f9e2a94b364b321380400
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:42 +0000

    Add package PyENCODE/0.2/PyENCODE-0.2.tar.gz

---
+++ packages/PyENCODE/PyENCODE-0.2.tar.gz/PyENCODE-0.2/pyencode/__init__.py
@@ -0,0 +1,38 @@
+'''
+=======================================================================
+PyENCODE: Convenience package for accessing ENCODE project data at UCSC
+=======================================================================
+
+This is a convenience package for accessing the raw data of the `ENCODE (Encyclopedia of DNA Elements) project <http://genome.ucsc.edu/ENCODE/>`_.
+
+The basic usage examples are the following:
+
+ * List ENCODE file collections::
+ 
+    >>> from pyencode import Encode
+    >>> e = Encode()
+    >>> for collection in sorted(e):
+    ...    print(collection.name)
+    AffyRnaChip
+    AwgDnaseMasterSites
+    ...
+    UwTfbs
+    
+  * List files in a single collection::
+
+    >>> for f in e.AwgSegmentation:
+    ...    print(f.name, f['cell'], f.url)
+    ('ChromhmmGm12878', 'GM12878', u'http://...
+    ...
+    ('SegwayK562', 'K562', u'http://...
+    
+  * Download certain file to cache and open for reading in binary::
+  
+    >>> f = e.AwgSegmentation.CombinedK562.fetch().open()
+
+Copyright 2014, Konstantin Tretyakov
+Licensed under MIT.
+'''
+
+from pyencode.encode import Encode
+
---
+++ packages/PyENCODE/PyENCODE-0.2.tar.gz/PyENCODE-0.2/setup.py
@@ -0,0 +1,52 @@
+'''
+PyENCODE: Convenience package for accessing ENCODE (Encyclopedia of DNA Elements) project data at UCSC
+
+Note that "python setup.py test" invokes pytest on the package. With appropriately
+configured setup.cfg, this will check both xxx_test modules and docstrings.
+
+Copyright 2014, Konstantin Tretyakov.
+Licensed under MIT.
+'''
+import sys
+from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
+
+# This is a plug-in for setuptools that will invoke py.test
+# when you run python setup.py test
+class PyTest(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        import pytest  # import here, because outside the required eggs aren't loaded yet
+        sys.exit(pytest.main(self.test_args))
+
+
+version = "0.2"
+
+setup(name="PyENCODE",
+      version=version,
+      description="Convenience package for accessing ENCODE (Encyclopedia of DNA Elements) project data at UCSC",
+      long_description=open("README.rst").read(),
+      classifiers=[ # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Scientific/Engineering :: Bio-Informatics'
+      ],
+      keywords="bioinformatics data-access encode", # Separate with spaces
+      author="Konstantin Tretyakov",
+      author_email="kt@ut.ee",
+      url="http://kt.era.ee/",
+      license="MIT",
+      packages=find_packages(exclude=['examples', 'tests']),
+      include_package_data=True,
+      zip_safe=True,
+      tests_require=['pytest'],
+      cmdclass={'test': PyTest},      
+      install_requires=['intervaltree_bio'],
+      entry_points={}
+)
