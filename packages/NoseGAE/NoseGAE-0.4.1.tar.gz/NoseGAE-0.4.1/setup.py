1bb271f93bdb142169eb83655cad9c472a30b49c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:10 +0000

    Add package NoseGAE/0.4.1/NoseGAE-0.4.1.tar.gz

---
+++ packages/NoseGAE/NoseGAE-0.4.1.tar.gz/NoseGAE-0.4.1/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+
+DESCRIPTION = "NoseGAE: nose plugin for Google App Engine testing"
+
+VERSION = '0.4.1'
+
+setup(
+    name='NoseGAE',
+    version=VERSION,
+    author="Jason Pellerin",
+    author_email="jpellerin@gmail.com",
+    maintainer="Josh Johnston",
+    maintainer_email="johnston.joshua+nosegae@gmail.com",
+    description=DESCRIPTION,
+    setup_requires=['setuptools-markdown'],
+    long_description_markdown_filename='README.md',
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Topic :: Software Development :: Testing"
+    ],
+    url='https://github.com/Trii/NoseGAE',
+    download_url='https://github.com/Trii/NoseGAE/tarball/' + VERSION,
+    license='BSD License',
+    entry_points={
+        'nose.plugins.0.10': ['nosegae = nosegae:NoseGAE']
+    },
+    py_modules=['nosegae'],
+    install_requires=['nose>=0.10.1'],
+    zip_safe=False
+)
