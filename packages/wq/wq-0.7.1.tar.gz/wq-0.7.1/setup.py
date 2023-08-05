fdaef162624b745fbc07d14c49fa065eb6ce8270
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:32 +0000

    Add package wq/0.7.1/wq-0.7.1.tar.gz

---
+++ packages/wq/wq-0.7.1.tar.gz/wq-0.7.1/setup.py
@@ -0,0 +1,56 @@
+import os
+from setuptools import setup
+
+LONG_DESCRIPTION = """
+A modular framework for building custom offline-capable desktop and mobile web apps.
+"""
+
+
+def long_description():
+    """Return long description from README.rst if it's present
+    because it doesn't get installed."""
+    try:
+        return open(os.path.join(
+            os.path.dirname(__file__), 'README.rst'
+        )).read()
+    except IOError:
+        return LONG_DESCRIPTION
+
+
+setup(
+    name='wq',
+    version='0.7.1',
+    author='S. Andrew Sheppard',
+    author_email='andrew@wq.io',
+    url='http://wq.io/',
+    license='MIT',
+    description=LONG_DESCRIPTION.strip(),
+    long_description=long_description(),
+    install_requires=[
+        'wq.app>=0.7.1,<0.8',
+        'wq.db>=0.7.1,<0.8',
+        'wq.io>=0.7.1,<0.8',
+        'wq.start>=0.7.0,<0.8',
+    ],
+    packages=['wq'],
+    namespace_packages=['wq'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: JavaScript',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Framework :: Django',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+        'Topic :: Text Processing :: Markup :: HTML',
+        'Topic :: Scientific/Engineering :: GIS',
+        'Topic :: Software Development :: Build Tools',
+        'Topic :: Software Development :: Pre-processors',
+        'Topic :: Database :: Database Engines/Servers',
+        'Topic :: Text Processing :: Markup :: XML',
+    ]
+)
