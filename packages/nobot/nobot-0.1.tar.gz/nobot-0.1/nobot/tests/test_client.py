c4e786a025f1ef88836f354a8b47f9f304d30ade
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:28 +0000

    Add package nobot/0.1/nobot-0.1.tar.gz

---
+++ packages/nobot/nobot-0.1.tar.gz/nobot-0.1/nobot/tests/test_client.py
@@ -0,0 +1,256 @@
+import mock
+import httpretty
+from django.forms import Form
+from six.moves.urllib.parse import urlencode
+
+from nobot.client import ReCaptchaClient, HumanCaptchaClient, RecaptchaResponse
+from nobot.fields import ReCaptchaField, HumanCaptchaField
+
+
+class SuccessfulReCaptchaClient(ReCaptchaClient):
+    def verify(self, challenge, response, remote_ip):
+        return RecaptchaResponse(is_valid=True, error_code=None)
+
+
+class SuccessfulHumanCaptchaClient(HumanCaptchaClient):
+    def verify(self, challenge, response, remote_ip):
+        return RecaptchaResponse(is_valid=True, error_code=None)
+
+
+class TestReCaptchaForm(Form):
+    captcha = ReCaptchaField(
+        attrs={'theme': 'white'},
+        client_class=SuccessfulReCaptchaClient)
+
+
+class TestHumanCaptchaForm(Form):
+    captcha = HumanCaptchaField(
+        attrs={'theme': 'white'},
+        client_class=SuccessfulHumanCaptchaClient)
+
+
+class TestReCaptchaClient(object):
+    def setup(self):
+        self.verify_data = {
+            'privatekey': 'privkey',
+            'remoteip': '127.0.0.1',
+            'challenge': 'test',
+            'response': 'test',
+        }
+
+    def test_simple_pass(self):
+        form_params = {'recaptcha_response_field': 'test'}
+        form = TestReCaptchaForm(form_params)
+        assert form.is_valid()
+
+    @mock.patch('django.template.loader.render_to_string')
+    def test_render_simple(self, render_to_string, activate_en):
+        client = ReCaptchaClient()
+
+        assert not client.nocaptcha
+
+        client.render({})
+
+        assert render_to_string.called_once_with(
+            'captcha/widget.html',
+            {
+                'api_server': '//www.google.com/recaptcha/api',
+                'public_key': 'pubkey',
+                'lang': 'en',
+                'options': '{"lang": "en"}',
+                'challenge_url': '//www.google.com/recaptcha/api/challenge?k=pubkey&hl=en',  # noqa
+                'noscript_url': '//www.google.com/recaptcha/api/noscript?k=pubkey&hl=en',  # noqa
+            }
+        )
+
+    @mock.patch('django.template.loader.render_to_string')
+    def test_render_has_error(self, render_to_string, activate_en):
+        client = ReCaptchaClient()
+        client.render({}, 'foo bar')
+
+        assert render_to_string.called_once_with(
+            'captcha/widget.html',
+            {
+                'api_server': '//www.google.com/recaptcha/api',
+                'public_key': 'pubkey',
+                'lang': 'en',
+                'options': '{"lang": "en"}',
+                'challenge_url': '//www.google.com/recaptcha/api/challenge?k=pubkey&hl=en&error=foo%20bar',  # noqa
+                'noscript_url': '//www.google.com/recaptcha/api/noscript?k=pubkey&hl=en&error=foo%20bar',  # noqa
+            }
+        )
+
+    @mock.patch('django.template.loader.render_to_string')
+    def test_render_uses_language(self, render_to_string, activate_en):
+        client = ReCaptchaClient()
+        client.render({'lang': 'de'}, 'foo bar')
+
+        assert render_to_string.called_once_with(
+            'captcha/widget.html',
+            {
+                'api_server': '//www.google.com/recaptcha/api',
+                'public_key': 'pubkey',
+                'lang': 'de',
+                'options': '{"lang": "de"}',
+                'challenge_url': '//www.google.com/recaptcha/api/challenge?k=pubkey&hl=de&error=foo%20bar',  # noqa
+                'noscript_url': '//www.google.com/recaptcha/api/noscript?k=pubkey&hl=de&error=foo%20bar',  # noqa
+            }
+        )
+
+    @httpretty.activate
+    def test_verify_sucess(self):
+        args = urlencode(self.verify_data)
+
+        httpretty.register_uri(
+            httpretty.GET,
+            'https://www.google.com/recaptcha/api/verify?' + args,
+            body='true\n\n',
+            status=200,
+            content_type='plain/text'
+        )
+
+        client = ReCaptchaClient()
+        response = client.verify('test', 'test', '127.0.0.1')
+        last_request = httpretty.last_request()
+
+        assert last_request.path.startswith('/recaptcha/api/verify')
+        assert response.is_valid
+
+    @httpretty.activate
+    def test_verify_wrong_arguments(self):
+        client = ReCaptchaClient()
+        response = client.verify('', 'test', '127.0.0.1')
+
+        assert isinstance(
+            httpretty.last_request(),
+            httpretty.core.HTTPrettyRequestEmpty)
+
+        assert not response.is_valid
+        assert response.error_code == 'incorrect-captcha-sol'
+
+    @httpretty.activate
+    def test_verify_error(self):
+        args = urlencode(self.verify_data)
+
+        httpretty.register_uri(
+            httpretty.GET,
+            'https://www.google.com/recaptcha/api/verify?' + args,
+            body='fail\nerror_code\n',
+            status=200,
+            content_type='plain/text'
+        )
+
+        client = ReCaptchaClient()
+        response = client.verify('test', 'test', '127.0.0.1')
+        last_request = httpretty.last_request()
+
+        assert last_request.path.startswith('/recaptcha/api/verify')
+        assert not response.is_valid
+        assert response.error_code == 'error_code'
+
+
+class TestHumanaptchaClient(object):
+    def setup(self):
+        self.verify_data = {
+            'secret': 'privkey',
+            'remoteip': '127.0.0.1',
+            'response': 'test',
+        }
+
+    def test_simple_pass(self):
+        form_params = {'g-recaptcha-response': 'test'}
+        form = TestHumanCaptchaForm(form_params)
+        assert form.is_valid()
+
+    @mock.patch('django.template.loader.render_to_string')
+    def test_render_simple(self, render_to_string, activate_en):
+        client = HumanCaptchaClient()
+
+        assert client.nocaptcha
+
+        client.render({})
+
+        assert render_to_string.called_once_with(
+            'captcha/widget.html',
+            {
+                'api_server': '//www.google.com/recaptcha/api',
+                'public_key': 'pubkey',
+                'lang': 'en',
+                'options': '{"lang": "en"}',
+                'challenge_url': '//www.google.com/recaptcha/api/challenge?k=pubkey&hl=en',  # noqa
+                'noscript_url': '//www.google.com/recaptcha/api/noscript?k=pubkey&hl=en',  # noqa
+            }
+        )
+
+    @mock.patch('django.template.loader.render_to_string')
+    def test_render_has_error(self, render_to_string, activate_en):
+        client = HumanCaptchaClient()
+        client.render({}, 'foo bar')
+
+        assert render_to_string.called_once_with(
+            'captcha/widget.html',
+            {
+                'api_server': '//www.google.com/recaptcha/api',
+                'public_key': 'pubkey',
+                'lang': 'en',
+                'options': '{"lang": "en"}',
+                'challenge_url': '//www.google.com/recaptcha/api/challenge?k=pubkey&hl=en&error=foo%20bar',  # noqa
+                'noscript_url': '//www.google.com/recaptcha/api/noscript?k=pubkey&hl=en&error=foo%20bar',  # noqa
+            }
+        )
+
+    @mock.patch('django.template.loader.render_to_string')
+    def test_render_uses_language(self, render_to_string, activate_en):
+        client = HumanCaptchaClient()
+        client.render({'lang': 'de'}, 'foo bar')
+
+        assert render_to_string.called_once_with(
+            'captcha/widget.html',
+            {
+                'api_server': '//www.google.com/recaptcha/api',
+                'public_key': 'pubkey',
+                'lang': 'de',
+                'options': '{"lang": "de"}',
+                'challenge_url': '//www.google.com/recaptcha/api/challenge?k=pubkey&hl=de&error=foo%20bar',  # noqa
+                'noscript_url': '//www.google.com/recaptcha/api/noscript?k=pubkey&hl=de&error=foo%20bar',  # noqa
+            }
+        )
+
+    @httpretty.activate
+    def test_verify_sucess(self):
+        args = urlencode(self.verify_data)
+
+        httpretty.register_uri(
+            httpretty.GET,
+            'https://www.google.com/recaptcha/api/siteverify?' + args,
+            body='',
+            status=200,
+            content_type='plain/text'
+        )
+
+        client = HumanCaptchaClient()
+        response = client.verify('test', 'test', '127.0.0.1')
+        last_request = httpretty.last_request()
+
+        assert last_request.path.startswith('/recaptcha/api/siteverify')
+        assert response.is_valid
+
+    @httpretty.activate
+    def test_verify_error(self):
+        args = urlencode(self.verify_data)
+
+        httpretty.register_uri(
+            httpretty.GET,
+            'https://www.google.com/recaptcha/api/siteverify?' + args,
+            body='fail\nerror_code\n',
+            status=400,
+            content_type='plain/text'
+        )
+
+        client = HumanCaptchaClient()
+        response = client.verify('test', 'test', '127.0.0.1')
+        last_request = httpretty.last_request()
+
+        assert last_request.path.startswith('/recaptcha/api/siteverify')
+        assert not response.is_valid
+        assert response.error_code is None
---
+++ packages/nobot/nobot-0.1.tar.gz/nobot-0.1/setup.py
@@ -0,0 +1,79 @@
+import sys
+import os
+import codecs
+from setuptools import setup, find_packages
+
+
+version = '0.1'
+
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    os.system('python setup.py bdist_wheel upload')
+    print('You probably want to also tag the version now:')
+    print('  git tag -a %s -m "version %s"' % (version, version))
+    print('  git push --tags')
+    sys.exit()
+
+
+def read(*parts):
+    filename = os.path.join(os.path.dirname(__file__), *parts)
+    with codecs.open(filename, encoding='utf-8') as fp:
+        return fp.read()
+
+
+test_requires = [
+    'pytest>=2.5.2',
+    'pytest-cov>=1.6',
+    'pytest-flakes>=0.2',
+    'pytest-pep8>=1.0.5',
+    'pytest-django>=2.6',
+    'pep8==1.4.6',
+    'httpretty>=0.8.4,<0.9',
+    'mock>=1.0.1,<1.1',
+]
+
+
+install_requires = [
+    'Django>=1.4,<1.8',
+    'requests>=1.1.0',
+    'six>=1.9.0',
+]
+
+
+setup(
+    name='nobot',
+    version=version,
+    description='Django recaptcha form field/widget app.',
+    long_description=(
+        read('README.rst') + read('AUTHORS.rst') + read('CHANGELOG.rst')),
+    author='Christopher Grebs',
+    author_email='cg@webshox.org',
+    install_requires=install_requires,
+    extras_require={
+        'tests': test_requires,
+    },
+    license='BSD',
+    url='https://github.com/EnTeQuak/nobot',
+    packages=find_packages(),
+    include_package_data=True,
+    classifiers=[
+        'Programming Language :: Python',
+        'License :: OSI Approved :: BSD License',
+        'Development Status :: 5 - Production/Stable',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Framework :: Django',
+    ],
+    zip_safe=False,
+)
