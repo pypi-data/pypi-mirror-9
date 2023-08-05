4e35dae7458a0775fec2cbfb43bb03acf44cba60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:43 +0000

    Add package hanyang_registration/1.1.2/hanyang_registration-1.1.2.tar.gz

---
+++ packages/hanyang_registration/hanyang_registration-1.1.2.tar.gz/hanyang_registration-1.1.2/setup.py
@@ -0,0 +1,20 @@
+#-*- coding:utf-8 -*-
+from setuptools import setup
+
+setup(
+    name = 'hanyang_registration',
+    packages = ['hanyang_registration'],
+    version = '1.1.2',
+    description = 'Hanyang Class Registration for python',
+    license = 'The MIT License',
+    author = 'Jin-Soo Han, Jaram',
+    author_email = 'jinsu0411@gmail.com',
+    url = 'https://github.com/Jaram/Hanyang-Class-Registration',
+    keywords = ['hanyang', 'registration'],
+    classifiers = [],
+    long_description=open('README.rst').read(),
+    install_requires=[
+       'rsa',
+       'requests'
+    ]
+)
