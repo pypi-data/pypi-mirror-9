3512e5ec0e02c2f644dc9208a9feed7a4c9fa073
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:19 +0000

    Add package Flask-Compress-nondebug/1.0.2/Flask-Compress-nondebug-1.0.2.tar.gz

---
+++ packages/Flask-Compress-nondebug/Flask-Compress-nondebug-1.0.2.tar.gz/Flask-Compress-nondebug-1.0.2/setup.py
@@ -0,0 +1,38 @@
+"""
+Flask-Compress
+------------
+
+Compress responses in your Flask app with gzip.
+
+Forked from https://github.com/wichitacode/flask-compress and removed the debugging option
+"""
+
+from setuptools import setup
+
+setup(
+    name='Flask-Compress-nondebug',
+    version='1.0.2',
+    url='https://github.com/wichitacode/flask-compress',
+    license='MIT',
+    author='William Fagan',
+    author_email='will@wichitacode.com',
+    description='Compress responses in your Flask app with gzip.',
+    long_description=__doc__,
+    py_modules=['flask_compress'],
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    install_requires=[
+        'Flask'
+    ],
+    test_suite='tests',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
