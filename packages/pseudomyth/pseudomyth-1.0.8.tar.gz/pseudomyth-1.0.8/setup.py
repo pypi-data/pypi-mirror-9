2a992f8f9f092eab199fe82e85356fdde8cbb4a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:35 +0000

    Add package pseudomyth/1.0.8/pseudomyth-1.0.8.tar.gz

---
+++ packages/pseudomyth/pseudomyth-1.0.8.tar.gz/pseudomyth-1.0.8/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+with open('README.rst') as readme_file:
+    README = readme_file.read()
+
+setup(
+    name='pseudomyth',
+    description=(
+        'A script with which to determine what order to watch anime episodes '
+        'in'
+    ),
+    url='https://github.com/colons/pseudomyth',
+    author='Iain Dawson',
+    author_email='pypi@colons.co',
+    version='1.0.8',
+    license="BSD",
+    platforms=['any'],
+    packages=['pseudomyth'],
+    scripts=['scripts/pseudomyth'],
+    long_description=README,
+    classifiers=[
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+    ],
+)
