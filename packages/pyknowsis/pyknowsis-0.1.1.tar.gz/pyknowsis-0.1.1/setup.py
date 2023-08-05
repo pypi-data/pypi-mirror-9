08a4e3a3d36c59ef822cb546d84cd6ff7b855dea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:14 +0000

    Add package pyknowsis/0.1.1/pyknowsis-0.1.1.tar.gz

---
+++ packages/pyknowsis/pyknowsis-0.1.1.tar.gz/pyknowsis-0.1.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+from pyknowsis import __version__
+
+from pip.req import parse_requirements
+
+
+install_requirements = [str(ir.req) for ir in parse_requirements('requirements.txt')]
+test_requirements = [str(ir.req) for ir in parse_requirements('requirements-testing.txt')]
+
+
+setup(
+    name='pyknowsis',
+    include_package_data=True,
+    version=__version__,
+    packages=[
+        'pyknowsis',
+        'tests'
+    ],
+    description='API Wrapper for the Knowsis API',
+    author='Knowsis Ltd',
+    dependency_links=[],
+    install_requires=install_requirements,
+    tests_require=test_requirements,
+    url="https://github.com/knowsis/pyknowsis"
+)
---
+++ packages/pyknowsis/pyknowsis-0.1.1.tar.gz/pyknowsis-0.1.1/tests/api_tests.py
@@ -0,0 +1,111 @@
+from datetime import datetime, time, timedelta
+import unittest
+
+from pyknowsis import KnowsisClient, Identifier
+
+oauth_consumer_key = ""
+oauth_consumer_secret = ""
+
+
+class TestAssetsEndpoint(unittest.TestCase):
+
+    def test_returns_correct_meta(self):
+
+        api = KnowsisClient(oauth_consumer_key, oauth_consumer_secret)
+
+        page = 2
+        pagesize = 5
+        assets = api.assets(page=page, pagesize=pagesize)
+
+        self.assertIsNotNone(assets.assets)
+        self.assertEqual(assets.meta.page, page)
+        self.assertEqual(assets.meta.pagesize, pagesize)
+        self.assertIsNotNone(assets.assets)
+        self.assertIsInstance(assets.assets, list)
+
+        self.assertEqual(len(assets.assets), assets.meta.items)
+
+
+class TestAssetEndpoint(unittest.TestCase):
+
+    def test_returns_correct_asset(self):
+
+        api = KnowsisClient(oauth_consumer_key, oauth_consumer_secret)
+
+        asset = api.asset(identifier="AAPL")
+
+        self.assertEqual(asset.asset_name, "Apple Inc")
+        self.assertIsInstance(asset.asset_identifiers, list)
+        self.assertIsInstance(asset.asset_identifiers[0], Identifier)
+
+
+class TestAssetSentimentEndpoint(unittest.TestCase):
+
+    def test_returns_correct_asset(self):
+
+        api = KnowsisClient(oauth_consumer_key, oauth_consumer_secret)
+
+        asset = api.asset_sentiment(identifier="AAPL")
+
+        self.assertEqual(asset.name, "Apple Inc")
+
+    def test_returns_data_for_last_period_by_default(self):
+
+        api = KnowsisClient(oauth_consumer_key, oauth_consumer_secret)
+
+        asset = api.asset_sentiment(identifier="AAPL")
+
+        current_day = datetime.combine(datetime.utcnow(), time.min)
+
+        self.assertEqual(asset.name, "Apple Inc")
+        self.assertEqual(asset.startdate, current_day)
+        self.assertEqual(asset.enddate,  current_day)
+
+    def test_returns_data_for_daterange_specified(self):
+
+        api = KnowsisClient(oauth_consumer_key, oauth_consumer_secret)
+        startdate = datetime.combine(datetime.utcnow(), time.min) - timedelta(days=5)
+        enddate = datetime.combine(datetime.utcnow(), time.min) - timedelta(days=2)
+
+        asset = api.asset_sentiment(identifier="AAPL", startdate=startdate, enddate=enddate)
+
+        self.assertEqual(asset.name, "Apple Inc")
+        self.assertEqual(asset.startdate,  startdate)
+        self.assertEqual(asset.enddate,  enddate)
+
+
+class TestAssetIntradaySentimentEndpoint(unittest.TestCase):
+
+    def test_returns_correct_asset(self):
+
+        api = KnowsisClient(oauth_consumer_key, oauth_consumer_secret)
+
+        asset = api.asset_intraday_sentiment(identifier="AAPL")
+
+        self.assertEqual(asset.name, "Apple Inc")
+
+    def test_returns_data_for_current_day_by_default(self):
+
+        api = KnowsisClient(oauth_consumer_key, oauth_consumer_secret)
+
+        asset = api.asset_intraday_sentiment(identifier="AAPL")
+
+        now = datetime.utcnow()
+        last_period = now - timedelta(minutes=now.minute % 5, seconds=now.second, microseconds=now.microsecond)
+
+        self.assertEqual(asset.name, "Apple Inc")
+        self.assertEqual(asset.startdate,  last_period)
+        self.assertEqual(asset.enddate,  last_period)
+
+    def test_returns_data_for_daterange_specified(self):
+
+        api = KnowsisClient(oauth_consumer_key, oauth_consumer_secret)
+
+        startdate = datetime.combine(datetime.utcnow(), time.min)
+        enddate = datetime.combine(datetime.utcnow(), time.min) + timedelta(hours=6)
+
+        asset = api.asset_intraday_sentiment(identifier="AAPL", startdate=startdate, enddate=enddate)
+
+        self.assertEqual(asset.name, "Apple Inc")
+        self.assertEqual(asset.startdate,  startdate)
+        self.assertEqual(asset.enddate,  enddate)
