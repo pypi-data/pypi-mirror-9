a8b35a5d79b2ad897c10909f8aa0b4434e0b25e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:06 +0000

    Add package maraithal/0.5/maraithal-0.5.tar.gz

---
+++ packages/maraithal/maraithal-0.5.tar.gz/maraithal-0.5/setup.py
@@ -0,0 +1,25 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = {
+    'description': 'Implementation of LSB Steganography to hide texts in images',
+    'author': 'Krishna Ram Prakash R',
+    'url': 'https://github.com/rkrp/maraithal',
+    'download_url': 'https://github.com/rkrp/maraithal',
+    'author_email': 'krp@gtux.in',
+    'version': '0.5',
+    'install_requires': ['pillow'],
+    'packages': ['maraithal'],
+    'scripts': ['bin/maraithal'],
+    'name': 'maraithal',
+    'license' : 'GPLv3',
+#    'classifiers' : {
+#        'Development Status :: 4 - Beta',
+#        'Topic :: Security',
+#        'Topic :: Security :: Cryptography',
+#        },
+}
+
+setup(**config)
