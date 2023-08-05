77ef9f99d0ad39e3e87f2f095a6b9327b21c16fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:32 +0000

    Add package faster-closeio/0.1.34/faster_closeio-0.1.34.tar.gz

---
+++ packages/faster-closeio/faster_closeio-0.1.34.tar.gz/faster_closeio-0.1.34/README.rst
@@ -0,0 +1,39 @@
+===============================
+closeio
+===============================
+
+.. image:: https://travis-ci.org/Thermondo/closeio.png?branch=master
+    :target: https://travis-ci.org/Thermondo/closeio
+
+.. image:: https://coveralls.io/repos/Thermondo/closeio/badge.png?branch=master
+    :target: https://coveralls.io/r/Thermondo/closeio
+
+.. image:: https://pypip.in/v/faster_closeio/badge.png
+    :target: https://crate.io/packages/faster_closeio
+
+.. image:: https://pypip.in/status/faster_closeio/badge.svg
+    :target: https://pypi.python.org/pypi/faster_closeio/
+    :alt: Development Status
+
+.. image:: https://pypip.in/py_versions/faster_closeio/badge.svg
+    :target: https://pypi.python.org/pypi/faster_closeio/
+    :alt: Supported Python versions
+
+.. image:: https://pypip.in/d/faster_closeio/badge.png
+    :target: https://crate.io/packages/faster_closeio/
+    :alt: Downloads
+
+.. image:: https://pypip.in/license/faster_closeio/badge.png
+    :target: https://pypi.python.org/pypi/faster_closeio/
+    :alt: License
+
+
+Slim API wrapper to access close.io CRM
+
+* Free software: BSD license
+* Documentation: http://closeio.rtfd.org.
+
+Features
+--------
+
+* TODO
---
+++ packages/faster-closeio/faster_closeio-0.1.34.tar.gz/faster_closeio-0.1.34/setup.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+
+try:
+    from setuptools import setup, Command
+except ImportError:
+    from distutils.core import setup
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    sys.exit()
+
+readme = open('README.rst').read()
+history = open('HISTORY.rst').read().replace('.. :changelog:', '')
+
+
+class PyTest(Command):
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        import sys
+        import subprocess
+
+        errno = subprocess.call([sys.executable, 'runtests.py'])
+        raise SystemExit(errno)
+
+setup(
+    name='faster_closeio',
+    version='0.1.34',
+    description='Slim API wrapper to access close.io CRM',
+    long_description=readme + '\n\n' + history,
+    author='Denis Cornehl',
+    author_email='denis.cornehl@gmail.com',
+    url='https://github.com/fasterweb/closeio',
+    packages=[
+        'closeio',
+        'closeio.contrib',
+        'closeio.contrib.django',
+    ],
+    package_dir={'closeio': 'closeio'},
+    include_package_data=True,
+    install_requires=[
+        'requests',
+        'slumber',
+        'python-dateutil',
+        'six',
+    ],
+    license="BSD",
+    zip_safe=False,
+    keywords='closeio',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: Implementation :: PyPy',
+    ],
+    entry_points={'pytest11': ['closeio = closeio.contrib.pytest_plugin']},
+    cmdclass={'test': PyTest},
+)
