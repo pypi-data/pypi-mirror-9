28d519dad4351febc65df26001356a16bc465584
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:07 +0000

    Add package myscipkg3/0.1/myscipkg3-0.1-py2-none-any.whl

---
+++ packages/myscipkg3/myscipkg3-0.1-py2-none-any.whl/myscipkg3-0.1.dist-info/DESCRIPTION.rst
@@ -0,0 +1,26 @@
+=============================
+myscipkg3
+=============================
+
+.. image:: https://travis-ci.org/ryanpdwyer/myscipkg3.png?branch=master
+    :target: https://travis-ci.org/ryanpdwyer/myscipkg3
+
+
+A package for science using numpy, matplotlib, readthedocs, etc.
+
+
+Features
+--------
+
+* TODO
+
+
+
+
+Documentation
+-------------
+
+The full documentation is at http://myscipkg3.rtfd.org.
+
+
+
---
+++ packages/myscipkg3/myscipkg3-0.1-py2-none-any.whl/myscipkg3/__init__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+============================
+myscipkg3
+============================
+"""
+import pkg_resources
+
+# Get __version__ from setup.py version number
+# See https://github.com/hgrecco/pint/blob/master/pint/__init__.py
+try:
+    __version__ = pkg_resources.get_distribution('pint').version
+except:
+    # we seem to have a local copy installed without setuptools
+    # so the reported version will be unknown
+    __version__ = "unknown"
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
