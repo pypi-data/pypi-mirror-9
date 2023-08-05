29578b693420da1b927e7e2e59dd66dd4a67cd89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:57 +0000

    Add package createbucket/0.1.0/createbucket-0.1.0.tar.gz

---
+++ packages/createbucket/createbucket-0.1.0.tar.gz/createbucket-0.1.0/setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+
+setup(
+    name='createbucket',
+    version='0.1.0',
+    author='Matt Warren',
+    author_email='matt.warren@gmail.com',
+    packages=['createbucket'],
+    url='https://github.com/mfwarren/createbucket',
+    license='LICENSE',
+    description='Simple command tool to create S3 Buckets.',
+    long_description=open('README.rst').read(),
+    install_requires=[
+        "boto",
+        "inquirer",
+    ],
+    entry_points={
+        'console_scripts': ['createbucket = createbucket.create_s3_bucket:main']
+    },
+)
