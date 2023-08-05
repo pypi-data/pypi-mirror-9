f7e63d1d09e997e946518bc26bb0b620f455f4cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:51 +0000

    Add package android-asset-resizer/1.2.1/android-asset-resizer-1.2.1.tar.gz

---
+++ packages/android-asset-resizer/android-asset-resizer-1.2.1.tar.gz/android-asset-resizer-1.2.1/setup.py
@@ -0,0 +1,39 @@
+import sys
+import os
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+import android_asset_resizer
+
+requires = [
+    'PIL',
+]
+
+setup(
+    name='android-asset-resizer',
+    version=android_asset_resizer.__version__,
+    description='A command-line utility for generating Android drawables in'
+                ' the required densities.',
+    long_description=open('README.rst').read(),
+    keywords='android asset resource res drawable resizer',
+    license=open('LICENSE').read(),
+    author='Tristan Waddington',
+    author_email='tristan.waddington@gmail.com',
+    url='https://github.com/twaddington/android-asset-resizer',
+    install_requires=requires,
+    packages=['android_asset_resizer'],
+    scripts=['bin/aaresize'],
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ]
+)
