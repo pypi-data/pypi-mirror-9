745af806f5ef63dd440dcf5953f56308ed9189b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:09 +0000

    Add package test_mm/0.1.3/test_mm-0.1.3.zip

---
+++ packages/test_mm/test_mm-0.1.3.zip/test_mm-0.1.3/README.rst
@@ -0,0 +1,7 @@
+test_mm
+-------
+
+To use, simply do::
+
+    >>> import test_mm
+    >>> print test_mm.test()
---
+++ packages/test_mm/test_mm-0.1.3.zip/test_mm-0.1.3/test_mm/text.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 12 14:01:45 2015
+
+@author: Magerl_4
+"""
+
+from markdown import markdown
+
+def test():
+    return "Radi!\n"
+
+def print_test():
+    return markdown("Radi!\n")
+    return 0
