4bb36e19e559529aa8b70c0afb99efa053e1c049
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:32 +0000

    Add package horsemeat/2.8.4/horsemeat-2.8.4.tar.gz

---
+++ packages/horsemeat/horsemeat-2.8.4.tar.gz/horsemeat-2.8.4/horsemeat/__init__.py
@@ -0,0 +1,53 @@
+# vim: set expandtab ts=4 sw=4 filetype=python:
+
+import functools
+import json
+import pprint
+import uuid
+
+from horsemeat.version import __version__
+
+class HorsemeatJSONEncoder(json.JSONEncoder):
+
+    def default(self, obj):
+
+        # TODO: when something can not be JSON-encoded, just return a
+        # message like "can not JSON-encode {0}".
+
+        import psycopg2.extras
+
+        # Any object that wants to be encoded into JSON should make a
+        # property called __jsondata__ that spits out something that can
+        # be JSON-encoded.
+        if hasattr(obj, '__jsondata__'):
+            return obj.__jsondata__
+
+        # This is to handle datetimes and dates.
+        elif hasattr(obj, 'isoformat') and callable(obj.isoformat):
+            return obj.isoformat()
+
+        elif isinstance(obj, uuid.UUID):
+          return str(obj)
+
+        elif isinstance(obj, (
+            psycopg2.extras.DateTimeTZRange,
+            psycopg2.extras.DateTimeRange,
+            psycopg2.extras.DateRange,
+            )):
+
+            return dict(lower=obj.lower, upper=obj.upper)
+
+        # Stick your own type check stuff here.
+        # End of your own stuff.
+
+        else:
+            return json.JSONEncoder.default(self, obj)
+
+
+# TODO: add a docstring on this guy.
+fancyjsondumps = functools.partial(
+    json.dumps,
+    cls=HorsemeatJSONEncoder,
+    sort_keys=True,
+    indent=4,
+    separators=(',', ': '))
