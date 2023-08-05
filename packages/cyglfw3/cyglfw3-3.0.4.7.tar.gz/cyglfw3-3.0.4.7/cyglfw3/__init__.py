a4686054107576d3d412db3d4d0b6d9f05797c71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:41 +0000

    Add package cyglfw3/3.0.4.7/cyglfw3-3.0.4.7.tar.gz

---
+++ packages/cyglfw3/cyglfw3-3.0.4.7.tar.gz/cyglfw3-3.0.4.7/cyglfw3/__init__.py
@@ -0,0 +1,8 @@
+from __future__ import absolute_import
+
+# the version of software
+# this is used by the setup.py script
+from .version import __version__
+
+from .glfw3 import *
+
---
+++ packages/cyglfw3/cyglfw3-3.0.4.7.tar.gz/cyglfw3-3.0.4.7/setup.py
@@ -0,0 +1,87 @@
+import os
+import sys
+from setuptools import setup, Extension
+from setuptools.dist import Distribution
+
+execfile('cyglfw3/version.py')
+
+# auto-install cython
+Distribution(dict(setup_requires='Cython'))
+
+# check cython is installed
+try:
+    from Cython.Distutils import build_ext
+    from Cython.Build import cythonize
+except ImportError:
+    print("Could not import Cython. Install `cython` and rerun.")
+    sys.exit(1)
+
+# set the GLFW compiler flags
+extra_compile_args = []
+extra_link_args = []
+
+platform = sys.platform.lower()
+if 'darwin' in platform or 'linux' in platform:
+    # check for homebrew or local installations on unix systems
+    extra_compile_args.append('-I/usr/local/include')
+    extra_link_args.append('-L/usr/local/lib')
+
+    # support macports
+    extra_compile_args.append('-I/opt/local/include')
+    extra_compile_args.append('-I/opt/local/lib')
+
+glfw_lib = 'glfw'
+if 'darwin' in platform:
+    # homebrew calls it libglfw3
+    if os.path.exists('/usr/local/lib/libglfw3.dylib'):
+        glfw_lib = 'glfw3'
+
+if 'win32' in platform:
+    glfw_lib = 'glfw3dll'
+    extra_compile_args.append('-I%s' % (os.path.join(os.environ['GLFW_ROOT'], 'include'),))
+    extra_link_args.append('/LIBPATH:%s' % (os.path.join(os.environ['GLFW_ROOT'], 'lib-vc2012'),))
+
+ext_modules = [
+    Extension('cyglfw3.glfw3', ['cyglfw3/glfw3.pyx'], libraries=[glfw_lib],
+        extra_compile_args=extra_compile_args,
+        extra_link_args=extra_link_args
+    )
+]
+ext_modules = cythonize(ext_modules)
+
+# import the README
+with open('README.rst') as f:
+    long_description = f.read()
+
+
+setup(
+    name="cyglfw3",
+    version=__version__,
+    description="Python bindings for GLFW 3+ using Cython",
+    long_description=long_description,
+    license = 'BSD',
+    author="Adam Griffiths",
+    author_email="adam.lw.griffiths@gmail.com",
+    install_requires=['Cython'],
+    url='http://github.com/adamlwgriffiths/cyglfw3',
+    packages=['cyglfw3'],
+    package_data={'cyglfw3':['*.pyx', '*.pxd', '*.c']},
+    cmdclass={'build_ext': build_ext},
+    requires=['cython'],
+    ext_modules=ext_modules,
+    platforms=['any'],
+    classifiers=(
+        'Development Status :: 4 - Beta',
+        'Natural Language :: English',
+        'Intended Audience :: Developers',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Topic :: Multimedia :: Graphics',
+        'Topic :: Multimedia :: Graphics :: 3D Rendering',
+        'License :: OSI Approved :: BSD License',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ),
+)
