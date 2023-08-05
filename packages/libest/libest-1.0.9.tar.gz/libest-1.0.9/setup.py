78cfde4235f12f425ffa18469cbcd963d533d1ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:11 +0000

    Add package libest/1.0.9/libest-1.0.9.tar.gz

---
+++ packages/libest/libest-1.0.9.tar.gz/libest-1.0.9/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the relevant file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='libest',
+    version='1.0.9',
+    packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
+    url='https://github.com/jneureuther/libest',
+    license='CC BY-SA 4.0',
+    author='Julian Neureuther',
+    author_email='dev@jneureuther.de',
+    description='Library to communicate with Exercise Submission Tool (https://est.informatik.uni-erlangen.de)',
+    long_description=long_description,
+    install_requires=['requests', 'python-magic', 'beautifulsoup4']
+)
