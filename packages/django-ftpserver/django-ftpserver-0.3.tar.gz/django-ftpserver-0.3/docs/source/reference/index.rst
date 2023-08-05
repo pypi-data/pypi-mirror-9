54f1c12806ee6878982507e2b3092a2eb13a9a8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:36 +0000

    Add package django-ftpserver/0.3/django-ftpserver-0.3.tar.gz

---
+++ packages/django-ftpserver/django-ftpserver-0.3.tar.gz/django-ftpserver-0.3/docs/source/reference/index.rst
@@ -0,0 +1,11 @@
+=========
+Reference
+=========
+
+.. toctree::
+   :maxdepth: 1
+
+   django_ftpserver.admin
+   django_ftpserver.authorizers
+   django_ftpserver.models
+   django_ftpserver.utils
---
+++ packages/django-ftpserver/django-ftpserver-0.3.tar.gz/django-ftpserver-0.3/setup.py
@@ -0,0 +1,35 @@
+import os
+from setuptools import setup, find_packages
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+
+def read(filename):
+    fullpath = os.path.join(current_dir, filename)
+    try:
+        with open(fullpath) as f:
+            return f.read()
+    except Exception:
+        return ""
+
+
+setup(
+    name='django-ftpserver',
+    version='0.3',
+    description="FTP server application for Django.",
+    long_description=read('README.rst'),
+    package_dir={'': 'src'},
+    packages=find_packages('src'),
+    author='Shinya Okano',
+    author_email='tokibito@gmail.com',
+    url='https://github.com/tokibito/django-ftpserver',
+    install_requires=['Django>=1.4', 'pyftpdlib'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Framework :: Django',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'License :: OSI Approved :: MIT License',
+    ])
