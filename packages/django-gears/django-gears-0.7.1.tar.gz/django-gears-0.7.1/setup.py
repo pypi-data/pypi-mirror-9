5474ea47a7ac09568c7144128465f81c92d3c91a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:03 +0000

    Add package django-gears/0.7.1/django-gears-0.7.1.tar.gz

---
+++ packages/django-gears/django-gears-0.7.1.tar.gz/django-gears-0.7.1/setup.py
@@ -0,0 +1,33 @@
+import os
+from setuptools import setup, find_packages
+
+
+def read(filename):
+    return open(os.path.join(os.path.dirname(__file__), filename)).read()
+
+
+setup(
+    name='django-gears',
+    version='0.7.1',
+    license='ISC',
+    description='Compiles and concatenates JavaScript and CSS assets.',
+    long_description=read('README.rst'),
+    url='https://github.com/gears/django-gears',
+    author='Mike Yumatov',
+    author_email='mike@yumatov.org',
+    packages=find_packages(exclude=['tests', 'tests.*']),
+    include_package_data=True,
+    install_requires=[
+        'Gears>=0.7',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: ISC License (ISCL)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet :: WWW/HTTP'],
+)
