9bbc3c5888a37a9b3536d7274c32b301a2b58940
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:15 +0000

    Add package metapub/0.3.2/metapub-0.3.2.tar.gz

---
+++ packages/metapub/metapub-0.3.2.tar.gz/metapub-0.3.2/metapub/text_mining.py
@@ -0,0 +1,13 @@
+import re
+
+re_doi = re.compile(r'(10[.][0-9]{2,}(?:[.][0-9]+)*/(?:(?!["&\'<>])\S)+)')
+
+def findall_dois_in_text(body):
+    return re_doi.findall(body)
+
+def find_doi_in_string(body):
+    try:
+        return re_doi.findall(body)[0]
+    except IndexError:
+        return None
+
