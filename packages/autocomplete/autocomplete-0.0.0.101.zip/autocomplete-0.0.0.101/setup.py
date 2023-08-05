fa14cb55116d5ab438cbb926130e3ac260570728
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:46 +0000

    Add package autocomplete/0.0.0.101/autocomplete-0.0.0.101.zip

---
+++ packages/autocomplete/autocomplete-0.0.0.101.zip/autocomplete-0.0.0.101/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+setup(name='autocomplete',
+      version='0.0.0.101',
+      description='tiny \'autocomplete\' engine',
+      keywords='autocomplete autosuggest suggest complete spell spellsuggest \
+                automatic spelling word suggest machine learning ai text \
+                conditional probability model probabilistic perspective \
+                Rodrigo Palacios rodrigo palacios im-rodrigo im_rodrigo \
+                rodricios',
+
+      author='Rodrigo Palacios',
+      author_email='rodrigopala91@gmail.com',
+      license='MIT',
+      packages=['autocomplete'],
+      install_requires=['bottle'],
+      url='https://github.com/rodricios/autocomplete',
+      scripts=['bin/autocomplete_server.py'],
+      data_files=[('autocomplete',['big.txt', 'models_compressed.pkl'])],
+      test_suite='nose.collector',
+      tests_require=['nose'],
+      include_package_data=True,
+      zip_safe=False)
