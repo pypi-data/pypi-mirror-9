a22cef25a22a8349e360c22704f94ffba2db58b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:13 +0000

    Add package evepaste/0.7/evepaste-0.7.tar.gz

---
+++ packages/evepaste/evepaste-0.7.tar.gz/evepaste-0.7/evepaste/exceptions.py
@@ -0,0 +1,12 @@
+"""
+evepaste.exceptions
+~~~~~~~~~~~~~~~~~~~
+
+"""
+
+
+class Unparsable(Exception):
+    """
+    This exception is raised whenever the given text is not parsable.
+    """
+    pass
---
+++ packages/evepaste/evepaste-0.7.tar.gz/evepaste-0.7/evepaste/parsers/survey_scanner.py
@@ -0,0 +1,31 @@
+"""
+evepaste.parsers.survey_scanner
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+Parse survey scanner results.
+
+"""
+import re
+
+from evepaste.utils import regex_match_lines, f_int
+
+SURVEY_SCANNER_RE = re.compile(r"""^([\S ]+)\t           # name
+                                    ([\d,'\.]+)\t        # quantity
+                                    ([\d,'\.]*\ (m|km))$ # distance
+                                """, re.X)
+
+
+def parse_survey_scanner(lines):
+    """ Parse survey scanner
+
+    :param string paste_string: A survey scanner result string
+    """
+    matches, bad_lines = regex_match_lines(SURVEY_SCANNER_RE, lines)
+
+    result = [{'name': name,
+               'quantity': f_int(quantity),
+               'distance': distance}
+              for name, quantity, distance, _ in matches]  # NOQA (F812)
+    if matches:
+        return result, []
+    else:
+        return result, bad_lines
