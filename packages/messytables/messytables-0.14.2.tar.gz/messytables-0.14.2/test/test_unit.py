35024043a5342200b9151aae61f75fefafebeecb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:23 +0000

    Add package messytables/0.14.2/messytables-0.14.2.tar.gz

---
+++ packages/messytables/messytables-0.14.2.tar.gz/messytables-0.14.2/test/test_unit.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+import unittest
+
+from messytables import dateparser, Cell
+
+
+class DateParserTest(unittest.TestCase):
+    def test_date_regex(self):
+        assert dateparser.is_date('2012 12 22')
+        assert dateparser.is_date('2012/12/22')
+        assert dateparser.is_date('2012-12-22')
+        assert dateparser.is_date('22.12.2012')
+        assert dateparser.is_date('12 12 22')
+        assert dateparser.is_date('22 Dec 2012')
+        assert dateparser.is_date('2012 12 22 13:17')
+        assert dateparser.is_date('2012 12 22 T 13:17')
+
+
+class CellReprTest(unittest.TestCase):
+    def test_repr_ok(self):
+        repr(Cell(value=u"\xa0"))
