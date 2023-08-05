d4146aae7dc1a7521ebeb94f8c8686e88c7d8998
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:34 +0000

    Add package Flask-Micropub/0.2.1/Flask-Micropub-0.2.1.tar.gz

---
+++ packages/Flask-Micropub/Flask-Micropub-0.2.1.tar.gz/Flask-Micropub-0.2.1/setup.py
@@ -0,0 +1,41 @@
+"""
+Flask-Micropub
+--------------
+
+This extension adds the ability to login to a Flask-based website
+using IndieAuth (https://indiewebcamp.com/IndieAuth), and to request
+a Micropub (https://indiewebcamp.com/Micropub) access token.
+"""
+from setuptools import setup
+
+
+setup(
+    name='Flask-Micropub',
+    version='0.2.1',
+    url='https://github.com/kylewm/flask-micropub/',
+    license='BSD',
+    author='Kyle Mahan',
+    author_email='kyle@kylewm.com',
+    description='Adds support for Micropub clients.',
+    long_description=__doc__,
+    py_modules=['flask_micropub'],
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    install_requires=[
+        'Flask',
+        'requests',
+        'BeautifulSoup4',
+    ],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
