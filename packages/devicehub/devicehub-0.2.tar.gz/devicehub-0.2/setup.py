688f7766896f03992b0e3cd59f1be56991b137c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:05 +0000

    Add package devicehub/0.2/devicehub-0.2.tar.gz

---
+++ packages/devicehub/devicehub-0.2.tar.gz/devicehub-0.2/setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+__author__ = 'Ionut Cotoi'
+from distutils.core import setup
+
+setup(name='devicehub',
+      version='0.2',
+      package_dir={'devicehub': 'devicehub'},
+      packages=['devicehub'],
+      )
