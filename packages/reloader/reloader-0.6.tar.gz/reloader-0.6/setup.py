b95b334e9451cdb4f9761affe3660da8ecf363d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:46 +0000

    Add package reloader/0.6/reloader-0.6.tar.gz

---
+++ packages/reloader/reloader-0.6.tar.gz/reloader-0.6/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+version = __import__('reloader').__version__
+
+setup(
+    name = 'reloader',
+    version = version,
+    description = 'Dependency-based Python module reloader',
+    author = 'Jon Parise',
+    author_email = 'jon@indelible.org',
+    license = "MIT License",
+    url = "https://github.com/jparise/python-reloader",
+    download_url = 'https://github.com/jparise/python-reloader/tarball/' + version,
+    classifiers = ['Intended Audience :: Developers',
+                   'License :: OSI Approved :: MIT License',
+                   'Development Status :: 4 - Beta',
+                   'Operating System :: OS Independent',
+                   'Programming Language :: Python'],
+    py_modules = ['reloader'],
+    test_suite = 'tests',
+)
