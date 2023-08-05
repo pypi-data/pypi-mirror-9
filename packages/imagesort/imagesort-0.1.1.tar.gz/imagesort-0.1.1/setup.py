eac5decba335b864709af640e711bd234eddc508
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:44 +0000

    Add package imagesort/0.1.1/imagesort-0.1.1.tar.gz

---
+++ packages/imagesort/imagesort-0.1.1.tar.gz/imagesort-0.1.1/setup.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+import os
+
+
+def readfile(fname):
+    with open(os.path.join(os.path.dirname(__file__), fname)) as f:
+        return f.read()
+
+
+setup(
+    name="imagesort",
+    version="0.1.1",
+    packages=find_packages(),
+    install_requires=[
+        'ExifRead>=1.4.0',
+    ],
+    include_package_data=True,
+
+    author="Børge Lanes",
+    author_email="borge.lanes@gmail.com",
+    description=('Organize image files by date taken'),
+    long_description=readfile("README.rst"),
+    license="MIT",
+    keywords="media",
+    url="https://github.com/leinz/imagesort",
+
+    entry_points={
+        'console_scripts': [
+            'imagesort = imagesort.imagesort:main',
+        ]
+    },
+
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: MacOS :: MacOS X',
+        'Topic :: Multimedia',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+    ],
+)
