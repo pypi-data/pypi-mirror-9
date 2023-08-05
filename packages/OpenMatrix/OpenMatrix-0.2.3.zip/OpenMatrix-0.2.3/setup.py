f243f0a11b79ed9f483742b5bf048e9c42d0ffb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:15 +0000

    Add package OpenMatrix/0.2.3/OpenMatrix-0.2.3.zip

---
+++ packages/OpenMatrix/OpenMatrix-0.2.3.zip/OpenMatrix-0.2.3/setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+
+setup(
+    name='OpenMatrix',
+    version='0.2.3',
+    author='Billy Charlton',
+    author_email='billy@worldofbilly.com',
+    packages=['openmatrix', 'openmatrix.test'],
+    url='https://sites.google.com/site/openmodeldata',
+    license='Apache',
+    description='OMX, the open matrix data format.',
+    long_description=open('README.txt').read(),
+    install_requires=[
+        "tables >= 2.3.0",
+        "numpy >= 1.5.0",
+    ],
+    classifiers=[
+        'License :: OSI Approved :: Apache Software License'
+    ]
+)
