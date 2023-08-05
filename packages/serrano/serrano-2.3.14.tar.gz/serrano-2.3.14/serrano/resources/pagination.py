a5e4de7bca6d3bb9e04680344a076fedf3f0cfd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:23 +0000

    Add package serrano/2.3.14/serrano-2.3.14.tar.gz

---
+++ packages/serrano/serrano-2.3.14.tar.gz/serrano-2.3.14/serrano/resources/pagination.py
@@ -0,0 +1,116 @@
+from django.db.models.query import QuerySet
+from django.core.cache import cache
+from django.core.paginator import Paginator
+from avocado.core.cache import cache_key
+from avocado.conf import settings
+from restlib2.params import Parametizer, IntParam
+from restlib2.resources import Resource
+
+__all__ = ('PaginatorResource', 'PaginatorParametizer')
+
+
+def _count(s):
+    if isinstance(s, QuerySet):
+        return s.count()
+
+    return len(s)
+
+
+class PaginatorParametizer(Parametizer):
+    page = IntParam(1)
+    limit = IntParam(20)
+
+
+class PaginatorResource(Resource):
+    parametizer = PaginatorParametizer
+
+    def get_paginator(self, queryset, limit):
+        paginator = Paginator(queryset, per_page=limit)
+        paginator.has_limit = bool(limit)
+
+        # Cache count for paginator to prevent redundant calls between requests
+        if settings.DATA_CACHE_ENABLED:
+            key = cache_key('paginator', kwargs={
+                'queryset': queryset
+            })
+
+            count = cache.get(key)
+
+            if count is None:
+                count = _count(queryset)
+                cache.set(key, count)
+        else:
+            count = _count(queryset)
+
+        paginator._count = count
+
+        if not limit:
+            # Prevent division by zero error in case count is zero
+            paginator.per_page = max(count, 1)
+
+        return paginator
+
+    def get_page_links(self, request, path, page, extra=None):
+        "Returns the page links."
+        uri = request.build_absolute_uri
+
+        # format string will be expanded below
+        if page.paginator.has_limit:
+            limit = page.paginator.per_page
+            params = {
+                'limit': '{limit}',
+                'page': '{page}',
+            }
+        else:
+            limit = None
+            params = {
+                'limit': '0',
+            }
+
+        if extra:
+            for key, value in extra.items():
+                # Use the original GET parameter if supplied and if the
+                # cleaned value is valid
+                if key in request.GET and value is not None and value != '':
+                    params.setdefault(key, request.GET.get(key))
+
+        # Stringify parameters. Since these are the original GET params,
+        # they do not need to be encoded
+        pairs = sorted(['{0}={1}'.format(k, v) for k, v in params.items()])
+
+        # Create path string
+        path_format = '{0}?{1}'.format(path, '&'.join(pairs))
+
+        links = {
+            'self': {
+                'href': uri(path_format.format(page=page.number,
+                                               limit=limit)),
+            },
+            'base': {
+                'href': uri(path),
+            }
+        }
+
+        if page.has_previous():
+            links['prev'] = {
+                'href': uri(
+                    path_format.format(page=page.previous_page_number(),
+                                       limit=limit)),
+            }
+
+        if page.has_next():
+            links['next'] = {
+                'href': uri(
+                    path_format.format(page=page.next_page_number(),
+                                       limit=limit)),
+            }
+
+        return links
+
+    def get_page_response(self, request, paginator, page):
+        return {
+            'count': paginator.count,
+            'limit': paginator.per_page if paginator.has_limit else 0,
+            'num_pages': paginator.num_pages,
+            'page_num': page.number,
+        }
---
+++ packages/serrano/serrano-2.3.14.tar.gz/serrano-2.3.14/setup.py
@@ -0,0 +1,59 @@
+import sys
+from setuptools import setup, find_packages
+
+install_requires = [
+    'avocado>=2.3.12,<2.4',
+    'restlib2>=0.4.2,<0.5.0',
+    'django-preserialize>=1.0.7,<1.1',
+]
+
+if sys.version_info < (2, 7):
+    install_requires.append('ordereddict>=1.1')
+
+kwargs = {
+    # Packages
+    'packages': find_packages(exclude=['tests', '*.tests', '*.tests.*',
+                                       'tests.*']),
+    'include_package_data': True,
+
+    # Dependencies
+    'install_requires': install_requires,
+
+    # Test dependencies
+    'tests_require': [
+        'avocado[permissions,search,extras]>=2.3.12,<2.4'
+        'coverage',
+        'whoosh',
+        'python-memcached>=1.48',
+        'django-objectset>=0.2.3',
+    ],
+
+    'test_suite': 'test_suite',
+
+    # Optional dependencies
+    'extras_require': {},
+
+    # Metadata
+    'name': 'serrano',
+    'version': __import__('serrano').get_version(),
+    'author': 'Byron Ruth',
+    'author_email': 'b@devel.io',
+    'description': 'Hypermedia implementation for Avocado',
+    'license': 'BSD',
+    'keywords': 'hypermedia rest api avocado serrano cilantro harvest',
+    'url': 'http://github.com/cbmi/serrano',
+    'classifiers': [
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Framework :: Django',
+        'Topic :: Internet :: WWW/HTTP',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Healthcare Industry',
+        'Intended Audience :: Information Technology',
+    ],
+}
+
+setup(**kwargs)
