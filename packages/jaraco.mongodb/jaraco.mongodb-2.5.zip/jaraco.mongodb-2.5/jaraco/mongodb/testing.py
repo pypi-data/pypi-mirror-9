63d40bca6e35a50ef243fff6ad4ad5aae2889a60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:46 +0000

    Add package jaraco.mongodb/2.5/jaraco.mongodb-2.5.zip

---
+++ packages/jaraco.mongodb/jaraco.mongodb-2.5.zip/jaraco.mongodb-2.5/jaraco/mongodb/testing.py
@@ -0,0 +1,44 @@
+import pprint
+import textwrap
+
+from .query import compat_explain
+
+
+def _rep_index_info(coll):
+    index_info = coll.index_information()
+    return "Indexes are:\n" + pprint.pformat(index_info)
+
+
+def assert_covered(cur):
+    """
+    Use the best knowledge about Cursor.explain() to ensure that the query
+    was covered by an index.
+    """
+    explanation = compat_explain(cur)
+    tmpl = textwrap.dedent("""
+        Query was not covered:
+        {explanation}
+        """).lstrip()
+    report = tmpl.format(explanation=pprint.pformat(explanation))
+    report += _rep_index_info(cur.collection)
+    docs = explanation['executionStats']['totalDocsExamined']
+    assert docs == 0, report
+
+
+def assert_distinct_covered(coll, field, query):
+    """
+    Ensure a distinct query is covered by an index.
+    """
+    assert coll.count(), "Unable to assert without a document"
+    db = coll.database
+    res = db.command('distinct', coll.name, key=field, query=query)
+    assert 'stats' in res, "Stats not supplied. Maybe SERVER-9126?"
+    stats = res['stats']
+    tmpl = textwrap.dedent("""
+        Distinct query was not covered:
+        {explanation}
+        """).lstrip()
+    report = tmpl.format(explanation=pprint.pformat(stats))
+    report += _rep_index_info(coll)
+    assert stats['nscannedObjects'] == 0, report
+    assert stats['n'], "No documents matched"
