741153228f861e05a21d4b79e94cd2fe1cb613fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:14 +0000

    Add package junos-eznc/1.1.0/junos-eznc-1.1.0.zip

---
+++ packages/junos-eznc/junos-eznc-1.1.0.zip/junos-eznc-1.1.0/setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup, find_packages
+
+# parse requirements
+req_lines = [line.strip() for line in open(
+    'requirements.txt').readlines()]
+install_reqs = list(filter(None, req_lines))
+
+setup(
+    name="junos-eznc",
+    namespace_packages=['jnpr'],
+    version="1.1.0",
+    author="Jeremy Schulman",
+    author_email="jnpr-community-netdev@juniper.net",
+    description=("Junos 'EZ' automation for non-programmers"),
+    license="Apache 2.0",
+    keywords="Junos NETCONF networking automation",
+    url="http://www.github.com/Juniper/py-junos-eznc",
+    package_dir={'': 'lib'},
+    packages=find_packages('lib'),
+    package_data={
+        'jnpr.junos.op': ['*.yml'],
+        'jnpr.junos.cfgro': ['*.yml']
+    },
+    install_requires=install_reqs,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: System Administrators',
+        'Intended Audience :: Telecommunications Industry',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: System :: Networking',
+        'Topic :: Text Processing :: Markup :: XML'
+    ],
+)
