03d8dd58278493c6d96691dea807d8d3657e9303
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:49 +0000

    Add package django-pyodbc-azure/1.1.8/django-pyodbc-azure-1.1.8.zip

---
+++ packages/django-pyodbc-azure/django-pyodbc-azure-1.1.8.zip/django-pyodbc-azure-1.1.8/setup.py
@@ -0,0 +1,35 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+CLASSIFIERS=[
+    'Development Status :: 4 - Beta',
+    'License :: OSI Approved :: BSD License',
+    'Framework :: Django',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 2.6',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.2',
+    'Programming Language :: Python :: 3.3',
+    'Topic :: Internet :: WWW/HTTP',
+]
+
+setup(
+    name='django-pyodbc-azure',
+    version='1.1.8',
+    description='Django backend for Microsoft SQL Server and Azure SQL Database using pyodbc',
+    long_description=open('README.rst').read(),
+    author='Michiya Takahashi',
+    url='https://github.com/michiya/django-pyodbc-azure',
+    license='BSD',
+    packages=['sql_server', 'sql_server.pyodbc'],
+    install_requires=[
+        'Django>=1.6,<1.7',
+        'pyodbc>=3.0',
+    ],
+    classifiers=CLASSIFIERS,
+    keywords='azure django',
+)
