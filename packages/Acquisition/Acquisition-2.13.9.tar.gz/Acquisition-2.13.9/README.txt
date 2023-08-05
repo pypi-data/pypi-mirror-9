d2c6af2034ee98a3f1904dad4192497417cf6d9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:34 +0000

    Add package Acquisition/2.13.9/Acquisition-2.13.9.tar.gz

---
+++ packages/Acquisition/Acquisition-2.13.9.tar.gz/Acquisition-2.13.9/README.txt
@@ -0,0 +1,1 @@
+Please refer to src/Acquisition/README.txt.
---
+++ packages/Acquisition/Acquisition-2.13.9.tar.gz/Acquisition-2.13.9/setup.py
@@ -0,0 +1,44 @@
+##############################################################################
+#
+# Copyright (c) 2007 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Setup for the Acquisition distribution
+"""
+import os
+from setuptools import setup, find_packages, Extension
+
+setup(name='Acquisition',
+      version = '2.13.9',
+      url='http://pypi.python.org/pypi/Acquisition',
+      license='ZPL 2.1',
+      description="Acquisition is a mechanism that allows objects to obtain "
+      "attributes from the containment hierarchy they're in.",
+      author='Zope Foundation and Contributors',
+      author_email='zope-dev@zope.org',
+      long_description=open(
+          os.path.join('src', 'Acquisition', 'README.txt')).read() + '\n' +
+          open('CHANGES.txt').read(),
+
+      packages=find_packages('src'),
+      package_dir={'': 'src'},
+
+      ext_modules=[Extension("Acquisition._Acquisition",
+                             [os.path.join('src', 'Acquisition',
+                                           '_Acquisition.c')],
+                             include_dirs=['include', 'src']),
+                   ],
+      install_requires=[
+        'ExtensionClass<4.0.dev',
+        'zope.interface'],
+      include_package_data=True,
+      zip_safe=False,
+      )
