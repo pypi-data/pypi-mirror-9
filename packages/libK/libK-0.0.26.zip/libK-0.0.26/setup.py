224ab5e6702abed11e0eb8cc4e70f81003e5afb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:47 +0000

    Add package libK/0.0.26/libK-0.0.26.zip

---
+++ packages/libK/libK-0.0.26.zip/libK-0.0.26/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+from setuptools.command.install import install
+
+
+class CustomInstallCommand(install):
+    """Customized setuptools install command - prints a friendly greeting."""
+
+    def run(self):
+        print "Hello, developer, how are you? :)"
+        install.run(self)
+
+
+setup(
+    name='libK',
+    version='0.0.26',
+    packages=['libK'],
+    url='https://bitbucket.org/Negash/libk',
+    license='',
+    author='Negash',
+    author_email='i@negash.ru',
+    description='Lib for fast coding',
+    install_requires=['pycurl', 'paramiko', 'pyquery', 'BeautifulSoup'],
+    cmdclass={
+        'install': CustomInstallCommand,
+    }
+)
