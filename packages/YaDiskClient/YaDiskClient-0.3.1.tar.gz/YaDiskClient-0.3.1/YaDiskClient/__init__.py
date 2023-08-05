6ebe141f5ffe2699b684688b2d02a6c3049458ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:05:54 +0000

    Add package YaDiskClient/0.3.1/YaDiskClient-0.3.1.tar.gz

---
+++ packages/YaDiskClient/YaDiskClient-0.3.1.tar.gz/YaDiskClient-0.3.1/YaDiskClient/__init__.py
@@ -0,0 +1,6 @@
+"""
+Client for Yandex.Disk.
+"""
+__version__ = '0.3.1'
+
+from .YaDiskClient import YaDiskException, YaDisk
---
+++ packages/YaDiskClient/YaDiskClient-0.3.1.tar.gz/YaDiskClient-0.3.1/setup.py
@@ -0,0 +1,19 @@
+#-*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+import YaDiskClient
+
+setup(
+    name='YaDiskClient',
+    version=YaDiskClient.__version__,
+    include_package_data=True,
+    py_modules=['YaDiskClient'],
+    url='https://github.com/TyVik/YaDiskClient',
+    license='MIT',
+    author='TyVik',
+    author_email='tyvik8@gmail.com',
+    description='Clent for Yandex.Disk',
+    long_description=open('README.rst').read(),
+    install_requires=['requests'],
+    packages=find_packages(),
+#    test_suite='YaDiskClient.TestYaDisk' # this line is commented because tests needs Yandex login and password
+)
