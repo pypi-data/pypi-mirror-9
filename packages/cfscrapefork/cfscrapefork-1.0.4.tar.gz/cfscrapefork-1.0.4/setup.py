f01898f942c41544ad32d74f9d6990a12562ed4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:47 +0000

    Add package cfscrapefork/1.0.4/cfscrapefork-1.0.4.tar.gz

---
+++ packages/cfscrapefork/cfscrapefork-1.0.4.tar.gz/cfscrapefork-1.0.4/setup.py
@@ -0,0 +1,17 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+      name='cfscrapefork',
+      version='1.0.4',
+      author='Steven Veshkini',
+      author_email='sveshkini@berkeley.edu',
+      packages=['cfscrape'],
+      url='https://github.com/StevenVeshkini/cloudflare-scrape',
+      description='cfscrapefork',
+      install_requires=[
+            "PyExecJS >= 1.1.0",
+            ],
+      )
