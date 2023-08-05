7b9b20225d282ab52a531fa229709eb34c389172
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:46 +0000

    Add package pychedelic/0.1.1/pychedelic-0.1.1.tar.gz

---
+++ packages/pychedelic/pychedelic-0.1.1.tar.gz/pychedelic-0.1.1/pychedelic/chunk.py
@@ -0,0 +1,114 @@
+import numpy
+import math
+
+from .config import config
+from .core import wav
+
+
+def ramp(initial, *values):
+    """
+    Generates a ramp array.
+
+    The following example generates a ramp that starts from 0, go to 1 in 10 seconds,
+    then go back to 0 in 5 seonds: 
+
+        >>> chunk.ramp(0, (1, 10), (0, 5))
+    """
+    ramp_blocks = []
+    for start, step, frame_count in _iter_ramps(initial, values):
+        block = numpy.ones((frame_count, 1)) * step
+        block = start - step + numpy.cumsum(block, axis=0)
+        ramp_blocks.append(block)
+    return numpy.concatenate(ramp_blocks)
+
+
+def _iter_ramps(initial, values):
+    values = list(values)
+    previous_target = initial
+    # This is used to fix the total duration of the ramps, and avoid accumulating
+    # rounding errors.
+    frames_offset = 0 
+
+    while len(values):
+        target, duration = values.pop(0)
+        frame_count = duration * config.frame_rate
+        frames_offset += frame_count % 1
+        frame_count = int(frame_count) + int(frames_offset)
+        frames_offset = frames_offset % 1
+
+        step = (target - previous_target) / float(frame_count - 1)
+        yield previous_target, step, frame_count
+        previous_target = target
+
+
+def resample(block, ratio):
+    """
+    Resamples `block`, returning a new block that has a play rate of `ratio`.
+    """
+    if ratio == 1: return block
+
+    frame_count_in = block.shape[0]
+    frame_count_out = math.floor((frame_count_in - 1) / ratio) + 1
+    x_in = numpy.arange(0, frame_count_in)
+    x_out = numpy.arange(0, frame_count_out) * ratio
+    
+    block_out = []
+    for block_ch in block.T:
+        block_out.append(numpy.interp(x_out, x_in, block_ch))
+    block_out = numpy.vstack(block_out).transpose()
+
+    return block_out
+
+
+def fix_channel_count(block, channel_count):
+    """
+    Up-mix / down-mix `block` to `channel_count` channels.
+    If `block` has too many channels, the extra channels are simply cropped,
+    If `block` doesn't have enough channels, the extra channels are copied from
+    the last channel available.
+    """
+    if block.shape[1] == channel_count: return block
+    elif block.shape[1] > channel_count:
+        return block[:,:channel_count]
+    elif block.shape[1] < channel_count:
+        return numpy.hstack([block, block[:,-1:]])
+
+
+def fix_frame_count(block, frame_count):
+    """
+    Fix the number of frames, bringing it to `frame_count` by adding or removing 
+    frames at the end of `block`.
+    """
+    sign = numpy.sign(frame_count)
+    frame_count = abs(frame_count)
+    if block.shape[0] == frame_count: return block
+    elif block.shape[0] < frame_count:
+        extra_frames = numpy.zeros((frame_count - block.shape[0], block.shape[1]))
+        if (sign == 1):
+            return numpy.vstack([block, extra_frames])
+        else:
+            return numpy.vstack([extra_frames, block])
+    elif block.shape[0] > frame_count:
+        if (sign == 1):
+            return block[:frame_count,:]
+        else:
+            return block[-frame_count:,:]
+
+
+def reshape(block, channel_count=None, frame_count=None):
+    """
+    Just combines `fix_frame_count` and `fix_channel_count` in one more handy function.
+    """
+    if channel_count != None:
+        block = fix_channel_count(block, channel_count)
+    if frame_count != None:
+        block = fix_frame_count(block, frame_count)
+    return block
+
+
+def read_wav(file_path):
+    """
+    Reads a whole wav file. Returns a tuple `(<samples>, <infos>)`.
+    """
+    wfile, infos = wav.open_read_mode(file_path)
+    return wav.read_all(wfile), infos
---
+++ packages/pychedelic/pychedelic-0.1.1.tar.gz/pychedelic-0.1.1/pychedelic/core/wav.py
@@ -0,0 +1,67 @@
+# TODO: support for 8-bit wavs ?
+import wave
+
+from . import pcm
+
+
+def open_write_mode(f, frame_rate, channel_count):
+    wfile = wave.open(f, mode='wb')
+    wfile.setsampwidth(2)
+    wfile.setframerate(frame_rate)
+    wfile.setnchannels(channel_count)
+    return wfile, _get_file_infos(wfile)
+
+
+def open_read_mode(f):
+    try:
+        wfile = wave.open(f, mode='rb')
+    except wave.Error as err:
+        raise FormatError(err)
+    sample_width = wfile.getsampwidth()       # Sample width in byte
+    if sample_width != 2:
+        raise FormatError('Sample width %s not supported yet' % sample_width)
+    return wfile, _get_file_infos(wfile)
+
+
+def seek(wfile, position, end=None):
+    end_frame = wfile.getnframes()
+    if end != None: end_frame = min(end * wfile.getframerate(), end_frame)
+    position_frame = position * wfile.getframerate()
+    wfile.setpos(int(round(position_frame)))
+    return int(round(end_frame - position_frame))
+
+
+def read_all(wfile):
+    start_frame = wfile.tell()
+    end_frame = wfile.getnframes()
+    frame_count = end_frame - start_frame
+    return pcm.string_to_samples(wfile.readframes(frame_count), wfile.getnchannels())
+
+
+def read_block(wfile, block_size):
+    start_frame = wfile.tell()
+    end_frame = min(start_frame + block_size, wfile.getnframes())
+    frame_count = end_frame - start_frame
+    return pcm.string_to_samples(wfile.readframes(frame_count), wfile.getnchannels())
+
+
+def write_block(wfile, block):
+    wfile.writeframes(pcm.samples_to_string(block))
+
+
+def _get_file_infos(wfile):
+    frame_rate = wfile.getframerate()
+    return {
+        'frame_rate': frame_rate,
+        'channel_count': wfile.getnchannels(),
+        'frame_count': wfile.getnframes(),
+        'duration': wfile.getnframes() / float(frame_rate),
+        'bit_depth': wfile.getsampwidth() * 8
+    }
+
+
+class FormatError(Exception):
+    """
+    Raised when attempting to read a wave file failed.
+    """
+    pass
