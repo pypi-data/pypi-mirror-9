fde07a36d57ba8f5c165a800a73f8e33029a39a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:55 +0000

    Add package iter_karld_tools/0.9.2/iter_karld_tools-0.9.2.tar.gz

---
+++ packages/iter_karld_tools/iter_karld_tools-0.9.2.tar.gz/iter_karld_tools-0.9.2/docs/source/iter_karld_tools.rst
@@ -0,0 +1,28 @@
+iter_karld_tools Package
+=============
+
+:mod:`iter_karld_tools` Package
+--------------------
+
+.. automodule:: iter_karld_tools
+    :members:
+    :undoc-members:
+    :show-inheritance:
+
+:mod:`_meta` Module
+-------------------
+
+.. automodule:: iter_karld_tools._meta
+    :members:
+    :undoc-members:
+    :show-inheritance:
+
+
+
+Subpackages
+-----------
+
+.. toctree::
+
+    iter_karld_tools.tests
+
---
+++ packages/iter_karld_tools/iter_karld_tools-0.9.2.tar.gz/iter_karld_tools-0.9.2/setup.py
@@ -0,0 +1,83 @@
+import os
+import sys
+import uuid
+
+try:
+    from setuptools import find_packages
+    from setuptools import setup
+    packages = find_packages()
+except ImportError:
+    from distutils.core import setup
+    packages = ['iter_karld_tools', 'iter_karld_tools.tests']
+
+from pip.req import parse_requirements
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    sys.exit()
+
+
+def read(fname):
+    """
+    Open and read a filename in this directory.
+    :param fname: `str` file name in this directory
+
+    Returns contents of file fname
+    """
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+def get_version():
+    import imp
+
+    with open('iter_karld_tools/_meta.py', 'rb') as fp:
+        mod = imp.load_source('_meta', 'iter_karld_tools', fp)
+
+    return mod.version
+
+
+def get_requirements(filename):
+    try:
+        reqs = list(parse_requirements(filename))
+    except TypeError:
+        reqs = list(parse_requirements(filename, session=uuid.uuid1()))
+
+    return [str(r.req) for r in reqs]
+
+
+def get_test_requires():
+    return get_requirements('requirements_dev.txt')
+
+
+try:
+    license_info = open('LICENSE').read()
+except:
+    license_info = 'APACHE 2.0'
+
+setup_args = dict(
+    name="iter_karld_tools",
+    version=get_version(),
+    author="John W Lockwood IV",
+    author_email="john@tackletronics.com",
+    description="More tools for iterators",
+    license=license_info,
+    keywords="data",
+    url="https://github.com/johnwlockwood/iter_karld_tools",
+    package_dir={'iter_karld_tools': 'iter_karld_tools'},
+    packages=packages,
+    tests_require=get_test_requires(),
+    long_description=read('README.rst'),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'Natural Language :: English',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+    ],
+)
+
+if __name__ == '__main__':
+    setup(**setup_args)
