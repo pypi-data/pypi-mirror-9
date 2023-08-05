6a852742746d10d8ad933abac05735a5f79e8697
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:15 +0000

    Add package pyggplot/7/pyggplot-7.tar.gz

---
+++ packages/pyggplot/pyggplot-7.tar.gz/pyggplot-7/setup.py
@@ -0,0 +1,21 @@
+try:
+    from setuptools import setup
+except:
+    from distutils.core import setup
+
+setup(
+    name='pyggplot',
+    version='7',
+    packages=['pyggplot',],
+    license='BSD',
+    #url='http://code.google.com/p/pydataframe/',
+    author='Florian Finkernagel',
+    description = "A pythonic wrapper around R's ggplot",
+    author_email='finkernagel@coonabibba.de',
+    long_description=open('README.md').read(),
+    install_requires=[
+        'pandas>=0.15',
+        'rpy2',
+        'ordereddict',
+        ]
+)
