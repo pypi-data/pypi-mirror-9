7d2551e74ea1224bc7add69b7a8aeed12adf567c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:34 +0000

    Add package recaptcha2/0.0/recaptcha2-0.0.tar.gz

---
+++ packages/recaptcha2/recaptcha2-0.0.tar.gz/recaptcha2-0.0/setup.py
@@ -0,0 +1,31 @@
+import os
+from setuptools import setup, find_packages
+
+
+version = "0.0"
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "recaptcha2",
+    version = version,
+    description = "",
+    long_description = read("README.rst"),
+    classifiers = [],
+    keywords = "",
+    author = "Bryan Chow",
+    author_email = "",
+    url = "https://github.com/bryanchow/python-recaptcha2",
+    download_url = (
+        "https://github.com/bryanchow/python-recaptcha2/tarball/master"
+    ),
+    license = "WTFPL",
+    packages = find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    include_package_data = True,
+    zip_safe = False,
+    install_requires = [
+        'furl',
+        'requests',
+    ],
+)
