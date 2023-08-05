db3d125a7a55529fb2b143b3fb1fbe3b10ced0a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:23 +0000

    Add package jmespath/0.6.1/jmespath-0.6.1.tar.gz

---
+++ packages/jmespath/jmespath-0.6.1.tar.gz/jmespath-0.6.1/setup.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import io
+
+from setuptools import setup, find_packages
+
+
+requires = []
+
+
+if sys.version_info[:2] == (2, 6):
+    # For python2.6 we have a few other dependencies.
+    # First we need an ordered dictionary so we use the
+    # 2.6 backport.
+    requires.append('ordereddict==1.1')
+    # Then we need simplejson.  This is because we need
+    # a json version that allows us to specify we want to
+    # use an ordereddict instead of a normal dict for the
+    # JSON objects.  The 2.7 json module has this.  For 2.6
+    # we need simplejson.
+    requires.append('simplejson==3.3.0')
+
+
+setup(
+    name='jmespath',
+    version='0.6.1',
+    description='JSON Matching Expressions',
+    long_description=io.open('README.rst', encoding='utf-8').read(),
+    author='James Saryerwinnie',
+    author_email='js@jamesls.com',
+    url='https://github.com/boto/jmespath',
+    scripts=['bin/jp'],
+    packages=find_packages(exclude=['tests']),
+    install_requires=requires,
+    classifiers=(
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+    ),
+)
