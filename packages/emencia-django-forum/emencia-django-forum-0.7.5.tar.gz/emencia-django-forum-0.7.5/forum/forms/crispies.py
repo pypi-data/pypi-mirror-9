04f94e1dc90fe6221a2efbaf6071a30cba1ae6b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:37 +0000

    Add package emencia-django-forum/0.7.5/emencia-django-forum-0.7.5.tar.gz

---
+++ packages/emencia-django-forum/emencia-django-forum-0.7.5.tar.gz/emencia-django-forum-0.7.5/forum/forms/crispies.py
@@ -0,0 +1,215 @@
+"""
+Crispy forms layouts
+"""
+from django.utils.translation import ugettext_lazy as _
+
+from crispy_forms.helper import FormHelper
+from crispy_forms_foundation.layout import Layout, Row, Column, ButtonHolderPanel, Submit
+
+def category_helper(form_tag=True):
+    """
+    Category's form layout helper
+    """
+    helper = FormHelper()
+    helper.form_action = '.'
+    helper.attrs = {'data_abide': ''}
+    helper.form_tag = form_tag
+    
+    helper.layout = Layout(
+        Row(
+            Column(
+                'title',
+                css_class='small-12'
+            ),
+        ),
+        Row(
+            Column(
+                'slug',
+                css_class='small-12 medium-10'
+            ),
+            Column(
+                'order',
+                css_class='small-12 medium-2'
+            ),
+        ),
+        Row(
+            Column(
+                'description',
+                css_class='small-12'
+            ),
+        ),
+        Row(
+            Column(
+                'visible',
+                css_class='small-12'
+            ),
+        ),
+        ButtonHolderPanel(
+            Submit('submit', _('Submit')),
+            css_class='text-right',
+        ),
+    )
+    
+    return helper
+
+
+
+def thread_helper(form_tag=True, edit_mode=False, for_moderator=False):
+    """
+    Thread's form layout helper
+    """
+    helper = FormHelper()
+    helper.form_action = '.'
+    helper.attrs = {'data_abide': ''}
+    helper.form_tag = form_tag
+    
+    fieldsets = [
+        Row(
+            Column(
+                'subject',
+                css_class='small-12'
+            ),
+        ),
+    ]
+    
+    # Category field only in edit form
+    if edit_mode:
+        fieldsets.append(
+            Row(
+                Column(
+                    'category',
+                    css_class='small-12'
+                ),
+            ),
+        )
+    
+    if for_moderator:
+        fieldsets.append(
+            Row(
+                Column(
+                    'sticky',
+                    css_class='small-12 medium-4'
+                ),
+                Column(
+                    'announce',
+                    css_class='small-12 medium-4'
+                ),
+                Column(
+                    'closed',
+                    css_class='small-12 medium-4'
+                ),
+            ),
+        )
+    
+    # First message is not in edit form
+    if not edit_mode:
+        fieldsets.append(
+            Row(
+                Column(
+                    'text',
+                    css_class='small-12'
+                ),
+            ),
+        )
+    
+    if for_moderator:
+        fieldsets.append(
+            Row(
+                Column(
+                    'visible',
+                    css_class='small-12'
+                ),
+            ),
+        )
+    
+    # Threadwatch option is not in edit form
+    if not edit_mode:
+        fieldsets.append(
+            Row(
+                Column(
+                    'threadwatch',
+                    css_class='small-12'
+                ),
+            ),
+        )
+    
+    fieldsets = fieldsets+[
+        ButtonHolderPanel(
+            Submit('submit', _('Submit')),
+            css_class='text-right',
+        ),
+    ]
+    
+    helper.layout = Layout(*fieldsets)
+    
+    return helper
+
+
+def post_helper(form_tag=True, edit_mode=False):
+    """
+    Post's form layout helper
+    """
+    helper = FormHelper()
+    helper.form_action = '.'
+    helper.attrs = {'data_abide': ''}
+    helper.form_tag = form_tag
+    
+    fieldsets = [
+        Row(
+            Column(
+                'text',
+                css_class='small-12'
+            ),
+        ),
+    ]
+    
+    # Threadwatch option is not in edit form
+    if not edit_mode:
+        fieldsets.append(
+            Row(
+                Column(
+                    'threadwatch',
+                    css_class='small-12'
+                ),
+            ),
+        )
+    
+    fieldsets = fieldsets+[
+        ButtonHolderPanel(
+            Submit('submit', _('Submit')),
+            css_class='text-right',
+        ),
+    ]
+    
+    helper.layout = Layout(*fieldsets)
+    
+    return helper
+
+def post_edit_helper(form_tag=True):
+    return post_helper(form_tag=form_tag, edit_mode=True)
+
+def post_delete_helper(form_tag=True):
+    """
+    Message's delete form layout helper
+    """
+    helper = FormHelper()
+    helper.form_action = '.'
+    helper.attrs = {'data_abide': ''}
+    helper.form_tag = form_tag
+    
+    helper.layout = Layout(
+        ButtonHolderPanel(
+            Row(
+                Column(
+                    'confirm',
+                    css_class='small-12 medium-8'
+                ),
+                Column(
+                    Submit('submit', _('Submit')),
+                    css_class='small-12 medium-4 text-right'
+                ),
+            ),
+        ),
+    )
+    
+    return helper
---
+++ packages/emencia-django-forum/emencia-django-forum-0.7.5.tar.gz/emencia-django-forum-0.7.5/forum/forms/post.py
@@ -0,0 +1,131 @@
+# -*- coding: utf-8 -*-
+"""
+Message post forms
+"""
+from django.conf import settings
+from django import forms
+from django.utils.translation import ugettext_lazy as _
+import django.dispatch
+
+from forum.forms import CrispyFormMixin
+from forum.utils.imports import safe_import_module
+from forum.models import new_message_posted_signal, Category, Post
+
+class PostCreateForm(CrispyFormMixin, forms.ModelForm):
+    """
+    Message post create form
+    """
+    crispy_form_helper_path = 'forum.forms.crispies.post_helper'
+    
+    def __init__(self, *args, **kwargs):
+        self.author = kwargs.pop("user", None)
+        self.thread_instance = kwargs.pop("thread", None)
+        
+        super(PostCreateForm, self).__init__(*args, **kwargs)
+        super(forms.ModelForm, self).__init__(*args, **kwargs)
+        
+        # Set the form field for Post.text
+        field_helper = safe_import_module(settings.FORUM_TEXT_FIELD_HELPER_PATH)
+        if field_helper is not None:
+            self.fields['text'] = field_helper(self, **{'label':_('message'), 'required':True})
+        
+        # Add threadwatch checkbox
+        self.fields['threadwatch'] = forms.BooleanField(label=_("Watch this thread"), initial=True, required=False, help_text=_("You will receive an email notification for each new post in this thread. You can disable it in the thread detail if needed."))
+
+    def clean_text(self):
+        """
+        Text content validation
+        """
+        text = self.cleaned_data.get("text")
+        validation_helper = safe_import_module(settings.FORUM_TEXT_VALIDATOR_HELPER_PATH)
+        if validation_helper is not None:
+            return validation_helper(self, text)
+        else:
+            return text
+    
+    def clean(self):
+        cleaned_data = super(PostCreateForm, self).clean()
+        if not self.author.is_staff and self.thread_instance.closed:
+            raise forms.ValidationError(_("This thread is closed, you can't add it a new post."))
+
+        return cleaned_data
+    
+    def save(self):
+        post_instance = self.thread_instance.post_set.create(
+            author=self.author,
+            text=self.cleaned_data["text"],
+        )
+        
+        # Save thread watch option
+        if self.cleaned_data.get("threadwatch", False) and self.thread_instance.threadwatch_set.filter(owner=self.author).count()==0:
+            threadwatch_instance = self.thread_instance.threadwatch_set.create(owner=self.author)
+        
+        # Find threadwatchs that are not for the message owner (it don't have to notified)
+        threadwatchs = self.thread_instance.threadwatch_set.all().exclude(owner=self.author)
+        if threadwatchs>0:
+            # Sending signal
+            new_message_posted_signal.send(sender=self, post_instance=post_instance, threadwatchs=threadwatchs)
+        
+        return post_instance
+    
+    class Meta:
+        model = Post
+        exclude = ('thread','author')
+
+class PostEditForm(CrispyFormMixin, forms.ModelForm):
+    """
+    Message post edit form
+    """
+    crispy_form_helper_path = 'forum.forms.crispies.post_edit_helper'
+    
+    def __init__(self, *args, **kwargs):
+        super(PostEditForm, self).__init__(*args, **kwargs)
+        super(forms.ModelForm, self).__init__(*args, **kwargs)
+        
+        # Set the form field for Post.text
+        field_helper = safe_import_module(settings.FORUM_TEXT_FIELD_HELPER_PATH)
+        if field_helper is not None:
+            self.fields['text'] = field_helper(self, **{'label':_('message'), 'required':True})
+
+    def clean_text(self):
+        """
+        Text content validation
+        """
+        text = self.cleaned_data.get("text")
+        validation_helper = safe_import_module(settings.FORUM_TEXT_VALIDATOR_HELPER_PATH)
+        if validation_helper is not None:
+            return validation_helper(self, text)
+        else:
+            return text
+        
+    def save(self):
+        self.instance = super(PostEditForm, self).save()
+        
+        return self.instance
+    
+    class Meta:
+        model = Post
+        exclude = ('thread','author')
+
+class PostDeleteForm(CrispyFormMixin, forms.ModelForm):
+    """
+    Message delete form
+    """
+    crispy_form_helper_path = 'forum.forms.crispies.post_delete_helper'
+    
+    confirm = forms.BooleanField(label=_("Confirm"), initial=False, required=True)
+    
+    def __init__(self, *args, **kwargs):
+        super(PostDeleteForm, self).__init__(*args, **kwargs)
+        super(forms.ModelForm, self).__init__(*args, **kwargs)
+        
+    def save(self):
+        thread_instance = self.instance.thread
+        
+        self.instance.delete()
+        
+        return thread_instance
+    
+    class Meta:
+        model = Post
+        exclude = ('thread','author','text')
