2a60e06334f1148cb95da40572e6bb32803ef2f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:31 +0000

    Add package django-rest-serializer-field-permissions/0.1/django-rest-serializer-field-permissions-0.1.tar.gz

---
+++ packages/django-rest-serializer-field-permissions/django-rest-serializer-field-permissions-0.1.tar.gz/django-rest-serializer-field-permissions-0.1/setup.py
@@ -0,0 +1,38 @@
+import os
+from setuptools import setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-rest-serializer-field-permissions',
+    version='0.1',
+    packages=['rest_framework_serializer_field_permissions'],
+    include_package_data=True,
+    license='GNU General Public License v3 (GPLv3)',
+    description='Field-by-field serializer permissions for Django Rest Framework.',
+    long_description=README,
+    url='https://intersis.org/',
+    author='The Intersis Foundation',
+    author_email='dev@intersis.org',
+    install_requires=[
+        'django>=1.7',
+        'djangorestframework>=3.0.1',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
