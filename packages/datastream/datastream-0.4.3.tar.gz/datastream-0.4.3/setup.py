100d7c94653141c3a69886556068706e9f1e8f74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:05:53 +0000

    Add package datastream/0.4.3/datastream-0.4.3.tar.gz

---
+++ packages/datastream/datastream-0.4.3.tar.gz/datastream-0.4.3/setup.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+import os
+
+from setuptools import setup, find_packages
+
+VERSION = '0.4.3'
+
+if __name__ == '__main__':
+    setup(
+        name='datastream',
+        version=VERSION,
+        description="Datastream API time-series library.",
+        long_description=open(os.path.join(os.path.dirname(__file__), 'README.rst')).read(),
+        author='wlan slovenija',
+        author_email='open@wlan-si.net',
+        url='https://github.com/wlanslovenija/datastream',
+        license='AGPLv3',
+        packages=find_packages(exclude=('*.tests', '*.tests.*', 'tests.*', 'tests')),
+        package_data={},
+        classifiers=[
+            'Development Status :: 4 - Beta',
+            'Intended Audience :: Developers',
+            'License :: OSI Approved :: GNU Affero General Public License v3',
+            'Operating System :: OS Independent',
+            'Programming Language :: Python',
+        ],
+        include_package_data=True,
+        zip_safe=False,
+        install_requires=[
+            'mongoengine>=0.8.1',
+            'pymongo>=2.2.1',
+            'pytz>=2012h',
+        ],
+        tests_require=[
+            'mongoengine>=0.8.1',
+            'pymongo>=2.2.1',
+            'pytz>=2012h',
+        ],
+        test_suite='tests',
+    )
