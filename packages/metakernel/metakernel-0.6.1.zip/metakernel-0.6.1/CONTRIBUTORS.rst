612c8fbcd0ad5be1aa1d65e19e56560d93addd48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:36 +0000

    Add package metakernel/0.6.1/metakernel-0.6.1.zip

---
+++ packages/metakernel/metakernel-0.6.1.zip/metakernel-0.6.1/CONTRIBUTORS.rst
@@ -0,0 +1,14 @@
+
+
+Development Leads
+````````````````````````````````
+
+- Doug Blank <doug.blank@gmail.com>
+- Steven Silvester <steven.silvester@ieee.org>
+
+
+Patches and Suggestions
+```````````````````````
+
+- Daniel Mendler @minad
+- Thomas Kluyver @takluyver
---
+++ packages/metakernel/metakernel-0.6.1.zip/metakernel-0.6.1/metakernel/utils/kernel.py
@@ -0,0 +1,17 @@
+import pkgutil
+import os
+
+
+def install_kernel_resources(destination, resource="metakernel", files=None):
+    """
+    Copy the resource files to the kernelspec folder.
+    """
+    if files is None:
+        files = ["logo-64x64.png", "logo-32x32.png"]
+    for filename in files:
+        try:
+            data = pkgutil.get_data(resource, filename)
+        except Exception:
+            data = pkgutil.get_data(resource, os.path.join('images', filename))
+        with open(os.path.join(destination, filename), "wb") as fp:
+            fp.write(data)
