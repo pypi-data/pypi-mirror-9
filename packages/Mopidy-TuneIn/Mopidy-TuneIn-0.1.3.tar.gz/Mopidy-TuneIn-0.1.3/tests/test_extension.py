3658a34323c5d4a75272a3cf9043766158a7b1ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:38 +0000

    Add package Mopidy-TuneIn/0.1.3/Mopidy-TuneIn-0.1.3.tar.gz

---
+++ packages/Mopidy-TuneIn/Mopidy-TuneIn-0.1.3.tar.gz/Mopidy-TuneIn-0.1.3/tests/test_extension.py
@@ -0,0 +1,23 @@
+from __future__ import unicode_literals
+
+import unittest
+
+from mopidy_tunein import Extension
+
+
+class ExtensionTest(unittest.TestCase):
+
+    def test_get_default_config(self):
+        ext = Extension()
+
+        config = ext.get_default_config()
+
+        self.assertIn('[tunein]', config)
+        self.assertIn('enabled = true', config)
+
+    def test_get_config_schema(self):
+        ext = Extension()
+
+        schema = ext.get_config_schema()
+
+        self.assertIn('timeout', schema)
