8f29620188953e4336ac519621c241489468bc37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:26 +0000

    Add package m3-objectpack/2.0.22.3/m3-objectpack-2.0.22.3.tar.gz

---
+++ packages/m3-objectpack/m3-objectpack-2.0.22.3.tar.gz/m3-objectpack-2.0.22.3/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+import os
+from setuptools import setup, find_packages
+
+
+def read(fname):
+    try:
+        return open(os.path.join(os.path.dirname(__file__), fname)).read()
+    except IOError:
+        return ''
+
+setup(
+    name="m3-objectpack",
+    version="2.0.22.3",
+    license='MIT',
+    description=read('DESCRIPTION'),
+    author="Alexey Pirogov",
+    author_email="pirogov@bars-open.ru",
+    url="https://bitbucket.org/barsgroup/objectpack",
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Environment :: Web Environment',
+        'Natural Language :: Russian',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'License :: OSI Approved :: MIT License',
+        'Development Status :: 5 - Production/Stable',
+    ],
+    package_dir={'': 'src'},
+    packages=find_packages('src'),
+    include_package_data=True,
+    long_description=read('README'),
+)
---
+++ packages/m3-objectpack/m3-objectpack-2.0.22.3.tar.gz/m3-objectpack-2.0.22.3/src/objectpack/dictionary_object_pack/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+File: __init__.py
+Author: Rinat F Sabitov
+Description:
+"""
+
+from actions import DictionaryObjectPack
