51e6fdc32d4096ce6892eac8481ce2515d83d6ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:47 +0000

    Add package gmail-client/0.0.7.7/gmail-client-0.0.7.7.tar.gz

---
+++ packages/gmail-client/gmail-client-0.0.7.7.tar.gz/gmail-client-0.0.7.7/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name = "gmail-client",
+    version = "0.0.7.7",
+    author = "Wilberto Morales",
+    author_email = "wilbertomorales777@gmail.com",
+    description = ("A Pythonic interface for Google Mail. Based of https://github.com/charlierguo/gmai"),
+    license = "MIT",
+    keywords = "google gmail client",
+    url = "https://github.com/wilbertom/gmail",
+    packages=['gmail_client', 'gmail_client.codecs'],
+    classifiers=[
+        "Topic :: Communications :: Email",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
