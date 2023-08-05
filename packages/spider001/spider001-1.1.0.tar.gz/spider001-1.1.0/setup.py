770a6b2dafcfd94a7af45fae02a999a5cb3ed6f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:05:55 +0000

    Add package spider001/1.1.0/spider001-1.1.0.tar.gz

---
+++ packages/spider001/spider001-1.1.0.tar.gz/spider001-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup (
+    name = 'spider001',
+    version = '1.1.0',
+    py_modules = ['spider001'],
+    author = 'jenny42',
+    author_email = 'jenny42@mail.ustc.edu.cn',
+    url = 'http://jenny42.com',
+    description = 'A simple printer of web pages',
+)
