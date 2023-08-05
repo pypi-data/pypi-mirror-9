a4f5c57dd070356901fc788d95eed1d037f2d8b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:45 +0000

    Add package arrow-fatisar/0.5.2/arrow-fatisar-0.5.2.tar.gz

---
+++ packages/arrow-fatisar/arrow-fatisar-0.5.2.tar.gz/arrow-fatisar-0.5.2/setup.py
@@ -0,0 +1,42 @@
+import os.path
+import re
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+
+def fpath(name):
+    return os.path.join(os.path.dirname(__file__), name)
+
+
+def read(fname):
+    return open(fpath(fname)).read()
+
+
+def grep(attrname):
+    pattern = r"{0}\W*=\W*'([^']+)'".format(attrname)
+    strval, = re.findall(pattern, file_text)
+    return strval
+
+
+file_text = read(fpath('arrow/__init__.py'))
+
+setup(
+    name='arrow-fatisar',
+    version=grep('__version__'),
+    description='("arrow" fork) Better dates and times for Python, with intervals',
+    url='https://github.com/fatisar/arrow',
+    author='Daniel Sarfati',
+    author_email="fatisar@gmail.com",
+    license='Apache 2.0',
+    packages=['arrow'],
+    zip_safe=False,
+    install_requires=[
+        'python-dateutil'
+    ],
+    test_suite="tests"
+)
+
