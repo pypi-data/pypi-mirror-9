1523c8dd1804c9d129168ae1169f07f033272f21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:57 +0000

    Add package Cetacean/1.0.0/Cetacean-1.0.0.tar.gz

---
+++ packages/Cetacean/Cetacean-1.0.0.tar.gz/Cetacean-1.0.0/setup.py
@@ -0,0 +1,34 @@
+# encoding: utf-8
+"""
+The HAL client that does almost nothing for you.
+
+Cetacean doesn't know about HTTP. You set up your own Requests client and use it
+to make requests. You feed then Cetacean the decoded bodies as strings and it
+helps you pull useful data out of them.
+"""
+
+from setuptools import setup
+
+import cetacean
+
+setup(
+    name="Cetacean",
+    version=cetacean.__version__,
+    author="Ben Hamill",
+    author_email="ben@benhamill.com",
+    url="http://github.com/benhamill/cetacean-python#readme",
+    license="MIT",
+    description="The HAL client that does almost nothing for you.",
+    long_description=__doc__,
+    py_modules=["cetacean"],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.4",
+        "Topic :: Internet :: WWW/HTTP",
+    ],
+)
