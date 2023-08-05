30bab814d335c40f72804646e7fd21d95652f49e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:43 +0000

    Add package js.bootstrap/3.3.1/js.bootstrap-3.3.1.zip

---
+++ packages/js.bootstrap/js.bootstrap-3.3.1.zip/js.bootstrap-3.3.1/README.rst
@@ -0,0 +1,25 @@
+js.bootstrap
+************
+
+.. image:: https://travis-ci.org/RedTurtle/js.bootstrap.png?branch=master
+
+Introduction
+============
+
+This library packages `twitter bootstrap`_ for `fanstatic`_. It is aware of different modes (normal, minified).
+
+.. _`fanstatic`: http://fanstatic.org
+.. _`twitter bootstrap`: http://getbootstrap.com
+
+This requires integration between your web framework and ``fanstatic``,
+and making sure that the original resources (shipped in the ``resources``
+directory in ``js.bootstrap``) are published to some URL.
+
+
+Contact
+=======
+
+.. image:: http://www.redturtle.it/redturtle_banner.png
+
+* | Andrew Mleczko <``andrew.mleczko at redturtle.net``>
+  | **RedTurtle Technology**, http://www.redturtle.net/
---
+++ packages/js.bootstrap/js.bootstrap-3.3.1.zip/js.bootstrap-3.3.1/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup, find_packages
+import os
+
+version = '3.3.1'
+
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+long_description = (
+    read('README.rst')
+    + '\n' +
+    read('js', 'bootstrap', 'test_bootstrap.txt')
+    + '\n' +
+    read('CHANGES.txt'))
+
+setup(
+    name='js.bootstrap',
+    version=version,
+    description="fanstatic twitter bootstrap.",
+    long_description=long_description,
+    classifiers=[],
+    keywords='fanstatic twitter bootstrap redturtle',
+    author='RedTurtle Developers',
+    url='https://github.com/RedTurtle/js.bootstrap',
+    author_email='sviluppoplone@redturtle.it',
+    license='BSD',
+    packages=find_packages(),
+    namespace_packages=['js'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'fanstatic',
+        'js.jquery',
+        'setuptools',
+        ],
+    entry_points={
+        'fanstatic.libraries': [
+            'bootstrap = js.bootstrap:library',
+            ],
+        },
+    )
