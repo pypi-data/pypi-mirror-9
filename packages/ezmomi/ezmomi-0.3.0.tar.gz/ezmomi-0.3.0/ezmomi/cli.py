f82faa035fe9d2555b59926f4876521c9bb4e3fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:29 +0000

    Add package ezmomi/0.3.0/ezmomi-0.3.0.tar.gz

---
+++ packages/ezmomi/ezmomi-0.3.0.tar.gz/ezmomi-0.3.0/ezmomi/cli.py
@@ -0,0 +1,41 @@
+'''
+ Command line definitions for ezmomi
+'''
+import argparse
+from params import add_params
+from ezmomi import EZMomi
+
+
+def cli():
+    # Set up command line arguments
+    parser = argparse.ArgumentParser(
+        description='Perform common vSphere API tasks'
+    )
+    subparsers = parser.add_subparsers(help='Command', dest='mode')
+
+    # set up each command section
+    add_params(subparsers)
+
+    # parse arguments
+    args = parser.parse_args()
+
+    # initialize ezmomi instance
+    ez = EZMomi(**vars(args))
+
+    kwargs = vars(args)
+
+    # choose your adventure
+    if kwargs['mode'] == 'list':
+        ez.list_objects()
+    elif kwargs['mode'] == 'clone':
+        ez.clone()
+    elif kwargs['mode'] == 'destroy':
+        ez.destroy()
+    elif kwargs['mode'] == 'status':
+        ez.status()
+    elif kwargs['mode'] == 'shutdown':
+        ez.shutdown()
+    elif kwargs['mode'] == 'powerOff':
+        ez.powerOff()
+    elif kwargs['mode'] == 'powerOn':
+        ez.powerOn()
