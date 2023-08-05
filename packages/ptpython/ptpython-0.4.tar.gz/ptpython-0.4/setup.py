292b21806b4cb9043509c42bbc514bcf03a465e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:56 +0000

    Add package ptpython/0.4/ptpython-0.4.tar.gz

---
+++ packages/ptpython/ptpython-0.4.tar.gz/ptpython-0.4/setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+import os
+from setuptools import setup, find_packages
+
+long_description = open(
+    os.path.join(
+        os.path.dirname(__file__),
+        'README.rst'
+    )
+).read()
+
+
+setup(
+    name='ptpython',
+    author='Jonathan Slenders',
+    version='0.4',
+    url='https://github.com/jonathanslenders/ptpython',
+    description='Python REPL build on top of prompt_toolkit',
+    long_description=long_description,
+    packages=find_packages('.'),
+    install_requires = [
+        'prompt_toolkit==0.30',
+        'jedi>=0.8.1',
+        'docopt',
+    ],
+    entry_points={
+        'console_scripts': [
+            'ptpython = ptpython.entry_points.run_ptpython:run',
+            'ptipython = ptpython.entry_points.run_ptipython:run',
+        ]
+    },
+    extra_require={
+        'ptipython':  ['ipython'] # For ptipython, we need to have IPython
+    }
+)
