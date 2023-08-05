fdbb7fc922fe0958be5ea8ef4a3b96e672420a9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:22 +0000

    Add package imagebot/1.0.1/imagebot-1.0.1.tar.gz

---
+++ packages/imagebot/imagebot-1.0.1.tar.gz/imagebot-1.0.1/setup.py
@@ -0,0 +1,30 @@
+import ez_setup
+ez_setup.use_setuptools()
+
+from setuptools import setup, find_packages
+import platform
+import imp
+
+install_requires = ['scrapy']
+
+try:
+	imp.find_module('PIL')
+except ImportError:
+	install_requires.append('Pillow')
+
+entry_points = {}
+entry_points['console_scripts'] = ['imagebot=imagebot.main:main']
+
+setup(	name='imagebot',
+	version='1.0.1',
+	description='A web bot to scrape images from websites.',
+	author='Amol Umrale',
+	author_email='babaiscool@gmail.com',
+	url='http://pypi.python.org/pypi/imagebot/',
+	packages=['imagebot', 'imagebot.spiders'],
+	package_data={'imagebot': ['tables.sql']},
+	scripts=['ez_setup.py'],
+	entry_points=entry_points,
+	install_requires=install_requires
+)
+
