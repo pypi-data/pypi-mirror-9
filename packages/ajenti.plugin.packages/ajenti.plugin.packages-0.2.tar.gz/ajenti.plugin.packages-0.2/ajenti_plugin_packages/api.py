ee467dca27df762c2ac07c3f2d47970ae037af85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:02 +0000

    Add package ajenti.plugin.packages/0.2/ajenti.plugin.packages-0.2.tar.gz

---
+++ packages/ajenti.plugin.packages/ajenti.plugin.packages-0.2.tar.gz/ajenti.plugin.packages-0.2/ajenti_plugin_packages/api.py
@@ -0,0 +1,35 @@
+from aj.api import *
+
+
+class Package (object):
+    def __init__(self, manager):
+        self.manager = manager
+        self.id = None
+        self.name = None
+        self.version = None
+        self.description = None
+        self.is_installed = None
+        self.is_upgradeable = None
+        self.installed_version = None
+
+
+@interface
+class PackageManager (object):
+    id = None
+    name = None
+    update_command = None
+
+    def __init__(self, context):
+        self.context = context
+
+    def list(self):
+        raise NotImplementedError
+
+    def get(self, id):
+        raise NotImplementedError
+
+    def update_lists(self, progress_callback):
+        raise NotImplementedError
+
+    def get_apply_cmd(self, selection):
+        raise NotImplementedError
