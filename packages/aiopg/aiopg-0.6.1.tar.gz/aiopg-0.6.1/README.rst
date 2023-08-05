697bb9ac4ba2975a2a0225ac8ca10468079988d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:18 +0000

    Add package aiopg/0.6.1/aiopg-0.6.1.tar.gz

---
+++ packages/aiopg/aiopg-0.6.1.tar.gz/aiopg-0.6.1/README.rst
@@ -0,0 +1,74 @@
+aiopg
+=======
+
+**aiopg** is a library for accessing a PostgreSQL_ database
+from the asyncio_ (PEP-3156/tulip) framework. It wraps
+asynchronous features of the Psycopg database driver.
+
+Example
+-------
+
+::
+
+   import asyncio
+   from aiopg.pool import create_pool
+
+   dsn = 'dbname=jetty user=nick password=1234 host=localhost port=5432'
+
+
+   @asyncio.coroutine
+   def test_select():
+       pool = yield from create_pool(dsn)
+
+       with (yield from pool) as conn:
+           cur = yield from conn.cursor()
+           yield from cur.execute('SELECT 1')
+           ret = yield from cur.fetchone()
+           assert ret == (1,), ret
+
+
+   asyncio.get_event_loop().run_until_complete(test_select())
+
+
+Example of SQLAlchemy optional integration
+-------------------------------------------
+
+::
+
+   import asyncio
+   from aiopg.sa import create_engine
+   import sqlalchemy as sa
+
+
+   metadata = sa.MetaData()
+
+   tbl = sa.Table('tbl', metadata,
+       sa.Column('id', sa.Integer, primary_key=True),
+       sa.Column('val', sa.String(255)))
+
+
+   @asyncio.coroutine
+   def go():
+       engine = yield from create_engine(user='aiopg',
+                                         database='aiopg',
+                                         host='127.0.0.1',
+                                         password='passwd')
+
+       with (yield from engine) as conn:
+           yield from conn.execute(tbl.insert().values(val='abc'))
+
+           res = yield from conn.execute(tbl.select())
+           for row in res:
+               print(row.id, row.val)
+
+
+   asyncio.get_event_loop().run_until_complete(go())
+
+.. _PostgreSQL: http://www.postgresql.org/
+.. _asyncio: http://docs.python.org/3.4/library/asyncio.html
+
+Please use::
+
+   $ python3 runtests.py
+
+for executing project's unittests
---
+++ packages/aiopg/aiopg-0.6.1.tar.gz/aiopg-0.6.1/setup.py
@@ -0,0 +1,67 @@
+import os
+import re
+import sys
+from setuptools import setup, find_packages
+
+
+install_requires = ['psycopg2>=2.5.2']
+
+PY_VER = sys.version_info
+
+if PY_VER >= (3, 4):
+    pass
+elif PY_VER >= (3, 3):
+    install_requires.append('asyncio')
+else:
+    raise RuntimeError("aiopg doesn't suppport Python earlier than 3.3")
+
+
+def read(f):
+    return open(os.path.join(os.path.dirname(__file__), f)).read().strip()
+
+extras_require = {'sa': ['sqlalchemy>=0.9'], }
+
+
+def read_version():
+    regexp = re.compile(r"^__version__\W*=\W*'([\d.abrc]+)'")
+    init_py = os.path.join(os.path.dirname(__file__), 'aiopg', '__init__.py')
+    with open(init_py) as f:
+        for line in f:
+            match = regexp.match(line)
+            if match is not None:
+                return match.group(1)
+        else:
+            raise RuntimeError('Cannot find version in aiopg/__init__.py')
+
+classifiers = [
+    'License :: OSI Approved :: BSD License',
+    'Intended Audience :: Developers',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.3',
+    'Programming Language :: Python :: 3.4',
+    'Operating System :: POSIX',
+    'Operating System :: MacOS :: MacOS X',
+    'Environment :: Web Environment',
+    'Development Status :: 4 - Beta',
+    'Topic :: Database',
+    'Topic :: Database :: Front-Ends',
+]
+
+
+setup(name='aiopg',
+      version=read_version(),
+      description=('Postgres integration with asyncio.'),
+      long_description='\n\n'.join((read('README.rst'), read('CHANGES.txt'))),
+      classifiers=classifiers,
+      platforms=['POSIX'],
+      author='Andrew Svetlov',
+      author_email='andrew.svetlov@gmail.com',
+      url='http://aiopg.readthedocs.org',
+      download_url='https://pypi.python.org/pypi/aiopg',
+      license='BSD',
+      packages=find_packages(),
+      install_requires=install_requires,
+      extras_require=extras_require,
+      provides=['aiopg'],
+      requires=['psycopg2'],
+      include_package_data = True)
