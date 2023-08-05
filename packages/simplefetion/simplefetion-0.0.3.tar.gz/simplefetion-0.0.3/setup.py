24db70bf10e6a8ccd31d9469df9af4844e506450
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:16 +0000

    Add package simplefetion/0.0.3/simplefetion-0.0.3.tar.gz

---
+++ packages/simplefetion/simplefetion-0.0.3.tar.gz/simplefetion-0.0.3/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name = 'simplefetion',
+    version = '0.0.3',
+    description = 'Simple Fetion: send message / group messages',
+    author = 'Cole Smith',
+    author_email = 'uniquecolesmith@gmail.com',
+    url = 'https://djangofetion.sinaapp.com/',
+    packages = ['simplefetion'],
+    include_package_data = True,
+    zip_safe = False,
+    classifiers = (
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ),
+)
---
+++ packages/simplefetion/simplefetion-0.0.3.tar.gz/simplefetion-0.0.3/simplefetion/simplefetion.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+from .simple_requests import cole_requests
+
+
+def send_a_message(fuser, fpass, rec, msg):
+    '''
+        description:
+            send one message to one of your fetion friends
+        Usage:
+            send_a_message(fetion_account, 
+                           fetion_password,
+                           receiver_phone,
+                           message_content)
+        For example:
+            from simplefetion import send_a_message
+            send_a_message('fetionaccount', 'fetionpassword', '13011111111', 'msg')    
+    '''
+    assert(fuser and fpass and rec and msg)
+    url = 'https://djangofetion.sinaapp.com/msg/'
+    data = {
+        'phone': fuser,
+        'password': fpass,
+        'to_phone': rec,
+        'content': msg
+    }
+    return cole_requests.post(url, data)
+
+def send_group_message(fuser, fpass, recs, msg):
+    '''
+        description:
+            send a group message to your fetion friends
+        Usage:
+            send_a_message(fetion_account, 
+                           fetion_password,
+                           receiver_phones,
+                           message_content)
+        Take Care:
+            receiver_phones must be a list type data
+        For example:
+            from simplefetion import send_group_message
+            send_group_message('fetionaccount', 'fetionpassword', ['13011111111', '13011111112'], 'msg')
+    '''
+    assert(fuser and fpass and recs and msg)
+    assert(isinstance(recs, list))
+    
+    url = 'https://djangofetion.sinaapp.com/gmsg/'
+
+    recs = ','.join(recs)
+    data = {
+        'phone': fuser,
+        'password': fpass,
+        'to_phone': recs,
+        'content': msg
+    }
+    return cole_requests.post(url, data, 10)
