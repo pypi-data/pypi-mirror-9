90ca9d3d589378c01104d3881fb3eaaf452ad576
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:21 +0000

    Add package mdfreader/0.0.1/mdfreader-0.0.1.tar.gz

---
+++ packages/mdfreader/mdfreader-0.0.1.tar.gz/mdfreader-0.0.1/mdfconverter.py
@@ -0,0 +1,11 @@
+﻿from PyQt4 import QtGui
+from mdfreaderui import MainWindow
+from sys import argv, exit
+from multiprocessing import freeze_support
+
+if __name__ == "__main__":
+    freeze_support()
+    app = QtGui.QApplication( argv )
+    myapp = MainWindow(  )
+    myapp.show()
+    exit( app.exec_() )
