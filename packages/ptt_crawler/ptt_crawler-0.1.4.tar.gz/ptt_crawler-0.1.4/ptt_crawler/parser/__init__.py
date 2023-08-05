fa3b74c1da724c5b1df2a109816a1f180da9e9c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:10 +0000

    Add package ptt_crawler/0.1.4/ptt_crawler-0.1.4.tar.gz

---
+++ packages/ptt_crawler/ptt_crawler-0.1.4.tar.gz/ptt_crawler-0.1.4/ptt_crawler/parser/__init__.py
@@ -0,0 +1,3 @@
+# flake8: noqa
+
+from .routes import routes
---
+++ packages/ptt_crawler/ptt_crawler-0.1.4.tar.gz/ptt_crawler-0.1.4/ptt_crawler/parser/routes.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# flake8: noqa
+
+import re
+from .list import parse as parse_list
+from .article import parse as parse_article
+
+ARTICLE_RE = re.compile("\/bbs\/[-_\w]+\/(?:\w+\.){4}html$")
+LIST_RE = re.compile("\/index\d*\.html$")
+
+
+def routes(url):
+    return article_page(url) \
+        or list_page(url)
+
+
+def article_page(url):
+    if ARTICLE_RE.search(url):
+        return parse_article
+
+
+def list_page(url):
+    if LIST_RE.search(url):
+        return parse_list
