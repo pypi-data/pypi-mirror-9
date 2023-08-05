a6aed693ca0a53b5eaeb4057a8c4c385bb47dcf8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:28 +0000

    Add package zmqdecorators/0.7.3/zmqdecorators-0.7.3.tar.gz

---
+++ packages/zmqdecorators/zmqdecorators-0.7.3.tar.gz/zmqdecorators-0.7.3/setup.py
@@ -0,0 +1,32 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+def git_version():
+    version = 'UNKNOWN'
+    try:
+        import subprocess
+        version = str(subprocess.check_output(['git', 'rev-parse', '--verify', '--short', 'HEAD'])).strip()
+    except subprocess.CalledProcessError,e:
+        print "Got error when trying to read git version: %s" % e
+    return version
+
+setup(
+    name='zmqdecorators',
+#    version='0.7.3dev-%s' % git_version(),
+    version='0.7.3',
+    author='Eero "rambo" af Heurlin',
+    author_email='rambo@iki.fi',
+    packages=['zmqdecorators',],
+    license='GNU LGPL',
+    long_description=open('README.md').read(),
+    description='Decorators for pyZMQ to make it almost as easy to use a DBUS',
+    install_requires=[
+        'tornado>=2.0',
+        'pyzmq>=2.0',
+        'pybonjour>=1.1',
+    ],
+    url='https://github.com/rambo/python-zmqdecorators',
+)
+
---
+++ packages/zmqdecorators/zmqdecorators-0.7.3.tar.gz/zmqdecorators-0.7.3/zmqdecorators/__init__.py
@@ -0,0 +1,15 @@
+"""This exposes the decorators, if you need access to all of the helpers import zmq_utilities
+
+See examples at https://github.com/rambo/python-zmqdecorators/tree/master/examples
+"""
+from zmq_utilities import service_baseclass as service
+from zmq_utilities import client_baseclass as client
+from zmq_utilities import signal
+from zmq_utilities import method
+from zmq_utilities import call 
+from zmq_utilities import call_sync
+from zmq_utilities import zmq_bonjour_connect_wrapper
+from zmq_utilities import subscribe_topic
+from zmq_utilities import subscribe_all
+from zmq_utilities import dt as server_tracker
+from zmq_utilities import ct as client_tracker
