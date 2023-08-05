2c1d644f2683e1de3694f2b0ce0108e24f9e1726
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:09 +0000

    Add package django-runner/0.1.1/django-runner-0.1.1.tar.gz

---
+++ packages/django-runner/django-runner-0.1.1.tar.gz/django-runner-0.1.1/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+setup(
+    name='django-runner',
+    version='0.1.1',
+    description='Run arbitrary script within Django environment',
+    author='Kohei Suzuki',
+    author_email='eagletmt@gmail.com',
+    url='https://github.com/eagletmt/django-runner',
+    packages=find_packages(),
+    license='MIT',
+    install_requires=[
+        'django',
+    ],
+)
