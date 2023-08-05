4d4e7ce0e020106708eda3fd95d90242f56a7ac5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:28 +0000

    Add package rlunch/0.2.1/rlunch-0.2.1.tar.gz

---
+++ packages/rlunch/rlunch-0.2.1.tar.gz/rlunch-0.2.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='rlunch',
+      version='0.2.1',
+      description='The lunch recommendation system for Ridibooks employees',
+      url='http://github.com/minsoopark/RidiLunch',
+      author='Minsoo Park',
+      author_email='minsoo1003@gmail.com',
+      license='MIT',
+      packages=['rlunch'],
+      zip_safe=False,
+      entry_points = {
+        'console_scripts': ['rlunch=rlunch.command_line:main'],
+      })
