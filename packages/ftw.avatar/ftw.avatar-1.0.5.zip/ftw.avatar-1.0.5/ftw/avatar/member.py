84ab6497ba41f49ce609dac16286ee814703d15e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:08 +0000

    Add package ftw.avatar/1.0.5/ftw.avatar-1.0.5.zip

---
+++ packages/ftw.avatar/ftw.avatar-1.0.5.zip/ftw.avatar-1.0.5/ftw/avatar/member.py
@@ -0,0 +1,57 @@
+from Products.CMFCore.utils import getToolByName
+from StringIO import StringIO
+from ftw.avatar.interfaces import IAvatarGenerator
+from ftw.avatar.utils import SwitchedToSystemUser
+from zope.component import getUtility
+from zope.component.hooks import getSite
+
+
+def get_user_id(userid):
+    portal = getSite()
+    membership = getToolByName(portal, 'portal_membership')
+    member = membership.getMemberById(userid)
+
+    if member:
+        return member.getId()
+
+    acl_users = getToolByName(portal, 'acl_users')
+    user = acl_users.getUser(userid)
+    if user:
+        return user.getId()
+
+    return None
+
+
+def get_name_of_user(userid):
+    portal = getSite()
+    membership = getToolByName(portal, 'portal_membership')
+    member = membership.getMemberById(userid)
+    if not member:
+        return userid
+    return member.getProperty('fullname', None) or userid
+
+
+def user_has_portrait(userid):
+    portal = getSite()
+    membership = getToolByName(portal, 'portal_membership')
+    memberdata = getToolByName(portal, 'portal_memberdata')
+    safe_id = membership._getSafeMemberId(userid)
+    return memberdata._getPortrait(safe_id) is not None
+
+
+def create_default_avatar(userid):
+    userid = get_user_id(userid)
+    if user_has_portrait(userid):
+        return
+
+    portrait = StringIO()
+    generator = getUtility(IAvatarGenerator)
+    if not generator.generate(get_name_of_user(userid), portrait):
+        return
+    portrait.seek(0)
+    setattr(portrait, 'filename', 'default.png')
+
+    portal = getSite()
+    membership = getToolByName(portal, 'portal_membership')
+    with SwitchedToSystemUser():
+        membership.changeMemberPortrait(portrait, id=userid)
