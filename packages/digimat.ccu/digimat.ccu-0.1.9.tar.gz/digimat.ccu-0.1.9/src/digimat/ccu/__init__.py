a3306d43bb28c255543424bb5227ae2ebcc56ca0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:56 +0000

    Add package digimat.ccu/0.1.9/digimat.ccu-0.1.9.tar.gz

---
+++ packages/digimat.ccu/digimat.ccu-0.1.9.tar.gz/digimat.ccu-0.1.9/src/digimat/ccu/__init__.py
@@ -0,0 +1,5 @@
+from ccu import CCU
+from ccu import CCUDevice
+from ccu import CCUAlarmDevice, CCUInfraredDevice, CCUDoorDevice
+from ccu import CCUAlarmZone
+from ccu import CCURelayDevice, CCUSingleRelayDevice, CCUChimeDevice
