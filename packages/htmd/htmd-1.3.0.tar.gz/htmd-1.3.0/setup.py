591e9d562c788f84fce6e0c3391283681a61199e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:19 +0000

    Add package htmd/1.3.0/htmd-1.3.0.tar.gz

---
+++ packages/htmd/htmd-1.3.0.tar.gz/htmd-1.3.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+
+setup(
+    name='htmd',
+    version='1.3.0',
+    packages=['htmd'],
+    include_package_data=True,
+    install_requires=requirements,
+    entry_points='''
+        [console_scripts]
+        htmd=htmd.cli:cli
+    ''',
+    zip_safe=False,  # Required to have template files as files (not strings)
+)
