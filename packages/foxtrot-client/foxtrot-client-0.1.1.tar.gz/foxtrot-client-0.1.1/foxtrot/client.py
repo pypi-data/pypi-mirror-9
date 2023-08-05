fce460b3b701e53a821d3d6f62f260ec2c330442
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:53 +0000

    Add package foxtrot-client/0.1.1/foxtrot-client-0.1.1.tar.gz

---
+++ packages/foxtrot-client/foxtrot-client-0.1.1.tar.gz/foxtrot-client-0.1.1/foxtrot/client.py
@@ -0,0 +1,40 @@
+import requests, json
+from urllib import urlencode
+from _required_fields import check_required
+from response import FoxtrotResponse
+
+class Foxtrot(object):
+  """Foxtrot API Client"""
+
+  host = 'api.foxtrot.io'
+  ssl = True
+
+  def __init__(self, api_key, version='v1'):
+    self.api_key = api_key
+    self.version = version
+    self.headers = {'content-type': 'application/json'}
+
+  def _build_url(self, endpoint, args):
+    proto = "https" if self.ssl else "http"
+    args['key'] = self.api_key
+    query = urlencode(args)
+    return "{}://{}/{}/{}?{}".format(proto, self.host, self.version, endpoint, query)
+
+  def get(self, endpoint, args):
+    url = self._build_url(endpoint, args)
+    response = requests.get(url, headers=self.headers)
+    return response.json()
+
+  def post(self, endpoint, data, args):
+    url = self._build_url(endpoint, args)
+    response = requests.post(url, data=json.dumps(data), headers=self.headers)
+    return response.json()
+
+  def poll(self, txid, args={}):
+    resp = self.get('poll', {'txid': txid})
+    return FoxtrotResponse.response_for('poll', resp, self)
+
+  @check_required
+  def optimize(self, data, args={}):
+    resp = self.post('optimize', data, args)
+    return FoxtrotResponse.response_for('optimize', resp, self)
