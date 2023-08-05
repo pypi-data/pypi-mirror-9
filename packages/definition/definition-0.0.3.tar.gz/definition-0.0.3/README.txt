11d887120d96d49379d207ce864473ec210210ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:19 +0000

    Add package definition/0.0.3/definition-0.0.3.tar.gz

---
+++ packages/definition/definition-0.0.3.tar.gz/definition-0.0.3/README.txt
@@ -0,0 +1,35 @@
+Definition
+=========
+
+Fetch definition from 5 or more online dictionaries
+Available easily from PyPi
+
+#### Installation
+
+```sh
+pip install definition
+```
+
+#### Usage
+To fetch definition of <query>
+
+```sh
+definition <query>
+```
+
+```sh
+$ definition -h
+
+Usage: def [-p] [-r [INT]] [-e [INT]] query
+
+Options:
+  --version             show program's version number and exit
+  -h, --help            show this help message and exit
+  -w, --word            Show the word of the day
+  -x, --xamples         Show 30 examples
+  -e EXAMPLE, --example=EXAMPLE
+                        Show specified number of examples
+  -r, --random          Show random words
+  -q, --quiet           don't print status messages to stdout
+  -p, --pronunciation   Show detailed pronunciation
+```
---
+++ packages/definition/definition-0.0.3.tar.gz/definition-0.0.3/setup.py
@@ -0,0 +1,29 @@
+from distutils.core import setup
+
+add_keywords = dict(
+    entry_points = {
+        'console_scripts': ['definition = definition.definition:main'],
+    },
+)
+
+fhan = open('requirements.txt', 'rU')
+requires = [line.strip() for line in fhan.readlines()]
+fhan.close()
+fhan = open('README.txt')
+long_description = fhan.read()
+fhan.close()
+
+setup(
+        name='definition',
+        description='Definition fetcher from 5 online dictionaries',
+        version='0.0.3',
+        packages=['definition'],
+        license='GPLv3+',
+        author='Shubham Chaudhary',
+        author_email='me@shubhamchaudhary.in',
+        url='https://github.com/shubhamchaudhary/definition',
+        long_description=long_description,
+        install_requires=requires,
+        **add_keywords
+)
+
