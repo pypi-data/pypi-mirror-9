9ba31da098e13a43fae009afaabf28f25df722a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:13 +0000

    Add package django-geoip/0.5.2/django-geoip-0.5.2.tar.gz

---
+++ packages/django-geoip/django-geoip-0.5.2.tar.gz/django-geoip-0.5.2/django_geoip/models.py
@@ -0,0 +1,160 @@
+# -*- coding: utf-8 -*-
+import socket
+import struct
+from abc import ABCMeta
+
+from django.db import models
+from django.db.models.base import ModelBase
+from django.utils.translation import ugettext_lazy as _
+
+# keep imports
+from . import compat
+from .settings import geoip_settings, ipgeobase_settings
+
+
+class Country(models.Model):
+    """ One country per row, contains country code and country name.
+    """
+    code = models.CharField(_('country code'), max_length=2, primary_key=True)
+    name = models.CharField(_('country name'), max_length=255, unique=True)
+
+    def __unicode__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = _('country')
+        verbose_name_plural = _('countries')
+
+
+class Region(models.Model):
+    """ Region is a some geographical entity that belongs to one Country,
+        Cities belong to one specific Region.
+        Identified by country and name.
+    """
+    country = models.ForeignKey(Country, related_name='regions')
+    name = models.CharField(_('region name'), max_length=255)
+
+    def __unicode__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = _('region')
+        verbose_name_plural = _('regions')
+        unique_together = (('country', 'name'), )
+
+
+class City(models.Model):
+    """ Geopoint that belongs to the Region and Country.
+        Identified by name and region.
+        Contains additional latitude/longitude info.
+    """
+    region = models.ForeignKey(Region, related_name='cities')
+    name = models.CharField(_('city name'), max_length=255)
+    latitude = models.DecimalField(max_digits=9, decimal_places=6, blank=True, null=True)
+    longitude = models.DecimalField(max_digits=9, decimal_places=6, blank=True, null=True)
+
+    def __unicode__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = _('city')
+        verbose_name_plural = _('cities')
+        unique_together = (('region', 'name'), )
+
+
+def inet_aton(ip):
+    """ Convert string IP representation to integer
+    """
+    return struct.unpack('!L', socket.inet_aton(ip))[0]
+
+
+class IpRangeManager(models.Manager):
+
+    def by_ip(self, ip):
+        """ Find the smallest range containing the given IP.
+        """
+        try:
+            number = inet_aton(ip)
+        except Exception:
+            raise IpRange.DoesNotExist
+
+        try:
+            return super(IpRangeManager, self).get_query_set()\
+                                              .filter(start_ip__lte=number, end_ip__gte=number)\
+                                              .order_by('end_ip', '-start_ip')[0]
+        except IndexError:
+            raise IpRange.DoesNotExist
+
+
+class IpRange(models.Model):
+    """ IP ranges are stored in separate table, one row for each ip range.
+
+        Each range might be associated with either country (for IP ranges outside of Russia and Ukraine)
+        or country, region and city together.
+
+        Ip range borders are `stored as long integers
+        <http://publibn.boulder.ibm.com/doc_link/en_US/a_doc_lib/libs/commtrf2/inet_addr.htm>`_
+    """
+    start_ip = models.BigIntegerField(_('Ip range block beginning, as integer'), db_index=True)
+    end_ip = models.BigIntegerField(_('Ip range block ending, as integer'), db_index=True)
+    country = models.ForeignKey(Country)
+    region = models.ForeignKey(Region, null=True)
+    city = models.ForeignKey(City, null=True)
+
+    objects = IpRangeManager()
+
+    class Meta:
+        verbose_name = _('IP range')
+        verbose_name_plural = _("IP ranges")
+
+
+class abstractclassmethod(classmethod):
+    """ Abstract classmethod decorator from python 3"""
+    __isabstractmethod__ = True
+
+    def __init__(self, callable):
+        callable.__isabstractmethod__ = True
+        super(abstractclassmethod, self).__init__(callable)
+
+
+class AbsractModel(ABCMeta, ModelBase):
+    pass
+
+
+class GeoLocationFacade(compat.with_metaclass(AbsractModel), models.Model):
+    """ Interface for custom geographic models.
+        Model represents a Facade pattern for concrete GeoIP models.
+    """
+
+    @abstractclassmethod
+    def get_by_ip_range(cls, ip_range):
+        """
+        Return single model instance for given IP range.
+        If no location matches the range, raises DoesNotExist exception.
+
+        :param ip_range: User's IpRange to search for.
+        :type ip_range: IpRange
+        :return: GeoLocationFacade single object
+        """
+        return NotImplemented
+
+    @abstractclassmethod
+    def get_default_location(cls):
+        """
+        Return default location for cases where ip geolocation fails.
+
+        :return: GeoLocationFacade
+        """
+        return NotImplemented
+
+    @abstractclassmethod
+    def get_available_locations(cls):
+        """
+        Return all locations available for users to select in frontend
+
+        :return: GeoLocationFacade
+        """
+        return NotImplemented
+
+    class Meta:
+        abstract = True
---
+++ packages/django-geoip/django-geoip-0.5.2.tar.gz/django-geoip-0.5.2/setup.py
@@ -0,0 +1,35 @@
+from os import path
+import codecs
+from setuptools import setup, find_packages
+
+read = lambda filepath: codecs.open(filepath, 'r', 'utf-8').read()
+
+
+setup(
+    name='django-geoip',
+    version='0.5.2',
+    author='Ilya Baryshev',
+    author_email='baryshev@gmail.com',
+    packages=find_packages(exclude=("tests", "test_app", "docs")),
+    url='https://github.com/futurecolors/django-geoip',
+    license='MIT',
+    description="App to figure out where your visitors are from by their IP address.",
+    long_description=read(path.join(path.dirname(__file__), 'README.rst')),
+    install_requires=[
+        'django-appconf>=0.6',
+        'requests>=1.0.4',
+    ],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
+)
