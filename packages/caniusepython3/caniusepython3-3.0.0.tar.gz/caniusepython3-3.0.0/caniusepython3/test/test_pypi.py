b2765b5ba0549d939fc262595876a869f14da3a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:23 +0000

    Add package caniusepython3/3.0.0/caniusepython3-3.0.0.tar.gz

---
+++ packages/caniusepython3/caniusepython3-3.0.0.tar.gz/caniusepython3-3.0.0/caniusepython3/test/test_pypi.py
@@ -0,0 +1,78 @@
+# Copyright 2014 Google Inc. All rights reserved.
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from __future__ import unicode_literals
+
+from caniusepython3 import pypi
+from caniusepython3.test import unittest
+
+
+class NameTests(unittest.TestCase):
+
+    def test_simple(self):
+        want = 'simple-name_with.everything-separator_known'
+        got = pypi.just_name(want)
+        self.assertEqual(got, want)
+
+    def test_requirements(self):
+        want = 'project.name'
+        got = pypi.just_name(want + '>=2.0.1')
+        self.assertEqual(got, want)
+
+    def test_bad_requirements(self):
+        # From the OpenStack requirements file:
+        # https://raw2.github.com/openstack/requirements/master/global-requirements.txt
+        want = 'warlock'
+        got = pypi.just_name(want + '>1.01<2')
+        self.assertEqual(got, want)
+
+    def test_metadata(self):
+        want = 'foo'
+        got = pypi.just_name("foo; sys.platform == 'okook'")
+        self.assertEqual(got, want)
+
+
+class OverridesTests(unittest.TestCase):
+
+    def test_all_lowercase(self):
+        for name in pypi.overrides():
+            self.assertEqual(name, name.lower())
+
+
+class NetworkTests(unittest.TestCase):
+
+    def py3_classifiers(self):
+        key_classifier = 'Programming Language :: Python :: 3'
+        classifiers = frozenset(pypi.py3_classifiers())
+        self.asssertIn(key_classifier, classifiers)
+        self.assertGreaterEqual(len(classifiers), 5)
+        for classifier in classifiers:
+            self.assertTrue(classifier.startswith(key_classifier))
+
+
+    def test_all_py3_projects(self):
+        projects = pypi.all_py3_projects()
+        self.assertGreater(len(projects), 3000)
+        self.assertTrue(all(project == project.lower() for project in projects))
+        self.assertTrue(frozenset(pypi.overrides().keys()).issubset(projects))
+
+    def test_all_py3_projects_explicit_overrides(self):
+        added_port = 'asdfasdfasdfadsffffdasfdfdfdf'
+        projects = pypi.all_py3_projects(set([added_port]))
+        self.assertIn(added_port, projects)
+
+    def test_all_projects(self):
+        projects = pypi.all_projects()
+        self.assertTrue(all(project == project.lower() for project in projects))
+        self.assertGreaterEqual(len(projects), 40000)
---
+++ packages/caniusepython3/caniusepython3-3.0.0.tar.gz/caniusepython3-3.0.0/setup.py
@@ -0,0 +1,50 @@
+from setuptools import setup
+
+import os
+
+
+# Installed name used for various commands (both script and setuptools).
+command_name = os.environ.get('CIU_ALT_NAME') or 'caniusepython3'
+
+with open('README_PyPI.rst') as file:
+    long_description = file.read()
+
+with open('dev_requirements.txt') as file:
+    tests_require = [dep.strip() for dep in file.readlines()]
+
+setup(name='caniusepython3',
+      version='3.0.0',
+      description='Determine what projects are blocking you from porting to Python 3',
+      long_description=long_description,
+      author='Brett Cannon',
+      author_email='brett@python.org',
+      url='https://github.com/brettcannon/caniusepython3',
+      packages=['caniusepython3', 'caniusepython3.test'],
+      include_package_data=True,
+      install_requires=['distlib', 'setuptools', 'pip',  # Input flexibility
+                        'argparse', 'futures'],  # Functionality
+      tests_require=tests_require,  # Testing, external due to Travis
+      test_suite='caniusepython3.test',
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Environment :: Console',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: Apache Software License',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2.6',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.2',
+          'Programming Language :: Python :: 3.3',
+          'Programming Language :: Python :: 3.4',
+      ],
+      entry_points={
+          'console_scripts': [
+              '{0} = caniusepython3.__main__:main'.format(command_name),
+          ],
+          'distutils.commands': [
+              '{0} = caniusepython3.command:Command'.format(command_name),
+          ],
+      },
+      zip_safe=True,
+)
