bbd88604b3ef9250edeccfd726c5c8a7a4f61d8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:56 +0000

    Add package rss2sn/1.0.1/rss2sn-1.0.1.tar.gz

---
+++ packages/rss2sn/rss2sn-1.0.1.tar.gz/rss2sn-1.0.1/setup.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+__version__ = "1.0.1"
+import os
+from distutils.core import setup
+
+setup(
+    name='rss2sn',
+    version=__version__,
+    url='http://git.tenak.net/?p=rss2sn.git',
+    author='marc0s',
+    author_email='marc0s@tenak.net',
+    py_modules=['rss2sn'],
+    install_requires=['StatusNet', ' feedparser', 'python-dateutil <= 1.5'],
+    license='GPL3',
+    description='A quick and dirty RSS to StatusNet bot',
+    long_description=open('README.md').read(),
+)
+
