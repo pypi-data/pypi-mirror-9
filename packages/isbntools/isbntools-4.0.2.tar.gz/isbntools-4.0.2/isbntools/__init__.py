2d2f6271c94bbd8643749589c97d51bd08370305
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:55 +0000

    Add package isbntools/4.0.2/isbntools-4.0.2.tar.gz

---
+++ packages/isbntools/isbntools-4.0.2.tar.gz/isbntools-4.0.2/isbntools/__init__.py
@@ -0,0 +1,6 @@
+"""Define isbntools version and support IDs."""
+
+__version__ = '4.0.2'                               # <-- literal IDs
+__support__ = 'py26, py27, py33, py34, pypy'        # <-- literal IDs
+
+
