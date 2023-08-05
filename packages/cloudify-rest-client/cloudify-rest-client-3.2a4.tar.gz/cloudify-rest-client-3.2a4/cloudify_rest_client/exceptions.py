1516e0a1099d3dce43bcbe387f9e740220441ab5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:28 +0000

    Add package cloudify-rest-client/3.2a4/cloudify-rest-client-3.2a4.tar.gz

---
+++ packages/cloudify-rest-client/cloudify-rest-client-3.2a4.tar.gz/cloudify-rest-client-3.2a4/cloudify_rest_client/exceptions.py
@@ -0,0 +1,154 @@
+########
+# Copyright (c) 2014 GigaSpaces Technologies Ltd. All rights reserved
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+#    * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    * See the License for the specific language governing permissions and
+#    * limitations under the License.
+
+
+class CloudifyClientError(Exception):
+
+    def __init__(self, message, server_traceback=None,
+                 status_code=-1, error_code=None):
+        super(CloudifyClientError, self).__init__(message)
+        self.status_code = status_code
+        self.error_code = error_code
+        self.server_traceback = server_traceback
+
+    def __str__(self):
+        if self.status_code != -1:
+            return '{0}: {1}'.format(self.status_code, self.message)
+        return self.message
+
+
+class DeploymentEnvironmentCreationInProgressError(CloudifyClientError):
+    """
+    Raised when there's attempt to execute a deployment workflow and
+    deployment environment creation workflow execution is still running.
+    In such a case, workflow execution should be retried after a reasonable
+    time or after the execution of deployment environment creation workflow
+    has terminated.
+    """
+
+    ERROR_CODE = 'deployment_environment_creation_in_progress_error'
+
+    def __init__(self, message, server_traceback=None,
+                 status_code=-1, error_code=None):
+        super(DeploymentEnvironmentCreationInProgressError,
+              self).__init__(message, server_traceback,
+                             status_code, error_code)
+
+
+class DeploymentEnvironmentCreationPendingError(CloudifyClientError):
+    """
+    Raised when there's attempt to execute a deployment workflow and
+    deployment environment creation workflow execution is pending.
+    In such a case, workflow execution should be retried after a reasonable
+    time or after the execution of deployment environment creation workflow
+    has terminated.
+    """
+
+    ERROR_CODE = 'deployment_environment_creation_pending_error'
+
+    def __init__(self, message, server_traceback=None,
+                 status_code=-1, error_code=None):
+        super(DeploymentEnvironmentCreationPendingError,
+              self).__init__(message, server_traceback,
+                             status_code, error_code)
+
+
+class IllegalExecutionParametersError(CloudifyClientError):
+    """
+    Raised when an attempt to execute a workflow with wrong/missing parameters
+    has been made.
+    """
+
+    ERROR_CODE = 'illegal_execution_parameters_error'
+
+    def __init__(self, message, server_traceback=None,
+                 status_code=-1, error_code=None):
+        super(IllegalExecutionParametersError, self).__init__(
+            message, server_traceback, status_code, error_code)
+
+
+class NoSuchIncludeFieldError(CloudifyClientError):
+    """
+    Raised when an _include query parameter contains a field which does not
+    exist for the queried data model.
+    """
+
+    ERROR_CODE = 'no_such_include_field_error'
+
+    def __init__(self, message, server_traceback=None,
+                 status_code=-1, error_code=None):
+        super(NoSuchIncludeFieldError, self).__init__(
+            message, server_traceback, status_code, error_code)
+
+
+class MissingRequiredDeploymentInputError(CloudifyClientError):
+    """
+    Raised when a required deployment input was not specified on deployment
+    creation.
+    """
+    ERROR_CODE = 'missing_required_deployment_input_error'
+
+    def __init__(self, message, server_traceback=None,
+                 status_code=-1, error_code=None):
+        super(MissingRequiredDeploymentInputError, self).__init__(
+            message, server_traceback, status_code, error_code)
+
+
+class UnknownDeploymentInputError(CloudifyClientError):
+    """
+    Raised when an unexpected input was specified on deployment creation.
+    """
+    ERROR_CODE = 'unknown_deployment_input_error'
+
+    def __init__(self, message, server_traceback=None,
+                 status_code=-1, error_code=None):
+        super(UnknownDeploymentInputError, self).__init__(
+            message, server_traceback, status_code, error_code)
+
+
+class FunctionsEvaluationError(CloudifyClientError):
+    """
+    Raised when function evaluation failed.
+    """
+    ERROR_CODE = 'functions_evaluation_error'
+
+    def __init__(self, message, server_traceback=None,
+                 status_code=-1, error_code=None):
+        super(FunctionsEvaluationError, self).__init__(
+            message, server_traceback, status_code, error_code)
+
+
+class UnknownModificationStageError(CloudifyClientError):
+    """
+    Raised when an unknown modification stage was provided.
+    """
+    ERROR_CODE = 'unknown_modification_stage_error'
+
+    def __init__(self, message, server_traceback=None,
+                 status_code=-1, error_code=None):
+        super(UnknownModificationStageError, self).__init__(
+            message, server_traceback, status_code, error_code)
+
+ERROR_MAPPING = dict([
+    (error.ERROR_CODE, error)
+    for error in [
+        DeploymentEnvironmentCreationInProgressError,
+        DeploymentEnvironmentCreationPendingError,
+        IllegalExecutionParametersError,
+        NoSuchIncludeFieldError,
+        MissingRequiredDeploymentInputError,
+        UnknownDeploymentInputError,
+        FunctionsEvaluationError,
+        UnknownModificationStageError]])
---
+++ packages/cloudify-rest-client/cloudify-rest-client-3.2a4.tar.gz/cloudify-rest-client-3.2a4/cloudify_rest_client/executions.py
@@ -0,0 +1,171 @@
+########
+# Copyright (c) 2014 GigaSpaces Technologies Ltd. All rights reserved
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+#    * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    * See the License for the specific language governing permissions and
+#    * limitations under the License.
+
+
+class Execution(dict):
+    """Cloudify workflow execution."""
+    TERMINATED = 'terminated'
+    FAILED = 'failed'
+    CANCELLED = 'cancelled'
+    PENDING = 'pending'
+    STARTED = 'started'
+    CANCELLING = 'cancelling'
+    FORCE_CANCELLING = 'force_cancelling'
+    END_STATES = [TERMINATED, FAILED, CANCELLED]
+
+    def __init__(self, execution):
+        self.update(execution)
+
+    @property
+    def id(self):
+        """
+        :return: The execution's id.
+        """
+        return self.get('id')
+
+    @property
+    def deployment_id(self):
+        """
+        :return: The deployment's id this execution is related to.
+        """
+        return self.get('deployment_id')
+
+    @property
+    def status(self):
+        """
+        :return: The execution's status.
+        """
+        return self.get('status')
+
+    @property
+    def error(self):
+        """
+        :return: The execution error in a case of failure, otherwise None.
+        """
+        return self.get('error')
+
+    @property
+    def workflow_id(self):
+        """
+        :return: The id of the workflow this execution represents.
+        """
+        return self.get('workflow_id')
+
+    @property
+    def parameters(self):
+        """
+        :return: The execution's parameters
+        """
+        return self.get('parameters')
+
+    @property
+    def created_at(self):
+        """
+        :return: The execution creation time.
+        """
+        return self.get('created_at')
+
+
+class ExecutionsClient(object):
+
+    def __init__(self, api):
+        self.api = api
+
+    def list(self, deployment_id=None, _include=None):
+        """Returns a list of executions.
+
+        :param deployment_id: Optional deployment id to get executions for.
+        :param _include: List of fields to include in response.
+        :return: Executions list.
+        """
+        uri = '/executions'
+        params = None
+        if deployment_id:
+            params = {'deployment_id': deployment_id}
+        response = self.api.get(uri, params=params, _include=_include)
+        return [Execution(item) for item in response]
+
+    def get(self, execution_id, _include=None):
+        """Get execution by its id.
+
+        :param execution_id: Id of the execution to get.
+        :param _include: List of fields to include in response.
+        :return: Execution.
+        """
+        assert execution_id
+        uri = '/executions/{0}'.format(execution_id)
+        response = self.api.get(uri, _include=_include)
+        return Execution(response)
+
+    def update(self, execution_id, status, error=None):
+        """Update execution with the provided status and optional error.
+
+        :param execution_id: Id of the execution to update.
+        :param status: Updated execution status.
+        :param error: Updated execution error (optional).
+        :return: Updated execution.
+        """
+
+        uri = '/executions/{0}'.format(execution_id)
+        params = {'status': status}
+        if error:
+            params['error'] = error
+        response = self.api.patch(uri, data=params)
+        return Execution(response)
+
+    def start(self, deployment_id, workflow_id, parameters=None,
+              allow_custom_parameters=False, force=False):
+        """Starts a deployment's workflow execution whose id is provided.
+
+        :param deployment_id: The deployment's id to execute a workflow for.
+        :param workflow_id: The workflow to be executed id.
+        :param parameters: Parameters for the workflow execution.
+        :param allow_custom_parameters: Determines whether to allow
+         parameters which weren't defined in the workflow parameters schema
+         in the blueprint.
+        :param force: Determines whether to force the execution of the workflow
+         in a case where there's an already running execution for this
+         deployment.
+        :raises: IllegalExecutionParametersError
+        :return: The created execution.
+        """
+        assert deployment_id
+        assert workflow_id
+        data = {
+            'deployment_id': deployment_id,
+            'workflow_id': workflow_id,
+            'parameters': parameters,
+            'allow_custom_parameters': str(allow_custom_parameters).lower(),
+            'force': str(force).lower()
+        }
+        uri = '/executions'
+        response = self.api.post(uri,
+                                 data=data,
+                                 expected_status_code=201)
+        return Execution(response)
+
+    def cancel(self, execution_id, force=False):
+        """Cancels the execution which matches the provided execution id.
+
+        :param execution_id: Id of the execution to cancel.
+        :param force: Boolean describing whether to send a 'cancel' or a 'force-cancel' action  # NOQA
+        :return: Cancelled execution.
+        """
+        uri = '/executions/{0}'.format(execution_id)
+        action = 'force-cancel' if force else 'cancel'
+        response = self.api.post(uri,
+                                 data={'action': action},
+                                 expected_status_code=200)
+        return Execution(response)
