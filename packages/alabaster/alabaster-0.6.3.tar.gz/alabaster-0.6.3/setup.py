f3c6b810029e5b9a22246d9d64808a5d25967fe4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:13 +0000

    Add package alabaster/0.6.3/alabaster-0.6.3.tar.gz

---
+++ packages/alabaster/alabaster-0.6.3.tar.gz/alabaster-0.6.3/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+# Version info -- read without importing
+_locals = {}
+with open('alabaster/_version.py') as fp:
+    exec(fp.read(), None, _locals)
+version = _locals['__version__']
+
+setup(
+    name='alabaster',
+    version=version,
+    description='A configurable sidebar-enabled Sphinx theme',
+    author='Jeff Forcier',
+    author_email='jeff@bitprophet.org',
+    url='https://github.com/bitprophet/alabaster',
+    packages=['alabaster'],
+    include_package_data=True,
+    classifiers=[
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: BSD License',
+          'Operating System :: OS Independent',
+          'Topic :: Documentation',
+          'Topic :: Software Development :: Documentation',
+    ],
+)
