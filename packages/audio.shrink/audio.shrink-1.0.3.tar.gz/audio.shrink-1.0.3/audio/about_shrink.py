89c6ed228fc2fbb80616d71597fbb49b7c109cb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:46 +0000

    Add package audio.shrink/1.0.3/audio.shrink-1.0.3.tar.gz

---
+++ packages/audio.shrink/audio.shrink-1.0.3.tar.gz/audio.shrink-1.0.3/audio/about_shrink.py
@@ -0,0 +1,16 @@
+# coding: utf-8
+
+metadata = dict(
+  __name__        = "audio.shrink",
+  __version__     = "1.0.3",
+  __license__     = "MIT License",  
+  __author__      = u"Sébastien Boisgérault <Sebastien.Boisgerault@mines-paristech.fr>",
+  __url__         = None,
+  __summary__     = "SHRINK Lossless Audio Codec",
+  __readme__      = None,
+  __classifiers__ = None,
+)
+
+globals().update(metadata)
+
+__all__ = metadata.keys()
