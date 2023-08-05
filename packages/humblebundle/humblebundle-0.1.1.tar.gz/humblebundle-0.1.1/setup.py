b81ff51022f95c98bdd01ec8e904349c854e0abf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:19 +0000

    Add package humblebundle/0.1.1/humblebundle-0.1.1.tar.gz

---
+++ packages/humblebundle/humblebundle-0.1.1.tar.gz/humblebundle-0.1.1/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+__author__ = "Joel Pedraza"
+__copyright__ = "Copyright 2014, Joel Pedraza"
+__license__ = "MIT"
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(name='humblebundle',
+      version='0.1.1',
+      description='Humble Indie Bundle API client',
+      author='Joel Pedraza',
+      author_email='joel@joelpedraza.com',
+      url='https://github.com/saik0/humblebundle-python',
+      download_url='https://github.com/saik0/humblebundle-python/tarball/0.1.1',
+      install_requires=['requests >= 2.0.0'],
+      packages=['humblebundle'],
+      )
