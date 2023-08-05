c1fa7f74698ee1559ee1aebb21d316cd5c7fb00b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:58 +0000

    Add package django-redis/3.8.1/django-redis-3.8.1.tar.gz

---
+++ packages/django-redis/django-redis-3.8.1.tar.gz/django-redis-3.8.1/setup.py
@@ -0,0 +1,45 @@
+from setuptools import setup
+
+description = """
+Full featured redis cache backend for Django.
+"""
+
+setup(
+    name = "django-redis",
+    url = "https://github.com/niwibe/django-redis",
+    author = "Andrei Antoukh",
+    author_email = "niwi@niwi.be",
+    version="3.8.1",
+    packages = [
+        "redis_cache",
+        "redis_cache.client",
+        "django_redis",
+        "django_redis.client",
+    ],
+    description = description.strip(),
+    install_requires=[
+        "redis>=2.10.0",
+    ],
+    zip_safe=False,
+    include_package_data = True,
+    package_data = {
+        "": ["*.html"],
+    },
+    classifiers = [
+        "Development Status :: 5 - Production/Stable",
+        "Operating System :: OS Independent",
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "License :: OSI Approved :: BSD License",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Utilities",
+    ],
+)
