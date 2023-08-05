a963c35c3f56252b6bb19367eb95c7288fd4fce3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:38 +0000

    Add package django_audit_trail/0.0.23/django_audit_trail-0.0.23.tar.gz

---
+++ packages/django_audit_trail/django_audit_trail-0.0.23.tar.gz/django_audit_trail-0.0.23/audit_trail/__init__.py
@@ -0,0 +1,19 @@
+from django.contrib.contenttypes.models import ContentType
+from watcher import AuditTrailWatcher
+from models import AuditTrail
+
+
+def audit_trail_watch(cls, **kwargs):
+    related_watcher = AuditTrailWatcher(**kwargs)
+    if related_watcher.contribute_to_class(cls):
+        related_watcher.init_signals()
+
+
+def get_for_object(obj):
+    ct = ContentType.objects.get_for_model(obj)
+    return AuditTrail.objects.filter(content_type=ct, object_id=obj.id)
+
+
+default_app_config = 'audit_trail.app.AuditTrailAppConfig'
+
+__all__ = ['audit_trail_watch', 'get_for_object', 'default_app_config']
