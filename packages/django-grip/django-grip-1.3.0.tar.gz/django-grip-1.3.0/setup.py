b3aaa424eaff3cba4861dc138744c9065f798a95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:52 +0000

    Add package django-grip/1.3.0/django-grip-1.3.0.tar.gz

---
+++ packages/django-grip/django-grip-1.3.0.tar.gz/django-grip-1.3.0/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+name='django-grip',
+version='1.3.0',
+description='Django GRIP library',
+author='Justin Karneges',
+author_email='justin@fanout.io',
+url='https://github.com/fanout/django-grip',
+license='MIT',
+py_modules=['django_grip'],
+install_requires=['pubcontrol>=2.0.0', 'gripcontrol>=2.3.0'],
+classifiers=[
+	'Topic :: Utilities',
+	'License :: OSI Approved :: MIT License'
+]
+)
