8b5f5748e4104082fedac80fc5c98446f555935d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:14 +0000

    Add package test_steps/0.5.1/test_steps-0.5.1.tar.gz

---
+++ packages/test_steps/test_steps-0.5.1.tar.gz/test_steps-0.5.1/setup.py
@@ -0,0 +1,29 @@
+
+from setuptools import setup
+import test_steps
+
+setup(
+    name='test_steps',
+    description='Simple way to run test steps and automatic logging',
+    author='Steven LI',
+    author_email='steven004@gmail.com',
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Unix",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: MacOS :: MacOS X",
+        "Topic :: Software Development :: Testing",
+        "Topic :: Software Development :: Quality Assurance",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Utilities",
+        "Topic :: System :: Logging",
+        "Programming Language :: Python :: 3"
+    ],
+    version=test_steps.__version__,
+    url = "https://github.com/steven004/TestSteps",
+    packages = ['test_steps'],
+    #include_package_data = True,
+)
+
