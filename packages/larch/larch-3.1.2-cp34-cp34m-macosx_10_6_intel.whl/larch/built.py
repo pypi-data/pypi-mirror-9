121e463215c1890186e35fa426789b60a3e9c7dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:44 +0000

    Add package larch/3.1.2/larch-3.1.2-cp34-cp34m-macosx_10_6_intel.whl

---
+++ packages/larch/larch-3.1.2-cp34-cp34m-macosx_10_6_intel.whl/larch/built.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+# This file automatically created while building Larch. Do not edit manually.
+configuration='DEFAULT'
+time='12:29:12 PM CST'
+date='07 Jan 2015'
+day='Wednesday'
+version='3.1.1-JAN2015-3-gee04b6b'
+
+build='%s (%s, %s %s)'%(version,day,date,time)
+from .apsw import apswversion, sqlitelibversion
+from .utilities import dicta
+versions = dicta({
+'larch':version,
+'apsw':apswversion(),
+'sqlite':sqlitelibversion(),
+})
+
+try:
+	import numpy
+	versions['numpy'] = numpy.version.version
+except:
+	versions['numpy'] = 'failed'
+
+try:
+	import scipy
+	versions['scipy'] = scipy.version.version
+except:
+	versions['scipy'] = 'failed'
+
+try:
+	import pandas
+	versions['pandas'] = pandas.version.version
+except:
+	versions['pandas'] = 'failed'
+
+import sys
+versions['python'] = "{0}.{1}.{2} {3}".format(*(sys.version_info))
+build_config='Larch %s built on %s, %s %s'%(configuration,day,date,time)
