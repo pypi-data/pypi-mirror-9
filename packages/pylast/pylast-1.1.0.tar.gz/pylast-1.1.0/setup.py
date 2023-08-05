4d60a46256e2619288a35605389b9b8184f4470d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:37 +0000

    Add package pylast/1.1.0/pylast-1.1.0.tar.gz

---
+++ packages/pylast/pylast-1.1.0.tar.gz/pylast-1.1.0/setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+import os
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name="pylast",
+    version="1.1.0",
+    author="Amr Hassan <amr.hassan@gmail.com>",
+    install_requires=['six'],
+    tests_require=['mock', 'pytest', 'coverage', 'pep8', 'pyyaml', 'pyflakes'],
+    description=("A Python interface to Last.fm "
+                 "(and other API compatible social networks)"),
+    author_email="amr.hassan@gmail.com",
+    url="https://github.com/pylast/pylast",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: Apache Software License",
+        "Topic :: Internet",
+        "Topic :: Multimedia :: Sound/Audio",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+    ],
+    keywords=["Last.fm", "music", "scrobble", "scrobbling"],
+    packages=find_packages(exclude=('tests*',)),
+    license="Apache2"
+)
+
+# End of file
