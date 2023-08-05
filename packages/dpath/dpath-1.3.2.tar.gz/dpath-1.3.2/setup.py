da71941a53b740d827617b3ea3a45a43a3cddf64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:25 +0000

    Add package dpath/1.3.2/dpath-1.3.2.tar.gz

---
+++ packages/dpath/dpath-1.3.2.tar.gz/dpath-1.3.2/setup.py
@@ -0,0 +1,39 @@
+from distutils.core import setup
+import dpath.version
+import os
+import sys
+
+long_description = open(
+    os.path.join(
+        os.path.dirname(__file__),
+        'README.rst'
+    )
+).read()
+
+if __name__ == "__main__":
+    setup(
+        name="dpath",
+        url="https://www.github.com/akesterson/dpath-python",
+        version=dpath.version.VERSION,
+        description="Filesystem-like pathing and searching for dictionaries",
+        long_description=long_description,
+        author=("Caleb Case, "
+                "Andrew Kesterson"),
+        author_email="calebcase@gmail.com, andrew@aklabs.net",
+        license="MIT",
+        install_requires=[],
+        scripts=[],
+        packages=["dpath"],
+        data_files=[],
+        classifiers=[
+            'Development Status :: 5 - Production/Stable',
+            'Environment :: Console',
+            'Intended Audience :: Developers',
+            'License :: OSI Approved :: MIT License',
+            'Natural Language :: English',
+            'Programming Language :: Python :: 2.7',
+            'Programming Language :: Python :: 3',
+            'Topic :: Software Development :: Libraries :: Python Modules',
+        ],
+    )
+
---
+++ packages/dpath/dpath-1.3.2.tar.gz/dpath-1.3.2/tests/test_util_new.py
@@ -0,0 +1,27 @@
+import nose
+import dpath.util
+
+def test_set_new_separator():
+    dict = {
+        "a": {
+            }
+        }
+    dpath.util.new(dict, ';a;b', 1, separator=";")
+    assert(dict['a']['b'] == 1)
+
+def test_set_new_dict():
+    dict = {
+        "a": {
+            }
+        }
+    dpath.util.new(dict, '/a/b', 1)
+    assert(dict['a']['b'] == 1)
+
+def test_set_new_list():
+    dict = {
+        "a": [
+            ]
+        }
+    dpath.util.new(dict, '/a/1', 1)
+    assert(dict['a'][1] == 1)
+    assert(dict['a'][0] == None)
