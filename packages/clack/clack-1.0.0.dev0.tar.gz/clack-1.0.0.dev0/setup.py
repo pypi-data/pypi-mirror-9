fa7b05ff537a24f6d5d3eb8a34818f02a5ad19b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:35 +0000

    Add package clack/1.0.0.dev0/clack-1.0.0.dev0.tar.gz

---
+++ packages/clack/clack-1.0.0.dev0.tar.gz/clack-1.0.0.dev0/setup.py
@@ -0,0 +1,42 @@
+import setuptools
+
+setuptools.setup(
+    name='clack',
+    version='1.0.0-dev',
+
+    author="Sam Clements",
+    author_email="sam@borntyping.co.uk",
+
+    url="https://github.com/borntyping/clack",
+    description="Run multiple iterations of commands from a config file",
+    long_description=open('README.rst').read(),
+    license="MIT",
+
+    packages=setuptools.find_packages(),
+    package_data={'clack': ['schema.json']},
+
+    install_requires=[
+        'click',
+        'jsonschema'
+    ],
+
+    entry_points={
+        'console_scripts': [
+            'clack = clack.cli:main',
+        ]
+    },
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'License :: OSI Approved',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: Unix',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Software Development :: Libraries'
+    ],
+)
