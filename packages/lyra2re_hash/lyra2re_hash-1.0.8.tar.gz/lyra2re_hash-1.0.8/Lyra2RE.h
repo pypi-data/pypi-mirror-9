103d15fb1be17adbd47b8f9011915d7a9b964465
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:15 +0000

    Add package lyra2re_hash/1.0.8/lyra2re_hash-1.0.8.tar.gz

---
+++ packages/lyra2re_hash/lyra2re_hash-1.0.8.tar.gz/lyra2re_hash-1.0.8/Lyra2RE.h
@@ -0,0 +1,16 @@
+#ifndef LYRA2RE_H
+#define LYRA2RE_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef unsigned int            uint32_t;
+
+void lyra2re_hash(const char* input, char* output);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
