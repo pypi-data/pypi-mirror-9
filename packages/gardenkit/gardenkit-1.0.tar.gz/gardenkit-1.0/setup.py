e2f708158afa3aa89dd5e8864f911d5381fc8582
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:24 +0000

    Add package gardenkit/1.0/gardenkit-1.0.tar.gz

---
+++ packages/gardenkit/gardenkit-1.0.tar.gz/gardenkit-1.0/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'gardenkit',
+  packages = ['gardenkit'], # this must be the same as the name above
+  version = '1.0',
+  description = 'GreenIQ Smart Garden Hub API',
+  author = 'Golan Derazon',
+  author_email = 'golan@greeniq.co',
+  url = 'https://bitbucket.org/greeniqrd/gardenkit-python-sdk', # use the URL to the github repo
+  download_url = 'https://github.com/peterldowns/mypackage/tarball/0.1', # I'll explain this in a second
+  keywords = ['IoT', 'home', 'automation'], # arbitrary keywords
+  classifiers = [],
+)
