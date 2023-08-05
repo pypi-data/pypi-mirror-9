e8e6dabd37b7923f0a25756007ca24a8ef5efcb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:14 +0000

    Add package bugsnag/2.3.0/bugsnag-2.3.0.tar.gz

---
+++ packages/bugsnag/bugsnag-2.3.0.tar.gz/bugsnag-2.3.0/bugsnag/django/middleware.py
@@ -0,0 +1,31 @@
+from __future__ import division, print_function, absolute_import
+
+import bugsnag
+import bugsnag.django
+
+class BugsnagMiddleware(object):
+    def __init__(self):
+        bugsnag.django.configure()
+
+    # pylint: disable-msg=R0201
+    def process_request(self, request):
+        bugsnag.configure_request(django_request=request)
+
+        return None
+
+    # pylint: disable-msg=W0613
+    def process_response(self, request, response):
+        bugsnag.clear_request_config()
+
+        return response
+
+    def process_exception(self, request, exception):
+        try:
+            bugsnag.auto_notify(exception)
+
+        except Exception as exc:
+            bugsnag.log("Error in exception middleware: %s" % exc)
+
+        bugsnag.clear_request_config()
+
+        return None
