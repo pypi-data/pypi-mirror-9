4a17f3d098e9d30105c20f6b76affc6f934e9e0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:08 +0000

    Add package jsonm/1.0.2/jsonm-1.0.2.tar.gz

---
+++ packages/jsonm/jsonm-1.0.2.tar.gz/jsonm-1.0.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+setup(
+    name="jsonm",
+    version="1.0.2",
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    install_requires=[],
+    scripts=[],
+    url="https://github.com/dantezhu/jsonm",
+    license="MIT",
+    author="dantezhu",
+    author_email="dantezhu@qq.com",
+    description="pyhon object to (json + redis)",
+)
