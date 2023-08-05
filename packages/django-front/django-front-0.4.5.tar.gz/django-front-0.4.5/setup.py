e136cd0cc1b55bdf146c9331707753e52b40800c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:08 +0000

    Add package django-front/0.4.5/django-front-0.4.5.tar.gz

---
+++ packages/django-front/django-front-0.4.5.tar.gz/django-front-0.4.5/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+setup(
+    name='django-front',
+    version='0.4.5',
+    description='A Django application to allow of front-end editing',
+    author='Marco Bonetti',
+    author_email='mbonetti@gmail.com',
+    url='https://github.com/mbi/django-front',
+    license='MIT',
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+    ],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'django-classy-tags >= 0.4',
+        'Django >= 1.4',
+        'six'
+    ]
+)
