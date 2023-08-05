758bd1477c6cb4eb324beef5b115e097d18ea19c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:31 +0000

    Add package django-social-launch/0.1.1/django_social_launch-0.1.1-py2.py3-none-any.whl

---
+++ packages/django-social-launch/django_social_launch-0.1.1-py2.py3-none-any.whl/django_social_launch_test_project/runtests.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+import os, sys
+import settings
+from django.core.management import call_command
+
+os.environ['DJANGO_SETTINGS_MODULE'] = '%s.settings' % settings.PACKAGE_MODULE
+sys.path.insert(0, settings.PACKAGE_PARENT_DIR)
+
+def runtests():
+    call_command('test', 'django_social_launch')
+    sys.exit()
+
+if __name__ == '__main__':
+    runtests()
