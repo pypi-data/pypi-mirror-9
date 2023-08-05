ad5623db61c68617ecf250315c56c327d242804d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:03 +0000

    Add package emanual/0.3.9/emanual-0.3.9.tar.gz

---
+++ packages/emanual/emanual-0.3.9.tar.gz/emanual-0.3.9/emanual/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+"""
+EManual CLI - Command Line Interface for EManual
+
+"""
+__version__ = '0.3.9'
+__author__ = 'Jayin Ton'
---
+++ packages/emanual/emanual-0.3.9.tar.gz/emanual-0.3.9/emanual/cli.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+
+
+from emanual import make, __version__
+import click
+
+
+def print_version(ctx, param, value):
+    if not value or ctx.resilient_parsing:
+        return
+    click.echo(__version__)
+    ctx.exit()
+
+
+@click.group()
+@click.option('--version', is_flag=True, callback=print_version,
+              expose_value=False, is_eager=True, help='Show the version')
+def main():
+    """
+    Welecome to EManual CLI!
+    """
+    pass
+
+
+@main.command('create')
+@click.argument('lang', )
+def create_info(lang):
+    """
+    Create the info.json File for Given Lang 
+    """
+    make.create_info(lang)
+
+
+@main.command('server')
+@click.option('--port', default=8000, help='port to listen')
+def server(port):
+    """
+    Server to preview the markdown files
+    """
+    import os
+
+    os.system('python -m SimpleHTTPServer %s' % port)
+
+
+@main.command('dist')
+@click.argument('lang', )
+def dist(lang):
+    """
+    Distributing the markdown
+    """
+    make.dist_zip(lang)
+
+
+@main.command('newsfeeds')
+@click.argument('update', default=True)
+def newsfeeds(update):
+    """
+    The cli for NewsFeeds module
+    """
+    if update:
+        make.newsfeeds_update()
