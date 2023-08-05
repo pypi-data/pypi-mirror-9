35571cc7d58a5027d686d82885097fa848998ddd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:40 +0000

    Add package djmicrosip_faexist/0.1.9/djmicrosip_faexist-0.1.9.zip

---
+++ packages/djmicrosip_faexist/djmicrosip_faexist-0.1.9.zip/djmicrosip_faexist-0.1.9/djmicrosip_faexist/config.py
@@ -0,0 +1,7 @@
+settings = {
+	'name':'Factura existencias',
+    'icon_class':'glyphicon glyphicon-file',
+    'url':r'/djmicrosip_faexist/',
+    'url_main_path':r'djmicrosip_faexist/',
+    'users':['SYSDBA',],
+}
