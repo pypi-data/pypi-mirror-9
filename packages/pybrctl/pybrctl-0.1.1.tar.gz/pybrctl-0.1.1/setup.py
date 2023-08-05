353597d72ab8509331780557b7beb4fb2a575a27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:02 +0000

    Add package pybrctl/0.1.1/pybrctl-0.1.1.tar.gz

---
+++ packages/pybrctl/pybrctl-0.1.1.tar.gz/pybrctl-0.1.1/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+from setuptools import setup, find_packages
+
+setup(
+	name = "pybrctl",
+	version = "0.1.1",
+	packages = find_packages(),
+	author = "Ido Nahshon",
+	author_email = "udragon@gmail.com",
+	description = "Python brctl wrapper",
+	license = "GPLv2",
+	keywords = "brctl, bridge-utils",
+	url = "https://github.com/udragon/pybrctl",
+	long_description = open("README.rst").read(),
+    classifiers = ['License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+                   'Programming Language :: Python',
+                   'Operating System :: POSIX',
+                   'Programming Language :: Python :: 2.7'],
+ 
+)
