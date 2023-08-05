36043584226a0186bbf02d65e2c8039fb6a2e821
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:14 +0000

    Add package arvados_fuse/0.1.20150209182855/arvados_fuse-0.1.20150209182855.tar.gz

---
+++ packages/arvados_fuse/arvados_fuse-0.1.20150209182855.tar.gz/arvados_fuse-0.1.20150209182855/setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+import os
+import sys
+
+from setuptools import setup, find_packages
+from setuptools.command.egg_info import egg_info
+
+SETUP_DIR = os.path.dirname(__file__) or '.'
+README = os.path.join(SETUP_DIR, 'README.rst')
+
+try:
+    import gittaggers
+    tagger = gittaggers.EggInfoFromGit
+except ImportError:
+    tagger = egg_info
+
+setup(name='arvados_fuse',
+      version='0.1',
+      description='Arvados FUSE driver',
+      long_description=open(README).read(),
+      author='Arvados',
+      author_email='info@arvados.org',
+      url="https://arvados.org",
+      download_url="https://github.com/curoverse/arvados.git",
+      license='GNU Affero General Public License, version 3.0',
+      packages=['arvados_fuse'],
+      scripts=[
+        'bin/arv-mount'
+        ],
+      install_requires=[
+        'arvados-python-client>=0.1.20141203150737.277b3c7',
+        'llfuse',
+        'python-daemon',
+        ],
+      test_suite='tests',
+      tests_require=['PyYAML'],
+      zip_safe=False,
+      cmdclass={'egg_info': tagger},
+      )
