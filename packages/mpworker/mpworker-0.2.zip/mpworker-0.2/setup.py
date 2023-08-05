a05f237a3fa811fdbf7581400b1629ff5d7c9925
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:38 +0000

    Add package mpworker/0.2/mpworker-0.2.zip

---
+++ packages/mpworker/mpworker-0.2.zip/mpworker-0.2/setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+
+setup(
+    name='mpworker',
+    version='0.2',
+    description='Easy to use stateful multiprocessing. Asyncio compatible.',
+    url='https://github.com/dustyrockpyle/mpworker',
+    download_url='https://github.com/dustyrockpyle/mpworker/tarball/v0.2',
+    license='MIT',
+    author='Dustin Pyle',
+    author_email='dustyrockpyle@gmail.com',
+    packages=['mpworker'],
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Intended Audience :: Developers',
+        'Development Status :: 3 - Alpha',
+    ],
+)
