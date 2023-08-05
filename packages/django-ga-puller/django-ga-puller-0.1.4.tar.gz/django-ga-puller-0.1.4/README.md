06236193bd82928fcf242ce3e660e4df8927e8d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:56 +0000

    Add package django-ga-puller/0.1.4/django-ga-puller-0.1.4.tar.gz

---
+++ packages/django-ga-puller/django-ga-puller-0.1.4.tar.gz/django-ga-puller-0.1.4/README.md
@@ -0,0 +1,71 @@
+django-ga-puller
+================
+
+Django app used to pull daily Google Analytics data into your django database. It provides abstract models which you should
+inherit from in your app.
+
+To Use
+------
+
+Add the app to your settings.py file:
+
+    INSTALLED_APPS = [...
+                      'ga_puller',
+                      ...]
+                      
+In your app,
+
+Create models that inherit from the abstract classes provided.  Here is a sample of a basic implementation:
+
+    from ga_puller.models import AnalyticsPage, DailyEventTrackingBase, DailyPageTrackingBase
+
+    class Page(AnalyticsPage):
+
+        def __unicode__(self):
+            return "%s" % self.page_path
+
+
+    class DailyEventData(DailyEventTrackingBase):
+    
+        page = models.ForeignKey('Page')
+        
+        @classmethod
+        def get_page_class(cls):
+            return Page
+
+
+    class DailyPageTrackingData(DailyPageTrackingBase):
+    
+        page = models.ForeignKey('Page')
+        
+        @classmethod
+        def get_page_class(cls):
+            return Page
+
+
+Add a list named analytics_import_models defining the data models to the __init__.py of your app module:
+
+    from models import DailyPageTrackingData, DailyEventData
+    analytics_import_models = [DailyPageTrackingData, DailyEventData]
+    
+
+Create a 'private' python package at the root level of your django project (at the same level as the manage.py file).
+
+The application requires a service account for authentication with Google.  These can be configured by logging into your account at https://cloud.google.com.
+
+Copy your Google API private key file to the private directory (name it privatekey.pem). 
+
+Create a python file named 'accounts.py' in the private directory with the following code (update with your account data):
+ 
+    service_account = 'xxxxxx@developer.gserviceaccount.com'
+    view_ids = {'app_name': '#########', ...}
+
+
+This package uses [Google APIs Client Library for Python](https://developers.google.com/api-client-library/python/) which should automatically be installed through setup.
+
+
+Release Notes:
+
+0.1.0: Initial Release
+0.1.1: Updated to support importing from separate views in google analytics.
+0.1.2: Increased the size of the label and page_path fields.
---
+++ packages/django-ga-puller/django-ga-puller-0.1.4.tar.gz/django-ga-puller-0.1.4/ga_puller/management/commands/analytics_import.py
@@ -0,0 +1,87 @@
+from optparse import make_option
+from django.core.management.base import BaseCommand, CommandError
+
+from apiclient.discovery import build
+from oauth2client.client import SignedJwtAssertionCredentials
+from private.accounts import service_account, view_ids
+import datetime
+import httplib2
+import importlib
+
+
+class Command(BaseCommand):
+    yesterday = datetime.date.today() - datetime.timedelta(days=1)
+    option_list = BaseCommand.option_list + (
+        make_option('--year', '-y', dest='year',
+                    default=yesterday.year,
+                    help='Year of analytics data to pull'),
+        make_option('--month', '-m', dest='start_month',
+                    default=yesterday.month,
+                    help='Month to start pulling analytics data for'),
+        make_option('--day', '-d', dest='start_day',
+                    default=yesterday.day,
+                    help='Day to start pulling analytics data for'),
+        make_option('--num_days', '-n', dest='num_days',
+                    default=1,
+                    help='Number of days to pull'),
+        make_option('--app', '-a', dest='app',
+                    help='Name of app to import data related to'),
+        make_option('--key', '-k', dest='key',
+                    default='private/privatekey.pem',
+                    help='Location of the private key file'
+                    )
+    )
+    help = 'Imports analytics data'
+
+    def handle(self, **options):
+        start_year = int(options["year"])
+        start_month = int(options["start_month"])
+        start_day = int(options["start_day"])
+        num_days = int(options["num_days"])
+        app = options["app"]
+        key_file_name = options["key"]
+
+        base_start_date = datetime.date(year=start_year, month=start_month, day=start_day)
+
+        app_module = importlib.import_module(app)
+        view_id = view_ids[app]
+
+        if 'analytics_import_models' not in app_module.__dict__:
+            raise CommandError("Module must contain list 'analytics_import_models' defining the analytics data models.")
+
+        for i in range(0, num_days):
+            start_date = base_start_date + datetime.timedelta(days=i)
+
+            for model in app_module.analytics_import_models:
+                self._load_data(view_id, model, start_date=start_date,
+                                key_file_name=key_file_name)
+
+    @staticmethod
+    def _load_data(view_id, data_model, start_date, key_file_name):
+        f = open(key_file_name, 'rb')
+        key = f.read()
+        f.close()
+        credentials = SignedJwtAssertionCredentials(service_account,
+                                                    key,
+                                                    scope='https://www.googleapis.com/auth/analytics.readonly')
+        http = httplib2.Http()
+        http = credentials.authorize(http)
+        service = build('analytics', 'v3', http=http)
+        data_query = service.data().ga().get(ids='ga:%s' % view_id,
+                                             metrics=data_model.get_metrics(),
+                                             start_date=start_date.strftime("%Y-%m-%d"),
+                                             end_date=start_date.strftime("%Y-%m-%d"),
+                                             dimensions=data_model.get_dimensions(),
+                                             sort=data_model.get_sort(),
+                                             segment='gaid::-1',
+                                             filters=data_model.get_filters(),
+                                             max_results=10000)
+        feed = data_query.execute()
+
+        if 'rows' in feed:
+            data_model.process_data(feed, start_date)
+            print("{} - {} - Processed data".format(start_date,
+                                                    data_model.__name__))
+        else:
+            print("{} - {} - No data available".format(start_date,
+                                                       data_model.__name__))
