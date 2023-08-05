e8c2d1f3ce53d7045e4b7d94bc78fdbb7a7b60f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:51 +0000

    Add package horn/0.01/Horn-0.0.1.tar.gz

---
+++ packages/horn/Horn-0.0.1.tar.gz/Horn-0.0.1/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+from distutils.core import setup, Extension
+
+setup(name="Horn",
+      version="0.0.1",
+      description="A collection of Flask Macros for Hy",
+      author="Matthias Pall Gissurarson",
+      url="https://github.com/Hylands/horn",
+      license='MIT',
+      packages=["horn"],
+      package_data={"horn":["flask.hy"]},
+      install_requires=["hy"],
+    )
