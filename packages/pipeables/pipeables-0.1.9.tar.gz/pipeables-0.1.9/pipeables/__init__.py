bbfc37b28e8edb53202f76f71ed911751cb25cbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:45 +0000

    Add package pipeables/0.1.9/pipeables-0.1.9.tar.gz

---
+++ packages/pipeables/pipeables-0.1.9.tar.gz/pipeables-0.1.9/pipeables/__init__.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+import os
+import inspect
+import imp
+import sys
+
+#get the path to the users directory of queries
+from os.path import expanduser
+home = expanduser("~")
+
+required_properties = ['proj','name','q','params','db']
+queries = {}
+sys.path.append(home+"/.pipeables")
+for f in os.listdir(home+"/.pipeables"):
+  if f.endswith(".py") and f != '__init__.py':
+    m = f.replace(".py","")
+    a,b,c = imp.find_module(m)
+    t = imp.load_module(m,a,b,c)
+    for k,v in t.__dict__.iteritems():
+      if inspect.isclass(v):
+        keys = map(lambda x: x[0], inspect.getmembers(v))
+        if len(required_properties) == len(set(keys) & set(required_properties)):
+          if v.proj not in queries.keys():
+            queries[v.proj] = {}
+          queries[v.proj][v.name] = v
+
