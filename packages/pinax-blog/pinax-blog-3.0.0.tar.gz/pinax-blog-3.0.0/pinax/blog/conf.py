ee740b56e41cb4734959c104d087ae8ec4f82b71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:48 +0000

    Add package pinax-blog/3.0.0/pinax-blog-3.0.0.tar.gz

---
+++ packages/pinax-blog/pinax-blog-3.0.0.tar.gz/pinax-blog-3.0.0/pinax/blog/conf.py
@@ -0,0 +1,29 @@
+from __future__ import unicode_literals
+
+from django.conf import settings  # noqa
+
+from appconf import AppConf
+
+
+DEFAULT_MARKUP_CHOICE_MAP = {
+    "creole": {"label": "Creole", "parser": "pinax.blog.parsers.creole_parser.parse"},
+    "markdown": {"label": "Markdown", "parser": "pinax.blog.parsers.markdown_parser.parse"}
+}
+
+
+class PinaxBlogAppConf(AppConf):
+
+    ALL_SECTION_NAME = "all"
+    SECTIONS = []
+    MARKUP_CHOICE_MAP = DEFAULT_MARKUP_CHOICE_MAP
+    MARKUP_CHOICES = DEFAULT_MARKUP_CHOICE_MAP
+    SLUG_UNIQUE = False
+
+    def configure_markup_choices(self, value):
+        return [
+            (key, value[key]["label"])
+            for key in value.keys()
+        ]
+
+    class Meta:
+        prefix = "pinax_blog"
