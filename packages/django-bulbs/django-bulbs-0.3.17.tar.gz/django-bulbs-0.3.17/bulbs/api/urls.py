dadd39c4675891ffbba57afd879a0428ecb100fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:28 +0000

    Add package django-bulbs/0.3.17/django-bulbs-0.3.17.tar.gz

---
+++ packages/django-bulbs/django-bulbs-0.3.17.tar.gz/django-bulbs-0.3.17/bulbs/api/urls.py
@@ -0,0 +1,27 @@
+from django.conf import settings
+from django.conf.urls import url, include
+
+from bulbs.cms_notifications.api import notifications_view
+from .views import api_v1_router, MeViewSet
+
+
+urlpatterns = (
+    url(r"^me/logout/?$", "django.contrib.auth.views.logout", name="logout"),
+    url(r"^me/?$", MeViewSet.as_view({"get": "retrieve"}), name="me"),
+    url(r"^", include(api_v1_router.urls))  # noqa
+)
+
+if "bulbs.promotion" in settings.INSTALLED_APPS:
+    urlpatterns += (
+        url(r"^", include("bulbs.promotion.urls")),
+    )
+
+if "bulbs.cms_notifications" in settings.INSTALLED_APPS:
+    urlpatterns += (
+        url(r"^notifications/(?P<pk>\d+)?", notifications_view, name="notifications"),
+    )
+
+if "bulbs.contributions" in settings.INSTALLED_APPS:
+    urlpatterns += (
+        url(r"^contributions/", include("bulbs.contributions.urls")),
+    )
