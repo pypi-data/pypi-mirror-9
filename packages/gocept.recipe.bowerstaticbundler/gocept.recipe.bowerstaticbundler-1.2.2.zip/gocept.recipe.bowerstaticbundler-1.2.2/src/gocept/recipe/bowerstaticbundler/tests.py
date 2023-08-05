85ff4726441d10ab617374e384fd1d956b660275
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:44 +0000

    Add package gocept.recipe.bowerstaticbundler/1.2.2/gocept.recipe.bowerstaticbundler-1.2.2.zip

---
+++ packages/gocept.recipe.bowerstaticbundler/gocept.recipe.bowerstaticbundler-1.2.2.zip/gocept.recipe.bowerstaticbundler-1.2.2/src/gocept/recipe/bowerstaticbundler/tests.py
@@ -0,0 +1,9 @@
+import unittest
+
+
+class TestCase(unittest.TestCase):
+    """Basic test case."""
+
+    def testTest(self):
+        """Dummy to be replaced by some more meaningful tests."""
+        self.assertEqual(42, 42)
