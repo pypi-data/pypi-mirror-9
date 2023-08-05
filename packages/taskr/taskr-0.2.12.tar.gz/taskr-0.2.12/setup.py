e0030d172365129beff7d6ff8d7e6e7edf829a5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:41 +0000

    Add package taskr/0.2.12/taskr-0.2.12.tar.gz

---
+++ packages/taskr/taskr-0.2.12.tar.gz/taskr-0.2.12/setup.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+
+#
+# Copyright 2014 sodastsai
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+from setuptools import setup, find_packages
+import os
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md'), 'r') as f:
+    long_description = f.read()
+
+setup(name='taskr',
+      version='0.2.12',
+      description='Python Command Line Utility',
+      author='sodastsai',
+      author_email='sodas2002@gmail.com',
+      license='Apache License Version 2.0',
+      url='https://github.com/sodastsai/taskr',
+      package_data={'': ['license.txt', 'README.md']},
+      long_description=long_description,
+      packages=find_packages(),
+      install_requires=[
+          'six>=1.8.0',
+      ],
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Environment :: Console',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: Apache Software License',
+          'Operating System :: MacOS :: MacOS X',
+          'Operating System :: POSIX :: Linux',
+          'Operating System :: Unix',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.4',
+          'Topic :: Software Development',
+          'Topic :: Utilities',
+      ])
