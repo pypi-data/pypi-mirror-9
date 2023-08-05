810ff5837e1168b1c56c6d91952aadee6d9bcdca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:51 +0000

    Add package dedupe/0.7.7.0.3/dedupe-0.7.7.0.3-py2.7-linux-x86_64.egg

---
+++ packages/dedupe/dedupe-0.7.7.0.3-py2.7-linux-x86_64.egg/dedupe/metric_tree.py
@@ -0,0 +1,25 @@
+from finenight import iadfa
+from finenight import possibleStates
+from finenight import fsc 
+
+DISTANCE=3
+
+class LevenshteinIndex(object) :
+    def __init__(self, stop_words=[]) :
+        self._index = iadfa.IncrementalAdfa()
+
+    def initSearch(self) :
+        self._index.initSearch()
+
+        transition_states = possibleStates.genTransitions(DISTANCE)
+        self.etr = fsc.ErrorTolerantRecognizer(DISTANCE, 
+                                               self._index, 
+                                               transition_states)
+        
+
+    def index(self, record) :
+        print record
+        self._index.createFromSortedListOfWords(record)
+
+    def search(self, doc, threshold=0) :
+        return self.etr.recognize(doc)
