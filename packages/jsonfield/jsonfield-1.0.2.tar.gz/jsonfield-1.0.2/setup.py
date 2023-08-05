046843326a764f7b2cb7e1edc477f79d45458286
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:20 +0000

    Add package jsonfield/1.0.2/jsonfield-1.0.2.tar.gz

---
+++ packages/jsonfield/jsonfield-1.0.2.tar.gz/jsonfield-1.0.2/setup.py
@@ -0,0 +1,49 @@
+from distutils.core import setup
+from distutils.core import Command
+
+class TestCommand(Command):
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        from django.conf import settings
+        settings.configure(DATABASES={'default': {'NAME': ':memory:',
+            'ENGINE': 'django.db.backends.sqlite3'}},
+            INSTALLED_APPS=('jsonfield',))
+        from django.core.management import call_command
+        import django
+
+        if django.VERSION[:2] >= (1, 7):
+            django.setup()
+        call_command('test', 'jsonfield')
+
+setup(name='jsonfield',
+    version='1.0.2',
+    packages=['jsonfield'],
+    license='MIT',
+    author='Brad Jasper',
+    author_email='bjasper@gmail.com',
+    url='https://github.com/bradjasper/django-jsonfield/',
+    description='A reusable Django field that allows you to store validated JSON in your model.',
+    long_description=open("README.rst").read(),
+    install_requires=['Django >= 1.4.3'],
+    tests_require=['Django >= 1.4.3'],
+    cmdclass={'test': TestCommand},
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Framework :: Django',
+    ],
+)
