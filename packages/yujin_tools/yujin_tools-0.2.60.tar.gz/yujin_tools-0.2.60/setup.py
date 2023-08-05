25042e78e48a6def8b50345156acc5a47523e1d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:48 +0000

    Add package yujin_tools/0.2.60/yujin_tools-0.2.60.tar.gz

---
+++ packages/yujin_tools/yujin_tools-0.2.60.tar.gz/yujin_tools-0.2.60/setup.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+#from distutils.core import setup
+#import sys
+
+# Setuptools' sdist does not respect package_data
+#from setuptools import setup, find_packages
+from distutils.core import setup
+
+import sys
+sys.path.insert(0, 'src')
+
+from yujin_tools import __version__
+
+setup(name='yujin_tools',
+      version= __version__,
+      packages=['yujin_tools', 'catkin_make'],
+      package_dir = {'yujin_tools':'src/yujin_tools', 'catkin_make':'src/catkin_make'},
+      scripts = [
+           'scripts/cfind',
+           'scripts/avahi-browse-concerts',
+           'scripts/avahi-browse-multimaster',
+           'scripts/git-big-picture',
+           'scripts/groot_build_installspace',
+           'scripts/masteruri',
+           'scripts/yujin_init_workspace',
+           'scripts/yujin_init_build',
+           'scripts/yujin_make',
+           'scripts/yujin_make_isolated',
+           'scripts/yujin_rsync_installspace',
+           'scripts/yujin_tools_settings',
+           'scripts/yujin_list_git_branches',
+      ],
+      package_data = {'yujin_tools': [
+           'cmake/*',
+           'templates/init_build/.bashrc',
+           'templates/init_build/konsole',
+           'templates/init_build/gnome-terminal',
+           'templates/init_build/eclipse',
+           'templates/init_build/android-studio',
+           'toolchains/buildroot/*',
+           'toolchains/ubuntu/*',
+           'toolchains/code_sourcery/*',
+	   'toolchains/nexell/*',
+           'platforms/default.cmake',
+           'platforms/generic/*',
+           'platforms/arm/*',
+           'platforms/intel/*',
+           ]},
+      author = "Daniel Stonier",
+      author_email = "d.stonier@gmail.com",
+      maintainer='Daniel Stonier',
+      url = "http://pypi.python.org/pypi/yujin_tools",
+      download_url = "https://github.com/yujinrobot/yujin_tools.git",
+      keywords = ["Yujin Robot"],
+      classifiers = [
+        "Programming Language :: Python",
+        "License :: OSI Approved :: BSD License" ],
+      description = "Utilities for yujin's development environment",
+      long_description = "Refer to the documentation at https://github.com/yujinrobot/yujin_tools.",
+      license = "BSD"
+#      install_requires=[
+#        'argparse',
+        #'python-catkin-pkg',
+#    ],
+      )
+
+# This no longer works...it has to be a special parsable syntax of pypi's.
+#      long_description = open('README.md').read(),
+
---
+++ packages/yujin_tools/yujin_tools-0.2.60.tar.gz/yujin_tools-0.2.60/src/yujin_tools/__init__.py
@@ -0,0 +1,7 @@
+
+__version__ = '0.2.60'
+
+from .init_workspace import init_workspace
+from .init_build import init_build
+from .make import make_main
+from .make_isolated import make_isolated_main
