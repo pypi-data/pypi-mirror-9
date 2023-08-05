01793a23838e6e505e4c8a83db6c623784f4fa0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:50 +0000

    Add package rumrunner/0.1.0/rumrunner-0.1.0.tar.gz

---
+++ packages/rumrunner/rumrunner-0.1.0.tar.gz/rumrunner-0.1.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+import rumrunner
+
+setup(
+    name='rumrunner',
+    version=rumrunner.__version__,
+    description='Client to send metrics to Speakeasy server',
+    author='Eric Wong',
+    py_modules=['rumrunner'],
+    install_requires=[
+        'pyzmq',
+        'simplejson',
+    ],
+    url='https://github.com/etdub/rumrunner',
+    classifiers=(
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+    ),
+)
