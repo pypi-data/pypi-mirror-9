c00606e8e05b9e919c45c30f4f2902a3d9684543
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:37 +0000

    Add package collections-extended/0.2.0/collections_extended-0.2.0-py2.py3-none-any.whl

---
+++ packages/collections-extended/collections_extended-0.2.0-py2.py3-none-any.whl/collections_extended/__init__.py
@@ -0,0 +1,36 @@
+'''collections_extended contains a few extra basic data structures'''
+
+__version__ = '0.2.0'
+
+from .bags import bag, frozenbag
+from .setlists import setlist, frozensetlist
+
+__all__ = ['collection', 'setlist', 'frozensetlist', 'bag', 'frozenbag']
+
+
+def collection(it=(), mutable=True, ordered=False, unique=False):
+	""" Return a Collection with the specified properties.
+
+	"""
+	if unique:
+		if ordered:
+			if mutable:
+				return setlist(it)
+			else:
+				return frozensetlist(it)
+		else:
+			if mutable:
+				return set(it)
+			else:
+				return frozenset(it)
+	else:
+		if ordered:
+			if mutable:
+				return list(it)
+			else:
+				return tuple(it)
+		else:
+			if mutable:
+				return bag(it)
+			else:
+				return frozenbag(it)
