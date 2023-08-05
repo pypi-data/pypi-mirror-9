e5c6097035989ac20599bf2b93193c43dcf8afc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:57 +0000

    Add package GeobricksDeployment/0.0.4/GeobricksDeployment-0.0.4.tar.gz

---
+++ packages/GeobricksDeployment/GeobricksDeployment-0.0.4.tar.gz/GeobricksDeployment-0.0.4/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+from setuptools import find_packages
+
+setup(
+    name='GeobricksDeployment',
+    version='0.0.4',
+    author='Simone Murzilli; Guido Barbaglia',
+    author_email='geobrickspy@gmail.com',
+    packages=find_packages(),
+    license='LICENSE.txt',
+    long_description=open('README.md').read(),
+    description='Geobricks Deployment.',
+    entry_points={
+        'console_scripts': [
+            'geobricksdeploy = geobricks_deployment.cli:main',
+        ]
+    },
+    install_requires=[
+        'argh'
+    ],
+    url='http://pypi.python.org/pypi/GeobricksDeployment/'
+)
