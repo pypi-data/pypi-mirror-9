3be15990ee016d81de4acf812f9f2a366e870b3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:55 +0000

    Add package django-modelcluster/0.5/django-modelcluster-0.5.tar.gz

---
+++ packages/django-modelcluster/django-modelcluster-0.5.tar.gz/django-modelcluster-0.5/setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name='django-modelcluster',
+    version='0.5',
+    description="Django extension to allow working with 'clusters' of models as a single unit, independently of the database",
+    author='Matthew Westcott',
+    author_email='matthew.westcott@torchbox.com',
+    url='https://github.com/torchbox/django-modelcluster',
+    packages=['modelcluster', 'tests'],
+    license='BSD',
+    long_description=open('README.rst').read(),
+    install_requires=[
+        "six>=1.6.1",
+    ],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Framework :: Django',
+    ],
+)
