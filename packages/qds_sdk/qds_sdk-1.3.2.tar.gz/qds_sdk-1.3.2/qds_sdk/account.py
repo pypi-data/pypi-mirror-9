64e550d438805809f1491ca17e380589b845cfd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:19 +0000

    Add package qds_sdk/1.3.2/qds_sdk-1.3.2.tar.gz

---
+++ packages/qds_sdk/qds_sdk-1.3.2.tar.gz/qds_sdk-1.3.2/qds_sdk/account.py
@@ -0,0 +1,9 @@
+"""
+The Accounts module contains the base definition for a Qubole account object
+"""
+
+from qds_sdk.resource import SingletonResource
+
+
+class Account(SingletonResource):
+    pass
