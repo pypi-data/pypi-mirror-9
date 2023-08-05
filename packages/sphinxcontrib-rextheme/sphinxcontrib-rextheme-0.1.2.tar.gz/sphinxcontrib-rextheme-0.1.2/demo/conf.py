22e57fd9ee47b323496dd3ca18ee49d217f03de6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:01 +0000

    Add package sphinxcontrib-rextheme/0.1.2/sphinxcontrib-rextheme-0.1.2.tar.gz

---
+++ packages/sphinxcontrib-rextheme/sphinxcontrib-rextheme-0.1.2.tar.gz/sphinxcontrib-rextheme-0.1.2/demo/conf.py
@@ -0,0 +1,9 @@
+
+# Enable `sphinxcontrib-rextheme` theme.
+html_theme = 'rex'
+
+# Standard configuration.
+project = u'A RexDB Project'
+master_doc = 'index'
+exclude_patterns = ['_build']
+
---
+++ packages/sphinxcontrib-rextheme/sphinxcontrib-rextheme-0.1.2.tar.gz/sphinxcontrib-rextheme-0.1.2/sphinxcontrib/rextheme.py
@@ -0,0 +1,11 @@
+#
+# Copyright (c) 2015, Prometheus Research, LLC
+#
+
+
+import os
+
+
+path = os.path.dirname(__file__)
+
+
