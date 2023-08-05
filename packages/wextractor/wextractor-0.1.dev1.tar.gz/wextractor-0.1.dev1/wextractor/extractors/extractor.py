d84ba6a9891822156114637ee172d5f7debaf8a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:21 +0000

    Add package wextractor/0.1.dev1/wextractor-0.1.dev1.tar.gz

---
+++ packages/wextractor/wextractor-0.1.dev1.tar.gz/wextractor-0.1.dev1/wextractor/extractors/extractor.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+class Extractor(object):
+    def __init__(self, target, header=None, dtypes=None):
+        '''
+        Initializes a new Extractor. Extractors pull data
+        out of different targets. The target (file, url, etc)
+        is passed in as an argument, along with whether or
+        not the first row of the target contains headers
+        '''
+        self.target = target
+        self.header = header
+        self.dtypes = dtypes
+
+        # if we have a header and dtypes, make sure they are the
+        # same length
+        if self.header and self.dtypes:
+            if len(self.header) != len(self.dtypes):
+                raise Exception('Number of headers must match number of dtypes')
+
+    def simple_cleanup(self, field):
+        '''
+        Method to replace spaces with underscores, pound signs
+        coerce to lowercase
+        '''
+        if type(field) in [str, unicode]:
+            clean = field.lower().replace(' ', '_').replace('#', 'number')
+        else:
+            clean = field
+
+        return clean
+
+    def extract(self):
+        '''
+        Each Extractor implementation must implement an
+        extract method. The method should return a
+        list of dictionaries formatted as follows:
+        [
+            {field: value, ...},
+            ...
+        ]
+        '''
+        raise NotImplementedError
