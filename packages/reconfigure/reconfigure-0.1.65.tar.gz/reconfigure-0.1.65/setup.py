8c9f5c89655d0d403187f24d85b79b1c89e5249e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:56 +0000

    Add package reconfigure/0.1.65/reconfigure-0.1.65.tar.gz

---
+++ packages/reconfigure/reconfigure-0.1.65.tar.gz/reconfigure-0.1.65/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+from setuptools import find_packages
+
+from reconfigure import __version__
+
+setup(
+    name='reconfigure',
+    version=__version__,
+    install_requires=[
+        'chardet',
+    ],
+    description='An ORM for config files',
+    license='LGPLv3',
+    author='Eugeny Pankov',
+    author_email='e@ajenti.org',
+    url='http://ajenti.org/',
+    classifiers=[
+        "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
+    ],
+    packages=find_packages(exclude=['*test*']),
+)
