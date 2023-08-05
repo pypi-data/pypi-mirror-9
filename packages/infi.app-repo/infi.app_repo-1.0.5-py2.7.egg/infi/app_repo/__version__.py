e3f63be24991fc98eae61a0fc68fd7b8428832aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:20 +0000

    Add package infi.app-repo/1.0.5/infi.app_repo-1.0.5-py2.7.egg

---
+++ packages/infi.app-repo/infi.app_repo-1.0.5-py2.7.egg/infi/app_repo/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "1.0.5"
+__git_commiter_name__ = "Guy Rozendorn"
+__git_commiter_email__ = "guy@rzn.co.il"
+__git_branch__ = '(detached from v1.0.5)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '078ada27f64aa06754847d6c0cdad6b632e02051'
+__git_head_subject__ = 'Finished release v1.0.5.'
+__git_head_message__ = '* release/v1.0.5:\n  APR-10 Installation and upgrade instructions are wrong for vmware\n  APR-10 Installation and upgrade instructions are wrong for vmware'
+__git_dirty_diff__ = ''
