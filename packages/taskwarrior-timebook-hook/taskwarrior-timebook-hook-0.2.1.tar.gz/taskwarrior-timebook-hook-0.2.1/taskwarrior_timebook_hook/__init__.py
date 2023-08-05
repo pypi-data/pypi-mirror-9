81cf409483b98737b91a16e5c36e9477d53c64be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:16 +0000

    Add package taskwarrior-timebook-hook/0.2.1/taskwarrior-timebook-hook-0.2.1.tar.gz

---
+++ packages/taskwarrior-timebook-hook/taskwarrior-timebook-hook-0.2.1.tar.gz/taskwarrior-timebook-hook-0.2.1/taskwarrior_timebook_hook/__init__.py
@@ -0,0 +1,36 @@
+import json
+import sys
+import subprocess
+
+
+def set_current_task(description=''):
+    returncode = subprocess.call([
+        't',
+        'change',
+        description
+    ])
+    if returncode != 0:
+        print "You are not currently clocked-in"
+
+
+def main(stdin):
+    lines = stdin.split('\n')
+    original = json.loads(lines[0])
+    modified = json.loads(lines[1])
+
+    if 'start' in original and 'start' not in modified:
+        # Stop tracking time to this task.
+        set_current_task()
+    elif 'start' in modified and 'start' not in original:
+        # Start tracking time to this task.
+        set_current_task(modified['description'])
+
+    return json.dumps(modified, separators=(',', ':'))
+
+
+def cmdline():
+    sys.stdout.write(main(sys.stdin.read()))
+
+
+if __name__ == '__main__':
+    cmdline()
