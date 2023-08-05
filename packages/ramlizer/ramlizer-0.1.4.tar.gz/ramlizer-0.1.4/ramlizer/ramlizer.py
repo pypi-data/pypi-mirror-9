0cd548b38a7257ebda14b1caeb10c5e25b9d8800
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:02 +0000

    Add package ramlizer/0.1.4/ramlizer-0.1.4.tar.gz

---
+++ packages/ramlizer/ramlizer-0.1.4.tar.gz/ramlizer-0.1.4/ramlizer/ramlizer.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+import yaml
+from RamlRoot import RamlRoot
+import os.path
+
+class RamlFile():
+    def __init__(self, read_file):
+        class Loader(yaml.Loader):
+            def __init__(self, stream):
+                self._root = os.path.split(stream.name)[0]
+                super(Loader, self).__init__(stream)
+
+            def include(self, node):
+                filename = os.path.join(self._root, self.construct_scalar(node))
+                with open(filename, 'r') as f:
+                    return yaml.load(f, Loader)
+        yaml.add_constructor('!include', Loader.include)
+        
+        self.yaml = yaml.load(read_file, Loader)
+        self.parse()
+        
+    def parse(self):
+        self.root = RamlRoot(self.yaml)
+        
+    def __str__(self):
+        return \
+'''[RamlFile:
+    {0}
+]'''.format(self.root)
---
+++ packages/ramlizer/ramlizer-0.1.4.tar.gz/ramlizer-0.1.4/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+from setuptools import setup
+
+setup(name='ramlizer',
+      version='0.1.4',
+      description='A parser for the RAML (RESTful API Modeling Language) file format.',
+      url='https://github.com/SandyChapman/ramlizer',
+      author='Sandy Chapman',
+      author_email='sandychapman@gmail.com',
+      license='MIT',
+      packages=['ramlizer'],
+      install_requires=['pyaml', 'jsonschema'],
+      zip_safe=False)
