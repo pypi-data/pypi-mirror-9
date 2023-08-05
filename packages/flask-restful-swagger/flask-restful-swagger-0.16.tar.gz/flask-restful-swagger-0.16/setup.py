bbc65ca22ee6fbe575e2885ef653f832705cd49a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:38 +0000

    Add package flask-restful-swagger/0.16/flask-restful-swagger-0.16.tar.gz

---
+++ packages/flask-restful-swagger/flask-restful-swagger-0.16.tar.gz/flask-restful-swagger-0.16/setup.py
@@ -0,0 +1,28 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+with open('README') as file:
+    long_description = file.read()
+
+setup(name='flask-restful-swagger',
+      version='0.16',
+      url='https://github.com/rantav/flask-restful-swagger',
+      zip_safe=False,
+      packages=['flask_restful_swagger'],
+      package_data={
+        'flask_restful_swagger': [
+          'static/*.*',
+          'static/css/*.*',
+          'static/images/*.*',
+          'static/lib/*.*',
+          'static/lib/shred/*.*',
+        ]
+      },
+      description='Extrarct swagger specs from your flast-restful project',
+      author='Ran Tavory',
+      license='MIT',
+      long_description=long_description,
+      install_requires=['Flask-RESTful>=0.2.12']
+      )
