6023001b0dcf302ea28ee177eebe95774a5ac7cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:46 +0000

    Add package papylon/0.5/papylon-0.5-py3-none-any.whl

---
+++ packages/papylon/papylon-0.5-py3-none-any.whl/papylon/arbitrary.py
@@ -0,0 +1,182 @@
+# -*- coding: utf-8 -*-
+import sys
+import random
+import struct
+import datetime
+
+from papylon.gen import Gen, choose, frequency, map
+from papylon.shrinker import (
+    IntShrinker, FloatShrinker, CharShrinker,
+    DateShrinker, ListShrinker, StrShrinker)
+
+
+class AbstractArbitrary:
+    def arbitrary(self):
+        raise NotImplementedError("AbstractArbitrary#arbitrary")
+
+    def shrink(self, value):
+        raise NotImplementedError("AbstractArbitrary#shrink")
+
+
+class ArbInteger(AbstractArbitrary):
+    def __init__(self):
+        self.gen = choose(-1-sys.maxsize, sys.maxsize)
+        self.shrinker = IntShrinker()
+
+    def arbitrary(self):
+        return self.gen.generate()
+
+    def shrink(self, value):
+        return self.shrinker.shrink(value)
+
+
+class ArbFloat(AbstractArbitrary):
+    def __init__(self):
+        def gen():
+            while True:
+                s = random.randint(0, 1)                # sign
+                e = random.randint(0, 0x7ff)            # exponent
+                f = random.randint(0, 0xfffffffffffff)  # fraction
+                yield struct.unpack('d', struct.pack('Q', (s << 63) | (e << 52) | f))[0]
+        self.gen = Gen(gen)
+        self.shrinker = FloatShrinker()
+
+    def arbitrary(self):
+        return self.gen.generate()
+
+    def shrink(self, value):
+        return self.shrinker.shrink(value)
+
+
+class ArbChar(AbstractArbitrary):
+    def __init__(self):
+        self.gen = frequency([(0xD800, map(chr, choose(0, 0xD800-1))),
+                              (0xFFFF-0xDFFF, map(chr, choose(0xdFFF+1, 0xFFFF)))])
+        self.shrinker = CharShrinker()
+
+    def arbitrary(self):
+        return self.gen.generate()
+
+    def shrink(self, value):
+        return self.shrinker.shrink(value)
+
+
+class ArbDate(AbstractArbitrary):
+    def __init__(self):
+        days_to_month_366 = [0, 31, 60, 91, 121, 152, 182, 213, 244, 274, 305, 335, 366]
+        days_to_month_365 = [0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334, 365]
+
+        def is_leap_year(year):
+            assert 1 <= year <= 9999    # contract
+            return year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)
+
+        def days_in_month(year, month):
+            assert 1 <= month <= 12     # contract
+            days = days_to_month_366 if is_leap_year(year) else days_to_month_365
+            return days[month] - days[month - 1]
+
+        def gen():
+            while True:
+                year = random.randint(1, 9999)
+                month = random.randint(1, 12)
+                day = random.randint(1, days_in_month(year, month))
+                hour = random.randint(0, 23)
+                minute = random.randint(0, 59)
+                second = random.randint(0, 59)
+                yield datetime.datetime(year, month, day, hour, minute, second)
+        self.gen = Gen(gen)
+        self.shrinker = DateShrinker()
+
+    def arbitrary(self):
+        return self.gen.generate()
+
+    def shrink(self, value):
+        return self.shrinker.shrink(value)
+
+
+class ArbList(AbstractArbitrary):
+    def __init__(self, arb_type, max_length):
+        def gen():
+            min_length = 0
+            while True:
+                length = random.randint(min_length, max_length)
+                yield [arb_type.arbitrary() for i in range(length)]
+        self.gen = Gen(gen)
+        self.shrinker = ListShrinker()
+
+    def arbitrary(self):
+        return self.gen.generate()
+
+    def shrink(self, value):
+        return self.shrinker.shrink(value)
+
+
+class ArbStr(AbstractArbitrary):
+    def __init__(self, max_length):
+        def gen():
+            min_length = 0
+            while True:
+                length = random.randint(min_length, max_length)
+                yield "".join(arb_char().arbitrary() for i in range(length))
+        self.gen = Gen(gen)
+        self.shrinker = StrShrinker()
+
+    def arbitrary(self):
+        return self.gen.generate()
+
+    def shrink(self, value):
+        return self.shrinker.shrink(value)
+
+
+def arb_int():
+    return ArbInteger()
+
+
+def arb_float():
+    return ArbFloat()
+
+
+def arb_char():
+    return ArbChar()
+
+
+def arb_date():
+    return ArbDate()
+
+
+def arb_list(arb_type, max_length=100):
+    return ArbList(arb_type, max_length=max_length)
+
+
+def arb_str(max_length=20):
+    return ArbStr(max_length=max_length)
+
+
+def from_gen(gen):
+    arb = AbstractArbitrary()
+    arb.gen = gen
+
+    def new_arbitrary(this):
+        return this.gen.generate()
+    arb.arbitrary.__func__.__code__ = new_arbitrary.__code__
+
+    def new_shrink(this, value):
+        return iter([])
+    arb.shrink.__func__.__code__ = new_shrink.__code__
+    return arb
+
+
+def from_gen_shrink(gen, shrinker):
+    arb = AbstractArbitrary()
+    arb.gen = gen
+    arb.shrinker = shrinker
+
+    def new_arbitrary(this):
+        return this.gen.generate()
+    arb.arbitrary.__func__.__code__ = new_arbitrary.__code__
+
+    def new_shrink(this, value):
+        return this.shrinker.shrink(value)
+    arb.shrink.__func__.__code__ = new_shrink.__code__
+
+    return arb
---
+++ packages/papylon/papylon-0.5-py3-none-any.whl/papylon/checker.py
@@ -0,0 +1,101 @@
+# -*- coding: utf-8 -*-
+import sys
+from papylon.gen import StopGeneration
+from papylon.printer import SimplePrinter
+
+
+class CheckResult:
+    PASSED = 0
+    FALSIFIED = 1
+    ERROR = 2
+    FAIL_TO_GENERATE = 3
+    TROUBLED = 4
+
+    def __init__(self, status):
+        self.status = status
+        self.result = None
+
+    @staticmethod
+    def pass_all(count):
+        result = CheckResult(CheckResult.PASSED)
+        result.result = (count,)
+        return result
+
+    @staticmethod
+    def falsify(run_count, inputs, shrunk_number):
+        result = CheckResult(CheckResult.FALSIFIED)
+        result.result = (run_count, inputs, shrunk_number)
+        return result
+
+    @staticmethod
+    def error(run_count, inputs, error):
+        result = CheckResult(CheckResult.ERROR)
+        result.result = (run_count, inputs, error)
+        return result
+
+    @staticmethod
+    def fail_to_generate(run_count, trial_to_generate):
+        result = CheckResult(CheckResult.FAIL_TO_GENERATE)
+        result.result = (run_count, trial_to_generate)
+        return result
+
+    @staticmethod
+    def trouble(error, ex_traceback):
+        result = CheckResult(CheckResult.TROUBLED)
+        result.result = (error, ex_traceback)
+        return result
+
+    def has_passed(self):
+        return self.status == CheckResult.PASSED
+
+    def has_falsified(self):
+        return self.status == CheckResult.FALSIFIED
+
+    def has_error_occurred(self):
+        return self.status == CheckResult.ERROR
+
+    def has_failed_to_generate(self):
+        return self.status == CheckResult.FAIL_TO_GENERATE
+
+    def has_troubled(self):
+        return self.status == CheckResult.TROUBLED
+
+    def get(self):
+        return self.result
+
+
+class PropChecker:
+    def __init__(self, count):
+        if count < 1:
+            raise ValueError("Argument `count` should be a integer greater than or equal to 1.")
+        self.count = count
+
+    def check(self, prop):
+        try:
+            for i in range(self.count):
+                prop_result = prop.execute()
+
+                if prop_result.has_finished():
+                    _, inputs, is_valid, shrunk_number = prop_result.get()
+                    if not is_valid:
+                        return CheckResult.falsify(i+1, inputs, shrunk_number)
+                else:
+                    _, inputs, error = prop_result.get()
+
+                    # failed to generate?
+                    if type(error) == StopGeneration:
+                        return CheckResult.fail_to_generate(i+1, error.trial_to_generate)
+                    else:
+                        return CheckResult.error(i+1, inputs, error)
+
+            return CheckResult.pass_all(self.count)
+        except Exception as error:
+            _, _, ex_traceback = sys.exc_info()
+            return CheckResult.trouble(error, ex_traceback)
+
+
+def check(prop, count=100, printer_class=SimplePrinter):
+    checker = PropChecker(count=count)
+    result = checker.check(prop)
+    printer = printer_class(result)
+    printer.print_result()
