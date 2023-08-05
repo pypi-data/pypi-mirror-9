141579f0fe755fb04d67fde7fa00fce7b0b76361
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:24 +0000

    Add package analyse/1.0.0/analyse-1.0.0.tar.gz

---
+++ packages/analyse/analyse-1.0.0.tar.gz/analyse-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+       name        = 'analyse',
+       version     = '1.0.0',
+       py_modules  = ['analyse'],
+       author      = 'sdddz',
+       author_email= '2462525363@qq.com',
+       url         = 'none',
+       description = 'It can tell average about lists and nested lists.',
+
+    )
