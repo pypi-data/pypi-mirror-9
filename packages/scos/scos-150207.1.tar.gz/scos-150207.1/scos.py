268525ee8f3e32f027e97faa3e18891d2be9000c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:25 +0000

    Add package scos/150207.1/scos-150207.1.tar.gz

---
+++ packages/scos/scos-150207.1.tar.gz/scos-150207.1/scos.py
@@ -0,0 +1,13 @@
+def testlist():
+	print('Here is a list you can copy and paste')
+	x = ['gta', ['locations', ['gi', ['subnets', ['10.10.1.0/24', ['.1 router', '.222 avaya']]], 'rc', 'bh']], 'ppl', ['locations', ['gi', 'sterling', 'jerome']]]
+	print(x)
+
+def list2outline(the_list, root_level=1):
+	for each_item in the_list:
+		if isinstance(each_item, list):
+			list2outline(each_item, root_level+1)
+		else:
+			for indent_level in range(root_level):
+				print("  ", end='')
+			print('*', each_item)
