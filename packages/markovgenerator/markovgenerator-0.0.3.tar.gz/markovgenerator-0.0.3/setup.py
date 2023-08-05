c67cafbc3ee963a846f3d0df7ec1134c86b22431
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:27 +0000

    Add package markovgenerator/0.0.3/markovgenerator-0.0.3.tar.gz

---
+++ packages/markovgenerator/markovgenerator-0.0.3.tar.gz/markovgenerator-0.0.3/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+from codecs import open 
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+setup(
+  name = 'markovgenerator',
+  version = '0.0.3',
+  description = 'Markov text generator',
+  license = 'MIT',
+  author = 'Amanda Pickering',
+  author_email = 'pickering.amanda@gmail.com',
+  install_requires = ['nltk', 'wsgiref'],
+  url = 'https://github.com/amandapickering/markovgenerator',
+  keywords = 'markov text generator natural language processing generative',
+  packages = find_packages(),
+)
