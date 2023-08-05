43de3d462a76f0a9b6d1299f23a4766c84c7a0cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:26 +0000

    Add package FullContact.py/0.0.2/FullContact.py-0.0.2.tar.gz

---
+++ packages/FullContact.py/FullContact.py-0.0.2.tar.gz/FullContact.py-0.0.2/setup.py
@@ -0,0 +1,40 @@
+"""
+FullContact.py
+--------------
+
+Simple Python interface for FullContact, using Requests.
+
+"""
+from setuptools import setup
+
+
+setup(
+    name='FullContact.py',
+    version='0.0.2',
+    url='https://github.com/garbados/fullcontact.py',
+    license='MIT',
+    author=['Max Thayer','Amer Almadani'],
+    author_email=['garbados@gmail.com', 'mad_dev@linuxmail.org'],
+    description='Simple Python interface for FullContact, using Requests',
+    long_description=file.read(open('README.md', 'r')),
+    packages=['fullcontact'],
+    zip_safe=False,
+    platforms='any',
+    install_requires=[
+        'requests',
+    ],
+    include_package_data=True,
+    tests_require=[
+        'Attest',
+    ],
+    test_loader='attest:auto_reporter.test_loader',
+    test_suite='tests.fc_tests',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
