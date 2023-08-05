c7eda17d2269abdb09a8f610b216899c0c4947cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:09 +0000

    Add package xaml/0.1.04/xaml-0.1.04.zip

---
+++ packages/xaml/xaml-0.1.04.zip/xaml-0.1.04/setup.py
@@ -0,0 +1,34 @@
+from distutils.core import setup
+import sys
+
+with open('README') as fh:
+    long_desc = fh.read()
+
+if sys.version_info < (3, 4):
+    requirements = ['scription', 'enum34']
+else:
+    requirements = ['scription']
+
+setup( name='xaml',
+       version= '0.1.04',
+       license='BSD License',
+       description='XML Abstract Markup Language',
+       long_description=long_desc,
+       py_modules=['xaml', 'xaml_test'],
+       provides=['xaml'],
+       install_requires=requirements,
+       author='Ethan Furman',
+       author_email='ethan@stoneleaf.us',
+       url='https://bitbucket.org/stoneleaf.xaml',
+       classifiers=[
+            'Development Status :: 3 - Alpha',
+            'Intended Audience :: Developers',
+            'License :: OSI Approved :: BSD License',
+            'Programming Language :: Python :: 2.7',
+            'Programming Language :: Python :: 3.3',
+            'Topic :: Software Development',
+            'Topic :: Text Processing :: Markup :: HTML',
+            'Topic :: Text Processing :: Markup :: XML',
+            ],
+    )
+
