be5d771d296764c4558d8303a5c3376f14b30c51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:08 +0000

    Add package django-environments/1.0a2/django-environments-1.0a2.tar.gz

---
+++ packages/django-environments/django-environments-1.0a2.tar.gz/django-environments-1.0a2/bin/deinitenv_generic.sh
@@ -0,0 +1,11 @@
+#
+# Deinitialize the settings environment
+#
+# Should be sourced with the 'source' command, e.g. 'source deinitenv'.
+#
+# The source command can be performed within virtualenvwrapper's
+# bin/predeactivate, or you could just copy the following lines
+# to bin/predeactivate. Alternatively, you may also symlink
+# bin/predeactivate to (a custom version of) this file.
+
+djexit
