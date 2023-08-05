5014d7f956a28a416c9d7713d058f98d8501b50b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:11 +0000

    Add package ppic/0.1/ppic-0.1.tar.gz

---
+++ packages/ppic/ppic-0.1.tar.gz/ppic-0.1/setup.py
@@ -0,0 +1,58 @@
+# -*- coding:utf-8 -*-
+
+import os
+import sys
+
+
+from setuptools import setup, find_packages
+here = os.path.abspath(os.path.dirname(__file__))
+try:
+    with open(os.path.join(here, 'README.rst')) as f:
+        README = f.read()
+    with open(os.path.join(here, 'CHANGES.txt')) as f:
+        CHANGES = f.read()
+except IOError:
+    README = CHANGES = ''
+
+
+install_requires = [
+    'pip',
+]
+
+
+docs_extras = [
+]
+
+tests_require = [
+]
+
+testing_extras = tests_require + [
+]
+
+setup(name='ppic',
+      version='0.1',
+      description='ppic is python package information collector.',
+      long_description=README + '\n\n' + CHANGES,
+      classifiers=[
+          "Programming Language :: Python",
+          "Programming Language :: Python :: Implementation :: CPython",
+      ],
+      keywords='',
+      author="podhmo",
+      author_email="",
+      url="https://github.com/podhmo/ppic",
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=install_requires,
+      extras_require={
+          'testing': testing_extras,
+          'docs': docs_extras,
+      },
+      tests_require=tests_require,
+      test_suite="ppic.tests",
+      entry_points="""
+      [console_scripts]
+      ppic = ppic:main
+""")
+
