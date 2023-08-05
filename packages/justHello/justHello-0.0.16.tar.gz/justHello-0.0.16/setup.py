8a50d548a86f3bbbeb185f7fadc9932be556b7b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:13 +0000

    Add package justHello/0.0.16/justHello-0.0.16.tar.gz

---
+++ packages/justHello/justHello-0.0.16.tar.gz/justHello-0.0.16/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+import sys
+
+setup(name='justHello',
+      version= "0.0.16",
+      description='only print Hello World',
+      long_description="""this tool is just sample of python packaging.""",
+      author='Kenichi Masuda',
+      author_email='masuken@gmail.com',
+      url='https://github.com/masudaK/printHello',
+      packages=find_packages(),
+      py_modules=[
+          ],
+      data_files=['requirements.txt'],
+      zip_safe = (sys.version>="2.5"), #2.5の頃はディレクトリとしてegg作らないとまずかった？
+      entry_points={
+          'console_scripts': ['justHello = bin.justHello:main']
+      },
+      install_requires=open('requirements.txt').read().splitlines(),
+      license='GNU Lesser General Public License v3 or later (LGPLv3+)',
+      keywords='',
+      platforms='Linux',
+      classifiers=['Intended Audience :: System Administrators',
+                   'License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)',
+                   'Natural Language :: Japanese',
+                   'Programming Language :: Python :: 3.4'
+                   ],
+      )
