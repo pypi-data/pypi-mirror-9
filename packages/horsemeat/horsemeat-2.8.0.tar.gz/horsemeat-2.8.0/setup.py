ac37ca936af069f1b0d371020d126daa4e859d32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:35 +0000

    Add package horsemeat/2.8.0/horsemeat-2.8.0.tar.gz

---
+++ packages/horsemeat/horsemeat-2.8.0.tar.gz/horsemeat-2.8.0/setup.py
@@ -0,0 +1,39 @@
+# vim: set expandtab ts=4 sw=4 filetype=python fileencoding=utf8:
+
+import os
+import sys
+
+from setuptools import find_packages, setup
+
+from horsemeat import __version__
+
+# Read __version__ from version.py
+with open(os.path.join(os.getcwd(), "horsemeat", "version.py")) as f:
+    exec(f.read())
+
+setup(
+
+    name='horsemeat',
+
+    author="216 Software, LLC",
+    author_email="info@216software.com",
+    url="https://GitHub.com/216software/horsemeat",
+    description='Web framework for the damned.  The mad.',
+
+    version=__version__,
+
+    packages=find_packages(),
+
+    include_package_data=True,
+
+    package_dir={'horsemeat': 'horsemeat'},
+
+    install_requires=[
+        'decorator',
+        'PyYAML',
+        'jinja2==2.6',
+        'psycopg2',
+        'Werkzeug',
+        'clepy',
+    ],
+)
