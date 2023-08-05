c09144cff29a828c06aa5100d36f93dd76f9e036
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:03 +0000

    Add package infi.reboot/0.2.9/infi.reboot-0.2.9.tar.gz

---
+++ packages/infi.reboot/infi.reboot-0.2.9.tar.gz/infi.reboot-0.2.9/setup.py
@@ -0,0 +1,50 @@
+
+SETUP_INFO = dict(
+    name = 'infi.reboot',
+    version = '0.2.9',
+    author = 'Guy Rozendorn',
+    author_email = 'guy@rzn.co.il',
+
+    url = 'https://github.com/Infinidat/infi.reboot',
+    license = 'PSF',
+    description = """A cross-platform module for handling reboot-pending operations.""",
+    long_description = """A cross-platform module for handling reboot-pending operations.""",
+
+    # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers = [
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: Python Software Foundation License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+
+    install_requires = ['infi.execute',
+'infi.pyutils',
+'setuptools'],
+    namespace_packages = ['infi'],
+
+    package_dir = {'': 'src'},
+    package_data = {'': []},
+    include_package_data = True,
+    zip_safe = False,
+
+    entry_points = dict(
+        console_scripts = [],
+        gui_scripts = [],
+        ),
+)
+
+if SETUP_INFO['url'] is None:
+    _ = SETUP_INFO.pop('url')
+
+def setup():
+    from setuptools import setup as _setup
+    from setuptools import find_packages
+    SETUP_INFO['packages'] = find_packages('src')
+    _setup(**SETUP_INFO)
+
+if __name__ == '__main__':
+    setup()
+
