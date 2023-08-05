adffef5531400991972758c7fcb426b6280a3d1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:18 +0000

    Add package arm-pyart/1.2.0/arm_pyart-1.2.0.zip

---
+++ packages/arm-pyart/arm_pyart-1.2.0.zip/arm_pyart-1.2.0/pyart/version.py
@@ -0,0 +1,10 @@
+
+# THIS FILE IS GENERATED FROM PYART SETUP.PY
+short_version = '1.2.0'
+version = '1.2.0'
+full_version = '1.2.0'
+git_revision = '24558341f7a84cf9b5c77e26665a015549da1d6e'
+release = True
+
+if not release:
+    version = full_version
