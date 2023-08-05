15f04cb911efc88b2e6131f0fe83866525975b40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:37 +0000

    Add package vsat/0.0.5/vsat-0.0.5.tar.gz

---
+++ packages/vsat/vsat-0.0.5.tar.gz/vsat-0.0.5/setup.py
@@ -0,0 +1,42 @@
+"""
+Setup file for VSAT
+"""
+
+import os
+from setuptools import setup
+
+def read(fname):
+    """
+    Utility that reads a file and returns it.
+    """
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name="vsat",
+
+    version="0.0.5",
+
+    author="Robin Ridler",
+
+    author_email="robin.ridler@gmail.com",
+
+    description=("Very simple asynchronous tasks for Python."),
+
+    license="BSD",
+
+    keywords="Simple Asynchronous Tasks",
+
+    url="https://github.com/rsr5/vsat",
+
+    packages=['vsat'],
+    long_description=read('README.rst'),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: BSD License",
+    ],
+
+    install_requires=[
+
+    ]
+)
