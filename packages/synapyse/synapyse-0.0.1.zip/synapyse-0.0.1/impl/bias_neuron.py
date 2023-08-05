d28287393893fab8ca30c067474188444bc0c65f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:35 +0000

    Add package synapyse/0.0.1/synapyse-0.0.1.zip

---
+++ packages/synapyse/synapyse-0.0.1.zip/synapyse-0.0.1/impl/bias_neuron.py
@@ -0,0 +1,16 @@
+from core.input_functions.input_function import InputFunction
+from core.neuron import Neuron
+
+__author__ = 'Douglas Eric Fonseca Rodrigues'
+
+
+class BiasNeuron(Neuron):
+    def __init__(self, activation_function):
+        """
+        :type activation_function: core.activation_functions.activation_function.ActivationFunction
+        """
+        Neuron.__init__(self, InputFunction, activation_function)
+
+    def compute_output(self):
+        self.output = 1.0
+        return self.output
