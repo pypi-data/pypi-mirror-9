c2cb3182c0d07c9a406431932b01fb4241ef1ef1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:49 +0000

    Add package Flask-RESTful-extend/0.3.3/Flask-RESTful-extend-0.3.3.tar.gz

---
+++ packages/Flask-RESTful-extend/Flask-RESTful-extend-0.3.3.tar.gz/Flask-RESTful-extend-0.3.3/flask_restful_extend/model_converter.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+from werkzeug.routing import BaseConverter
+from werkzeug.exceptions import NotFound
+
+
+def register_model_converter(model, app):
+    """Add url converter for model
+
+    Example:
+        class Student(db.model):
+            id = Column(Integer, primary_key=True)
+            name =  Column(String(50))
+
+        register_model_converter(Student)
+
+        @route('/classmates/<Student:classmate>')
+        def get_classmate_info(classmate):
+            pass
+
+    This only support model's have single primary key.
+    You need call this function before create view function.
+    """
+    if hasattr(model, 'id'):
+        class Converter(_ModelConverter):
+            _model = model
+        app.url_map.converters[model.__name__] = Converter
+
+
+class _ModelConverter(BaseConverter):
+    _model = None
+
+    def to_python(self, inst_id):
+        instance = self._model.query.get(inst_id)
+        if instance is None:
+            raise NotFound(u'{}(id={}) not exists，request invalid'.format(self._model.__name__, inst_id))
+        return instance
+
+    def to_url(self, inst):
+        return str(inst.id)
---
+++ packages/Flask-RESTful-extend/Flask-RESTful-extend-0.3.3.tar.gz/Flask-RESTful-extend-0.3.3/setup.py
@@ -0,0 +1,29 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name='Flask-RESTful-extend',
+    version='0.3.3',
+    url='https://github.com/anjianshi/flask-restful-extend',
+    license='MIT',
+    author='anjianshi',
+    author_email='anjianshi@gmail.com',
+    description="Improve Flask-RESTFul's behavior. Add some new features.",
+    packages=['flask_restful_extend'],
+    zip_safe=False,
+    platforms='any',
+    install_requires=['Flask>=0.10', 'Flask-RESTful>=0.3', 'Flask-SQLAlchemy', "six"],
+    keywords=['flask', 'python', 'rest', 'api'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+    ],
+)
