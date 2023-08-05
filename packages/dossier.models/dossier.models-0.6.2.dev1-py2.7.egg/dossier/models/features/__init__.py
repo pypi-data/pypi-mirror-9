e761bdc71124b4a43ce44086b7b21c470d042e47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:01 +0000

    Add package dossier.models/0.6.2.dev1/dossier.models-0.6.2.dev1-py2.7.egg

---
+++ packages/dossier.models/dossier.models-0.6.2.dev1-py2.7.egg/dossier/models/features/__init__.py
@@ -0,0 +1,12 @@
+'''
+Feature extraction
+==================
+``dossier.models.features`` provides some convenience functions for
+feature extraction.
+
+.. autofunction:: noun_phrases
+'''
+from dossier.models.features.sip import noun_phrases
+
+
+__all__ = ['noun_phrases']
