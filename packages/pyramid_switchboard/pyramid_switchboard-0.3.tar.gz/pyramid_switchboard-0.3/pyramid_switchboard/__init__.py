db07ea546cb6fa43bc435988eccb5fce4fe56b59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:18 +0000

    Add package pyramid_switchboard/0.3/pyramid_switchboard-0.3.tar.gz

---
+++ packages/pyramid_switchboard/pyramid_switchboard-0.3.tar.gz/pyramid_switchboard-0.3/pyramid_switchboard/__init__.py
@@ -0,0 +1,56 @@
+from pyramid.config import Configurator
+from pyramid.wsgi import wsgiapp2
+
+from switchboard import configure
+from switchboard.signals import request_finished
+
+
+def request_tween_factory(handler, registry):
+
+    def request_tween(request):
+        from switchboard import operator
+        # Inject the request into Switchboard's context.
+        operator.get_request = lambda: request
+        response = handler(request)
+        # Notify Switchboard that we're finished with the request.
+        request_finished.send(request)
+        return response
+
+    return request_tween
+
+
+def includeme(config):
+    """ Activate the switchboard; usually called via
+    ``config.include('pyramid_switchboard')`` instead of being invoked
+    directly. """
+    settings = config.registry.settings
+    # By setting nested to True, we're only looking for switchboard.* settings.
+    configure(settings, nested=True)
+
+    # Setup the tween for injecting the request into Switchboard's context.
+    config.add_tween('pyramid_switchboard.request_tween_factory')
+
+    # Create the new application using the updated settings.
+    application = make_application(settings, config.registry)
+    config.add_route('switchboard', '/_switchboard/*subpath')
+    permission = settings.get('switchboard.permission', 'admin')
+    config.add_view(wsgiapp2(application), route_name='switchboard',
+                    permission=permission)
+
+
+def make_application(settings, parent_registry):
+    """ WSGI application for rendering the switchboard admin."""
+    config = Configurator(settings=settings)
+    # Setup routes and views.
+    config.registry.parent_registry = parent_registry
+    config.add_route('switchboard.main', '/')
+    config.add_route('switchboard.add', '/add')
+    config.add_route('switchboard.update', '/update')
+    config.add_route('switchboard.status', '/status')
+    config.add_route('switchboard.delete', '/delete')
+    config.add_route('switchboard.add_condition', '/add_condition')
+    config.add_route('switchboard.remove_condition', '/remove_condition')
+    config.add_route('switchboard.history', '/history')
+    config.include('pyramid_mako')
+    config.scan('pyramid_switchboard.views')
+    return config.make_wsgi_app()
---
+++ packages/pyramid_switchboard/pyramid_switchboard-0.3.tar.gz/pyramid_switchboard-0.3/setup.py
@@ -0,0 +1,41 @@
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+try:
+    README = open(os.path.join(here, 'README.rst')).read()
+except IOError:
+    README = ''
+
+install_requires = [
+    'pyramid',
+    'pyramid_mako',
+    'switchboard'
+    ]
+
+setup(name='pyramid_switchboard',
+      version='0.3',
+      description=('A package which wraps the switchboard feature flipper '
+                   'for Pyramid application development'),
+      long_description=README,
+      classifiers=[
+          "Intended Audience :: Developers",
+          "Programming Language :: Python",
+          "Programming Language :: Python :: 2.6",
+          "Programming Language :: Python :: 2.7",
+          "Framework :: Pyramid",
+          "Topic :: Internet :: WWW/HTTP :: WSGI",
+          "License :: OSI Approved :: Apache Software License",
+      ],
+      keywords='wsgi pylons pyramid featureflipper',
+      author=("Kyle Adams"),
+      author_email="kadams54@gmail.com",
+      url="https://github.com/switchboardpy/pyramid_switchboard",
+      license="Apache License",
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=install_requires,
+      entry_points='',
+      )
