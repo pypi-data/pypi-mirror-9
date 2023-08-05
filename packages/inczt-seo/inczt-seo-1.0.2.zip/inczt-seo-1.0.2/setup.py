49885c1301dcdd4655c109502dd0f05083961a69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:44 +0000

    Add package inczt-seo/1.0.2/inczt-seo-1.0.2.zip

---
+++ packages/inczt-seo/inczt-seo-1.0.2.zip/inczt-seo-1.0.2/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+    name = 'inczt-seo',
+    version = '1.0.2',
+    py_modules = ['seo'],
+    author = 'zhanglei',
+    author_email = 'zlgood1991@gmail.com',
+    url = 'http://www.zhanglei.ac.cn',
+    description = 'A test python code!',
+    )
