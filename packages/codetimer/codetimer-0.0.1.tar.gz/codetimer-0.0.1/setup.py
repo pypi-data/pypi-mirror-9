e7efca27137840397a684531e45fc560122e9d25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:39 +0000

    Add package codetimer/0.0.1/codetimer-0.0.1.tar.gz

---
+++ packages/codetimer/codetimer-0.0.1.tar.gz/codetimer-0.0.1/setup.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+__author__ = 'Min'
+
+from distutils.core import setup
+
+setup (
+        name               = 'codetimer',
+        version             = '0.0.1',
+        py_modules      = ['codetimer'],
+        author              = 'Min',
+        author_email     = 'wwwee98@gmail.com',
+        url                    = 'https://www.facebook.com/yingerking',
+        description        = 'Simple code timer',
+        keywords = ['runtime', 'timer', 'codetimer']
+    )
