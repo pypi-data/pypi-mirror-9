534f2f511224daf400237480dee3a362fcd75f2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:40 +0000

    Add package vr.common/3.17/vr.common-3.17.zip

---
+++ packages/vr.common/vr.common-3.17.zip/vr.common-3.17/setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+
+import sys
+
+import setuptools
+
+PY3 = sys.version_info >= (3,)
+
+py2_reqs = ['suds==0.4'] if not PY3 else []
+
+params = dict(
+    name='vr.common',
+    namespace_packages=['vr'],
+    version='3.17',
+    author='Brent Tubbs',
+    author_email='brent.tubbs@gmail.com',
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    url='https://bitbucket.org/yougov/velociraptor',
+    install_requires=[
+        'isodate>=0.4.4',
+        'six>=1.4.1',
+        'utc',
+        'requests',
+        'PyYAML>=3.10',
+        'sseclient==0.0.8',
+    ] + py2_reqs,
+    description='Libraries and for deploying with Velociraptor',
+)
+
+if __name__ == '__main__':
+    setuptools.setup(**params)
