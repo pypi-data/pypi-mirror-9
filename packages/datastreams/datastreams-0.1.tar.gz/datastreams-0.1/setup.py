8bba520b8b245f328d4035adac1a775904a5d359
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:21 +0000

    Add package datastreams/0.1/datastreams-0.1.tar.gz

---
+++ packages/datastreams/datastreams-0.1.tar.gz/datastreams-0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'datastreams',
+  packages = ['datastreams'],
+  version = '0.1',
+  description = 'A module for managing data in streams.',
+  author = 'Christopher Su',
+  author_email = 'chris+py@christopher.su',
+  url = 'https://github.com/csu/datastreams',
+  install_requires=[
+      "pymongo == 2.7.2"
+  ]
+)
