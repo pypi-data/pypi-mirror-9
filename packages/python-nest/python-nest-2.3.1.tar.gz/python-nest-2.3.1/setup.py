4ff365c8041dcf54c2fed8fffda84611cbfad2d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:48 +0000

    Add package python-nest/2.3.1/python-nest-2.3.1.tar.gz

---
+++ packages/python-nest/python-nest-2.3.1.tar.gz/python-nest-2.3.1/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+import io
+
+from setuptools import setup
+
+
+# NOTE(jkoelker) Subjective guidelines for Major.Minor.Micro ;)
+#                Bumping Major means an API contract change.
+#                Bumping Minor means API bugfix or new functionality.
+#                Bumping Micro means CLI change of any kind unless it is
+#                    significant enough to warrant a minor/major bump.
+version = '2.3.1'
+
+
+setup(name='python-nest',
+      version=version,
+      description='Python API and command line tool for talking to the '
+                  'Nest™ Thermostat',
+      long_description=io.open('README.rst', encoding='UTF-8').read(),
+      keywords='nest thermostat',
+      author='Jason Kölker',
+      author_email='jason@koelker.net',
+      url='https://github.com/jkoelker/python-nest/',
+      packages=['nest'],
+      install_requires=['requests'],
+      entry_points={
+          'console_scripts': ['nest=nest.command_line:main'],
+      }
+      )
