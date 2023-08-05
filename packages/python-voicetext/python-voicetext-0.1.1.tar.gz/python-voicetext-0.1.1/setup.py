84e8611fc0158f89f53cb5c71b3c71c0f9d84239
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:33 +0000

    Add package python-voicetext/0.1.1/python-voicetext-0.1.1.tar.gz

---
+++ packages/python-voicetext/python-voicetext-0.1.1.tar.gz/python-voicetext-0.1.1/setup.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from setuptools  import setup, find_packages
+
+setup(name='python-voicetext',
+      version='0.1.1',
+      license='Apache License 2.0',
+      description='Python library of VoiceText Web API',
+      long_description='See also https://github.com/youtalk/python-voicetext#readme',
+      author='Yutaka Kondo',
+      author_email='yutaka.kondo@youtalk.jp',
+      url='https://github.com/youtalk/python-voicetext',
+      packages=find_packages(),
+      download_url='https://github.com/youtalk/python-voicetext/releases/tag/0.1.1',
+      install_requires=['requests', 'PyAudio'],
+      platforms = ['POSIX', 'Mac OS X', 'Windows'],
+      classifiers=[
+          'Environment :: Console',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: Apache Software License',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+          'Topic :: Multimedia :: Sound/Audio'
+      ]
+)
