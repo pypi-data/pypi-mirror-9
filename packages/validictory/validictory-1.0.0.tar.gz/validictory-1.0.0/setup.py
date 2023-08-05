5b59808771dc6ef61e6fb474fee2fb3195b24f08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:08 +0000

    Add package validictory/1.0.0/validictory-1.0.0.tar.gz

---
+++ packages/validictory/validictory-1.0.0.tar.gz/validictory-1.0.0/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+from validictory import __version__
+DESCRIPTION = "general purpose python data validator"
+LONG_DESCRIPTION = open('README.rst').read()
+
+setup(name='validictory',
+      version=__version__,
+      description=DESCRIPTION,
+      long_description=LONG_DESCRIPTION,
+      author='James Turk',
+      author_email='jturk@sunlightfoundation.com',
+      url='http://github.com/sunlightlabs/validictory',
+      license="MIT License",
+      platforms=["any"],
+      packages=find_packages(),
+      test_suite="validictory.tests",
+      classifiers=["Development Status :: 4 - Beta",
+                   "Intended Audience :: Developers",
+                   "License :: OSI Approved :: BSD License",
+                   "Natural Language :: English",
+                   "Operating System :: OS Independent",
+                   "Programming Language :: Python :: 2.7",
+                   "Programming Language :: Python :: 3.3",
+                   "Programming Language :: Python :: 3.4",
+                   "Topic :: Software Development :: Libraries :: Python Modules",
+                   ],
+     )
