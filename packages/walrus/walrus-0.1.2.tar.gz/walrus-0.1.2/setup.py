62b18a64ded68c55061098f2febf7041f28741bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:15 +0000

    Add package walrus/0.1.2/walrus-0.1.2.tar.gz

---
+++ packages/walrus/walrus-0.1.2.tar.gz/walrus-0.1.2/setup.py
@@ -0,0 +1,36 @@
+import os
+
+from setuptools import setup
+
+cur_dir = os.path.dirname(__file__)
+readme = os.path.join(cur_dir, 'README.md')
+if os.path.exists(readme):
+    with open(readme) as fh:
+        long_description = fh.read()
+else:
+    long_description = ''
+
+setup(
+    name='walrus',
+    version=__import__('walrus').__version__,
+    description='walrus',
+    long_description=long_description,
+    author='Charles Leifer',
+    author_email='coleifer@gmail.com',
+    url='http://github.com/coleifer/',
+    py_modules=['walrus'],
+    package_data={
+        'walrus': [
+            'scripts/*',
+            'stopwords.txt',
+        ],
+    },
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
+    test_suite='walrus',
+)
