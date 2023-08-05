1562d30ccc0b7f23898c7556c7e478b9d42a8bcb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:35 +0000

    Add package uniseg/0.6.3/uniseg-0.6.3.zip

---
+++ packages/uniseg/uniseg-0.6.3.zip/uniseg-0.6.3/setup.py
@@ -0,0 +1,28 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+setup(
+    name = 'uniseg',
+    version = '0.6.3',
+    author = 'Masaaki Shibata',
+    author_email = 'mshibata@emptypage.jp',
+    url = 'https://bitbucket.org/emptypage/uniseg-python',
+    description = 'A pure Python module to determine Unicode text segmentations',
+    packages = ['uniseg'],
+    package_data = {
+        'uniseg': ['ucd.sqlite3']
+    },
+    scripts = ['unibreak.py', 'uniwrap.py', 'wxwrapdemo.py'],
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Text Processing',
+    ],
+)
