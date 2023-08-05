40abc5555cda6fe389266d5e9058a0cefe0f1f05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:07 +0000

    Add package test_mm/0.1.2/test_mm-0.1.2.zip

---
+++ packages/test_mm/test_mm-0.1.2.zip/test_mm-0.1.2/setup.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 12 11:52:31 2015
+
+@author: Magerl_4
+"""
+
+from setuptools import setup
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+setup(name = 'test_mm',
+      version = '0.1.2',
+      description = 'PyPI tutorial.',
+      long_description = 'Tutorial for making politically correct \
+                              Python packages.',
+      classifiers = [
+        'Development Status :: 1 - Planning',
+        'License :: Free For Home Use',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Education :: Testing'
+      ],
+      keywords = 'test tutorial',
+      url = 'https://mmagerl@bitbucket.org/mmagerl/packaging_tutorial.git',
+      author = 'mmagerl',
+      author_email = 'mmagerl@example.com',
+      license = 'MM',
+      packages = ['test_mm'],
+      install_requires = [
+            'markdown',
+      ],
+      include_package_data = True,
+      zip_safe = False)
