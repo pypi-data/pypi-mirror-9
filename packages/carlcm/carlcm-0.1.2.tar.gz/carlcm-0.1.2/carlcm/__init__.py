aeea5d7164d2aa3434089e8d7b446fedeb726f4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:40 +0000

    Add package carlcm/0.1.2/carlcm-0.1.2.tar.gz

---
+++ packages/carlcm/carlcm-0.1.2.tar.gz/carlcm-0.1.2/carlcm/__init__.py
@@ -0,0 +1,4 @@
+
+from .context import Context, MockContext
+from .modules import *
+#import .modules as modules
---
+++ packages/carlcm/carlcm-0.1.2.tar.gz/carlcm-0.1.2/setup.py
@@ -0,0 +1,30 @@
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from distutils.core import setup, find_packages
+
+version = '0.1.2'
+
+config = {
+    'description': 'CarlCM',
+    'author': 'Carl J Mackey',
+    'url': 'https://github.com/cjmackey/carlcm',
+    'download_url': 'https://github.com/cjmackey/carlcm/tarball/' + version,
+    'author_email': 'carljmackey@gmail.com',
+    'version': version,
+    'install_requires': ['nose', 'mock', 'coverage',
+                         'jinja2', 'pyfakefs', 'python-consul'],
+    'packages': find_packages(),
+    'scripts': [],
+    'name': 'carlcm',
+    'classifiers': [
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Topic :: System :: Installation/Setup',
+    ],
+}
+
+setup(**config)
