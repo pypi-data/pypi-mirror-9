3896d531be069d5a69f743c5fdfa26207dd3d6c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:46 +0000

    Add package destiny_bot/0.0.8/destiny_bot-0.0.8.tar.gz

---
+++ packages/destiny_bot/destiny_bot-0.0.8.tar.gz/destiny_bot-0.0.8/destiny_bot.py
@@ -0,0 +1,26 @@
+from helga.plugins import command
+
+from scraper import nightfall_info, heroic_info, daily_info, crucible_info, get_soup
+
+nf_help = 'Displays info on the current nightfall from destinytracker.com'
+
+@command('nightfall', aliases=('nf',), help=nf_help)
+def nightfall(client, channel, nick, message, cmd, args):
+    soup = get_soup()
+    return nightfall_info(soup)
+
+hero_help = 'Displays info on the current heroic from destinytracker.com'
+@command('heroic', aliases=('hero',), help=hero_help)
+def heroic(client, channel, nick, message, cmd, args):
+    soup = get_soup()
+    return heroic_info(soup)
+
+daily_help = 'Displays info on the current heroic daily from planetdestiny.com'
+@command('daily', aliases=(), help=daily_help)
+def daily(client, channel, nick, message, cmd, args):
+    return daily_info()
+
+crucible_help = 'Displays info on the current daily crucible mode form planetdestiny.com'
+@command('crucible', aliases=(), help=crucible_help)
+def crucible(client, channel, nick, message, cmd, args):
+    return crucible_info()
---
+++ packages/destiny_bot/destiny_bot-0.0.8.tar.gz/destiny_bot-0.0.8/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='destiny_bot',
+    version='0.0.8',
+    description='Set of commands that scrape destinytracker.com for info.',
+    author='Nolan Brubaker',
+    author_email='palendae@gmail.com',
+    packages=find_packages(),
+    py_modules=['destiny_bot', 'scraper'],
+    include_package_data=True,
+    zip_safe=True,
+    install_requires=[
+        'beautifulsoup4==4.3.2',
+        'requests==2.2.1',
+        'helga==1.6.4',
+    ],
+    entry_points = dict(
+        helga_plugins=[
+            'nightfall = destiny_bot:nightfall',
+            'heroic = destiny_bot:heroic',
+            'daily = destiny_bot:daily',
+            'crucible = destiny_bot:crucible',
+        ]
+    )
+)
