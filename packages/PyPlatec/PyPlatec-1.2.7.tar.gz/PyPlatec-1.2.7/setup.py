ee47b755f4348041f438fd62a359eb638e5bce4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:40 +0000

    Add package PyPlatec/1.2.7/PyPlatec-1.2.7.tar.gz

---
+++ packages/PyPlatec/PyPlatec-1.2.7.tar.gz/PyPlatec-1.2.7/setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup, Extension
+
+extra_compile_args = "-std=c++11"
+
+pyplatec = Extension('platec',                    
+                    sources = [
+                        'platec_src/platecmodule.cpp',
+                        'platec_src/platecapi.cpp',
+                        'platec_src/plate.cpp',
+                        'platec_src/lithosphere.cpp',
+                        'platec_src/heightmap.cpp',
+                        'platec_src/rectangle.cpp',
+                        'platec_src/simplexnoise.cpp',
+                        'platec_src/sqrdmd.cpp',
+                        'platec_src/noise.cpp'],
+                     language='c++',
+                     extra_compile_args=[extra_compile_args],
+                    )
+
+setup (name = 'PyPlatec',
+       version = '1.2.7',
+       author = "Federico Tomassetti",
+       author_email = "f.tomassetti@gmail.com",
+       url = "https://github.com/ftomassetti/pyplatec",
+       description = 'Plates simulation library',
+       ext_modules = [pyplatec])
