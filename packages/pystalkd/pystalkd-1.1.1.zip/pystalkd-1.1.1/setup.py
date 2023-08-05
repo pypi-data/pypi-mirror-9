3b771ae17cf6458f7fce8b323c02ec5900540787
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:28 +0000

    Add package pystalkd/1.1.1/pystalkd-1.1.1.zip

---
+++ packages/pystalkd/pystalkd-1.1.1.zip/pystalkd-1.1.1/setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+import os
+long_description = 'See https://github.com/menezes-/pystalkd'
+if os.path.exists('README.rst'):
+    long_description = open('README.rst').read()
+
+setup(
+    name='pystalkd',
+    version='1.1.1',
+    packages=['pystalkd'],
+    url='https://github.com/menezes-/pystalkd',
+    download_url='https://github.com/menezes-/pystalkd/archive/master.zip',
+    keywords=['beanstalkd', 'python3', 'bindings'],
+    license='Apache-2.0',
+    author='Gabriel',
+    author_email='gabrielmenezesvi@gmail.com',
+    description='Beanstalkd bindings for python3',
+    long_description=long_description
+)
