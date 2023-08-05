bd765c70d3320db5a814af8dd163e47df8ac8d4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:00 +0000

    Add package plunchy/1.0.0/plunchy-1.0.0.tar.gz

---
+++ packages/plunchy/plunchy-1.0.0.tar.gz/plunchy-1.0.0/setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+
+version ='1.0.0'
+setup(
+    name = 'plunchy',
+    py_modules = ['plunchy'],
+    scripts = ['bin/plunchy'],
+    version = version,
+    description = "A simpler interface into OS X's launchctl",
+    author = 'Bill Israel',
+    author_email = 'bill.israel@gmail.com',
+    url = 'https://github.com/epochblue/plunchy',
+    keywords = ['os x', 'launchctl', 'lunchy'],
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Environment :: MacOS X',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: MacOS :: MacOS X',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Topic :: Utilities',
+    ],
+)
