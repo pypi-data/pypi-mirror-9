a854842f240f80a0fcb0683b9b6cdc95440ef2dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:15 +0000

    Add package pysamstats/0.20b2/pysamstats-0.20b2.tar.gz

---
+++ packages/pysamstats/pysamstats-0.20b2.tar.gz/pysamstats-0.20b2/setup.py
@@ -0,0 +1,40 @@
+from distutils.core import setup
+from ast import literal_eval
+from distutils.extension import Extension
+from Cython.Build import cythonize
+import pysam
+
+
+def get_version(source='pysamstats.pyx'):
+    with open(source) as f:
+        for line in f:
+            if line.startswith('__version__'):
+                return literal_eval(line.partition('=')[2].lstrip())
+    raise ValueError("__version__ not found")
+
+
+extensions = [Extension('pysamstats',
+                        sources=['pysamstats.pyx'],
+                        include_dirs=pysam.get_include())]
+
+
+setup(
+    name='pysamstats',
+    version=get_version(),
+    author='Alistair Miles',
+    author_email='alimanfoo@googlemail.com',
+    url='https://github.com/alimanfoo/pysamstats',
+    license='MIT Licenses',
+    description='A Python utility for calculating statistics against genome '
+                'position based on sequence alignments from a SAM or BAM file.',
+    scripts=['scripts/pysamstats'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+    ext_modules=cythonize(extensions),
+)
