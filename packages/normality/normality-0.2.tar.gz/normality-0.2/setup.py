cbc5e3bd8c996fb125237fa7dc8b41a60cb2967e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:07 +0000

    Add package normality/0.2/normality-0.2.tar.gz

---
+++ packages/normality/normality-0.2.tar.gz/normality-0.2/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup
+
+
+setup(
+    name='normality',
+    version='0.2',
+    description="Micro-library to normalize text strings",
+    long_description="",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3'
+    ],
+    keywords='text unicode normalization slugs',
+    author='Friedrich Lindenberg',
+    author_email='friedrich@pudo.org',
+    url='http://github.com/pudo/normality',
+    license='MIT',
+    py_modules=['normality'],
+    namespace_packages=[],
+    include_package_data=False,
+    zip_safe=True,
+    install_requires=[
+        'six',
+        'unidecode'
+    ],
+    tests_require=[],
+    test_suite='test',
+    entry_points={
+    }
+)
