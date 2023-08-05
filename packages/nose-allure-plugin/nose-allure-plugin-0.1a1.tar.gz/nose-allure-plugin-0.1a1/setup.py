daaf8df31499a82d51d64e649844c4b21566953b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:17 +0000

    Add package nose-allure-plugin/0.1a1/nose-allure-plugin-0.1a1.tar.gz

---
+++ packages/nose-allure-plugin/nose-allure-plugin-0.1a1.tar.gz/nose-allure-plugin-0.1a1/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+__author__ = "chipiga86@gmail.com"
+
+
+from setuptools import setup
+
+setup(
+    name='nose-allure-plugin',
+    version='0.1-alpha1',
+    description='Nose plugin for allure framework',
+    long_description=open('README.md').read(),
+    author='Sergey Chipiga',
+    author_email='chipiga86@gmail.com',
+    py_modules=["nose_allure"],
+    url="https://github.com/sergeychipiga/nose-allure-plugin",
+    install_requires=[
+        'nose',
+        'pytest-allure-adaptor'
+    ],
+    entry_points={
+        'nose.plugins.0.10': [
+            'allure = nose_allure:Allure'
+        ]
+    }
+)
