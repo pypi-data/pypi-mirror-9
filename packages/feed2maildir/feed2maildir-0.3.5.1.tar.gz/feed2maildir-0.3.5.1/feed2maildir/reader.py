0badf7a7fd6686e2491f86b9e2687c3f24368965
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:36 +0000

    Add package feed2maildir/0.3.5.1/feed2maildir-0.3.5.1.tar.gz

---
+++ packages/feed2maildir/feed2maildir-0.3.5.1.tar.gz/feed2maildir-0.3.5.1/feed2maildir/reader.py
@@ -0,0 +1,19 @@
+import feedparser
+
+class Reader:
+    """Get updates on the feeds supplied"""
+
+    def __init__(self, feeds, silent=False):
+        self.feeds = []
+        self.silent = silent
+        for feed in feeds:
+            f = feedparser.parse(feeds[feed])
+            if f.bozo:
+                self.output('WARNING: could not parse feed {}'.format(feed))
+            else:
+                self.feeds.append(f)
+
+    def output(self, arg):
+        if not self.silent:
+            print(arg)
+
