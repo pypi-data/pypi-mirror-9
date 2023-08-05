20cd48a5894488b10c6e3bad01cdcf3f84d18894
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:21 +0000

    Add package jingerly/0.0.2/jingerly-0.0.2.tar.gz

---
+++ packages/jingerly/jingerly-0.0.2.tar.gz/jingerly-0.0.2/setup.py
@@ -0,0 +1,30 @@
+import os
+from setuptools import setup
+
+
+def read(fname):
+    with open(os.path.join(os.path.dirname(__file__), fname)) as fd:
+        return fd.read()
+
+setup(
+    name="jingerly",
+    version="0.0.2",
+    author="William Mayor",
+    author_email="jinjerly@williammayor.co.uk",
+    description=("Project templating using Jinja2."),
+    license="MIT",
+    keywords="project management template",
+    url="https://github.com/WilliamMayor/jingerly",
+    py_modules=['jingerly'],
+    install_requires=['jinja2', 'requests'],
+    scripts=['bin/jingerly'],
+    long_description=read('README.md'),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Topic :: Software Development :: Code Generators'
+    ],
+)
