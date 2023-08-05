cc5bdaff8447e70b24b426fab35899d956f7cfa2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:58 +0000

    Add package tendo/0.2.7/tendo-0.2.7.tar.gz

---
+++ packages/tendo/tendo-0.2.7.tar.gz/tendo-0.2.7/tendo/__init__.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# Licensed to PSF under a Contributor Agreement.
+# See http://www.python.org/psf/license for licensing details.
+from __future__ import absolute_import
+
+__author__ = "Sorin Sbarnea"
+__copyright__ = "Copyright 2010-2013, Sorin Sbarnea"
+__email__ = "sorin(dot)sbarnea(at)gmail.com"
+__status__ = "Production"
+from . import version
+__date__ = "2013-09-10"
+
+__all__ = ['tee', 'colorer', 'unicode',
+           'execfile2', 'singleton', 'ansiterm', 'version']
+
+"""
+Tendo is tested with Python 2.5-3.3
+"""
+
+import sys
+if sys.hexversion < 0x02050000:
+    sys.exit("Python 2.5 or newer is required by tendo module.")
