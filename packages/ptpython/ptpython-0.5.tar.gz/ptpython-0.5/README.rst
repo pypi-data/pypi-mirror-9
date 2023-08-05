65d28dadfbe7d901c1fa8abae4c9fd010dd960af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:55 +0000

    Add package ptpython/0.5/ptpython-0.5.tar.gz

---
+++ packages/ptpython/ptpython-0.5.tar.gz/ptpython-0.5/README.rst
@@ -0,0 +1,190 @@
+ptpython: a better Python REPL
+==============================
+
+.. image:: https://pypip.in/version/ptpython/badge.svg
+    :target: https://pypi.python.org/pypi/ptpython/
+    :alt: Latest Version
+
+``ptpython`` is an advanced Python REPL built on top of the `prompt_toolkit
+<http://github.com/jonathanslenders/python-prompt-toolkit>`_ library.
+
+It works best on all Posix systems like Linux, BSD and OS X. But it should work
+as well on Windows. It works on all Python versions from 2.6 up to 3.4.
+
+
+Installation
+************
+
+To install ``ptpython``, type:
+
+::
+
+    pip install ptpython
+
+
+The REPL
+********
+
+Run ``ptpython`` to get an interactive Python prompt with syntax highlighting,
+code completion, etc...
+
+.. image :: https://github.com/jonathanslenders/ptpython/raw/master/docs/images/example1.png
+
+By default, you will have Emacs key bindings, but if you prefer Vi bindings
+(like in the above screenshot) then run ``ptpython --vi``.
+
+If you want to embed the REPL inside your application at one point, do:
+
+.. code:: python
+
+    from ptpython.repl import embed
+    embed(globals(), locals(), vi_mode=False, history_filename=None)
+
+
+Autocompletion
+**************
+
+``Tab`` and ``shift+tab`` complete the input.
+In Vi-mode, you can also use ``Ctrl+N`` and ``Ctrl+P``.
+
+There is even completion on file names inside strings:
+
+.. image :: https://github.com/jonathanslenders/ptpython/raw/master/docs/images/file-completion.png
+
+
+Multiline editing
+*****************
+
+Usually, multi-line editing mode will automatically turn on when you press enter
+after a colon, however you can always turn it on by pressing ``F7``.
+
+To execute the input in multi-line mode, you can either press ``Alt+Enter``, or
+``Esc`` followed by ``Enter``. (If you want the first to work in the OS X
+terminal, you have to check the "Use option as meta key" checkbox in your
+terminal settings. For iTerm2, you have to check "Left option acts as +Esc" in
+the options.)
+
+.. image :: https://github.com/jonathanslenders/ptpython/raw/master/docs/images/multiline.png
+
+
+Syntax validation
+*****************
+
+Before execution, ``ptpython`` will see whether the input is syntactically
+correct Python code. If not, it will show a warning, and move the cursor to the
+error.
+
+.. image :: https://github.com/jonathanslenders/ptpython/raw/master/docs/images/validation.png
+
+
+Tab pages
+*********
+
+Suppose that you were editing a long multiline Python command, but suddenly you
+want to test something else in between in the same shell and go back. We have
+tab pages for this. Each tab has its own input buffer, but they are all
+evaluated in the same environment (sharing locals/globals).
+
+.. image :: https://github.com/jonathanslenders/ptpython/raw/master/docs/images/tab-pages.png
+
+The shortcuts:
+
++------------------------+-----------------------+
+| Create a new tab page  | Control-T             |
++------------------------+-----------------------+
+| Close current tab page | Control-D             |
++------------------------+-----------------------+
+| Navigate to next tab   | Control-Arrow-right   |
+|                        | (Or in Vi-mode: 'gt') |
++------------------------+-----------------------+
+| Navigate to previous   | Control-Arrow-left    |
+| tab                    | (Or in Vi-mode: 'gT') |
++------------------------+-----------------------+
+
+Other features
+***************
+
+Running system commands: Press ``Meta-!`` in Emacs mode or just ``!`` in Vi
+navigation mode to see the "Shell command" prompt. There you can enter system
+commands without leaving the REPL.
+
+Selecting text: Press ``Control+Space`` in Emacs mode on ``V`` (major V) in Vi
+navigation mode.
+
+
+You love IPython?
+*****************
+
+Run ``ptipython`` (prompt_toolkit - IPython), to get a nice interactive shell
+with all the power that IPython has to offer, like magic functions and shell
+integration. Make sure that IPython has been installed. (``pip install
+ipython``)
+
+.. image :: https://github.com/jonathanslenders/ptpython/raw/master/docs/images/ipython.png
+
+
+You are using Django?
+*********************
+
+`django-extensions <https://github.com/django-extensions/django-extensions>`_
+has a ``shell_plus`` management command. When ``ptpython`` has been installed,
+it will by default use ``ptpython`` or ``ptipython``.
+
+
+PDB
+***
+
+There is an experimental PDB replacement: `ptpdb
+<https://github.com/jonathanslenders/ptpdb>`_.
+
+
+About Windows support
+*********************
+
+``prompt_toolkit`` works still a little better on systems like Linux and OS X
+than on Windows, but it certainly is usable. One thing that still needs
+attention is the colorscheme. Windows terminals don't support all colors, so we
+have to create another colorscheme for Windows.
+
+.. image :: https://github.com/jonathanslenders/ptpython/raw/master/docs/images/windows.png
+
+
+FAQ
+---
+
+Q
+ The ``Ctrl-S`` forward search doesn't work and freezes my terminal.
+A
+ Try to run ``stty -ixon`` in your terminal to disable flow control.
+
+Q
+ The ``Meta``-key doesn't work.
+A
+ For some terminals you have to enable the Alt-key to act as meta key, but you
+ can also type ``Escape`` before any key instead.
+
+
+Alternatives
+************
+
+Have a look at the alternatives.
+
+- `BPython <http://bpython-interpreter.org/downloads.html>`_
+
+If you find another alternative, you can create an issue and we'll list it
+here. If you find a nice feature somewhere that is missing in ``ptpython``,
+also create a GitHub issue and mabye we'll implement it.
+
+
+Special thanks to
+*****************
+
+- `Pygments <http://pygments.org/>`_: Syntax highlighter.
+- `Jedi <http://jedi.jedidjah.ch/en/latest/>`_: Autocompletion library.
+- `Docopt <http://docopt.org/>`_: Command-line interface description language.
+- `wcwidth <https://github.com/jquast/wcwidth>`_: Determine columns needed for a wide characters.
+- `prompt_toolkit <http://github.com/jonathanslenders/python-prompt-toolkit>`_ for the interface.
+
+.. |PyPI| image:: https://pypip.in/version/prompt-toolkit/badge.svg
+    :target: https://pypi.python.org/pypi/prompt-toolkit/
+    :alt: Latest Version
---
+++ packages/ptpython/ptpython-0.5.tar.gz/ptpython-0.5/ptpython/ipython.py
@@ -0,0 +1,225 @@
+"""
+
+Adaptor for using the input system of `prompt_toolkit` with the IPython
+backend.
+
+This gives a powerful interactive shell that has a nice user interface, but
+also the power of for instance all the %-magic functions that IPython has to
+offer.
+
+"""
+from __future__ import unicode_literals
+
+from prompt_toolkit import AbortAction
+from prompt_toolkit import Exit
+from prompt_toolkit.completion import Completion, Completer
+from prompt_toolkit.contrib.completers import PathCompleter, WordCompleter, SystemCompleter
+from prompt_toolkit.contrib.regular_languages.compiler import compile
+from prompt_toolkit.contrib.regular_languages.completion import GrammarCompleter
+from prompt_toolkit.contrib.regular_languages.lexer import GrammarLexer
+from prompt_toolkit.document import Document
+from prompt_toolkit.layout.controls import TokenListControl
+
+from ptpython.python_input import PythonCommandLineInterface, PythonValidator, PythonCompleter
+
+from IPython.terminal.embed import InteractiveShellEmbed as _InteractiveShellEmbed
+from IPython.terminal.ipapp import load_default_config
+from IPython import utils as ipy_utils
+from IPython.core.inputsplitter import IPythonInputSplitter
+
+from pygments.lexers import PythonLexer, BashLexer
+from pygments.token import Token
+
+__all__ = (
+    'embed',
+)
+
+
+class IPythonPrompt(TokenListControl):
+    """
+    Prompt showing something like "In [1]:".
+    """
+    def __init__(self, prompt_manager):
+        def get_tokens(cli):
+            text = prompt_manager.render('in', color=False, just=False)
+            return [(Token.Layout.Prompt, text)]
+
+        super(IPythonPrompt, self).__init__(get_tokens)
+
+
+class IPythonValidator(PythonValidator):
+    def __init__(self, *args, **kwargs):
+        super(IPythonValidator, self).__init__(*args, **kwargs)
+        self.isp = IPythonInputSplitter()
+
+    def validate(self, document):
+        document = Document(text=self.isp.transform_cell(document.text))
+        super(IPythonValidator, self).validate(document)
+
+
+def create_ipython_grammar():
+    """
+    Return compiled IPython grammar.
+    """
+    return compile(r"""
+        \s*
+        (
+            (?P<percent>%)(
+                (?P<magic>pycat|run|loadpy|load)  \s+ (?P<filename>[^\s]+)     |
+                (?P<magic>pushd|cd|ls)            \s+ (?P<directory>[^\s]+)    |
+                (?P<magic>pdb)                    \s+ (?P<pdb_arg>[^\s]+)      |
+                (?P<magic>autocall)               \s+ (?P<autocall_arg>[^\s]+) |
+                (?P<magic>time|timeit|prun)       \s+ (?P<python>.+)           |
+                (?P<magic>psource|pfile|pinfo|pinfo2) \s+ (?P<python>.+)       |
+                (?P<magic>system)                 \s+ (?P<system>.+)           |
+                (?P<magic>unalias)                \s+ (?P<alias_name>.+)       |
+                (?P<magic>[^\s]+)   .* |
+            ) .*            |
+            !(?P<system>.+) |
+            (?![%!]) (?P<python>.+)
+        )
+        \s*
+    """)
+
+
+def create_completer(get_globals, get_locals, magics_manager, alias_manager):
+    g = create_ipython_grammar()
+
+    return GrammarCompleter(g, {
+        'python': PythonCompleter(get_globals, get_locals),
+        'magic': MagicsCompleter(magics_manager),
+        'alias_name': AliasCompleter(alias_manager),
+        'pdb_arg': WordCompleter(['on', 'off'], ignore_case=True),
+        'autocall_arg': WordCompleter(['0', '1', '2'], ignore_case=True),
+        'filename': PathCompleter(only_directories=False, file_filter=lambda name: name.endswith('.py')),
+        'directory': PathCompleter(only_directories=True),
+        'system': SystemCompleter(),
+    })
+
+
+def create_lexer():
+    g = create_ipython_grammar()
+
+    return GrammarLexer(
+        g,
+        tokens={
+            'percent': Token.Operator,
+            'magic': Token.Keyword,
+            'filename': Token.Name,
+        },
+        lexers={
+            'python': PythonLexer,
+            'system': BashLexer,
+        })
+
+
+class MagicsCompleter(Completer):
+    def __init__(self, magics_manager):
+        self.magics_manager = magics_manager
+
+    def get_completions(self, document, complete_event):
+        text = document.text_before_cursor.lstrip()
+
+        for m in sorted(self.magics_manager.magics['line']):
+            if m.startswith(text):
+                yield Completion('%s' % m, -len(text))
+
+
+class AliasCompleter(Completer):
+    def __init__(self, alias_manager):
+        self.alias_manager = alias_manager
+
+    def get_completions(self, document, complete_event):
+        text = document.text_before_cursor.lstrip()
+        #aliases = [a for a, _ in self.alias_manager.aliases]
+        aliases = self.alias_manager.aliases
+
+        for a, cmd in sorted(aliases, key=lambda a: a[0]):
+            if a.startswith(text):
+                yield Completion('%s' % a, -len(text),
+                                 display_meta=cmd)
+
+
+class IPythonCommandLineInterface(PythonCommandLineInterface):
+    """
+    Override our `PythonCommandLineInterface` to add IPython specific stuff.
+    """
+    def __init__(self, ipython_shell, *a, **kw):
+        kw['_completer'] = create_completer(kw['get_globals'], kw['get_globals'],
+                                            ipython_shell.magics_manager,
+                                            ipython_shell.alias_manager)
+        kw['_lexer'] = create_lexer()
+        kw['_validator'] = IPythonValidator()
+        kw['_python_prompt_control'] = IPythonPrompt(ipython_shell.prompt_manager)
+
+        super(IPythonCommandLineInterface, self).__init__(*a, **kw)
+        self.ipython_shell = ipython_shell
+
+
+class InteractiveShellEmbed(_InteractiveShellEmbed):
+    """
+    Override the `InteractiveShellEmbed` from IPython, to replace the front-end
+    with our input shell.
+    """
+    def __init__(self, *a, **kw):
+        vi_mode = kw.pop('vi_mode', False)
+        history_filename = kw.pop('history_filename', None)
+
+        super(InteractiveShellEmbed, self).__init__(*a, **kw)
+
+        def get_globals():
+            return self.user_ns
+
+        self._cli = IPythonCommandLineInterface(
+            self, get_globals=get_globals, vi_mode=vi_mode,
+            history_filename=history_filename)
+
+    def raw_input(self, prompt=''):
+        print('')
+        try:
+            string = self._cli.cli.read_input(on_exit=AbortAction.RAISE_EXCEPTION).text
+
+            # In case of multiline input, make sure to append a newline to the input,
+            # otherwise, IPython will ask again for more input in some cases.
+            if '\n' in string:
+                return string + '\n\n'
+            else:
+                return string
+        except Exit:
+            self.ask_exit()
+            return ''
+
+
+def initialize_extensions(shell, extensions):
+    """
+    Partial copy of `InteractiveShellApp.init_extensions` from IPython.
+    """
+    try:
+        iter(extensions)
+    except TypeError:
+        pass  # no extensions found
+    else:
+        for ext in extensions:
+            try:
+                shell.extension_manager.load_extension(ext)
+            except:
+                ipy_utils.warn.warn(
+                    "Error in loading extension: %s" % ext +
+                    "\nCheck your config files in %s" % ipy_utils.path.get_ipython_dir())
+                shell.showtraceback()
+
+
+def embed(**kwargs):
+    """
+    Copied from `IPython/terminal/embed.py`, but using our `InteractiveShellEmbed` instead.
+    """
+    config = kwargs.get('config')
+    header = kwargs.pop('header', u'')
+    compile_flags = kwargs.pop('compile_flags', None)
+    if config is None:
+        config = load_default_config()
+        config.InteractiveShellEmbed = config.TerminalInteractiveShell
+        kwargs['config'] = config
+    shell = InteractiveShellEmbed.instance(**kwargs)
+    initialize_extensions(shell, config['InteractiveShellApp']['extensions'])
+    shell(header=header, stack_depth=2, compile_flags=compile_flags)
