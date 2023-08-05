f757f7e1567a8dcac5460fcd954e34072c4fed5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:16 +0000

    Add package djmicrosip_faexist/0.1.3/djmicrosip_faexist-0.1.3.zip

---
+++ packages/djmicrosip_faexist/djmicrosip_faexist-0.1.3.zip/djmicrosip_faexist-0.1.3/djmicrosip_faexist/forms.py
@@ -0,0 +1,72 @@
+#encoding:utf-8
+from django import forms
+import autocomplete_light
+from .models import *
+import autocomplete_light
+from datetime import datetime
+from microsip_api.comun.sic_db import first_or_none
+from django.conf import settings
+
+class PuntoVentaDocumentoForm(forms.ModelForm):
+    cliente = forms.ModelChoiceField(Cliente.objects.all(),widget=autocomplete_light.ChoiceWidget('ClienteAutocomplete'), required=True)
+    linea = forms.ModelChoiceField(LineaArticulos.objects.all(),widget=autocomplete_light.ChoiceWidget('LineaArticulosAutocomplete'), required=True)
+    
+    def clean(self, *args, **kwargs):
+        cleaned_data = self.cleaned_data
+        
+        cajero = cleaned_data.get('cajero')
+        caja = cleaned_data.get('caja')
+        
+        if cajero and not cajero.operar_cajas == 'T' and not CajeroCaja.objects.filter(cajero=cajero, caja=caja).exists():
+            raise forms.ValidationError(u'La caja [%s] no puede ser operada por el cajero [%s]'%(caja, cajero))
+        
+        
+        apertura_ultima = first_or_none( CajaMovimiento.objects.filter(caja=caja, movimiento_tipo = 'A').order_by('-fecha','-hora'))
+        cierre = None
+        if apertura_ultima:
+            #
+            cierre = first_or_none( CajaMovimiento.objects.filter(caja=caja, movimiento_tipo = 'C', fecha__gte=apertura_ultima.fecha, hora__gt=apertura_ultima.hora.strftime('%H:%M:%S') ))
+
+        if not caja:
+            raise forms.ValidationError(u'Selecciona una caja')
+
+        if not apertura_ultima or cierre and caja:
+            raise forms.ValidationError(u'la caja %s no esta abierta por favor abrela para continuar.'%caja.nombre)
+
+        return cleaned_data
+    
+    class Meta:
+        model = PuntoVentaDocumento
+        fields = ['cliente', 'caja','cajero', 'almacen']
+
+
+class PreferenciasManageForm(forms.Form):
+    periodo_fecha_inicio = forms.DateField()
+    proveedor = forms.IntegerField(widget=forms.HiddenInput())
+    almacen = forms.IntegerField(widget=forms.HiddenInput())
+    ventas_caja = forms.ModelChoiceField(queryset=Caja.objects.all(), required=True)
+    ventas_almacen = forms.ModelChoiceField(queryset=Almacen.objects.all(), required=True)
+    ventas_cajero = forms.ModelChoiceField(queryset=Cajero.objects.all(), required=True)
+    ventas_vendedor = forms.ModelChoiceField(queryset=Vendedor.objects.all(), required=True)
+    cliente = forms.ModelChoiceField(Cliente.objects.all(),widget=autocomplete_light.ChoiceWidget('ClienteAutocomplete'), required=True)
+    
+    def __init__(self,*args,**kwargs):
+        bases_de_datos = settings.MICROSIP_DATABASES.keys()
+        empresas = []
+        for database_conexion in bases_de_datos:
+            try:
+                database_conexion = u'%s'%database_conexion
+            except UnicodeDecodeError:
+                pass
+            else:
+                
+                conexion_id, empresa = database_conexion.split('-')
+                conexion = ConexionDB.objects.get(pk=int(conexion_id))
+                database_conexion_name = "%s-%s"%(conexion.nombre, empresa)
+
+                empresa_option = [database_conexion, database_conexion_name]
+                empresas.append(empresa_option)
+                        
+        super(PreferenciasManageForm,self).__init__(*args,**kwargs)
+        self.fields['database'] = forms.ChoiceField(choices= empresas)
+        
---
+++ packages/djmicrosip_faexist/djmicrosip_faexist-0.1.3.zip/djmicrosip_faexist-0.1.3/djmicrosip_faexist/static/djmicrosip_faexist/js/main.js
@@ -0,0 +1,157 @@
+function getProveedores(){
+  var empresa_nombre = $("#id_database").val();
+  $.ajax({
+    url:'/djmicrosip_faexist/get_proveedores_byempresa/', 
+    type : 'get', 
+    data:{
+      'empresa_conexion': empresa_nombre,
+    }, 
+    success: function(data){
+      $("#id_proveedores").html('');
+      $("#id_proveedores").append("<option value=''>Seleciona un proveedor </option>");
+      data.proveedores.forEach(function(proveedor){
+        $("#id_proveedores").append("<option value="+ proveedor.id +">"+ proveedor.nombre+"</option>");
+      });
+
+      if ($("#id_proveedor").val() != "" ){
+        $("#id_proveedores").val($("#id_proveedor").val());
+      }
+    },
+    error: function() {
+      alert('fallo algo');
+    },
+  });
+}
+
+function getAlmacenes(){
+  var empresa_nombre = $("#id_database").val();
+  $.ajax({
+    url:'/djmicrosip_faexist/get_almacenes_byempresa/', 
+    type : 'get', 
+    data:{
+      'empresa_conexion': empresa_nombre,
+    }, 
+    success: function(data){
+      $("#id_almacenes").html('');
+      $("#id_almacenes").append("<option value=''>Seleciona un almacen </option>");
+      data.almacenes.forEach(function(almacen){
+        $("#id_almacenes").append("<option value="+ almacen.ALMACEN_ID +">"+ almacen.nombre+"</option>");
+      });
+
+      if ($("#id_almacen").val() != "" ){
+        $("#id_almacenes").val($("#id_almacen").val());
+      }
+    },
+    error: function() {
+      alert('fallo algo');
+    },
+  });
+}
+
+$("#id_database").on("change", function(){
+  $("#id_proveedor").val("");
+  $("#id_almacen").val("");
+  getProveedores(); 
+  getAlmacenes();
+});
+
+$("#id_proveedores").on("change", function(){
+  var selected_proveedor =  $("#id_proveedores").val();
+  $("#id_proveedor").val(selected_proveedor);
+});
+
+$("#id_almacenes").on("change", function(){
+  var selected_almacen =  $("#id_almacenes").val();
+  $("#id_almacen").val(selected_almacen);
+});
+
+function validForm(){
+  if ($("#id_periodo_fecha_inicio").val() == null || $("#id_periodo_fecha_inicio").val() == "" ) {
+    alert("Campo fecha requerido");
+    return false
+  }
+
+  if ($("#id_cliente").val() == null) {
+    alert("Campo cliente requerido");
+    return false
+  }
+
+  if ($("#id_ventas_caja").val() == '') {
+    alert("Campo Caja(Ventas) requerido");
+    return false
+  }
+
+  if ($("#id_ventas_almacen").val() == '') {
+    alert("Campo Almacen(Ventas) requerido");
+    return false
+  }
+  
+  if ($("#id_ventas_cajero").val() == '') {
+    alert("Campo Cajero(Ventas) requerido");
+    return false
+  }
+
+  if ($("#id_ventas_vendedor").val() == '') {
+    alert("Campo Vendedor(Ventas) requerido");
+    return false
+  }
+
+
+  if ($("#id_database").val() == null) {
+    alert("Campo empresa requerido");
+    return false
+  }
+
+  if ($("#id_proveedor").val() == '') {
+    alert("Campo proveedor requerido");
+    return false
+  }
+
+  if ($("#id_almacen").val() == '') {
+    alert("Campo almacen requerido");
+    return false
+  }
+
+  return true;
+}
+
+$("#id_btn_save").on("click", function(){
+  busqueda_fecha_inicio = $("#id_periodo_fecha_inicio").val();
+  busqueda_cliente_facturas_id = $("#id_cliente").val();
+  compras_empresa_id = $("#id_database").val();
+  compras_proveedor_id = $("#id_proveedor").val();
+  compras_almacen_id = $("#id_almacen").val();
+  ventas_caja_id = $("#id_ventas_caja").val();
+  ventas_almacen_id = $("#id_ventas_almacen").val();
+  ventas_cajero_id = $("#id_ventas_cajero").val();
+  ventas_vendedor_id = $("#id_ventas_vendedor").val();
+
+
+  if (validForm()){
+    $.ajax({
+      url:'/djmicrosip_faexist/guardar_preferencias/', 
+      type : 'get', 
+      data:{
+        'busqueda_fecha_inicio': busqueda_fecha_inicio,
+        'busqueda_cliente_facturas_id': busqueda_cliente_facturas_id,
+        'compras_proveedor_id': compras_proveedor_id,
+        'compras_empresa_id': compras_empresa_id,
+        'compras_almacen_id':compras_almacen_id,
+        'ventas_caja':ventas_caja_id,
+        'ventas_almacen':ventas_almacen_id,
+        'ventas_cajero':ventas_cajero_id,
+        'ventas_vendedor':ventas_vendedor_id,
+      }, 
+      success: function(data){
+        alert(data.msg);
+      },
+      error: function() {
+        alert('fallo algo');
+      },
+    }); 
+  }
+
+});
+
+getProveedores();
+getAlmacenes();
