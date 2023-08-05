ec980b2f221bd6ea0b4580fe11e0fe209066cbae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:52 +0000

    Add package saltobserver/0.9.1/saltobserver-0.9.1.tar.gz

---
+++ packages/saltobserver/saltobserver-0.9.1.tar.gz/saltobserver-0.9.1/saltobserver/views.py
@@ -0,0 +1,84 @@
+from saltobserver import app, redis_pool
+
+from flask import Response
+from flask import render_template, redirect, url_for, request
+
+from redis import Redis
+
+import json
+import time
+
+@app.route('/_get_function_data/<minion>/<jid>/')
+def get_function_data(minion, jid):
+    """AJAX access for loading function/job details."""
+    redis = Redis(connection_pool=redis_pool)
+    data = redis.get('{0}:{1}'.format(minion, jid))
+    return Response(response=data, status=200, mimetype="application/json")
+
+@app.route('/jobs/<jid>/')
+def jobs(jid):
+    ret = list()
+    redis = Redis(connection_pool=redis_pool)
+    for minion in redis.keys('*:%s' % jid):
+        ret.append(minion.split(':')[0])
+    # TODO: this following line is just for the navigation-bar highlight. make
+    # it less ugly?
+    function = json.loads(redis.get('{0}:{1}'.format(ret[0], jid)))['fun']
+    try:
+        timestamp = time.strptime(jid, "%Y%m%d%H%M%S%f")
+        at_time = time.strftime('%Y-%m-%d, at %H:%M:%S', timestamp)
+    except Exception:
+        at_time = None
+    return render_template('jobs.html', minions=ret, time=at_time, function=function)
+
+@app.route('/jobs/')
+def jobsearch():
+    if request.args.get('jid', None):
+        return redirect(url_for('jobs', jid=request.args.get('jid')))
+    return render_template('jobform.html')
+
+@app.route('/history/<minion>/<function>/')
+def history(minion, function):
+    ret = list()
+    redis = Redis(connection_pool=redis_pool)
+    try:
+        for jid in redis.lrange('{0}:{1}'.format(minion, function), 0, -1):
+            timestamp = time.strptime(jid, "%Y%m%d%H%M%S%f")
+            ret.append((jid, time.strftime('%Y-%m-%d, at %H:%M:%S', timestamp)))
+    except Exception:
+        pass
+    return render_template('history.html', jids=ret)
+
+@app.route('/history/')
+def historysearch():
+    if request.args.get('minion', None) and request.args.get('function', None):
+        return redirect(url_for('history', minion=request.args.get('minion'), function=request.args.get('function')))
+    return render_template('historyform.html')
+
+@app.route('/functions/<function>/')
+def functions(function):
+    functions = list()
+    times_list = list()
+    redis = Redis(connection_pool=redis_pool)
+    for minion in redis.sort('minions', alpha=True):
+        try:
+            jid = redis.lindex('{0}:{1}'.format(minion, function), 0)
+            times_run = redis.llen('{0}:{1}'.format(minion, function))
+            if times_run > 0:
+                times_list.append(times_run)
+            timestamp = time.strptime(jid, "%Y%m%d%H%M%S%f")
+            functions.append((minion, jid, time.strftime('%Y-%m-%d, at %H:%M:%S', timestamp)))
+        except Exception:
+            continue
+    return render_template('functions.html', functions=functions, average_run=float(sum(times_list))/len(times_list) if len(times_list) > 0 else 0)
+
+@app.route('/functions/')
+def functionsearch():
+    if request.args.get('function', None):
+        return redirect(url_for('functions', function=request.args.get('function')))
+    return render_template('functionform.html')
+
+@app.route('/')
+def index():
+    #return functions(app.config['DEFAULT_FUNCTION']) # work around for mitsuhiko/werkzeug#382, if needed
+    return redirect(url_for('functions', function=request.args.get('function', app.config['DEFAULT_FUNCTION'])))
---
+++ packages/saltobserver/saltobserver-0.9.1.tar.gz/saltobserver-0.9.1/setup.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+import codecs
+from os import path
+
+from setuptools import setup
+
+with open('README.rst', 'rt') as f:
+    long_description = f.read()
+
+setup(
+    name='saltobserver',
+    version='0.9.1',
+    description='A simple webapp for presenting data as offered by SaltStack\'s Redis Returner',
+    long_description=long_description,
+    url='https://github.com/analogbyte/saltobserver',
+    author='Daniel Nägele',
+    author_email='saltobserver@danieln.de',
+    license='MIT',
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Framework :: Flask',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+    ],
+    keywords='saltstack redis returner salt states',
+    packages=['saltobserver'],
+    package_data={
+        'saltobserver': [
+            'templates/*.html',
+            'static/style.css',
+            ]
+        },
+    install_requires=['flask', 'flask_sockets', 'gunicorn', 'redis'],
+    scripts = ['scripts/run_saltobserver'],
+)
