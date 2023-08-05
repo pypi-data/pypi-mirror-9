4392758c52f885963a8a1f549aa4281bc316b3c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:32 +0000

    Add package SimpleAciUiLogServer/1.1.1/SimpleAciUiLogServer-1.1.1.tar.gz

---
+++ packages/SimpleAciUiLogServer/SimpleAciUiLogServer-1.1.1.tar.gz/SimpleAciUiLogServer-1.1.1/setup.py
@@ -0,0 +1,42 @@
+import os
+
+from setuptools import setup, find_packages
+
+with open('LICENSE.txt') as f:
+    lic = f.read()
+
+__version__ = ""
+exec(open(os.path.join('SimpleAciUiLogServer', 'version.py')).read())
+
+PKGNAME = 'SimpleAciUiLogServer'
+URL = 'https://github.com/datacenter/' + PKGNAME
+DOWNLOADURL = URL + '/releases/tag/' + str(__version__)
+
+setup(
+    name=PKGNAME,
+    version=__version__,
+    description='A remote API Inspector written in Python',
+    long_description=open('README.rst').read(),
+    packages=find_packages(),
+    url=URL,
+    download_url = DOWNLOADURL,
+    license=lic,
+    author='Mike Timm',
+    author_email='mtimm@cisco.com',
+    zip_safe=False,
+    classifiers=(
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+    ),
+    scripts=[os.path.join('SimpleAciUiLogServer', 'SimpleAciUiLogServer.py')],
+    entry_points={
+        "console_scripts": [
+            "acilogserv=SimpleAciUiLogServer:main",
+            "SimpleAciUiLogServer=SimpleAciUiLogServer:main"
+        ],
+    },
+)
