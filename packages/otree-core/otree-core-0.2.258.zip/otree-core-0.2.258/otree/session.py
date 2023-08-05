5793dfb2a27149d8238343cfb1af6ecc0c242f62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:46 +0000

    Add package otree-core/0.2.258/otree-core-0.2.258.zip

---
+++ packages/otree-core/otree-core-0.2.258.zip/otree-core-0.2.258/otree/session.py
@@ -0,0 +1,231 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import re
+
+from django.conf import settings
+from django.db import transaction
+
+from otree import constants
+from otree.models.user import Experimenter
+from otree.models.session import Session, SessionExperimenter, Participant
+from otree.common_internal import (
+    get_session_module, get_models_module, get_app_constants,
+    min_players_multiple,
+)
+
+
+def gcd(a, b):
+    """Return greatest common divisor using Euclid's Algorithm."""
+    while b:
+        a, b = b, a % b
+    return a
+
+
+def lcm(a, b):
+    """Return lowest common multiple."""
+    return a * b // gcd(a, b)
+
+
+def lcmm(*args):
+    """Return lcm of args."""
+    return reduce(lcm, args)
+
+
+class SessionType(object):
+
+    def __init__(self, **kwargs):
+        """this code allows default values for these attributes to be set on
+        the class, that can then be overridden by SessionType instances.
+
+        sessions.py uses this.
+
+        """
+
+        attrs = [
+            'name',
+            'app_sequence',
+            'fixed_pay',
+            'num_bots',
+            'display_name',
+            'money_per_point',
+            'num_demo_participants',
+            'doc',
+            'vars',
+            'show_on_demo_page',
+            'group_by_arrival_time',
+        ]
+
+        for attr_name in attrs:
+            attr_value = kwargs.get(attr_name)
+            if attr_value is not None:
+                setattr(self, attr_name, attr_value)
+            if getattr(self, attr_name, None) is None:
+                if attr_name == 'doc':
+                    self.doc = ''
+                if attr_name == 'vars':
+                    self.vars = {}
+                else:
+                    msg = (
+                        'Required attribute SessionType.{} is missing or None'
+                    )
+                    raise AttributeError(msg.format(attr_name))
+
+        if not re.match(r'^\w+$', self.name):
+            msg = (
+                'Session "{}": name must be alphanumeric with no '
+                'spaces (underscores allowed).'
+            )
+            raise ValueError(msg.format(self.name))
+
+        if len(self.app_sequence) != len(set(self.app_sequence)):
+            raise ValueError('app_sequence cannot contain duplicate elements')
+
+        if len(self.app_sequence) == 0:
+            raise ValueError('Need at least one subsession.')
+
+        self.doc = self.doc.strip()
+
+    def __repr__(self):
+        mem = hex(id(self))
+        return "<SessionType '{}' at {}>".format(self.name, mem)
+
+    def lcm(self):
+        min_multiple_list = []
+        for app_name in self.app_sequence:
+            app_constants = get_app_constants(app_name)
+            # if players_per_group is None, 0, etc.
+            min_multiple = min_players_multiple(
+                app_constants.players_per_group
+            )
+            min_multiple_list.append(min_multiple)
+        return lcmm(*min_multiple_list)
+
+
+# =============================================================================
+# FUNCTIONS
+# =============================================================================
+
+def get_session_types_list(demo_only=False):
+    session_types = get_session_module().session_types()
+    if demo_only:
+        return [
+            session_type for session_type in session_types
+            if session_type.show_on_demo_page
+        ]
+    else:
+        return session_types
+
+
+def get_session_types_dict(demo_only=False):
+    return {
+        session_type.name: session_type
+        for session_type in get_session_types_list(demo_only)
+    }
+
+
+@transaction.atomic
+def create_session(session_type_name, label='', num_participants=None,
+                   special_category=None, _pre_create_id=None):
+
+    # 2014-5-2: i could implement this by overriding the __init__ on the
+    # Session model, but I don't really know how that works, and it seems to
+    # be a bit discouraged: http://goo.gl/dEXZpv
+    # 2014-9-22: preassign to groups for demo mode.
+
+    try:
+        session_type = get_session_types_dict()[session_type_name]
+    except KeyError:
+        msg = 'Session type "{}" not found in sessions.py'
+        raise ValueError(
+            msg.format(session_type_name)
+        )
+    session = Session.objects.create(
+        session_type_name=session_type.name,
+        label=label, fixed_pay=session_type.fixed_pay,
+        special_category=special_category,
+        money_per_point=session_type.money_per_point,
+        session_experimenter=SessionExperimenter.objects.create(),
+        _pre_create_id=_pre_create_id,
+    )
+
+    def bulk_create(model, descriptions):
+        model.objects.bulk_create([
+            model(session=session, **description)
+            for description in descriptions
+        ])
+        return model.objects.filter(session=session).order_by('pk')
+
+    if num_participants is None:
+        if special_category == constants.session_special_category_demo:
+            num_participants = session_type.num_demo_participants
+        elif special_category == constants.session_special_category_bots:
+            num_participants = session_type.num_bots
+
+    # check that it divides evenly
+    session_lcm = session_type.lcm()
+    if num_participants % session_lcm:
+        msg = (
+            'SessionType {}: Number of participants ({}) does not divide '
+            'evenly into group size ({})'
+        ).format(session_type.name, num_participants, session_lcm)
+        raise ValueError(msg)
+
+    participants = bulk_create(
+        Participant,
+        [{'id_in_session': i} for i in range(1, num_participants + 1)]
+    )
+
+    subsessions = []
+    for app_name in session_type.app_sequence:
+        if app_name not in settings.INSTALLED_OTREE_APPS:
+            msg = ("Your session contains a subsession app named '{}'. "
+                   "You need to add this to INSTALLED_OTREE_APPS "
+                   "in settings.py.")
+            raise ValueError(msg.format(app_name))
+
+        models_module = get_models_module(app_name)
+        app_constants = get_app_constants(app_name)
+
+        round_numbers = range(1, app_constants.num_rounds + 1)
+
+        subs = bulk_create(models_module.Subsession, [
+            {'round_number': round_number}
+            for round_number in round_numbers
+        ])
+
+        # Create players
+        models_module.Player.objects.bulk_create([
+            models_module.Player(
+                session=session,
+                subsession=subsession,
+                round_number=round_number,
+                participant=participant
+            )
+            for round_number, subsession in zip(round_numbers, subs)
+            for participant in participants
+        ])
+
+        subsessions.extend(subs)
+
+    # Create experimenters and bind subsessions to them
+    experimenters = bulk_create(Experimenter, [
+        {'subsession': subsession}
+        for subsession in subsessions
+    ])
+    sub_by_pk = {s.pk: s for s in subsessions}
+    for experimenter in experimenters:
+        idpk = experimenter.subsession_object_id
+        sub_by_pk[idpk]._experimenter = experimenter
+        sub_by_pk[idpk].save()
+
+    session._create_groups_and_initialize()
+
+    session.build_session_user_to_user_lookups()
+    session.ready = True
+    session.save()
+
+    return session
+
+
+default_app_config = 'otree.session.apps.OtreeSessionConfig'
---
+++ packages/otree-core/otree-core-0.2.258.zip/otree-core-0.2.258/otree/urls.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import inspect
+
+from django.conf import urls
+from django.utils.importlib import import_module
+
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.contrib import admin
+from django.views.generic.base import RedirectView
+from django.conf import settings
+
+import vanilla
+
+from otree.common_internal import get_models_module
+
+
+
+
+def url_patterns_from_module(module_name):
+    """automatically generates URLs for all Views in the module,
+    So that you don't need to enumerate them all in urlpatterns.
+    URLs take the form "gamename/ViewName".
+    See the method url_pattern() for more info
+
+    So call this function in your urls.py and pass it the names of all
+    Views modules as strings.
+
+    """
+
+    views_module = import_module(module_name)
+
+
+    all_views = [
+        ViewCls for _, ViewCls in inspect.getmembers(views_module)
+        if hasattr(ViewCls, 'url_pattern') and
+        inspect.getmodule(ViewCls) == views_module
+    ]
+
+    view_urls = []
+    for ViewCls in all_views:
+        if hasattr(ViewCls, 'url_name'):
+            view_urls.append(urls.url(ViewCls.url_pattern(), ViewCls.as_view(), name=ViewCls.url_name()))
+        else:
+            view_urls.append(urls.url(ViewCls.url_pattern(), ViewCls.as_view()))
+
+    return urls.patterns('', *view_urls)
+
+
+def augment_urlpatterns(urlpatterns):
+
+    urlpatterns += urls.patterns(
+        '',
+        urls.url(r'^$', RedirectView.as_view(url='/demo'), name='demo'),
+        #urls.url(r'^admin/', urls.include(admin.site.urls)),
+    )
+
+    urlpatterns += staticfiles_urlpatterns()
+
+    used_names_in_url = set()
+    for app_name in settings.INSTALLED_OTREE_APPS:
+        models_module = get_models_module(app_name)
+        name_in_url = models_module.Constants.name_in_url
+        if name_in_url in used_names_in_url:
+            msg = (
+                "App {} has name_in_url='{}', "
+                "which is already used by another app"
+            ).format(app_name, name_in_url)
+            raise ValueError(msg)
+
+        used_names_in_url.add(name_in_url)
+        views_module_name = '{}.views'.format(app_name)
+        utilities_module_name = '{}._builtin'.format(app_name)
+        urlpatterns += url_patterns_from_module(views_module_name)
+        urlpatterns += url_patterns_from_module(utilities_module_name)
+
+    urlpatterns += url_patterns_from_module('otree.views.concrete')
+    urlpatterns += url_patterns_from_module('otree.views.demo')
+    urlpatterns += url_patterns_from_module('otree.views.admin')
+
+    return urlpatterns
