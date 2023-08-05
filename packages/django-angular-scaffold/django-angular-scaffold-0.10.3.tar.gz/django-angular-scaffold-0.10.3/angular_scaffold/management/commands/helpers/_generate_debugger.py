ac01d195a05a4ddd2c2ebc9a94ddfc0689b7402b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:30 +0000

    Add package django-angular-scaffold/0.10.3/django-angular-scaffold-0.10.3.tar.gz

---
+++ packages/django-angular-scaffold/django-angular-scaffold-0.10.3.tar.gz/django-angular-scaffold-0.10.3/angular_scaffold/management/commands/helpers/_generate_debugger.py
@@ -0,0 +1,52 @@
+import os
+import hashlib
+
+def generate_debugger(directory, password):
+    m = hashlib.md5()
+    m.update(password)
+    password_hash = m.hexdigest()
+    logger_file = os.path.join(directory, 'assets', 'app', 'config', 'logger.js')
+    print "Creating: " + logger_file
+    with open(logger_file, 'w') as f:
+        f.write("""app.config(function ($logProvider) {
+    "use strict";
+    //Enables debug when ?debug=1&password=*password*
+    var password = "%s",
+        querystring = (window.location.search ? window.location.search.substring(1) :
+                window.location.hash.indexOf('?') !== -1 ? window.location.hash.split('?')[1] : ""),
+        params = {};
+    angular.forEach(querystring.split('&'), function (pair) {
+        params[pair.split('=')[0]] = pair.split('=')[1];
+    });
+    $logProvider.debugEnabled(false);
+    if (params.hasOwnProperty('debug') && params.hasOwnProperty('password')) {
+        if (params.debug && md5(params.password) === password) {
+            $logProvider.debugEnabled(true);
+            console.info("Logging Enabled");
+            console.log = function () {};
+        }
+    }
+});
+""" % password_hash)
+    _debugger_docs = """#Logging
+
+The logs of this project are using angular's `$logProvider` service. By default
+they are disabled, however, you can enable them in the browser by passing the proper credentials.
+
+##Enabling Logging
+
+To enable logging in the browser, pass the following query string on the end of your URL:
+
+```
+?debug=1&password=%s
+```
+
+##Using Logging
+
+Now that logging can be enabled and disabled for debugging, make sure you provide good,
+logging via `$log.debug(message)`. This functionally replaces `console.log()`.
+""" % password
+    if not os.path.exists(os.path.join(directory, 'docs')):
+        os.makedirs(os.path.join(directory, 'docs'))
+    with open(os.path.join(directory, 'docs', 'logging.md'), 'w') as f:
+        f.write(_debugger_docs)
