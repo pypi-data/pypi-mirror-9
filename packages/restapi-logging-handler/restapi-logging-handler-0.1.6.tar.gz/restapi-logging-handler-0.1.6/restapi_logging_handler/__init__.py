ae40bf4c8a59992bb2de5f15788aeea96091d451
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:46 +0000

    Add package restapi-logging-handler/0.1.6/restapi-logging-handler-0.1.6.tar.gz

---
+++ packages/restapi-logging-handler/restapi-logging-handler-0.1.6.tar.gz/restapi-logging-handler-0.1.6/restapi_logging_handler/__init__.py
@@ -0,0 +1,4 @@
+from __future__ import absolute_import
+
+from restapi_logging_handler.loggly_handler import LogglyHandler
+from restapi_logging_handler.restapi_logging_handler import RestApiHandler
---
+++ packages/restapi-logging-handler/restapi-logging-handler-0.1.6.tar.gz/restapi-logging-handler-0.1.6/setup.py
@@ -0,0 +1,28 @@
+import os
+from setuptools import setup
+
+
+try:
+    import pypandoc
+    description = pypandoc.convert('README.md', 'rst')
+except (IOError, ImportError):
+    description = open('README.md').read()
+
+setup(
+    name='restapi-logging-handler',
+    version='0.1.6',
+    description='A handler for the python logging module that allows \
+        sending logs through a REST-ful API. With Loggly support.',
+    long_description=description,
+    packages=['restapi_logging_handler'],
+    install_requires=['requests'],
+    author='RJ Gilligan',
+    author_email='rj.gilligan@nrgnergy.com',
+    url='https://github.com/narwhaljames/restapi-logging-handler.git',
+    download_url='https://github.com/narwhaljames/restapi-logging-handler/tarball/0.1.6',
+    keywords=['rest', 'api', 'logging', 'handler', 'loggly'],
+    classifiers=[],
+    license='MIT',
+    test_suite='nose.collector',
+    tests_require=['nose'],
+)
