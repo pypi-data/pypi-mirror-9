adbd8bd269fc94cfefb3252d6467ef876e3a0ce5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:10 +0000

    Add package django-pgcrypto-fields/0.6.3/django_pgcrypto_fields-0.6.3-py3-none-any.whl

---
+++ packages/django-pgcrypto-fields/django_pgcrypto_fields-0.6.3-py3-none-any.whl/pgcrypto_fields/__init__.py
@@ -0,0 +1,20 @@
+from django.conf import settings
+
+
+CAST_TO_TEXT = "nullif(%s, NULL)::text"
+DIGEST_SQL = "digest(%s, 'sha512')"
+HMAC_SQL = "hmac(%s, '{}', 'sha512')".format(settings.PGCRYPTO_KEY)
+
+INTEGER_PGP_PUB_ENCRYPT_SQL = "pgp_pub_encrypt({}, dearmor('{}'))".format(
+    CAST_TO_TEXT,
+    settings.PUBLIC_PGP_KEY,
+)
+INTEGER_PGP_SYM_ENCRYPT_SQL = "pgp_sym_encrypt({}, '{}')".format(
+    CAST_TO_TEXT,
+    settings.PGCRYPTO_KEY,
+)
+
+PGP_PUB_ENCRYPT_SQL = "pgp_pub_encrypt(%s, dearmor('{}'))".format(
+    settings.PUBLIC_PGP_KEY,
+)
+PGP_SYM_ENCRYPT_SQL = "pgp_sym_encrypt(%s, '{}')".format(settings.PGCRYPTO_KEY)
