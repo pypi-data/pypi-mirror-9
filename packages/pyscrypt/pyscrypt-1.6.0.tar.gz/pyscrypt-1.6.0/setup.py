27acf3d07ff8f5222dd5d0ec886477ce060323eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:02 +0000

    Add package pyscrypt/1.6.0/pyscrypt-1.6.0.tar.gz

---
+++ packages/pyscrypt/pyscrypt-1.6.0.tar.gz/pyscrypt-1.6.0/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+LONG_DESCRIPTION = '''A very simple, pure-Python implementation of the scrypt
+password-based key derivation function and scrypt file format library with no
+dependencies beyond standard Python libraries. See README.md for API
+reference and details.'''
+
+setup(name = 'pyscrypt',
+      version = '1.6.0',
+      description = 'Pure-Python Implementation of the scrypt password-based key derivation function and scrypt file format library',
+      long_description = LONG_DESCRIPTION,
+      author = 'Richard Moore',
+      author_email = 'pyscrypt@ricmoo.com',
+      url = 'https://github.com/ricmoo/pyscrypt',
+      packages = ['pyscrypt'],
+      classifiers = [
+          'Topic :: Security :: Cryptography',
+          'License :: OSI Approved :: MIT License',
+      ],
+      license = "License :: OSI Approved :: MIT License",
+     )
