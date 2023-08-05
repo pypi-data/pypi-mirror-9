9da43f529169e850f578a79b1a1ea4b42d41b63f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:30 +0000

    Add package boto-s3-shim/0.1.0/boto-s3-shim-0.1.0.tar.gz

---
+++ packages/boto-s3-shim/boto-s3-shim-0.1.0.tar.gz/boto-s3-shim-0.1.0/boto_s3_shim/__init__.py
@@ -0,0 +1,22 @@
+import os
+from urllib.parse import urlparse
+from boto.s3.connection import S3Connection
+from boto.s3.connection import OrdinaryCallingFormat
+
+def override():
+    override = os.environ.get('S3_OVERRIDE', False)
+    if override:
+        override_with_url(override)
+
+def override_with_url(url):
+    override_to = urlparse(str(override))
+
+    old_init = S3Connection.__init__
+    def new__init__(self, *k, **kwargs):
+        kwargs['host'] = override_to.hostname
+        kwargs['port'] = int(override_to.port)
+        kwargs['calling_format'] = OrdinaryCallingFormat()
+        kwargs['is_secure'] = override_to.scheme == 'secure'
+        old_init(self, *k, **kwargs)
+
+    S3Connection.__init__ = new__init__
---
+++ packages/boto-s3-shim/boto-s3-shim-0.1.0.tar.gz/boto-s3-shim-0.1.0/setup.py
@@ -0,0 +1,26 @@
+import os
+from setuptools import setup
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name="boto-s3-shim",
+    version="0.1.0",
+    author="Graham Christensen",
+    author_email="graham@grahamc.com",
+    description="Inject an alternative S3 server into any boto-based project",
+    long_description=read('README.rst'),
+    license="MIT",
+    keywords="boto s3 fakes3",
+    url="http://github.com/grahamc/boto_s3_shim",
+    packages=["boto_s3_shim"],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
