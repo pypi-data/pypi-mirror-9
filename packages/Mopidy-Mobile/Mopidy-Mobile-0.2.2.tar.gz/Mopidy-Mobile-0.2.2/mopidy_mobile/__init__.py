c1ff629f7331f8c180b2cefa3c229c05f877221d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:53 +0000

    Add package Mopidy-Mobile/0.2.2/Mopidy-Mobile-0.2.2.tar.gz

---
+++ packages/Mopidy-Mobile/Mopidy-Mobile-0.2.2.tar.gz/Mopidy-Mobile-0.2.2/mopidy_mobile/__init__.py
@@ -0,0 +1,26 @@
+from __future__ import unicode_literals
+
+from mopidy import config, ext
+
+__version__ = '0.2.2'
+
+
+class Extension(ext.Extension):
+
+    dist_name = 'Mopidy-Mobile'
+    ext_name = 'mobile'
+    version = __version__
+
+    def get_default_config(self):
+        from os import path
+        conf_file = path.join(path.dirname(__file__), 'ext.conf')
+        return config.read(conf_file)
+
+    def get_config_schema(self):
+        schema = super(Extension, self).get_config_schema()
+        schema['title'] = config.String()
+        return schema
+
+    def setup(self, registry):
+        from .app import factory
+        registry.add('http:app', {'name': 'mobile', 'factory': factory})
