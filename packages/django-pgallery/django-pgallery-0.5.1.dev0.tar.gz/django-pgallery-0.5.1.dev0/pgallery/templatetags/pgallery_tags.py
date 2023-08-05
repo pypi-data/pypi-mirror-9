113c6abbb22065585add39f8a8860a4639f2034d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:56 +0000

    Add package django-pgallery/0.5.1.dev0/django-pgallery-0.5.1.dev0.tar.gz

---
+++ packages/django-pgallery/django-pgallery-0.5.1.dev0.tar.gz/django-pgallery-0.5.1.dev0/pgallery/templatetags/pgallery_tags.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) Zbigniew Siciarz 2012-2015.
+
+from __future__ import unicode_literals
+
+from django import template
+
+from pgallery.models import Gallery, Photo
+
+register = template.Library()
+
+
+@register.assignment_tag
+def get_recent_galleries(count=3):
+    """
+    Returns most recent galleries.
+    """
+    return Gallery.objects.published().order_by('-shot_date')[:count]
+
+
+@register.assignment_tag
+def get_gallery_archive_dates():
+    """
+    Returns datetime objects for all months in which galleries were added.
+    """
+    return Gallery.objects.published().dates('shot_date', 'month', order='DESC')
+
+
+@register.assignment_tag
+def get_popular_tags(count=10):
+    """
+    Returns most popular tags.
+    """
+    return Photo.objects.popular_tags(count)
