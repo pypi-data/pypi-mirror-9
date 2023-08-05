4226d68166e18746a1c7d5d5e5700ad6bf726c6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:50 +0000

    Add package bread/1.5.3/bread-1.5.3.tar.gz

---
+++ packages/bread/bread-1.5.3.tar.gz/bread-1.5.3/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(name='bread',
+      version='1.5.3',
+      description='Binary format parsing made easier',
+      url='https://github.com/alexras/bread',
+      author='Alex Rasmussen',
+      author_email='alexras@acm.org',
+      classifiers = [
+          "Development Status :: 4 - Beta",
+          "Environment :: Other Environment",
+          "Intended Audience :: Developers",
+          "License :: OSI Approved :: MIT License",
+          "Natural Language :: English",
+          "Operating System :: OS Independent",
+          "Programming Language :: Python",
+          "Programming Language :: Python :: 2.7",
+          "Programming Language :: Python :: 3",
+          "Topic :: Software Development :: Libraries :: Python Modules"],
+      license='MIT',
+      packages=['bread'],
+      requires=['bitstring'],
+      install_requires=['bitstring'])
