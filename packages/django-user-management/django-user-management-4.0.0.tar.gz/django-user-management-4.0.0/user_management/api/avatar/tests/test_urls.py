2987ec6395119d0cd47c738f7b2794078a8fbc41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:55 +0000

    Add package django-user-management/4.0.0/django-user-management-4.0.0.tar.gz

---
+++ packages/django-user-management/django-user-management-4.0.0.tar.gz/django-user-management-4.0.0/user_management/api/avatar/tests/test_urls.py
@@ -0,0 +1,20 @@
+from incuna_test_utils.testcases.urls import URLTestCase
+
+from .. import views
+
+
+class TestURLs(URLTestCase):
+    """Ensure the urls work."""
+
+    def test_profile_avatar_url(self):
+        self.assert_url_matches_view(
+            view=views.ProfileAvatar,
+            expected_url='/profile/avatar',
+            url_name='user_management_api:profile_avatar')
+
+    def test_user_avatar_url(self):
+        self.assert_url_matches_view(
+            view=views.UserAvatar,
+            expected_url='/users/1/avatar',
+            url_name='user_management_api:user_avatar',
+            url_kwargs={'pk': 1})
