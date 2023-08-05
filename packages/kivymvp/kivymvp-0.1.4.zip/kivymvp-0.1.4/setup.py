3cac576768528664b86801ed54058c8185b675b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:01 +0000

    Add package kivymvp/0.1.4/kivymvp-0.1.4.zip

---
+++ packages/kivymvp/kivymvp-0.1.4.zip/kivymvp-0.1.4/setup.py
@@ -0,0 +1,16 @@
+# coding=utf8
+
+from setuptools import setup
+
+setup(
+    name="kivymvp",
+    version="0.1.4",
+    description="ui framework for kivy following the MVP pattern",
+    url="https://github.com/mkraemer67/kivymvp",
+    author="Martin Krämer",
+    author_email="mkraemer.de@gmail.com",
+    license="Apache",
+    packages=["kivymvp"],
+    include_package_data=True,
+    zip_safe=False
+)
