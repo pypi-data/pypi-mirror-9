c9683f77f6713b68b32bb33d0b7bdde2abc38870
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:16 +0000

    Add package sped/0.2.dev1/sped-0.2.dev1.tar.gz

---
+++ packages/sped/sped-0.2.dev1.tar.gz/sped-0.2.dev1/setup.py
@@ -0,0 +1,44 @@
+from setuptools import setup, find_packages
+from setuptools.command.test import test as test_command
+from sped import __version__
+
+
+class PyTest(test_command):
+    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
+
+    def initialize_options(self):
+        test_command.initialize_options(self)
+        self.pytest_args = []
+
+    def finalize_options(self):
+        test_command.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        import sys
+        import pytest
+        errno = pytest.main(self.pytest_args)
+        sys.exit(errno)
+
+
+setup(
+    name='sped',
+    packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
+    version=__version__,
+    description='Biblioteca para geração dos arquivos do Sistema Público de Escrituração Digital (SPED) para Python.',
+    author='Sergio Garcia',
+    author_email='sergio@ginx.com.br',
+    url='https://github.com/sped-br/python-sped',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+    ],
+    keywords='sped fiscal contábil contabilidade receita federal',
+    tests_require=['pytest'],
+    cmdclass={'test': PyTest},
+)
