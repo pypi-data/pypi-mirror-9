16d5a5a60ddcef2c4a7187e5b2d07970646ba0c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:38 +0000

    Add package nanpy/0.9.3/nanpy-0.9.3.tar.gz

---
+++ packages/nanpy/nanpy-0.9.3.tar.gz/nanpy-0.9.3/setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+classifiers = [
+# Get more strings from
+# http://pypi.python.org/pypi?%3Aaction=list_classifiers
+"License :: OSI Approved :: MIT License",
+"Natural Language :: English",
+"Operating System :: OS Independent",
+"Programming Language :: Python",
+"Programming Language :: Python :: 2",
+"Programming Language :: Python :: 2.6",
+"Programming Language :: Python :: 2.7",
+"Programming Language :: Python :: 3",
+"Programming Language :: Python :: 3.2",
+"Programming Language :: Python :: 3.3",
+"Programming Language :: Python :: 3.4",
+]
+
+setup(name="nanpy",
+      version="0.9.3",
+      description="Use your Arduino board with Python",
+      license="MIT",
+      author="Andrea Stagi",
+      author_email="stagi.andrea@gmail.com",
+      url="http://github.com/nanpy/nanpy",
+      packages = find_packages(),
+      keywords= "arduino library prototype",
+      install_requires=[
+        "pyserial",
+      ],
+      classifiers=classifiers,
+      zip_safe = True)
