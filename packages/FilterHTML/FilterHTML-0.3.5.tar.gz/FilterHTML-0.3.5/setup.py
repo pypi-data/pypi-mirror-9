26616180de1f2f522b0c8e9a98f185672851746f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:53 +0000

    Add package FilterHTML/0.3.5/FilterHTML-0.3.5.tar.gz

---
+++ packages/FilterHTML/FilterHTML-0.3.5.tar.gz/FilterHTML-0.3.5/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+   name='FilterHTML',
+   version='0.3.5',
+   py_modules=['FilterHTML'],
+   author='David Collien',
+   author_email='me@dcollien.com',
+   url='http://filterhtml.com',
+   license='MIT',
+   description='FilterHTML: A whitelisting HTML filter for Python and JavaScript',
+   long_description='FilterHTML: A whitelisting HTML filter. Allows only a simple to define (and easy to customise) subset of HTML to pass through, with attribute, class, and style parsing.',
+   platforms=['any'],
+)
