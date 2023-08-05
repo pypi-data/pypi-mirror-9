8596bdb2d511136e4d8a7969dd76a9a7ad82b206
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:18 +0000

    Add package popbill/1.0.2/popbill-1.0.2.tar.gz

---
+++ packages/popbill/popbill-1.0.2.tar.gz/popbill-1.0.2/README.rst
@@ -0,0 +1,35 @@
+####
+popbill.py
+####
+popbill 1.0.2
+
+팝빌 API for Python.
+
+.. image:: https://api.travis-ci.org/linkhub-sdk/popbill.py.svg?branch=master
+        :target: https://travis-ci.org/linkhub-sdk/popbill.py
+
+.. image:: https://pypip.in/d/popbill/badge.svg
+        :target: https://pypi.python.org/pypi/popbill
+
+* requirements
+ 
+  * Python 2.6+
+  * linkhub
+
+************
+Installation
+************
+
+Install via pip:
+
+::
+
+    $ pip install popbill
+
+Install from source:
+
+::
+
+    $ git clone git://github.com/linkhub-sdk/popbill.py.git
+    $ cd popbill.py
+    $ python setup.py install
---
+++ packages/popbill/popbill-1.0.2.tar.gz/popbill-1.0.2/setup.py
@@ -0,0 +1,42 @@
+from __future__ import print_function
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+import sys
+
+version = '1.0.2'
+
+if sys.version_info <= (2, 5):
+    error = "ERROR: popbill requires Python Version 2.6 or above...exiting."
+    print(error, file=sys.stderr)
+    sys.exit(1)
+
+setup(name = "popbill",
+      version = version,
+      description = "Popbill API SDK Library",
+      long_description = "Popbill API SDK. Consist of Taxinvice Service. http://www.popbill.com",
+      author = "Kim Seongjun",
+      author_email = "pallet027@gmail.com",
+      url = "https://github.com/linkhub-sdk/Popbill.py",
+      download_url = "https://github.com/linkhub-sdk/Popbill.py/archive/"+version+".tar.gz",
+      packages = ["popbill"],
+      install_requires=[
+          'linkhub',
+      ],
+      license = "MIT",
+      platforms = "Posix; MacOS X; Windows",
+      classifiers = ["Development Status :: 5 - Production/Stable",
+                     "Intended Audience :: Developers",
+                     "License :: OSI Approved :: MIT License",
+                     "Operating System :: OS Independent",
+                     "Topic :: Internet",
+                     "Programming Language :: Python :: 2",
+                     "Programming Language :: Python :: 2.6",
+                     "Programming Language :: Python :: 2.7",
+                     "Programming Language :: Python :: 3",
+                     "Programming Language :: Python :: 3.3",
+                     "Programming Language :: Python :: 3.4"]
+      )
