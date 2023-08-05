c93d6153acc8be65d15cc3be4e0d7254cd204523
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:43 +0000

    Add package Backgroundr/1.1.4/Backgroundr-1.1.4.tar.gz

---
+++ packages/Backgroundr/Backgroundr-1.1.4.tar.gz/Backgroundr-1.1.4/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Backgroundr",
+
+    keywords="Backgrounds Wallpapers Images",
+
+    version="1.1.4",
+
+    install_requires=[
+        "Pillow",
+    ],
+
+    packages=find_packages(),
+
+    package_data={
+        'fonts': ['backgroundr/fonts/*']
+    },
+
+    url="https://github.com/smg247/backgroundr",
+
+    author="Stephen Goeddel",
+
+    author_email="stephen@stephengoeddel.com"
+
+)
