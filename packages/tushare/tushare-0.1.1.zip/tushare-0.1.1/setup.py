e709826f29805712968a83515a3a2e3c24409a81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:03 +0000

    Add package tushare/0.1.1/tushare-0.1.1.zip

---
+++ packages/tushare/tushare-0.1.1.zip/tushare-0.1.1/setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+import codecs
+import os
+import sys
+
+def read(fname):
+    return codecs.open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name='tushare',
+    version='0.1.1',
+    description='TuShare is a utility for crawling historical data of China stocks',
+    long_description=read("README.md"),
+    author='Jimmy Liu',
+    author_email='jimmysoa@sina.cn',
+    license='BSD',
+    url='https://github.com/jimmysoa/tushare',
+    keywords='china stock data',
+    classifiers=['Development Status :: 4 - Beta',
+    'Programming Language :: Python :: 2.7',
+    'License :: OSI Approved :: BSD License'],
+    packages=['tushare','tushare.stock'],
+)
