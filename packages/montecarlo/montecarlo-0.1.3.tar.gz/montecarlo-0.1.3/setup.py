18215e7305cde21dce879b9310101b9eb30f986e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:17 +0000

    Add package montecarlo/0.1.3/montecarlo-0.1.3.tar.gz

---
+++ packages/montecarlo/montecarlo-0.1.3.tar.gz/montecarlo-0.1.3/setup.py
@@ -0,0 +1,19 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+with open('README.md') as file:
+    long_description = file.read()
+
+setup(
+  name = 'montecarlo',
+  packages = ['montecarlo'],
+  version = '0.1.3',
+  description = 'A small Python library for creating simple Monte Carlo simulations.',
+  long_description=long_description,
+  author = 'Christopher Su',
+  author_email = 'chris+gh@christopher.su',
+  url = 'https://github.com/csu/pymontecarlo',
+  download_url = 'https://github.com/csu/pymontecarlo/archive/master.zip',
+)
