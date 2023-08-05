54205055dda3632c6d7eaf1c60583ceb79e7c01e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:14 +0000

    Add package django-cached-field/1.2.11/django-cached-field-1.2.11.tar.gz

---
+++ packages/django-cached-field/django-cached-field-1.2.11.tar.gz/django-cached-field-1.2.11/README.rst
@@ -0,0 +1,177 @@
+Django Cached Field
+===================
+
+Introduction
+------------
+
+Using Django ORM and Celery, cache expensive-to-calculate attributes.
+
+Installation
+------------
+
+You can make things easy on yourself::
+
+    pip install django-cached-field
+
+Or, for a manual installation, you can clone the repo and install it
+using python and setup.py::
+
+    git clone git://github.com/aquameta/django-cached-field.git
+    cd django-cached-field/
+    python setup.py install
+
+Tested with django 1.3.1, celery 2.3.1, and django-celery 2.3.3, but I
+would entertain other minimums if someone was willing to test them.
+
+Configuration
+-------------
+
+Use of this library under at least version >= 1.6 of Django should not
+require any configuration changes; just import and use. For older
+Djangos, two settings changes are probably required for things to
+work: make sure it's a registered app, make sure celery sees its tasks
+file::
+
+   INSTALLED_APPS += ['django_cached_field',]
+   CELERY_IMPORTS += ['django_cached_field.tasks',]
+
+If you're going to have expiration dates on your values, and you want
+to use timezone-aware datetimes, you will need to set this setting to
+True::
+
+   CACHED_FIELD_USE_TIMEZONE = False  # default
+
+One setting for test environments: whether recalculation should happen
+when flagged as stale (default) or be left to the next time the
+attribute is accessed. This is useful for optimizing testing
+environments where you don't care that your cached values are invalid
+or that the expense of calculation is applied to a user. Note that, in
+this situation, you wouldn't need celery. ::
+
+   CACHED_FIELD_EAGER_RECALCULATION = True  # default
+
+This is a global option, so individual exceptions should instead be
+handled by passing the ``and_recalculate`` argument to the
+``flag_FIELD_as_stale`` call.
+
+Example
+-------
+
+Say you have a CTO who believes everything belongs in the database and
+a slow method on one of your models::
+
+    class Lamppost(models.Model):
+        # ...
+        @property
+        def slow_full_name(self):
+            ackermann(5, 2)
+            return 'The %s %s of %s' % (self.weight, self.first_name, self.country)
+
+Ugh; too slow. Let's cache that (but not with, say, a dedicated
+caching system). We'll want a few tools. `Celery
+<http://celeryproject.org/>` with `django-celery
+<http://github.com/ask/django-celery>` will need to be set up and
+humming along smoothly. Then we'll add in our cached field and rename
+our method appropriately::
+
+    from django_cached_field import CachedIntegerField
+
+    class Lamppost(models.Model):
+        # ...
+        slow_full_name = CachedTextField(null=True)
+
+        def calculate_slow_full_name(self):
+            ackermann(5, 2)
+            return 'The %s %s of %s' % (self.weight, self.first_name, self.country)
+
+(Yeah, ``calculate_*`` is just a convention. I clearly haven't given
+up the rails ghost, but you can pass in your own method name with
+the ``calculation_method_name`` arg to the field declaration.)
+
+Next, migrate your db schema to include the new cached field using
+south, or roll your own. Note that at least two fields will be added
+to this table, ``cached_slow_full_name`` of type *text*,
+``slow_full_name_recalculation_needed`` of type *boolean*, probably
+defaulting to true, and possibly ``slow_full_name_expires_after`` of
+type *datetime*, if we pass ``temporal_triggers=True`` into the field
+declaration (more on that later).
+
+Already that's kinda better. ``lamppost.slow_full_name`` may take a
+while the first time it gets called for a given record, but from then
+on, it'll be nigh instant. Of course, at this point, it will never
+change after that first call.
+
+The remaining important piece of the puzzle is to invalidate our cache
+using ``flag_slow_full_name_as_stale``. It is probably changed in some
+views.py (this example code could be more clever about noticing if the
+relevant values are updated)::
+
+    @render_to('lamppost/edit.html')
+    def edit(request, lamppost_id):
+        lamppost = Lamppost.objects.get(pk=lamppost_id)
+        if request.METHOD == 'POST':
+            form = LamppostForm(request.POST)
+            if form.is_valid():
+                form.save()
+                form.instance.flag_slow_full_name_as_stale()
+                return HttpResponseRedirect(
+                    reverse('lamppost_view', args=(lamppost.pk,)))
+        else:
+            form = LamppostForm()
+        return {'form': form, 'lamppost': lamppost}
+
+**This is the hardest part as the developer.** Caching requires you
+hunt down every place the value could be changed and calling that
+``flag_slow_full_name_as_stale`` method. Is country assigned a random
+new value every morning at cron'o'clock? That flag had best be stale
+by cron'o'one. Do you calculate weight based on the sum of all
+associated pigeons? Hook into the pigeons landing. And takeoff. And
+everything that changes an individual pigeon's weight. As Abraham
+Lincoln said, "There are only two hard problems in programming:
+naming, cache invalidation and off-by-one errors."
+
+One possible invalidation scheme you might want to use is expiration
+dates. We know the pigeons on our lamppost are going to die and turn
+into ghosts, right::
+
+    class Pigeon(models.Model):
+        death_day = models.DateField()
+
+        def die(self):
+            self.weight = 0
+            self.save()
+
+And rather than bother the pigeon-death-handling system, we'll take
+note of their death as they land::
+
+    class Lamppost(models.Model):
+        #...
+        def notice_pigeon_landing(self, pigeon):
+            earliest = self.pigeon_set.all().aggregate(
+                models.Min('death_date'))['death_date']
+            self.expire_slow_full_name_after(earliest)
+
+Or maybe you only want the cache to ever be valid for 30 minutes, lest
+**They** have too easy a time of tracking your thoughts. So, yeah, you
+get the idea.
+
+Caveats
+-------
+
+* Race condition if you flag a field as stale in a db transaction that takes longer to complete than the celery job takes to be called (so commit your transactions before invalidating the cache).
+* All ORM methods (e.g. ``order_by``, ``filter``) can only access this field through ``cached_FIELD``.
+* ``recalculate_FIELD`` uses ``.update(cached_FIELD=`` to set the value. Don't expect ``.save`` to be called.
+* ``flag_FIELD_as_stale`` uses ``.update``, as well.
+* This may break if you try to add this mixin to a field class that multiply-inherits (I'm currently grabbing an arbitrary, non-CachedFieldMixin class and making the real field with it).
+* The FIELD_recalculation_needed field is accessed by regex in at least one place, so problems will result from user fields that match the same pattern.
+
+TODO
+----
+
+* Figure out if we can turn temporal_triggers into a celery job that happens once at the given time.
+* All my tests are in the project I pulled this out of, but based on models therein. I don't have experience making tests for standalone django libraries. Someone wanna point me to a tutorial?
+* Recalculation task will not adapt to recalculation_needed_field_name option
+* Replace use of _recalculation_needed regex with class-level registry of cached fields.
+* Fix race condition with https://github.com/davehughes/django-transaction-signals ?
+* Or maybe with https://github.com/chrisdoble/django-celery-transactions ?
+* Make sure Django-1.7-style migrations still catch all the helper fields.
---
+++ packages/django-cached-field/django-cached-field-1.2.11.tar.gz/django-cached-field-1.2.11/django_cached_field/__init__.py
@@ -0,0 +1,261 @@
+from datetime import datetime, date
+from django.db import models
+from django.conf import settings
+from django.utils import timezone
+from django.utils.functional import curry
+from django_cached_field.tasks import offload_cache_recalculation
+
+if getattr(settings, 'CACHED_FIELD_USE_TIMEZONE', False):
+    now = timezone.now
+else:
+    now = datetime.now
+
+
+def _flag_FIELD_as_stale(self, field=None, and_recalculate=None, commit=True):
+    if and_recalculate is None:
+        and_recalculate = True
+        if hasattr(settings, 'CACHED_FIELD_EAGER_RECALCULATION'):
+            and_recalculate = settings.CACHED_FIELD_EAGER_RECALCULATION
+    already_flagged_for_recalculation = type(self).objects.filter(pk=self.pk).values_list(
+        field.recalculation_needed_field_name, flat=True)[0]
+    if already_flagged_for_recalculation:
+        kwargs = {}  # This won't trigger an actual UPDATE.
+    else:
+        # Invalidate the cache
+        setattr(self, field.recalculation_needed_field_name, True)
+        kwargs = {field.recalculation_needed_field_name: True}
+    if commit:
+        type(self).objects.filter(pk=self.pk).update(**kwargs)
+        if and_recalculate:
+            self.trigger_cache_recalculation()
+    return kwargs
+
+
+def _expire_FIELD_after(self, when=None, field=None):
+    if when is not None and not isinstance(when, datetime):
+        when = datetime.now().replace(
+            year=when.year, month=when.month, day=when.day,
+            hour=0, minute=0, second=0, microsecond=0)
+        if getattr(settings, 'CACHED_FIELD_USE_TIMEZONE', False):
+            when = when.replace(tzinfo=timezone.utc)
+    setattr(self, field.expiration_field_name, when)
+    type(self).objects.filter(pk=self.pk).update(**{field.expiration_field_name: when})
+
+
+def _recalculate_FIELD(self, field=None, commit=True):
+    if commit:
+        type(self).objects.filter(pk=self.pk).update(
+            **{field.recalculation_needed_field_name: False})
+    val = getattr(self, field.calculation_method_name)()
+    self._set_FIELD(val, field=field)
+    setattr(self, field.recalculation_needed_field_name, False)
+    kwargs = {field.cached_field_name: val}
+    if not commit:
+        kwargs[field.recalculation_needed_field_name] = False
+    if field.temporal_triggers:
+        expires = getattr(self, field.expiration_field_name)
+        if expires and expires < now():
+            setattr(self, field.expiration_field_name, None)
+            kwargs[field.expiration_field_name] = None
+    if commit:
+        type(self).objects.filter(pk=self.pk).update(**kwargs)
+    else:
+        return kwargs
+
+
+def _get_FIELD(self, field=None):
+    val = getattr(self, field.cached_field_name)
+    flag = getattr(self, field.recalculation_needed_field_name)
+    if field.temporal_triggers:
+        expiration = getattr(self, field.expiration_field_name)
+        flag = flag or (expiration is not None and expiration < now())
+    if flag is True:
+        self._recalculate_FIELD(field=field)
+        val = getattr(self, field.cached_field_name)
+    return val
+
+
+def _set_FIELD(self, val, field=None):
+    setattr(self, field.cached_field_name, val)
+
+
+def trigger_cache_recalculation(self):
+    obj_name = self._meta.object_name
+    if self._meta.proxy:
+        obj_name = self._meta.proxy_for_model._meta.object_name
+    offload_cache_recalculation.delay(
+        self._meta.app_label, obj_name, self.pk)
+
+
+def ensure_class_has_cached_field_methods(cls):
+    # :TODO: can this be done with a mixin?
+    for func in (trigger_cache_recalculation, _set_FIELD, _get_FIELD,
+                 _recalculate_FIELD, _flag_FIELD_as_stale,
+                 _expire_FIELD_after):
+        if not hasattr(cls, func.__name__):
+            setattr(cls, func.__name__, func)
+
+
+class CachedFieldMixin(object):
+    """Include this when you want to make a field that:
+      * accesses its value with @property FIELD
+      * stores its value on the db-level in cached_FIELD
+      * keeps a boolean on the db-level: FIELD_recalculation_needed
+      * sets above to True with flag_FIELD_as_stale()
+      * flag_FIELD_as_stale also queues a delayed
+        offload_cache_recalculation if and_recalculate is set to True
+        (the default).
+      * performs recalculation with recalculate_FIELD()
+      * accepts `commit' flag to recalculate_FIELD and
+        flag_FIELD_as_stale that, if false, prevents calling
+        .update or .trigger_cache_recalculation
+      * recalculates automatically if FIELD is accessed and
+        cached_FIELD is None or FIELD_recalculation_needed is True
+      * possibly recalculates automatically after a specified datetime
+      * calculates its value in a user-defined calculate_FIELD(), which
+        should return the value
+    Init args:
+      `calculation_method_name' to specify a method other than calculate_FIELD
+      `cached_field_name' to specify a field name other than cached_FIELD
+      `recalculation_needed_field_name' to specify a field name other than
+        FIELD_recalculation_needed
+      `temporal_triggers' to turn on expirations
+    """
+
+    def __init__(self, calculation_method_name=None, cached_field_name=None,
+                 recalculation_needed_field_name=None, temporal_triggers=False,
+                 db_index_on_temporal_trigger_field=False,
+                 db_index_on_recalculation_needed_field=False,
+                 expiration_field_name=None, *args, **kwargs):
+        self.temporal_triggers = temporal_triggers
+        self.db_index_on_temporal_trigger_field = db_index_on_temporal_trigger_field
+        self.db_index_on_recalculation_needed_field = db_index_on_recalculation_needed_field
+        if expiration_field_name:
+            self._expiration_field_name = expiration_field_name
+        if calculation_method_name:
+            self._calculation_method_name = calculation_method_name
+        if cached_field_name:
+            self._cached_field_name = cached_field_name
+        if recalculation_needed_field_name:
+            self._recalculation_needed_field_name = recalculation_needed_field_name
+        self.init_args_for_field = args
+        self.init_kwargs_for_field = kwargs
+
+    def contribute_to_class(self, cls, name):
+        ensure_class_has_cached_field_methods(cls)
+        self.name = name
+        setattr(cls,
+                'recalculate_{}'.format(self.name),
+                curry(cls._recalculate_FIELD, field=self))
+        setattr(cls,
+                self.name,
+                property(curry(cls._get_FIELD, field=self), curry(cls._set_FIELD, field=self)))
+
+        proper_field = (set(type(self).__bases__) - set((CachedFieldMixin,))).pop()  # :MC: ew.
+        proper_field = proper_field(*self.init_args_for_field, **self.init_kwargs_for_field)
+        setattr(cls, self.cached_field_name, proper_field)
+        proper_field.contribute_to_class(cls, self.cached_field_name)
+
+        flag_field = models.BooleanField(
+            default=True, db_index=self.db_index_on_recalculation_needed_field)
+        setattr(cls, self.recalculation_needed_field_name, flag_field)
+        flag_field.contribute_to_class(cls, self.recalculation_needed_field_name)
+
+        if self.temporal_triggers:
+            setattr(cls,
+                    'expire_{}_after'.format(self.name),
+                    curry(cls._expire_FIELD_after, field=self))
+            expire_field = models.DateTimeField(
+                null=True, db_index=self.db_index_on_temporal_trigger_field)
+            setattr(cls, self.expiration_field_name, expire_field)
+            expire_field.contribute_to_class(cls, self.expiration_field_name)
+
+        setattr(cls, 'flag_{}_as_stale'.format(self.name), curry(cls._flag_FIELD_as_stale, field=self))
+
+    @property
+    def cached_field_name(self):
+        return getattr(self, '_cached_field_name',
+                       'cached_{}'.format(self.name))
+
+    @property
+    def recalculation_needed_field_name(self):
+        return getattr(self, '_recalculation_needed_field_name',
+                       '{}_recalculation_needed'.format(self.name))
+
+    @property
+    def calculation_method_name(self):
+        return getattr(self, '_calculation_method_name',
+                       'calculate_{}'.format(self.name))
+
+    @property
+    def expiration_field_name(self):
+        return getattr(self, '_expiration_field_name',
+                       '{}_expires_after'.format(self.name))
+
+
+class CachedBigIntegerField(CachedFieldMixin, models.BigIntegerField):
+    pass
+
+
+class CachedBooleanField(CachedFieldMixin, models.BooleanField):
+    pass
+
+
+class CachedCharField(CachedFieldMixin, models.CharField):
+    pass
+
+
+class CachedDateField(CachedFieldMixin, models.DateField):
+    pass
+
+
+class CachedDateTimeField(CachedFieldMixin, models.DateTimeField):
+    pass
+
+
+class CachedDecimalField(CachedFieldMixin, models.DecimalField):
+    pass
+
+
+class CachedEmailField(CachedFieldMixin, models.EmailField):
+    pass
+
+
+class CachedFloatField(CachedFieldMixin, models.FloatField):
+    pass
+
+
+class CachedIntegerField(CachedFieldMixin, models.IntegerField):
+    pass
+
+
+class CachedIPAddressField(CachedFieldMixin, models.IPAddressField):
+    pass
+
+
+class CachedNullBooleanField(CachedFieldMixin, models.NullBooleanField):
+    pass
+
+
+class CachedPositiveIntegerField(CachedFieldMixin, models.PositiveIntegerField):
+    pass
+
+
+class CachedPositiveSmallIntegerField(CachedFieldMixin, models.PositiveSmallIntegerField):
+    pass
+
+
+class CachedSlugField(CachedFieldMixin, models.SlugField):
+    pass
+
+
+class CachedSmallIntegerField(CachedFieldMixin, models.SmallIntegerField):
+    pass
+
+
+class CachedTextField(CachedFieldMixin, models.TextField):
+    pass
+
+
+class CachedTimeField(CachedFieldMixin, models.TimeField):
+    pass
