0ec0de6e6faf29c6291f98a2b60ff119bba0464c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:11 +0000

    Add package pycrest/0.0.4/pycrest-0.0.4.zip

---
+++ packages/pycrest/pycrest-0.0.4.zip/pycrest-0.0.4/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+setup(
+    name='pycrest',
+    version='0.0.4',
+    packages=['pycrest'],
+    url='https://github.com/Dreae/PyCrest',
+    license='MIT License',
+    author='Dreae',
+    author_email='penitenttangentt@gmail.com',
+    description='Easy interface to the CREST API',
+    install_requires=['requests'],
+    test_suite='nose.collector',
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: Implementation :: PyPy",
+    ]
+)
