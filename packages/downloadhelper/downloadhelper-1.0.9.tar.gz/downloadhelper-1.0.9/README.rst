36f448cec91428417d6a1acd213d17d9729c2007
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:26 +0000

    Add package downloadhelper/1.0.9/downloadhelper-1.0.9.tar.gz

---
+++ packages/downloadhelper/downloadhelper-1.0.9.tar.gz/downloadhelper-1.0.9/README.rst
@@ -0,0 +1,26 @@
+# Downloadhelper
+```
+    Downloadhelper is an Apache2 Licensed HTTP Download library, 
+  written in Python, for human beings
+```
+
+```python
+  >>> from downloadhelper import download
+  >>> download('http://partner.myworld2015.org/media/chinese/HOWTO/MYWorld_HowTo_OnlineSurvey_Chinese.pdf', '我的世界.pdf')
+  ID:[1/1] Dir: .                                                 
+  File: 我的世界.pdf [ 379.82 KB ]
+  24.00KB [  6.32%  ] [Speed:  8.00KB/s Timeleft: 00:00:44] 
+  (388940, 388940, 0)
+```
+
+## Features
+--------------------
+* support: Resume broken downloads
+
+## Installation
+-------------------------
+```bash
+  $ pip install Downloadhelper
+  or
+  $ pip install git+https://gitcafe.com/Potter/downloadhelper.git
+```
---
+++ packages/downloadhelper/downloadhelper-1.0.9.tar.gz/downloadhelper-1.0.9/setup.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+# coding=utf-8
+import os
+import sys
+
+import downloadhelper
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    sys.exit()
+
+packages = [
+    'downloadhelper',
+]
+
+requires = [
+    'requests',
+]
+
+try:
+    with open('README.rst', 'r') as f:
+        readme = f.read()
+except :
+    readme = ''
+
+try:
+    with open('HISTORY.rst', 'r') as f:
+        history = f.read()
+except :
+    history = ''
+
+setup(
+    name='downloadhelper',
+    version=downloadhelper.VERSION,
+    description='Python Download Helper',
+    long_description=readme + '\n\n' + history,
+    author='Cole Smith',
+    author_email='uniquecolesmith@gmail.com',
+    url='',
+    packages=packages,
+    package_data={'': ['LICENSE', 'NOTICE'], 'downloadhelper': ['*.pem']},
+    package_dir={'downloadhelper': 'downloadhelper'},
+    include_package_data=True,
+    install_requires=requires,
+    license='Apache 2.0',
+    zip_safe=False,
+    classifiers=(
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4'
+    ),
+    extras_require={
+        'secuirity': ['pyOpenSSL', 'ndg-httpsclient', 'pyasn1'],
+    },
+    entry_points = {'console_scripts': ['downloadhelper = downloadhelper:main', 'downloadget = downloadhelper:get']}
+)
