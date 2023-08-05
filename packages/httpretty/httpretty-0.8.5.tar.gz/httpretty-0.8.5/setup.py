1948142f6e700b51ebc46514cd1582dd8862b300
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:17 +0000

    Add package httpretty/0.8.5/httpretty-0.8.5.tar.gz

---
+++ packages/httpretty/httpretty-0.8.5.tar.gz/httpretty-0.8.5/setup.py
@@ -0,0 +1,102 @@
+# #!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# <HTTPretty - HTTP client mock for Python>
+# Copyright (C) <2011>  Gabriel Falcao <gabriel@nacaolivre.org>
+#
+# Permission is hereby granted, free of charge, to any person
+# obtaining a copy of this software and associated documentation
+# files (the "Software"), to deal in the Software without
+# restriction, including without limitation the rights to use,
+# copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following
+# conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+# OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+# HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+# WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
+import ast
+import os
+import re
+from setuptools import setup, find_packages
+
+
+class VersionFinder(ast.NodeVisitor):
+
+    def __init__(self):
+        self.version = None
+
+    def visit_Assign(self, node):
+        if node.targets[0].id == '__version__':
+            self.version = node.value.s
+
+
+def read_version():
+    """Read version from httpretty/version.py without loading any files"""
+    finder = VersionFinder()
+    finder.visit(ast.parse(local_file('httpretty', '__init__.py')))
+    return finder.version
+
+
+def parse_requirements(path):
+    """Rudimentary parser for the `requirements.txt` file
+
+    We just want to separate regular packages from links to pass them to the
+    `install_requires` and `dependency_links` params of the `setup()`
+    function properly.
+    """
+    try:
+        requirements = map(str.strip, local_file(path).splitlines())
+    except IOError:
+        raise RuntimeError("Couldn't find the `requirements.txt' file :(")
+
+    links = []
+    pkgs = []
+    for req in requirements:
+        if not req:
+            continue
+        if 'http:' in req or 'https:' in req:
+            links.append(req)
+            name, version = re.findall("\#egg=([^\-]+)-(.+$)", req)[0]
+            pkgs.append('{0}=={1}'.format(name, version))
+        else:
+            pkgs.append(req)
+
+    return pkgs, links
+
+
+local_file = lambda *f: \
+    open(os.path.join(os.path.dirname(__file__), *f)).read()
+
+
+install_requires, dependency_links = \
+    parse_requirements('requirements.txt')
+
+
+setup(name='httpretty',
+    version=read_version(),
+    description='HTTP client mock for Python',
+    long_description=local_file('README.rst'),
+    author='Gabriel Falcao',
+    author_email='gabriel@nacaolivre.org',
+    url='http://github.com/gabrielfalcao/httpretty',
+    zip_safe=False,
+    packages=find_packages(exclude=['*tests*']),
+    tests_require=parse_requirements('test-requirements.txt'),
+    install_requires=install_requires,
+    dependency_links=dependency_links,
+    license='MIT',
+    test_suite='nose.collector',
+    classifiers=["Intended Audience :: Developers",
+                 "License :: OSI Approved :: MIT License",
+                 "Topic :: Software Development :: Testing"],
+)
