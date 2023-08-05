e63e627798a777980a3de8ffe2ab40425addc94b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:13 +0000

    Add package garland/0.2.0/garland-0.2.0-py2.py3-none-any.whl

---
+++ packages/garland/garland-0.2.0-py2.py3-none-any.whl/garland.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+
+__author__ = 'Ben Lopatin'
+__email__ = 'ben@wellfire.co'
+__version__ = '0.2.0'
+
+import sys
+import importlib
+
+if sys.version_info.major == 3:
+    from importlib import reload  # Common interface
+    from unittest.mock import patch
+else:
+    from mock import patch
+
+
+def mock_decorator(*a, **k):
+    """
+    An 'empty' decorator that returns the underlying function.
+
+    """
+    # This is a decorator without parameters, e.g.
+    #
+    # @login_required
+    # def some_view(request):
+    #     ...
+    #
+    if a:
+        if callable(a[0]):
+            def wrapper(*args, **kwargs):
+                return a[0](*args, **kwargs)
+            return wrapper
+
+    # This is a decorator with parameters, e.g.
+    #
+    # @render_template("index.html")
+    # def some_view(request):
+    #     ...
+    #
+    def real_decorator(function):
+        def wrapper(*args, **kwargs):
+            return function(*args, **kwargs)
+        return wrapper
+    return real_decorator
+
+
+def tinsel(to_patch, module_name, decorator=mock_decorator):
+    """
+    Decorator for simple in-place decorator mocking for tests
+
+    :param to_patch: string path of the function to patch
+    :param module_name: complete string path of the module to reload
+    :return:
+    """
+    def fn_decorator(function):
+        def wrapper(*args, **kwargs):
+            with patch(to_patch, decorator):
+                m = importlib.import_module(module_name)
+                reload(m)
+                function(*args, **kwargs)
+
+            reload(m)
+        return wrapper
+    return fn_decorator
+
+
