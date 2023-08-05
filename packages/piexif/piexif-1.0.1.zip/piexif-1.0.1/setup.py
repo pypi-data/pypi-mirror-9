c64d15719ad23bad8578d7453e465975cd4ce960
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:00 +0000

    Add package piexif/1.0.1/piexif-1.0.1.zip

---
+++ packages/piexif/piexif-1.0.1.zip/piexif-1.0.1/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup
+import sys
+
+import piexif
+
+
+sys.path.append('./piexif')
+sys.path.append('./tests')
+
+with open("README.rst", "r") as f:
+    description = f.read()
+
+setup(
+    name = "piexif",
+    version = piexif.VERSION,
+    author = "hMatoba",
+    author_email = "hiroaki.mtb@outlook.com",
+    description = "To simplify exif manipulations with python. " +
+                  "Writing, reading, and more...",
+    long_description = description,
+    license = "MIT",
+    keywords = ["exif", "jpeg"],
+    url = "https://github.com/hMatoba/Piexif",
+    packages = ['piexif'],
+    test_suite = 's_test.suite',
+    classifiers = [
+        "Development Status :: 5 - Production/Stable",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: Implementation :: IronPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "License :: OSI Approved :: MIT License",
+        "Topic :: Multimedia",
+        "Topic :: Printing",
+    ]
+)
