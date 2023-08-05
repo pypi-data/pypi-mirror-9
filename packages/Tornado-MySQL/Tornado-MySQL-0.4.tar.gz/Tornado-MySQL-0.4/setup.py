124e5e2f46ca45d9f35d8375716343514abdd1a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:25 +0000

    Add package Tornado-MySQL/0.4/Tornado-MySQL-0.4.tar.gz

---
+++ packages/Tornado-MySQL/Tornado-MySQL-0.4.tar.gz/Tornado-MySQL-0.4/setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+try:
+    with open('README.rst') as f:
+        readme = f.read()
+except IOError:
+    readme = ''
+
+setup(
+    name="Tornado-MySQL",
+    version="0.4",
+    url='https://github.com/PyMySQL/Tornado-MySQL',
+    author='INADA Naoki',
+    author_email='songofacandy@gmail.com',
+    description='Pure Python MySQL Driver for Tornado',
+    install_requires=['tornado>=4.0'],
+    long_description=readme,
+    license="MIT",
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Database',
+    ],
+)
