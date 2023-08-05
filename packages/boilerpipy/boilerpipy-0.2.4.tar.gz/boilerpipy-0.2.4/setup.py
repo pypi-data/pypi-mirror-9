621f623cf9dcdc51403dd8ba4c253603e3e628b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:01 +0000

    Add package boilerpipy/0.2.4/boilerpipy-0.2.4.tar.gz

---
+++ packages/boilerpipy/boilerpipy-0.2.4.tar.gz/boilerpipy-0.2.4/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+with open('README.rst') as file:
+    long_description = file.read()
+
+version = '0.2.4'
+
+setup(
+    name='boilerpipy',
+    version=version,
+    description='Readability/Boilerpipe extractor in Python',
+    author='Harshavardhana',
+    author_email='harsha@harshavardhana.net',
+    url='https://github.com/harshavardhana/boilerpipy.git',
+    license='Apache',
+    platforms=['any'],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    packages=find_packages(),
+    scripts=['bin/readability', 'bin/query'],
+    install_requires=['lxml', 'beautifulsoup4', 'urlparse2'],
+    long_description=long_description,
+)
