e654b6df844587e533b3cb21dcc96e10853e8505
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:49 +0000

    Add package jsonapi/0.6.3/jsonapi-0.6.3.tar.gz

---
+++ packages/jsonapi/jsonapi-0.6.3.tar.gz/jsonapi-0.6.3/jsonapi/resource.py
@@ -0,0 +1,374 @@
+""" Resource definition.
+
+There are two tipes of resources:
+    * simple resources
+    * model resources
+
+Simple resources require name Meta property to be defined.
+Example:
+    class SimpleResource(Resource):
+        class Meta:
+            name = "simple_name"
+
+Django model resources require model to be defined
+Example:
+    class ModelResource(Resource):
+        class Meta:
+            model = "myapp.mymodel"
+
+There are several optional Meta parameters:
+    * fieldnames_include = None
+    * fieldnames_exclude = None
+    * page_size = None
+    * allowed_methods = ('GET',)
+
+Properties:
+
+    * name_plural
+    * is_model
+    * is_inherited
+    * is_auth_user
+
+"""
+from . import six
+import ast
+import inspect
+import logging
+import django
+from django.core.paginator import Paginator
+from django.forms import ModelForm
+from django.db import models
+
+from .utils import classproperty
+from .django_utils import get_model_name, get_model_by_name
+from .serializers import Serializer
+from .auth import Authenticator
+from .request_parser import RequestParser
+
+__all__ = 'Resource',
+
+logger = logging.getLogger(__name__)
+
+
+class ResourceManager(object):
+
+    """ Resource utils functionality."""
+
+    @staticmethod
+    def get_concrete_model(model):
+        """ Get model defined in Meta.
+
+        :param str or django.db.models.Model model:
+        :return: model or None
+        :rtype django.db.models.Model or None:
+        :raise ValueError: model is not found or abstract
+
+        """
+        if not(inspect.isclass(model) and issubclass(model, models.Model)):
+            model = get_model_by_name(model)
+
+        return model
+
+    @staticmethod
+    def get_resource_name(meta):
+        """ Define resource name based on Meta information.
+
+        :param Resource.Meta meta: resource meta information
+        :return: name of resource
+        :rtype: str
+        :raises ValueError:
+
+        """
+        if meta.name is None and not meta.is_model:
+            msg = "Either name or model for resource.Meta shoud be provided"
+            raise ValueError(msg)
+
+        name = meta.name or get_model_name(
+            ResourceManager.get_concrete_model(meta.model))
+        return name
+
+
+def merge_metas(*metas):
+    """ Merge meta parameters.
+
+    next meta has priority over current, it will overwrite attributes.
+
+    :param class or None meta: class with properties.
+    :return class: merged meta.
+
+    """
+    metadict = {}
+    for meta in metas:
+        metadict.update(meta.__dict__)
+
+    metadict = {k: v for k, v in metadict.items() if not k.startswith('__')}
+    return type('Meta', (object, ), metadict)
+
+
+class ResourceMetaClass(type):
+
+    """ Metaclass for JSON:API resources.
+
+    .. versionadded:: 0.5.0
+
+    Meta.is_auth_user whether model is AUTH_USER or not
+    Meta.is_inherited whether model has parent or not.
+
+    Meta.is_model: whether resource based on model or not
+
+    NOTE: is_inherited is used for related fields queries. For fields it is only
+    parent model used (django.db.models.Model).
+
+    """
+
+    def __new__(mcs, name, bases, attrs):
+        cls = super(ResourceMetaClass, mcs).__new__(mcs, name, bases, attrs)
+        metas = [getattr(base, 'Meta', None) for base in bases]
+        metas.append(cls.Meta)
+        cls.Meta = merge_metas(*metas)
+
+        # NOTE: Resource.Meta should be defined before metaclass returns
+        # Resource.
+        if name == "Resource":
+            return cls
+
+        cls.Meta.is_model = bool(getattr(cls.Meta, 'model', False))
+        cls.Meta.name = ResourceManager.get_resource_name(cls.Meta)
+
+        if cls.Meta.is_model:
+            model = ResourceManager.get_concrete_model(cls.Meta.model)
+            cls.Meta.model = model
+            if model._meta.abstract:
+                raise ValueError(
+                    "Abstract model {} could not be resource".format(model))
+
+        return cls
+
+
+@six.add_metaclass(ResourceMetaClass)
+class Resource(Serializer, Authenticator):
+
+    """ Base JSON:API resource class."""
+
+    class Meta:
+        name = None
+        fieldnames_include = None
+        fieldnames_exclude = None
+        page_size = None
+        allowed_methods = 'GET',
+
+        @classproperty
+        def name_plural(cls):
+            return "{0}s".format(cls.name)
+
+    @classmethod
+    def get_queryset(cls, user=None, **kwargs):
+        """ Get objects queryset.
+
+        Method is used to generate objects queryset for resource operations.
+        It is aimed to:
+            * Filter objects based on user. Object could be in queryset only if
+            there is attribute-ForeignKey-ManyToMany path from current resource
+            to current auth_user.
+            * Select related objects (or prefetch them) based on requested
+            requested objects to include
+
+        NOTE: use user from parameters, it could be authenticated not with
+            session, so request.user might not work
+
+        """
+        queryset = cls.Meta.model.objects
+
+        if cls.Meta.authenticators:
+            model_info = cls.Meta.api.model_inspector.models[cls.Meta.model]
+            user_filter = models.Q()
+            for path in model_info.auth_user_paths:
+                querydict = {path: user} if path else {"id": user.id}
+                user_filter = user_filter | models.Q(**querydict)
+
+            queryset = queryset.filter(user_filter)
+
+        return queryset
+
+    @classmethod
+    def update_get_queryset(cls, queryset, **kwargs):
+        """ Update permission queryset for GET operations."""
+        return queryset
+
+    @classmethod
+    def update_post_queryset(cls, queryset, **kwargs):
+        """ Update permission queryset for POST operations."""
+        return queryset
+
+    @classmethod
+    def update_put_queryset(cls, queryset, **kwargs):
+        """ Update permission queryset for PUT operations."""
+        return queryset
+
+    @classmethod
+    def update_delete_queryset(cls, queryset, **kwargs):
+        """ Update permission queryset for delete operations."""
+        return queryset
+
+    @classmethod
+    def get_form(cls, fields=None):
+        """ Create Partial Form based on given fields.
+
+        :param list fields: list of field names.
+
+        """
+        meta_attributes = {"model": cls.Meta.model}
+        if django.VERSION[:2] >= (1, 6):
+            meta_attributes["fields"] = '__all__'
+
+        if fields is not None:
+            meta_attributes["fields"] = fields
+
+        Form = type('Form', (ModelForm,), {
+            "Meta": type('Meta', (object,), meta_attributes)
+        })
+        return Form
+
+    @classmethod
+    def get(cls, request=None, **kwargs):
+        """ Get resource http response.
+
+        :return str: resource
+
+        """
+        user = cls.authenticate(request)
+        queryset = cls.get_queryset(user=user, **kwargs)
+        queryargs = RequestParser.parse(
+            "&".join(["=".join(i) for i in request.GET.items()]))
+
+        # Filters
+        filters = queryargs.get("filters", {})
+        if kwargs.get('ids'):
+            filters["id__in"] = kwargs.get('ids')
+
+        queryset = queryset.filter(**filters)
+
+        # Sort
+        if 'sort' in kwargs:
+            queryset = queryset.order_by(*kwargs['sort'])
+
+        # Fields serialisation
+        # NOTE: currently filter only own fields
+        model_info = cls.Meta.api.model_inspector.models[cls.Meta.model]
+        fields_own = model_info.fields_own
+        if queryargs['fields']:
+            fieldnames = queryargs['fields']
+            fieldnames.append("id")  # add id to fieldset
+            fields_own = [f for f in fields_own if f.name in fieldnames]
+
+        objects = queryset
+        meta = {}
+        if cls.Meta.page_size is not None:
+            paginator = Paginator(queryset, cls.Meta.page_size)
+            page = int(queryargs.get('page') or 1)
+            meta["count"] = paginator.count
+            meta["num_pages"] = paginator.num_pages
+            meta["page_size"] = cls.Meta.page_size
+            meta["page"] = page
+            objects = paginator.page(page)
+
+            meta["page_next"] = objects.next_page_number() \
+                if objects.has_next() else None
+            meta["page_prev"] = objects.previous_page_number() \
+                if objects.has_previous() else None
+
+        fields_include = set(queryargs.get("include", []))
+        fields_to_one = [f for f in model_info.fields_to_one
+                         if f.name in fields_include]
+        fields_to_many = [f for f in model_info.fields_to_many
+                          if f.name in fields_include]
+
+
+        response = cls.dump_documents(
+            cls,
+            objects,
+            fields_own=fields_own,
+            fields_to_one=fields_to_one,
+            fields_to_many=fields_to_many
+        )
+        if meta:
+            response["meta"] = meta
+        return response
+
+    @classmethod
+    def post(cls, request=None, **kwargs):
+        jdata = request.body.decode('utf8')
+        data = ast.literal_eval(jdata)
+        items = data[cls.Meta.name_plural]
+        is_collection = isinstance(items, list)
+
+        if not is_collection:
+            items = [items]
+
+        objects = []
+        Form = cls.get_form()
+        for item in items:
+            form = Form(item)
+            objects.append(form.save())
+
+        model_info = cls.Meta.api.model_inspector.models[cls.Meta.model]
+        data = [
+            cls.dump_document(
+                m,
+                fields_own=model_info.fields_own,
+                fields_to_one=model_info.fields_to_one,
+            )
+            for m in objects
+        ]
+
+        if not is_collection:
+            data = data[0]
+
+        response = {cls.Meta.name_plural: data}
+        return response
+
+    @classmethod
+    def put(cls, request=None, **kwargs):
+        # TODO: check ids for elements.
+        # TODO: check form is valid for elements.
+        # TODO: check kwargs has ids.
+        jdata = request.body.decode('utf8')
+        data = ast.literal_eval(jdata)
+        items = data[cls.Meta.name_plural]
+        is_collection = isinstance(items, list)
+
+        if not is_collection:
+            items = [items]
+
+        objects_map = cls.Meta.model.objects.in_bulk(kwargs["ids"])
+
+        objects = []
+        Form = cls.get_form()
+        for item in items:
+            instance = objects_map[item["id"]]
+            form = Form(item, instance=instance)
+            objects.append(form.save())
+
+        model_info = cls.Meta.api.model_inspector.models[cls.Meta.model]
+        data = [
+            cls.dump_document(
+                m,
+                fields_own=model_info.fields_own,
+                fields_to_one=model_info.fields_to_one,
+            )
+            for m in objects
+        ]
+
+        if not is_collection:
+            data = data[0]
+
+        response = {cls.Meta.name_plural: data}
+        return response
+
+    @classmethod
+    def delete(cls, request=None, **kwargs):
+        # TODO: raise Error if there are no ids.
+        user = cls.authenticate(request)
+        queryset = cls.get_queryset(user=user, **kwargs)
+        queryset.filter(id__in=kwargs['ids']).delete()
+        return ""
---
+++ packages/jsonapi/jsonapi-0.6.3.tar.gz/jsonapi-0.6.3/tests/testapp/tests/test_api.py
@@ -0,0 +1,435 @@
+from django.test import TestCase
+from jsonapi.api import API
+from jsonapi.resource import Resource
+from mixer.backend.django import mixer
+import django
+import unittest
+import json
+
+from ..models import Author
+from ..urls import api
+
+
+class TestApi(TestCase):
+    # NOTE: if use django-admin command --top-level-directory, need to specify
+    # urls = 'testapp.url', otherwise api is created two times (different
+    # obbjects), tests are failed. After specification TEST_DISCOVER_TOP_LEVEL
+    # in settings, this line is not required.
+    # urls = 'testapp.urls'
+
+    def setUp(self):
+        self.api = API()
+
+    def test_resource_registration(self):
+        class TestResource(Resource):
+            class Meta:
+                name = 'test'
+
+        self.api.register(TestResource)
+        self.assertEqual(self.api.resource_map['test'], TestResource)
+
+    def test_resource_registration_decorator(self):
+        @self.api.register
+        class TestResource(Resource):
+            class Meta:
+                name = 'test'
+
+        self.assertEqual(self.api.resource_map['test'], TestResource)
+
+    def test_resource_registration_decorator_params(self):
+        @self.api.register()
+        class TestResource(Resource):
+            class Meta:
+                name = 'test'
+
+        self.assertEqual(self.api.resource_map['test'], TestResource)
+
+    def test_meta_params_add_during_registration(self):
+        @self.api.register(name='test', param='param')
+        class TestResource(Resource):
+            class Meta:
+                name = '_test'
+
+        self.assertEqual(self.api.resource_map['test'], TestResource)
+        self.assertEqual(TestResource.Meta.name, 'test')
+        self.assertEqual(TestResource.Meta.param, 'param')
+
+    def test_recource_api_reference(self):
+        class TestResource(Resource):
+            class Meta:
+                name = 'test'
+
+        self.assertFalse(hasattr(TestResource.Meta, 'api'))
+        self.api.register(TestResource)
+        self.assertTrue(TestResource.Meta.api is self.api)
+
+    def test_resource_name_collapse_same_name(self):
+        class NewsResource(Resource):
+            class Meta:
+                name = 'news'
+
+        class NewsOtherResource(Resource):
+            class Meta:
+                name = 'news'
+
+        self.api.register(NewsResource)
+        with self.assertRaises(ValueError):
+            # The same resource name
+            self.api.register(NewsOtherResource)
+
+        NewsOtherResource.Meta.name = "allowed"
+        with self.assertRaises(ValueError):
+            # The same resource name
+            self.api.register(NewsOtherResource, name='news')
+
+    def test_resource_name_collapse_with_plural(self):
+        class NewsResource(Resource):
+            class Meta:
+                name = 'news'
+
+        class NewResource(Resource):
+            class Meta:
+                name = 'new'
+
+        self.api.register(NewsResource)
+        with self.assertRaises(ValueError):
+            # NewResource.name_plural = NewsResource.name
+            self.api.register(NewResource)
+
+        NewResource.Meta.name = "allowed"
+        with self.assertRaises(ValueError):
+            # NewResource.name_plural = NewsResource.name
+            self.api.register(NewResource, name='new')
+
+    def test_resource_name_collapse_with_singular(self):
+        class NewsResource(Resource):
+            class Meta:
+                name = 'news'
+
+        class NewssResource(Resource):
+            class Meta:
+                name = 'newss'
+
+        self.api.register(NewsResource)
+        with self.assertRaises(ValueError):
+            # NewsResource.name_plural = NewssResource.name
+            self.api.register(NewssResource)
+
+        NewssResource.Meta.name = "allowed"
+        with self.assertRaises(ValueError):
+            # NewsResource.name_plural = NewssResource.name
+            self.api.register(NewssResource, name="newss")
+
+    def test_resource_name(self):
+        class AResource(Resource):
+            class Meta:
+                name = 'a'
+
+        self.api.register(AResource)
+        AResource.Meta.name = 'b'
+        self.assertEqual(AResource.Meta.name_plural, 'bs')
+        self.assertEqual(self.api.resource_map['b'], AResource)
+        self.assertNotIn('a', self.api.resource_map)
+
+    @unittest.skip("Not implemented")
+    def test_content_type_validation(self):
+        response = self.client.get('/api', content_type='application/json')
+        self.assertEqual(response.status_code, 415)
+        self.assertEqual(
+            str(response.content),
+            "Content-Type SHOULD be application/vnd.api+json")
+
+    @unittest.skipIf(django.VERSION[:2] == (1, 5),
+                     "FIXME: For some reason does not work. Tested manually")
+    def test_base_url(self):
+        self.client.get('/api', content_type='application/vnd.api+json')
+        self.assertEqual(api.base_url, "http://testserver")
+
+    @unittest.skipIf(django.VERSION[:2] == (1, 5),
+                     "FIXME: For some reason does not work. Tested manually")
+    def test_api_url(self):
+        self.client.get('/api', content_type='application/vnd.api+json')
+        self.assertEqual(api.api_url, "http://testserver/api")
+
+
+class TestApiClient(TestCase):
+    def test_resource_get_empty(self):
+        response = self.client.get(
+            '/api/author',
+            content_type='application/vnd.api+json'
+        )
+        self.assertEqual(response.status_code, 200)
+        data = json.loads(response.content.decode('utf8'))
+        data_expected = {
+            "authors": []
+        }
+        self.assertEqual(data, data_expected)
+
+    def test_resource_own_fields_serialization(self):
+        mixer.blend('testapp.author')
+        response = self.client.get(
+            '/api/author',
+            content_type='application/vnd.api+json'
+        )
+        self.assertEqual(response.status_code, 200)
+        data = json.loads(response.content.decode('utf8'))
+        self.assertEqual(len(data["authors"]), 1)
+
+    def test_create_model(self):
+        self.assertEqual(Author.objects.count(), 0)
+        # NOTE: send individual resource
+        response = self.client.post(
+            '/api/author',
+            {
+                "authors": {
+                    "name": "author"
+                },
+            },
+            content_type='application/vnd.api+json',
+            HTTP_ACCEPT='application/vnd.api+json'
+        )
+
+        self.assertEqual(Author.objects.count(), 1)
+        author = Author.objects.get()
+
+        expected_data = {
+            "authors": {
+                "id": author.id,
+                "name": author.name,
+            }
+        }
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(author.name, "author")
+        data = json.loads(response.content.decode("utf-8"))
+        self.assertEqual(data, expected_data)
+        self.assertTrue(response.has_header("Location"))
+
+        location = "http://testserver/api/author/{}".format(author.id)
+        self.assertEqual(response["Location"], location)
+
+    def test_create_models(self):
+        self.assertEqual(Author.objects.count(), 0)
+        # TODO: try to decrease number of queries
+        # NOTE: send resource collection
+        with self.assertNumQueries(3):
+            response = self.client.post(
+                '/api/author',
+                {
+                    "authors": [
+                        {"name": "author1"},
+                        {"name": "author2"},
+                        {"name": "author3"},
+                    ]
+                },
+                content_type='application/vnd.api+json',
+                HTTP_ACCEPT='application/vnd.api+json'
+            )
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(Author.objects.count(), 3)
+        authors = Author.objects.all()
+        authors_names = Author.objects.values_list('name', flat=True)
+        self.assertEqual(set(authors_names), {"author1", "author2", "author3"})
+
+        expected_data = {
+            "authors": [{
+                "id": author.id,
+                "name": author.name,
+            } for author in authors]
+        }
+
+        data = json.loads(response.content.decode("utf-8"))
+        self.assertEqual(data, expected_data)
+        self.assertTrue(response.has_header("Location"))
+
+        location = "http://testserver/api/author/1,2,3"
+        self.assertEqual(response["Location"], location)
+
+    def test_update_model(self):
+        author = mixer.blend("testapp.author", name="")
+        response = self.client.put(
+            '/api/author/{}'.format(author.id),
+            {
+                "authors": {
+                    "id": author.id,
+                    "name": "author",
+                },
+            },
+            content_type='application/vnd.api+json',
+            HTTP_ACCEPT='application/vnd.api+json'
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(Author.objects.count(), 1)
+        author = Author.objects.get()
+        self.assertEqual(author.name, "author")
+
+    def test_update_models(self):
+        authors = mixer.cycle(2).blend("testapp.author", name="")
+        response = self.client.put(
+            '/api/author/{}'.format(",".join([str(a.id) for a in authors])),
+            {
+                "authors": [{
+                    "id": a.id,
+                    "name": "author",
+                } for a in authors],
+            },
+            content_type='application/vnd.api+json',
+            HTTP_ACCEPT='application/vnd.api+json'
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(Author.objects.count(), 2)
+        for author in Author.objects.all():
+            self.assertEqual(author.name, "author")
+
+    def test_update_model_missing_ids(self):
+        mixer.blend("testapp.author")
+        response = self.client.put(
+            '/api/author',
+            {
+                "authors": {
+                    "name": "author"
+                },
+            },
+            content_type='application/vnd.api+json',
+            HTTP_ACCEPT='application/vnd.api+json'
+        )
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(
+            response.content.decode("utf-8"),
+            "Request SHOULD have resource ids")
+
+    def test_delete_model(self):
+        author = mixer.blend("testapp.author")
+        response = self.client.delete(
+            '/api/author/{}'.format(author.id),
+            content_type='application/vnd.api+json',
+            HTTP_ACCEPT='application/vnd.api+json'
+        )
+        self.assertEqual(response.status_code, 204)
+        self.assertEqual(Author.objects.count(), 0)
+
+    def test_delete_models(self):
+        authors = mixer.cycle(2).blend("testapp.author")
+        response = self.client.delete(
+            '/api/author/{}'.format(",".join([str(a.id) for a in authors])),
+            content_type='application/vnd.api+json',
+            HTTP_ACCEPT='application/vnd.api+json'
+        )
+        self.assertEqual(response.status_code, 204)
+        self.assertEqual(Author.objects.count(), 0)
+
+    def test_delete_model_missing_ids(self):
+        response = self.client.delete(
+            '/api/author',
+            content_type='application/vnd.api+json',
+            HTTP_ACCEPT='application/vnd.api+json'
+        )
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(
+            response.content.decode("utf-8"),
+            "Request SHOULD have resource ids")
+
+    def test_get_top_level_links(self):
+        post = mixer.blend("testapp.post")
+        response = self.client.get(
+            '/api/post',
+            content_type='application/vnd.api+json',
+            HTTP_ACCEPT='application/vnd.api+json'
+        )
+        self.assertEqual(response.status_code, 200)
+        expected_data = {
+            "posts": [{
+                "id": post.id,
+                "title": post.title,
+                "links": {
+                    "user": post.user_id,
+                    "author": post.author_id,
+                }
+            }],
+            "links": {
+                "posts.author": "http://testserver/api/author/{posts.author}",
+                "posts.user": "http://testserver/api/user/{posts.user}",
+            }
+        }
+
+        self.maxDiff = None
+        data = json.loads(response.content.decode("utf-8"))
+        self.assertEqual(data, expected_data)
+
+    def test_get_include(self):
+        post = mixer.blend("testapp.post")
+        response = self.client.get(
+            '/api/post?include=author',
+            content_type='application/vnd.api+json',
+            HTTP_ACCEPT='application/vnd.api+json'
+        )
+        data = json.loads(response.content.decode("utf-8"))["linked"]
+        expected_data = {
+            "authors": [{
+                "id": post.author_id,
+                "name": post.author.name
+            }]
+        }
+        self.assertEqual(data, expected_data)
+
+    def test_get_include_null(self):
+        """ Test include optional foreign key field."""
+        post1 = mixer.blend("testapp.post")
+        post2 = mixer.blend("testapp.post", user__username="username")
+        user = post2.user
+        response = self.client.get(
+            '/api/post?include=user',
+            content_type='application/vnd.api+json',
+            HTTP_ACCEPT='application/vnd.api+json'
+        )
+        self.assertEqual(response.status_code, 200)
+        data = json.loads(response.content.decode("utf-8"))
+        expected_data = {
+            "posts": [{
+                "id": post.id,
+                "title": post.title,
+                "links": {
+                    "author": post.author_id,
+                    "user": post.user_id,
+                }
+            } for post in [post1, post2]],
+            "linked": {
+                "users": [{
+                    "id": user.id,
+                    "date_joined": user.date_joined.isoformat(),
+                    "email": user.email,
+                    'first_name': user.first_name,
+                    'is_active': user.is_active,
+                    'is_staff': user.is_staff,
+                    'is_superuser': user.is_superuser,
+                    'last_login': user.last_login.isoformat(),
+                    'last_name': user.last_name,
+                    'password': user.password,
+                    'username': user.username,
+                }]
+            },
+            'links': {
+                'posts.author': 'http://testserver/api/author/{posts.author}',
+                'posts.user': 'http://testserver/api/user/{posts.user}'
+            },
+        }
+        self.maxDiff = None
+        self.assertEqual(data, expected_data)
+
+    def test_get_include_many(self):
+        comment = mixer.blend("testapp.comment")
+        response = self.client.get(
+            '/api/post?include=comments',
+            content_type='application/vnd.api+json',
+            HTTP_ACCEPT='application/vnd.api+json'
+        )
+        data = json.loads(response.content.decode("utf-8"))["linked"]
+        expected_data = {
+            "comments": [{
+                "id": comment.id,
+                "links": {
+                    "author": comment.author_id,
+                    "post": comment.post_id,
+                },
+            }]
+        }
+        self.assertEqual(data, expected_data)
