d98c845e0b65f494a6b53d691a7d2fefdc81eb9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:24 +0000

    Add package django-security/0.2.0b/django-security-0.2.0b.tar.gz

---
+++ packages/django-security/django-security-0.2.0b.tar.gz/django-security-0.2.0b/security/models.py
@@ -0,0 +1,75 @@
+# Copyright (c) 2011, SD Elements. See LICENSE.txt for details.
+
+from django.contrib.auth.models import User
+from django.db import models
+from django.utils import timezone
+
+class PasswordExpiry(models.Model):
+    """
+    Associate a password expiry date with a user. For now, this date is
+    effectively just a flag to tell us whether the user has ever changed
+    their password, used to force users to change their initial passwords
+    when they log in for the first time. Instances are created by
+    security.RequirePasswordChangeMiddleware.
+    """
+
+    user = models.ForeignKey(User, unique=True) # Not one-to-one because some
+                                                # users may never receive an
+                                                # expiry date.
+    password_expiry_date = models.DateTimeField(auto_now_add=True,
+                                                null=True,
+                                                help_text="The date and time "
+                                                          "when the user's "
+                                                          "password expires. If "
+                                                          "this is empty, the "
+                                                          "password never "
+                                                          "expires.")
+
+    def is_expired(self):
+        if self.password_expiry_date is None:
+            return False
+        else:
+            return self.password_expiry_date <= timezone.now()
+
+    def never_expire(self):
+        self.password_expiry_date = None
+        self.save()
+
+    class Meta:
+        verbose_name_plural = "PasswordExpiries"
+
+# http://www.w3.org/TR/CSP/#sample-violation-report
+class CspReport(models.Model):
+    """
+    Content Security Policy violation report object. Each report represents
+    a single alert raised by client browser in response to CSP received from
+    the server. 
+    
+    Each alert means the browser was unable to access a web resource
+    (image, CSS, frame, script) because server's policy prohibited it from accessing
+    it. These alerts should be reviewed on regular basis, as they will occur in
+    two cases: first, false positives where too
+    restrictive CSP is blocking legitimate website features and needs tuning. Second,
+    when real attacks were fired against the user and this raises a question how
+    the malicious code appeared on your website. 
+    
+    CSP reports are available in Django admin view. To be logged into databse,
+    CSP reports view needs to be configured properly. See csp_report_ 
+    view for more information. Content Security Policy can be switched
+    on for a web application using ContentSecurityPolicyMiddleware_ middleware.
+    """
+
+    # data from CSP report
+    document_uri = models.URLField(max_length=1000, help_text="The address of the protected resource, with any fragment component removed")
+    referrer = models.URLField(max_length=1000, help_text="The referrer attribute of the protected resource")
+    blocked_uri = models.URLField(max_length=1000, help_text="URI of the resource that was prevented from loading due to the policy violation, with any fragment component removed")
+    violated_directive = models.CharField(max_length=1000, help_text="The policy directive that was violated")
+    original_policy = models.TextField(null=True, max_length=1000, help_text="The original policy as received by the user-agent.")
+
+    # metadata
+    date_received = models.DateTimeField(auto_now_add=True, help_text="When this report was received")
+    sender_ip = models.GenericIPAddressField(help_text="IP of the browser sending this report")
+    user_agent = models.CharField(max_length=1000, help_text="User-Agent of reporting browser")
+
+    def __unicode__(self):
+        return 'CSP Report: {0} from {1}'.format(self.blocked_uri, self.document_uri)
---
+++ packages/django-security/django-security-0.2.0b.tar.gz/django-security-0.2.0b/security/views.py
@@ -0,0 +1,97 @@
+# Copyright (c) 2011, SD Elements. See LICENSE.txt for details.
+
+import json
+
+from django.http import HttpResponseForbidden, HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from security.models import CspReport
+
+import logging
+log = logging.getLogger(__name__)
+
+def require_ajax(view):
+    """
+    A view decorator which ensures that the request being proccessed
+    by view is an AJAX request. We return a 403 error if the request
+    is not an AJAX request.
+    """
+    def check_ajax(request, *args, **kwargs):
+        if request.is_ajax():
+            return view(request, *args, **kwargs)
+        else:
+            return HttpResponseForbidden()
+    return check_ajax
+
+@csrf_exempt
+def csp_report(request, csp_save=False, csp_log=True):
+    """
+    .. _csp_report:
+
+    Collect Content Security Policy reports from browsers. This view has
+    two optional keyword arguments:
+
+        ``csp_save``    if True, reports will be saved as ``CspReport`` objects
+                        in database; this table is registered with Django Admin,
+                        so they can be later viewed in admin console
+        ``csp_log``     if True, reports will be logged through Django logging
+                        facility under ``security`` class
+
+    By default only logging is enabled. To collect reports, this view needs to be added
+    to project's urls.py. Examples:
+
+    Default mode, only logger enable, no database logging:
+
+        ``url(r'^csp-report/$', security.views.csp_report),``
+
+    Logger and database enabled:
+
+        ``url(r'^csp-report/$', security.views.csp_report, kwargs={'csp_save':True,'csp_log':True}),``
+    """
+
+    # http://www.w3.org/TR/CSP/#sample-violation-report
+    if not request.method == 'POST':
+        log.debug('Unexpect CSP report method {0}'.format(request.method))
+        return HttpResponseForbidden()
+
+    if (not request.META.has_key('CONTENT_TYPE')) or request.META['CONTENT_TYPE'] != 'application/json':
+        log.debug('Missing CSP report Content-Type {0}'.format(request.META))
+        return HttpResponseForbidden()
+
+    try:
+        csp_dict = json.loads(request.body)
+    except:
+        log.debug('Cannot JSON decode CSP report {0}'.format(request.body))
+        return HttpResponseForbidden()
+
+    if not csp_dict.has_key('csp-report'):
+        log.debug('Invalid CSP report structure {0}'.format(csp_dict))
+        return HttpResponseForbidden()
+
+    report = csp_dict['csp-report']
+    reporting_ip = request.META['REMOTE_ADDR']
+    reporting_ua = request.META['HTTP_USER_AGENT']
+
+    # log message about received CSP violation to Django log
+    if csp_log:
+        log.warn('Content Security Policy violation: {0}, reporting IP {1}, user agent {2}'.format(report, reporting_ip, reporting_ua))
+
+    # save received CSP violation to database
+    if csp_save:
+        csp_report = CspReport(document_uri=report.get('document-uri'),
+                referrer=report.get('referrer'),
+                blocked_uri=report.get('blocked-uri'),
+                violated_directive=report.get('violated-directive'),
+                original_policy=report.get('original-policy'),
+                sender_ip=reporting_ip,
+                user_agent=reporting_ua)
+
+        csp_report.save()
+
+    # return 204 No Content to the client
+    # per http://www.w3.org/TR/CSP/#report-uri
+    # "Note: The user agent ignores the fetched resource"
+    resp = HttpResponse()
+    resp.status_code = 204
+
+    return resp
+
