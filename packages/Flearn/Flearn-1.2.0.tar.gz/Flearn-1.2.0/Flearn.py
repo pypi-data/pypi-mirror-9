0cd94064ff1cf5fcb0de3c2fa94c723824159717
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:14 +0000

    Add package Flearn/1.2.0/Flearn-1.2.0.tar.gz

---
+++ packages/Flearn/Flearn-1.2.0.tar.gz/Flearn-1.2.0/Flearn.py
@@ -0,0 +1,13 @@
+#conding=utf-8
+
+"""this is the 'new.py' module and it provides one function XXX"""
+
+def print_lol(the_list, level = 0):
+	"""this function take one xxxx"""
+	for each_item in the_list:
+		if isinstance(each_item, list):
+			print_lol(each_item, level + 1)
+		else:
+                        for tab_stop in range(level):
+                                print("\t", end = '')
+			print(each_item)
---
+++ packages/Flearn/Flearn-1.2.0.tar.gz/Flearn-1.2.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name          = 'Flearn',
+        version       = '1.2.0',
+        py_modules    = ['Flearn'],
+        author        = 'thebook',
+        author_email  = 'thebook@admin.com',
+        url           = 'http://www.thebook.com',
+        description   = 'a simple exam'
+        )
