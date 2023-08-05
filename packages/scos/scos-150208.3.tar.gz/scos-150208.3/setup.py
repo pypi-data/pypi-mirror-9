8cbd7d4b9d8c97eca77850f665477fd44c9120bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:20 +0000

    Add package scos/150208.3/scos-150208.3.tar.gz

---
+++ packages/scos/scos-150208.3.tar.gz/scos-150208.3/setup.py
@@ -0,0 +1,12 @@
+
+from distutils.core import setup
+
+setup(
+	name         = 'scos',
+	version      = '150208.3',
+	py_modules   = ['scos'],
+	author       = 'scostesu',
+	author_email = 'scostesu@gmail.com',
+	url          = 'agathering.org',
+	description  = 'formats the contents of embedded list to dokuwiki unordered list markup',
+	)
