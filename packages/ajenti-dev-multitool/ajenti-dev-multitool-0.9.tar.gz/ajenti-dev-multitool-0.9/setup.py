e73bbd2b910fef89a59d02a1a2b70bb6d9c9e096
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:57 +0000

    Add package ajenti-dev-multitool/0.9/ajenti-dev-multitool-0.9.tar.gz

---
+++ packages/ajenti-dev-multitool/ajenti-dev-multitool-0.9.tar.gz/ajenti-dev-multitool-0.9/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+from distutils.core import setup
+from setuptools import find_packages
+
+
+setup(
+    name='ajenti-dev-multitool',
+    version='0.9',
+    install_requires=[
+        'pyyaml',
+    ],
+    description='-',
+    author='Eugeny Pankov',
+    author_email='e@ajenti.org',
+    url='http://ajenti.org/',
+    scripts=['ajenti-dev-multitool'],
+)
