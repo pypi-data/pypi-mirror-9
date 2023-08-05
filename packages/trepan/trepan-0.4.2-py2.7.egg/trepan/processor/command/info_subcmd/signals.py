4632e94abe1c8fe66a57c5e23b73a627a07059e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:32 +0000

    Add package trepan/0.4.2/trepan-0.4.2-py2.7.egg

---
+++ packages/trepan/trepan-0.4.2-py2.7.egg/trepan/processor/command/info_subcmd/signals.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+#   Copyright (C) 2009, 2015 Rocky Bernstein <rocky@gnu.org>
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# Our local modules
+from trepan.processor.command import base_subcmd as Mbase_subcmd
+
+
+class InfoSignals(Mbase_subcmd.DebuggerSubcommand):
+    'What debugger does when program gets various signals'
+    min_abbrev = 3  # info sig
+    need_stack = False
+    short_help = 'What debugger does when program gets various signals'
+
+    def run(self, args):
+        self.debugger.sigmgr.info_signal(['signal'] + args)
+        return
+    pass
+
+if __name__ == '__main__':
+    from trepan.processor.command import mock, info as Minfo
+    d, cp = mock.dbg_setup()
+    i = Minfo.InfoCommand(cp)
+    sub = InfoSignals(i)
+    sub.run([])
+    pass
