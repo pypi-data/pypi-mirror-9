ba6c4419823d21e539879d87007c97c24ec89b3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:37 +0000

    Add package onespacemedia-cms/1.1.1/onespacemedia-cms-1.1.1.tar.gz

---
+++ packages/onespacemedia-cms/onespacemedia-cms-1.1.1.tar.gz/onespacemedia-cms-1.1.1/cms/forms.py
@@ -0,0 +1,115 @@
+"""Form widgets used by the CMS."""
+import re
+import string
+
+from django import forms
+from django.conf import settings
+from django.contrib.auth.forms import PasswordChangeForm, AdminPasswordChangeForm
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.core.exceptions import ValidationError
+from django.utils.safestring import mark_safe
+
+from cms import debug
+
+try:
+    import json
+except:
+    from django.utils import simplejson as json
+
+
+class HtmlWidget(forms.Textarea):
+
+    """A textarea that is converted into a TinyMCE editor."""
+
+    def __init__(self, *args, **kwargs):
+        """Initializes the HtmlWidget."""
+        self.richtext_settings = getattr(settings, "RICHTEXT_SETTINGS", {}).get(kwargs.pop("richtext_settings", "default"), {})
+        super(HtmlWidget, self).__init__(*args, **kwargs)
+
+    @debug.print_exc
+    def get_media(self):
+        """Returns the media used by the widget."""
+        assets = [
+            staticfiles_storage.url("cms/js/tiny_mce/tiny_mce.js"),
+            staticfiles_storage.url("cms/js/jquery.cms.js"),
+            staticfiles_storage.url("pages/js/jquery.cms.pages.js"),
+            staticfiles_storage.url("media/js/jquery.cms.media.js"),
+        ]
+        return forms.Media(js=assets)
+
+    media = property(
+        get_media,
+        doc = "The media used by the widget.",
+    )
+
+    def render(self, name, value, attrs=None):
+        """Renders the widget."""
+
+        # Add on the JS initializer.
+        attrs = attrs or {}
+        attrs['class'] = "tiny-mce"
+
+        # Get the standard widget.
+        html = super(HtmlWidget, self).render(name, value, attrs)
+
+        try:
+            element_id = attrs["id"]
+        except KeyError:
+            pass
+        else:
+            # Customize the config.
+            richtext_settings = self.richtext_settings.copy()
+            # Cache the asset URL.
+            if "content_css" in richtext_settings:
+                richtext_settings["content_css"] = staticfiles_storage.url(richtext_settings["content_css"])
+            # Add in the initializer.
+            settings_js = json.dumps(richtext_settings)
+            html += u'<script>django.jQuery("#{element_id}").cms("htmlWidget",{settings_js})</script>'.format(
+                element_id = element_id,
+                settings_js = settings_js,
+            )
+        # All done!
+        return mark_safe(html)
+
+
+# Checks a string against some rules
+def password_validation(password):
+
+    errors = []
+
+    if len(password) < 8:
+        errors.append('Your password needs to be at least 8 characters long.')
+
+    if password.lower() == password:
+        errors.append('Your password needs include at least 1 uppercase character.')
+
+    if password.upper() == password:
+        errors.append('Your password needs include at least 1 lowercase character.')
+
+    if not re.findall(r"[\d]", password):
+        errors.append('Your password needs include at least 1 number.')
+
+    if not re.findall(r"[{}]".format(re.escape(string.punctuation)), password):
+        errors.append('Your password needs include at least 1 special character.')
+
+    return errors
+
+
+class CMSPasswordChangeForm(PasswordChangeForm):
+    def clean_new_password1(self):
+        password = self.cleaned_data.get('new_password1')
+        if password_validation(password):
+            raise ValidationError([
+                ValidationError(error) for error in password_validation(password)
+            ])
+        return password
+
+
+class CMSAdminPasswordChangeForm(AdminPasswordChangeForm):
+    def clean_password1(self):
+        password = self.cleaned_data.get('password1')
+        if password_validation(password):
+            raise ValidationError([
+                ValidationError(error) for error in password_validation(password)
+            ])
+        return password
---
+++ packages/onespacemedia-cms/onespacemedia-cms-1.1.1.tar.gz/onespacemedia-cms-1.1.1/cms/middleware.py
@@ -0,0 +1,48 @@
+"""Custom middleware used by the pages application."""
+
+import re
+
+from django.conf import settings
+from django.template.response import SimpleTemplateResponse
+
+from cms.models import publication_manager, PublicationManagementError
+
+
+class PublicationMiddleware(object):
+
+    """Middleware that enables preview mode for admin users."""
+
+    def __init__(self):
+        """Initializes the PublicationMiddleware."""
+        self.exclude_urls = [
+            re.compile(url)
+            for url in
+            getattr(settings, "PUBLICATION_MIDDLEWARE_EXCLUDE_URLS", ())
+        ]
+
+    def process_request(self, request):
+        """Starts preview mode, if available."""
+        if not any(pattern.match(request.path_info[1:]) for pattern in self.exclude_urls):
+            # See if preview mode is requested.
+            try:
+                preview_mode = bool(int(request.GET.get("preview", 0)))
+            except ValueError:
+                preview_mode = False
+            # Only allow preview mode if the user is a logged in administrator.
+            preview_mode = preview_mode and request.user.is_authenticated() and request.user.is_staff and request.user.is_active
+            publication_manager.begin(not preview_mode)
+
+    def process_response(self, request, response):
+        """Cleans up after preview mode."""
+        # Render the response if we're in a block of publication management.
+        if publication_manager.select_published_active():
+            if isinstance(response, SimpleTemplateResponse):
+                response = response.render()
+        # Clean up all blocks.
+        while True:
+            try:
+                publication_manager.end()
+            except PublicationManagementError:
+                break
+        # Carry on as normal.
+        return response
