36fa93adfa1e1314c8900c0f3c661dcf7b97ff38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:22 +0000

    Add package MarkdownPP/1.1/MarkdownPP-1.1.tar.gz

---
+++ packages/MarkdownPP/MarkdownPP-1.1.tar.gz/MarkdownPP-1.1/MarkdownPP/Modules/Include.py
@@ -0,0 +1,70 @@
+# Copyright 2015 John Reese
+# Licensed under the MIT license
+
+from __future__ import absolute_import
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import re
+from os import path
+
+from MarkdownPP.Module import Module
+from MarkdownPP.Transform import Transform
+
+includere = re.compile("^!INCLUDE\s+(?:\"([^\"]+)\"|'([^']+)')\s*$")
+
+
+class Include(Module):
+    """
+    Module for recursively including the contents of other files into the
+    current document using a command like `!INCLUDE "path/to/filename"`.
+    Target paths can be absolute or relative to the file containing the command
+    """
+
+    # includes should happen before anything else
+    priority = 0
+
+    def transform(self, data):
+        transforms = []
+
+        linenum = 0
+        for line in data:
+            match = includere.search(line)
+            if match:
+                includedata = self.include(match)
+
+                transform = Transform(linenum=linenum, oper="swap",
+                                      data=includedata)
+                transforms.append(transform)
+
+            linenum += 1
+
+        return transforms
+
+    def include(self, match, pwd=""):
+        if match.group(1) is None:
+            filename = match.group(2)
+        else:
+            filename = match.group(1)
+
+        if not path.isabs(filename):
+            filename = path.join(pwd, filename)
+
+        if path.isfile(filename):
+            f = open(filename, "r")
+            data = f.readlines()
+            f.close()
+
+            # recursively include file data
+            linenum = 0
+            for line in data:
+                match = includere.search(line)
+                if match:
+                    dirname = path.dirname(filename)
+                    data[linenum:linenum+1] = self.include(match, dirname)
+
+                linenum += 1
+
+            return data
+
+        return []
---
+++ packages/MarkdownPP/MarkdownPP-1.1.tar.gz/MarkdownPP-1.1/setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+from os import path
+import shutil
+
+if path.isfile('README.md'):
+    shutil.copyfile('README.md', 'README')
+
+setup(name="MarkdownPP",
+      description="Markdown preprocessor",
+      version="1.1",
+      author="John Reese",
+      author_email="john@noswap.com",
+      url="https://github.com/jreese/markdown-pp",
+      classifiers=['License :: OSI Approved :: MIT License',
+                   'Operating System :: OS Independent',
+                   'Programming Language :: Python',
+                   'Programming Language :: Python :: 2.6',
+                   'Programming Language :: Python :: 2.7',
+                   'Programming Language :: Python :: 3',
+                   'Programming Language :: Python :: 3.2',
+                   'Programming Language :: Python :: 3.3',
+                   'Programming Language :: Python :: 3.4',
+                   'Topic :: Utilities',
+                   'Development Status :: 4 - Beta',
+                   ],
+      license='MIT License',
+      scripts=['bin/markdown-pp'],
+      packages=['MarkdownPP', 'MarkdownPP/Modules'],
+      )
