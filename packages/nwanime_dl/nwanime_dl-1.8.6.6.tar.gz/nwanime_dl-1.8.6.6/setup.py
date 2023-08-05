d0518e738ca8c73c7a6e550db7b33cd459cef29a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:23 +0000

    Add package nwanime_dl/1.8.6.6/nwanime_dl-1.8.6.6.tar.gz

---
+++ packages/nwanime_dl/nwanime_dl-1.8.6.6.tar.gz/nwanime_dl-1.8.6.6/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+import os
+try:
+    import pypandoc
+    description=pypandoc.convert('readme.md','rst')
+except:
+    description=''
+setup(name='nwanime_dl',
+      version='1.8.6.6',
+      description='downloads anime from nwanime',
+      long_description=description,
+      url='https://github.com/thekindlyone/nwanime-dl',
+      author='thekindlyone',
+      author_email='dodo.dodder@gmail.com',
+      license='GNU GPL v2',
+      packages=['nwanime_dl'],
+      install_requires=[
+          'beautifulsoup4',
+          'lxml',
+          'requests'   ],
+      scripts=['bin/nwanime-dl'],
+      classifiers=[
+          "Development Status :: 4 - Beta",
+          "Topic :: Utilities",
+          "License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)",
+          "Operating System :: POSIX :: Linux",
+          "Programming Language :: Python :: 2.7"          
+      ],
+      zip_safe=False)
