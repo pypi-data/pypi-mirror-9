790991e14749401818e99d8e82c99dff688bc0b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:55 +0000

    Add package constants_manager/0.0.1/constants_manager-0.0.1.tar.gz

---
+++ packages/constants_manager/constants_manager-0.0.1.tar.gz/constants_manager-0.0.1/constants_manager/constants_manager.py
@@ -0,0 +1,26 @@
+try:
+    import ConfigParser as configparser
+except ImportError:
+    import configparser
+import os
+import logging
+
+
+class ConstantsManager():
+    def __init__(self, config_file_name='constants.ini', constants_name='ENV'):
+        self.config_file_name = config_file_name
+        self.constants_name = constants_name
+
+    def __get_environment(self):
+        __env_default = 'DEFAULT'
+        __constants_name = self.constants_name
+        __env = os.environ[__constants_name] if __constants_name in os.environ else __env_default
+        return __env
+
+    def get(self, key):
+        config = configparser.RawConfigParser()
+        config.read(self.config_file_name)
+        __env = self.__get_environment()
+        __val = config.get(__env, key)
+        return __val
+
