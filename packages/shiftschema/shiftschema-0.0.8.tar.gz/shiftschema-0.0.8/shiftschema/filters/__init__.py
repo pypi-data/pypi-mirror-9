5a7d3773f0b65528461f2d285170696cd45ff9a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:58 +0000

    Add package shiftschema/0.0.8/shiftschema-0.0.8.tar.gz

---
+++ packages/shiftschema/shiftschema-0.0.8.tar.gz/shiftschema-0.0.8/shiftschema/filters/__init__.py
@@ -0,0 +1,18 @@
+from shiftschema.filters.abstract_filter import AbstractFilter
+from shiftschema.filters.digits import Digits
+from shiftschema.filters.strip import Strip
+from shiftschema.filters.lowercase import Lowercase
+from shiftschema.filters.uppercase import Uppercase
+
+
+# todo implement these filters:
+# boolean.py
+# camel_case_to_separator.py
+# datetime_format.py
+# html_entities.py
+# regex_replace.py
+# separator_to_camel_case.py
+# string.py
+# strip_html.py
+# strip_newlines.py
+# url_normalize.py
