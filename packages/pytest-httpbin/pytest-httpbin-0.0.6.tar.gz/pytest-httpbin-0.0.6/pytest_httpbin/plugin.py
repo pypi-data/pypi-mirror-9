34a424c9a96ec5a429a093572aa64199089b290b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:05:51 +0000

    Add package pytest-httpbin/0.0.6/pytest-httpbin-0.0.6.tar.gz

---
+++ packages/pytest-httpbin/pytest-httpbin-0.0.6.tar.gz/pytest-httpbin-0.0.6/pytest_httpbin/plugin.py
@@ -0,0 +1,41 @@
+from __future__ import absolute_import
+import pytest
+from httpbin import app as httpbin_app
+from . import serve, certs
+
+@pytest.fixture(scope='session')
+def httpbin(request):
+    server = serve.Server(application=httpbin_app)
+    server.start()
+    request.addfinalizer(server.stop)
+    return server
+
+
+@pytest.fixture(scope='session')
+def httpbin_secure(request):
+    server = serve.SecureServer(application=httpbin_app)
+    server.start()
+    request.addfinalizer(server.stop)
+    return server
+
+
+@pytest.fixture(scope='session', params=['http', 'https'])
+def httpbin_both(request, httpbin, httpbin_secure):
+    if request.param == 'http':
+        return httpbin
+    elif request.param == 'https':
+        return httpbin_secure
+
+
+@pytest.fixture(scope='class')
+def class_based_httpbin(request, httpbin):
+    request.cls.httpbin = httpbin
+
+@pytest.fixture(scope='class')
+def class_based_httpbin_secure(request, httpbin_secure):
+    request.cls.httpbin_secure = httpbin_secure
+
+
+@pytest.fixture(scope='function')
+def httpbin_ca_bundle(monkeypatch):
+    monkeypatch.setenv('REQUESTS_CA_BUNDLE', certs.where())
---
+++ packages/pytest-httpbin/pytest-httpbin-0.0.6.tar.gz/pytest-httpbin-0.0.6/setup.py
@@ -0,0 +1,61 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+import re
+
+with open("pytest_httpbin/version.py") as f:
+    code = compile(f.read(), "pytest_httpbin/version.py", 'exec')
+    exec(code)
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+# Get the long description from the relevant file
+with codecs.open(os.path.join(here, 'DESCRIPTION.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name="pytest-httpbin",
+
+    # There are various approaches to referencing the version. For a discussion,
+    # see http://packaging.python.org/en/latest/tutorial.html#version
+    version=__version__,
+
+    description="Easily test your HTTP library against a local copy of httpbin",
+    long_description=long_description,
+
+    # The project URL.
+    url='https://github.com/kevin1024/pytest-httpbin',
+
+    # Author details
+    author='Kevin McCarthy',
+    author_email='me@kevinmccarthy.org',
+
+    # Choose your license
+    license='MIT',
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Testing',
+        'Topic :: Software Development :: Libraries',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+    ],
+
+    # What does your project relate to?
+    keywords='pytest-httpbin testing pytest httpbin',
+    packages=find_packages(exclude=["contrib", "docs", "tests*"]),
+    include_package_data = True, # include files listed in MANIFEST.in
+    install_requires = ['Flask','decorator','httpbin','six'],
+
+    # the following makes a plugin available to pytest
+    entry_points = {
+        'pytest11': [
+            'httpbin = pytest_httpbin.plugin',
+        ]
+    },
+)
