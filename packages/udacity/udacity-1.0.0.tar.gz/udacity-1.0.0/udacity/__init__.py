fc07e6ce16ff447f072d57779a9b059459859ce1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:49 +0000

    Add package udacity/1.0.0/udacity-1.0.0.tar.gz

---
+++ packages/udacity/udacity-1.0.0.tar.gz/udacity-1.0.0/udacity/__init__.py
@@ -0,0 +1,7 @@
+__copyright__ = '(c) 2015 Ty-Lucas Kelley'
+__license__ = 'MIT License'
+__title__ = 'Udacity API'
+__all__ = ['catalog', 'user']
+
+from udacity.catalog import Catalog
+from udacity.user import User
