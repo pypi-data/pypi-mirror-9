fb9187d6c37ee128581e081751f1a3a955671f83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:00 +0000

    Add package djangocms-snippet/1.4/djangocms-snippet-1.4.tar.gz

---
+++ packages/djangocms-snippet/djangocms-snippet-1.4.tar.gz/djangocms-snippet-1.4/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import setup
+from djangocms_snippet import __version__
+
+CLASSIFIERS = [
+    'Development Status :: 5 - Production/Stable',
+    'Environment :: Web Environment',
+    'Framework :: Django',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: BSD License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Topic :: Communications',
+    'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    'Topic :: Internet :: WWW/HTTP :: Dynamic Content :: Message Boards',
+    'Programming Language :: Python :: 2.6',
+    'Programming Language :: Python :: 2.7',
+]
+
+setup(
+    name='djangocms-snippet',
+    version=__version__,
+    description='HTML snippet plugin for django CMS',
+    author='Divio AG',
+    author_email='info@divio.ch',
+    url='https://github.com/divio/djangocms-snippet',
+    packages=['djangocms_snippet', 'djangocms_snippet.migrations',
+              'djangocms_snippet.migrations_django'],
+    license='LICENSE.txt',
+    platforms=['OS Independent'],
+    classifiers=CLASSIFIERS,
+    long_description=open('README.md').read(),
+    include_package_data=True,
+    zip_safe=False
+)
