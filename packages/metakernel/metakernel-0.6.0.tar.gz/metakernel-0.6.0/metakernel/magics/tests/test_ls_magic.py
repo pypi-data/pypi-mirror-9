2190a259f581a49546ab61eb0e0f7c183c1ced68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:38 +0000

    Add package metakernel/0.6.0/metakernel-0.6.0.tar.gz

---
+++ packages/metakernel/metakernel-0.6.0.tar.gz/metakernel-0.6.0/metakernel/magics/tests/test_ls_magic.py
@@ -0,0 +1,17 @@
+
+import os
+from metakernel.tests.utils import (get_kernel, get_log_text,
+                                    clear_log_text)
+
+
+def test_ls_magic():
+    kernel = get_kernel()
+    kernel.do_execute("%ls /tmp")
+    text = get_log_text(kernel)
+    assert text.startswith("/tmp/"), text[:100]
+    clear_log_text(kernel)
+
+    kernel.do_execute("%ls /tmp --recursive")
+    text = get_log_text(kernel)
+    assert text.startswith("/tmp/"), text[:100]
+    clear_log_text(kernel)
