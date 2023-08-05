e730d93a118f4acda558e1d7ab977508fc1f8003
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:13 +0000

    Add package atomicpress/1.0.7/atomicpress-1.0.7.tar.gz

---
+++ packages/atomicpress/atomicpress-1.0.7.tar.gz/atomicpress-1.0.7/atomicpress/__init__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+atomicpress
+----------
+AtomicPress is a static blog generator for python developers that don't want the Wordpress security hassle.
+"""
+
+__title__ = 'atomicpress'
+__version__ = '1.0.7'
+__build__ = 107
+__author__ = 'Martin Sandström'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2014 Martin Sandström'
+
