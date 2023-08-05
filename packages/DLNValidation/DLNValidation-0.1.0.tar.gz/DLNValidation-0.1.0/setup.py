27b2bf483407bef32c4db9a0c0fc302dfda586dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:53 +0000

    Add package DLNValidation/0.1.0/DLNValidation-0.1.0.tar.gz

---
+++ packages/DLNValidation/DLNValidation-0.1.0.tar.gz/DLNValidation-0.1.0/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='DLNValidation',
+    version='0.1.0',
+    author='Mary Stufflebeam',
+    author_email='mary@postmates.com',
+    py_modules=['dl_number_validation'],
+    url='http://pypi.python.org/pypi/DLNValidation/',
+    license='LICENSE.txt',
+    description='Provides validation of the format of USA drivers license numbers',
+    long_description=open('README.txt').read(),
+    install_requires=[],
+)
