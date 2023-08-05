fa7b46cda46e67b09a9c9e9c06d7ec9dc4c9566e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:28 +0000

    Add package sendwithus/1.4.1/sendwithus-1.4.1.tar.gz

---
+++ packages/sendwithus/sendwithus-1.4.1.tar.gz/sendwithus-1.4.1/setup.py
@@ -0,0 +1,30 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+with open('README.md') as fp:
+    long_description = fp.read()
+
+setup(
+    name='sendwithus',
+    version='1.4.1',
+    author='sendwithus',
+    author_email='us@sendwithus.com',
+    packages=find_packages(),
+    scripts=[],
+    url='https://github.com/sendwithus/sendwithus_python',
+    license='LICENSE.txt',
+    description='Python API client for sendwithus.com',
+    long_description=long_description,
+    test_suite="sendwithus.test",
+    install_requires=[
+        "requests >= 1.1.0",
+        "six >= 1.9.0"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "License :: OSI Approved :: Apache Software License",
+        "Development Status :: 5 - Production/Stable",
+        "Topic :: Communications :: Email"
+    ]
+)
