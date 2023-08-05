ec9d7af9e275c359f63c993961ceacc01f418ac4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:25 +0000

    Add package uranium/0.0.30/uranium-0.0.30.tar.gz

---
+++ packages/uranium/uranium-0.0.30.tar.gz/uranium-0.0.30/uranium/compat.py
@@ -0,0 +1,17 @@
+"""
+A bunch of compat mappings.
+
+This helps catalog python 2 and 3 differences,
+so they can be removed if at some point in 2050.
+"""
+
+try:
+    from UserDict import DictMixin
+except ImportError:
+    from collections import MutableMapping as DictMixin
+
+
+try:
+    from UserDict import IterableUserDict as UserDict
+except ImportError:
+    from collections import UserDict
