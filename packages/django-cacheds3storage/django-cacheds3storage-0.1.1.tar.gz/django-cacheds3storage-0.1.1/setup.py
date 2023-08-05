f4c528afe41a5139f296f661b2af503bf86e032a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:18 +0000

    Add package django-cacheds3storage/0.1.1/django-cacheds3storage-0.1.1.tar.gz

---
+++ packages/django-cacheds3storage/django-cacheds3storage-0.1.1.tar.gz/django-cacheds3storage-0.1.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name="django-cacheds3storage",
+    version="0.1.1",
+    author="Anders Pearson",
+    author_email="anders@columbia.edu",
+    url="https://github.com/ccnmtl/django-cacheds3storage",
+    description="S3 cached storage",
+    long_description="s3 cached storage",
+    install_requires=[
+        "python-dateutil",
+        ],
+    scripts=[],
+    license="BSD",
+    platforms=["any"],
+    zip_safe=False,
+    packages=['cacheds3storage'],
+    include_package_data=True,
+    )
