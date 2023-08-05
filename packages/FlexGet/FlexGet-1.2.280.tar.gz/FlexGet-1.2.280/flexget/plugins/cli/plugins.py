5a8344126ff926c511fbb70b01c931faab1ffe36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:57 +0000

    Add package FlexGet/1.2.280/FlexGet-1.2.280.tar.gz

---
+++ packages/FlexGet/FlexGet-1.2.280.tar.gz/FlexGet-1.2.280/flexget/plugins/cli/plugins.py
@@ -0,0 +1,41 @@
+from __future__ import unicode_literals, division, absolute_import, print_function
+import logging
+
+from flexget import options
+from flexget.event import event
+from flexget.logger import console
+from flexget.plugin import get_plugins
+
+log = logging.getLogger('plugins')
+
+
+@event('manager.subcommand.plugins')
+def plugins_summary(manager, options):
+    console('-' * 79)
+    console('%-20s%-30s%s' % ('Name', 'Roles (priority)', 'Info'))
+    console('-' * 79)
+
+    # print the list
+    for plugin in sorted(get_plugins(phase=options.phase, group=options.group)):
+        # do not include test classes, unless in debug mode
+        if plugin.get('debug_plugin', False) and not options.debug:
+            continue
+        flags = []
+        if plugin.instance.__doc__:
+            flags.append('--doc')
+        if plugin.builtin:
+            flags.append('builtin')
+        if plugin.debug:
+            flags.append('debug')
+        handlers = plugin.phase_handlers
+        roles = ', '.join('%s(%s)' % (phase, handlers[phase].priority) for phase in handlers)
+        console('%-20s%-30s%s' % (plugin.name, roles, ', '.join(flags)))
+
+    console('-' * 79)
+
+
+@event('options.register')
+def register_parser_arguments():
+    plugins_subparser = options.register_command('plugins', plugins_summary, help='print registered plugin summaries')
+    plugins_subparser.add_argument('--group', help='show plugins belonging to this group')
+    plugins_subparser.add_argument('--phase', help='show plugins that act on this phase')
---
+++ packages/FlexGet/FlexGet-1.2.280.tar.gz/FlexGet-1.2.280/flexget/plugins/input/generate.py
@@ -0,0 +1,33 @@
+from __future__ import unicode_literals, division, absolute_import
+import logging
+
+from flexget import plugin
+from flexget.event import event
+from flexget.entry import Entry
+from flexget import validator
+
+log = logging.getLogger(__name__.rsplit('.')[-1])
+
+
+class Generate(object):
+    """Generates n number of random entries. Used for debugging purposes."""
+
+    schema = {'type': 'integer'}
+
+    def on_task_input(self, task, config):
+        amount = config or 0  # hackily makes sure it's an int value
+        entries = []
+        for i in range(amount):
+            entry = Entry()
+            import string
+            import random
+            entry['url'] = 'http://localhost/generate/%s/%s' % (i, ''.join([random.choice(string.letters + string.digits) for x in range(1, 30)]))
+            entry['title'] = ''.join([random.choice(string.letters + string.digits) for x in range(1, 30)])
+            entry['description'] = ''.join([random.choice(string.letters + string.digits) for x in range(1, 1000)])
+            entries.append(entry)
+        return entries
+
+
+@event('plugin.register')
+def register_plugin():
+    plugin.register(Generate, 'generate', api_ver=2, debug=True)
