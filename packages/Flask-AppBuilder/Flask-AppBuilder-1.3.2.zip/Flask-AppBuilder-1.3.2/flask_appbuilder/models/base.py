a639e494d75df7d16be2dd146b4faf34330bc68e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:41 +0000

    Add package Flask-AppBuilder/1.3.2/Flask-AppBuilder-1.3.2.zip

---
+++ packages/Flask-AppBuilder/Flask-AppBuilder-1.3.2.zip/Flask-AppBuilder-1.3.2/flask_appbuilder/models/base.py
@@ -0,0 +1,246 @@
+import logging
+from functools import reduce
+from flask_babelpkg import lazy_gettext
+from .filters import Filters
+
+log = logging.getLogger(__name__)
+
+
+class BaseInterface(object):
+    """
+        Base class for all data model interfaces.
+        Sub class it to implement your own interface for some data engine.
+    """
+    obj = None
+
+    filter_converter_class = None
+    """ when sub classing override with your own custom filter converter """
+
+    """ Messages to display on CRUD Events """
+    add_row_message = lazy_gettext('Added Row')
+    edit_row_message = lazy_gettext('Changed Row')
+    delete_row_message = lazy_gettext('Deleted Row')
+    delete_integrity_error_message = lazy_gettext('Associated data exists, please delete them first')
+    add_integrity_error_message = lazy_gettext('Integrity error, probably unique constraint')
+    edit_integrity_error_message = lazy_gettext('Integrity error, probably unique constraint')
+    general_error_message = lazy_gettext('General Error')
+
+    def __init__(self, obj):
+        self.obj = obj
+
+    def _get_attr_value(self, item, col):
+        if not hasattr(item, col):
+            # it's an inner obj attr
+            return reduce(getattr, col.split('.'), item)
+        if hasattr(getattr(item, col), '__call__'):
+            # its a function
+            return getattr(item, col)()
+        else:
+            # its attribute
+            return getattr(item, col)
+
+    def get_filters(self, search_columns=[]):
+        return Filters(self.filter_converter_class, self, search_columns)
+
+    def get_values_item(self, item, show_columns):
+        return [self._get_attr_value(item, col) for col in show_columns]
+
+    def get_values(self, lst, list_columns):
+        """
+            Get Values: formats values for list template.
+            returns [{'col_name':'col_value',....},{'col_name':'col_value',....}]
+
+            :param lst:
+                The list of item objects from query
+            :param list_columns:
+                The list of columns to include
+        """
+        retlst = []
+        for item in lst:
+            retdict = {}
+            for col in list_columns:
+                retdict[col] = self._get_attr_value(item, col)
+            retlst.append(retdict)
+        return retlst
+
+    """
+        Returns the models class name
+        usefull for auto title on views
+    """
+    @property
+    def model_name(self):
+        return self.obj.__class__.__name__
+
+    """
+        Next methods must be overridden
+    """
+    def query(self, filters=None, order_column='', order_direction='',
+              page=None, page_size=None):
+        pass
+
+    def is_image(self, col_name):
+        return False
+
+    def is_file(self, col_name):
+        return False
+
+    def is_gridfs_file(self, col_name):
+        return False
+
+    def is_gridfs_image(self, col_name):
+        return False
+
+    def is_string(self, col_name):
+        return False
+
+    def is_text(self, col_name):
+        return False
+
+    def is_integer(self, col_name):
+        return False
+
+    def is_float(self, col_name):
+        return False
+
+    def is_boolean(self, col_name):
+        return False
+
+    def is_date(self, col_name):
+        return False
+
+    def is_datetime(self, col_name):
+        return False
+
+    def is_relation(self, prop):
+        return False
+
+    def is_relation_col(self, col):
+        return False
+
+    def is_relation_many_to_one(self, prop):
+        return False
+
+    def is_relation_many_to_many(self, prop):
+        return False
+
+    def is_relation_one_to_one(self, prop):
+        return False
+
+    def is_relation_one_to_many(self, prop):
+        return False
+
+    def is_nullable(self, col_name):
+        return True
+
+    def is_unique(self, col_name):
+        return False
+
+    def is_pk(self, col_name):
+        return False
+
+    def is_fk(self, col_name):
+        return False
+
+    def get_max_length(self, col_name):
+        return -1
+
+    def get_min_length(self, col_name):
+        return -1
+
+    """
+    -----------------------------------------
+           FUNCTIONS FOR CRUD OPERATIONS
+    -----------------------------------------
+    """
+
+    def add(self, item):
+        """
+            Adds object
+        """
+        raise NotImplementedError
+
+    def edit(self, item):
+        """
+            Edit (change) object
+        """
+        raise NotImplementedError
+
+    def delete(self, item):
+        """
+            Deletes object
+        """
+        raise NotImplementedError
+
+    def get_col_default(self, col_name):
+        pass
+
+    def get_keys(self, lst):
+        """
+            return a list of pk values from object list
+        """
+        pk_name = self.get_pk_name()
+        return [getattr(item, pk_name) for item in lst]
+
+    def get_pk_name(self, item):
+        """
+            Returns the primary key name
+        """
+        raise NotImplementedError
+
+    def get_pk_value(self, item):
+        return getattr(item, self.get_pk_name())
+
+    def get(self, pk):
+        """
+            return the record from key
+        """
+        pass
+
+    def get_related_model(self, prop):
+        raise NotImplementedError
+
+    def get_related_interface(self, col_name):
+        """
+            Returns a BaseInterface for the related model
+            of column name.
+
+            :param col_name: Column name with relation
+            :return: BaseInterface
+        """
+        raise NotImplementedError
+
+    def get_related_obj(self, col_name, value):
+        raise NotImplementedError
+
+    def get_related_fk(self, model):
+        raise NotImplementedError
+
+    def get_columns_list(self):
+        """
+            Returns a list of all the columns names
+        """
+        return []
+
+    def get_user_columns_list(self):
+        """
+            Returns a list of user viewable columns names
+        """
+        return self.get_columns_list()
+
+    def get_search_columns_list(self):
+        """
+            Returns a list of searchable columns names
+        """
+        return []
+
+    def get_order_columns_list(self, list_columns=None):
+        """
+            Returns a list of order columns names
+        """
+        return []
+
+    def get_relation_fk(self, prop):
+        pass
+
+
+
---
+++ packages/Flask-AppBuilder/Flask-AppBuilder-1.3.2.zip/Flask-AppBuilder-1.3.2/flask_appbuilder/security/mongoengine/manager.py
@@ -0,0 +1,335 @@
+import logging
+from werkzeug.security import generate_password_hash
+from ...models.mongoengine.interface import MongoEngineInterface
+from .models import User, Role, PermissionView, Permission, ViewMenu
+from ..views import AuthDBView, AuthOIDView, ResetMyPasswordView, AuthLDAPView, AuthOAuthView, AuthRemoteUserView, \
+    ResetPasswordView, UserDBModelView, UserLDAPModelView, UserOIDModelView, UserOAuthModelView, UserRemoteUserModelView,\
+    RoleModelView, PermissionViewModelView, ViewMenuModelView, PermissionModelView, UserStatsChartView
+#from .registerviews import RegisterUserDBView, RegisterUserOIDView
+from ..manager import BaseSecurityManager
+
+log = logging.getLogger(__name__)
+
+
+class SecurityManager(BaseSecurityManager):
+    """
+        Responsible for authentication, registering security views,
+        role and permission auto management
+
+        If you want to change anything just inherit and override, then
+        pass your own security manager to AppBuilder.
+    """
+    user_model = User
+    """ Override to set your own User Model """
+    role_model = Role
+    """ Override to set your own User Model """
+    permission_model = Permission
+    viewmenu_model = ViewMenu
+    permissionview_model = PermissionView
+
+    userdbmodelview = UserDBModelView
+    """ Override if you want your own user db view """
+    userldapmodelview = UserLDAPModelView
+    """ Override if you want your own user ldap view """
+    useroidmodelview = UserOIDModelView
+    """ Override if you want your own user OID view """
+    useroauthmodelview = UserOAuthModelView
+    """ Override if you want your own user OAuth view """
+    userremoteusermodelview = UserRemoteUserModelView
+    """ Override if you want your own user REMOTE_USER view """
+    authdbview = AuthDBView
+    """ Override if you want your own Authentication DB view """
+    authldapview = AuthLDAPView
+    """ Override if you want your own Authentication LDAP view """
+    authoidview = AuthOIDView
+    """ Override if you want your own Authentication OID view """
+    authoauthview = AuthOAuthView
+    """ Override if you want your own Authentication OAuth view """
+    authremoteuserview = AuthRemoteUserView
+    """ Override if you want your own Authentication OAuth view """
+    #registeruserdbview = RegisterUserDBView
+    """ Override if you want your own register user db view """
+    #registeruseroidview = RegisterUserOIDView
+    """ Override if you want your own register user db view """
+    resetmypasswordview = ResetMyPasswordView
+    """ Override if you want your own reset my password view """
+    resetpasswordview = ResetPasswordView
+    """ Override if you want your own reset password view """
+    rolemodelview = RoleModelView
+    permissionmodelview = PermissionModelView
+    userstatschartview = UserStatsChartView
+    viewmenumodelview = ViewMenuModelView
+    permissionviewmodelview = PermissionViewModelView
+
+    def __init__(self, appbuilder):
+        """
+            SecurityManager contructor
+            param appbuilder:
+                F.A.B AppBuilder main object
+            """
+        self.userdbmodelview.datamodel = MongoEngineInterface(self.user_model)
+        self.userldapmodelview.datamodel = MongoEngineInterface(self.user_model)
+        self.useroidmodelview.datamodel = MongoEngineInterface(self.user_model)
+        self.useroauthmodelview.datamodel = MongoEngineInterface(self.user_model)
+        self.userremoteusermodelview.datamodel = MongoEngineInterface(self.user_model)
+        self.userstatschartview.datamodel = MongoEngineInterface(self.user_model)
+        self.rolemodelview.datamodel = MongoEngineInterface(self.role_model)
+        self.permissionmodelview.datamodel=MongoEngineInterface(self.permission_model)
+        self.viewmenumodelview.datamodel=MongoEngineInterface(self.viewmenu_model)
+        self.permissionviewmodelview.datamodel=MongoEngineInterface(self.permissionview_model)
+        super(SecurityManager, self).__init__(appbuilder)
+        self.create_db()
+
+    def find_user(self, username=None, email=None):
+        if username:
+            return self.user_model.objects(username=username).first()
+        elif email:
+            return self.user_model.objects(email=email).first()
+
+    def get_all_users(self):
+        return User.objects
+
+    def add_user(self, username, first_name, last_name, email, role, password=''):
+        """
+            Generic function to create user
+        """
+        try:
+            user = self.user_model()
+            user.first_name = first_name
+            user.last_name = last_name
+            user.username = username
+            user.email = email
+            user.active = True
+            user.roles.append(role)
+            user.password = generate_password_hash(password)
+            user.save()
+            log.info("Added user %s to user list." % username)
+            return user
+        except Exception as e:
+            log.error(
+                "Error adding new user to database. {0}".format(
+                    str(e)))
+            return False
+
+    def count_users(self):
+        return len(self.user_model.objects)
+
+    def update_user(self, user):
+        try:
+            user.save()
+        except Exception as e:
+            log.error(
+                "Error updating user to database. {0}".format(
+                    str(e)))
+            return False
+
+    def get_user_by_id(self, pk):
+        return self.user_model.objects(pk=pk).first()
+
+    def load_user(self, pk):
+        return self.get_user_by_id(pk)
+
+
+    """
+        ----------------------------------------
+            PERMISSION MANAGEMENT
+        ----------------------------------------
+    """
+    def add_role(self, name):
+        role = self.find_role(name)
+        if role is None:
+            try:
+                role = self.role_model(name=name)
+                role.save()
+                return role
+            except Exception as e:
+                log.error("Add Role: {0}".format(str(e)))
+        return role
+
+    def find_role(self, name):
+        return self.role_model.objects(name=name).first()
+
+    def get_all_roles(self):
+        return self.role_model.objects
+
+    def get_public_permissions(self):
+        role = self.find_role(self.auth_role_public)
+        return role.permissions
+
+    def find_permission(self, name):
+        """
+            Finds and returns a Permission by name
+        """
+        return self.permission_model.objects(name=name).first()
+
+    def add_permission(self, name):
+        """
+            Adds a permission to the backend, model permission
+            
+            :param name:
+                name of the permission: 'can_add','can_edit' etc...
+        """
+        perm = self.find_permission(name)
+        if perm is None:
+            try:
+                perm = self.permission_model(name=name)
+                perm.save()
+                return perm
+            except Exception as e:
+                log.error("Add Permission: {0}".format(str(e)))
+        return perm
+
+    def del_permission(self, name):
+        """
+            Deletes a permission from the backend, model permission
+
+            :param name:
+                name of the permission: 'can_add','can_edit' etc...
+        """
+        perm = self.find_permission(name)
+        if perm:
+            try:
+                perm.delete()
+            except Exception as e:
+                log.error("Del Permission Error: {0}".format(str(e)))
+
+    # ----------------------------------------------
+    #       PRIMITIVES VIEW MENU
+    #----------------------------------------------
+    def find_view_menu(self, name):
+        """
+            Finds and returns a ViewMenu by name
+        """
+        return self.viewmenu_model.objects(name=name).first()
+
+    def get_all_view_menu(self):
+        return self.viewmenu_model.objects
+
+    def add_view_menu(self, name):
+        """
+            Adds a view or menu to the backend, model view_menu
+            param name:
+                name of the view menu to add
+        """
+        view_menu = self.find_view_menu(name)
+        if view_menu is None:
+            try:
+                view_menu = self.viewmenu_model(name=name)
+                view_menu.save()
+                return view_menu
+            except Exception as e:
+                log.error("Add View Menu Error: {0}".format(str(e)))
+        return view_menu
+
+    def del_view_menu(self, name):
+        """
+            Deletes a ViewMenu from the backend
+
+            :param name:
+                name of the ViewMenu
+        """
+        obj = self.find_view_menu(name)
+        if obj:
+            try:
+                obj.delete()
+            except Exception as e:
+                log.error("Del Permission Error: {0}".format(str(e)))
+
+    #----------------------------------------------
+    #          PERMISSION VIEW MENU
+    #----------------------------------------------
+    def find_permission_view_menu(self, permission_name, view_menu_name):
+        """
+            Finds and returns a PermissionView by names
+        """
+        permission = self.find_permission(permission_name)
+        view_menu = self.find_view_menu(view_menu_name)
+        return self.permissionview_model.objects(permission=permission, view_menu=view_menu).first()
+
+    def find_permissions_view_menu(self, view_menu):
+        """
+            Finds all permissions from ViewMenu, returns list of PermissionView
+
+            :param view_menu: ViewMenu object
+            :return: list of PermissionView objects
+        """
+        return self.permissionview_model.objects(view_menu=view_menu)
+
+    def add_permission_view_menu(self, permission_name, view_menu_name):
+        """
+            Adds a permission on a view or menu to the backend
+            
+            :param permission_name:
+                name of the permission to add: 'can_add','can_edit' etc...
+            :param view_menu_name:
+                name of the view menu to add
+        """
+        vm = self.add_view_menu(view_menu_name)
+        perm = self.add_permission(permission_name)
+        pv = self.permissionview_model()
+        pv.view_menu, pv.permission = vm, perm
+        try:
+            pv.save()
+            log.info("Created Permission View: %s" % (str(pv)))
+            return pv
+        except Exception as e:
+            log.error("Creation of Permission View Error: {0}".format(str(e)))
+
+    def del_permission_view_menu(self, permission_name, view_menu_name):
+        try:
+            pv = self.find_permission_view_menu(permission_name, view_menu_name)
+            # delete permission on view
+            pv.delete()
+            # if no more permission on permission view, delete permission
+            pv = self.permissionview_model.objects(permission=pv.permission)
+            if not pv:
+                self.del_permission(pv.permission.name)
+            log.info("Removed Permission View: %s" % (str(permission_name)))
+        except Exception as e:
+            log.error("Remove Permission from View Error: {0}".format(str(e)))
+
+    def exist_permission_on_views(self, lst, item):
+        for i in lst:
+            if i.permission.name == item:
+                return True
+        return False
+
+    def exist_permission_on_view(self, lst, permission, view_menu):
+        for i in lst:
+            if i.permission.name == permission and i.view_menu.name == view_menu:
+                return True
+        return False
+
+    def add_permission_role(self, role, perm_view):
+        """
+            Add permission-ViewMenu object to Role
+            
+            :param role:
+                The role object
+            :param perm_view:
+                The PermissionViewMenu object
+        """
+        if perm_view not in role.permissions:
+            try:
+                role.permissions.append(perm_view)
+                role.save()
+                log.info("Added Permission %s to role %s" % (str(perm_view), role.name))
+            except Exception as e:
+                log.error("Add Permission to Role Error: {0}".format(str(e)))
+
+    def del_permission_role(self, role, perm_view):
+        """
+            Remove permission-ViewMenu object to Role
+            
+            :param role:
+                The role object
+            :param perm_view:
+                The PermissionViewMenu object
+        """
+        if perm_view in role.permissions:
+            try:
+                role.permissions.remove(perm_view)
+                role.save()
+                log.info("Removed Permission %s to role %s" % (str(perm_view), role.name))
+            except Exception as e:
+                log.error("Remove Permission to Role Error: {0}".format(str(e)))
