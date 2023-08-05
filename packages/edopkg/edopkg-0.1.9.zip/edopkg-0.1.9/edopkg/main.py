f423a7fbb4c0748d432ee805c964002cdf286b70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:01 +0000

    Add package edopkg/0.1.9/edopkg-0.1.9.zip

---
+++ packages/edopkg/edopkg-0.1.9.zip/edopkg-0.1.9/edopkg/main.py
@@ -0,0 +1,259 @@
+# encoding: utf-8
+import os
+import sys
+import ConfigParser
+import yaml
+import getpass
+from edopkg import config, pyaml
+from config import EDO_CONFIG_PATH, OC_API, SECTION, ACCOUNT, INSTANCE, \
+USERNAME, CLIENT_ID, CLIENT_SECRET, HELP_INFO
+from utils import get_wo_client
+from package import EdoPackage
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+PKG_COMMAND = ['clone', 'push', 'pull', 'server', 'config']
+
+def main():
+
+    # 命令解析
+    cmd = ''.join(sys.argv[1:2])
+    if cmd == '':
+        print HELP_INFO
+    elif cmd == 'server':
+        section = ''.join(sys.argv[2:3])
+    elif cmd == 'clone':
+        pkg_name = sys.argv[2]
+    else:
+        path_filter = ''.join(sys.argv[2:3])
+
+    #  获取配置对象
+    edo_configparse = ConfigParser.ConfigParser()
+    if not os.path.exists(EDO_CONFIG_PATH):
+        #  初始化配置文件
+        section= read_init_section(SECTION)
+        oc_api, account, instance, username, password = read_detail_inputs()
+        edo_configparse.add_section('edopkg')
+        edo_configparse.set('edopkg', 'server', section)
+        save_config(edo_configparse, EDO_CONFIG_PATH, section, oc_api, account,
+                    instance, username, password)
+    edo_configparse.read(EDO_CONFIG_PATH)
+
+    #  执行非同步命令
+    if cmd == '':
+        return
+    elif cmd not in PKG_COMMAND:
+        print 'command error'
+        return
+    elif cmd == 'server':
+        if section in edo_configparse.sections():
+            # 重新设置服务器配置
+            edo_configparse.set('edopkg', 'server', section)
+            edo_configparse.write(open(EDO_CONFIG_PATH, 'w'))
+            print u' 默认配置已经修改为：' + section
+        elif section:
+            print u'配置名称 %s 不存在' % section
+
+        #  显示当前服务器配置
+        print_server_config(edo_configparse)
+        return
+    elif cmd == 'config':
+        print_server_config(edo_configparse)
+        section, action = read_section_input(edo_configparse)
+        if action == 'modify':
+            edo_config = dict(edo_configparse.items(section))
+            oc_api, account, instance, username, password = read_detail_inputs(
+                oc_api=edo_config['oc_api'],
+                account=edo_config['account'],
+                instance=edo_config['instance'],
+                username=edo_config['username']
+            )
+        else:
+            oc_api, account, instance, username, password = read_detail_inputs()
+        save_config(edo_configparse, EDO_CONFIG_PATH, section, oc_api, account,
+                    instance, username, password)
+        return
+
+    #  确认同步操作
+    if cmd in ['push', 'pull'] and not confirm_cmd(cmd):
+        print u'同步被取消'
+        return
+
+    #  计算local_root
+    if cmd == 'clone':
+        local_root = os.path.abspath(pkg_name)
+        if os.path.exists(local_root):
+            print 'package already exited'
+            return
+    else:
+        local_root = find_package_root()
+        if not local_root:
+            print 'can not find the package'
+            return
+        # 相对路径转换: 计算path_filter相对于local_root的子路径
+        abspath = os.path.abspath(path_filter) if path_filter else local_root
+        path_filter = '' if abspath == local_root else os.path.relpath(abspath, local_root)
+
+    # 生成服务端连接
+    section = edo_configparse.get('edopkg', 'server')
+    edo_config = dict(edo_configparse.items(section))
+    wo_client = get_wo_client(  edo_config['oc_api'],
+                                edo_config['client_id'],
+                                edo_config['client_secret'],
+                                edo_config['account'],
+                                edo_config['instance'],
+                                edo_config['username'],
+                                edo_config['password'])
+
+    # 初始化包管理器
+    edo_pkg = EdoPackage(wo_client, local_root)
+
+    # 执行同步命令
+    if cmd == 'pull':
+        edo_pkg.pull(path_filter)
+    elif cmd == 'push':
+        edo_pkg.push(path_filter)
+    elif cmd == 'clone':
+        edo_pkg.clone()
+
+def print_server_config(edo_configparse):
+    active_section = edo_configparse.get('edopkg', 'server')
+    info_template = u'%s, 账号 %s, 站点 %s '
+    for section in edo_configparse.sections():
+        if section == 'edopkg':
+            continue
+        oc_api = edo_configparse.get(section, 'oc_api')
+        account = edo_configparse.get(section, 'account')
+        instance = edo_configparse.get(section, 'instance')
+        if section == active_section:
+            head = u'  * %s:' % section
+        else:
+            head = u'    %s:' % section
+        print ''.join([head.ljust(20), info_template % (oc_api, account, instance)])
+
+def save_config(edo_configparse, path, section, oc_api, account, instance, username, password):
+    if section not in edo_configparse.sections():
+        edo_configparse.add_section(section)
+    edo_configparse.set(section, 'client_id', CLIENT_ID)
+    edo_configparse.set(section, 'client_secret', CLIENT_SECRET)
+    edo_configparse.set(section, 'oc_api', oc_api)
+    edo_configparse.set(section, 'account', account)
+    edo_configparse.set(section, 'instance', instance)
+    edo_configparse.set(section, 'username', username)
+    edo_configparse.set(section, 'password', password)
+    edo_configparse.write(open(path, 'w'))
+    print u' 配置文件已保存到：%s ' % path
+
+def confirm_cmd(cmd):
+    print u'%s 命令会删除不存在的内容，您确定要继续么？' % cmd
+    sys.stdout.write(u'您的选择[y/n](y:继续 | n:取消):')
+    return raw_input().lower() == 'y'
+
+def read_section_input(edo_configparse):
+    print u'''您可以选择：
+    1.修改已存在的配置
+    2.新建一个配置
+    '''
+    sys.stdout.write(u'您的选择[1/2](或输入其他命令退出)：')
+    num = raw_input()
+    if num not in ['1', '2']:
+        sys.exit(0)
+    # 检查用户输入的配置名
+    if num == '1':
+        while True:
+            #  默认修改当前配置
+            section = get_input(u'请输入配置名', default=edo_configparse.get('edopkg', 'server'))
+            if section not in edo_configparse.sections():
+                print u'不存在此配置，请确认后重新输入'
+            else:
+                break
+    else:
+        while True:
+            sys.stdout.write(u'请输入配置名：')
+            section = raw_input()
+            if section in edo_configparse.sections():
+                print u'配置已存在，请确认后重新输入'
+            else:
+                break
+    print u'配置名：%s' % section
+    action = 'modify' if num == '1' else 'add'
+    return section, action
+
+def read_init_section(section):
+    print u'''
+ -----------------------------
+ 需要输入配置信息以进行初始化
+ -----------------------------'''
+    section = get_input(u'请输入配置名', default=section)
+    return section
+
+def read_detail_inputs(oc_api=OC_API, account=ACCOUNT,instance=INSTANCE,
+                       username=USERNAME):
+    print u'''
+ -----------------------------
+ 请输入具体的配置数据
+ -----------------------------
+ 字段的具体含义：
+
+ oc_api: oc服务地址
+ account: 公司(子域名)名称
+ instance: 站点实例， 只有一个站点时为default
+ username: 用户名
+ password: 密码
+ -----------------------------
+ 请输入数据：
+ '''
+    while True:
+        oc_api = get_input('oc_api', default=oc_api)
+        account = get_input('account', default=account)
+        instance = get_input('instance', default=instance)
+        username = get_input('username', default=username)
+
+        print u'''
+ -----------------------------
+ 您的输入：
+ -----------------------------'''
+        print ' oc_api: %s' % oc_api
+        print ' account: %s' % account
+        print ' instance: %s' % instance
+        print ' username: %s' % username
+        print u'''
+ -----------------------------
+ 您确认要输入的是以上的数据么？[y/n]（确认：y | 重新输入：n）：'''
+        if raw_input().lower() in ['y', '']:
+            break
+        else:
+            print u'请重新输入数据:'
+
+    while True:
+        password = getpass.getpass('password: ')
+        confirm_pwd = getpass.getpass('confirm password: ')
+        if password == confirm_pwd:
+            break
+        else:
+            print u'两次输入的密码不一致,  请重新输入'
+    return oc_api, account, instance, username, password
+
+def get_input(msg, default =''):
+    msg = ' %s[%s]: ' % (msg, default)
+    sys.stdout.write(msg)
+    input_info = raw_input()
+    return input_info if input_info else default
+
+def find_package_root():
+    # 优先在当前目录下查找
+    base_path = os.getcwd()
+    config_path = os.path.join(base_path, 'config.yaml')
+    # 不存在时往上级查找
+    while not os.path.exists(config_path):
+        if base_path != os.path.dirname(base_path):
+            base_path = os.path.dirname(base_path)
+            config_path = os.path.join(base_path, 'config.yaml')
+        else:
+            # 到达根时退出
+            return False
+    return base_path
+
+if __name__ == '__main__':
+    main()
---
+++ packages/edopkg/edopkg-0.1.9.zip/edopkg-0.1.9/edopkg/package.py
@@ -0,0 +1,235 @@
+# encoding: utf-8
+import os
+import sys
+import shutil
+import yaml
+import traceback
+from edopkg import pyaml
+from utils import complie_script
+
+# 优先检测代码并上传， 最后上传form
+OBJ_CATS = ['script', 'workflow', 'mdset', 'rule', 'template',
+        'stage','skin', 'resource', 'form']
+
+class EdoPackage:
+
+    def __init__(self, wo_client, local_root):
+        self.remote_pkg = wo_client.package
+        self.local_root = local_root
+        self.pkg_name = os.path.basename(local_root)
+
+    def clone(self):
+        os.makedirs(self.local_root)
+        try:
+            self.pull()
+        except Exception as e:
+            shutil.rmtree(self.local_root)
+            print u'\nclone失败, 请确认是否存在软件包：%s' % self.pkg_name
+            raise e
+
+    def pull(self, path_filter=''):
+        sys.stdout.write(u'\r正在下载数据...')
+        if not path_filter or path_filter == 'config.yaml':
+            pkg_info = self.remote_pkg.get(self.pkg_name, detail=False)
+            self.write_yaml('config.yaml', pkg_info)
+
+        # 写入软件包对象
+        for obj_cat in OBJ_CATS:
+            obj_folder = obj_cat + 's'
+
+            # 过滤
+            if path_filter and not path_filter.startswith(obj_folder):
+                continue
+
+            # 计算文件的过滤条件
+            filename_filter = path_filter[len(obj_folder) + 1:]
+
+            #  删除同步：清空本地文件夹
+            if not filename_filter:
+                abs_folder_path = os.path.join(self.local_root, obj_folder)
+                if os.path.exists(abs_folder_path):
+                    shutil.rmtree(abs_folder_path)
+
+            # 逐个对象同步
+            for obj_name in getattr(self.remote_pkg, 'list_' + obj_folder)(self.pkg_name):
+                self.pull_obj(obj_cat, obj_name, obj_folder, filename_filter)
+
+        print u'\r下载成功'.ljust(60)
+
+    def push(self, path_filter=''):
+        sys.stdout.write(u'\r正在上传数据...')
+
+        if not path_filter or path_filter == 'config.yaml':
+            pkg_info = self.read_yaml('config.yaml')
+            pkg_list = self.remote_pkg.list()
+            if self.pkg_name not in pkg_list:
+                status = self.remote_pkg.new(self.pkg_name, pkg_info)
+            status = self.remote_pkg.set(self.pkg_name, pkg_info)
+
+        # 读取软件包对象
+        for obj_cat in OBJ_CATS:
+            obj_folder = obj_cat + 's'
+            # 过滤
+            if path_filter and not path_filter.startswith(obj_folder):
+                continue
+            filename_filter = path_filter[len(obj_folder) + 1:]
+
+
+            # 删除同步：删除远程的对象
+            if not filename_filter:
+                for obj_name in getattr(self.remote_pkg, 'list_' + obj_folder)(self.pkg_name):
+                    if obj_cat == 'resource':
+                        self.remote_pkg.remove_resource(self.pkg_name, obj_name)
+                    else:
+                        getattr(self.remote_pkg, 'remove_' + obj_cat)(obj_name)
+
+            # 判断是否存在文件夹
+            cat_path = os.path.join(self.local_root, obj_folder)
+            if not os.path.exists(cat_path):
+                continue
+
+            # 获取同步列表
+            obj_list = os.listdir(cat_path)
+
+            # 根据文件类型进行过滤
+            obj_list = filter(lambda f:not f.startswith(('.', '~')), obj_list)
+            obj_list = filter(lambda f:not f.endswith(('.bak', '~')), obj_list)
+            if obj_cat == 'template':
+                obj_list == filter(lambda f:f.endswith('.pt'), obj_list)
+            elif obj_cat != 'resource':
+                obj_list == filter(lambda f:f.endswith('.yaml'), obj_list)
+
+            # 对于表单进行特殊处理
+            if obj_cat == 'form':
+                first_list, after_list = [], []
+                for obj_filename in obj_list:
+                    obj_data = self.read_yaml(os.path.join(self.local_root, cat_path, obj_filename))
+                    # 优先上传类型为DataContainer的表单
+                    if ''.join(obj_data['object_types']) == 'DataContainer':
+                        first_list.append(obj_filename)
+                    else:
+                        after_list.append(obj_filename)
+                # 合并列表
+                obj_list = first_list + after_list
+
+            # 逐个上传
+            for obj_filename in obj_list:
+                # 过滤
+                if filename_filter and not obj_filename.startswith(filename_filter):
+                    continue
+
+                #  同步
+                if os.path.isdir(os.path.join(self.local_root, obj_folder, obj_filename)):
+                    self.push_folder(os.path.join(obj_folder, obj_filename))
+                else:
+                    self.push_obj(obj_cat, obj_folder, obj_filename)
+
+        print u'\r上传成功'.ljust(60)
+
+    def push_folder(self, obj_folder, obj_cat='resource'):
+        for obj_filename in os.listdir(os.path.join(self.local_root, obj_folder)):
+            if os.path.isdir(os.path.join(self.local_root, obj_folder, obj_filename)):
+                self.push_folder(os.path.join(obj_folder, obj_filename))
+            else:
+                self.push_obj(obj_cat, obj_folder, obj_filename)
+
+    def pull_obj(self, obj_cat, obj_name, obj_folder, filename_filter):
+        # 读取文件名
+        if obj_cat == 'resource':
+            obj_filename = os.path.normpath(obj_name)
+        elif obj_cat == 'template':
+           obj_filename = obj_name.split(':')[1] + '.pt'
+        else:
+           obj_filename = obj_name.split(':')[1] + '.yaml'
+
+        # 过滤
+        if filename_filter and not obj_filename.startswith(filename_filter):
+            return
+
+        # 更新提示
+        sys.stdout.write('\rpulling ' + obj_name.ljust(60))
+
+        # 下载数据
+        if obj_cat == 'resource':
+            obj_data = self.remote_pkg.get_resource(self.pkg_name, path = obj_name)
+        else:
+            obj_data = getattr(self.remote_pkg, 'get_' + obj_cat)(obj_name)
+
+        # 写入数据
+        obj_path = os.path.join(obj_folder, obj_filename)
+
+        # 判断文件夹是否存在
+        dirname = os.path.dirname(os.path.join(self.local_root, obj_path))
+        if not os.path.exists( dirname):
+            os.makedirs(dirname)
+
+        if obj_cat == 'template':
+            self.write_file(obj_path, obj_data['template'])
+        elif obj_cat == 'resource':
+            self.write_response(obj_path, obj_data)
+        else:
+            self.write_yaml(obj_path, obj_data)
+
+    def push_obj(self, obj_cat, obj_folder, obj_filename):
+        # 更新提示
+        sys.stdout.write('\rpushing ' + obj_filename.ljust(60))
+
+        obj_name = obj_filename.split('.')[0]
+        # 读取数据
+        obj_path = os.path.join(obj_folder, obj_filename)
+        if obj_cat == 'template':
+            obj_data = self.read_template(obj_name, obj_path)
+        elif obj_cat == 'resource':
+            obj_stream = self.read_stream(obj_path)
+        else:
+            obj_data = self.read_yaml(obj_path)
+
+        # 代码检测
+        if obj_cat == 'script':
+            script = obj_data.get('script', '')
+            try:
+                complie_script(script)
+            except:
+                print u'\n脚本代码 %s 存在错误，以下是错误信息：' % obj_filename
+                traceback.print_exc(0)
+                print u'\n上传中止'
+                sys.exit(2)
+
+        # 上传数据
+        if obj_cat == 'template':
+            status = getattr(self.remote_pkg, 'register_' + obj_cat)(
+                self.pkg_name, obj_data, overwrite = True)
+        elif obj_cat == 'resource':
+            remote_path = obj_filename.replace(os.sep, '/')
+            status = self.remote_pkg.add_resource(
+                self.pkg_name, remote_path, obj_stream, overwrite=True)
+        else:
+            status = getattr(self.remote_pkg, 'register_' + obj_cat)(
+                self.pkg_name, obj_data, overwrite = True)
+
+    def write_yaml(self, path, data):
+        open(os.path.join(self.local_root, path), 'wb').write(pyaml.dump(data))
+
+    def write_file(self, path, data='', response=''):
+        open(os.path.join(self.local_root, path), 'wb').write(data)
+
+    def write_response(self, response):
+        with open(path, 'wb') as f:
+            for block in response.iter_content(chunk_size=1024):
+                if not block:
+                    break
+                f.write(block)
+                f.flush()
+
+    def read_yaml(self, path):
+        return yaml.load(open(os.path.join(self.local_root, path)).read())
+
+    def read_template(self, obj_name, path):
+        data = open(os.path.join(self.local_root, path)).read()
+        return {'name':obj_name, 'title':obj_name, 'template':data}
+
+    def read_stream(self, path):
+        return open(os.path.join(self.local_root, path))
+
+
+
