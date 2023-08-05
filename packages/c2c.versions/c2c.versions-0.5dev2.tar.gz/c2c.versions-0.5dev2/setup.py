11cb70ef3035b03724da76848694c0df93193f6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:31 +0000

    Add package c2c.versions/0.5dev2/c2c.versions-0.5dev2.tar.gz

---
+++ packages/c2c.versions/c2c.versions-0.5dev2.tar.gz/c2c.versions-0.5dev2/setup.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.rst')).read()
+
+install_requires = [
+    'PyYAML',
+]
+
+setup(
+    name='c2c.versions',
+    version='0.5',
+    description='Command to tests program version',
+    long_description=README,
+    namespace_packages=['c2c'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: System :: Installation/Setup',
+    ],
+    author='Stéphane Brunner',
+    author_email='stephane.brunner@camptocamp.com',
+    url='http://github.com/sbrunner/c2c.versions',
+    license='BSD',
+    keywords='check package programme version',
+    packages=find_packages(exclude=["*.tests", "*.tests.*"]),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=install_requires,
+    entry_points={
+        "console_scripts": [
+            "c2c-versions = c2c.versions:main",
+        ]
+    }
+)
