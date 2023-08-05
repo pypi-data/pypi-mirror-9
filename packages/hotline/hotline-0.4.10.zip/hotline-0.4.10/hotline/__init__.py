fd8e814372873beafc52c1b076645da985e084d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:17 +0000

    Add package hotline/0.4.10/hotline-0.4.10.zip

---
+++ packages/hotline/hotline-0.4.10.zip/hotline-0.4.10/hotline/__init__.py
@@ -0,0 +1,12 @@
+__title__ = "hotline"
+__author__ = "Dan Bradham"
+__email__ = "danielbradham@gmail.com"
+__url__ = "http://github.com/danbradham/hotline"
+__version__ = "0.4.10"
+__license__ = "MIT"
+__description__ = "A pop-up input field with customizable settings."
+
+
+from .app import HotLine, show
+from .store import Store
+from .config import ConfigBase, Config
---
+++ packages/hotline/hotline-0.4.10.zip/hotline-0.4.10/setup.py
@@ -0,0 +1,58 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+import os
+import sys
+import hotline
+
+
+if sys.argv[-1] == 'cheeseit!':
+    os.system('python setup.py sdist upload')
+    sys.exit()
+elif sys.argv[-1] == 'cheeseit':
+    os.system('python setup.py sdist upload -r pypitest')
+    sys.exit()
+
+
+packages = [
+    'hotline',
+    'hotline.contexts',
+    'hotline.ui'
+]
+
+package_data = {
+    '': ['LICENSE', 'README.rst'],
+    'hotline': ['conf/*.*']
+}
+
+
+with open('README.rst') as f:
+    readme = f.read()
+
+
+setup(
+    name='hotline',
+    version=hotline.__version__,
+    description=hotline.__description__,
+    long_description=readme,
+    author=hotline.__author__,
+    author_email=hotline.__email__,
+    url=hotline.__url__,
+    license=hotline.__license__,
+    packages=packages,
+    package_data=package_data,
+    package_dir={'hotline': 'hotline'},
+    include_package_data=True,
+    classifiers=(
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ),
+)
