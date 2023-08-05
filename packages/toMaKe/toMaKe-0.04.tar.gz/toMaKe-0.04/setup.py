454e4157fd6ce53f178304172f4a94dbe0190333
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:17 +0000

    Add package toMaKe/0.04/toMaKe-0.04.tar.gz

---
+++ packages/toMaKe/toMaKe-0.04.tar.gz/toMaKe-0.04/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+# one line description
+description = "tools for Mad & Kea"
+
+version = 0.04
+
+setup(name='toMaKe',
+      version=version,
+      url="https://github.com/mfiers/toMaKe",
+      description=description,
+      author='Mark Fiers',
+      author_email='mark.fiers.42@gmail.com',
+      include_package_data=True,
+      packages=find_packages(),
+      install_requires=[
+          'fantail',
+          'humanize',
+          'jinja2',
+      ],
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Environment :: Console',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.7',
+      ]
+      )
