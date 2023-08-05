afe24f10e9b69183bac3b2a52eea322a436d7f3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:14 +0000

    Add package joe/0.0.4/joe-0.0.4.tar.gz

---
+++ packages/joe/joe-0.0.4.tar.gz/joe-0.0.4/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+
+version = '0.0.4'
+
+setup(
+    name='joe',
+    version=version,
+    description='joe generates .gitignore files from the command line for you.',
+    long_description=open('README.rst').read(),
+    author='Karan Goel',
+    author_email='karan@goel.io',
+    license='MIT',
+    keywords=['gitignore', 'git', 'github', 'command line', 'cli'],
+    url='http://github.com/karan/joe',
+    download_url=('https://github.com/karan/joe/archive/%s.tar.gz' % version),
+    packages=find_packages(),
+    package_data={
+        'joe': ['data/*.gitignore']
+    },
+    install_requires=[
+        'docopt>=0.6.1',
+    ],
+    entry_points={
+        'console_scripts': [
+            'joe=joe.joe:main'
+        ],
+    }
+)
