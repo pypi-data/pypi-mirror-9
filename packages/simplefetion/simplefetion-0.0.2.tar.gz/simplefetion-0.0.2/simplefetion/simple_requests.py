57a7fb6cc428128696c6316ff15660ca3903e2c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:15 +0000

    Add package simplefetion/0.0.2/simplefetion-0.0.2.tar.gz

---
+++ packages/simplefetion/simplefetion-0.0.2.tar.gz/simplefetion-0.0.2/simplefetion/simple_requests.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+import urllib2
+import urllib
+
+class cole_requests:
+    def __init__(self):
+        pass
+
+    @classmethod
+    def get(self, url, to=5, *args, **kwargs):
+        req = urllib2.Request(url)
+        res_data = urllib2.urlopen(req, timeout=to)
+        return res_data.read()
+
+    @classmethod
+    def post(self, url, data={}, to=5, *args, **kwargs):
+        data = urllib.urlencode(data)
+        req = urllib2.Request(url, data=data)
+        res_data = urllib2.urlopen(req, timeout=to)
+        return res_data.read()
+
+
+if __name__ == '__main__':
+    print('Usage:')
+    print('\t cole_requests.get(url,...)')
+    print('\t cole_requests.post(url, data, ...)')
+    print('\t a = cole_requests()')
+    print('\t a.get(url,...)')
+    print('\t a.post(url, data, ...)')
