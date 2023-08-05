2f22ad26a4be95119e6173d59183820870d04012
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:43 +0000

    Add package metacritic/0.2.8/metacritic-0.2.8.tar.gz

---
+++ packages/metacritic/metacritic-0.2.8.tar.gz/metacritic-0.2.8/setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+
+setup(
+  name = 'metacritic',
+  packages = ['metacritic'],
+  version = '0.2.8',
+  description = 'A Python API for Metacritic.',
+  author = 'Christopher Su',
+  author_email = 'chris+pypi@christopher.su',
+  url = 'https://github.com/csu/pymetacritic',
+  download_url = 'https://github.com/csu/pymetacritic/tarball/0.1',
+  keywords = ['api', 'metacritic', 'scraper'],
+  classifiers = [],
+  install_requires = [
+      'beautifulsoup4',
+      'requests',
+      'wsgiref',
+  ],
+)
