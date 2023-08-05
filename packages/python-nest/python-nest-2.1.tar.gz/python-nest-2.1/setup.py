820a1d231bde5e82ec8f20130381e935bbe93fc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:48 +0000

    Add package python-nest/2.1/python-nest-2.1.tar.gz

---
+++ packages/python-nest/python-nest-2.1.tar.gz/python-nest-2.1/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+import io
+
+from setuptools import setup
+
+setup(name='python-nest',
+      version='2.1',
+      description='Python API and command line tool for talking to the '
+                  'Nest™ Thermostat',
+      long_description=io.open('README.rst', encoding='UTF-8').read(),
+      keywords='nest thermostat',
+      author='Jason Kölker',
+      author_email='jason@koelker.net',
+      url='https://github.com/jkoelker/python-nest/',
+      packages=['nest'],
+      install_requires=['requests'],
+      entry_points={
+          'console_scripts': ['nest=nest_thermostat.command_line:main'],
+      }
+      )
