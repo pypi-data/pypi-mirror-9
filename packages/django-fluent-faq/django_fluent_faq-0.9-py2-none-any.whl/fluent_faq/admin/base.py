13693491815333589950db6e4d22242eb813a16b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:36 +0000

    Add package django-fluent-faq/0.9/django_fluent_faq-0.9-py2-none-any.whl

---
+++ packages/django-fluent-faq/django_fluent_faq-0.9-py2-none-any.whl/fluent_faq/admin/base.py
@@ -0,0 +1,169 @@
+from django.conf import settings
+from django.contrib.admin.widgets import AdminTextInputWidget, AdminTextareaWidget
+from django.core.exceptions import ValidationError, ImproperlyConfigured
+from django.core.urlresolvers import NoReverseMatch
+from django.utils.translation import ugettext, ugettext_lazy as _
+from fluent_faq import appsettings
+from fluent_contents.admin import PlaceholderFieldAdmin
+from fluent_utils.softdeps.fluent_pages import mixed_reverse
+from fluent_utils.dry.admin import MultiSiteAdminMixin
+from parler.admin import TranslatableAdmin
+from parler.forms import TranslatableModelForm
+from parler.models import TranslationDoesNotExist
+
+
+
+class FaqBaseModelForm(TranslatableModelForm):
+    """
+    Base form for FAQ questions.
+    """
+    def clean_slug(self):
+        """
+        Test whether the slug is unique within the language domain.
+        """
+        slug = self.cleaned_data['slug']
+        dup_qs = self._meta.model.objects.translated(self.language_code, slug=slug)
+        if self.instance and self.instance.pk:
+            dup_qs = dup_qs.exclude(pk=self.instance.pk)
+
+        if dup_qs.exists():
+            raise ValidationError(_("The slug is not unique"))
+
+        return slug
+
+
+
+class FaqBaseModelAdmin(MultiSiteAdminMixin, TranslatableAdmin, PlaceholderFieldAdmin):
+    """
+    Base admin for FAQ questions
+    """
+    filter_site = appsettings.FLUENT_FAQ_FILTER_SITE_ID
+    list_display = ('title', 'language_column', 'modification_date', 'actions_column')
+    form = FaqBaseModelForm
+    search_fields = ('translations__slug', 'translations__title')
+
+    # Using fieldnames here works because formfield_for_dbfield() is overwritten.
+    formfield_overrides = {
+        'meta_keywords': {
+            'widget': AdminTextInputWidget(attrs={'class': 'vLargeTextField'})
+        },
+        'meta_description': {
+            'widget': AdminTextareaWidget(attrs={'rows': 3})
+        },
+    }
+
+    FIELDSET_GENERAL = (None, {
+        'fields': ('title', 'slug',),
+    })
+    FIELDSET_PUBLICATION = (_('Publication settings'), {
+        'fields': ('order',),
+        #'classes': ('collapse',),
+    })
+    FIELDSET_SEO = (_('SEO settings'), {
+        'fields': ('meta_keywords', 'meta_description'),
+        'classes': ('collapse',),
+    })
+
+    fieldsets = (
+        FIELDSET_GENERAL,
+        FIELDSET_PUBLICATION,
+        FIELDSET_SEO,
+    )
+
+    class Media:
+        css = {
+            'all': ('fluent_faq/admin/admin.css',)
+        }
+
+
+    def get_prepopulated_fields(self, request, obj=None):
+        # Needed for django-parler
+        return {'slug': ('title',),}
+
+
+    def save_model(self, request, obj, form, change):
+        # Automatically store the user in the author field.
+        if not change:
+            obj.author = request.user
+        obj.save()
+
+
+    def formfield_for_dbfield(self, db_field, **kwargs):
+        """
+        Allow formfield_overrides to contain field names too.
+        """
+        overrides = self.formfield_overrides.get(db_field.name)
+        if overrides:
+            kwargs.update(overrides)
+
+        return super(FaqBaseModelAdmin, self).formfield_for_dbfield(db_field, **kwargs)
+
+
+    def render_change_form(self, request, context, add=False, change=False, form_url='', obj=None):
+        # When the page is accessed via a pagetype, warn that the node can't be previewed yet.
+        context['preview_error'] = ''
+        if 'fluent_pages' in settings.INSTALLED_APPS:
+            from fluent_pages.urlresolvers import PageTypeNotMounted, MultipleReverseMatch
+            try:
+                self._reverse_faqpage_index(request, obj)
+            except PageTypeNotMounted:
+                from fluent_faq.pagetypes.faqpage.models import FaqPage
+                context['preview_error'] = ugettext("The {object_name} can't be previewed yet, a '{page_type_name}' page needs to be created first.").format(object_name=self.model._meta.verbose_name, page_type_name=FaqPage._meta.verbose_name)
+            except MultipleReverseMatch:
+                # When 'faqquestion_index is ambiguous (because there are multiple FAQ nodes in the fluent-pages tree),
+                # the edit page will automatically pick an option.
+                pass
+            except NoReverseMatch:
+                # Since forgetting the pagetype app is easy, give off a warning to help developers
+                # find their way with these apps.
+                raise ImproperlyConfigured(
+                    "To use django-fluent-faq, either include('fluent_faq.urls') in the URLConf, "
+                    "or add the 'fluent_faq.pagetypes.faqpage' app to the INSTALLED_APPS."
+                )
+
+        return super(FaqBaseModelAdmin, self).render_change_form(request, context, add, change, form_url, obj)
+
+
+    def _reverse_faqpage_index(self, request, obj=None):
+        # Internal method with "protected access" to handle translation differences.
+        # This is only called when 'fluent_pages' is in the INSTALLED_APPS.
+        return mixed_reverse('faqquestion_index')
+
+
+    @classmethod
+    def get_actions_column(cls, faqquestion):
+        return u' '.join(cls._actions_column_icons(faqquestion))
+
+
+    @classmethod
+    def _actions_column_icons(cls, object):
+        actions = []
+        if cls.can_preview_object(object):
+            try:
+                url = object.get_absolute_url()
+            except (NoReverseMatch, TranslationDoesNotExist):
+                # A FaqQuestion is already added, but the URL can no longer be resolved.
+                # This can either mean that urls.py is missing a 'fluent_faq.urls' (unlikely),
+                # or that this is a PageTypeNotMounted exception because the "FAQ page" node was removed.
+                # In the second case, the edit page should still be reachable, and the "view on site" link will give an alert.
+                pass
+            else:
+                actions.append(
+                    u'<a href="{url}" title="{title}" target="_blank"><img src="{static}fluent_faq/img/admin/world.gif" width="16" height="16" alt="{title}" /></a>'.format(
+                        url=url, title=_('View on site'), static=settings.STATIC_URL)
+                )
+        return actions
+
+
+    @classmethod
+    def can_preview_object(cls, object):
+        """ Override whether the node can be previewed. """
+        #return hasattr(faqquestion, 'get_absolute_url') and faqquestion.is_published
+        return True
+
+
+    def actions_column(self, faqquestion):
+        return self.get_actions_column(faqquestion)
+
+    actions_column.allow_tags = True
+    actions_column.short_description = _('Actions')
---
+++ packages/django-fluent-faq/django_fluent_faq-0.9-py2-none-any.whl/fluent_faq/models.py
@@ -0,0 +1,161 @@
+from django.conf import settings
+from django.contrib.sites.models import Site
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+from fluent_faq import appsettings
+from parler.fields import TranslatedField
+from parler.models import TranslatableModel, TranslatedFields
+from parler.utils.context import switch_language
+from fluent_contents.models import PlaceholderField, ContentItemRelation
+from fluent_faq.urlresolvers import faq_reverse
+from fluent_faq.managers import FaqQuestionManager, FaqCategoryManager
+from fluent_utils.softdeps.taggit import TagsMixin
+
+
+class FaqBaseModel(TranslatableModel):
+    """
+    Shared functionality for published content.
+    """
+    # SEO
+    meta_keywords = models.CharField(_('keywords'), max_length=255, blank=True, default='', help_text=_("When this field is not filled in, the the tags will be used."))
+    meta_description = models.CharField(_('description'), max_length=255, blank=True, default='', help_text=_("When this field is not filled in, the contents or intro text will be used."))
+
+    # Metadata
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name=_('author'), editable=False)
+    creation_date = models.DateTimeField(_('creation date'), editable=False, auto_now_add=True)
+    modification_date = models.DateTimeField(_('last modification'), editable=False, auto_now=True)
+
+    parent_site = models.ForeignKey(Site, editable=False, default=Site.objects.get_current)
+
+    class Meta:
+        abstract = True
+
+    def get_absolute_url(self):
+        return self.default_url
+
+    @property
+    def default_url(self):
+        """
+        The internal implementation of :func:`get_absolute_url`.
+        This function can be used when overriding :func:`get_absolute_url` in the settings.
+        For example::
+
+            ABSOLUTE_URL_OVERRIDES = {
+                'fluent_faq.FaqQuestion': lambda o: "http://example.com" + o.default_url
+            }
+        """
+        with switch_language(self):
+            root = faq_reverse('faqquestion_index', ignore_multiple=True, language_code=self.get_current_language())
+            return root + self.get_relative_url()
+
+    @property
+    def url(self):
+        """
+        The URL of the entry, provided for template code.
+        """
+        return self.get_absolute_url()
+
+    def get_relative_url(self):
+        raise NotImplementedError("get_relative_url")
+
+
+class FaqCategory(FaqBaseModel):
+    """
+    Topic of the FAQ
+    """
+    # Be compatible with django-orderable table layout,
+    # unfortunately, there isn't a good canonical version of it yet.
+    order = models.PositiveIntegerField(db_index=True, blank=True, null=True)
+
+    title = TranslatedField(any_language=True)
+    translations = TranslatedFields(
+        title = models.CharField(_("title"), max_length=200),
+        slug = models.SlugField(_("slug")),
+    )
+
+    objects = FaqCategoryManager()
+
+    class Meta:
+        verbose_name = _("FAQ Category")
+        verbose_name_plural = _("FAQ Categories")
+        ordering = ('order', 'creation_date')
+
+    def __unicode__(self):
+        # self.title is configured with any_language=True, so always returns a value.
+        return self.title
+
+    def get_relative_url(self):
+        return u'{0}/'.format(self.slug)
+
+    @property
+    def faq_questions(self):
+        """
+        Fetch the active FAQ questions in this category.
+        """
+        return self.questions.active_translations()
+
+
+
+class FaqQuestion(TagsMixin, FaqBaseModel):
+    """
+    Category in the FAQ.
+    """
+    # This is a separate model instead of using django-categories because:
+    # - content needs to be placed on the category.
+    # - the title and slug can be translated!
+
+    # Be compatible with django-orderable table layout,
+    # unfortunately, there isn't a good canonical version of it yet.
+    order = models.PositiveIntegerField(db_index=True, blank=True, null=True)
+
+    title = TranslatedField(any_language=True)
+    translations = TranslatedFields(
+        title = models.CharField(_("title"), max_length=200),
+        slug = models.SlugField(_("slug")),
+    )
+    contents = PlaceholderField("faq_answer", verbose_name=_("answer"))
+    contentitem_set = ContentItemRelation()  # this makes sure the admin can find all deleted objects too.
+
+    # Organisation
+    category = models.ForeignKey(FaqCategory, verbose_name=_("Category"), related_name='questions')
+
+    objects = FaqQuestionManager()
+
+    class Meta:
+        verbose_name = _("FAQ Question")
+        verbose_name_plural = _("FAQ Questions")
+        ordering = ('order', 'creation_date')
+
+    def __unicode__(self):
+        # self.title is configured with any_language=True, so always returns a value.
+        return self.title
+
+    def get_relative_url(self):
+        """
+        Return the link path from the archive page.
+        """
+        # Return the link style, using the permalink style setting.
+        return u'{0}{1}/'.format(self.category.get_relative_url(), self.slug)
+
+    def similar_objects(self, num=None, **filters):
+        """
+        Find similar objects using related tags.
+        """
+        #TODO: filter appsettings.FLUENT_FAQ_FILTER_SITE_ID:
+        #    filters.setdefault('parent_site', self.parent_site_id)
+
+        # FIXME: Using super() doesn't work, calling directly.
+        return TagsMixin.similar_objects(self, num=num, **filters)
+
+
+def _register_anyurlfield_type():
+    try:
+        from any_urlfield.models import AnyUrlField
+        from any_urlfield.forms.widgets import SimpleRawIdWidget
+    except ImportError:
+        pass
+    else:
+        AnyUrlField.register_model(FaqQuestion, widget=SimpleRawIdWidget(FaqQuestion))
+
+if 'any_urlfield' in settings.INSTALLED_APPS:
+    _register_anyurlfield_type()
