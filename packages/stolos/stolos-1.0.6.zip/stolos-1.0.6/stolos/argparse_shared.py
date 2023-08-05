cb79df2c11cdd57d1e228e3ee759bb319656c7a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:47 +0000

    Add package stolos/1.0.6/stolos-1.0.6.zip

---
+++ packages/stolos/stolos-1.0.6.zip/stolos-1.0.6/stolos/argparse_shared.py
@@ -0,0 +1,29 @@
+"""
+We leverage argparse_tools to manage how arguments are passed in from
+the command-line.  This file contains argparse options that may be shared.
+"""
+import os
+from argparse_tools import (
+    build_arg_parser, add_argument, group, mutually_exclusive, lazy_kwargs)
+import argparse
+
+
+# This code block exists for linting
+argparse
+build_arg_parser
+add_argument
+group
+mutually_exclusive
+lazy_kwargs
+
+
+def zookeeper_hosts(parser):
+    parser.add_argument(
+        '--zookeeper_hosts', default=os.environ["ZOOKEEPER_HOSTS"])
+
+
+@lazy_kwargs
+def app_name(parser,
+             help='The name of a task whose status is tracked with ZooKeeper',
+             **kwargs):
+    parser.add_argument('-a', '--app_name', help=help, **kwargs)
