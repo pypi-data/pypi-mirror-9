b2bd1e13d231eb5ef61b25bf7b34404aa3c9f082
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:27 +0000

    Add package copytext/0.1.6/copytext-0.1.6.tar.gz

---
+++ packages/copytext/copytext-0.1.6.tar.gz/copytext-0.1.6/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='copytext',
+    version='0.1.6',
+    description='A library for accessing a spreadsheet as a native Python object suitable for templating.',
+    long_description=open('README').read(),
+    author='NPR Visuals Team',
+    author_email='nprapps@npr.org',
+    url='http://copytext.readthedocs.org/',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    py_modules=['copytext'],
+    install_requires = [
+        'openpyxl>=1.8.5',
+        'MarkupSafe>=0.21'
+    ]
+)
