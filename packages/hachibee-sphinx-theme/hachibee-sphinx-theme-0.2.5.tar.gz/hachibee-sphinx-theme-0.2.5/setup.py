09d68c32329dc9036588516c716d2902fb98b253
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:43 +0000

    Add package hachibee-sphinx-theme/0.2.5/hachibee-sphinx-theme-0.2.5.tar.gz

---
+++ packages/hachibee-sphinx-theme/hachibee-sphinx-theme-0.2.5.tar.gz/hachibee-sphinx-theme-0.2.5/setup.py
@@ -0,0 +1,30 @@
+import os
+from setuptools import setup, find_packages
+
+DESCRIPTION = open(
+    os.path.join(os.path.dirname(__file__), 'README.rst')).read().strip()
+
+classifiers = [
+    "Development Status :: 4 - Beta",
+    "Environment :: Web Environment",
+    "License :: OSI Approved :: MIT License",
+    "Programming Language :: Python",
+    "Topic :: Software Development :: Documentation",
+]
+
+setup(
+    name='hachibee-sphinx-theme',
+    version='0.2.5',
+    author='OGURA_Daiki',
+    author_email='8hachibee125@gmail.com',
+    url='https://github.com/hachibeeDI/Sphinx-hachibee-theme',
+    description='a simple sphinx theme',
+    long_description=DESCRIPTION,
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    license="MIT",
+    include_package_data=True,
+    entry_points='',
+    keywords=['sphinx', 'sphinx.theme', ],
+    classifiers=classifiers,
+)
