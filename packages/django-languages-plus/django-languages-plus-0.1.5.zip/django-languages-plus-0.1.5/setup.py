0daa9f65502bf9990b680f67d52d5f201a747deb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:09 +0000

    Add package django-languages-plus/0.1.5/django-languages-plus-0.1.5.zip

---
+++ packages/django-languages-plus/django-languages-plus-0.1.5.zip/django-languages-plus-0.1.5/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='django-languages-plus',
+    version='0.1.5',
+    author='Andrew Cordery',
+    author_email='cordery@gmail.com',
+    packages=find_packages(),
+    url='https://github.com/cordery/django-languages-plus',
+    zip_safe=False,
+    include_package_data=True,
+    license='LICENSE.txt',
+    description='A django model & fixture containing common languages and culture codes',
+    long_description=open('README.rst').read(),
+    install_requires=[
+        "Django >= 1.2",
+    ],
+)
