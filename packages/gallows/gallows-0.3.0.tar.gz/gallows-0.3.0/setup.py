fc2229eb520b777bcbdb436ece6bd5b6f7c83984
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:13 +0000

    Add package gallows/0.3.0/gallows-0.3.0.tar.gz

---
+++ packages/gallows/gallows-0.3.0.tar.gz/gallows-0.3.0/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='gallows',
+      version='0.3.0',
+      description=("The word game Hangman based on 'Invent Your "
+                   "Own Computer Games with Python'."),
+      author='Vijay Kumar B.',
+      author_email='vijaykumar@bravegnu.org',
+      url='http://github.com/chennaipy/hangman',
+      license="BSD 2-Clause",
+      scripts=['gallows.py'],
+      install_requires=[
+          'six',
+      ],
+      tests_require=[
+          'unittest2',
+          'mock',
+      ],
+      test_suite="test"
+      )
