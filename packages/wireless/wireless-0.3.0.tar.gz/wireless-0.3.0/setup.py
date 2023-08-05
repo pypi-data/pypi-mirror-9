38ebce59ea3796e7efcdd1b04ff9b4ac93c8eb05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:49 +0000

    Add package wireless/0.3.0/wireless-0.3.0.tar.gz

---
+++ packages/wireless/wireless-0.3.0.tar.gz/wireless-0.3.0/setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+# auto-convert README.md
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+except (ImportError, OSError):
+    # we'll just use the poorly formatted Markdown file instead
+    long_description = open('README.md').read()
+
+setup(
+    name='wireless',
+    version='0.3.0',
+    description='A dead simple, cross-platform Python library to connect to ' +
+    'wireless networks.',
+    long_description=long_description,
+    url='https://github.com/joshvillbrandt/wireless',
+    author='Josh Villbrandt',
+    author_email='josh@javconcepts.com',
+    license=open('LICENSE').read(),
+    packages=['wireless'],
+    setup_requires=[
+        'tox',
+        'nose',
+        'flake8'
+    ],
+    install_requires=[
+    ],
+    scripts=[],
+    test_suite='tests',
+    zip_safe=False
+)
