dd13fd6f0618de7bd61129e16e051e6d5c5b652a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:51 +0000

    Add package revu/0.2/revu-0.2.tar.gz

---
+++ packages/revu/revu-0.2.tar.gz/revu-0.2/setup.py
@@ -0,0 +1,26 @@
+from revu import __appname__, __version__
+from setuptools import setup
+
+
+long_description = ""
+
+setup(
+    name=__appname__,
+    version=__version__,
+    packages=[
+        'revu',
+        'revu.queues',
+    ],
+    author="Paul Tagliamonte",
+    author_email="tag@pault.ag",
+    long_description=long_description,
+    description='n/a',
+    license="Expat",
+    url="http://pault.ag/",
+    platforms=['any'],
+    entry_points={
+        'console_scripts': [
+            'revu = revu.cli:main',
+        ]
+    }
+)
