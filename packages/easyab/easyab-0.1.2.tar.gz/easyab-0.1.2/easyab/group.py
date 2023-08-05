f3474528ef5cf6c076462d7a5bb9b7b96cdab157
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:03 +0000

    Add package easyab/0.1.2/easyab-0.1.2.tar.gz

---
+++ packages/easyab/easyab-0.1.2.tar.gz/easyab-0.1.2/easyab/group.py
@@ -0,0 +1,12 @@
+'''
+Created on: Dec 04, 2014
+
+@author: qwang
+'''
+
+def group_user(locale, did):
+    '''
+    Use did to group user, return number between 1 to 16 as group index.
+    '''
+    did_hash = hash(did)
+    return did_hash % 16 + 1
---
+++ packages/easyab/easyab-0.1.2.tar.gz/easyab-0.1.2/setup.py
@@ -0,0 +1,17 @@
+import os
+from setuptools import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "easyab",
+    version = "0.1.2",
+    author = "easyab",
+    author_email = "easyab@bainainfo.com",
+    description = "An A/B Testing framework",
+    long_description = read("README.txt"),
+    license = "BSD",
+    keywords = "a/b testing",
+    packages = ['easyab'],
+)
