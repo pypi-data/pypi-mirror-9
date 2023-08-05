4eba2e58d592f93e17d94a521d3252d143aec433
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:16 +0000

    Add package gphoto2/0.11.1/gphoto2-0.11.1.tar.gz

---
+++ packages/gphoto2/gphoto2-0.11.1.tar.gz/gphoto2-0.11.1/src/swig-gp2.4/__init__.py
@@ -0,0 +1,14 @@
+__version__ = "0.11.1"
+
+from gphoto2.gphoto2_abilities_list import *
+from gphoto2.gphoto2_camera import *
+from gphoto2.gphoto2_context import *
+from gphoto2.gphoto2_file import *
+from gphoto2.gphoto2_filesys import *
+from gphoto2.gphoto2_list import *
+from gphoto2.gphoto2_port import *
+from gphoto2.gphoto2_port_info_list import *
+from gphoto2.gphoto2_port_log import *
+from gphoto2.gphoto2_result import *
+from gphoto2.gphoto2_version import *
+from gphoto2.gphoto2_widget import *
