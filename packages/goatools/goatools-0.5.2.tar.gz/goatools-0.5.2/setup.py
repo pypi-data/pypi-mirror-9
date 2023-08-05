b593bb2283f544f68cb2b1084963ce7b58c222e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:19 +0000

    Add package goatools/0.5.2/goatools-0.5.2.tar.gz

---
+++ packages/goatools/goatools-0.5.2.tar.gz/goatools-0.5.2/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+from setuptools import setup
+from glob import glob
+
+classifiers = [
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Science/Research',
+    'License :: OSI Approved :: BSD License',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 2',
+    'Topic :: Scientific/Engineering :: Bio-Informatics',
+    ]
+
+exec(open("goatools/version.py").read())
+setup(
+    name="goatools",
+    version=__version__,
+    author='Haibao Tang',
+    author_email='tanghaibao@gmail.com',
+    packages=['goatools'],
+    scripts=glob('scripts/*.py'),
+    license='BSD',
+    classifiers=classifiers,
+    url='http://github.com/tanghaibao/goatools',
+    description="Python scripts to find enrichment of GO terms",
+    long_description=open("README.rst").read(),
+    install_requires=['fisher', 'pygraphviz']
+    )
