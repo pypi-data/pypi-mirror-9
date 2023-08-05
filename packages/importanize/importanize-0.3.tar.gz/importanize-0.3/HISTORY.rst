c407fd9401686572a34fd5b087119f4c8a2efa26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:51 +0000

    Add package importanize/0.3/importanize-0.3.tar.gz

---
+++ packages/importanize/importanize-0.3.tar.gz/importanize-0.3/HISTORY.rst
@@ -0,0 +1,51 @@
+.. :changelog:
+
+History
+-------
+
+0.3 (2015-01-18)
+~~~~~~~~~~~~~~~~
+
+* Using tokens to parse Python files. As a result this allows to
+  fix how comments are handled
+  (see `#21 <https://github.com/miki725/importanize/issues/21>`_ for example)
+
+0.2 (2014-10-30)
+~~~~~~~~~~~~~~~~
+
+* New "exclude" config which allows to skip files
+* Presetving origin file new line characters
+* Traversing parent paths to find importanize config file
+
+0.1.4 (2014-10-12)
+~~~~~~~~~~~~~~~~~~
+
+* Multiple imports (e.g. ``import a, b``) are normalized
+  instead of exiting
+* Multiple imports with the same stem are combined into
+  single import statement
+  (see `#17 <https://github.com/miki725/importanize/issues/17>`_ for example)
+
+0.1.3 (2014-09-15)
+~~~~~~~~~~~~~~~~~~
+
+* Fixed where single line triple-quote docstrings would cause
+  none of the imports to be recognized
+
+0.1.2 (2014-09-15)
+~~~~~~~~~~~~~~~~~~
+
+* Fixed where import leafs were not properly sorted for
+  mixed case (aka CamelCase)
+
+0.1.1 (2014-09-07)
+~~~~~~~~~~~~~~~~~~
+
+* Ignoring comment blocks when parsing for imports
+* Fixed bug when imports start on a first line,
+  extra lines were being added to the file.
+
+0.1.0 (2014-09-07)
+~~~~~~~~~~~~~~~~~~
+
+* First release on PyPI.
---
+++ packages/importanize/importanize-0.3.tar.gz/importanize-0.3/importanize/utils.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+from __future__ import print_function, unicode_literals
+import operator
+import os
+import sys
+from contextlib import contextmanager
+from importlib import import_module
+
+
+@contextmanager
+def ignore_site_packages_paths():
+    paths = sys.path[:]
+    # remove working directory so that all
+    # local imports fail
+    if os.getcwd() in sys.path:
+        sys.path.remove(os.getcwd())
+    # remove all third-party paths
+    # so that only stdlib imports will succeed
+    sys.path = list(set(filter(
+        None,
+        filter(lambda i: all(('site-packages' not in i,
+                              'python' in i or 'pypy' in i)), sys.path)
+    )))
+    yield
+    sys.path = paths
+
+
+def is_std_lib(module):
+    if not module:
+        return False
+
+    if module in sys.builtin_module_names:
+        return True
+
+    with ignore_site_packages_paths():
+        imported_module = sys.modules.pop(module, None)
+        try:
+            import_module(module)
+        except ImportError:
+            return False
+        else:
+            return True
+        finally:
+            if imported_module:
+                sys.modules[module] = imported_module
+
+
+def list_strip(data):
+    """
+    Return list of stripped strings from given list
+    """
+    return list(map(operator.methodcaller('strip'), data))
+
+
+def read(path):
+    with open(path, 'rb') as fid:
+        return fid.read().decode('utf-8')
+
+
+def list_split(iterable, split):
+    segment = []
+
+    for i in iterable:
+        if i == split:
+            yield segment
+            segment = []
+        else:
+            segment.append(i)
+
+    if segment:
+        yield segment
