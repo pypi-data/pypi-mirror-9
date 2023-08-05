2e006ee037305faa04f1eedc68b06dc2641388ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:57 +0000

    Add package lazylights/2.0.0rc1/lazylights-2.0.0rc1.tar.gz

---
+++ packages/lazylights/lazylights-2.0.0rc1.tar.gz/lazylights-2.0.0rc1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='lazylights',
+      version='2.0.0rc1',
+      author='Matt Papi',
+      author_email='mmpapi@gmail.com',
+      py_modules=['lazylights'],
+      scripts=[],
+      url='http://github.com/mpapi/lazylights/',
+      license='LICENSE',
+      description='Python tools for controlling Lifx bulbs.',
+      install_requires=[])
