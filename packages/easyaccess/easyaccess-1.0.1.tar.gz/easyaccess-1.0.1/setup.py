86434f4a69938a06361f46b33f1f1ec9292eb470
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:26 +0000

    Add package easyaccess/1.0.1/easyaccess-1.0.1.tar.gz

---
+++ packages/easyaccess/easyaccess-1.0.1.tar.gz/easyaccess-1.0.1/setup.py
@@ -0,0 +1,28 @@
+import sys
+import os
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+prjdir = os.path.dirname(__file__)
+
+def read(filename):
+    return open(os.path.join(prjdir, filename)).read()
+
+extra_link_args = []
+libraries = []
+library_dirs = []
+include_dirs = []
+setup(
+    name='easyaccess',
+    version='1.0.1',
+    author='Matias Carrasco Kind',
+    author_email='mcarras2@illinois.edu',
+    scripts=['easyaccess'],
+    py_modules=['easyaccess','config'],
+    license='NCSA',
+    description='Easy Access to access DES DB',
+    long_description=read('README.md'),
+    url='https://github.com/mgckind/easyaccess',
+    install_requires=['pandas','termcolor','pyfits >= 3.3','cx_Oracle'],
+)
