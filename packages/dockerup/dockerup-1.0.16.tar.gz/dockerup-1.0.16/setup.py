9c75e004229fddd3e94bea0c6cf68d85721dc005
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:53 +0000

    Add package dockerup/1.0.16/dockerup-1.0.16.tar.gz

---
+++ packages/dockerup/dockerup-1.0.16.tar.gz/dockerup-1.0.16/setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+
+setup(name='dockerup',
+	description='Docker container bootstrapper',
+	long_description="""
+	Docker container bootstrapper that synchronizes state from a configuration file.
+	It can load configuration from EC2 user-data, and automatically handles container
+	updates and cleanup.
+	""",
+	author='Jeremy Jongsma',
+	author_email='jeremy@barchart.com',
+	url='https://github.com/barchart/dockerup',
+	version='1.0.16',
+	packages=['dockerup'],
+	scripts=['bin/dockerup'],
+	data_files=[
+		('/etc/dockerup', ['etc/dockerup.conf']),
+		('/etc/dockerup/containers.d', ['etc/dockerup.json.sample']),
+	],
+	install_requires=['docker-py>=0.7.0'])
