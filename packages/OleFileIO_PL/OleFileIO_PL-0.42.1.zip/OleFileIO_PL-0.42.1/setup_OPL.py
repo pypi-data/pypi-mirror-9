23f4934f08fd1ba1f562ef60a9edbed859ae416c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:25 +0000

    Add package OleFileIO_PL/0.42.1/OleFileIO_PL-0.42.1.zip

---
+++ packages/OleFileIO_PL/OleFileIO_PL-0.42.1.zip/OleFileIO_PL-0.42.1/setup_OPL.py
@@ -0,0 +1,17 @@
+# setup script to upload olefile as OleFileIO_PL to PyPI
+
+# 2014-10-01 Philippe Lagadec
+
+import setup
+
+setup.name = 'OleFileIO_PL'
+
+# setup.long_desc = """
+# IMPORTANT NOTE:
+# ===============
+#
+# Since v0.40, OleFileIO_PL has been renamed to olefile. Please use `olefile <https://pypi.python.org/pypi/olefile/>`_ instead of OleFileIO_PL.
+#
+# """ + setup.long_desc
+
+setup.main()
