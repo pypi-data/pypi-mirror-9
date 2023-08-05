e4b4fe52da810535b30ff6a13210a9461f9c3612
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:23 +0000

    Add package geoshape/1.3/geoshape-1.3.tar.gz

---
+++ packages/geoshape/geoshape-1.3.tar.gz/geoshape-1.3/geoshape/__init__.py
@@ -0,0 +1,10 @@
+try:
+    from celery_app import app as celery_app # flake8: noqa
+except ImportError:
+    pass
+
+__version__ = (1, 3, 0, 'final', 0)
+
+def get_version():
+    import geoshape.version
+    return geoshape.version.get_version(__version__)
