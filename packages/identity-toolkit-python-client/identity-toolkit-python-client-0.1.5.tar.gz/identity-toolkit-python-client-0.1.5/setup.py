d7ae0f807cf29f5b60e8e9b78f2cd667c4ca1424
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:36 +0000

    Add package identity-toolkit-python-client/0.1.5/identity-toolkit-python-client-0.1.5.tar.gz

---
+++ packages/identity-toolkit-python-client/identity-toolkit-python-client-0.1.5.tar.gz/identity-toolkit-python-client-0.1.5/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+install_requires = [
+  'oauth2client>=1.3.2',
+  'pyOpenSSL==0.14',
+  'simplejson>=2.3.2',
+  ]
+
+packages = ['identitytoolkit',]
+
+setup(
+  name = 'identity-toolkit-python-client',
+  packages = packages,
+  install_requires = install_requires,
+  license="Apache 2.0",
+  version = '0.1.5',
+  description = 'Google Identity Toolkit python client library',
+  author = 'Jin Liu',
+  url = 'https://github.com/google/identity-toolkit-python-client',
+  download_url = 'https://github.com/google/identity-toolkit-python-client/archive/master.zip',
+  keywords = ['identity', 'google', 'login', 'toolkit'], # arbitrary keywords
+  classifiers = [],
+)
