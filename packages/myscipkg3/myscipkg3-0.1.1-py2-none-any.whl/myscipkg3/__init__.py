2755395bf700554ae65e7fbf64ad956fc0bd0ab2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:08 +0000

    Add package myscipkg3/0.1.1/myscipkg3-0.1.1-py2-none-any.whl

---
+++ packages/myscipkg3/myscipkg3-0.1.1-py2-none-any.whl/myscipkg3/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+============================
+myscipkg3
+============================
+"""
+
+import numpy
+
+# Versioneer versioning
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
