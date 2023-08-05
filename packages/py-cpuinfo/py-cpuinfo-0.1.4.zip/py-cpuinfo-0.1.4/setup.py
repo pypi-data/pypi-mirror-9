dd5d0edb5b58f8827c27643da0aa6e4344a51585
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:36 +0000

    Add package py-cpuinfo/0.1.4/py-cpuinfo-0.1.4.zip

---
+++ packages/py-cpuinfo/py-cpuinfo-0.1.4.zip/py-cpuinfo-0.1.4/setup.py
@@ -0,0 +1,34 @@
+# Copyright (c) 2014, Matthew Brennan Jones <matthew.brennan.jones@gmail.com>
+# Py-cpuinfo is a Python module to show the cpuinfo of a processor
+# It uses a MIT style license
+# It is hosted at: https://github.com/workhorsy/py-cpuinfo
+
+import os
+from setuptools import setup
+
+with open(os.path.join(os.getcwd(), 'README.rst'), 'r') as f:
+    readme_content = f.read()
+
+setup(
+    name = "py-cpuinfo",
+    version = "0.1.4",
+    author = "Matthew Brennan Jones",
+    author_email = "matthew.brennan.jones@gmail.com",
+    description = "Get CPU info with pure Python 2 & 3",
+    long_description=readme_content,
+    license = "MIT",
+    url = "https://github.com/workhorsy/py-cpuinfo",
+    packages=['cpuinfo'],
+    entry_points = {
+        'console_scripts': ['cpuinfo = cpuinfo.cpuinfo:main'],
+    },
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3"
+    ],
+)
+
