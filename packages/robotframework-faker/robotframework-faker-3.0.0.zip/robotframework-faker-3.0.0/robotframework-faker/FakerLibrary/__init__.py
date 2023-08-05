17c41877dba832e3d1ba115f509cb4e0ec7c72f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:25 +0000

    Add package robotframework-faker/3.0.0/robotframework-faker-3.0.0.zip

---
+++ packages/robotframework-faker/robotframework-faker-3.0.0.zip/robotframework-faker-3.0.0/robotframework-faker/FakerLibrary/__init__.py
@@ -0,0 +1,15 @@
+__author__ = 'gkisel'
+
+import pkg_resources
+
+from keywords import FakerKeywords
+
+
+__version__ = pkg_resources.get_distribution("robotframework-faker").version
+
+
+class FakerLibrary(FakerKeywords):
+    """
+
+    """
+    ROBOT_LIBRARY_SCOPE = 'GLOBAL'
