860dd8731937532fb69a081d65b75fd53fa2fe03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:13 +0000

    Add package install-freedesktop/0.1.2/install_freedesktop-0.1.2.tar.gz

---
+++ packages/install-freedesktop/install_freedesktop-0.1.2.tar.gz/install_freedesktop-0.1.2/CHANGES.rst
@@ -0,0 +1,19 @@
+Changes
+=======
+
+0.1.2 (2015-02-01)
+------------------
+
+* Simplify subcommand invocation (hopefully more correct and less fragile)
+
+0.1.1 (2015-01-26)
+------------------
+
+* Use README/CHANGES in long_description for PyPI
+* Fix syntax in README examples
+* Support older setuptools (CentOS 6)
+
+0.1.0 (2015-01-26)
+------------------
+
+* Initial release
---
+++ packages/install-freedesktop/install_freedesktop-0.1.2.tar.gz/install_freedesktop-0.1.2/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup
+import codecs
+
+with codecs.open('README.rst', encoding='utf-8') as f:
+    README = f.read()
+with codecs.open('CHANGES.rst', encoding='utf-8') as f:
+    CHANGES = f.read()
+
+setup(
+    name='install_freedesktop',
+    version='0.1.2',
+    description='Setuptools extension to install freedesktop.org app icons',
+    long_description=README + '\n\n' + CHANGES,
+    author='Jacob Welsh',
+    author_email='jacob@welshcomputing.com',
+    url='https://github.com/welshjf/install_freedesktop',
+    license='Apache License 2.0',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Topic :: Desktop Environment',
+        'Topic :: Software Development :: Build Tools',
+    ],
+    py_modules=['install_freedesktop'],
+    install_requires=[
+        'setuptools',
+    ],
+    entry_points={
+        'distutils.commands': [
+            'install_desktop = install_freedesktop:install_desktop',
+        ],
+        'distutils.setup_keywords': [
+            'desktop_entries = install_freedesktop:check_desktop_entries',
+        ],
+    },
+)
