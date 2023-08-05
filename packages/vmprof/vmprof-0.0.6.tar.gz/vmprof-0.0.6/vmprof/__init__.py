c05214872c2025d8ff7098e6915ac2faa32feef3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:34 +0000

    Add package vmprof/0.0.6/vmprof-0.0.6.tar.gz

---
+++ packages/vmprof/vmprof-0.0.6.tar.gz/vmprof-0.0.6/vmprof/__init__.py
@@ -0,0 +1,143 @@
+#import requests
+#import urlparse
+import json
+#import zlib
+#import base64
+import tempfile
+import logging
+import _vmprof
+import os, sys
+
+from vmprof.reader import read_prof, LibraryData
+
+from vmprof.addrspace import AddressSpace, Stats
+from vmprof.profiler import Profiler, read_profile
+
+IS_PYPY = hasattr(sys, 'pypy_translation_info')
+
+if not IS_PYPY:
+    _virtual_ips_so_far = None
+    _prof_fileno = -1
+
+    def enable(fileno, period_usec=-1):
+        global _prof_fileno
+        global _virtual_ips_so_far
+
+        def pack_virtual_ips(tup):
+            import struct
+
+            l = []
+            for k, v in tup:
+                l.append('\x02')
+                l.append(struct.pack('QQ', k, len(v)))
+                l.append(v)
+            return "".join(l)
+
+        _prof_fileno = fileno
+        if _virtual_ips_so_far is not None:
+            _vmprof.enable(fileno, period_usec,
+                           pack_virtual_ips(_virtual_ips_so_far))
+        else:
+            _vmprof.enable(fileno, period_usec)
+
+    def disable():
+        global _virtual_ips_so_far
+        global _prof_fileno
+
+        _vmprof.disable()
+        f = os.fdopen(os.dup(_prof_fileno))
+        f.seek(0)
+        _virtual_ips_so_far = read_prof(f, virtual_ips_only=True)
+        _prof_fileno = -1
+
+else:
+    def enable(fileno, period_usec=-1):
+        _vmprof.enable(fileno, period_usec)
+
+    def disable():
+        _vmprof.disable()
+
+class xxx_vmprof(object):
+
+    def __init__(self, host, port=80, logger=None):
+        self.host = host
+        self.port = port
+        self.logger = logger or self.get_default_logger()
+
+    def get_default_logger(self):
+        default_logger = logging.getLogger(self.__class__.__name__)
+        default_logger.setLevel(logging.INFO)
+        default_logger.addHandler(logging.StreamHandler())
+        return default_logger
+
+    def validate_args(self, *args, **kwargs):
+        return True
+
+    def __call__(self, f):
+        def func(*args, **kwargs):
+            if not self.validate_args(*args, **kwargs):
+                return f(*args, **kwargs)
+
+            lib = ffi.dlopen(None)
+            prof_file = "%s.prof" % tempfile.mkstemp()[1]
+            prof_sym_file = "%s.sym" % prof_file
+
+            lib.vmprof_enable(prof_file, -1)
+            ret = f(*args, **kwargs)
+            lib.vmprof_disable()
+
+            prof = open(prof_file, 'rb').read()
+            prof_sym = open(prof_sym_file, 'rb').read()
+
+            period, profiles, symmap = read_prof(prof)
+            libs = read_ranges(symmap)
+
+            for lib in libs:
+                lib.read_object_data()
+            libs.append(
+                LibraryData(
+                    '<virtual>',
+                    0x8000000000000000L,
+                    0x8fffffffffffffffL,
+                    True,
+                    symbols=read_sym_file(prof_sym))
+            )
+            addrspace = AddressSpace(libs)
+            filtered_profiles, addr_set = addrspace.filter_addr(profiles)
+            d = {}
+            for addr in addr_set:
+                name, _ = addrspace.lookup(addr | 0x8000000000000000L)
+                d[addr] = name
+
+            data = zlib.compress(json.dumps({
+                'profiles': filtered_profiles, 'addresses': d
+            }))
+
+            response = requests.post(
+                "http://%s:%s/api/log/" % (self.host, self.port),
+                data={'data': base64.b64encode(data)}
+            )
+
+            self.logger.info("Log: %s" % response.json())
+
+            return ret
+
+        return func
+
+
+class DjangoVMPROF(xxx_vmprof):
+
+    def __init__(self, host, port=80, token="", logger=None):
+        self.host = host
+        self.port = port
+        self.token = token
+        self.logger = logger or self.get_default_logger()
+
+    def validate_args(self, environ, start_response):
+        qs = dict(urlparse.parse_qsl(environ.get('QUERY_STRING', '')))
+        token = qs.get('vmprof')
+        if token == self.token:
+            return True
+        return False
+
+
---
+++ packages/vmprof/vmprof-0.0.6.tar.gz/vmprof-0.0.6/vmprof/profiler.py
@@ -0,0 +1,72 @@
+
+import vmprof
+import tempfile
+from vmprof.addrspace import AddressSpace, Stats
+from vmprof.reader import read_prof, LibraryData
+
+class VMProfError(Exception):
+    pass
+
+class ProfilerContext(object):
+    done = False
+    
+    def __init__(self):
+        self.tmpfile = tempfile.NamedTemporaryFile()
+
+    def __enter__(self):
+        vmprof.enable(self.tmpfile.fileno(), 1000)
+
+    def __exit__(self, type, value, traceback):
+        vmprof.disable()
+        self.done = True
+
+# lib_cache is global on purpose
+def read_profile(prof_filename, lib_cache={}, extra_libs=None,
+                 virtual_only=True):
+    prof = open(prof_filename, 'rb')
+
+    period, profiles, virtual_symbols, libs = read_prof(prof)
+
+    if not virtual_only:
+        for i, lib in enumerate(libs):
+            if lib.name in lib_cache:
+                libs[i] = lib_cache[lib.name]
+            else:
+                lib.read_object_data(lib.start)
+                lib_cache[lib.name] = lib
+    libs.append(
+        LibraryData(
+            '<virtual>',
+            0x7000000000000000,
+            0x7fffffffffffffff,
+            True,
+            symbols=virtual_symbols)
+    )
+    if extra_libs:
+        libs += extra_libs
+    addrspace = AddressSpace(libs)
+    filtered_profiles, addr_set = addrspace.filter_addr(profiles, virtual_only)
+    d = {}
+    for addr in addr_set:
+        name, _, _ = addrspace.lookup(addr)
+        d[addr] = name
+    return Stats(filtered_profiles, d)
+
+class Profiler(object):
+    ctx = None
+
+    def __init__(self):
+        self._lib_cache = {}
+
+    def measure(self):
+        self.ctx = ProfilerContext()
+        return self.ctx
+
+    def get_stats(self):
+        if not self.ctx:
+            raise VMProfError("no profiling done")
+        if not self.ctx.done:
+            raise VMProfError("profiling in process")
+        res = read_profile(self.ctx.tmpfile.name)
+        self.ctx = None
+        return res
