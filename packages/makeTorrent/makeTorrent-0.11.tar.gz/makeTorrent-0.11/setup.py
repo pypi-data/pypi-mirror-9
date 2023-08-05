67ce89a1f45214024590a23551cf08683b88b446
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:54 +0000

    Add package makeTorrent/0.11/makeTorrent-0.11.tar.gz

---
+++ packages/makeTorrent/makeTorrent-0.11.tar.gz/makeTorrent-0.11/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+    name='makeTorrent',
+    version='0.11',
+    packages=['makeTorrent'],
+    install_requires=[
+        'bencode',
+    ],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    description='Basic library for creating torrents',
+    long_description=open('README.rst').read(),
+    author='Stewart Rutledge',
+    author_email='stew.rutledge@gmail.com',
+    url='https://github.com/stewrutledge/makeTorrent'
+)
