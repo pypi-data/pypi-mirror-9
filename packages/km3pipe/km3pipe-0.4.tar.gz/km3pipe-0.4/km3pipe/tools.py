8c05aa2f4b8fb48a5ebb301f3d459d60f877a6e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:55 +0000

    Add package km3pipe/0.4/km3pipe-0.4.tar.gz

---
+++ packages/km3pipe/km3pipe-0.4.tar.gz/km3pipe-0.4/km3pipe/tools.py
@@ -0,0 +1,66 @@
+# coding=utf-8
+# Filename: tools.py
+# pylint: disable=locally-disabled
+"""
+Some frequently used logic.
+
+"""
+from __future__ import division, absolute_import, print_function
+
+__author__ = 'tamasgal'
+
+from collections import namedtuple
+
+def unpack_nfirst(seq, nfirst):
+    """Unpack the nfrist items from the list and return the rest.
+
+    >>> a, b, c, rest = unpack_nfirst((1, 2, 3, 4, 5), 3)
+    >>> a, b, c
+    (1, 2, 3)
+    >>> rest
+    (4, 5)
+
+    """
+    iterator = iter(seq)
+    for item in xrange(nfirst):
+        yield next(iterator, None)
+    yield tuple(iterator)
+
+
+def split(string, callback=None):
+    """Split the string and execute the callback function on each part.
+
+    >>> string = "1 2 3 4"
+    >>> parts = split(string, int)
+    >>> parts
+    [1, 2, 3, 4]
+
+    """
+    if callback:
+        return [callback(i) for i in string.split()]
+    else:
+        return string.split()
+
+def namedtuple_with_defaults(typename, field_names, default_values=[]):
+    """Create a namedtuple with default values
+
+    >>> Node = namedtuple_with_defaults('Node', 'val left right')
+    >>> Node()
+    Node(val=None, left=None, right=None)
+    >>> Node = namedtuple_with_defaults('Node', 'val left right', [1, 2, 3])
+    >>> Node()
+    Node(val=1, left=2, right=3)
+    >>> Node = namedtuple_with_defaults('Node', 'val left right', {'right':7})
+    >>> Node()
+    Node(val=None, left=None, right=7)
+    >>> Node(4)
+    Node(val=4, left=None, right=7)
+    """
+    T = namedtuple(typename, field_names)
+    T.__new__.__defaults__ = (None,) * len(T._fields)
+    if isinstance(default_values, collections.Mapping):
+        prototype = T(**default_values)
+    else:
+        prototype = T(*default_values)
+    T.__new__.__defaults__ = tuple(prototype)
+    return T
