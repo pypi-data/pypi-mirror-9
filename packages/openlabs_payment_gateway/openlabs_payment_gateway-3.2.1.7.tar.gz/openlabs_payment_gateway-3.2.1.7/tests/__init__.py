3275d07ab6220f2eae6c2067df96039bc0477d53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:52 +0000

    Add package openlabs_payment_gateway/3.2.1.7/openlabs_payment_gateway-3.2.1.7.tar.gz

---
+++ packages/openlabs_payment_gateway/openlabs_payment_gateway-3.2.1.7.tar.gz/openlabs_payment_gateway-3.2.1.7/tests/__init__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+    test_views_depends
+
+    Test tryton views and fields dependency.
+
+    :copyright: (C) 2013-2014 by Openlabs Technologies & Consulting (P) Limited
+    :license: BSD, see LICENSE for more details.
+"""
+import unittest
+
+import trytond.tests.test_tryton
+from .test_view_depends import TestViewsDepends
+from test_transaction import TestTransaction
+
+
+def suite():
+    """
+    Define suite
+    """
+    test_suite = trytond.tests.test_tryton.suite()
+    test_suite.addTests([
+        unittest.TestLoader().loadTestsFromTestCase(TestViewsDepends),
+        unittest.TestLoader().loadTestsFromTestCase(TestTransaction),
+    ])
+    return test_suite
+
+if __name__ == '__main__':
+    unittest.TextTestRunner(verbosity=2).run(suite())
