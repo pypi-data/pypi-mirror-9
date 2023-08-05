840bb7c102efc7e7cd15f0485b9daa32a0d3e21c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:52 +0000

    Add package konf/1.1.1/konf-1.1.1.tar.gz

---
+++ packages/konf/konf-1.1.1.tar.gz/konf-1.1.1/setup.py
@@ -0,0 +1,40 @@
+import os
+from setuptools import setup
+
+version = "1.1.1"
+
+description = (
+    'Konf is a Python package which designed to simplify the use of variables in configuration files. '
+    'json and yaml supported out of the box. '
+)
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+long_description = read('README.rst')
+
+
+setup(name='konf',
+      author="Vladimir Chub",
+      author_email="vartagg@users.noreply.github.com",
+      url='http://github.com/vartagg/konf/',
+
+      description=description,
+      long_description=long_description,
+      keywords="konf,json,yaml,config",
+      license="BSD",
+      version=version,
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: BSD License',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3'
+      ],
+      packages=['konf'],
+      install_requires=['PyYAML', 'good'],
+      tests_require=['nose'],
+      test_suite='nose.collector',
+      entry_points={},
+)
