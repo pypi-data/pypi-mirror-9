52873573d526c2e378b82205a7cbb42ad6ce470b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:24 +0000

    Add package pyscrypt/1.6.2/pyscrypt-1.6.2.tar.gz

---
+++ packages/pyscrypt/pyscrypt-1.6.2.tar.gz/pyscrypt-1.6.2/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+LONG_DESCRIPTION = '''A very simple, pure-Python implementation of the scrypt
+password-based key derivation function and scrypt file format library with no
+dependencies beyond standard Python libraries. See README.md for API
+reference and details.'''
+
+setup(name = 'pyscrypt',
+      version = '1.6.2',
+      description = 'Pure-Python Implementation of the scrypt password-based key derivation function and scrypt file format library',
+      long_description = LONG_DESCRIPTION,
+      author = 'Richard Moore',
+      author_email = 'pyscrypt@ricmoo.com',
+      url = 'https://github.com/ricmoo/pyscrypt',
+      packages = ['pyscrypt'],
+      classifiers = [
+          'Topic :: Security :: Cryptography',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 3',
+      ],
+      license = "License :: OSI Approved :: MIT License",
+     )
---
+++ packages/pyscrypt/pyscrypt-1.6.2.tar.gz/pyscrypt-1.6.2/tests/run-tests-file.py
@@ -0,0 +1,95 @@
+import sys
+sys.path += '..'
+
+import os
+
+# Python 2 vs Python 3
+try:
+    from cStringIO import StringIO as StringIO
+except ImportError:
+    from io import BytesIO as StringIO
+
+try:
+    file
+
+except NameError:
+    file = open
+
+import pyscrypt
+from pyscrypt import ScryptFile
+
+print("Version: %s" % ".".join(str(p) for p in pyscrypt.VERSION))
+
+# Test decrypted output is equal to the input
+for text_length in [3, 16, 127, 128, 129, 1500]:
+    plaintext = os.urandom(text_length)
+
+    # Encrypt the text to memory
+    fp = StringIO()
+    sf = ScryptFile(fp = fp, password = b"password", N = 1024, r = 1, p = 1, mode = ScryptFile.MODE_WRITE)
+    sf.write(plaintext)
+    sf.finalize()
+    fp.seek(0)
+    crypttext = fp.getvalue()
+    fp.close()
+
+    # Decrypt the text from memory
+    fp = StringIO(crypttext)
+    sf = ScryptFile(fp = fp, password = b"password", mode = ScryptFile.MODE_READ)
+    decrypted = sf.read()
+    sf.close()
+
+    result = {True: "pass", False: "fail"}[decrypted == plaintext]
+    print("Test Encrypt/Decrypt: text_length=%s result=%s valid=%s" % (text_length, result, sf.valid))
+
+
+# Generate some files to make sure the tarsnap scrypt utility can read them
+for length in (10, 100, 1000):
+    path_scrypt = '/tmp/test-%d.scrypt' % length
+    path_text = '/tmp/test-%d.txt' % length
+    text = b"Hello world" * length
+    sf = ScryptFile(file(path_scrypt, ScryptFile.MODE_WRITE), b"password", 1024, 1, 1)
+    sf.write(text)
+    sf.close()
+    file(path_text, ScryptFile.MODE_WRITE).write(text)
+    print("Created %s and %s. Check with tarsnap." % (path_scrypt, path_text))
+
+text = b"Hello world" * 100
+with ScryptFile('/tmp/test-with.scrypt', b'password', 1024, 1, 1) as f:
+    f.write(text)
+with ScryptFile('/tmp/test-with.scrypt', b'password') as f:
+    new_text = f.read()
+result = {True: "pass", False: "fail"}[text == new_text]
+print("Test With filename: result=%s" % result)
+
+# Open some files created with the tarsnap utility and read them
+for filename in ('test1', 'test2'):
+    path_scrypt = os.path.join('tests/', filename + '.scrypt')
+    path_text = os.path.join('tests/', filename + '.txt')
+
+    valid = ScryptFile.verify_file(file(path_scrypt, ScryptFile.MODE_READ), b'password')
+    result = {True: "pass", False: "fail"}[valid]
+    print("Test Verify: filename=%s result=%s" % (path_scrypt, result))
+
+    for test in (0, 1, 2):
+        sf = ScryptFile(file(path_scrypt, ScryptFile.MODE_READ), b'password')
+        f = file(path_text, ScryptFile.MODE_READ)
+
+        # Test small read (smaller than a block)
+        if test == 0:
+            content_scrypt = sf.read(5)
+            content_text = f.read(5)
+
+        # Test large read (larger than a block)
+        elif test == 1:
+            content_scrypt = sf.read(1005)
+            content_text = f.read(1005)
+
+        # Test full read
+        elif test == 2:
+            content_scrypt = sf.read()
+            content_text = f.read()
+
+        result = {True: "pass", False: "fail"}[content_scrypt == content_text]
+        print("Test Decrypt: dec(%r) == %r result=%s valid=%s" % (path_scrypt, path_text, result, sf.valid))
+
