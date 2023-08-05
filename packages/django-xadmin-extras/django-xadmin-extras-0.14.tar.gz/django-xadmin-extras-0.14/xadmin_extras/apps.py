90bc45189af2fae34fcccdbe58494824fdcb373d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:04 +0000

    Add package django-xadmin-extras/0.14/django-xadmin-extras-0.14.tar.gz

---
+++ packages/django-xadmin-extras/django-xadmin-extras-0.14.tar.gz/django-xadmin-extras-0.14/xadmin_extras/apps.py
@@ -0,0 +1,18 @@
+# coding=utf-8
+
+
+class AdminAppMixin(object):
+
+    def init_menu(self):
+        """Add custom menu entries to the menu displayed for this app
+
+        Return a list of dicts, each dict will be a entry for the submenu of
+        the app:
+            {'url': '/admin/.../', 'icon': 'bolt', 'title': 'Custom'}
+        also 'perm' and 'order' keys can be added.
+
+        """
+        return []
+
+    def has_menu_permission(self, obj=None):
+        return True
