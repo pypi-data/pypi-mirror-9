bea000dd37c1767817d594e7ece434bfbe7f12d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:47 +0000

    Add package pycrunchbase/0.2.0/pycrunchbase-0.2.0-py2.py3-none-any.whl

---
+++ packages/pycrunchbase/pycrunchbase-0.2.0-py2.py3-none-any.whl/pycrunchbase/resource/relationship.py
@@ -0,0 +1,42 @@
+import six
+
+from .page import Page
+from .pageitem import NonePageItemSingleton
+
+
+class Relationship(Page):
+    """A Relationhip represents relationship between a Node and interesting
+    information regarding the Node.
+
+    This is a summary returned alongside the Node details information, e.g.
+    ad call to /organizatin/example will return many properties and many
+    relationships.
+
+    To get more details of this relationship, call :class:`CrunchBase`'s
+    :meth:`more`.
+    """
+
+    def __str__(self):
+        return ("{total} {name} {items}").format(
+            total=self.total_items,
+            name=self.name,
+            items=self.items,
+        )
+
+
+@six.python_2_unicode_compatible
+class NoneRelationship(Relationship):
+    def __init__(self):
+        super(NoneRelationship, self).__init__(
+            None, {'paging': {}, 'items': {}})
+
+    def get(self, _):
+        return NonePageItemSingleton
+
+    def __len__(self):
+        return 0
+
+    def __str__(self):
+        return "NoneRelationship"
+
+NoneRelationshipSingleton = NoneRelationship()
