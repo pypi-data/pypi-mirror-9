b7c5ba5235749f28fba2a3fba22171a88549c7a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:19 +0000

    Add package django-facebook-auth/3.7.3/django-facebook-auth-3.7.3.tar.gz

---
+++ packages/django-facebook-auth/django-facebook-auth-3.7.3.tar.gz/django-facebook-auth-3.7.3/facebook_auth/utils.py
@@ -0,0 +1,23 @@
+from django.conf import settings
+import facepy
+
+from facebook_auth import graph_api
+
+GRAPH_MAX_TRIES = 3
+
+
+def get_from_graph_api(graphAPI, query):
+    for i in range(GRAPH_MAX_TRIES):
+        try:
+            return graphAPI.get(query)
+        except facepy.FacepyError as e:
+            if i == GRAPH_MAX_TRIES - 1 or getattr(e, 'code', None) != 1:
+                raise
+
+
+def get_application_graph(version):
+    token = (facepy.utils
+             .get_application_access_token(settings.FACEBOOK_APP_ID,
+                                           settings.FACEBOOK_APP_SECRET,
+                                           api_version=version))
+    return graph_api.get_graph(token)
