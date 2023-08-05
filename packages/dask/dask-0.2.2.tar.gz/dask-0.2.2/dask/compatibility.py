5680fd35aebab32a4b5681e3ed93b4172666e6a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:42 +0000

    Add package dask/0.2.2/dask-0.2.2.tar.gz

---
+++ packages/dask/dask-0.2.2.tar.gz/dask-0.2.2/dask/compatibility.py
@@ -0,0 +1,16 @@
+from __future__ import absolute_import, division, print_function
+
+import sys
+
+PY3 = sys.version_info[0] == 3
+PY2 = sys.version_info[0] == 2
+
+if PY3:
+    import builtins
+    from queue import Queue
+    unicode = str
+else:
+    import __builtin__ as builtins
+    from Queue import Queue
+    import operator
+    unicode = unicode
