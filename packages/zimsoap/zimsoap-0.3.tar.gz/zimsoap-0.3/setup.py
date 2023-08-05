628ff22dc35a1dc8c513d525f4b411473293cf72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:57 +0000

    Add package zimsoap/0.3/zimsoap-0.3.tar.gz

---
+++ packages/zimsoap/zimsoap-0.3.tar.gz/zimsoap-0.3/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+from setuptools import setup
+
+try:
+    README = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
+except IOError:
+    README = ''
+
+
+setup(name='zimsoap',
+      version='0.3',
+      description='A high-level library to access programaticaly Zimbra \
+                   SOAP API features',
+      long_description=README,
+      author='Jocelyn Delalande',
+      author_email='jdelalande@oasiswork.fr',
+      url='https://github.com/oasiswork/zimsoap/',
+      packages=['zimsoap'],
+      install_requires=['python-zimbra>=1.0']
+      )
