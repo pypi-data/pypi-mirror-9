3ff9a0a70f3c4d84557ec801bbdffd2a0e08214d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:44 +0000

    Add package eelbrain/0.12.1/eelbrain-0.12.1.tar.gz

---
+++ packages/eelbrain/eelbrain-0.12.1.tar.gz/eelbrain-0.12.1/eelbrain/datasets.py
@@ -0,0 +1,265 @@
+'''
+Defines some basic example datasets that are used in testing.
+'''
+import os
+
+import numpy as np
+
+import mne
+from mne import minimum_norm as mn
+
+from . import load
+from ._data_obj import Dataset, Factor, Var, NDVar, Sensor, UTS
+from .design import permute
+
+
+def get_loftus_masson_1994():
+    "Dataset used for illustration purposes by Loftus and Masson (1994)"
+    ds = Dataset()
+    ds['subject'] = Factor(range(1, 11), tile=3)
+    ds['exposure'] = Var([1, 2, 5], repeat=10)
+    ds['n_recalled'] = Var([10, 6, 11, 22, 16, 15, 1, 12, 9, 8,
+                            13, 8, 14, 23, 18, 17, 1, 15, 12, 9,
+                            13, 8, 14, 25, 20, 17, 4, 17, 12, 12])
+    return ds
+
+
+def _mne_source_space(subject, src_tag, subjects_dir):
+    """Load mne source space"""
+    src_file = os.path.join(subjects_dir, subject, 'bem',
+                            '%s-%s-src.fif' % (subject, src_tag))
+    src = src_tag[:3]
+    if os.path.exists(src_file):
+        return mne.read_source_spaces(src_file, False)
+    elif src == 'ico':
+        return mne.setup_source_space(subject, src_file, 'ico4',
+                                      subjects_dir=subjects_dir, add_dist=True)
+    elif src == 'vol':
+        mri_file = os.path.join(subjects_dir, subject, 'mri', 'orig.mgz')
+        bem_file = os.path.join(subjects_dir, subject, 'bem',
+                                'sample-5120-5120-5120-bem-sol.fif')
+        return mne.setup_volume_source_space(subject, src_file, pos=10.,
+                                             mri=mri_file, bem=bem_file,
+                                             mindist=0., exclude=0.,
+                                             subjects_dir=subjects_dir)
+    else:
+        raise ValueError("src_tag=%s" % repr(src_tag))
+
+
+def get_mne_sample(tmin=-0.1, tmax=0.4, baseline=(None, 0), sns=False,
+                   src=None, sub="modality=='A'", fixed=False, snr=2,
+                   method='dSPM', rm=False, stc=False):
+    """Load events and epochs from the MNE sample data
+
+    Parameters
+    ----------
+    tmin, tmax baseline :
+        Epoch parameters.
+    sns : bool
+        Add sensor space data as NDVar as ``ds['sns']``.
+    src : None | 'ico' | 'vol'
+        Add source space data as NDVar as ``ds['src']``.
+    sub : str | None
+        Expresion for subset of events to load. For a very small dataset use e.g.
+        ``[0,1]``.
+    fixed : bool
+        MNE inverse parameter.
+    snr : scalar
+        MNE inverse parameter.
+    method : str
+        MNE inverse parameter.
+    rm : bool
+        Pretend to be a repeated measures dataset (adds 'subject' variable).
+    stc : bool
+        Add mne SourceEstimate for source space data as ``ds['stc']``.
+
+    Returns
+    -------
+    ds : Dataset
+        Dataset with epochs from the MNE sample dataset.
+    """
+    data_dir = mne.datasets.sample.data_path()
+    meg_dir = os.path.join(data_dir, 'MEG', 'sample')
+    raw_file = os.path.join(meg_dir, 'sample_audvis_filt-0-40_raw.fif')
+    event_file = os.path.join(meg_dir, 'sample_audvis_filt-0-40_evt.fif')
+    subjects_dir = os.path.join(data_dir, 'subjects')
+    subject = 'sample'
+    label_path = os.path.join(subjects_dir, subject, 'label', '%s.label')
+
+    if not os.path.exists(event_file):
+        raw = mne.io.Raw(raw_file)
+        events = mne.find_events(raw, stim_channel='STI 014')
+        mne.write_events(event_file, events)
+    ds = load.fiff.events(raw_file, events=event_file)
+    ds.index()
+    ds.info['subjects_dir'] = subjects_dir
+    ds.info['subject'] = subject
+    ds.info['label'] = label_path
+
+    # get the trigger variable form the dataset for eaier access
+    trigger = ds['trigger']
+
+    # use trigger to add various labels to the dataset
+    ds['condition'] = Factor(trigger, labels={1:'LA', 2:'RA', 3:'LV', 4:'RV',
+                                              5:'smiley', 32:'button'})
+    ds['side'] = Factor(trigger, labels={1: 'L', 2:'R', 3:'L', 4:'R',
+                                         5:'None', 32:'None'})
+    ds['modality'] = Factor(trigger, labels={1: 'A', 2:'A', 3:'V', 4:'V',
+                                             5:'None', 32:'None'})
+
+    if rm:
+        ds = ds.sub('trigger < 5')
+        ds = ds.equalize_counts('side % modality')
+        subject_f = ds.eval('side % modality').enumerate_cells()
+        ds['subject'] = subject_f.as_factor('s%r', random=True)
+
+    if sub:
+        ds = ds.sub(sub)
+
+    load.fiff.add_mne_epochs(ds, tmin, tmax, baseline)
+    if sns:
+        ds['sns'] = load.fiff.epochs_ndvar(ds['epochs'])
+
+    if src is None:
+        return ds
+    elif src == 'ico':
+        src_tag = 'ico-4'
+    elif src == 'vol':
+        src_tag = 'vol-10'
+    else:
+        raise ValueError("src = %r" % src)
+    epochs = ds['epochs']
+
+    # get inverse operator
+    inv_file = os.path.join(meg_dir, 'sample_eelbrain_%s-inv.fif' % src_tag)
+    if os.path.exists(inv_file):
+        inv = mne.minimum_norm.read_inverse_operator(inv_file)
+    else:
+        fwd_file = os.path.join(meg_dir, 'sample-%s-fwd.fif' % src_tag)
+        bem_dir = os.path.join(subjects_dir, subject, 'bem')
+        bem_file = os.path.join(bem_dir, 'sample-5120-5120-5120-bem-sol.fif')
+        trans_file = os.path.join(meg_dir, 'sample_audvis_raw-trans.fif')
+
+        if os.path.exists(fwd_file):
+            fwd = mne.read_forward_solution(fwd_file)
+        else:
+            src_ = _mne_source_space(subject, src_tag, subjects_dir)
+            fwd = mne.make_forward_solution(epochs.info, trans_file, src_,
+                                            bem_file, fwd_file)
+
+        cov_file = os.path.join(meg_dir, 'sample_audvis-cov.fif')
+        cov = mne.read_cov(cov_file)
+        inv = mn.make_inverse_operator(epochs.info, fwd, cov, None, None,
+                                       fixed)
+        mne.minimum_norm.write_inverse_operator(inv_file, inv)
+    ds.info['inv'] = inv
+
+    stcs = mn.apply_inverse_epochs(epochs, inv, 1. / (snr ** 2), method)
+    ds['src'] = load.fiff.stc_ndvar(stcs, subject, src_tag, subjects_dir)
+    if stc:
+        ds['stc'] = stcs
+
+    return ds
+
+
+def get_uts(utsnd=False, seed=0):
+    """Create a sample Dataset with 60 cases and random data.
+
+    Parameters
+    ----------
+    utsnd : bool
+        Add a sensor by time NDVar (called 'utsnd').
+    seed : None | int
+        If not None, call ``numpy.random.seed(seed)`` to ensure replicability.
+
+    Returns
+    -------
+    ds : Dataset
+        Datasets with data from random distributions.
+    """
+    if seed is not None:
+        np.random.seed(seed)
+
+    ds = Dataset()
+
+    # add a model
+    ds['A'] = Factor(['a0', 'a1'], repeat=30)
+    ds['B'] = Factor(['b0', 'b1'], repeat=15, tile=2)
+    ds['rm'] = Factor(('R%.2i' % i for i in xrange(15)), tile=4, random=True)
+    ds['ind'] = Factor(('R%.2i' % i for i in xrange(60)), random=True)
+
+    # add dependent variables
+    rm_var = np.tile(np.random.normal(size=15), 4)
+    y = np.hstack((np.random.normal(size=45), np.random.normal(1, size=15)))
+    y += rm_var
+    ds['Y'] = Var(y)
+    ybin = np.random.randint(0, 2, size=60)
+    ds['YBin'] = Factor(ybin, labels={0:'c1', 1:'c2'})
+    ycat = np.random.randint(0, 3, size=60)
+    ds['YCat'] = Factor(ycat, labels={0:'c1', 1:'c2', 2:'c3'})
+
+    # add a uts NDVar
+    time = UTS(-.2, .01, 100)
+    y = np.random.normal(0, .5, (60, len(time)))
+    y += rm_var[:, None]
+    y[:15, 20:60] += np.hanning(40) * 1  # interaction
+    y[:30, 50:80] += np.hanning(30) * 1  # main effect
+    ds['uts'] = NDVar(y, dims=('case', time))
+
+    # add sensor NDVar
+    if utsnd:
+        locs = np.array([[-1.0, 0.0, 0.0],
+                         [ 0.0, 1.0, 0.0],
+                         [ 1.0, 0.0, 0.0],
+                         [ 0.0, -1.0, 0.0],
+                         [ 0.0, 0.0, 1.0]])
+        sensor = Sensor(locs, sysname='test_sens')
+        sensor.set_connectivity(connect_dist=1.75)
+
+        y = np.random.normal(0, 1, (60, 5, len(time)))
+        y += rm_var[:, None, None]
+        # add interaction
+        win = np.hanning(50)
+        y[:15, 0, 50:] += win * 3
+        y[:15, 1, 50:] += win * 2
+        y[:15, 4, 50:] += win
+        # add main effect
+        y[30:, 2, 25:75] += win * 2.5
+        y[30:, 3, 25:75] += win * 1.5
+        y[30:, 4, 25:75] += win
+        # add spectral effect
+        freq = 15.0  # >= 2
+        x = np.sin(time.times * freq * 2 * np.pi)
+        for i in xrange(30):
+            shift = np.random.randint(0, 100 / freq)
+            y[i, 2, 25:75] += 1.1 * win * x[shift: 50+shift]
+            y[i, 3, 25:75] += 1.5 * win * x[shift: 50+shift]
+            y[i, 4, 25:75] += 0.5 * win * x[shift: 50+shift]
+
+        dims = ('case', sensor, time)
+        ds['utsnd'] = NDVar(y, dims=dims)
+
+    return ds
+
+
+def get_uv(seed=0):
+    """Dataset with random univariate data
+
+    Parameters
+    ----------
+    seed : None | int
+        Seed the numpy random state before generating random data.
+    """
+    if seed is not None:
+        np.random.seed(seed)
+
+    ds = permute([('A', ('a1', 'a2')),
+                  ('B', ('b1', 'b2')),
+                  ('rm', ['s%03i' % i for i in xrange(20)])])
+    ds['rm'].random = True
+    ds['intvar'] = Var(np.random.randint(5, 15, 80))
+    ds['intvar'][:20] += 3
+    ds['fltvar'] = Var(np.random.normal(0, 1, 80))
+    ds['fltvar'][:40] += 1.
+    ds['index'] = Var(np.repeat([True, False], 40))
+    return ds
---
+++ packages/eelbrain/eelbrain-0.12.1.tar.gz/eelbrain-0.12.1/setup.py
@@ -0,0 +1,193 @@
+"""
+setyup.py
+=========
+
+This is the setup.py script for Eelbrain.
+
+To create the Alias build on Mac Os X run::
+
+    $ python setup.py py2app -A
+
+After that, Eelbrain.app can be found in the dist/ folder. The application
+can be moved to another location without breaking the links, but will always
+reflect changes to the source code in eelbrain/, which consequently has to
+stay in the same location.
+
+http://packages.python.org/py2app
+
+http://docs.python.org/distutils/index.html
+
+"""
+from ez_setup import use_setuptools
+use_setuptools()
+
+import re
+import sys
+from setuptools import setup, find_packages, Extension
+import numpy as np
+
+# version must be in X.X.X format, e.g., "0.0.3dev"
+with open('eelbrain/__init__.py') as fid:
+    text = fid.read()
+match = re.search("__version__ = '([.\w]+)'", text)
+if match is None:
+    raise ValueError("No valid version string found in:\n\n" + text)
+version = match.group(1)
+if version.count('.') != 2 and not version.endswith('dev'):
+    raise ValueError("Invalid version string extracted: %r" % version)
+
+if len(sys.argv) > 1:
+    arg = sys.argv[1]
+else:
+    arg = None
+
+# Cython extensions
+ext = [Extension("eelbrain._stats.opt", ["eelbrain/_stats/opt.c"])]
+
+# basic setup arguments
+kwargs = dict(
+              name='eelbrain',
+              version=version,
+              description="MEG/EEG analysis tools",
+              url="https://pythonhosted.org/eelbrain",
+              author="Christian Brodbeck",
+              author_email='christianbrodbeck@nyu.edu',
+              license='GPL3',
+              long_description=open('README.txt').read(),
+              install_requires=['tex >= 1.8',
+                                'docutils >= 0.11',
+                                'mne >= 0.8',
+                                'nibabel >= 1.3'],
+              extras_require={'full': ['numpy >= 1.8',
+                                       'scipy >= 0.11.0',
+                                       'matplotlib >= 1.1'],
+                              'plot.brain': ['mayavi >= 4.3.1',
+#                                              'pysurfer >= 0.5'
+# PySurfer breaks the setup because it raises an error when nibabel is not installed
+                                             ]},
+              include_dirs=[np.get_include()],
+              ext_modules=ext,
+              )
+
+# Either PIL or Pillow is fine...
+try:
+    import PIL
+except ImportError:
+    kwargs['install_requires'].append('pillow')
+
+
+# py2app -----------------------------------------------------------------------
+py2app_alias_script = """
+# Startup script for py2app.
+import os
+import site
+import sys
+
+# extend sys.path
+paths = %s
+for path in paths:
+    if path not in sys.path:
+        site.addsitedir(path)
+
+
+# launch Eelbrain
+import eelbrain.wxterm
+
+os.chdir(os.path.expanduser('~'))
+eelbrain.wxterm.launch(True)
+""" % str(sys.path)
+
+if arg == 'py2app':
+    doctypes = [
+                {"CFBundleTypeExtensions": ["py"],
+                 "CFBundleTypeName": "Python Script",
+                 "CFBundleTypeRole": "Editor",
+                 "CFBundleTypeIconFile": "icons/system-icons/pydoc.icns",
+                 },
+                {"CFBundleTypeExtensions": ["pickled"],
+                 "CFBundleTypeName": "Pickled Python Object",
+                 "CFBundleTypeRole": "Editor",
+                 "CFBundleTypeIconFile": "icons/system-icons/pickled.icns",
+                 },
+                ]
+
+    OPTIONS = {
+               'argv_emulation': True,
+               'site_packages': True,
+               'iconfile': 'icons/system-icons/eelbrain.icns',
+               # py2app's iconfile option places the specified file
+               # in the new app bundle's Resources directory
+#               'packages': 'wx',
+#               'resources': ['icons/system-icons/eelbrain.icns',
+#                          'resources/License.txt'
+#                          ],
+               'plist': dict(CFBundleName="Eelbrain",
+                             CFBundleShortVersionString=version,
+                             CFBundleGetInfoString="Eelbrain " + version,
+                             CFBundleExecutable="Eelbrain",
+                             CFBundleIdentifier="com.christianmbrodbeck.Eelbrain",
+                             CFBundleDocumentTypes=doctypes,
+#                             CFBundleIconFile = 'eelbrain.icns',
+                             ),
+               }
+
+    # create the startup script
+    if '-A' in sys.argv:
+        script_path = 'scripts/eelbrain_alias.py'
+        with open(script_path, 'w') as fid:
+            fid.write(py2app_alias_script)
+    else:
+        script_path = 'scripts/eelbrain_py2app.py'
+
+    kwargs.update(
+                  app=[script_path],
+                  options={'py2app': OPTIONS},
+                  setup_requires=['py2app'])
+# cx_freeze??? ---
+# elif arg =='build':
+#    from cx_Freeze import setup, Executable
+#    kwargs.update(executables = [Executable("scripts/eelbrain")])
+else:
+    # normal & py2exe ---
+    kwargs['packages'] = find_packages()
+    # py2exe ---
+    if arg == 'py2exe':
+        # http://wiki.wxpython.org/DistributingYourApplication
+        import py2exe, matplotlib
+        data_files = matplotlib.get_py2exe_datafiles()
+
+        # DLL files that various Python modules depend on
+        data_files.append(('.', [
+#                                 "C:\Python27\DLLs\msvcp90.dll",
+#                                 "C:\Python27\lib\site-packages\wx\gdiplus.dll",
+                                 "C:\python27\scripts\MK2_CORE.DLL",
+                                 "C:\python27\scripts\MK2_P4P.DLL",
+                                 "C:\python27\scripts\MK2IOMP5MD.DLL",
+                                  ]))
+
+
+        OPTIONS = dict(
+#                       skip_archive = True,
+#                       bundle_files = 2, # This tells py2exe to bundle everything
+                       excludes=['_cairo', '_cocoaagg', '_emf', '_fltkagg',
+                                 '_gtkagg', '_qt4agg', '_tkagg'],
+                       includes=['matplotlib.numerix.random_array',
+                                 'scipy.io.matlab.streams'],
+                       dll_excludes=['libgdk-win32-2.0-0.dll',
+                                     'libgobject-2.0-0.dll'])
+        win_icon = "icons/system-icons/eelbrain.ico"
+        kwargs.update(windows=[{"script": 'scripts/eelbrain',
+                                   "icon_resources": [(0, win_icon)],
+                                   }],
+                      data_files=data_files,
+                      options={'py2exe': OPTIONS},
+                      # com_server=['myserver'],
+                      )
+
+    # normal -----------------------------------------------------------------------
+    else:
+        kwargs['scripts'] = ['scripts/eelbrain']
+
+
+setup(**kwargs)
+
