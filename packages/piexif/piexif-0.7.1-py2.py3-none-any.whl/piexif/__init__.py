8403924370ce0bb01ba1608536f9fba61edcdc06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:10 +0000

    Add package piexif/0.7.1/piexif-0.7.1-py2.py3-none-any.whl

---
+++ packages/piexif/piexif-0.7.1-py2.py3-none-any.whl/piexif/__init__.py
@@ -0,0 +1,7 @@
+from ._remove import remove
+from ._load_and_dump import load, dump, ZerothIFD, ExifIFD, GPSIFD
+from ._transplant import transplant
+from ._insert import insert
+
+
+VERSION = '0.7.1'
