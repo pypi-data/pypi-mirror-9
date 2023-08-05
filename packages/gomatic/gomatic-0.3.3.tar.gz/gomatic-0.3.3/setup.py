6d586b53663e0a1165f3db30e9649b46c6fd6c27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:05:50 +0000

    Add package gomatic/0.3.3/gomatic-0.3.3.tar.gz

---
+++ packages/gomatic/gomatic-0.3.3.tar.gz/gomatic-0.3.3/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='gomatic',
+      version='0.3.3',
+      description='API for configuring GoCD',
+      url='https://github.com/SpringerSBM/gomatic',
+      author='Springer Science+Business Media',
+      author_email='tools-engineering@groups.springer.com',
+      license='MIT',
+      packages=['gomatic'],
+      install_requires=[
+          'requests'
+      ],
+      zip_safe=False)
