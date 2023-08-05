bee9672769b080f3e9a9eb73ff11e959ced11a69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:28 +0000

    Add package clyent/0.3.3/clyent-0.3.3.tar.gz

---
+++ packages/clyent/clyent-0.3.3.tar.gz/clyent-0.3.3/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='clyent',
+    version="0.3.3",
+    author='Continuum Analytics',
+    author_email='srossross@gmail.com',
+    url='http://github.com/binstar/clyent',
+    description='Command line client Library for windwos and posix',
+    packages=find_packages(),
+    license="BSD",
+)
+
