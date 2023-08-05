74d8a0e28e0bb5a89e37e166a62a104d5e67a63f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:04 +0000

    Add package sloop/0.2.0/sloop-0.2.0.tar.gz

---
+++ packages/sloop/sloop-0.2.0.tar.gz/sloop-0.2.0/setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+
+install_requires = [
+    'requests',
+]
+
+
+setup(
+  name = 'sloop',
+  packages = ['sloop'], # this must be the same as the name above
+  version = '0.2.0',
+  description = 'Django package of the sloop push notification service',
+  author = 'H. Yigit Guler',
+  author_email = 'yigit@hipolbs.com',
+  url = 'https://github.com/hipo/django-sloop', # use the URL to the github repo
+  keywords='django, push',
+  classifiers = [],
+  install_requires=install_requires,
+
+)
+
