2bce73616837fa97348701432c994611c51b4a94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:57 +0000

    Add package dataplicity/0.3.3.3/dataplicity-0.3.3.3.tar.gz

---
+++ packages/dataplicity/dataplicity-0.3.3.3.tar.gz/dataplicity-0.3.3.3/dataplicity/constants.py
@@ -0,0 +1,8 @@
+SERVER_URL = "https://api.dataplicity.com"
+PUSH_URL = "https://sync.dataplicity.com/pushwait/"
+CONF_PATH = "/etc/dataplicity/dataplicity.conf"
+SETTINGS_PATH = "/var/dataplicity/"
+FIRMWARE_PATH = "/srv/dataplicity/fw/"
+TIMELINE_PATH = "/tmp/dataplicitytimeline/"
+PID_PATH = "/var/run/dataplicity.pid"
+M2M_URL = "wss://m2m.dataplicity.com/m2m/"
