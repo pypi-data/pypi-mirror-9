c7ec6c59d18cb2abc84cd1d8dcdccf6e02ae827e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:09 +0000

    Add package heisenberg-ec2/1.0.1/heisenberg-ec2-1.0.1.tar.gz

---
+++ packages/heisenberg-ec2/heisenberg-ec2-1.0.1.tar.gz/heisenberg-ec2-1.0.1/setup.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    sys.exit()
+
+setup(
+    name='heisenberg-ec2',
+    version='1.0.1',
+    description='A utility for searching and connecting to EC2 instances with caching for quicker interaction',
+    author='Adam Petrovic',
+    author_email='adam@petrovic.com.au',
+    url='https://github.com/adampetrovic/heisenberg',
+    download_url='https://github.com/adampetrovic/heisenberg/tarball/1.0.1',
+    packages=[
+        'heisenberg',
+        'heisenberg.command',
+    ],
+    scripts=['bin/ec2'],
+    include_package_data=True,
+    install_requires=[
+        'boto',
+        'texttable',
+    ],
+    zip_safe=False,
+    keywords='heisenberg',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+    ],
+)
