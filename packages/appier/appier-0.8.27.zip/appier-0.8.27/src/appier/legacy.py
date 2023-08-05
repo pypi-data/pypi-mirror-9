6b7dd501c5ab4510e84d6f11a380f0fcbb7aeeb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:39 +0000

    Add package appier/0.8.27/appier-0.8.27.zip

---
+++ packages/appier/appier-0.8.27.zip/appier-0.8.27/src/appier/legacy.py
@@ -0,0 +1,324 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Hive Appier Framework
+# Copyright (C) 2008-2015 Hive Solutions Lda.
+#
+# This file is part of Hive Appier Framework.
+#
+# Hive Appier Framework is free software: you can redistribute it and/or modify
+# it under the terms of the Apache License as published by the Apache
+# Foundation, either version 2.0 of the License, or (at your option) any
+# later version.
+#
+# Hive Appier Framework is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# Apache License for more details.
+#
+# You should have received a copy of the Apache License along with
+# Hive Appier Framework. If not, see <http://www.apache.org/licenses/>.
+
+__author__ = "João Magalhães <joamag@hive.pt>"
+""" The author(s) of the module """
+
+__version__ = "1.0.0"
+""" The version of the module """
+
+__revision__ = "$LastChangedRevision$"
+""" The revision number of the module """
+
+__date__ = "$LastChangedDate$"
+""" The last change date of the module """
+
+__copyright__ = "Copyright (c) 2008-2015 Hive Solutions Lda."
+""" The copyright for the module """
+
+__license__ = "Apache License, Version 2.0"
+""" The license for the module """
+
+import os
+import imp
+import sys
+import functools
+
+import urllib #@UnusedImport
+
+root = sys.path.pop(0)
+try: import urllib2
+except ImportError: urllib2 = None
+finally: sys.path.insert(0, root)
+
+root = sys.path.pop(0)
+try: import httplib
+except ImportError: httplib = None
+finally: sys.path.insert(0, root)
+
+root = sys.path.pop(0)
+try: import http
+except ImportError: http = None
+finally: sys.path.insert(0, root)
+
+root = sys.path.pop(0)
+try: import urllib.error
+except ImportError: pass
+finally: sys.path.insert(0, root)
+
+root = sys.path.pop(0)
+try: import urllib.request
+except ImportError: pass
+finally: sys.path.insert(0, root)
+
+root = sys.path.pop(0)
+try: import http.client
+except ImportError: pass
+finally: sys.path.insert(0, root)
+
+try: import HTMLParser
+except ImportError: import html.parser; HTMLParser = html.parser
+
+try: import cPickle
+except ImportError: import pickle; cPickle = pickle
+
+try: import cStringIO
+except ImportError: import io; cStringIO = io
+
+try: import StringIO as _StringIO
+except ImportError: import io; _StringIO = io
+
+try: import urlparse as _urlparse
+except ImportError: import urllib.parse; _urlparse = urllib.parse
+
+PYTHON_3 = sys.version_info[0] >= 3
+""" Global variable that defines if the current python
+interpreter is at least python 3 compliant, this is used
+to take some of the conversion decision for runtime """
+
+if PYTHON_3: LONG = int
+else: LONG = long #@UndefinedVariable
+
+if PYTHON_3: BYTES = bytes
+else: BYTES = str #@UndefinedVariable
+
+if PYTHON_3: UNICODE = str
+else: UNICODE = unicode #@UndefinedVariable
+
+if PYTHON_3: OLD_UNICODE = None
+else: OLD_UNICODE = unicode #@UndefinedVariable
+
+if PYTHON_3: STRINGS = (str,)
+else: STRINGS = (str, unicode) #@UndefinedVariable
+
+if PYTHON_3: INTEGERS = (int,)
+else: INTEGERS = (int, long) #@UndefinedVariable
+
+# saves a series of global symbols that are going to be
+# used latter for some of the legacy operations
+_ord = ord
+_chr = chr
+_str = str
+_bytes = bytes
+_range = range
+
+try: _xrange = xrange #@UndefinedVariable
+except: _xrange = None
+
+if PYTHON_3: Request = urllib.request.Request
+else: Request = urllib2.Request
+
+if PYTHON_3: HTTPHandler = urllib.request.HTTPHandler
+else: HTTPHandler = urllib2.HTTPHandler
+
+if PYTHON_3: HTTPError = urllib.error.HTTPError
+else: HTTPError = urllib2.HTTPError
+
+if PYTHON_3: HTTPConnection = http.client.HTTPConnection
+else: HTTPConnection = httplib.HTTPConnection
+
+if PYTHON_3: HTTPSConnection = http.client.HTTPSConnection
+else: HTTPSConnection = httplib.HTTPSConnection
+
+try: _execfile = execfile #@UndefinedVariable
+except: _execfile = None
+
+try: _reduce = reduce #@UndefinedVariable
+except: _reduce = None
+
+try: _reload = reload #@UndefinedVariable
+except: _reload = None
+
+try: _unichr = unichr #@UndefinedVariable
+except: _unichr = None
+
+def with_meta(meta, *bases):
+    return meta("Class", bases, {})
+
+def eager(iterable):
+    if PYTHON_3: return list(iterable)
+    return iterable
+
+def iteritems(associative):
+    if PYTHON_3: return associative.items()
+    return associative.iteritems()
+
+def iterkeys(associative):
+    if PYTHON_3: return associative.keys()
+    return associative.iterkeys()
+
+def itervalues(associative):
+    if PYTHON_3: return associative.values()
+    return associative.itervalues()
+
+def items(associative):
+    if PYTHON_3: return eager(associative.items())
+    return associative.items()
+
+def keys(associative):
+    if PYTHON_3: return eager(associative.keys())
+    return associative.keys()
+
+def values(associative):
+    if PYTHON_3: return eager(associative.values())
+    return associative.values()
+
+def xrange(start, stop = None, step = 1):
+    if PYTHON_3: return _range(start, stop, step) if stop else _range(start)
+    return _xrange(start, stop, step) if stop else _range(start)
+
+def range(start, stop = None, step = None):
+    if PYTHON_3: return eager(_range(start, stop, step)) if stop else eager(_range(start))
+    return _range(start, stop, step) if stop else _range(start)
+
+def ord(value):
+    if PYTHON_3 and type(value) == int: return value
+    return _ord(value)
+
+def chr(value):
+    if PYTHON_3: return _bytes([value])
+    if type(value) in INTEGERS: return _chr(value)
+    return value
+
+def chri(value):
+    if PYTHON_3: return value
+    if type(value) in INTEGERS: return _chr(value)
+    return value
+
+def bytes(value):
+    if not PYTHON_3: return value
+    if value == None: return value
+    if type(value) == _bytes: return value
+    return value.encode("latin-1")
+
+def str(value):
+    if not PYTHON_3: return value
+    if value == None: return value
+    if type(value) == _str: return value
+    return value.decode("latin-1")
+
+def orderable(value):
+    if not PYTHON_3: return value
+    return Orderable(value)
+
+def u(value, encoding = "utf-8"):
+    if PYTHON_3: return value
+    return value.decode(encoding)
+
+def is_str(value):
+    return type(value) == _str
+
+def is_unicode(value):
+    if PYTHON_3: return type(value) == _str
+    else: return type(value) == unicode #@UndefinedVariable
+
+def is_bytes(value):
+    if PYTHON_3: return type(value) == _bytes
+    else: return type(value) == _str #@UndefinedVariable
+
+def execfile(path, global_vars, local_vars = None):
+    if local_vars == None: local_vars = global_vars
+    if not PYTHON_3: return _execfile(path, global_vars, local_vars)
+    file = open(path)
+    try: data = file.read()
+    finally: file.close()
+    code = compile(data, path, "exec")
+    exec(code, global_vars, local_vars)
+
+def walk(path, visit, arg):
+    for root, dirs, _files in os.walk(path):
+        names = os.listdir(root)
+        visit(arg, root, names)
+        for dir in list(dirs):
+            exists = dir in names
+            not exists and dirs.remove(dir)
+
+def reduce(*args, **kwargs):
+    if PYTHON_3: return functools.reduce(*args, **kwargs)
+    return _reduce(*args, **kwargs)
+
+def reload(*args, **kwargs):
+    if PYTHON_3: return imp.reload(*args, **kwargs)
+    return _reload(*args, **kwargs)
+
+def unichr(*args, **kwargs):
+    if PYTHON_3: return _chr(*args, **kwargs)
+    return _unichr(*args, **kwargs)
+
+def urlopen(*args, **kwargs):
+    if PYTHON_3: return urllib.request.urlopen(*args, **kwargs)
+    else: return urllib2.urlopen(*args, **kwargs) #@UndefinedVariable
+
+def build_opener(*args, **kwargs):
+    if PYTHON_3: return urllib.request.build_opener(*args, **kwargs)
+    else: return urllib2.build_opener(*args, **kwargs) #@UndefinedVariable
+
+def urlparse(*args, **kwargs):
+    return _urlparse.urlparse(*args, **kwargs)
+
+def urlencode(*args, **kwargs):
+    if PYTHON_3: return urllib.parse.urlencode(*args, **kwargs)
+    else: return urllib.urlencode(*args, **kwargs) #@UndefinedVariable
+
+def quote(*args, **kwargs):
+    if PYTHON_3: return urllib.parse.quote(*args, **kwargs)
+    else: return urllib.quote(*args, **kwargs) #@UndefinedVariable
+
+def quote_plus(*args, **kwargs):
+    if PYTHON_3: return urllib.parse.quote_plus(*args, **kwargs)
+    else: return urllib.quote_plus(*args, **kwargs) #@UndefinedVariable
+
+def unquote(*args, **kwargs):
+    if PYTHON_3: return urllib.parse.unquote(*args, **kwargs)
+    else: return urllib.unquote(*args, **kwargs) #@UndefinedVariable
+
+def unquote_plus(*args, **kwargs):
+    if PYTHON_3: return urllib.parse.unquote_plus(*args, **kwargs)
+    else: return urllib.unquote_plus(*args, **kwargs) #@UndefinedVariable
+
+def parse_qs(*args, **kwargs):
+    if PYTHON_3: return urllib.parse.parse_qs(*args, **kwargs)
+    else: return _urlparse.parse_qs(*args, **kwargs) #@UndefinedVariable
+
+def cmp_to_key(*args, **kwargs):
+    if PYTHON_3: return dict(key = functools.cmp_to_key(*args, **kwargs)) #@UndefinedVariable
+    else: return dict(cmp = args[0])
+
+def StringIO(*args, **kwargs):
+    if PYTHON_3: return cStringIO.StringIO(*args, **kwargs)
+    else: return _StringIO.StringIO(*args, **kwargs)
+
+def BytesIO(*args, **kwargs):
+    if PYTHON_3: return cStringIO.BytesIO(*args, **kwargs)
+    else: return cStringIO.StringIO(*args, **kwargs)
+
+class Orderable(tuple):
+    """
+    Simple tuple type wrapper that provides a simple
+    first element ordering, that is compatible with
+    both the python 2 and python 3+ infra-structures.
+    """
+
+    def __cmp__(self, value):
+        return self[0].__cmp__(value[0])
+
+    def __lt__(self, value):
+        return self[0].__lt__(value[0])
---
+++ packages/appier/appier-0.8.27.zip/appier-0.8.27/src/appier/log.py
@@ -0,0 +1,227 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Hive Appier Framework
+# Copyright (C) 2008-2015 Hive Solutions Lda.
+#
+# This file is part of Hive Appier Framework.
+#
+# Hive Appier Framework is free software: you can redistribute it and/or modify
+# it under the terms of the Apache License as published by the Apache
+# Foundation, either version 2.0 of the License, or (at your option) any
+# later version.
+#
+# Hive Appier Framework is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# Apache License for more details.
+#
+# You should have received a copy of the Apache License along with
+# Hive Appier Framework. If not, see <http://www.apache.org/licenses/>.
+
+__author__ = "João Magalhães <joamag@hive.pt>"
+""" The author(s) of the module """
+
+__version__ = "1.0.0"
+""" The version of the module """
+
+__revision__ = "$LastChangedRevision$"
+""" The revision number of the module """
+
+__date__ = "$LastChangedDate$"
+""" The last change date of the module """
+
+__copyright__ = "Copyright (c) 2008-2015 Hive Solutions Lda."
+""" The copyright for the module """
+
+__license__ = "Apache License, Version 2.0"
+""" The license for the module """
+
+import inspect
+import logging
+import threading
+
+LOGGING_FORMAT = "%(asctime)s [%(levelname)s] %(message)s"
+""" The format to be used for the logging operation in
+the app, these operations are going to be handled by
+multiple stream handlers """
+
+LOGGING_FORMAT_TID = "%(asctime)s [%(levelname)s] [%(thread)d] %(message)s"
+""" The format to be used for the logging operation in
+the app, these operations are going to be handled by
+multiple stream handlers, this version of the string
+includes the thread identification number and should be
+used for messages called from outside the main thread """
+
+MAX_LENGTH = 10000
+""" The maximum amount of messages that are kept in
+memory until they are discarded, avoid a very large
+number for this value or else a large amount of memory
+may be used for logging purposes """
+
+SILENT = logging.CRITICAL + 1
+""" The "artificial" silent level used to silent a logger
+or an handler, this is used as an utility for debugging
+purposes more that a real feature for production systems """
+
+LEVELS = (
+    "DEBUG",
+    "INFO",
+    "WARNING",
+    "ERROR",
+    "CRITICAL"
+)
+""" The sequence of levels from the least sever to the
+most sever this sequence may be used to find all the
+levels that are considered more sever that a level """
+
+LEVEL_ALIAS = {
+    "DEBU" : "DEBUG",
+    "WARN" : "WARNING",
+    "INF" : "INFO",
+    "ERR" : "ERROR",
+    "CRIT" : "CRITICAL"
+}
+""" Map defining a series of alias that may be used latter
+for proper debug level resolution """
+
+class MemoryHandler(logging.Handler):
+    """
+    Logging handler that is used to store information in
+    memory so that anyone else may consult it latter as
+    long as the execution session is the same.
+    """
+
+    def __init__(self, level = logging.NOTSET, max_length = MAX_LENGTH):
+        logging.Handler.__init__(self, level = level)
+        self.max_length = max_length
+        self.messages = []
+        self.messages_l = {}
+
+        formatter = ThreadFormatter(LOGGING_FORMAT)
+        self.setFormatter(formatter)
+
+    def get_messages_l(self, level):
+        # in case the level is not found in the list of levels
+        # it's not considered valid and so an empty list is returned
+        try: index = LEVELS.index(level)
+        except: return []
+
+        # retrieves the complete set of levels that are considered
+        # equal or more severe than the requested one
+        levels = LEVELS[:index + 1]
+
+        # creates the list that will hold the various message
+        # lists associated with the current severity level
+        messages_l = []
+
+        # iterates over the complete set of levels considered
+        # equal or less sever to add the respective messages
+        # list to the list of message lists
+        for level in levels:
+            _messages_l = self.messages_l.get(level, [])
+            self.messages_l[level] = _messages_l
+            messages_l.append(_messages_l)
+
+        # returns the complete set of messages lists that
+        # have a level equal or less severe that the one
+        # that has been requested by argument
+        return messages_l
+
+    def emit(self, record):
+        # formats the current record according to the defined
+        # logging rules so that we can used the resulting message
+        # for any logging purposes
+        message = self.format(record)
+
+        # retrieves the level (as a string) associated with
+        # the current record to emit and uses it to retrieve
+        # the associated messages list
+        level = record.levelname
+        messages_l = self.get_messages_l(level)
+
+        # inserts the message into the messages queue and in
+        # case the current length of the message queue overflows
+        # the one defined as maximum must pop message from queue
+        self.messages.insert(0, message)
+        messages_s = len(self.messages)
+        if messages_s > self.max_length: self.messages.pop()
+
+        # iterates over all the messages list included in the retrieve
+        # messages list to add the logging message to each of them
+        for _messages_l in messages_l:
+            # inserts the message into the proper level specific queue
+            # and in case it overflows runs the same pop operation as
+            # specified also for the more general queue
+            _messages_l.insert(0, message)
+            messages_s = len(_messages_l)
+            if messages_s > self.max_length: _messages_l.pop()
+
+    def get_latest(self, count = None, level = None):
+        count = count or 100
+        level = level.upper() if level else level
+        level = LEVEL_ALIAS.get(level, level)
+        messages = self.messages_l.get(level, ()) if level else self.messages
+        return messages[:count]
+
+class ThreadFormatter(logging.Formatter):
+    """
+    Custom formatter class that changing the default format
+    behavior so that the thread identifier is printed when
+    the threading printing the log records is not the main one.
+    """
+
+    def format(self, record):
+        # retrieves the reference to the current thread and verifies
+        # if it represent the current process main thread, then selects
+        # the appropriate formating string taking that into account
+        current = threading.current_thread()
+        is_main = current.name == "MainThread"
+        if is_main: self._fmt = LOGGING_FORMAT
+        else: self._fmt = LOGGING_FORMAT_TID
+        return logging.Formatter.format(self, record)
+
+def rotating_handler(
+    path = "appier.log",
+    max_bytes = 1048576,
+    max_log = 5,
+    encoding = None,
+    delay = False
+):
+    return logging.handlers.RotatingFileHandler(
+        path,
+        maxBytes = max_bytes,
+        backupCount = max_log,
+        encoding = encoding,
+        delay = delay
+    )
+
+def smtp_handler(
+    host = "localhost",
+    port = 25,
+    sender = "no-reply@appier.com",
+    receivers = [],
+    subject = "Appier logging",
+    username = None,
+    password = None,
+    stls = False
+):
+    address = (host, port)
+    if username and password: credentials = (username, password)
+    else: credentials = None
+    has_secure = in_signature(logging.handlers.SMTPHandler.__init__, "secure")
+    if has_secure: kwargs = dict(secure = () if stls else None)
+    else: kwargs = dict()
+    return logging.handlers.SMTPHandler(
+        address,
+        sender,
+        receivers,
+        subject,
+        credentials = credentials,
+        **kwargs
+    )
+
+def in_signature(callable, name):
+    spec = inspect.getargspec(callable)
+    args = spec[0]; kwargs = spec[2]
+    return (args and name in args) or (kwargs and "secure" in kwargs)
