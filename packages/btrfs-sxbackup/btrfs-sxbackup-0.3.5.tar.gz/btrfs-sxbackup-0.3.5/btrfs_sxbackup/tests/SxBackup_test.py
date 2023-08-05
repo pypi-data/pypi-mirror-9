76a4aaeeee4fae3aa2ea17feaad4b872760619e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:35 +0000

    Add package btrfs-sxbackup/0.3.5/btrfs-sxbackup-0.3.5.tar.gz

---
+++ packages/btrfs-sxbackup/btrfs-sxbackup-0.3.5.tar.gz/btrfs-sxbackup-0.3.5/btrfs_sxbackup/tests/SxBackup_test.py
@@ -0,0 +1,11 @@
+import unittest
+
+
+class ConfigurationTest(unittest.TestCase):
+    def testconfiguration(self):
+        print("hello")
+
+
+class SnapshotTest(unittest.TestCase):
+    def testsnapshot(self):
+        print("hello2")
