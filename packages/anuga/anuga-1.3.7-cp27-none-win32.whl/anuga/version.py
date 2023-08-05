593197f73c082599f3dc7f4ca77f3c73f66e89db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:36 +0000

    Add package anuga/1.3.7/anuga-1.3.7-cp27-none-win32.whl

---
+++ packages/anuga/anuga-1.3.7-cp27-none-win32.whl/anuga/version.py
@@ -0,0 +1,11 @@
+
+# THIS FILE IS GENERATED FROM ANUGA SETUP.PY
+short_version = '1.3.1'
+version = '1.3.1'
+full_version = '1.3.1'
+git_revision = 'Unknown'
+svn_revision = '9638'
+release = True
+
+if not release:
+    version = full_version
