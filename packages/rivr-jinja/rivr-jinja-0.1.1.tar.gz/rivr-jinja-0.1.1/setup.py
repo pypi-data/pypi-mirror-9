e629b861a1c796ffad63a26b2032011e064be844
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:16 +0000

    Add package rivr-jinja/0.1.1/rivr-jinja-0.1.1.tar.gz

---
+++ packages/rivr-jinja/rivr-jinja-0.1.1.tar.gz/rivr-jinja-0.1.1/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='rivr-jinja',
+    version='0.1.1',
+    description='rivr integration for using the Jinja template engine.',
+    url='https://github.com/rivrproject/rivr-jinja',
+    packages=find_packages(),
+    install_requires=[
+        'rivr',
+        'Jinja2'
+    ],
+    author='Kyle Fuller',
+    author_email='inbox@kylefuller.co.uk',
+    license='BSD',
+    classifiers=(
+      'Programming Language :: Python :: 2',
+      'Programming Language :: Python :: 2.7',
+      'Programming Language :: Python :: 3',
+      'Programming Language :: Python :: 3.2',
+      'Programming Language :: Python :: 3.3',
+      'Programming Language :: Python :: 3.4',
+      'License :: OSI Approved :: BSD License',
+    )
+)
+
