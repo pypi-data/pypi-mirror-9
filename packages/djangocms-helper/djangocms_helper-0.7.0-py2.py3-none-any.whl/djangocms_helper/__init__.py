a143e4d27df0d0cc920ce9f6562d43da7ddb9903
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:38 +0000

    Add package djangocms-helper/0.7.0/djangocms_helper-0.7.0-py2.py3-none-any.whl

---
+++ packages/djangocms-helper/djangocms_helper-0.7.0-py2.py3-none-any.whl/djangocms_helper/__init__.py
@@ -0,0 +1,8 @@
+"""djangocms-helper - Helpers for django CMS plugin development"""
+
+__version__ = '0.7.0'
+__author__ = 'Iacopo Spalletti <i.spalletti@nephila.it>'
+__all__ = ['runner']
+ 
+
+HELPER_FILE = 'cms_helper.py'
