197927f9574c0e8e763bc69280f063d713d2ca11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:16 +0000

    Add package NiftyPy/0.2.1/NiftyPy-0.2.1.tar.gz

---
+++ packages/NiftyPy/NiftyPy-0.2.1.tar.gz/NiftyPy-0.2.1/NiftyPy/__init__.py
@@ -0,0 +1,16 @@
+
+# NiftyPy - Python interface to NiftyRec, NiftyReg, NiftySeg 
+# Stefano Pedemonte
+# Centre for Medical Image Computing (CMIC), University College London (UCL)
+# 2009 - 2012, London 
+# Aalto University, School of Science, Helsinki
+# Summer 2013, Helsinki 
+# Martinos Center for Biomedical Imaging, Harvard University/MGH, Boston
+# Jan. 2015, Boston
+
+
+
+from Common import *
+import NiftyRec
+import NiftyReg
+#import NiftySeg
