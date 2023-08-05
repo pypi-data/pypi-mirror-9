de13a1cc3a2762f41057c16f13848c2c2e1db8d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:03 +0000

    Add package omdb/0.3.0/omdb-0.3.0.tar.gz

---
+++ packages/omdb/omdb-0.3.0.tar.gz/omdb-0.3.0/omdb/__init__.py
@@ -0,0 +1,15 @@
+"""OMDb API library.
+"""
+
+from .api import (
+    set_default,
+    search,
+    search_movie,
+    search_episode,
+    search_series,
+    get,
+    title,
+    imdbid,
+    request,
+    Client
+)
