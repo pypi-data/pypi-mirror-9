8de7131ab54425ba0cb2bb564fe5b9b81887a5b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:02 +0000

    Add package ocflib/7/ocflib-7.tar.gz

---
+++ packages/ocflib/ocflib-7.tar.gz/ocflib-7/ocflib/account/validators.py
@@ -0,0 +1,70 @@
+import difflib
+import pwd
+import string
+
+import cracklib
+
+import ocflib.constants as constants
+
+
+def validate_username(username):
+    """Validate a username, raising a descriptive exception if problems are
+    encountered."""
+
+    if username_reserved(username):
+        raise Exception("Username is reserved")
+
+    if not 3 <= len(username) <= 8:
+        raise Exception("Username must be between 3 and 8 characters")
+
+    if not all(c.islower() for c in username):
+        raise Exception("Username must be all lowercase letters")
+
+
+def validate_password(username, password, strength_check=True):
+    """Validate a password, raising a descriptive exception if problems are
+    encountered. Optionally checks password strength."""
+
+    if strength_check:
+        if len(password) < 8:
+            raise Exception("Password must be at least 8 characters")
+
+        s = difflib.SequenceMatcher()
+        s.set_seqs(password, username)
+
+        if s.ratio() > 0.6:
+            raise Exception("Password is too similar to username")
+
+        try:
+            cracklib.FascistCheck(password)
+        except ValueError as e:
+            raise Exception("Password problem: {}".format(e))
+
+    # sanity check; note we don't use string.whitespace since we don't want
+    # tabs or newlines
+    allowed = string.digits + string.ascii_letters + string.punctuation + ' '
+
+    if not all(c in allowed for c in password):
+        raise Exception("Password contains forbidden characters")
+
+
+def user_exists(username):
+    try:
+        pwd.getpwnam(username)
+    except KeyError:
+        return False
+    else:
+        return True
+
+
+def username_reserved(username):
+    if username.startswith('ocf'):
+        return True
+    return username in constants.RESERVED_USERNAMES
+
+
+def username_queued(username):
+    """Returns if the username has already been requested and is queued to be
+    created."""
+    with open(constants.QUEUED_ACCOUNTS_PATH) as f:
+        return any(line.startswith(username + ":") for line in f)
