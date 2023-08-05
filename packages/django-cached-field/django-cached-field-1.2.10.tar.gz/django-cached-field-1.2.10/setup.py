debc8242ccc4b662122bfe02cc16eb8f0ac875fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:59 +0000

    Add package django-cached-field/1.2.10/django-cached-field-1.2.10.tar.gz

---
+++ packages/django-cached-field/django-cached-field-1.2.10.tar.gz/django-cached-field-1.2.10/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+import sys, os
+
+version = '1.2.10'
+
+setup(name='django-cached-field',
+      version=version,
+      description="Celery-deferred, cached fields on Django ORM for expensive-to-calculate data",
+      long_description="""Celery-deferred, cached fields on Django ORM for expensive-to-calculate data""",
+      classifiers=[],  # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='django caching',
+      author='Martin Chase',
+      author_email='outofculture@gmail.com',
+      url='https://github.com/aquameta/django-cached-field',
+      license='BSD',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+        'django>=1.3.1',
+        'celery>=3.0',
+        'django-celery>=3.0',
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
