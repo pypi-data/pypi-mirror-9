9587e65e7972e905e08778ac2a528924d7992bdf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:16 +0000

    Add package pip_extras/0.0.3/pip_extras-0.0.3.tar.gz

---
+++ packages/pip_extras/pip_extras-0.0.3.tar.gz/pip_extras-0.0.3/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pip_extras",
+    version="0.0.3",
+    description="Provides functionality for updating all your outdated packages.",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.4",
+    ],
+    url='https://github.com/russel/pip_extras',
+    author='Ross Binden & Alistair Broomhead',
+    keywords='pip',
+    license='MIT',
+    packages=find_packages(),
+    entry_points={
+        "console_scripts": [
+            "pip_update=pip_extras:main",
+            ],
+        }
+    )
