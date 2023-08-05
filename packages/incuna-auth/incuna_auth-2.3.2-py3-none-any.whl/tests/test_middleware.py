5f28d58aec5076e02774b587cb6d5cb2c697ceb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:39 +0000

    Add package incuna-auth/2.3.2/incuna_auth-2.3.2-py3-none-any.whl

---
+++ packages/incuna-auth/incuna_auth-2.3.2-py3-none-any.whl/tests/test_middleware.py
@@ -0,0 +1,189 @@
+import re
+from base64 import b64encode
+from unittest import TestCase
+
+import mock
+from django.test.utils import override_settings
+
+from incuna_auth.middleware import (
+    basic_auth,
+    FeinCMSLoginRequiredMiddleware,
+    LoginRequiredMiddleware,
+)
+from incuna_auth.models import AccessStateExtensionMixin as AccessState
+from .utils import RequestTestCase
+
+NO_URLS = []
+ALL_URLS = [re.compile(r'^')]
+
+
+def base64_encode_for_py2or3(text):
+    """Encode a base-64 string in a way that works in Python 2 or 3."""
+    return b64encode(text.encode('utf-8')).decode('utf-8')
+
+
+class TestLoginRequiredMiddleware(RequestTestCase):
+    middleware = LoginRequiredMiddleware(check=False)
+    EXEMPT_URLS = 'incuna_auth.middleware.LoginRequiredMiddleware.EXEMPT_URLS'
+    PROTECTED_URLS = 'incuna_auth.middleware.LoginRequiredMiddleware.PROTECTED_URLS'
+
+    def make_request(self, auth, method='get', url='/fake-request/', **kwargs):
+        return self.create_request(method, auth=auth, url=url, **kwargs)
+
+    @mock.patch(EXEMPT_URLS, ALL_URLS)
+    def test_exempt_url(self):
+        request = self.make_request(auth=False)
+        response = self.middleware.process_request(request)
+        self.assertIsNone(response)
+
+    @mock.patch(EXEMPT_URLS, NO_URLS)
+    @mock.patch(PROTECTED_URLS, NO_URLS)
+    def test_unprotected_url(self):
+        request = self.make_request(auth=False)
+        response = self.middleware.process_request(request)
+        self.assertIsNone(response)
+
+    @mock.patch(EXEMPT_URLS, NO_URLS)
+    @mock.patch(PROTECTED_URLS, ALL_URLS)
+    def test_is_auth(self):
+        request = self.make_request(auth=True)
+        response = self.middleware.process_request(request)
+        self.assertIsNone(response)
+
+    @mock.patch(EXEMPT_URLS, NO_URLS)
+    @mock.patch(PROTECTED_URLS, ALL_URLS)
+    def test_non_auth_get(self):
+        request = self.make_request(auth=False)
+        response = self.middleware.process_request(request)
+        message = 'You must be logged in to view this page.'
+
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/')
+        self.assertEqual(request._messages.store, [message])
+
+    @mock.patch(EXEMPT_URLS, NO_URLS)
+    @mock.patch(PROTECTED_URLS, ALL_URLS)
+    def test_non_auth_post(self):
+        request = self.make_request(auth=False, method='post')
+        response = self.middleware.process_request(request)
+        self.assertEqual(response.status_code, 403)
+
+
+class TestFeinCMSLoginRequiredMiddleware(RequestTestCase):
+    middleware = FeinCMSLoginRequiredMiddleware()
+    AUTH_STATE = AccessState.STATE_AUTH_ONLY
+    OTHER_STATE = ('other', 'Other state')
+
+    get_page_method = (
+        'incuna_auth.middleware.permission_feincms.' +
+        'FeinCMSPermissionMiddleware._get_page_from_path'
+    )
+
+    class DummyFeinCMSPage:
+        def __init__(self, access_state):
+            self.access_state = access_state
+
+    def make_request(self, auth, access_state, **kwargs):
+        """Create a request with a suitable feincms_page attribute."""
+        request = self.create_request(auth=auth, **kwargs)
+        request.feincms_page = self.DummyFeinCMSPage(access_state=access_state)
+        return request
+
+    def test_unprotected_url(self):
+        request = self.make_request(
+            auth=False,
+            access_state=AccessState.STATE_ALL_ALLOWED
+        )
+        with mock.patch(self.get_page_method, return_value=request.feincms_page):
+            response = self.middleware.process_request(request)
+        self.assertIsNone(response)
+
+    def test_other_state_url(self):
+        request = self.make_request(auth=False, access_state=self.OTHER_STATE)
+        with mock.patch(self.get_page_method, return_value=request.feincms_page):
+            response = self.middleware.process_request(request)
+        self.assertIsNone(response)
+
+    def test_is_auth(self):
+        request = self.make_request(auth=True, access_state=self.AUTH_STATE)
+        with mock.patch(self.get_page_method, return_value=request.feincms_page):
+            response = self.middleware.process_request(request)
+        self.assertIsNone(response)
+
+    def test_non_auth_get(self):
+        request = self.make_request(auth=False, access_state=self.AUTH_STATE)
+        with mock.patch(self.get_page_method, return_value=request.feincms_page):
+            response = self.middleware.process_request(request)
+        message = 'You must be logged in to view this page.'
+
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/')
+        self.assertEqual(request._messages.store, [message])
+
+    def test_non_auth_post(self):
+        request = self.make_request(
+            auth=False,
+            access_state=self.AUTH_STATE,
+            method='post'
+        )
+        with mock.patch(self.get_page_method, return_value=request.feincms_page):
+            response = self.middleware.process_request(request)
+        self.assertEqual(response.status_code, 403)
+
+
+class TestBasicAuthMiddleware(TestCase):
+
+    class DummyRequest(object):
+        META = {}
+
+        def __init__(self, auth_method=None, username='', password=''):
+            if auth_method is None:
+                return
+
+            auth = username + ':' + password
+            http_auth_string = auth_method + ' ' + base64_encode_for_py2or3(auth)
+            self.META = {'HTTP_AUTHORIZATION': http_auth_string}
+
+    def setUp(self):
+        self.middleware = basic_auth.BasicAuthenticationMiddleware()
+
+    def test_basic_challenge(self):
+        response = basic_auth.basic_challenge()
+        self.assertEqual(response.status_code, 401)
+        self.assertEqual(response['WWW-Authenticate'], 'Basic realm="Restricted Access"')
+
+    def test_basic_authenticate_success(self):
+        request = self.DummyRequest('basic', 'user', 'pass')
+        result = basic_auth.basic_authenticate(request.META['HTTP_AUTHORIZATION'])
+        self.assertTrue(result)
+
+    def test_basic_authenticate_failure_wrong_type(self):
+        request = self.DummyRequest('non_basic', 'user', 'pass')
+        result = basic_auth.basic_authenticate(request.META['HTTP_AUTHORIZATION'])
+        self.assertEqual(result, None)
+
+    def test_basic_authenticate_failure_wrong_credentials(self):
+        request = self.DummyRequest('basic', 'other_user', 'other_pass')
+        result = basic_auth.basic_authenticate(request.META['HTTP_AUTHORIZATION'])
+        self.assertFalse(result)
+
+    @override_settings(BASIC_WWW_AUTHENTICATION=False)
+    def test_lack_of_www_authentication(self):
+        request = self.DummyRequest('basic', 'other_user', 'other_pass')
+        result = self.middleware.process_request(request)
+        self.assertEqual(result, None)
+
+    def test_no_http_auth_in_meta(self):
+        request = self.DummyRequest(None)
+        result = self.middleware.process_request(request)
+        self.assertEqual(result.status_code, basic_auth.basic_challenge().status_code)
+
+    def test_passes_basic_authentication(self):
+        request = self.DummyRequest('basic', 'user', 'pass')
+        result = self.middleware.process_request(request)
+        self.assertEqual(result, None)
+
+    def test_falls_through_to_basic_challenge(self):
+        request = self.DummyRequest('basic', 'other_user', 'other_pass')
+        result = self.middleware.process_request(request)
+        self.assertEqual(result.status_code, basic_auth.basic_challenge().status_code)
---
+++ packages/incuna-auth/incuna_auth-2.3.2-py3-none-any.whl/tests/test_permission.py
@@ -0,0 +1,145 @@
+import mock
+
+from incuna_auth.middleware import permission
+from incuna_auth.middleware.utils import compile_urls
+from .utils import RequestTestCase
+
+
+class TestBasePermissionMiddleware(RequestTestCase):
+    middleware_class = permission.BasePermissionMiddleware
+    middleware_path = 'incuna_auth.middleware.permission.BasePermissionMiddleware.{}'
+
+    def setUp(self):
+        self.middleware = self.middleware_class()
+
+    def test_deny_access(self):
+        """Assert that a typical (GET) redirect defaults to /."""
+        response = self.middleware.deny_access(self.create_request())
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/')
+
+    def test_deny_access_post(self):
+        """Assert that a POST request results in a HTTP 403 (Forbidden) error."""
+        response = self.middleware.deny_access(self.create_request(method='post'))
+        self.assertEqual(response.status_code, 403)
+
+    def test_deny_access_no_message(self):
+        """Assert that a blank `message` parameter results in no Django message."""
+        request = self.create_request()
+        self.middleware.deny_access(request)
+        self.assertEqual(request._messages.store, [])
+
+    def test_deny_access_message(self):
+        """Assert that a non-blank `message` parameter results in a Django message."""
+        request = self.create_request()
+        method = self.middleware_path.format('get_access_denied_message')
+        message = 'I am a message'
+        with mock.patch(method, return_value=message):
+            self.middleware.deny_access(request)
+            self.assertEqual(request._messages.store, [message])
+
+    def test_process_request_default(self):
+        """Assert that the default process_request implementation does nothing."""
+        response = self.middleware.process_request(self.create_request())
+        self.assertIsNone(response)
+
+    def test_process_request_default_protected(self):
+        """
+        Assert that the default process_request implementation does nothing even if the
+        resource is protected.
+        """
+        method = self.middleware_path.format('is_resource_protected')
+        with mock.patch(method, return_value=True):
+            response = self.middleware.process_request(self.create_request())
+            self.assertIsNone(response)
+
+    def test_process_request_denies_access(self):
+        """
+        Assert that the default process_request implementation denies access when both
+        is_resource_protected and deny_access_condition return True.
+        """
+        resource_method = self.middleware_path.format('is_resource_protected')
+        condition_method = self.middleware_path.format('deny_access_condition')
+        with mock.patch(resource_method, return_value=True):
+            with mock.patch(condition_method, return_value=True):
+                response = self.middleware.process_request(self.create_request())
+                self.assertEqual(response.status_code, 302)
+
+    def test_default_unauthorised_redirect_url(self):
+        """
+        Assert the default implementation of get_unauthorised_redirect_url returns '/'.
+        """
+        self.assertEqual('/', self.middleware.get_unauthorised_redirect_url())
+
+    def test_default_access_denied_message(self):
+        """
+        Assert the default implementation of get_access_denied_message returns ''.
+        """
+        self.assertEqual('', self.middleware.get_access_denied_message())
+
+
+class TestLoginMiddlewareMixin(RequestTestCase):
+    middleware_class = permission.LoginPermissionMiddlewareMixin
+
+    def setUp(self):
+        # We don't need to mix this into anything because we're only calling the methods
+        # it implements.
+        self.middleware = self.middleware_class()
+
+    def test_deny_access_condition(self):
+        """Assert that deny_access_condition disallows an anonymous user."""
+        request = self.create_request(auth=False)
+        self.assertTrue(self.middleware.deny_access_condition(request))
+
+    def test_deny_access_condition_allow(self):
+        """Assert that deny_access_condition allows an authenticated user."""
+        request = self.create_request(auth=True)
+        self.assertFalse(self.middleware.deny_access_condition(request))
+
+    def test_access_denied_message(self):
+        """Assert the message returned by get_access_denied_message."""
+        expected_message = 'You must be logged in to view this page.'
+        self.assertEqual(expected_message, self.middleware.get_access_denied_message())
+
+
+class TestUrlPermissionMiddleware(RequestTestCase):
+    middleware_class = permission.UrlPermissionMiddleware
+    middleware_path = 'incuna_auth.middleware.permission.UrlPermissionMiddleware.{}'
+    URL = '/fake-request/'
+    ALL_URLS = compile_urls([r'^'])
+
+    def setUp(self):
+        self.middleware = self.middleware_class()
+
+    def make_request(self, url=URL, **kwargs):
+        """Create a request with a suitable URL attribute."""
+        request = self.create_request(url=url, **kwargs)
+        return request
+
+    def test_url_exempt(self):
+        """
+        Assert that the URL is not protected when the request matches an item in
+        exempt_urls.
+        """
+        request = self.make_request()
+        method = self.middleware_path.format('get_exempt_url_patterns')
+        with mock.patch(method, return_value=self.ALL_URLS):
+            self.assertFalse(self.middleware.is_resource_protected(request))
+
+    def test_url_protected(self):
+        """
+        Assert that the URL is protected when the request matches an item in
+        protected_urls.
+        """
+        request = self.make_request()
+        self.assertTrue(self.middleware.is_resource_protected(request))
+
+    def test_url_unprotected(self):
+        """
+        Assert that the URL is not protected when the request matches neither exempt_urls
+        nor protected_urls.
+        """
+        request = self.make_request()
+        method = self.middleware_path.format('get_protected_url_patterns')
+        with mock.patch(method, return_value=[]):
+            self.assertFalse(self.middleware.is_resource_protected(request))
