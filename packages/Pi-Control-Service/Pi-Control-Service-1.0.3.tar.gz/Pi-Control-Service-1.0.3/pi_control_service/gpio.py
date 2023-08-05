61d5c3aba33ed6b11f6dbe4e4dbaa547b68ba2d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:44 +0000

    Add package Pi-Control-Service/1.0.3/Pi-Control-Service-1.0.3.tar.gz

---
+++ packages/Pi-Control-Service/Pi-Control-Service-1.0.3.tar.gz/Pi-Control-Service-1.0.3/pi_control_service/gpio.py
@@ -0,0 +1,41 @@
+from rpc import RPCService
+from pi_pin_manager import PinManager
+
+from mixins import ServiceUtils
+
+
+ALLOWED_ACTIONS = ('on', 'off', 'read', 'get_config')
+
+
+class GPIOService(ServiceUtils, RPCService):
+
+    def __init__(self, rabbit_url, device_key, pin_config):
+        self._pins = PinManager(config_file=pin_config)
+        super(GPIOService, self).__init__(
+            rabbit_url=rabbit_url,
+            exchange='gpio_service',
+            routing_key=device_key,
+            request_action=self._perform_gpio_action)
+
+    def _perform_gpio_action(self, instruction):
+        try:
+            if instruction['action'] not in ALLOWED_ACTIONS:
+                return self._error("'action' must be one of: {0}".format(', '.join(ALLOWED_ACTIONS)))
+        except KeyError:
+            return self._error("'action' must be defined")
+
+        try:
+            return self._response(getattr(self._pins, instruction['action'])(int(instruction['pin'])))
+        except ValueError:
+            return self._error("'pin' value must be an integer")
+        except KeyError:
+            try:
+                return self._response(getattr(self._pins, instruction['action'])())
+            except Exception as e:
+                return self._error(e.message)
+        except Exception as e:
+            return self._error(e.message)
+
+    def stop(self):
+        self._pins.cleanup()
+        super(GPIOService, self).stop()
---
+++ packages/Pi-Control-Service/Pi-Control-Service-1.0.3.tar.gz/Pi-Control-Service-1.0.3/setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+setup(
+    name='Pi-Control-Service',
+    version='1.0.3',
+    author='Brian Hines',
+    author_email='brian@projectweekend.net',
+    packages=['pi_control_service'],
+    url='http://projectweekend.github.io/Pi-Control-Service',
+    license='LICENSE.txt',
+    description='Control a Raspberry Pi from anywhere.',
+    long_description=open('README.txt').read(),
+    install_requires=[
+        "pika == 0.9.14",
+        "Pi-Pin-Manager == 0.1.0",
+        "pyecho == 0.0.2",
+    ],
+)
