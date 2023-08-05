73675bd7c3818e6d448dca27c02faf5aea62461e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:31 +0000

    Add package flask-bigtempo/0.2/flask-bigtempo-0.2.tar.gz

---
+++ packages/flask-bigtempo/flask-bigtempo-0.2.tar.gz/flask-bigtempo-0.2/flask_bigtempo/__init__.py
@@ -0,0 +1,5 @@
+__version__ = '0.2'
+
+
+from .store.core import DatastoreAPI
+from .bigtempo.core import BigtempoAPI
---
+++ packages/flask-bigtempo/flask-bigtempo-0.2.tar.gz/flask-bigtempo-0.2/flask_bigtempo/store/defaults.py
@@ -0,0 +1,6 @@
+DEFAULT_HOST = 'http://localhost:5000'
+
+DEFAULT_JSON_FORMAT = 'columns'
+DEFAULT_DATE_FORMAT = 'iso'
+
+API_URL_PREFIX = '/api/store'
