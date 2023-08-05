4c605d7184fae1b44140c9e3b23c714454bfb935
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:19 +0000

    Add package quicklock/0.1.6/quicklock-0.1.6.tar.gz

---
+++ packages/quicklock/quicklock-0.1.6.tar.gz/quicklock-0.1.6/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'quicklock',
+  packages = ['quicklock'],
+  version = '0.1.6',
+  description = 'A simple Python resource lock to ensure only one process at a time is operating with a particular resource.',
+  author = 'Nate Ferrero',
+  author_email = 'nateferrero@gmail.com',
+  url = 'https://github.com/NateFerrero/quicklock',
+  download_url = 'https://github.com/NateFerrero/quicklock/tarball/0.1.6',
+  keywords = ['lock', 'locking', 'singleton', 'process', 'resource', 'exclusive lock'],
+  classifiers = [],
+)
