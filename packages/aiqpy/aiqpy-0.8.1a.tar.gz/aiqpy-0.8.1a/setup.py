2912f0b338f84d2059c6f13adb5d01f284dfa0bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:05:57 +0000

    Add package aiqpy/0.8.1a/aiqpy-0.8.1a.tar.gz

---
+++ packages/aiqpy/aiqpy-0.8.1a.tar.gz/aiqpy-0.8.1a/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name='aiqpy',
+    version='0.8.1a',
+    description='Python bindings for connecting to a AIQ8 server',
+    author='Erik Lundberg',
+    author_email='lundbergerik@gmail.com',
+    packages=['aiqpy', 'tools'],
+    install_requires=[
+        'requests',
+        'six',
+        'click'
+    ],
+    keywords=['aiq8', 'appear', 'rest', 'api'],
+    zip_safe=False,
+    entry_points={
+        'console_scripts': [
+           'aiqpyprofile = tools.aiqpyprofile:main'
+        ]
+    }
+)
