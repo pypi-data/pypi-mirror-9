dfad4400b998ba9ed43df278339d291f16c0ee38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:40 +0000

    Add package awesome-slugify/1.6.2/awesome-slugify-1.6.2.tar.gz

---
+++ packages/awesome-slugify/awesome-slugify-1.6.2.tar.gz/awesome-slugify-1.6.2/setup.py
@@ -0,0 +1,34 @@
+# coding=utf8
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='awesome-slugify',
+    version='1.6.2',
+
+    author='Dmitry Voronin',
+    author_email='dimka665@gmail.com',
+
+    url='https://github.com/dimka665/awesome-slugify',
+    description='Python flexible slugify function',
+
+    packages=find_packages(),
+    install_requires=[
+        'regex',
+        'Unidecode>=0.04.14,<0.05',
+    ],
+
+    license='GNU GPLv3',
+    classifiers=[
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
+    keywords='slugify slug transliteration russian german unicode translation flexible',
+)
