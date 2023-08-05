efb6863bda9c86aad9637d9cce930a3b87d2582c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:06 +0000

    Add package python-consul/0.3.9/python-consul-0.3.9.tar.gz

---
+++ packages/python-consul/python-consul-0.3.9.tar.gz/python-consul-0.3.9/consul/tornado.py
@@ -0,0 +1,59 @@
+from __future__ import absolute_import
+
+from six.moves import urllib
+
+from tornado import httpclient
+from tornado import gen
+
+from consul import base
+
+
+__all__ = ['Consul']
+
+
+class HTTPClient(object):
+    def __init__(self, host='127.0.0.1', port=8500, scheme='http'):
+        self.host = host
+        self.port = port
+        self.scheme = scheme
+        self.base_uri = '%s://%s:%s' % (self.scheme, self.host, self.port)
+        self.client = httpclient.AsyncHTTPClient()
+
+    def uri(self, path, params=None):
+        uri = self.base_uri + path
+        if not params:
+            return uri
+        return '%s?%s' % (uri, urllib.parse.urlencode(params))
+
+    def response(self, response):
+        return base.Response(
+            response.code, response.headers, response.body.decode('utf-8'))
+
+    @gen.coroutine
+    def _request(self, callback, request):
+        try:
+            response = yield self.client.fetch(request)
+        except httpclient.HTTPError as e:
+            if e.code == 599:
+                raise base.Timeout
+            response = e.response
+        raise gen.Return(callback(self.response(response)))
+
+    def get(self, callback, path, params=None):
+        uri = self.uri(path, params)
+        return self._request(callback, uri)
+
+    def put(self, callback, path, params=None, data=''):
+        uri = self.uri(path, params)
+        request = httpclient.HTTPRequest(uri, method='PUT', body=data)
+        return self._request(callback, request)
+
+    def delete(self, callback, path, params=None):
+        uri = self.uri(path, params)
+        request = httpclient.HTTPRequest(uri, method='DELETE')
+        return self._request(callback, request)
+
+
+class Consul(base.Consul):
+    def connect(self, host, port, scheme):
+        return HTTPClient(host, port, scheme)
