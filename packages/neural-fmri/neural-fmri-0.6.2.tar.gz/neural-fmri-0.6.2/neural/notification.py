8c25b6d1578199d37e8a5ecbd1a61ca945b9c869
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:24 +0000

    Add package neural-fmri/0.6.2/neural-fmri-0.6.2.tar.gz

---
+++ packages/neural-fmri/neural-fmri-0.6.2.tar.gz/neural-fmri-0.6.2/neural/notification.py
@@ -0,0 +1,156 @@
+''' Provides a centralized notification function
+
+The functions will try to detect the specific system and situation to tailor
+the notification to the most appropriate method
+'''
+import sys,os,io
+import platform
+import random,string
+import smtplib
+from email.mime.text import MIMEText
+import neural.term
+
+class SMTPServer:
+    def __init__(self,from_addr,server,username=None,password=None,SSL=False):
+        self.from_addr = from_addr
+        self.server = server
+        self.username = username
+        self.password = password
+        self.SSL = SSL
+
+default_SMTP = None
+
+def enable_email(from_email,server,username=None,password=None,SSL=False):
+    global default_SMTP
+    default_SMTP = SMTPServer(from_email,server,username,password,SSL)
+
+def email(to,msg,subject='Neural notification'):
+    if default_SMTP==None:
+        raise RuntimeError('Email not enabled, must run ``enable_email`` first!')
+    msg = MIMEText(msg)
+    msg['Subject'] = subject
+    msg['From'] = default_SMTP.from_addr
+    msg['To'] = to
+    if default_SMTP.SSL:
+        s = smtplib.SMTP_SSL(default_SMTP.server)
+    else:
+        s = smtplib.SMTP(default_SMTP.server)
+    if default_SMTP.username:
+        s.login(default_SMTP.username,default_SMTP.password)
+    s.sendmail(to,default_SMTP.from_addr,msg.as_string())
+    s.quit()
+
+# Log levels:
+class level:
+    debug, informational, warning, error, critical = range(5)
+
+interactive_enabled = False
+email_digest = None
+
+#! variable to hold list of nested notifications
+_notify_tree = []
+
+class notify:
+    def __init__(self,text,log=False,email=False,level=level.informational):
+        '''notify user or log information
+    
+        :text:      the content of the message
+        :log:       if ``True``, will save the message
+        :email:     add to email digests
+        :level:     priority of message, should be '''
+        self.text = text
+        self.log = log
+        self.email = email
+        self.level = level
+        self.id = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(6))
+        if interactive_enabled:
+            notify_interactive(self)
+        else:
+            notify_normal(self)
+        return
+    
+    def __enter__(self):
+        _notify_tree.append(self)
+    
+    def __exit__(self, type, value, traceback):
+        _notify_tree.pop()
+
+def notify_interactive(n):
+    pass
+
+def notify_normal(n):
+    prefix = ''
+    if(len(_notify_tree) > 0):
+        prefix += '  '*len(_notify_tree) + '- '
+    n_prefixed = prefix + n.text + '\n'
+    try:
+        if os.isatty(sys.stderr.fileno()):
+            color = 'green'
+            if n.level >= level.warning:
+                color = 'yellow'
+            if n.level >= level.error:
+                color = 'red'
+            n_prefixed = neural.term.color(n_prefixed,color)
+    except io.UnsupportedOperation:
+        pass
+    sys.stderr.write(n_prefixed)
+    sys.stderr.flush()
+
+### Email notification
+
+class email_digest:
+    def __init__(self,email_address,level=None):
+        '''start an email digest
+        
+        Will try to capture all notifications sent within the following code block that are
+        sent with ``email=True``. In addition, if ``level`` is given, will only collect notifications
+        with the given level or higher'''
+        pass
+    
+    def __enter__(self):
+        pass
+    
+    def __exit__(self, type, value, traceback):
+        pass
+    
+
+### Platform-specific methods:
+
+if platform.system() == 'Darwin':
+    if int(platform.release().split(".")[0])>=12:
+        try:
+            from pync import Notifier
+        except ImportError: 
+            pass
+        else:
+            def notify_mountainlion(n):
+                notify_normal(n)
+                return
+                group_id = '%d-%s' % (os.getpid(),n.id)
+                text = n.text
+                if len(_notify_tree):
+                    temp_tree = [x.text for x in _notify_tree] + [text]
+                    text = '\n'.join(['%s%s' % ('  '*i,temp_tree[i]) for i in xrange(len(temp_tree))])
+                    group_id = '%d-%s' % (os.getpid(),_notify_tree[0].id)
+                    Notifier.remove(group_id)
+                Notifier.notify(text,title='Jarvis',group=group_id)
+            notify_interactive = notify_mountainlion
+            interactive_enabled = True
+
+try:
+    in_ipython = isinstance(sys.stdout,IPython.kernel.zmq.iostream.OutStream)
+except NameError:
+    pass
+else:
+    if in_ipython:
+        # We're inside iPython Notebook
+        try:
+            import IPython.display
+        except ImportError:
+            pass
+        else:
+            def notify_ipython_html(text):
+                html = IPython.display.HTML( '<b>:: %s</b>' % text)
+                IPython.display.display_html(html)
+            notify_interactive = notify_ipython_html
+            interactive_enabled = True
---
+++ packages/neural-fmri/neural-fmri-0.6.2.tar.gz/neural-fmri-0.6.2/neural/utils.py
@@ -0,0 +1,217 @@
+''' This module contains generic helper functions
+
+Many of these are imported into the root-level of the package to make accessing them easier '''
+import neural
+#from notify import notify
+import os,subprocess
+import datetime
+import hashlib
+import zlib, base64
+
+#! A list of archives this library understands
+archive_formats = {
+    'zip': {'suffix':'zip', 'command':lambda output,filename: ['unzip','-o','-d',output,filename]},
+    'tarball': {'suffix':('tar.gz','tgz'), 'command':lambda output,filename: ['tar','zx','-C',output,'-f',filename]},
+    'tarball-bzip': {'suffix':('tar.bz2','tbz'), 'command':lambda output,filename: ['tar','jx','-C',output,'-f',filename]},
+    '7zip': {'suffix':'7z', 'command':lambda output,filename: ['7z','x','-y','-o%s' % output, filename]}
+}
+
+def is_archive(filename):
+    '''returns boolean of whether this filename looks like an archive'''
+    for archive in archive_formats:
+        if filename.endswith(archive_formats[archive]['suffix']):
+            return True
+    return False
+
+def archive_basename(filename):
+    '''returns the basename (name without extension) of a recognized archive file'''
+    for archive in archive_formats:
+        if filename.endswith(archive_formats[archive]['suffix']):
+            return filename.rstrip('.' + archive_formats[archive]['suffix'])
+    return False
+
+def unarchive(filename,output_dir='.'):
+    '''unpacks the given archive into ``output_dir``'''
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+    for archive in archive_formats:
+        if filename.endswith(archive_formats[archive]['suffix']):
+            return subprocess.call(archive_formats[archive]['command'](output_dir,filename))==0
+    return False
+
+def flatten(nested_list):
+    '''converts a list-of-lists to a single flat list'''
+    return_list = []
+    for i in nested_list:
+        if isinstance(i,list):
+            return_list += flatten(i)
+        else:
+            return_list.append(i)
+    return return_list
+
+class run_in:
+    '''temporarily changes into another directory
+    
+    If the directory name you pass doesn't exist, but matches the current directory
+    you are in, it will silently ignore your silliness. Otherwise, it will raise an
+    ``OSError``. If the argument ``create`` is True, it will create the directory instead
+    of raising an error.
+    
+    Example::   
+        with run_in('another_directory'):
+            do_some_stuff_there()
+    '''
+    def __init__(self,working_directory,create=False):
+        self.working_directory = working_directory
+        self.create = create
+
+    def __enter__(self):
+        self.old_cwd = os.getcwd()
+        if self.working_directory:
+            if os.path.exists(self.working_directory):
+                os.chdir(self.working_directory)
+            elif self.old_cwd.endswith(self.working_directory.rstrip('/')):
+                # Silly, we're already in that directory
+                pass
+            else:
+                if self.create:
+                    os.makedirs(self.working_directory)
+                    os.chdir(self.working_directory)
+                else:
+                    raise IOError('Attempting to run_in the non-existent directory "%s"' % self.working_directory)
+    
+    def __exit__(self, type, value, traceback):
+        os.chdir(self.old_cwd)
+
+class RunResult:
+    '''result of calling :meth:`run`
+    
+    when used as a string, will try to reasonably return the filename of the primary output
+    of the command (if known)'''    
+    def __init__(self,output=None,return_code=None,output_filename=None):
+        self.output_filename = output_filename
+        self.output = output
+        self.return_code = return_code
+    
+    def __str__(self):
+        return self.output_filename
+
+def run(command,products=None,working_directory='.',force_local=False):
+    '''wrapper to run external programs
+    
+    :command:           list containing command and parameters 
+                        (formatted the same as subprocess; must contain only strings)
+    :products:          string or list of files that are the products of this command
+                        if all products exist, the command will not be run, and False returned
+    :working_directory: will chdir to this directory
+    :force_local:       when used with `neural.scheduler`, setting to ``True`` will disable
+                        all job distribution functions
+    
+    Returns result in form of :class:`RunResult`
+    '''
+    with run_in(working_directory):
+        if products:
+            if isinstance(products,basestring):
+                products = [products]
+            if all([os.path.exists(x) for x in products]):
+                return False
+        
+        command = flatten(command)
+        command = [str(x) for x in command]
+        with neural.notify('Running %s...' % command[0]):
+            out = None
+            returncode = 0
+            try:
+                out = subprocess.check_output(command)
+            except subprocess.CalledProcessError, e:
+                neural.notify('''ERROR: %s returned a non-zero status
+
+----COMMAND------------
+%s
+-----------------------
+
+                    
+----OUTPUT-------------
+%s
+-----------------------
+Return code: %d
+''' % (command[0],' '.join(command),e.output,e.returncode),level=neural.level.error)
+                returncode = e.returncode
+            result = RunResult(out,returncode)
+            if products and returncode==0:
+                result.output_filename = products[0]
+            return result
+
+def log(fname,msg):
+    ''' generic logging function '''
+    with open(fname,'a') as f:
+        f.write(datetime.datetime.now().strftime('%m-%d-%Y %H:%M:\n') + msg + '\n')
+
+def hash(filename):
+    '''returns string of MD5 hash of given filename'''
+    buffer_size = 10*1024*1024
+    m = hashlib.md5()
+    with open(filename) as f:
+        buff = f.read(buffer_size)
+        while len(buff)>0:
+            m.update(buff)
+            buff = f.read(buffer_size)          
+    dig = m.digest()
+    return ''.join(['%x' % ord(x) for x in dig])
+
+def hash_str(string):
+    '''returns string of MD5 hash of given string'''
+    m = hashlib.md5()
+    m.update(string)
+    dig = m.digest()
+    return ''.join(['%x' % ord(x) for x in dig])
+
+class simple_timer:
+    '''a simple way to time a single run of a function
+    
+    Example::
+        with simple_timer():
+            do_stuff()
+    '''
+    def __init__(self):
+        self.start_time = None
+    
+    def __enter__(self):
+        self.start_time = datetime.datetime.now()
+        print 'timer start time: %s' % self.start_time.strftime('%Y-%m-%d %H:%M:%S')
+    
+    def __exit__(self, type, value, traceback):
+        self.end_time = datetime.datetime.now()
+        print 'timer end time: %s' % self.end_time.strftime('%Y-%m-%d %H:%M:%S')
+        print 'time elapsed: %s' % str(self.end_time-self.start_time)
+
+def factor(n):
+    '''return set of all prime factors for a number'''
+    return set(reduce(list.__add__, ([i, n//i] for i in range(1, int(n**0.5) + 1) if n % i == 0)))
+
+def compress(data, level=9):
+    ''' return compressed, ASCII-encoded string '''
+    return base64.encodestring(zlib.compress(data,9))
+
+def decompress(data):
+    ''' return uncompressed string '''
+    return zlib.decompress(base64.decodestring(data))
+
+def which(program):
+    '''returns full path to program name or ``None`` if not found
+    taken from: http://stackoverflow.com/questions/377017/test-if-executable-exists-in-python'''
+    def is_exe(fpath):
+        return os.path.isfile(fpath) and os.access(fpath, os.X_OK)
+
+    fpath, fname = os.path.split(program)
+    if fpath:
+        if is_exe(program):
+            return program
+    else:
+        for path in os.environ["PATH"].split(os.pathsep):
+            path = path.strip('"')
+            exe_file = os.path.join(path, program)
+            if is_exe(exe_file):
+                return exe_file
+
+    return None
