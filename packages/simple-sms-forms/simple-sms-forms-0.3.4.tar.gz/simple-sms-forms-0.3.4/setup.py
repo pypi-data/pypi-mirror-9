fcb0f5e0a08984b1488cf88c247e5294b2614033
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:10 +0000

    Add package simple-sms-forms/0.3.4/simple-sms-forms-0.3.4.tar.gz

---
+++ packages/simple-sms-forms/simple-sms-forms-0.3.4.tar.gz/simple-sms-forms-0.3.4/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.3.4'
+
+setup(name='simple-sms-forms',
+      version=version,
+      description="Simple SMS text processor for Django/Rapidsms",
+      long_description="""\
+A small package to help in parsing simple text messages to come up with ordered data. Aimed at RapidSMS""",
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='RapidSMS, SMS, Django',
+      author='Andre Lesa',
+      author_email='andre@andrelesa.com',
+      url='https://github.com/AndreLesa/simplesmsforms',
+      license='MIT',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=True,
+      install_requires=[
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
