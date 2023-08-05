68ab2b7c91be5effee728204481050ea480acbbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:13 +0000

    Add package django-charting/0.1.4/django-charting-0.1.4.tar.gz

---
+++ packages/django-charting/django-charting-0.1.4.tar.gz/django-charting-0.1.4/charting/__init__.py
@@ -0,0 +1,12 @@
+from __future__ import unicode_literals
+
+from charts import Chart
+from columns import BaseColumn, StringColumn, NumberColumn
+
+__version__ = '0.1.4'
+
+__charts__ = ['Chart', ]
+
+__columns__ = ['BaseColumn', 'StringColumn', 'NumberColumn', ]
+
+__all__ = __charts__ + __columns__
