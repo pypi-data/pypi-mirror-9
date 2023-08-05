c9407791e41962bc357925a50ed28bdff45a5722
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:38 +0000

    Add package python-twitch/0.3.5/python-twitch-0.3.5.tar.gz

---
+++ packages/python-twitch/python-twitch-0.3.5.tar.gz/python-twitch-0.3.5/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+from twitch import VERSION
+
+setup(
+    name='python-twitch',
+    description='Python Module for interaction with twitch.tv',
+    version=VERSION,
+    packages=['twitch'],
+    license='GPLv3',
+    author='winlu',
+    author_email='derwinlu+python-twitch@gmail.com',
+    url='https://pypi.python.org/pypi/python-twitch',
+)
+
