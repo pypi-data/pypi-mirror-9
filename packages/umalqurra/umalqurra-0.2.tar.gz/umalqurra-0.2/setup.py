80377737494b6be92f5fdca53475d4de2ab6176b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:35 +0000

    Add package umalqurra/0.2/umalqurra-0.2.tar.gz

---
+++ packages/umalqurra/umalqurra-0.2.tar.gz/umalqurra-0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='umalqurra',
+      version='0.2',
+      description='Date Api that support Hijri Umalqurra calendar. Features: 1-Convert Gregorian to Hijri 2-Convert Hijri to Gregorian 3-give the arabic name of the hijri month 4-give the english name of the gregorian month 5-give the day name in Arabic and English 6-give the current day both in Hijri and Gregorian',
+      url='https://github.com/tytkal/python-hijiri-ummalqura',
+      author=' Khalid Al-hussayen',
+      author_email='tytkal@gmail.com',
+      license='Waqef',
+      packages=['umalqurra'],
+      zip_safe=False)
