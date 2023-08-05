9fa1ebd5319258d32e70dbf6918110aad3c5f272
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:15 +0000

    Add package partialorder/0.0.1421587592/partialorder-0.0.1421587592.tar.gz

---
+++ packages/partialorder/partialorder-0.0.1421587592.tar.gz/partialorder-0.0.1421587592/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+	description = 'Provides a partial ordering version of functools.total_ordering',
+	author = 'Mike Lang',
+	author_email = 'mikelang3000@gmail.com',
+	py_modules = ['partialorder'],
+	version = '0.0.1421587592',
+	long_description = 'Provides a partial ordering version of functools.total_ordering',
+	name = 'partialorder',
+)
