ce7fef86bbe5917424d544d4fd626870630bf6df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:12 +0000

    Add package hatak/0.2.7.1/hatak-0.2.7.1.tar.gz

---
+++ packages/hatak/hatak-0.2.7.1.tar.gz/hatak-0.2.7.1/setup.py
@@ -0,0 +1,33 @@
+# -*- encoding: utf-8 -*-
+from setuptools import setup, find_packages
+
+install_requires = [
+    'bael.project>=0.1.4',
+    'pyramid',
+    'pyyaml',
+]
+
+
+if __name__ == '__main__':
+    setup(
+        name='hatak',
+        version='0.2.7.1',
+        description='Small Pyramid extension/project managment',
+        url='https://github.com/socek/hatak',
+        license='Apache License 2.0',
+        packages=find_packages('src'),
+        package_dir={'': 'src'},
+        install_requires=install_requires,
+        namespace_packages=['bael'],
+        zip_safe=False,
+        package_data={
+            '': [
+                'hatak/templates/*.ini',
+                'hatak/templates/*.tpl',
+                'hatak/templates/project/*.tpl'],
+        },
+        entry_points="""\
+          [console_scripts]
+              hatak = bael.hatak.recipe:run
+        """,
+    )
