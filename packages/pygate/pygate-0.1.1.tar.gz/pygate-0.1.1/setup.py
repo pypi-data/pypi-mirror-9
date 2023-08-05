a88cae26560e6e97f08d2ce9582c06f747db98c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:58 +0000

    Add package pygate/0.1.1/pygate-0.1.1.tar.gz

---
+++ packages/pygate/pygate-0.1.1.tar.gz/pygate-0.1.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='pygate',
+      version='0.1.1',
+      description='Decorators and tools to \'gate\' a function (check arguments and return values)',
+      url='',
+      author='James Larsen',
+      author_email='somesocks@gmail.com',
+      license='MIT',
+      packages=['pygate'],
+      zip_safe=True)
+
