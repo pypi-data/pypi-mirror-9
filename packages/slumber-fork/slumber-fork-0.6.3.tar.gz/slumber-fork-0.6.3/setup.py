12017ddd2b1f25c10a6fa9f6021678900b3e7f2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:05 +0000

    Add package slumber-fork/0.6.3/slumber-fork-0.6.3.tar.gz

---
+++ packages/slumber-fork/slumber-fork-0.6.3.tar.gz/slumber-fork-0.6.3/setup.py
@@ -0,0 +1,40 @@
+import os
+import sys
+from setuptools import setup
+
+install_requires = ["requests"]
+tests_require = ["mock"]
+
+base_dir = os.path.dirname(os.path.abspath(__file__))
+
+version = "0.6.3"
+
+if sys.argv[-1] == 'publish':
+    os.system("git tag -a %s -m 'v%s'" % (version, version))
+    os.system("python setup.py sdist bdist_wheel upload -r pypi")
+    print("Published version %s, do `git push --tags` to push new tag to remote" % version)
+    sys.exit()
+
+if sys.argv[-1] == 'syncci':
+    os.system("panci --to=tox .travis.yml > tox.ini");
+    sys.exit();
+
+setup(
+    name = "slumber-fork",
+    version = version,
+    description = "Fork of Slumber, A library that makes consuming a REST API easier and more convenient",
+    long_description="\n\n".join([
+        open(os.path.join(base_dir, "README.rst"), "r").read(),
+        open(os.path.join(base_dir, "CHANGELOG.rst"), "r").read()
+    ]),
+    url = "http://github.com/futurice/slumber",
+    author = "Donald Stufft",
+    author_email = "donald.stufft@gmail.com",
+    maintainer = "Samuel Giles",
+    maintainer_email = "sam.e.giles@gmail.com",
+    packages = ["slumber"],
+    zip_safe = False,
+    install_requires = install_requires,
+    tests_require = tests_require,
+    test_suite = "tests.get_tests",
+)
