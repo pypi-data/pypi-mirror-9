9edd6938e70d92b771e9db1710f1aa113d8e6340
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:08 +0000

    Add package mf2util/0.1.5/mf2util-0.1.5.tar.gz

---
+++ packages/mf2util/mf2util-0.1.5.tar.gz/mf2util-0.1.5/setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+from distutils.core import setup, Command
+
+
+class PyTest(Command):
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        import sys
+        import subprocess
+        errno = subprocess.call([sys.executable, 'runtests.py'])
+        raise SystemExit(errno)
+
+
+setup(name='mf2util',
+      version='0.1.5',
+      description='Python Microformats2 utilities',
+      long_description='Companion package for mf2py microformats2 parser. '
+      'Provides utility functions to interpret post permalinks for display '
+      'as comments or reply-context',
+      author='Kyle Mahan',
+      author_email='kyle@kylewm.com',
+      url='http://indiewebcamp.com/mf2util',
+      packages=['mf2util'],
+      test_requires=['pytest'],
+      cmdclass={'test': PyTest},
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: BSD License',
+          'Natural Language :: English',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 3',
+          'Topic :: Text Processing :: Markup :: HTML'
+      ]
+  )
