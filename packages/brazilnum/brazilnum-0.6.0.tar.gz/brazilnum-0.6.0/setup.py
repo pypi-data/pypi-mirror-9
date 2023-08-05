894dd1f44f805bca4f60f2fa507186c75ba4e44c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:40 +0000

    Add package brazilnum/0.6.0/brazilnum-0.6.0.tar.gz

---
+++ packages/brazilnum/brazilnum-0.6.0.tar.gz/brazilnum-0.6.0/setup.py
@@ -0,0 +1,30 @@
+from distutils.core import setup
+
+setup(
+        name = 'brazilnum',
+        packages = ['brazilnum'],
+        version = '0.6.0',
+        description = 'Validators for Brazilian CNPJ, CPF, and PIS/PASEP numbers.',
+        author = 'Chris Poliquin',
+        author_email = 'cpoliquin@hbs.edu',
+        url = 'https://github.com/poliquin/brazilnum',
+        keywords = ['brazil', 'cnpj', 'cpf', 'pis', 'pasep'],
+        classifiers = [
+            'Programming Language :: Python',
+            'Programming Language :: Python :: 3',
+            'License :: OSI Approved :: MIT License',
+            'Operating System :: OS Independent',
+            'Development Status :: 4 - Beta',
+            'Intended Audience :: Developers',
+            'Topic :: Utilities'
+            ],
+        long_description = """\
+Validate Brazilian Identification Numbers
+-----------------------------------------
+
+Python functions for working with CNPJ, CPF, and PIS/PASEP numbers,
+which identify firms and people in Brazil.
+"""
+)
+
+
