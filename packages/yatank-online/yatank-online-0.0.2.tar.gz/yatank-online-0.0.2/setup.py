723f095981a6d8ee2970169743029df9efe8207c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:05 +0000

    Add package yatank-online/0.0.2/yatank-online-0.0.2.tar.gz

---
+++ packages/yatank-online/yatank-online-0.0.2.tar.gz/yatank-online-0.0.2/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='yatank-online',
+    version='0.0.2',
+    description='Yandex.Tank OnlineReport plugin',
+    author='Alexey Lavrenuke',
+    author_email='direvius@gmail.com',
+    url='https://github.com/yandex-load/yatank-online/',
+    packages=['yatank_OnlineReport'],
+    package_data={'yatank_OnlineReport': [
+        'templates/*',
+        'static/favicon.ico',
+        'static/css/*',
+        'static/js/*.js',
+        'static/js/*.coffee',
+        'static/js/vendor/*.js',
+        'static/fonts/*',
+    ]},
+    install_requires=[
+        'tornado',
+        'tornadio2',
+        'pyjade',
+    ],
+)
