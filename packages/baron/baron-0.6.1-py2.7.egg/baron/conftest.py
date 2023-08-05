db9577dc923950ea578d5e553c2165e02e4edb5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:12 +0000

    Add package baron/0.6.1/baron-0.6.1-py2.7.egg

---
+++ packages/baron/baron-0.6.1-py2.7.egg/baron/conftest.py
@@ -0,0 +1,8 @@
+import json
+
+def pytest_assertrepr_compare(config, op, left, right):
+    if isinstance(left, list) and isinstance(right, list) and op == "==":
+        with open("/tmp/a", "w") as a:
+            a.write(json.dumps(left, indent=4, sort_keys=True))
+        with open("/tmp/b", "w") as b:
+            b.write(json.dumps(right, indent=4, sort_keys=True))
