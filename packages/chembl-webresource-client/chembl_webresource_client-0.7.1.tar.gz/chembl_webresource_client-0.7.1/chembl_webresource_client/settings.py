b394525fc19c5e8be818e4f6701a654698d4110b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:10 +0000

    Add package chembl-webresource-client/0.7.1/chembl_webresource_client-0.7.1.tar.gz

---
+++ packages/chembl-webresource-client/chembl_webresource_client-0.7.1.tar.gz/chembl_webresource_client-0.7.1/chembl_webresource_client/settings.py
@@ -0,0 +1,27 @@
+__author__ = 'mnowotka'
+
+from chembl_webresource_client.singleton import Singleton
+
+
+@Singleton
+class Settings:
+    WEBSERVICE_PROTOCOL = 'https'
+    WEBSERVICE_DOMAIN = 'www.ebi.ac.uk'
+    WEBSERVICE_PREFIX = '/chemblws'
+    CACHING = True
+    FAST_SAVE = True
+    CONCURRENT_SIZE = 50
+    ASYNC_TRESHOLD = 10
+    CACHE_NAME = 'chembl_webresource_client'
+    RESPECT_RATE_LIMIT = True
+    TIMEOUT = 3.0
+    UTILS_SPORE_URL = 'https://www.ebi.ac.uk/chembl/api/utils/spore'
+    PROXIES = None
+
+    @property
+    def webservice_root_url(self):
+        return '%s://%s%s' % (self.WEBSERVICE_PROTOCOL, self.WEBSERVICE_DOMAIN, self.WEBSERVICE_PREFIX)
+
+    def clear_cache(self):
+        from requests_cache import clear
+        clear()
