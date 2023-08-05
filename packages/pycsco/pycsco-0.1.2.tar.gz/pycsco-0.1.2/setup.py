2f8c29c80e3b1fc2140919843fe0d9e257efc41a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:59 +0000

    Add package pycsco/0.1.2/pycsco-0.1.2.tar.gz

---
+++ packages/pycsco/pycsco-0.1.2.tar.gz/pycsco-0.1.2/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+  name='pycsco',
+  packages=find_packages(),
+  version='0.1.2',
+  description='Python modules to simplify working with Cisco NX-OS devices ',
+  author='Jason Edelman',
+  author_email='jedelman8@gmail.com',
+  url='https://github.com/jedelman8/pycsco',
+  download_url='https://github.com/jedelman8/pycsco/tarball/0.1.2',
+  install_requires=[
+      'xmltodict==0.9.2',
+  ],
+)
