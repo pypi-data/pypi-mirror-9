32afe81ccc22fa12b1258c6411b5229bc406c99b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:02 +0000

    Add package trytond_nereid_checkout/3.2.0.13/trytond_nereid_checkout-3.2.0.13.tar.gz

---
+++ packages/trytond_nereid_checkout/trytond_nereid_checkout-3.2.0.13.tar.gz/trytond_nereid_checkout-3.2.0.13/__init__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+'''
+
+    nereid_checkout
+
+    :copyright: (c) 2010-2013 by Openlabs Technologies & Consulting (P) Ltd.
+    :license: GPLv3, see LICENSE for more details
+
+'''
+from trytond.pool import Pool
+
+from sale import Sale
+from payment import Website, NereidPaymentMethod
+from checkout import Cart, Checkout, Party, Address
+
+
+def register():
+    Pool.register(
+        Cart,
+        Sale,
+        Party,
+        Website,
+        Checkout,
+        NereidPaymentMethod,
+        Address,
+        type_="model", module="nereid_checkout"
+    )
