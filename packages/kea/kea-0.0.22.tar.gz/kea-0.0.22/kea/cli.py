8e43b64b944d82c24f07a0ecc330dfeeb49ce5e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:20 +0000

    Add package kea/0.0.22/kea-0.0.22.tar.gz

---
+++ packages/kea/kea-0.0.22.tar.gz/kea-0.0.22/kea/cli.py
@@ -0,0 +1,28 @@
+
+import os
+import sys
+
+import leip
+
+from kea import Kea
+
+
+def dispatch():
+    """
+    Run the MadMax app
+    """
+    app.run()
+
+#check if there is a kea command
+
+command = None
+for i in range(1, len(sys.argv)):
+    if not sys.argv[i].startswith('-'):
+        command = sys.argv[i]
+        break
+
+
+if command in ['conf', 'snipset', 'jobset', 'js', 'run', 'jobrun', 'jr', 'list_executors']:
+    app = leip.app('kea', partial_parse = True)
+else:
+    app = Kea()
