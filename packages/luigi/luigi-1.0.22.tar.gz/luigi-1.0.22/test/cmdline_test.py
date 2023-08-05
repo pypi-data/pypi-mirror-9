352e7e84930d5d7d17576abf06ad5812c73d6270
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:45 +0000

    Add package luigi/1.0.22/luigi-1.0.22.tar.gz

---
+++ packages/luigi/luigi-1.0.22.tar.gz/luigi-1.0.22/test/cmdline_test.py
@@ -0,0 +1,156 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright 2012-2015 Spotify AB
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+import ConfigParser
+import logging
+import os
+import subprocess
+import unittest
+import warnings
+
+import luigi
+import mock
+from luigi.mock import MockFile
+
+
+class SomeTask(luigi.Task):
+    n = luigi.IntParameter()
+
+    def output(self):
+        return File('/tmp/test_%d' % self.n)
+
+    def run(self):
+        f = self.output().open('w')
+        f.write('done')
+        f.close()
+
+
+class AmbiguousClass(luigi.Task):
+    pass
+
+
+class AmbiguousClass(luigi.Task):
+    pass
+
+
+class NonAmbiguousClass(luigi.ExternalTask):
+    pass
+
+
+class NonAmbiguousClass(luigi.Task):
+
+    def run(self):
+        NonAmbiguousClass.has_run = True
+
+
+class TaskWithSameName(luigi.Task):
+
+    def run(self):
+        self.x = 42
+
+
+class TaskWithSameName(luigi.Task):
+    # there should be no ambiguity
+
+    def run(self):
+        self.x = 43
+
+
+class WriteToFile(luigi.Task):
+    filename = luigi.Parameter()
+
+    def output(self):
+        return luigi.LocalTarget(self.filename)
+
+    def run(self):
+        f = self.output().open('w')
+        print >>f, 'foo'
+        f.close()
+
+
+class CmdlineTest(unittest.TestCase):
+
+    def setUp(self):
+        global File
+        File = MockFile
+        MockFile.fs.clear()
+
+    @mock.patch("logging.getLogger")
+    def test_cmdline_main_task_cls(self, logger):
+        luigi.run(['--local-scheduler', '--no-lock', '--n', '100'], main_task_cls=SomeTask)
+        self.assertEqual(dict(MockFile.fs.get_all_data()), {'/tmp/test_100': 'done'})
+
+    @mock.patch("logging.getLogger")
+    def test_cmdline_other_task(self, logger):
+        luigi.run(['--local-scheduler', '--no-lock', 'SomeTask', '--n', '1000'])
+        self.assertEqual(dict(MockFile.fs.get_all_data()), {'/tmp/test_1000': 'done'})
+
+    @mock.patch("logging.getLogger")
+    def test_cmdline_ambiguous_class(self, logger):
+        self.assertRaises(Exception, luigi.run, ['--local-scheduler', '--no-lock', 'AmbiguousClass'])
+
+    @mock.patch("logging.getLogger")
+    @mock.patch("warnings.warn")
+    def test_cmdline_non_ambiguous_class(self, warn, logger):
+        luigi.run(['--local-scheduler', '--no-lock', 'NonAmbiguousClass'])
+        self.assertTrue(NonAmbiguousClass.has_run)
+
+    @mock.patch("logging.getLogger")
+    @mock.patch("logging.StreamHandler")
+    def test_setup_interface_logging(self, handler, logger):
+        handler.return_value = mock.Mock(name="stream_handler")
+        with mock.patch("luigi.interface.setup_interface_logging.has_run", new=False):
+            luigi.interface.setup_interface_logging()
+            self.assertEqual([mock.call(handler.return_value)], logger.return_value.addHandler.call_args_list)
+
+        with mock.patch("luigi.interface.setup_interface_logging.has_run", new=False):
+            self.assertRaises(ConfigParser.NoSectionError, luigi.interface.setup_interface_logging, '/blah')
+
+    @mock.patch("warnings.warn")
+    @mock.patch("luigi.interface.setup_interface_logging")
+    def test_cmdline_logger(self, setup_mock, warn):
+        with mock.patch("luigi.interface.core") as env_params:
+            env_params.return_value.logging_conf_file = None
+            luigi.run(['SomeTask', '--n', '7', '--local-scheduler', '--no-lock'])
+            self.assertEqual([mock.call(None)], setup_mock.call_args_list)
+
+        with mock.patch("luigi.configuration.get_config") as getconf:
+            getconf.return_value.get.side_effect = ConfigParser.NoOptionError(section='foo', option='bar')
+            getconf.return_value.get_boolean.return_value = True
+
+            luigi.interface.setup_interface_logging.call_args_list = []
+            luigi.run(['SomeTask', '--n', '42', '--local-scheduler', '--no-lock'])
+            self.assertEqual([], setup_mock.call_args_list)
+
+    @mock.patch('argparse.ArgumentParser.print_usage')
+    def test_non_existent_class(self, print_usage):
+        self.assertRaises(SystemExit, luigi.run, ['--local-scheduler', '--no-lock', 'XYZ'])
+
+    def test_bin_luigi(self):
+        t = luigi.LocalTarget(is_tmp=True)
+        cmd = ['./bin/luigi', '--module', 'cmdline_test', 'WriteToFile', '--filename', t.path, '--local-scheduler', '--no-lock']
+        env = os.environ.copy()
+        env['PYTHONPATH'] = env.get('PYTHONPATH', '') + ':.:test'
+        subprocess.check_call(cmd, env=env, stderr=subprocess.STDOUT)
+        self.assertTrue(t.exists())
+
+    @mock.patch('argparse.ArgumentParser.print_usage')
+    def test_no_task(self, print_usage):
+        self.assertRaises(SystemExit, luigi.run, ['--local-scheduler', '--no-lock'])
+
+if __name__ == '__main__':
+    unittest.main()
