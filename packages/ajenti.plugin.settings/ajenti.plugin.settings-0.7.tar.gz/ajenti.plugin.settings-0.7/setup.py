c302fc3a24d1744c99ffe4f5cf5593f49512851f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:04 +0000

    Add package ajenti.plugin.settings/0.7/ajenti.plugin.settings-0.7.tar.gz

---
+++ packages/ajenti.plugin.settings/ajenti.plugin.settings-0.7.tar.gz/ajenti.plugin.settings-0.7/setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+import os
+
+__requires = filter(None, open('requirements.txt').read().splitlines())
+
+setup(
+    name='ajenti.plugin.settings',
+    version='0.7',
+    install_requires=__requires,
+    description='Ajenti settings plugin',
+    author='Ajenti project',
+    author_email='e@ajenti.org',
+    url='http://ajenti.org',
+    packages=find_packages(),
+    include_package_data=True,
+)
