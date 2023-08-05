f93e8a1d17f2198cd0704d2ad89c3eab246ceb21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:53 +0000

    Add package geonode-user-messages/0.1.2/geonode-user-messages-0.1.2.tar.gz

---
+++ packages/geonode-user-messages/geonode-user-messages-0.1.2.tar.gz/geonode-user-messages-0.1.2/setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+
+
+setup(
+    name = "geonode-user-messages",
+    version = "0.1.2",
+    author = "Eldarion",
+    author_email = "development@eldarion.com",
+    description = "Fork of user-messages: a reusable private user messages application for Django",
+    long_description = open("README.rst").read(),
+    license = "BSD",
+    url = "http://github.com/GeoNode/user_messages",
+    packages = [
+        "user_messages",
+        "user_messages.tests",
+        "user_messages.templatetags",
+    ],
+    classifiers = [
+        "Development Status :: 3 - Alpha",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Framework :: Django",
+    ]
+)
