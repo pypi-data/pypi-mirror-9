b37355b46de1df4e6fb48458d6aaf31e30663153
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:11 +0000

    Add package dogepy/46.3/dogepy-46.3.tar.gz

---
+++ packages/dogepy/dogepy-46.3.tar.gz/dogepy-46.3/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(name='dogepy',
+      version='46.3',
+      description='So doge, much art, wow',
+      url='https://github.com/laasli/doge',
+      author='Laasli, Morz',
+      author_email='morzzz007@gmail.com',
+      license='BSD',
+      packages=['dogepy'],
+      zip_safe=True)
