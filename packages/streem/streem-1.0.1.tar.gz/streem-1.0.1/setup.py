ae60e3c86cfc5f10073c25439ca01f5242078908
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:15 +0000

    Add package streem/1.0.1/streem-1.0.1.tar.gz

---
+++ packages/streem/streem-1.0.1.tar.gz/streem-1.0.1/setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+
+setup(
+    name = "streem",
+    packages = ["streem"],
+    version = "1.0.1",
+    description = "Turning streams into trees",
+    author = "Roman Odaisky",
+    author_email = "roma@qwertty.com",
+    url = "https://github.com/rdaysky/streem",
+    download_url = "https://github.com/rdaysky/streem/archive/v1.0.1.tar.gz",
+    keywords = ["stream", "tree"],
+    install_requires = ["more_itertools"],
+    classifiers = [
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development",
+    ],
+)
