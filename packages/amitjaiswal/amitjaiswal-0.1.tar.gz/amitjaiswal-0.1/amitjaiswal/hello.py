f1f77d8cfea1df40cc1e4a8fbb5a751c5a67ed04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:49 +0000

    Add package amitjaiswal/0.1/amitjaiswal-0.1.tar.gz

---
+++ packages/amitjaiswal/amitjaiswal-0.1.tar.gz/amitjaiswal-0.1/amitjaiswal/hello.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# Filename: hello.py
+#author:amit jaiswal
+#version: 0.1
+
+def helloworld():
+   print "hello"
+
+version = '0.1'
+   
+   
+# end of module   
---
+++ packages/amitjaiswal/amitjaiswal-0.1.tar.gz/amitjaiswal-0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'amitjaiswal',
+  packages = ['amitjaiswal'], # this must be the same as the name above
+  version = '0.1',
+  description = 'A random test lib',
+  author = 'Amit Jaiswal',
+  author_email = 'amitjaiswal396@gmail.com',
+  url = 'https://github.com/amitjaiswal396/amitjaiswal', # use the URL to the github repo
+  download_url = 'https://github.com/amitjaiswal396/amitjaiswal/tarball/0.1', # I'll explain this in a second
+  keywords = ['testing', 'logging', 'example'], # arbitrary keywords
+  classifiers = [],
+)
