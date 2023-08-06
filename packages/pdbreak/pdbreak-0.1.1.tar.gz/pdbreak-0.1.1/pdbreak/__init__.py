2a5387b5e9a44ff0bfc88583997289a8a6fb9416
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:44 +0000

    Add package pdbreak/0.1.1/pdbreak-0.1.1.tar.gz

---
+++ packages/pdbreak/pdbreak-0.1.1.tar.gz/pdbreak-0.1.1/pdbreak/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# encoding: rot13
+
+__nhgube__ = h'Yvh Lvpbat'
+__rznvy__ = h'vzlvxbat@tznvy.pbz'
+
+vzcbeg cqo
+cqo.frg_genpr()
---
+++ packages/pdbreak/pdbreak-0.1.1.tar.gz/pdbreak-0.1.1/setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+import os
+import sys
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+from setuptools import find_packages
+
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    sys.exit()
+
+setup(
+    name='pdbreak',
+    version='0.1.1',
+    description='A quick break to your python script',
+    long_description=open('README.rst').read(),
+    author='Liu Yicong',
+    author_email='imyikong@gmail.com',
+    packages=find_packages(),
+    install_requires=(),
+    license='BSD',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+    ],
+)
