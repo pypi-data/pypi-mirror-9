4d520ab81b1fb4f0622e6c2d9bc7efe0612b7314
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:55 +0000

    Add package unittester/2/unittester-2.tar.gz

---
+++ packages/unittester/unittester-2.tar.gz/unittester-2/setup.py
@@ -0,0 +1,16 @@
+# coding=utf-8
+"""
+appinstance
+erik@a8.nl (04-03-15)
+license: GNU-GPL2
+"""
+from setuptools import setup
+setup(name='unittester',
+      version='2',
+      description='Run python unit-tests as command-line applications (class and method based).',
+      url='https://github.com/erikdejonge/unittester',
+      author='Erik de Jonge',
+      author_email='erik@a8.nl',
+      license='GPL',
+      packages=['unittester'],
+      zip_safe=True, requires=['pyprofiler'])
