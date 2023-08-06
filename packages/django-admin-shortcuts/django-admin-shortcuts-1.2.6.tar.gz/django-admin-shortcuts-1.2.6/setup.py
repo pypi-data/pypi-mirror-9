6bc31472b164a7f43c86e8063e2442579b404e8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:45 +0000

    Add package django-admin-shortcuts/1.2.6/django-admin-shortcuts-1.2.6.tar.gz

---
+++ packages/django-admin-shortcuts/django-admin-shortcuts-1.2.6.tar.gz/django-admin-shortcuts-1.2.6/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+import os
+import admin_shortcuts
+
+CLASSIFIERS = [
+    "Development Status :: 3 - Alpha",
+    "Framework :: Django",
+    "Intended Audience :: Developers",
+    "License :: OSI Approved :: BSD License",
+    "Operating System :: OS Independent",
+    "Programming Language :: Python :: 2.6",
+    "Programming Language :: Python :: 2.7",
+    "Topic :: Software Development",
+    "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+]
+
+setup(
+    author="Ales Kocjancic",
+    author_email="alesdotio@gmail.com",
+    name='django-admin-shortcuts',
+    version=admin_shortcuts.__version__,
+    description='Add simple and pretty shortcuts to the django admin homepage.',
+    long_description=open(os.path.join(os.path.dirname(__file__), 'README.rst')).read(),
+    url='https://github.com/alesdotio/django-admin-shortcuts',
+    packages=find_packages(),
+    include_package_data = True,
+    zip_safe = False,
+    license='BSD License',
+    platforms=['OS Independent'],
+    classifiers=CLASSIFIERS,
+    install_requires=[
+        'Django>=1.2',
+    ],
+)
+
