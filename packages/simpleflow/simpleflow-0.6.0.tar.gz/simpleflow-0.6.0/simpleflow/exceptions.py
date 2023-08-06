972ad97088ee329471b406b58d4c0b95bf3073fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:23:23 +0000

    Add package simpleflow/0.6.0/simpleflow-0.6.0.tar.gz

---
+++ packages/simpleflow/simpleflow-0.6.0.tar.gz/simpleflow-0.6.0/simpleflow/exceptions.py
@@ -0,0 +1,56 @@
+class ExecutionBlocked(Exception):
+    pass
+
+
+class TaskException(Exception):
+    """
+    Wrap an exception raised by a task.
+
+    """
+    def __init__(self, task, exception):
+        """
+        :param exception: raised by a task.
+        :type  exception: TaskFailed.
+
+        """
+        self.task = task
+        self.exception = exception
+
+    def __repr__(self):
+        return '{}(task={} exception={})'.format(
+            self.__class__.__name__,
+            self.task,
+            self.exception)
+
+
+class TaskFailed(Exception):
+    """
+    Wrap the error's *reason* and *details* for an task that failed.
+
+    :param reason: of the failure.
+    :type  reason: str.
+    :param details: of the failure.
+    :type  details: str.
+
+    """
+    def __init__(self, reason, details=None):
+        super(TaskFailed, self).__init__(self, reason, details)
+        self.reason = reason
+        self.details = None
+
+    def __repr__(self):
+        return '{}(reason="{}")'.format(
+            self.__class__.__name__,
+            self.reason,
+            self.details)
+
+
+class TimeoutError(Exception):
+    def __init__(self, timeout_type='unknown timeout', timeout_value=None):
+        self.timeout_type = timeout_type
+        self.timeout_value = timeout_value
+
+    def __repr__(self):
+        return '{}({})'.format(
+            self.__class__.__name__,
+            self.timeout_type)
