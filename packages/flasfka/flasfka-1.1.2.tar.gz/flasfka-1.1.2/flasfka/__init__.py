7e9ea375ecbf19a01aa4e27d3b391a403db581ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:21 +0000

    Add package flasfka/1.1.2/flasfka-1.1.2.tar.gz

---
+++ packages/flasfka/flasfka-1.1.2.tar.gz/flasfka-1.1.2/flasfka/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+__version__ = "1.1.2"
+
+# "import flask" is wrapped in a try/catch clause for setup.py to get the
+# version without having flask
+try:
+    import flask
+    app = flask.Flask(__name__)
+    from . import api
+except ImportError:
+    pass
