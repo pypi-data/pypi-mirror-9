dbd1e1d0173c5d9b790189f0c9b68156cff455cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:46 +0000

    Add package django-advanced-reports/0.9.7/django-advanced-reports-0.9.7.tar.gz

---
+++ packages/django-advanced-reports/django-advanced-reports-0.9.7.tar.gz/django-advanced-reports-0.9.7/advanced_reports/__init__.py
@@ -0,0 +1,23 @@
+__version__ = '0.9.7'
+
+
+REGISTRY = {}
+
+
+def register(advreport):
+    REGISTRY[advreport.slug] = advreport
+
+
+def get_report_for_slug(slug):
+    return REGISTRY.get(slug, lambda: None)()
+
+
+def get_report_or_404(slug):
+    advreport = get_report_for_slug(slug)
+    if advreport is None:
+        from django.http import Http404
+        raise Http404('No AdvancedReport matches the given query.')
+
+    advreport.internal_mode = False
+    advreport.report_header_visible = True
+    return advreport
---
+++ packages/django-advanced-reports/django-advanced-reports-0.9.7.tar.gz/django-advanced-reports-0.9.7/advanced_reports/urls.py
@@ -0,0 +1,24 @@
+from django.conf.urls import patterns, url
+from advanced_reports.views import *
+
+urlpatterns = patterns('',
+    url(r'^(?P<slug>[^/]+)/$', list, name='advanced_reports_list'),
+    
+    url(r'^(?P<slug>[^/]+)/form/(?P<method>[^/]+)/(?P<object_id>[^/]+)/$', ajax_form, name='advanced_reports_form'),
+    url(r'^(?P<slug>[^/]+)/form/(?P<method>[^/]+)/(?P<object_id>[^/]+)/(?P<param>[^/]+)/$', ajax_form, name='advanced_reports_form'),
+    
+    url(r'^(?P<slug>[^/]+)/action/(?P<method>[^/]+)/(?P<object_id>[^/]+)/$', action, name='advanced_reports_action'),
+    url(r'^(?P<slug>[^/]+)/action/(?P<method>[^/]+)/(?P<object_id>[^/]+)/(?P<param>[^/]+)/$', action, name='advanced_reports_action'),
+    
+    url(r'^(?P<slug>[^/]+)/ajax/(?P<method>[^/]+)/(?P<object_id>[^/]+)/$', ajax, name='advanced_reports_ajax'),
+    url(r'^(?P<slug>[^/]+)/ajax/(?P<method>[^/]+)/(?P<object_id>[^/]+)/(?P<param>[^/]+)/$', ajax, name='advanced_reports_ajax'),
+    
+    url(r'^(?P<slug>[^/]+)/count/$', count, name='advanced_reports_count'),
+
+    url(r'^api/(?P<slug>[^/]+)/$', api_list, name='advanced_reports_api_list'),
+    url(r'^api/(?P<slug>[^/]+)/action/(?P<method>[^/]+)/(?P<object_id>[^/]+)/$', api_action, name='advanced_reports_api_action'),
+    url(r'^api/(?P<slug>[^/]+)/form/(?P<method>[^/]+)/(?P<object_id>[^/]+)/$', api_form,
+        name='advanced_reports_api_form'),
+    url(r'^api/(?P<slug>[^/]+)/form/(?P<method>[^/]+)/$', api_form,
+        name='advanced_reports_api_form'),
+)
