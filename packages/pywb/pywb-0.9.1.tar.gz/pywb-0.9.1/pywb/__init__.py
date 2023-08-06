8ba6a2a0bfbd49236b55edc11519e9048829ab1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:56 +0000

    Add package pywb/0.9.1/pywb-0.9.1.tar.gz

---
+++ packages/pywb/pywb-0.9.1.tar.gz/pywb-0.9.1/pywb/__init__.py
@@ -0,0 +1,10 @@
+__version__ = '0.9.1'
+
+DEFAULT_CONFIG = 'pywb/default_config.yaml'
+
+
+def get_test_dir():
+    import os
+    return os.path.join(os.path.dirname(os.path.realpath(__file__)),
+                                        '..',
+                                        'sample_archive') + os.path.sep
