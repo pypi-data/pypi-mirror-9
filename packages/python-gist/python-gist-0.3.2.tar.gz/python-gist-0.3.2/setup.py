9e088b2557e04ce86f2bf4a3b3a440111d68b168
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:10:05 +0000

    Add package python-gist/0.3.2/python-gist-0.3.2.tar.gz

---
+++ packages/python-gist/python-gist-0.3.2.tar.gz/python-gist-0.3.2/setup.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+
+import setuptools
+
+
+setuptools.setup(
+        name='python-gist',
+        version='0.3.2',
+        description='Manage github gists',
+        license='MIT',
+        long_description=(open('README.rst').read()),
+        author='Joshua Downer',
+        author_email='joshua.downer@gmail.com',
+        url='http://github.com/jdowner/gist',
+        keywords='gist github git',
+        packages=['gist'],
+        package_data={
+          '': ['share/*', '*.rst', 'LICENSE'],
+        },
+        data_files=[
+          ('share/gist/', [
+              'README.rst',
+              'LICENSE',
+              'share/gist.bash',
+              'share/gist.zsh',
+              'share/gist-fzsl.bash',
+              'share/gist-fzf.bash',
+              ]),
+        ],
+        scripts=['bin/gist'],
+        install_requires=[
+            'docopt',
+            'python-gnupg',
+            'pep8',
+            'requests',
+            'responses',
+            'simplejson',
+            'tox',
+            ],
+        platforms=['Unix'],
+        test_suite="tests",
+        classifiers=[
+            'Development Status :: 4 - Beta',
+            'Environment :: Console',
+            'Intended Audience :: Developers',
+            'Intended Audience :: End Users/Desktop',
+            'Intended Audience :: System Administrators',
+            'License :: OSI Approved :: MIT License',
+            'Operating System :: Unix',
+            'Programming Language :: Python',
+            'Programming Language :: Python :: 2',
+            'Programming Language :: Python :: 2.7',
+            'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3.3',
+            'Programming Language :: Python :: 3.4',
+            'Topic :: Software Development',
+            'Topic :: Software Development :: Version Control',
+            'Topic :: Utilities',
+            ]
+        )
