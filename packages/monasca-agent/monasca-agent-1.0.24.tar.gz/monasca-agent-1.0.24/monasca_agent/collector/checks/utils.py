30ac6dd7edcec88c952329526fa712959f6fdda1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:36:11 +0000

    Add package monasca-agent/1.0.24/monasca-agent-1.0.24.tar.gz

---
+++ packages/monasca-agent/monasca-agent-1.0.24.tar.gz/monasca-agent-1.0.24/monasca_agent/collector/checks/utils.py
@@ -0,0 +1,11 @@
+import base64
+
+
+def add_basic_auth(request, username, password):
+    """A helper to add basic authentication to a urllib2 request.
+
+    We do this across a variety of checks so it's good to have this in one place.
+    """
+    auth_str = base64.encodestring('%s:%s' % (username, password)).strip()
+    request.add_header('Authorization', 'Basic %s' % auth_str)
+    return request
