8800e4f1a659521052f22ebb1a0da0e69c783da8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:16 +0000

    Add package eloquent/0.2/eloquent-0.2.tar.gz

---
+++ packages/eloquent/eloquent-0.2.tar.gz/eloquent-0.2/setup.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+__version__ = '0.2'
+
+setup(
+    name='eloquent',
+    license='MIT',
+    version=__version__,
+    description='The Eloquent ORM provides a simple yet beautiful ActiveRecord implementation.',
+    long_description=open('README.rst').read(),
+    author='Sébastien Eustace',
+    author_email='sebastien.eustace@gmail.com',
+    url='https://github.com/sdispater/eloquent',
+    download_url='https://github.com/sdispater/eloquent/archive/%s.tar.gz' % __version__,
+    packages=find_packages(),
+    install_requires=['simplejson', 'arrow', 'inflection', 'six'],
+    tests_require=['pytest', 'mock', 'flexmock'],
+    test_suite='nose.collector',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
---
+++ packages/eloquent/eloquent-0.2.tar.gz/eloquent-0.2/tests/utils.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+
+from . import mock
+from eloquent.connections.connection_interface import ConnectionInterface
+from eloquent.query.processors.processor import QueryProcessor
+from eloquent.database_manager import DatabaseManager
+from eloquent.connectors.connection_factory import ConnectionFactory
+from eloquent.query.builder import QueryBuilder
+from eloquent.orm.model import Model
+
+
+class MockConnection(ConnectionInterface):
+
+    def set_reconnector(self, reconnector):
+        return mock.MagicMock()
+
+    def prepare_mock(self):
+        self.table = mock.MagicMock()
+        self.select = mock.MagicMock()
+        self.insert = mock.MagicMock()
+        self.update = mock.MagicMock()
+        self.delete = mock.MagicMock()
+        self.statement = mock.MagicMock()
+
+        return self
+
+
+class MockProcessor(QueryProcessor):
+
+    def prepare_mock(self):
+        self.process_select = mock.MagicMock()
+        self.process_insert_get_id = mock.MagicMock()
+
+        return self
+
+
+class MockManager(DatabaseManager):
+
+    def prepare_mock(self):
+        self._make_connection = mock.MagicMock(
+            side_effect=lambda name: MockConnection().prepare_mock()
+        )
+
+        return self
+
+
+class MockFactory(ConnectionFactory):
+
+    def prepare_mock(self):
+        self.make = mock.MagicMock(return_value=MockConnection().prepare_mock())
+
+        return self
+
+
+class MockQueryBuilder(QueryBuilder):
+
+    def prepare_mock(self):
+        self.from__ = 'foo_table'
+
+        return self
+
+
+class MockModel(Model):
+
+    def prepare_mock(self):
+        self.get_key_name = mock.MagicMock(return_value='foo')
+        self.get_table = mock.MagicMock(return_value='foo_table')
+        self.get_qualified_key_name = mock.MagicMock(return_value='foo_table.foo')
+
+        return self
