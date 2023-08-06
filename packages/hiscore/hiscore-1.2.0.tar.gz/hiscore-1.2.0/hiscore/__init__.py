854a9fbdbe451300c79b18d8830407ad67d92f4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:23 +0000

    Add package hiscore/1.2.0/hiscore-1.2.0.tar.gz

---
+++ packages/hiscore/hiscore-1.2.0.tar.gz/hiscore-1.2.0/hiscore/__init__.py
@@ -0,0 +1,9 @@
+__author__ = 'Abraham Othman'
+__copyright__ = 'Copyright 2015, Abraham Othman'
+
+__version__ = '1.2.0'
+__maintainer__ = 'Abraham Othman'
+__email__ = 'aothman@cs.cmu.edu'
+
+from .engine import create, HiScoreEngine, Point
+from .errors import MonotoneError, MonotoneBoundsError, ScoreCreationError
