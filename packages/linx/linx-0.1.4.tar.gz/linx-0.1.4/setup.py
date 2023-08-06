179af3f04f39563642a250cd4f75dd96cd9c0032
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:21 +0000

    Add package linx/0.1.4/linx-0.1.4.tar.gz

---
+++ packages/linx/linx-0.1.4.tar.gz/linx-0.1.4/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+
+setup(
+    name='linx',
+    version='0.1.4',
+    author='Andrei Marcu',
+    author_email='andrei@marcu.net',
+    url='https://github.com/andreimarcu/linx-cli',
+    license='LICENSE.txt',
+    description='Command line interface for Linx.li',
+    install_requires=[
+        "requests",
+        "argparse"
+    ],
+    scripts=['linx/linx']
+
+)
