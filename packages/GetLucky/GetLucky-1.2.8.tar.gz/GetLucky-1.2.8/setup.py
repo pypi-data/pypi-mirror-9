9d4b6733754e2e4d9f456115b7f7cacce5a60e6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:43 +0000

    Add package GetLucky/1.2.8/GetLucky-1.2.8.tar.gz

---
+++ packages/GetLucky/GetLucky-1.2.8.tar.gz/GetLucky-1.2.8/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name='GetLucky',
+    version='1.2.8',
+    author='Tanner Baldus',
+    author_email='tbaldu285@gmail.com',
+    packages=['getLucky'],
+    scripts=[],
+    url='http://pypi.python.org/pypi/getlucky/',
+    license='LICENSE.txt',
+    description='A CLI to quickly set the mood with Songza.',
+    long_description=open('README.txt').read(),
+    install_requires=[
+        "requests >= 2.2.1",
+        "beautifulsoup4 >= 4.3.2",
+        "docopt >= 0.6.1",
+    ],
+    entry_points={
+        'console_scripts': [
+         'gl = getLucky.getLucky:main',
+        ],
+        }
+)
