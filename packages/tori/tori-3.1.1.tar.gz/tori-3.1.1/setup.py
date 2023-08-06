99ad04fde16344f69cc79451ec0415aaafe9ff6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:10 +0000

    Add package tori/3.1.1/tori-3.1.1.tar.gz

---
+++ packages/tori/tori-3.1.1.tar.gz/tori-3.1.1/setup.py
@@ -0,0 +1,39 @@
+try:
+    from setuptools import setup
+except:
+    from distutils.core import setup
+
+setup(
+    name         = 'tori',
+    version      = '3.1.1',
+    description  = 'A collection of libraries and a micro web framework based on Tornado framework',
+    license      = 'MIT',
+    author       = 'Juti Noppornpitak',
+    author_email = 'juti_n@yahoo.co.jp',
+    url          = 'http://shiroyuki.com/projects/tori.html',
+    packages     = [
+        'tori',
+        'tori.cli',
+        'tori.data',
+        'tori.decorator',
+        'tori.session',
+        'tori.session.entity',
+        'tori.session.repository',
+        'tori.socket',
+        'tori.template'
+    ],
+    classifiers   = [
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries'
+    ],
+    scripts          = ['bin/nest'],
+    install_requires = ['passerine', 'imagination', 'kotoba', 'tornado', 'jinja2']
+)
