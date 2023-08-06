b466a4718c56dcbea680e92170c688d337e34674
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:29 +0000

    Add package htrc/0.0.6/htrc-0.0.6.tar.gz

---
+++ packages/htrc/htrc-0.0.6.tar.gz/htrc-0.0.6/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name = 'htrc',
+    packages = ['htrc'],
+    version = '0.0.6',
+    description = 'Set of Python Libraries for HTRC APIs.',
+    author='Milinda Pathirage',
+    author_email='milinda.pathirage@gmail.com',
+    url='https://github.com/milinda/htrc-sdk-for-python',
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Development Status :: 1 - Planning',
+        'Environment :: Console',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Information Analysis'
+    ]
+)
