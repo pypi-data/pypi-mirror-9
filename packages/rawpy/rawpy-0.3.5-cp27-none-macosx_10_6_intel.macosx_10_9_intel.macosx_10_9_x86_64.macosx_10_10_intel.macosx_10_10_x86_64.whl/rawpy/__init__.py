e052b1ac6ee7df99b0ff76d91ccc740ba6e6a534
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:23 +0000

    Add package rawpy/0.3.5/rawpy-0.3.5-cp27-none-macosx_10_6_intel.macosx_10_9_intel.macosx_10_9_x86_64.macosx_10_10_intel.macosx_10_10_x86_64.whl

---
+++ packages/rawpy/rawpy-0.3.5-cp27-none-macosx_10_6_intel.macosx_10_9_intel.macosx_10_9_x86_64.macosx_10_10_intel.macosx_10_10_x86_64.whl/rawpy/__init__.py
@@ -0,0 +1,19 @@
+from __future__ import absolute_import
+
+from ._version import __version__, __version_info__
+
+import rawpy._rawpy
+globals().update(rawpy._rawpy.__dict__)
+
+def imread(path):
+    """
+    Convenience function that creates a :class:`rawpy.RawPy` instance, opens the given file,
+    unpacks the RAW data, and returns the :class:`rawpy.RawPy` instance for further processing.
+    
+    :param str path: path to RAW image that will be opened
+    :rtype: :class:`rawpy.RawPy`
+    """
+    d = RawPy()
+    d.open_file(path)
+    d.unpack()
+    return d
