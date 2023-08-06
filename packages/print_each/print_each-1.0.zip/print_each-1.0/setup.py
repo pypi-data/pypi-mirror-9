789b7d622c3d2eca5e84766f60fa708fc18c0071
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:37 +0000

    Add package print_each/1.0/print_each-1.0.zip

---
+++ packages/print_each/print_each-1.0.zip/print_each-1.0/setup.py
@@ -0,0 +1,10 @@
+__author__ = 'oupeng'
+from distutils.core import setup
+setup(
+    name='print_each',
+    version='1.0',
+    py_modules=['headPython'],
+    author='test',
+    author_email='test@test.com',
+    url='www.test.com'
+)
