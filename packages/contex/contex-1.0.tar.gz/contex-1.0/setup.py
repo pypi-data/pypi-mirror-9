43899d88f90f8e3b6964b38ea54b0b050fc6cd48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:36 +0000

    Add package contex/1.0/contex-1.0.tar.gz

---
+++ packages/contex/contex-1.0.tar.gz/contex-1.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+setup(name='contex',
+      version='1.0',
+      description='Contextual string manipulation',
+      long_description=readme(),
+      url='https://notabug.org/Uglemat/Contex',
+      author='Mattias Ugelvik',
+      author_email='uglemat@gmail.com',
+      license='GPL3+',
+      packages=['contex'],
+      classifiers=[
+          "Topic :: Text Processing :: General",
+          "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+          'Programming Language :: Python :: 3'
+      ],
+      test_suite='nose.collector',
+      tests_require=['nose'],
+      package_data={
+          'readme': ['README.rst'],
+      },
+      include_package_data=True,
+      zip_safe=False)
