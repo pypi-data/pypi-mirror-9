6cdb223f1ca6db3e65a457fb8d8bb486bff57d2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:19 +0000

    Add package django-ma-settings/1.0.5/django-ma-settings-1.0.5.tar.gz

---
+++ packages/django-ma-settings/django-ma-settings-1.0.5.tar.gz/django-ma-settings-1.0.5/setup.py
@@ -0,0 +1,35 @@
+import os
+from setuptools import setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-ma-settings',
+    version='1.0.5',
+    packages=['ma_settings'],
+    include_package_data=True,
+    license='BSD License',  # example license
+    description='A simple Django app to have simple typed settings.',
+    long_description=README,
+    url='https://github.com/MasterAlish/django-ma-settings',
+    author='Alisher Alikulov',
+    author_email='masteraalish@gmail.com',
+    keywords='django-settings',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License', # example license
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        # Replace these appropriately if you are stuck on Python 2.
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
