08c8c622ee4c4c9803cc3351f1c1a72cfbe908e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:55 +0000

    Add package hwmasfoy/1.0.0/hwmasfoy-1.0.0.zip

---
+++ packages/hwmasfoy/hwmasfoy-1.0.0.zip/hwmasfoy-1.0.0/hwmasfoy.py
@@ -0,0 +1,17 @@
+
+""" this is helloworld project from Mahfuz"""
+
+def callme(par):
+	x = 22+3+par 
+	list1 = {1,2,3,4} 
+	
+	for i in list1:
+		print i;	
+		
+	print x	
+	
+	return x
+	
+
+
+    
---
+++ packages/hwmasfoy/hwmasfoy-1.0.0.zip/hwmasfoy-1.0.0/setup.py
@@ -0,0 +1,14 @@
+
+
+from distutils.core import setup
+
+setup(
+        name        = 'hwmasfoy',
+        version     = '1.0.0',
+        py_modules  = ['hwmasfoy'],
+        author      = 'masfoy',
+        author_email = 'masfoy@gmail.com',
+        url          = 'http://www.headhirstlabs.com',
+        description  = 'a simple hello world is going to be distributed',
+
+    )
