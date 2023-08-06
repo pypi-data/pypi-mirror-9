1e2caa5dd1155a28903b1671a67b1492d10247c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:45 +0000

    Add package wpedit/0.4.5/wpedit-0.4.5.tar.gz

---
+++ packages/wpedit/wpedit-0.4.5.tar.gz/wpedit-0.4.5/setup.py
@@ -0,0 +1,17 @@
+__author__ = 'GHajba'
+from distutils.core import setup
+
+setup(
+    name='wpedit',
+    packages=['wpedit'],
+    version='0.4.5',
+    description='Sends (offline) articles written in MarkDown to WordPress via the XML-RPC API, and loads the draft articles from WordPress to the local machine. Works with proxies too.',
+    author='Gabor Laszlo Hajba',
+    author_email='gabor.hajba@gmail.com',
+    url='https://github.com/ghajba/wp-editor',
+    download_url='https://github.com/ghajba/wp-editor/tarball/0.4.5',
+    keywords=['wordpress', 'offline', 'edit', 'markdown', 'xml', 'editor'],
+    classifiers=['Programming Language :: Python',
+                 'License :: OSI Approved :: MIT License', ],
+    requires=[('Markdown'), ("python.wordpress.xmlrpc")],
+)
