0274f0e30a96cef106134063842e66dd45a32f56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:07:54 +0000

    Add package django-rest-hooks-delivery/0.2.1/django-rest-hooks-delivery-0.2.1.tar.gz

---
+++ packages/django-rest-hooks-delivery/django-rest-hooks-delivery-0.2.1.tar.gz/django-rest-hooks-delivery-0.2.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+version = __import__('rest_hooks_delivery').VERSION
+
+setup(
+    name='django-rest-hooks-delivery',
+    description=('Various webhook deliverers for django-rest-hooks and '
+                 'django-rest-hooks-ng.'),
+    version=version,
+    author='PressLabs',
+    author_email='ping@presslabs.com',
+    url='http://github.com/PressLabs/django-rest-hooks-delivery',
+    install_requires=['Django>=1.7', 'requests', 'django-jsonfield'],
+    packages=['rest_hooks_delivery'],
+    include_package_data=True,
+    classifiers = ['Development Status :: 4 - Beta',
+                   'Environment :: Web Environment',
+                   'Framework :: Django',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved :: MIT License',
+                   'Operating System :: OS Independent',
+                   'Programming Language :: Python',
+                   'Topic :: Utilities'],
+)
