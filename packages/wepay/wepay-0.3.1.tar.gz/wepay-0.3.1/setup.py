f77b8f5fbada13637c05b90f6a4ad5aaefcde4ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:51 +0000

    Add package wepay/0.3.1/wepay-0.3.1.tar.gz

---
+++ packages/wepay/wepay-0.3.1.tar.gz/wepay-0.3.1/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+import wepay
+import os.path
+
+long_description = open('README.rst').read() if os.path.isfile('README.rst') else 'A Python SDK for our WePay API'
+
+version_str = '%s.%s.%s' % (
+    wepay.VERSION[0],
+    wepay.VERSION[1],
+    wepay.VERSION[2]
+)
+
+setup(
+    name='wepay',
+    version=version_str,
+    packages=['wepay'],
+    description='A Python SDK for our WePay API.',
+    long_description=long_description,
+    author='WePay API Team',
+    author_email='api@wepay.com',
+    license='MIT License',
+    url='https://github.com/wepay/Python-SDK',
+    platforms=["any"],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
+    install_requires=['requests==2.2.1']
+)
