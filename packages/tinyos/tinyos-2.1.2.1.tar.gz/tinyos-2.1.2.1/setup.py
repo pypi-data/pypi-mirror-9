9bb8aa11f2e32197bd90ad4b7a97c3bcdfe28e3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:34 +0000

    Add package tinyos/2.1.2.1/tinyos-2.1.2.1.tar.gz

---
+++ packages/tinyos/tinyos-2.1.2.1.tar.gz/tinyos-2.1.2.1/setup.py
@@ -0,0 +1,24 @@
+"""setup instructions for setuptools."""
+
+import os
+from setuptools import setup, find_packages
+
+def read(*paths):
+    """Build a file path from *paths* and return the contents."""
+    with open(os.path.join(*paths), "r") as fptr:
+        return fptr.read()
+
+setup(
+    name="tinyos",
+    version="2.1.2.1",
+    description="Highly experimental TinyOS python tools (Unofficial Packaging)",
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 2.7",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    packages=find_packages(exclude="tests"),
+    install_requires=["pyserial"],
+    test_suite="nose.collector",
+    tests_require=["nose"],
+)
