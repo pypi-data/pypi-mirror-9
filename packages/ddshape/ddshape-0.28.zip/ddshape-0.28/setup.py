5534cac0a7e65bf66aa6a3e0b671620445ac8cd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:12 +0000

    Add package ddshape/0.28/ddshape-0.28.zip

---
+++ packages/ddshape/ddshape-0.28.zip/ddshape-0.28/setup.py
@@ -0,0 +1,21 @@
+# -*- encoding:utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    name='ddshape',
+    version='0.28',
+    packages=find_packages(),
+
+    package_data={
+        'ddshape': ['Template/*.tpl']
+    },
+
+    author='Xing',
+    author_email='1281961491@qq.com',
+    url='https://github.com/xingtingyang/ddshape',
+    license='BSD style',
+    description='this is a package which include some modules that you can use them to create shape.dat used by ddscat',
+    long_description=open('README.rst').read()
+
+)
