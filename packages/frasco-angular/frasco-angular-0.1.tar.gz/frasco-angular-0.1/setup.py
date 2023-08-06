59e5acb9203fae87d22cf29402ad92e57029d4a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:36:11 +0000

    Add package frasco-angular/0.1/frasco-angular-0.1.tar.gz

---
+++ packages/frasco-angular/frasco-angular-0.1.tar.gz/frasco-angular-0.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='frasco-angular',
+    version='0.1',
+    url='http://github.com/frascoweb/frasco-angular',
+    license='MIT',
+    author='Maxime Bouroumeau-Fuseau',
+    author_email='maxime.bouroumeau@gmail.com',
+    description="Angular integration for Frasco",
+    packages=find_packages(),
+    package_data={
+        'frasco_angular': ['templates/*.html', 'static/*.js']
+    },
+    zip_safe=False,
+    platforms='any',
+    install_requires=[
+        'frasco',
+        'frasco-assets'
+    ]
+)
