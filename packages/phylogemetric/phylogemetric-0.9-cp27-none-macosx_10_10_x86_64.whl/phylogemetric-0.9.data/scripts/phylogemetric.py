eee3f1ba299b8dac6adf932149fa86972f906b29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:41 +0000

    Add package phylogemetric/0.9/phylogemetric-0.9-cp27-none-macosx_10_10_x86_64.whl

---
+++ packages/phylogemetric/phylogemetric-0.9-cp27-none-macosx_10_10_x86_64.whl/phylogemetric-0.9.data/scripts/phylogemetric.py
@@ -0,0 +1,43 @@
+#!python
+#coding=utf-8
+"""Calculates a network metric"""
+__author__ = 'Simon J. Greenhill <simon@simon.net.nz>'
+__copyright__ = 'Copyright (c) 2015 Simon J. Greenhill'
+__license__ = 'New-style BSD'
+
+import os
+
+try:
+    from nexus import NexusReader
+except ImportError:
+    raise ImportError("Please install python-nexus")
+
+from phylogemetric import DeltaScoreMetric
+from phylogemetric import QResidualMetric
+
+
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser(
+        description='Calculates a phylogenetic network metric from a nexus file'
+    )
+    parser.add_argument("method", help="Method [delta/qresidual]")
+    parser.add_argument("filename", help="nexusfile")
+    args = parser.parse_args()
+    
+    if not os.path.isfile(args.filename):
+        raise IOError("File %s does not exist" % args.filename)
+    nex = NexusReader(args.filename)
+    
+    if args.method in ('q', 'qresidual'):
+        metric = QResidualMetric(nex.data.matrix)
+    elif args.method in ('d', 'delta'):
+        metric = DeltaScoreMetric(nex.data.matrix)
+    else:
+        raise SystemExit(
+            "Unknown method %s. Please choose either 'delta' or 'q'"
+            % args.method
+        )
+    
+    metric.score()
+    metric.pprint()
