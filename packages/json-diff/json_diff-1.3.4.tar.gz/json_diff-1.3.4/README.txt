8f968ae337a80e3dffb596dea28ccf98180cbc62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:03 +0000

    Add package json-diff/1.3.4/json_diff-1.3.4.tar.gz

---
+++ packages/json-diff/json_diff-1.3.4.tar.gz/json_diff-1.3.4/README.txt
@@ -0,0 +1,9 @@
+Compares two JSON files (http://json.org) and generates a new JSON file
+with the result. Allows exclusion of some keys from the comparison, or
+in other way to include only some keys.
+
+The project’s website is at https://fedorahosted.org/json_diff/
+Patches and pull requests are welcome, but please keep the script compatible
+with python 2.4.
+
+Released under MIT/X11 license.
---
+++ packages/json-diff/json_diff-1.3.4.tar.gz/json_diff-1.3.4/setup.py
@@ -0,0 +1,40 @@
+# coding: utf-8
+from setuptools import setup
+import os.path
+import json_diff
+
+
+def read(fname):
+    f = open(os.path.join(os.path.dirname(__file__), fname))
+    out = "\n" + f.read().replace("\r\n", "\n")
+    f.close()
+    return out
+
+
+def get_long_description():
+    return read("README.txt") \
+        + "\nChangelog:\n" + "=" * 10 + "\n" \
+        + read("NEWS.txt")
+
+setup(
+    name='json_diff',
+    version='%s' % json_diff.__version__,
+    description='Generates diff between two JSON files',
+    author='Matěj Cepl',
+    author_email='mcepl@redhat.com',
+    url='https://fedorahosted.org/json_diff/',
+    py_modules=['json_diff'],
+    long_description=get_long_description(),
+    keywords=['json', 'diff'],
+    test_suite='test.test_json_diff',
+    classifiers=[
+        "Programming Language :: Python",
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Information Technology",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Text Processing :: General",
+    ]
+)
