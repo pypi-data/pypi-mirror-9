9cc7bf5d209ca3dad0112bfa1eac04e7a6c1d26c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:38 +0000

    Add package flopy/3.1.0/flopy-3.1.0.zip

---
+++ packages/flopy/flopy-3.1.0.zip/flopy-3.1.0/flopy/version.py
@@ -0,0 +1,4 @@
+#flopy version file automatically created using...updateversion.py
+#            created on......March 17, 2015 12:24:50
+__version__='3.1.0'
+__build__='3.1.0.501'
---
+++ packages/flopy/flopy-3.1.0.zip/flopy-3.1.0/setup.py
@@ -0,0 +1,35 @@
+import os
+import sys
+from distutils.core import setup
+
+# To use:
+#	   python setup.py bdist --format=wininst
+
+from flopy import __version__, __name__, __author__
+
+#--trap someone trying to install flopy with python 3
+if not sys.version_info[0] == 2:
+    print "Sorry, Python 3 is not supported (yet)"
+    sys.exit(1) # return non-zero value for failure
+
+long_description = ''
+ 
+try:
+   import pypandoc
+   long_description = pypandoc.convert('README.md', 'rst')
+except:
+   pass  
+       
+setup(name=__name__,
+      description='FloPy is a Python package to create, run, and post-process MODFLOW-based models.',
+      long_description=long_description,  
+      author=__author__,
+      author_email='mark.bakker@tudelft.nl, vincent.post@flinders.edu.au, langevin@usgs.gov, jdhughes@usgs.gov, jwhite@usgs.gov, jjstarn@usgs.gov, mnfienen@usgs.gov, frances.alain@gmail.com',
+      url='https://github.com/modflowpy/flopy/',
+      license='New BSD',
+      platforms='Windows, Mac OS-X',
+      #setup_requires=['numpy>=1.9','matplotlib>=1.4'],
+      install_requires=['numpy>=1.9', 'matplotlib>=1.4'],
+      packages=['flopy', 'flopy.modflow', 'flopy.modpath', 'flopy.mt3d', 'flopy.seawat', 'flopy.utils', 'flopy.plot'],
+      # use this version ID if .svn data cannot be found
+      version=__version__ )
