f02755d2032db75adb6899f7c8cabc844f3700fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:13 +0000

    Add package pyodesiutils/0.0.4/pyodesiutils-0.0.4.tar.gz

---
+++ packages/pyodesiutils/pyodesiutils-0.0.4.tar.gz/pyodesiutils-0.0.4/setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+
+"""
+pyodesiutils
+-------------
+Utils library to work with DDI xml file of ODESI
+"""
+
+from os import path
+from setuptools import setup
+
+long_description = open(path.join(path.dirname(__file__), 'README.rst')).read()
+
+setup(
+    name='pyodesiutils',
+    version='0.0.4',
+    url='https://github.com/guinslym/pyodesi_utils',
+    license='MIT',
+    author='Guinsly Mondesir',
+    install_requires=['beautifulsoup4'],
+    author_email='guinslym@gmail.com',
+    description='Helper for ODESI xml DDI documentation file',
+    long_description=long_description,
+    keywords = ['ddi', 'idd', 'statcan', 'survey', 'xml'],
+    packages=['pyodesiutils'],
+    classifiers=[
+    'Programming Language :: Python :: 2.7',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python'
+    ],
+        )
