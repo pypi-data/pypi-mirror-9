00377a4e42a1ff69d47ee698cf9cd3b31c06fe80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:23 +0000

    Add package taskwarrior-capsules/0.2.2/taskwarrior-capsules-0.2.2.tar.gz

---
+++ packages/taskwarrior-capsules/taskwarrior-capsules-0.2.2.tar.gz/taskwarrior-capsules-0.2.2/taskwarrior_capsules/capsule.py
@@ -0,0 +1,172 @@
+import subprocess
+import warnings
+
+from configobj import ConfigObj
+from taskw.warrior import TaskWarriorShellout
+from verlib import NormalizedVersion
+
+from . import __version__
+from .exceptions import CapsuleProgrammingError
+
+
+class PluginError(Exception):
+    pass
+
+
+class PluginValidationError(PluginError):
+    pass
+
+
+class PluginOperationError(PluginError):
+    pass
+
+
+class TaskwarriorCapsuleBase(object):
+    def get_taskwarrior_version(self):
+        taskwarrior_version = subprocess.Popen(
+            ['task', '--version'],
+            stdout=subprocess.PIPE
+        ).communicate()[0]
+        return NormalizedVersion(taskwarrior_version)
+
+    def validate(self, **kwargs):
+        if not (self.MIN_VERSION and self.MAX_VERSION):
+            warnings.warn(
+                "%s does not specify compatible which taskwarrior-capsule "
+                "versions it is compatible with; you may encounter "
+                "compatibility problems. " % (
+                    self.plugin_name
+                )
+            )
+        else:
+            curr_version = NormalizedVersion(__version__)
+            min_version = NormalizedVersion(self.MIN_VERSION)
+            max_version = NormalizedVersion(self.MAX_VERSION)
+
+            if not min_version <= curr_version <= max_version:
+                warnings.warn(
+                    "Plugin '%s' is not compatible with version %s of "
+                    "taskwarrior-capsules; "
+                    "minimum version: %s; "
+                    "maximum version %s." % (
+                        self.plugin_name,
+                        __version__,
+                        min_version,
+                        max_version,
+                    ),
+                )
+
+        if not (self.MAX_TASKWARRIOR_VERSION and self.MIN_TASKWARRIOR_VERSION):
+            warnings.warn(
+                "%s does not specify which taskwarrior versions it is "
+                "compatible with; you may encounter compatibility "
+                "problems. " % (
+                    self.plugin_name
+                )
+            )
+        else:
+            tw_version = self.get_taskwarrior_version()
+            min_tw_version = NormalizedVersion(self.MIN_TASKWARRIOR_VERSION)
+            max_tw_version = NormalizedVersion(self.MAX_TASKWARRIOR_VERSION)
+
+            if not min_tw_version <= tw_version <= max_tw_version:
+                warnings.warn(
+                    "Plugin '%s' is not compatible with version %s of "
+                    "taskwarrior; "
+                    "minimum version: %s; "
+                    "maximum version %s." % (
+                        self.plugin_name,
+                        tw_version,
+                        min_tw_version,
+                        max_tw_version,
+                    ),
+                )
+
+        return True
+
+
+class CommandCapsule(TaskwarriorCapsuleBase):
+    MIN_VERSION = None
+    MAX_VERSION = None
+    MIN_TASKWARRIOR_VERSION = None
+    MAX_TASKWARRIOR_VERSION = None
+
+    def __init__(self, meta, plugin_name, **kwargs):
+        self.meta = meta
+        self.plugin_name = plugin_name
+        self.client = TaskWarriorShellout(marshal=True)
+        for k, v in kwargs.items():
+            setattr(self, k, v)
+        super(CommandCapsule, self).__init__()
+
+    @property
+    def configuration_filename(self):
+        return self.meta.get_metadata_path(
+            '%s.json' % self.plugin_name,
+        )
+
+    @property
+    def global_configuration(self):
+        return self.meta.configuration
+
+    @property
+    def configuration(self):
+        if not hasattr(self, '_config'):
+            self._config = ConfigObj(self.configuration_filename)
+        return self._config
+
+    def get_description(self):
+        try:
+            return self.__doc__.strip()
+        except AttributeError:
+            raise None
+
+    def get_matching_tasks(self, filter_args):
+        filter_command = filter_args + ['status:pending', 'export']
+        results = self.client._get_json(*filter_command)
+
+        tasks = []
+        for result in results:
+            _, task = self.client.get_task(
+                uuid=result['uuid']
+            )
+            tasks.append(task)
+
+        return tasks
+
+    @classmethod
+    def execute(
+        cls, variant, meta, command_name, filter_args, extra_args, **kwargs
+    ):
+        cmd = cls(
+            meta=meta,
+            plugin_name=command_name
+        )
+        cmd.validate(**kwargs)
+
+        command_name_map = {
+            'preprocessor': 'preprocess',
+            'command': 'handle',
+            'postprocessor': 'postprocess',
+        }
+
+        if hasattr(cls, command_name_map.get(variant)):
+            return getattr(
+                cmd,
+                command_name_map[variant]
+            )(
+                filter_args,
+                extra_args,
+                **kwargs
+            )
+
+        raise CapsuleProgrammingError(
+            "%s was called as a %s but the %s method is not implemented!" % (
+                cls.__name__,
+                variant,
+                command_name_map[variant],
+            )
+        )
+
+    def handle(self, filter_args, extra_args, **kwargs):
+        raise NotImplementedError()
---
+++ packages/taskwarrior-capsules/taskwarrior-capsules-0.2.2.tar.gz/taskwarrior-capsules-0.2.2/taskwarrior_capsules/cmdline.py
@@ -0,0 +1,91 @@
+import pkg_resources
+import subprocess
+import sys
+
+from blessings import Terminal
+
+from .exceptions import CapsuleError
+from .capsule import CommandCapsule
+from .capsule_meta import CapsuleMeta
+from .data import BUILT_IN_COMMANDS
+
+
+def get_installed_capsules(variant='command'):
+    groupmap = {
+        'command': 'taskwarrior_capsules',
+        'preprocessor': 'taskwarrior_preprocessor_capsules',
+        'postprocessor': 'taskwarrior_postprocessor_capsules',
+    }
+
+    possible_commands = {}
+    for entry_point in (
+        pkg_resources.iter_entry_points(group=groupmap[variant])
+    ):
+        try:
+            loaded_class = entry_point.load()
+        except ImportError:
+            continue
+        if not issubclass(loaded_class, CommandCapsule):
+            continue
+        possible_commands[entry_point.name] = loaded_class
+
+    return possible_commands
+
+
+def main(args=None):
+    if args is None:
+        args = sys.argv[1:]
+
+    term = Terminal()
+    commands = get_installed_capsules('command')
+    preprocessors = get_installed_capsules('preprocessor')
+    postprocessors = get_installed_capsules('postprocessor')
+    meta = CapsuleMeta()
+
+    command = None
+    filter_args = None
+    extra_args = None
+    for idx, arg in enumerate(args):
+        if arg in commands or arg in BUILT_IN_COMMANDS:
+            command_name = arg
+            if arg in commands:
+                command = commands[arg]
+            filter_args = args[0:idx]
+            extra_args = args[idx+1:]
+
+    for processor_name, processor in preprocessors.items():
+        filter_args, extra_args, command = processor.execute(
+            'preprocessor', meta, command_name, filter_args, extra_args,
+            terminal=term,
+        )
+
+    if command:
+        try:
+            result = command.execute(
+                'command', meta, command_name, filter_args, extra_args,
+                terminal=term,
+            )
+        except CapsuleError as e:
+            print(
+                "{t.red}The {capsule_name} taskwarrior capsule "
+                "encountered an error processing your request: "
+                "{t.normal}{t.red}{t.bold}{error}{t.normal}".format(
+                    capsule_name=command_name,
+                    t=term,
+                    error=str(e)
+                )
+            )
+            sys.exit(90)
+    else:
+        # Run this as a normal command
+        task_args = ['task'] + args
+        result = subprocess.call(task_args)
+
+    for processor_name, processor in postprocessors.items():
+        filter_args, extra_args, command = processor.execute(
+            'postprocessor', meta, command_name, filter_args, extra_args,
+            terminal=term,
+            result=result
+        )
+
+    sys.exit(result)
