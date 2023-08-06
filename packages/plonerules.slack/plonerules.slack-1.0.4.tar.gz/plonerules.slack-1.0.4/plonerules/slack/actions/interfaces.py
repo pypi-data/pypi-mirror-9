9b01eaaa724ff56ed8b9b0d51693ee6226ad55a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:27 +0000

    Add package plonerules.slack/1.0.4/plonerules.slack-1.0.4.tar.gz

---
+++ packages/plonerules.slack/plonerules.slack-1.0.4.tar.gz/plonerules.slack-1.0.4/plonerules/slack/actions/interfaces.py
@@ -0,0 +1,35 @@
+from zope.interface import Interface
+from zope import schema
+
+
+class ISlackAction(Interface):
+    """Definition of the configuration available for a Slack action"""
+    
+    chanel = schema.TextLine(
+        title=u"Channel",
+        description=u"The channel to post (ex: #general)",
+        required=True
+    )
+    username = schema.TextLine(
+        title=u"Username",
+        description=u"The username to use for post",
+        required=True
+    )
+    token = schema.TextLine(
+        title=u"Webhook Token",
+        description=u"The token (available in API settings on slack)",
+        required=True
+    )
+    emoji = schema.TextLine(
+        title=u"emoji to use",
+        description=u"An emoji picked in http://www.emoji-cheat-sheet.com/ (ex: ':dash:')",
+        required=True
+    )
+     
+    message = schema.Text(
+        title=u"Message",
+        description=u"Type in here the message that you \
+want to post. Some defined content can be replaced: ${title} will be replaced \
+by the title of the newly created item.  ${url} will be replaced by the \
+URL of the newly created item, ${username} by the who made the action and ${description} by the description",
+        required=True)
