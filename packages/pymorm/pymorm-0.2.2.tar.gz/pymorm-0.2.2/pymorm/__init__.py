d17974ebd8d642980a103f65a056f5ade786ac87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:15 +0000

    Add package pymorm/0.2.2/pymorm-0.2.2.tar.gz

---
+++ packages/pymorm/pymorm-0.2.2.tar.gz/pymorm-0.2.2/pymorm/__init__.py
@@ -0,0 +1,75 @@
+__author__ = 'walter'
+from bunch import *
+from pymongo.cursor import Cursor
+from pymongo.errors import OperationFailure
+from bson import ObjectId
+
+__all__ = ["MongoObject", "MongoObjectMeta"]
+
+
+class CollectionMethod(object):
+    def __init__(self, mappedclass, method):
+        self.mappedclass = mappedclass
+        self.method = method
+
+    def __call__(self, *args, **kwargs):
+        result = self.method(*args, **kwargs)
+        if isinstance(result, dict):
+            return self.mappedclass(result)
+        elif isinstance(result, Cursor):
+            # trick to map Cursor.next() on the appropriate mapped class.
+            result.next = self.map_cursor(result.next)
+        return result
+
+    def map_cursor(self, next):
+        def _do_map_cursor():
+            return self.mappedclass(next())
+        return _do_map_cursor
+
+
+class Query(object):
+    def __init__(self, mappedclass, collection):
+        self.mappedclass = mappedclass
+        self.collection = collection
+
+    def __getattr__(self, item):
+        return CollectionMethod(self.mappedclass, getattr(self.collection, item))
+
+
+class MongoObjectMeta(type):
+    def __new__(mcs, *args, **kwargs):
+        result = type(*args)
+        result.query = Query(result, result.__collection__)
+        return result
+
+
+class MongoObject(Bunch):
+    __defaults__ = {}
+
+    def __init__(self, *args, **kw):
+        super(MongoObject, self).__init__(*args, **kw)
+        for key, value in self.__defaults__.items():
+            if not self.get(key):
+                self[key] = hasattr(value, '__call__') and value() or value
+
+    @classmethod
+    def add(cls, document):
+        new_id = ObjectId(cls.query.insert(cls(document)))
+        return cls.query.find_one({"_id": new_id})
+
+    @classmethod
+    def get_all(cls, *args, **kw):
+        return [d for d in cls.query.find(*args, **kw)]
+
+    def remove(self):
+        result = self.__class__.query.remove({"_id": self._id})
+        if not result.ok:
+            raise OperationFailure("Can not delete the document")
+        return result.n
+
+    def commit(self, silent_fail=True):
+        result = self.__class__.query.update({"_id": self._id}, self)
+        if not silent_fail and result.updatedExisting is not True:
+            raise OperationFailure("The document to update doesn't exist anymore. "
+                                   "Updates to the `_id` field are not allowed through `commit`.")
+        return result
---
+++ packages/pymorm/pymorm-0.2.2.tar.gz/pymorm-0.2.2/test/test.py
@@ -0,0 +1,36 @@
+__author__ = 'walter'
+
+from pymorm import MongoObject, MongoObjectMeta
+from pymongo import MongoClient
+
+db = MongoClient("mongodb://localhost:27017/pymorm").get_default_database()
+
+
+class Test(MongoObject):
+    __metaclass__ = MongoObjectMeta
+    __collection__ = db.tests
+
+    __defaults__ = {"username": "Test",
+                    "happiness": lambda: "poor"}
+
+    def test_method(self, test):
+        return test
+
+    @property
+    def test_property(self):
+        return "test"
+
+    @classmethod
+    def test_classmethod(cls):
+        return cls.__name__
+
+
+user = Test.add({})
+user2 = Test.add({"username": "Walter"})
+user.happiness = "a lot!"
+print user
+print user2
+user.commit()
+assert user.remove() == 1, "Problem with delete"
+assert len(Test.get_all({})) == 1, "Something went wrong, the number of documents is %s" % len(Test.get_all({}))
+Test.query.remove()
