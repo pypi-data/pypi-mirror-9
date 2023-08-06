86d8ac890d626f2afa47d89eea8432a268d5713a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:23 +0000

    Add package falcon-wing/0.2.1/falcon-wing-0.2.1.tar.gz

---
+++ packages/falcon-wing/falcon-wing-0.2.1.tar.gz/falcon-wing-0.2.1/wing/serialization/__init__.py
@@ -0,0 +1,22 @@
+from .. import settings
+
+
+def get_serializer(req):
+    type = settings.SERIALIZER_DEFAULT
+
+    if settings.SERIALIZER_REQUEST_PARAM:
+        type = req.get_param(settings.SERIALIZER_REQUEST_PARAM) or type
+
+    return _get_serializer_by_type(type)
+
+def _get_serializer_by_type(type):
+    if type == 'json':
+        from . import json
+
+        return json
+    elif type == 'yaml':
+        from . import yaml
+
+        return yaml
+
+    raise NotImplemented
