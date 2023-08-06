bd0d06c2ff0a266a47881f312a26bb261949a0dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:07:43 +0000

    Add package drf-timestamp-pagination/0.0.3/drf-timestamp-pagination-0.0.3.tar.gz

---
+++ packages/drf-timestamp-pagination/drf-timestamp-pagination-0.0.3.tar.gz/drf-timestamp-pagination-0.0.3/drf_timestamp_pagination/serializers.py
@@ -0,0 +1,23 @@
+from rest_framework.pagination import BasePaginationSerializer, NextPageField
+from rest_framework.templatetags.rest_framework import replace_query_param
+
+
+class NextTimestampField(NextPageField):
+    page_field = 'next'
+
+    def to_representation(self, value):
+        if not value.has_next():
+            return None
+
+        page = value.next_page_timestamp()
+        request = self.context.get('request')
+        url = request and request.build_absolute_uri() or ''
+
+        return replace_query_param(url, self.page_field, page)
+
+
+class TimestampPaginationSerializer(BasePaginationSerializer):
+    """
+    Pagination serializer to work with TimestampPaginator
+    """
+    next = NextTimestampField(source='*')
