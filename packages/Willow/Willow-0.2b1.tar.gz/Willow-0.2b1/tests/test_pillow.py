1d8c76380b023b8886e449ffea987c3565363c4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:37 +0000

    Add package Willow/0.2b1/Willow-0.2b1.tar.gz

---
+++ packages/Willow/Willow-0.2b1.tar.gz/Willow-0.2b1/tests/test_pillow.py
@@ -0,0 +1,53 @@
+import unittest
+import io
+import imghdr
+
+from willow.backends import pillow as pillow_backend
+
+
+class TestPillowOperations(unittest.TestCase):
+    def setUp(self):
+        with open('tests/images/transparent.png', 'rb') as f:
+            self.backend = pillow_backend.PillowBackend.from_file(f)
+
+    def test_get_size(self):
+        width, height = pillow_backend.get_size(self.backend)
+        self.assertEqual(width, 200)
+        self.assertEqual(height, 150)
+
+    def test_resize(self):
+        pillow_backend.resize(self.backend, 100, 75)
+        self.assertEqual(self.backend.image.size, (100, 75))
+
+    def test_crop(self):
+        pillow_backend.crop(self.backend, 10, 10, 100, 100)
+        self.assertEqual(self.backend.image.size, (90, 90))
+
+    def test_save_as_jpeg(self):
+        output = io.BytesIO()
+        pillow_backend.save_as_jpeg(self.backend, output)
+        output.seek(0)
+
+        self.assertEqual(imghdr.what(output), 'jpeg')
+
+    def test_save_as_png(self):
+        output = io.BytesIO()
+        pillow_backend.save_as_png(self.backend, output)
+        output.seek(0)
+
+        self.assertEqual(imghdr.what(output), 'png')
+
+    def test_save_as_gif(self):
+        output = io.BytesIO()
+        pillow_backend.save_as_gif(self.backend, output)
+        output.seek(0)
+
+        self.assertEqual(imghdr.what(output), 'gif')
+
+    def test_has_alpha(self):
+        has_alpha = pillow_backend.has_alpha(self.backend)
+        self.assertTrue(has_alpha)
+
+    def test_has_animation(self):
+        has_animation = pillow_backend.has_animation(self.backend)
+        self.assertFalse(has_animation)
