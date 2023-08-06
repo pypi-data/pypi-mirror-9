dcc0d51bcad8fec96bdaec3b43c2217c09626d56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:35 +0000

    Add package cobertura-clover-transform/1.1.2/cobertura-clover-transform-1.1.2.tar.gz

---
+++ packages/cobertura-clover-transform/cobertura-clover-transform-1.1.2.tar.gz/cobertura-clover-transform-1.1.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="cobertura-clover-transform",
+    version='1.1.2',
+    packages=find_packages(),
+    include_package_data=True,
+    description="Tools for transforming Cobertura test "
+                "coverage XML into Clover-style XML",
+    install_requires=['lxml'],
+    author='Chris Wacek',
+    author_email='cwacek@gmail.com',
+    url='http://github.com/cwacek/cobertura-clover-transform',
+    license='MIT',
+    keywords='cobertura coverage test clover xml'
+)
