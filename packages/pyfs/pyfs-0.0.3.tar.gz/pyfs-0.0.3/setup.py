2aa32688dbeb73a9c08dd0ae7fbc2f0e1371e0d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:22 +0000

    Add package pyfs/0.0.3/pyfs-0.0.3.tar.gz

---
+++ packages/pyfs/pyfs-0.0.3.tar.gz/pyfs-0.0.3/setup.py
@@ -0,0 +1,43 @@
+from distutils.core import setup
+import fs
+
+try:
+    long_description = fs.read('README.txt')
+except IOError:
+    long_description = fs.read('README.md')
+
+setup(
+    name='pyfs',
+    packages=['fs'],
+    version='0.0.3',
+    description='a pythonic file system wrapper for humans',
+    long_description=long_description,
+    author='Christoph Koerner',
+    author_email='office@chaosmail.at',
+    url='https://github.com/chaosmail/python-fs',
+    download_url='https://github.com/chaosmail/python-fs/releases',
+    license='MIT',
+    keywords= ['fs', 'file system', 'filesystem', 'wrapper'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: System :: Filesystems',
+        'Topic :: System :: Operating System',
+        'Topic :: Utilities',
+    ],
+)
