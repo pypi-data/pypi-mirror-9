528bee10ee39b8eebcec30e987ff2e9966636597
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:30 +0000

    Add package django-imger/0.2/django-imger-0.2.2.tar.gz

---
+++ packages/django-imger/django-imger-0.2.2.tar.gz/django-imger-0.2.2/README.rst
@@ -0,0 +1,48 @@
+=====
+Imger
+=====
+
+Imger is a client side resizing and placement tool for images.
+Images are stored in the settings.MEDIA_ROOT so settings.py needs to include:
+MEDIA_ROOT and MEDIA_URL
+
+https://github.com/4shaw/django-imger
+
+Quick start
+-----------
+
+1. Add MEDIA_ROOT and MEDIA_URL to settings.py. something like::
+
+	MEDIA_URL = '/media/'
+	MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+2. Make sure imger static files are in your static directory::
+
+	/static/imger
+
+3. Add "imger" to your INSTALLED_APPS setting like this::
+
+    INSTALLED_APPS = (
+        ...
+        'imger',
+    )
+
+4. Import ImgerField into your models.yp and create an ImgerField on a model::
+
+    from imger.fields import ImgerField
+
+	...
+	image_path = ImgerField(upload_to='testing', imger_settings={'width'=400, 'height'=300, 'quality'=80})
+
+	No documentation yet but for a quick reference imger_settings can include:
+
+	width :: default 300
+	height :: default 200
+	quality :: default 100
+	mime :: default 'image/jpeg'
+	autobrowse :: default true
+	note :: default ''
+
+5. Start the development server and visit http://127.0.0.1:8000/admin/
+   Navigate to your model with the imger field and upolad an image
+   (you'll need the Admin app enabled).
---
+++ packages/django-imger/django-imger-0.2.2.tar.gz/django-imger-0.2.2/imger/widgets.py
@@ -0,0 +1,46 @@
+from django import forms
+from django.utils.safestring import mark_safe
+from django.conf import settings
+import json
+
+
+class ImgerWidget(forms.Widget):
+
+    def __init__(self, attrs=None, **kwargs):
+        self.imger_settings = attrs['imger_settings']
+        super(ImgerWidget, self).__init__(**kwargs)
+
+    class Media:
+        js = (
+            'imger/js/jquery-1.11.1.min.js',
+            'imger/js/jquery.nouislider.js',
+            'imger/js/form2js.js',
+            'imger/js/canvas-to-blob.min.js',
+            'imger/js/imger-compress.js',
+            'imger/js/imger-ui.js',
+            'imger/js/imger-init.js'
+        )
+        css = {
+            'all': (
+                'imger/css/bootstrap.css',
+                'imger/css/bootstrap-theme.css',
+                'imger/css/imger.css',
+                'imger/css/jquery.nouislider.css'
+            )
+        }
+
+    def render(self, name, value, attrs=None):
+        imger_settings = self.imger_settings
+        imger_json = json.dumps(imger_settings)
+
+        static_url = settings.STATIC_URL
+
+        if value is None:
+            currently = r''
+        else:
+            currently = r'%s' % (value)
+
+        if not static_url.endswith('/'):
+            static_url = r'%s/' % (static_url)
+
+        return mark_safe("<p>Currently: %s<br/>Change: <button data-static_url=\"%s\" data-imger='%s' id=\"ImgerBrowseBTN\" type=\"button\">Browse</button> <span id=\"ImgerBrowseLabel\">No image selected...</span><input value=\"%s\" id=\"ImgerDataURL\" name=\"%s\" type=\"hidden\" /></p>" % (value, static_url, imger_json, currently, name))
