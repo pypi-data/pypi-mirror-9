1ee4f26c18a55bfb0eb082c5f26ed4b0ec8710ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:44 +0000

    Add package MQLAlchemy/0.1.3/MQLAlchemy-0.1.3-py3-none-any.whl

---
+++ packages/MQLAlchemy/MQLAlchemy-0.1.3-py3-none-any.whl/mqlalchemy/_compat.py
@@ -0,0 +1,19 @@
+## -*- coding: utf-8 -*-\
+"""
+    mqlalchemy._compat
+    ~~~~~~~~~~~~~~~~~~
+
+    Python2/Python3 support helper library.
+
+    :copyright: (c) 2015 by Nicholas Repole and contributors.
+                See AUTHORS for more details.
+    :license: MIT - See LICENSE for more details.
+"""
+
+import sys
+if sys.version_info[0] == 2:    # pragma no cover
+    bytes = str
+    str = unicode
+else:    # pragma no cover
+    str = str
+    bytes = bytes
