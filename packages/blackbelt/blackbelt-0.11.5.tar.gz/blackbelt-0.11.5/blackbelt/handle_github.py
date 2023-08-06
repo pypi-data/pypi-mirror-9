f4781ef109e882580e810445ab50f1be14cd6275
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:52 +0000

    Add package blackbelt/0.11.5/blackbelt-0.11.5.tar.gz

---
+++ packages/blackbelt/blackbelt-0.11.5.tar.gz/blackbelt-0.11.5/blackbelt/handle_github.py
@@ -0,0 +1,337 @@
+from datetime import datetime, timedelta
+import json
+import re
+from subprocess import check_output
+from time import sleep
+import webbrowser
+
+import click
+import requests
+
+from .config import config
+from .handle_trello import (
+    get_current_working_ticket,
+    get_ticket_ready,
+    comment_ticket,
+    move_to_deployed
+)
+from .hipchat import post_message
+from .circle import wait_for_tests
+from .version import VERSION
+from .notify import notify
+
+GITHUB_CLIENT_ID = "c9f51ce9cb320bf86f16"
+
+UA_STRING = "black-belt/%s" % VERSION
+
+PR_PHRASE_PREFIX = "Pull request for"
+
+
+def get_github_repo():
+    return check_output(['git', 'config', '--get', 'remote.origin.url']).strip()
+
+
+def get_remote_repo_info(github_repo_info):
+    match = re.match(r".*github.com:(?P<owner>[a-zA-Z\_\-]+)/{1}(?P<name>[a-zA-Z\-\_]+)\.git$", github_repo_info)
+    if not match:
+        raise ValueError("Cannot parse repo info. Bad remote?")
+    return match.groupdict()
+
+
+def get_pr_info(pr_url):
+    match = re.match(r".*github.com/(?P<owner>\S+)/{1}(?P<name>\S+)/pull/{1}(?P<number>\d+).*$", pr_url)
+    if not match:
+        raise ValueError("Cannot parse pull request URL, bad format")
+    return match.groupdict()
+
+
+def get_username():
+    url = "https://api.github.com/user"
+
+    headers = {
+        'Authorization': "token %s" % config['github']['access_token']
+    }
+
+    res = requests.get(url, headers=headers)
+
+    return res.json()['login']
+
+
+def pull_request(card_url):
+    """
+    Sends current branch for code review.
+
+    #. Inspects current repository for branches
+    #. If CARD_URL parameter is not specified, it inspects ``Doing`` on the :term:`Work Board` for the current working ticket (you should have only one working ticket in ``Doing`` that is assigned only to you). If it is specified, it inspects ``Doing`` on the :term:`Work Board` for the selected working ticket, and will raise an error if ticket is not found.
+    #. Creates a pull request that references the trello card and references the PR on the card as well
+    #. Moves the card to ``Ready``
+    #. Opens the browser with the PR for further editing/review
+    """
+    branch = get_current_branch()
+    repo = get_github_repo()
+
+    if 'github.com' not in repo:
+        raise ValueError("Current git origin not on github.com; aborting")
+
+    ticket = get_current_working_ticket(card_url)
+    md_link = "[%(name)s](%(url)s)" % ticket
+
+    pr_description = """
+
+%(phrase)s %(link)s.
+
+    """ % {
+        'phrase': PR_PHRASE_PREFIX,
+        'link': md_link
+    }
+
+    repo_info = get_remote_repo_info(repo)
+
+    url = "https://api.github.com/repos/%(owner)s/%(name)s/pulls" % repo_info
+
+    payload = {
+        'title': ticket['name'],
+        'base': 'master',
+        'head': "%(owner)s:%(branch)s" % {'branch': branch, 'owner': repo_info['owner']},
+        'body': pr_description
+    }
+
+    headers = {
+        'Authorization': "token %s" % config['github']['access_token']
+    }
+
+    r = requests.post(url, data=json.dumps(payload), headers=headers)
+
+    if r.status_code != 201:
+        print r.json()
+        raise ValueError("PR ended with status code %s: %s" % (r.status_code, r))
+
+    get_ticket_ready(ticket)
+
+    pr_info = r.json()
+
+    ticket_comment = "Sending [pull request #%(number)s](%(html_url)s)." % pr_info
+
+    comment_ticket(ticket, ticket_comment)
+
+    print "Pull request %(pr_id)s for trello card %(ticket_url)s send!" % {
+        'pr_id': pr_info['number'],
+        'ticket_url': ticket['url']
+    }
+
+    webbrowser.open(pr_info['html_url'])
+
+
+def get_current_branch():
+    return check_output(['git', 'rev-parse', '--abbrev-ref', 'HEAD']).strip()
+
+
+def get_current_sha():
+    return check_output(['git', 'rev-parse', 'HEAD']).strip()
+
+
+def verify_merge(pr_info, headers, max_waiting_time=30, retry_time=0.1):
+    merge_url = "https://api.github.com/repos/%(owner)s/%(name)s/pulls/%(number)s/merge" % pr_info
+    start_time = datetime.now()
+    succeeded = False
+
+    def do_request():
+        r = requests.get(merge_url, headers=headers)
+
+        if (r.status_code == 404):
+            if datetime.now() < start_time + timedelta(seconds=max_waiting_time):
+                sleep(retry_time)
+                return False
+            else:
+                raise ValueError("GitHub says PR hasn't been merged yet and I've reached the waiting time of %s seconds" % max_waiting_time)
+
+        elif (r.status_code not in [200, 204]):
+            raise ValueError("Can't get PR merge info with status code %s" % r.status_code)
+
+        else:
+            return True
+
+    while not succeeded:
+        succeeded = do_request()
+
+
+def merge(pr_url):
+    """
+    This merges PR on Github into master:
+
+    #. Inspects the current repository and the pull request
+    #. Switches to master and brings it up to date
+    #. Merges the PR locally and pushes to master
+    #. Deletes the merged branch from the remote repository/github
+
+    TODO:
+
+    * Comment the associated Trello card
+
+    """
+
+    pr_info = get_pr_info(pr_url)
+
+    pr_api_url = "https://api.github.com/repos/%(owner)s/%(name)s/pulls/%(number)s" % pr_info
+
+    headers = {
+        'Authorization': "token %s" % config['github']['access_token'],
+        'User-Agent': UA_STRING
+    }
+
+    r = requests.get(pr_api_url, headers=headers)
+
+    if (r.status_code != 200):
+        raise ValueError("Cannot retrieve PR info with status code %s: %s" % (r.status_code, r))
+
+    pr = r.json()
+
+    if pr['state'] != 'open':
+        raise ValueError("PR is %(state)s instead of still being open; not merging" % pr)
+
+    gh_repo = pr['base']['repo']
+
+    if get_github_repo() not in [
+        gh_repo['git_url'],
+        gh_repo['ssh_url'],
+        gh_repo['clone_url']
+    ]:
+        raise ValueError("The pull request is for the repository %s, while your origin is set up for %s" % (
+            gh_repo['git_url'], get_github_repo()
+        ))
+
+    sha = pr['head']['sha']
+
+    if get_current_branch() != 'master':
+        check_output(['git', 'checkout', 'master'])
+
+    check_output(['git', 'pull'])
+
+    check_output(['git', 'merge', sha, '-m', "Merging pull request #%(number)s: %(title)s " % pr])
+
+    check_output(['git', 'push', 'origin', 'master'])
+
+    merge_sha = get_current_sha()
+
+    verify_merge(pr_info, headers)
+
+    # All good, delete branch
+    branch_url = "https://api.github.com/repos/%(owner)s/%(name)s/git/refs/heads/%(branch)s" % {
+        'owner': pr_info['owner'],
+        'name': pr_info['name'],
+        'branch': pr['head']['ref']
+
+    }
+    r = requests.delete(branch_url, headers=headers)
+
+    if (r.status_code != 204):
+        raise ValueError("Failed to delete branch after merging pull request, go do it manually")
+
+    print "#%(number)s merged!" % pr_info
+    post_message("@here [%(owner)s/%(name)s] Merged PR #%(number)s: %(title)s (%(commits)s commits, %(comments)s comments)" % {
+        'owner': pr_info['owner'],
+        'name': pr_info['name'],
+        'number': pr_info['number'],
+        'title': pr['title'],
+        'comments': pr['comments'],
+        'commits': pr['commits']
+    })
+
+    return {
+        'sha': merge_sha,
+        'owner': pr_info['owner'],
+        'name': pr_info['name'],
+        'number': pr_info['number'],
+        'description': pr['body'],
+        'html_url': pr['html_url'],
+        'title': pr['title'],
+        'branch': pr['head']['ref']
+    }
+
+
+def get_pr_ticket_id(description):
+    match = re.search(PR_PHRASE_PREFIX + ' ' + r"\[.*\]\(https://trello.com/c/(?P<id>\w+)/.*\)", description)
+    if not match or not 'id' in match.groupdict():
+        raise ValueError("Can't find URL in the PR description")
+
+    return match.groupdict()['id']
+
+
+def deploy(pr_url):
+    """
+    Deploys PR to production
+
+    #. Does :ref:`pr-merge`
+    #. Inform people on HipChat about the merge and the deployment intent
+    #. Prepares Heroku deploy slugs using ``grunt create-slug``
+    #. Waits for CircleCI tests to pass
+    #. TODO: If they fail, asks for retry
+    #. Asks for deploy confirmation
+    #. Notify others on HipChat about deploy
+    #. Deploys
+    #. Creates a release on GitHub, using merged branch name as 'ref'.
+    #. If it can figure out related Trello card (looks for "Pull request for <link>"), moves it to "Deployed by" column
+    #. Does *not* bring beer yet, unfortunately
+
+    """
+    merge_info = merge(pr_url)
+
+    check_output(['grunt', 'create-slug'])
+
+    print "Waiting for tests to pass..."
+
+    ci_info = wait_for_tests(
+        sha=merge_info['sha'],
+        owner=merge_info['owner'],
+        name=merge_info['name']
+    )
+
+    if ci_info['failed']:
+        notify('Apiary Deployment', "Tests FAILED for %s" % merge_info['sha'])
+        raise ValueError("Circle build failed. TODO: Auto retry.")
+    notify('Apiary Deployment', "New version %s ready for deploy" % merge_info['sha'])
+
+    # Insert newline
+    print ''
+
+    click.confirm("Ready for deploy! Do you want me to deploy %s as the new version of Apiary?" % merge_info['sha'], abort=True)
+
+    post_message("@here Deploying \"%(title)s\" in 15 seconds" % merge_info)
+
+    sleep(15)
+
+    check_output(['grunt', 'deploy-slug'])
+
+    comment = "Deployed by me with version %s. Please verify it works." % merge_info['sha']
+
+    try:
+        ticket_id = get_pr_ticket_id(merge_info['description'])
+        move_to_deployed(card_id=ticket_id, comment=comment)
+    except ValueError:
+        if click.prompt("Moving card failed. Open PR in browser?", default=True):
+            webbrowser.open(merge_info['html_url'])
+
+    create_release(ref=merge_info['branch'], payload='', description="Deployed to production")
+
+def create_release(ref, payload, description):
+    """ Create release in github after deploy to production """
+
+    url = "https://api.github.com/repos/%(owner)s/%(name)s/deployments" % repo_info
+
+    body = {
+        'ref': ref,
+        'payload': payload,
+        'description': description
+    }
+
+    headers = {
+        'Authorization': "token %s" % config['github']['access_token']
+    }
+
+    r = requests.post(url, data=json.dumps(body), headers=headers)
+
+    if r.status_code != 201:
+        print r.json()
+        raise ValueError("Create github release ended with status code %s: %s" % (r.status_code, r))
+
+
---
+++ packages/blackbelt/blackbelt-0.11.5.tar.gz/blackbelt-0.11.5/pavement.py
@@ -0,0 +1,120 @@
+from paver.easy import *
+from paver.setuputils import setup
+
+options = environment.options
+
+NAME = 'blackbelt'
+VERSION = '0.11.5'
+
+requires = ['click', 'requests', 'PyGithub']
+
+setup(
+    name=NAME,
+    version=VERSION,
+    description='Project automation the Apiary way',
+    long_description="""
+    Black Belt: Project Automation The Apiary Way
+    ==================================================
+
+    Integrates useful GitHub workflow with Trello, Hipchat and Circle CI.
+
+    Please visit `the Black Belt documentation <http://black-belt.readthedocs.org/en/latest/>`_ for more details.
+    """,
+    author='Lukas Linhart',
+    author_email='lukas@apiary.io',
+    url='http://github.com/apiaryio/black-belt',
+    license='MIT',
+    packages=[NAME, NAME + '.apis', NAME + '.commands'],
+    install_requires=requires,
+    # requires=requires,
+    tests_require=['nose', 'virtualenv'],
+    test_suite='nose.collector',
+    zip_safe=False,
+    # Yes, entry_points are nice and shiny and should be used in 21st century
+    # Unfortunately, having some dependencies, the load_entry_point in the ge-
+    # nerated console script blows up with pkg_resources.DistributionNotFound: requests
+    # if it was installed with pip.
+    # Pradoxically, solution is to easy_install -U all the dependencies.
+    # Therefore, just fall back to the good, old, simple disutils script that works.
+    scripts=['distutils_scripts/bb'],
+    # entry_points={
+    #     'console_scripts': [
+    #         'bb = blackbelt.tasks:main'
+    #     ]
+    # },
+    classifiers=[
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Environment :: Console",
+        "Topic :: Documentation",
+        "Topic :: Utilities",
+        "Topic :: Software Development :: Build Tools",
+    ]
+)
+
+options.setup.package_data = paver.setuputils.find_package_data("blackbelt", package="blackbelt",
+                                                only_in_packages=False)
+
+
+@task
+@consume_args
+def bump(args):
+    version = map(int, VERSION.split('.')[0:3])
+
+    if len(args) > 0 and args[0] == 'major':
+        version[1] += 1
+        version[2] = 0
+    else:
+        version[2] += 1
+
+    version = map(str, version)
+
+    module_content = "VERSION='%s'\n" % '.'.join(version)
+
+    # bump version in blackbelt
+    with open(path('blackbelt/version.py'), 'w') as f:
+        f.write(module_content)
+
+    # bump version in sphinx
+    conf = []
+    with open(path('docs/source/conf.py'), 'r') as f:
+        for line in f.readlines():
+            if line.startswith('version = '):
+                line = "version = '%s'\n" % '.'.join(version[0:2])
+            elif line.startswith('release = '):
+                line = "release = '%s'\n" % '.'.join(version[0:3])
+
+            conf.append(line)
+
+    with open(path('docs/source/conf.py'), 'w') as f:
+        f.writelines(conf)
+
+    # bump version in this pavement file itself
+    # we cannot just import it because it implies blackbelt already been
+    # installed/in path, which may cause some interesting problems
+    # solvable by mangling with sys.path, but this just feels...better
+    conf = []
+    with open(path('./pavement.py'), 'r') as f:
+        for line in f.readlines():
+            if line.startswith('VERSION = '):
+                line = "VERSION = '%s'\n" % '.'.join(version)
+
+            conf.append(line)
+
+    with open(path('./pavement.py'), 'w') as f:
+        f.writelines(conf)
+
+    sh("git commit pavement.py blackbelt/version.py docs/source/conf.py -m 'Version bump to %s'" % '.'.join(version))
+
+
+@task
+def release():
+    sh("git tag -s '%(name)s-%(version)s' -m 'Version bump to %(version)s'" % {'name': NAME, 'version': VERSION})
+    sh("git push --tags")
+    sh("python setup.py register sdist upload")
+    sh("python setup.py bdist_wheel upload")
