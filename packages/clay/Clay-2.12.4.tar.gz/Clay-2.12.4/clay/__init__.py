43502a6a641e87ecc4aeb2cb744c421ca5930812
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:40 +0000

    Add package clay/2.12.4/Clay-2.12.4.tar.gz

---
+++ packages/clay/Clay-2.12.4.tar.gz/Clay-2.12.4/clay/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+# Clay
+
+An amazing prototyping tool
+
+------------
+© [Juan-Pablo Scaletti](http://jpscaletti.com).
+[MIT License](http://www.opensource.org/licenses/mit-license.php)
+
+"""
+from .main import Clay  # noqa
+
+
+__version__ = '2.12.4'
