6057a31671945cc9c0acef7ebb01e17fa21112fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:45 +0000

    Add package spinklepypi/1.0.0/spinklepypi-1.0.0.tar.gz

---
+++ packages/spinklepypi/spinklepypi-1.0.0.tar.gz/spinklepypi-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup (
+    name = 'spinklepypi',
+    version = '1.0.0',
+    py_modules = ['nester'],
+    author = 'cbpython',
+    author_email = 'sdkjson@hotmail.com',
+    url = 'http://whatever.com',
+    description = 'annpying nester list thing',
+    )
+    
