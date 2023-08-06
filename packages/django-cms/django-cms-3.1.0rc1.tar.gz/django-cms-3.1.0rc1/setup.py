34bc67c2568affa42b7ede8e1426384635d1b86c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:34 +0000

    Add package django-cms/3.1.0rc1/django-cms-3.1.0rc1.tar.gz

---
+++ packages/django-cms/django-cms-3.1.0rc1.tar.gz/django-cms-3.1.0rc1/setup.py
@@ -0,0 +1,49 @@
+from setuptools import setup, find_packages
+import os
+import cms
+
+
+CLASSIFIERS = [
+    'Development Status :: 5 - Production/Stable',
+    'Environment :: Web Environment',
+    'Framework :: Django',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: BSD License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    'Topic :: Software Development',
+    'Topic :: Software Development :: Libraries :: Application Frameworks',
+    "Programming Language :: Python :: 2.6",
+    "Programming Language :: Python :: 2.7",
+    "Programming Language :: Python :: 3.3",
+    "Programming Language :: Python :: 3.4",
+]
+
+setup(
+    author="Patrick Lauber",
+    author_email="digi@treepy.com",
+    name='django-cms',
+    version=cms.__version__,
+    description='An Advanced Django CMS',
+    long_description=open(os.path.join(os.path.dirname(__file__), 'README.rst')).read(),
+    url='https://www.django-cms.org/',
+    license='BSD License',
+    platforms=['OS Independent'],
+    classifiers=CLASSIFIERS,
+    install_requires=[
+        'Django>=1.6',
+        'django-classy-tags>=0.5',
+        'html5lib',
+        'django-treebeard==3.0',
+        'django-sekizai>=0.7',
+        'djangocms-admin-style'
+    ],
+    extras_require={
+        'south': ['south>=1.0.0'],
+    },
+    packages=find_packages(exclude=["project", "project.*"]),
+    include_package_data=True,
+    zip_safe=False,
+    test_suite='runtests.main',
+)
