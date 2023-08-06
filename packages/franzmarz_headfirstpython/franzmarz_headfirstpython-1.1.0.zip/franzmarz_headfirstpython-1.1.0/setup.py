cc081299193be0347d9dfe6b4fc8c4afd0338872
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:55 +0000

    Add package franzmarz_headfirstpython/1.1.0/franzmarz_headfirstpython-1.1.0.zip

---
+++ packages/franzmarz_headfirstpython/franzmarz_headfirstpython-1.1.0.zip/franzmarz_headfirstpython-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'franzmarz_headfirstpython',
+    version = '1.1.0',
+    py_modules = ['franzmarz_headfirstpython'],
+    author = 'Frank',
+    author_email = 'frank_mtz27@hotmail.com',
+    url = 'www.padmarsystems.com',
+    description = 'Data analizer',
+    )
