4369ff71207d85c4d38ce2f3f3ab6c698d3475dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:20 +0000

    Add package control/0.7.0/control-0.7.0-py2.py3-none-any.whl

---
+++ packages/control/control-0.7.0-py2.py3-none-any.whl/control/version.py
@@ -0,0 +1,10 @@
+
+# THIS FILE IS GENERATED FROM SETUP.PY
+short_version = '0.6.5'
+version = '0.6.5'
+full_version = '0.6.5'
+git_revision = '5ac5100c8ba8e0b2b58ac54e3ad158b5e11bb7b6'
+release = True
+
+if not release:
+    version = full_version
