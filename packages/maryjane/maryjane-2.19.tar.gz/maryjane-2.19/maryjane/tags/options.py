cee889ca963403fd94ebc179b57d3590bad542c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:03 +0000

    Add package maryjane/2.19/maryjane-2.19.tar.gz

---
+++ packages/maryjane/maryjane-2.19.tar.gz/maryjane-2.19/maryjane/tags/options.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from maryjane.tags.base import DictionaryTag
+__author__ = 'vahid'
+
+
+class OptionsTag(DictionaryTag):
+
+    def __repr__(self):
+        return '<Options>'
+
