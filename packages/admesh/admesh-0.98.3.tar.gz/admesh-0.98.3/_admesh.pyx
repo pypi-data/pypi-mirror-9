370e08917949594976acebdc85e930c2c95b0a63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:02 +0000

    Add package admesh/0.98.3/admesh-0.98.3.tar.gz

---
+++ packages/admesh/admesh-0.98.3.tar.gz/admesh-0.98.3/_admesh.pyx
@@ -0,0 +1,71 @@
+from cadmesh cimport *
+
+
+class AdmeshError(Exception):
+    pass
+
+
+cdef class Stl:
+    cdef stl_file _c_stl_file
+    cdef bint _opened
+
+    def __cinit__(self, path=''):
+        self._opened = False
+        if path:
+            self.open(path)
+
+    def get_stats(self):
+        return self._c_stl_file.stats
+
+    stats = property(lambda self: self.get_stats())
+
+    def open(self, path):
+        """stl_open"""
+        by_path = path.encode('UTF-8')
+        stl_open(&self._c_stl_file, by_path)
+        if stl_get_error(&self._c_stl_file):
+            stl_clear_error(&self._c_stl_file)
+            raise AdmeshError('stl_open')
+        self._opened = True
+
+    def repair(self,
+               fixall_flag=True,
+               exact_flag=False,
+               tolerance_flag=False,
+               tolerance=0,
+               increment_flag=False,
+               increment=0,
+               nearby_flag=False,
+               iterations=2,
+               remove_unconnected_flag=False,
+               fill_holes_flag=False,
+               normal_directions_flag=False,
+               normal_values_flag=False,
+               reverse_all_flag=False,
+               verbose_flag=True):
+        """stl_repair"""
+        if not self._opened:
+            raise AdmeshError('STL not opened')
+        stl_repair(&self._c_stl_file,
+                   fixall_flag,
+                   exact_flag,
+                   tolerance_flag,
+                   tolerance,
+                   increment_flag,
+                   increment,
+                   nearby_flag,
+                   iterations,
+                   remove_unconnected_flag,
+                   fill_holes_flag,
+                   normal_directions_flag,
+                   normal_values_flag,
+                   reverse_all_flag,
+                   verbose_flag)
+        if stl_get_error(&self._c_stl_file):
+            stl_clear_error(&self._c_stl_file)
+            raise AdmeshError('stl_repair')
+
+
+    def __dealloc__(self):
+        if self._opened:
+            stl_close(&self._c_stl_file)
---
+++ packages/admesh/admesh-0.98.3.tar.gz/admesh-0.98.3/_cadmesh.pxd
@@ -0,0 +1,65 @@
+cdef extern from "admesh/stl.h":
+    ctypedef struct stl_vertex:
+        float x
+        float y
+        float z
+
+    ctypedef struct stl_normal:
+        float x
+        float y
+        float z
+
+    ctypedef struct stl_facet:
+        pass
+
+    cdef enum stl_type:
+        binary, ascii, inmemory
+
+    ctypedef struct stl_edge:
+        pass
+
+    ctypedef struct stl_hash_edge:
+        pass
+
+    ctypedef struct stl_neighbors:
+        pass
+
+    ctypedef struct v_indices_struct:
+        pass
+
+    ctypedef struct stl_stats:
+        char          header[81]
+        stl_type      type
+        int           number_of_facets
+        stl_vertex    max
+        stl_vertex    min
+        stl_vertex    size
+        float         bounding_diameter
+        float         shortest_edge
+        float         volume
+        unsigned      number_of_blocks
+        int           connected_edges
+        int           connected_facets_1_edge
+        int           connected_facets_2_edge
+        int           connected_facets_3_edge
+        int           facets_w_1_bad_edge
+        int           facets_w_2_bad_edge
+        int           facets_w_3_bad_edge
+        int           original_num_facets
+        int           edges_fixed
+        int           degenerate_facets
+        int           facets_removed
+        int           facets_added
+        int           facets_reversed
+        int           backwards_edges
+        int           normals_fixed
+        int           number_of_parts
+        int           malloced
+        int           freed
+        int           facets_malloced
+        int           collisions
+        int           shared_vertices
+        int           shared_malloced
+
+    ctypedef struct stl_file:
+        stl_stats     stats
