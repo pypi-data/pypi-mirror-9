14a0372b2a54a4cf26b52ae67601fafb6d9baf07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:54 +0000

    Add package circleclient/0.1.6/circleclient-0.1.6.tar.gz

---
+++ packages/circleclient/circleclient-0.1.6.tar.gz/circleclient-0.1.6/setup.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+"""
+Setup script for circleclient.
+
+"""
+
+import os
+import sys
+
+from setuptools import setup, find_packages
+
+
+readme = open('README.rst').read()
+doclink = """
+Documentation
+-------------
+
+The full documentation is at http://circleclient.rtfd.org."""
+history = open('HISTORY.rst').read().replace('.. :changelog:', '')
+
+
+setup(
+    name='circleclient',
+    version='0.1.6',
+    author='Jakub Jarosz',
+    author_email='qba73@postpro.net',
+    description='Python client for CircleCI API',
+    long_description=readme + '\n\n' + doclink + '\n\n' + history,
+    url='http://github.com/qba73/circleclient/',
+    packages=[
+        'circleclient'
+    ],
+    package_dir={'circleclient': 'circleclient'},
+    include_package_data=True,
+    install_requires=['requests'],
+    license='MIT',
+    zip_safe=False,
+    keywords=['ci', 'testing', 'qa', 'circleclient'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Software Development :: Testing',
+        'Topic :: Software Development :: Quality Assurance'
+    ],
+)
