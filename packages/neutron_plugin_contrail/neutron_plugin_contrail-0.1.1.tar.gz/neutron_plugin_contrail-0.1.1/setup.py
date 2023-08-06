43ce9c952fe5a20032b952a2ff5692a5943e41d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:11 +0000

    Add package neutron_plugin_contrail/0.1.1/neutron_plugin_contrail-0.1.1.tar.gz

---
+++ packages/neutron_plugin_contrail/neutron_plugin_contrail-0.1.1.tar.gz/neutron_plugin_contrail-0.1.1/setup.py
@@ -0,0 +1,21 @@
+#
+# Copyright (c) 2013 Juniper Networks, Inc. All rights reserved.
+#
+
+from setuptools import setup, find_packages
+
+def requirements(filename):
+    with open(filename) as f:
+        lines = f.read().splitlines()
+    return lines
+
+setup(
+    name='neutron_plugin_contrail',
+    version='0.1.1',
+    packages=find_packages(),
+    package_data={'': ['*.html', '*.css', '*.xml']},
+    zip_safe=False,
+    long_description="Contrail neutron plugin",
+    test_suite='neutron_plugin_contrail.tests',
+    tests_require=requirements('test-requirements.txt'),
+)
