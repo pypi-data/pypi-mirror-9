62ca65b2234cea3c7c4b5c0ebfb80c489133a27c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:15 +0000

    Add package testing.cassandra/1.1.4/testing.cassandra-1.1.4.tar.gz

---
+++ packages/testing.cassandra/testing.cassandra-1.1.4.tar.gz/testing.cassandra-1.1.4/setup.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+import sys
+from setuptools import setup, find_packages
+
+classifiers = [
+    "Development Status :: 4 - Beta",
+    "Intended Audience :: Developers",
+    "License :: OSI Approved :: Apache Software License",
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 2.6",
+    "Programming Language :: Python :: 2.7",
+    "Topic :: Database",
+    "Topic :: Software Development",
+    "Topic :: Software Development :: Testing",
+]
+
+install_requires = [
+    'pycassa',
+    'PyYAML',
+]
+if sys.version_info < (2, 7):
+    install_requires.append('unittest2')
+
+
+setup(
+    name='testing.cassandra',
+    version='1.1.4',
+    description='automatically setups a cassandra instance in a temporary directory, and destroys it after testing',
+    long_description=open('README.rst').read(),
+    classifiers=classifiers,
+    keywords=[],
+    author='Takeshi Komiya',
+    author_email='i.tkomiya at gmail.com',
+    url='http://bitbucket.org/tk0miya/testing.cassandra',
+    license='Apache License 2.0',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    package_data={'': ['buildout.cfg']},
+    include_package_data=True,
+    install_requires=install_requires,
+    extras_require=dict(
+        testing=[
+            'mock',
+            'nose',
+        ],
+    ),
+    test_suite='nose.collector',
+    tests_require=[
+        'mock',
+        'nose',
+    ],
+)
