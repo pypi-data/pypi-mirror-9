750e4e84a562bc6fc0d69750289c6be84af07504
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:12 +0000

    Add package rebnypy/0.1.0/rebnypy-0.1.0.tar.gz

---
+++ packages/rebnypy/rebnypy-0.1.0.tar.gz/rebnypy-0.1.0/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='rebnypy',
+      version='0.1.0',
+      description="A Python library for interacting with the REBNY listing service",
+      author='Kevin McCarthy',
+      author_email='me@kevinmccarthy.org',
+      url='https://github.com/RealGeeks/rebnypy',
+      packages = [
+        'rebnypy',
+      ],
+      package_dir={
+        'rebnypy': 'rebnypy',
+      },
+      license='MIT',
+      install_requires=['requests'],
+      tests_require=['pytest','mock'],
+      classifiers=[
+          'Environment :: Console',
+          'Intended Audience :: Developers',
+          'Programming Language :: Python',
+          'License :: OSI Approved :: MIT License',
+      ],
+)
