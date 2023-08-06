0b2628dd152630624ece1a4b26d5ac638cdf5c09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:29:38 +0000

    Add package ExtensionClass/4.1.2/ExtensionClass-4.1.2.tar.gz

---
+++ packages/ExtensionClass/ExtensionClass-4.1.2.tar.gz/ExtensionClass-4.1.2/CHANGES.rst
@@ -0,0 +1,85 @@
+Changelog
+=========
+
+4.1.2 (2015-04-03)
+------------------
+
+- Fix calling of `__class_init__` hook by Python implementation.
+
+4.1.1 (2015-03-20)
+------------------
+
+- Avoid wrapping ``__parent__`` in pure-Python version.  Matches
+  change made to C version in afb8488.  See issue #3.
+
+4.1 (2014-12-18)
+------------------
+
+- Housekeeping changes only.
+
+4.1b1 (2014-11-12)
+------------------
+
+- Added compatibility with Python 3.4.
+
+4.1a1 (2013-05-04)
+------------------
+
+- Added compatibility with Python 3.2 and 3.3 using the Python reference
+  implementation.
+
+- Add Python reference implementation. Used by default on PyPy.
+
+4.0 (2013-02-24)
+----------------
+
+- Added trove classifiers to project metadata.
+
+4.0a1 (2011-12-13)
+------------------
+
+- Don't create wrappers when retrieving parent pointers.
+
+2.13.2 (2010-06-16)
+-------------------
+
+- LP #587760: Handle tp_basicsize correctly.
+
+2.13.1 (2010-04-03)
+-------------------
+
+- Removed undeclared testing dependency on zope.testing.
+
+- Removed cruft in ``pickle/pickle.c`` related to removed ``__getnewargs__``.
+
+2.13.0 (2010-02-22)
+-------------------
+
+- Avoid defining ``__getnewargs__`` as not to defeat the ZODB persistent
+  reference optimization. Refs https://bugs.launchpad.net/zope2/+bug/143657.
+  In order to take advantage of this optimization, you need to re-save your
+  objects.
+
+2.12.0 (2010-02-14)
+-------------------
+
+- Removed old build artifacts and some metadata cleanup.
+
+- Added support for method cache in ExtensionClass. Patch contributed by
+  Yoshinori K. Okuji. See https://bugs.launchpad.net/zope2/+bug/486182.
+
+2.11.3 (2009-08-02)
+-------------------
+
+- Further 64-bit fixes (Python 2.4 compatibility).
+
+2.11.2 (2009-08-02)
+-------------------
+
+- Fixed 64-bit compatibility issues for Python 2.5.x / 2.6.x.  See 
+  http://www.python.org/dev/peps/pep-0353/ for details.
+
+2.11.1 (2009-02-19)
+-------------------
+
+- Initial egg release.
---
+++ packages/ExtensionClass/ExtensionClass-4.1.2.tar.gz/ExtensionClass-4.1.2/setup.py
@@ -0,0 +1,81 @@
+##############################################################################
+#
+# Copyright (c) 2007 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Setup for the ExtensionClass distribution
+"""
+import os
+import platform
+import sys
+from setuptools import setup, find_packages, Extension
+
+with open('README.rst') as f:
+    README = f.read()
+
+with open('CHANGES.rst') as f:
+    CHANGES = f.read()
+
+# PyPy won't build the extension.
+py_impl = getattr(platform, 'python_implementation', lambda: None)
+is_pypy = py_impl() == 'PyPy'
+is_pure = 'PURE_PYTHON' in os.environ
+py3k = sys.version_info >= (3, )
+if is_pypy or is_pure or py3k:
+    ext_modules = []
+else:
+    ext_modules = [
+        Extension("ExtensionClass._ExtensionClass",
+                  [os.path.join('src', 'ExtensionClass',
+                                '_ExtensionClass.c')],
+                  include_dirs=['src']),
+        Extension("ComputedAttribute._ComputedAttribute",
+                  [os.path.join('src', 'ComputedAttribute',
+                                '_ComputedAttribute.c')],
+                  include_dirs=['src']),
+        Extension("MethodObject._MethodObject",
+                  [os.path.join('src', 'MethodObject',
+                                '_MethodObject.c')],
+                  include_dirs=['src']),
+    ]
+
+setup(
+    name='ExtensionClass',
+    version='4.1.2',
+    url='http://pypi.python.org/pypi/ExtensionClass',
+    license='ZPL 2.1',
+    description='Metaclass for subclassable extension types',
+    author='Zope Foundation and Contributors',
+    author_email='zope-dev@zope.org',
+    long_description='\n\n'.join([README, CHANGES]),
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    classifiers=[
+        "Development Status :: 6 - Mature",
+        "Environment :: Web Environment",
+        "Framework :: Zope2",
+        "License :: OSI Approved :: Zope Public License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+    ],
+    ext_modules=ext_modules,
+    include_package_data=True,
+    zip_safe=False,
+)
