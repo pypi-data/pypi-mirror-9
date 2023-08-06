397bf69ea2305cd979c18a9d4367e032393d6f3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:48 +0000

    Add package django-simple-invoice/0.3.3/django-simple-invoice-0.3.3.tar.gz

---
+++ packages/django-simple-invoice/django-simple-invoice-0.3.3.tar.gz/django-simple-invoice-0.3.3/invoice/pdf.py
@@ -0,0 +1,29 @@
+try:
+    from django.utils import importlib
+except ImportError:
+    import importlib
+
+from invoice.conf import settings
+
+
+def header_func(*args, **kwargs):
+    inv_module = importlib.import_module(settings.INV_MODULE)
+    inv_module.draw_header(*args, **kwargs)
+
+
+def address_func(*args, **kwargs):
+    inv_module = importlib.import_module(settings.INV_MODULE)
+    inv_module.draw_address(*args, **kwargs)
+
+
+def footer_func(*args, **kwargs):
+    inv_module = importlib.import_module(settings.INV_MODULE)
+    inv_module.draw_footer(*args, **kwargs)
+
+
+def draw_pdf(*args, **kwargs):
+    try:
+        inv_module = importlib.import_module(settings.INV_MODULE)
+        return inv_module.draw_pdf(*args, **kwargs)
+    except:
+        return False
---
+++ packages/django-simple-invoice/django-simple-invoice-0.3.3.tar.gz/django-simple-invoice-0.3.3/setup.py
@@ -0,0 +1,52 @@
+import re
+from os import path
+from setuptools import setup, find_packages
+
+
+def read(*parts):
+    return open(path.join(path.dirname(__file__), *parts)).read()
+
+
+def find_version(*file_paths):
+    version_file = read(*file_paths)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
+
+setup(
+    name='django-simple-invoice',
+    version=find_version('invoice', '__init__.py'),
+    description='Invoicing application for Django',
+    author='Thomas Loiret, Mathias Wolff',
+    author_email='mathiaswolff@mac.com',
+    license="GPL v3",
+    long_description="details are available in README.md file",
+    keywords="invoice django application pdf",
+    url='https://github.com/mwolff/django-simple-invoice',
+    packages=find_packages(),
+    zip_safe=False,
+    package_data={
+        'menu_manager': [
+            'locale/*/LC_MESSAGES/*',
+            'templates/invoice/*',
+            'static/*',
+        ],
+    },
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ],
+    install_requires=[
+        'django>=1.5',
+        'reportlab>=2.7',
+        'PyPDF2>=1.20',
+        'django-extensions>=1.3.3',
+    ]
+)
