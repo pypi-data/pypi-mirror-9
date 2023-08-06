b5695be8560af7b4b22bac2a12689de39bb6ce5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:07:32 +0000

    Add package django-canary-endpoint/0.2.0/django-canary-endpoint-0.2.0.tar.gz

---
+++ packages/django-canary-endpoint/django-canary-endpoint-0.2.0.tar.gz/django-canary-endpoint-0.2.0/tests/projects/example.py
@@ -0,0 +1,77 @@
+import os
+import sys
+
+
+# Project
+#########
+
+DEBUG = True
+
+ROOT = os.path.abspath(os.path.dirname(__file__))
+sys.path.append(ROOT)
+
+VERSION = '1.2.3'
+
+ROOT_URLCONF = 'tests.projects.example'
+
+SECRET_KEY = 'foo'
+
+
+# Databases
+###########
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': ':memory:',
+        'USER': 'bar',
+        'PASSWORD': 'baz',
+    }
+}
+
+
+# RQ
+####
+
+RQ_QUEUES = {
+    'default': {
+        'URL': 'redis://foo',
+        'DEFAULT_TIMEOUT': 360,
+        'DB': 0,
+    },
+}
+
+
+# Services
+##########
+
+HTTP_ENDPOINT = 'http://service/'
+
+HTTP_ENDPOINT_WITH_CANARY = 'http://service/_status/'
+
+
+# Canary
+########
+
+from canary_endpoint import Canary
+from canary_endpoint.resources.databases import DjangoDatabase
+from canary_endpoint.resources.rq import DjangoRQ
+from canary_endpoint.resources.search import ElasticSearch
+from canary_endpoint.resources.services import Service, ServiceWithCanary
+
+canary = Canary('example', root=ROOT, version=VERSION, resources=[
+    DjangoDatabase(statements=['SELECT 1 FROM foo;']),
+    DjangoRQ(),
+    ElasticSearch('es_resource', host='http://elasticsearch.example:9200'),
+    Service('foo', url=HTTP_ENDPOINT),
+    ServiceWithCanary('bar', url=HTTP_ENDPOINT_WITH_CANARY),
+])
+
+
+# URLs
+######
+
+from canary_endpoint.views import status
+from django.conf.urls import patterns, url
+
+urlpatterns = patterns('', url(r'^_status/$', status, {'canary': canary}))
