0718f197c04f4f63f3ba9a19c5f1a7e133a0fa4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:55 +0000

    Add package openlabs_nereid_catalog_variants/3.4.0.1/openlabs_nereid_catalog_variants-3.4.0.1.tar.gz

---
+++ packages/openlabs_nereid_catalog_variants/openlabs_nereid_catalog_variants-3.4.0.1.tar.gz/openlabs_nereid_catalog_variants-3.4.0.1/product.py
@@ -0,0 +1,191 @@
+# -*- coding: utf-8 -*-
+"""
+    product.py
+
+    :copyright: (c) 2014 by Openlabs Technologies & Consulting (P) Limited
+    :license: BSD, see LICENSE for more details.
+"""
+from functools import partial
+
+from trytond.model import ModelView, ModelSQL, fields
+from trytond.pool import PoolMeta
+from trytond.pyson import Eval
+from nereid import url_for, request
+from flask import json
+from babel import numbers
+
+__all__ = [
+    'Template', 'Product', 'ProductVariationAttributes', 'ProductAttribute',
+]
+__metaclass__ = PoolMeta
+
+
+class Template:
+    "Product Template"
+    __name__ = 'product.template'
+
+    variation_attributes = fields.One2Many(
+        'product.variation_attributes', 'template', 'Variation Attributes',
+    )
+
+    @classmethod
+    def __setup__(cls):
+        super(Template, cls).__setup__()
+        cls._error_messages.update({
+            'missing_attributes':
+                "Please define following attributes for product %s: %s"
+        })
+
+    def validate_variation_attributes(self):
+        for product in self.products_displayed_on_eshop:
+            product.validate_attributes()
+
+    @classmethod
+    def validate(cls, templates):
+        super(Template, cls).validate(templates)
+        for template in templates:
+            template.validate_variation_attributes()
+
+    def get_product_variation_data(self, as_json=False):
+        """Returns json data for product for variants
+        """
+        variants = []
+        varying_attributes = []
+        currency_format = partial(
+            numbers.format_currency,
+            currency=request.nereid_website.company.currency.code,
+            locale=request.nereid_website.default_locale.language.code
+        )
+
+        for varying_attrib in self.variation_attributes:
+            # TODO: This assumes that the attribute is a selection
+            # attribute.
+            #
+            # I feel that this can be improved, by just looking at every
+            # attribute and looking at the attribute values and making a
+            # set out of it. This will be costly, but could be easily
+            # cached and invalidated on product data change.
+            varying_attributes.append({
+                'sequence': varying_attrib.sequence,
+                'name': varying_attrib.attribute.name,
+                'string': varying_attrib.attribute.string,
+                'widget': varying_attrib.widget,
+
+                # TODO: Add support for more attribute types
+                'options': json.loads(varying_attrib.attribute.selection_json),
+            })
+
+        varying_attribute_names = [
+            varattr['name'] for varattr in varying_attributes
+        ]
+
+        for product in self.products_displayed_on_eshop:
+            product_attributes = product.attributes or {}
+            res = dict([av for av in filter(
+                lambda x: x[0] in varying_attribute_names,
+                product_attributes.iteritems()
+            )])
+            variants.append({
+                'id': product.id,
+                'rec_name': product.rec_name,
+                'name': product.template.name,
+                'code': product.code,
+                'price': currency_format(product.sale_price(1)),
+                'url': url_for('product.product.render', uri=product.uri),
+                'image_urls': [
+                    {
+                        'large': (
+                            image.transform_command().thumbnail(500, 500, 'a')
+                            .url()
+                        ),
+                        'thumbnail': (
+                            image.transform_command().thumbnail(120, 120, 'a')
+                            .url()
+                        ),
+                        'regular': image.url,
+                    }
+                    for image in product.get_images()
+                ],
+                'attributes': res,
+            })
+
+        rv = {
+            'variants': variants,
+            'varying_attributes': varying_attributes,
+        }
+        if as_json:
+            return json.dumps(rv)
+        return rv
+
+
+class Product:
+    "Product"
+    __name__ = 'product.product'
+
+    @classmethod
+    def __setup__(cls):
+        super(Product, cls).__setup__()
+        cls._error_messages.update({
+            'missing_attributes':
+                "Please define following attributes for product %s: %s"
+        })
+
+    def validate_attributes(self):
+        """Check if product defines all the attributes specified in
+        template variation attributes.
+        """
+        if not self.displayed_on_eshop:
+            return
+        required_attrs = set(
+            [v.attribute.name for v in self.template.variation_attributes]
+        )
+        missing = required_attrs - \
+            set(self.attributes.keys() if self.attributes else [])
+        if missing:
+            self.raise_user_error(
+                "missing_attributes",
+                (self.rec_name, ','.join(map(unicode, missing)))
+            )
+
+    @classmethod
+    def validate(cls, products):
+        super(Product, cls).validate(products)
+        for product in products:
+            product.validate_attributes()
+
+
+class ProductVariationAttributes(ModelSQL, ModelView):
+    "Variation attributes for product template"
+    __name__ = 'product.variation_attributes'
+
+    sequence = fields.Integer('Sequence')
+    template = fields.Many2One('product.template', 'Template', required=True)
+    attribute = fields.Many2One(
+        'product.attribute', 'Attribute', required=True,
+        domain=[('sets', '=',
+                Eval('_parent_template', {}).get('attribute_set', -1))],
+    )
+    widget = fields.Selection([
+        ('dropdown', 'Dropdown'),
+        ('swatches', 'Swatches'),
+    ], 'Widget', required=True)
+
+    @staticmethod
+    def default_widget():
+        return 'dropdown'
+
+    @staticmethod
+    def default_sequence():
+        return 10
+
+
+class ProductAttribute:
+    __name__ = 'product.attribute'
+
+    @classmethod
+    def __setup__(cls):
+        super(ProductAttribute, cls).__setup__()
+        cls._sql_constraints += [
+            ('unique_name', 'UNIQUE(name)',
+                'Attribute name must be unique!'),
+        ]
---
+++ packages/openlabs_nereid_catalog_variants/openlabs_nereid_catalog_variants-3.4.0.1.tar.gz/openlabs_nereid_catalog_variants-3.4.0.1/tests/test_product.py
@@ -0,0 +1,334 @@
+# -*- coding: utf-8 -*-
+"""
+    tests/test_product.py
+
+    :copyright: (C) 2014 by Openlabs Technologies & Consulting (P) Limited
+    :license: BSD, see LICENSE for more details.
+"""
+import unittest
+from decimal import Decimal
+
+import trytond.tests.test_tryton
+from trytond.tests.test_tryton import POOL, USER, DB_NAME, CONTEXT
+from nereid.testing import NereidTestCase
+from trytond.transaction import Transaction
+from trytond.exceptions import UserError
+
+from trytond.config import config
+config.set('database', 'path', '/tmp')
+
+
+class TestProduct(NereidTestCase):
+    "Product Test Case"
+
+    def setup_defaults(self):
+        """
+        Setup the defaults
+        """
+        usd, = self.Currency.create([{
+            'name': 'US Dollar',
+            'code': 'USD',
+            'symbol': '$',
+        }])
+        party1, = self.Party.create([{
+            'name': 'Openlabs',
+        }])
+        company, = self.Company.create([{
+            'party': party1.id,
+            'currency': usd.id
+        }])
+        party2, = self.Party.create([{
+            'name': 'Guest User',
+        }])
+        party3, = self.Party.create([{
+            'name': 'Registered User',
+        }])
+        self.registered_user, = self.NereidUser.create([{
+            'party': party3.id,
+            'display_name': 'Registered User',
+            'email': 'email@example.com',
+            'password': 'password',
+            'company': company.id,
+        }])
+
+        # Create website
+        en_us, = self.Language.search([('code', '=', 'en_US')])
+
+        self.locale_en_us, = self.Locale.create([{
+            'code': 'en_US',
+            'language': en_us.id,
+            'currency': usd.id,
+        }])
+        self.NereidWebsite.create([{
+            'name': 'localhost',
+            'company': company.id,
+            'application_user': USER,
+            'default_locale': self.locale_en_us.id,
+            'currencies': [('add', [usd.id])],
+        }])
+
+    def create_static_file(self, file_buffer, folder_name):
+        """
+        Creates the static file for testing
+        """
+        folder, = self.StaticFolder.create([{
+            'folder_name': folder_name,
+            'description': 'Test Folder'
+        }])
+
+        return self.StaticFile.create([{
+            'name': 'test.png',
+            'folder': folder.id,
+            'file_binary': file_buffer,
+        }])[0]
+
+    def setUp(self):
+        """
+        Set up data used in the tests.
+        this method is called before each test execution.
+        """
+        trytond.tests.test_tryton.install_module('nereid_catalog_variants')
+
+        self.Currency = POOL.get('currency.currency')
+        self.Site = POOL.get('nereid.website')
+        self.Product = POOL.get('product.product')
+        self.Company = POOL.get('company.company')
+        self.NereidUser = POOL.get('nereid.user')
+        self.Language = POOL.get('ir.lang')
+        self.NereidWebsite = POOL.get('nereid.website')
+        self.Party = POOL.get('party.party')
+        self.Template = POOL.get('product.template')
+        self.Uom = POOL.get('product.uom')
+        self.Locale = POOL.get('nereid.website.locale')
+        self.ProductAttribute = POOL.get('product.attribute')
+        self.ProductAttributeSet = POOL.get('product.attribute.set')
+        self.VariationAttributes = POOL.get('product.variation_attributes')
+        self.StaticFolder = POOL.get("nereid.static.folder")
+        self.StaticFile = POOL.get("nereid.static.file")
+
+    def test0010_product_variation_attributes(self):
+        '''
+        Test if product has all the attributes of variation_attributes.
+        '''
+        with Transaction().start(DB_NAME, USER, context=CONTEXT):
+            self.setup_defaults()
+            uom, = self.Uom.search([], limit=1)
+
+            # Create attributes
+            attribute1, = self.ProductAttribute.create([{
+                'name': 'size',
+                'type_': 'selection',
+                'string': 'Size',
+                'selection': 'm: M\nl:L\nxl:XL'
+            }])
+            attribute2, = self.ProductAttribute.create([{
+                'name': 'color',
+                'type_': 'selection',
+                'string': 'Color',
+                'selection': 'blue: Blue\nblack:Black'
+            }])
+            attribute3, = self.ProductAttribute.create([{
+                'name': 'attrib',
+                'type_': 'char',
+                'string': 'Attrib',
+            }])
+            attribute4, = self.ProductAttribute.create([{
+                'name': 'ø',
+                'type_': 'char',
+                'string': 'ø',
+            }])
+
+            # Create attribute set
+            attrib_set, = self.ProductAttributeSet.create([{
+                'name': 'Cloth',
+                'attributes': [
+                    ('add', [attribute1.id, attribute2.id, attribute4.id])
+                ]
+            }])
+
+            # Create product template with attribute set
+            template1, = self.Template.create([{
+                'name': 'THis is Product',
+                'type': 'goods',
+                'list_price': Decimal('10'),
+                'cost_price': Decimal('5'),
+                'default_uom': uom.id,
+                'attribute_set': attrib_set.id,
+            }])
+
+            # Create variation attributes
+            self.VariationAttributes.create([{
+                'template': template1.id,
+                'attribute': attribute1.id,
+            }, {
+                'template': template1.id,
+                'attribute': attribute2.id,
+            }, {
+                'template': template1.id,
+                'attribute': attribute4.id,
+            }])
+
+            # Try to create product with no attributes
+            with self.assertRaises(UserError):
+                self.Product.create([{
+                    'template': template1.id,
+                    'displayed_on_eshop': True,
+                    'uri': 'uri1',
+                    'code': 'SomeProductCode',
+                }])
+
+            # Try to create product with only one attribute
+            with self.assertRaises(UserError):
+                self.Product.create([{
+                    'template': template1.id,
+                    'displayed_on_eshop': True,
+                    'uri': 'uri2',
+                    'code': 'SomeProductCode',
+                    'attributes': {'color': 'blue'}
+                }])
+
+            # Finally create product with all attributes mentioned in
+            # template variation_attributes.
+            product1, = self.Product.create([{
+                'template': template1.id,
+                'displayed_on_eshop': True,
+                'uri': 'uri3',
+                'code': 'SomeProductCode',
+                'attributes': {
+                    'color': 'blue',
+                    'size': 'L',
+                    'ø': 'something'
+                }
+            }])
+            self.assert_(product1)
+
+    def test_0020_product_variation_data(self):
+        """
+        Test get_product_variation_data method.
+        """
+        with Transaction().start(DB_NAME, USER, context=CONTEXT):
+            self.setup_defaults()
+            uom, = self.Uom.search([], limit=1)
+            app = self.get_app()
+
+            with app.test_request_context():
+                # Create attributes
+                attribute1, = self.ProductAttribute.create([{
+                    'name': 'size',
+                    'type_': 'selection',
+                    'string': 'Size',
+                    'selection': 'm: M\nl:L\nxl:XL'
+                }])
+                attribute2, = self.ProductAttribute.create([{
+                    'name': 'color',
+                    'type_': 'selection',
+                    'string': 'Color',
+                    'selection': 'blue: Blue\nblack:Black'
+                }])
+
+                # Create attribute set
+                attrib_set, = self.ProductAttributeSet.create([{
+                    'name': 'Cloth',
+                    'attributes': [
+                        ('add', [attribute1.id, attribute2.id])
+                    ]
+                }])
+
+                # Create product template with attribute set
+                template1, = self.Template.create([{
+                    'name': 'THis is Product',
+                    'type': 'goods',
+                    'list_price': Decimal('10'),
+                    'cost_price': Decimal('5'),
+                    'default_uom': uom.id,
+                    'attribute_set': attrib_set.id,
+                }])
+
+                # Create variation attributes
+                self.VariationAttributes.create([{
+                    'template': template1.id,
+                    'attribute': attribute1.id,
+                }, {
+                    'template': template1.id,
+                    'attribute': attribute2.id,
+                }])
+
+                product1, = self.Product.create([{
+                    'template': template1.id,
+                    'displayed_on_eshop': True,
+                    'uri': 'uri3',
+                    'code': 'SomeProductCode',
+                    'attributes': {
+                        'color': 'blue',
+                        'size': 'L',
+                        'ø': 'something'
+                    }
+                }])
+
+                self.assertGreater(
+                    len(template1.get_product_variation_data()), 0
+                )
+
+    def test_0030_product_variation_data_images(self):
+        """
+        Test get_product_variation_data method for images.
+        """
+        with Transaction().start(DB_NAME, USER, context=CONTEXT):
+            self.setup_defaults()
+            uom, = self.Uom.search([], limit=1)
+            file1 = self.create_static_file(buffer('test'), 'test')
+            file2 = self.create_static_file(buffer('test-again'), 'test-again')
+            app = self.get_app()
+
+            product_template, = self.Template.create([{
+                'name': 'test template',
+                'type': 'goods',
+                'list_price': Decimal('10'),
+                'cost_price': Decimal('5'),
+                'default_uom': uom.id,
+                'description': 'Description of template',
+                'products': [('create', self.Template.default_products())],
+                'media': [('create', [{
+                    'static_file': file1.id,
+                }])],
+            }])
+
+            product, = product_template.products
+
+            self.Product.write([product], {
+                'displayed_on_eshop': True,
+                'uri': 'uri1',
+                'media': [('create', [{
+                    'static_file': file2.id,
+                }])],
+            })
+
+            with app.test_request_context('/'):
+                res = product.get_product_variation_data()
+                self.assertGreater(res, 0)
+
+                self.assertFalse(
+                    res['variants'][0]['image_urls'][0]['thumbnail']
+                    is None
+                )
+                self.assertFalse(
+                    res['variants'][0]['image_urls'][0]['large']
+                    is None
+                )
+                self.assertEqual(
+                    res['variants'][0]['rec_name'], product.rec_name
+                )
+
+
+def suite():
+    """
+    Define suite
+    """
+    test_suite = trytond.tests.test_tryton.suite()
+    test_suite.addTests(
+        unittest.TestLoader().loadTestsFromTestCase(TestProduct)
+    )
+    return test_suite
+
+if __name__ == '__main__':
+    unittest.TextTestRunner(verbosity=2).run(suite())
