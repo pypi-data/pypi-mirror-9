16820f488327bb754e95ee2868a40f642fb9e8a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:51 +0000

    Add package GeobricksGISRaster/0.1.10/GeobricksGISRaster-0.1.10.tar.gz

---
+++ packages/GeobricksGISRaster/GeobricksGISRaster-0.1.10.tar.gz/GeobricksGISRaster-0.1.10/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+from setuptools import find_packages
+
+setup(
+    name='GeobricksGISRaster',
+    version='0.1.10',
+    author='Simone Murzilli; Guido Barbaglia',
+    author_email='geobrickspy@gmail.com',
+    packages=find_packages(),
+    license='LICENSE.txt',
+    long_description=open('README.md').read(),
+    description='Geobricks library to process raster layers.',
+    install_requires=[
+        'watchdog',
+        'flask',
+        'flask-cors',
+        'rasterio',
+        # TODO: how to handle the version?? (This is need for RedHat)
+        #'GDAL=1.9.1',
+        #'pygdal', #this will throw an error because is based on the currently installed gdal version
+        # this is not a real dependendency, it's just passed on the method
+        'GeobricksCommon',
+        'GeobricksProj4ToEPSG'
+    ],
+    url='http://pypi.python.org/pypi/GeobricksGISRaster/',
+    keywords=['geobricks', 'geoserver', 'gis', 'raster']
+)
