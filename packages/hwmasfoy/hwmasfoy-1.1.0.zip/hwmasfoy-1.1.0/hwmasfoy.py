bd7512c4f2a1612c5f693cbee218e57ee9ef61bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:56 +0000

    Add package hwmasfoy/1.1.0/hwmasfoy-1.1.0.zip

---
+++ packages/hwmasfoy/hwmasfoy-1.1.0.zip/hwmasfoy-1.1.0/hwmasfoy.py
@@ -0,0 +1,30 @@
+
+
+""" this is helloworld project from Mahfuz"""
+
+
+sentence = "All good things come to those who wait."
+
+def callme(par):
+	x = 22+3+par 
+	list1 = {1,2,3,4} 
+	
+	for i in list1:
+		print i;	
+		
+	print x	
+	
+	return x
+	
+
+def splitsent(sent): 
+	sent1 = sent.split(' ') 
+	return sent1
+
+def sortwords(sent): 
+	sent1 = sorted(sent) 
+	return sent1
+
+
+
+
