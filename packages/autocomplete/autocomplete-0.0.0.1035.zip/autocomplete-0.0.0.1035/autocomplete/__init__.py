87dfc29b0c244057dc29ab2458a0291dd7bb1612
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:04 +0000

    Add package autocomplete/0.0.0.1035/autocomplete-0.0.0.1035.zip

---
+++ packages/autocomplete/autocomplete-0.0.0.1035.zip/autocomplete-0.0.0.1035/autocomplete/__init__.py
@@ -0,0 +1,37 @@
+import os
+
+import bottle
+
+from bottle import route, run, request, template, view
+
+import autocomplete
+
+from autocomplete import models, helpers
+
+"""load the classic Norvig big.txt corpus"""
+
+def run_server(port_num=8080):
+
+    models.load_models()
+    bottle.TEMPLATE_PATH.insert(0, os.path.join(os.path.dirname(__file__),'views'))
+    bottle.debug(True)
+
+    @route('/<wordA>/<wordB>')
+    def index(wordA, wordB):
+        return  dict(autocomplete.predict(wordA, wordB))
+
+    @route('/bigcorpus')
+    @view('bigcorpus')
+    def bigcorpus():
+        return {'suggestions':models.WORDS_MODEL}
+
+    run(host='localhost', port=port_num)
+
+
+if __name__ == "__main__":
+    """load the classic Norvig big.txt corpus"""
+    print("training!")
+
+    models.load_models()
+
+    print("done training!")
