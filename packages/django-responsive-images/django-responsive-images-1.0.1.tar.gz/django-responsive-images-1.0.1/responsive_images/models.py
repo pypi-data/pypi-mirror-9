3d847c68607d0eaff947f810bfe5e1a62d197eb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:34 +0000

    Add package django-responsive-images/1.0.1/django-responsive-images-1.0.1.tar.gz

---
+++ packages/django-responsive-images/django-responsive-images-1.0.1.tar.gz/django-responsive-images-1.0.1/responsive_images/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+
+
+class ImageModel(models.Model):
+    width = models.PositiveIntegerField()
+    height = models.PositiveIntegerField()
+    
+    class Meta:
+        abstract = True
+    
+    @property
+    def size(self):
+        return (self.width, self.height)
+
+
+class OriginalImage(ImageModel):
+    image_file = models.ImageField(
+        height_field='height',
+        width_field='width',
+        db_index=True
+    )
+
+
+class ResizedImage(ImageModel):
+    original = models.ForeignKey(OriginalImage, db_index=True)
+    image_file = models.ImageField(
+        upload_to='responsive_images',
+        height_field='height',
+        width_field='width',
+    )
+    crop = models.CharField(max_length=10)
+    
+    class Meta:
+        unique_together = ('original', 'width', 'height', 'crop')
