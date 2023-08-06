46a8315e1b39a20f7f5985e7f0bd66ff90943a3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:24 +0000

    Add package soldier/0.0.1.4/soldier-0.0.1.4.tar.gz

---
+++ packages/soldier/soldier-0.0.1.4.tar.gz/soldier-0.0.1.4/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='soldier',
+    version='0.0.1.4',
+    author='Yash Mehrotra',
+    author_email='yashmehrotra95@gmail.com',
+    packages=['soldier'],
+    scripts=[],
+    url='https://pypi.python.org/pypi/soldier',
+    license='LICENSE',
+    description='Subprocess with all guns blazing',
+    long_description=open('README.txt').read(),
+    install_requires=[],
+)
