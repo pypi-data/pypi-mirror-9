09b6094d1e2ddba0245a9006ed1570b7f2115416
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:37 +0000

    Add package httputil/0.2/httputil-0.2.tar.gz

---
+++ packages/httputil/httputil-0.2.tar.gz/httputil-0.2/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='httputil',
+    packages=['httputil'],
+    version='0.2',
+    description='Utilities to deal with HTTP data',
+    license='Public Domain',
+    author='Vovan Kuznetsov',
+    author_email='vovanec@gmail.com',
+    url='https://github.com/vovanec/httputil',
+    download_url='https://github.com/vovanec/httputil/tarball/0.2',
+    keywords=['http', 'dechunk', 'decompress'],
+    classifiers=[],
+)
