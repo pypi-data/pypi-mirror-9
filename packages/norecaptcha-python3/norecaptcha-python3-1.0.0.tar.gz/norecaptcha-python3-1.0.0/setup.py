99847cf43400cc3a61368eaa3ce9e8f57f74c482
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:15 +0000

    Add package norecaptcha-python3/1.0.0/norecaptcha-python3-1.0.0.tar.gz

---
+++ packages/norecaptcha-python3/norecaptcha-python3-1.0.0.tar.gz/norecaptcha-python3-1.0.0/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+
+setup(
+    name='norecaptcha-python3',
+    version='1.0.0',
+    packages=['norecaptcha3'],
+    license='MIT',
+    author='Zeioth',
+    author_email='test@gmail.com',
+    description='Python 3 server-side library for google No CAPTCHA reCAPTCHA services.',
+    long_description='Python 3 server-side library for google No CAPTCHA reCAPTCHA services.',
+    install_requires='',
+    include_package_data=True,
+    url='https://github.com/Zeioth/norecaptcha-python3',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ]
+)
