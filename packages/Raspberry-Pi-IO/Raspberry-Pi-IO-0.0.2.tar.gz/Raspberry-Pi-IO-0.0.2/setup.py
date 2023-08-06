6f8b9efe78e1a3a473b6efc5da085fa3dc2e85e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:46 +0000

    Add package Raspberry-Pi-IO/0.0.2/Raspberry-Pi-IO-0.0.2.tar.gz

---
+++ packages/Raspberry-Pi-IO/Raspberry-Pi-IO-0.0.2.tar.gz/Raspberry-Pi-IO-0.0.2/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(
+    name='Raspberry-Pi-IO',
+    version='0.0.2',
+    author='Brian Hines',
+    author_email='brian@projectweekend.net',
+    packages=['raspberry_pi_io'],
+    url='https://github.com/exitcodezero/raspberry-pi-io',
+    license='LICENSE.txt',
+    description='A Python service for remotely controlling GPIO.',
+    long_description=open('README.txt').read(),
+    install_requires=[
+        "pika == 0.9.14",
+        "requests == 2.6.0",
+    ],
+)
