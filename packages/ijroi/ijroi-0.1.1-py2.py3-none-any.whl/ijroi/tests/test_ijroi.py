77d74a9d2b9c78b34f001123e9237800d84774ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:26 +0000

    Add package ijroi/0.1.1/ijroi-0.1.1-py2.py3-none-any.whl

---
+++ packages/ijroi/ijroi-0.1.1-py2.py3-none-any.whl/ijroi/tests/test_ijroi.py
@@ -0,0 +1,42 @@
+import os
+
+import numpy as np
+import py
+import pytest
+
+import ijroi
+
+def get_fixture(name):
+    dirpath = py.path.local(__file__).dirpath()
+    return dirpath.join("fixtures", name)
+
+def test_ijroi_import():
+    assert ijroi.__version__
+
+def test_rectangle():
+    fixture = get_fixture("subpixel_rectangle.roi")
+    with fixture.open("rb") as f:
+        rect = ijroi.read_roi(f)
+    assert (rect == np.array([[5, 4], [5, 8], [10, 8], [10, 4]])).all()
+    assert rect.dtype == np.float32
+
+    fixture = get_fixture("integer_rectangle.roi")
+    with fixture.open("rb") as f:
+        rect = ijroi.read_roi(f)
+    assert (rect == np.array([[5, 4], [5, 8], [10, 8], [10, 4]])).all()
+    assert rect.dtype == np.int16
+
+def test_freehand_circle():
+    fixture = get_fixture("freehand_circle.roi")
+    with fixture.open("rb") as f:
+        circle = ijroi.read_roi(f)
+    assert abs(circle[:, 1].mean()-10) < 0.01
+    assert abs(circle[:, 0].mean()-15) < 0.01
+
+def test_integer_freehand():
+    fixture = get_fixture("freehand_integer.roi")
+    with fixture.open("rb") as f:
+        freehand = ijroi.read_roi(f)
+    assert len(freehand) == 3
+    assert all(freehand[2, :] == [1, 10])
+    assert freehand.dtype == np.int16
