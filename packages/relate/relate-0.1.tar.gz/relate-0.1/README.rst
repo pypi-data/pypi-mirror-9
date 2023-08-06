50b031abc85bf427921cab31f05d9235ed04e8c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:24 +0000

    Add package relate/0.1/relate-0.1.tar.gz

---
+++ packages/relate/relate-0.1.tar.gz/relate-0.1/README.rst
@@ -0,0 +1,15 @@
+Relation
+--------
+A python class implementing a discrete, mathematical relationship.  Supports all cardinalities:
+
+- 1:1
+- 1:M (and its inverse M:1)
+- M:M (or M:N if you insist)
+
+Useful for all manners of invertible container goodness, including:
+
+- aliasing (1:1)
+- tagging (M:M)
+- partitioning (1:M)
+- assignment (M:1)
+
---
+++ packages/relate/relate-0.1.tar.gz/relate-0.1/setup.py
@@ -0,0 +1,25 @@
+"""Distutils file for relate"""
+
+from setuptools import setup
+from src import __version__
+
+setup(
+    name         = 'relate',
+    version      = __version__,
+    description  = 'Discrete mathematical relation',
+    author       = 'Scott Howard James',
+    author_email = 'scott.analysis.james@gmail.com',
+    url='https://github.com/scott-howard-james/relate.git',
+    license      = 'MIT',
+    long_description = open('README.rst').read(),
+    package_dir  = {'relate' : 'src'},
+    packages     = ['relate'],
+    classifiers  = [
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: Freeware',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        ],
+    )
