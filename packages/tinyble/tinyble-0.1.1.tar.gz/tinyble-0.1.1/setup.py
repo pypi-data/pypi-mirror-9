c93a9cebbee68a08dbbcffcbe7ee8833e346851e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:30 +0000

    Add package tinyble/0.1.1/tinyble-0.1.1.tar.gz

---
+++ packages/tinyble/tinyble-0.1.1.tar.gz/tinyble-0.1.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='tinyble',
+      version='0.1.1',
+      description='A tiny NoSQL database with in-memory caching',
+      url='https://github.com/StevenSLXie/tinyble',
+      author='Steven S.L. Xie',
+      author_email='stevenslxie@gmail.com',
+      license='MIT',
+      packages=['tinyble'],
+      zip_safe=False)
