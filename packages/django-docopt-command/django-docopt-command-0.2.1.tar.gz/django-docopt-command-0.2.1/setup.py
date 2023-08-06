897b05c0184787c25c748e1d31fb3e9ea3349348
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:01 +0000

    Add package django-docopt-command/0.2.1/django-docopt-command-0.2.1.tar.gz

---
+++ packages/django-docopt-command/django-docopt-command-0.2.1.tar.gz/django-docopt-command-0.2.1/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+
+version = '0.2.1'
+
+setup(
+    name='django-docopt-command',
+    version=version,
+    packages=find_packages(),
+    license='Apache License, Version 2.0',
+    include_package_data=True,
+    zip_safe=False,
+    author='Marco Braak',
+    author_email='mbraak@ridethepony.nl',
+    install_requires=['docopt'],
+    description='Django-docopt-command allows you to write Django manage.py commands using the docopt library',
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy"
+    ]
+)
