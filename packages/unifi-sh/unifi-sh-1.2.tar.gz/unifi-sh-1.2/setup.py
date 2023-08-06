afee71971a64b0d9ab710d20957a0d1d10239e6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:00 +0000

    Add package unifi-sh/1.2/unifi-sh-1.2.tar.gz

---
+++ packages/unifi-sh/unifi-sh-1.2.tar.gz/unifi-sh-1.2/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+from commands import getoutput
+
+version = '1.2'
+
+setup(name='unifi-sh',
+      version=version,
+      description='Updated API towards Ubiquity Networks UniFi controller',
+      author='Original: Jakob Borg, forked by Social Hotspot',
+      author_email='info@socialhotspot.nl',
+      url='https://github.com/SocialHotspot/unifi-api',
+      packages=['unifi-sh'],
+     )
