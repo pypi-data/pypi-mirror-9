d349db865453aeef22757ac6df90a8fae1364ab6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:16 +0000

    Add package httmock/1.2.3/httmock-1.2.3.tar.gz

---
+++ packages/httmock/httmock-1.2.3.tar.gz/httmock-1.2.3/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+import os
+
+LICENSE = open(
+    os.path.join(os.path.dirname(__file__), 'LICENSE')).read().strip()
+
+DESCRIPTION = open(
+    os.path.join(os.path.dirname(__file__), 'README.md')).read().strip()
+
+setup(
+    name='httmock',
+    version='1.2.3',
+    description='A mocking library for requests.',
+    author='Patryk Zawadzki',
+    author_email='patrys@room-303.com',
+    url='https://github.com/patrys/httmock',
+    py_modules=['httmock'],
+    keywords=['requests', 'testing', 'mock'],
+    classifiers=[
+        'Programming Language :: Python',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Testing',
+        'Operating System :: OS Independent'],
+    install_requires=['requests >= 1.0.0'],
+    license=LICENSE,
+    long_description=DESCRIPTION,
+    test_suite='tests')
