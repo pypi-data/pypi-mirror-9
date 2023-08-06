42223cafb787ffb43f0fd8d2f61553898544ff03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:52 +0000

    Add package django-skin/0.1/django-skin-0.1.zip

---
+++ packages/django-skin/django-skin-0.1.zip/django-skin-0.1/setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf8
+
+from setuptools import setup, find_packages
+
+setup(
+    name='django-skin',
+    version='0.1',
+    packages=find_packages(),
+    url='https://github.com/dwatkinsweb/django-skin',
+    license='MIT',
+    author='David Watkins',
+    author_email='dwatkinsweb@gmail.com',
+    description='Add template skins to django.',
+    platforms='any',
+    install_requires=[
+        'Django==1.6'
+    ],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+    test_suite='nose.collector',
+    tests_require=[
+        'nose',
+        'mock'
+    ],
+)
