b79cfeb6d131194a296b15a63f943cbb5e2341c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:35 +0000

    Add package ftcbz/2.0.0/ftcbz-2.0.0.tar.gz

---
+++ packages/ftcbz/ftcbz-2.0.0.tar.gz/ftcbz-2.0.0/setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# coding=utf-8
+
+import sys
+from setuptools import setup
+# from src.ftcbz.ftcbz import program_version
+
+if not sys.version_info >= (3, 3, 0):
+    print("ERROR: You cannot install because python version should >= 3.3")
+    sys.exit(1)
+
+setup(
+    name='ftcbz',
+    version='2.0.0',
+    author='Civa Lin',
+    author_email='larinawf@gmail.com',
+    license='MIT',
+    url='https://bitbucket.org/civalin/ftcbz',
+    description="A script to archive multiple comic book dir to .cbz format",
+    long_description='''''',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Topic :: Multimedia :: Graphics :: Graphics Conversion",
+        "Topic :: System :: Archiving"],
+    install_requires=[],
+    setup_requires=[],
+    package_dir={'': 'src'},
+    packages=['ftcbz'],
+    entry_points={
+        'console_scripts': ['ftcbz = ftcbz.ftcbz:main'],
+        'setuptools.installation': ['eggsecutable = ftcbz.ftcbz:main']
+        },
+    keywords='cbz comic archive',
+    )
