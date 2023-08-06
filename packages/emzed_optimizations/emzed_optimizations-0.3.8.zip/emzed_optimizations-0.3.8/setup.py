e8f63523f94869d95ba0a3c34797f6c75f858771
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:34 +0000

    Add package emzed_optimizations/0.3.8/emzed_optimizations-0.3.8.zip

---
+++ packages/emzed_optimizations/emzed_optimizations-0.3.8.zip/emzed_optimizations-0.3.8/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+from distutils.extension import Extension
+import numpy
+
+ext_modules = [
+    Extension("emzed_optimizations.sample",
+              ["emzed_optimizations/sample.c"],
+
+              )
+]
+
+version = "0.3.8"
+
+setup(name="emzed_optimizations",
+      version=version,
+      author="Uwe Schmitt",
+      author_email="mail@uweschmitt.info",
+      description="particular optimizations for speeding up emzed",
+      license="BSD",
+      url="http://github.com/uweschmitt/emzed_optimizations",
+      packages=["emzed_optimizations"],
+      zip_safe=False,
+      include_dirs=[numpy.get_include()],
+      ext_modules=ext_modules)
