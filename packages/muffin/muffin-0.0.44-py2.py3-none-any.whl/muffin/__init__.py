a68cfc155ab3fcd89758b8760bb56a696c9cca79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:21 +0000

    Add package muffin/0.0.44/muffin-0.0.44-py2.py3-none-any.whl

---
+++ packages/muffin/muffin-0.0.44-py2.py3-none-any.whl/muffin/__init__.py
@@ -0,0 +1,21 @@
+"""
+    muffin description.
+
+"""
+
+# Package information
+# ===================
+
+__version__ = "0.0.44"
+__project__ = "muffin"
+__author__ = "Kirill Klenov <horneds@gmail.com>"
+__license__ = "MIT"
+
+from asyncio import *           # noqa
+from aiohttp.web import *       # noqa
+
+CONFIGURATION_ENVIRON_VARIABLE = 'MUFFIN_CONFIG'
+
+from .app import Application    # noqa
+from .handler import Handler    # noqa
+from .utils import to_coroutine # noqa
