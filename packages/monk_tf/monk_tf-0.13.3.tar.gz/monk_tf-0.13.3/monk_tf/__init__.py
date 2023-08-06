5287fa8335911a9657c929b51d6f632b798e64ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:10 +0000

    Add package monk_tf/0.13.3/monk_tf-0.13.3.tar.gz

---
+++ packages/monk_tf/monk_tf-0.13.3.tar.gz/monk_tf-0.13.3/monk_tf/__init__.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2012, 2013 DResearch Fahrzeugelektronik GmbH
+# Maintained by project-monk@dresearch-fe.de
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+"""
+This is the package overview of :term:`MONK`. If anything is unclear, you might
+have a look into :ref:`chap-intro`.
+
+The following texts describe the three layers that were explained in
+:ref:`intro-layers`:
+"""
+
+__title__ = "monk_tf"
+__version__ = "0.13.3"
+__author__ = "DResearch Fahrzeugelektronik GmbH"
+__license__ = "GPLv3+"
+__copyright__ = "Copyright 2012, 2013, 2014 DResearch Fahrzeugelektronik GmbH"
