999336c432f6287e2422f64081f6206bbd03122c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:29:54 +0000

    Add package serpy/0.0.2/serpy-0.0.2-py2.py3-none-any.whl

---
+++ packages/serpy/serpy-0.0.2-py2.py3-none-any.whl/serpy/fields.py
@@ -0,0 +1,128 @@
+from operator import attrgetter
+import six
+import types
+
+
+class Field(object):
+    """:class:`Field` is used to define what attributes will be serialized.
+
+    A :class:`Field` maps a property or function on an object to a value in the
+    serialized result. Subclass this to make custom fields. For most simple
+    cases, overriding :meth:`Field.transform_value` should give enough
+    flexibility. If more control is needed, override :meth:`Field.as_getter`.
+
+    :param str attr: The attribute to get on the object, using the same format
+        as ``operator.attrgetter``. If this is not supplied, the name this
+        field was assigned to on the serializer will be used.
+    :param bool call: Whether the value should be called after it is retrieved
+        from the object. Useful if an object has a method to be serialized.
+    """
+    #: Set to ``True`` if the value function returned from
+    #: :meth:`Field.as_getter` requires the serializer to be passed in as the
+    #: first argument. Otherwise, the object will be the only parameter.
+    getter_takes_serializer = False
+
+    def __init__(self, attr=None, call=False):
+        self.attr = attr
+        self.call = call
+
+    def transform_value(self, value):
+        """Transform the serialized value.
+
+        Override this method to clean and validate values serialized by this
+        field. For example to implement an ``int`` field: ::
+
+            def transform_value(self, value):
+                return int(value)
+
+        :param value: The value fetched from the object being serialized.
+        """
+        return value
+    transform_value._serpy_base_implementation = True
+
+    def _is_transform_value_overriden(self):
+        transform = self.transform_value
+        # If transform isn't a method, it must have been overriden.
+        if not isinstance(transform, types.MethodType):
+            return True
+        return not getattr(transform, '_serpy_base_implementation', False)
+
+    def as_getter(self, serializer_field_name, serializer_cls):
+        """Returns a function that fetches an attribute from an object.
+
+        When a :class:`Serializer` is defined, each :class:`Field` will be
+        converted into a getter function using this method. During
+        serialization, each getter will be called with the object being
+        serialized, and the return value will be passed through
+        :meth:`Field.transform_value`.
+
+        If a :class:`Field` has ``getter_takes_serializer = True``, then the
+        getter returned from this method will be called with the
+        :class:`Serializer` instance as the first argument, and the object
+        being serialized as the second.  Otherwise the getter will be called
+        with the object as the only argument.
+
+        :param str serializer_field_name: The name this field was assigned to
+            on the serializer.
+        :param serializer_cls: The :class:`Serializer` this field is a part of.
+        """
+        attr_name = self.attr
+        if attr_name is None:
+            attr_name = serializer_field_name
+        return attrgetter(attr_name)
+
+
+class StrField(Field):
+    """A :class:`Field` that converts the value to a string."""
+    transform_value = staticmethod(six.text_type)
+
+
+class IntField(Field):
+    """A :class:`Field` that converts the value to an integer."""
+    transform_value = staticmethod(int)
+
+
+class FloatField(Field):
+    """A :class:`Field` that converts the value to a float."""
+    transform_value = staticmethod(float)
+
+
+class BoolField(Field):
+    """A :class:`Field` that converts the value to a boolean."""
+    transform_value = staticmethod(bool)
+
+
+class MethodField(Field):
+    """A :class:`Field` that calls a method on the :class:`Serializer`.
+
+    This is useful if a :class:`Field` needs to serialize a value that may come
+    from multiple attributes on an object. For example: ::
+
+        class FooSerializer(Serializer):
+            plus = MethodField()
+            minus = MethodField('do_minus')
+
+            def get_plus(self, foo_obj):
+                return foo_obj.bar + foo_obj.baz
+
+            def do_minus(self, foo_obj):
+                return foo_obj.bar - foo_obj.baz
+
+        foo = Foo(bar=5, baz=10)
+        FooSerializer(foo).data
+        # {'plus': 15, 'minus': -5}
+
+    :param str method: The method on the serializer to call. Defaults to
+        ``'get_<field name>'``.
+    """
+    getter_takes_serializer = True
+
+    def __init__(self, method=None, **kwargs):
+        super(MethodField, self).__init__(**kwargs)
+        self.method = method
+
+    def as_getter(self, serializer_field_name, serializer_cls):
+        method_name = self.method
+        if method_name is None:
+            method_name = 'get_{0}'.format(serializer_field_name)
+        return getattr(serializer_cls, method_name)
---
+++ packages/serpy/serpy-0.0.2-py2.py3-none-any.whl/serpy/serializer.py
@@ -0,0 +1,117 @@
+from serpy.fields import Field
+import six
+
+
+class SerializerBase(Field):
+    _field_map = {}
+
+
+def _compile_field_to_tuple(field, name, serializer_cls):
+    getter = field.as_getter(name, serializer_cls)
+
+    # Only set a transform function if it has been overriden for performance.
+    transform = None
+    if field._is_transform_value_overriden():
+        transform = field.transform_value
+
+    return name, getter, transform, field.call, field.getter_takes_serializer
+
+
+class SerializerMeta(type):
+
+    @staticmethod
+    def _get_fields(direct_fields, serializer_cls):
+        field_map = {}
+        # Get all the fields from base classes.
+        for cls in serializer_cls.__mro__[::-1]:
+            if issubclass(cls, SerializerBase):
+                field_map.update(cls._field_map)
+        field_map.update(direct_fields)
+
+        compiled_fields = [
+            _compile_field_to_tuple(field, name, serializer_cls)
+            for name, field in field_map.items()
+        ]
+
+        return field_map, compiled_fields
+
+    def __new__(cls, name, bases, attrs):
+        # Fields declared directly on the class.
+        direct_fields = {}
+
+        # Take all the Fields from the attributes.
+        for attr_name, field in attrs.items():
+            if isinstance(field, Field):
+                direct_fields[attr_name] = field
+        for k in direct_fields.keys():
+            del attrs[k]
+
+        real_cls = super(SerializerMeta, cls).__new__(cls, name, bases, attrs)
+
+        field_map, compiled_fields = cls._get_fields(direct_fields, real_cls)
+
+        real_cls._field_map = field_map
+        real_cls._compiled_fields = tuple(compiled_fields)
+        return real_cls
+
+
+class Serializer(six.with_metaclass(SerializerMeta, SerializerBase)):
+    """:class:`Serializer` is used as a base for custom serializers.
+
+    The :class:`Serializer` class is also a subclass of :class:`Field`, and can
+    be used as a :class:`Field` to create nested schemas. A serializer is
+    defined by subclassing :class:`Serializer` and adding each :class:`Field`
+    as a class variable:
+
+    Example: ::
+
+        class FooSerializer(Serializer):
+            foo = Field()
+            bar = Field()
+
+        foo = Foo(foo='hello', bar=5)
+        FooSerializer(foo).data
+        # {'foo': 'hello', 'bar': 5}
+
+    :param obj: The object or objects to serialize.
+    :param bool many: If ``obj`` is a collection of objects, set ``many`` to
+        ``True`` to serialize to a list.
+    """
+    def __init__(self, obj=None, many=False, **kwargs):
+        super(Serializer, self).__init__(**kwargs)
+        self.obj = obj
+        self.many = many
+        self._data = None
+
+    def _to_value(self, obj, fields):
+        v = {}
+        for name, getter, transform, call, getter_takes_serializer in fields:
+            if getter_takes_serializer:
+                result = getter(self, obj)
+            else:
+                result = getter(obj)
+                if call:
+                    result = result()
+                if transform:
+                    result = transform(result)
+            v[name] = result
+
+        return v
+
+    def transform_value(self, obj):
+        fields = self._compiled_fields
+        if self.many:
+            to_value = self._to_value
+            return [to_value(o, fields) for o in obj]
+        return self._to_value(obj, fields)
+
+    @property
+    def data(self):
+        """Get the serialized data from the :class:`Serializer`.
+
+        The data will be cached for future accesses.
+        """
+        # Cache the data for next time .data is called.
+        if self._data is None:
+            self._data = self.transform_value(self.obj)
+        return self._data
