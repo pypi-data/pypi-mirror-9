402831954bc6e81d2adba60217575544629fb12d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:21 +0000

    Add package scrubadub/0.0.1/scrubadub-0.0.1.tar.gz

---
+++ packages/scrubadub/scrubadub-0.0.1.tar.gz/scrubadub-0.0.1/scrubadub/__init__.py
@@ -0,0 +1,15 @@
+
+# convenient imports
+from .scrubbers import Scrubber
+
+
+__version__ = VERSION = "0.0.1"
+
+
+def clean_with_placeholders(text, cls=None):
+    """Public facing function to clean ``text`` using the scrubber ``cls`` by
+    replacing all personal information with ``{{PLACEHOLDERS}}``.
+    """
+    cls = cls or Scrubber
+    scrubber = cls()
+    return scrubber.clean_with_placeholders(text)
