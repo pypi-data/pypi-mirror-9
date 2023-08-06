a89e923b42b1894efc0248f748cbe076c187a53c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:12 +0000

    Add package django-djconfig/0.3.1.1/django-djconfig-0.3.1.1.tar.gz

---
+++ packages/django-djconfig/django-djconfig-0.3.1.1.tar.gz/django-djconfig-0.3.1/djconfig/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+
+from .conf import config
+from .registry import register
+
+__version__ = "0.3.1"
+__all__ = ['config', 'register']
