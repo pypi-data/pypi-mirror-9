18e30943cf35e79760867b481608f15e7a0d9147
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:42 +0000

    Add package Mopidy-dLeyna/0.3.0/Mopidy_dLeyna-0.3.0-py2.py3-none-any.whl

---
+++ packages/Mopidy-dLeyna/Mopidy_dLeyna-0.3.0-py2.py3-none-any.whl/mopidy_dleyna/dleyna.py
@@ -0,0 +1,11 @@
+from __future__ import absolute_import, unicode_literals
+
+MANAGER_IFACE = 'com.intel.dLeynaServer.Manager'
+
+SERVER_BUS_NAME = 'com.intel.dleyna-server'
+
+SERVER_ROOT_PATH = '/com/intel/dLeynaServer'
+
+ALBUM_TYPE_EX = 'container.album.musicAlbum'
+
+ARTIST_TYPE_EX = 'container.person.musicArtist'
