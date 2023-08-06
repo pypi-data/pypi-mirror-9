24a52fdbcba8d4bd237334d893d3d64b6d8e3249
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:07:45 +0000

    Add package graphite.theme/1.1.1/graphite.theme-1.1.1.tar.gz

---
+++ packages/graphite.theme/graphite.theme-1.1.1.tar.gz/graphite.theme-1.1.1/setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.1.1'
+
+setup(name='graphite.theme',
+      version=version,
+      description="Graphite Theme for Bika LIMS",
+      long_description=open("README.md").read() + "\n\n" +
+                       open("docs/CHANGELOG.txt").read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?:action=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        ],
+      keywords=['diazo', 'theme', 'lims', 'bika', 'opensource'],
+      author='Naralabs',
+      author_email='info@naralabs.com',
+      url='http://github.com/naralabs/graphite.theme',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['graphite'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'bika.lims==3.1.7',
+          'plone.app.theming',
+      ],
+      extras_require={
+          'test': ['plone.app.testing',]
+      },
+      entry_points="""
+      # -*- Entry points: -*-
+
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      #setup_requires=["PasteScript"],
+      #paster_plugins=["ZopeSkel"],
+      )
