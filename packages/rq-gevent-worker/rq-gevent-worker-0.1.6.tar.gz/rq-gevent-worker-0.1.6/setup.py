e934dcb04c55fb84d558cea721f57d4751431f9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:36 +0000

    Add package rq-gevent-worker/0.1.6/rq-gevent-worker-0.1.6.tar.gz

---
+++ packages/rq-gevent-worker/rq-gevent-worker-0.1.6.tar.gz/rq-gevent-worker-0.1.6/setup.py
@@ -0,0 +1,43 @@
+"""
+rq-gevent-worker
+================
+
+Implement a new worker based on gevent
+
+Install
+-------
+
+.. code-block:: bash
+
+    $ pip install rq-gevent-worker
+
+Usage
+-----
+
+.. code-block:: bash
+
+    $ rqgeventworker -h
+
+    $ export PYTHONPATH=<your project import path>:$PYTHONPATH; rqgeventworker
+
+For more information: https://github.com/zhangliyong/rq-gevent-worker
+"""
+from setuptools import setup
+
+setup(
+    name='rq-gevent-worker',
+    version='0.1.6',
+    py_modules=['rq_gevent_worker'],
+    entry_points={
+        'console_scripts': [
+            'rqgeventworker=rq_gevent_worker:main',
+            ]
+        },
+    url='https://github.com/zhangliyong/rq-gevent-worker',
+    license='BSD',
+    author='Lyon Zhang',
+    author_email='lyzhang87@gmail.com',
+    description='Implement a new worker based on gevent',
+    long_description=__doc__,
+    install_requires=['rq >= 0.4.6', 'gevent >= 1.0'],
+)
