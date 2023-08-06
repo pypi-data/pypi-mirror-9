abfc8d4feef2fa6b889a699072d5405f282447ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:38 +0000

    Add package i3-dstatus/0.0.1/i3-dstatus-0.0.1.tar.gz

---
+++ packages/i3-dstatus/i3-dstatus-0.0.1.tar.gz/i3-dstatus-0.0.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='i3-dstatus',
+    version='0.0.1',
+    author='Tony Crisci',
+    author_email='tony@dubstepdish.com',
+    url='https://github.com/acrisci/i3-dstatus',
+    license='BSD',
+    description='The ultimate DIY statusline generator for i3',
+    long_description=open('README.rst').read(),
+
+    install_requires=['dbus-python', 'yaml', 'netifaces', 'i3ipc', 'ijson'],
+
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 3',
+    ],
+
+    package_data={'i3dstatus': ['generators/*']},
+
+    scripts=['i3-dstatus'],
+
+    packages=find_packages(),
+)
