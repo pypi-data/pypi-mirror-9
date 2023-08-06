4322ef84e6daed35f75c7802e54adb6d1dc02484
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:34 +0000

    Add package fido/1.0.0/fido-1.0.0.tar.gz

---
+++ packages/fido/fido-1.0.0.tar.gz/fido-1.0.0/fido/__about__.py
@@ -0,0 +1,15 @@
+__all__ = [
+    "__title__", "__summary__", "__uri__", "__version__", "__author__",
+    "__email__", "__license__"
+]
+
+__title__ = "fido"
+__summary__ = "Intelligent asynchronous HTTP client"
+__uri__ = "https://github.com/Yelp/fido"
+
+__version__ = "1.0.0"
+
+__author__ = "John Billings"
+__email__ = "billings@yelp.com"
+
+__license__ = "Copyright Yelp 2014, All Rights Reserved"
