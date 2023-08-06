37b1b0b06be65d457ce1f722ce57f09c81d25080
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:43 +0000

    Add package python-odesk/0.5.7/python-odesk-0.5.7.tar.gz

---
+++ packages/python-odesk/python-odesk-0.5.7.tar.gz/python-odesk-0.5.7/odesk/routers/__init__.py
@@ -0,0 +1,11 @@
+# Python bindings to oDesk API
+# python-odesk version 0.5
+# (C) 2010-2015 oDesk
+"""Routers package.
+
+All communication with oDesk API is handled by the routers.
+
+When you import ``odesk`` package and instantiate ``odesk.Client`` class,
+routers are dynamically attatched to your ``client`` object.
+
+"""
