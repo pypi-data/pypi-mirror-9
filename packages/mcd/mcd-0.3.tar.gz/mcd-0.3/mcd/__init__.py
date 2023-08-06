f322434b4890eba6ee1591e121079e780febbe2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:58 +0000

    Add package mcd/0.3/mcd-0.3.tar.gz

---
+++ packages/mcd/mcd-0.3.tar.gz/mcd-0.3/mcd/__init__.py
@@ -0,0 +1,9 @@
+"""Mel cepstral distortion (MCD) computations in python."""
+
+# Copyright 2014, 2015 Matt Shannon
+
+# This file is part of mcd.
+# See `License` for details of license and warranty.
+
+
+__version__ = '0.3'
