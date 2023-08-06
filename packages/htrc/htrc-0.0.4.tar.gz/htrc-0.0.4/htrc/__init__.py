6548f02b86fb8a97d071d1c0e61a4783ecbd0ca6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:10 +0000

    Add package htrc/0.0.4/htrc-0.0.4.tar.gz

---
+++ packages/htrc/htrc-0.0.4.tar.gz/htrc-0.0.4/htrc/__init__.py
@@ -0,0 +1,3 @@
+__author__ = 'Milinda Pathirage'
+
+__all__ = ["dataapi"]
---
+++ packages/htrc/htrc-0.0.4.tar.gz/htrc-0.0.4/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name = 'htrc',
+    packages = ['htrc'],
+    version = '0.0.4',
+    description = 'Set of Python Libraries for HTRC APIs.',
+    author='Milinda Pathirage',
+    author_email='milinda.pathirage@gmail.com',
+    url='https://github.com/milinda/htrc-sdk-for-python',
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Development Status :: 1 - Planning',
+        'Environment :: Console',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Information Analysis'
+    ]
+)
