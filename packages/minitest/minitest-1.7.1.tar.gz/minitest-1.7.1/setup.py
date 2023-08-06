4d9e1e31dda72e75a5ae4896972c8e296582a349
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:08 +0000

    Add package minitest/1.7.1/minitest-1.7.1.tar.gz

---
+++ packages/minitest/minitest-1.7.1.tar.gz/minitest-1.7.1/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='minitest',
+    version='1.7.1',
+    author='Colin Ji',
+    author_email='jichen3000@gmail.com',
+    packages=['minitest'],
+    url='https://pypi.python.org/pypi/minitest',
+    description='Minitest is inspired by Ruby minispec.',
+    long_description=open('README.txt').read(),
+)
