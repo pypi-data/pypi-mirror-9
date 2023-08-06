6f17b6540439049fc086bfd52ea05dc3645339fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:23:05 +0000

    Add package medialog.fullpagetheme/0.2/medialog.fullpagetheme-0.2.zip

---
+++ packages/medialog.fullpagetheme/medialog.fullpagetheme-0.2.zip/medialog.fullpagetheme-0.2/README.txt
@@ -0,0 +1,10 @@
+Introduction
+============
+
+A diazo theme, making 
+"Full folder view" like
+http://alvarotrigo.com/fullPage/
+
+(under development)
+
+Currently: it only works for collections
---
+++ packages/medialog.fullpagetheme/medialog.fullpagetheme-0.2.zip/medialog.fullpagetheme-0.2/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.2'
+
+setup(name='medialog.fullpagetheme',
+      version=version,
+      description="FullpageTheme",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='',
+      author='Espen Moe-Nilssen',
+      author_email='espen@medialog.no',
+      url='https://github.com/espenmn/medialog.fullpagetheme',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['medialog'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'z3c.jbot',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
