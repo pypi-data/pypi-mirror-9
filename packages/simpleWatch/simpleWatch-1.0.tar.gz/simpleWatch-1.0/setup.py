0eb359b04b4573d6de17fa0b64b76d902ef76e09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:46 +0000

    Add package simpleWatch/1.0/simpleWatch-1.0.tar.gz

---
+++ packages/simpleWatch/simpleWatch-1.0.tar.gz/simpleWatch-1.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+setup(
+    name='simpleWatch',
+    packages=['src'],
+    version='1.0',
+    description='Extemely simple file watcher',
+    author='Marcus Buffett',
+    author_email='marcusbuffett@me.com',
+    url='https://github.com/marcusbuffett/simpleWatch/tree/master',
+    download_url='',
+    entry_points={
+        'console_scripts': [
+            'swatch = src.main:main',
+        ],
+    },
+    keywords=['file-watcher, watch, file'],
+    classifiers=["Programming Language :: Python :: 3"],
+)
