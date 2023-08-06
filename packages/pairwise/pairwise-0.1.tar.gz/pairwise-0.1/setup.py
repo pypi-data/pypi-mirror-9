e5e929d46ddeae89a497bd78c9f7721825aeda9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:52 +0000

    Add package pairwise/0.1/pairwise-0.1.tar.gz

---
+++ packages/pairwise/pairwise-0.1.tar.gz/pairwise-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pairwise',
+      version='0.1',
+      description='Tools for statistical analysis of pairwise survey data.',
+      url='https://github.com/davefol/pairwise',
+      author='Dave Fol',
+      author_email='dof5@cornell.edu',
+      license='GNU',
+      packages=['pairwise'],
+      zip_safe=False)
