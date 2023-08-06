5a8554629746f8337c5e90ecd2935afb8c56d4ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:37:07 +0000

    Add package moya/0.5.10/moya-0.5.10.tar.gz

---
+++ packages/moya/moya-0.5.10.tar.gz/moya-0.5.10/moya/command/sub/serve.py
@@ -0,0 +1,70 @@
+from ...command import SubCommand
+from ...wsgi import WSGIApplication
+from ...loggingconf import init_logging
+from ...compat import PY2
+
+from fs.opener import fsopendir
+import os.path
+from os.path import join as pathjoin
+
+import sys
+from wsgiref.simple_server import WSGIRequestHandler, make_server
+
+
+if PY2:
+    from thread import interrupt_main
+else:
+    from _thread import interrupt_main
+
+
+import logging
+log = logging.getLogger('moya.runtime')
+
+
+class RequestHandler(WSGIRequestHandler):
+
+    # Disable simple_server's logging to stdout
+    def log_message(self, format, *args):
+        pass
+
+
+class Serve(SubCommand):
+    """Serve static files with moya.static"""
+    help = "serve static files"
+
+    def add_arguments(self, parser):
+        parser.add_argument(dest="fs", metavar="PATH",
+                            help="Path to serve")
+        parser.add_argument('--host', dest='host', default='127.0.0.1',
+                            help="Host")
+        parser.add_argument('-p', '--port', default='8000',
+                            help="port")
+
+    def run(self):
+        args = self.args
+        fs = fsopendir(args.fs)
+
+        from ...command.sub import project_serve
+        location = os.path.dirname(project_serve.__file__)
+
+        init_logging(pathjoin(location, 'logging.ini'))
+        application = WSGIApplication(location, 'settings.ini', 'main', disable_autoreload=True)
+        application.archive.filesystems['static'] = fs
+
+        server = make_server(args.host,
+                             int(args.port),
+                             application,
+                             handler_class=RequestHandler)
+        log.info("server started on http://{}:{}".format(args.host, args.port))
+
+
+        def handle_error(request, client_address):
+            _type, value, tb = sys.exc_info()
+            if isinstance(value, KeyboardInterrupt):
+                interrupt_main()
+        server.handle_error = handle_error
+
+        try:
+            server.serve_forever()
+        finally:
+            application.close()
