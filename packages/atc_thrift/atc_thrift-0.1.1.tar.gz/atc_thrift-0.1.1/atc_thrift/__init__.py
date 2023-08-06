01dbb878322c90be398df18733035fc640e62c95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:18 +0000

    Add package atc_thrift/0.1.1/atc_thrift-0.1.1.tar.gz

---
+++ packages/atc_thrift/atc_thrift-0.1.1.tar.gz/atc_thrift-0.1.1/atc_thrift/__init__.py
@@ -0,0 +1,2 @@
+__all__ = ['ttypes', 'constants', 'Atcd']
+__version__ = '0.1.1'
---
+++ packages/atc_thrift/atc_thrift-0.1.1.tar.gz/atc_thrift-0.1.1/setup.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+#
+#  Copyright (c) 2014, Facebook, Inc.
+#  All rights reserved.
+#
+#  This source code is licensed under the BSD-style license found in the
+#  LICENSE file in the root directory of this source tree. An additional grant
+#  of patent rights can be found in the PATENTS file in the same directory.
+#
+#
+import os
+import re
+import sys
+from setuptools import setup
+
+readme = open('README.md', 'r')
+
+
+def get_version(package):
+    """
+    Return package version as listed in `__version__` in `init.py`.
+    """
+    init_py = open(os.path.join(package, '__init__.py')).read()
+    return re.search("__version__ = ['\"]([^'\"]+)['\"]", init_py).group(1)
+
+version = get_version('atc_thrift')
+
+if sys.argv[-1] == 'publish':
+    if os.system("pip freeze | grep wheel"):
+        print("wheel not installed.\nUse `pip install wheel`.\nExiting.")
+        sys.exit()
+    if os.system("pip freeze | grep twine"):
+        print("twine not installed.\nUse `pip install twine`.\nExiting.")
+        sys.exit()
+    os.system("python setup.py sdist bdist_wheel")
+    os.system("twine upload dist/*")
+    print("You probably want to also tag the version now:")
+    print("  git tag -a %s -m 'version %s'" % (version, version))
+    print("  git push --tags")
+    sys.exit()
+
+setup(
+    name='atc_thrift',
+    version=version,
+    description='ATC Thrift Library',
+    author='Emmanuel Bretelle',
+    author_email='chantra@fb.com',
+    url='https://github.com/facebook/augmented-traffic-control',
+    packages=['atc_thrift'],
+    classifiers=['Programming Language :: Python', ],
+    long_description=readme.read(),
+    install_requires=['thrift']
+)
