c6b0b463816deb8eaa04645134600360ab552a32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:02 +0000

    Add package caching-s3-proxy/0.6/caching-s3-proxy-0.6.tar.gz

---
+++ packages/caching-s3-proxy/caching-s3-proxy-0.6.tar.gz/caching-s3-proxy-0.6/proxy/cache.py
@@ -0,0 +1,99 @@
+"""
+Filesystem-backed LRU cache.
+"""
+
+import ordereddict
+from contextlib import contextmanager
+import cPickle as pickle
+import logging
+import os
+import tempfile
+import time
+
+
+class LRUCache(object):
+    def __init__(self, capacity=None, cache_dir=None):
+        if not capacity:
+            # 1GB
+            capacity = (10*10**9)
+        if not cache_dir:
+            cache_dir = tempfile.gettempdir()
+        logging.basicConfig(level=logging.INFO)
+        self.logger = logging.getLogger(__name__)
+        self.capacity = capacity
+        self.cache = ordereddict.OrderedDict()
+        self.cache_dir = cache_dir
+        self.total_size = 0
+        if not os.path.exists(self.cache_dir):
+            os.mkdir(self.cache_dir)
+        self.lock_path = os.path.join(self.cache_dir, 'lock')
+        if os.path.exists(self.lock_path):
+            os.unlink(self.lock_path)
+        self.logger.info('using cache dir %s' % self.cache_dir)
+        self.index_file = os.path.join(self.cache_dir, 'index')
+        with flock(self.lock_path):
+            if os.path.exists(self.index_file):
+                (self.total_size, self.cache) = \
+                        pickle.load(open(self.index_file, 'r'))
+
+    def __getitem__(self, key):
+        abspath = os.path.join(self.cache_dir, key)
+        with flock(self.lock_path):
+            (self.total_size, self.cache) = \
+                    pickle.load(open(self.index_file, 'r'))
+            self.cache.pop(key)
+            size = os.path.getsize(abspath)
+            with open(abspath, 'r') as cachefile:
+                value = cachefile.read()
+                # bump this to the top, since it was accessed most recently
+                self.cache[key] = size
+                pickle.dump((self.total_size, self.cache),
+                        open(self.index_file, 'w'))
+                return value
+
+    def __setitem__(self, key, value):
+        abspath = os.path.join(self.cache_dir, key)
+        with flock(self.lock_path):
+            if os.path.exists(self.index_file):
+                (self.total_size, self.cache) = \
+                        pickle.load(open(self.index_file, 'r'))
+            # remove files until we're under the limit, if we hit capacity
+            while self.total_size >= self.capacity:
+                self.logger.info('cache hit capacity %s' % self.capacity)
+                cache_key, size = self.cache.popitem(last=False)
+                os.remove(os.path.join(self.cache_dir, cache_key))
+                self.logger.info('evicted %s from cache' % cache_key)
+                self.total_size -= size
+            with open(abspath, 'w') as cachefile:
+                cachefile.write(value)
+            size = os.path.getsize(abspath)
+            self.cache[key] = size
+            self.total_size += size
+            pickle.dump((self.total_size, self.cache),
+                    open(self.index_file, 'w'))
+
+    def __contains__(self, key):
+        return key in self.cache
+
+
+# based on https://github.com/erikrose/shiva
+# and also https://github.com/dmfrey/FileLock
+@contextmanager
+def flock(lock_path, timeout=300):
+    """Context manager that acquires and releases a file-based lock.
+    """
+    start_time = time.time()
+    while True:
+        try:
+            fd = os.open(lock_path, os.O_CREAT|os.O_EXCL)
+            break
+        except OSError:
+            if (time.time() - start_time) >= timeout:
+                raise Exception("Timeout occured.")
+
+    try:
+        yield fd
+    finally:
+        if fd:
+            os.close(fd)
+            os.remove(lock_path)
---
+++ packages/caching-s3-proxy/caching-s3-proxy-0.6.tar.gz/caching-s3-proxy-0.6/setup.py
@@ -0,0 +1,34 @@
+import codecs
+import glob
+import os
+from setuptools import setup
+
+
+setup(
+    name='caching-s3-proxy',
+    version='0.6',
+    description=('provides an unauthenticated plain HTTP frontend for'
+                 ' public and private S3 buckets, and caches on the'
+                 ' filesystem using an LRU cache'),
+    author='Rob Helmer',
+    author_email='rhelmer@rhelmer.org',
+    license='MPL',
+    url='https://github.com/rhelmer/caching-s3-proxy',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: Mozilla Public License 1.1 (MPL 1.1)',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet :: WWW/HTTP :: WSGI :: Application',
+        ],
+    packages=['proxy'],
+    keywords=['caching', 'lru', 's3', 'proxy', 'unauthenticated'],
+    install_requires=['boto>=2.36.0', 'wsgiref>=0.1.2', 'ordereddict>=1.1'],
+    entry_points={
+        'console_scripts': [
+            'caching-s3-proxy = proxy.run:main'
+            ],
+        },
+    test_suite='nose.collector',
+    zip_safe=False,
+),
