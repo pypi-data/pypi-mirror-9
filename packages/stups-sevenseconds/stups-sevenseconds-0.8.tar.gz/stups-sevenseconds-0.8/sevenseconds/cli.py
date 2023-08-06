55ea7f691ac689cb4c9c9a65a8aae7152b257d70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:24 +0000

    Add package stups-sevenseconds/0.8/stups-sevenseconds-0.8.tar.gz

---
+++ packages/stups-sevenseconds/stups-sevenseconds-0.8.tar.gz/stups-sevenseconds-0.8/sevenseconds/cli.py
@@ -0,0 +1,139 @@
+import fnmatch
+import traceback
+import click
+import keyring
+import yaml
+import socket
+from sevenseconds.aws import configure_account
+
+import sevenseconds
+from sevenseconds.console import AliasedGroup, error, Action, info, warning
+import boto.cloudtrail
+import boto.exception
+import boto.vpc
+import boto.route53
+import boto.elasticache
+import boto.rds2
+import boto.iam
+from aws_saml_login import authenticate, assume_role, write_aws_credentials
+
+
+CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
+
+
+def print_version(ctx, param, value):
+    if not value or ctx.resilient_parsing:
+        return
+    click.echo('AWS Account Configurator {}'.format(sevenseconds.__version__))
+    ctx.exit()
+
+
+@click.group(cls=AliasedGroup, context_settings=CONTEXT_SETTINGS)
+@click.option('-V', '--version', is_flag=True, callback=print_version, expose_value=False, is_eager=True)
+def cli():
+    pass
+
+
+@cli.command()
+@click.argument('file', type=click.File('rb'))
+@click.argument('region_name')
+@click.argument('security_group')
+def update_security_group(file, region_name, security_group):
+    '''Update a Security Group and allow access from all trusted networks, NAT instances and bastion hosts'''
+    config = yaml.safe_load(file)
+    accounts = config.get('accounts', {})
+
+    addresses = set()
+
+    for name, cidr in config.get('global', {}).get('trusted_networks', {}).items():
+        info('Adding trusted network {} ({})'.format(name, cidr))
+        addresses.add(cidr)
+
+    for account_name, cfg in accounts.items():
+        if not cfg:
+            cfg = {}
+        cfg.update(config.get('global', {}))
+        for region in cfg['regions']:
+            domains = set(['odd-{}.{}'.format(region, cfg.get('domain').format(account_name=account_name))])
+            for az in 'a', 'b', 'c':
+                domains.add('nat-{}{}.{}'.format(region, az, cfg.get('domain').format(account_name=account_name)))
+            for domain in sorted(domains):
+                with Action('Checking {}'.format(domain)):
+                    try:
+                        ai = socket.getaddrinfo(domain, 443, family=socket.AF_INET, type=socket.SOCK_STREAM)
+                    except:
+                        ai = []
+                        pass
+                    for _, _, _, _, ip_port in ai:
+                        ip, _ = ip_port
+                        addresses.add('{}/32'.format(ip))
+
+    info('\n'.join(sorted(addresses)))
+
+    conn = boto.ec2.connect_to_region(region_name)
+    for sg in conn.get_all_security_groups():
+        if security_group in sg.name:
+            with Action('Updating security group {}..'.format(sg.name)) as act:
+                for cidr in sorted(addresses):
+                    try:
+                        sg.authorize(ip_protocol='tcp', from_port=443, to_port=443, cidr_ip=cidr)
+                    except boto.exception.EC2ResponseError as e:
+                        if 'already exists' not in e.message:
+                            raise
+                    act.progress()
+
+
+@cli.command()
+@click.argument('file', type=click.File('rb'))
+@click.argument('account_name_pattern')
+@click.option('--saml-user', envvar='SAML_USER')
+@click.option('--saml-password', envvar='SAML_PASSWORD')
+@click.option('--dry-run', is_flag=True)
+def configure(file, account_name_pattern, saml_user, saml_password, dry_run):
+    '''Configure a single AWS account'''
+    config = yaml.safe_load(file)
+    accounts = config.get('accounts', {})
+
+    account_names = sorted(fnmatch.filter(accounts.keys(), account_name_pattern))
+
+    if not account_names:
+        error('No configuration found for account {}'.format(account_name_pattern))
+        return
+
+    for account_name in account_names:
+        cfg = accounts.get(account_name) or {}
+        cfg.update(config.get('global', {}))
+
+        saml_url = cfg.get('saml_identity_provider_url')
+        saml_role = cfg.get('saml_admin_login_role')
+
+        if saml_user and saml_url and saml_role:
+            if not saml_password:
+                saml_password = keyring.get_password('sevenseconds', saml_user)
+            if not saml_password:
+                saml_password = click.prompt('Please enter your SAML password', hide_input=True)
+
+            with Action('Authenticating against {}..'.format(saml_url)):
+                saml_xml, roles = authenticate(saml_url, saml_user, saml_password)
+            keyring.set_password('sevenseconds', saml_user, saml_password)
+
+            account_alias = cfg.get('alias', account_name).format(account_name=account_name)
+            matching_roles = [(parn, rarn, aname)
+                              for parn, rarn, aname in roles if aname == account_alias and rarn.endswith(saml_role)]
+            if not matching_roles:
+                error('No matching role found for account {}: {}'.format(account_name, roles))
+                warning('Skipping account configuration of {} due to missing credentials'.format(account_name))
+                continue
+            role = matching_roles[0]
+            with Action('Assuming role {}..'.format(role)):
+                key_id, secret, session_token = assume_role(saml_xml, role[0], role[1])
+            write_aws_credentials('default', key_id, secret, session_token)
+
+        try:
+            configure_account(account_name, cfg, dry_run)
+        except Exception:
+            error('Error while configuring {}: {}'.format(account_name, traceback.format_exc()))
+
+
+def main():
+    cli()
