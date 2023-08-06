2885d6cefd1c26f8c197d6f39de12988de67b879
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:53 +0000

    Add package djangorestframework-gis/0.8/djangorestframework-gis_0.8.tar.gz

---
+++ packages/djangorestframework-gis/djangorestframework-gis_0.8.tar.gz/djangorestframework-gis-0.8/setup.py
@@ -0,0 +1,59 @@
+import sys
+import os
+from setuptools import setup, find_packages
+from setuptools.command.test import test
+from rest_framework_gis import get_version
+
+
+def get_install_requires():
+    """
+    parse requirements.txt, ignore links, exclude comments
+    """
+    requirements = []
+    for line in open('requirements.txt').readlines():
+        # skip to next iteration if comment or empty line
+        if line.startswith('#') or line == '' or line.startswith('http') or line.startswith('git'):
+            continue
+        # add line to requirements
+        requirements.append(line)
+    return requirements
+
+
+if sys.argv[-1] == 'publish':
+    os.system("python setup.py sdist upload")
+    args = {'version': get_version()}
+    print("You probably want to also tag the version now:")
+    print("  git tag -a %(version)s -m 'version %(version)s'" % args)
+    print("  git push --tags")
+    sys.exit()
+
+
+setup(
+    name='djangorestframework-gis',
+    version=get_version(),
+    license='BSD',
+    author='Douglas Meehan',
+    author_email='dmeehan@gmail.com',
+    description='Geographic add-ons for Django Rest Framework',
+    url='https://github.com/djangonauts/django-rest-framework-gis',
+    download_url='https://github.com/djangonauts/django-rest-framework-gis/releases',
+    platforms=['Platform Indipendent'],
+    keywords=['django', 'rest-framework', 'gis', 'geojson'],
+    packages=find_packages(exclude=['tests', 'tests.*']),
+    install_requires=get_install_requires(),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Topic :: Internet :: WWW/HTTP',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Framework :: Django',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ]
+)
---
+++ packages/djangorestframework-gis/djangorestframework-gis_0.8.tar.gz/djangorestframework-gis-0.8/tests/django_restframework_gis_tests/serializers.py
@@ -0,0 +1,69 @@
+from rest_framework import pagination, serializers
+from rest_framework_gis import serializers as gis_serializers
+
+from .models import *
+
+
+__all__ = [
+    'LocationGeoSerializer',
+    'PaginatedLocationGeoSerializer',
+    'LocationGeoFeatureSerializer',
+    'LocationGeoFeatureSlugSerializer',
+    'LocationGeoFeatureFalseIDSerializer',
+    'PaginatedLocationGeoFeatureSerializer',
+]
+
+
+class LocationGeoSerializer(gis_serializers.GeoModelSerializer):
+    """ location geo serializer  """
+
+    details = serializers.HyperlinkedIdentityField(view_name='api_location_details')
+
+    class Meta:
+        model = Location
+        geo_field = 'geometry'
+
+
+class PaginatedLocationGeoSerializer(pagination.PaginationSerializer):
+
+    class Meta:
+        object_serializer_class = LocationGeoSerializer
+
+
+class LocationGeoFeatureSerializer(gis_serializers.GeoFeatureModelSerializer):
+    """ location geo serializer  """
+
+    details = serializers.HyperlinkedIdentityField(view_name='api_geojson_location_details')
+    fancy_name = serializers.SerializerMethodField()
+
+    def get_fancy_name(self, obj):
+        return u'Kool %s' % obj.name
+
+    class Meta:
+        model = Location
+        geo_field = 'geometry'
+
+
+class LocationGeoFeatureSlugSerializer(LocationGeoFeatureSerializer):
+    """ use slug as id attribute  """
+
+    class Meta:
+        model = Location
+        geo_field = 'geometry'
+        id_field = 'slug'
+        fields = ['name', 'slug']
+
+
+class LocationGeoFeatureFalseIDSerializer(LocationGeoFeatureSerializer):
+    """ id attribute set as False """
+
+    class Meta:
+        model = Location
+        geo_field = 'geometry'
+        id_field = False
+
+
+class PaginatedLocationGeoFeatureSerializer(pagination.PaginationSerializer):
+
+    class Meta:
+        object_serializer_class = LocationGeoFeatureSerializer
