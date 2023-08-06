ef163112863056045a7a0bb6f81c1ddd3276e2ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:40 +0000

    Add package update-conf.py/0.4.0/update-conf.py-0.4.0.tar.gz

---
+++ packages/update-conf.py/update-conf.py-0.4.0.tar.gz/update-conf.py-0.4.0/README.rst
@@ -0,0 +1,103 @@
+update-conf.py
+==============
+
+Generate config files from ``conf.d`` like directories.
+
+Split your config file into smaller files in a ``conf.d`` like
+directory. The generated config file will be the concatenation of all
+splitted config files (also called snippets). The spplited files will be
+merged in the lexical order of their names.
+
+Files ending with ``.bak``, ``.old`` and other similar terminations will
+be ignored.
+
+This project was based in the `update-conf.d
+project <https://github.com/Atha/update-conf.d>`__.
+
+Install
+-------
+
+This project requires Python 2.6 or newer.
+
+**PS:** It's possible to use Python 3 also. However, it is not well
+tested.
+
+In Ubuntu/Debian:
+
+.. code:: sh
+
+    apt-get install python
+
+To install:
+
+.. code:: sh
+
+    pip install update-conf.py
+
+It's possible to clone the project in Github and install it via
+``setuptools``:
+
+.. code:: sh
+
+    git clone git@github.com:rarylson/update-conf.py.git
+    cd update-conf.py
+    python setup.py install
+
+Usage
+-----
+
+If you run:
+
+.. code:: sh
+
+    update-conf.py -f /etc/snmp/snmpd.conf
+
+The script will merge the splitted config files in the directory
+``/etc/snmp/snmpd.conf.d`` into the file ``/etc/snmp/snmpd.conf``.
+
+If the directory containing the splitted files uses a diferent name
+pattern, you can pass its name as an argument:
+
+.. code:: sh
+
+    update-conf.py -f /etc/snmp/snmpd.conf -d /etc/snmp/snmpd.d
+
+It's also possible to define frequent used options in a config file
+(``/etc/update-conf.py.conf``). For example:
+
+.. code:: ini
+
+    [snmpd]
+    file = /etc/snmp/snmpd.conf
+    dir = /etc/snmp/snmpd.d
+
+Now, you can run:
+
+.. code:: sh
+
+    update-conf.py -n snmpd
+
+To get help:
+
+.. code:: sh
+
+    update-conf.py --help
+
+License
+-------
+
+This software is released under the `Revised BSD
+License <https://github.com/rarylson/update-conf.py/blob/master/LICENSE>`__.
+
+Changelog
+---------
+
+You can see the changelog
+`here <https://github.com/rarylson/update-conf.py/blob/master/CHANGELOG.md>`__.
+
+TODO
+----
+
+-  Publish this software in a Ubuntu PPA;
+
+   -  Ubuntu 12.04 and Ubuntu 14.04.
---
+++ packages/update-conf.py/update-conf.py-0.4.0.tar.gz/update-conf.py-0.4.0/setup.py
@@ -0,0 +1,191 @@
+"""update-conf.py setup.py file
+"""
+
+import os
+from os.path import abspath, dirname, join, isfile
+import shutil
+from distutils import log
+from setuptools import setup, Command
+from setuptools.command.register import register
+from setuptools.command.install import install
+
+from update_conf_py import main
+
+# Consts
+GITHUB_URL = "https://github.com/rarylson/update-conf.py"
+README_MD = "README.md"
+README_RST = "README.rst"
+
+# Important vars
+cur_dir = abspath(dirname(__file__))
+readme_md = join(cur_dir, README_MD)
+readme_rst = join(cur_dir, README_RST)
+sample_config = join("samples", main.CONFIG_NAME)
+sample_config_path = join(cur_dir, sample_config)
+# Get description from the first line of the module docstring.
+description = main.__doc__.split('\n')[0]
+# Get the long description from the 'README.rst' file (if it exists). Else,
+# use the module doc string.
+# The 'README.rst' is required when registering on Pypi.
+long_description = ""
+using_rst = False
+try:
+    with open(readme_rst, 'r') as f:
+        long_description = f.read()
+        using_rst = True
+except IOError:
+    long_description = main.__doc__
+
+
+class GenerateRstCommand(Command):
+    """Generate a README.rst file
+
+    This file can be used after in the register command.
+    """
+
+    description = "generate a README.rst file from README.md"
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        if isfile(readme_rst):
+            os.remove(readme_rst)
+
+    def run(self):
+        import tempfile
+        import shutil
+        import re
+
+        import pypandoc
+
+        tmp_readme_md = join(tempfile.gettempdir(), README_MD)
+        shutil.copy(readme_md, tmp_readme_md)
+        try:
+            with open(tmp_readme_md, "r") as f:
+                md = f.read()
+            # Convert links that points to a relative URL
+            # The markdown file may be relative URLs (like [Page](page)).
+            # However, we do not want these links in Pypi (they will be
+            # broken). We want to replace them by absolutive URLs (like
+            # [Page]({url}/blob/master/page)).
+            # For now, the conversions are hardcoded.
+            md_link_re = r"\(LICENSE\)"
+            md_link_new = r"({0}/blob/master/LICENSE)".format(GITHUB_URL)
+            new_md = re.sub(md_link_re, md_link_new, md)
+            md_link_re = r"\(CHANGELOG\.md\)"
+            md_link_new = r"({0}/blob/master/CHANGELOG.md)".format(GITHUB_URL)
+            new_md = re.sub(md_link_re, md_link_new, new_md)
+            with open(tmp_readme_md, "w") as f:
+                f.write(new_md)
+            # Now, convert to RST
+            rst = pypandoc.convert(tmp_readme_md, "rst")
+            with open(readme_rst, "w") as f:
+                f.write(rst)
+        finally:
+            os.remove(tmp_readme_md)
+
+
+class RegisterCommand(register):
+    """Check if we're using README.rst before register in Pypi
+    """
+
+    def finalize_options(self):
+        if not using_rst:
+            raise Exception("{} file not found".format(README_RST))
+
+        return register.finalize_options(self)
+
+
+class InstallCommand(install):
+    """Install the global config file if it exists
+    """
+
+    def run(self):
+        result = install.run(self)
+        if os.access(main.SYSTEM_CONFIG, os.W_OK):
+            log.info("Copying {0} to {1}".format(
+                sample_config, main.SYSTEM_CONFIG))
+            shutil.copy(sample_config_path, main.SYSTEM_CONFIG)
+        else:
+            log.warn(
+                "Skiping copy of {0} to {1}. You do not have permission "
+                "to do this.".format(sample_config, main.SYSTEM_CONFIG))
+
+        return result
+
+
+# Setup
+setup(
+    # Main software info
+    name=main.__program__,
+    version=main.__version__,
+    description=description,
+    long_description=long_description,
+    license=main.__license__,
+    author=main.__author__,
+    author_email=main.__email__,
+    url=GITHUB_URL,
+    # FIXME: Currently, setuptools do not understand this option (it's used
+    # only by twine). When using setuptools, for now, ignore the warning. In
+    # a future, only pass this option when using twine.
+    bugtrack_url="{0}/issues".format(GITHUB_URL),
+    download_url="{0}/tarball/{1}".format(GITHUB_URL, main.__version__),
+    keywords="system unix config split snippets sysadmin",
+    packages=["update_conf_py"],
+
+    # Requirements
+    install_requires=[
+        "argparse>=1.2",
+        "configparser>=2.3",
+    ],
+
+    # Classifiers
+    # See: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved",
+        "Natural Language :: English",
+        "Operating System :: POSIX",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Topic :: System :: Systems Administration",
+        "Topic :: Utilities",
+    ],
+
+    # Entry points
+    # The script itself is defined here.
+    entry_points={
+        "console_scripts": [
+            "update-conf.py = update_conf_py:run",
+        ],
+    },
+
+    # Data files
+    data_files=[
+        (join("share", main.__program__), [sample_config, ]),
+    ],
+
+    # Extra
+    extras_require={
+        "dev": [
+            "setuptools>=0.8",
+            "pypandoc>=0.9",
+        ],
+    },
+
+    # Tests
+    test_suite="tests",
+
+    # Commands
+    cmdclass={
+        "generate_rst": GenerateRstCommand,
+        "register": RegisterCommand,
+        "install": InstallCommand,
+    }
+)
