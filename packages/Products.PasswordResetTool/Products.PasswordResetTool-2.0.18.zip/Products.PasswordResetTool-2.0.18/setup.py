4a0605977011d83988d050b7fab2313b5598630a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:29:54 +0000

    Add package Products.PasswordResetTool/2.0.18/Products.PasswordResetTool-2.0.18.zip

---
+++ packages/Products.PasswordResetTool/Products.PasswordResetTool-2.0.18.zip/Products.PasswordResetTool-2.0.18/setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup, find_packages
+
+version = '2.0.18'
+
+setup(name='Products.PasswordResetTool',
+      version=version,
+      description="Password reset tool for Plone",
+      long_description=open("README.txt").read() + "\n" + \
+              open("CHANGES.txt").read(),
+      classifiers=[
+        "Framework :: Plone",
+        "Framework :: Zope2",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Programming Language :: Python",
+        ],
+      keywords='password reset plone',
+      author='J. Cameron Cooper',
+      author_email='prt@jcameroncooper.com',
+      url='http://pypi.python.org/pypi/Products.PasswordResetTool',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['Products'],
+      include_package_data=True,
+      zip_safe=False,
+      extras_require=dict(
+        test=[
+            'plone.app.testing',
+        ]
+      ),
+      install_requires=[
+          'setuptools',
+          'plone.memoize',
+          'zope.component',
+          'zope.i18nmessageid',
+          'zope.i18n',
+          'zope.interface',
+          'Products.CMFPlone',
+          'Products.CMFCore',
+          'Acquisition',
+          'DateTime',
+          'Zope2',
+      ],
+      )
