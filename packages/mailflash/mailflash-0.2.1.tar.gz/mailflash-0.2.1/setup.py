3dd495c5b89988fd15dab1ad2c400b309e89accd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:23:01 +0000

    Add package mailflash/0.2.1/mailflash-0.2.1.tar.gz

---
+++ packages/mailflash/mailflash-0.2.1.tar.gz/mailflash-0.2.1/setup.py
@@ -0,0 +1,46 @@
+"""
+mailflash
+----------
+
+A simple library to send emails.
+
+Please refer to the online documentation for details.
+
+Links
+`````
+
+* `documentation <https://github.com/nicolas-van/mailflash>`_
+"""
+from setuptools import setup
+
+
+setup(
+    name='mailflash',
+    version='0.2.1',
+    url='https://github.com/nicolas-van/mailflash',
+    license='BSD',
+    author='Nicolas Vanhoren',
+    author_email='nicolas.vanhoren@gmail.com',
+    description='Simple library to send emails',
+    long_description=__doc__,
+    py_modules=[
+        'mailflash'
+    ],
+    test_suite='nose.collector',
+    zip_safe=False,
+    platforms='any',
+    install_requires=[
+        'blinker',
+    ],
+    tests_require=[
+        'nose',
+        'blinker',
+    ],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
