b754aea6ad9bffe7b0537c3b5fd0c6926a7b3e78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:10:04 +0000

    Add package natsort/3.5.5/natsort-3.5.5-py2.py3-none-any.whl

---
+++ packages/natsort/natsort-3.5.5-py2.py3-none-any.whl/natsort/__init__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from __future__ import (print_function, division,
+                        unicode_literals, absolute_import)
+
+# Local imports.
+from natsort.natsort import (natsort_key, natsort_keygen, ns,
+                             natsorted, humansorted, versorted,
+                             realsorted, index_realsorted,
+                             index_natsorted, index_versorted,
+                             index_humansorted, order_by_index)
+from natsort._version import __version__
+
+__all__ = [
+    'natsort_key',
+    'natsort_keygen',
+    'natsorted',
+    'versorted'
+    'humansorted',
+    'realsorted',
+    'index_natsorted',
+    'index_versorted',
+    'index_humansorted',
+    'index_realsorted',
+    'order_by_index',
+    'ns',
+]
