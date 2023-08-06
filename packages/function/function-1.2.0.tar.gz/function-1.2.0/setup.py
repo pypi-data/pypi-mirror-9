0f22f9808e8d58ddbcd55fead1c13eda7ad1437d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:50 +0000

    Add package function/1.2.0/function-1.2.0.tar.gz

---
+++ packages/function/function-1.2.0.tar.gz/function-1.2.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+
+setup(
+	name         = 'function',
+	version      = '1.2.0',
+    py_modules   = ['function'],
+    author       = 'Peter',
+    author_email = 'a184999@gmail.com',
+    description  = 'A simple nest list',
+)
