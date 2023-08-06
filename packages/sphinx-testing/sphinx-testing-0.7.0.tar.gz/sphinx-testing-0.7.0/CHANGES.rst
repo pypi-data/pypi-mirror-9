8503fa250360f39fd55c9dd61ebe4d56a8433e8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:37 +0000

    Add package sphinx-testing/0.7.0/sphinx-testing-0.7.0.tar.gz

---
+++ packages/sphinx-testing/sphinx-testing-0.7.0.tar.gz/sphinx-testing-0.7.0/CHANGES.rst
@@ -0,0 +1,46 @@
+Changelog
+==========
+
+0.7.0 (2015-03-21)
+-------------------
+- PR#1 Enable verbosity and parallel arguments.
+
+0.6.0 (2014-10-17)
+-------------------
+- Add python 3.2 support (with Sphinx < 1.3)
+- Replace ListOutput with StringIO
+
+0.5.2 (2014-10-16)
+-------------------
+- Fix bug:
+
+   - Fix srcdir and confdir are not abspath
+
+0.5.1 (2014-10-15)
+-------------------
+- Reimplement with_app decorator as class
+- Set copy_srcdir_to_tmpdir=True if write_docstring is specified
+
+0.5.0 (2014-10-15)
+-------------------
+- Add write_docstring option to with_app()
+
+0.4.0 (2014-09-30)
+-------------------
+- Add pathlib like accessors to path class: parent, name, suffix and stem
+- Fix bug:
+
+   - #1 Fix exc assignment in with_app decorator.
+
+0.3.0 (2014-09-27)
+-------------------
+- Rename sphinx.testing package to sphinx_testing
+
+0.2.0 (2014-09-24)
+-------------------
+- Add path#utime() and path#listdir()
+- Change interface of @with_app: Give `status` and `warning` to decorated function as a argument
+
+0.1.0 (2014-09-20)
+-------------------
+- Initial release
---
+++ packages/sphinx-testing/sphinx-testing-0.7.0.tar.gz/sphinx-testing-0.7.0/tests/test_util.py
@@ -0,0 +1,184 @@
+# -*- coding: utf-8 -*-
+
+import sys
+if sys.version_info < (2, 7):
+    import unittest2 as unittest
+else:
+    import unittest
+
+if sys.version_info < (3,):
+    unittest.TestCase.assertCountEqual = unittest.TestCase.assertItemsEqual
+
+if sys.version_info < (3, 3):
+    from mock import patch
+else:
+    from unittest.mock import patch
+
+import os
+import sphinx
+from six import StringIO
+from sphinx_testing.path import path
+from sphinx_testing.tmpdir import mkdtemp
+from sphinx_testing.util import TestApp, with_app
+
+
+class TestSphinxTesting(unittest.TestCase):
+    def test_TestApp(self):
+        try:
+            srcdir = path(__file__).dirname() / 'examples'
+            app = TestApp(srcdir=srcdir)
+            self.assertIsInstance(app._status, StringIO)
+            self.assertIsInstance(app._warning, StringIO)
+
+            if sphinx.__version__ < '1.0.0':
+                app.build(True, None)
+            else:
+                app.build()
+            self.assertIn('index.html', os.listdir(app.outdir))
+        finally:
+            app.cleanup()
+
+    def test_TestApp_when_srcdir_specified(self):
+        try:
+            srcdir = path(__file__).dirname() / 'examples'
+            app = TestApp(srcdir=srcdir)
+            self.assertEqual(srcdir, app.srcdir)
+            self.assertNotEqual(app.srcdir, app.builddir.dirname())
+            self.assertTrue(app.builddir.isdir())
+            self.assertCountEqual(['conf.py', 'index.rst'],
+                                  os.listdir(app.srcdir))
+            self.assertEqual((srcdir / 'conf.py').read_text(),
+                             (app.srcdir / 'conf.py').read_text())
+            self.assertEqual((srcdir / 'index.rst').read_text(),
+                             (app.srcdir / 'index.rst').read_text())
+        finally:
+            app.cleanup()
+
+        self.assertFalse(app.builddir.exists())
+
+    def test_TestApp_when_srcdir_is_None(self):
+        with self.assertRaises(AssertionError):
+            TestApp(srcdir=None)
+
+    def test_TestApp_when_create_new_srcdir(self):
+        try:
+            app = TestApp(create_new_srcdir=True)
+            self.assertIsNotNone(app.srcdir)
+            self.assertEqual(['conf.py'], os.listdir(app.srcdir))
+            self.assertEqual('', (app.srcdir / 'conf.py').read_text())
+        finally:
+            app.cleanup()
+
+    def test_TestApp_when_srcdir_and_create_new_srcdir_conflict(self):
+        with self.assertRaises(AssertionError):
+            TestApp(srcdir='examples', create_new_srcdir=True)
+
+    def test_TestApp_when_copy_srcdir_to_tmpdir(self):
+        try:
+            srcdir = path(__file__).dirname() / 'examples'
+            app = TestApp(srcdir=srcdir, copy_srcdir_to_tmpdir=True)
+            self.assertNotEqual(srcdir, app.srcdir)
+            self.assertEqual(app.srcdir, app.builddir.dirname())
+            self.assertTrue(app.builddir.isdir())
+            self.assertCountEqual(['_build', 'conf.py', 'index.rst'],
+                                  os.listdir(app.srcdir))
+            self.assertEqual((srcdir / 'conf.py').read_text(),
+                             (app.srcdir / 'conf.py').read_text())
+            self.assertEqual((srcdir / 'index.rst').read_text(),
+                             (app.srcdir / 'index.rst').read_text())
+        finally:
+            app.cleanup()
+
+        self.assertFalse(app.srcdir.exists())
+        self.assertFalse(app.builddir.exists())
+
+    def test_TestApp_cleanup(self):
+        app = TestApp(create_new_srcdir=True)
+        self.assertTrue(app.builddir.exists())
+
+        with patch("sphinx.theming.Theme") as Theme:
+            with patch("sphinx.ext.autodoc.AutoDirective") as AutoDirective:
+                app.cleanup()
+                self.assertEqual(1, Theme.themes.clear.call_count)
+                self.assertEqual(1, AutoDirective._registry.clear.call_count)
+                self.assertFalse(app.builddir.exists())
+
+    def test_TestApp_cleanup_when_cleanup_on_errors(self):
+        app = TestApp(create_new_srcdir=True, cleanup_on_errors=False)
+        self.assertTrue(app.builddir.exists())
+
+        with patch("sphinx.theming.Theme") as Theme:
+            with patch("sphinx.ext.autodoc.AutoDirective") as AutoDirective:
+                app.cleanup(error=True)
+                self.assertEqual(0, Theme.themes.clear.call_count)
+                self.assertEqual(0, AutoDirective._registry.clear.call_count)
+                self.assertTrue(app.builddir.exists())
+
+        with patch("sphinx.theming.Theme") as Theme:
+            with patch("sphinx.ext.autodoc.AutoDirective") as AutoDirective:
+                app.cleanup(error=None)
+                self.assertEqual(1, Theme.themes.clear.call_count)
+                self.assertEqual(1, AutoDirective._registry.clear.call_count)
+                self.assertFalse(app.builddir.exists())
+
+    def test_with_app(self):
+        srcdir = path(__file__).dirname() / 'examples'
+        builddir = []
+
+        @with_app(srcdir=srcdir, copy_srcdir_to_tmpdir=True)
+        def execute(app, status, warning):
+            (app.srcdir / 'unknown.rst').write_text('')
+            builddir.append(app.builddir)  # store to check outside of func
+            if sphinx.__version__ < '1.0.0':
+                app.build(True, None)
+            else:
+                app.build()
+
+            self.assertIsInstance(status, StringIO)
+            self.assertIsInstance(warning, StringIO)
+            self.assertIn('index.html', os.listdir(app.outdir))
+            self.assertIn('Running Sphinx', status.getvalue())
+            self.assertIn("WARNING: document isn't included in any toctree",
+                          warning.getvalue())
+
+        execute()
+        self.assertFalse(builddir[0].exists())
+
+    @patch("sphinx_testing.util.mkdtemp")
+    def test_with_app_bad_args(self, _mkdtemp):
+        tmpdir = _mkdtemp.return_value = mkdtemp()
+        srcdir = path(__file__).dirname() / 'examples'
+
+        @with_app(srcdir=srcdir, copy_srcdir_to_tmpdir=True)
+        def execute(oops):
+            pass
+
+        with self.assertRaises(TypeError):
+            # TypeError: execute() takes 1 positional argument but 3 were given
+            execute()
+
+        self.assertFalse(tmpdir.exists())
+
+    def test_with_app_write_docstring1(self):
+        srcdir = path(__file__).dirname() / 'examples'
+
+        @with_app(srcdir=srcdir, copy_srcdir_to_tmpdir=True,
+                  write_docstring=True)
+        def execute(app, status, warning):
+            """ Hello world """
+            content = (app.srcdir / 'index.rst').read_text()
+            self.assertEqual('Hello world ', content)
+
+        execute()
+
+    def test_with_app_write_docstring2(self):
+        srcdir = path(__file__).dirname() / 'examples'
+
+        @with_app(srcdir=srcdir, copy_srcdir_to_tmpdir=True,
+                  write_docstring='hello.rst')
+        def execute(app, status, warning):
+            """ Hello world """
+            content = (app.srcdir / 'hello.rst').read_text()
+            self.assertEqual('Hello world ', content)
+
+        execute()
