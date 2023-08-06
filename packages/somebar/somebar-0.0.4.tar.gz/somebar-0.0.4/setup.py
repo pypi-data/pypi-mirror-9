33c29f039f58bc6a04fc3344abd928ca7e66f8e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:29:43 +0000

    Add package somebar/0.0.4/somebar-0.0.4.tar.gz

---
+++ packages/somebar/somebar-0.0.4.tar.gz/somebar-0.0.4/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+from os.path import join, dirname
+
+
+setup(
+    name = 'somebar',
+    scripts = ['somebar'],
+    version = '0.0.4',
+    license = 'MIT License',
+    description = 'Simple taskbar widget that displays color dot or custom icon',
+    author = 'Ivan',
+    author_email = 'ivan.vershigora@gmail.com',
+    url = 'https://github.com/limpbrains/somebar',
+    download_url  ='https://github.com/limpbrains/somebar/tarball/0.0.4',
+    keywords = ['AnyBar', 'somebar', 'taskbar', 'indicator'],
+    packages = find_packages(),
+    classifiers = [
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Operating System :: POSIX :: Linux',
+    ],
+    package_data = {'somebar_icons' : ['*.png']},
+    install_requires = [
+        # 'gi'
+    ],
+)
