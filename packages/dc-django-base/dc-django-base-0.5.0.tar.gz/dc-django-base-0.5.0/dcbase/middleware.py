ae3ac41f19f993904801ab61311c7f466225be12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:07 +0000

    Add package dc-django-base/0.5.0/dc-django-base-0.5.0.tar.gz

---
+++ packages/dc-django-base/dc-django-base-0.5.0.tar.gz/dc-django-base-0.5.0/dcbase/middleware.py
@@ -0,0 +1,23 @@
+import datetime
+
+from dcbase.apps import TIMEZONE_SESSION_KEY
+from django.utils import timezone
+
+
+class DcBaseMiddleware(object):
+    def process_request(self, request):
+        timezone_name = request.session.get(TIMEZONE_SESSION_KEY)
+        if timezone_name:
+            timezone.activate(timezone_name)
+            return
+
+        try:
+            tzoffset = int(request.COOKIES.get('_tz_offset'))
+            delta = datetime.timedelta(minutes=0-tzoffset)
+            tz = datetime.timezone(delta)
+            timezone.activate(tz)
+            return
+        except (TypeError, ValueError):
+            pass
+
+        timezone.deactivate()
