ed42c48c7ea46eb88151361a657db441c342fafc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:48 +0000

    Add package Wallace/0.0.5/Wallace-0.0.5.tar.gz

---
+++ packages/Wallace/Wallace-0.0.5.tar.gz/Wallace-0.0.5/wallace/db/base/attrs/__init__.py
@@ -0,0 +1,12 @@
+from wallace.db.base.attrs.base import DataType
+from wallace.db.base.attrs.datatypes import Boolean, ByteArray, Float
+from wallace.db.base.attrs.datatypes import Integer, JSON, Moment, Now
+from wallace.db.base.attrs.datatypes import String, Unicode, UUID, UUID4
+
+
+__all__ = [
+    'DataType',
+
+    'Boolean', 'ByteArray', 'Float', 'Integer', 'JSON', 'Moment', 'Now',
+    'String', 'Unicode', 'UUID', 'UUID4',
+]
