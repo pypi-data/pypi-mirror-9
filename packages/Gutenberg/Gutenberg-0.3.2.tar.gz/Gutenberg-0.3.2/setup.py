12eae85c21b120e7e95a6f35983f66dc7e9d27d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:07 +0000

    Add package Gutenberg/0.3.2/Gutenberg-0.3.2.tar.gz

---
+++ packages/Gutenberg/Gutenberg-0.3.2.tar.gz/Gutenberg-0.3.2/setup.py
@@ -0,0 +1,19 @@
+"""Library installer."""
+
+
+from setuptools import find_packages
+from setuptools import setup
+
+
+setup(
+    name='Gutenberg',
+    version='0.3.2',
+    author='Clemens Wolff',
+    author_email='clemens.wolff+pypi@gmail.com',
+    packages=find_packages(exclude=['tests']),
+    url='https://github.com/c-w/Gutenberg',
+    download_url='http://pypi.python.org/pypi/Gutenberg',
+    license='LICENSE.txt',
+    description='Library to interface with Project Gutenberg',
+    long_description=open('README.rst').read(),
+    install_requires=list(line.strip() for line in open('requirements.pip')))
