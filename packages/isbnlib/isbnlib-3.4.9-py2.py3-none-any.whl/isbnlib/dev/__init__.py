91b564a0982fb797b66b00e59ecdebad7b048f4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:07:37 +0000

    Add package isbnlib/3.4.9/isbnlib-3.4.9-py2.py3-none-any.whl

---
+++ packages/isbnlib/isbnlib-3.4.9-py2.py3-none-any.whl/isbnlib/dev/__init__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from . import bouth23, helpers, vias
+from ._data import Metadata, stdmeta
+from ._exceptions import (DataNotFoundAtServiceError, DataWrongShapeError,
+                          ISBNLibDevException, ISBNLibHTTPError,
+                          ISBNLibURLError, NoAPIKeyError,
+                          NoDataForSelectorError, NotValidMetadataError,
+                          RecordMappingError, ServiceIsDownError)
+from .webquery import WEBQuery
+from .webservice import WEBService
+
+# alias
+ISBNToolsDevException = ISBNLibDevException
+ISBNToolsHTTPError = ISBNLibHTTPError
+ISBNToolsURLError = ISBNLibURLError
+
+__all__ = ('ISBNToolsDevException',
+           'ISBNLibDevException',
+           'ISBNToolsHTTPError', 'ISBNToolsURLError',
+           'ISBNLibHTTPError', 'ISBNLibURLError',
+           'DataNotFoundAtServiceError',
+           'ServiceIsDownError', 'DataWrongShapeError',
+           'NotValidMetadataError',
+           'NoDataForSelectorError',
+           'RecordMappingError', 'NoAPIKeyError',
+           'Metadata', 'stdmeta',
+           'WEBService', 'WEBQuery', 'vias', 'bouth23', 'helpers'
+           )
