5d83ffe6eb7e62b524985f61477038e647bad360
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:17 +0000

    Add package pants-build-utils/0.1/pants-build-utils-0.1.tar.gz

---
+++ packages/pants-build-utils/pants-build-utils-0.1.tar.gz/pants-build-utils-0.1/setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+from setuptools import setup, find_packages
+
+setup(version='0.1',
+      description='Stupid pants utils',
+      author='David Blackman',
+      author_email='david@blackmad.com',
+      url='https://www.python.org/sigs/distutils-sig/',
+      name = 'pants-build-utils',
+      packages = find_packages(),
+      entry_points = {
+        'console_scripts': [
+          'pants-smart-compile = pants.smart_compile:main',
+        ]
+      }
+     )
