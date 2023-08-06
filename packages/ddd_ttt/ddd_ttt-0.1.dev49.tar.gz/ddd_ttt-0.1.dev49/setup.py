93f066bca4c5fd015e56b12133cf7af905a08c66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:02 +0000

    Add package ddd_ttt/0.1.dev49/ddd_ttt-0.1.dev49.tar.gz

---
+++ packages/ddd_ttt/ddd_ttt-0.1.dev49.tar.gz/ddd_ttt-0.1.dev49/setup.py
@@ -0,0 +1,21 @@
+import os, sys
+try:
+    from setuptools import setup, Command
+except ImportError:
+    from distribute_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup, Command
+
+def main():
+    setup(
+        name='ddd_ttt',
+        description='test project (please ignore)',
+        version='0.1.dev49',
+        author='Holger Krekel',
+        author_email='holger at merlinux.eu',
+        py_modules=["dddttt"],
+        zip_safe=False,
+    )
+
+if __name__ == '__main__':
+    main()
