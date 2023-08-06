ecabe3e102de01ae51769136ce064d28c3a6d072
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:07:59 +0000

    Add package agile/1.3.0/agile-1.3.0.tar.gz

---
+++ packages/agile/agile-1.3.0.tar.gz/agile-1.3.0/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+
+install_requires = [
+    'mock==1.0.1',
+    'coverage',
+    'nose==1.3.4',
+    'rednose==0.4.1',
+    'sure==1.2.10',
+    'steadymark',
+    'httpretty'
+]
+
+
+if __name__ == '__main__':
+    setup(
+        name='agile',
+        version='1.3.0',
+        description=(
+            'A meta-package containing a full toolset for agile development with TDD'
+        ),
+        author='Gabriel Falcao',
+        author_email='gabriel@nacaolivre.org',
+        include_package_data=True,
+        url='http://github.com/gabrielfalcao/agile',
+        packages=[],
+        install_requires=install_requires,
+    )
