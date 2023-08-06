983e3a34b420b0005f90c46f672fab789cea7483
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:20:49 +0000

    Add package ex04_schachtler/0.3/ex04_schachtler-0.3.tar.gz

---
+++ packages/ex04_schachtler/ex04_schachtler-0.3.tar.gz/ex04_schachtler-0.3/ex04_schachtler.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+Aufgabe Seite 35 & 37 "Python von Kopf bis Fuß"
+Der auf Seite 29 entwickelte Code soll nun in eine Python modul
+geschrieben werden, damit er immer wieder verwendet werden kann.
+
+Dies ist das Modul "ex04_schachtler.py". Es stellt eine Funktion names
+print_lvl() bereit, die eine Liste mit beliebig vielen eingebetteten
+Listen ausgibt
+'''
+
+def print_lvl(liste,einzug=False, ebene=0):
+    '''Diese Funktion erwartet ein positionelles Argument namens
+    "liste", das eine beliebige Python-Liste (mit eventuellen
+    eingebetteten Listen) ist. Jedes Element der Liste wird (rekursiv)
+    auf dem Bildschirm jeweils in einer eigenen Zeile ausgegeben.
+    Mit dem zweiten Argument "ebene" können bei eingebetteten Listen
+    Tabulatoren eingesetzt werden.
+    '''
+
+    for element in liste:
+        if isinstance(element, list):
+            print_lvl(element, einzug, ebene+1)
+        else:
+            if einzug:
+                print("\t"*ebene, end=" ")
+            print(element)
