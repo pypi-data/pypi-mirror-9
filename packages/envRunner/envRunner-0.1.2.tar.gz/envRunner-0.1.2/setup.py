afc384bacc9638d71ff65c28da264feacab5e888
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:36:00 +0000

    Add package envRunner/0.1.2/envRunner-0.1.2.tar.gz

---
+++ packages/envRunner/envRunner-0.1.2.tar.gz/envRunner-0.1.2/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(name='envRunner',
+    version='0.1.2',
+    description='Run a command with environment variables set from a url or file',
+    author='Sean Reed',
+    author_email='sean@reed.pub',
+    url='https://github.com/HeqetLabs/envRunner',
+    install_requires=['docopt', 'requests', 'PyYAML', 'wsgiref'],
+    setup_requires=['setuptools-markdown'],
+    long_description_markdown_filename='README.md',
+    scripts=['envRunner'],
+    keywords=['environment', 'runner'],
+)
