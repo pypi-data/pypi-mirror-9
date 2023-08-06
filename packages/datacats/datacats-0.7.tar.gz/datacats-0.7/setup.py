12d003c76fa50786f730f0e43aa2b0936733d170
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:23:09 +0000

    Add package datacats/0.7/datacats-0.7.tar.gz

---
+++ packages/datacats/datacats-0.7.tar.gz/datacats-0.7/setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+# Copyright 2014-2015 Boxkite Inc.
+
+# This file is part of the DataCats package and is released under
+# the terms of the GNU Affero General Public License version 3.0.
+# See LICENSE.txt or http://www.fsf.org/licensing/licenses/agpl-3.0.html
+
+from setuptools import setup
+import sys
+
+install_requires=[
+    'setuptools',
+    'docopt',
+    'docker-py>=1.1.0',
+    'requests>=2.5.2',  # help with docker-py requirement
+]
+
+exec(open("datacats/version.py").read())
+
+setup(
+    name='datacats',
+    version=__version__,
+    description='CKAN Data Catalog Developer Tools built on Docker',
+    license='AGPL3',
+    author='Boxkite',
+    author_email='contact@boxkite.ca',
+    url='https://github.com/boxkite/datacats',
+    packages=[
+        'datacats',
+        'datacats.tests',
+        'datacats.cli',
+        ],
+    install_requires=install_requires,
+    include_package_data=True,
+    test_suite='datacats.tests',
+    zip_safe=False,
+    entry_points = """
+        [console_scripts]
+        datacats=datacats.cli.main:main
+        """,
+    )
+
