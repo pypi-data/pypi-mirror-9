00e44d2e01efcb6d5f1544d6a5fdb28a56741f5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:48 +0000

    Add package sky/0.0.0/sky-0.0.0.tar.gz

---
+++ packages/sky/sky-0.0.0.tar.gz/sky-0.0.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='sky',
+      version='0.0.0',
+      description='The extremely limited yet powerful scraper.',
+      url='https://www.linkedin.com/profile/view?id=190745232',
+      author='Pascal van Kooten',
+      author_email='kootenpv@gmail.com',
+      license='GPL',
+      packages=['sky'],
+      install_requires=[
+      
+      ],
+      zip_safe=False)
