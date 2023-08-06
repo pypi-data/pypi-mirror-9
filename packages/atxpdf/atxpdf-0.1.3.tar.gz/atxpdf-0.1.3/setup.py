c148315aabf70e7b3622f1fa586361f58135fdc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:39 +0000

    Add package atxpdf/0.1.3/atxpdf-0.1.3.tar.gz

---
+++ packages/atxpdf/atxpdf-0.1.3.tar.gz/atxpdf-0.1.3/setup.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+
+
+"""setup.py: setuptools control."""
+
+
+import re
+from setuptools import setup
+from distutils.core import setup
+
+version = re.search(
+    '^__version__\s*=\s*"(.*)"',
+    open('atxpdf/atxpdf.py').read(),
+    re.M
+    ).group(1)
+
+
+with open("README.rst", "rb") as f:
+    long_descr = f.read().decode("utf-8")
+
+
+setup(
+    name = "atxpdf",
+    packages = ["atxpdf"],
+    entry_points = {
+        "console_scripts": ['atxpdf = atxpdf.atxpdf:main']
+        },
+    install_requires = [
+        "argh",
+        "argcomplete",
+        "Pillow",
+        "fpdf"
+    ],
+    version = version,
+    description = "AtExpert PDF generator.",
+    long_description = long_descr,
+    author = "Frederik Van Leeckwyck",
+    author_email = "hogeblekker@hotmail.com",
+    url = "http://example.org",
+    )
