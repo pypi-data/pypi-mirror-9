3161f7087ba34e8d269d463feaf5939bd9a83678
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:15 +0000

    Add package onespacemedia-cms/1.6.1/onespacemedia-cms-1.6.1.tar.gz

---
+++ packages/onespacemedia-cms/onespacemedia-cms-1.6.1.tar.gz/onespacemedia-cms-1.6.1/cms/apps/media/models.py
@@ -0,0 +1,185 @@
+"""Models used by the static media management application."""
+from PIL import Image
+
+from django.db import models
+from django.contrib import admin
+from django.contrib.admin.widgets import ForeignKeyRawIdWidget
+
+import os
+
+
+class Label(models.Model):
+
+    """
+    A notional label used to organise static media.
+
+    This does not correspond to a physical label on the disk.
+    """
+
+    name = models.CharField(max_length=200)
+
+    def __unicode__(self):
+        """Returns the name of the label."""
+        return self.name
+
+    class Meta:
+        ordering = ("name",)
+
+
+class File(models.Model):
+
+    """A static file."""
+
+    title = models.CharField(max_length=200,
+                             help_text="The title will be used as the default rollover text when this media is embedded in a web page.")
+
+    labels = models.ManyToManyField(
+        Label,
+        blank=True,
+        help_text="Labels are used to help organise your media. They are not visible to users on your website.",
+    )
+
+    file = models.FileField(
+        upload_to="uploads/files",
+        max_length=250,
+    )
+
+    def get_absolute_url(self):
+        """Generates the absolute URL of the image."""
+        return self.file.url
+
+    def __unicode__(self):
+        """Returns the title of the media."""
+        return self.title
+
+    class Meta:
+        ordering = ("title",)
+
+    def is_image(self):
+        from .admin import FILE_ICONS, IMAGE_FILE_ICON, UNKNOWN_FILE_ICON
+
+        _, extension = os.path.splitext(self.file.name)
+        extension = extension.lower()[1:]
+        icon = FILE_ICONS.get(extension, UNKNOWN_FILE_ICON)
+        return icon == IMAGE_FILE_ICON
+
+    def width(self):
+        if self.is_image():
+            with open(self.file.path, "rb") as f:
+                try:
+                    image = Image.open(f)
+                except IOError:
+                    return 0
+            image.verify()
+            return image.size[0]
+        return 0
+
+    def height(self):
+        if self.is_image():
+            with open(self.file.path, "rb") as f:
+                try:
+                    image = Image.open(f)
+                except IOError:
+                    return 0
+            image.verify()
+            return image.size[1]
+        return 0
+
+
+class FileRefField(models.ForeignKey):
+
+    """A foreign key to a File, constrained to only select image files."""
+
+    def __init__(self, **kwargs):
+        kwargs["to"] = File
+        kwargs.setdefault("related_name", "+")
+        kwargs.setdefault("on_delete", models.PROTECT)
+        super(FileRefField, self).__init__(**kwargs)
+
+    def formfield(self, **kwargs):
+        defaults = {
+            "widget": ForeignKeyRawIdWidget(self.rel, admin.site),
+        }
+        return super(FileRefField, self).formfield(**defaults)
+
+
+IMAGE_FILTER = {
+    "file__iregex": r"\.(png|gif|jpg|jpeg)$"
+}
+
+
+class ImageRefField(FileRefField):
+
+    """A foreign key to a File, constrained to only select image files."""
+
+    def __init__(self, **kwargs):
+        kwargs["limit_choices_to"] = IMAGE_FILTER
+        super(ImageRefField, self).__init__(**kwargs)
+
+
+VIDEO_FILTER = {
+    "file__iregex": r"\.(webm|mp4|m4v)$"
+}
+
+
+class VideoFileRefField(FileRefField):
+
+    """A foreign key to a File, constrained to only select video files."""
+
+    def __init__(self, **kwargs):
+        kwargs["limit_choices_to"] = VIDEO_FILTER
+        super(VideoFileRefField, self).__init__(**kwargs)
+
+
+class Video(models.Model):
+
+    title = models.CharField(
+        max_length=200,
+    )
+
+    image = ImageRefField(
+        blank=True,
+        null=True,
+    )
+
+    high_resolution_mp4 = VideoFileRefField(
+        verbose_name="high resolution MP4",
+        blank=True,
+        null=True,
+    )
+
+    low_resolution_mp4 = VideoFileRefField(
+        verbose_name="low resolution MP4",
+        blank=True,
+        null=True
+    )
+
+    webm = VideoFileRefField(
+        verbose_name="WebM",
+        blank=True,
+        null=True,
+    )
+
+    def __unicode__(self):
+        """Returns the title of the media."""
+        return self.title
+
+    class Meta:
+        ordering = ("title",)
+
+
+class VideoRefField(models.ForeignKey):
+
+    """A foreign key to a File, constrained to only select image files."""
+
+    def __init__(self, **kwargs):
+        kwargs["to"] = Video
+        kwargs.setdefault("related_name", "+")
+        kwargs.setdefault("on_delete", models.PROTECT)
+        super(VideoRefField, self).__init__(**kwargs)
+
+    def formfield(self, **kwargs):
+        defaults = {
+            "widget": ForeignKeyRawIdWidget(self.rel, admin.site),
+        }
+        return super(VideoRefField, self).formfield(**defaults)
---
+++ packages/onespacemedia-cms/onespacemedia-cms-1.6.1.tar.gz/onespacemedia-cms-1.6.1/cms/apps/pages/views.py
@@ -0,0 +1,22 @@
+"""Views used by the pages app."""
+
+from django.contrib.contenttypes.models import ContentType
+from django.views.generic import TemplateView
+
+
+class ContentIndexView(TemplateView):
+
+    """Displays the index page for a page."""
+
+    def get_template_names(self):
+        """Returns the list of template names."""
+        content_cls = ContentType.objects.get_for_id(self.request.pages.current.content_type_id).model_class()
+        params = {
+            "model_name": content_cls.__name__.lower(),
+            "app_label": content_cls._meta.app_label,
+        }
+        return (
+            "{app_label}/{model_name}.html".format(**params),
+            "{app_label}/base.html".format(**params),
+            "base.html",
+        )
