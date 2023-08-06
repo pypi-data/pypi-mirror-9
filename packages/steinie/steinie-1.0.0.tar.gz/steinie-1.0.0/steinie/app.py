93067b28dd0a72a85e3494bac390df9a8f8b2651
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:15 +0000

    Add package steinie/1.0.0/steinie-1.0.0.tar.gz

---
+++ packages/steinie/steinie-1.0.0.tar.gz/steinie-1.0.0/steinie/app.py
@@ -0,0 +1,35 @@
+from werkzeug import serving
+from werkzeug import wrappers
+
+from . import routing
+from . import utils
+
+
+class Steinie(routing.Router):
+    def __init__(self, host="127.0.0.1", port=5151, debug=False):
+        self.host = host
+        self.port = port
+        self.debug = debug
+        super(Steinie, self).__init__()
+
+    def __call__(self, environ, start_response):
+        return self.wsgi_app(environ, start_response)
+
+    def wsgi_app(self, environ, start_response):
+        request = wrappers.Request(environ)
+        response = wrappers.Response()
+        new_response = utils.wrap_middleware_around_route(
+            self.middleware,
+            self.handle,
+            self
+        )(request, response)
+        if not isinstance(new_response, wrappers.Response):
+            if new_response is None:
+                new_response = ""
+            response.data = new_response
+        else:
+            response = new_response
+        return response(environ, start_response)
+
+    def run(self):
+        serving.run_simple(self.host, self.port, self, use_debugger=self.debug)
