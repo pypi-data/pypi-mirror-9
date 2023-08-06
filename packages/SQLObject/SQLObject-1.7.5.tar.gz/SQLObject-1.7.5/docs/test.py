97e4bbf9af54594571422704b08b6b3c6801fe5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:52 +0000

    Add package SQLObject/1.7.5/SQLObject-1.7.5.tar.gz

---
+++ packages/SQLObject/SQLObject-1.7.5.tar.gz/SQLObject-1.7.5/docs/test.py
@@ -0,0 +1,12 @@
+import sys, os
+import doctest
+
+sys.path.insert(
+    0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+def test():
+    for doc in ['SQLObject.txt']:
+        doctest.testfile(doc, optionflags=doctest.ELLIPSIS)
+
+if __name__ == '__main__':
+    test()
