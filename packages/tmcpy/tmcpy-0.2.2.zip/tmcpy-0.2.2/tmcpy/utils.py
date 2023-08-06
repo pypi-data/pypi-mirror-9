9b8e58aca475569560d99f9c31b7cf107f4d3f36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:38 +0000

    Add package tmcpy/0.2.2/tmcpy-0.2.2.zip

---
+++ packages/tmcpy/tmcpy-0.2.2.zip/tmcpy-0.2.2/tmcpy/utils.py
@@ -0,0 +1,38 @@
+# coding: utf-8
+from __future__ import absolute_import, unicode_literals
+import six
+
+from tmcpy.message import ConfirmMessage, QueryMessage
+from tmcpy.messageio import writer
+
+__all__ = [
+    'confirm_message',
+    'query_message',
+    'to_binary',
+]
+
+
+def confirm_message(message_id, token):
+    cm = ConfirmMessage()
+    cm.token = token
+    cm.update_content({'id': message_id})
+    return writer(cm)
+
+
+def query_message(**kwargs):
+    return writer(QueryMessage(**kwargs))
+
+
+def to_binary(value, encoding='utf-8'):
+    """Convert value to binary string, default encoding is utf-8
+
+    :param value: Value to be converted
+    :param encoding: Desired encoding
+    """
+    if not value:
+        return b''
+    if isinstance(value, six.binary_type):
+        return value
+    if isinstance(value, six.text_type):
+        return value.encode(encoding)
+    return six.binary_type(value)
