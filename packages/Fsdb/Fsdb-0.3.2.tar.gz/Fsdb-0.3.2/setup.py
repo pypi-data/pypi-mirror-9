5c94b2643fb7cf902a94b12e038ca7d60836c718
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:23:31 +0000

    Add package Fsdb/0.3.2/Fsdb-0.3.2.tar.gz

---
+++ packages/Fsdb/Fsdb-0.3.2.tar.gz/Fsdb-0.3.2/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+import os
+
+def read(fname):
+    with open(os.path.join(os.path.dirname(__file__), fname)) as buf:
+        return buf.read()
+
+setup(
+    name = "Fsdb",
+    version = "0.3.2",
+    packages=['fsdb'],
+
+    author = "Ael",
+    author_email = "tommy.ael@gmail.com",
+    long_description=read('README.rst'),
+    description = ("File system database. Easily manage file storing"),
+    keywords = "database file storing db",
+    url = "https://github.com/ael-code/pyFsdb",
+    license = "LGPLv3",
+    test_suite='tests',
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
+        "Topic :: Database",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 2 :: Only"
+    ]
+)
+
