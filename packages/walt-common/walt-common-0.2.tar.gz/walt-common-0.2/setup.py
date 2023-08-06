6009e8a1fd7f501c5d88be0db408e3ce523cc98f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:21 +0000

    Add package walt-common/0.2/walt-common-0.2.tar.gz

---
+++ packages/walt-common/walt-common-0.2.tar.gz/walt-common-0.2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+setup(
+    name = "walt-common",
+    version = "0.2",
+    packages = find_packages(),
+    install_requires = ['rpyc>=3.3','plumbum>=1.4.2'],
+
+    # metadata for upload to PyPI
+    author = "Etienne Duble",
+    author_email = "etienne.duble@imag.fr",
+    description = "WalT (Wireless Testbed) common python modules.",
+    license = "LGPL",
+    keywords = "WalT wireless testbed",
+    url = "http://walt.forge.imag.fr/",
+
+    namespace_packages = ['walt'],
+)
+
