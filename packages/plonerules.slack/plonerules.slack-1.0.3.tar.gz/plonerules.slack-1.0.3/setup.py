171f2a704d5928e465ab7c67ff376fcba9fdd3f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:24 +0000

    Add package plonerules.slack/1.0.3/plonerules.slack-1.0.3.tar.gz

---
+++ packages/plonerules.slack/plonerules.slack-1.0.3.tar.gz/plonerules.slack-1.0.3/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0.3'
+
+setup(name='plonerules.slack',
+      version=version,
+      description="Post to a slack channel action for plone rules",
+      # Get more strings from
+      # http://pypi.python.org/pypi?:action=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        ],
+      keywords='slack plone rules',
+      author='Steve Giraud',
+      author_email='sgiraud01@gmail.com',
+      url='https://github.com/sgiraud01/plonerules.slack/',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['plonerules'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'slacker',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      setup_requires=["PasteScript"],
+      paster_plugins=["ZopeSkel"],
+      )
