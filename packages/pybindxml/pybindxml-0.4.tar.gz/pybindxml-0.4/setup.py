6949e12e931a339765ecfa3ecb20ca4044f0ba31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:51 +0000

    Add package pybindxml/0.4/pybindxml-0.4.tar.gz

---
+++ packages/pybindxml/pybindxml-0.4.tar.gz/pybindxml-0.4/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='pybindxml',
+    version = '0.4',
+    description='Read ISC BIND\'s statistics XML for processing.',
+    author='Jeffrey Forman',
+    author_email='code@jeffreyforman.net',
+    license='MIT',
+    packages = ['pybindxml'],
+    url = 'http:://github.com/jforman/pybindxml',
+    install_requires = ['beautifulsoup4', 'lxml'],
+)
