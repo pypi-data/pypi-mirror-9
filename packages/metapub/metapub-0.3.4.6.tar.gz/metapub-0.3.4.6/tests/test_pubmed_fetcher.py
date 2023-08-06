fe8354fa0bc7d2fb11093e745143371398551074
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:15 +0000

    Add package metapub/0.3.4.6/metapub-0.3.4.6.tar.gz

---
+++ packages/metapub/metapub-0.3.4.6.tar.gz/metapub-0.3.4.6/tests/test_pubmed_fetcher.py
@@ -0,0 +1,14 @@
+import unittest
+
+from metapub import PubMedFetcher
+from metapub.pubmedcentral import *
+
+class TestPubmedFetcher(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+ 
