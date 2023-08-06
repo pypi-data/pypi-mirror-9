48ecf4c5faa9c002af9110d1f4c044dd2c2fdfb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:23 +0000

    Add package tblib/1.0.1/tblib-1.0.1.tar.gz

---
+++ packages/tblib/tblib-1.0.1.tar.gz/tblib-1.0.1/setup.py
@@ -0,0 +1,37 @@
+# -*- encoding: utf8 -*-
+from setuptools import setup, find_packages
+
+import os
+
+setup(
+    name="tblib",
+    version="1.0.1",
+    url='https://github.com/ionelmc/python-tblib',
+    download_url='',
+    license='BSD',
+    description="Traceback fiddling library.",
+    long_description=open(os.path.join(os.path.dirname(__file__), 'README.rst')).read(),
+    author='Ionel Cristian Mărieș',
+    author_email='contact@ionelmc.ro',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: Unix',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Debuggers',
+        'Topic :: Utilities',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+    ],
+    install_requires=[
+        'six',
+    ]
+)
---
+++ packages/tblib/tblib-1.0.1.tar.gz/tblib-1.0.1/src/tblib/__init__.py
@@ -0,0 +1,140 @@
+try:
+    from __pypy__ import tproxy
+except ImportError:
+    tproxy = None
+try:
+    from .cpython import tb_set_next
+except ImportError:
+    tb_set_next = None
+
+if not tb_set_next and not tproxy:
+    raise ImportError("Cannot use tblib. Runtime not supported.")
+
+import sys
+
+from types import CodeType
+from types import TracebackType
+
+PY3 = sys.version_info[0] == 3
+
+
+class _AttrDict(dict):
+    def __getattr__(self, attr):
+        return self[attr]
+
+
+class __traceback_maker(Exception):
+    pass
+
+
+class Code(object):
+    def __init__(self, code):
+        self.co_filename = code.co_filename
+        self.co_name = code.co_name
+        self.co_nlocals = code.co_nlocals
+        self.co_stacksize = code.co_stacksize
+        self.co_flags = code.co_flags
+        self.co_firstlineno = code.co_firstlineno
+
+
+class Frame(object):
+    def __init__(self, frame):
+        self.f_globals = {
+            k: v for k, v in frame.f_globals.items() if k in ("__file__", "__name__")
+        }
+        self.f_code = Code(frame.f_code)
+
+
+class Traceback(object):
+    def __init__(self, tb):
+        self.tb_frame = Frame(tb.tb_frame)
+        self.tb_lineno = tb.tb_lineno
+        if tb.tb_next is None:
+            self.tb_next = None
+        else:
+            self.tb_next = Traceback(tb.tb_next)
+
+    def as_traceback(self):
+        if tproxy:
+            return tproxy(TracebackType, self.__tproxy_handler)
+        elif tb_set_next:
+            f_code = self.tb_frame.f_code
+            code = compile('\n' * (self.tb_lineno - 1) + 'raise __traceback_maker', self.tb_frame.f_code.co_filename, 'exec')
+            if PY3:
+                code = CodeType(
+                    0, 0,
+                    f_code.co_nlocals, f_code.co_stacksize, f_code.co_flags,
+                    code.co_code, code.co_consts, code.co_names, code.co_varnames,
+                    f_code.co_filename, f_code.co_name,
+                    code.co_firstlineno, b"",
+                    (), ()
+                )
+            else:
+                code = CodeType(
+                    0,
+                    f_code.co_nlocals, f_code.co_stacksize, f_code.co_flags,
+                    code.co_code, code.co_consts, code.co_names, code.co_varnames,
+                    f_code.co_filename.encode(), f_code.co_name.encode(),
+                    code.co_firstlineno, b"",
+                    (), ()
+                )
+
+            try:
+                exec(code, self.tb_frame.f_globals, {})
+            except:
+                tb = sys.exc_info()[2].tb_next
+                tb_set_next(tb, self.tb_next and self.tb_next.as_traceback())
+                return tb
+        else:
+            raise RuntimeError("Cannot re-create traceback !")
+
+    def __tproxy_handler(self, operation, *args, **kwargs):
+        if operation in ('__getattribute__', '__getattr__'):
+            if args[0] == 'tb_next':
+                return self.tb_next and self.tb_next.as_traceback()
+            else:
+                return getattr(self, args[0])
+        else:
+            return getattr(self, operation)(*args, **kwargs)
+
+    def to_dict(self):
+        """Convert a Traceback into a dictionary representation"""
+        if self.tb_next is None:
+            tb_next = None
+        else:
+            tb_next = self.tb_next.to_dict()
+
+        code = {
+            k: v
+            for k, v in self.tb_frame.f_code.__dict__.items()
+            if k.startswith('co_')
+        }
+        frame = {
+            'f_globals': self.tb_frame.f_globals,
+            'f_code': code
+        }
+        return {
+            'tb_frame': frame,
+            'tb_lineno': self.tb_lineno,
+            'tb_next': tb_next
+        }
+
+    @classmethod
+    def from_dict(cls, dct):
+        if dct['tb_next']:
+            tb_next = cls.from_dict(dct['tb_next'])
+        else:
+            tb_next = None
+
+        frame = _AttrDict((
+            ('f_globals', dct['tb_frame']['f_globals']),
+            ('f_code', _AttrDict((k, v) for k, v in dct['tb_frame']['f_code'].items()))
+        ))
+        tb = _AttrDict((
+            ('tb_frame', frame),
+            ('tb_lineno', dct['tb_lineno']),
+            ('tb_next', tb_next)
+        ))
+        return cls(tb)
+
+
