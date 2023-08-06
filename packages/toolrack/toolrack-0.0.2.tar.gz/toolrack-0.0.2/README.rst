578d6feca04b320ef5a4303ac2a48b5702a2a4ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:46 +0000

    Add package toolrack/0.0.2/toolrack-0.0.2.tar.gz

---
+++ packages/toolrack/toolrack-0.0.2.tar.gz/toolrack-0.0.2/README.rst
@@ -0,0 +1,53 @@
+ToolRack
+========
+
+This library is a collection of miscellaneous utility functions and classes.
+
+`documentation <http://toolrack.readthedocs.org/>`_ |
+`sources <https://bitbucket.org/ack/toolrack>`_ |
+`issues <https://bitbucket.org/ack/toolrack/issues>`_
+
+
+Install
+-------
+
+ToolRack can be installed from `PyPI <https://pypi.python.org/>`_.
+
+As a user run::
+
+  $ pip install toolrack
+
+
+Development installation
+------------------------
+
+The source tree is available available at
+`<https://bitbucket.com/ack/toolrack>`_, users should install `Virtualenv
+<https://virtualenv.pypa.io/>`_ for development.
+
+As a user run::
+
+  $ virtualenv <target-dir>
+  $ . <target-dir>/bin/activate
+  $ git clone https://bitbucket.org/ack/toolrack.git
+  $ cd toolrack
+  $ python setup.py develop
+
+
+Available modules
+-----------------
+
+:mod:`toolrack.fsmap`
+     Dict-like access to directories and contained files.
+
+:mod:`toolrack.json_util`
+     Formatting utility to indent JSON text.
+
+:mod:`toolrack.path`
+     Functions for searching directory trees.
+
+:mod:`toolrack.script`
+     A Script class to redure boilerplate when creating scripts.
+
+:mod:`toolrack.testing`
+     Unit-test base classes and fixtures providing common functionalities.
---
+++ packages/toolrack/toolrack-0.0.2.tar.gz/toolrack-0.0.2/setup.py
@@ -0,0 +1,51 @@
+#
+# This file is part of ToolRack.
+
+# ToolRack is free software: you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+
+# ToolRack is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with ToolRack.  If not, see <http://www.gnu.org/licenses/>.
+
+from setuptools import setup, find_packages
+
+from toolrack import __version__, __doc__ as description
+
+
+config = {
+    'name': 'toolrack',
+    'version': __version__,
+    'license': 'GPLv3+',
+    'description': description,
+    'long_description': open('README.rst').read(),
+    'author': 'Alberto Donato',
+    'author_email': '<alberto.donato@gmail.com>',
+    'maintainer': 'Alberto Donato',
+    'maintainer_email': '<alberto.donato@gmail.com>',
+    'url': 'https://bitbucket.org/ack/toolrack',
+    'download_url': 'https://bitbucket.org/ack/toolrack/downloads',
+    'packages': find_packages(),
+    'include_package_data': True,
+    'entry_points': {'console_scripts': [
+        'json-indent = toolrack.scripts.json_indent:script']},
+    'test_suite': 'toolrack',
+    'install_requires': ['fixtures'],
+    'keywords': 'library utility',
+    'classifiers': [
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Utilities',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules']}
+
+setup(**config)
