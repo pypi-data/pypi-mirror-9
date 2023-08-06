f30272bdefb6c48c801a78ad8af1d9784f8ef649
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:31 +0000

    Add package python-gist/0.1.11/python-gist-0.1.11.tar.gz

---
+++ packages/python-gist/python-gist-0.1.11.tar.gz/python-gist-0.1.11/setup.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+
+import setuptools
+import gist
+
+setuptools.setup(
+        name='python-gist',
+        version=gist.__version__,
+        description='Manage github gists',
+        license='MIT',
+        long_description=(open('README.rst').read()),
+        author='Joshua Downer',
+        author_email='joshua.downer@gmail.com',
+        url='http://github.com/jdowner/gist',
+        keywords='gist github git',
+        packages=['gist'],
+        package_data={
+          '': ['share/*', '*.rst', 'LICENSE'],
+        },
+        data_files=[
+          ('share/gist/', [
+              'README.rst',
+              'LICENSE',
+              'share/gist.bash',
+              'share/gist-fzsl.bash',
+              'share/gist-fzf.bash',
+              ]),
+        ],
+        scripts=['bin/gist'],
+        install_requires=[
+            'docopt',
+            'pep8',
+            'requests',
+            'responses',
+            'simplejson',
+            'six',
+            'tox',
+            ],
+        platforms=['Unix'],
+        classifiers=[
+            'Development Status :: 3 - Alpha',
+            'Environment :: Console',
+            'Intended Audience :: Developers',
+            'Intended Audience :: End Users/Desktop',
+            'Intended Audience :: System Administrators',
+            'License :: OSI Approved :: MIT License',
+            'Operating System :: Unix',
+            'Programming Language :: Python',
+            'Programming Language :: Python :: 2',
+            'Programming Language :: Python :: 2.7',
+            'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3.3',
+            'Programming Language :: Python :: 3.4',
+            'Topic :: Software Development',
+            'Topic :: Software Development :: Version Control',
+            'Topic :: Utilities',
+            ]
+        )
