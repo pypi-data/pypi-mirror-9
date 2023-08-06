a02a3fd4109cdb43e122d3a3058722e46a0d93c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:23 +0000

    Add package TensorToolbox/0.3.7/TensorToolbox-0.3.7.tar.gz

---
+++ packages/TensorToolbox/TensorToolbox-0.3.7.tar.gz/TensorToolbox-0.3.7/Docs/Sphinx/auto/refs.el
@@ -0,0 +1,11 @@
+(TeX-add-style-hook "refs"
+ (lambda ()
+    (LaTeX-add-bibitems
+     "Khoromskij2011"
+     "Khoromskij2010"
+     "Oseledets2011"
+     "Oseledets2010"
+     "Savostyanov2013"
+     "Savostyanov2011"
+     "Bigoni2015")))
+
