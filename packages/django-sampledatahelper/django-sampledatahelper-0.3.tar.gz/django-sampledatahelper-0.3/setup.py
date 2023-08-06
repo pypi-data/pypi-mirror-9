87d423ed020a30a8b290f37c64b5a548a3e3943c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:18 +0000

    Add package django-sampledatahelper/0.3/django-sampledatahelper-0.3.tar.gz

---
+++ packages/django-sampledatahelper/django-sampledatahelper-0.3.tar.gz/django-sampledatahelper-0.3/setup.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+from setuptools import setup, find_packages
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'tests.settings'
+
+setup(
+    name = 'django-sampledatahelper',
+    version = ":versiontools:sampledatahelper:",
+    description = "Helper class for generate sampledata",
+    long_description = "",
+    keywords = 'django, data, example',
+    author = 'Jesús Espino García',
+    author_email = 'jespinog@gmail.com',
+    url = 'https://github.com/kaleidos/django-sampledatahelper',
+    license = 'BSD',
+    include_package_data = True,
+    packages = find_packages(),
+    package_data={
+        'sampledatahelper': ['static/*', 'l10n/names/*',]
+    },
+    install_requires=[
+        'six',
+        'django',
+        'sampledata',
+    ],
+    setup_requires = [
+        'versiontools >= 1.8',
+    ],
+    test_suite = 'nose.collector',
+    tests_require = ['nose >= 1.3.0', 'django >= 1.3.0'],
+    classifiers = [
+        "Programming Language :: Python",
+        'Development Status :: 4 - Beta',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP',
+    ]
+)
