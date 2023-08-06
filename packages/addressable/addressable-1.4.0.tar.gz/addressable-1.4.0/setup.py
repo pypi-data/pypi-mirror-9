2e1e9e76afe0d63dbda15be21411722db4560413
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:53 +0000

    Add package addressable/1.4.0/addressable-1.4.0.tar.gz

---
+++ packages/addressable/addressable-1.4.0.tar.gz/addressable-1.4.0/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(name='addressable',
+      description='Use lists like you would dictionaries.',
+      long_description=open('README.rst').read(),
+      author='Stijn Debrouwere',
+      author_email='stijn@debrouwere.org',
+      url='https://github.com/debrouwere/python-addressable/',
+      download_url='http://www.github.com/debrouwere/python-addressable/tarball/master',
+      version='1.4.0',
+      license='ISC',
+      packages=find_packages(),
+      install_requires=[
+            'pylev>=1',      
+            ], 
+      keywords='utility',
+      classifiers=['Development Status :: 5 - Production/Stable',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved :: MIT License',
+                   'Operating System :: OS Independent',
+                   'Topic :: Utilities',
+                   'Programming Language :: Python',
+                   'Programming Language :: Python :: 2',
+                   'Programming Language :: Python :: 2.7', 
+                   'Programming Language :: Python :: 3',
+                   'Programming Language :: Python :: 3.4', 
+                   ],
+      )
