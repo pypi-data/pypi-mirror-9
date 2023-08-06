dbaddccc0a7412bd9df0435335a42b0be0c8d17b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:14 +0000

    Add package jdatetime/1.4/jdatetime-1.4.tar.gz

---
+++ packages/jdatetime/jdatetime-1.4.tar.gz/jdatetime-1.4/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name='jdatetime',
+        version='1.4',
+        packages=['jdatetime',],
+        license='Python Software Foundation License',
+        keywords='Jalali implementation of Python datetime',
+        platforms='any',
+        long_description=open('README.txt').read(),
+)
