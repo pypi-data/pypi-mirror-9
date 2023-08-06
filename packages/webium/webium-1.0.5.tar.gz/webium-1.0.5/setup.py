b35019a5977e47911f62ce8f7ff79c3cea7bc872
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:53 +0000

    Add package webium/1.0.5/webium-1.0.5.tar.gz

---
+++ packages/webium/webium-1.0.5.tar.gz/webium-1.0.5/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+version = '1.0.5'
+
+setup(
+    name='webium',
+    version=version,
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+    ],
+    packages=[
+        'webium',
+    ],
+    include_package_data=True,
+    author='Wargaming.NET',
+    author_email='i_khrol@wargaming.net',
+    url='https://github.com/wgnet/webium',
+    description='Webium is a Page Object pattern implementation library for Python '
+                '(http://martinfowler.com/bliki/PageObject.html). '
+                'It allows you to extend WebElement class to your custom controls '
+                'like Link, Button and group them as pages.',
+    install_requires=[
+        'selenium',
+        'waiting',
+    ],
+    entry_points={
+        'nose.plugins': ['browser_closer = webium.plugins.browser_closer:BrowserCloserPlugin'],
+    },
+)
