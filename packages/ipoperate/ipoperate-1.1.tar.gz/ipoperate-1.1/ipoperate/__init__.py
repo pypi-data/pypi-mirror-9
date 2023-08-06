9b37cbd8d2a1e12181d48f968f2d800cdf6ee2b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:36:47 +0000

    Add package ipoperate/1.1/ipoperate-1.1.tar.gz

---
+++ packages/ipoperate/ipoperate-1.1.tar.gz/ipoperate-1.1/ipoperate/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+ 
+__VERSION__ = '1.1' 
+__AUTHOR__ = 'xdays'
+
+try:
+    from .ipoperate import *
+except:
+    error = 'import error'
---
+++ packages/ipoperate/ipoperate-1.1.tar.gz/ipoperate-1.1/setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+ 
+from setuptools import setup
+import ipoperate
+import os
+
+
+setup(
+    name='ipoperate',
+    version=ipoperate.__VERSION__,
+    description='magic functions to operate ips and nets',
+    long_description = open('README.md').read(),
+    license='License :: OSI Approved :: MIT License',
+    platforms='Platform Independent',
+    author='Alair Zhang (xdays)',
+    author_email='easedays@gmail.com',
+    url='http://www.xdays.info',
+    packages=['ipoperate'],
+    keywords=['ipoperate', 'ip2net', 'nets2net', 'ip2bin'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+    ],
+    scripts=['bin/iprange']
+) 
