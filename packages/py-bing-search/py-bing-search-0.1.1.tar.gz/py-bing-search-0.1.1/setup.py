3effa5c7116329bb3a1b4f990cb196c07d135747
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:08 +0000

    Add package py-bing-search/0.1.1/py-bing-search-0.1.1.tar.gz

---
+++ packages/py-bing-search/py-bing-search-0.1.1.tar.gz/py-bing-search-0.1.1/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+import os
+import platform
+
+DESCRIPTION = "A simple lightweight python wrapper for the Azure Bing Search API."
+VERSION = '0.1.1'
+LONG_DESCRIPTION = None
+try:
+    LONG_DESCRIPTION = open('README.md').read()
+except:
+    pass
+
+CLASSIFIERS = [
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Topic :: Software Development :: Libraries :: Python Modules',
+]
+
+INSTALL_REQUIRES = [
+    'requests',
+]
+
+setup(
+    name='py-bing-search',
+    version=VERSION,
+    author=u'tristan tao',
+    author_email='tristan@teamleada.com',
+    py_modules=['py-bing-search'],
+    url='https://github.com/tristantao/py-bing-search',
+    download_url = 'https://github.com/tristantao/py-bing-search/tarball/0.1.1',
+    license='MIT',
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    classifiers=CLASSIFIERS,
+    install_requires=INSTALL_REQUIRES,
+)
