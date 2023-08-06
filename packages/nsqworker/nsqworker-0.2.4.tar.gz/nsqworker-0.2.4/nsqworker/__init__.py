2da17fb8dad8986ce8ec12bbb3cb216bef03f16e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:04 +0000

    Add package nsqworker/0.2.4/nsqworker-0.2.4.tar.gz

---
+++ packages/nsqworker/nsqworker-0.2.4.tar.gz/nsqworker-0.2.4/nsqworker/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2013 feilong.me All rights reserved.
+#
+# @author: Felinx Lee <felinx.lee@gmail.com>
+# Created on May 4, 2013
+#
+
+from nsqworker.workers.worker import Worker
+
+__version__ = '0.2.4'
