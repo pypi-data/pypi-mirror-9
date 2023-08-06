85469a29fba7a70f1140e4275f27006077910f8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:47 +0000

    Add package meerkat/0.2.1/meerkat-0.2.1.tar.gz

---
+++ packages/meerkat/meerkat-0.2.1.tar.gz/meerkat-0.2.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+setup(
+  name = 'meerkat',
+  packages = ['meerkat'],
+  version = '0.2.1',
+  description = 'A program for reciprocal space reconstruction',
+  author = 'Arkadiy Simonov, Dmitry Logvinovich',
+  author_email = 'arkadiy.simonov@alumni.ethz.ch',
+  url = 'https://github.com/aglie/meerkat.git',
+#  download_url = 
+  keywords = ['crystallography', 'single crystal', 'reciprocal space reconstruction'],
+  classifiers = [],
+  install_requires = ['fabio','h5py'],
+)
