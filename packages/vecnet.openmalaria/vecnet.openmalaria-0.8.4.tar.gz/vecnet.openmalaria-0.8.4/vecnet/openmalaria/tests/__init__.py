71621875fed5764362717cf310ee3d3bd7a31851
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:50 +0000

    Add package vecnet.openmalaria/0.8.4/vecnet.openmalaria-0.8.4.tar.gz

---
+++ packages/vecnet.openmalaria/vecnet.openmalaria-0.8.4.tar.gz/vecnet.openmalaria-0.8.4/vecnet/openmalaria/tests/__init__.py
@@ -0,0 +1,8 @@
+from .test_experiment import *
+from .test_output_parser import *
+from .test_scenario import *
+from .test_get_schema_version import *
+from .test_healthsystem import *
+
+if __name__ == "__main__":
+    unittest.main()
