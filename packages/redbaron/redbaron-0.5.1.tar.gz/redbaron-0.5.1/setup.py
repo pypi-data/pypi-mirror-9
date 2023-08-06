5f1c73f48da714ffeea835b90ed64b228b94c164
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:56 +0000

    Add package redbaron/0.5.1/redbaron-0.5.1.tar.gz

---
+++ packages/redbaron/redbaron-0.5.1.tar.gz/redbaron-0.5.1/setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+# -*- coding:Utf-8 -*-
+
+from setuptools import setup
+
+try:
+    from pypandoc import convert
+    read_md = lambda f: convert(f, 'rst')
+except ImportError:
+    print("warning: pypandoc module not found, could not convert Markdown to RST")
+    read_md = lambda f: open(f, 'r').read()
+
+
+setup(name='redbaron',
+      version='0.5.1',
+      description='Abstraction on top of baron, a FST for python to make writing refactoring code a realistic task',
+      author='Laurent Peuch',
+      long_description=read_md("README.md") + "\n\n" + open("CHANGELOG", "r").read(),
+      author_email='cortex@worlddomination.be',
+      url='https://github.com/Psycojoker/redbaron',
+      install_requires=['baron>=0.6.1', 'pygments'],
+      license= 'lgplv3+',
+      py_modules=["redbaron"],
+      keywords='baron fst ast refactoring',
+      classifiers=['Development Status :: 3 - Alpha',
+                   'Intended Audience :: Developers',
+                   'Programming Language :: Python :: 2',
+                   'Programming Language :: Python :: 2.7',
+                   'Programming Language :: Python :: 3',
+                   'Programming Language :: Python :: 3.3',
+                   'Programming Language :: Python :: 3.4',
+                   'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
+                   'Topic :: Software Development',
+                   'Topic :: Software Development :: Code Generators',
+                   'Topic :: Software Development :: Libraries',
+                   ],
+     )
