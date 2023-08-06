a9a18823e52c4b1bfa894aa939f337a7efd5b2af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:01 +0000

    Add package consumerism/0.2.3/consumerism-0.2.3-py2-none-any.whl

---
+++ packages/consumerism/consumerism-0.2.3-py2-none-any.whl/consumerism/__init__.py
@@ -0,0 +1,29 @@
+import logging
+
+from .consumer import Consumer
+from .processor import Processor
+from .message import AbstractMessage
+
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
+
+
+class AbstractResource(object):
+    """
+    Base class for a resource.
+    Do not inherit, just implement the interface.
+    """
+
+    logger = None
+
+    def invoke(self, *args, **kwargs):
+        """
+        Invokes the named action with the supplied parameters.
+        """
+        raise NotImplementedError
+
+
+__all__ = [
+    'Consumer', 'Processor',
+    'AbstractMessage', 'AbstractResource'
+]
