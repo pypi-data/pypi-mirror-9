20be45024d8cc54b211586eb15ceb0bf469ce362
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:05 +0000

    Add package pyprofiler/29/pyprofiler-29.tar.gz

---
+++ packages/pyprofiler/pyprofiler-29.tar.gz/pyprofiler-29/setup.py
@@ -0,0 +1,28 @@
+# coding=utf-8
+"""
+Profiler utility for python
+Active8 (04-03-15)
+license: GNU-GPL2
+"""
+
+from setuptools import setup
+setup(name='pyprofiler',
+      version='29',
+      description='Profiler utility for python, graphical and textual, whole program or segments',
+      url='https://github.com/erikdejonge/pyprofiler',
+      author='Erik de Jonge',
+      author_email='erik@a8.nl',
+      license='GPL',
+      packages=['pyprofiler'],
+      zip_safe=True,
+      classifiers=[
+          "Programming Language :: Python",
+          "Programming Language :: Python :: 3",
+          "Development Status :: 4 - Beta ",
+          "Intended Audience :: Developers",
+          "License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
+          "Operating System :: POSIX",
+          "Topic :: Software Development :: Libraries :: Python Modules",
+          "Topic :: Software Development :: Testing",
+          "Topic :: System",
+      ])
