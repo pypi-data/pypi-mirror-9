36df2efaca482bd699427d345ca465b617bc7aa6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:36:54 +0000

    Add package AdvOptParse/0.2.8/AdvOptParse-0.2.8.tar.gz

---
+++ packages/AdvOptParse/AdvOptParse-0.2.8.tar.gz/AdvOptParse-0.2.8/advoptparse/__init__.py
@@ -0,0 +1,15 @@
+# =========================================================
+# Copyright: (c) 2015 Katharina Sabel
+# License  : GPL 2.0 (See LICENSE)
+# Comment  : Init file with version number
+#
+# =========================================================
+
+# import pkg_resources
+# __VERSION__ = pkg_resources.require("advoptparse")[0].version
+
+# __all__ = ['parser', 'version']
+
+import parser
+import version
+import console
