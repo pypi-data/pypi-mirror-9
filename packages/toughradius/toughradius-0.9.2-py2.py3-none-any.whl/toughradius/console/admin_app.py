8df998f793f6cec4c5c6416dd1e9b9336770e351
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:17 +0000

    Add package toughradius/0.9.2/toughradius-0.9.2-py2.py3-none-any.whl

---
+++ packages/toughradius/toughradius-0.9.2-py2.py3-none-any.whl/toughradius/console/admin_app.py
@@ -0,0 +1,148 @@
+#!/usr/bin/env python
+#coding:utf-8
+from autobahn.twisted import choosereactor
+choosereactor.install_optimal_reactor(True)
+import sys,os
+from twisted.internet import reactor
+from twisted.python.logfile import DailyLogFile
+from bottle import TEMPLATE_PATH,MakoTemplate
+from bottle import mako_template as render
+from bottle import run as runserver
+from toughradius.console.admin.admin import app as mainapp
+from toughradius.console.admin.ops import app as ops_app
+from toughradius.console.admin.business import app as bus_app
+from toughradius.console.admin.card import app as card_app
+from toughradius.console.admin.product import app as product_app
+from toughradius.console.base import *
+from toughradius.console.libs import sqla_plugin,utils
+from toughradius.console.libs.smail import mail
+from toughradius.console.websock import websock
+from toughradius.console import tasks
+from toughradius.console import models
+from toughradius.console import base
+import toughradius
+import bottle
+import functools
+import time
+
+reactor.suggestThreadPoolSize(30)
+
+subapps = [ops_app,bus_app,card_app,product_app]
+
+def error403(error):
+    return render("error",msg=u"Unauthorized access %s"%error.exception)
+    
+def error404(error):
+    return render("error",msg=u"Not found %s"%error.exception)
+
+def error500(error):
+    return render("error",msg=u"Server Internal error %s"%error.exception)
+
+def init_application(config):
+    log.msg("start init application...")
+    TEMPLATE_PATH.append(os.path.join(os.path.split(__file__)[0],"admin/views/"))
+    for _app in [mainapp]+subapps:
+        _app.error_handler[403] = error403
+        _app.error_handler[404] = error404
+        _app.error_handler[500] = error500
+        
+    log.msg("init plugins..")
+    engine,metadata = models.get_engine(config)
+    sqla_pg = sqla_plugin.Plugin(engine,metadata,keyword='db',create=False,commit=False,use_kwargs=False)
+    session = sqla_pg.new_session()
+    _sys_param_value = functools.partial(get_param_value,session)
+    _get_product_name = functools.partial(get_product_name,session)
+    
+    bottle.debug(_sys_param_value('radiusd_address')=='1')
+    
+    log.msg("init template context...")
+    MakoTemplate.defaults.update(**dict(
+        sys_version = toughradius.__version__,
+        get_cookie = get_cookie,
+        fen2yuan = utils.fen2yuan,
+        fmt_second = utils.fmt_second,
+        currdate = utils.get_currdate,
+        bb2mb = utils.bb2mb,
+        bbgb2mb = utils.bbgb2mb,
+        kb2mb = utils.kb2mb,
+        mb2kb = utils.mb2kb,
+        sec2hour = utils.sec2hour,
+        request = request,
+        sys_param_value = _sys_param_value,
+        get_product_name = _get_product_name,
+        permit = permit,
+        all_menus = permit.build_menus(order_cats=[u"系统管理",u"营业管理",u"运维管理"])
+    ))
+    
+    # connect radiusd websocket admin port 
+    log.msg("init websocket client...")
+    wsparam = (
+        _sys_param_value('radiusd_address'),
+        _sys_param_value('radiusd_admin_port')
+    )
+    reactor.callLater(1, websock.connect,*wsparam)
+    log.msg("init tasks...")
+    reactor.callLater(2, tasks.start_online_stat_job, sqla_pg.new_session)
+    reactor.callLater(3, tasks.start_flow_stat_job, sqla_pg.new_session)
+    reactor.callLater(4, tasks.start_expire_notify_job, sqla_pg.new_session)
+   
+    log.msg("init operator rules...")
+    for _super in session.query(models.SlcOperator.operator_name).filter_by(operator_type=0):
+        permit.bind_super(_super[0])
+        
+    log.msg("init sendmail..")
+    mail.setup(
+        server=_sys_param_value('smtp_server'),
+        user=_sys_param_value('smtp_user'),
+        pwd=_sys_param_value('smtp_pwd'),
+        fromaddr=_sys_param_value('smtp_user'),
+        sender=_sys_param_value('smtp_sender')
+    )
+
+    log.msg("mount app and install plugins...")
+    mainapp.install(sqla_pg)
+    for _app in subapps:
+        _app.install(sqla_pg)
+        mainapp.mount(_app.config['__prefix__'],_app)
+    
+
+###############################################################################
+# run server                                                                 
+###############################################################################
+from bottle import ServerAdapter
+class TwistedService(ServerAdapter):
+    def run(self, handler):
+        from twisted.web import server, wsgi
+        from twisted.python.threadpool import ThreadPool
+        from twisted.internet import reactor
+        thread_pool = ThreadPool()
+        thread_pool.start()
+        reactor.addSystemEventTrigger('after', 'shutdown', thread_pool.stop)
+        factory = server.Site(wsgi.WSGIResource(reactor, thread_pool, handler))
+        reactor.listenTCP(self.port, factory, interface=self.host)
+
+
+def run(config,is_service=False):
+    logfile = config.get('admin','logfile')
+    log.startLogging(DailyLogFile.fromFullPath(logfile))
+    # update aescipher,timezone
+    utils.aescipher.setup(config.get('DEFAULT','secret'))
+    base.scookie.setup(config.get('DEFAULT','secret'))
+    utils.update_tz(config.get('DEFAULT','tz'))
+    init_application(config)
+    if not is_service:
+        runserver(
+            mainapp, host='0.0.0.0', 
+            port=config.getint('admin','port') ,
+            debug=config.getboolean('DEFAULT','debug')  ,
+            reloader=False,
+            server="twisted"
+        )
+    else:
+        from twisted.web import server, wsgi
+        from twisted.python.threadpool import ThreadPool
+        from twisted.internet import reactor
+        from twisted.application import service, internet
+        factory = server.Site(wsgi.WSGIResource(reactor, reactor.getThreadPool(), mainapp))
+        return internet.TCPServer(config.getint('admin','port'), factory)
+
---
+++ packages/toughradius/toughradius-0.9.2-py2.py3-none-any.whl/toughradius/tools/config.py
@@ -0,0 +1,310 @@
+#!/usr/bin/env python
+#coding:utf-8
+import os
+import ConfigParser
+from toughradius.tools.shell import shell as sh
+from toughradius.tools.secret import gen_secret
+
+def find_config(conf_file=None):
+    windows_dir = os.getenv("WINDIR") and os.path.join(os.getenv("WINDIR"),'radiusd.conf') or None
+    cfgs = [
+        conf_file,
+        '/etc/radiusd.conf',
+        '/var/toughradius/radiusd.conf',
+        './radiusd.conf',
+        '~/radiusd.conf',
+        windows_dir
+    ]
+    config = ConfigParser.ConfigParser()
+    flag = False
+    for c in cfgs:
+        if c and os.path.exists(c):
+            config.read(c)
+            flag = True
+            break
+   
+    if not flag:
+        raise Exception('no config file')
+                
+    return config
+    
+    
+
+def setup_config():
+    sh.info("set config...")
+    config_path = sh.read('set your config file path,[ /etc/radiusd.conf ]') or '/etc/radiusd.conf'
+    config = ConfigParser.RawConfigParser()
+    sh.info("set default option")
+    config.set('DEFAULT', 'debug', (sh.read("set debug [0/1] [0]:") or '0') )
+    config.set('DEFAULT', 'tz', (sh.read("time zone [ CST-8 ]:") or 'CST-8') )
+    config.set('DEFAULT','secret',gen_secret(32))
+    
+    sh.info("set database option")
+    config.add_section('database')
+    config.set('database','dbtype', (sh.read("database type [mysql]:") or 'mysql' ))
+    config.set('database','host',( sh.read("database host [127.0.0.1]:") or '127.0.0.1' ))
+    config.set('database','port',(sh.read("database port [3306]:") or '3306'))
+    config.set('database','db',(sh.read("database dbname [toughradius]:") or 'toughradius' ))
+    config.set('database','user',(sh.read("database user [root]:") or 'root' ))
+    config.set('database','passwd',(sh.read("database passwd []:") or '' ))
+    config.set('database','maxusage',(sh.read("db pool maxusage [30]:") or '30'))
+    config.set('database','charset','utf8' )
+    
+    sh.info("set radiusd option")
+    config.add_section('radiusd')
+    config.set('radiusd','authport',(sh.read("radiusd authport [1812]:") or '1812'))
+    config.set('radiusd','acctport',(sh.read("radiusd acctport [1813]:") or '1813'))
+    config.set('radiusd','adminport',(sh.read("radiusd adminport [1815]:") or '1815'))
+    config.set('radiusd','cache_timeout',(sh.read("radiusd cache_timeout (second) [600]:") or '600'))
+    config.set('radiusd', 'logfile', (sh.read("log file [ logs/radiusd.log ]:") or 'logs/radiusd.log') )
+    
+    sh.info("set mysql backup ftpserver option")
+    config.add_section('backup')
+    config.set('backup','ftphost',(sh.read("backup ftphost [127.0.0.1]:") or '127.0.0.1' ))
+    config.set('backup','ftpport',(sh.read("backup ftpport [21]:") or '21'))
+    config.set('backup','ftpuser',(sh.read("backup ftpuser [ftpuser]:") or 'ftpuser' ))
+    config.set('backup','ftppwd',(sh.read("backup ftppwd [ftppwd]:") or 'ftppwd' ))
+    
+    sh.info("set admin option")
+    config.add_section('admin')
+    config.set('admin','port',(sh.read("admin http port [1816]:") or '1816'))
+    config.set('admin', 'logfile', (sh.read("log file [ logs/admin.log ]:") or 'logs/admin.log') )
+    
+    sh.info("set customer option")
+    config.add_section('customer')
+    config.set('customer','port',(sh.read("customer http port [1817]:") or '1817'))
+    config.set('customer', 'logfile', (sh.read("log file [ logs/customer.log ]:") or 'logs/customer.log') )
+    
+    with open(config_path,'wb') as configfile:
+        config.write(configfile)
+        sh.succ("config save to %s"%config_path)
+        
+def echo_my_cnf():
+    return '''[client]
+#password=your_password
+port=3306
+socket=/var/toughradius/mysql/mysql.sock
+
+[mysqld]
+back_log=60
+datadir=/var/toughradius/mysql
+socket=/var/toughradius/mysql/mysql.sock
+default-storage-engine=InnoDB
+symbolic-links=0
+
+wait_timeout=31536000
+interactive_timeout=31536000
+
+log-bin=mysql-bin
+max_allowed_packet=32M
+
+# explicit_defaults_for_timestamp
+
+# Recommended in standard MySQL setup
+sql_mode=NO_ENGINE_SUBSTITUTION,STRICT_TRANS_TABLES
+
+innodb_data_home_dir=/var/toughradius/mysql
+innodb_data_file_path=ibdata1:256M;ibdata2:256M:autoextend
+innodb_buffer_pool_size=128M 
+innodb_log_group_home_dir=/var/toughradius/mysql
+innodb_additional_mem_pool_size=16M
+innodb_log_file_size=32M
+innodb_log_buffer_size=8M
+innodb_flush_log_at_trx_commit=1
+innodb_lock_wait_timeout=50
+innodb_thread_concurrency=4
+
+[mysqld_safe]
+log-error=/var/toughradius/log/mysqld.log
+pid-file=/var/toughradius/mysql/mysqld.pid    
+'''
+
+def echo_radiusd_cnf():
+    return '''[DEFAULT]
+debug = 0
+tz = CST-8
+secret = %s
+
+[database]
+dbtype = mysql
+host = 127.0.0.1
+port = 3306
+db = toughradius
+maxusage = 10
+charset = utf8
+user = root
+passwd = 
+
+[radiusd]
+acctport = 1813
+adminport = 1815
+authport = 1812
+cache_timeout = 600
+logfile = /var/toughradius/log/radiusd.log
+
+[admin]
+port = 1816
+logfile = /var/toughradius/log/admin.log
+
+[customer]
+port = 1817
+logfile = /var/toughradius/log/customer.log
+
+[backup]
+ftpserver = 127.0.0.1
+ftpport = 21
+ftpuser = user
+ftppwd = pwd 
+'''%gen_secret(32)
+
+def echo_supervisord_cnf():
+    return '''[unix_http_server]
+file=/tmp/supervisor.sock   ; (the path to the socket file)
+
+[inet_http_server]         ; inet (TCP) server disabled by default
+port=127.0.0.1:9001        ; (ip_address:port specifier, *:port for all iface)
+
+[supervisord]
+logfile=/var/toughradius/log/supervisord.log ; (main log file;default $CWD/supervisord.log)
+logfile_maxbytes=64MB        ; (max main logfile bytes b4 rotation;default 50MB)
+logfile_backups=4           ; (num of main logfile rotation backups;default 10)
+loglevel=info                ; (log level;default info; others: debug,warn,trace)
+pidfile=/tmp/supervisord.pid ; (supervisord pidfile;default supervisord.pid)
+nodaemon=false               ; (start in foreground if true;default false)
+minfds=1024                  ; (min. avail startup file descriptors;default 1024)
+minprocs=200                 ; (min. avail process descriptors;default 200)
+
+[rpcinterface:supervisor]
+supervisor.rpcinterface_factory = supervisor.rpcinterface:make_main_rpcinterface
+
+[supervisorctl]
+;serverurl=unix:///tmp/supervisor.sock ; use a unix:// URL  for a unix socket
+serverurl=http://127.0.0.1:9001 ; use an http:// url to specify an inet socket
+
+[program:radiusd]
+command=toughctl -radiusd 
+process_name=%(program_name)s
+numprocs=1
+directory=/var/toughradius
+autostart=true
+autorestart=true
+user=root
+redirect_stderr=true
+stdout_logfile=/var/toughradius/log/radiusd.log
+
+[program:rad_console]
+command=toughctl -admin 
+process_name=%(program_name)s
+numprocs=1
+directory=/var/toughradius
+autostart=true
+autorestart=true
+user=root
+redirect_stderr=true
+stdout_logfile=/var/toughradius/log/admin.log
+
+[program:rad_customer]
+command=toughctl -customer 
+process_name=%(program_name)s
+numprocs=1
+directory=/var/toughradius
+autostart=true
+autorestart=true
+user=root
+redirect_stderr=true
+stdout_logfile=/var/toughradius/log/customer.log
+'''
+
+def echo_centos6_service():
+    return '''#!/bin/sh
+# /etc/rc.d/init.d/toughradius
+# chkconfig: 345 91 05
+# description: ToughRADIUS Server
+# processname: toughradius
+# Source init functions
+. /etc/rc.d/init.d/functions
+
+start()
+{
+    echo -n $"Starting ToughRADIUS: "
+    toughctl --start all
+    echo ‘start ToughRADIUS done’ 
+}
+
+stop()
+{
+    echo -n $"Shutting down ToughRADIUS: "
+    toughctl --stop all
+    echo 'shutdown ToughRADIUS done'
+}
+
+status_app()
+{
+    ps aux | grep toughctl
+}
+
+case "$1" in
+
+start)
+    start
+;;
+
+stop)
+    stop
+;;
+
+status)
+    status_app
+;;
+
+restart)
+    stop
+    start
+;;
+
+*)
+    echo "Usage: $0 {start|stop|restart|status}"
+;;
+
+esac
+'''
+
+def echo_centos7_service():
+    return '''[Unit]  
+Description=toughrad  
+After=network.target remote-fs.target nss-lookup.target  
+   
+[Service]  
+Type=forking  
+PIDFile=/var/toughradius/toughrad.pid
+ExecStart=toughctl --start all 
+ExecReload=toughctl --restart all 
+ExecStop=toughctl --stop all 
+PrivateTmp=true  
+
+[Install]  
+WantedBy=multi-user.target
+'''
+
+def echo_app_tac(app):
+    if app == 'radiusd':
+        return '''from twisted.application import service, internet
+from toughradius.tools import config
+from toughradius.radiusd import server
+application = service.Application("ToughRADIUS Radiusd Application")
+service = server.run(config.find_config(),True)
+service.setServiceParent(application)'''
+    elif app == 'admin':
+        return '''from twisted.application import service, internet
+from toughradius.tools import config
+from toughradius.console import admin_app
+application = service.Application("ToughRADIUS Admin Application")
+service = admin_app.run(config.find_config(),True)
+service.setServiceParent(application)'''
+    elif app == 'customer':
+        return '''from twisted.application import service, internet
+from toughradius.tools import config
+from toughradius.console import customer_app
+application = service.Application("ToughRADIUS Customer Application")
+service = customer_app.run(config.find_config(),True)
+service.setServiceParent(application)'''
