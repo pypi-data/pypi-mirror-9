1b9970a169affbe2e8c77aba4049cd030d31602d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:13 +0000

    Add package JustIRC/0.1/JustIRC-0.1.tar.gz

---
+++ packages/JustIRC/JustIRC-0.1.tar.gz/JustIRC-0.1/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+setup(
+    name = "JustIRC",
+    py_modules = ["JustIRC"],
+    version = "0.1",
+    description = "An event-driven IRC library",
+    author = "Gokberk Yaltirakli",
+    author_email = "webdosusb@gmail.com",
+    url = "https://github.com/gkbrk/JustIRC",
+    download_url = "https://github.com/gkbrk/JustIRC/tarball/master",
+    keywords = ["irc", "event"]
+)
