366de4ce67c1cce93bd8bb0fd30ae158d467e43a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:10 +0000

    Add package ElasticMapping/1.2/ElasticMapping-1.2.tar.gz

---
+++ packages/ElasticMapping/ElasticMapping-1.2.tar.gz/ElasticMapping-1.2/elasticmapping/__init__.py
@@ -0,0 +1,1 @@
+from .mapping import ElasticMapping, Nest # noqa
---
+++ packages/ElasticMapping/ElasticMapping-1.2.tar.gz/ElasticMapping-1.2/elasticmapping/types.py
@@ -0,0 +1,75 @@
+# ElasticMapping
+# File: types.py
+# Desc: base Elasticsearch types
+
+
+class CallableDict(dict):
+    BASE = None
+    OVERRIDES = None
+
+    def __call__(self, overrides):
+        new_dict = CallableDict(self)
+        new_dict.OVERRIDES = overrides
+        new_dict.BASE = self
+        return new_dict
+
+
+BASE_TYPE = {
+    'store': False,
+    'doc_values': False
+}
+
+STRING = CallableDict({
+    'type': 'string',
+    'index': 'analyzed'
+})
+
+FLOAT = CallableDict({
+    'type': 'float'
+})
+
+DOUBLE = CallableDict({
+    'type': 'double'
+})
+
+INTEGER = CallableDict({
+    'type': 'integer'
+})
+
+LONG = CallableDict({
+    'type': 'long'
+})
+
+SHORT = CallableDict({
+    'type': 'short'
+})
+
+BYTE = CallableDict({
+    'type': 'byte'
+})
+
+BOOLEAN = CallableDict({
+    'type': 'boolean'
+})
+
+DATE = CallableDict({
+    'type': 'date',
+    'format': 'date'
+})
+
+TIME = CallableDict({
+    'type': 'date',
+    'format': 'time'
+})
+
+DATETIME = CallableDict({
+    'type': 'date',
+    'format': 'date_optional_time'
+})
+
+
+TYPES = {
+    name: type
+    for name, type in locals().items()
+    if isinstance(type, CallableDict)
+}
