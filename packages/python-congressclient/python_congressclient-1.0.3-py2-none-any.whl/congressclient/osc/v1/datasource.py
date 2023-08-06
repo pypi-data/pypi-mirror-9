cae50abcbfab8152b41fcbb0e540a7f3f5bd6572
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:12 +0000

    Add package python-congressclient/1.0.3/python_congressclient-1.0.3-py2-none-any.whl

---
+++ packages/python-congressclient/python_congressclient-1.0.3-py2-none-any.whl/congressclient/osc/v1/datasource.py
@@ -0,0 +1,281 @@
+#   Copyright 2012-2013 OpenStack, LLC.
+#
+#   Licensed under the Apache License, Version 2.0 (the "License"); you may
+#   not use this file except in compliance with the License. You may obtain
+#   a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#   WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#   License for the specific language governing permissions and limitations
+#   under the License.
+
+"""Datasource action implemenations"""
+
+import logging
+
+from cliff import command
+from cliff import lister
+from cliff import show
+import six
+
+from congressclient.common import parseractions
+from congressclient.common import utils
+
+
+class ListDatasources(lister.Lister):
+    """List Datasources."""
+
+    log = logging.getLogger(__name__ + '.ListDatasources')
+
+    def get_parser(self, prog_name):
+        parser = super(ListDatasources, self).get_parser(prog_name)
+        return parser
+
+    def take_action(self, parsed_args):
+        client = self.app.client_manager.congressclient
+        data = client.list_datasources()['results']
+        columns = ['id', 'name', 'enabled', 'type', 'config']
+        formatters = {'Datasources': utils.format_list}
+        return (columns,
+                (utils.get_dict_properties(s, columns,
+                                           formatters=formatters)
+                 for s in data))
+
+
+class ListDatasourceTables(lister.Lister):
+    """List datasource tables."""
+
+    log = logging.getLogger(__name__ + '.ListDatasourceTables')
+
+    def get_parser(self, prog_name):
+        parser = super(ListDatasourceTables, self).get_parser(prog_name)
+        parser.add_argument(
+            'datasource_name',
+            metavar="<datasource-name>",
+            help="Name of the datasource")
+        return parser
+
+    def take_action(self, parsed_args):
+        self.log.debug('take_action(%s)' % parsed_args)
+        client = self.app.client_manager.congressclient
+        results = client.list_datasources()
+        datasource_id = utils.get_resource_id_from_name(
+            parsed_args.datasource_name, results)
+        data = client.list_datasource_tables(datasource_id)['results']
+        columns = ['id']
+        formatters = {'DatasourceTables': utils.format_list}
+        return (columns,
+                (utils.get_dict_properties(s, columns,
+                                           formatters=formatters)
+                 for s in data))
+
+
+class ShowDatasourceStatus(show.ShowOne):
+    """List status for datasource."""
+
+    log = logging.getLogger(__name__ + '.ShowDatasourceStatus')
+
+    def get_parser(self, prog_name):
+        parser = super(ShowDatasourceStatus, self).get_parser(prog_name)
+        parser.add_argument(
+            'datasource_name',
+            metavar="<datasource-name>",
+            help="Name of the datasource")
+        return parser
+
+    def take_action(self, parsed_args):
+        self.log.debug('take_action(%s)' % parsed_args)
+        client = self.app.client_manager.congressclient
+
+        results = client.list_datasources()
+        datasource_id = utils.get_resource_id_from_name(
+            parsed_args.datasource_name, results)
+
+        data = client.list_datasource_status(datasource_id)
+        return zip(*sorted(six.iteritems(data)))
+
+
+class ShowDatasourceSchema(lister.Lister):
+    """Show schema for datasource."""
+
+    log = logging.getLogger(__name__ + '.ShowDatasourceSchema')
+
+    def get_parser(self, prog_name):
+        parser = super(ShowDatasourceSchema, self).get_parser(prog_name)
+        parser.add_argument(
+            'datasource_name',
+            metavar="<datasource-name>",
+            help="Name of the datasource")
+        return parser
+
+    def take_action(self, parsed_args):
+        self.log.debug('take_action(%s)' % parsed_args)
+        client = self.app.client_manager.congressclient
+        results = client.list_datasources()
+        datasource_id = utils.get_resource_id_from_name(
+            parsed_args.datasource_name, results)
+        data = client.show_datasource_schema(datasource_id)
+        formatters = {'columns': utils.format_long_dict_list}
+        newdata = [{'table': x['table_id'],
+                    'columns': x['columns']}
+                   for x in data['tables']]
+        columns = ['table', 'columns']
+        return (columns,
+                (utils.get_dict_properties(s, columns,
+                                           formatters=formatters)
+                 for s in newdata))
+
+
+class ShowDatasourceTableSchema(lister.Lister):
+    """Show schema for datasource table."""
+
+    log = logging.getLogger(__name__ + '.ShowDatasourceTableSchema')
+
+    def get_parser(self, prog_name):
+        parser = super(ShowDatasourceTableSchema, self).get_parser(prog_name)
+        parser.add_argument(
+            'datasource_name',
+            metavar="<datasource-name>",
+            help="Name of the datasource")
+        parser.add_argument(
+            'table_name',
+            metavar="<table-name>",
+            help="Name of the table")
+        return parser
+
+    def take_action(self, parsed_args):
+        self.log.debug('take_action(%s)' % parsed_args)
+        client = self.app.client_manager.congressclient
+        data = client.show_datasource_table_schema(
+            parsed_args.datasource_name,
+            parsed_args.table_name)
+        columns = ['name', 'description']
+        return (columns,
+                (utils.get_dict_properties(s, columns)
+                 for s in data['columns']))
+
+
+class ListDatasourceRows(lister.Lister):
+    """List datasource rows."""
+
+    log = logging.getLogger(__name__ + '.ListDatasourceRows')
+
+    def get_parser(self, prog_name):
+        parser = super(ListDatasourceRows, self).get_parser(prog_name)
+        parser.add_argument(
+            'datasource_name',
+            metavar="<datasource-name>",
+            help="Name of the datasource to show")
+        parser.add_argument(
+            'table',
+            metavar="<table>",
+            help="Table to get the datasource rows from")
+        return parser
+
+    def take_action(self, parsed_args):
+        self.log.debug('take_action(%s)' % parsed_args)
+        client = self.app.client_manager.congressclient
+
+        results = client.list_datasources()
+        datasource_id = utils.get_resource_id_from_name(
+            parsed_args.datasource_name, results)
+        results = client.list_datasource_rows(datasource_id,
+                                              parsed_args.table)['results']
+        if results:
+            columns = client.show_datasource_table_schema(
+                datasource_id, parsed_args.table)['columns']
+            columns = [col['name'] for col in columns]
+        else:
+            columns = ['data']  # doesn't matter because the rows are empty
+        return (columns, (x['data'] for x in results))
+
+
+class ShowDatasourceTable(show.ShowOne):
+    """Show Datasource Table properties."""
+
+    log = logging.getLogger(__name__ + '.ShowDatasourceTable')
+
+    def get_parser(self, prog_name):
+        parser = super(ShowDatasourceTable, self).get_parser(prog_name)
+        parser.add_argument(
+            'datasource_name',
+            metavar='<datasource-name>',
+            help="Name of datasource")
+        parser.add_argument(
+            'table_id',
+            metavar='<table-id>',
+            help="Table id")
+
+        return parser
+
+    def take_action(self, parsed_args):
+        self.log.debug('take_action(%s)' % parsed_args)
+        client = self.app.client_manager.congressclient
+        data = client.show_datasource_table(parsed_args.datasource_name,
+                                            parsed_args.table_id)
+        return zip(*sorted(six.iteritems(data)))
+
+
+class CreateDatasource(show.ShowOne):
+    """Create a datasource."""
+
+    log = logging.getLogger(__name__ + '.CreateDatasource')
+
+    def get_parser(self, prog_name):
+        parser = super(CreateDatasource, self).get_parser(prog_name)
+        parser.add_argument(
+            'driver',
+            metavar="<datasource-driver>",
+            help="Selected datasource driver")
+        parser.add_argument(
+            'name',
+            metavar="<datasource-name>",
+            help="Name you want to call the datasource")
+        parser.add_argument(
+            '--description',
+            metavar="<datasource-description>",
+            help="Description of the datasource")
+
+        parser.add_argument(
+            '--config',
+            metavar="<key=value>",
+            action=parseractions.KeyValueAction,
+            help="config dictionary to pass in")
+
+        return parser
+
+    def take_action(self, parsed_args):
+        self.log.debug('take_action(%s)' % parsed_args)
+        client = self.app.client_manager.congressclient
+        body = {'name': parsed_args.name,
+                'driver': parsed_args.driver,
+                'config': parsed_args.config}
+        if parsed_args.description:
+            body['description'] = parsed_args.description
+        results = client.create_datasource(body)
+        return zip(*sorted(six.iteritems(results)))
+
+
+class DeleteDatasource(command.Command):
+    """Delete a datasource."""
+
+    log = logging.getLogger(__name__ + '.DeleteDatasource')
+
+    def get_parser(self, prog_name):
+        parser = super(DeleteDatasource, self).get_parser(prog_name)
+        parser.add_argument(
+            'datasource',
+            metavar="<datasource-name>",
+            help="Name of the datasource to delete")
+        return parser
+
+    def take_action(self, parsed_args):
+        self.log.debug('take_action(%s)' % parsed_args)
+        client = self.app.client_manager.congressclient
+        results = client.list_datasources()
+        datasource_id = utils.get_resource_id_from_name(
+            parsed_args.datasource, results)
+        client.delete_datasource(datasource_id)
---
+++ packages/python-congressclient/python_congressclient-1.0.3-py2-none-any.whl/congressclient/v1/client.py
@@ -0,0 +1,177 @@
+#   Copyright 2014 VMWare.
+#
+#   Licensed under the Apache License, Version 2.0 (the "License"); you may
+#   not use this file except in compliance with the License. You may obtain
+#   a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#   WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#   License for the specific language governing permissions and limitations
+#   under the License.
+
+from keystoneclient import adapter
+
+
+class Client(object):
+    """Client for the Congress v1 API.
+
+    Example::
+        import keystoneclient
+        from congressclient.v1 import client
+        auth = keystoneclient.auth.identity.v2.Password(
+            auth_url=AUTH_URL, username=USERNAME,
+            password=PASSWORD, tenant_name=TENANT_NAME)
+        session = keystoneclient.session.Session(auth=auth)
+        congress = client.Client(session=session,
+                                 auth=None,
+                                 interface='publicURL',
+                                 service_type='policy',
+                                 region_name='RegionOne')
+        congress.create_policy_rule(..)
+
+    """
+
+    policy = '/v1/policies'
+    policy_path = '/v1/policies/%s'
+    policy_rules = '/v1/policies/%s/rules'
+    policy_rules_path = '/v1/policies/%s/rules/%s'
+    policy_tables = '/v1/policies/%s/tables'
+    policy_table_path = '/v1/policies/%s/tables/%s'
+    policy_rows = '/v1/policies/%s/tables/%s/rows'
+    policy_rows_trace = '/v1/policies/%s/tables/%s/rows?trace=True'
+    policy_rules = '/v1/policies/%s/rules'
+    policies = '/v1/policies'
+    policy_action = '/v1/policies/%s?%s'
+    datasources = '/v1/data-sources'
+    datasource_path = '/v1/data-sources/%s'
+    datasource_tables = '/v1/data-sources/%s/tables'
+    datasource_table_path = '/v1/data-sources/%s/tables/%s'
+    datasource_status = '/v1/data-sources/%s/status'
+    datasource_schema = '/v1/data-sources/%s/schema'
+    datasource_table_schema = '/v1/data-sources/%s/tables/%s/spec'
+    datasource_rows = '/v1/data-sources/%s/tables/%s/rows'
+    driver = '/v1/system/drivers'
+    driver_path = '/v1/system/drivers/%s'
+
+    def __init__(self, **kwargs):
+        super(Client, self).__init__()
+
+        kwargs.setdefault('user_agent', 'python-congressclient')
+        self.httpclient = adapter.LegacyJsonAdapter(**kwargs)
+
+    def create_policy(self, body):
+        resp, body = self.httpclient.post(
+            self.policy, body=body)
+        return body
+
+    def delete_policy(self, policy):
+        resp, body = self.httpclient.delete(
+            self.policy_path % policy)
+        return body
+
+    def show_policy(self, policy):
+        resp, body = self.httpclient.get(
+            self.policy_path % policy)
+        return body
+
+    def create_policy_rule(self, policy_name, body=None):
+        resp, body = self.httpclient.post(
+            self.policy_rules % policy_name, body=body)
+        return body
+
+    def delete_policy_rule(self, policy_name, rule_id):
+        resp, body = self.httpclient.delete(
+            self.policy_rules_path % (policy_name, rule_id))
+        return body
+
+    def show_policy_rule(self, policy_name, rule_id):
+        resp, body = self.httpclient.get(
+            self.policy_rules_path % (policy_name, rule_id))
+        return body
+
+    def list_policy_rows(self, policy_name, table, trace=None):
+        if trace:
+            query = self.policy_rows_trace
+        else:
+            query = self.policy_rows
+        resp, body = self.httpclient.get(query % (policy_name, table))
+        return body
+
+    def list_policy_rules(self, policy_name):
+        resp, body = self.httpclient.get(self.policy_rules % (policy_name))
+        return body
+
+    def list_policy(self):
+        resp, body = self.httpclient.get(self.policies)
+        return body
+
+    def list_policy_tables(self, policy_name):
+        resp, body = self.httpclient.get(self.policy_tables % (policy_name))
+        return body
+
+    def execute_policy_action(self, policy_name, action, trace, delta, body):
+        uri = "?action=%s&trace=%s&delta=%s" % (action, trace, delta)
+        resp, body = self.httpclient.post(
+            (self.policy_path % policy_name) + str(uri), body=body)
+        return body
+
+    def show_policy_table(self, policy_name, table_id):
+        resp, body = self.httpclient.get(self.policy_table_path %
+                                         (policy_name, table_id))
+        return body
+
+    def list_datasources(self):
+        resp, body = self.httpclient.get(self.datasources)
+        return body
+
+    def list_datasource_tables(self, datasource_name):
+        resp, body = self.httpclient.get(self.datasource_tables %
+                                         (datasource_name))
+        return body
+
+    def list_datasource_rows(self, datasource_name, table_name):
+        resp, body = self.httpclient.get(self.datasource_rows %
+                                         (datasource_name, table_name))
+        return body
+
+    def list_datasource_status(self, datasource_name):
+        resp, body = self.httpclient.get(self.datasource_status %
+                                         datasource_name)
+        return body
+
+    def show_datasource_schema(self, datasource_name):
+        resp, body = self.httpclient.get(self.datasource_schema %
+                                         datasource_name)
+        return body
+
+    def show_datasource_table_schema(self, datasource_name, table_name):
+        resp, body = self.httpclient.get(self.datasource_table_schema %
+                                         (datasource_name, table_name))
+        return body
+
+    def show_datasource_table(self, datasource_name, table_id):
+        resp, body = self.httpclient.get(self.datasource_table_path %
+                                         (datasource_name, table_id))
+        return body
+
+    def create_datasource(self, body=None):
+        resp, body = self.httpclient.post(
+            self.datasources, body=body)
+        return body
+
+    def delete_datasource(self, datasource):
+        resp, body = self.httpclient.delete(
+            self.datasource_path % datasource)
+        return body
+
+    def list_drivers(self):
+        resp, body = self.httpclient.get(self.driver)
+        return body
+
+    def show_driver(self, driver):
+        resp, body = self.httpclient.get(self.driver_path %
+                                         (driver))
+        return body
