8002887b0deb2ca70dea675bbdaa667d876dda20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:13 +0000

    Add package plone.app.collection/1.1.3/plone.app.collection-1.1.3.zip

---
+++ packages/plone.app.collection/plone.app.collection-1.1.3.zip/plone.app.collection-1.1.3/setup.py
@@ -0,0 +1,58 @@
+from setuptools import setup, find_packages
+
+version = '1.1.3'
+
+setup(name='plone.app.collection',
+      version=version,
+      description="This package adds 'saved search' functionality to Plone.",
+      long_description=open("README.rst").read() + "\n" +
+                       open("CHANGES.rst").read(),
+      classifiers=[
+        "Framework :: Plone",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Programming Language :: Python",
+        ],
+      keywords='',
+      author='Plone Foundation',
+      author_email='plone-developers@lists.sourceforge.net',
+      url='http://pypi.python.org/pypi/plone.app.collection',
+      license='GPL version 2',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['plone', 'plone.app'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'plone.app.contentlisting',
+          'plone.app.form',
+          'plone.app.portlets',
+          'plone.app.querystring>=1.2.2',  # custom_query support
+          'plone.app.vocabularies',
+          'plone.app.widgets',
+          'plone.portlet.collection',
+          'plone.portlets',
+          'Products.Archetypes>=1.10.4.dev0',
+          'Products.CMFCore',
+          'Products.CMFPlone',
+          'Products.CMFQuickInstallerTool',
+          'Products.validation',
+          'transaction',
+          'zope.component',
+          'zope.configuration',
+          'zope.formlib',
+          'zope.i18nmessageid',
+          'zope.interface',
+          'zope.schema',
+          'Zope2',
+      ],
+      extras_require={
+          'test': [
+              'plone.app.testing [robot]',
+              'Products.ATContentTypes [test]',
+              ],
+      },
+      entry_points="""
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
