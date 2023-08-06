6dcd84c1d9e2112f63328fdae5240f32d4d089cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:45 +0000

    Add package jr_nester/1.0.0/jr_nester-1.0.0.tar.gz

---
+++ packages/jr_nester/jr_nester-1.0.0.tar.gz/jr_nester-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'jr_nester',
+    version = '1.0.0',
+    py_modules = ['jr_nester'],
+    author = 'hfpython',
+    author_email = 'hfpython@headfirstlabs.com',
+    url = 'http://www.headfirstlabs.com',
+    description = 'A simple printer of nestled lists',
+    )
