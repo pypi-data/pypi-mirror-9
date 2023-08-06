3024e063f0dd0c064e902f8434be0ada6a58759f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:46 +0000

    Add package link/0.2.9/link-0.2.9.tar.gz

---
+++ packages/link/link-0.2.9.tar.gz/link-0.2.9/link/wrappers/__init__.py
@@ -0,0 +1,12 @@
+"""
+I don't exactly love that you have to do this.  I will look for a new design
+"""
+from apiwrappers import *
+from dbwrappers import *
+from nosqlwrappers import *
+from consolewrappers import *
+from atlassianwrappers import *
+from alexawrappers import *
+from hivewrappers import *
+from elasticsearchwrappers import *
+from liverailwrappers import *
