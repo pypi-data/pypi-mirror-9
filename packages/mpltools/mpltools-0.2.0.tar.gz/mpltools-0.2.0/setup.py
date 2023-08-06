b4f3700ef59f7ed2136b094aa38b1a983601fd27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:02 +0000

    Add package mpltools/0.2.0/mpltools-0.2.0.tar.gz

---
+++ packages/mpltools/mpltools-0.2.0.tar.gz/mpltools-0.2.0/setup.py
@@ -0,0 +1,25 @@
+#! /usr/bin/env python
+from distutils.core import setup
+
+import setuptools
+
+
+with open('README.rst') as f:
+    long_description = f.read()
+
+
+setup(
+    name='mpltools',
+    version='0.2.0',  # Must be kept consistent with `mpltools/__init__.py`.
+    description='Tools for Matplotlib',
+    long_description=long_description,
+    author='Tony S. Yu',
+    author_email='tsyu80@gmail.com',
+    license='Modified BSD',
+    url='http://tonysyu.github.com/mpltools/',
+    download_url='http://github.com/tonysyu/mpltools',
+    packages=setuptools.find_packages(),
+    package_data={'mpltools': ['mpltoolsrc', 'style/*.rc']},
+    include_package_data=True,
+    install_requires=['matplotlib>=1.0', 'configobj>=4.4.0', 'future>=0.12.4']
+)
