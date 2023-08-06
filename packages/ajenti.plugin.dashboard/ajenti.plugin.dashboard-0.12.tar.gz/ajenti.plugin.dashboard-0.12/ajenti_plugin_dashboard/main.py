9a8967f31f2e62fbe5f0fc007a0724a23c21bc3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:22 +0000

    Add package ajenti.plugin.dashboard/0.12/ajenti.plugin.dashboard-0.12.tar.gz

---
+++ packages/ajenti.plugin.dashboard/ajenti.plugin.dashboard-0.12.tar.gz/ajenti.plugin.dashboard-0.12/ajenti_plugin_dashboard/main.py
@@ -0,0 +1,20 @@
+from aj.api import *
+from aj.plugins.core.api.sidebar import *
+
+
+@component(SidebarItemProvider)
+class ItemProvider(SidebarItemProvider):
+    def __init__(self, context):
+        pass
+
+    def provide(self):
+        return [
+            {
+                'attach': 'category:general',
+                'name': 'Dashboard',
+                'icon': 'bar-chart',
+                'url': '/view/dashboard',
+                'children': [
+                ]
+            }
+        ]
