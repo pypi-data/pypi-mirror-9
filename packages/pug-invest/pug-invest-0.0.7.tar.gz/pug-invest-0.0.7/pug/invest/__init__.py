9bba0a9cead4a4302101bae9519f63bd4246c9df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:49 +0000

    Add package pug-invest/0.0.7/pug-invest-0.0.7.tar.gz

---
+++ packages/pug-invest/pug-invest-0.0.7.tar.gz/pug-invest-0.0.7/pug/invest/__init__.py
@@ -0,0 +1,11 @@
+from package_info import __license__, __version__, __name__, __doc__, __author__, __authors__
+
+import sim
+import plot
+import util
+
+__all__ = [
+    'sim',
+    'plot',
+    'util',
+]
