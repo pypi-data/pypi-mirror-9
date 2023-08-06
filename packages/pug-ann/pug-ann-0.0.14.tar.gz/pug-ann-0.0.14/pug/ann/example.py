ba4d326f6719c40d11a4e583aea6aa54e23a32b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:48 +0000

    Add package pug-ann/0.0.14/pug-ann-0.0.14.tar.gz

---
+++ packages/pug-ann/pug-ann-0.0.14.tar.gz/pug-ann-0.0.14/pug/ann/example.py
@@ -0,0 +1,25 @@
+"""Example pybrain network training to predict the weather
+
+Installation:
+
+    pip install pug-ann
+
+Usage:
+
+    >>> from pug.ann import example
+    >>> example.predict_weather('San Francisco, CA')
+"""
+
+from pug.ann.data import weather
+from pug.ann import util
+
+def predict_weather(location='Camas, WA', years=range(2011, 2015), verbosity=2):
+    df = weather.daily(location, years=years, verbosity=verbosity).sort()
+    ds, means, stds = util.dataset_from_dataframe(df, delays=[1,2,3,4], inputs=['Min TemperatureF', 'Max TemperatureF', ' Min Sea Level PressureIn', u' Max Sea Level PressureIn', ' WindDirDegrees'], outputs=[u'Max TemperatureF'])
+    nn = util.ann_from_ds(ds)
+    trainer = util.build_trainer(nn, ds)
+    training_err, validation_err = trainer.trainUntilConvergence(maxEpochs=200, verbose=bool(verbosity))
+    return validation_err
+
+if __name__ == '__main__':
+    print(predict_weather())
