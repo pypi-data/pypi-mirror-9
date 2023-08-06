50c82d3f21a9d9e8bfbc74639b84fd41202abafa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:54 +0000

    Add package bael.project/0.1.5/bael.project-0.1.5.tar.gz

---
+++ packages/bael.project/bael.project-0.1.5.tar.gz/bael.project-0.1.5/setup.py
@@ -0,0 +1,26 @@
+# -*- encoding: utf-8 -*-
+from setuptools import setup, find_packages
+
+install_requires = [
+    'baelfire>=0.2.5',
+    'virtualenv',
+]
+
+if __name__ == '__main__':
+    setup(name='bael.project',
+          version='0.1.5',
+          description='Simple project generator.',
+          license='Apache License 2.0',
+          packages=find_packages('src'),
+          package_dir={'': 'src'},
+          namespace_packages=['bael'],
+          install_requires=install_requires,
+          zip_safe=False,
+          package_data={
+              '': ['project/templates/*.tpl', 'project/templates/*.py'],
+          },
+          entry_points="""\
+              [console_scripts]
+                  pyproject = bael.project.recipe:run
+          """,
+          )
