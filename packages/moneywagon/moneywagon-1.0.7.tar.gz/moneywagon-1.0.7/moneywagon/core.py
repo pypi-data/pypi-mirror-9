237c1bc2002fbd0b5a649f8767b45ca0a4e52783
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:12 +0000

    Add package moneywagon/1.0.7/moneywagon-1.0.7.tar.gz

---
+++ packages/moneywagon/moneywagon-1.0.7.tar.gz/moneywagon-1.0.7/moneywagon/core.py
@@ -0,0 +1,147 @@
+from __future__ import print_function
+import requests
+
+useragent = 'moneywagon 1.0.6'
+
+class SkipThisService(Exception):
+    pass
+
+class NoService(Exception):
+    pass
+
+class NoData(Exception):
+    pass
+
+class Service(object):
+    """
+    All Services should subclass this class, and implement their own `get_*` method
+    """
+    supported_cryptos = None
+
+    def __init__(self, verbose=False, responses=None):
+        self.responses = responses or {} # for caching
+        self.verbose = verbose
+
+    def __repr__(self):
+        return "%s (%s in cache)" % (self.__class__.__name__, len(self.responses))
+
+    def get_url(self, url, *args, **kwargs):
+        return self._external_request('get', url, *args, **kwargs)
+
+    def post_url(self, url, *args, **kwargs):
+        return self._external_request('post', url, *args, **kwargs)
+
+    def _external_request(self, method, url, *args, **kwargs):
+        """
+        Wrapper for requests.get with useragent automatically set.
+        And also all requests are reponses are cached.
+        """
+        if url in self.responses.keys() and method == 'get':
+            return self.responses[url] # return from cache if its there
+
+        headers = kwargs.pop('headers', None)
+        custom = {'User-Agent': useragent}
+        if headers:
+            headers.update(custom)
+            kwargs['headers'] = headers
+        else:
+            kwargs['headers'] = custom
+
+        if self.verbose: print("URL: %s" % url)
+
+        response = getattr(requests, method)(url, *args, **kwargs)
+        if method == 'get':
+            self.responses[url] = response # cache for later
+        return response
+
+    def get_price(self, crypto, fiat):
+        """
+        Makes call to external service, and returns the price for given
+        fiat/crypto pair. Returns two item tuple: (price, best_market)
+        """
+        raise NotImplementedError()
+
+    def get_historical_price(self, crypto, fiat, at_time):
+        """
+        """
+        raise NotImplementedError()
+
+    def get_transactions(self, crypto, address):
+        """
+        """
+        raise NotImplementedError()
+
+    def get_balance(self, crypto, address):
+        """
+        Get the amount of coin in the address passed in.
+        Always returns a single float.
+        """
+        raise NotImplementedError()
+
+    def push_tx(self, crypto, tx_hex):
+        """
+        Push transaction to the miner network. Returns nothing if done
+        successfully.
+        """
+        raise NotImplementedError()
+
+
+class AutoFallback(object):
+    """
+    Calls a succession of services until one returns a value.
+    """
+    service_method_name = None # the relevant name of the method on each service class
+
+    def __init__(self, services=None, verbose=False, responses=None):
+        """
+        Each service class is instantiated here so the service instances stay
+        in scope for the entire life of this object. This way the service
+        objects can cache responses.
+        """
+        self.services = []
+        if not services:
+            from moneywagon.services import ALL_SERVICES
+            services = ALL_SERVICES
+        for Service in services:
+            self.services.append(
+                Service(verbose=verbose, responses=responses)
+            )
+
+        self.verbose = verbose
+
+    def _try_each_service(self, *args, **kwargs):
+        """
+        Try each service until one returns a response. This function only
+        catches the bare minimum of exceptions from the service class. We want
+        exceptions to be raised so the service classes can be debugged and
+        fixed quickly.
+        """
+        for service in self.services:
+            crypto = (args and args[0]) or kwargs['crypto']
+            if service.supported_cryptos and (crypto.lower() not in service.supported_cryptos):
+                if self.verbose:
+                    print("SKIP:", "%s not supported for %s" % (crypto, service.__class__.__name__))
+                continue
+            try:
+                if self.verbose: print("* Trying:", service)
+                return getattr(service, self.service_method_name)(*args, **kwargs)
+            except (KeyError, IndexError, TypeError, ValueError) as exc:
+                # API has probably changed, therefore service class broken
+                if self.verbose: print("FAIL:", exc.__class__.__name__, exc)
+            except SkipThisService as exc:
+                # service classes can raise this exception if for whatever reason
+                # that service can't return a response, but maybe another one can.
+                if self.verbose: print("SKIP:", exc.__class__.__name__, exc)
+            except NotImplementedError as exc:
+                if self.verbose: print("SKIP:", exc.__class__.__name__, exc)
+
+        raise NoService(self.no_service_msg(*args, **kwargs))
+
+    def no_service_msg(self, *args, **kwargs):
+        """
+        This function is called when all Services have been tried and no value
+        can be returned. It much take the same args and kwargs as in the method
+        spefified in `self.method_name`. Returned is a string for the error message.
+        It should say something informative.
+        """
+        return "All either skipped or failed."
---
+++ packages/moneywagon/moneywagon-1.0.7.tar.gz/moneywagon-1.0.7/moneywagon/services.py
@@ -0,0 +1,283 @@
+from .core import Service, NoService, NoData, SkipThisService
+import arrow
+
+class Bitstamp(Service):
+    supported_cryptos = ['btc']
+
+    def get_price(self, crypto, fiat):
+        if fiat.lower() != 'usd':
+            raise SkipThisService('Bitstamp only does USD->BTC')
+
+        url = "https://www.bitstamp.net/api/ticker/"
+        response = self.get_url(url).json()
+        return (float(response['last']), 'bitstamp')
+
+
+class BlockCypher(Service):
+    supported_cryptos = ['btc', 'ltc', 'uro']
+
+    def get_balance(self, crypto, address):
+        url = "http://api.blockcypher.com/v1/%s/main/addrs/%s" % (crypto, address)
+        response = self.get_url(url)
+        return response.json()['balance'] / 1.0e8
+
+
+class Blockr(Service):
+    supported_cryptos = ['btc', 'ltc', 'ppc', 'mec', 'qrk', 'dgc', 'tbtc']
+
+    def get_balance(self, crypto, address):
+        url = "http://%s.blockr.io/api/v1/address/info/%s" % (crypto, address)
+        response = self.get_url(url)
+        return response.json()['data']['balance']
+
+    def get_transactions(self, crypto, address):
+        url = 'http://%s.blockr.io/api/v1/address/txs/%s' % (crypto, address)
+        response = self.get_url(url)
+
+        transactions = []
+        for tx in response.json()['data']['txs']:
+            transactions.append(dict(
+                date=arrow.get(tx['time_utc']).datetime,
+                amount=tx['amount'],
+                txid=tx['tx'],
+                confirmations=tx['confirmations'],
+            ))
+        return transactions
+
+    def push_tx(self, crypto, tx):
+        url = "http://%s.blockr.io/api/v1/tx/push" % crypto
+        response = self.post_url(url, {'tx': tx})
+        return response.json()['data']
+
+class BTCE(Service):
+    def get_price(self, crypto, fiat):
+        pair = "%s_%s" % (crypto, fiat)
+        url = "https://btc-e.com/api/3/ticker/" + pair
+        response = self.get_url(url).json()
+        return (response[pair]['last'], 'btc-e')
+
+
+class Cryptonator(Service):
+    def get_price(self, crypto, fiat):
+        pair = "%s-%s" % (crypto, fiat)
+        url = "https://www.cryptonator.com/api/ticker/%s" % pair
+        response = self.get_url(url).json()
+        return float(response['ticker']['price']), 'cryptonator'
+
+class Winkdex(Service):
+    supported_cryptos = ['btc']
+
+    def get_price(self, crypto, fiat):
+        if fiat != 'usd':
+            raise SkipThisService("winkdex is btc->usd only")
+        url = "https://winkdex.com/api/v0/price"
+        return self.get_url(url).json()['price'] / 100.0, 'winkdex'
+
+class BitEasy(Service):
+    supported_cryptos = ['btc']
+
+    def get_balance(self, crypto, address):
+        url = "https://api.biteasy.com/blockchain/v1/addresses/" + address
+        response = self.get_url(url)
+        return response.json()['data']['balance'] / 1e8
+
+
+class BlockChainInfo(Service):
+    supported_cryptos = ['btc']
+
+    def get_balance(self, crypto, address):
+        url = "http://blockchain.info/address/%s?format=json" % address
+        response = self.get_url(url)
+        return float(response.json()['final_balance']) * 1e-8
+
+##################################
+
+class BitcoinAbe(Service):
+    supported_cryptos = ['btc']
+    base_url = "http://bitcoin-abe.info/chain/Bitcoin"
+
+    def get_balance(self, crypto, address):
+        url = self.base_url + "/q/addressbalance/" + address
+        response = self.get_url(url)
+        return float(response.content)
+
+class LitecoinAbe(BitcoinAbe):
+    supported_cryptos = ['ltc']
+    base_url = "http://bitcoin-abe.info/chain/Litecoin"
+
+class NamecoinAbe(BitcoinAbe):
+    supported_cryptos = ['nmc']
+    base_url = "http://bitcoin-abe.info/chain/Namecoin"
+
+class DogeChainInfo(BitcoinAbe):
+    supported_cryptos = ['doge']
+    base_url = "https://dogechain.info/chain/Dogecoin"
+
+class VertcoinOrg(BitcoinAbe):
+    supported_cryptos = ['vtc']
+    base_url = "https://explorer.vertcoin.org/chain/Vertcoin"
+
+class AuroraCoinEU(BitcoinAbe):
+    supported_cryptos = ['aur']
+    base_url = 'http://blockexplorer.auroracoin.eu/chain/AuroraCoin'
+
+class Atorox(BitcoinAbe):
+    supported_cryptos = ['aur']
+    base_url = "http://auroraexplorer.atorox.net/chain/AuroraCoin"
+
+##################################
+
+class FeathercoinCom(Service):
+    supported_cryptos = ['ftc']
+
+    def get_balance(self, crypto, address):
+        url= "http://api.feathercoin.com/?output=balance&address=%s&json=1" % address
+        response = self.get_url(url)
+        return float(response.json()['balance'])
+
+
+class NXTPortal(Service):
+    supported_cryptos = ['nxt']
+
+    def get_balance(self, crypto, address):
+        url='http://nxtportal.org/nxt?requestType=getAccount&account=' + address
+        response = self.get_url(url)
+        return float(response.json()['balanceNQT']) * 1e-8
+
+    def get_transactions(self, crypto, address):
+        url = 'http://nxtportal.org/transactions/account/%s?num=50' % address
+        response = self.get_url(url)
+        transactions = []
+        for tx in txs:
+            transactions.append(dict(
+                date=arrow.get(tx['time']).datetime,
+                amount=tx['value'],
+                txid=tx['txid'],
+                confirmations=tx['confirmations'],
+            ))
+
+        return transactions
+
+class CryptoID(Service):
+    supported_cryptos = [
+        'drk', 'bc', 'bay', 'block', 'cann', 'uno', 'vrc', 'xc', 'uro', 'aur',
+        'pot', 'cure', 'arch', 'swift', 'karm', 'dgc', 'lxc', 'sync', 'byc',
+        'pc', 'fibre', 'i0c', 'nobl', 'gsx', 'flt', 'ccn', 'rlc', 'rby', 'apex',
+        'vior', 'ltcd', 'zeit', 'carbon', 'super', 'dis', 'ac', 'vdo', 'ioc',
+        'xmg', 'cinni', 'crypt', 'excl', 'mne', 'seed', 'qslv', 'maryj', 'key',
+        'oc', 'ktk', 'voot', 'glc', 'drkc', 'mue', 'gb', 'piggy', 'jbs', 'grs',
+        'icg', 'rpc', ''
+    ]
+
+    def get_balance(self, crypto, address):
+        url ="http://chainz.cryptoid.info/%s/api.dws?q=getbalance&a=%s" % (crypto, address)
+        return float(self.get_url(url).content)
+
+
+class CryptapUS(Service):
+    supported_cryptos = [
+        'nmc', 'wds', 'ber', 'scn', 'sc0', 'wdc', 'nvc', 'cas', 'myr'
+    ]
+    def get_balance(self, crypto, address):
+        url = "http://cryptap.us/%s/explorer/q/addressbalance/%s" % (crypto, address)
+        return float(self.get_url(url).content)
+
+
+class BTER(Service):
+    def get_price(self, crypto, fiat):
+        url_template = "http://data.bter.com/api/1/ticker/%s_%s"
+        url = url_template % (crypto, fiat)
+
+        response = self.get_url(url).json()
+
+        if response['result'] == 'false': # bter api returns this as string
+            # bter doesn't support this pair, we need to make 2 calls and
+            # do the math ourselves. The extra http request isn't a problem because
+            # of caching. BTER only has USD, BTC and CNY
+            # markets, so any other fiat will likely fail.
+
+            url = url_template % (crypto, 'btc')
+            response = self.get_url(url)
+            altcoin_btc = float(response['last'])
+
+            url = url_template % ('btc', fiat)
+            response = self.get_url(url)
+            btc_fiat = float(response['last'])
+
+            return (btc_fiat * altcoin_btc), 'bter (calculated)'
+
+        return float(response['last'] or 0), 'bter'
+
+
+class CoinSwap(Service):
+    def get_price(self, crypto, fiat):
+        chunk = ("%s/%s" % (crypto, fiat)).upper()
+        url = "https://api.coin-swap.net/market/stats/%s" % chunk
+        response = self.get_url(url).json()
+        return float(response['lastprice']), 'coin-swap'
+
+
+class ChainSo(Service):
+    supported_cryptos = ['doge']
+
+    def get_transactions(self, crypto, address):
+        url = "https://chain.so/api/v2/get_tx_unspent/DOGE/" + address
+        response = self.get_url(url)
+
+        transactions = []
+        for tx in response.json()['data']['txs']:
+            transactions.append(dict(
+                date=arrow.get(tx['time']).datetime,
+                amount=tx['value'],
+                txid=tx['txid'],
+                confirmations=tx['confirmations'],
+            ))
+        return transactions
+
+
+class ExCoIn(Service):
+    def get_price(self, crypto, fiat):
+        url = "https://api.exco.in/v1/exchange/%s/%s" % (fiat, crypto)
+        response = self.get_url(url).json()
+        return float(response['last_price']), 'exco.in'
+
+################################################
+
+class BitpayInsight(Service):
+    supported_cryptos = ['btc']
+    domain = "http://insight.bitpay.com"
+
+    def get_balance(self, crypto, address):
+        url = "/api/addr/%s/balance" % (self.domain, address)
+        return float(self.get_url(url).content) / 1e8
+
+    def get_transactions(self, crypto, address):
+        url = "%s/api/txs/?address=%s" % (self.domain, address)
+        response = self.get_url(url)
+
+        transactions = []
+        for tx in response.json()['txs']:
+            my_outs = [
+                float(x['value']) for x in tx['vout'] if address in x['scriptPubKey']['addresses']
+            ]
+            transactions.append(dict(
+                amount=sum(my_outs),
+                date=arrow.get(tx['time']).datetime,
+                txid=tx['txid'],
+                confirmations=tx['confirmations'],
+            ))
+
+        return transactions
+
+
+class ReddcoinCom(BitpayInsight):
+    supported_cryptos = ['rdd']
+    domain = "http://live.reddcoin.com"
+
+
+class BirdOnWheels(BitpayInsight):
+    supported_cryptos = ['myr']
+    domain = "http://birdonwheels5.no-ip.org:3000"
+
+# some meta magic to get a list of all service classes.
+ALL_SERVICES = [x for x in globals().copy().values() if hasattr(x, 'mro') and x.mro()[-2] == Service and x != Service]
