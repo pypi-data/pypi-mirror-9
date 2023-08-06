7b360e45cd32b00233ce680e7bfe3cb61911c2ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:18 +0000

    Add package jsc/5.2/jsc-5.2.tar.gz

---
+++ packages/jsc/jsc-5.2.tar.gz/jsc-5.2/jsc/__init__.py
@@ -0,0 +1,6 @@
+__version__ = "5.2"
+__description__ = "Jumpstarter Console"
+__long_description__ = """
+Jumpstarter Console - A command line tool for managing your Jumpstarter projects.
+please visit https://github.com/jumpstarter-io/jsc for more information
+"""
