866b66757a444e7422a77f54d7db9071455118b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:05 +0000

    Add package aioutils/0.2.0/aioutils-0.2.0.tar.gz

---
+++ packages/aioutils/aioutils-0.2.0.tar.gz/aioutils-0.2.0/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+import sys
+
+from setuptools import setup
+
+from aioutils import __version__
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    sys.exit()
+
+with open('HISTORY.md') as f:
+    history = f.read()
+
+setup(
+    name='aioutils',
+    version=__version__,
+    description='Python3 Asyncio Utils',
+    long_description= history,
+    author='Jingchao Hu',
+    author_email='jingchaohu@gmail.com',
+    url='https://github.com/observerss/aioutils',
+    packages=['aioutils'],
+    license='Apache 2.0',
+    zip_safe=True,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4'
+        ],
+    )
