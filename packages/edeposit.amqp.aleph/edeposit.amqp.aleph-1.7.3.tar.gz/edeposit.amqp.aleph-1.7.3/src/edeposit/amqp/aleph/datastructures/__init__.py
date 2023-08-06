f8d77ee763ec7d91394a9935a7ca7411fb4daac6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:21 +0000

    Add package edeposit.amqp.aleph/1.7.3/edeposit.amqp.aleph-1.7.3.tar.gz

---
+++ packages/edeposit.amqp.aleph/edeposit.amqp.aleph-1.7.3.tar.gz/edeposit.amqp.aleph-1.7.3/src/edeposit/amqp/aleph/datastructures/__init__.py
@@ -0,0 +1,7 @@
+# __import__('pkg_resources').declare_namespace(__name__)
+
+from results import *
+from requests import *
+from alephrecord import *
+from epublication import *
+from semanticinfo import *
