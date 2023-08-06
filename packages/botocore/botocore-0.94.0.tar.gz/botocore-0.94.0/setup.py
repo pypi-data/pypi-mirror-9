bf57f8d8658b0b299ac78b3e5a62440ab11cdc3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:27 +0000

    Add package botocore/0.94.0/botocore-0.94.0.tar.gz

---
+++ packages/botocore/botocore-0.94.0.tar.gz/botocore-0.94.0/setup.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+
+"""
+distutils/setuptools install script.
+"""
+
+import sys
+import botocore
+
+from setuptools import setup, find_packages
+
+
+requires = ['jmespath==0.6.1',
+            'python-dateutil>=2.1,<3.0.0']
+
+
+if sys.version_info[:2] == (2, 6):
+    # For python2.6 we have a few other dependencies.
+    # First we need an ordered dictionary so we use the
+    # 2.6 backport.
+    requires.append('ordereddict==1.1')
+    # Then we need simplejson.  This is because we need
+    # a json version that allows us to specify we want to
+    # use an ordereddict instead of a normal dict for the
+    # JSON objects.  The 2.7 json module has this.  For 2.6
+    # we need simplejson.
+    requires.append('simplejson==3.3.0')
+
+
+setup(
+    name='botocore',
+    version=botocore.__version__,
+    description='Low-level, data-driven core of boto 3.',
+    long_description=open('README.rst').read(),
+    author='Mitch Garnaat',
+    author_email='garnaat@amazon.com',
+    url='https://github.com/boto/botocore',
+    scripts=[],
+    packages=find_packages(exclude=['tests*']),
+    package_data={'botocore': ['data/*.json', 'data/aws/*.json'],
+                  'botocore.vendored.requests': ['*.pem']},
+    package_dir={'botocore': 'botocore'},
+    include_package_data=True,
+    install_requires=requires,
+    license=open("LICENSE.txt").read(),
+    classifiers=(
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Natural Language :: English',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+    ),
+)
