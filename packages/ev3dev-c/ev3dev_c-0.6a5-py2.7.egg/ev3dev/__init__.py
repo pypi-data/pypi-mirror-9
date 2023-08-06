d8206ed83e345732fdc90ea7aeddce975c010f23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:04 +0000

    Add package ev3dev-c/0.6a5/ev3dev_c-0.6a5-py2.7.egg

---
+++ packages/ev3dev-c/ev3dev_c-0.6a5-py2.7.egg/ev3dev/__init__.py
@@ -0,0 +1,9 @@
+from ev3 import *
+
+__library__ = "ev3dev-c"
+__version__ = "0.6a5"
+__kernel__ = "3.16.7-ckt6-ev3dev1"
+__description__ = "LEGO Mindstorms EV3 Debian C library + Python, Ruby and Perl wrappers"
+__author__ = "Vitaly Kravtsov"
+__author_email__ = "in4lio@gmail.com"
+__url__ = "https://github.com/in4lio/ev3dev-c"
