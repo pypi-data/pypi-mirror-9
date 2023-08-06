b3f81b180b0d408ddd45d220ed62d017f658d1ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:03 +0000

    Add package MaterialDjango/0.1a4/MaterialDjango-0.1a4.tar.gz

---
+++ packages/MaterialDjango/MaterialDjango-0.1a4.tar.gz/MaterialDjango-0.1a4/setup.py
@@ -0,0 +1,24 @@
+#! /usr/bin/env python
+
+from setuptools import setup
+from setuptools import find_packages
+
+
+setup(name="MaterialDjango",
+      version="0.1a4",
+      packages=find_packages(),
+      # scripts=[],
+      author="Jack Laxson",
+      author_email="jack@getpizza.cat",
+      description="A Django library bringing paper-ui and core-ui polymer elements to django",
+      license="MIT",
+      include_package_data=True,
+      install_requires=["django", "vulcanize", 'bower.py'],
+      url="https://github.com/Colorless-Green-Ideas/MaterialDjango",
+      keywords="polymer paper-ui widgets theme vulcanize bower",
+      classifiers=["License :: OSI Approved :: MIT License",
+      'Intended Audience :: Developers', 'Development Status :: 3 - Alpha', 
+      "Framework :: Django", "Topic :: Text Processing :: Markup :: HTML", 
+      'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+      'Programming Language :: Python :: 2',]
+      )
