9a14fe7dbdd59b0c6738ae3f163ef8772476a805
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:00 +0000

    Add package Products.PortalTransforms/2.1.5/Products.PortalTransforms-2.1.5.zip

---
+++ packages/Products.PortalTransforms/Products.PortalTransforms-2.1.5.zip/Products.PortalTransforms-2.1.5/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup, find_packages
+
+version = '2.1.5'
+
+setup(name='Products.PortalTransforms',
+      version=version,
+      description="MIME based content transformations",
+      long_description=open("README.rst").read() + "\n" + \
+                       open("CHANGES.rst").read(),
+      classifiers=[
+        "Framework :: Zope2",
+        "Operating System :: OS Independent",
+        ],
+      keywords='Zope Plone Transform',
+      author='Benjamin Saller',
+      author_email='plone-developers@lists.sourceforge.net',
+      url='http://pypi.python.org/pypi/Products.PortalTransforms',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['Products'],
+      include_package_data=True,
+      zip_safe=False,
+      extras_require=dict(
+        test=[
+            'zope.testing',
+            'Products.Archetypes',
+            'Pillow',
+        ],
+      ),
+      install_requires=[
+          'setuptools',
+          'plone.intelligenttext',
+          'zope.interface',
+          'zope.structuredtext',
+          'Products.CMFCore',
+          'Products.CMFDefault',
+          'Products.MimetypesRegistry',
+          'Acquisition',
+          'ZODB3',
+          'Zope2',
+          'Markdown>=1.7',
+      ],
+      )
