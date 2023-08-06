7db481c3a4130e25672aad5a957e7e6c7095a071
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:30 +0000

    Add package plotutils/0.3.2/plotutils-0.3.2.tar.gz

---
+++ packages/plotutils/plotutils-0.3.2.tar.gz/plotutils-0.3.2/plotutils/__init__.py
@@ -0,0 +1,4 @@
+"""
+A module with various plotting utilities
+"""
+from .plotutils import setfig,plot2dhist
---
+++ packages/plotutils/plotutils-0.3.2.tar.gz/plotutils-0.3.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+setup(name = "plotutils",
+    version = "0.3.2",
+    description = "Simple matplotlib-related utility functions.",
+    long_description = readme(),
+    author = "Timothy D. Morton",
+    author_email = "tim.morton@gmail.com",
+    url = "https://github.com/timothydmorton/plotutils",
+    packages = ['plotutils'],
+    classifiers=[
+      'Development Status :: 3 - Alpha',
+      'Intended Audience :: Science/Research',
+      'License :: OSI Approved :: GNU General Public License (GPL)',
+      'Operating System :: OS Independent',
+      'Programming Language :: Python',
+      ],
+    zip_safe=False,
+    install_requires=['matplotlib']
+) 
