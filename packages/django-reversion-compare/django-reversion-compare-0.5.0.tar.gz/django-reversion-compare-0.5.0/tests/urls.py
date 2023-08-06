07093932c6cc8af7456aa7ec05986c36832143ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:00 +0000

    Add package django-reversion-compare/0.5.0/django-reversion-compare-0.5.0.tar.gz

---
+++ packages/django-reversion-compare/django-reversion-compare-0.5.0.tar.gz/django-reversion-compare-0.5.0/tests/urls.py
@@ -0,0 +1,28 @@
+# coding: utf-8
+
+from django.conf.urls import patterns, include, url
+from django.conf import settings
+
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.contrib import admin
+from django.shortcuts import redirect
+
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Uncomment the admin/doc line below to enable admin documentation:
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    url(r'^admin/', include(admin.site.urls)),
+
+    url(
+        r'^media/(?P<path>.*)$', 'django.views.static.serve',
+        {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}
+    ),
+
+    # redirect root view to admin page:
+    url(r'^$', lambda x: redirect("admin:index")),
+)
+
+urlpatterns += staticfiles_urlpatterns()
