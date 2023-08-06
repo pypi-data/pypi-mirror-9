e89b40eb0f2e4fdf893e61e9d4a6634fb26a93a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:07:40 +0000

    Add package PostgreSQL-Audit/0.5.1/PostgreSQL-Audit-0.5.1.tar.gz

---
+++ packages/PostgreSQL-Audit/PostgreSQL-Audit-0.5.1.tar.gz/PostgreSQL-Audit-0.5.1/postgresql_audit/__init__.py
@@ -0,0 +1,16 @@
+from .base import (  # noqa
+    activity_base,
+    assign_actor,
+    ImproperlyConfigured,
+    versioning_manager,
+    VersioningManager
+)
+from .expressions import jsonb_change_key_name, jsonb_merge  # noqa
+from .migrations import (  # noqa
+    add_column,
+    alter_column,
+    change_column_name,
+    remove_column
+)
+
+__version__ = '0.5.1'
