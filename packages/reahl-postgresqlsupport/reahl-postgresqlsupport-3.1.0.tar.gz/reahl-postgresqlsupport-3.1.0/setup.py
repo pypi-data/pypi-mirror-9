41a8883025d90f504532f10894eee9f420c64f27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:12 +0000

    Add package reahl-postgresqlsupport/3.1.0/reahl-postgresqlsupport-3.1.0.tar.gz

---
+++ packages/reahl-postgresqlsupport/reahl-postgresqlsupport-3.1.0.tar.gz/reahl-postgresqlsupport-3.1.0/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, Command
+class InstallTestDependencies(Command):
+    user_options = []
+    def run(self):
+        from setuptools.command import easy_install
+        easy_install.main(self.distribution.tests_require)
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+setup(
+    name='reahl-postgresqlsupport',
+    version='3.1.0',
+    description='Support for using PostgreSQL with Reahl.',
+    long_description='Reahl is a web application framework that allows a Python programmer to work in terms of useful abstractions - using a single programming language.\n\nThis package contains infrastructure necessary to use Reahl with PostgreSQL.\n\nSee http://www.reahl.org/docs/3.1/tutorial/gettingstarted.d.html for installation instructions. ',
+    url='http://www.reahl.org',
+    maintainer='Iwan Vosloo',
+    maintainer_email='iwan@reahl.org',
+    packages=['reahl'],
+    py_modules=['setup'],
+    include_package_data=False,
+    package_data={'': ['*/LC_MESSAGES/*.mo']},
+    namespace_packages=['reahl'],
+    install_requires=['reahl-component>=3.1,<3.2', 'psycopg2>=2.5,<2.5.9999'],
+    setup_requires=['reahl-bzrsupport>=3.1,<3.2'],
+    tests_require=[],
+    test_suite='tests',
+    entry_points={
+        'reahl.component.databasecontrols': [
+            'PostgresqlControl = reahl.postgresqlsupport:PostgresqlControl'    ],
+        'reahl.eggs': [
+            'Egg = reahl.component.eggs:ReahlEgg'    ],
+                 },
+    extras_require={},
+    cmdclass={'install_test_dependencies': InstallTestDependencies}
+)
