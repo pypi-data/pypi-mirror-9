078e169a11ac3bcf6b0592323de216d763742cd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:38 +0000

    Add package fabdeploit/0.12.0/fabdeploit-0.12.0.tar.gz

---
+++ packages/fabdeploit/fabdeploit-0.12.0.tar.gz/fabdeploit-0.12.0/fabdeploit/__init__.py
@@ -0,0 +1,15 @@
+from __future__ import absolute_import
+
+from . import git
+from . import django
+from . import virtualenv
+from . import drupal
+
+Git = git.Git
+GitFilter = git.GitFilter
+Django = django.Django
+Virtualenv = virtualenv.Virtualenv
+Virtualenv2 = virtualenv.Virtualenv2
+Virtualenv3 = virtualenv.Virtualenv3
+VirtualenvGit = virtualenv.VirtualenvGit
+Drupal = drupal.Drupal
