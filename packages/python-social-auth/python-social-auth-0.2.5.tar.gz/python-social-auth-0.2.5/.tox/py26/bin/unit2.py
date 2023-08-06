9197168e00649b2c7a3bf5e08e0416160636701c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:26 +0000

    Add package python-social-auth/0.2.5/python-social-auth-0.2.5.tar.gz

---
+++ packages/python-social-auth/python-social-auth-0.2.5.tar.gz/python-social-auth-0.2.5/.tox/py26/bin/unit2.py
@@ -0,0 +1,7 @@
+#!/home/omab/src/python-social-auth/.tox/py26/bin/python2.6
+
+__unittest = True
+
+from unittest2.main import main_
+
+main_()
---
+++ packages/python-social-auth/python-social-auth-0.2.5.tar.gz/python-social-auth-0.2.5/.tox/py34/bin/unit2.py
@@ -0,0 +1,7 @@
+#!/home/omab/src/python-social-auth/.tox/py34/bin/python3.4
+
+__unittest = True
+
+from unittest2 import main_
+
+main_()
