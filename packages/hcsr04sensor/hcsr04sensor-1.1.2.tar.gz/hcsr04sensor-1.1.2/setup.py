630b5cd76781cd1aa7adef6ba5f5153a44dd6e19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:03 +0000

    Add package hcsr04sensor/1.1.2/hcsr04sensor-1.1.2.tar.gz

---
+++ packages/hcsr04sensor/hcsr04sensor-1.1.2.tar.gz/hcsr04sensor-1.1.2/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+version = '1.1.2'
+
+setup(name='hcsr04sensor',
+      version=version,
+      description='Module to access the HCSR04 sensor on a Raspberry Pi',
+      long_description=open("./README.md", "r").read(),
+      classifiers=[
+          "Development Status :: 5 - Production/Stable",
+          "Environment :: Console",
+          "Intended Audience :: End Users/Desktop",
+          "Natural Language :: English",
+          "Operating System :: POSIX :: Linux",
+          "Programming Language :: Python :: 2 :: Only",
+          "Programming Language :: Python :: 2.7",
+          "Topic :: Home Automation",
+          "License :: OSI Approved :: MIT License",
+          ],
+      author='Al Audet',
+      author_email='alaudet@linuxnorth.org',
+      url='https://github.com/alaudet/hcsr04sensor',
+      download_url='https://github.com/alaudet/hcsr04sensor/releases',
+      license='MIT License',
+      packages=['hcsr04sensor'],
+      install_requires=['RPi.GPIO']
+      )
