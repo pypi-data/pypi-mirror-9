f2e33783c9b682a7f169ec5e23919c765455a8cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:18 +0000

    Add package Mixpanel-Expert/0.1.0/Mixpanel-Expert-0.1.0.tar.gz

---
+++ packages/Mixpanel-Expert/Mixpanel-Expert-0.1.0.tar.gz/Mixpanel-Expert-0.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name="Mixpanel-Expert",
+    version='0.1.0',
+    url='https://github.com/mixpanel/mixpanel-expert',
+    packages=['expert'],
+    license='LICENSE.txt',
+    description='Mixpanel Tools and Utils',
+    long_description=open('README.txt').read()
+)
