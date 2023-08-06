a07e0d78f6e284ffbb892e17c4ab5a1ea0935153
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:59 +0000

    Add package optional/0.0.1/optional-0.0.1.tar.gz

---
+++ packages/optional/optional-0.0.1.tar.gz/optional-0.0.1/setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+from distutils.core import setup
+
+
+setup(name='optional',
+      version='0.0.1',
+      description='Optional Class for Python2/3',
+      author='Alex',
+      url='https://github.com/alex----/optional',
+      py_modules=['optional'])
