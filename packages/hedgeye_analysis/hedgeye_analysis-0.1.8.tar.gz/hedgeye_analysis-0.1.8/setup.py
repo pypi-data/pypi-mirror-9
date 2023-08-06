8b42dd3860330db3beccdb0d4dc180b09597b689
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:36:53 +0000

    Add package hedgeye_analysis/0.1.8/hedgeye_analysis-0.1.8.tar.gz

---
+++ packages/hedgeye_analysis/hedgeye_analysis-0.1.8.tar.gz/hedgeye_analysis-0.1.8/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+      name = 'hedgeye_analysis',
+      version = '0.1.8',
+      description = 'Python analysis tools used by Hedgeye',
+      url = 'https://github.com/hedgeyedev/hedgeye-python-analysis/hedgeye_analysis',
+      author='James Lavin',
+      author_email='jlavin@hedgeye.com',
+      license='MIT',
+      packages=find_packages(exclude=['test','test.*']),
+      install_requires=[
+          'IPython',
+          'matplotlib',
+          'numpy',
+          'pandas',
+          'seaborn',
+      ],
+      test_suite='nose.collector',
+      classifiers = [
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.4'
+      ],
+      zip_safe=False
+      )
