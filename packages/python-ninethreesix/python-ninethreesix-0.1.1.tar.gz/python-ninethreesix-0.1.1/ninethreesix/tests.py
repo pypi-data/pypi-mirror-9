0d0b6036e77afaa3375517ec1df09ec1e839a6a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:29:43 +0000

    Add package python-ninethreesix/0.1.1/python-ninethreesix-0.1.1.tar.gz

---
+++ packages/python-ninethreesix/python-ninethreesix-0.1.1.tar.gz/python-ninethreesix-0.1.1/ninethreesix/tests.py
@@ -0,0 +1,26 @@
+from nose.tools import eq_
+from .password import Password
+
+
+def test_init():
+    p = Password()
+
+    # Test value of default parameters
+    eq_(p.num_words, 3)
+    eq_(p.min_len, 3)
+    eq_(p.max_len, 6)
+    eq_(p.content.find("\n"), -1)  # should contain no newlines
+
+
+def test_password():
+    p = Password()
+    result = p.password()
+    eq_(type(result), list)
+    eq_(len(result), 3)
+
+
+def test_as_string():
+    p = Password()
+    result = p.as_string()
+    eq_(type(result), str)  # should return a sring
+    eq_(result.count("-"), 2)  # count the delimiters
