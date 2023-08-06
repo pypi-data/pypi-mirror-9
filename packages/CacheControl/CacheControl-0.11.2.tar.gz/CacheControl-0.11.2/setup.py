679fe7fe9a204693281df9d42e31d3eae823f1a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:35 +0000

    Add package CacheControl/0.11.2/CacheControl-0.11.2.tar.gz

---
+++ packages/CacheControl/CacheControl-0.11.2.tar.gz/CacheControl-0.11.2/setup.py
@@ -0,0 +1,41 @@
+import setuptools
+
+long_description = open('README.rst').read()
+
+VERSION = '0.11.2'
+
+setup_params = dict(
+    name='CacheControl',
+    version=VERSION,
+    author='Eric Larson',
+    author_email='eric@ionrock.org',
+    license='MIT',
+    url='https://github.com/ionrock/cachecontrol',
+    keywords='requests http caching web',
+    packages=setuptools.find_packages(),
+    description='httplib2 caching for requests',
+    long_description=long_description,
+    install_requires=[
+        'requests',
+    ],
+    extras_require = {
+        'filecache': ['lockfile'],
+    },
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Internet :: WWW/HTTP',
+    ],
+)
+
+
+if __name__ == '__main__':
+    setuptools.setup(**setup_params)
