c6b3a63413e23b452f2442402b4e5fae0a83489f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:07:34 +0000

    Add package wechatpy/0.8.0/wechatpy-0.8.0.zip

---
+++ packages/wechatpy/wechatpy-0.8.0.zip/wechatpy-0.8.0/wechatpy/enterprise/client/api/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, unicode_literals
+
+from .department import WeChatDepartment  # NOQA
+from .media import WeChatMedia  # NOQA
+from .message import WeChatMessage  # NOQA
+from .menu import WeChatMenu  # NOQA
+from .tag import WeChatTag  # NOQA
+from .user import WeChatUser  # NOQA
+from .misc import WeChatMisc  # NOQA
+from .agent import WeChatAgent  # NOQA
+from .batch import WeChatBatch  # NOQA
