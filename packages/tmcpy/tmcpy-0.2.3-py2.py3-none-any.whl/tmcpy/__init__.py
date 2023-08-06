a1155f8d4ec39fc1025a2aa58551dae34ded56a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:20 +0000

    Add package tmcpy/0.2.3/tmcpy-0.2.3-py2.py3-none-any.whl

---
+++ packages/tmcpy/tmcpy-0.2.3-py2.py3-none-any.whl/tmcpy/__init__.py
@@ -0,0 +1,10 @@
+# coding: utf8
+from __future__ import absolute_import, unicode_literals
+from tmcpy.client import TmcClient
+
+__all__ = [
+    'TmcClient',
+    '__version__',
+]
+
+__version__ = '0.2.3'
