1a377e6cdd2c1d1d98dac2c98a8e504eb9a9e1ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:36:49 +0000

    Add package crosservice/0.2.10.1/crosservice-0.2.10.1.tar.gz

---
+++ packages/crosservice/crosservice-0.2.10.1.tar.gz/crosservice-0.2.10.1/CHANGES.rst
@@ -0,0 +1,66 @@
+v.1.0
+=====
+
+* Initial verion
+
+0.2.0
+=====
+
+* Redesign handlers and signals
+* Replace json serializer by pickle
+* Add more logs to client and server
+
+0.2.1
+=====
+
+Dummy release
+
+0.2.2
+=====
+
+* Check required data input in handlers
+* Refacroting
+
+0.2.3
+=====
+
+* Add abstract handlers
+
+0.2.4
+=====
+
+* Cover and log all exceptions in handler's run method in BaseHandler
+
+0.2.5
+=====
+
+* Fix BaseResult class
+* Add some readme text
+
+0.2.6
+=====
+
+* Fix in handler's required data check
+* Fix typos
+
+0.2.7
+=====
+
+* Bugfix
+
+0.2.8
+=====
+
+* Getting result as dict
+* Clean up keys, passed to handler
+* Resource usage improvements
+
+0.2.9
+=====
+
+* Improve BaseResult class
+
+0.2.10
+======
+
+* Add protocol version into messages
---
+++ packages/crosservice/crosservice-0.2.10.1.tar.gz/crosservice-0.2.10.1/setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+
+_version = '0.2.10.1'
+
+setup(
+    name='crosservice',
+    version='0.2.10.1',
+    packages=['crosservice'],
+    install_requires=open('requirements.txt').read(),
+    license='GPL',
+    author='derfenix',
+    author_email='derfenix@gmail.com',
+    description='Gevent powered cross service communication tool',
+    url='https://github.com/derfenix/crosservice',
+    download_url='https://github.com/derfenix/crosservice/archive/v.{0}'
+                 '.tar.gz'.format(_version),
+    keywords=['communication', 'gevent', 'socket'],
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Topic :: Internet',
+        'Topic :: Software Development :: Libraries',
+    ],
+)
