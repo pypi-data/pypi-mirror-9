06edab5bbf5012f8c572880e19199c5b24fe0340
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:58 +0000

    Add package eelbrain/0.14.8/eelbrain-0.14.8.tar.gz

---
+++ packages/eelbrain/eelbrain-0.14.8.tar.gz/eelbrain-0.14.8/eelbrain/experiment/__init__.py
@@ -0,0 +1,6 @@
+# Author: Christian Brodbeck <christianbrodbeck@nyu.edu>
+
+"""Objects for managing files"""
+
+from ._experiment import TreeModel, FileTree
+from ._mne_experiment import MneExperiment
