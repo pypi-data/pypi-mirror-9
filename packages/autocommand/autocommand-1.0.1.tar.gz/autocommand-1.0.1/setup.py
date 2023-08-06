41cbf938ad41ef33eef0f87ed7dfd66bfbd9cfd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:51 +0000

    Add package autocommand/1.0.1/autocommand-1.0.1.tar.gz

---
+++ packages/autocommand/autocommand-1.0.1.tar.gz/autocommand-1.0.1/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+
+def getfile(filename):
+    with open(filename) as file:
+        return file.read()
+
+setup(
+    name='autocommand',
+    version='1.0.1',
+    py_modules=['autocommand'],
+    package_dir={'': 'src'},
+    platforms='any',
+    license='LGPLv3',
+    author='Nathan West',
+    url='https://github.com/Lucretiel/autocommand',
+    description='A library to create a command-line program from a function',
+    long_description=getfile('README.rst'),
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
