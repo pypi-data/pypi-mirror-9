d18eff1190c5ccd87d24f0793f21bac34a5a3ee3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:36:00 +0000

    Add package sms_utilities/0.0.3/sms_utilities-0.0.3.tar.gz

---
+++ packages/sms_utilities/sms_utilities-0.0.3.tar.gz/sms_utilities-0.0.3/sms_utilities/SMS_Parse.py
@@ -0,0 +1,35 @@
+"""SMS parsing."""
+
+import sys
+
+from smspdu import SMS_SUBMIT
+
+from rpdu import RPDU
+from SMS_Helper import clean, smspdu_charstring_to_hex
+
+
+def parse(rp_message):
+    rpdu = RPDU.fromPDU(rp_message)
+    sms_submit = SMS_SUBMIT.fromPDU(rpdu.user_data, rpdu.rp_originator_address)
+    exports = [
+        ("vbts_text", sms_submit.user_data),
+        ("vbts_tp_user_data", smspdu_charstring_to_hex(sms_submit.tp_ud)),
+        ("vbts_tp_data_coding_scheme", sms_submit.tp_dcs),
+        ("vbts_tp_protocol_id", sms_submit.tp_pid),
+        ("vbts_tp_dest_address", sms_submit.tp_da),
+        ("vbts_tp_dest_address_type", sms_submit.tp_toa),
+        ("vbts_tp_message_type", sms_submit.tp_mti),
+        ("vbts_rp_dest_address", rpdu.rp_destination_address),
+        ("vbts_rp_originator_address", rpdu.rp_originator_address),
+        ("vbts_rp_originator_address_type", rpdu.rp_originator_address_type),
+        ("vbts_rp_message_reference", rpdu.rp_message_reference),
+        ("vbts_rp_message_type", rpdu.rp_mti)
+    ]
+    exports = [(x, clean(y)) for (x, y) in exports]
+    return exports
+
+if __name__ == '__main__':
+    h = "001000038100000e05df04810011000005cbb7fb0c02"
+    if len(sys.argv) > 1:
+        h = sys.argv[1]
+    print parse(h)
---
+++ packages/sms_utilities/sms_utilities-0.0.3.tar.gz/sms_utilities-0.0.3/sms_utilities/__init__.py
@@ -0,0 +1,3 @@
+"""Core modules."""
+
+import rpdu, SMS_Deliver, SMS_Helper, SMS_Parse, SMS_Submit
