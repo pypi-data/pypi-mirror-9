205be51f78419a518d7bf298321cd208a6dfefbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:29 +0000

    Add package myfirstest/1.0.0/myfirstest-1.0.0.zip

---
+++ packages/myfirstest/myfirstest-1.0.0.zip/myfirstest-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+		name 		= 'myfirstest',
+		version	 	= '1.0.0',
+		py_modules  = ['test'],
+		author 		= 'hfpython',
+		author_email = 'callable',
+		url  		= 'basestring',
+		description = 'abs'
+	)
---
+++ packages/myfirstest/myfirstest-1.0.0.zip/myfirstest-1.0.0/test.py
@@ -0,0 +1,3 @@
+def print_lol(the_list):
+	print(the_list)
+print_lol("hello")
