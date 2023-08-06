50c3e25b7214c70d94b193b5faf6f4e53becdf5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:53 +0000

    Add package ssh-exec-test/0.1.0/ssh-exec-test-0.1.0.tar.gz

---
+++ packages/ssh-exec-test/ssh-exec-test-0.1.0.tar.gz/ssh-exec-test-0.1.0/setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    author="Jorge Niedbalski R. <jnr@metaklass.org>",
+    name="ssh-exec-test",
+    version="0.1.0",
+    setup_requires=['pbr'],
+    pbr=True,
+)
