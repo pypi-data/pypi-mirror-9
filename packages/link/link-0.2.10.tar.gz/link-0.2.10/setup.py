c892b1b79ac89e7909d6cf154f3d3f641e0d420f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:43 +0000

    Add package link/0.2.10/link-0.2.10.tar.gz

---
+++ packages/link/link-0.2.10.tar.gz/link-0.2.10/setup.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+from setuptools import setup
+import os
+import link
+
+dir = os.path.split(os.path.abspath(__file__))[0]
+
+
+DESCRIPTION = "Easy and consistent access to the objects you care about"
+LONG_DESCRIPTION = "Easy and consistent access to the objects you care about"
+URL = ''
+DOWNLOAD_URL = ''
+CLASSIFIERS = ['Development Status :: 4 - Beta',     
+               'Programming Language :: Python',
+               'Programming Language :: Python :: 2'
+              ]
+EMAIL = ''
+SETUP_ARGS = {}
+DATA_FILES = [('link/configs', ['link/configs/link.config'])]
+REQUIRES = ['requests>=2.0.0', 'requests_oauthlib>=0.4.0' ]
+
+try:
+    import numpy
+    import pandas
+    import xmltodict
+except:
+    print "We highly suggest you install numpy and Pandas for some functionality" 
+    print "easy_install numpy"
+    print "easy_install pandas"
+    print "xmltodict is used in Alexa's json wrapper"
+    print "pip install xmltodict"  
+  
+# write out the version file so we can keep track on what version the built
+# package is
+
+# call setup so it can build the package
+setup(name=link.__title__,
+      version=link.__version__,
+      description=DESCRIPTION,
+      long_description=LONG_DESCRIPTION,
+      license=link.__license__,
+      maintainer_email=EMAIL,
+      maintainer=link.__author__,
+      url=URL,
+      packages=['link', 'link.wrappers', 'link.configs'],
+      #package_data = {'link.configs': ['link.configs/*config']},
+      install_requires = REQUIRES,
+      #data_files = DATA_FILES,
+      classifiers=CLASSIFIERS,
+      **SETUP_ARGS)
