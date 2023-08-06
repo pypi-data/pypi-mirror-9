1863399450e07cbe99bab73829ef6bfed3fdeda3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:52:55 +0000

    Add package currint/1.2.0/currint-1.2.0.tar.gz

---
+++ packages/currint/currint-1.2.0.tar.gz/currint-1.2.0/currint/amount.py
@@ -0,0 +1,84 @@
+from decimal import Decimal, ROUND_HALF_UP, InvalidOperation
+
+
+class Amount(object):
+    """
+    An amount of a currency.
+    """
+
+    def __init__(self, currency, value):
+        """
+        Initialises the Amount with a Currency object and an
+        integer value of its minor unit (i.e. cents for USD)
+        """
+        assert isinstance(value, (int, long))
+        assert not isinstance(currency, basestring)
+        self.currency = currency
+        self.value = value
+
+    @classmethod
+    def from_code_and_minor(cls, currency_code, value):
+        """
+        Initialises the amount with a currency code and an integer value
+        of minor units
+        """
+        from .currency import currencies
+        try:
+            return cls(currencies[currency_code.upper()], value)
+        except KeyError:
+            raise ValueError("Invalid currency code %s" % currency_code)
+
+    @classmethod
+    def from_code_and_major(cls, currency_code, value):
+        """
+        Initialises the amount with a currency code and a value
+        in the major unit (e.g. "1.43", Decimal("1.43"), 10)
+        """
+        from .currency import currencies
+        try:
+            currency = currencies[currency_code.upper()]
+            return cls(currency, currency.major_to_minor(Decimal(value)))
+        except KeyError:
+            raise ValueError("Invalid currency code %s" % currency_code)
+        except InvalidOperation:
+            raise ValueError("Invalid currency value %s" % value)
+
+    def __str__(self):
+        return unicode(self).encode("utf8")
+
+    def __unicode__(self):
+        return self.currency.format(self.value)
+
+    def __repr__(self):
+        return "<Amount %s, %s>" % (self.currency, self.value)
+
+    def __eq__(self, other):
+        return (self.currency == other.currency) and (self.value == other.value)
+
+    def __ne__(self, other):
+        return not (self == other)
+
+    def __add__(self, other):
+        if self.currency != other.currency:
+            raise ValueError("You cannot add amounts of different currencies (%s and %s)" % (self.currency, other.currency))
+        return Amount(self.currency, self.value + other.value)
+
+    def __sub__(self, other):
+        if self.currency != other.currency:
+            raise ValueError("You cannot subtract amounts of different currencies (%s and %s)" % (self.currency, other.currency))
+        return Amount(self.currency, self.value - other.value)
+
+    def __nonzero__(self):
+        return bool(self.value)
+
+    def apply_factor(self, other):
+        if not isinstance(other, (int, long, Decimal)):
+            raise ValueError("You can only apply an integer, long or Decimal factor to an Amount")
+        return Amount(
+            self.currency,
+            int(Decimal(self.value * other).to_integral(ROUND_HALF_UP)),
+        )
+
+    def to_major_decimal(self):
+        "Returns our value as a Decimal of major units"
+        return self.currency.minor_to_major(self.value)
---
+++ packages/currint/currint-1.2.0.tar.gz/currint-1.2.0/currint/tests/test_amount.py
@@ -0,0 +1,125 @@
+# encoding: utf8
+from __future__ import unicode_literals
+from decimal import Decimal
+from unittest import TestCase
+from ..currency import currencies
+from ..amount import Amount
+
+
+class AmountTests(TestCase):
+
+    def test_equality(self):
+        self.assertEqual(
+            Amount(currencies["GBP"], 132),
+            Amount(currencies["GBP"], 132),
+        )
+        self.assertNotEqual(
+            Amount(currencies["GBP"], 132),
+            Amount(currencies["USD"], 132),
+        )
+        self.assertNotEqual(
+            Amount(currencies["GBP"], 132),
+            Amount(currencies["GBP"], 99),
+        )
+
+    def test_add(self):
+        self.assertEqual(
+            Amount(currencies["GBP"], 132) + Amount(currencies["GBP"], 100),
+            Amount(currencies["GBP"], 232),
+        )
+        with self.assertRaises(ValueError):
+            Amount(currencies["GBP"], 132) + Amount(currencies["USD"], 100)
+
+    def test_subtract(self):
+        self.assertEqual(
+            Amount(currencies["GBP"], 132) - Amount(currencies["GBP"], 100),
+            Amount(currencies["GBP"], 32),
+        )
+        with self.assertRaises(ValueError):
+            Amount(currencies["GBP"], 132) - Amount(currencies["USD"], 100)
+
+    def test_format(self):
+        self.assertEqual(
+            unicode(Amount(currencies["USD"], 132)),
+            "$1.32",
+        )
+        self.assertEqual(
+            unicode(Amount(currencies["GBP"], 132)),
+            "£1.32",
+        )
+        self.assertEqual(
+            unicode(Amount(currencies["MRO"], 7)),
+            "1.2 MRO",
+        )
+
+    def test_bool(self):
+        self.assertTrue(Amount(currencies["USD"], 1))
+        self.assertFalse(Amount(currencies["USD"], 0))
+        self.assertTrue(Amount.from_code_and_major("USD", Decimal('0.01')))
+        self.assertFalse(Amount.from_code_and_major("USD", Decimal('0.00')))
+
+    def test_apply_factor(self):
+        self.assertEqual(
+            Amount(currencies["GBP"], 150).apply_factor(2),
+            Amount(currencies["GBP"], 300),
+        )
+        self.assertEqual(
+            Amount(currencies["GBP"], 100).apply_factor(Decimal("1.004")),
+            Amount(currencies["GBP"], 100),
+        )
+        self.assertEqual(
+            Amount(currencies["GBP"], 100).apply_factor(Decimal("1.005")),
+            Amount(currencies["GBP"], 101),
+        )
+        with self.assertRaises(ValueError):
+            Amount(currencies["GBP"], 100).apply_factor(1.005)
+
+    def test_from_code_and_minor(self):
+        self.assertEqual(
+            Amount.from_code_and_minor("GBP", 300),
+            Amount(currencies["GBP"], 300),
+        )
+        self.assertEqual(
+            Amount.from_code_and_minor("gbp", 300),
+            Amount(currencies["GBP"], 300),
+        )
+        with self.assertRaises(ValueError):
+            Amount.from_code_and_minor("WAITWHAT", 100)
+
+    def test_from_code_and_major(self):
+        self.assertEqual(
+            Amount.from_code_and_major("GBP", "3.00"),
+            Amount(currencies["GBP"], 300),
+        )
+        self.assertEqual(
+            Amount.from_code_and_major("gbp", ".10"),
+            Amount(currencies["GBP"], 10),
+        )
+        self.assertEqual(
+            Amount.from_code_and_major("GBP", 10),
+            Amount(currencies["GBP"], 1000),
+        )
+        self.assertEqual(
+            Amount.from_code_and_major("GBP", Decimal("10.01")),
+            Amount(currencies["GBP"], 1001),
+        )
+        with self.assertRaises(ValueError):
+            Amount.from_code_and_major("WAITWHAT", 100)
+        with self.assertRaises(ValueError):
+            Amount.from_code_and_major("GBP", "12.432")
+        with self.assertRaises(ValueError):
+            Amount.from_code_and_major("GBP", "aaaaaaah")
+
+    def test_to_major_decimal(self):
+        self.assertEqual(
+            Amount(currencies["GBP"], 300).to_major_decimal(),
+            Decimal("3.00"),
+        )
+        self.assertEqual(
+            Amount(currencies["USD"], 3).to_major_decimal(),
+            Decimal("0.03"),
+        )
+        self.assertEqual(
+            Amount(currencies["MRO"], 7).to_major_decimal(),
+            Decimal("1.4"),  # It's written 1.2, but is 1.4 of the major unit
+        )
