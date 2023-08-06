79ac7835ae20a53fedaf8aedb719631feab4141d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:01 +0000

    Add package pylfer/0.0.1/pylfer-0.0.1.tar.gz

---
+++ packages/pylfer/pylfer-0.0.1.tar.gz/pylfer-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='pylfer',
+    version='0.0.1',
+    packages=[],
+    url='',
+    license='MIT',
+    author='eric-czech',
+    author_email='eric.allen.czech@gmail.com',
+    description='Python bridge for various javascript based data visualization frameworks (e.g. d3 and highcharts)'
+)
