0eb6915dc737059bf13c2dd03ab5f197a772bc63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:00 +0000

    Add package elv/1.0.7/elv-1.0.7.tar.gz

---
+++ packages/elv/elv-1.0.7.tar.gz/elv-1.0.7/elv/__init__.py
@@ -0,0 +1,25 @@
+from .elv import (
+    Parse,
+    ParseSSB,
+    Transaction,
+    Transactions,
+    formats,
+    parse,
+    parse_stream,
+)
+
+__author__ = "Christian Stigen Larsen"
+__copyright__ = "Copyright (C) 2015 Christian Stigen Larsen"
+__email__ = "csl@csl.name"
+__license__ = "Affero GPL v3 or later"
+__version__ = "1.0.7"
+
+__all__ = [
+    "Parse",
+    "ParseSSB",
+    "Transaction",
+    "Transactions",
+    "formats",
+    "parse",
+    "parse_stream",
+]
