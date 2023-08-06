a9c77c44cbbd41b010f4ef98cda54343dc721602
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:40 +0000

    Add package combination_all/1.0.2/combination_all-1.0.2.zip

---
+++ packages/combination_all/combination_all-1.0.2.zip/combination_all-1.0.2/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+
+    name        = 'combination_all',
+    version     = '1.0.2',
+    py_modules  = ['combination_all'],
+    author      = 'tndls9304',
+    author_mail = 'tndls9304@yonsei.ac.kr',
+    url         = 'http://cadcam.yonsei.ac.kr',
+    description = 'If you have list that contains "n" data, you will get nC1 + nC2 + ... nCn for output',
+    )
