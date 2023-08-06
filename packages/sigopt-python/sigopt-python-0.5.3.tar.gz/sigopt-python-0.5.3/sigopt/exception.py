f47428ac88cfe08834d843f876cc9fe15aae6ef6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:44 +0000

    Add package sigopt-python/0.5.3/sigopt-python-0.5.3.tar.gz

---
+++ packages/sigopt-python/sigopt-python-0.5.3.tar.gz/sigopt-python-0.5.3/sigopt/exception.py
@@ -0,0 +1,19 @@
+import copy
+
+
+class SigOptException(Exception):
+  pass
+
+
+class ApiException(SigOptException):
+  def __init__(self, body, status_code):
+    self.message = body.get('message', None)
+    self._body = body
+    if self.message is not None:
+      super(ApiException, self).__init__(self.message)
+    else:
+      super(ApiException, self).__init__()
+    self.status_code = status_code
+
+  def to_json(self):
+    return copy.deepcopy(self._body)
---
+++ packages/sigopt-python/sigopt-python-0.5.3.tar.gz/sigopt-python-0.5.3/sigopt/resource.py
@@ -0,0 +1,44 @@
+from .endpoint import ApiEndpoint, BoundApiEndpoint
+
+class BoundApiResource(object):
+  def __init__(self, resource, id):
+    self._resource = resource
+    self._id = id
+
+  def fetch(self):
+    fetch_endpoint = self._resource._endpoints[None]
+    return BoundApiEndpoint(self, fetch_endpoint).__call__()
+
+  def __getattr__(self, attr):
+    endpoint = self._resource._endpoints.get(attr)
+    if endpoint:
+      return BoundApiEndpoint(self, endpoint)
+    else:
+      raise AttributeError(attr)
+
+
+class ApiResource(object):
+  def __init__(self, conn, name, response_cls, endpoints):
+    self._conn = conn
+    self._name = name
+    self._response_cls = response_cls
+    self._endpoints = dict((
+      (endpoint._name, endpoint)
+      for endpoint
+      in endpoints + [ApiEndpoint(None, self._response_cls, 'GET')]
+    ))
+
+  def __call__(self, id):
+    return BoundApiResource(self, id)
+
+  def create(self, **kwargs):
+    return self._response_cls(
+      self._conn._post(self._base_url('create'), kwargs),
+    )
+
+  def _base_url(self, suffix):
+    return '{api_url}/v0/{name}/{suffix}'.format(
+      api_url=self._conn.api_url,
+      name=self._name,
+      suffix=suffix,
+      )
