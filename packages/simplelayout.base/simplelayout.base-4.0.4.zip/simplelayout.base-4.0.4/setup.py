3e69022cca5e59cb5528ce3f437a33ade0b9874e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:54 +0000

    Add package simplelayout.base/4.0.4/simplelayout.base-4.0.4.zip

---
+++ packages/simplelayout.base/simplelayout.base-4.0.4.zip/simplelayout.base-4.0.4/setup.py
@@ -0,0 +1,59 @@
+from setuptools import setup, find_packages
+import os
+
+version = '4.0.4'
+
+tests_require = [
+    'ftw.builder',
+    'ftw.testbrowser',
+    'ftw.testing',
+    'plone.app.testing',
+    'plone.mocktestcase',
+    'simplelayout.types.common',
+    ]
+
+setup(name='simplelayout.base',
+      version=version,
+      description="SimpleLayout is an easy to use plone package for "
+      "creating content pages",
+      long_description=(open("README.rst").read() + "\n" +
+                        open(os.path.join("docs", "HISTORY.txt")).read()),
+
+      # Get more strings from
+      # http://www.python.org/pypi?%3Aaction=list_classifiers
+
+      classifiers=[
+        "Framework :: Plone",
+        'Framework :: Plone :: 4.1',
+        'Framework :: Plone :: 4.2',
+        'Framework :: Plone :: 4.3',
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+
+      keywords='',
+      author='4teamwork GmbH',
+      author_email='mailto:info@4teamwork.ch',
+      url='https://github.com/4teamwork/simplelayout.base',
+      license='GPL2',
+
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['simplelayout'],
+      include_package_data=True,
+      zip_safe=False,
+
+      install_requires=[
+        'setuptools',
+        'archetypes.schemaextender',
+        'simplelayout.ui.base>=3.0.1',
+        'simplelayout.ui.dragndrop'
+        ],
+      tests_require=tests_require,
+      extras_require=dict(tests=tests_require),
+
+      entry_points="""
+      # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
