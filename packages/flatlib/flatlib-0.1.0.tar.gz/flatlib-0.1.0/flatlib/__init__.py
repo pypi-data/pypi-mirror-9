04f2aeaabe559044599316320c0209acad1ce66f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:15 +0000

    Add package flatlib/0.1.0/flatlib-0.1.0.tar.gz

---
+++ packages/flatlib/flatlib-0.1.0.tar.gz/flatlib-0.1.0/flatlib/__init__.py
@@ -0,0 +1,12 @@
+"""
+    This file is part of flatlib - (C) FlatAngle
+    Author: João Ventura (flatangleweb@gmail.com)
+
+"""
+
+import os
+
+
+# Library and resource paths
+PATH_LIB = os.path.dirname(__file__) + os.sep
+PATH_RES = PATH_LIB + 'resources' + os.sep
