203de05dc2e9a569b04835cfda153b9ad91ddc0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:23 +0000

    Add package postcode.io/0.1.1/postcode.io-0.1.1.zip

---
+++ packages/postcode.io/postcode.io-0.1.1.zip/postcode.io-0.1.1/setup.py
@@ -0,0 +1,18 @@
+__author__ = 'gokhan'
+
+from setuptools import setup
+
+setup(
+    name='postcode.io',
+    packages=['lib'],
+    version='0.1.1',
+    author='Gokhan Karadas',
+    author_email='gokhan.karadas1992@gmail.com',
+    download_url="https://github.com/previousdeveloper/PythonPostcodesWrapper",
+    url='https://github.com/previousdeveloper/PythonPostcodesWrapper',
+    license='LICENSE',
+    description='Postcode & Geolocation API for UK for python',
+    install_requires=['requests'],
+    include_package_data=True
+)
+
