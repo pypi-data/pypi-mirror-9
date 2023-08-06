a93ee038e728ba85e12426d16bd30a549e0b40e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:45 +0000

    Add package hazelnut/0.1/hazelnut-0.1.tar.gz

---
+++ packages/hazelnut/hazelnut-0.1.tar.gz/hazelnut-0.1/hazelnut/core.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__title__ = 'hazelnut'
+__version__ = '0.1'
+__author__ = 'Martin Simon <me@martinsimon.me>'
+__repo__ = 'https://github.com/c0ding/hazelnut'
+__license__ = 'Apache v2.0 License'
+
+class MemInfo(object):
+
+	def __init__(self, path='/proc/meminfo'):
+		self.path = path
+
+	def __str__(self):
+		with open(self.get_path(), 'r') as f:
+			lines = [line.strip() for line in f]
+			return '\n'.join(lines)
+
+	def __repr__(self):
+		return self.__str__()
+
+	def get_path(self):
+		return self.path
+	
+	def dict(self):
+		d = {}
+		with open(self.get_path(), 'r') as f:
+			d = dict(x.strip().split(None, 1) for x in f)
+		return d
+
+	def search(self, input):
+		with open(self.get_path(), 'r') as f:
+			match = [s for s in f if input in s]
+			# will have to figure out a way to make this case insensitive.
+			# mem.search('Swap') and mem.search('swap') should both match.
+			return match
