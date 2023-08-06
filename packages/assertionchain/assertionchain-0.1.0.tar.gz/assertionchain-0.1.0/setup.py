995ef09ec7615dfc3f9c771db7f9de8ba60d9e2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:12 +0000

    Add package assertionchain/0.1.0/assertionchain-0.1.0.tar.gz

---
+++ packages/assertionchain/assertionchain-0.1.0.tar.gz/assertionchain-0.1.0/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='assertionchain',
+    description='Utility for chaining commands and incrementally checking the results',
+    version='0.1.0',
+    author='Justin Iso',
+    author_email='justin+assertionchain@justiniso.com',
+    url='https://github.com/justiniso/assertionchain',
+    download_url='https://github.com/justiniso/assertionchain/tarball/0.1.0',
+    install_requires=[],
+    tests_require=[],
+    test_suite='tests',
+    setup_requires=[]
+)
