6f4252ad32f52459e25886bdf039ad159ed4bf3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:37:07 +0000

    Add package monasca-ui/1.0.22/monasca-ui-1.0.22.tar.gz

---
+++ packages/monasca-ui/monasca-ui-1.0.22.tar.gz/monasca-ui-1.0.22/monitoring/dashboard.py
@@ -0,0 +1,32 @@
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
+
+# Copyright 2012 Nebula, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from django.utils.translation import ugettext_lazy as _
+from django.conf import settings  # noqa
+
+import horizon
+
+service_type = getattr(settings, 'MONITORING_SERVICE_TYPE', 'monitoring')
+
+class Monitoring(horizon.Dashboard):
+    name = _("Monitoring")
+    slug = "monitoring"
+    panels = ('overview', 'alarmdefs', 'alarms', 'notifications',)
+    default_panel = 'overview'
+    policy_rules = (("monitoring", "monitoring:monitoring"),)
+    permissions = (('openstack.services.' + service_type),)
+
+horizon.register(Monitoring)
