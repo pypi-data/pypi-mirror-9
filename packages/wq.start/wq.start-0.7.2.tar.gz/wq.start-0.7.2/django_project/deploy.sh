97927784154c49cdb5aa1f95c5645b57862b342e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:13 +0000

    Add package wq.start/0.7.2/wq.start-0.7.2.tar.gz

---
+++ packages/wq.start/wq.start-0.7.2.tar.gz/wq.start-0.7.2/django_project/deploy.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+# Exit on error
+set -e
+
+# Dump wq configuration object to file (requires Django 1.7)
+CONFIG=`db/manage.py dump_config`
+echo "define($CONFIG);" > app/js/data/config.js
+
+# Build javascript with wq.app
+cd app;
+wq build $1;
+
+# Force important files through any unwanted server caching
+cd ../;
+sed -i "s/{{project_name}}.js/{{project_name}}.js?v="$1"/" htdocs-build/{{project_name}}.appcache
+sed -i "s/{{project_name}}.css/{{project_name}}.css?v="$1"/" htdocs-build/{{project_name}}.appcache
+
+# Preserve Django's static files (e.g. admin)
+if [ -d htdocs/static ]; then
+    cp -a htdocs/static htdocs-build/static
+fi;
+
+# Replace existing htdocs with new version
+rm -rf htdocs;
+mv -i htdocs-build/ htdocs;
+
+# Restart Django
+touch db/{{project_name}}/wsgi.py
