9fabc5053d663aa207cd7f6a721386143759616a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:40 +0000

    Add package plone.app.multilingual/2.0.0/plone.app.multilingual-2.0.0.zip

---
+++ packages/plone.app.multilingual/plone.app.multilingual-2.0.0.zip/plone.app.multilingual-2.0.0/setup.py
@@ -0,0 +1,55 @@
+from setuptools import setup, find_packages
+import os
+
+version = '2.0.0'
+
+setup(
+    name='plone.app.multilingual',
+    version=version,
+    description="Multilingual Plone UI package, enables maintenance of "
+                "translations for both Dexterity types and Archetypes",
+    long_description="\n\n".join([
+        open("README.rst").read(),
+        open(os.path.join("docs", "CREDITS.txt")).read(),
+        open("CHANGES.rst").read(),
+    ]),
+    classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    url='https://github.com/plone/plone.app.multilingual',
+    license='GPL',
+    author='Ramon Navarro, Victor Fernandez de Alba, awello et al',
+    author_email='r.navarro@iskra.cat',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    namespace_packages=['plone', 'plone.app'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        'z3c.relationfield',
+        'plone.behavior',
+        'plone.dexterity',
+        'plone.app.z3cform',
+        'plone.app.registry',
+        'plone.formwidget.contenttree',
+        'Products.PloneLanguageTool',
+    ],
+    extras_require={
+        'archetypes': ['archetypes.multilingual'],
+        'test': [
+            'plone.app.testing[robot]>=4.2.2',
+            'plone.app.robotframework',
+            'plone.app.contenttypes',
+            'archetypes.multilingual',
+            'decorator',  # BBB
+        ],
+    },
+    entry_points="""
+    # -*- Entry points: -*-
+    [z3c.autoinclude.plugin]
+    target = plone
+    """,
+)
---
+++ packages/plone.app.multilingual/plone.app.multilingual-2.0.0.zip/plone.app.multilingual-2.0.0/src/plone/app/multilingual/tests/test_vocabularies.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+import unittest2 as unittest
+from plone.app.multilingual import api
+from plone.app.multilingual.browser.vocabularies import untranslated_languages
+from plone.app.multilingual.testing import PAM_FUNCTIONAL_TESTING
+from plone.dexterity.utils import createContentInContainer
+
+
+class TestVocabularies(unittest.TestCase):
+    layer = PAM_FUNCTIONAL_TESTING
+
+    def setUp(self):
+        self.portal = self.layer['portal']
+
+    def test_content_is_translated_into_all_languages(self):
+        a_ac = createContentInContainer(
+            self.portal['ca'], 'Document', title=u"Test document")
+
+        api.translate(a_ac, 'en')
+        api.translate(a_ac, 'es')
+
+        self.assertEqual(len(untranslated_languages(a_ac)), 0)
+
+    def test_content_is_not_translated_to_any_language(self):
+        a_ac = createContentInContainer(
+            self.portal, 'Document', title=u"Test document")
+
+        languages = untranslated_languages(a_ac).by_token.keys()
+
+        self.assertEqual(len(languages), 3)
+        self.assertIn('ca', languages)
+        self.assertIn('es', languages)
+        self.assertIn('en', languages)
