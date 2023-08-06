908da22c1b972e0b7baaa6599f16611a75194c9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:55 +0000

    Add package arc_library/1.0.32/arc_library-1.0.32.tar.gz

---
+++ packages/arc_library/arc_library-1.0.32.tar.gz/arc_library-1.0.32/arclib/django/__init__.py
@@ -0,0 +1,2 @@
+from object import ArcReturnJSONObject
+__all__ = ['functions', 'models', 'object', 'storage', 'tag']
---
+++ packages/arc_library/arc_library-1.0.32.tar.gz/arc_library-1.0.32/arclib/django/static.py
@@ -0,0 +1,29 @@
+#-*- coding: utf-8 -*-
+import os
+from django.http import HttpResponse
+from django.template import RequestContext, loader
+from django.core.urlresolvers import reverse
+from django.views.decorators.csrf import csrf_exempt
+from django.templatetags.static import static
+from datepop.settings import STATIC_DIR
+
+@csrf_exempt
+def render_static_file(request, static_file=None):
+    if static_file is None:
+        query_dict = request.POST
+        static_file = query_dict['static_file']
+    f = open(os.path.join(STATIC_DIR, static_file), 'r')
+    data = f.read()
+
+    t = loader.get_template_from_string(data)
+    c = RequestContext(request)
+    return HttpResponse(t.render(c))
+
+@csrf_exempt
+def url_reverse(request, view=None, args=None):
+    if view is None and args is None:
+        query_dict = request.POST
+        view = query_dict['view']
+        args = query_dict['args']
+    arg_list = args.split(',')
+    return HttpResponse(reverse(view, args=arg_list))
