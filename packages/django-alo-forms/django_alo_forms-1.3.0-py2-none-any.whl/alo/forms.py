36449bcca71e993057d0e467d45fc6e3c2995273
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:09 +0000

    Add package django-alo-forms/1.3.0/django_alo_forms-1.3.0-py2-none-any.whl

---
+++ packages/django-alo-forms/django_alo_forms-1.3.0-py2-none-any.whl/alo/forms.py
@@ -0,0 +1,33 @@
+try:
+    from django.contrib.gis.forms import *
+except ImportError:
+    import sys
+    sys.stdout.write("Warning: Could not find the GEOS library.\n")
+    from django.forms import *
+    
+from .mixins import QueryFormMixin
+from .operators import AND, OR
+
+
+class QueryModelForm(ModelForm, QueryFormMixin):
+    def __init__(self, *args, **kwargs):
+        super(QueryModelForm, self).__init__(*args, **kwargs)
+        self.set_meta()
+        choice_fields = (ModelChoiceField, ModelMultipleChoiceField)
+        for field in self.fields.values():
+            if not isinstance(field, choice_fields):
+                field.required = False
+                    
+    def full_clean(self):
+        super(QueryModelForm, self).full_clean()
+        self.clean_extralogic()
+
+
+class QueryForm(Form, QueryFormMixin):    
+    def __init__(self, *args, **kwargs):
+        super(QueryForm, self).__init__(*args, **kwargs)
+        self.set_meta()
+    
+    def full_clean(self):
+        super(QueryForm, self).full_clean()
+        self.clean_extralogic()
