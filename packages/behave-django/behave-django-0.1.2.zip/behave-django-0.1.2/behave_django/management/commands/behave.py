4b40a7365253a148ee67c870ff04f24a1d2b0d7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:19 +0000

    Add package behave-django/0.1.2/behave-django-0.1.2.zip

---
+++ packages/behave-django/behave-django-0.1.2.zip/behave-django-0.1.2/behave_django/management/commands/behave.py
@@ -0,0 +1,59 @@
+from __future__ import absolute_import
+from optparse import make_option
+import sys
+
+from behave.configuration import options
+from behave.__main__ import main as behave_main
+from django.core.management.base import BaseCommand
+from django.test.runner import DiscoverRunner
+
+
+def get_behave_options():
+    new_options = []
+
+    conflicts = [
+        '--no-color',
+        '--version'
+    ]
+
+    for fixed, keywords in options:
+        long_option = None
+        for option in fixed:
+            if option.startswith("--"):
+                long_option = option
+                break
+
+        # Do not add conflicting options
+        if long_option in conflicts:
+            continue
+
+        if long_option:
+            # type isn't a valid keyword for make_option
+            if hasattr(keywords.get('type'), '__call__'):
+                del keywords['type']
+            # config_help isn't a valid keyword for make_option
+            if 'config_help' in keywords:
+                del keywords['config_help']
+
+            new_options.append(
+                make_option(long_option, **keywords)
+            )
+    return tuple(new_options)
+
+
+class Command(BaseCommand):
+    help = 'Runs behave tests'
+    option_list = BaseCommand.option_list + get_behave_options()
+
+    def handle(self, *args, **options):
+        # Configure django environment
+        django_test_runner = DiscoverRunner()
+        django_test_runner.setup_test_environment()
+        old_config = django_test_runner.setup_databases()
+
+        # Run Behave tests
+        behave_main(args=sys.argv[2:])
+
+        # Teardown django environment
+        django_test_runner.teardown_databases(old_config)
+        django_test_runner.teardown_test_environment()
---
+++ packages/behave-django/behave-django-0.1.2.zip/behave-django-0.1.2/setup.py
@@ -0,0 +1,44 @@
+import os
+from setuptools import find_packages, setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='behave-django',
+    version='0.1.2',
+    packages=find_packages(exclude=['test*']),
+    include_package_data=True,
+    license='MIT License',
+    description='Behave BDD integration for Django',
+    long_description=README,
+    url='https://github.com/mixxorz/behave-django',
+    author='Mitchel Cabuloy',
+    author_email='mixxorz@gmail.com',
+    install_requires=[
+        'behave',
+        'Django>=1.7'
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Environment :: Plugins',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Testing',
+    ],
+)
