268f0ce200853dd527edebf079df2ca5b602658b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:29:40 +0000

    Add package python-translate/1.0.3/python-translate-1.0.3.tar.gz

---
+++ packages/python-translate/python-translate-1.0.3.tar.gz/python-translate-1.0.3/setup.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+setup(
+    name="python-translate",
+    version="1.0.3",
+    author="Adam Zieliński",
+    author_email="adam@sf2.guru",
+    packages=find_packages(),
+    include_package_data=True,
+    url="https://github.com/adamziel/python_translate",
+
+    license="MIT",
+    description="Non-gettext translations for python.",
+
+    # Dependent packages (distributions)
+    install_requires=[
+        "PyYAML==3.11",
+        "codegen==1.0",
+        "polib==1.0.6"
+    ],
+)
