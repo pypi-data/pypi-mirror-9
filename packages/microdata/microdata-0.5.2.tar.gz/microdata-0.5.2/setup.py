cf2aeaf1e6014209997b1188edecf42949959bb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:22 +0000

    Add package microdata/0.5.2/microdata-0.5.2.tar.gz

---
+++ packages/microdata/microdata-0.5.2.tar.gz/microdata-0.5.2/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+import sys
+
+extra = {}
+if sys.version_info >= (3,):
+    extra['use_2to3'] = True
+
+setup(
+    name = 'microdata',
+    version = '0.5.2',
+    description = "html5lib extension for parsing microdata",
+    author = "Ed Summers",
+    author_email = "ehs@pobox.com",
+    url = "http://github.com/edsu/microdata",
+    py_modules = ['microdata'],
+    scripts = ['microdata.py'],
+    test_suite = 'test',
+    install_requires = ['html5lib'],
+    **extra
+)
