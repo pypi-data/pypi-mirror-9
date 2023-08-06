bc8d765ffeb756216788c66afebfcab858e90b78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:04 +0000

    Add package podhub.follower/0.0.211/podhub.follower-0.0.211.tar.gz

---
+++ packages/podhub.follower/podhub.follower-0.0.211.tar.gz/podhub.follower-0.0.211/podhub/follower/views.py
@@ -0,0 +1,30 @@
+from . import app
+from feed import Feed
+from flask import jsonify, request
+
+
+@app.route('/')
+def index():
+    return jsonify()
+
+
+@app.route('/audio')
+def feed():
+    url = request.args.get('feed_url')
+    if not url:
+        return jsonify(error_message='feed_url required.')
+
+    index = request.args.get('index')
+    if not index:
+        index = -1
+
+    feed = Feed(url=url)
+
+    try:
+        entry = feed.entries[index]
+    except TypeError:
+        return jsonify(error_message='index must be an integer.'), 400
+    except IndexError:
+        return jsonify(error_message='episode {} not found'.format(index)), 400
+
+    return jsonify(feed_url=entry.audio)
