0c6cc69e0761f4116a8ce454b5bb7cd259762225
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:57 +0000

    Add package anymarkup/0.3.1/anymarkup-0.3.1.tar.gz

---
+++ packages/anymarkup/anymarkup-0.3.1.tar.gz/anymarkup-0.3.1/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    name='anymarkup',
+    version='0.3.1',
+    description='Parse/serialize any markup format',
+    long_description=''.join(open('README.rst').readlines()),
+    keywords='xml, yaml, json, ini',
+    author='Slavek Kabrda',
+    author_email='slavek.kabrda@gmail.com',
+    url='https://github.com/bkabrda/anymarkup',
+    license='BSD',
+    packages=['anymarkup'],
+    install_requires=open('requirements.txt').read().splitlines(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        ]
+)
