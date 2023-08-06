8a0b89438d5ec8d6dfef5878a08bb25951f1ae21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:13 +0000

    Add package script-manager/0.0.9/script-manager-0.0.9.tar.gz

---
+++ packages/script-manager/script-manager-0.0.9.tar.gz/script-manager-0.0.9/examples/scripts.py
@@ -0,0 +1,27 @@
+__author__ = 'yetone'
+
+from script_manager import Manager
+
+test_manager = Manager()
+
+test0_manager = Manager()
+
+
+@test_manager.command
+def hello(a, b, c=False, name='yetone'):
+    print('test.hello: <a: {}, b: {}, c: {}> by {}'.format(a, b, c, name))
+
+
+@test_manager.command
+def hi(a, b, c=False, name='yetone'):
+    print('test.hi: <a: {}, b: {}, c: {}> by {}'.format(a, b, c, name))
+
+
+@test0_manager.command
+def say(a, b, c=False, name='yetone'):
+    print('test0.say: <a: {}, b: {}, c: {}> by {}'.format(a, b, c, name))
+
+
+@test0_manager.command
+def sing(a, b, c=False, name='yetone'):
+    print('test0.sing: <a: {}, b: {}, c: {}> by {}'.format(a, b, c, name))
---
+++ packages/script-manager/script-manager-0.0.9.tar.gz/script-manager-0.0.9/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+version = '0.0.9'
+
+setup(
+    name='script-manager',
+    version=version,
+    keywords=('command-line interface', 'flask_script', 'argparse', 'script'),
+    description='A command-line interface. Just a simple and crude implementation of Flask-Script.',
+    url='http://github.com/yetone/script-manager',
+    license='MIT License',
+    author='yetone',
+    author_email='i@yetone.net',
+    packages=find_packages(),
+    platforms='any',
+    tests_require=(
+        'pytest',
+    )
+)
