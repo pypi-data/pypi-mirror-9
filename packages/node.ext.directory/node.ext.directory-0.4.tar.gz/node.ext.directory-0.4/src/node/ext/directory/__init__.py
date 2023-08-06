33c6eac68af243ac8ef7399924c807c27ae7a1df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:15 +0000

    Add package node.ext.directory/0.4/node.ext.directory-0.4.tar.gz

---
+++ packages/node.ext.directory/node.ext.directory-0.4.tar.gz/node.ext.directory-0.4/src/node/ext/directory/__init__.py
@@ -0,0 +1,9 @@
+from directory import (
+    FileStorage,
+    File,
+    DirectoryStorage,
+    Directory,
+    file_factories,
+    MODE_TEXT,
+    MODE_BINARY,
+)
---
+++ packages/node.ext.directory/node.ext.directory-0.4.tar.gz/node.ext.directory-0.4/src/node/ext/directory/interfaces.py
@@ -0,0 +1,37 @@
+from zope.interface import Attribute
+from zope.lifecycleevent import IObjectAddedEvent
+from node.interfaces import (
+    INode,
+    ILeaf,
+    ICallable,
+)
+
+
+class IFileAddedEvent(IObjectAddedEvent):
+    """An File has been added to directory.
+    """
+
+
+class IFile(INode, ILeaf, ICallable):
+    """Marker interface for a file.
+    """
+
+
+class IDirectory(INode, ICallable):
+    """Directory target interface.
+    """
+
+    backup = Attribute(u"Create backup files of handled files. Defaults "
+                        "to False")
+
+    child_directory_factory = Attribute(u"Factory creating concrete node "
+                                        u"instances for directory children")
+
+    default_file_factory = Attribute(u"Default factory creating concrete node "
+                                     u"instances for file children")
+
+    file_factories = Attribute(u"Dict containing file names or endings as "
+                               u"keys with the corresponding file node "
+                               u"creating factory.")
+
+    ignores = Attribute(u"child keys to ignore")
