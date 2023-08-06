88e027946ba0715d66a699f5469bcec4f853fe4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:43 +0000

    Add package metameta/0.0.0.3/metameta-0.0.0.3.tar.gz

---
+++ packages/metameta/metameta-0.0.0.3.tar.gz/metameta-0.0.0.3/setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name = 'metameta',
+      version = '0.0.0.3',
+      description = 'Toolset for analyzing'\
+          + 'metatranscriptome data mapped onto metagenomic data',
+      classifiers = [
+            'Development Status :: 1 - Planning',
+            'Intended Audience :: Science/Research',
+            'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+            'Natural Language :: English',
+            'Operating System :: OS Independent',
+            'Programming Language :: Python :: 3.4',
+            'Topic :: Scientific/Engineering :: Bio-Informatics'
+          ],
+      keywords = 'bioinformatics metadata metagenome metatranscriptome'\
+          + 'short reads mapping alignment',
+      url = 'https://github.com/Brazelton-Lab/metameta',
+      download_url = 'https://github.com/Brazelton-Lab/metameta/tarball/'\
+          + '0.0.0.2',
+      author = 'Alex Hyer',
+      author_email = 'theonehyer@gmail.com',
+      license = 'GPL',
+      packages = ['metameta'],
+      include_package_data = True,
+      zip_safe = False,
+      test_suite = 'nose.collector',
+      tests_require = ['nose'],
+      entry_points = {
+          'console_scripts':['metameta=metameta:main']
+          }
+      )
