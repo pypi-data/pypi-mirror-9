0548472ed2688b32ea9666df4a4672fa3c74fcd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:24 +0000

    Add package beecloud/1.2/beecloud-1.2.tar.gz

---
+++ packages/beecloud/beecloud-1.2.tar.gz/beecloud-1.2/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'beecloud',
+    version = '1.2',
+      
+    keywords = ('beecloud', 'pay'),
+    description = 'beecloud, make pay more simple',
+    license = 'Copyright 2014-2015 beecloud',
+    install_requires = [],
+
+    author = 'Joseph Gao',
+    author_email = 'g@beecloud.cn',
+    
+    packages = find_packages(),
+    platforms = 'any',
+)
