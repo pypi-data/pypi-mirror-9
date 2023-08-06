6c860068fdb2773974f74c6a94531788776f8746
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:06 +0000

    Add package haralyzer/1.0.3/haralyzer-1.0.3.tar.gz

---
+++ packages/haralyzer/haralyzer-1.0.3.tar.gz/haralyzer-1.0.3/setup.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+install_reqs = ['python-dateutil']
+
+readme = open('README.rst').read()
+doclink = """
+Documentation
+-------------
+
+The full documentation is in the repository, run 'make doc' to get it."""
+history = open('HISTORY.rst').read().replace('.. :changelog:', '')
+
+setup(
+    name='haralyzer',
+    version='1.0.3',
+    description='A python framework for getting useful stuff out of HAR files',
+    long_description=readme + '\n\n' + doclink + '\n\n' + history,
+    author='Justin Crown',
+    author_email='justincrown1@gmail.com',
+    url='https://github.com/mrname/haralyzer',
+    download_url='https://github.com/mrname/haralyzer/tarball/1.0',
+    packages=[
+        'haralyzer'
+    ],
+    package_dir={'haralyzer': 'haralyzer'},
+    license='Commercial',
+    zip_safe=False,
+    keywords='har',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+    ],
+)
