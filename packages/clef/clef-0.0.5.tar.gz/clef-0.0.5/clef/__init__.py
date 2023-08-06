0af7ec1931d22ca68731be804507cbb0ae74058a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:49 +0000

    Add package clef/0.0.5/clef-0.0.5.tar.gz

---
+++ packages/clef/clef-0.0.5.tar.gz/clef-0.0.5/clef/__init__.py
@@ -0,0 +1,16 @@
+from .clef import (
+        APIError,
+        InvalidAppIDError,
+        InvalidAppSecretError,
+        InvalidOAuthCodeError,
+        InvalidOAuthTokenError,
+        InvalidLogoutTokenError,
+        ServerError,
+        ConnectionError,
+        NotFoundError,
+        ClefAPI,
+        initialize,
+        get_login_information,
+        get_logout_information,
+        clef_api
+)
