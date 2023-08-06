f6054974df517796468b80b0839c552d18134dd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:29 +0000

    Add package icalendar/3.9.0/icalendar-3.9.0.tar.gz

---
+++ packages/icalendar/icalendar-3.9.0.tar.gz/icalendar-3.9.0/setup.py
@@ -0,0 +1,58 @@
+import codecs
+import setuptools
+import sys
+
+
+version = '3.9.0'
+shortdesc = 'iCalendar parser/generator'
+longdesc = codecs.open('README.rst', encoding='utf-8').read()
+longdesc += codecs.open('CHANGES.rst', encoding='utf-8').read()
+longdesc += codecs.open('LICENSE.rst', encoding='utf-8').read()
+
+
+tests_require = []
+install_requires = [
+    'python-dateutil',
+    'pytz',
+    'setuptools',
+]
+
+if sys.version_info[:2] == (2, 6):
+    # Python unittest2 only needed for Python 2.6
+    tests_require.append('unittest2')
+    # OrderedDict was added in 2.7
+    install_requires.append('ordereddict')
+
+
+setuptools.setup(
+    name='icalendar',
+    version=version,
+    description=shortdesc,
+    long_description=longdesc,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+    ],
+    keywords='calendar calendaring ical icalendar event todo journal '
+             'recurring',
+    author='Plone Foundation',
+    author_email='plone-developers@lists.sourceforge.net',
+    url='https://github.com/collective/icalendar',
+    license='BSD',
+    packages=setuptools.find_packages('src'),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=install_requires,
+    extras_require={
+        'test': tests_require
+    }
+)
