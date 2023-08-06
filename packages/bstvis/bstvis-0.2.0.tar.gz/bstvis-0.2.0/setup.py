1c2a5b50c4c96a8d186f0675eccddea2ebb75ab2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:44 +0000

    Add package bstvis/0.2.0/bstvis-0.2.0.tar.gz

---
+++ packages/bstvis/bstvis-0.2.0.tar.gz/bstvis-0.2.0/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='bstvis',
+    version='0.2.0',
+
+    description='A visualisation framework for binary search trees and algorithms.',
+
+    # url='https://github.com/pypa/sampleproject',
+
+    author='Hannes Frank',
+    author_email='frankhannes@googlemail.com',
+
+    url='https://github.com/hannesfrank/bstvis',
+
+    license='MIT',
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+
+        'License :: OSI Approved :: MIT License',
+
+        'Programming Language :: Python :: 3',
+
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX',
+        'Operating System :: Unix',
+        'Operating System :: MacOS'
+    ],
+
+    keywords='bst visualisation'
+)
