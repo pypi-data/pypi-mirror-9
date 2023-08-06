6cb6900b7f210e125f1b4ddb4e6b817adbdfe4ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:17 +0000

    Add package sphinx-gallery/0.0.7/sphinx-gallery-0.0.7.tar.gz

---
+++ packages/sphinx-gallery/sphinx-gallery-0.0.7.tar.gz/sphinx-gallery-0.0.7/setup.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+# Author: Óscar Nájera
+# License: 3-clause BSD
+"""
+Installer Sphinx extension for gallery generator
+"""
+
+from setuptools import setup, find_packages
+import sphinxgallery
+
+with open('README.rst') as f:
+    long_description = f.read()
+
+
+setup(
+    name="sphinx-gallery",
+    description="Sphinx extension to automatically generate an examples gallery",
+    long_description=long_description,
+    version=sphinxgallery.__version__,
+    packages=find_packages(),
+    package_data={'sphinxgallery': ['_static/gallery.css', '_static/no_image.png']},
+    url="https://github.com/sphinx-gallery/sphinx-gallery",
+    author="Óscar Nájera",
+    author_email='najera.oscar@gmail.com',
+    # XXX : don't force requirements in setup.py as it tends to break people
+    # install_requires= ['Sphinx', 'matplotlib', 'pillow'],
+    install_requires=[],
+    setup_requires=['nose>=1.0'],
+    license='3-clause BSD',
+    classifiers=['Intended Audience :: Developers',
+                 'Development Status :: 3 - Alpha',
+                 'Framework :: Sphinx :: Extension',
+                 'Programming Language :: Python',
+                 ],
+)
