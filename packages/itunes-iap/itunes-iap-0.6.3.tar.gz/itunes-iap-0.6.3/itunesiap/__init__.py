a13fc46b2238957318e2d5377e0e618d606ed520
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:33 +0000

    Add package itunes-iap/0.6.3/itunes-iap-0.6.3.tar.gz

---
+++ packages/itunes-iap/itunes-iap-0.6.3.tar.gz/itunes-iap-0.6.3/itunesiap/__init__.py
@@ -0,0 +1,22 @@
+"""
+    itunes-iap
+    ~~~~~~~~~~
+
+    Itunes In-app Purchase verification api.
+
+    :copyright: (c) 2013 Jeong YunWon - 2014 Andy Briggs
+    :license: 2-clause BSD.
+"""
+
+from six import PY3
+import pkg_resources
+
+__version__ = pkg_resources.resource_string('itunesiap', 'version.txt').strip()
+if PY3:
+    __version__ = __version__.decode('ascii')
+VERSION = tuple(int(v) for v in __version__.split('.'))
+
+from .core import Request, Receipt, set_verification_mode
+from .exceptions import InvalidReceipt, ModeNotAvailable, RequestError
+from .shortcut import verify
+
