9e9b8c47768e17ac61be8229444d94199e3da9db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:23:01 +0000

    Add package django-good-choices/1.4/django-good-choices-1.4.tar.gz

---
+++ packages/django-good-choices/django-good-choices-1.4.tar.gz/django-good-choices-1.4/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+
+setup(
+    name = 'django-good-choices',
+    version = '1.4',
+    description = 'Simple and convenient approach to "choices" in Django',
+    author = 'Piotr Wasilewski',
+    author_email = 'piotrek@piotrek.io',
+    url = 'https://github.com/piotrekio/django-good-choices',
+    py_modules = ['good_choices']
+)
