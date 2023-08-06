1139d7fbb9aee643a439c1dfb6b1cc60770454f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:58 +0000

    Add package i2cdev/1.2.4/i2cdev-1.2.4.tar.gz

---
+++ packages/i2cdev/i2cdev-1.2.4.tar.gz/i2cdev-1.2.4/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+from i2cdev import __version__
+
+setup(
+    name='i2cdev',
+    version=__version__,
+    description='Simple I2C Library for linux',
+    author='Garrett Berg',
+    author_email='garrett@cloudformdesign.com',
+    url='https://github.com/cloudformdesign/i2cdev',
+    py_modules=['i2cdev'],
+    license='MIT',
+    platforms=['Linux'],
+    classifiers=[
+        # 'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: System :: Operating System Kernels',
+        'Topic :: Software Development :: Embedded Systems',
+        'Topic :: System :: Hardware :: Hardware Drivers',
+    ]
+)
