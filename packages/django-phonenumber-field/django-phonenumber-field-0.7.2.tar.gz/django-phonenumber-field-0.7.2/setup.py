516ef45aca24b05c53d39d23b5cf2bfd645d5f27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:36:29 +0000

    Add package django-phonenumber-field/0.7.2/django-phonenumber-field-0.7.2.tar.gz

---
+++ packages/django-phonenumber-field/django-phonenumber-field-0.7.2.tar.gz/django-phonenumber-field-0.7.2/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+from phonenumber_field import __version__
+
+
+setup(
+    name="django-phonenumber-field",
+    version=__version__,
+    url='http://github.com/stefanfoulis/django-phonenumber-field',
+    license='BSD',
+    platforms=['OS Independent'],
+    description="An international phone number field for django models.",
+    install_requires=[
+        'phonenumbers>=7.0.2',
+    ],
+    long_description=open('README.rst').read(),
+    author='Stefan Foulis',
+    author_email='stefan.foulis@gmail.com',
+    maintainer='Stefan Foulis',
+    maintainer_email='stefan.foulis@gmail.com',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP',
+    ]
+)
