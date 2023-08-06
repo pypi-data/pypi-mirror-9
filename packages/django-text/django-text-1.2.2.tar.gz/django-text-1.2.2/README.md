c4ceb62252201d18abe830b2c645468ce1a9b3bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:57 +0000

    Add package django-text/1.2.2/django-text-1.2.2.tar.gz

---
+++ packages/django-text/django-text-1.2.2.tar.gz/django-text-1.2.2/README.md
@@ -0,0 +1,93 @@
+:warning:  This project is in early development and has bugs, contribution is welcome.
+
+# django-text
+
+Intuitive text editing for the Django Admin.
+
+## Installation
+
+Install the package with pip.
+
+```shell
+$ pip install django-text
+```
+
+Add `'text'` to your installed packages.
+
+```python
+# settings.py
+
+INSTALLED_APPS = (
+    # ...
+    'text',
+)
+```
+
+Add `text.middleware.AutoPopulateMiddleware` to your middleware.
+
+```python
+# settings.py
+
+MIDDLEWARE = (
+    # ...
+    'text.middleware.AutoPopulateMiddleware',
+)
+```
+
+Run `migrate`.
+
+```shell
+$ python manage.py migrate
+```
+
+## Usage
+
+### Template tags
+
+Add `editable` tags to your templates. Beware this tag will show the name
+of the text node if there is no corresponding text nodes in the database.
+
+```html
+<h1>{% editable header %}</h1>
+
+<div class="content">
+    {% editable text_body %}
+</div>
+```
+
+You can also use the `blockeditable` tag that let's you specify a default text
+that will show up if there is no database entry.
+
+```html
+<div class="content">
+    <h1>
+        {% blockeditable header %}
+            Read My Awesome Text
+        {% endblockeditable %}
+    </h1>
+    
+    {% blockeditable content %}
+        Put your default text here!
+    {% endblockeditable %}
+</div>
+```
+
+The `blockeditable` tags works with translation tags inside of it. So if you already
+have a translated site, you can wrap your content with this tag and only
+add text nodes for some of the languages that you support.
+
+### Content editing
+
+![django-text in action](/docs/printscreen.png)
+
+Now add text nodes with the corresponding names in the Django Admin. Currently raw text and [markdown](http://daringfireball.net/projects/markdown/) is supported.
+
+Missing text nodes will be added to the database automatically when their
+template tags are rendered. Blocktags will be added with their default
+text and inline tags will be added with the name of the node as the text content.
+
+To disable automatic updating of missing text nodes add the following to your settings.
+
+```python
+AUTOPOPULATE_TEXT = False
+```
---
+++ packages/django-text/django-text-1.2.2.tar.gz/django-text-1.2.2/text/models.py
@@ -0,0 +1,100 @@
+from django.db import models
+from django.conf import settings
+from django.utils.translation import get_language
+from django.utils.safestring import mark_safe
+
+import markdown
+
+
+class Text(models.Model):
+    TYPE_TEXT = 0
+    TYPE_MARKDOWN = 1
+    TYPES = (
+        (TYPE_TEXT, 'Text'),
+        (TYPE_MARKDOWN, 'Markdown'),
+    )
+
+    name = models.CharField(max_length=50, db_index=True)
+    body = models.TextField()
+    type = models.IntegerField(
+        choices=TYPES, blank=False, default=TYPE_MARKDOWN)
+    language = models.CharField(
+        choices=settings.LANGUAGES,
+        max_length=5,
+        default=settings.LANGUAGE_CODE)
+
+    class Meta:
+        unique_together = ('name', 'language', )
+        index_together = ['name', 'language', ]
+        ordering = ('name', 'language', )
+
+    def __unicode__(self):
+        return self.text_id
+
+    def render(self):
+        text = self.body
+        if self.type is self.TYPE_MARKDOWN:
+            text = markdown.markdown(text, output_format='html5')
+        return mark_safe(text)
+
+    def save(self, *args, **kwargs):
+        ret_val = super(Text, self).save(*args, **kwargs)
+        return ret_val
+
+    @property
+    def text_id(self):
+        return "%s_%s" % (self.name, self.language)
+
+
+def subdict_add(obj, sub, key, value):
+    if sub not in obj:
+        obj[sub] = {}
+    obj[sub][key] = value
+
+
+def subdict_get(obj, sub, key):
+    return obj[sub][key]
+
+
+def subdict_del(obj, sub, key):
+    del obj[sub][key]
+
+
+def in_sub(obj, sub, key):
+    return sub in obj and key in obj[sub]
+
+
+def subset_add(obj, sub, value):
+    if sub not in obj:
+        obj[sub] = set()
+    obj[sub].add(value)
+
+
+def subset_remove(obj, sub, value):
+    obj[sub].remove(value)
+
+
+class TextSetter(object):
+    def __init__(self):
+        self.texts = {}
+
+    def set(self, name, text):
+        language = get_language()
+        subdict_add(self.texts, language, name, text)
+
+    def save(self):
+        for language, texts in self.texts.iteritems():
+            for name, value in texts.iteritems():
+                text = Text(
+                    name=name,
+                    body=value,
+                    language=language,
+                    type=Text.TYPE_TEXT)
+                text.save()
+        self.clear()
+
+    def clear(self):
+        self.texts = {}
+
+
+text_setter = TextSetter()
