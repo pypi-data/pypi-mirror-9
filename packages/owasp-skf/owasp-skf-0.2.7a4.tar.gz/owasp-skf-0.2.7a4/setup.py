82b68eaf2be980fa5138fd98d2ee055c7187ece8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:35 +0000

    Add package owasp-skf/0.2.7a4/owasp-skf-0.2.7a4.tar.gz

---
+++ packages/owasp-skf/owasp-skf-0.2.7a4.tar.gz/owasp-skf-0.2.7a4/setup.py
@@ -0,0 +1,27 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(name='owasp-skf',
+    version='0.2.7a4',
+    description='The OWASP Security Knowledge Framework',
+    url='https://github.com/blabla1337/skf-flask',
+    author='Glenn ten Cate, Riccardo ten Cate',
+    author_email='gtencate@schubergphilis.com, r.tencate77@gmail.com',
+    license='AGPLV3',
+    packages=['skf'],
+    # trying to add files...
+    include_package_data = True,
+    long_description="""\
+    The Security Knowledge Framework is an fully open-source Python-Flask web-application.
+    It is an expert system application that uses OWASP Application Security Verification Standard 
+    """,
+    install_requires=open('requirements.txt').read().splitlines(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Framework :: Flask",
+        "License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)",
+    ])
+
+
