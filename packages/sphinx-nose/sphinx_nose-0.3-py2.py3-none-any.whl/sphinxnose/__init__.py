f5a403717be293218792fbafecd6b428c92e9684
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:03 +0000

    Add package sphinx-nose/0.3/sphinx_nose-0.3-py2.py3-none-any.whl

---
+++ packages/sphinx-nose/sphinx_nose-0.3-py2.py3-none-any.whl/sphinxnose/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+VERSION = (0, 3)
+
+__author__ = 'Tzu-ping Chung'
+__email__ = 'uranusjr@gmail.com'
+__version__ = '.'.join(str(v) for v in VERSION)
+
+from .plugin import SphinxDoctest   # noqa
