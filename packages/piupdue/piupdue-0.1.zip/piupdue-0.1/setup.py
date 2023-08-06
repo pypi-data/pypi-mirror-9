a2b39c3bcf6e7d5437ba13d3b2a888783b9996dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:52 +0000

    Add package piupdue/0.1/piupdue-0.1.zip

---
+++ packages/piupdue/piupdue-0.1.zip/piupdue-0.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+import os
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+setup(
+    name='piupdue',
+    version='0.1',
+	description='Upload code to Arduino Due from Python.',
+    url='https://github.com/johngrantuk/piupdue',
+	author='John Grant',
+	author_email='johngrantuk@googlemail.com',
+    license='MIT',
+	packages=find_packages(),
+    
+    long_description=readme(),
+    
+)
