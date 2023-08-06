b393e5610d2e3ab1e9da907aea6b4ae673d9f898
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:54 +0000

    Add package django-cratis-base/0.1.5/django-cratis-base-0.1.5.tar.gz

---
+++ packages/django-cratis-base/django-cratis-base-0.1.5.tar.gz/django-cratis-base-0.1.5/setup.py
@@ -0,0 +1,22 @@
+import os
+from setuptools import setup, find_packages
+# from tests import PyTest
+
+setup(
+    name='django-cratis-base',
+    version='0.1.5',
+    packages=find_packages(),
+
+    url='',
+    license='MIT License',
+    author='Alex Rudakov',
+    author_email='ribozz@gmail.com',
+    description='Django-cratis is a way to group together django applications, so they form reusable features.',
+    long_description='',
+    install_requires=[
+        'django-cratis>=0.6.0',
+        'dj-database-url',
+        'south',
+    ]
+)
+
