6892745d75c42e5fbbdd43ded18804bc341a50af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:48 +0000

    Add package pycrunchbase/0.2.4/pycrunchbase-0.2.4.tar.gz

---
+++ packages/pycrunchbase/pycrunchbase-0.2.4.tar.gz/pycrunchbase-0.2.4/src/pycrunchbase/__init__.py
@@ -0,0 +1,30 @@
+from .pycrunchbase import (
+    CrunchBase,
+)
+from .resource import (
+    Acquisition,
+    FundingRound,
+    IPO,
+    Organization,
+    Page,
+    PageItem,
+    Person,
+    Product,
+    Relationship,
+)
+
+
+__version__ = "0.2.4"
+
+__all__ = [
+    'Acquisition',
+    'FundingRound',
+    'IPO',
+    'Organization',
+    'Page',
+    'PageItem',
+    'Person',
+    'Product',
+    'Relationship',
+    'CrunchBase'
+]
