e428eeff4785c1a393a753381b8be51da5aa5487
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:28 +0000

    Add package docker-compose/1.2.0rc2/docker-compose-1.2.0rc2.tar.gz

---
+++ packages/docker-compose/docker-compose-1.2.0rc2.tar.gz/docker-compose-1.2.0rc2/tests/unit/cli_test.py
@@ -0,0 +1,154 @@
+from __future__ import unicode_literals
+from __future__ import absolute_import
+import logging
+import os
+import tempfile
+import shutil
+from .. import unittest
+
+import docker
+import mock
+from six import StringIO
+
+from compose.cli import main
+from compose.cli.main import TopLevelCommand
+from compose.cli.errors import ComposeFileNotFound
+from compose.service import Service
+
+
+class CLITestCase(unittest.TestCase):
+    def test_default_project_name(self):
+        cwd = os.getcwd()
+
+        try:
+            os.chdir('tests/fixtures/simple-composefile')
+            command = TopLevelCommand()
+            project_name = command.get_project_name(command.get_config_path())
+            self.assertEquals('simplecomposefile', project_name)
+        finally:
+            os.chdir(cwd)
+
+    def test_project_name_with_explicit_base_dir(self):
+        command = TopLevelCommand()
+        command.base_dir = 'tests/fixtures/simple-composefile'
+        project_name = command.get_project_name(command.get_config_path())
+        self.assertEquals('simplecomposefile', project_name)
+
+    def test_project_name_with_explicit_uppercase_base_dir(self):
+        command = TopLevelCommand()
+        command.base_dir = 'tests/fixtures/UpperCaseDir'
+        project_name = command.get_project_name(command.get_config_path())
+        self.assertEquals('uppercasedir', project_name)
+
+    def test_project_name_with_explicit_project_name(self):
+        command = TopLevelCommand()
+        name = 'explicit-project-name'
+        project_name = command.get_project_name(None, project_name=name)
+        self.assertEquals('explicitprojectname', project_name)
+
+    def test_project_name_from_environment_old_var(self):
+        command = TopLevelCommand()
+        name = 'namefromenv'
+        with mock.patch.dict(os.environ):
+            os.environ['FIG_PROJECT_NAME'] = name
+            project_name = command.get_project_name(None)
+        self.assertEquals(project_name, name)
+
+    def test_project_name_from_environment_new_var(self):
+        command = TopLevelCommand()
+        name = 'namefromenv'
+        with mock.patch.dict(os.environ):
+            os.environ['COMPOSE_PROJECT_NAME'] = name
+            project_name = command.get_project_name(None)
+        self.assertEquals(project_name, name)
+
+    def test_filename_check(self):
+        self.assertEqual('docker-compose.yml', get_config_filename_for_files([
+            'docker-compose.yml',
+            'docker-compose.yaml',
+            'fig.yml',
+            'fig.yaml',
+        ]))
+
+        self.assertEqual('docker-compose.yaml', get_config_filename_for_files([
+            'docker-compose.yaml',
+            'fig.yml',
+            'fig.yaml',
+        ]))
+
+        self.assertEqual('fig.yml', get_config_filename_for_files([
+            'fig.yml',
+            'fig.yaml',
+        ]))
+
+        self.assertEqual('fig.yaml', get_config_filename_for_files([
+            'fig.yaml',
+        ]))
+
+        self.assertRaises(ComposeFileNotFound, lambda: get_config_filename_for_files([]))
+
+    def test_get_project(self):
+        command = TopLevelCommand()
+        command.base_dir = 'tests/fixtures/longer-filename-composefile'
+        project = command.get_project(command.get_config_path())
+        self.assertEqual(project.name, 'longerfilenamecomposefile')
+        self.assertTrue(project.client)
+        self.assertTrue(project.services)
+
+    def test_help(self):
+        command = TopLevelCommand()
+        with self.assertRaises(SystemExit):
+            command.dispatch(['-h'], None)
+
+    def test_setup_logging(self):
+        main.setup_logging()
+        self.assertEqual(logging.getLogger().level, logging.DEBUG)
+        self.assertEqual(logging.getLogger('requests').propagate, False)
+
+    @mock.patch('compose.cli.main.dockerpty', autospec=True)
+    def test_run_with_environment_merged_with_options_list(self, mock_dockerpty):
+        command = TopLevelCommand()
+        mock_client = mock.create_autospec(docker.Client)
+        mock_project = mock.Mock()
+        mock_project.get_service.return_value = Service(
+            'service',
+            client=mock_client,
+            environment=['FOO=ONE', 'BAR=TWO'],
+            image='someimage')
+
+        command.run(mock_project, {
+            'SERVICE': 'service',
+            'COMMAND': None,
+            '-e': ['BAR=NEW', 'OTHER=THREE'],
+            '--user': None,
+            '--no-deps': None,
+            '--allow-insecure-ssl': None,
+            '-d': True,
+            '-T': None,
+            '--entrypoint': None,
+            '--service-ports': None,
+            '--rm': None,
+        })
+
+        _, _, call_kwargs = mock_client.create_container.mock_calls[0]
+        self.assertEqual(
+            call_kwargs['environment'],
+            {'FOO': 'ONE', 'BAR': 'NEW', 'OTHER': 'THREE'})
+
+
+def get_config_filename_for_files(filenames):
+    project_dir = tempfile.mkdtemp()
+    try:
+        make_files(project_dir, filenames)
+        command = TopLevelCommand()
+        command.base_dir = project_dir
+        return os.path.basename(command.get_config_path())
+    finally:
+        shutil.rmtree(project_dir)
+
+
+def make_files(dirname, filenames):
+    for fname in filenames:
+        with open(os.path.join(dirname, fname), 'w') as f:
+            f.write('')
+
---
+++ packages/docker-compose/docker-compose-1.2.0rc2.tar.gz/docker-compose-1.2.0rc2/tests/unit/project_test.py
@@ -0,0 +1,240 @@
+from __future__ import unicode_literals
+from .. import unittest
+from compose.service import Service
+from compose.project import Project
+from compose.container import Container
+from compose import config
+
+import mock
+import docker
+
+class ProjectTest(unittest.TestCase):
+    def test_from_dict(self):
+        project = Project.from_dicts('composetest', [
+            {
+                'name': 'web',
+                'image': 'busybox:latest'
+            },
+            {
+                'name': 'db',
+                'image': 'busybox:latest'
+            },
+        ], None)
+        self.assertEqual(len(project.services), 2)
+        self.assertEqual(project.get_service('web').name, 'web')
+        self.assertEqual(project.get_service('web').options['image'], 'busybox:latest')
+        self.assertEqual(project.get_service('db').name, 'db')
+        self.assertEqual(project.get_service('db').options['image'], 'busybox:latest')
+
+    def test_from_dict_sorts_in_dependency_order(self):
+        project = Project.from_dicts('composetest', [
+            {
+                'name': 'web',
+                'image': 'busybox:latest',
+                'links': ['db'],
+            },
+            {
+                'name': 'db',
+                'image': 'busybox:latest',
+                'volumes_from': ['volume']
+            },
+            {
+                'name': 'volume',
+                'image': 'busybox:latest',
+                'volumes': ['/tmp'],
+            }
+        ], None)
+
+        self.assertEqual(project.services[0].name, 'volume')
+        self.assertEqual(project.services[1].name, 'db')
+        self.assertEqual(project.services[2].name, 'web')
+
+    def test_from_config(self):
+        dicts = config.from_dictionary({
+            'web': {
+                'image': 'busybox:latest',
+            },
+            'db': {
+                'image': 'busybox:latest',
+            },
+        })
+        project = Project.from_dicts('composetest', dicts, None)
+        self.assertEqual(len(project.services), 2)
+        self.assertEqual(project.get_service('web').name, 'web')
+        self.assertEqual(project.get_service('web').options['image'], 'busybox:latest')
+        self.assertEqual(project.get_service('db').name, 'db')
+        self.assertEqual(project.get_service('db').options['image'], 'busybox:latest')
+
+    def test_get_service(self):
+        web = Service(
+            project='composetest',
+            name='web',
+            client=None,
+            image="busybox:latest",
+        )
+        project = Project('test', [web], None)
+        self.assertEqual(project.get_service('web'), web)
+
+    def test_get_services_returns_all_services_without_args(self):
+        web = Service(
+            project='composetest',
+            name='web',
+        )
+        console = Service(
+            project='composetest',
+            name='console',
+        )
+        project = Project('test', [web, console], None)
+        self.assertEqual(project.get_services(), [web, console])
+
+    def test_get_services_returns_listed_services_with_args(self):
+        web = Service(
+            project='composetest',
+            name='web',
+        )
+        console = Service(
+            project='composetest',
+            name='console',
+        )
+        project = Project('test', [web, console], None)
+        self.assertEqual(project.get_services(['console']), [console])
+
+    def test_get_services_with_include_links(self):
+        db = Service(
+            project='composetest',
+            name='db',
+        )
+        web = Service(
+            project='composetest',
+            name='web',
+            links=[(db, 'database')]
+        )
+        cache = Service(
+            project='composetest',
+            name='cache'
+        )
+        console = Service(
+            project='composetest',
+            name='console',
+            links=[(web, 'web')]
+        )
+        project = Project('test', [web, db, cache, console], None)
+        self.assertEqual(
+            project.get_services(['console'], include_deps=True),
+            [db, web, console]
+        )
+
+    def test_get_services_removes_duplicates_following_links(self):
+        db = Service(
+            project='composetest',
+            name='db',
+        )
+        web = Service(
+            project='composetest',
+            name='web',
+            links=[(db, 'database')]
+        )
+        project = Project('test', [web, db], None)
+        self.assertEqual(
+            project.get_services(['web', 'db'], include_deps=True),
+            [db, web]
+        )
+
+    def test_use_volumes_from_container(self):
+        container_id = 'aabbccddee'
+        container_dict = dict(Name='aaa', Id=container_id)
+        mock_client = mock.create_autospec(docker.Client)
+        mock_client.inspect_container.return_value = container_dict
+        project = Project.from_dicts('test', [
+            {
+                'name': 'test',
+                'image': 'busybox:latest',
+                'volumes_from': ['aaa']
+            }
+        ], mock_client)
+        self.assertEqual(project.get_service('test')._get_volumes_from(), [container_id])
+
+    def test_use_volumes_from_service_no_container(self):
+        container_name = 'test_vol_1'
+        mock_client = mock.create_autospec(docker.Client)
+        mock_client.containers.return_value = [
+            {
+                "Name": container_name,
+                "Names": [container_name],
+                "Id": container_name,
+                "Image": 'busybox:latest'
+            }
+        ]
+        project = Project.from_dicts('test', [
+            {
+                'name': 'vol',
+                'image': 'busybox:latest'
+            },
+            {
+                'name': 'test',
+                'image': 'busybox:latest',
+                'volumes_from': ['vol']
+            }
+        ], mock_client)
+        self.assertEqual(project.get_service('test')._get_volumes_from(), [container_name])
+
+    @mock.patch.object(Service, 'containers')
+    def test_use_volumes_from_service_container(self, mock_return):
+        container_ids = ['aabbccddee', '12345']
+        mock_return.return_value = [
+            mock.Mock(id=container_id, spec=Container)
+            for container_id in container_ids]
+
+        project = Project.from_dicts('test', [
+            {
+                'name': 'vol',
+                'image': 'busybox:latest'
+            },
+            {
+                'name': 'test',
+                'image': 'busybox:latest',
+                'volumes_from': ['vol']
+            }
+        ], None)
+        self.assertEqual(project.get_service('test')._get_volumes_from(), container_ids)
+
+    def test_use_net_from_container(self):
+        container_id = 'aabbccddee'
+        container_dict = dict(Name='aaa', Id=container_id)
+        mock_client = mock.create_autospec(docker.Client)
+        mock_client.inspect_container.return_value = container_dict
+        project = Project.from_dicts('test', [
+            {
+                'name': 'test',
+                'image': 'busybox:latest',
+                'net': 'container:aaa'
+            }
+        ], mock_client)
+        service = project.get_service('test')
+        self.assertEqual(service._get_net(), 'container:'+container_id)
+
+    def test_use_net_from_service(self):
+        container_name = 'test_aaa_1'
+        mock_client = mock.create_autospec(docker.Client)
+        mock_client.containers.return_value = [
+            {
+                "Name": container_name,
+                "Names": [container_name],
+                "Id": container_name,
+                "Image": 'busybox:latest'
+            }
+        ]
+        project = Project.from_dicts('test', [
+            {
+                'name': 'aaa',
+                'image': 'busybox:latest'
+            },
+            {
+                'name': 'test',
+                'image': 'busybox:latest',
+                'net': 'container:aaa'
+            }
+        ], mock_client)
+
+        service = project.get_service('test')
+        self.assertEqual(service._get_net(), 'container:'+container_name)
