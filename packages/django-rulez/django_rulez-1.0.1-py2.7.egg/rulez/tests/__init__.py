1ce985525f3f39fa1d7de3cdca967ed5c8efb9bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:17 +0000

    Add package django-rulez/1.0.1/django_rulez-1.0.1-py2.7.egg

---
+++ packages/django-rulez/django_rulez-1.0.1-py2.7.egg/rulez/tests/__init__.py
@@ -0,0 +1,5 @@
+from rulez.tests.roles_helpers import *
+from rulez.tests.registry import *
+from rulez.tests.backend import *
+from rulez.tests.signals import *
+from rulez.tests.templatetags import *
