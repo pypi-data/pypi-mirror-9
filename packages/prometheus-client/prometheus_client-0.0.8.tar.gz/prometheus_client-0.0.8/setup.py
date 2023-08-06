127416f32616619838dad4ce53943175838b4d3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:21 +0000

    Add package prometheus-client/0.0.8/prometheus_client-0.0.8.tar.gz

---
+++ packages/prometheus-client/prometheus_client-0.0.8.tar.gz/prometheus_client-0.0.8/setup.py
@@ -0,0 +1,24 @@
+import os
+from setuptools import setup
+
+setup(
+    name = "prometheus_client",
+    version = "0.0.8",
+    author = "Brian Brazil",
+    author_email = "brian.brazil@gmail.com",
+    description = ("Python client for the Prometheus monitoring system."),
+    long_description = ("See https://github.com/prometheus/client_python/blob/master/README.md for documentation."),
+    license = "Apache Software License 2.0",
+    keywords = "prometheus monitoring instrumentation client",
+    url = "https://github.com/prometheus/client_python",
+    packages=['prometheus_client'],
+    test_suite="tests",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Information Technology",
+        "Intended Audience :: System Administrators",
+        "Topic :: System :: Monitoring",
+        "License :: OSI Approved :: Apache Software License",
+    ],
+)
