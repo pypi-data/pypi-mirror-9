f3bf4a511d8a9fdc0f8dd437f95a97c2977d32ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:44 +0000

    Add package primer/1.4.3/primer-1.4.3.zip

---
+++ packages/primer/primer-1.4.3.zip/primer-1.4.3/primer/__init__.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python
+
+"""
+Package of prime number objects.
+"""
+
+from .primer import prime
+from .primer import primes
+from .primer import primorial
+from .primer import sieve
