0c64a8f1691a4082cf9137577a2de31af6bcee77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:13 +0000

    Add package stackinabox/0.6/stackinabox-0.6.tar.gz

---
+++ packages/stackinabox/stackinabox-0.6.tar.gz/stackinabox-0.6/stackinabox/util_httpretty.py
@@ -0,0 +1,37 @@
+"""
+Stack-In-A-Box: HTTPretty Support
+"""
+import logging
+import re
+
+from httpretty import register_uri
+from httpretty.http import HttpBaseClass
+
+from stackinabox.stack import StackInABox
+from stackinabox.utils import CaseInsensitiveDict
+
+
+logger = logging.getLogger(__name__)
+
+
+def httpretty_callback(request, uri, headers):
+    method = request.method
+    response_headers = CaseInsensitiveDict()
+    response_headers.update(headers)
+    request_headers = CaseInsensitiveDict()
+    request_headers.update(request.headers)
+    request.headers = request_headers
+    return StackInABox.call_into(method,
+                                 request,
+                                 uri,
+                                 response_headers)
+
+
+def httpretty_registration(uri):
+    logger.debug('Registering Stack-In-A-Box at {0} under Python HTTPretty'
+                 .format(uri))
+    StackInABox.update_uri(uri)
+    regex = re.compile('(http)?s?(://)?{0}:?(\d+)?/'.format(uri),
+                       re.I)
+    for method in HttpBaseClass.METHODS:
+        register_uri(method, regex, body=httpretty_callback)
