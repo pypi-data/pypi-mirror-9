d2a71ee85150ae91d51d9f1493a998e2ba7b7a49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:54 +0000

    Add package redis_structures/0.0.1a0/redis_structures-0.0.1a0.tar.gz

---
+++ packages/redis_structures/redis_structures-0.0.1a0.tar.gz/redis_structures-0.0.1a0/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3 -S
+import os
+import uuid
+from distutils.core import setup
+from pip.req import parse_requirements
+
+# parse_requirements() returns generator of pip.req.InstallRequirement objects
+install_reqs = parse_requirements(
+    os.path.dirname(os.path.realpath(__file__))+\
+    "/requirements.txt",
+    session=uuid.uuid1())
+setup(
+    name='redis_structures',
+    version='0.0.1a0',
+    description='Redis data structures wrapped with Python 3.',
+    author='Jared Lunde',
+    author_email='jared.lunde@gmail.com',
+    url='https://github.com/jaredlunde/redis_structures',
+    install_requires=[str(ir.req) for ir in install_reqs],
+    packages=['redis_structures', 'redis_structures.vital'])
