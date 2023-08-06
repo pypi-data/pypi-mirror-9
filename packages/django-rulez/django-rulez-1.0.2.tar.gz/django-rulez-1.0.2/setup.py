920a31c986cd6f8ef79858e0a2d6146de8917782
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:17 +0000

    Add package django-rulez/1.0.2/django-rulez-1.0.2.tar.gz

---
+++ packages/django-rulez/django-rulez-1.0.2.tar.gz/django-rulez-1.0.2/setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+import os
+from setuptools import setup, find_packages
+from distutils.core import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+version = '1.0.2'
+
+setup(
+    name='django-rulez',
+    version=version,
+    description="A lean and mean object-level rules system for the Django framework",
+    long_description=read('README.rst'),
+    classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        "Framework :: Django",
+        "Environment :: Web Environment",
+    ],
+    keywords=['authorization', 'backends', 'django', 'rules', 'permissions', 'rulez'],
+    author='Christopher Glass',
+    author_email='tribaal@gmail.com',
+    url='http://github.com/chrisglass/django-rulez',
+    license='BSD',
+    packages=find_packages(),
+    zip_safe=False,
+)
