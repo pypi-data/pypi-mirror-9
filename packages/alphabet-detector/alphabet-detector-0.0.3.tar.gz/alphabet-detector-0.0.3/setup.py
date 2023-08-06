7bbf0a59ac9dc54a02ad9de27042ea528a316aad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:11 +0000

    Add package alphabet-detector/0.0.3/alphabet-detector-0.0.3.tar.gz

---
+++ packages/alphabet-detector/alphabet-detector-0.0.3.tar.gz/alphabet-detector-0.0.3/setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+setup(
+  name = 'alphabet-detector',
+  packages = ['alphabet_detector'],
+  version = '0.0.3',
+  description = 'A library to detect what alphabet something is written in.',
+  author = 'Eli Finkelshteyn',
+  author_email = 'iefinkel@gmail.com',
+  url = 'https://github.com/EliFinkelshteyn/alphabet-detector',
+  download_url = 'https://github.com/EliFinkelshteyn/alphabet-detector/tarball/0.0.1', # I'll explain this in a second
+  keywords = ['alphabet', 'charset', 'detect', 'islatin'], # arbitrary keywords
+  classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
