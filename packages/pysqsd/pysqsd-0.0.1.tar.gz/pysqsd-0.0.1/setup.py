2a54bafbd347cd3d92f2e92c554375b9ab7f309f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:35 +0000

    Add package pysqsd/0.0.1/pysqsd-0.0.1.tar.gz

---
+++ packages/pysqsd/pysqsd-0.0.1.tar.gz/pysqsd-0.0.1/setup.py
@@ -0,0 +1,32 @@
+import os
+import inspect
+from setuptools import setup, find_packages
+import pysqsd
+
+setup(
+    name='pysqsd',
+    version="0.0.1",
+    description="SQS Daemon inspired by Elastic Beanstalk's sqsd",
+    author='Florent Vilmart',
+    author_email='florentvilmart@me.com',
+    url='http://github.com/flovilmart/pysqsd/tree/master',
+    packages=find_packages(),
+    zip_safe=False,
+    license='MIT', 
+    install_requires=[
+        'boto',
+    ],
+    keywords='aws sqs queue',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ], 
+    entry_points = {
+          'console_scripts': [
+                'pysqsd = pysqsd:main', 
+          ],
+    },
+)
