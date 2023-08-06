b3a65d2fc5b9a4f699ffc6f7837a9cfc89289b84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:15 +0000

    Add package scikit-allel/0.8.0/scikit-allel-0.8.0.tar.gz

---
+++ packages/scikit-allel/scikit-allel-0.8.0.tar.gz/scikit-allel-0.8.0/allel/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# flake8: noqa
+from __future__ import absolute_import, print_function, division
+
+
+__version__ = '0.8.0'
+
+
+import allel.model as model
+import allel.bcolz as bcolz
+import allel.stats as stats
+import allel.plot as plot
+import allel.io as io
+import allel.constants as constants
