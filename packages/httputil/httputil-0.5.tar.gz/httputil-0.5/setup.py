7d0ff0ea4ef86efe566c9976c55761f1e2dd77df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:43 +0000

    Add package httputil/0.5/httputil-0.5.tar.gz

---
+++ packages/httputil/httputil-0.5.tar.gz/httputil-0.5/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+
+install_requires = ['ujson',
+                    'tornado',
+                    'requests']
+
+
+tests_require = install_requires + ['vmock',
+                                    'nose',
+                                    'nosexcover']
+
+
+setup(
+    name='httputil',
+    packages=find_packages(),
+    version='0.5',
+    description='Various utilities to deal with HTTP stuff.',
+    author='Vovan Kuznetsov',
+    author_email='vovanec@gmail.com',
+    maintainer_email='vovanec@gmail.com',
+    url='https://github.com/vovanec/httputil',
+    download_url='https://github.com/vovanec/httputil/tarball/0.5',
+    keywords=['http', 'dechunk', 'decompress', 'client', 'request'],
+    license='MIT',
+    classifiers=['License :: OSI Approved :: MIT License',
+                 'Development Status :: 4 - Beta',
+                 'Intended Audience :: Developers',
+                 'Programming Language :: Python :: 3.4'],
+    install_requires=install_requires,
+    tests_require=tests_require,
+    test_suite='nose.collector',
+    extras_require={
+        'test': tests_require,
+    },
+)
