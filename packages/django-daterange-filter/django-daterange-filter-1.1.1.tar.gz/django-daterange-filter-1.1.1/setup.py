dd52fb228680669997b2df5453c5ea37fc389156
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:19 +0000

    Add package django-daterange-filter/1.1.1/django-daterange-filter-1.1.1.tar.gz

---
+++ packages/django-daterange-filter/django-daterange-filter-1.1.1.tar.gz/django-daterange-filter-1.1.1/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='django-daterange-filter',
+    version='1.1.1',
+    description='Allow to filter by a custom date range on the Django Admin',
+    long_description=open('README.rst').read(-1),
+    author='Tomas Zulberti',
+    author_email='tzulberti@gmail.com',
+    url='http://github.com/tzulberti/django-datefilterspec',
+    keywords=[
+        'django admin',
+        'django date range',
+    ],
+    install_requires=[
+        "Django",
+    ],
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Framework :: Django',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.4',
+        'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Internet :: WWW/HTTP :: WSGI',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Environment :: Web Environment',
+        'Operating System :: OS Independent'
+    ],
+    license = 'License :: OSI Approved :: BSD License',
+)
