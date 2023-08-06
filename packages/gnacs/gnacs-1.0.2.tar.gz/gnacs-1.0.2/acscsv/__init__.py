42ab01ca5cdcb8ac9d6849b528d5d7429c51789d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:14 +0000

    Add package gnacs/1.0.2/gnacs-1.0.2.tar.gz

---
+++ packages/gnacs/gnacs-1.0.2.tar.gz/gnacs-1.0.2/acscsv/__init__.py
@@ -0,0 +1,13 @@
+__all__ = [
+        'disqus_acs'
+        , 'foursquare_acs'
+        , 'newsgator_acs'
+        , 'reflect_json'
+        , 'stocktwits_acs'
+        , 'stocktwits_native'
+        , 'twitter_acs'
+        , 'twitter_acs_fields'
+        , 'tumblr_acs'
+        , 'wordpress_acs'
+        , 'snowflake'
+        ]
---
+++ packages/gnacs/gnacs-1.0.2.tar.gz/gnacs-1.0.2/setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+
+GNACS_VERSION_NUMBER = "1.0.2"
+
+if '__main__' == __name__:
+    setup(
+        name='gnacs',
+        version=GNACS_VERSION_NUMBER,
+        author='Scott Hendrickson, Josh Montague, Jinsub Hong, Jeff Kolb, Brian Lehman, Fiona Pigott',
+        author_email='drskippy@twitter.com',
+        packages=['acscsv'],
+        scripts=['gnacs.py'],
+        url='https://github.com/DrSkippy27/Gnacs',
+        download_url='https://github.com/DrSkippy27/Gnacs/tags/%s'%(GNACS_VERSION_NUMBER),
+        license='LICENSE.txt',
+        description='Gnip normalized activity JSON to csv parser (Disqus Comments, Wordpress Posts and Comments, Twitter, Tumblr, Foursquare, StockTwits)',
+        install_requires=[
+                    "ujson >= 1.2",
+                        ]
+        )
