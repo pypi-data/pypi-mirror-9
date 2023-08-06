2141d43545e0c4a7c358321d2a24ddaef8db3d98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:39 +0000

    Add package pipette/0.1.7/pipette-0.1.7.tar.gz

---
+++ packages/pipette/pipette-0.1.7.tar.gz/pipette-0.1.7/src/pipette/__init__.py
@@ -0,0 +1,13 @@
+'''
+pipette
+=======
+
+A library implementing a protocol to simplify programming pipeline-like
+chains of intercommunicating processes.
+
+Copyright (c) 2014-2015 Yauhen Yakimovich
+Licensed under the MIT License (MIT). Read a copy of LICENSE distributed with
+this code.
+'''
+pipette_version = '0.1.7'
+__version__ = pipette_version
