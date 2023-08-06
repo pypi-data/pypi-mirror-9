2b62db7f031e8e8bc3832031fd7139051d318a79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:38 +0000

    Add package uniseg/0.7.0/uniseg-0.7.0.zip

---
+++ packages/uniseg/uniseg-0.7.0.zip/uniseg-0.7.0/setup.py
@@ -0,0 +1,41 @@
+from ast import literal_eval
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+DIR_SRC = 'uniseg'
+VERSION = literal_eval(open(DIR_SRC+'/version.py').read())
+
+
+setup(
+    name = 'uniseg',
+    version = VERSION,
+    author = 'Masaaki Shibata',
+    author_email = 'mshibata@emptypage.jp',
+    url = 'https://bitbucket.org/emptypage/uniseg-python',
+    description\
+        = 'A pure Python library to determine Unicode text segmentations',
+    long_description = open('README.txt').read(),
+    license = 'MIT',
+    packages = ['uniseg'],
+    package_data = {
+        'uniseg': ['ucd.sqlite3', 'docs/*.html', 'samples/*.py']
+    },
+    entry_points = {
+        'console_scripts': ['uniseg-dbpath = uniseg.db:print_dbpath'],
+    },
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Text Processing',
+    ],
+    zip_safe = False,
+)
