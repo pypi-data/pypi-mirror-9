6a0ebcfef26dee7b02935efcd1a378b90763024c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:12 +0000

    Add package zato-client/2.0.3.3/zato-client-2.0.3.3.tar.gz

---
+++ packages/zato-client/zato-client-2.0.3.3.tar.gz/zato-client-2.0.3.3/setup.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+
+"""
+Copyright (C) 2013 Dariusz Suchojad <dsuch at zato.io>
+
+Licensed under LGPLv3, see LICENSE.txt for terms and conditions.
+"""
+
+# flake8: noqa
+import os
+from setuptools import setup, find_packages
+
+version = '2.0.3.3'
+
+long_description = description = 'Convenience Python client for Zato ESB and app server (https://zato.io)'
+
+def parse_requirements(requirements):
+    ignored = ['#', 'setuptools', '-e']
+
+    with open(requirements) as f:
+        return [line for line in f if line.strip() and not any(line.startswith(prefix) for prefix in ignored)]
+
+setup(
+      name = 'zato-client',
+      version = version,
+
+      author = 'Zato Developers',
+      author_email = 'info@zato.io',
+      url = 'https://zato.io',
+      license = 'GNU Lesser General Public License v3 (LGPLv3)',
+      platforms = 'OS Independent',
+      description = description,
+      long_description = description,
+
+      package_dir = {'':'src'},
+      packages = find_packages('src'),
+      namespace_packages = ['zato'],
+
+      install_requires = parse_requirements(
+          os.path.join(os.path.dirname(os.path.realpath(__file__)), 'requirements.txt')),
+      
+      keywords=('soa eai esb middleware messaging queueing asynchronous integration performance http zeromq framework events agile broker messaging server jms enterprise python middleware clustering amqp nosql websphere mq wmq mqseries ibm amqp zmq'),
+      classifiers = [
+          'Development Status :: 5 - Production/Stable',
+          'Environment :: Console',
+          'Framework :: Buildout',
+          'Intended Audience :: Customer Service',
+          'Intended Audience :: Developers',
+          'Intended Audience :: Financial and Insurance Industry',
+          'Intended Audience :: Healthcare Industry',
+          'Intended Audience :: Information Technology',
+          'Intended Audience :: Telecommunications Industry',
+          'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
+          'Natural Language :: English',
+          'Operating System :: OS Independent',
+          'Programming Language :: C',
+          'Programming Language :: Python :: 2 :: Only',
+          'Programming Language :: Python :: 2.7',
+          'Operating System :: POSIX :: Linux',
+          'Operating System :: MacOS :: MacOS X',
+          'Topic :: Communications',
+          'Topic :: Database',
+          'Topic :: Internet',
+          'Topic :: Internet :: WWW/HTTP :: WSGI :: Server',
+          'Topic :: Internet :: File Transfer Protocol (FTP)',
+          'Topic :: Software Development :: Libraries :: Python Modules',
+          'Topic :: Software Development :: Object Brokering',
+          ],
+
+      zip_safe = False,
+)
