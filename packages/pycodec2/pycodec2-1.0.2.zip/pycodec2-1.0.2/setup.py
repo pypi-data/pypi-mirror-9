c16cbedf3658dd57436388ba99aa79dfe9d90e06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:17 +0000

    Add package pycodec2/1.0.2/pycodec2-1.0.2.zip

---
+++ packages/pycodec2/pycodec2-1.0.2.zip/pycodec2-1.0.2/setup.py
@@ -0,0 +1,34 @@
+from distutils.core import setup
+from distutils.extension import Extension
+from Cython.Distutils import build_ext
+
+VERSION = '1.0.2'
+
+ext_modules = [
+    Extension("pycodec2",
+              [
+                  "pycodec2/codec2.pxd",
+                  "pycodec2/pycodec2.pyx",
+              ],
+              libraries=["codec2"]) # Unix-like specific
+]
+
+setup(
+  name = "pycodec2",
+  packages = ['pycodec2'],
+  version = VERSION,
+  description = 'Cython wrapper for codec2',
+  author = 'Grzegorz Milka',
+  author_email = 'grzegorzmilka@gmail.com',
+  url = 'https://github.com/gregorias/pycodec2',
+  download_url = 'https://github.com/gregorias/pycodec2/tarball/' + VERSION,
+  keywords = ['codec2', 'audio', 'voice'],
+  classifiers = ['Topic :: Multimedia :: Sound/Audio :: Speech',
+                 'License :: OSI Approved :: BSD License',
+                 'Programming Language :: Python :: 2',
+                 'Programming Language :: Python :: 3',
+                 'Development Status :: 5 - Production/Stable',
+                 ],
+  cmdclass = {"build_ext": build_ext},
+  ext_modules = ext_modules
+)
