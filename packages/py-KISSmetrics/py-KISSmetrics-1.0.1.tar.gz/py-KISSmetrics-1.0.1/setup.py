d3b36cff5f479bffa30173818a2b398fd0a78ab5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:54 +0000

    Add package py-KISSmetrics/1.0.1/py-KISSmetrics-1.0.1.tar.gz

---
+++ packages/py-KISSmetrics/py-KISSmetrics-1.0.1.tar.gz/py-KISSmetrics-1.0.1/setup.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+import os
+import re
+
+base_path = os.path.dirname(__file__)
+
+fp = open(os.path.join(base_path, 'KISSmetrics', '__init__.py'))
+VERSION = re.compile(r".*__version__ = '(.*?)'",
+                     re.S).match(fp.read()).group(1)
+fp.close()
+
+version = VERSION
+
+def read(fname):
+    try:
+        path = os.path.join(os.path.dirname(__file__), fname)
+        return open(path).read()
+    except IOError:
+        return ""
+
+requirements =      ['urllib3>=1.2,<2.0']
+test_requirements = ['pytest',
+                     'pytest-cov']
+
+setup(
+    name='py-KISSmetrics',
+    version=version,
+    license='MIT',
+    description="Official KISSmetrics client library.",
+    long_description=read('DESCRIPTION.rst'),
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Software Development :: Libraries',
+    ],
+    keywords='',
+    author='Ernest W. Durbin III',
+    author_email='ewdurbin@gmail.com',
+    url='https://github.com/kissmetrics/py-KISSmetrics/',
+    packages=['KISSmetrics'],
+    test_suite='KISSmetrics.tests',
+    install_requires=requirements,
+    tests_require=test_requirements,
+)
