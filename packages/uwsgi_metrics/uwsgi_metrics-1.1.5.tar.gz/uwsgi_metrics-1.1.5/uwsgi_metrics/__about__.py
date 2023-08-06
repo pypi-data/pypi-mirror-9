6181e997cce1f81af05898db7c37d42f7fd3c1f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:17 +0000

    Add package uwsgi_metrics/1.1.5/uwsgi_metrics-1.1.5.tar.gz

---
+++ packages/uwsgi_metrics/uwsgi_metrics-1.1.5.tar.gz/uwsgi_metrics-1.1.5/uwsgi_metrics/__about__.py
@@ -0,0 +1,15 @@
+__all__ = [
+    "__title__", "__summary__", "__uri__", "__version__", "__author__",
+    "__email__", "__license__"
+]
+
+__title__ = "uwsgi_metrics"
+__summary__ = "Metrics for uWSGI services"
+__uri__ = "https://github.com/Yelp/uwsgi_metrics"
+
+__version__ = "1.1.5"
+
+__author__ = "John Billings"
+__email__ = "billings@yelp.com"
+
+__license__ = "Apache Software License 2.0"
