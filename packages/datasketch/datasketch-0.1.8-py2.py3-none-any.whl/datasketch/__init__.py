81832b21937d7f8c01f160ac71d8edc9d101b918
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:23 +0000

    Add package datasketch/0.1.8/datasketch-0.1.8-py2.py3-none-any.whl

---
+++ packages/datasketch/datasketch-0.1.8-py2.py3-none-any.whl/datasketch/__init__.py
@@ -0,0 +1,8 @@
+"""
+Data sketch module for Python
+=============================
+
+datasketch is a Python module integrating various data sketch
+algorithms. It aims to provide efficient approximation alternatives
+to exact solutions to data mining and data integration problems.
+"""
