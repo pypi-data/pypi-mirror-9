cb2d69bfd24633d27d40ea4a926ec4caa5ac4e33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:42 +0000

    Add package django-storages-redux/1.2.3/django-storages-redux-1.2.3.tar.gz

---
+++ packages/django-storages-redux/django-storages-redux-1.2.3.tar.gz/django-storages-redux-1.2.3/setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup
+
+import storages
+
+def read(filename):
+    with open(filename) as f:
+        return f.read()
+
+setup(
+    name='django-storages-redux',
+    version=storages.__version__,
+    packages=['storages', 'storages.backends'],
+    author='Josh Schneier',
+    author_email='josh.schneier@gmail.com',
+    license='BSD',
+    description='Support for many storages (S3, MogileFS, etc) in Django.',
+    long_description=read('README.rst') + '\n\n' + read('CHANGELOG.rst'),
+    url='https://github.com/jschneier/django-storages',
+    classifiers=[
+        'Framework :: Django',
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
+    tests_require=[
+        'Django>=1.5',
+        'pytest',
+        'mock',
+        'boto>=2.32.0'
+    ],
+    test_suite='tests',
+    zip_safe=False
+)
