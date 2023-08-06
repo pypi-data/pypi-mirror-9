88379dcf35dccb66b882c8a2dd4d071e704cb4ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:04 +0000

    Add package rfc3986/0.2.1/rfc3986-0.2.1-py2-none-any.whl

---
+++ packages/rfc3986/rfc3986-0.2.1-py2-none-any.whl/rfc3986/exceptions.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+class RFC3986Exception(Exception):
+    pass
+
+
+class InvalidAuthority(RFC3986Exception):
+    def __init__(self, authority):
+        super(InvalidAuthority, self).__init__(
+            "The authority ({0}) is not valid.".format(authority))
+
+
+class ResolutionError(RFC3986Exception):
+    def __init__(self, uri):
+        super(ResolutionError, self).__init__(
+            "{0} is not an absolute URI.".format(uri.unsplit()))
