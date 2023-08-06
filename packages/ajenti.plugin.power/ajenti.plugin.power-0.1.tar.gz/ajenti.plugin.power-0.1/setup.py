a16adee1910747acb7e9afc2589b48e7ad79aaeb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:10 +0000

    Add package ajenti.plugin.power/0.1/ajenti.plugin.power-0.1.tar.gz

---
+++ packages/ajenti.plugin.power/ajenti.plugin.power-0.1.tar.gz/ajenti.plugin.power-0.1/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+import os
+
+__requires = filter(None, open('requirements.txt').read().splitlines())
+
+setup(
+    name='ajenti.plugin.power',
+    version='0.1',
+    install_requires=__requires,
+    description='Power management',
+    long_description='A Power management plugin for Ajenti panel',
+    author='Ajenti project',
+    author_email='e@ajenti.org',
+    url='http://ajenti.org',
+    packages=find_packages(),
+    include_package_data=True,
+)
