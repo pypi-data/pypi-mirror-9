b3d4d53ce31e495b267b0b6bce96f30dd8a287e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:17 +0000

    Add package outstandingKnife/1.0.2/outstandingKnife-1.0.2.tar.gz

---
+++ packages/outstandingKnife/outstandingKnife-1.0.2.tar.gz/outstandingKnife-1.0.2/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='outstandingKnife',
+    version='1.0.2',
+    author='Michael Imelfort',
+    author_email='mike@mikeimelfort.com',
+    packages=['outstandingknife'],
+    scripts=['bin/outstandingKnife'],
+    url='http://pypi.python.org/pypi/outstandingKnife/',
+    license='GPLv3',
+    description='outstandingKnife',
+    long_description=open('README.md').read(),
+    install_requires=[],
+)
