caf9145b8a6ab53ef148db321c9f1f6687691a90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:36:48 +0000

    Add package dedupe/0.8.0.0.0/dedupe-0.8.0.0.0-py2.7-linux-x86_64.egg

---
+++ packages/dedupe/dedupe-0.8.0.0.0-py2.7-linux-x86_64.egg/dedupe/variables/set.py
@@ -0,0 +1,32 @@
+from .base import FieldType
+from dedupe import predicates
+from simplecosine.cosine import CosineSetSimilarity
+
+class SetType(FieldType) :
+    type = "Set"
+
+    _predicate_functions = (predicates.wholeSetPredicate,
+                            predicates.commonSetElementPredicate,
+                            predicates.lastSetElementPredicate,
+                            predicates.commonTwoElementsPredicate,
+                            predicates.commonThreeElementsPredicate,
+                            predicates.magnitudeOfCardinality,
+                            predicates.firstSetElementPredicate)
+    
+    _canopy_thresholds = (0.2, 0.4, 0.6, 0.8)
+
+    def __init__(self, definition) :
+        super(SetType, self).__init__(definition)
+
+        canopy_predicates = [predicates.TfidfSetPredicate(threshold, 
+                                                       self.field)
+                             for threshold in self._canopy_thresholds]
+
+        self.predicates += canopy_predicates
+
+        if 'corpus' not in definition :
+            definition['corpus'] = [] 
+
+        self.comparator = CosineSetSimilarity(definition['corpus'])
+
+
