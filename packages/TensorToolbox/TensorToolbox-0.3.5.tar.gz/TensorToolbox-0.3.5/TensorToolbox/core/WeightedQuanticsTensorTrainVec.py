db3eda8c06453c7dfe2422aa4dab35f25a4be89f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:07:51 +0000

    Add package TensorToolbox/0.3.5/TensorToolbox-0.3.5.tar.gz

---
+++ packages/TensorToolbox/TensorToolbox-0.3.5.tar.gz/TensorToolbox-0.3.5/TensorToolbox/core/WeightedQuanticsTensorTrainVec.py
@@ -0,0 +1,36 @@
+#
+# This file is part of TensorToolbox.
+#
+# TensorToolbox is free software: you can redistribute it and/or modify
+# it under the terms of the LGNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# TensorToolbox is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# LGNU Lesser General Public License for more details.
+#
+# You should have received a copy of the LGNU Lesser General Public License
+# along with TensorToolbox.  If not, see <http://www.gnu.org/licenses/>.
+#
+# DTU UQ Library
+# Copyright (C) 2014-2015 The Technical University of Denmark
+# Scientific Computing Section
+# Department of Applied Mathematics and Computer Science
+#
+# Author: Daniele Bigoni
+#
+
+__all__ = ['WQTTvec']
+
+import sys
+import warnings
+import logging 
+
+import numpy as np
+from scipy import sparse as scsp
+
+from TensorToolbox.core import TTvec, Candecomp, TensorWrapper
+
+from TensorToolbox import multilinalg as mla
