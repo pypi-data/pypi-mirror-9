2f6c482919a545fc8be74b0c8b6212a358711450
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:07:32 +0000

    Add package getgist/0.0.1/GetGist-0.0.1.tar.gz

---
+++ packages/getgist/GetGist-0.0.1.tar.gz/GetGist-0.0.1/README.md
@@ -0,0 +1,51 @@
+# GetGist
+
+Easily download any file from a public [GitHub Gist](http://gist.github.com) account, with a one single command.
+
+[![Last Version](https://pypip.in/version/getgist/badge.svg)](https://pypi.python.org/pypi/getgist/)
+[![Downloads](https://pypip.in/download/getgist/badge.svg)](https://pypi.python.org/pypi/getgist/)
+[![Lincense](https://pypip.in/license/getgist/badge.svg)](https://pypi.python.org/pypi/getgist/)
+
+## Why?
+
+Because of reasons I do not have a `dotfile` repository. I prefer to store my `.vimrc`, `.gitconfig`, `.bash_whatever` etc. as [Gists](http://gist.github.com/).
+
+Thus, to be honest, I wrote this script so update my `.vimrc` with one single command.
+
+## Install
+
+`$ pip install getgist`
+
+*GetGist* runs with Python 2 or 3 and is built only with core modules (no dependencies but Python).
+
+## Usage
+
+Just run `getgist <user> <file>`. For example:
+
+```
+user@localhost ~ $ getgist cuducos .vimrc
+  Fetching https://api.github.com/users/cuducos/gists …
+  Fetching https://gist.githubusercontent.com/cuducos/409fac6ac23bf515f495/raw/f8acc26f0422b02fc282c5b4e97b70710044dbb3/.vimrc …
+  Replace existing .vimrc ? (y/n) y
+  Deleting existing .vimrc …
+  Saving new .vimrc …
+  Done!
+user@localhost ~ $
+```
+
+If you decide not to delete your copy of the local file, it will be renamed with extensions such as `.bkp`, `.bkp.1`, `.bkp.2` etc.
+
+## Contributing
+
+Feel free to [report an issue](http://github.com/cuducos/getgist/issues), [open a pull request](http://github.com/cuducos/getgist/pulls), or [drop a line](http://twitter.com/cuducos).
+
+### To do list
+
+* Authenticate users to:
+  * Have a default user (`$ getgist .vimrc` to mine, `$ getgist johndoe .vimrc` to get John's one)
+  * Allow the possibility of getting private Gists
+* Write tests for `Gist.__backup()`
+
+## License
+
+So far I consider this script just public domain.
---
+++ packages/getgist/GetGist-0.0.1.tar.gz/GetGist-0.0.1/getgist.py
@@ -0,0 +1,121 @@
+# coding: utf-8
+import argparse
+import json
+import os
+try:
+    from urllib2 import urlopen
+except ImportError:
+    from urllib.request import urlopen
+
+
+class Gist(object):
+
+    def __init__(self, user=False, file_name=False, assume_yes=False):
+
+        # set main variables
+        self.user = user
+        self.file_name = file_name
+        self.assume_yes = assume_yes
+
+        # load arguments
+        if not self.user or not self.file_name:
+
+            # set argparse
+            parser = argparse.ArgumentParser()
+            parser.add_argument('user', help='Gist username')
+            parser.add_argument('file_name', help='Gist file name')
+            parser.add_argument('-y', '--yes-to-all',
+                                help='Assume `yes` to all prompts',
+                                action="store_true")
+
+            # load values from argparse
+            args = parser.parse_args()
+            if not user:
+                self.user = args.user
+            if not file_name:
+                self.file_name = args.file_name
+            if not assume_yes:
+                self.assume_yes = args.yes_to_all
+
+        # set support variables
+        self.local_dir = os.path.dirname(os.path.realpath(__file__))
+        self.local_path = os.path.join(self.local_dir, self.file_name)
+        self.info = self.__load_info()
+
+    @property
+    def id(self):
+        return self.info.get('id', None)
+
+    @property
+    def raw_url(self):
+        return self.info.get('raw_url', None)
+
+    def load(self):
+        return self.__curl(self.raw_url)
+
+    def save(self):
+
+        # check if file exists
+        if os.path.exists(self.local_path):
+
+            # delete or backup existing file?
+            confirm = 'y'
+            if not self.assume_yes:
+                message = '  Delete existing {} ? (y/n) '
+                try:
+                    confirm = raw_input(message.format(self.file_name))
+                except NameError:
+                    confirm = input(message.format(self.file_name))
+
+            # delete exitsing file
+            if confirm.lower() == 'y':
+                self.__output('Deleting existing {} …'.format(self.file_name))
+                os.remove(self.local_path)
+
+            # backup existing file
+            else:
+                self.__backup()
+
+        # save new file
+        with open(self.local_path, 'w') as file_handler:
+            contents = self.load()
+            self.__output('Saving new {} …'.format(self.file_name))
+            file_handler.write(contents)
+        self.__output('Done!')
+
+    def __backup(self):
+        count = 0
+        name = '{}.bkp'.format(self.file_name)
+        backup = os.path.join(self.local_dir, name)
+        while os.path.exists(backup):
+            count += 1
+            name = '{}.bkp.{}'.format(self.file_name, count)
+            backup = os.path.join(self.local_dir, name)
+        self.__output('Moving existing {} to {}…'.format(self.file_name, name))
+        os.rename(os.path.join(self.local_dir, self.file_name), backup)
+
+    def __load_info(self):
+        url = 'https://api.github.com/users/{}/gists'.format(self.user)
+        gists = json.loads(str(self.__curl(url)))
+        for gist in gists:
+            if self.file_name in gist['files']:
+                return {'id': gist['id'],
+                        'raw_url': gist['files'][self.file_name]['raw_url']}
+        return False
+
+    def __curl(self, url):
+        request = urlopen(url)
+        self.__output('Fetching {} …'.format(url))
+        status = request.getcode()
+        if status == 200:
+            contents = request.read()
+            return contents.decode('utf-8')
+        self.__output('[Fail] HTTP Status {}'.format(url, status))
+        return False
+
+    def __output(self, message):
+        print('  {}'.format(message))
+
+if __name__ == '__main__':
+    gist = Gist()
+    gist.save()
