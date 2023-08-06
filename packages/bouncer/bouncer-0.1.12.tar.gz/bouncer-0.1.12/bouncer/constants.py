ac86278b9a055bd3386b4b637ca1f0e73a434873
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:22 +0000

    Add package bouncer/0.1.12/bouncer-0.1.12.tar.gz

---
+++ packages/bouncer/bouncer-0.1.12.tar.gz/bouncer-0.1.12/bouncer/constants.py
@@ -0,0 +1,23 @@
+# Actions
+MANAGE = 'manage'
+READ = 'read'
+UPDATE = 'update'
+EDIT = 'edit'
+CREATE = 'create'
+
+# OLD SCHOOL
+GET = 'get'
+PUT = 'put'
+PATCH = 'patch'
+POST = 'post'
+DELETE = 'delete'
+
+# NEW SCHOOL
+INDEX = 'index'
+SHOW = 'show'
+NEW = 'new'
+CREATE = 'create'
+
+
+# Special Target
+ALL = 'all'
---
+++ packages/bouncer/bouncer-0.1.12.tar.gz/bouncer-0.1.12/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+required_modules = []
+
+setup(name='bouncer',
+      version='0.1.12',
+      description='Simple Declarative Authentication based on Ryan Bates excellent cancan library',
+      url='http://github.com/jtushman/bouncer',
+      author='Jonathan Tushman',
+      author_email='jonathan@zefr.com',
+      install_requires=required_modules,
+      license='MIT',
+      packages=['bouncer'],
+      tests_require=['nose'],
+      test_suite='nose.collector',
+      zip_safe=False)
