5471fbc0f70269227af9e945cb19db35b017ab9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:07:54 +0000

    Add package bumpversion/0.5.2/bumpversion-0.5.2.tar.gz

---
+++ packages/bumpversion/bumpversion-0.5.2.tar.gz/bumpversion-0.5.2/setup.py
@@ -0,0 +1,38 @@
+import re
+from setuptools import setup
+
+description = 'Version-bump your software with a single command!'
+
+long_description = re.sub(
+  "\`(.*)\<#.*\>\`\_",
+  r"\1",
+  str(open('README.rst', 'rb').read()).replace(description, '')
+)
+
+setup(
+    name='bumpversion',
+    version='0.5.2',
+    url='https://github.com/peritus/bumpversion',
+    author='Filip Noetzel',
+    author_email='filip+bumpversion@j03.de',
+    license='MIT',
+    packages=['bumpversion'],
+    description=description,
+    long_description=long_description,
+    entry_points={
+        'console_scripts': [
+            'bumpversion = bumpversion:main',
+        ]
+    },
+    classifiers=(
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: Implementation :: PyPy',
+    ),
+)
