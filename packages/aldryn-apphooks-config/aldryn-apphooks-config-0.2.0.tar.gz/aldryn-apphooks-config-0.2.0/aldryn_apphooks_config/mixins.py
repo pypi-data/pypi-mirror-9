a9a49b9a3e28ef571edcb1c2b9c33b7c117fcae8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:32 +0000

    Add package aldryn-apphooks-config/0.2.0/aldryn-apphooks-config-0.2.0.tar.gz

---
+++ packages/aldryn-apphooks-config/aldryn-apphooks-config-0.2.0.tar.gz/aldryn-apphooks-config-0.2.0/aldryn_apphooks_config/mixins.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from .utils import get_app_instance
+
+
+class AppConfigMixin(object):
+    """
+    This mixin must be attached to any class-based views used which implements AppHookConfig.
+
+    It provides:
+    * current namespace in self.namespace
+    * namespace configuration in self.config
+    * current application in the `current_app` context variable
+    """
+    def dispatch(self, request, *args, **kwargs):
+        self.namespace, self.config = get_app_instance(request)
+        return super(AppConfigMixin, self).dispatch(request, *args, **kwargs)
+
+    def render_to_response(self, context, **response_kwargs):
+        response_kwargs['current_app'] = self.namespace
+        return super(AppConfigMixin, self).render_to_response(context, **response_kwargs)
