749f40baf4545938a4566a1f8278dbda8da0339b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:42 +0000

    Add package pyapi-gitlab/7.5.2/pyapi-gitlab-7.5.2.tar.gz

---
+++ packages/pyapi-gitlab/pyapi-gitlab-7.5.2.tar.gz/pyapi-gitlab-7.5.2/setup.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+setup(
+    name = "pyapi-gitlab",
+    version = "7.5.2",
+    packages = find_packages(),
+    install_requires = ['requests'],
+    extras_require = {
+        'markdown':  ["markdown"]
+    },
+    # metadata for upload to PyPI
+    author = "Itxaka Serrano Garcia",
+    author_email = "itxakaserrano@gmail.com",
+    description = "Gitlab API wrapper for Gitlab 7.X",
+    license = "GPL3",
+    keywords = "gitlab git wrapper",
+    url = "http://github.com/itxaka/pyapi-gitlab/",
+    test_suite="gitlab_tests",
+)
