ceb7c8c46029d30736975dd311b51943dfb36ca8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:47 +0000

    Add package analyzerdam/0.1.0/analyzerdam-0.1.0.tar.gz

---
+++ packages/analyzerdam/analyzerdam-0.1.0.tar.gz/analyzerdam-0.1.0/analyzerdam/DAMFactory.py
@@ -0,0 +1,40 @@
+'''
+Created on Nov 9, 2011
+
+@author: ppa
+'''
+
+from analyzer.lib.errors import Errors, UfException
+
+
+class DAMFactory(object):
+    ''' DAM factory '''
+    @staticmethod
+    def createDAM(damType, settings=None):
+        ''' create DAM '''
+        if 'yahoo' == damType:
+            from analyzerdam.yahooDAM import YahooDAM
+            dam=YahooDAM()
+        elif 'google' == damType:
+            from analyzerdam.googleDAM import GoogleDAM
+            dam=GoogleDAM()
+        elif 'excel' == damType:
+            from analyzerdam.excelDAM import ExcelDAM
+            dam=ExcelDAM()
+        elif 'hbase' == damType:
+            from analyzerdam.hbaseDAM import HBaseDAM
+            dam=HBaseDAM()
+        elif 'sql' == damType:
+            from analyzerdam.sqlDAM import SqlDAM
+            dam=SqlDAM()
+        else:
+            raise UfException(Errors.INVALID_DAM_TYPE,
+                              "DAM type is invalid %s" % damType)
+
+        dam.setup(settings)
+        return dam
+
+    @staticmethod
+    def getAvailableTypes():
+        ''' return all available types '''
+        return ['yahoo', 'google', 'excel', 'hbase', 'sql']
