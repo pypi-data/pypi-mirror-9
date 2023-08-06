278a00f3e019c4c6cf51f220eead294a6a8f95bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:05 +0000

    Add package PICOS/1.1.0/PICOS-1.1.0.post2.tar.gz

---
+++ packages/PICOS/PICOS-1.1.0.post2.tar.gz/PICOS-1.1.0/setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+
+setup(
+    name='PICOS',
+    version='1.1.0',
+    author='G. Sagnol',
+    author_email='sagnol@zib.de',
+    packages=['picos'],
+    license='LICENSE.txt',
+    description='A Python Interface to Conic Optimization Solvers.',
+    long_description=open('README.txt').read(),
+    install_requires=[
+        "CVXOPT >= 1.1.4",
+        "numpy  >= 1.6.2",
+        "six >= 1.8.0"
+    ],
+    url='http://picos.zib.de',
+    download_url='http://picos.zib.de/dist/PICOS-1.1.0.tar.gz'
+) 
