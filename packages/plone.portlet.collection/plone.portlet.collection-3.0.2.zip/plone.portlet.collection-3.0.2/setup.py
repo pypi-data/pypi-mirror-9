e4eea373deb7e46f2ac167eb060d9012df0da993
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:16 +0000

    Add package plone.portlet.collection/3.0.2/plone.portlet.collection-3.0.2.zip

---
+++ packages/plone.portlet.collection/plone.portlet.collection-3.0.2.zip/plone.portlet.collection-3.0.2/setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup, find_packages
+
+version = '3.0.2'
+
+setup(name='plone.portlet.collection',
+      version=version,
+      description="A portlet that fetches results from a collection",
+      long_description=open("README.rst").read() + "\n" +
+      open("CHANGES.rst").read(),
+      classifiers=[
+          "Environment :: Web Environment",
+          "Framework :: Plone",
+          "Framework :: Zope2",
+          "License :: OSI Approved :: GNU General Public License (GPL)",
+          "Operating System :: OS Independent",
+          "Programming Language :: Python",
+      ],
+      keywords='',
+      author='Plone Foundation',
+      author_email='plone-developers@lists.sourceforge.net',
+      url='http://pypi.python.org/pypi/plone.portlet.collection',
+      license='GPL version 2',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['plone', "plone.portlet"],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'plone.memoize',
+          'plone.portlets',
+          'plone.app.portlets',
+          'plone.app.vocabularies',
+          'plone.app.form',
+      ],
+      extras_require={
+          'test': [
+              'plone.app.testing',
+              'plone.app.contenttypes',
+          ],
+      }
+      )
