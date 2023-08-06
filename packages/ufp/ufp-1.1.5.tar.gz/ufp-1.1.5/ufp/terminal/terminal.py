19458e7eab14f591daf056d64837337ef1306cd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:02 +0000

    Add package ufp/1.1.5/ufp-1.1.5.tar.gz

---
+++ packages/ufp/ufp-1.1.5.tar.gz/ufp-1.1.5/ufp/terminal/terminal.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+
+from __future__ import unicode_literals, absolute_import, division, print_function
+import sys
+import ANSIColors
+
+__all__ = ['feed', 'pause']
+
+def feed(*msg) :
+	"""
+	기존 줄을 지우고, 입력한 메시지를 화면에 씁니다.
+	
+	:param msg: 화면에 출력한 문자열 또는 print 함수 출력 옵션 설정
+	"""
+	ANSIColors.clearLine()
+	print(*msg, end='')
+	sys.stdout.flush()
+	pass
+
+def pause() :
+	"""
+	사용자의 입력을 대기합니다.
+	
+	엔터를 눌러야 입력 대기가 끝납니다.
+	"""
+	print('[Enter]를 눌러 다음으로 진행합니다...', end = '');
+	raw_input("");
+	pass
