7240b7ae1f6de3334537055a47553a3113b73fd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:26 +0000

    Add package dj-email-url/0.0.3/dj-email-url-0.0.3.tar.gz

---
+++ packages/dj-email-url/dj-email-url-0.0.3.tar.gz/dj-email-url-0.0.3/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+
+with open('README.rst') as f:
+    long_description = f.read()
+
+setup(
+    name='dj-email-url',
+    version='0.0.3',
+    url='https://github.com/migonzalvar/dj-email-url',
+    license='BSD',
+    author='Miguel Gonzalez',
+    author_email='migonzalvar@gmail.com',
+    description='Use an URL to configure email backend settings in your '
+                'Django Application.',
+    long_description=long_description,
+    py_modules=['dj_email_url'],
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
