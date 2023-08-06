28c0060432e6d064d96ab04a547ef0807a5fdb44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:09 +0000

    Add package django-jinja/1.3.3/django-jinja-1.3.3.tar.gz

---
+++ packages/django-jinja/django-jinja-1.3.3.tar.gz/django-jinja-1.3.3/django_jinja/apps.py
@@ -0,0 +1,15 @@
+import django
+
+from django.apps import AppConfig
+from django_jinja import base
+
+
+class DjangoJinjaAppConfig(AppConfig):
+    name = "django_jinja"
+    verbose_name = "Django Jinja"
+
+    def ready(self):
+        base.patch_django_for_autoescape()
+
+        if django.VERSION[:2] == (1, 7):
+            base.setup()
