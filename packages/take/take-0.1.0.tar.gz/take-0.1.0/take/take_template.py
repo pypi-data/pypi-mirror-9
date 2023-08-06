36f1eb48ceef0096180bb48f39f9691e4f75389f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:21 +0000

    Add package take/0.1.0/take-0.1.0.tar.gz

---
+++ packages/take/take-0.1.0.tar.gz/take-0.1.0/take/take_template.py
@@ -0,0 +1,27 @@
+from pyquery import PyQuery
+
+from .parser import parse
+
+
+class TakeTemplate(object):
+
+    @staticmethod
+    def from_file(path, **kwargs):
+        with open(path, 'rb') as f:
+            return TakeTemplate(f.read().decode('utf-8'), **kwargs)
+
+    def __init__(self, src, **kwargs):
+        self.node = parse(src)
+        self.base_url = kwargs.get('base_url', None)
+
+    def take(self, *args, **kwargs):
+        base_url = kwargs.pop('base_url', None) or self.base_url
+        _doc = PyQuery(*args, **kwargs)
+        if base_url:
+            _doc.make_links_absolute(base_url)
+        rv = {}
+        self.node.do(None, rv=rv, value=_doc, last_value=_doc)
+        return rv
+
+    def __call__(self, *args, **kwargs):
+        return self.take(*args, **kwargs)
