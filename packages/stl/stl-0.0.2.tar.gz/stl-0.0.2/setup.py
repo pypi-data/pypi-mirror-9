9cbb17f257ba36242477b14fc46fc6d64321c53e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:16 +0000

    Add package stl/0.0.2/stl-0.0.2.tar.gz

---
+++ packages/stl/stl-0.0.2.tar.gz/stl-0.0.2/setup.py
@@ -0,0 +1,33 @@
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name="stl",
+    version="0.0.2",
+    author="Martin Atkins",
+    author_email="mart@degeneration.co.uk",
+    description="Read and write STL 3D geometry files in both the ASCII and the binary flavor",
+
+    packages=['stl'],
+    install_requires=[
+    ],
+    setup_requires=[
+        'nose>=1.0',
+        'sphinx>=0.5',
+    ],
+    tests_require=[
+        'nose>=1.0',
+        'coverage',
+        'mock',
+        'pep8',
+    ],
+    test_suite='nose.collector',
+
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Intended Audience :: Developers",
+    ],
+)
