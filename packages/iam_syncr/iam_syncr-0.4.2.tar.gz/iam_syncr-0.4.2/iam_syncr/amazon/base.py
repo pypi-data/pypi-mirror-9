54f99f0b7ef2c875419d65206d527d524df8b33e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:52 +0000

    Add package iam_syncr/0.4.2/iam_syncr-0.4.2.tar.gz

---
+++ packages/iam_syncr/iam_syncr-0.4.2.tar.gz/iam_syncr-0.4.2/iam_syncr/amazon/base.py
@@ -0,0 +1,90 @@
+from iam_syncr.errors import SyncrError
+
+from boto.iam.connection import IAMConnection
+from boto.s3.connection import S3Connection
+import logging
+import boto
+import sys
+import six
+
+if six.PY2:
+    KMSConnection = None
+else:
+    from boto.kms.layer1 import KMSConnection
+
+log = logging.getLogger("iam_syncr.amazon.base")
+
+class Amazon(object):
+    dry_run = False
+    connection = None
+
+    # Account info that is overridden by __init__
+    accounts = None
+    account_id = None
+    account_name = None
+
+    @classmethod
+    def set_boto_useragent(self, app_name, version):
+        """Put this app in the useragent used by boto"""
+        __import__("boto")
+        useragent = sys.modules["boto.connection"].UserAgent
+        if app_name not in useragent:
+            sys.modules["boto.connection"].UserAgent = "{0} {1}/{2}".format(useragent, app_name, version)
+
+    def __init__(self, account_id, account_name, accounts, dry_run=False):
+        self.changes = False
+        self.dry_run = dry_run
+        self.accounts = accounts
+        self.account_id = account_id
+        self.account_name = account_name
+
+    def setup(self):
+        """Make sure our current credentials are for this account and set self.connection"""
+        try:
+            connection = IAMConnection()
+        except boto.exception.NoAuthHandlerFound:
+            raise SyncrError("Export AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY before running this script (your aws credentials)")
+
+        try:
+            result = connection.list_roles()
+        except boto.exception.BotoServerError as error:
+            if error.status == 403:
+                raise SyncrError("Your credentials aren't allowed to look at iam :(")
+            else:
+                raise
+
+        roles = self.all_roles = result["list_roles_response"]["list_roles_result"]["roles"]
+        if not roles:
+            raise SyncrError("There are no roles in your account, I can't figure out the account id")
+
+        amazon_account_id = roles[0]['arn'].split(":")[4]
+        if str(self.account_id) != str(amazon_account_id):
+            raise SyncrError("Please use credentials for the right account", expect=self.account_id, got=amazon_account_id)
+
+        # If reached this far, the credentials belong to the correct account :)
+        self.connection = connection
+        return connection
+
+    @property
+    def s3_connection(self):
+        if getattr(self, "_s3_connection", None) is None:
+            try:
+                self._s3_connection = S3Connection()
+            except boto.exception.NoAuthHandlerFound:
+                raise SyncrError("Export AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY before running this script (your aws credentials)")
+        return self._s3_connection
+
+    def kms_connection_for(self, location):
+        if KMSConnection is None:
+            raise SyncrError("Sorry, need python3 to do anything related to kms")
+
+        if getattr(self, "_kms_connections", None) is None:
+            self._kms_connections = {}
+
+        if location not in self._kms_connections:
+            try:
+                self._kms_connections[location] = boto.kms.connect_to_region(location)
+            except boto.exception.NoAuthHandlerFound:
+                raise SyncrError("Export AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY before running this script (your aws credentials)")
+        return self._kms_connections[location]
+
---
+++ packages/iam_syncr/iam_syncr-0.4.2.tar.gz/iam_syncr-0.4.2/iam_syncr/amazon/kms.py
@@ -0,0 +1,96 @@
+from iam_syncr.amazon.documents import AmazonDocuments
+from iam_syncr.amazon.common import AmazonMixin
+from iam_syncr.errors import BadAlias, BadRole
+
+import logging
+import boto
+import six
+
+if not six.PY2:
+    import boto.kms.exceptions
+
+log = logging.getLogger("iam_syncr.amazon.kms")
+
+class AmazonKms(AmazonMixin, object):
+    def __init__(self, amazon, connection):
+        self.amazon = amazon
+        self.documents = AmazonDocuments()
+        self.connection = connection
+
+    def key_info(self, alias):
+        """Return what amazon knows about this key"""
+        try:
+            return self.connection.describe_key("alias/{0}".format(alias))["KeyMetadata"]
+        except boto.kms.exceptions.NotFoundException:
+            return False
+
+    def current_policy(self, key):
+        """Return the current policy for this key"""
+        try:
+            return self.connection.get_key_policy(key["KeyId"], "default")["Policy"]
+        except boto.kms.exceptions.NotFoundException:
+            return "{}"
+
+    def has_key(self, alias):
+        """Return whether amazon has info about this key"""
+        return bool(self.key_info(alias))
+
+    def create_key(self, alias, description, permission_document=None):
+        """Create a key"""
+        with self.catch_boto_400("Couldn't create key", document=permission_document, alias=alias):
+            for _ in self.change("+", "key", alias=alias):
+                key = self.connection.create_key(permission_document, description)["KeyMetadata"]
+                self.connection.create_alias("alias/{0}".format(alias), key["KeyId"])
+
+    def modify_key(self, alias, description, permission_document):
+        """Modify a key"""
+        key = self.key_info(alias)
+        if not key:
+            return
+
+        current_description = key["Description"]
+        if current_description != description:
+            for _ in self.change("M", "key_description", key=alias, description=description):
+                self.connection.update_key_description(key["KeyId"], description)
+
+        current_policy = self.current_policy(key)
+        changes = list(self.documents.compare_two_documents(current_policy, permission_document))
+        if changes:
+            with self.catch_boto_400("Couldn't modify policy", "Key {0} policy".format(alias), permission_document, key=alias):
+                for _ in self.change("M", "key_policy", key=alias, changes=changes, description=description):
+                    self.connection.put_key_policy(key["KeyId"], 'default', permission_document)
+
+    def modify_grant(self, alias, description, grant):
+        """Modify grants on a key"""
+        key = self.key_info(alias)
+        if not key:
+            raise BadAlias("Where did the key go?", alias=alias)
+
+        key_id = key["KeyId"]
+        new_grants = []
+        current_grants = self.connection.list_grants(key_id)["Grants"]
+        for policy in current_grants:
+            policy["Operations"] = sorted(policy["Operations"])
+
+        for policy in grant:
+            nxt = {"GranteePrincipal": self.user_from_arn(policy["grantee"]), "RetireePrincipal": self.user_from_arn(policy.get("Retiree")), "Operations": sorted(policy["operations"]), "Constraints": policy.get("constraints"), "GrantTokens": policy.get("grant_tokens")}
+            nxt = dict((key, val) for key, val in nxt.items() if val is not None)
+
+            if not any(all(current[key] == val for key, val in nxt.items()) for current in current_grants):
+                new_grants.append(policy)
+
+        for policy in new_grants:
+            for _ in self.change("+", "key_grant", key=alias, grantee=policy["grantee"]):
+                self.connection.create_grant(key_id, policy["grantee"], retiring_principal=policy.get("retiree"), operations=policy["operations"], constraints=policy.get("constraints"), grant_tokens=policy.get("grant_tokens"))
+
+    def user_from_arn(self, arn):
+        """Convert an arn into the user id"""
+        if arn is None:
+            return
+
+        role_ids = [item["role_id"] for item in self.amazon.all_roles if item['arn'] == arn]
+        if len(role_ids) != 1:
+            raise BadRole("Didn't find a single role id for specified arn", got=role_ids, arn=arn)
+
+        return role_ids[0]
+
