5c73d1e77e06fc9373d84469558d5cec401f72d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:32 +0000

    Add package openlabs_shipping/3.2.2.3/openlabs_shipping-3.2.2.3.tar.gz

---
+++ packages/openlabs_shipping/openlabs_shipping-3.2.2.3.tar.gz/openlabs_shipping-3.2.2.3/__init__.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+    __init__.py
+
+    :copyright: (c) 2014-2015 by Openlabs Technologies & Consulting (P) Limited
+    :license: BSD, see LICENSE for more details.
+"""
+from trytond.pool import Pool
+
+from carrier import Carrier, CarrierConfig
+from party import (
+    Address, AddressValidationMsg, AddressValidationWizard,
+    AddressValidationSuggestionView
+)
+from shipment import (
+    ShipmentOut, StockMove, GenerateShippingLabelMessage, GenerateShippingLabel,
+    ShippingCarrierSelector, ShippingLabelNoModules
+)
+from sale import Sale, SaleLine
+from log import CarrierLog
+
+
+def register():
+    Pool.register(
+        CarrierConfig,
+        Carrier,
+        CarrierLog,
+        Address,
+        ShipmentOut,
+        StockMove,
+        Sale,
+        SaleLine,
+        GenerateShippingLabelMessage,
+        ShippingLabelNoModules,
+        ShippingCarrierSelector,
+        AddressValidationMsg,
+        AddressValidationSuggestionView,
+        module='shipping', type_='model'
+    )
+    Pool.register(
+        GenerateShippingLabel,
+        AddressValidationWizard,
+        module='shipping', type_='wizard'
+    )
