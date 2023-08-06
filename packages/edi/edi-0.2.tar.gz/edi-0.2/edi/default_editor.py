e323997aab800d55aac6df2bb88036b01ac7a34f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:10 +0000

    Add package edi/0.2/edi-0.2.tar.gz

---
+++ packages/edi/edi-0.2.tar.gz/edi-0.2/edi/default_editor.py
@@ -0,0 +1,36 @@
+import os, sys
+
+def default_editor():
+    """If no editors are specified in .edi.yml, somehow figure out what constitutes a default editor."""
+
+    # Standard UNIX environment variables to set the editor.
+    visual_env_var = os.environ.get("VISUAL")
+    if visual_env_var:
+        return visual_env_var + " {0}"
+
+    editor_var = os.environ.get("EDITOR")
+    if editor_var:
+        return editor + " {0}"
+
+    # TODO: Need to use something like this to get default Mac OS text editor:
+    # defaults write com.apple.LaunchServices LSHandlers -array-add '{LSHandlerContentType=public.plain-text;LSHandlerRoleAll=com.sublimetext.3;}'
+    
+    # Windows-based system.
+    windows = 'win32' in str(sys.platform).lower()
+    # Standard Linux 2+ system.
+    linux = ('linux' in str(sys.platform).lower())
+    osx = ('darwin' in str(sys.platform).lower())
+    hpux = ('hpux' in str(sys.platform).lower()) # Complete guess.
+    solaris = ('sunos' in str(sys.platform).lower())
+    cygwin = ('cygwin' in str(sys.platform).lower())
+
+    # Ubuntu defaults for if X windows is enabled or if only terminal is available
+    if os.environ.get("DISPLAY"):
+        if os.path.exists("/usr/bin/gnome-text-editor"):
+            return "gnome-text-editor {0}"
+    else:
+        if os.path.exists("/usr/bin/editor"):
+            return "editor {0}"
+
+    # Last resort
+    return "vi {0}"
