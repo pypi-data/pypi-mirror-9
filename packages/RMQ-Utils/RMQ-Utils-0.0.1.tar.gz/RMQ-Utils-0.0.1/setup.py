af90b7ea61cc59231b8aa0382e716d969deeed5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:04 +0000

    Add package RMQ-Utils/0.0.1/RMQ-Utils-0.0.1.tar.gz

---
+++ packages/RMQ-Utils/RMQ-Utils-0.0.1.tar.gz/RMQ-Utils-0.0.1/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(
+    name='RMQ-Utils',
+    version='0.0.1',
+    author='Brian Hines',
+    author_email='brian@projectweekend.net',
+    packages=['rmq_utils'],
+    url='https://github.com/projectweekend/RMQ-Utils',
+    license='LICENSE.txt',
+    description='Utilities for managing RabbitMQ.',
+    long_description=open('README.txt').read(),
+    install_requires=[
+        "pika == 0.9.14",
+        "pyrabbit == 1.1.0",
+    ],
+)
