b4d9aa39b1e6cc3aa82ebda9a39d29ddd1e212ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:48 +0000

    Add package django-pesapal/0.2.1/django-pesapal-0.2.1.tar.gz

---
+++ packages/django-pesapal/django-pesapal-0.2.1.tar.gz/django-pesapal-0.2.1/HISTORY.rst
@@ -0,0 +1,48 @@
+.. :changelog:
+
+History
+-------
+0.2.1 (2015-04-03)
+++++++++++++++++++
+- Added test sandbox
+- Updated Django version
+- Updated django-uuidfield
+
+0.2 (2015-03-17)
+++++++++++++++++++
+- Support anonymous checkouts
+- Add support for getting payment status
+- Major structural refactoring. Use mixins
+- Use Mixins and XML Builder
+
+0.1.5 (2014-09-25)
+++++++++++++++++++
+- Pin dependencies to specific versions
+- Update how imports should be done
+- Remove imports from __init__.py
+
+0.1.4 (2014-09-23)
+++++++++++++++++++
+- Fix import bug. Tests for projects using this fail in Shippable
+- Set max Django version to 1.7
+
+0.1.3 (2014-07-18)
+++++++++++++++++++
+- Packaging for PyPi
+
+0.1.2 (2014-06-30)
+++++++++++++++++++
+- Fix import bug in urls.py
+- Fix how callback url is constructed
+- Fix: Live URL uses https
+
+0.1.1 (2014-06-30)
+++++++++++++++++++
+- Refactor handling of redirect urls. Model validation of transaction and merchant reference
+- Rename settings to conf. Set default oauth redirect url
+- Add django-uuidfield to dependencies
+
+0.1.0 (2014-06-30)
+++++++++++++++++++
+
+* First release on PyPI.
---
+++ packages/django-pesapal/django-pesapal-0.2.1.tar.gz/django-pesapal-0.2.1/setup.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+
+import django_pesapal
+
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from distutils.core import setup
+
+version = django_pesapal.__version__
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    print("You probably want to also tag the version now:")
+    print("  git tag -a %s -m 'version %s'" % (version, version))
+    print("  git push --tags")
+    sys.exit()
+
+readme = open('README.rst').read()
+history = open('HISTORY.rst').read().replace('.. :changelog:', '')
+
+setup(
+    name='django-pesapal',
+    version=version,
+    description="""A django port of pesapal payment gateway""",
+    long_description=readme + '\n\n' + history,
+    author='Billy Odero',
+    url='https://github.com/odero/django-pesapal',
+    packages=find_packages(exclude=['sandbox*']),
+    include_package_data=True,
+    install_requires=[
+        'Django>=1.5.4,<=1.6.11',
+        'oauth2==1.5.211',
+        'django-uuidfield<=0.6.0',
+    ],
+    dependency_links=[
+        "https://github.com/dcramer/django-uuidfield/archive/9bd27e9.zip#egg=django-uuidfield-0.6.0",
+    ],
+    license="BSD",
+    zip_safe=False,
+    keywords='django-pesapal payment pesapal',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
