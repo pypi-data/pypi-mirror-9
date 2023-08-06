98631bc853af57800704ba2459f26f30b070f63e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:20:58 +0000

    Add package arcgis-rest-query/0.0.10/arcgis-rest-query-0.0.10.tar.gz

---
+++ packages/arcgis-rest-query/arcgis-rest-query-0.0.10.tar.gz/arcgis-rest-query-0.0.10/setup.py
@@ -0,0 +1,22 @@
+import sys
+from setuptools import setup
+
+install_requires = [
+    "argparse>=1.2.1",
+    "requests>=2.4.3"
+]
+
+setup(
+    name='arcgis-rest-query',
+    version='0.0.10',
+    description='A tool to download a layer from an ArcGIS web service as GeoJSON',
+    author='Ken Schwencke',
+    author_email='schwank@gmail.com',
+    url='https://github.com/Schwanksta/python-arcgis-rest-query',
+    license='MIT',
+    packages=('arcgis',),
+    scripts=(
+        'bin/arcgis-get',
+    ),
+    install_requires=install_requires,
+)
