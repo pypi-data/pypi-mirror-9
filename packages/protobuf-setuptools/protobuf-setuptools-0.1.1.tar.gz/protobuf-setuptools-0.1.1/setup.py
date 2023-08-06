bda2303d473fd344123a14c362ea1fb4ab0391bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:23:16 +0000

    Add package protobuf-setuptools/0.1.1/protobuf-setuptools-0.1.1.tar.gz

---
+++ packages/protobuf-setuptools/protobuf-setuptools-0.1.1.tar.gz/protobuf-setuptools-0.1.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='protobuf-setuptools',
+    version='0.1.1',
+    license='Apache License, Version 2.0',
+    author="Ivan Kalinin",
+    author_email="pupssman@yandex-team.ru",
+    description='setuptools build_proto command',
+    long_description=open('README.rst').read(),
+    url='https://github.com/pupssman/protobuf-setuptools',
+    
+    py_modules=['protobuf_setuptools'],
+    entry_points = {
+        "distutils.commands": [
+            "build_proto = protobuf_setuptools:ProtoBuild",
+        ],
+    }
+)
