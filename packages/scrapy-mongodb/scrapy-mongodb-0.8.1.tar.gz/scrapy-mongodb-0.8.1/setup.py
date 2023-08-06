2993365e71409ebe31b2b3f674284e353f6aff43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:50 +0000

    Add package scrapy-mongodb/0.8.1/scrapy-mongodb-0.8.1.tar.gz

---
+++ packages/scrapy-mongodb/scrapy-mongodb-0.8.1.tar.gz/scrapy-mongodb-0.8.1/setup.py
@@ -0,0 +1,27 @@
+"""
+Setup script for PyPi
+"""
+
+from distutils.core import setup
+setup(
+    name='scrapy-mongodb',
+    version='0.8.1',
+    license='Apache License, Version 2.0',
+    description='Pipeline to MongoDB for Scrapy. Supports MongoDB replica sets',
+    author='Sebastian Dahlgren',
+    author_email='sebastian.dahlgren@gmail.com',
+    url='http://sebdah.github.com/scrapy-mongodb/',
+    keywords="scrapy mongodb",
+    py_modules=['scrapy_mongodb'],
+    platforms=['Any'],
+    install_requires=[
+        'pymongo >= 2.4.1'
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python'
+    ]
+)
