687b5033043215aa73a99b24072423cd5019f3d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:28 +0000

    Add package incuna-test-utils/6.2.5/incuna-test-utils-6.2.5.tar.gz

---
+++ packages/incuna-test-utils/incuna-test-utils-6.2.5.tar.gz/incuna-test-utils-6.2.5/setup.py
@@ -0,0 +1,38 @@
+from setuptools import find_packages, setup
+
+
+version = '6.2.5'
+
+
+extras_require = {
+    'images': [
+        'Pillow>=2.8.1,<2.9',
+    ],
+}
+
+
+setup(
+    name='incuna-test-utils',
+    packages=find_packages(),
+    include_package_data=True,
+    version=version,
+    description='Custom TestCases and other test helpers for Django apps',
+    long_description=open('README.md').read(),
+    author='Incuna',
+    author_email='admin@incuna.com',
+    url='https://github.com/incuna/incuna-test-utils/',
+    install_requires=[],
+    extras_require=extras_require,
+    zip_safe=False,
+    license='BSD',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Software Development :: Testing',
+    ],
+)
