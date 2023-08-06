f8abe25469edda08f6f6c1b29bf91b0488365e12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:23 +0000

    Add package rejected/3.4.1/rejected-3.4.1-py2-none-any.whl

---
+++ packages/rejected/rejected-3.4.1-py2-none-any.whl/rejected/__init__.py
@@ -0,0 +1,15 @@
+"""
+Rejected is a Python RabbitMQ Consumer Framework and Controller Daemon
+
+"""
+__author__ = 'Gavin M. Roy <gavinmroy@gmail.com>'
+__since__ = "2009-09-10"
+__version__ = "3.4.1"
+
+from consumer import Consumer
+from consumer import PublishingConsumer
+from consumer import SmartConsumer
+from consumer import SmartPublishingConsumer
+from consumer import ConsumerException
+from consumer import MessageException
+
