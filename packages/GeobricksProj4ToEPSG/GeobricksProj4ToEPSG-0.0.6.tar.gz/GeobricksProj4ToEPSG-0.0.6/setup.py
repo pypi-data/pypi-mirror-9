a338a018aad12663f0f5ee2bc6dbfd1346030f80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:14 +0000

    Add package GeobricksProj4ToEPSG/0.0.6/GeobricksProj4ToEPSG-0.0.6.tar.gz

---
+++ packages/GeobricksProj4ToEPSG/GeobricksProj4ToEPSG-0.0.6.tar.gz/GeobricksProj4ToEPSG-0.0.6/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+from setuptools import find_packages
+
+setup(
+    name='GeobricksProj4ToEPSG',
+    version='0.0.6',
+    author='Simone Murzilli; Guido Barbaglia',
+    author_email='geobrickspy@gmail.com',
+    packages=find_packages(),
+    license='LICENSE.txt',
+    long_description=open('README.md').read(),
+    description='Geobricks Proj4 to EPSG code convert library',
+    install_requires=[
+        'watchdog',
+        'flask',
+        'flask-cors'
+    ],
+    url='http://pypi.python.org/pypi/GeobricksProj4ToEPSG/',
+    keywords=['geobricks', 'proj4', 'epsg', 'gis', 'geo']
+)
