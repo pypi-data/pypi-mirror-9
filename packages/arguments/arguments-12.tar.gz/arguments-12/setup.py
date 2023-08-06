7ccc6a43bf8307143c68aa2e28122471679fd49f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:56 +0000

    Add package arguments/12/arguments-12.tar.gz

---
+++ packages/arguments/arguments-12.tar.gz/arguments-12/setup.py
@@ -0,0 +1,17 @@
+# coding=utf-8
+"""
+appinstance
+Active8 (04-03-15)
+license: GNU-GPL2
+"""
+from setuptools import setup
+setup(name='arguments',
+      version='12',
+      description='Argument parser based on docopt',
+      url='https://github.com/erikdejonge/arguments',
+      author='Erik de Jonge',
+      author_email='erik@a8.nl',
+      license='GPL',
+      packages=['arguments'],
+      zip_safe=True,
+      install_requires=['docopt', 'schema', 'consoleprinter'])
