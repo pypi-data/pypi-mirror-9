7a73d903791b1a8f283a8935f7fbeb95f78d677f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:43 +0000

    Add package consumerism/0.2.5/consumerism-0.2.5-py2-none-any.whl

---
+++ packages/consumerism/consumerism-0.2.5-py2-none-any.whl/consumerism/message.py
@@ -0,0 +1,20 @@
+class AbstractMessage(object):
+    """
+    Base class for processed messages.
+    """
+
+    def __init__(self, body_dict):
+        self.message = body_dict
+
+    def resource_name(self):
+        """
+        Returns the name of the resource which should act upon this message.
+        """
+        raise NotImplementedError
+
+    def invoke_args(self):
+        """
+        Returns arguments which should be passed to the resource's invoke
+        method.
+        """
+        raise NotImplementedError
---
+++ packages/consumerism/consumerism-0.2.5-py2-none-any.whl/consumerism/processor.py
@@ -0,0 +1,71 @@
+import logging
+
+
+class Processor(object):
+    """
+    Processes SQS messages.
+    """
+
+    def __init__(self, resource_factory, message_class):
+        """
+        Create a new processor.
+        The resource factory should return objects which implement the
+        AbstractResource interface.
+        """
+        self.resource_factory = resource_factory
+        self.message_class = message_class
+        self.logger = logging.getLogger(__name__)
+
+    def process(self, body, log_handler=None):
+        """
+        Dispatch the JSON message to the correct Resource handler and
+        perform the requested action.
+        Optionally attach a supplied log handler to the resource created
+        to fulfill the processing request.
+        """
+        try:
+            message = self.message_class(body)
+            resource_name = message.resource_name()
+            invoke_args, invoke_kwargs = message.invoke_args()
+        except Exception as e:
+            # Discard malformed messages
+            self.logger.warn(
+                'Discarding malformed message, missing field "{}", body={}'
+                .format(e, body))
+            return True
+
+        try:
+            resource = self.resource_factory.new(resource_name)
+        except:
+            self.logger.critical(
+                'Could not create resource "{}", body={}'
+                .format(resource_name, body),
+                exc_info=True)
+            return False
+
+        try:
+            with ResourceActionLogger(log_handler, resource):
+                result = resource.invoke(*invoke_args, **invoke_kwargs)
+        except:
+            self.logger.critical(
+                'Encountered exception while invoking resource "{}"'
+                .format(resource_name),
+                exc_info=True
+            )
+            return False
+
+        return result
+
+
+class ResourceActionLogger(object):
+    def __init__(self, handler, resource):
+        self.handler = handler
+        self.resource = resource
+
+    def __enter__(self):
+        if hasattr(self.resource, 'logger'):
+            self.resource.logger.addHandler(self.handler)
+
+    def __exit__(self, type, value, traceback):
+        if hasattr(self.resource, 'logger'):
+            self.resource.logger.removeHandler(self.handler)
