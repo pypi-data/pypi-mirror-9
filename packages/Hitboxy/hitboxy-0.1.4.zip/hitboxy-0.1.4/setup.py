4c6af3fc5a19e06932b690df740e3bac48379144
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:27 +0000

    Add package Hitboxy/0.1.4/hitboxy-0.1.4.zip

---
+++ packages/Hitboxy/hitboxy-0.1.4.zip/hitboxy-0.1.4/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup( 
+    name = 'hitboxy', 
+    description = 'Wrapper around the Hitbox API',
+    version = '0.1.4',
+    author = 'David Ewelt', 
+    author_email = 'uranoxyd@gmail.com',
+    url = 'https://bitbucket.org/Uranoxyd/hitboxy',
+    license = 'BSD',
+    packages = find_packages(),
+    include_package_data = True,
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        
+        'Intended Audience :: Developers',        
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4'
+    ],
+    keywords = 'hitbox hitbox.tv api'
+)
