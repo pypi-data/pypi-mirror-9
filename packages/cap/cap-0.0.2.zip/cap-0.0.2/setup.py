6759879180bf4f0755021f23ce606f7f12fde5dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:52 +0000

    Add package cap/0.0.2/cap-0.0.2.zip

---
+++ packages/cap/cap-0.0.2.zip/cap-0.0.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+REQUIREMENTS = ['enum34']
+
+setup(
+    name='cap',
+    version='0.0.2',
+    py_modules=['cap'],
+    install_requires=REQUIREMENTS,
+    url='https://github.com/code-museum/capture',
+    license='GNU General Public License, version 2',
+    author='code-museum',
+    author_email='code-museum@users.noreply.github.com',
+    description='Cap: lightweight package for use network captures',
+    keywords="network capture packet pcap"
+)
