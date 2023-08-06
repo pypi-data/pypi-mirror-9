8e0d9c31c8badb26e107da0fb756d4f05064e7f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:49 +0000

    Add package clef/0.0.6/clef-0.0.6.tar.gz

---
+++ packages/clef/clef-0.0.6.tar.gz/clef-0.0.6/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+with open('LONG_DESCRIPTION.rst') as f:
+    long_description = f.read()
+
+setup(
+    name='clef',
+    packages=['clef'],
+    version='0.0.6',
+    description='A Python wrapper for the Clef API',
+    long_description=long_description,
+    author='Grace Wong',
+    author_email='gwongz@gmail.com',
+    url='https://github.com/gwongz/clef-python',
+    download_url='https://github.com/gwongz/python-clef/tarball/0.0.6',
+    license='MIT',
+    keywords=['clef', 'api'],
+    classifiers=[
+        'Natural Language :: English',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
+
