6a7bd10f8b8757cd032f76f2208bd61985a1b02d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:20 +0000

    Add package requests-ucloud/0.1.2/requests-ucloud-0.1.2.tar.gz

---
+++ packages/requests-ucloud/requests-ucloud-0.1.2.tar.gz/requests-ucloud-0.1.2/ucloudauth/__init__.py
@@ -0,0 +1,18 @@
+# -*- coding:utf8 -*-
+
+"""
+ucloudauth
+~~~~~~~~~~~~~~~
+
+This module contains the authentication handlers for UCloud Service
+"""
+
+__version__ = "0.1.2"
+__author__ = "SkyLothar"
+__email__ = "allothar@gmail.com"
+__url__ = "http://github.com/skylothar/requests-ucloud"
+
+__all__ = ["UCloudAuth", "UFileAuth"]
+
+from .common_sign import UCloudAuth
+from .ufile import UFileAuth
