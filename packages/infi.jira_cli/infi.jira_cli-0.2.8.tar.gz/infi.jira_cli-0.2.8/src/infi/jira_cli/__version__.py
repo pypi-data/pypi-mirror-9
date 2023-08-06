eb5653bb3f2e10d41a79286e20110bb2878fc085
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:59 +0000

    Add package infi.jira_cli/0.2.8/infi.jira_cli-0.2.8.tar.gz

---
+++ packages/infi.jira_cli/infi.jira_cli-0.2.8.tar.gz/infi.jira_cli-0.2.8/src/infi/jira_cli/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.2.8"
+__git_commiter_name__ = "Guy Rozendorn"
+__git_commiter_email__ = "guy@rzn.co.il"
+__git_branch__ = '(detached from v0.2.8)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = 'a3b99f586817efd08de30fcbcb01ac4b2996b4c3'
+__git_head_subject__ = 'Finished release v0.2.8.'
+__git_head_message__ = '* release/v0.2.8:\n  TRIVIAL attachmetns, footer and header\n  TRIVIAL fixing'
+__git_dirty_diff__ = ''
