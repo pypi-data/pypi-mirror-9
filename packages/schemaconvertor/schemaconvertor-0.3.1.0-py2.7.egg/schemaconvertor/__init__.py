770f5f92e7fe54657a9759f3fd04c2205d3919a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:59 +0000

    Add package schemaconvertor/0.3.1.0/schemaconvertor-0.3.1.0-py2.7.egg

---
+++ packages/schemaconvertor/schemaconvertor-0.3.1.0-py2.7.egg/schemaconvertor/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+from . import convertor
+
+__author__ = 'Liu Yicong'
+__email__ = 'imyikong@gmail.com'
+__version__ = convertor.__version__
+__all__ = ["__author__", "__email__", "__version__", "convertor"]
