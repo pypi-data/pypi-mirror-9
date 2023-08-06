2bc1434670bbe304ef6ab6651084dc673d62ad08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:07:46 +0000

    Add package django_image_proxy/0.0.7/django_image_proxy-0.0.7.tar.gz

---
+++ packages/django_image_proxy/django_image_proxy-0.0.7.tar.gz/django_image_proxy-0.0.7/image_proxy/templatetags/thumbnail.py
@@ -0,0 +1,15 @@
+from django.template import Library
+
+from django.conf import settings
+from urlparse import urljoin
+from django.core.urlresolvers import reverse_lazy
+
+register = Library()
+
+@register.simple_tag(takes_context=True)
+def thumbnail(context, source, size=None, method="crop"):
+    if source and size:
+        return reverse_lazy("proxy_image_full", args=[source, size, method])
+    elif source:
+        return reverse_lazy("proxy_image", args=[source])
+    return None
