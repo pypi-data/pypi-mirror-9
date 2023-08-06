03a706a0a9d5d966a9360e6ff93131d4bbb197cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:19 +0000

    Add package b3j0f.aop/0.7.1/b3j0f.aop-0.7.1.zip

---
+++ packages/b3j0f.aop/b3j0f.aop-0.7.1.zip/b3j0f.aop-0.7.1/b3j0f/aop/__init__.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+
+# --------------------------------------------------------------------
+# The MIT License (MIT)
+#
+# Copyright (c) 2014 Jonathan Labéjof <jonathan.labejof@gmail.com>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+# --------------------------------------------------------------------
+
+"""
+Aspect Oriented Programming Library for Python
+
+Provides tools to (un)weave and get advices, and check joinpoint status.
+"""
+
+__version__ = "0.7.1"
+
+__all__ = [
+    'weave', 'unweave', 'weave_on', 'get_advices',
+    'Advice',
+    'get_intercepted', 'is_intercepted',
+    'Joinpoint', 'JoinpointError'
+]
+
+from .joinpoint import (
+    Joinpoint, get_intercepted, is_intercepted, JoinpointError
+)
+from .advice import (
+    weave, unweave, weave_on, Advice, get_advices
+)
