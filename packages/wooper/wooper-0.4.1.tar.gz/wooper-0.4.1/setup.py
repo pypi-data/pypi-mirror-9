4385c1a42ae209fe4bb44354144dd5421b78ad18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:02 +0000

    Add package wooper/0.4.1/wooper-0.4.1.tar.gz

---
+++ packages/wooper/wooper-0.4.1.tar.gz/wooper-0.4.1/setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+LONG_DESCRIPTION = open('README.md').read()
+
+setup(
+    name='wooper',
+    version="0.4.1",
+    description="FrisbyJS-inspired REST API testing helpers and steps \
+for 'behave' behavior-driven development testing library",
+    long_description=LONG_DESCRIPTION,
+    author='Yauhen Kirylau',
+    author_email='actionless.loveless@gmail.com',
+    url='http://github.com/actionless/wooper',
+    license=open('LICENSE').read(),
+    platforms=["any"],
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'requests==2.3.0',
+        'behave==1.2.5'
+    ],
+    dependency_links=[
+        "https://github.com/actionless/behave/zipball/master#egg=behave-1.2.5a1",
+    ],
+    classifiers=[
+        # @TODO: change status
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        # @TODO: add testers
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        # @TODO: add version
+        'Programming Language :: Python',
+        'Topic :: Software Development',
+    ],
+)
