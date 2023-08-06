f1afa498ac7f4a1823c6615a31cb137ce090fe34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:52:55 +0000

    Add package pylibconfig2/0.2.1/pylibconfig2-0.2.1.tar.gz

---
+++ packages/pylibconfig2/pylibconfig2-0.2.1.tar.gz/pylibconfig2-0.2.1/pylibconfig2/__init__.py
@@ -0,0 +1,11 @@
+from pylibconfig2.conf_types import \
+    Config, \
+    ConfError, \
+    ConfGroup, \
+    ConfList, \
+    ConfArray
+from pylibconfig2.parsing import \
+    ParseException, \
+    ParseFatalException
+
+PyLibConfigErrors = (ConfError, ParseException, ParseFatalException)
