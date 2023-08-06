e430d57b9476584f340943c165fc612778e4867f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:03 +0000

    Add package armord/0.1.5/armord-0.1.5.zip

---
+++ packages/armord/armord-0.1.5.zip/armord-0.1.5/src/armord/main.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Hive Armor Daemon
+# Copyright (c) 2008-2015 Hive Solutions Lda.
+#
+# This file is part of Hive Armor Daemon.
+#
+# Hive Armor Daemon is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Hive Armor Daemon is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Hive Armor Daemon. If not, see <http://www.gnu.org/licenses/>.
+
+__author__ = "João Magalhães <joamag@hive.pt>"
+""" The author(s) of the module """
+
+__version__ = "1.0.0"
+""" The version of the module """
+
+__revision__ = "$LastChangedRevision$"
+""" The revision number of the module """
+
+__date__ = "$LastChangedDate$"
+""" The last change date of the module """
+
+__copyright__ = "Copyright (c) 2008-2015 Hive Solutions Lda."
+""" The copyright for the module """
+
+__license__ = "GNU General Public License (GPL), Version 3"
+""" The license for the module """
+
+import appier
+import appier_extras
+
+class ArmordApp(appier.WebApp):
+
+    def __init__(self):
+        appier.WebApp.__init__(
+            self,
+            name = "armord",
+            parts = (
+                appier_extras.AdminPart,
+            )
+        )
+        self.theme = "modern"
+        self.style = "romantic"
+        self.libs = "current"
+        self.login_redirect = "base.index"
+
+def main():
+    app = ArmordApp()
+    app.serve()
+
+if __name__ == "__main__":
+    app = ArmordApp()
+    app.serve()
