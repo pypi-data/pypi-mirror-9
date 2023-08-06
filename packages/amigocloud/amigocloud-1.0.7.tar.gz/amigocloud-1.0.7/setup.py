23f51ade326515f033029d4a170e5d0813602b41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:05 +0000

    Add package amigocloud/1.0.7/amigocloud-1.0.7.tar.gz

---
+++ packages/amigocloud/amigocloud-1.0.7.tar.gz/amigocloud-1.0.7/setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+with open('VERSION.txt', 'r') as v:
+    version = v.read().strip()
+
+with open('REQUIREMENTS.txt', 'r') as r:
+    requires = r.read().split()
+
+with open('README.rst', 'r') as r:
+    readme = r.read()
+
+download_url = 'https://github.com/amigocloud/python-amigocloud/tarball/%s'
+
+
+setup(
+    name='amigocloud',
+    packages=['amigocloud'],
+    version=version,
+    description='Python client for the AmigoCloud REST API',
+    long_description=readme,
+    author='Julio M Alegria',
+    author_email='julio@amigocloud.com',
+    url='https://github.com/amigocloud/python-amigocloud',
+    download_url=download_url % version,
+    install_requires=requires,
+    license='MIT'
+)
