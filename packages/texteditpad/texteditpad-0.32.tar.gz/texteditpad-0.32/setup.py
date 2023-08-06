8f58b31760a9fb4be683deb69382df29f9b0c722
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:36 +0000

    Add package texteditpad/0.32/texteditpad-0.32.tar.gz

---
+++ packages/texteditpad/texteditpad-0.32.tar.gz/texteditpad-0.32/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+__version__ = '0.32'
+
+setup(
+    name='texteditpad',
+    py_modules=['texteditpad'],
+    version=__version__,
+    description='Simple textbox editing widget with Emacs-like keybindings',
+    long_description=open('README.rst').read(),
+    url='https://github.com/yskmt/texteditpad',
+    author='Yusuke Sakamoto',
+    author_email='yus.sakamoto@gmail.com',
+    license='MIT',
+    keywords='curses terminal text edit box pad',
+    classifiers=[
+        'Intended Audience :: End Users/Desktop',
+        'Environment :: Console :: Curses',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: POSIX',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Terminals',
+        'Topic :: Utilities',
+        'Topic :: Text Editors'
+    ],
+)
