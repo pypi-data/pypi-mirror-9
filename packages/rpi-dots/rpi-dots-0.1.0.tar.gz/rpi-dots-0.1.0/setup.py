57e782a811a5f1240a2943887bb236dbb338c967
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:36:52 +0000

    Add package rpi-dots/0.1.0/rpi-dots-0.1.0.tar.gz

---
+++ packages/rpi-dots/rpi-dots-0.1.0.tar.gz/rpi-dots-0.1.0/setup.py
@@ -0,0 +1,36 @@
+import os
+from setuptools import setup, find_packages
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name="rpi-dots",
+    version="0.1.0",
+    author="Ben Nuttall",
+    author_email="ben@raspberrypi.org",
+    description="Software for the DOTS board for the Raspberry Pi",
+    long_description=read('README.rst'),
+    license="BSD",
+    keywords=[
+        "raspberrypi",
+        "dots",
+        "dots board",
+        "connect the dots",
+        "bare conductive",
+        "conductive paint",
+    ],
+    url="https://github.com/raspberrypilearning/dots",
+    packages=find_packages(),
+    install_requires=[
+        "pygame",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "Topic :: Games/Entertainment :: Puzzle Games",
+        "License :: OSI Approved :: BSD License",
+        "Development Status :: 4 - Beta",
+    ],
+)
