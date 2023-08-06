e96e4ea5d857a07b911be3b35e4686751d5f1ac9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:25 +0000

    Add package CANard/0.2.1/CANard-0.2.1.tar.gz

---
+++ packages/CANard/CANard-0.2.1.tar.gz/CANard-0.2.1/setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+
+setup(
+    name='CANard',
+    version='0.2.1',
+    author='Eric Evenchick',
+    author_email='eric@evenchick.com',
+    packages=['canard', 'canard.test', 'canard.hw', 'canard.utils', 'canard.proto'],
+    scripts=[],
+    url='http://github.com/ericevenchick/CANard',
+    license='GPLv3',
+    description='Library for interacting with Controller Area Network (CAN)',
+    long_description=open('README.rst').read(),
+    classifiers=['Development Status :: 3 - Alpha',
+		 'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+		 'Topic :: Software Development :: Libraries',
+		 'Topic :: Software Development :: Embedded Systems']
+
+
+)
