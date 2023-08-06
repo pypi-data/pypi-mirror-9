9b17b77a7784fbb3e595ccf9d6826c021a798092
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:55 +0000

    Add package gapipy/0.1.39/gapipy-0.1.39-py2.py3-none-any.whl

---
+++ packages/gapipy/gapipy-0.1.39-py2.py3-none-any.whl/gapipy/resources/dossier/accommodation_dossier.py
@@ -0,0 +1,29 @@
+from __future__ import unicode_literals
+
+from ..base import Resource
+from .details import DossierDetail, DossierDetailsMixin
+
+
+class AccommodationDossier(Resource, DossierDetailsMixin):
+    _resource_name = 'accommodation_dossiers'
+
+    _as_is_fields = [
+        'id', 'href', 'name',
+        'website',
+        'property_type',
+        'address',
+        'features',
+        'emails',
+        'phone_numbers',
+        'rooms',
+    ]
+
+    _date_time_fields_local = ['date_created', 'date_last_modified']
+
+    _resource_fields = [
+        ('location', 'Place'),
+    ]
+
+    _model_collection_fields = [
+        ('details', DossierDetail),
+    ]
