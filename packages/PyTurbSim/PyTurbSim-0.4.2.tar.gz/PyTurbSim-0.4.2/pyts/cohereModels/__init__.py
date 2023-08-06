4b8570cd231adcaa08a40af3c6ee33704752ff0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:31 +0000

    Add package PyTurbSim/0.4.2/PyTurbSim-0.4.2.tar.gz

---
+++ packages/PyTurbSim/PyTurbSim-0.4.2.tar.gz/PyTurbSim-0.4.2/pyts/cohereModels/__init__.py
@@ -0,0 +1,11 @@
+## \namespace cohereModels
+"""
+This is the PyTurbSim 'coherence models' package.
+
+For more information or to use this module import the cohereModels.api
+package, e.g.:
+import pyts.cohereModels.api as cm
+
+"""
+
+
