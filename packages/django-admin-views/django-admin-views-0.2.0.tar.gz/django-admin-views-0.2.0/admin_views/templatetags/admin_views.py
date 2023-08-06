5a2de53aec881584318dc7c7c1e8488844a56424
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:52 +0000

    Add package django-admin-views/0.2.0/django-admin-views-0.2.0.tar.gz

---
+++ packages/django-admin-views/django-admin-views-0.2.0.tar.gz/django-admin-views-0.2.0/admin_views/templatetags/admin_views.py
@@ -0,0 +1,42 @@
+from django import template
+from django.conf import settings
+from django.core.urlresolvers import reverse
+from django.contrib.admin import site
+
+from ..admin import AdminViews
+
+register = template.Library()
+
+@register.simple_tag
+def get_admin_views(app_name, perms):
+    output = []
+    STATIC_URL = settings.STATIC_URL
+
+    for k, v in site._registry.items():
+        if app_name.lower() not in str(k._meta):
+            continue
+
+        if isinstance(v, AdminViews):
+            for type, name, link, perm in v.output_urls:
+                if perm and not perm in perms:
+                    continue
+                if type == 'url':
+                    img_url = "%sadmin_views/icons/link.png" % STATIC_URL
+                    alt_text = "Link to '%s'" % name
+                else:
+                    img_url = "%sadmin_views/icons/view.png" % STATIC_URL
+                    alt_text = "Custom admin view '%s'" % name
+
+                output.append(
+                        u"""<tr>
+                              <th scope="row">
+                                  <img src="%s" alt="%s" />
+                                  <a href="%s">%s</a></th>
+                              <td>&nbsp;</td>
+                              <td>&nbsp;</td>
+                           </tr>
+                        """ % (img_url, alt_text, link, name)
+                    )
+
+    return "".join(output)
+
