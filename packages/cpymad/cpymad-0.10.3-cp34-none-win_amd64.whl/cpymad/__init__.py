236f79a4aac4eed8a6a417711f0d9e4c7a0ed22e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:40 +0000

    Add package cpymad/0.10.3/cpymad-0.10.3-cp34-none-win_amd64.whl

---
+++ packages/cpymad/cpymad-0.10.3-cp34-none-win_amd64.whl/cpymad/__init__.py
@@ -0,0 +1,30 @@
+# encoding: utf-8
+from __future__ import unicode_literals
+
+__title__ = 'cpymad'
+__version__ = '0.10.3'
+
+__summary__ = 'Cython binding to MAD-X'
+__uri__ = 'https://github.com/hibtc/cpymad'
+
+__author__ = 'PyMAD developers'
+__author_email__ = 'pymad@cern.ch'
+__maintainer__ = 'Thomas Gläßle'
+__maintainer_email__ = 't_glaessle@gmx.de'
+
+__license__ = 'CC0, Apache, Non-Free'
+__copyright__ = 'See file COPYING.rst'
+
+# Trove classifiers: https://pypi.python.org/pypi?:action=list_classifiers
+__classifiers__ = [
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Science/Research',
+    'License :: CC0 1.0 Universal (CC0 1.0) Public Domain Dedication',
+    'License :: OSI Approved :: Apache Software License',
+    'License :: Other/Proprietary License',
+    'Operating System :: Microsoft :: Windows',
+    'Operating System :: POSIX :: Linux',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3.4',
+    'Topic :: Scientific/Engineering :: Physics',
+]
