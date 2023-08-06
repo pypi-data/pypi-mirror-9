325b007f368fd250401ffc3765ce187d773cb489
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:22 +0000

    Add package singularity-logfetch/0.14.0/singularity-logfetch-0.14.0.tar.gz

---
+++ packages/singularity-logfetch/singularity-logfetch-0.14.0.tar.gz/singularity-logfetch-0.14.0/logfetch/callbacks.py
@@ -0,0 +1,19 @@
+import sys
+from termcolor import colored
+
+CALLBACK_FORMAT = '{0}/{1}'
+
+def generate_callback(request, destination, filename, chunk_size, verbose):
+  path = CALLBACK_FORMAT.format(destination, filename) if destination else filename
+
+  def callback(response, **kwargs):
+    with open(path, 'wb') as f:
+      for chunk in response.iter_content(chunk_size):
+        f.write(chunk)
+      if verbose:
+        sys.stderr.write(colored('Downloaded ', 'green') + colored(path, 'white') + '\n')
+      else:
+        sys.stderr.write(colored('.', 'green'))
+
+  return callback
+
