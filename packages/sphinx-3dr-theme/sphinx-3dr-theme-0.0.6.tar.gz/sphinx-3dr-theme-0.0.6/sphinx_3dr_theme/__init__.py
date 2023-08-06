88a8f37b6ef7173db5c0dc34ad8a8c9de336a4d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:01 +0000

    Add package sphinx-3dr-theme/0.0.6/sphinx-3dr-theme-0.0.6.tar.gz

---
+++ packages/sphinx-3dr-theme/sphinx-3dr-theme-0.0.6.tar.gz/sphinx-3dr-theme-0.0.6/sphinx_3dr_theme/__init__.py
@@ -0,0 +1,11 @@
+import os
+
+VERSION = (0, 0, 6)
+
+__version__ = ".".join(str(v) for v in VERSION)
+__version_full__ = __version__
+
+def get_html_theme_path():
+    """Return list of HTML theme paths."""
+    cur_dir = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
+    return cur_dir
