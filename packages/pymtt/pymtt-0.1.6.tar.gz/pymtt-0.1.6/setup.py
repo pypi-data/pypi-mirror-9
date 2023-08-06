76a6fc454d6317ac41f2fdc3f78c083b7256adec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:22 +0000

    Add package pymtt/0.1.6/pymtt-0.1.6.tar.gz

---
+++ packages/pymtt/pymtt-0.1.6.tar.gz/pymtt-0.1.6/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(name='pymtt',
+      version='0.1.6',
+      description='Command-line Text Transformer',
+      long_description=open('README.rst').read(),
+      author='Pavel Klemenkov',
+      author_email='pklemenkov@gmail.com',
+      url='https://github.com/pklemenkov/pymtt',
+      download_url='https://github.com/pklemenkov/pymtt/tarball/master',
+      license='GPL',
+      packages=['pymtt', 'pymtt.filters'],
+      install_requires=[
+          'Jinja2 >=2.3'
+      ],
+      scripts=[
+          'bin/pymtt'
+      ]
+      )
