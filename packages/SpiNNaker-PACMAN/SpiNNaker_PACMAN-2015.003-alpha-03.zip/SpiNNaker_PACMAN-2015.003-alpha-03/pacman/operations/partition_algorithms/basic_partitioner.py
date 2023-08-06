2dbfd21ade641f4b2b55bb567bec12d1b577ebd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:02 +0000

    Add package SpiNNaker-PACMAN/2015.003-alpha-03/SpiNNaker_PACMAN-2015.003-alpha-03.zip

---
+++ packages/SpiNNaker-PACMAN/SpiNNaker_PACMAN-2015.003-alpha-03.zip/SpiNNaker_PACMAN-2015.003-alpha-03/pacman/operations/partition_algorithms/basic_partitioner.py
@@ -0,0 +1,132 @@
+from pacman.model.constraints.abstract_constraints.\
+    abstract_partitioner_constraint import \
+    AbstractPartitionerConstraint
+from pacman.model.graph_mapper.graph_mapper import \
+    GraphMapper
+from pacman.model.graph_mapper.slice import Slice
+from pacman.operations.abstract_algorithms.abstract_partition_algorithm\
+    import AbstractPartitionAlgorithm
+from pacman.model.partitioned_graph.partitioned_graph import PartitionedGraph
+from pacman.model.constraints.partitioner_constraints.\
+    partitioner_maximum_size_constraint \
+    import PartitionerMaximumSizeConstraint
+from pacman.utilities.progress_bar import ProgressBar
+from pacman.utilities import utility_calls
+from pacman.exceptions import PacmanPartitionException
+
+import logging
+from pacman.utilities.resource_tracker import ResourceTracker
+
+logger = logging.getLogger(__name__)
+
+
+class BasicPartitioner(AbstractPartitionAlgorithm):
+    """ An basic algorithm that can partition a partitionable_graph based
+        on the number of atoms in the vertices.
+    """
+
+    def __init__(self):
+        """
+        """
+        AbstractPartitionAlgorithm.__init__(self)
+        self._supported_constraints.append(PartitionerMaximumSizeConstraint)
+
+    @staticmethod
+    def _get_ratio(top, bottom):
+        if bottom == 0:
+            return 1.0
+        return top / bottom
+
+    # inherited from AbstractPartitionAlgorithm
+    def partition(self, graph, machine):
+        utility_calls.check_algorithm_can_support_constraints(
+            constrained_vertices=graph.vertices,
+            supported_constraints=self._supported_constraints,
+            abstract_constraint_type=AbstractPartitionerConstraint)
+
+        # start progress bar
+        progress_bar = ProgressBar(len(graph.vertices),
+                                   "on partitioning the partitionable_graph's"
+                                   " vertices")
+        vertices = graph.vertices
+        subgraph = PartitionedGraph(label="partitioned_graph for partitionable"
+                                          "_graph {}".format(graph.label))
+        graph_to_subgraph_mapper = GraphMapper(graph.label, subgraph.label)
+        resource_tracker = ResourceTracker(machine)
+
+        # Partition one vertex at a time
+        for vertex in vertices:
+
+            # Get the usage of the first atom, then assume that this
+            # will be the usage of all the atoms
+            requirements = vertex.get_resources_used_by_atoms(Slice(0, 1),
+                                                              graph)
+
+            # Locate the maximum resources available
+            max_resources_available = \
+                resource_tracker.get_maximum_constrained_resources_available(
+                    vertex.constraints)
+
+            # Find the ratio of each of the resources - if 0 is required,
+            # assume the ratio is the max available
+            atoms_per_sdram = self._get_ratio(
+                max_resources_available.sdram.get_value(),
+                requirements.sdram.get_value())
+            atoms_per_dtcm = self._get_ratio(
+                max_resources_available.dtcm.get_value(),
+                requirements.dtcm.get_value())
+            atoms_per_cpu = self._get_ratio(
+                max_resources_available.cpu.get_value(),
+                requirements.cpu.get_value())
+
+            max_atom_values = [atoms_per_sdram, atoms_per_dtcm, atoms_per_cpu]
+
+            max_atoms_constraints = utility_calls.locate_constraints_of_type(
+                vertex.constraints, PartitionerMaximumSizeConstraint)
+            for max_atom_constraint in max_atoms_constraints:
+                max_atom_values.append(max_atom_constraint.size)
+
+            atoms_per_core = min(max_atom_values)
+
+            # Partition into subvertices
+            counted = 0
+            while counted < vertex.n_atoms:
+
+                # Determine subvertex size
+                remaining = vertex.n_atoms - counted
+                if remaining > atoms_per_core:
+                    alloc = atoms_per_core
+                else:
+                    alloc = remaining
+
+                # Create and store new subvertex, and increment elements
+                #  counted
+                if counted < 0 or counted + alloc - 1 < 0:
+                    raise PacmanPartitionException("Not enough resources"
+                                                   " available to create"
+                                                   " subvertex")
+
+                vertex_slice = Slice(counted, counted + (alloc - 1))
+                subvertex_usage = vertex.get_resources_used_by_atoms(
+                    vertex_slice, graph)
+
+                subvert = vertex.create_subvertex(
+                    vertex_slice, subvertex_usage,
+                    "{}:{}:{}".format(vertex.label, counted, alloc - 1),
+                    self._get_remaining_constraints(vertex))
+                subgraph.add_subvertex(subvert)
+                graph_to_subgraph_mapper.add_subvertex(
+                    subvert, counted, counted + alloc - 1, vertex)
+                counted = counted + alloc
+
+                # update allocated resources
+                resource_tracker.allocate_constrained_resources(
+                    subvertex_usage, vertex.constraints)
+
+            # update and end progress bars as needed
+            progress_bar.update()
+        progress_bar.end()
+
+        self._generate_sub_edges(subgraph, graph_to_subgraph_mapper, graph)
+
+        return subgraph, graph_to_subgraph_mapper
---
+++ packages/SpiNNaker-PACMAN/SpiNNaker_PACMAN-2015.003-alpha-03.zip/SpiNNaker_PACMAN-2015.003-alpha-03/pacman/operations/placer_algorithms/radial_placer.py
@@ -0,0 +1,118 @@
+from pacman.model.constraints.placer_constraints\
+    .placer_chip_and_core_constraint import PlacerChipAndCoreConstraint
+from pacman.model.constraints.tag_allocator_constraints.\
+    tag_allocator_require_iptag_constraint \
+    import TagAllocatorRequireIptagConstraint
+from pacman.model.constraints.tag_allocator_constraints.\
+    tag_allocator_require_reverse_iptag_constraint \
+    import TagAllocatorRequireReverseIptagConstraint
+from pacman.model.constraints.placer_constraints.\
+    placer_radial_placement_from_chip_constraint \
+    import PlacerRadialPlacementFromChipConstraint
+from pacman.model.constraints.abstract_constraints.abstract_placer_constraint \
+    import AbstractPlacerConstraint
+from pacman.model.placements.placements import Placements
+from pacman.model.placements.placement import Placement
+from pacman.utilities import utility_calls
+from pacman.utilities.ordered_set import OrderedSet
+from pacman.utilities.resource_tracker import ResourceTracker
+from pacman.utilities.progress_bar import ProgressBar
+from pacman.exceptions import PacmanPlaceException
+from pacman.operations.abstract_algorithms.abstract_placer_algorithm \
+    import AbstractPlacerAlgorithm
+
+from collections import deque
+import logging
+logger = logging.getLogger(__name__)
+
+
+class RadialPlacer(AbstractPlacerAlgorithm):
+    """ A placement algorithm that can place a partitioned graph onto a
+        machine choosing chips radiating in a circle from 0, 0
+    """
+
+    def __init__(self):
+        """
+        """
+        AbstractPlacerAlgorithm.__init__(self)
+        self._supported_constraints.append(
+            PlacerRadialPlacementFromChipConstraint)
+        self._supported_constraints.append(
+            TagAllocatorRequireIptagConstraint)
+        self._supported_constraints.append(
+            TagAllocatorRequireReverseIptagConstraint)
+        self._supported_constraints.append(PlacerChipAndCoreConstraint)
+
+    def place(self, partitioned_graph, machine):
+
+        # check that the algorithm can handle the constraints
+        utility_calls.check_algorithm_can_support_constraints(
+            constrained_vertices=partitioned_graph.subvertices,
+            supported_constraints=self._supported_constraints,
+            abstract_constraint_type=AbstractPlacerConstraint)
+
+        placements = Placements()
+        ordered_subverts = utility_calls.sort_objects_by_constraint_authority(
+            partitioned_graph.subvertices)
+
+        # Iterate over subvertices and generate placements
+        progress_bar = ProgressBar(len(ordered_subverts),
+                                   "for placing the partitioned_graphs "
+                                   "subvertices")
+        resource_tracker = ResourceTracker(
+            machine, self._generate_radial_chips(machine))
+        for vertex in ordered_subverts:
+            self._place_vertex(vertex, resource_tracker, machine, placements)
+            progress_bar.update()
+        progress_bar.end()
+        return placements
+
+    def _place_vertex(self, vertex, resource_tracker, machine, placements):
+
+        # Check for the radial placement constraint
+        radial_constraints = utility_calls.locate_constraints_of_type(
+            [vertex], PlacerRadialPlacementFromChipConstraint)
+        start_x = 0
+        start_y = 0
+        for constraint in radial_constraints:
+            if start_x is None:
+                start_x = constraint.x
+            elif start_x != constraint.x:
+                raise PacmanPlaceException("Non-matching constraints")
+            if start_y is None:
+                start_y = constraint.y
+            elif start_y != constraint.y:
+                raise PacmanPlaceException("Non-matching constraints")
+        chips = None
+        if start_x is not None and start_y is not None:
+            chips = self._generate_radial_chips(machine, resource_tracker,
+                                                start_x, start_y)
+
+        # Create and store a new placement
+        (x, y, p, _, _) = resource_tracker.allocate_constrained_resources(
+            vertex.resources_required, vertex.constraints, chips)
+        placement = Placement(vertex, x, y, p)
+        placements.add_placement(placement)
+
+    def _generate_radial_chips(self, machine, resource_tracker=None,
+                               start_chip_x=0, start_chip_y=0):
+        first_chip = machine.get_chip_at(start_chip_x, start_chip_y)
+        done_chips = set()
+        found_chips = OrderedSet()
+        search = deque([first_chip])
+        while len(search) > 0:
+            chip = search.pop()
+            if (resource_tracker is None or
+                    resource_tracker.is_chip_available(chip.x, chip.y)):
+                found_chips.add((chip.x, chip.y))
+            done_chips.add(chip)
+
+            # Examine the links of the chip to find the next chips
+            for link in chip.router.links:
+                next_chip = machine.get_chip_at(link.destination_x,
+                                                link.destination_y)
+
+                # Don't search found chips again
+                if next_chip not in done_chips:
+                    search.appendleft(next_chip)
+        return found_chips
