d22973f8df2b204c196dec7d50b378c16c0b62df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:24 +0000

    Add package benchpress/0.1/benchpress-0.1.linux-x86_64.tar.gz

---
+++ packages/benchpress/benchpress-0.1.linux-x86_64.tar.gz/usr/local/share/benchpress/suites/array14.py
@@ -0,0 +1,33 @@
+from default import *
+
+scripts = [
+    ('Convolve 2D', 'convolve_2d',  '--size=25'),
+    ('Convolve 3D', 'convolve_3d',  '--size=5'),
+    ('Wireworld',   'wireworld',    '--size=1000*10'),
+    ('27-Point',    'point27',      '--size=400*10'),
+    ('Jacobi',      'jacobi',       '--size=1250'),
+    ('Jacobi Fixed','jacobi_fixed', '--size=1250*600')
+]
+
+managers= [('node', 'node', '', None)]
+
+numpy = {
+    'bridges':  [('NumPy', 'python benchmark/python/{script}.py {args} --bohrium=False', None)],
+    'scripts':  scripts,
+}
+
+bohrium = {
+    'bridges':  [('Bohrium',    'python benchmark/python/{script}.py {args} --bohrium=True', None)],
+    'managers': [('node',       'node', '',  None)],
+    'engines':  [
+        #('cpu_fused',   'cpu',  {"BH_VE_CPU_JIT_DUMPSRC": "1", "BH_VE_CPU_JIT_FUSION": "1"}),
+        ('cpu_sij',     'cpu',  {"BH_VE_CPU_JIT_DUMPSRC": "1", "BH_VE_CPU_JIT_FUSION": "0"}),
+    ],
+    'scripts':  scripts
+}
+
+suites = [
+    bohrium,
+    numpy
+]
+
