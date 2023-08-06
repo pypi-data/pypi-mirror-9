f5dd7e01efc5909b3cf35dbf6412a1c7532a2f7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:16 +0000

    Add package pycrunchbase/0.2.5/pycrunchbase-0.2.5.tar.gz

---
+++ packages/pycrunchbase/pycrunchbase-0.2.5.tar.gz/pycrunchbase-0.2.5/src/pycrunchbase/resource/__init__.py
@@ -0,0 +1,23 @@
+from .acquisition import Acquisition
+from .fundinground import FundingRound
+from .fundraise import FundRaise
+from .ipo import IPO
+from .organization import Organization
+from .page import Page
+from .pageitem import PageItem
+from .person import Person
+from .product import Product
+from .relationship import Relationship
+
+__all__ = [
+    'Acquisition',
+    'FundingRound',
+    'FundRaise',
+    'IPO',
+    'Organization',
+    'Page',
+    'PageItem',
+    'Person',
+    'Product',
+    'Relationship',
+]
