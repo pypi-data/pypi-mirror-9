27ba271c18db4d189a662330e7f45315bc34fd1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:42 +0000

    Add package HelpGen4Android/0.3.1/HelpGen4Android-0.3.1.tar.gz

---
+++ packages/HelpGen4Android/HelpGen4Android-0.3.1.tar.gz/HelpGen4Android-0.3.1/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+setup(name='HelpGen4Android',
+		version='0.3.1',
+		url='https://github.com/Eagles2F/HelpGen4Android',
+		author='Evan Lee',
+		author_email='thueeliyifan@gmail.com',
+		license = 'MIT',
+		packages = ['HelpGen4Android'],
+		desciption = 'A simple python tool for maintaining android app help files from mediawiki',
+		classifiers = ['Development Status :: 3 - Alpha','Intended Audience :: Developers',
+			'Topic :: Software Development :: Documentation', 'License :: OSI Approved :: MIT License',
+			'Programming Language :: Python :: 2.7',],
+		keywords = 'mobile app development',
+		install_requires = ['bs4']
+		)
