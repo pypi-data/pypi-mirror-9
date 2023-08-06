0db4a8b9e14af5eea2948a5815c1534dc1bc1b0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:36:27 +0000

    Add package django-render-csv/1.0.4/django-render-csv-1.0.4.tar.gz

---
+++ packages/django-render-csv/django-render-csv-1.0.4.tar.gz/django-render-csv-1.0.4/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup	
+
+setup(
+	name='django-render-csv',
+	version='1.0.4',
+	description='utilities to render csv in django',
+	author='yangchen',
+	author_email='yuhan534@126.com',
+	url='https://github.com/limijiaoyin/django-render-csv',
+	install_requires=['django'],
+	py_modules=['django_render_csv'],
+	scripts=[],
+	keywords=["django", "response", "csv", "utilities"]
+)
+
