18f0e23110273116c86b8bfb38be070845f14801
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:11 +0000

    Add package QtPy/0.1.2/QtPy-0.1.2.tar.gz

---
+++ packages/QtPy/QtPy-0.1.2.tar.gz/QtPy-0.1.2/qtpy/QtNetwork.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright © 2014-2015 Colin Duquesnoy
+# Copyright © 2011 Pierre Raybaut
+#
+# Licensed under the terms of the MIT License
+# (see LICENSE.txt for details)
+
+"""
+Provides QtNetwork classes and functions.
+"""
+
+import os
+from qtpy import QT_API
+from qtpy import PYQT5_API
+from qtpy import PYQT4_API
+from qtpy import PYSIDE_API
+from qtpy import PythonQtError
+
+
+if os.environ[QT_API] in PYQT5_API:
+    from PyQt5.QtNetwork import *                             # analysis:ignore
+elif os.environ[QT_API] in PYQT4_API:
+    from PyQt4.QtNetwork import *                             # analysis:ignore
+elif os.environ[QT_API] in PYSIDE_API:
+    from PySide.QtNetwork import *                            # analysis:ignore
+else:
+    raise PythonQtError('No Qt bindings could be found')
+
+
