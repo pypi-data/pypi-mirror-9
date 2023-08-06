54fd908714bca6fd9d4f1465fff836451839af01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:50 +0000

    Add package OpenAddresses-Machine/1.6.3/OpenAddresses-Machine-1.6.3.tar.gz

---
+++ packages/OpenAddresses-Machine/OpenAddresses-Machine-1.6.3.tar.gz/OpenAddresses-Machine-1.6.3/openaddr/paths.py
@@ -0,0 +1,5 @@
+cache = '/var/opt/openaddresses-cache'
+conform = '/var/opt/openaddresses-conform'
+sources = '/var/opt/openaddresses/sources'
+
+sources = '/Users/migurski/Sites/OpenAddresses/sources'
