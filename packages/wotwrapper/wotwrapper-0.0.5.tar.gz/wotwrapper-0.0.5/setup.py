037bfcf130a572dd85027b0ecebb280cff9fb28f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:07 +0000

    Add package wotwrapper/0.0.5/wotwrapper-0.0.5.tar.gz

---
+++ packages/wotwrapper/wotwrapper-0.0.5.tar.gz/wotwrapper-0.0.5/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+	name='wotwrapper',
+	version='0.0.5',
+	description='A library to wrap a python object onto the Wotio Operating Enviroment',
+	url='http://github.com/wotio/wotwrapper',
+	author='Andrew Khoury',
+	author_email='drew@wot.io',
+	license='MIT',
+	install_requires=[
+		'websocket-client',
+		'requests'
+	],
+	packages=['wotwrapper'],
+	zip_safe=False
+)
