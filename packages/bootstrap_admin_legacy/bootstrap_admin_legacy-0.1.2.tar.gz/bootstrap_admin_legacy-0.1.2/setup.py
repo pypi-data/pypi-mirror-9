59f09df05548a3e043da6a8803311a4c8c43618b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:03 +0000

    Add package bootstrap_admin_legacy/0.1.2/bootstrap_admin_legacy-0.1.2.tar.gz

---
+++ packages/bootstrap_admin_legacy/bootstrap_admin_legacy-0.1.2.tar.gz/bootstrap_admin_legacy-0.1.2/setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+import os
+from setuptools import setup, find_packages
+
+f = open(os.path.join(os.path.dirname(__file__), 'README.rst'))
+readme = f.read()
+f.close()
+
+setup(
+    name='bootstrap_admin_legacy',
+    version='0.1.2',
+    description='Bootstrap theme for Django admin',
+    long_description=readme,
+    author='Douglas Miranda',
+    author_email='douglasmirandasilva@gmail.com',
+    url='https://github.com/douglasmiranda/django-admin-bootstrap',
+    license='BSD',
+    maintainer = 'alrusdi',
+    maintainer_email = 'alrusdi@gmail.com',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
+    keywords='django,admin,skin,theme,bootstrap,responsive',
+)
