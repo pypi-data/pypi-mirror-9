28a44b3da36857e02d43407e84b4fabb8bc67617
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:58 +0000

    Add package ban_commons/0.0.1/ban_commons-0.0.1.tar.gz

---
+++ packages/ban_commons/ban_commons-0.0.1.tar.gz/ban_commons-0.0.1/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+        name = 'ban_commons',
+        version = '0.0.1',
+        keywords = ('ban', 'commons'),
+        description = 'eking commons',
+        license = 'MIT License',
+
+        url = 'http://liluo.org',
+        author = 'zhaowenlei',
+        author_email = 'zhaowenlei789@sina.com',
+
+        packages = find_packages(),
+        include_package_data = True,
+        platforms = 'any',
+        install_requires = [],
+)
