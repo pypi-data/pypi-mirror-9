61b7560557cd137f2ceba14ff3ccb2d51578cadc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:29 +0000

    Add package chanjo-report/0.0.7/chanjo_report-0.0.7-py2.py3-none-any.whl

---
+++ packages/chanjo-report/chanjo_report-0.0.7-py2.py3-none-any.whl/chanjo_report/__init__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+chanjo_report
+~~~~~~~~~~~~~
+
+Chanjo Report automatically renders a coverage report from Chanjo ouput.
+
+:copyright: (c) 2014 by Robin Andeer
+:licence: MIT, see LICENCE for more details
+"""
+from __future__ import absolute_import, unicode_literals
+
+# generate your own AsciiArt at:
+# patorjk.com/software/taag/#f=Calvin%20S&t=Chanjo Report
+__banner__ = r"""
+╔═╗┬ ┬┌─┐┌┐┌ ┬┌─┐  ╦═╗┌─┐┌─┐┌─┐┬─┐┌┬┐
+║  ├─┤├─┤│││ ││ │  ╠╦╝├┤ ├─┘│ │├┬┘ │   by Robin Andeer
+╚═╝┴ ┴┴ ┴┘└┘└┘└─┘  ╩╚═└─┘┴  └─┘┴└─ ┴
+"""
+
+__title__ = 'chanjo-report'
+__summary__ = 'Chanjo Report automatically renders coverage reports from Chanjo ouput.'
+__uri__ = 'https://github.com/robinandeer/chanjo-report'
+
+__version__ = '0.0.7'
+
+__author__ = 'Robin Andeer'
+__email__ = 'robin.andeer@gmail.com'
+
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2014 Robin Andeer'
