a30ac9029a83bd03c2ed6a9f19ca1dfea479442c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:20:48 +0000

    Add package bu-cascade/1.1/bu_cascade-1.1.tar.gz

---
+++ packages/bu-cascade/bu_cascade-1.1.tar.gz/bu_cascade-1.1/bu_cascade/assets/page.py
@@ -0,0 +1,10 @@
+__author__ = 'ejc84332'
+
+import asset
+
+
+class Page(asset.Asset):
+
+    def __init__(self, ws_connector):
+        super(self.__class__, self).__init__(ws_connector)
+        self.asset_type = "page"
