a3db6f793b0669186c3ac343d4a5073664d9b746
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:07:39 +0000

    Add package dnatools/0.1/dnatools-0.1.tar.gz

---
+++ packages/dnatools/dnatools-0.1.tar.gz/dnatools-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='dnatools',
+            version='0.1',
+            description='Functions for working with DNA sequences',
+            url='http://pythonforbiologists.com',
+            author='Martin Jones',
+            author_email='martin@pythonforbiologists.com',
+            license='MIT',
+            packages=['dnatools']
+      )
