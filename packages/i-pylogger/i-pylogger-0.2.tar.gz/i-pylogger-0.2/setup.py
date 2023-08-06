66a3f073947fba7f4203cd762b6016338b2e84f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:25 +0000

    Add package i-pylogger/0.2/i-pylogger-0.2.tar.gz

---
+++ packages/i-pylogger/i-pylogger-0.2.tar.gz/i-pylogger-0.2/setup.py
@@ -0,0 +1,14 @@
+import distutils.core
+
+version = '0.2'
+
+distutils.core.setup(
+        name='i-pylogger',
+        version=version,
+        packages=['pylogger'],
+        author='Justin',
+        author_email='justinli.ljt@gmail.com',
+        url='https://github.com/jizhouli/py-logger',
+        license='http://opensource.org/licenses/MIT',
+        description='a easy-to-use logging wrapper for Python'
+        )
