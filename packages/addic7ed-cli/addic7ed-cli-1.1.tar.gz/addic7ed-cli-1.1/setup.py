2bd8d6ee31faff40bf7be1a875932e4c31501886
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:37 +0000

    Add package addic7ed-cli/1.1/addic7ed-cli-1.1.tar.gz

---
+++ packages/addic7ed-cli/addic7ed-cli-1.1.tar.gz/addic7ed-cli-1.1/setup.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+setup(name='addic7ed-cli',
+      version='1.1',
+      description='A commandline access to addic7ed subtitles',
+      long_description=readme(),
+      url='https://github.com/BenoitZugmeyer/addic7ed-cli/',
+      author='Benoît Zugmeyer',
+      author_email='bzugmeyer@gmail.com',
+      license='Expat',
+      packages=['addic7ed_cli'],
+      install_requires=[
+          'pyquery>=1.2.4',
+          'requests>=2.2.1',
+      ],
+      entry_points={
+          'console_scripts': ['addic7ed=addic7ed_cli.__init__:main'],
+      },
+      test_suite='nose.collector',
+      tests_require=['nose'],
+      zip_safe=True,
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Environment :: Console',
+          'Intended Audience :: End Users/Desktop',
+          'License :: OSI Approved :: MIT License',
+          'Operating System :: MacOS :: MacOS X',
+          'Operating System :: POSIX',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2.6',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3.3',
+          'Programming Language :: Python :: 3.4',
+          'Topic :: Multimedia',
+      ],
+      )
