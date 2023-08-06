24cb89491337a33ee4e1161a8770b616b2a767f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:09 +0000

    Add package plone.app.customerize/1.3.2/plone.app.customerize-1.3.2.zip

---
+++ packages/plone.app.customerize/plone.app.customerize-1.3.2.zip/plone.app.customerize-1.3.2/README.rst
@@ -0,0 +1,11 @@
+Overview
+========
+
+This package integrates five.customerize_ into Plone, which enables site
+administrators to customize templates used for Five/Zope-style views,
+viewlets and portlets through the web via the ZMI in a way similar to
+overriding filesystem-based skin elements via the portal skin "customize"
+procedure.
+
+  .. _five.customerize: http://svn.zope.org/five.customerize/
+
---
+++ packages/plone.app.customerize/plone.app.customerize-1.3.2.zip/plone.app.customerize-1.3.2/setup.py
@@ -0,0 +1,52 @@
+from setuptools import setup, find_packages
+
+version = '1.3.2'
+
+setup(name = 'plone.app.customerize',
+      version = version,
+      description = 'Integrate five.customerize into Plone.',
+      long_description = open("README.rst").read() + '\n' +
+                         open('CHANGES.rst').read(),
+      classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Plone',
+        'Framework :: Zope2',
+        'Intended Audience :: Other Audience',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Site Management',
+      ],
+      keywords='customerize plone views page templates zmi',
+      author='Plone Foundation',
+      author_email='plone-developers@lists.sourceforge.net',
+      url='http://pypi.python.org/pypi/plone.app.customerize/',
+      license='GPL version 2',
+      packages = find_packages(),
+      namespace_packages = ['plone', 'plone.app'],
+      include_package_data = True,
+      extras_require=dict(
+          test=[
+            'plone.app.layout',
+            'plone.app.testing',
+            'zope.testing',
+          ]
+      ),
+      install_requires = [
+        'setuptools',
+        'five.customerize',
+        'plone.browserlayer',
+        'plone.portlets',
+        'zope.component',
+        'zope.interface',
+        'zope.publisher',
+        'zope.viewlet',
+        'Products.CMFCore',
+        'Acquisition',
+        'Zope2',
+      ],
+      platforms = 'Any',
+      zip_safe = False,
+)
