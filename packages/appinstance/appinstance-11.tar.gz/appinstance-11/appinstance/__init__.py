d9daf920d62a35eabf5b1cc315e5ef4c8e9159ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:55 +0000

    Add package appinstance/11/appinstance-11.tar.gz

---
+++ packages/appinstance/appinstance-11.tar.gz/appinstance-11/appinstance/__init__.py
@@ -0,0 +1,8 @@
+# coding=utf-8
+"""
+appinstance
+-
+Active8 (04-03-15)
+author: erik@a8.nl
+"""
+from appinstance import *
---
+++ packages/appinstance/appinstance-11.tar.gz/appinstance-11/setup.py
@@ -0,0 +1,17 @@
+# coding=utf-8
+"""
+appinstance
+Active8 (04-03-15)
+license: GNU-GPL2
+"""
+from setuptools import setup
+setup(name='appinstance',
+      version='11',
+      description='Check if an app with the same name is running, supports parameters.',
+      url='https://github.com/erikdejonge/appinstance',
+      author='Erik de Jonge',
+      author_email='erik@a8.nl',
+      license='GPL',
+      packages=['appinstance'],
+      zip_safe=True,
+      install_requires=['psutil'])
