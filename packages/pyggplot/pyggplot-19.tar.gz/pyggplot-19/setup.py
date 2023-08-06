1ce1c6b899dc93ec56b23d7a8c624868da7546d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:39 +0000

    Add package pyggplot/19/pyggplot-19.tar.gz

---
+++ packages/pyggplot/pyggplot-19.tar.gz/pyggplot-19/setup.py
@@ -0,0 +1,28 @@
+try:
+    from setuptools import setup, find_packages
+except:
+    from distutils.core import setup
+import os
+
+long_description = "Please visit https://github.com/TyberiusPrime/pyggplot for full description"
+if os.path.exists('README.txt'):
+    with open('README.txt') as op:
+        long_description = op.read()
+
+setup(
+    name='pyggplot',
+    version='19',
+    packages=['pyggplot',],
+    license='BSD',
+    #url='http://code.google.com/p/pydataframe/',
+    author='Florian Finkernagel',
+    description = "A pythonic wrapper around R's ggplot",
+    author_email='finkernagel@coonabibba.de',
+    long_description=open('README.txt').read(),
+    package_data={'pyggplot': ['LICENSE.txt', 'README.txt']},
+    include_package_data=True,    install_requires=[
+        'pandas>=0.15',
+        'rpy2',
+        'ordereddict',
+        ]
+)
