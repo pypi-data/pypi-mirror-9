70f17d359e3394ce1ef920dc46cfbfb14c0f00ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:13 +0000

    Add package pybaco/0.2/pybaco-0.2.tar.gz

---
+++ packages/pybaco/pybaco-0.2.tar.gz/pybaco-0.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name='pybaco',
+    description='Python Base Converter',
+    author='Marco Rougeth',
+    author_email='marco@rougeth.com',
+    url='http://github.com/rougeh/pybaco',
+    version='0.2',
+    py_modules=['baco'],
+)
