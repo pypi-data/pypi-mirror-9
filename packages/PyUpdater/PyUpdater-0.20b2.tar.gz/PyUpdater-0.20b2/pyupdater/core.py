c9a0db1d600e75ab908d7caf5d3beff0abe2d9fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:07 +0000

    Add package PyUpdater/0.20b2/PyUpdater-0.20b2.tar.gz

---
+++ packages/PyUpdater/PyUpdater-0.20b2.tar.gz/PyUpdater-0.20b2/pyupdater/core.py
@@ -0,0 +1,108 @@
+# --------------------------------------------------------------------------
+# Copyright 2014 Digital Sapphire Development Team
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# --------------------------------------------------------------------------
+import os
+
+from pyupdater.config import PyiUpdaterConfig
+from pyupdater.key_handler import KeyHandler
+from pyupdater.package_handler import PackageHandler
+from pyupdater.storage import Storage
+from pyupdater.uploader import Uploader
+
+
+class Core(object):
+    """Processes, signs & uploads updates
+
+        Kwargs:
+
+            config (obj): config object
+    """
+    def __init__(self, config=None, db=None):
+        self.config = PyiUpdaterConfig()
+        # Important to keep this before updating config
+        if config is not None:
+            self.update_config(config, db)
+
+    def update_config(self, config, db):
+        u"""Updates internal config
+
+        Args:
+
+            config (obj): config object
+        """
+        if not hasattr(config, u'DATA_DIR'):
+            config.DATA_DIR = None
+        if config.DATA_DIR is None:
+            config.DATA_DIR = os.getcwd()
+
+        if db is None:
+            self.db = Storage(config.DATA_DIR)
+        else:
+            self.db = db
+        self.config.update_config(config)
+        self._update(self.config, self.db)
+
+    def _update(self, config, db):
+        self.kh = KeyHandler(config, db)
+        self.ph = PackageHandler(config, db)
+        self.up = Uploader(config)
+
+    def setup(self):
+        u"""Sets up root dir with required PyiUpdater folders
+        """
+        self.ph.setup()
+
+    def process_packages(self):
+        u"""Creates hash for updates & adds information about update to
+        version file
+        """
+        self.ph.process_packages()
+
+    def set_uploader(self, requested_uploader):
+        u"""Sets upload destination
+
+        Args:
+
+            requested_uploader (str): upload service. i.e. s3, scp
+        """
+        self.up.set_uploader(requested_uploader)
+
+    def upload(self):
+        u"""Uploads files in deploy folder
+        """
+        self.up.upload()
+
+    def make_keys(self, count=3):
+        u"""Creates signing keys
+        """
+        self.kh.make_keys(count)
+
+    def revoke_key(self, count):
+        self.kh.revoke_key(count)
+
+    def get_recent_revoked_key(self):
+        return self.kh.get_recent_revoked_key()
+
+    def sign_update(self):
+        u"Signs version file with signing key"
+        self.kh.sign_update()
+
+    def get_public_keys(self):
+        u"Returns public key"
+        return self.kh.get_public_keys()
+
+    def print_public_key(self):
+        u"Prints public key to console"
+        self.kh.print_public_key()
---
+++ packages/PyUpdater/PyUpdater-0.20b2.tar.gz/PyUpdater-0.20b2/setup.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python
+
+from setuptools import Command, find_packages, setup
+import subprocess
+import sys
+
+import versioneer
+
+versioneer.VCS = 'git'
+versioneer.versionfile_source = 'pyupdater/_version.py'
+versioneer.versionfile_build = 'pyupdater/_version.py'
+versioneer.tag_prefix = ''  # tags are like 1.2.0
+versioneer.parentdir_prefix = 'PyUpdater-'  # dirname like 'myproject-1.2.0'
+
+
+class PyTest(Command):
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        errno = subprocess.call([sys.executable, u'runtests.py', u'-v', u'-x',
+                                u'--cov', u'pyupdater', u'-n', u'4'])
+        raise SystemExit(errno)
+
+
+class PyTestCover(Command):
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        errno = subprocess.call([sys.executable, u'runtests.py', u'tests',
+                                 u'--cov', u'pyupdater', u'-n', u'1'])
+        raise SystemExit(errno)
+
+
+class PyTestMyCover(Command):
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        errno = subprocess.call([sys.executable, u'runtests.py', u'tests',
+                                 u'--cov-report', u'html', u'--cov',
+                                 u'pyupdater', u'-n', u'4'])
+        raise SystemExit(errno)
+
+with open(u'requirements.txt', u'r') as f:
+    required = f.read().splitlines()
+
+cmd_class = versioneer.get_cmdclass()
+cmd_class.update({u'test': PyTest,
+                  u'ctest': PyTestCover,
+                  u'mytest': PyTestMyCover})
+
+extra_s3 = 'PyUpdater-s3-Plugin == 2.0.4'
+extra_scp = 'PyUpdater-scp-Plugin == 2.0.2'
+extra_patch = 'bsdiff4 == 1.1.4'
+
+setup(
+    name='PyUpdater',
+    version=versioneer.get_version(),
+    description='Simple App update framwork',
+    author='Johny Mo Swag',
+    author_email='johnymoswag@gmail.com',
+    url='docs.pyupdater.org',
+    download_url=('https://github.com/JohnyMoSwag/Py'
+                  'Updater/archive/master.zip'),
+    license='Apache License 2.0',
+    extras_require={
+        's3': extra_s3,
+        'scp': extra_scp,
+        'patch': extra_patch,
+        'all': [extra_s3, extra_scp, extra_patch]
+        },
+    tests_require=['pytest', ],
+    cmdclass=cmd_class,
+    install_requires=required,
+    packages=find_packages(),
+    entry_points="""
+    [console_scripts]
+    pyupdater=pyupdater.wrapper:main
+    """,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Operating System :: OS Independent',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 2.7'],
+    )
