9636d4652a09bf4082931867704ee094d51c0b55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:42 +0000

    Add package pyenty/0.1.1/pyenty-0.1.1.zip

---
+++ packages/pyenty/pyenty-0.1.1.zip/pyenty-0.1.1/setup.py
@@ -0,0 +1,20 @@
+import os
+from distutils.core import setup
+
+version = '0.1.1'
+
+setup(
+    name="pyenty",
+    packages=['pyenty'],
+    version=version,
+    keywords=["mongo", "mongodb", "entity", "motor", "tornado"],
+    description="An simple object-document mapping for mongodb-motor and tornado applications.",
+    author="Leonardo Souza",
+    author_email="leo.desouza@gmail.com",
+    url="https://github.com/leodesouza/pyenty",
+    download_url="https://github.com/leodesouza/pyenty/tree/v0.1.1",
+    classifiers=[
+        "License :: OSI Approved :: Apache Software License",
+    ],
+    license='http://www.apache.org/licenses/LICENSE-2.0',
+)
