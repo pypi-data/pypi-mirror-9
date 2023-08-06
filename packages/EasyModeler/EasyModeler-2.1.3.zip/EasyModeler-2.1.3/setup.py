bcf70fce215f722df2129e9ab9c69717003adab8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:14 +0000

    Add package EasyModeler/2.1.3/EasyModeler-2.1.3.zip

---
+++ packages/EasyModeler/EasyModeler-2.1.3.zip/EasyModeler-2.1.3/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='EasyModeler',
+      version='2.1.3',
+      description='Simple ODE Tools for Modelers',
+      url='https://bitbucket.org/evanleeturner/easymodel',
+      author='Evan L Turner',
+      author_email='evanlee.turner@gmail.com',
+      license='BSD',
+      packages=['emlib'],
+      zip_safe=False)
