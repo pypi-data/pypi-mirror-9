cc4df4628f6f08f6ee8bb27b4260d9ff04c1c250
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:44 +0000

    Add package seantis.agencies/0.6.2/seantis.agencies-0.6.2.tar.gz

---
+++ packages/seantis.agencies/seantis.agencies-0.6.2.tar.gz/seantis.agencies-0.6.2/seantis/agencies/browser/organization.py
@@ -0,0 +1,53 @@
+from five import grok
+
+from plone.folder.interfaces import IExplicitOrdering
+from plone.uuid.interfaces import IUUID
+from zope.component import queryUtility
+from zope.event import notify
+from zope.interface import implements
+
+from seantis.agencies.browser.base import BaseView
+from seantis.agencies.interfaces import IActivityEvent
+from seantis.agencies.types import IOrganization
+from seantis.plonetools import tools
+
+
+class ResourceViewedEvent(object):
+    implements(IActivityEvent)
+
+
+class OrganizationView(BaseView):
+
+    grok.require('zope2.View')
+    grok.context(IOrganization)
+    grok.name('view')
+
+    template = grok.PageTemplateFile('templates/organization.pt')
+    event_fired = False
+
+    def suborganizations(self):
+        return [
+            (brain["Title"] if "Title" in brain else "", brain.getURL())
+            for brain in self.context.suborganizations()
+        ]
+
+    def memberships(self):
+        memberships = []
+        for brain in self.context.memberships():
+            obj = brain.getObject()
+            person = obj.person.to_object
+            name = person.title if person else u''
+            memberships.append((obj.role, name, obj.prefix, brain.getURL()))
+
+        if self.context.display_alphabetically:
+            sortkey = lambda m: tools.unicode_collate_sortkey()(m[1])
+            memberships = sorted(memberships, key=sortkey)
+
+        return memberships
+
+    def update(self, **kwargs):
+        super(OrganizationView, self).update(**kwargs)
+
+        if not self.event_fired:
+            notify(ResourceViewedEvent())
+            self.event_fired = True
---
+++ packages/seantis.agencies/seantis.agencies-0.6.2.tar.gz/seantis.agencies-0.6.2/seantis/agencies/types/__init__.py
@@ -0,0 +1,110 @@
+import logging
+log = logging.getLogger('seantis.agencies')
+
+from datetime import date
+from five import grok
+
+from zope import schema
+from zope.interface import Interface, Invalid
+
+from plone import api
+from plone.app.z3cform.wysiwyg import WysiwygFieldWidget
+from plone.dexterity.content import Container
+from plone.directives import form
+from plone.app.dexterity.behaviors.exclfromnav import IExcludeFromNavigation
+
+from collective.dexteritytextindexer import searchable
+
+from seantis.people.types.base import PersonBase
+from seantis.people.interfaces import IMembership as IPeopleMembership
+from seantis.people.content import Membership as PeopleMembership
+from seantis.agencies import _
+
+
+class IMember(form.Schema):
+    form.model("member.xml")
+
+
+class Member(PersonBase):
+    pass
+
+
+@form.default_value(field=IExcludeFromNavigation['exclude_from_nav'])
+def excludeFromNavDefaultValue(data):
+    return True
+
+
+class IOrganization(form.Schema):
+
+    searchable('title')
+    title = schema.TextLine(
+        title=_(u'Title')
+    )
+
+    searchable('description')
+    description = schema.Text(
+        title=_(u'Description'),
+        required=False
+    )
+
+    searchable('portrait')
+    form.widget(portrait=WysiwygFieldWidget)
+    portrait = schema.Text(
+        title=_(u'Portrait'),
+        required=False
+    )
+
+    display_alphabetically = schema.Bool(
+        title=_(u'Display memberships alphabetically'),
+        description=_(
+            u'Tick this box to sort memberships alphabetically by name rather '
+            u'than by their position in the folder.'
+        ),
+        default=False
+    )
+
+
+class Organization(Container):
+
+    pdfexportable = True
+
+    def memberships(self):
+        catalog = api.portal.get_tool('portal_catalog')
+        folder_path = '/'.join(self.getPhysicalPath())
+
+        memberships = catalog(
+            path={'query': folder_path, 'depth': 1},
+            portal_type='seantis.agencies.membership',
+            sort_on='getObjPositionInParent',
+        )
+
+        return memberships
+
+    def suborganizations(self):
+        catalog = api.portal.get_tool('portal_catalog')
+        folder_path = '/'.join(self.getPhysicalPath())
+
+        suborganizations = catalog(
+            path={'query': folder_path, 'depth': 1},
+            portal_type='seantis.agencies.organization',
+            sort_on='getObjPositionInParent',
+        )
+
+        return suborganizations
+
+
+class IMembership(IPeopleMembership):
+
+    start = schema.TextLine(
+        title=_(u"Start of membership"),
+        required=False
+    )
+
+    prefix = schema.TextLine(
+        title=_(u"Prefix"),
+        required=False
+    )
+
+
+class Membership(PeopleMembership):
+    pass
