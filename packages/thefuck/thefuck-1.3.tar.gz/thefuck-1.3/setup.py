6865c3355952b44678ea2489ce2f0176ef86d297
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:46 +0000

    Add package thefuck/1.3/thefuck-1.3.tar.gz

---
+++ packages/thefuck/thefuck-1.3.tar.gz/thefuck-1.3/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+
+setup(name='thefuck',
+      version=1.3,
+      description="Magnificent app which corrects your previous console command",
+      author='Vladimir Iakovlev',
+      author_email='nvbn.rm@gmail.com',
+      url='https://github.com/nvbn/thefuck',
+      license='MIT',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      entry_points={'console_scripts': [
+          'thefuck = thefuck.main:main']})
