f9fc072a714ce189331cbb9ecd01bd1dcfb07c1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:45 +0000

    Add package Plone/5.0b1/Plone-5.0b1.zip

---
+++ packages/Plone/Plone-5.0b1.zip/Plone-5.0b1/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup
+import os.path
+
+version = '5.0b1'
+
+setup(name='Plone',
+      version=version,
+      description='The Plone Content Management System',
+      long_description=open('README.rst').read() + '\n' +
+                       open(os.path.join('docs', 'CHANGES.rst')).read(),
+      classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Plone',
+        'Framework :: Zope2',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        ],
+      keywords='Plone CMF python Zope',
+      author='Plone Foundation',
+      author_email='plone-developers@lists.sourceforge.net',
+      url='http://plone.org/',
+      license='GPL version 2',
+      packages=[],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'Products.Archetypes',
+          'Products.ATContentTypes >= 2.1.3',
+          'Products.CMFPlacefulWorkflow',
+          'Products.CMFPlone',
+          'plone.app.caching',
+          'plone.app.dexterity',
+          'plone.app.iterate',
+          'plone.app.openid',
+          'plone.app.upgrade',
+      ],
+      )
