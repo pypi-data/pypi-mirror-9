051042d67f0fe4a775df67347178a486c79f4e5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:35 +0000

    Add package Joshua_helloworld/1.0.0/Joshua_helloworld-1.0.0.zip

---
+++ packages/Joshua_helloworld/Joshua_helloworld-1.0.0.zip/Joshua_helloworld-1.0.0/helloworld.py
@@ -0,0 +1,8 @@
+#coding=utf-8
+__author__ = 'Joshua'
+
+
+def print_hello(the_list):
+    print 'Hello world' + the_list
+
+'''This is my first python'''
---
+++ packages/Joshua_helloworld/Joshua_helloworld-1.0.0.zip/Joshua_helloworld-1.0.0/setup.py
@@ -0,0 +1,13 @@
+__author__ = 'Joshua'
+import helloworld
+from distutils.core import setup
+
+setup(
+    name='Joshua_helloworld',
+    version = '1.0.0',
+    py_modules = ['helloworld'],
+    author='Joshua',
+    author_email='youhusky@126.com',
+    description='test',
+)
+
