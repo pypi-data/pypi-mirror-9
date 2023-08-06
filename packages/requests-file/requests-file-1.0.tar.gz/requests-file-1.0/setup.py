58a8cf0ed1d1378bb7e09b87b2c14fd08abe5c08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:36:54 +0000

    Add package requests-file/1.0/requests-file-1.0.tar.gz

---
+++ packages/requests-file/requests-file-1.0.tar.gz/requests-file-1.0/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+import os
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+requires = [line.strip() for line in open("requirements.txt").readlines()]
+
+setup(
+    name='requests-file',
+    version='1.0',
+    description='File transport adapter for Requests',
+    author='David Shea',
+    author_email='dshea@redhat.com',
+    url='http://github.com/dashea/requests-file',
+    py_modules=['requests_file'],
+    install_requires=requires,
+    license='Apache 2.0',
+    test_suite='tests',
+    classifiers=('Development Status :: 3 - Alpha',
+                 'Environment :: Plugins',
+                 'Intended Audience :: Developers',
+                 'License :: OSI Approved :: Apache Software License',
+                 )
+    )
