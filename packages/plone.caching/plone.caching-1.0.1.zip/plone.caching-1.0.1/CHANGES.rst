69f8891745e479912350512d4fa4b05124ccb413
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:50 +0000

    Add package plone.caching/1.0.1/plone.caching-1.0.1.zip

---
+++ packages/plone.caching/plone.caching-1.0.1.zip/plone.caching-1.0.1/CHANGES.rst
@@ -0,0 +1,43 @@
+Changelog
+=========
+
+1.0.1 (2015-03-21)
+------------------
+
+- Fix ruleset registry test isolation so that is no longer order dependent.
+  [jone]
+
+
+1.0 - 2011-05-13
+----------------
+
+- Release 1.0 Final.
+  [esteele]
+
+- Add MANIFEST.in.
+  [WouterVH]
+
+
+1.0b2 - 2011-02-10
+------------------
+
+- Updated tests to reflect operation parameter overrides can now use
+  plone.registry FieldRefs. Requires plone.registry >= 1.0b3.
+  [optilude]
+
+- Removed monkey patches unneeded since Zope 2.12.4.
+  [optilude]
+
+
+1.0b1 - 2010-08-04
+------------------
+
+- Preparing release to coincide with plone.app.caching 1.0b1
+  [optilude]
+
+
+1.0a1 - 2010-04-22
+------------------
+
+- Initial release
+  [optilude, newbery]
---
+++ packages/plone.caching/plone.caching-1.0.1.zip/plone.caching-1.0.1/setup.py
@@ -0,0 +1,53 @@
+import os
+from setuptools import setup, find_packages
+
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+version = '1.0.1'
+
+long_description = (
+    read('README.rst')
+    + '\n' +
+#    read('plone', 'caching', 'README.txt')
+#    + '\n' +
+    read('CHANGES.rst')
+    + '\n'
+    )
+
+setup(name='plone.caching',
+      version=version,
+      description="Zope 2 integration for z3c.caching",
+      long_description=long_description,
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='plone http caching',
+      author='Plone Foundation',
+      author_email='plone-developers@lists.sourceforge.net',
+      url='http://pypi.python.org/pypi/plone.caching',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['plone'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'z3c.caching [zcml]',
+          'plone.registry',
+          'zope.interface',
+          'zope.component',
+          'zope.i18nmessageid',
+          'zope.schema',
+          'plone.transformchain',
+          'five.globalrequest',
+          'Zope2 >= 2.12.4',
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
