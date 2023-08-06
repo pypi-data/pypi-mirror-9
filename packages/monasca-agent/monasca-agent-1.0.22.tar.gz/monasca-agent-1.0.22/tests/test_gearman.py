ebf2f83f22f4a3a27d6fab102bd2bfa601c5ddf5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:12 +0000

    Add package monasca-agent/1.0.22/monasca-agent-1.0.22.tar.gz

---
+++ packages/monasca-agent/monasca-agent-1.0.22.tar.gz/monasca-agent-1.0.22/tests/test_gearman.py
@@ -0,0 +1,28 @@
+import unittest
+from tests.common import load_check
+from nose.plugins.skip import SkipTest
+
+
+class GearmanTestCase(unittest.TestCase):
+
+    def testMetrics(self):
+        raise SkipTest('Requires Gearman')
+
+        config = {
+            'instances': [{
+                'dimensions': {'first': 'second'}
+            }]
+        }
+        agent_config = {
+            'version': '0.1',
+            'api_key': 'toto'
+        }
+
+        self.check = load_check('gearmand', config, agent_config)
+
+        self.check.check(config['instances'][0])
+
+        metrics = self.check.get_metrics()
+        self.assertTrue(isinstance(metrics, list))
+        self.assertTrue(len(metrics) == 4)
+        self.assertTrue(len([k for k in metrics if "second" in k[3]['dimensions']['first']]) == 4)
