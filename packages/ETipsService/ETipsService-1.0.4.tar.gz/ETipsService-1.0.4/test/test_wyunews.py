a411e7cfbbc70b456e252d1abfa3e611327bc45c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:54 +0000

    Add package ETipsService/1.0.4/ETipsService-1.0.4.tar.gz

---
+++ packages/ETipsService/ETipsService-1.0.4.tar.gz/ETipsService-1.0.4/test/test_wyunews.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from service.wyunews import WyuNews
+import json
+if __name__ == '__main__':
+    wyunews = WyuNews()
+    print (json.dumps(wyunews.get_wyu_news(1)))
+
+    # print wyunews.get_news_content('http://www.wyu.cn/news/news_zxtz/201481811490835123.htm')
+    print wyunews.get_news_content('http://www.wyu.cn/news/news_xnxw/20153181021141118.htm')
