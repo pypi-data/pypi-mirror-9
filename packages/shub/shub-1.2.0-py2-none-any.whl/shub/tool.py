038468d961e0dd1f13d2a42d5718b2d5fbd96db3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:25 +0000

    Add package shub/1.2.0/shub-1.2.0-py2-none-any.whl

---
+++ packages/shub/shub-1.2.0-py2-none-any.whl/shub/tool.py
@@ -0,0 +1,29 @@
+import click, importlib
+from shub.utils import missing_modules
+
+def missingmod_cmd(modules):
+    modlist = ", ".join(modules)
+    @click.command(help="*DISABLED* - requires %s" % modlist)
+    @click.pass_context
+    def cmd(ctx):
+        click.echo("Error: '%s' command requires %s" % (ctx.info_name, modlist))
+        ctx.exit(1)
+    return cmd
+
+@click.group(help="Scrapinghub command-line client")
+def cli():
+    pass
+
+module_deps = {
+    "deploy": ["scrapy", "setuptools"],
+    "login": [],
+}
+
+for command, modules in module_deps.iteritems():
+    m = missing_modules(*modules)
+    if m:
+        cli.add_command(missingmod_cmd(m), command)
+    else:
+        module_path = "shub." + command
+        command_module = importlib.import_module(module_path)
+        cli.add_command(command_module.cli, command)
---
+++ packages/shub/shub-1.2.0-py2-none-any.whl/shub/utils.py
@@ -0,0 +1,34 @@
+import sys, imp, os, netrc
+
+SCRAPY_CFG_FILE = os.path.expanduser("~/.scrapy.cfg")
+NETRC_FILE = os.path.expanduser('~/.netrc')
+
+def missing_modules(*modules):
+    """Receives a list of module names and returns those which are missing"""
+    missing = []
+    for module_name in modules:
+        try:
+            imp.find_module(module_name)
+        except ImportError:
+            missing.append(module_name)
+    return missing
+
+def find_api_key():
+    """Finds and returns the Scrapy Cloud APIKEY"""
+    key = os.getenv("SHUB_APIKEY")
+    if not key:
+        key = get_key_netrc()
+    return key
+
+def get_key_netrc():
+    """Gets the key from the netrc file"""
+    try:
+        info = netrc.netrc()
+    except IOError:
+        return
+    try:
+        key, account, password = info.authenticators("scrapinghub.com") 
+    except TypeError:
+        return
+    if key:
+        return key
