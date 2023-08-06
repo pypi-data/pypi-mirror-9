3d2a0743b5a69d1a6a3433a445875bdf2b162f7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:07:44 +0000

    Add package globussh/0.1.1/globussh-0.1.1.tar.gz

---
+++ packages/globussh/globussh-0.1.1.tar.gz/globussh-0.1.1/README.rst
@@ -0,0 +1,37 @@
+globussh
+========
+|Version Status| |Downloads|
+
+Python wrappers around the globus command line interface using ssh.
+
+Example
+-------
+.. code:: python
+
+   >>> from globussh import transfer
+   >>> src = "globusid#endpoint1/path/to/file1"
+   >>> dst = "glbousid#endpoint2/path/to/file2"
+   >>> transfer("{:s} {:s}".format(src, dst), label="example_transfer")
+   >>> scp(src, dst, label="example_scp")
+
+Install
+-------
+
+``globussh`` is on the Python Package Index (PyPI):
+
+::
+
+    pip install globussh
+
+``globush`` expects ssh to have a host ``globus`` in ``~/.ssh/config``, e.g.:
+
+::
+    host globus
+    HostName cli.globusonline.org
+    User maxhutch
+
+.. |Version Status| image:: https://pypip.in/v/globussh/badge.png
+   :target: https://pypi.python.org/pypi/globussh/
+.. |Downloads| image:: https://pypip.in/d/globussh/badge.png
+   :target: https://pypi.python.org/pypi/globussh/
+
---
+++ packages/globussh/globussh-0.1.1.tar.gz/globussh-0.1.1/globussh/core.py
@@ -0,0 +1,122 @@
+from subprocess import call, check_output, Popen, PIPE
+
+def exists(path):
+    foo = check_output(args=["/usr/bin/ssh", "globus", "ls {:s}".format(path)])
+    return not len(foo) == 0
+
+
+def wait(taskid):
+  return call(args=["/usr/bin/ssh", "globus", "wait -q {:s}".format(taskid)])
+
+
+def cancel(taskid):
+  return call(args=["/usr/bin/ssh", "globus", "cancel {:s}".format(taskid)])
+
+
+def transfer(transfer, 
+             label="globussh_transfer", 
+             activate=False, 
+             sync=3,
+             deadline=None,
+             encrypt=False,
+             verify=False,
+             delete=False,
+             block=True):
+    taskid = check_output(args=["/usr/bin/ssh",
+                                "globus",
+                                "transfer --generate-id"]
+                          ).decode("utf-8")
+    opts = []
+    if sync is not None:
+      opts.append("-s {:d}".format(sync))
+    if activate:
+      opts.append("-g")
+    if deadline is not None:
+      opts.append("-d {:s}".format(deadline))
+    if encrypt:
+      opts.append("--encrypt")
+    if verify:
+      opts.append("--verify-checksum")
+    else:
+      opts.append("--no-verify-checksum")
+    if delete:
+      opts.append("--delete")
+
+    p = Popen(args=["/usr/bin/ssh", "globus", "transfer"] 
+                 + opts
+                 + ["--label={:s}".format(label),
+                    "--taskid={:s}".format(taskid)],
+                    universal_newlines=True,
+                    stdin=PIPE)
+    p.communicate(input=transfer)
+    p.wait()
+
+    if block:
+      wait(taskid)
+    return taskid
+
+
+def scp(source,
+        dest,
+        label="globussh_scp", 
+        activate=False, 
+        sync=3,
+        deadline=None,
+        encrypt=False,
+        verify=False,
+        delete=False,
+        recursive=False,
+        block=True):
+
+    opts = []
+    if recursive:
+      opts.append("-r")
+    if sync is not None:
+      opts.append("-s {:d}".format(sync))
+    if activate:
+      opts.append("-g")
+    if deadline is not None:
+      opts.append("-d {:s}".format(deadline))
+    if encrypt:
+      opts.append("--encrypt")
+    if verify:
+      opts.append("--verify-checksum")
+    else:
+      opts.append("--no-verify-checksum")
+    if delete:
+      opts.append("--delete")
+    if not block:
+      opts.append("-D")
+
+    return call(args=["/usr/bin/ssh", "globus", "scp"] 
+                 + opts
+                 + [source, dest]) 
+
+def rm(path,
+       label="globussh_rm", 
+       activate=False, 
+       ignore=False,
+       deadline=None,
+       recursive=False,
+       block=True):
+
+    opts = []
+    if recursive:
+      opts.append("-r")
+    if ignore:
+      opts.append("-f")
+    if activate:
+      opts.append("-g")
+    if deadline is not None:
+      opts.append("-d {:s}".format(deadline))
+    if not block:
+      opts.append("-D")
+
+    return call(args=["/usr/bin/ssh", "globus", "rm"] 
+                 + opts
+                 + [path]) 
+
+
+def rename(old, new):
+    return call(args=["/usr/bin/ssh", "globus", "rename"] 
+                 + [old, new])
