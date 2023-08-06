1d416a2a6eb661dbe991d446339528b33cdc566b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:40 +0000

    Add package tomek/0.2/tomek-0.2.tar.gz

---
+++ packages/tomek/tomek-0.2.tar.gz/tomek-0.2/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+
+setup(
+    name='tomek',
+    py_modules=['tomek'],
+    version='0.2',
+    description='Need more Tomek in your Django?',
+    author='Marc Tamlyn',
+    author_email='marc.tamlyn@gmail.com',
+    url='https://github.com/mjtamlyn/tomek',
+    license='BSD',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
+)
