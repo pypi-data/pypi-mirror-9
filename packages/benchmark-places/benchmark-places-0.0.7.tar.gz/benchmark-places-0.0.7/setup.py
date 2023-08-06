7a7edf4cb157666047baaf1c073e1c6845c62c5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:36:49 +0000

    Add package benchmark-places/0.0.7/benchmark-places-0.0.7.tar.gz

---
+++ packages/benchmark-places/benchmark-places-0.0.7.tar.gz/benchmark-places-0.0.7/setup.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name='benchmark-places',
+    version='0.0.7',
+    author=u'Ken Koontz',
+    author_email='kenneth.koontz@gmail.com',
+    packages=find_packages(),
+    license='MIT licence, see LICENCE.txt',
+    url='https://github.com/usebenchmark/benchmarkplaces.git',
+    description='Place abstraction',
+    long_description=open('README.rst').read(),
+    install_requires = [
+        'rauth',
+        'requests'
+    ],
+    zip_safe=False,
+)
