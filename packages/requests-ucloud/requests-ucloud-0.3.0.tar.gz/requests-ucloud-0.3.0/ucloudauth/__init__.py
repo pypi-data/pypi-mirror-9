cfeb19b2d6a471ef3fe56104810ff5646352d6f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:19 +0000

    Add package requests-ucloud/0.3.0/requests-ucloud-0.3.0.tar.gz

---
+++ packages/requests-ucloud/requests-ucloud-0.3.0.tar.gz/requests-ucloud-0.3.0/ucloudauth/__init__.py
@@ -0,0 +1,35 @@
+# -*- coding:utf8 -*-
+
+"""
+ucloudauth
+~~~~~~~~~~~~~~~
+
+This module contains the authentication handlers for UCloud Service
+"""
+
+__version__ = "0.3.0"
+__author__ = "SkyLothar"
+__email__ = "allothar@gmail.com"
+__url__ = "http://github.com/skylothar/requests-ucloud"
+
+__all__ = ["UCloudOmniAuth", "UCloudAuth", "UFileAuth"]
+
+from .common_sign import UCloudAuth
+from .ufile import UFileAuth
+
+import requests
+
+
+class UCloudOmniAuth(requests.auth.AuthBase):
+    def __init__(self, public_key, private_key):
+        self._ucloud_auth = UCloudAuth(public_key, private_key)
+        self._ufile_auth = UFileAuth(public_key, private_key)
+
+    def __call__(self, req):
+        """Sign the request"""
+        url = requests.compat.urlsplit(req.url)
+        u_host = url.netloc.split(".", 1)[1]
+        if u_host.startswith("ufile.") and u_host.endswith(".ucloud.cn"):
+            return self._ufile_auth(req)
+        else:
+            return self._ucloud_auth(req)
