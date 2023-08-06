c7cb7ba41857dd36d74e6997b8b3c0415c40782e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:44 +0000

    Add package restructuredtext-lint/0.11.0/restructuredtext_lint-0.11.0.tar.gz

---
+++ packages/restructuredtext-lint/restructuredtext_lint-0.11.0.tar.gz/restructuredtext_lint-0.11.0/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='restructuredtext_lint',
+    version='0.11.0',
+    description='reStructuredText linter',
+    long_description=open('README.rst').read(),
+    keywords=[
+        'restructuredtext',
+        'restructured text',
+        'rest',
+        'rst',
+        'lint'
+    ],
+    author='Todd Wolfson',
+    author_email='todd@twolfson.com',
+    url='https://github.com/twolfson/restructuredtext-lint',
+    download_url='https://github.com/twolfson/restructuredtext-lint/archive/master.zip',
+    entry_points={
+        'console_scripts': [
+            'restructuredtext-lint = restructuredtext_lint.cli:main',
+            'rst-lint = restructuredtext_lint.cli:main'
+        ]
+    },
+    packages=find_packages(),
+    license='UNLICENSE',
+    install_requires=open('requirements.txt').readlines(),
+    # https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: Public Domain',
+        'Operating System :: OS Independent',
+        'Topic :: Text Processing :: Markup'
+    ]
+)
