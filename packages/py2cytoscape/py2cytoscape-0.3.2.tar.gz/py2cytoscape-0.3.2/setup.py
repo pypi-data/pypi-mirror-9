fc2e9662099ff3d4c38e8e8f63f62009a0a5d577
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:42 +0000

    Add package py2cytoscape/0.3.2/py2cytoscape-0.3.2.tar.gz

---
+++ packages/py2cytoscape/py2cytoscape-0.3.2.tar.gz/py2cytoscape-0.3.2/setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Setup script for py2cytoscape
+
+Yo install, run:
+
+python setup.py install
+"""
+
+from setuptools import setup, find_packages
+
+setup(
+    name='py2cytoscape',
+    version='0.3.2',
+    description='Utility package for using Cytoscape and Cytoscape.js from Python',
+    author='Keiichiro Ono',
+    author_email='kono@ucsd.edu',
+    url='http://www.cytoscape.org/',
+    license='MIT License',
+    keywords=['data visualization','cytoscape'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'License :: OSI Approved :: MIT License',
+        'Intended Audience :: Developers',
+    ],
+    test_suite='tests',
+    packages=find_packages(),
+    package_data={'py2cytoscape': ['cytoscapejs/*.js', 'cytoscapejs/*.html']}
+)
