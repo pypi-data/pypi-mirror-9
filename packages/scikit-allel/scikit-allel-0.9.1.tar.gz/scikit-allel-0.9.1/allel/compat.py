4dde2f7aaf605846265f3753b481ed8244d89ec7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:59 +0000

    Add package scikit-allel/0.9.1/scikit-allel-0.9.1.tar.gz

---
+++ packages/scikit-allel/scikit-allel-0.9.1.tar.gz/scikit-allel-0.9.1/allel/compat.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+# flake8: noqa
+from __future__ import absolute_import, print_function, division
+
+
+import sys
+import itertools
+
+
+PY2 = sys.version_info[0] == 2
+
+
+if PY2:
+    range = xrange
+    map = itertools.imap
+    string_types = basestring,
+    text_type = unicode
+    binary_type = str
+    integer_types = int, long
+    zip = itertools.izip
+    zip_longest = itertools.izip_longest
+    reduce = reduce
+    from urllib import unquote_plus
+else:
+    range = range
+    map = map
+    string_types = str,
+    text_type = str
+    binary_type = bytes
+    integer_types = int,
+    zip = zip
+    zip_longest = itertools.zip_longest
+    import functools
+    reduce = functools.reduce
+    from urllib.parse import unquote_plus
