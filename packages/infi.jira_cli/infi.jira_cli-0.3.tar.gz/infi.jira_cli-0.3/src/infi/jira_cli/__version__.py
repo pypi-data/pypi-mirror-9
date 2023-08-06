a9002b94694038d1f81cd2e548e81c227055fb59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:40 +0000

    Add package infi.jira_cli/0.3/infi.jira_cli-0.3.tar.gz

---
+++ packages/infi.jira_cli/infi.jira_cli-0.3.tar.gz/infi.jira_cli-0.3/src/infi/jira_cli/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.3"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = '(detached from v0.3)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '84dd1daae7866f286f7cf4cfc8f98a585cfb2799'
+__git_head_subject__ = 'Finished release v0.3.'
+__git_head_message__ = ''
+__git_dirty_diff__ = ''
