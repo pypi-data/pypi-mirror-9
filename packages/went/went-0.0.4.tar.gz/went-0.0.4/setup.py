de0e6cab4220dee550ae88293a016ccb3d4b85a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:20 +0000

    Add package went/0.0.4/went-0.0.4.tar.gz

---
+++ packages/went/went-0.0.4.tar.gz/went-0.0.4/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+import pip
+from pip.req import parse_requirements
+from pip.exceptions import InstallationError
+
+try:
+    try:
+        requirements = parse_requirements("requirements.txt", session=pip.download.PipSession())
+    except AttributeError:
+        requirements = parse_requirements("requirements.txt")
+
+    install_requires = [str(r.req) for r in requirements]
+except InstallationError:
+    install_requires = []
+
+try:
+    long_description = open("README.rst").read()
+except IOError:
+    long_description = ""
+
+setup(
+    name="went",
+    version="0.0.4",
+    description="Tools for implementing a webmention enpoint.",
+    license="MIT",
+    author="Giovanni T. Parra",
+    author_email='fiatjaf@gmail.com',
+    install_requires=install_requires,
+    long_description=long_description,
+    packages=['went']
+)
