a10e2fa909b38a9723eec4339956eef32a8c509c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:02 +0000

    Add package reposmon/16/reposmon-16.tar.gz

---
+++ packages/reposmon/reposmon-16.tar.gz/reposmon-16/reposmon/__init__.py
@@ -0,0 +1,14 @@
+# coding=utf-8
+"""
+pip
+Active8 (05-03-15)
+license: GNU-GPL2
+"""
+from __future__ import absolute_import
+from __future__ import unicode_literals
+from __future__ import print_function
+from __future__ import division
+from future import standard_library
+standard_library.install_aliases()
+
+from .reposmon import *
