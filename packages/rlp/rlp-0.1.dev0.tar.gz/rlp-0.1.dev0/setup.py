5ea8338ee42bd4a7f751eed77b49d6ec1bdd47a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:52 +0000

    Add package rlp/0.1.dev0/rlp-0.1.dev0.tar.gz

---
+++ packages/rlp/rlp-0.1.dev0.tar.gz/rlp-0.1.dev0/setup.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+with open('README.md') as readme_file:
+    readme = readme_file.read()
+
+requirements = [
+    # TODO: put package requirements here
+]
+
+test_requirements = [
+    # TODO: put package test requirements here
+]
+
+setup(
+    name='rlp',
+    version='0.1.dev0',
+    description="A package for encoding and decoding data in and from Recursive Length Prefix notation",
+    long_description=readme,
+    author="jnnk",
+    author_email='jnnknnj@gmail.com',
+    url='https://github.com/ethereum/pyrlp',
+    packages=[
+        'rlp',
+    ],
+    package_dir={'rlp':
+                 'rlp'},
+    include_package_data=True,
+    install_requires=requirements,
+    license="BSD",
+    zip_safe=False,
+    keywords='rlp ethereum',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
+    test_suite='tests',
+    tests_require=test_requirements
+)
