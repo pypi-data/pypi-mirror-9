1bdf0f3526d4312aa323c1194ec181d03bf7a88f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:45 +0000

    Add package pdftools/1.0.4/pdftools-1.0.4.zip

---
+++ packages/pdftools/pdftools-1.0.4.zip/pdftools-1.0.4/setup.py
@@ -0,0 +1,19 @@
+#! python3
+from setuptools import setup
+from pdftools import __version__
+
+setup(
+    name='pdftools',
+    version=__version__,
+    packages=['pdftools'],
+    scripts=['pdfsplit.py', 'pdfmerge.py', 'pdfrotate.py', 'pdfzip.py',
+             'pdfinsert.py', 'pdfremove.py', 'pdfadd.py'],
+    url='https://github.com/MrLeeh/pdftools',
+    license='MIT',
+    author='Stefan Lehmann',
+    author_email='Stefan.St.Lehmann@gmail.com',
+    description='A collection of convenience scripts for pdf manipulation, '
+                'based on the PyPdf2 package.',
+    install_requires=['PyPdf2'],
+    maintainer='Stefan Lehmann'
+)
