4c177d18c941eaebdaea5d25a01a9c22240d3fce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:01 +0000

    Add package django-lazyloader/0.2/django-lazyloader-0.2.tar.gz

---
+++ packages/django-lazyloader/django-lazyloader-0.2.tar.gz/django-lazyloader-0.2/lazyloader/static/lazyloader/js/demo.js
@@ -0,0 +1,40 @@
+$(document).ready(function() {
+
+    $('#btn-true').click(function() {
+
+        start = $(".object").length;
+        end = start + parseInt($('#next').val(), 10);
+
+        // Create URL
+        var url = '/lazyloader/lazyloader-demoobject-html-' + start + '-' + end + '/';
+        console.log(url);
+        var request = $.get(url, {
+            // Get parameters
+            "column": "stock",
+            "search_value": true
+        }, function (data) {
+            // Add fetched data to the container
+            $("#container").append(data);
+        });
+
+    });
+    $('#btn-false').click(function() {
+
+        start = $(".object").length;
+        end = start + parseInt($('#next').val(), 10);
+
+        // Create URL
+        var url = '/lazyloader/lazyloader-demoobject-html-' + start + '-' + end + '/';
+        console.log(url);
+        var request = $.get(url, {
+            // Get parameters
+            "column": "stock",
+            "search_value": false
+        }, function (data) {
+            // Add fetched data to the container
+            $("#container").append(data);
+        });
+
+    });
+
+});
---
+++ packages/django-lazyloader/django-lazyloader-0.2.tar.gz/django-lazyloader-0.2/setup.py
@@ -0,0 +1,34 @@
+import os
+from setuptools import setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-lazyloader',
+    version='0.2',
+    packages=['lazyloader'],
+    include_package_data=True,
+    license='GNU General Public License v3 (GPLv3)',
+    description='Lazy-Loading module for Django',
+    long_description=README,
+    author='Julian Triet',
+    author_email='julian.triet@openinteractive.ch',
+    install_requires=[
+        'django'
+    ],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ]
+)
