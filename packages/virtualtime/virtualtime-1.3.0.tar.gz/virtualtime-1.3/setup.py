b1e0b2998a137f3284568f982679c79d0331de70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:44 +0000

    Add package virtualtime/1.3/virtualtime-1.3.0.tar.gz

---
+++ packages/virtualtime/virtualtime-1.3.0.tar.gz/virtualtime-1.3/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+setup(
+    name='virtualtime',
+    version='1.3',
+    packages=['virtualtime', 'virtualtime.datetime_tz'],
+    license='Apache License, Version 2.0',
+    description='Implements a system for simulating a virtual time.',
+    long_description=open('README.md').read(),
+    url='http://www.sjsoft.com/',
+    author='St James Software',
+    author_email='support@sjsoft.com',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: Apache Software License',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 2 :: Only',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Software Development :: Testing',
+    ],
+    install_requires = ['python-datetime-tz == 0.2-j5p1'],
+    extras_require = {
+        'tests':  ["nose", 'decorator'],
+        },
+    dependency_links = ["https://github.com/st-james-software/python-datetime-tz/archive/v0.2-j5p1.zip#egg=python-datetime-tz-0.2-j5p1"]
+)
