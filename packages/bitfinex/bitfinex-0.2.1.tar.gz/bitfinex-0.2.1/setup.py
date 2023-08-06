2ee429c0438d96c6de367507b06dec6ef5f6f108
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:02 +0000

    Add package bitfinex/0.2.1/bitfinex-0.2.1.tar.gz

---
+++ packages/bitfinex/bitfinex-0.2.1.tar.gz/bitfinex-0.2.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(name='bitfinex',
+    version='0.2.1',
+    description = 'Python client for the Bitfinex API',
+    author = 'Scott Barr',
+    author_email = 'scottjbarr@gmail.com',
+    url = 'https://github.com/scottjbarr/bitfinex',
+    license = 'MIT',
+    packages=['bitfinex'],
+    scripts = ['scripts/watch_orderbook'],
+    download_url = 'https://github.com/scottjbarr/bitfinex/tarball/0.2.1',
+    keywords = ['bitcoin', 'btc'],
+    classifiers = [],
+    zip_safe=True)
