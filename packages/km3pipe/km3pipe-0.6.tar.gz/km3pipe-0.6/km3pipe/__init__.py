c2e2fe2f3619ce66145cefdfb60bea484e1558a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:02 +0000

    Add package km3pipe/0.6/km3pipe-0.6.tar.gz

---
+++ packages/km3pipe/km3pipe-0.6.tar.gz/km3pipe-0.6/km3pipe/__init__.py
@@ -0,0 +1,24 @@
+# coding=utf-8
+# Filename: __init__.py
+"""
+The extemporary KM3NeT analysis framework.
+
+"""
+from __future__ import division, absolute_import, print_function
+
+from km3pipe.__version__ import version, version_info
+
+try:
+    from km3pipe.core import Pipeline, Module, Pump, Blob, Geometry
+except ImportError:
+    print("Numpy is needed for KM3Pipe")
+
+__author__ = "Tamas Gal"
+__copyright__ = ("Copyright 2015, Tamas Gal and the KM3NeT collaboration "
+                 "(http://km3net.org)")
+__credits__ = []
+__license__ = "MIT"
+__version__ = version
+__maintainer__ = "Tamas Gal"
+__email__ = "tgal@km3net.de"
+__status__ = "Development"
