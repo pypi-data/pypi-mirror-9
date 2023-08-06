e0092e7fc5101e90da9c31c282c056c806f2796e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:15 +0000

    Add package haigha/0.7.3/haigha-0.7.3.tar.gz

---
+++ packages/haigha/haigha-0.7.3.tar.gz/haigha-0.7.3/setup.py
@@ -0,0 +1,37 @@
+import haigha
+import os
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+requirements = map(str.strip, open('requirements.txt').readlines())
+
+setup(
+    name='haigha',
+    version=haigha.__version__,
+    author='Vitaly Babiy, Aaron Westendorf',
+    author_email="vbabiy@agoragames.com, aaron@agoragames.com",
+    packages = ['haigha', 'haigha.frames', 'haigha.classes', 'haigha.transports', 'haigha.connections'],
+    install_requires = requirements,
+    url='https://github.com/agoragames/haigha',
+    license="LICENSE.txt",
+    description='Synchronous and asynchronous AMQP client library',
+    long_description=open('README.rst').read(),
+    keywords=['python', 'amqp', 'event', 'rabbitmq'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: BSD License',
+        "Intended Audience :: Developers",
+        "Operating System :: POSIX",
+        "Topic :: Communications",
+        "Topic :: System :: Distributed Computing",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries'
+    ]
+)
