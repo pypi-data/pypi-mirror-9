7abf5e83b41fe640a082868bbde6d949921cab70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:07 +0000

    Add package pelican/3.2.2/pelican-3.2.2.tar.gz

---
+++ packages/pelican/pelican-3.2.2.tar.gz/pelican-3.2.2/setup.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+requires = ['feedgenerator>=1.5', 'jinja2 >= 2.6', 'pygments', 'docutils', 'pytz',
+            'blinker', 'unidecode', 'six']
+
+try:
+    import argparse  # NOQA
+except ImportError:
+    requires.append('argparse')
+
+entry_points = {
+    'console_scripts': [
+        'pelican = pelican:main',
+        'pelican-import = pelican.tools.pelican_import:main',
+        'pelican-quickstart = pelican.tools.pelican_quickstart:main',
+        'pelican-themes = pelican.tools.pelican_themes:main'
+   ]
+}
+
+
+README = open('README.rst').read()
+CHANGELOG = open('docs/changelog.rst').read()
+
+
+setup(
+    name="pelican",
+    version="3.2.2",
+    url='http://getpelican.com/',
+    author='Alexis Metaireau',
+    author_email='authors@getpelican.com',
+    description="A tool to generate a static blog from reStructuredText or "
+                "Markdown input files.",
+    long_description=README + '\n' + CHANGELOG,
+    packages=['pelican', 'pelican.tools'],
+    include_package_data=True,
+    install_requires=requires,
+    entry_points=entry_points,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+         'Environment :: Console',
+         'License :: OSI Approved :: GNU Affero General Public License v3',
+         'Operating System :: OS Independent',
+         'Programming Language :: Python :: 2.7',
+         'Programming Language :: Python :: 3',
+         'Programming Language :: Python :: 3.2',
+         'Programming Language :: Python :: 3.3',
+         'Topic :: Internet :: WWW/HTTP',
+         'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    test_suite='pelican.tests',
+)
