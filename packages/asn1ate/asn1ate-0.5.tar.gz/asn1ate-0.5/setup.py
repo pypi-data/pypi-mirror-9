4bd5dbbbcb8ecd93ac77645c619b9e43fb550de1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:54 +0000

    Add package asn1ate/0.5/asn1ate-0.5.tar.gz

---
+++ packages/asn1ate/asn1ate-0.5.tar.gz/asn1ate-0.5/setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(
+    name='asn1ate',
+    version='0.5',
+    description='ASN.1 translation library.',
+    author='Kim Gräsman',
+    author_email='kim.grasman@gmail.com',
+    license='BSD',
+    long_description=open('README.txt').read(),
+    url='http://github.com/kimgr/asn1ate',
+    packages=[
+        'asn1ate',
+        'asn1ate.support',
+    ],
+    platforms=['any'],
+    requires=[
+        'pyparsing (>=2.0.0)',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Code Generators',
+    ]
+)
