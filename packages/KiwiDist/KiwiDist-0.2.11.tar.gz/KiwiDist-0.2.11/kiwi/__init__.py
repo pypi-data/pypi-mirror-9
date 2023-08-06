65c1cbaeb159aa4b860d84930d4562b2f228e0fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:28 +0000

    Add package KiwiDist/0.2.11/KiwiDist-0.2.11.tar.gz

---
+++ packages/KiwiDist/KiwiDist-0.2.11.tar.gz/KiwiDist-0.2.11/kiwi/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""Kiwi: a tool to combine gene-set analyses with biological networks.
+:author: Francesco Gatto and Leif Väremo, 2014
+:email: kiwi@sysbio.se
+:affiliation: Department of Biological Engineering, Chalmers, Göteborg, Sweden"""
+
+from kiwi_module import plot
