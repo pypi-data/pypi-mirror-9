44ffc314afe676e6ffc77308613303a2490d9ca5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:43 +0000

    Add package python-tutum/0.12.6/python-tutum-0.12.6.tar.gz

---
+++ packages/python-tutum/python-tutum-0.12.6.tar.gz/python-tutum-0.12.6/tutum/api/stack.py
@@ -0,0 +1,34 @@
+import json
+
+from .base import Mutable
+
+
+class Stack(Mutable):
+    """Represents a Tutum Stack object"""
+
+    endpoint = "/stack"
+
+    def start(self):
+        """Starts the stack in Tutum.
+
+        :returns: bool -- whether or not the operation succeeded
+        :raises: TutumApiError
+        """
+        return self._perform_action("start")
+
+    def stop(self):
+        """Stops the stack in Tutum.
+
+        :returns: bool -- whether or not the operation succeeded
+        :raises: TutumApiError
+        """
+        return self._perform_action("stop")
+
+    def redeploy(self):
+        """Redeploy the stack in Tutum.
+
+        :returns: bool -- whether or not the operation succeeded
+        :raises: TutumApiError
+        """
+        return self._perform_action("redeploy")
+
