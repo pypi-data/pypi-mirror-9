0acd066d96be165619dfd2f90d88746dbbf52d97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:19 +0000

    Add package vpr_netfile_parser/0.3.post3/vpr_netfile_parser-0.3.post3.tar.gz

---
+++ packages/vpr_netfile_parser/vpr_netfile_parser-0.3.post3.tar.gz/vpr_netfile_parser-0.3.post3/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+import os
+import sys
+sys.path.insert(0, '.')
+from distutils.core import setup, Extension
+from Cython.Build import cythonize
+
+import vpr_netfile_parser
+import version
+
+include_dirs = vpr_netfile_parser.get_includes()
+sys.path += include_dirs
+
+cc_config = dict(include_dirs=include_dirs, language='c++',
+                 extra_compile_args=['-O3', '-std=c++0x', '-Wfatal-errors'])
+
+
+cython_exts = [Extension('vpr_netfile_parser.VprNetParser',
+                          ['vpr_netfile_parser/VprNetParser.pyx',
+                           'vpr_netfile_parser/VprNetParser_ragel.cpp'],
+                         **cc_config)]
+
+
+setup(name="vpr_netfile_parser",
+      version=version.getVersion(),
+      description="C++ VPR `.net` netlist-file parser, with Cython bindings",
+      keywords="fpga placement python C++ cython vpr",
+      author="Christian Fobel",
+      url="https://github.com/cfobel/vpr_netfile_parser",
+      license="GPL",
+      long_description="""""",
+      packages=['vpr_netfile_parser'],
+      package_data={'vpr_netfile_parser': ['VprNetParser.hpp',
+                                           'VprNetParser.pyx',
+                                           'VprNetParser.pxd']},
+      ext_modules=cythonize(cython_exts))
