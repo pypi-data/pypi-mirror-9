98d1cb2bf1118efa29ecba6b3b2c575ee34c2880
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:28 +0000

    Add package jsonobject/0.6.2/jsonobject-0.6.2.tar.gz

---
+++ packages/jsonobject/jsonobject-0.6.2.tar.gz/jsonobject-0.6.2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+with open('README.md') as readme_file:
+    long_description = readme_file.read()
+
+setup(
+    name='jsonobject',
+    version='0.6.2',
+    author='Danny Roberts',
+    author_email='droberts@dimagi.com',
+    description='A library for dealing with JSON as python objects',
+    long_description=long_description,
+    url='https://github.com/dannyroberts/jsonobject',
+    packages=['jsonobject'],
+    install_requires=[],
+    tests_require=['unittest2'],
+    test_suite='test',
+)
