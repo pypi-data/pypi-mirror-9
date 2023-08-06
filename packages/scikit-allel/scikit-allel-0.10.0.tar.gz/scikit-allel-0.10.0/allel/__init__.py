d8c2649111f22e16a030bf6f454b1961a870f517
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:36:48 +0000

    Add package scikit-allel/0.10.0/scikit-allel-0.10.0.tar.gz

---
+++ packages/scikit-allel/scikit-allel-0.10.0.tar.gz/scikit-allel-0.10.0/allel/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# flake8: noqa
+
+
+__version__ = '0.10.0'
+
+
+import allel.model as model
+import allel.bcolz as bcolz
+import allel.stats as stats
+import allel.plot as plot
+import allel.io as io
+import allel.constants as constants
---
+++ packages/scikit-allel/scikit-allel-0.10.0.tar.gz/scikit-allel-0.10.0/docs/stats.rst
@@ -0,0 +1,13 @@
+Statistics
+==========
+
+Statistical functions for use with variant call data.
+
+.. toctree::
+    :maxdepth: 3
+
+    stats/diversity
+    stats/distance
+    stats/hw
+    stats/ld
+    stats/window
