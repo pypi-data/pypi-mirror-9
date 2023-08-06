759f77dfca09655acfc64e59e86a06ca4e1f8bfe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:35 +0000

    Add package datasift-graphite/1.0/datasift-graphite-1.0.tar.gz

---
+++ packages/datasift-graphite/datasift-graphite-1.0.tar.gz/datasift-graphite-1.0/setup.py
@@ -0,0 +1,28 @@
+"""
+Setup.
+
+Installs console script and deps.
+"""
+from setuptools import setup
+
+
+setup(
+    name='datasift-graphite',
+    version='1.0',
+    description='Graph your Datasift account metrics in Graphite.',
+    long_description=open('README.md').read(),
+    author='Lex Toumbourou',
+    author_email='lextoumbourou@gmail.com',
+    url='https://github.com/lextoumbourou/datasift-graphite',
+    license='MIT License',
+    install_requires=['datasift'],
+    entry_points={
+        'console_scripts': ['datasift-graphite = datasift_graphite:main'],
+    },
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7'
+    ]
+)
