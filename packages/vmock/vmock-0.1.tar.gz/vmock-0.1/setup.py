a7e298077aabc8dbfc6199044ac87ecc992e409c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:42 +0000

    Add package vmock/0.1/vmock-0.1.tar.gz

---
+++ packages/vmock/vmock-0.1.tar.gz/vmock-0.1/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+install_requires = [
+]
+
+tests_require = install_requires + [
+    'nose', 'nosexcover'
+]
+
+setup(
+    name='vmock',
+    description='Advanced Mock library following record and replay approach.',
+    version='0.1',
+    author='Volodymyr Burenin',
+    author_email='vburenin@gmail.net',
+    maintainer='Volodymyr Burenin',
+    maintainer_email='vburenin@gmail.com',
+    packages=find_packages(),
+    install_requires=install_requires,
+    tests_require=tests_require,
+    test_suite='nose.collector',
+    extras_require={
+        'test': tests_require,
+    },
+    url='https://github.com/vburenin/vmock',
+    license='License :: MIT',
+    classifiers=['License :: OSI Approved :: MIT License',
+                 'Development Status :: 5 - Production/Stable',
+                 'Intended Audience :: Developers',
+                 'Programming Language :: Python :: 3.2',
+                 'Programming Language :: Python :: 3.3',
+                 'Programming Language :: Python :: 3.4',
+                 'Topic :: Software Development :: Quality Assurance',
+                 ],
+)
