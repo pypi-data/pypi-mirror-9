4c613bedce06c6fca4632d4f928ad485439a6a2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:18 +0000

    Add package pyramid-services/0.2/pyramid_services-0.2-py2.py3-none-any.whl

---
+++ packages/pyramid-services/pyramid_services-0.2-py2.py3-none-any.whl/pyramid_services/__init__.py
@@ -0,0 +1,107 @@
+from zope.interface import (
+    Interface,
+    implementedBy,
+    providedBy,
+)
+from zope.interface.interfaces import IInterface
+from zope.interface.adapter import AdapterRegistry
+
+_marker = object()
+
+class IServiceClassifier(Interface):
+    """ A marker interface to differentiate services from other objects
+    in a shared registry."""
+
+def includeme(config):
+    config.add_request_method(find_service)
+    config.add_request_method(
+        lambda _: AdapterRegistry(),
+        'service_cache',
+        reify=True,
+    )
+
+    config.add_directive('register_service', register_service)
+    config.add_directive('register_service_factory', register_service_factory)
+
+class SingletonServiceWrapper(object):
+    def __init__(self, service):
+        self.service = service
+
+    def __call__(self, context, request):
+        return self.service
+
+def register_service(
+    config,
+    service,
+    iface=Interface,
+    context=Interface,
+    name='',
+):
+    service = config.maybe_dotted(service)
+    service_factory = SingletonServiceWrapper(service)
+    config.register_service_factory(
+        service_factory,
+        iface,
+        context=context,
+        name=name,
+    )
+
+def register_service_factory(
+    config,
+    service_factory,
+    iface=Interface,
+    context=Interface,
+    name='',
+):
+    service_factory = config.maybe_dotted(service_factory)
+    iface = config.maybe_dotted(iface)
+    context = config.maybe_dotted(context)
+
+    if not IInterface.providedBy(context):
+        context_iface = implementedBy(context)
+    else:
+        context_iface = context
+
+    def register():
+        adapters = config.registry.adapters
+        adapters.register(
+            (IServiceClassifier, context_iface),
+            iface,
+            name,
+            service_factory,
+        )
+
+    discriminator = ('service factories', (iface, context, name))
+    if isinstance(service_factory, SingletonServiceWrapper):
+        type_name = type(service_factory.service).__name__
+    else:
+        type_name = type(service_factory).__name__
+
+    intr = config.introspectable(
+        category_name='pyramid_services',
+        discriminator=discriminator,
+        title=str((iface.__name__, context.__name__, name)),
+        type_name=type_name,
+    )
+    intr['name'] = name
+    intr['type'] = iface
+    intr['context'] = context
+    config.action(discriminator, register, introspectables=(intr,))
+
+def find_service(request, iface=Interface, context=_marker, name=''):
+    if context is _marker:
+        context = request.context
+
+    context_iface = providedBy(context)
+    svc_types = (IServiceClassifier, context_iface)
+
+    cache = request.service_cache
+    svc = cache.lookup(svc_types, iface, name=name, default=None)
+    if svc is None:
+        adapters = request.registry.adapters
+        svc_factory = adapters.lookup(svc_types, iface, name=name)
+        if svc_factory is None:
+            raise ValueError('could not find registered service')
+        svc = svc_factory(context, request)
+        cache.register(svc_types, iface, name, svc)
+    return svc
