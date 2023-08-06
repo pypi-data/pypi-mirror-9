c57bf02698be641a344e7f6d3df3bd8fb4fb8832
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:06 +0000

    Add package django-regex-field/0.1.5/django_regex_field-0.1.5-py2.py3-none-any.whl

---
+++ packages/django-regex-field/django_regex_field-0.1.5-py2.py3-none-any.whl/regex_field/fields.py
@@ -0,0 +1,58 @@
+import re
+
+from six import add_metaclass
+
+from django.core.exceptions import ValidationError
+from django.db.models import SubfieldBase
+from django.db.models.fields import CharField
+
+
+@add_metaclass(SubfieldBase)
+class RegexField(CharField):
+    """
+    A field that stores a regular expression and compiles it when accessed.
+    """
+    description = 'A regular expression'
+    # Maintain a cache of compiled regexs for faster lookup
+    compiled_regex_cache = {}
+
+    def get_prep_value(self, value):
+        value = self.to_python(value)
+        return self.value_to_string(value)
+
+    def get_compiled_regex(self, value):
+        if value not in self.compiled_regex_cache:
+            self.compiled_regex_cache[value] = re.compile(value)
+        return self.compiled_regex_cache[value]
+
+    def to_python(self, value):
+        """
+        Handles the following cases:
+        1. If the value is already the proper type (a regex), return it.
+        2. If the value is a string, compile and return the regex.
+
+        Raises: A ValidationError if the regex cannot be compiled.
+        """
+        if isinstance(value, type(re.compile(''))):
+            return value
+        else:
+            if value is None and self.null:
+                return None
+            else:
+                try:
+                    return self.get_compiled_regex(value)
+                except:
+                    raise ValidationError('Invalid regex {0}'.format(value))
+
+    def value_to_string(self, obj):
+        if obj is None:
+            return None
+        else:
+            return obj.pattern
+
+
+try:
+    from south.modelsinspector import add_introspection_rules
+    add_introspection_rules([], ['^regex_field\.fields\.RegexField'])
+except ImportError:
+    pass
