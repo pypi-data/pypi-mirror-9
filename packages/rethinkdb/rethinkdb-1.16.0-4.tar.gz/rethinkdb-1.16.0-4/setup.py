7e14d55dfc3c66bdb992c2721860a281d53523be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:29 +0000

    Add package rethinkdb/1.16.0-4/rethinkdb-1.16.0-4.tar.gz

---
+++ packages/rethinkdb/rethinkdb-1.16.0-4.tar.gz/rethinkdb-1.16.0-4/setup.py
@@ -0,0 +1,23 @@
+# Copyright 2010-2013 RethinkDB, all rights reserved.
+
+from setuptools import setup
+
+setup(
+    name="rethinkdb",
+    zip_safe=True,
+    version="1.16.0-4",
+    description="This package provides the Python driver library for the RethinkDB database server.",
+    url="http://rethinkdb.com",
+    maintainer="RethinkDB Inc.",
+    maintainer_email="bugs@rethinkdb.com",
+    packages=['rethinkdb'],
+    entry_points={
+        'console_scripts':[
+            'rethinkdb-import = rethinkdb._import:main',
+            'rethinkdb-dump = rethinkdb._dump:main',
+            'rethinkdb-export = rethinkdb._export:main',
+            'rethinkdb-restore = rethinkdb._restore:main',
+            'rethinkdb-index-rebuild = rethinkdb._index_rebuild:main'
+        ]
+    }
+)
