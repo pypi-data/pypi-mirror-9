4795504c174040fb34634e15e151360250c90298
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:23 +0000

    Add package SQLObject/3.0.0a1dev-20150327/SQLObject-3.0.0a1dev_20150327-py2.7.egg

---
+++ packages/SQLObject/SQLObject-3.0.0a1dev_20150327-py2.7.egg/sqlobject/dberrors.py
@@ -0,0 +1,55 @@
+"""dberrors: database exception classes for SQLObject.
+
+   These classes are dictated by the DB API v2.0:
+
+   http://www.python.org/topics/database/DatabaseAPI-2.0.html
+"""
+
+from sqlobject.compat import PY2
+
+if not PY2:
+    StandardError = Exception
+
+
+class Error(StandardError):
+    pass
+
+
+class Warning(StandardError):
+    pass
+
+
+class InterfaceError(Error):
+    pass
+
+
+class DatabaseError(Error):
+    pass
+
+
+class InternalError(DatabaseError):
+    pass
+
+
+class OperationalError(DatabaseError):
+    pass
+
+
+class ProgrammingError(DatabaseError):
+    pass
+
+
+class IntegrityError(DatabaseError):
+    pass
+
+
+class DataError(DatabaseError):
+    pass
+
+
+class NotSupportedError(DatabaseError):
+    pass
+
+
+class DuplicateEntryError(IntegrityError):
+    pass
