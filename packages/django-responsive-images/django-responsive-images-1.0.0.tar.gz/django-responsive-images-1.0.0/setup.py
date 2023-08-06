2fb4adb5629a9dcfe08a579a96fc56110e8aef20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:32 +0000

    Add package django-responsive-images/1.0.0/django-responsive-images-1.0.0.tar.gz

---
+++ packages/django-responsive-images/django-responsive-images-1.0.0.tar.gz/django-responsive-images-1.0.0/setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+
+import responsive_images
+
+
+with open('README.rst') as readme:
+    long_desc = readme.read()
+
+setup(
+    name='django-responsive-images',
+    version=responsive_images.__version__,
+    author='James Lecker Jr',
+    author_email='james@convectivesoftware.com',
+    url='https://github.com/ConvectiveSoftware/django-responsive-images',
+    description='An Django app for resizing images and generating src and srcset values via template tags.',
+    long_description = long_desc,
+    download_url='https://github.com/ConvectiveSoftware/django-responsive-images/tarball/{}'.format(responsive_images.__version__),
+    packages=['responsive_images', 'responsive_images.templatetags'],
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ],
+)
