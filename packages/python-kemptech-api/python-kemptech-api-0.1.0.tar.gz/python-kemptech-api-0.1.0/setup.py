7ebb7d0c04d69a000000a053c57c27c238cd54cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:21 +0000

    Add package python-kemptech-api/0.1.0/python-kemptech-api-0.1.0.tar.gz

---
+++ packages/python-kemptech-api/python-kemptech-api-0.1.0.tar.gz/python-kemptech-api-0.1.0/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+from setuptools import find_packages
+from setuptools import setup
+
+setup(name="python-kemptech-api",
+      version="0.1.0",
+      packages=find_packages(),
+      author="KEMP Technologies",
+      author_email="smcgough@kemptechnologies.com",
+      maintainer="Shane McGough",
+      maintainer_email="smcgough@kemptechnologies.com",
+      description="KEMP Technologies Python API",
+      long_description=open('README.rst').read(),
+      license="Apache",
+      keywords="python api kemptech kemp technologies restfull loadmaster",
+      url="http://pypi.python.org/pypi/python-kemptech-api/",
+      classifiers=[
+          'License :: OSI Approved :: Apache Software License',
+          'Operating System :: POSIX',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 3.4',
+          'Topic :: Internet',
+      ],
+      install_requires=['requests>=2.3.0']
+)
