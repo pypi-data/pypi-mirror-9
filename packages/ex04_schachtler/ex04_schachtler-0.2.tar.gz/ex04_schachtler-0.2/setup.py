edbf0cc747e09322fc50dc3b2eabea7925ac001d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:23:32 +0000

    Add package ex04_schachtler/0.2/ex04_schachtler-0.2.tar.gz

---
+++ packages/ex04_schachtler/ex04_schachtler-0.2.tar.gz/ex04_schachtler-0.2/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name = "ex04_schachtler",
+	version = "0.2",
+	py_modules = ["ex04_schachtler"],
+	author = "Saburgin",
+	author_email = "none",
+	url = "none",
+	description = "Einfache Ausgabe verschachtelter Listen"
+)
