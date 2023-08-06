ada27cf78872d745e36d7ad5cbc0f8f7ffed8775
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:42 +0000

    Add package cnamedtuple/0.1.5/cnamedtuple-0.1.5.tar.gz

---
+++ packages/cnamedtuple/cnamedtuple-0.1.5.tar.gz/cnamedtuple-0.1.5/setup.py
@@ -0,0 +1,35 @@
+from distutils.core import setup, Extension
+import sys
+
+long_description = None
+
+if 'upload' in sys.argv:
+    with open('README.rst') as readme:
+        long_description = readme.read()
+
+setup(
+    name='cnamedtuple',
+    version='0.1.5',
+    description='collections.namedtuple implemented in c.',
+    author='Joe Jevnik',
+    author_email='joe@quantopian.com',
+    packages=[
+        'cnamedtuple',
+    ],
+    long_description=long_description,
+    license='Apache 2.0',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Operating System :: OS Independent',
+        'Topic :: Utilities',
+    ],
+    url="https://github.com/llllllllll/cnamedtuple",
+    ext_modules=[
+        Extension('cnamedtuple._namedtuple', ['cnamedtuple/_namedtuple.c']),
+    ],
+)
