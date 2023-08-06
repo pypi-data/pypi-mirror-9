56a67367b9fcf946d7b0aafa40800f2e46a8380c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:03 +0000

    Add package django-sizefield/0.7/django-sizefield-0.7.zip

---
+++ packages/django-sizefield/django-sizefield-0.7.zip/django-sizefield-0.7/setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+import os
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+setup(
+    name='django-sizefield',
+    version='0.7',
+    author='Mathieu Leplatre',
+    author_email='contact@mathieu-leplatre.info',
+    url='https://github.com/leplatrem/django-sizefield',
+    download_url="http://pypi.python.org/pypi/django-sizefield/",
+    description="A model field to store a file size, whose edition and display shows units.",
+    long_description=open(os.path.join(here, 'README.rst')).read() + '\n\n' +
+                     open(os.path.join(here, 'CHANGES')).read(),
+    license='LPGL, see LICENSE file.',
+    install_requires=[
+        'Django',
+    ],
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=['Topic :: Utilities', 
+                 'Natural Language :: English',
+                 'Operating System :: OS Independent',
+                 'Intended Audience :: Developers',
+                 'Environment :: Web Environment',
+                 'Framework :: Django',
+                 'Development Status :: 5 - Production/Stable',
+                 'Programming Language :: Python :: 2.7'],
+)
