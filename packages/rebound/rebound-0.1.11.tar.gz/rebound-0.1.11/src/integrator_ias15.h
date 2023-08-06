d3e6f161a362f0c186a852929c7a108f31e7e1c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:02 +0000

    Add package rebound/0.1.11/rebound-0.1.11.tar.gz

---
+++ packages/rebound/rebound-0.1.11.tar.gz/rebound-0.1.11/src/integrator_ias15.h
@@ -0,0 +1,44 @@
+/**
+ * @file 	integrator_ias15.h
+ * @brief 	Interface for numerical particle integrator
+ * @author 	Hanno Rein <hanno@hanno-rein.de>
+ * 
+ * @section 	LICENSE
+ * Copyright (c) 2015 Hanno Rein
+ *
+ * This file is part of rebound.
+ *
+ * rebound is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * rebound is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with rebound.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+#ifndef _INTEGRATOR_IAS15_H
+#define _INTEGRATOR_IAS15_H
+void integrator_ias15_part1();
+void integrator_ias15_part2();
+void integrator_ias15_synchronize();
+void integrator_ias15_reset();
+
+/*
+ * This parameter controls the accuracy of the integrator.
+ * Set to 0 to make IAS15 a non-adaptive integrator.
+ * Default: 1e-9.
+ **/
+extern double integrator_ias15_epsilon;
+
+/*
+ * The minimum timestep to be used in the adaptive integrator.
+ * Default is 0 (no minimal timestep).
+ **/
+extern double integrator_ias15_min_dt;
+#endif
