657acf26d5575a1b15af58e964484cf531ec164c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:07:59 +0000

    Add package rosbag_pandas/0.3.7.2/rosbag_pandas-0.3.7.2.tar.gz

---
+++ packages/rosbag_pandas/rosbag_pandas-0.3.7.2.tar.gz/rosbag_pandas-0.3.7.2/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(name='rosbag_pandas',
+        version='0.3.7.2',
+        author = 'Adam Taylor',
+        author_email = 'aktaylor08@gmail.com',
+        description='Create a Python pandas data frame from a ros bag file',
+        py_modules=['rosbag_pandas'],
+        scripts=['scripts/bag_graph.py', 'scripts/bag2csv.py'],
+        keywords = ['ROS', 'rosbag', 'pandas'],
+        )
