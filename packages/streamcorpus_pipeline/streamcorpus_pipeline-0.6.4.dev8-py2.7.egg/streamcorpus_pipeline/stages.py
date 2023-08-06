51810b78a034d0a755924251d70b79f7b901434a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:47 +0000

    Add package streamcorpus_pipeline/0.6.4.dev8/streamcorpus_pipeline-0.6.4.dev8-py2.7.egg

---
+++ packages/streamcorpus_pipeline/streamcorpus_pipeline-0.6.4.dev8-py2.7.egg/streamcorpus_pipeline/stages.py
@@ -0,0 +1,415 @@
+#!/usr/bin/env python
+'''Registry of stages for streamcorpus_pipeline.
+
+.. This software is released under an MIT/X11 open source license.
+   Copyright 2012-2014 Diffeo, Inc.
+
+Stages are implemented as callable Python objects.  In almost all
+cases the name of the stage in the configuration file is the same as
+its class name.
+
+Readers
+=======
+
+.. autoclass:: streamcorpus_pipeline._convert_kba_json.convert_kba_json
+.. autoclass:: streamcorpus_pipeline._local_storage.from_local_chunks
+.. autoclass:: streamcorpus_pipeline._local_storage.from_local_files
+.. autoclass:: streamcorpus_pipeline._kvlayer.from_kvlayer
+.. autoclass:: streamcorpus_pipeline._s3_storage.from_s3_chunks
+.. autoclass:: streamcorpus_pipeline._john_smith.john_smith
+.. autoclass:: streamcorpus_pipeline._yaml_files_list.yaml_files_list
+.. autoclass:: streamcorpus_pipeline._spinn3r_feed_storage.from_spinn3r_feed
+.. autoclass:: streamcorpus_pipeline._serifxml.from_serifxml
+
+Incremental transforms
+======================
+
+.. autoclass:: streamcorpus_pipeline._clean_html.clean_html
+.. autoclass:: streamcorpus_pipeline._clean_visible.clean_visible
+.. autoclass:: streamcorpus_pipeline._pdf_to_text.pdf_to_text
+.. autoclass:: streamcorpus_pipeline._docx_to_text.docx_to_text
+.. autoclass:: streamcorpus_pipeline._title.title
+.. autoclass:: streamcorpus_pipeline._filters.debug_filter
+.. autoclass:: streamcorpus_pipeline._filters.filter_domains
+.. autoclass:: streamcorpus_pipeline._filters.filter_domains_substrings
+.. autoclass:: streamcorpus_pipeline._fix_text.fix_text
+.. autoclass:: streamcorpus_pipeline._dedup.dedup
+.. autoclass:: streamcorpus_pipeline._dump_label_stats.dump_label_stats
+.. autoclass:: streamcorpus_pipeline._filters.id_filter
+.. autofunction:: streamcorpus_pipeline._guess_media_type.file_type_stats
+.. autoclass:: streamcorpus_pipeline._filters.filter_languages
+.. autoclass:: streamcorpus_pipeline._filters.filter_tagger_ids
+.. autoclass:: streamcorpus_pipeline._find.find
+.. autoclass:: streamcorpus_pipeline._find.find_doc_ids
+.. autoclass:: streamcorpus_pipeline._guess_media_type.guess_media_type
+.. autoclass:: streamcorpus_pipeline._handle_unconvertible_spinn3r.handle_unconvertible_spinn3r
+.. autoclass:: streamcorpus_pipeline._hyperlink_labels.hyperlink_labels
+.. autoclass:: streamcorpus_pipeline._upgrade_streamcorpus.keep_annotated
+.. autoclass:: streamcorpus_pipeline._language.language
+.. autoclass:: streamcorpus_pipeline._filters.remove_raw
+.. autoclass:: streamcorpus_pipeline._filters.dump_stream_id_abs_url
+.. autoclass:: streamcorpus_pipeline._upgrade_streamcorpus.upgrade_streamcorpus
+.. autoclass:: streamcorpus_pipeline._upgrade_streamcorpus_v0_3_0.upgrade_streamcorpus_v0_3_0
+.. autoclass:: streamcorpus_pipeline._tokenizer.nltk_tokenizer
+
+Batch transforms
+================
+
+.. autoclass:: streamcorpus_pipeline._taggers.byte_offset_align_labels
+.. autoclass:: streamcorpus_pipeline._taggers.line_offset_align_labels
+.. autoclass:: streamcorpus_pipeline._taggers.name_align_labels
+.. autoclass:: streamcorpus_pipeline._lingpipe.lingpipe
+.. autoclass:: streamcorpus_pipeline._serif.serif
+        
+Writers
+=======
+
+.. autoclass:: streamcorpus_pipeline._local_storage.to_local_chunks
+.. autoclass:: streamcorpus_pipeline._local_storage.to_local_tarballs
+.. autoclass:: streamcorpus_pipeline._kvlayer.to_kvlayer
+.. autoclass:: streamcorpus_pipeline._s3_storage.to_s3_chunks
+.. autoclass:: streamcorpus_pipeline._s3_storage.to_s3_tarballs
+
+API
+===
+
+All stage objects are constructed with a single parameter, the
+dictionary of configuration specific to the stage.  The stage objects
+can be passed directly to the ``streamcorpus_pipeline.Pipeline``
+constructor.
+
+.. currentmodule:: streamcorpus_pipeline.stages
+
+.. autoclass:: StageRegistry
+   :members:
+   :show-inheritance:
+
+.. autoclass:: PipelineStages
+   :members:
+   :show-inheritance:
+
+.. autoclass:: Configured
+   :members:
+   :show-inheritance:
+
+.. autoclass:: BatchTransform
+   :members:
+   :show-inheritance:
+
+.. autoclass:: IncrementalTransform
+   :members:
+   :show-inheritance:
+
+'''
+from __future__ import absolute_import
+from abc import ABCMeta, abstractmethod
+from collections import Callable, MutableMapping
+import imp
+import logging
+import time
+
+import pkg_resources
+
+logger = logging.getLogger(__name__)
+
+
+class StageRegistry(MutableMapping):
+    '''List of known stages for a pipeline.
+
+    This is a dictionary, and so ``registry[stagename]`` will retrieve
+    the class object or constructor function for the stage.  Each
+    stage in the dictionary is a callable of a single parameter, the
+    configuration dictionary, which returns a callable appropriate
+    for its stage type.  Typical patterns are actual classes and
+    wrapper functions::
+
+        class a_stage(streamcorpus_pipeline.stages.Configurable):
+            config_name = 'a_stage'
+            def __init__(self, config, *args, **kwargs):
+                super(AStage, self).__init__(config, *args, **kwargs)
+                self.param = self.config.get('param')
+            def __call__(self, si, context):
+                logger.debug('param is %s', self.param)
+                return si
+
+        def b_stage(config):
+            param = config.get('param')
+            def do_stage(si, context):
+               logger.debug('param is %s', param)
+               return si
+            return do_stage
+
+        registry = StageRegistry()
+        registry['a_stage'] = a_stage
+        registry['b_stage'] = b_stage
+
+    '''
+    def __init__(self, *args, **kwargs):
+        super(StageRegistry, self).__init__(*args, **kwargs)
+        self.stages = {}
+
+    # MutableMapping special methods
+
+    def __getitem__(self, k):
+        if k not in self.stages:
+            raise KeyError('%r not in %r' % (k, self.stages.keys()))
+        return self.stages[k]
+
+    def __setitem__(self, k, v):
+        self.stages[k] = v
+
+    def __delitem__(self, k):
+        del self.stages[k]
+
+    def __iter__(self):
+        return self.stages.iterkeys()
+
+    def __len__(self):
+        return len(self.stages)
+
+    # StageRegistry-specific methods
+    def tryload_stage(self, moduleName, functionName, name=None):
+        '''Try to load a stage into self, ignoring errors.
+
+        If loading a module fails because of some subordinate load
+        failure, just give a warning and move on.  On success the
+        stage is added to the stage dictionary.
+
+        :param str moduleName: name of the Python module
+        :param str functionName: name of the stage constructor
+        :param str name: name of the stage, defaults to `functionName`
+
+        '''
+        if name is None:
+            name = functionName
+        try:
+            mod = __import__(moduleName, globals(), locals(), [functionName])
+        except ImportError, exc:
+            logger.warn('cannot load stage {}: cannot load module {}'
+                        .format(name, moduleName), exc_info=exc)
+            return
+
+        if not hasattr(mod, functionName):
+            logger.warn('cannot load stage {}: module {} missing {}'
+                        .format(name, moduleName, functionName))
+            return
+
+        self[name] = getattr(mod, functionName)
+
+    def load_external_stages(self, path):
+        '''Add external stages from the Python module in `path`.
+
+        `path` must be a path to a Python module source that contains
+        a `Stages` dictionary, which is a map from stage name to callable.
+
+        :param str path: path to the module file
+        '''
+        mod = imp.load_source('', path)
+        self.update(mod.Stages)
+
+    def load_module_stages(self, mod):
+        '''Add external stages from the Python module `mod`.
+
+        If `mod` is a string, then it will be interpreted as the name
+        of a module; otherwise it is an actual module object.  The
+        module should exist somewhere in :data:`sys.path`.  The module
+        must contain a `Stages` dictionary, which is a map from stage
+        name to callable.
+
+        :param mod: name of the module or the module itself
+        :raise exceptions.ImportError: if `mod` cannot be loaded or does
+          not contain ``Stages``
+
+        '''
+        if isinstance(mod, basestring):
+            mod = __import__(mod, globals=globals(), locals=locals(),
+                             fromlist=['Stages'], level=0)
+        if not hasattr(mod, 'Stages'):
+            raise ImportError(mod)
+        self.update(mod.Stages)
+
+    def init_stage(self, name, config):
+
+        '''Construct and configure a stage from known stages.
+
+        `name` must be the name of one of the stages in this.  `config`
+        is the configuration dictionary of the containing object, and its `name`
+        member will be passed into the stage constructor.
+
+        :param str name: name of the stage
+        :param dict config: parent object configuration
+        :return: callable stage
+        :raise exceptions.KeyError: if `name` is not a known stage
+
+        '''
+        subconfig = config.get(name, {})
+        ctor = self[name]
+        return ctor(subconfig)
+
+
+class Configured(object):
+    '''Any object containing a configuration.
+
+    The configuration is passed to the constructor.
+
+    .. attribute:: config
+
+        The configuration dictionary for this object.
+
+    '''
+    def __init__(self, config, *args, **kwargs):
+        super(Configured, self).__init__(*args, **kwargs)
+        self.config = config
+
+
+class BatchTransform(Configured):
+    '''Transform that acts on an entire :class:`streamcorpus.Chunk` file.'''
+    __metaclass__ = ABCMeta
+
+    @abstractmethod
+    def process_path(self, chunk_path):
+        '''Process a chunk file at a specific path.
+
+        Implementations should work in place, writing results out to the
+        same `chunk_path`.  The pipeline guarantees that this stage
+        will have a key ``tmp_dir_path`` in its configuration, and this
+        can be used to store an intermediate file which is then renamed
+        over `chunk_path`.
+        '''
+        raise NotImplementedError('BatchTransform.process_path not implemented')
+
+    @abstractmethod
+    def shutdown(self):
+        '''Do an orderly shutdown of this stage.
+
+        If the stage spawns a child process, kill it, and do any other
+        required cleanup.  :meth:`streamcorpus_pipeline.Pipeline._cleanup`
+        will call this method on every batch transform stage, regardless
+        of whether this is the currently running stage or not.
+        '''
+        raise NotImplementedError('shutdown is a required method for all BatchTransforms')
+
+
+class IncrementalTransform(Configured, Callable):
+    '''Transform that acts on individual streamcorpus StreamItem objects.
+
+    The pipeline can run any stage with an appropriate :meth:`__call__`
+    method.  The :meth:`__call__` or :meth:`process_item` methods are
+    called once per stream item, reusing the same transform object.
+
+    '''
+    __metaclass__ = ABCMeta
+    @abstractmethod
+    def process_item(self, stream_item, context):
+        '''Process a single :class:`streamcorpus.StreamItem` object.
+
+        `context` is shared across all stages.  It is guaranteed to
+        contain ``i_str`` nominally containing the original input file
+        name and ``data`` nominally containing the auxiliary data from
+        the work item.
+
+        This function may modify `stream_item` in place, construct
+        a new item and return it, or return :const:`None` to drop
+        the item.
+
+        :param stream_item: stream item to process
+        :paramtype stream_item: :class:`streamcorpus.StreamItem`
+        :param dict context: additional shared context data
+        :return: same or different stream item or :const:`None`
+
+        '''
+        raise NotImplementedError('BatchTransform.process_path not implemented')
+
+    def __call__(self, stream_item, context):
+        '''Entry point from the pipeline.
+
+        This implementation calls :meth:`process_item`.
+
+        '''
+        return self.process_item(stream_item, context)
+
+    def shutdown(self):
+        '''Do an orderly shutdown of this stage.
+
+        Incremental transforms are generally simple Python code and
+        a complicated shutdown is not required.  The pipeline does not
+        call this method.
+        '''
+        pass
+
+
+class PipelineStages(StageRegistry):
+    '''The list of stages, preloaded with streamcorpus_pipeline stages.'''
+    def __init__(self, *args, **kwargs):
+        start = time.time()
+        super(PipelineStages, self).__init__(*args, **kwargs)
+
+        # data source readers (read data from somewhere into pipeline)
+        self.tryload_stage('_convert_kba_json', 'convert_kba_json')
+        self.tryload_stage('_local_storage', 'from_local_chunks')
+        self.tryload_stage('_local_storage', 'from_local_files')
+        self.tryload_stage('_kvlayer', 'from_kvlayer')
+        self.tryload_stage('_s3_storage', 'from_s3_chunks')
+        self.tryload_stage('_john_smith', 'john_smith')
+        self.tryload_stage('_yaml_files_list', 'yaml_files_list')
+        self.tryload_stage('_spinn3r_feed_storage', 'from_spinn3r_feed')
+        self.tryload_stage('_serifxml', 'from_serifxml')
+
+        # StreamItem stages
+        # (alphabetical by stage name)
+        self.tryload_stage('_clean_html', 'clean_html')
+        self.tryload_stage('_clean_visible', 'clean_visible')
+        self.tryload_stage('_pdf_to_text', 'pdf_to_text')
+        self.tryload_stage('_docx_to_text', 'docx_to_text')
+        self.tryload_stage('_title', 'title')
+        self.tryload_stage('_filters', 'debug_filter')
+        self.tryload_stage('_filters', 'filter_domains')
+        self.tryload_stage('_filters', 'filter_domains_substrings')
+        self.tryload_stage('_fix_text', 'fix_text')
+        self.tryload_stage('_dedup', 'dedup')
+        self.tryload_stage('_dump_label_stats', 'dump_label_stats')
+        self.tryload_stage('_filters', 'id_filter')
+        self.tryload_stage('_guess_media_type', 'file_type_stats')
+        self.tryload_stage('_filters', 'filter_languages')
+        self.tryload_stage('_filters', 'filter_tagger_ids')
+        self.tryload_stage('_find', 'find')
+        self.tryload_stage('_find', 'find_doc_ids')
+        self.tryload_stage('_guess_media_type', 'guess_media_type')
+        self.tryload_stage('_handle_unconvertible_spinn3r', 'handle_unconvertible_spinn3r')
+        self.tryload_stage('_hyperlink_labels', 'hyperlink_labels')
+        self.tryload_stage('_upgrade_streamcorpus', 'keep_annotated')
+        self.tryload_stage('_upgrade_streamcorpus', 'keep_annotated', 'keep_annotatoted')
+        self.tryload_stage('_language', 'language')
+        self.tryload_stage('_filters', 'remove_raw')
+        self.tryload_stage('_filters', 'dump_stream_id_abs_url')
+        self.tryload_stage('_set_source', 'set_source')
+        self.tryload_stage('_upgrade_streamcorpus', 'upgrade_streamcorpus')
+        self.tryload_stage('_upgrade_streamcorpus_v0_3_0', 'upgrade_streamcorpus_v0_3_0')
+        self.tryload_stage('_tokenizer', 'nltk_tokenizer')
+
+        # BatchTransform
+        self.tryload_stage('_taggers', 'byte_offset_align_labels')
+        self.tryload_stage('_taggers', 'char_offset_align_labels')
+        #self.tryload_stage('_taggers', 'line_offset_align_labels')
+        self.tryload_stage('_taggers', 'name_align_labels')
+        self.tryload_stage('_taggers', 'multi_token_match_align_labels')
+        self.tryload_stage('_lingpipe', 'lingpipe')
+        self.tryload_stage('_serif', 'serif')
+        
+        # 'writers' move data out of the pipeline
+        self.tryload_stage('_local_storage', 'to_local_chunks')
+        self.tryload_stage('_local_storage', 'to_local_tarballs')
+        self.tryload_stage('_kvlayer', 'to_kvlayer')
+        self.tryload_stage('_s3_storage', 'to_s3_chunks')
+        self.tryload_stage('_s3_storage', 'to_s3_tarballs')
+
+        # load from setuptools 'entry_points' of other installed packages
+        for entry_point in pkg_resources.iter_entry_points('streamcorpus_pipeline.stages'):
+            try:
+                name = entry_point.name
+                stage_constructor = entry_point.load()
+                self[name] = stage_constructor
+            except:
+                logger.error('failure loading plugin entry point: %r', entry_point and entry_point.name, exc_info=True)
+
+        logger.debug('streamcorpus_pipeline.PipelineStages init in %s sec', time.time() - start)
---
+++ packages/streamcorpus_pipeline/streamcorpus_pipeline-0.6.4.dev8-py2.7.egg/streamcorpus_pipeline/tests/test_kvlayer.py
@@ -0,0 +1,207 @@
+'''tests for {to,from}_kvlayer reader/writer stages
+
+.. This software is released under an MIT/X11 open source license.
+   Copyright 2012-2015 Diffeo, Inc.
+'''
+from __future__ import absolute_import
+import contextlib
+import hashlib
+import logging
+
+import kvlayer
+import pytest
+import streamcorpus
+
+from streamcorpus_pipeline._kvlayer import from_kvlayer, to_kvlayer, \
+    serialize_si_key
+from streamcorpus_pipeline._kvlayer_table_names import STREAM_ITEMS_TABLE, \
+    STREAM_ITEM_TABLE_DEFS, STREAM_ITEM_VALUE_DEFS, STREAM_ITEMS_SOURCE_INDEX
+from streamcorpus_pipeline.tests._test_data import get_test_v0_3_0_chunk_path
+import yakonfig
+
+logger = logging.getLogger(__name__)
+
+
+class MiniScp(object):
+    config_name = 'streamcorpus_pipeline'
+    sub_modules = [from_kvlayer, to_kvlayer]
+
+
+@pytest.fixture(scope='function')
+def configurator(request, namespace_string, redis_address):
+    base_config = {
+        'kvlayer': {
+            'storage_type': 'local',
+            'app_name': 'streamcorpus_pipeline',
+            'namespace': namespace_string,
+        },
+        'streamcorpus_pipeline': {
+            'to_kvlayer': {},
+            'from_kvlayer': {},
+        },
+    }
+
+    @contextlib.contextmanager
+    def make_config(overlay={}):
+        config = yakonfig.merge.overlay_config(base_config, overlay)
+        with yakonfig.defaulted_config([kvlayer, MiniScp], config=config):
+            yield
+            client = kvlayer.client()
+            client.delete_namespace()
+    return make_config
+
+
+def test_kvlayer_simple(configurator, tmpdir):
+    abs_url = 'test://test.stream.item/'
+    si = streamcorpus.make_stream_item('2000-01-01T12:34:00.000123Z',
+                                       abs_url)
+    chunkfile = str(tmpdir.join('chunk.sc.xz'))
+    with streamcorpus.Chunk(path=chunkfile, mode='wb') as chunk:
+        chunk.add(si)
+
+    with configurator():
+        writer = to_kvlayer(yakonfig.get_global_config(
+            'streamcorpus_pipeline', 'to_kvlayer'))
+        stream_ids = writer(chunkfile, {}, '')
+        stream_ids = list(stream_ids)
+        assert len(stream_ids) == 1
+        assert (stream_ids[0] ==
+                b'\x98\\\x1e>\xd72V\xcd\x9a9\x99\x19\xfe\x93\xcfv8m\xf48')
+
+        kvlclient = kvlayer.client()
+        kvlclient.setup_namespace(STREAM_ITEM_TABLE_DEFS,
+                                  STREAM_ITEM_VALUE_DEFS)
+        print repr(list(kvlclient.scan_keys(STREAM_ITEMS_TABLE)))
+        for (k, v) in kvlclient.get(
+                STREAM_ITEMS_TABLE,
+                (hashlib.md5(abs_url).digest(), 946730040)):
+            assert v is not None
+
+        reader = from_kvlayer(yakonfig.get_global_config(
+            'streamcorpus_pipeline', 'from_kvlayer'))
+        sis = list(reader(''))
+        assert len(sis) == 1
+        assert sis[0].stream_time.epoch_ticks == si.stream_time.epoch_ticks
+        assert sis[0].abs_url == si.abs_url
+
+        # test streamid "{time}-{hash}" i_str format
+        reader = from_kvlayer(yakonfig.get_global_config(
+            'streamcorpus_pipeline', 'from_kvlayer'))
+        sis = list(reader(stream_ids[0]))
+        assert len(sis) == 1
+        assert sis[0].stream_time.epoch_ticks == si.stream_time.epoch_ticks
+        assert sis[0].abs_url == si.abs_url
+
+
+def test_kvlayer_negative(configurator, tmpdir):
+    si = streamcorpus.make_stream_item('1969-07-20T20:18:00.000000Z',
+                                       'test://test.stream.item/')
+    chunkfile = str(tmpdir.join('chunk.sc.xz'))
+    with streamcorpus.Chunk(path=chunkfile, mode='wb') as chunk:
+        chunk.add(si)
+
+    with configurator():
+        writer = to_kvlayer(yakonfig.get_global_config(
+            'streamcorpus_pipeline', 'to_kvlayer'))
+        stream_ids = writer(chunkfile, {}, '')
+        for sid in stream_ids:
+            pass  # exercise the generator
+
+        reader = from_kvlayer(yakonfig.get_global_config(
+            'streamcorpus_pipeline', 'from_kvlayer'))
+        sis = list(reader(''))
+        assert len(sis) == 1
+        assert sis[0].stream_time.epoch_ticks == si.stream_time.epoch_ticks
+        assert sis[0].abs_url == si.abs_url
+
+
+@contextlib.contextmanager
+def chunks(configurator, test_data_dir, overlay={}):
+    with configurator(overlay):
+        path = get_test_v0_3_0_chunk_path(test_data_dir)
+        config = yakonfig.get_global_config('streamcorpus_pipeline',
+                                            'to_kvlayer')
+        writer = to_kvlayer(config)
+
+        # name_info and i_str are not used by the writer
+        i_str = ''
+        name_info = {}
+        writer(path, name_info, i_str)
+
+        client = kvlayer.client()
+        client.setup_namespace(STREAM_ITEM_TABLE_DEFS, STREAM_ITEM_VALUE_DEFS)
+        yield path, client
+
+
+@pytest.mark.slow
+def test_kvlayer_reader_and_writer(configurator, test_data_dir):
+    with chunks(configurator, test_data_dir) as (path, client):
+        # check that index table was created
+        all_si_keys = []
+        for si_key in client.scan_keys(STREAM_ITEMS_TABLE):
+            all_si_keys.append(si_key)
+        logger.info('%d doc_ids', len(all_si_keys))
+
+        # make a reader
+        config = yakonfig.get_global_config('streamcorpus_pipeline',
+                                            'from_kvlayer')
+        reader = from_kvlayer(config)
+
+        # test it with different i_str inputs:
+        all_keys_istr = (serialize_si_key(all_si_keys[0]) + '<' +
+                         serialize_si_key(all_si_keys[-1]))
+        for i_str in [
+                '',
+                all_keys_istr]:
+            stream_ids = []
+            for si in reader(i_str):
+                stream_ids.append(si.stream_id)
+            _input_chunk_ids = [si.stream_id
+                                for si in streamcorpus.Chunk(path)]
+            input_chunk_ids = list(set(_input_chunk_ids))
+            logger.info('%d inserts, %d unique',
+                        len(_input_chunk_ids), len(input_chunk_ids))
+            input_chunk_ids.sort()
+            stream_ids.sort()
+            assert len(input_chunk_ids) == len(stream_ids)
+            assert input_chunk_ids == stream_ids
+
+
+@pytest.mark.slow
+def test_kvlayer_index_with_source(configurator, test_data_dir):
+    overlay = {
+        'streamcorpus_pipeline': {
+            'to_kvlayer': {
+                'indexes': ['with_source'],
+            },
+        },
+    }
+    with chunks(configurator, test_data_dir, overlay) as (path, client):
+        # Every item in the ...with_source index should match a real item
+        for k, v in client.scan(STREAM_ITEMS_SOURCE_INDEX):
+            assert v == 'WEBLOG'  # by inspection
+            for kk, sixz in client.get(STREAM_ITEMS_TABLE, k):
+                errs, sibytes = streamcorpus.decrypt_and_uncompress(sixz)
+                assert errs == []
+                for si in streamcorpus.Chunk(data=sibytes):
+                    assert si.source == v
+
+
+@pytest.mark.slow
+def test_kvlayer_both_indexes(configurator, test_data_dir):
+    overlay = {
+        'streamcorpus_pipeline': {
+            'to_kvlayer': {
+                'indexes': ['with_source'],
+            },
+        },
+    }
+    with chunks(configurator, test_data_dir, overlay) as (path, client):
+        total = 0
+        for k, v in client.scan(STREAM_ITEMS_SOURCE_INDEX):
+            total += 1
+            count = 0
+            for kk, sixz in client.get(STREAM_ITEMS_TABLE, k):
+                count += 1
+            assert count == 1
+        assert total == 31
