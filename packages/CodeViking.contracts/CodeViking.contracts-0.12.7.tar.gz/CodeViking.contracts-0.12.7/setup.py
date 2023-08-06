d4f726e64d6fceb3dd834e9d12b70300d4c627da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:45 +0000

    Add package CodeViking.contracts/0.12.7/CodeViking.contracts-0.12.7.tar.gz

---
+++ packages/CodeViking.contracts/CodeViking.contracts-0.12.7.tar.gz/CodeViking.contracts-0.12.7/setup.py
@@ -0,0 +1,51 @@
+import sys
+
+from setuptools import setup
+# noinspection PyPep8Naming
+from setuptools.command.test import test as TestCommand
+exec(open('codeviking/contracts/_version.py').read())
+
+class PyTest(TestCommand):
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        # import here, cause outside the eggs aren't loaded
+        import pytest
+
+        errno = pytest.main(self.test_args)
+        sys.exit(errno)
+
+from codeviking.contracts import __version__
+
+LONG_DESCRIPTION = open('README.rst').read()
+
+setup(
+    name='CodeViking.contracts',
+    version=__version__,
+    namespace_packages=['codeviking'],
+    url='https://bitbucket.org/codeviking/python-codeviking.contracts/',
+    author='Dan Bullok',
+    author_email='opensource@codeviking.com',
+    description='Function and method call contracts',
+    long_description=LONG_DESCRIPTION,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+    platforms='any',
+    packages=['codeviking.contracts'],
+    license="MIT",
+    tests_require=['pytest'],
+    cmdclass={'test': PyTest}
+)
