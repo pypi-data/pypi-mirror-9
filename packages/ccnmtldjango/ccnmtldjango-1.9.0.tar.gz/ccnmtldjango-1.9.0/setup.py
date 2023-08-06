24e641804bb9f0078544a79bece5bb37831f121a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:23:11 +0000

    Add package ccnmtldjango/1.9.0/ccnmtldjango-1.9.0.tar.gz

---
+++ packages/ccnmtldjango/ccnmtldjango-1.9.0.tar.gz/ccnmtldjango-1.9.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(name='ccnmtldjango',
+      version="1.9.0",
+      author="Anders Pearson",
+      author_email='anders@columbia.edu',
+      description='Paste template for Django development at CCNMTL',
+      long_description=open('README.md').read(),
+      url='http://github.com/ccnmtl/ccnmtldjango/',
+      packages=find_packages(),
+      install_requires = ["PasteScript"],
+      zip_safe=False,
+      package_data = {'' : ['*.*']},
+      include_package_data=True,
+      entry_points="""
+      [paste.paster_create_template]
+      ccnmtldjango = ccnmtldjango:CcnmtlDjangoTemplate
+      """
+      )
