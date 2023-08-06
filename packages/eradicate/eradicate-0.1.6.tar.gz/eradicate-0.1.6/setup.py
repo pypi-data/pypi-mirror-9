47d5636252e83abf9abce02cf1d2e1d2c7a19af9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:30 +0000

    Add package eradicate/0.1.6/eradicate-0.1.6.tar.gz

---
+++ packages/eradicate/eradicate-0.1.6.tar.gz/eradicate-0.1.6/setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+"""Setup for eradicate."""
+
+from __future__ import unicode_literals
+
+import ast
+from distutils import core
+
+
+def version():
+    """Return version string."""
+    with open('eradicate.py') as input_file:
+        for line in input_file:
+            if line.startswith('__version__'):
+                return ast.parse(line).body[0].value.s
+
+
+with open('README.rst') as readme:
+    core.setup(
+        name='eradicate',
+        version=version(),
+        description='Removes commented-out code.',
+        long_description=readme.read(),
+        license='Expat License',
+        author='Steven Myint',
+        url='https://github.com/myint/eradicate',
+        classifiers=['Environment :: Console',
+                     'Intended Audience :: Developers',
+                     'License :: OSI Approved :: MIT License',
+                     'Programming Language :: Python :: 2.7',
+                     'Programming Language :: Python :: 3',
+                     'Programming Language :: Python :: 3.2',
+                     'Programming Language :: Python :: 3.3',
+                     'Programming Language :: Python :: 3.4',
+                     'Topic :: Software Development :: Quality Assurance'],
+        keywords='clean, format, commented-out code',
+        py_modules=['eradicate'],
+        scripts=['eradicate'])
