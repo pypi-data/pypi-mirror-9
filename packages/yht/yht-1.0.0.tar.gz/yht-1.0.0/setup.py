dbd36be1865e1d5efe1cb2e1d132ec9f5bd45766
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:21 +0000

    Add package yht/1.0.0/yht-1.0.0.tar.gz

---
+++ packages/yht/yht-1.0.0.tar.gz/yht-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+setup (
+    name = 'yht',
+    version = '1.0.0',
+    py_modules = ['yht'],
+    author = 'yhtme',
+    author_email = 'haityang@163.com',
+    url = 'http://www.baidu.com',
+    description = 'a sample',
+    )
+        
