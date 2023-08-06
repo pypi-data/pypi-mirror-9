4894e5f9165907f465c9fee59c8840ebef8a7996
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:33 +0000

    Add package robotframework-jalali/0.0.1/robotframework-jalali-0.0.1.zip

---
+++ packages/robotframework-jalali/robotframework-jalali-0.0.1.zip/robotframework-jalali-0.0.1/robotframework-jalali/jalaliLibrary/__init__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+import jdatetime
+
+__author__ = 'esnaashari'
+
+class JALALILibrary():
+    """
+
+    """
+    ROBOT_LIBRARY_SCOPE = 'TEST SUITE'
+    __version__ = pkg_resources.get_distribution("robotframework-jalalidate").version
+
+    def __init__(self):
+        pass
+
+    def today_date(self):
+		return jdatetime.date.today()
+		
