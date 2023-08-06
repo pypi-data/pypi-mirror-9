a84b9236f1e6128e9dd7cd571d247227e5744cb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:36:24 +0000

    Add package cpc_api/0.1.5/cpc_api-0.1.5-py2-none-any.whl

---
+++ packages/cpc_api/cpc_api-0.1.5-py2-none-any.whl/cpc_api/api.py
@@ -0,0 +1,80 @@
+# -*- coding: utf-8 -*-
+
+from operator import itemgetter
+import requests
+import warnings
+
+with warnings.catch_warnings():
+    warnings.filterwarnings("ignore", category=UserWarning)
+    from fuzzywuzzy.process import extractBests
+
+
+__all__ = ['CPCApi']
+
+
+def memoize(f):
+    cache = {}
+
+    def aux(*args, **kargs):
+        k = (args, tuple(sorted(kargs.items())))
+        if k not in cache:
+            cache[k] = f(*args, **kargs)
+        return cache[k]
+    return aux
+
+
+class CPCApi(object):
+    format = 'json'
+
+    def __init__(self, ptype='depute', legislature=None):
+        """
+        type: depute or senateur
+        legislature: 2007-2012 or None
+        """
+
+        assert(ptype in ['depute', 'senateur'])
+        assert(legislature in ['2007-2012', None])
+
+        self.ptype = ptype
+        self.ptype_plural = ptype + 's'
+        self.base_url = 'http://%s.nos%s.fr' % (legislature or 'www', self.ptype_plural)
+
+    def synthese(self, month=None):
+        """
+        month format: YYYYMM
+        """
+        if month is None:
+            month = 'data'
+        url = '%s/synthese/%s/%s' % (self.base_url, month, self.format)
+
+        data = requests.get(url).json()
+        return [depute[self.ptype] for depute in data[self.ptype_plural]]
+
+    def parlementaire(self, slug_name):
+        url = '%s/%s/%s' % (self.base_url, slug_name, self.format)
+        return requests.get(url).json()[self.ptype]
+
+    def picture(self, slug_name, pixels='60'):
+        return requests.get(self.picture_url(slug_name, pixels=pixels))
+
+    def picture_url(self, slug_name, pixels='60'):
+        return '%s/%s/photo/%s/%s' % (self.base_url, self.ptype, slug_name, pixels)
+
+    def search(self, q, page=1):
+        # XXX : the response with json format is not a valid json :'(
+        # Temporary return csv raw data
+        url = '%s/recherche/%s?page=%s&format=%s' % (self.base_url, q, page, 'csv')
+        return requests.get(url).content
+
+    @memoize
+    def parlementaires(self, active=None):
+        if active is None:
+            url = '%s/%s/%s' % (self.base_url, self.ptype_plural, self.format)
+        else:
+            url = '%s/%s/enmandat/%s' % (self.base_url, self.ptype_plural, self.format)
+
+        data = requests.get(url).json()
+        return [depute[self.ptype] for depute in data[self.ptype_plural]]
+
+    def search_parlementaires(self, q, field='nom', limit=5):
+        return extractBests(q, self.parlementaires(), processor=itemgetter(field), limit=limit)
