c5b3480b5654a1e978a82ec3dc1369869c807c4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:54 +0000

    Add package sendgrid-django/1.2.0/sendgrid-django-1.2.0.tar.gz

---
+++ packages/sendgrid-django/sendgrid-django-1.2.0.tar.gz/sendgrid-django-1.2.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+__version__ = None
+with open('sgbackend/version.py') as f:
+    exec(f.read())
+
+setup(
+    name='sendgrid-django',
+    version=str(__version__),
+    author='Yamil Asusta',
+    author_email='yamil@sendgrid.com',
+    url='https://github.com/elbuo8/sendgrid-django',
+    packages=find_packages(),
+    license='MIT',
+    description='SendGrid Backend for Django',
+    long_description=open('./README.rst').read(),
+    install_requires=["sendgrid==1.3.0"],
+)
