5e3accce5696ace3c76d89763b7e70f88321c769
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:19 +0000

    Add package nester_tad_pimp/1.4.0/nester_tad_pimp-1.4.0.zip

---
+++ packages/nester_tad_pimp/nester_tad_pimp-1.4.0.zip/nester_tad_pimp-1.4.0/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='nester_tad_pimp',
+    version='1.4.0',
+    author='Thiago Dias',
+    author_email='thgdias@gmail.com',
+    description='Testing nester... use your head, python!'
+)
