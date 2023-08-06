358d5f097dcb8d98fc9d5fa0d965564e52f34b99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:22 +0000

    Add package weppy-BS3/0.2/weppy-BS3-0.2.tar.gz

---
+++ packages/weppy-BS3/weppy-BS3-0.2.tar.gz/weppy-BS3-0.2/setup.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+weppy-BS3
+---------
+
+Add bootstrap3 ui elements to Weppy.
+"""
+
+from setuptools import setup
+
+setup(
+    name='weppy-BS3',
+    version='0.2',
+    url='https://github.com/gi0baro/weppy-bs3',
+    license='BSD',
+    author='Giovanni Barillari',
+    author_email='gi0baro@d4net.org',
+    description='Bootstrap3 UI elements for weppy',
+    long_description=__doc__,
+    packages=['weppy_bs3'],
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    install_requires=[
+        'weppy',
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
