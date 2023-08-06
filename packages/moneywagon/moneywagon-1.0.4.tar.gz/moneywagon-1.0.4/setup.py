791eafd6a809cf696c355f6c555fd110af599981
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:41 +0000

    Add package moneywagon/1.0.4/moneywagon-1.0.4.tar.gz

---
+++ packages/moneywagon/moneywagon-1.0.4.tar.gz/moneywagon-1.0.4/setup.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    name="moneywagon",
+    version='1.0.4',
+    description='Toolset for getting the exchange rate for cryptocurrencies',
+    long_description=open('README.md').read(),
+    author='Chris Priest',
+    author_email='cp368202@ohiou.edu',
+    url='https://github.com/priestc/moneywagon',
+    packages=find_packages(),
+    scripts=['bin/moneywagon'], # coming soon
+    include_package_data=True,
+    license='LICENSE',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+    ],
+    install_requires=[
+        'requests',
+        'pytz',
+        'arrow'
+    ]
+)
