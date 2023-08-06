9dc5c0626794973c2f94947c37a26ea24d504580
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:06 +0000

    Add package seapi/0.1.1.dev1/seapi-0.1.1.dev1.tar.gz

---
+++ packages/seapi/seapi-0.1.1.dev1.tar.gz/seapi-0.1.1.dev1/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages  # Always prefer setuptools over distutils
+from os import path
+
+setup(
+    name='seapi',
+
+    version='0.1.1.dev1',
+
+    description='a python library to query SiliconExpert API to gain information about electronic components and manufacturers/suppliers',
+
+    url='https://github.com/biwa7636/seapi',
+
+    author='Gayatri Ghanakota, Richard Rymer, Bin Wang',
+    author_email='binwang.cu@gmail.com',
+
+    license='MIT',
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+    ],
+
+    keywords='api siliconexpert components',
+
+    packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
+
+    install_requires=['oauth2'],
+
+    entry_points={
+        'console_scripts': [
+            'seapi=seapi:main',
+        ],
+    },
+)
