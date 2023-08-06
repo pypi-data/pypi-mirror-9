2e9cff5cda318926f228bea352f0329582f517ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:52 +0000

    Add package docker-fabric/0.3.2/docker-fabric-0.3.2.tar.gz

---
+++ packages/docker-fabric/docker-fabric-0.3.2.tar.gz/docker-fabric-0.3.2/dockerfabric/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+
+__version__ = '0.3.2'
+
+DEFAULT_SOCAT_VERSION = '1.7.2.4'
---
+++ packages/docker-fabric/docker-fabric-0.3.2.tar.gz/docker-fabric-0.3.2/setup.py
@@ -0,0 +1,50 @@
+from distutils.spawn import find_executable
+import os
+from setuptools import setup, find_packages
+
+from dockerfabric import __version__
+
+
+def include_readme():
+    try:
+        import pandoc
+    except ImportError:
+        return ''
+    pandoc.core.PANDOC_PATH = find_executable('pandoc')
+    readme_file = os.path.join(os.path.dirname(__file__), 'README.md')
+    doc = pandoc.Document()
+    with open(readme_file, 'r') as rf:
+        doc.markdown = rf.read()
+        return doc.rst
+
+
+setup(
+    name='docker-fabric',
+    version=__version__,
+    packages=find_packages(),
+    install_requires=['six', 'Fabric>=1.8.0', 'docker-py>=0.5.0', 'docker-map>=0.3.0'],
+    extras_require={
+        'yaml': ['PyYAML'],
+    },
+    license='MIT',
+    author='Matthias Erll',
+    author_email='matthias@erll.de',
+    url='https://github.com/merll/docker-fabric',
+    description='Integration of Docker deployments into Fabric.',
+    long_description=include_readme(),
+    platforms=['OS Independent'],
+    keywords=['docker', 'fabric'],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Software Development :: Build Tools',
+        'Topic :: System :: Software Distribution',
+        'Development Status :: 4 - Beta',
+        'Programming Language :: Python :: 2.7',
+    ],
+    include_package_data=True,
+)
