78fe35b63e3534b7599c6fd22ad6c3e5f554d88f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:56 +0000

    Add package unittester/13/unittester-13.tar.gz

---
+++ packages/unittester/unittester-13.tar.gz/unittester-13/setup.py
@@ -0,0 +1,17 @@
+# coding=utf-8
+"""
+appinstance
+Active8 (04-03-15)
+license: GNU-GPL2
+"""
+from setuptools import setup
+setup(name='unittester',
+      version='13',
+      description='Run python unit-tests as command-line applications (class and method based).',
+      url='https://github.com/erikdejonge/unittester',
+      author='Erik de Jonge',
+      author_email='erik@a8.nl',
+      license='GPL',
+      packages=['unittester'],
+      zip_safe=True,
+      install_requires=['consoleprinter', 'pyprofiler'])
