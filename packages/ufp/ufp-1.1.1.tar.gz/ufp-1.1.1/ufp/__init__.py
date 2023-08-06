6b020220a4f0bb037fad6d23f643e0c95d258f27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:19 +0000

    Add package ufp/1.1.1/ufp-1.1.1.tar.gz

---
+++ packages/ufp/ufp-1.1.1.tar.gz/ufp-1.1.1/ufp/__init__.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+"""
+====================================
+ :mod:`mod2` 모듈2 
+====================================
+.. moduleauthor:: 별님
+.. note:: Future's GPL License
+
+@package ufp
+@brief 사용자 함수 묶음.
+@version v4\n
+	bash -> c++ -> (qt ->) python
+@author 별님 <w7dn1ng75r@gmail.com>
+@details \n
+	홈페이지 : http://thestars3.tistory.com/
+"""
+
+from __future__ import unicode_literals, absolute_import
+
+__title__ = 'ufp'
+__version__ = '1.1.1'
+__author__ = '별님'
+__license__ = 'GPL v3'
+__copyright__ = 'Copyright 2015 별님'
+
+from .ufp import *
+
+#하위 호환성 유지를 위한 설정
+from . import _compatibleness
