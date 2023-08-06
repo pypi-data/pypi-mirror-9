84e9de4d14312244849069e72a5a8184a62c003f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:37:31 +0000

    Add package django-mailgun-provider/0.2.3/django-mailgun-provider-0.2.3.tar.gz

---
+++ packages/django-mailgun-provider/django-mailgun-provider-0.2.3.tar.gz/django-mailgun-provider-0.2.3/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+import os
+import platform
+
+DESCRIPTION = "A Django email backend for Mailgun"
+
+LONG_DESCRIPTION = None
+try:
+    LONG_DESCRIPTION = open('README.rst').read()
+except:
+    pass
+
+CLASSIFIERS = [
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Topic :: Software Development :: Libraries :: Python Modules',
+    'Framework :: Django',
+]
+
+setup(
+    name='django-mailgun-provider',
+    version='0.2.3',
+    packages=['django_mailgun'],
+    author='Bradley Whittington',
+    author_email='radbrad182@gmail.com',
+    url='http://github.com/bradwhittington/django-mailgun/',
+    license='MIT',
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    platforms=['any'],
+    install_requires=['requests'],
+    classifiers=CLASSIFIERS,
+)
