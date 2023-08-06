6c7640f43302c0854b1f6a05cde13078dc053553
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:43 +0000

    Add package django-entity-history/0.2.1/django_entity_history-0.2.1-py2.py3-none-any.whl

---
+++ packages/django-entity-history/django_entity_history-0.2.1-py2.py3-none-any.whl/entity_history/__init__.py
@@ -0,0 +1,8 @@
+# flake8: noqa
+from .version import __version__
+
+from .models import (
+    EntityActivationEvent, EntityRelationshipActivationEvent, get_sub_entities_at_times, get_entities_at_times, EntityHistory
+)
+
+django_app_config = 'entity_history.apps.EntityHistoryConfig'
