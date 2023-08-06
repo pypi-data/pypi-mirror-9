819c86acd78bc5f99bb91db4585dfaa1d0a310b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:43 +0000

    Add package udemy-dl/0.1.3/udemy-dl-0.1.3.tar.gz

---
+++ packages/udemy-dl/udemy-dl-0.1.3.tar.gz/udemy-dl-0.1.3/src/udemy_dl/download.py
@@ -0,0 +1,44 @@
+import os
+import sys
+import wget
+import subprocess
+import requests
+
+class DLException(Exception):
+    pass
+
+def download(link, filename):
+    if 'youtube.com' in link:
+        youtube_dl(link, filename)
+    else:
+        try:
+            curl_dl(link, filename)
+        except OSError:
+            if not os.path.exists(filename):
+                wget.download(link, filename)
+            else:
+                raise DLException('Failed to download this lecture')
+
+
+def curl_dl(link, filename):
+    command = ['curl', '-C', '-', link, '-o', filename,'--insecure']
+
+    cert_path = requests.certs.where()
+    if cert_path:
+        command.extend(['--cacert', cert_path])
+    subprocess.call(command)
+
+
+def dl_progress(num_blocks, block_size, total_size):
+    progress = num_blocks * block_size * 100 / total_size
+    if num_blocks != 0:
+        sys.stdout.write(4 * '\b')
+    sys.stdout.write('%3d%%' % (progress))
+
+
+def youtube_dl(link, filename):
+    try:
+        subprocess.call(['youtube-dl', '-o', filename, link])
+    except OSError:
+        raise DLException('Install youtube-dl to download this lecture')
+
