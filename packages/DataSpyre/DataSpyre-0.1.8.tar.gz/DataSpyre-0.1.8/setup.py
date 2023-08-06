6e9446e7edac59a57afa672865a521a41fc9b8f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:32 +0000

    Add package DataSpyre/0.1.8/DataSpyre-0.1.8.tar.gz

---
+++ packages/DataSpyre/DataSpyre-0.1.8.tar.gz/DataSpyre-0.1.8/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='DataSpyre',
+    version='0.1.8',
+    description='Spyre makes it easy to build interactive web applications, and requires no knowledge of HTML, CSS, or Javascript.',
+    url='https://github.com/adamhajari/spyre',
+    author='Adam Hajari',
+    author_email='adam@nextbigsound.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Framework :: CherryPy',
+        'Intended Audience :: Education',
+        'Intended Audience :: Science/Research',
+        'Environment :: Web Environment',
+        'Topic :: Scientific/Engineering',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+    ],
+    keywords='web application template data visualization',
+    include_package_data = True,    # include everything in source control
+    packages = ['spyre'],  # include all packages under src
+    package_data = {
+        '': ['*.js','*.css','*.html'],
+        'public': ['js/*.js','css/*.css'],
+    },
+    install_requires=[
+        "pandas",
+        "cherrypy",
+        "jinja2",
+        "matplotlib",
+    ]
+)
