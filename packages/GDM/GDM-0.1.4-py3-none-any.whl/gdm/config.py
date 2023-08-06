5b5ea739fc2bd101c76bceda1067a1ec9f64a165
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:07:59 +0000

    Add package GDM/0.1.4/GDM-0.1.4-py3-none-any.whl

---
+++ packages/GDM/GDM-0.1.4-py3-none-any.whl/gdm/config.py
@@ -0,0 +1,157 @@
+"""Wrappers for the dependency configuration files."""
+
+import os
+import sys
+import shutil
+import logging
+
+import yorm
+
+from . import common
+from .shell import ShellMixin, GitMixin
+
+logging.getLogger('yorm').setLevel(logging.INFO)
+log = common.logger(__name__)
+
+
+@yorm.map_attr(repo=yorm.standard.String)
+@yorm.map_attr(dir=yorm.standard.String)
+@yorm.map_attr(rev=yorm.standard.String)
+@yorm.map_attr(link=yorm.standard.String)
+class Source(yorm.extended.AttributeDictionary, ShellMixin, GitMixin):
+
+    """A dictionary of `git` and `ln` arguments."""
+
+    def __init__(self, repo, dir, rev='master', link=None):  # pylint: disable=W0622
+        super().__init__()
+        self.repo = repo
+        self.dir = dir
+        self.rev = rev
+        self.link = link
+        if not self.repo:
+            raise ValueError("'repo' missing on {}".format(repr(self)))
+        if not self.dir:
+            raise ValueError("'dir' missing on {}".format(repr(self)))
+
+    def __repr__(self):
+        return "<source {}>".format(self)
+
+    def __str__(self):
+        fmt = "'{r}' @ '{v}' in '{d}'"
+        if self.link:
+            fmt += " <- '{s}'"
+        return fmt.format(r=self.repo, v=self.rev, d=self.dir, s=self.link)
+
+    def update_files(self, force=False):
+        """Ensure the source matches the specified revision."""
+        log.info("updating source files...")
+
+        # Fetch the latest changes and revert the working tree if it exists
+        if os.path.exists(self.dir):
+            self.cd(self.dir)
+            if self.git_changes() and not force:
+                sys.exit("\n" + "uncomitted changes"
+                         " ('--force' to overwrite): {}".format(os.getcwd()))
+            self.git_revert()
+            self.git_fetch(self.repo)
+
+        # If it doesn't exist, clone a new one
+        else:
+            self.git_clone(self.repo, self.dir)
+            self.cd(self.dir)
+
+        # Update the working tree to the specified revision
+        self.git_update(self.rev)
+
+    def create_link(self, root, force=False):
+        """Create a link from the target name to the current directory."""
+        if self.link:
+            log.info("creating a symbolic link...")
+            target = os.path.join(root, self.link)
+            source = os.path.relpath(os.getcwd(), os.path.dirname(target))
+            if os.path.islink(target):
+                os.remove(target)
+            elif os.path.exists(target):
+                if force:
+                    shutil.rmtree(target)
+                else:
+                    sys.exit("\n" + "preexisting link location"
+                             " ('--force' to overwrite): {}".format(target))
+            self.ln(source, target)
+
+
+@yorm.map_attr(all=Source)
+class Sources(yorm.container.List):
+
+    """A list of source dependencies."""
+
+
+@yorm.map_attr(location=yorm.standard.String)
+@yorm.map_attr(sources=Sources)
+@yorm.store_instances("{self.root}/{self.filename}")
+class Config(ShellMixin):
+
+    """A dictionary of dependency configuration options."""
+
+    FILENAMES = ('gdm.yml', 'gdm.yaml', '.gdm.yml', '.gdm.yaml')
+
+    def __init__(self, root, filename=FILENAMES[0], location='gdm_sources'):
+        super().__init__()
+        self.root = root
+        self.filename = filename
+        self.location = location
+        self.sources = []
+
+    @property
+    def path(self):
+        """Get the full path to the configuration file."""
+        return os.path.join(self.root, self.filename)
+
+    def install_deps(self, force=False):
+        """Get all sources, recursively."""
+        path = os.path.join(self.root, self.location)
+
+        if not self.indent:
+            print()
+
+        if not os.path.isdir(path):
+            self.mkdir(path)
+        self.cd(path)
+        print()
+
+        count = 0
+        for source in self.sources:
+
+            source.indent = self.indent + self.INDENT
+            source.update_files(force=force)
+            source.create_link(self.root, force=force)
+            count += 1
+            print()
+
+            count += install_deps(root=os.getcwd(),
+                                  indent=source.indent + self.INDENT)
+
+            self.cd(path, visible=False)
+
+        return count
+
+
+def load(root):
+    """Load the configuration for the current project."""
+    config = None
+    for filename in os.listdir(root):
+        if filename.lower() in Config.FILENAMES:
+            config = Config(root, filename)
+            log.debug("loaded config: %s", config.path)
+            break
+    return config
+
+
+def install_deps(root, indent=0, force=False):
+    """Install the dependences listed in the project's configuration file."""
+    config = load(root)
+    if config:
+        config.indent = indent
+        return config.install_deps(force=force)
+    else:
+        return 0
---
+++ packages/GDM/GDM-0.1.4-py3-none-any.whl/gdm/shell.py
@@ -0,0 +1,123 @@
+"""Utilities to call shell programs."""
+
+import os
+import sys
+import logging
+
+from sh import Command, ErrorReturnCode
+
+from . import common
+
+logging.getLogger('sh').setLevel(logging.WARNING)
+log = common.logger(__name__)
+
+
+def _call(name, *args, catch=True):
+    """Call a shell program with arguments."""
+    if name == 'mkdir' and len(args) == 2 and args[0] == '-p':
+        os.makedirs(args[1])
+    elif name == 'cd' and len(args) == 1:
+        os.chdir(args[0])
+    else:
+        try:
+            program = Command(name)
+            program(*args,
+                    _err=lambda line: log.debug(line.strip()),
+                    _out=lambda line: log.debug(line.strip()))
+        except ErrorReturnCode as exc:
+            msg = "\n  IN: '{}'{}".format(os.getcwd(), exc)
+            if catch:
+                sys.exit(msg)
+            else:
+                raise common.CallException(msg)
+
+
+class _Base:
+
+    """Functions to call shell commands."""
+
+    INDENT = 2
+    indent = 0
+
+    def _call(self, *args, visible=True, catch=True):
+        if visible:
+            self._display_in(*args)
+        _call(*args, catch=catch)
+
+    def _display_in(self, *args):
+        print("{}$ {}".format(' ' * self.indent, ' '.join(args)))
+
+
+class ShellMixin(_Base):
+
+    """Provides classes with shell utilities."""
+
+    def mkdir(self, path):
+        self._call('mkdir', '-p', path)
+
+    def cd(self, path, visible=True):
+        self._call('cd', path, visible=visible)
+
+    def ln(self, source, target):
+        dirpath = os.path.dirname(target)
+        if not os.path.isdir(dirpath):
+            self.mkdir(dirpath)
+        self._call('ln', '-s', source, target)
+
+
+class GitMixin(_Base):
+
+    """Provides classes with Git utilities."""
+
+    def git_clone(self, repo, dir):  # pylint: disable=W0622
+        """Clone a new working tree."""
+        self._clone(repo, dir)
+
+    def git_fetch(self, repo):
+        """Fetch the latest changes from the remote repository."""
+        self._fetch(repo)
+
+    def git_changes(self):
+        """Determine if there are changes in the working tree."""
+        try:
+            kwargs = {'visible': False, 'catch': False}
+            self._git('update-index', '-q', '--refresh', **kwargs)
+            self._git('diff-files', '--quiet', **kwargs)
+            self._git('diff-index', '--cached', '--quiet', 'HEAD', **kwargs)
+        except common.CallException:
+            return True
+        else:
+            return False
+
+    def git_revert(self):
+        """Revert all changes in the working tree."""
+        self._stash(visible=False)
+        self._reset()
+        self._clean(visible=False)
+
+    def git_update(self, rev):
+        """Update the working tree to the specified revision."""
+        self._checkout(rev)
+
+    def _git(self, *args, **kwargs):
+        self._call('git', *args, **kwargs)
+
+    def _clone(self, repo, dir):  # pylint: disable=W0622
+        self._git('clone', repo, dir)
+
+    def _fetch(self, repo):
+        self._git('remote', 'remove', 'origin', visible=False)
+        self._git('remote', 'add', 'origin', repo)
+        self._git('fetch', '--all', '--tags', '--force', '--prune')
+
+    def _stash(self, visible=True):
+        self._git('stash', visible=visible)
+
+    def _clean(self, visible=True):
+        self._git('clean', '--force', '-d', '-x', visible=visible)
+
+    def _reset(self):
+        self._git('reset', '--hard')
+
+    def _checkout(self, rev):
+        self._git('checkout', rev)
