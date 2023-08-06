095b8410252a1e06830436f1a79b6e46f4221abb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:55 +0000

    Add package django-sqlpaginator-unknown/0.1.5/django-sqlpaginator-unknown-0.1.5.tar.gz

---
+++ packages/django-sqlpaginator-unknown/django-sqlpaginator-unknown-0.1.5.tar.gz/django-sqlpaginator-unknown-0.1.5/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+__version__ = ''
+execfile('sqlpaginator/version.py')
+
+description = '''django app that does pagination and ordering using raw
+sql on a Model. It has the same API as the django.core.pagination.Paginator'''
+
+setup(
+    name='django-sqlpaginator-unknown',
+    packages=['sqlpaginator'],
+    version=__version__,
+    description=description,
+    author='Bulkan Evcimen',
+    author_email='bulkan@gmail.com',
+    url='https://github.com/bulkan/django-sqlpaginator',
+    #dependency_links=["https://github.com/andialbrecht/sqlparse/tarball/master"],
+    install_requires=[
+        #'distribute',
+        'sqlparse'
+    ],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7'
+    ]
+)
