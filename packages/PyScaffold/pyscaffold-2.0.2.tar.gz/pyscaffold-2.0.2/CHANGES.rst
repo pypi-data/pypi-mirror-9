d8beddb77a2d10e0a539c3d94a8b53f9487ae7d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:00 +0000

    Add package PyScaffold/2.0.2/pyscaffold-2.0.2.tar.gz

---
+++ packages/PyScaffold/pyscaffold-2.0.2.tar.gz/pyscaffold-2.0.2/CHANGES.rst
@@ -0,0 +1,179 @@
+=============
+Release Notes
+=============
+
+Version 2.0.2, 2015-03-04
+=========================
+
+- FIX: Version retrieval with setup.py install
+- argparse example for version retrieval in skeleton.py
+- FIX: import my_package should be quiet (verbose=False)
+
+Version 2.0.1, 2015-02-27
+=========================
+
+- FIX: Installation bug under Windows 7
+
+Version 2.0, 2015-02-25
+=======================
+
+- Split configuration and logic into setup.cfg and setup.py
+- Removed .pre from version string (newer PEP 440)
+- FIX: Sphinx now works if package name does not equal project name
+- Allow namespace packages with --with-namespace
+- Added a skeleton.py as a console_script template
+- Set `v0.0` as initial tag to support PEP440 version inference
+- Integration of the Versioneer functionality into setup.py
+- Usage of `data_files` configuration instead of `MANIFEST.in`
+- Allow configuration of `package_data` in `setup.cfg`
+- Link from Sphinx docs to AUTHORS.rst
+
+Version 1.4, 2014-12-16
+=======================
+
+- Added numpydoc flag --with-numpydoc
+- Fix: Add django to requirements if --with-django
+- Fix: Don't overwrite index.rst during update
+
+Version 1.3.2, 2014-12-02
+=========================
+
+- Fix: path of Travis install script
+
+Version 1.3.1, 2014-11-24
+=========================
+
+- Fix: --with-tox tuple bug #28
+
+Version 1.3, 2014-11-17
+=======================
+
+- Support for Tox (https://tox.readthedocs.org/)
+- flake8: exclude some files
+- Usage of UTF8 as file encoding
+- Fix: create non-existent files during update
+- Fix: unit tests on MacOS
+- Fix: unit tests on Windows
+- Fix: Correct version when doing setup.py install
+
+Version 1.2, 2014-10-13
+=======================
+
+- Support pre-commit hooks (http://pre-commit.com/)
+
+Version 1.1, 2014-09-29
+=======================
+
+- Changed COPYING to LICENSE
+- Support for all licenses from http://choosealicense.com/
+- Fix: Allow update of license again
+- Update to Versioneer 0.12
+
+Version 1.0, 2014-09-05
+=======================
+
+- Fix when overwritten project has a git repository
+- Documentation updates
+- License section in Sphinx
+- Django project support with --with-django flag
+- Travis project support with --with-travis flag
+- Replaced sh with own implementation
+- Fix: new `git describe` version to PEP440 conversion
+- conf.py improvements
+- Added source code documentation
+- Fix: Some Python 2/3 compatibility issues
+- Support for Windows
+- Dropped Python 2.6 support
+- Some classifier updates
+
+Version 0.9, 2014-07-27
+=======================
+
+- Documentation updates due to RTD
+- Added a --force flag
+- Some cleanups in setup.py
+
+Version 0.8, 2014-07-25
+=======================
+
+- Update to Versioneer 0.10
+- Moved sphinx-apidoc from setup.py to conf.py
+- Better support for `make html`
+
+Version 0.7, 2014-06-05
+=======================
+
+- Added Python 3.4 tests and support
+- Flag --update updates only some files now
+- Usage of setup_requires instead of six code
+
+Version 0.6.1, 2014-05-15
+=========================
+
+- Fix: Removed six dependency in setup.py
+
+Version 0.6, 2014-05-14
+=======================
+
+- Better usage of six
+- Return non-zero exit status when doctests fail
+- Updated README
+- Fixes in Sphinx Makefile
+
+Version 0.5, 2014-05-02
+=======================
+
+- Simplified some Travis tests
+- Nicer output in case of errors
+- Updated PyScaffold's own setup.py
+- Added --junit_xml and --coverage_xml/html option
+- Updated .gitignore file
+
+Version 0.4.1, 2014-04-27
+=========================
+
+- Problem fixed with pytest-cov installation
+
+Version 0.4, 2014-04-23
+=======================
+
+- PEP8 and PyFlakes fixes
+- Added --version flag
+- Small fixes and cleanups
+
+Version 0.3, 2014-04-18
+=======================
+
+- PEP8 fixes
+- More documentation
+- Added update feature
+- Fixes in setup.py
+
+Version 0.2, 2014-04-15
+=======================
+
+- Checks when creating the project
+- Fixes in COPYING
+- Usage of sh instead of GitPython
+- PEP8 fixes
+- Python 3 compatibility
+- Coverage with Coverall.io
+- Some more unittests
+
+Version 0.1.2, 2014-04-10
+=========================
+
+- Bugfix in Manifest.in
+- Python 2.6 problems fixed
+
+Version 0.1.1, 2014-04-10
+=========================
+
+- Unittesting with Travis
+- Switch to string.Template
+- Minor bugfixes
+
+Version 0.1, 2014-04-03
+=======================
+
+- First release
---
+++ packages/PyScaffold/pyscaffold-2.0.2.tar.gz/pyscaffold-2.0.2/pyscaffold/runner.py
@@ -0,0 +1,203 @@
+# -*- coding: utf-8 -*-
+"""
+Command-Line-Interface of PyScaffold
+"""
+from __future__ import absolute_import, print_function
+
+import argparse
+import os.path
+import sys
+
+import pyscaffold
+from six.moves import range
+
+from . import info, repo, shell, structure, templates, utils
+
+__author__ = "Florian Wilhelm"
+__copyright__ = "Blue Yonder"
+__license__ = "new BSD"
+
+
+def parse_args(args):
+    """
+    Parse command line parameters
+
+    :param args: command line parameters as list of strings
+    :return: command line parameters as :obj:`argparse.Namespace`
+    """
+    parser = argparse.ArgumentParser(
+        description="PyScaffold is a tool for easily putting up the scaffold "
+                    "of a Python project.")
+    parser.add_argument(
+        dest="project",
+        help="project name",
+        metavar="PROJECT")
+    parser.add_argument(
+        "-p",
+        "--package",
+        dest="package",
+        required=False,
+        default=None,
+        help="package name (default: project name)",
+        metavar="NAME")
+    parser.add_argument(
+        "-d",
+        "--description",
+        dest="description",
+        required=False,
+        default=None,
+        help="package description (default: '')",
+        metavar="TEXT")
+    parser.add_argument(
+        "-u",
+        "--url",
+        dest="url",
+        required=False,
+        default=None,
+        help="package url (default: '')",
+        metavar="URL")
+    license_choices = templates.licenses.keys()
+    parser.add_argument(
+        "-l",
+        "--license",
+        dest="license",
+        choices=license_choices,
+        required=False,
+        default=None,
+        help="package license from {choices} (default: {default})".format(
+            choices=str(license_choices), default="No license"),
+        metavar="LICENSE")
+    parser.add_argument(
+        "-f",
+        "--force",
+        dest="force",
+        action="store_true",
+        default=False,
+        help="force overwriting an existing directory")
+    parser.add_argument(
+        "--update",
+        dest="update",
+        action="store_true",
+        default=False,
+        help="update an existing project by replacing the most important files"
+             " like setup.py, versioneer.py etc. Use additionally --force to "
+             "replace all scaffold files.")
+    parser.add_argument(
+        "--with-travis",
+        dest="travis",
+        action="store_true",
+        default=False,
+        help="generate Travis configuration files")
+    parser.add_argument(
+        "--with-django",
+        dest="django",
+        action="store_true",
+        default=False,
+        help="generate Django project files")
+    parser.add_argument(
+        "--with-pre-commit",
+        dest="pre_commit",
+        action="store_true",
+        default=False,
+        help="generate pre-commit configuration file")
+    parser.add_argument(
+        "--with-tox",
+        dest="tox",
+        action="store_true",
+        default=False,
+        help="generate Tox configuration file")
+    parser.add_argument(
+        "--with-numpydoc",
+        dest="numpydoc",
+        action="store_true",
+        default=False,
+        help="add numpydoc to Sphinx configuration file")
+    parser.add_argument(
+        "--with-namespace",
+        dest="namespace",
+        default=None,
+        help="put your project inside a namespace package",
+        metavar="NS1[.NS2]")
+
+    version = pyscaffold.__version__
+    parser.add_argument('-v',
+                        '--version',
+                        action='version',
+                        version='PyScaffold {ver}'.format(ver=version))
+    opts = parser.parse_args(args)
+    if opts.package is None:
+        opts.package = utils.make_valid_identifier(opts.project)
+    # Strip (back)slash when added accidentally during update
+    opts.project = opts.project.rstrip(os.sep)
+    # Initialize empty list of all requirements
+    utils.safe_set(opts, 'requirements', list())
+    return opts
+
+
+def prepare_namespace(namespace_str):
+    """
+    Check the validity of namespace_str and split it up into a list
+
+    :param namespace_str: namespace as string, e.g. "com.blue_yonder"
+    :return: list of namespaces, e.g. ["com", "com.blue_yonder"]
+    """
+    namespaces = namespace_str.split('.')
+    for namespace in namespaces:
+        if not utils.is_valid_identifier(namespace):
+            raise RuntimeError(
+                "{} is not a valid namespace package.".format(namespace))
+    return ['.'.join(namespaces[:i+1]) for i in range(len(namespaces))]
+
+
+def main(args):
+    """
+    Main entry point of PyScaffold
+
+    :param args: command line parameters as list of strings
+    """
+    args = parse_args(args)
+    if not info.is_git_installed():
+        raise RuntimeError("Make sure git is installed and working.")
+    if not info.is_git_configured():
+        raise RuntimeError(
+            'Make sure git is configured. Run:\n' +
+            '  git config --global user.email "you@example.com"\n' +
+            '  git config --global user.name "Your Name"\n' +
+            "to set your account's default identity.")
+    if os.path.exists(args.project):
+        if not args.update and not args.force:
+            raise RuntimeError(
+                "Directory {dir} already exists! Use --update to update an "
+                "existing project or --force to overwrite an existing "
+                "directory.".format(dir=args.project))
+    if args.update:
+        args = info.project(args)
+    # Set additional attributes of args
+    if args.django:
+        structure.create_django_proj(args)
+        args.requirements.append('django')
+    if args.numpydoc:
+        utils.safe_set(args, 'numpydoc_sphinx_ext', ", 'numpydoc'")
+    else:
+        utils.safe_set(args, 'numpydoc_sphinx_ext', "")
+    if args.namespace:
+        args.namespace = prepare_namespace(args.namespace)
+    # Convert list of
+    proj_struct = structure.make_structure(args)
+    structure.create_structure(proj_struct, update=args.update or args.force)
+    if not args.update and not repo.is_git_repo(args.project):
+        repo.init_commit_repo(args.project, proj_struct)
+        repo.add_tag(args.project, "v0.0", "Initial tag to determine PEP440")
+
+
+@shell.called_process_error2exit_decorator
+@utils.exceptions2exit([RuntimeError])
+def run():
+    """
+    Entry point for setup.py
+    """
+    main(sys.argv[1:])
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
