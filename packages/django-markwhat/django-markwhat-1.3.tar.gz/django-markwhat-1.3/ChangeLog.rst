55587daadfd928795580dffe1e87c0706ced396d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:29:39 +0000

    Add package django-markwhat/1.3/django-markwhat-1.3.tar.gz

---
+++ packages/django-markwhat/django-markwhat-1.3.tar.gz/django-markwhat-1.3/ChangeLog.rst
@@ -0,0 +1,43 @@
+1.3 2015-04-04
+====================
+----
+
+* Feature #11: Correct capitalization / spacing for reStructuredText and 
+docutils in ``README.rst``.
+* Feature #12: PEP440 compliance with version number.
+* Feature #14: Add ``commonmark`` filter.
+* Feature #17: Add ``commonmark`` filter to ``README.rst``.
+* Fix #15: Fix typos in run_tests.py
+* New awesome contibutors/authors joined the project.
+
+
+2014.9.20 2014-09-20
+====================
+----
+
+* Fix #9: Django package name typo.
+* Feature: Add `AUTHORS.rst`` file.
+* Feature: Reorganized ``requirements``.
+* Feature: Runnning tests with ``tox``.
+* Feature: Add support for ``Python3`` and ``PyPy``.
+* Feature: Add ``wheel`` package support.
+* Feature: Change version numbering to date based.
+* Feature: Update ``Python-Markdown``, ``textile`` and ``docutils`` parser modules to latest.
+
+
+1.1 2014-01-19
+===============
+----
+
+* Added ChangeLog.rst
+* Fixed #2: Removing backward compatibility, From markup.py.
+* Fixed #1: Re-Indention and Re-Style the code regards to pep8.
+* Fixed #7: Testing on travis-ci - https://travis-ci.org/Alir3z4/django-markwhat
+
+
+1.0 2013-01-08
+==============
+----
+
+* Initial version.
+
---
+++ packages/django-markwhat/django-markwhat-1.3.tar.gz/django-markwhat-1.3/setup.py
@@ -0,0 +1,44 @@
+from distutils.core import setup
+
+setup(
+    name='django-markwhat',
+    version=".".join(map(str, __import__('django_markwhat').__version__)),
+    packages=['django_markwhat', 'django_markwhat.templatetags'],
+    url='http://pypi.python.org/pypi/django-markwhat',
+    license=open('LICENSE').read(),
+    author='Alireza Savand',
+    author_email='alireza.savand@gmail.com',
+    install_requires=['Django', ],
+    description="""A collection of template filters that implement
+     common markup languages.""",
+    long_description=open('README.rst').read(),
+    keywords=[
+        'django',
+        'markdown',
+        'markup',
+        'textile',
+        'rst',
+        'reStructuredText',
+        'docutils',
+        'web'
+    ],
+    platforms='OS Independent',
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Development Status :: 6 - Mature",
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.0",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Topic :: Software Development",
+        "Topic :: Software Development :: Libraries",
+    ],
+)
