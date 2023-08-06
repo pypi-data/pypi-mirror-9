f5da3fdfe9019b1151841d3c60f178f63d35ffe2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:13 +0000

    Add package Schemer/0.2.6/Schemer-0.2.6.tar.gz

---
+++ packages/Schemer/Schemer-0.2.6.tar.gz/Schemer-0.2.6/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+
+setuptools.setup(
+    name="Schemer",
+    version="0.2.6",
+    author="Tom Leach",
+    author_email="tom@gc.io",
+    description="Powerful schema-based validation of Python dicts",
+    license="BSD",
+    keywords="validation schema dict list",
+    url="http://github.com/gamechanger/schemer",
+    packages=["schemer"],
+    long_description="Schemer allows users to declare schemas for Python dicts and lists and then validate actual dicts and lists against those schemas.",
+    tests_require=['mock', 'nose']
+    )
