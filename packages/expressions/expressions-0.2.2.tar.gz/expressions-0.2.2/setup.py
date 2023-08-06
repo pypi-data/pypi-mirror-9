d4f4fdf916104333abf72032237fed874707504b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:45 +0000

    Add package expressions/0.2.2/expressions-0.2.2.tar.gz

---
+++ packages/expressions/expressions-0.2.2.tar.gz/expressions-0.2.2/setup.py
@@ -0,0 +1,34 @@
+import sys
+from setuptools import setup, find_packages
+
+setup(
+    name = "expressions",
+    version = "0.2.2",
+
+    packages = find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
+    package_data = {
+        # If any package contains *.txt or *.rst files, include them:
+        '': ['*.txt', '*.rst'],
+    },
+
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Utilities'
+    ],
+
+    test_suite = "tests",
+
+    # metadata for upload to PyPI
+    author = "Stefan Urbanek",
+    author_email = "stefan.urbanek@gmail.com",
+    description = "Extensible arithmetic expression parser and compiler",
+    license = "MIT license",
+    keywords = "arithmetic expression",
+    url = "http://databrewery.org"
+
+)
