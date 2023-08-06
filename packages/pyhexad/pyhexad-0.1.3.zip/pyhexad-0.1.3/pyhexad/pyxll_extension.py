0b9c038b288324e59a97bf0d49a43a73ce48fee2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:02 +0000

    Add package pyhexad/0.1.3/pyhexad-0.1.3.zip

---
+++ packages/pyhexad/pyhexad-0.1.3.zip/pyhexad-0.1.3/pyhexad/pyxll_extension.py
@@ -0,0 +1,15 @@
+##############################################################################
+# Copyright by The HDF Group.                                                #
+# All rights reserved.                                                       #
+#                                                                            #
+# This file is part of PyHexad. The full PyHexad copyright notice, including #
+# terms governing use, modification, and redistribution, is contained in     #
+# the file COPYING, which can be found at the root of the source code        #
+# distribution tree.  If you do not have access to this file, you may        #
+# request a copy from help@hdfgroup.org.                                     #
+##############################################################################
+
+def load():
+    """ Loads the PyXLL modules to be exposed to Excel. """
+
+    import pyhexad
