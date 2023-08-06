9aa980a1db40e6014853ddbd375d43d5384d5c35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:05 +0000

    Add package pelican/1.2.3/pelican-1.2.3-py2.py3-none-any.whl

---
+++ packages/pelican/pelican-1.2.3-py2.py3-none-any.whl/pelican/settings.py
@@ -0,0 +1,30 @@
+import os
+
+_DEFAULT_THEME = os.sep.join([os.path.dirname(os.path.abspath(__file__)),
+                              "themes/notmyidea"])
+_DEFAULT_CONFIG = {'PATH': None,
+                   'THEME': _DEFAULT_THEME,
+                   'OUTPUT_PATH': 'output/',
+                   'MARKUP': ('rst', 'md'),
+                   'STATIC_PATHS': ['css', 'images'],
+                   'FEED': 'feeds/all.atom.xml',
+                   'CATEGORY_FEED': 'feeds/%s.atom.xml',
+                   'SITENAME': 'A Pelican Blog',
+                   'DISPLAY_PAGES_ON_MENU': True,
+                   'PDF_PROCESSOR': False,
+                   'DEFAULT_CATEGORY': 'misc',
+                   'FALLBACK_ON_FS_DATE': True,
+                   'CSS_FILE': 'main.css',
+                  }
+
+def read_settings(filename):
+    """Load a Python file into a dictionary.
+    """
+    context = _DEFAULT_CONFIG.copy()
+    if filename:
+        tempdict = {}
+        execfile(filename, tempdict)
+        for key in tempdict:
+            if key.isupper():
+                context[key] = tempdict[key]
+    return context
