76f3e433e459f5b634b5576e28bd1741179dbc52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:18 +0000

    Add package palladium/0.9.1/palladium-0.9.1.tar.gz

---
+++ packages/palladium/palladium-0.9.1.tar.gz/palladium-0.9.1/setup.py
@@ -0,0 +1,75 @@
+import os
+from setuptools import setup, find_packages
+
+version = '0.9.1'
+
+install_requires = [
+    'docopt',
+    'flask',
+    'joblib',
+    'numpy',
+    'pandas',
+    'psutil',
+    'scikit-learn',
+    'ujson',
+    ]
+
+tests_require = [
+    'pytest',
+    'pytest-cov',
+    ]
+
+docs_require = [
+    'julia',
+    'rpy2',
+    'Sphinx',
+    'sphinx_rtd_theme',
+    ]
+
+here = os.path.abspath(os.path.dirname(__file__))
+try:
+    README = open(os.path.join(here, 'README.rst'), encoding='utf-8').read()
+    CHANGES = open(os.path.join(here, 'CHANGES.txt'), encoding='utf-8').read()
+except IOError:
+    README = CHANGES = ''
+
+
+setup(name='palladium',
+      version=version,
+      description='Framework for setting up predictive analytics services',
+      long_description=README,
+      url='https://github.com/ottogroup/palladium',
+      author='Otto Group',
+      author_email='palladium@ottogroup.com',
+      license='Apache License, Version 2.0',
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: Apache Software License',
+          'Programming Language :: Python :: 3.3',
+          'Programming Language :: Python :: 3.4',
+      ],
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=install_requires,
+      extras_require={
+          'testing': tests_require,
+          'docs': docs_require,
+          'julia': ['julia'],
+          'R': ['rpy2'],
+          },
+      entry_points={
+          'console_scripts': [
+              'pld-devserver = palladium.server:devserver_cmd',
+              'pld-fit = palladium.fit:fit_cmd',
+              'pld-grid-search = palladium.fit:grid_search_cmd',
+              'pld-list = palladium.eval:list_cmd',
+              'pld-test = palladium.eval:test_cmd',
+              'pld-version = palladium.util:version_cmd',
+              ],
+          'pytest11': [
+              'palladium = palladium.tests',
+              ],
+          },
+      )
