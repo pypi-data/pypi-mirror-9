57533ffb6f95a4306cb4d776f003d5ad93168e28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:17 +0000

    Add package numtogreek/0.8.0b5/numtogreek-0.8.0b5.zip

---
+++ packages/numtogreek/numtogreek-0.8.0b5.zip/numtogreek-0.8.0b5/numtogreek/__init__.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+#==================================================================================
+#  Copyright:
+#            
+#      Copyright (C) 2012 - 2015 Konstas Marmatakis <marmako@gmail.com>
+#
+#   License:
+#  
+#      This program is free software; you can redistribute it and/or modify
+#      it under the terms of the GNU General Public License version 2 as
+#      published by the Free Software Foundation.
+#
+#      This program is distributed in the hope that it will be useful,
+#      but WITHOUT ANY WARRANTY; without even the implied warranty of
+#      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#      GNU General Public License for more details.
+#
+#      You should have received a copy of the GNU General Public License
+#      along with this package; if not, write to the Free Software
+#      Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+#================================================================================== 
+
+__version__ = '0.7.9b3'
