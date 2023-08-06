14ba7cac7bb4eefeb17f44809a57cf71156f8285
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:05 +0000

    Add package fb-re2/1.0.0/fb-re2-1.0.0.tar.gz

---
+++ packages/fb-re2/fb-re2-1.0.0.tar.gz/fb-re2-1.0.0/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+from distutils.core import setup, Extension
+
+setup(
+    name="fb-re2",
+    version="1.0.0",
+    url="https://github.com/facebook/pyre2",
+    description="Python wrapper for Google's RE2",
+    classifiers=[
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Development Status :: 5 - Production/Stable",
+    ],
+    author="David Reiss",
+    author_email="dreiss@fb.com",
+    maintainer="Siddharth Agarwal",
+    maintainer_email="sid0@fb.com",
+    py_modules = ["re2"],
+    ext_modules = [Extension("_re2",
+      sources = ["_re2.cc"],
+      libraries = ["re2"],
+      )],
+    )
