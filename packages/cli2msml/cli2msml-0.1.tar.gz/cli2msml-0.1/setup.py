9ce459ff1c6b1c4abb331bfad8d3bc0960e3c023
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:24 +0000

    Add package cli2msml/0.1/cli2msml-0.1.tar.gz

---
+++ packages/cli2msml/cli2msml-0.1.tar.gz/cli2msml-0.1/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+
+setup(
+    name='cli2msml',
+    version='0.1',
+    url='https://github.com/CognitionGuidedSurgery/cli2msml',
+    license='GPL v3',
+    py_modules=['cli2msml'],
+    author='Alexander Weigl',
+    author_email='uiduw@student.kit.edu',
+    description='Converter for CLI apps into MSML Operators',
+    install_requires=['path.py', 'colorama', 'pyyaml', 'jinja2'],
+    entry_points={
+        'console_scripts': [
+            'cli2msml=cli2msml:main'
+        ],
+    },
+    classifiers=(
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)", "Operating System :: Unix",
+        "Operating System :: POSIX",
+        "Operating System :: Microsoft :: Windows",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Topic :: Utilities",
+    )
+)
