75116d12f65f20aad77d44034fdd9e85e58a15ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:05 +0000

    Add package time-sanitize/1.0.0/time-sanitize-1.0.0.tar.gz

---
+++ packages/time-sanitize/time-sanitize-1.0.0.tar.gz/time-sanitize-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup (
+	name		= 'time-sanitize',
+	version		= '1.0.0',
+	py_modules	= ['sanitize'],
+	author		= 'tobypy16',
+	author_email	= 'computerkid342@gmail.com',
+	url		= 'linuxandbeyond.com',
+	description	= 'A function that unifys different time formats (e.g, min:sec, min-sec, min.sec) to min.sec'
+)
