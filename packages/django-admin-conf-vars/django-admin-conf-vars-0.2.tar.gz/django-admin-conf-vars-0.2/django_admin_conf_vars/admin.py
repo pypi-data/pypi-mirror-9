3f6d1937329807e6b7a5913871a0ff8953458a93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:11 +0000

    Add package django-admin-conf-vars/0.2/django-admin-conf-vars-0.2.tar.gz

---
+++ packages/django-admin-conf-vars/django-admin-conf-vars-0.2.tar.gz/django-admin-conf-vars-0.2/django_admin_conf_vars/admin.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from .models import ConfVar
+
+
+class ConfVarAdmin(admin.ModelAdmin):
+    list_display = ('name', 'value',)
+    list_editable = ('value',)
+    fields = ('name', 'value',)
+
+
+    def get_readonly_fields(self, request, obj=None):
+        if obj:
+            return ('name',) +  self.readonly_fields
+        return self.readonly_fields
+
+    def has_add_permission(self, request):
+        return False
+
+
+admin.site.register(ConfVar, ConfVarAdmin)
