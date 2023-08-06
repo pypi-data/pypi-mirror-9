f9c954b94b42ccdade4b5bf203221913b760e702
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:33 +0000

    Add package bottle-utils/0.3/bottle-utils-0.3.tar.gz

---
+++ packages/bottle-utils/bottle-utils-0.3.tar.gz/bottle-utils-0.3/setup.py
@@ -0,0 +1,43 @@
+import os
+from setuptools import setup, find_packages
+
+def read(fname):
+    """ Return content of specified file """
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+VERSION = '0.3'
+
+setup(
+    name='bottle-utils',
+    version=VERSION,
+    author='Outernet Inc',
+    author_email='branko@outernet.is',
+    description=('Assortment of frequently used utilities for Bottle '
+                   'framework'),
+    license='BSD',
+    keywords='bottle utils i18n http lazy csrf ajax',
+    url='http://outernet-project.github.io/bottle-utils/',
+    packages=find_packages(),
+    long_description=read('README.rst'),
+    install_requires=[
+        'bottle-utils-common==%s' % VERSION,
+        'bottle-utils-ajax==%s' % VERSION,
+        'bottle-utils-csrf==%s' % VERSION,
+        'bottle-utils-flash==%s' % VERSION,
+        'bottle-utils-html==%s' % VERSION,
+        'bottle-utils-http==%s' % VERSION,
+        'bottle-utils-i18n==%s' % VERSION,
+        'bottle-utils-lazy==%s' % VERSION,
+        'bottle-utils-meta==%s' % VERSION,
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Topic :: Utilities',
+        'Framework :: Bottle',
+        'Environment :: Web Environment',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
+)
