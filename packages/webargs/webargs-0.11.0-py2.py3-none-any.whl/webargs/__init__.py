7568c7dddef6d9a8b30f5cde4fe84b71b1afbe64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:07:38 +0000

    Add package webargs/0.11.0/webargs-0.11.0-py2.py3-none-any.whl

---
+++ packages/webargs/webargs-0.11.0-py2.py3-none-any.whl/webargs/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from webargs.core import Arg, WebargsError, ValidationError, RequiredArgMissingError, Missing
+
+__version__ = '0.11.0'
+__author__ = 'Steven Loria'
+__license__ = 'MIT'
+
+
+__all__ = ['Arg', 'WebargsError', 'ValidationError', 'RequiredArgMissingError', 'Missing']
