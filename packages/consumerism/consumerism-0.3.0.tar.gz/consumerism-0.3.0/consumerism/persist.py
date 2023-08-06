47f6ee3483dc0534c6ab820c68542ed1bc64315a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:43 +0000

    Add package consumerism/0.3.0/consumerism-0.3.0.tar.gz

---
+++ packages/consumerism/consumerism-0.3.0.tar.gz/consumerism-0.3.0/consumerism/persist.py
@@ -0,0 +1,40 @@
+DISABLED = 'disabled'
+DEAD = 'dead'
+SUCCESS = 'success'
+ALL = {DEAD, SUCCESS}
+
+
+class PersistStrategy(object):
+    """
+    Strategy to determine when a message should be persisted.
+
+    Configured by configuring which event(s) should cause persistence.
+    """
+    def __init__(self, events):
+        if events is None:
+            events = [DISABLED]
+        if isinstance(events, basestring):
+            events = [events]
+        if not isinstance(events, set):
+            events = set(events)
+
+        self.events = events
+
+    def should_persist(self, succeeded, will_retry):
+        decision = (
+            (DISABLED not in self.events)
+            and
+            (
+                (succeeded and SUCCESS in self.events) or
+                (not will_retry and DEAD in self.events)
+            )
+        )
+        return decision
+
+
+class PersistBackend(object):  # pragma: nocover
+    """
+    Abstract base class for persistence strategies.
+    """
+    def persist(self, queue, message):
+        raise NotImplementedError
