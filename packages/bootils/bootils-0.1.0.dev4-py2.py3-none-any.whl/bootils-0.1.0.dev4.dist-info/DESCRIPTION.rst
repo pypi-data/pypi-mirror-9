50aa3193b93f99561ba0bf7c8b1daa9a2678bd6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:51 +0000

    Add package bootils/0.1.0.dev4/bootils-0.1.0.dev4-py2.py3-none-any.whl

---
+++ packages/bootils/bootils-0.1.0.dev4-py2.py3-none-any.whl/bootils-0.1.0.dev4.dist-info/DESCRIPTION.rst
@@ -0,0 +1,20 @@
+It is comprised of a ``bootils`` Python package with building blocks
+for process and resource management, and a CLI tool named ``nanny`` that
+watches your child process after starting, until it grows up
+into a stable running state.
+
+
+Copyright ©  2015 1&1 Group <jh@web.de>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
---
+++ packages/bootils/bootils-0.1.0.dev4-py2.py3-none-any.whl/bootils/__init__.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+# pylint: disable=bad-whitespace
+# flake8: noqa
+"""
+    Bootils offers process boot-strapping utilities that support writing
+    robust application/service launcher and process life-cycle management scripts.
+
+    It is comprised of a ``bootils`` Python package with building blocks
+    for process and resource management, and a CLI tool named ``nanny`` that
+    watches your child process after starting, until it grows up
+    into a stable running state.
+
+
+    Copyright ©  2015 1&1 Group <jh@web.de>
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+"""
+from __future__ import absolute_import, unicode_literals, print_function
+
+__url__             = "https://github.com/Build-The-Web/bootils"
+__version__         = "0.1.0"
+__license__         = "Apache 2.0"
+__author__          = "Jürgen Hermann"
+__author_email__    = "jh@web.de"
+__keywords__        = "hosted.by.github init.d startup launcher monitoring resilience watchdog log-scanner"
+
+__all__ = []
