802c6dc34672f91413d52138490c4fd3b2970ec8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:44 +0000

    Add package sodapy/0.1.6/sodapy-0.1.6.tar.gz

---
+++ packages/sodapy/sodapy-0.1.6.tar.gz/sodapy-0.1.6/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+execfile("sodapy/version.py")
+
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+except(IOError, ImportError):
+    long_description = open('README').read()
+
+
+kwargs = {
+    "name": "sodapy",
+    "version": str(__version__),
+    "packages": ["sodapy"],
+    "description": "Python bindings for the Socrata Open Data API",
+    "long_description": long_description,
+    "author": "Cristina Munoz",
+    "maintainer": "Cristina Munoz",
+    "author_email": "hi@xmunoz.com",
+    "maintainer_email": "hi@xmunoz.com",
+    "license": "MIT",
+    "install_requires": ["py", "pytest", "requests", "requests-mock", "six"],
+    "url": "https://github.com/xmunoz/sodapy",
+    "download_url": "https://github.com/xmunoz/sodapy/archive/master.tar.gz",
+    "keywords": "soda socrata opendata api",
+    "classifiers": [
+        "Programming Language :: Python",
+        "Topic :: Software Development",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ]
+}
+
+setup(**kwargs)
