d0ba26ad83d64fba9b6fe6c45684ad54eb5465bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:24 +0000

    Add package databaker/0.0.11/databaker-0.0.11-py2-none-any.whl

---
+++ packages/databaker/databaker-0.0.11-py2-none-any.whl/databaker/constants.py
@@ -0,0 +1,16 @@
+from xypath import DOWN, UP, LEFT, RIGHT
+import bake
+
+OBS = -4
+DATAMARKER = -3
+GEOG = -2
+TIME = -1
+TIMEUNIT = 0
+
+ABOVE = UP
+BELOW = DOWN
+
+DIRECTLY = True
+CLOSEST = False
+
+PARAMS = lambda: bake.Opt.params
