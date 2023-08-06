91eeb4fd505b4859ece59a84ff6d74319e83e781
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:07 +0000

    Add package ucsc_snapshots/0.1.6/ucsc_snapshots-0.1.6.tar.gz

---
+++ packages/ucsc_snapshots/ucsc_snapshots-0.1.6.tar.gz/ucsc_snapshots-0.1.6/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# encoding: utf-8
+import ez_setup
+ez_setup.use_setuptools()
+
+from setuptools import setup
+
+entry_points = """
+[console_scripts]
+ucsc_snapshots = ucsc_snapshots.ucsc_snapshots:main
+"""
+setup(name='ucsc_snapshots',
+      version='0.1.6',
+      description='fetch images from the UCSC genome browser using BED regions',
+      author='Jay Hesselberth',
+      author_email='jay.hesselberth@gmail.com',
+      license='MIT',
+      url='https://github.com/jayhesselberth/ucsc_snapshots',
+      install_requires=['pybedtools', 'path.py', 'ucscsession'],
+      entry_points=entry_points,
+      long_description=open('README.md').read(),
+      classifiers=['License :: OSI Approved :: MIT License'],
+)
