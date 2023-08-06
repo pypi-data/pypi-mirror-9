9ff169003a7d561656ce824c28b277119176a281
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:07:52 +0000

    Add package wheezy.routing/0.1.157/wheezy.routing-0.1.157.tar.gz

---
+++ packages/wheezy.routing/wheezy.routing-0.1.157.tar.gz/wheezy.routing-0.1.157/setup.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+
+import os
+
+try:
+    from setuptools import setup
+except:
+    from distutils.core import setup  # noqa
+
+extra = {}
+try:
+    from Cython.Build import cythonize
+    p = os.path.join('src', 'wheezy', 'routing')
+    extra['ext_modules'] = cythonize(
+        [os.path.join(p, '*.py')],
+        exclude=os.path.join(p, '__init__.py'),
+        nthreads=2, quiet=True)
+except ImportError:
+    pass
+
+README = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
+
+setup(
+    name='wheezy.routing',
+    version='0.1',
+    description='A lightweight path routing library',
+    long_description=README,
+    url='https://bitbucket.org/akorn/wheezy.routing',
+
+    author='Andriy Kornatskyy',
+    author_email='andriy.kornatskyy at live.com',
+
+    license='MIT',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.4',
+        'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Internet :: WWW/HTTP :: WSGI',
+        'Topic :: Internet :: WWW/HTTP :: WSGI :: Application',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+    keywords='routing path url patterns match web mapping',
+    packages=['wheezy', 'wheezy.routing'],
+    package_dir={'': 'src'},
+    namespace_packages=['wheezy'],
+
+    zip_safe=False,
+    install_requires=[
+    ],
+    extras_require={
+        'dev': [
+            'wsgiref',
+            'coverage',
+            'nose',
+            'pytest',
+            'pytest-pep8',
+            'pytest-cov',
+            'mocker'
+        ]
+    },
+
+    platforms='any',
+    **extra
+)
---
+++ packages/wheezy.routing/wheezy.routing-0.1.157.tar.gz/wheezy.routing-0.1.157/src/wheezy/routing/__init__.py
@@ -0,0 +1,11 @@
+
+"""
+"""
+
+# flake8: noqa
+
+from wheezy.routing.router import PathRouter
+from wheezy.routing.router import url
+
+
+__version__ = '0.1.157'
