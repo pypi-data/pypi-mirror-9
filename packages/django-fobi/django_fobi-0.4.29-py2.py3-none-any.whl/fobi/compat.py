8406c1c333838fec92fe7c6bc4685f35541b4384
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:37 +0000

    Add package django-fobi/0.4.29/django_fobi-0.4.29-py2.py3-none-any.whl

---
+++ packages/django-fobi/django_fobi-0.4.29-py2.py3-none-any.whl/fobi/compat.py
@@ -0,0 +1,21 @@
+__title__ = 'fobi.compat'
+__author__ = 'Artur Barseghyan <artur.barseghyan@gmail.com>'
+__copyright__ = 'Copyright (c) 2013-2015 Artur Barseghyan'
+__license__ = 'GPL 2.0/LGPL 2.1'
+__all__ = ('AUTH_USER_MODEL', 'User',)
+
+from django.conf import settings
+
+from nine.user import User
+
+# Sanity checks. Possibly rely on the dynamic username field in future.
+user = User()
+
+if not hasattr(user, 'username'):
+    from dash.exceptions import ImproperlyConfigured
+    raise ImproperlyConfigured("Your custom user model ({0}.{1}) doesn't "
+                               "have ``username`` property, while "
+                               "``django-fobi`` relies on its' presence."
+                               "".format(user._meta.app_label, user._meta.object_name))
+
+AUTH_USER_MODEL = settings.AUTH_USER_MODEL
