16c30ab137234e1e74e29367574bc724534703b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:15 +0000

    Add package horsemeat/2.11.1/horsemeat-2.11.1.tar.gz

---
+++ packages/horsemeat/horsemeat-2.11.1.tar.gz/horsemeat-2.11.1/horsemeat/configwrapper.py
@@ -0,0 +1,508 @@
+# vim: set expandtab ts=4 sw=4 filetype=python:
+
+import abc
+import contextlib
+import datetime
+import json
+import logging
+import logging.config
+import math
+import os
+import smtplib
+import sys
+import textwrap
+import traceback
+import uuid
+import warnings
+
+import clepy
+import jinja2
+import pkg_resources
+import psycopg2, psycopg2.extras
+import yaml
+
+from horsemeat import fancyjsondumps
+
+log = logging.getLogger(__name__)
+
+class ConfigWrapper(object):
+
+    """
+    Use this class to interact with our configuration files.  Add properties
+    and methods so that app code doesn't have to think too much about the
+    way the data is stored in the config files, and so that we can
+    reorganize stuff.
+    """
+
+    __metaclass__ = abc.ABCMeta
+
+    # Subclasses must define this configmodule attribute.  There's
+    # nothing in the abc module that I can use to force that to happen,
+    # but it sure would be nice.  In the subclass, set it to a string
+    # like:
+    #
+    #   "trailhead.configs.yaml_files"
+    #
+    # I can't make it an abstract property, because properties are not
+    # available in classmethods.
+    configmodule = None
+
+    # Later, you can set up a particular instance as the default
+    # instance, by using the set_as_default instance method.
+    default_instance = None
+
+    # Keep these things at the class level, so that all instances share
+    # them.
+    postgresql_connection = None
+    jinja2_environment = None
+    smtp_connection = None
+
+    def __init__(self, config_dictionary, yaml_file_name=None):
+        self.config_dictionary = config_dictionary
+        self.yaml_file_name = yaml_file_name
+
+    @classmethod
+    def from_yaml_file_name(cls, filename):
+
+        """
+        Loads one of the yaml files in the yamlfiles folder.
+
+        >>> cw = ConfigWrapper.from_yaml_file_name('dev.yaml') # doctest: +SKIP
+        """
+
+        if not filename:
+            raise ValueError("Sorry, I need a filename!")
+
+        elif not cls.configmodule:
+            raise ValueError("Sorry, you need to set cls.configmodule!")
+
+        else:
+
+            stream = pkg_resources.resource_stream(
+                cls.configmodule,
+                filename)
+
+            self = cls(
+                yaml.safe_load(stream),
+                yaml_file_name=filename)
+
+            return self
+
+    @classmethod
+    def load_yaml(cls, path_to_file):
+
+        """
+        First check if this is an absolute patch.
+        Next check if it is an file installed in this package.
+        """
+
+        if os.access(path_to_file, os.R_OK):
+
+            self = cls(
+                yaml.safe_load(open(path_to_file).read()),
+                yaml_file_name=os.path.basename(path_to_file))
+
+            return self
+
+        elif pkg_resources.resource_exists(
+            cls.configmodule,
+            path_to_file):
+
+            return cls.from_yaml_file_name(path_to_file)
+
+        else:
+            raise IOError("Sorry, can not load {0}!".format(
+                path_to_file))
+
+
+    @property
+    def should_register_composite_types(self):
+
+          return self.config_dictionary['postgresql'].get(
+               'should_register_composite_types', False)
+
+    def get_postgresql_connection(self, register_composite_types=True):
+
+        if not self.postgresql_connection:
+
+            self.make_database_connection(
+                register_composite_types=register_composite_types)
+
+        return self.postgresql_connection
+
+    # Short aliases are fun too.
+    get_pgconn = get_postgresql_connection
+
+    @contextlib.contextmanager
+    def get_autocommitting_postgresql_connection(self):
+
+        """
+        Use this thing like this::
+
+        >>> with cw.get_autocommitting_postgresql_connection \
+        ... as pgconn: # doctest: +SKIP
+        ...     cursor = pgconn.cursor()
+        ...     cursor.execute("update ...")
+
+        and the transaction will be committed when you leave the with
+        block.
+
+        """
+
+        try:
+            yield self.get_postgresql_connection()
+
+        finally:
+            log.info("Committing postgresql connection...")
+            self.postgresql_connection.commit()
+
+    @property
+    def database_host(self):
+        return self.config_dictionary['postgresql']['host']
+
+    @property
+    def database_port(self):
+        return self.config_dictionary['postgresql']['port']
+
+    @property
+    def database_database(self):
+        return self.config_dictionary['postgresql']['database']
+
+    # Since database_database sounds stupid, I'm setting up an alias
+    # "database_name".
+    database_name = database_database
+
+    @property
+    def database_user(self):
+        return self.config_dictionary['postgresql']['user']
+
+    @property
+    def database_password(self):
+        return self.config_dictionary['postgresql']['password']
+
+    def make_database_connection(self, register_composite_types=True):
+
+        pgconn = psycopg2.connect(
+            connection_factory=psycopg2.extras.NamedTupleConnection,
+            port=self.database_port,
+            database=self.database_name,
+            host=self.database_host,
+            user=self.database_user,
+            password=self.database_password)
+
+        log.info("Just made postgresql connection {0}.".format(
+            pgconn))
+
+        # Keep a reference to this connection on the class, so that
+        # other instances can just recycle this connection.
+        self.__class__.postgresql_connection = pgconn
+
+        psycopg2.extras.register_uuid()
+        psycopg2.extras.register_hstore(pgconn, globally=True)
+
+        psycopg2.extensions.register_type(psycopg2.extensions.UNICODE)
+        psycopg2.extensions.register_type(psycopg2.extensions.UNICODEARRAY)
+
+        if register_composite_types:
+            self.register_composite_types(pgconn)
+
+        return pgconn
+
+    # Make aliases because Matt can't remember stuff good.
+    create_postgresql_connection = make_database_connection
+    make_postgresql_connection = make_database_connection
+
+    def register_composite_types(self, pgconn):
+
+        """
+        Subclasses can define this if they want to.
+        """
+
+        pass
+
+    def configure_logging(self, process_type='default'):
+
+        if 'logging' not in self.config_dictionary:
+            raise MissingConfig("You need to set a logging key!")
+
+        elif process_type not in self.config_dictionary['logging']:
+
+            raise MissingConfig("I don't know how to configure "
+                "logging for {0} processes!  You need to set a key for "
+                "{0} processes in the logging section.".format(
+                    process_type))
+
+        else:
+
+            x = self.config_dictionary['logging'][process_type]
+
+            if isinstance(x, str):
+
+                warnings.warn("Stop it! Define logging configuration "
+                    "the same YAML file!")
+
+                logging.config.fileConfig(
+                    pkg_resources.resource_filename(
+                        'horsemeat.configs.logging_configs',
+                        x))
+
+            elif isinstance(x, dict):
+                logging.config.dictConfig(x)
+                sys.excepthook = log_uncaught_exceptions
+
+            else:
+                raise Exception("can not deal with x: {0}.".format(x))
+
+        log.info('Just configured logging...')
+
+
+    def set_as_default(self):
+
+        ConfigWrapper.default_instance = self
+        return self
+
+    @classmethod
+    def get_default(cls):
+
+        if cls.default_instance:
+            return cls.default_instance
+
+        else:
+            raise ValueError("Sorry!  No default configwrapper "
+                "instance has been set!")
+
+    @property
+    def app_secret(self):
+
+        try:
+            return self.config_dictionary['app']['secret']
+
+        except KeyError:
+            raise MissingConfig("Didn't find an app secret!")
+
+    @property
+    def cloud_base_url(self):
+
+        try:
+            return self.config_dictionary['cloudfiles']['base-url']
+
+        except KeyError:
+            raise MissingConfig("Didn't find a cloudfiles base url!")
+
+    @property
+    def cloud_secret_key(self):
+
+        try:
+            return self.config_dictionary['cloudfiles']['temp-url-key']
+
+        except KeyError:
+            raise MissingConfig("Didn't find a cloudfiles secret key!")
+
+    @property
+    def cloud_file_time_available(self):
+
+        try:
+            return int(self.config_dictionary['cloudfiles']['time-available'])
+
+        except KeyError:
+            raise MissingConfig("Didn't find cloudfiles time available!")
+
+    def make_jinja2_environment(self):
+
+        j = jinja2.Environment(
+            autoescape=True,
+
+            # Tell jinja2 to blow up when we use an undefined name
+            # in a template.
+            undefined=jinja2.StrictUndefined,
+
+            loader=jinja2.PrefixLoader({
+            }),
+
+            extensions=['jinja2.ext.loopcontrols'],
+        )
+
+        log.info("Just built a jinja2 environment")
+
+        self.__class__.jinja2_environment = j
+
+        # Add a bunch of stuff to the template namespace.
+        j.globals['ceil'] = math.ceil
+        j.globals['clepy'] = clepy
+        j.globals['datetime'] = datetime
+        j.globals['dir'] = dir
+        j.globals['enumerate'] = enumerate
+        j.globals['float'] = float
+        j.globals['getattr'] = getattr
+        j.globals['hasattr'] = hasattr
+        j.globals['id'] = id
+        j.globals['int'] = int
+        j.globals['json'] = json
+        j.globals['fancyjsondumps'] = fancyjsondumps
+        j.globals['len'] = len
+        j.globals['mathset'] = set
+        j.globals['max'] = max
+        j.globals['round'] = round
+        j.globals['sorted'] = sorted
+        j.globals['str'] = str
+        j.globals['type'] = type
+        j.globals['uuid'] = uuid
+        j.globals['zip'] = zip
+
+        # Give jinja a reference to the configwrapper.
+        j.globals['cw'] = self
+
+        self.add_more_stuff_to_jinja2_globals()
+
+        return j
+
+    def add_more_stuff_to_jinja2_globals(self):
+        log.info(textwrap.dedent("""
+            Nothing extra to add.  You can add stuff in your
+            subclass if you want to..
+            """))
+
+    def get_jinja2_environment(self):
+
+        if not self.jinja2_environment:
+            self.make_jinja2_environment()
+
+        return self.jinja2_environment
+
+    @property
+    def j(self):
+        return sef.get_jinja2_environment()
+
+
+    @property
+    def scheme(self):
+        return self.config_dictionary['app']['scheme']
+
+    @property
+    def host(self):
+        return self.config_dictionary['app']['host']
+
+    @property
+    def web_host(self):
+        return '{scheme}://{host}'.format(
+            scheme=self.scheme,
+            host=self.host)
+
+    def make_location_from_path(self, path):
+
+        """
+        >>> cw = ConfigWrapper({
+        ...     'app': {
+        ...         'scheme': 'http', 'host': 'example.com'}}) # doctest: +SKIP
+
+        >>> cw.make_location_from_path('/login') # doctest: +SKIP
+        'http://example.com/login'
+
+        """
+
+        return '{0}://{1}{2}'.format(
+            self.scheme,
+            self.host,
+            path)
+
+    @property
+    def smtp_host(self):
+        return self.config_dictionary['smtp']['host']
+
+    def make_smtp_connection(self):
+
+        """
+        I'm not trying any fancy recycled connection stuff here.  It
+        seems like the localhost SMTP server closes old connections
+        automatically.
+        """
+
+        return smtplib.SMTP(self.smtp_host)
+
+    @property
+    def launch_debugger_on_error(self):
+
+        """
+        This returns False if the config file has no entry.
+        """
+
+        if 'launch_debugger_on_error' \
+        not in self.config_dictionary['app']:
+            return False
+
+        else:
+            return self.config_dictionary['app']['launch_debugger_on_error']
+
+    def connect_everything(self):
+
+        """
+        Make connections to all the external services we need.
+        """
+
+        # TODO: figure out an elegant way for subclasses to run this as
+        # well as their own on-bootup stuff.
+
+        self.get_postgresql_connection()
+
+    @property
+    def dev_mode(self):
+
+        return 'dev' == self.config_dictionary['app'].get(
+            'mode',
+            'production')
+
+    @property
+    def production_mode(self):
+        return not self.dev_mode
+
+    def build_webapp(self):
+
+        self.set_as_default()
+        self.configure_logging()
+
+        if self.production_mode:
+            self.run_production_mode_stuff()
+
+        j = self.get_jinja2_environment()
+        pgconn = self.get_postgresql_connection()
+
+        return self.dispatcher_class(j, pgconn, self)
+
+    def run_production_mode_stuff(self):
+
+        """
+        Subclasses can add stuff in here if they want.
+        """
+
+        log.info("Nothing to do for production-mode stuff")
+
+    @abc.abstractproperty
+    def dispatcher_class(self):
+
+        print "you have to define this in the subclass!"
+
+        raise NotImplementedError
+
+    @property
+    def pidfile(self):
+        return self.config_dictionary["app"]["pidfile"]
+
+    @property
+    def webapp_port(self):
+        return self.config_dictionary["app"]["webapp_port"]
+
+
+class MissingConfig(KeyError):
+
+    """
+    You need to add some more stuff to your config file!
+    """
+
+def log_uncaught_exceptions(ex_cls, ex, tb):
+    log.critical(''.join(traceback.format_tb(tb)))
+    log.critical('{0}: {1}'.format(ex_cls, ex))
+
+    if hasattr(ex, 'errors') and ex.errors:
+        log.critical(ex.errors)
+
+
---
+++ packages/horsemeat/horsemeat-2.11.1.tar.gz/horsemeat-2.11.1/horsemeat/webapp/dispatcher.py
@@ -0,0 +1,324 @@
+# vim: set expandtab ts=4 sw=4 filetype=python fileencoding=utf-8:
+# -*- coding: utf-8 -*-
+
+import abc
+import datetime
+import importlib
+import inspect
+import logging
+import sys
+import warnings
+import traceback
+
+import jinja2
+import werkzeug.debug
+
+from horsemeat import configwrapper
+from horsemeat.webapp.handler import Handler
+from horsemeat.webapp.response import Response
+
+log = logging.getLogger(__name__)
+
+# Tell python to always SHOW the deprecation warnings.
+warnings.filterwarnings('always', category=DeprecationWarning)
+
+class Dispatcher(object):
+
+    """
+    This is the webapp that gunicorn talks to.
+    """
+
+    __metaclass__ = abc.ABCMeta
+
+    # TODO: maybe replace this with an abstract method like "make
+    # request instance".
+    @abc.abstractproperty
+    def request_class(self):
+
+        raise NotImplementedError
+
+    @abc.abstractproperty
+    def error_page(self):
+
+        raise NotImplementedError
+
+
+    def __init__(self, jinja2_environment, pgconn, config_wrapper):
+
+        self.jinja2_environment = jinja2_environment
+        self.pgconn = pgconn
+        self.config_wrapper = config_wrapper
+
+        self.handlers = []
+        self.make_handlers()
+        self.run_all_on_startup_methods()
+
+        log.info("Dispatcher __init__ complete!  Framework is ready.")
+
+    @property
+    def cw(self):
+        return self.config_wrapper
+
+
+    def __call__(self, environ, start_response):
+
+        """
+        This is the WSGI app interface.
+
+        Every time a request hits gunicorn, this method fires.
+        """
+
+        try:
+
+            req = self.request_class(
+                self.pgconn,
+                self.config_wrapper,
+                environ)
+
+            # TODO: Figure out if there is some more elegant approach to
+            # making the request object visible in the template.
+            self.jinja2_environment.globals['request'] = req
+            self.jinja2_environment.globals['req'] = req
+
+            log.info('Got request {0} {1}'.format(
+                req.REQUEST_METHOD, req.path_and_qs))
+
+            handle_function = self.dispatch(req)
+
+            resp = handle_function(req)
+
+            if not isinstance(resp, Response):
+                raise Exception("Handler didn't return a response object!")
+
+            # Maybe if the route method returned self, rather than
+            # self.handle, at this point, we could do a test like
+            #
+            #     if callable(getattr(h, 'after_handle', None))
+            #
+            # and then all this stuff could be defined on the
+            # handler.Handler class rather than in here.
+
+            # TODO: make this happen as an automatic side effect of
+            # reading the data, so that there is absolutely no risk at
+            # all of forgetting to do this.
+            if req.news_message_cookie_popped:
+                resp.mark_news_message_as_expired()
+
+            # Update the signed-in user's session expires column.
+            if req.user:
+
+                new_expires_time = req.session.maybe_update_session_expires_time(
+                    self.pgconn)
+
+            self.pgconn.commit()
+
+            start_response(resp.status, resp.headers)
+
+            log.info('Replying with status %s.\n' % resp.status)
+
+            if resp.status.startswith('30'):
+                log.info('Redirecting to {0}.'.format(resp.headers))
+
+            return resp.body
+
+        except Exception as ex:
+
+            self.pgconn.rollback()
+            log.critical(ex, exc_info=1)
+
+            if self.cw.launch_debugger_on_error:
+                raise
+
+            else:
+
+                log.critical('address bar: {0}'.format(req.address_bar))
+                log.critical('post body: {0}'.format(req.wz_req.form))
+
+                log.critical(environ)
+
+                start_response(
+                    '500 ERROR',
+                    [('Content-Type', 'text/html; charset=utf-8')],
+                    sys.exc_info())
+
+                s = self.error_page.render()
+
+                return [s.encode('utf8')]
+
+
+    def dispatch(self, request):
+
+        """
+        Return the first handler that wants to handle this request.
+
+        Fun fact -- in your handler's route method, you can
+        return any callable object, not necessarilly self.handle.
+
+        So the router can inspect 100% of the incoming data and decide
+        if it should dispatch to its handle_good_data method or the
+        handle_invalid_data method.
+
+        """
+
+        for candidate in self.handlers:
+
+            thing = candidate.route(request)
+
+            if thing:
+
+                # Check if we got a function back.  This is the
+                # preferred behavior.
+                if callable(thing):
+
+                    log.info(
+                        'Dispatching to {0}.{1}.{2}.'.format(
+                        candidate.__class__.__module__,
+                        candidate.__class__.__name__,
+                        thing.__name__,
+                    ))
+
+                    return thing
+
+                # Check if the thing we got has a thing.handle method.
+                elif inspect.ismethod(getattr(thing, 'handle', None)):
+
+                    warnings.warn(
+                        'Return self.handle rather than self',
+                        DeprecationWarning)
+
+                    log.info(
+                        'Dispatching to {0}.{1}.{2}.'.format(
+                        candidate.__class__.__module__,
+                        candidate.__class__.__name__,
+                        thing.handle.im_func.func_name,
+                    ))
+
+                    return thing.handle
+
+    @classmethod
+    def app_from_yaml(cls, yaml_file_name):
+
+        try:
+            cw = configwrapper.ConfigWrapper.from_yaml_file_name(
+                yaml_file_name)
+
+            # Now, other people can get a reference this by using
+            # configwrapper.ConfigWrapper.get_default().
+            cw.set_as_default()
+
+            cw.configure_logging()
+            cw.verify_config_file()
+
+        except Exception as ex:
+            traceback.print_exc()
+            raise
+
+        try:
+
+            j = cw.get_jinja2_environment()
+            pgconn = cw.get_postgresql_connection()
+
+            if cw.launch_debugger_on_error:
+
+                return werkzeug.debug.DebuggedApplication(
+                    cls(j, pgconn, cw),
+                    evalex=True)
+
+            else:
+                return cls(j, pgconn, cw)
+
+        except Exception as ex:
+            log.critical(ex, exc_info=1)
+            raise
+
+
+    @staticmethod
+    def convert_string_to_class(s):
+
+        """
+        Give me a string like 'a.b.c.d.e', and I will return the object
+        e from within module a.b.c.d.e.
+
+        The object e can be a class, a function, a dictionary, whatever.
+
+        I had to write this because __import__ allows dynamic **module**
+        imports, but not dynamic object imports from within modules.
+
+        Example usage::
+
+        >>> f = Dispatcher.convert_string_to_class('os.path.join')
+        >>> import os
+        >>> f is os.path.join
+        True
+
+        I got this trick from this stack overflow page:
+
+        http://stackoverflow.com/questions/2179251/dynamically-import-a-callable-given-the-full-module-path
+
+        """
+
+        module_name, irrelevant_junk, class_name = s.rpartition('.')
+
+        m = importlib.import_module(module_name)
+
+        obj = getattr(m, class_name)
+
+        return obj
+
+    def make_handlers_from_module_string(self, s):
+
+        m = importlib.import_module(s)
+
+        # This is one really big gigantic list comprehension, but don't
+        # be scared!  It does three things:
+
+        # 1.  Make a list of the objects contained in the module m.
+
+        # 2.  Filter down that list to just the objects that are
+        #     subclasses of the Handler class and that defined all the
+        #     virtual methods.
+
+        # 3.  Instantiate all the classes in the filtered list.
+
+        # TODO: stop passing in lists of modules.  Pass in lists of
+        # classes, and then we won't need this wacky complex logic to
+        # ignore some classes we get.  And we also will provide more
+        # control about the order.
+
+        return [cls(self.config_wrapper, self)
+
+            for name, cls in inspect.getmembers(m)
+
+            if cls != Handler # don't instantiate the base class!
+
+            and inspect.isclass(cls)
+            and issubclass(cls, Handler)
+
+            # This is nasty -- I'm testing if this cls has a route
+            # method that has been defined.  The point of this is that
+            # each project that uses horsemeat defines its own Handler
+            # that subclasses the horsemeat handler.  But those also
+            # only have virtual (abstract) methods, so we should not
+            # instantiate them.
+            and getattr(cls.route, '__isabstractmethod__', False) is False
+            ]
+
+
+    @abc.abstractmethod
+    def make_handlers(self):
+
+        raise NotImplementedError
+
+
+    def run_all_on_startup_methods(self):
+
+        for h in self.handlers:
+
+            if hasattr(h, 'on_startup') \
+            and inspect.ismethod(h.on_startup):
+
+                h.on_startup()
+
+        self.cw.get_pgconn().commit()
+
+        log.info("All on-startup methods ran and database committed.")
