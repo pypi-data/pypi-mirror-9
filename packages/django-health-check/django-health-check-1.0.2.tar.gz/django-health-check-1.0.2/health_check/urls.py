a0d317d7d4ae63b00e1bc8adaa420e8264253e48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:54 +0000

    Add package django-health-check/1.0.2/django-health-check-1.0.2.tar.gz

---
+++ packages/django-health-check/django-health-check-1.0.2.tar.gz/django-health-check-1.0.2/health_check/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import patterns, include, url
+
+import health_check
+health_check.autodiscover()
+
+urlpatterns = patterns('',
+    url(r'^$', 'health_check.views.home', name='health_check_home'),
+)
---
+++ packages/django-health-check/django-health-check-1.0.2.tar.gz/django-health-check-1.0.2/health_check/views.py
@@ -0,0 +1,19 @@
+from django.http import HttpResponse, HttpResponseServerError
+from django.template import loader
+from health_check.plugins import plugin_dir
+
+
+def home(request):
+    plugins = []
+    working = True
+    for plugin_class, plugin in plugin_dir._registry.items():
+        plugin = plugin_class()
+        if not plugin.status:  # Will return True or None
+            working = False
+        plugins.append(plugin)
+    plugins.sort(key=lambda x: x.identifier())
+
+    if working:
+        return HttpResponse(loader.render_to_string("health_check/dashboard.html", {'plugins': plugins}))
+    else:
+        return HttpResponseServerError(loader.render_to_string("health_check/dashboard.html", {'plugins': plugins}))
