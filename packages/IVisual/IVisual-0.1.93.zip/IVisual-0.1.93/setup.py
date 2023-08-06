a5b6961e762f0475239dbf4ce8d3d7f49d31e407
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:57 +0000

    Add package IVisual/0.1.93/IVisual-0.1.93.zip

---
+++ packages/IVisual/IVisual-0.1.93.zip/IVisual-0.1.93/setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+
+setup(
+    name='IVisual',
+    version='0.1.93',
+    author='John Coady',
+    author_email='johncoady@shaw.ca',
+    packages=['ivisual', 'ivisual.test'],
+    package_data={'ivisual': ['data/*.js']},
+    url='http://pypi.python.org/pypi/IVisual/',
+    license='LICENSE.txt',
+    description='VPython visual inline for IPython Notebook',
+    long_description=open('README.txt').read(),
+    classifiers=[
+          'Framework :: IPython',
+          'Development Status :: 4 - Beta',
+          'Environment :: Web Environment',
+          'Intended Audience :: End Users/Desktop',
+          'Natural Language :: English',
+          'Programming Language :: Python :: 2.7',
+          'Topic :: Multimedia :: Graphics :: 3D Modeling',
+          'Topic :: Multimedia :: Graphics :: 3D Rendering',
+          'Topic :: Scientific/Engineering :: Visualization',
+    ],
+)
