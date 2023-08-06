35bb53c52daf4e39487e93f63d5b3cd1ad38ff75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:46 +0000

    Add package linx/0.1.3/linx-0.1.3.tar.gz

---
+++ packages/linx/linx-0.1.3.tar.gz/linx-0.1.3/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+
+setup(
+    name='linx',
+    version='0.1.3',
+    author='Andrei Marcu',
+    author_email='andrei@marcu.net',
+    url='https://github.com/andreimarcu/linx-cli',
+    license='LICENSE.txt',
+    description='Command line interface for Linx.li',
+    install_requires=[
+        "requests",
+    ],
+    scripts=['linx/linx']
+
+)
