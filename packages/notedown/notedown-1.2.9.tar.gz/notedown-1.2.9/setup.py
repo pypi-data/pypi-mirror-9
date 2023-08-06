08c7f021eb82b4dedb6dcc0540611facd238c144
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:04 +0000

    Add package notedown/1.2.9/notedown-1.2.9.tar.gz

---
+++ packages/notedown/notedown-1.2.9.tar.gz/notedown-1.2.9/setup.py
@@ -0,0 +1,25 @@
+import subprocess
+
+from setuptools import setup
+
+pandoc = subprocess.Popen(['pandoc', 'README.md', '--to', 'rst'],
+                          stdout=subprocess.PIPE)
+
+rst_readme = pandoc.communicate()[0]
+
+setup(
+    name="notedown",
+    version="1.2.9",
+    description="Convert markdown to IPython notebook.",
+    long_description=rst_readme,
+    packages=['notedown'],
+    author="Aaron O'Leary",
+    author_email='dev@aaren.me',
+    license='BSD 2-Clause',
+    url='http://github.com/aaren/notedown',
+    install_requires=['ipython < 3.0a', 'jinja2', 'pandoc-attributes'],
+    entry_points={'console_scripts': ['notedown = notedown:cli', ]},
+    package_dir={'notedown': 'notedown'},
+    package_data={'notedown': ['templates/markdown.tpl']},
+    include_package_data=True,
+)
