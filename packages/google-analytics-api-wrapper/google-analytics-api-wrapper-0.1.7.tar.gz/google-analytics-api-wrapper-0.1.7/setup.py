c6b4d8127475e2ea9f2804194dc454e4599f64ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:42 +0000

    Add package google-analytics-api-wrapper/0.1.7/google-analytics-api-wrapper-0.1.7.tar.gz

---
+++ packages/google-analytics-api-wrapper/google-analytics-api-wrapper-0.1.7.tar.gz/google-analytics-api-wrapper-0.1.7/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='google-analytics-api-wrapper',
+    version='0.1.7',
+    packages=['analytics_query'],
+    url='https://github.com/tikazyq/google-analytics-api-wrapper',
+    download_url='https://github.com/tikazyq/google-analytics-api-wrapper/tarball/master',
+    license='http://opensource.org/licenses/MIT',
+    author='Yeqing Zhang',
+    author_email='tikazyq@gmail.com',
+    description='The Goolge Analytics wrapper is a convenient tool to extract data from GA via API. It is especially '
+                'useful when the user has many GA profiles / web properties.',
+    keywords=['google-analytics', 'ga', 'pandas', 'dataframe', 'api']
+)
