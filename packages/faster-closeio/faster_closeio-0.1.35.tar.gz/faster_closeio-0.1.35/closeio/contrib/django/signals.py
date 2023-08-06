774992ceb806b4b16e3fcf60e9d556791d09c42e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:19 +0000

    Add package faster-closeio/0.1.35/faster_closeio-0.1.35.tar.gz

---
+++ packages/faster-closeio/faster_closeio-0.1.35.tar.gz/faster_closeio-0.1.35/closeio/contrib/django/signals.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+
+from django.dispatch import Signal
+
+
+closeio_event = Signal(providing_args=["instance", "model", "event"], use_caching=True)
+
+closeio_create = Signal(providing_args=["instance", "model"], use_caching=True)
+
+closeio_update = Signal(providing_args=["instance", "model"], use_caching=True)
+
+closeio_delete = Signal(providing_args=["instance_id", "model"], use_caching=True)
+
+closeio_merge = Signal(providing_args=["source_id", "destination_id", "model"], use_caching=True)
+
+lead_create = Signal(providing_args=["instance"], use_caching=True)
+
+lead_update = Signal(providing_args=["instance"], use_caching=True)
+
+lead_delete = Signal(providing_args=["instance_id"], use_caching=True)
+
+lead_merge = Signal(providing_args=["source_id", "destination_id"], use_caching=True)
---
+++ packages/faster-closeio/faster_closeio-0.1.35.tar.gz/faster_closeio-0.1.35/closeio/contrib/django/views.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+import json
+import logging
+
+from django.http import HttpResponse, HttpResponseBadRequest
+from django.utils.decorators import method_decorator
+from django.views.generic import View
+from django.views.decorators.csrf import csrf_exempt
+
+from closeio import utils
+
+from . import signals
+
+
+logger = logging.getLogger(__name__)
+
+
+class CloseIOWebHook(View):
+    @method_decorator(csrf_exempt)
+    def dispatch(self, *args, **kwargs):
+        return super(CloseIOWebHook, self).dispatch(*args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        try:
+            query = json.loads(request.body)
+        except (ValueError, TypeError):
+            logger.exception("CloseIO webhook request could not be parsed.")
+            return HttpResponseBadRequest()
+
+        try:
+            event = query['event']
+            model = query['model']
+            data = utils.parse(query['data'])
+        except KeyError:
+            logger.exception("CloseIO webhook request could not be dispatched.")
+            return HttpResponseBadRequest()
+
+        data_to_send = dict(
+            instance=data
+        )
+
+        if event == 'create':
+            signals.closeio_create.send(
+                sender=self.__class__,
+                model=model,
+                **data_to_send
+            )
+
+        elif event == 'update':
+            signals.closeio_update.send(
+                sender=self.__class__,
+                model=model,
+                **data_to_send
+            )
+
+        elif event == 'delete':
+            data_to_send = dict(
+                instance_id=data.get('id', '')
+            )
+            signals.closeio_delete.send(
+                sender=self.__class__,
+                model=model,
+                **data_to_send
+            )
+
+        elif event == 'merge':
+            data_to_send = dict(
+                source_id=data.get('source_id', ''),
+                destination_id=data.get('destination_id', ''),
+            )
+            signals.closeio_merge.send(
+                sender=self.__class__,
+                model=model,
+                **data_to_send
+            )
+
+        expl_signal_name = '%s_%s' % (model, event)
+
+        if hasattr(signals, expl_signal_name):
+            signal = getattr(signals, expl_signal_name)
+            signal.send(
+                sender=self.__class__,
+                **data_to_send
+            )
+
+        signals.closeio_event.send(
+            sender=self.__class__,
+            model=model,
+            event=event,
+            instance=data,
+        )
+
+        return HttpResponse()
