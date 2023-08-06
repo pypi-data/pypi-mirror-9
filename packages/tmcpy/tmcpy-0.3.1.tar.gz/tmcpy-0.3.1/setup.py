1b30823fc543159249e780315eeb6d621c5dab12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:22 +0000

    Add package tmcpy/0.3.1/tmcpy-0.3.1.tar.gz

---
+++ packages/tmcpy/tmcpy-0.3.1.tar.gz/tmcpy-0.3.1/setup.py
@@ -0,0 +1,56 @@
+# coding: utf-8
+from setuptools import setup, find_packages
+
+version = '0.3.1'
+
+setup(
+    name='tmcpy',
+    version=version,
+    description="淘宝平台消息服务客户端 for Python",
+    long_description="""
+tmcpy
+=======================
+
+淘宝平台消息服务python版本
+
+Usage:
+```python
+import logging
+
+from tmcpy import TmcClient
+
+logging.basicConfig(level=logging.DEBUG)
+
+tmc = TmcClient('ws://mc.api.tbsandbox.com/', 'appkey', 'appsecret', 'default',
+    query_message_interval=50)
+
+
+def print1():
+    print 'on_open'
+
+
+tmc.on("on_open", print1)
+try:
+    ioloop.IOLoop.instance().start()
+except KeyboardInterrupt:
+    pass
+finally:
+    tmc.close()
+```
+
+""",
+    keywords='taobao tmc python',
+    author='baocaixiong,messense',
+    author_email='baocaixiong@gmail.com,messense@icloud.com',
+    url='https://github.com/messense/tmcpy',
+    license='MIT',
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        # -*- Extra requirements: -*-
+        'tornado>=4.1',
+        'six>=1.9.0',
+        'simplejson>=3.6.5'
+    ]
+)
