7bdef998f5e7e4644ca82c99c2a41f732516c8a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:49 +0000

    Add package m3-core/3.0.1.3/m3-core-3.0.1.3.tar.gz

---
+++ packages/m3-core/m3-core-3.0.1.3.tar.gz/m3-core-3.0.1.3/setup.py
@@ -0,0 +1,37 @@
+#coding: utf-8
+import os
+from setuptools import setup, find_packages
+
+def read(fname):
+    try:
+        return open(os.path.join(os.path.dirname(__file__),
+            fname)).read()
+    except IOError:
+        return ''
+
+
+setup(name='m3-core',
+      version='3.0.1.3',
+      url='https://bitbucket.org/barsgroup/m3-core',
+      license='MIT',
+      author='BARS Group',
+      author_email='bars@bars-open.ru',
+      package_dir={'': 'src'},
+      packages=find_packages('src'),
+      description=read('DESCRIPTION.md'),
+      install_requires=read('REQUIREMENTS'),
+      long_description=read('README.md'),
+      include_package_data=True,
+      classifiers=[
+        'Intended Audience :: Developers',
+        'Environment :: Web Environment',
+        'Natural Language :: Russian',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'License :: OSI Approved :: MIT License',
+        'Development Status :: 5 - Production/Stable',
+      ],
+)
+
+
