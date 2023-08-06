8588f69e72ff3122868b1ee4871c404d5b1de738
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:09 +0000

    Add package iasap/0.0.1/iasap-0.0.1.tar.gz

---
+++ packages/iasap/iasap-0.0.1.tar.gz/iasap-0.0.1/iasap/test_sql.py
@@ -0,0 +1,13 @@
+# Copyright 2011-2015 梅濁酒(umedoblock)
+
+import unittest
+
+from sql import *
+print(dir())
+
+class TestSQL(unittest.TestCase):
+    def test_sql(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
