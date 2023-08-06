54199aa19c12f65cea4d47015892190e8d25238b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:52 +0000

    Add package settingsd/0.8.9/settingsd-0.8.9.tar.gz

---
+++ packages/settingsd/settingsd-0.8.9.tar.gz/settingsd-0.8.9/settingsd/finders.py
@@ -0,0 +1,56 @@
+# encoding: utf8
+"""
+Finders
+"""
+
+from __future__ import absolute_import
+from __future__ import print_function
+
+from . import utils
+
+
+def directory(settings, path):
+    import os
+
+    if not os.path.isdir(path):
+        return None
+
+    parts = list()
+    for name in os.listdir(path):
+        part = {'uri': os.path.join(path, name)}
+
+        def get_data(part=part):
+            with open(part['uri']) as fp:
+                return fp.read()
+
+        part['get_data'] = get_data
+        parts.append(part)
+
+    return parts
+
+
+def zipfile(settings, path):
+    import zipimport
+
+    try:
+        # abuse zipimport over zipfile because it's faster and potentially
+        # cached if we are already operating/importing from the archive
+        importer = zipimport.zipimporter(path)
+    except zipimport.ZipImportError:
+        return None
+
+    parts = list()
+    for info in importer._files.values():
+        if not info[0].startswith(path):
+            # random item in archive we don't care about
+            continue
+
+        part = {'uri': info[0]}
+
+        def get_data(part=part):
+            return importer.get_data(part['uri'])
+
+        part['get_data'] = get_data
+        parts.append(part)
+
+    return parts
---
+++ packages/settingsd/settingsd-0.8.9.tar.gz/settingsd-0.8.9/settingsd/loaders.py
@@ -0,0 +1,79 @@
+# encoding: utf8
+"""
+Loaders
+"""
+
+from __future__ import absolute_import
+from __future__ import print_function
+
+from . import utils
+
+
+def from_key(settings, keys):
+    loader = _load_from(settings, keys, 'key')
+    return loader
+
+
+def from_mime(settings, keys):
+    loader = _load_from(settings, keys, 'mime')
+    return loader
+
+
+def from_ext(settings, keys):
+    loader = _load_from(settings, keys, 'ext')
+    return loader
+
+
+def _load_from(settings, keys, suffix):
+    datum = keys[suffix.lower()]
+    opt = 'SETTINGSD_LOADER_FROM_' + suffix.upper()
+    registry = utils.getopt(settings, opt)
+    loader = registry.get(datum)
+    if loader and not hasattr(loader, '__call__'):
+        loader = utils.resolve_import(settings, loader)
+    if loader:
+        loader = loader(settings, keys)
+    return loader
+
+
+def python(settings, keys):
+    ns = utils.namespace(settings)
+    #TODO: SETTINGSD_COMPILE_FLAGS
+    code = keys['get_data']()
+    code = compile(code, keys['uri'], 'exec')
+    # eval can handle code objects compiled with exec (python[23])
+    eval(code, ns)
+    # signal successful loading
+    return True
+
+
+def json(settings, keys):
+    from json import loads
+    ns = utils.namespace(settings)
+    data = keys['get_data']()
+    new_ns = loads(data)
+    # ensure stable load ordering
+    for k in new_ns:
+        ns[k] = new_ns[k]
+    return True
+
+
+class JSONLoader(object):
+
+    def __init__(self, settings, keys):
+        self.keys = keys
+        self.cache = None
+
+    def __get__(self, settings, owner):
+        if settings is None:
+            return self
+
+        if not self.cache:
+            from json import loads
+            data = keys['get_data']()
+            self.cache = loads(data)
+
+        return self.cache
+
+    def __set__(self, settings, value):
+        pass
