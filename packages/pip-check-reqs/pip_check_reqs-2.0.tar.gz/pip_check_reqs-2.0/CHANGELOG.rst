0cd5a5e95653438d6ab6366010edb383d18e3911
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:36:18 +0000

    Add package pip-check-reqs/2.0/pip_check_reqs-2.0.tar.gz

---
+++ packages/pip-check-reqs/pip_check_reqs-2.0.tar.gz/pip_check_reqs-2.0/CHANGELOG.rst
@@ -0,0 +1,66 @@
+
+Release History
+---------------
+
+2.0 **renamed package to pip_check_reqs**
+
+- added tool pip-extra-reqs to find packages installed but not used
+  (contributed by Josh Hesketh)
+
+1.2.1
+
+- relax requirement to 6.0+
+
+1.2.0
+
+- bumped pip requirement to 6.0.8+
+- updated use of pip internals to match that version
+
+1.1.9
+
+- test fixes and cleanup
+- remove hard-coded simplejson debugging behaviour
+
+1.1.8
+
+- use os.path.realpath to avoid symlink craziness on debian/ubuntu
+
+1.1.7
+
+- tweak to debug output
+
+1.1.6
+
+- add debug (very verbose) run output
+
+1.1.5
+
+- add header to output to make it clearer when in a larger test run
+- fix tests and self-test
+
+1.1.4
+
+- add --version
+- remove debug print from released code lol
+
+1.1.3
+
+- fix program to generate exit code useful for testing
+
+1.1.2
+
+- corrected version of vendored search_packages_info() from pip
+- handle relative imports
+
+1.1.1
+
+- fixed handling of import from __future__
+- self-tested and added own requirements.txt
+- cleaned up usage to require a file or directory to scan (rather than
+  defaulting to ".")
+- vendored code from pip 1.6dev which fixes bug in search_packages_info
+  until pip 1.6 is released
+
+1.1.0 
+
+- implemented --ignore-module
---
+++ packages/pip-check-reqs/pip_check_reqs-2.0.tar.gz/pip_check_reqs-2.0/setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+with open(path.join(here, 'CHANGELOG.rst'), encoding='utf-8') as f:
+    long_description += f.read()
+
+from pip_check_reqs import __version__
+
+setup(
+    name='pip_check_reqs',
+    version=__version__,
+    description=
+        'Find packages that should or should not be in requirements for a '
+        'project',
+    long_description=long_description,
+    url='https://github.com/r1chardj0n3s/pip-missing-reqs',
+    author='Richard Jonees',
+    author_email='r1chardj0n3s@gmail.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+    ],
+    packages=['pip_check_reqs'],
+    entry_points={
+        'console_scripts': [
+            'pip-missing-reqs=pip_check_reqs.find_missing_reqs:main',
+            'pip-extra-reqs=pip_check_reqs.find_extra_reqs:main',
+        ],
+    },
+)
