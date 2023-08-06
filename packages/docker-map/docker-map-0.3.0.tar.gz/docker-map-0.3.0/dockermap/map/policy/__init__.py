16f7062f90f2911bb37760142a90ebae08a13f7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:52 +0000

    Add package docker-map/0.3.0/docker-map-0.3.0.tar.gz

---
+++ packages/docker-map/docker-map-0.3.0.tar.gz/docker-map-0.3.0/dockermap/map/policy/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from .actions import ACTION_ATTACHED_FLAG, ACTION_DEPENDENCY_FLAG
+
+from .simple import SimplePolicy
+from .resume import ResumeUpdatePolicy
---
+++ packages/docker-map/docker-map-0.3.0.tar.gz/docker-map-0.3.0/dockermap/map/policy/actions.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+
+ACTION_DEPENDENCY_FLAG = 1 << 1
+ACTION_ATTACHED_FLAG = 1 << 2
