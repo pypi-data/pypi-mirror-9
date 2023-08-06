852071ae17375d8dfb4325de0d50fdd8a6c9cd8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:28 +0000

    Add package OpenElectrons_i2c/1.5/OpenElectrons_i2c-1.6.tar.gz

---
+++ packages/OpenElectrons_i2c/OpenElectrons_i2c-1.6.tar.gz/OpenElectrons_i2c-1.6/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='OpenElectrons_i2c',
+    version='1.6',
+    description='OpenElectrons library',
+    author='openelectrons.com',
+    author_email='contact@openelectrons.com',
+    url='http://www.openelectrons.com/index.php?module=pagemaster&PAGE_user_op=view_page&PAGE_id=55',
+    py_modules=['OpenElectrons_i2c', 'OE_explorerlib'],
+    )
