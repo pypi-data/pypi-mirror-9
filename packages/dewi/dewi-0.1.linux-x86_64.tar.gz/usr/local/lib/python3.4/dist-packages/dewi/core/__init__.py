c58494d7a0dedee06872f03534b122adcd81ba2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:13 +0000

    Add package dewi/0.1/dewi-0.1.linux-x86_64.tar.gz

---
+++ packages/dewi/dewi-0.1.linux-x86_64.tar.gz/usr/local/lib/python3.4/dist-packages/dewi/core/__init__.py
@@ -0,0 +1,20 @@
+# Copyright 2015 Laszlo Attila Toth
+# Distributed under the terms of the GNU General Public License v3
+
+import collections
+
+from dewi.core.commandregistry import CommandRegistry, CommandRegistrar
+from dewi.core.context import Context
+
+from dewi.loader.plugin import Plugin
+
+
+class CorePlugin(Plugin):
+    def get_description(self):
+        return "Core features of DEWI"
+
+    def get_dependencies(self) -> collections.Iterable:
+        return ('dewi.core.commandregistry.CommandRegistryPlugin',)
+
+    def load(self, c: Context):
+        pass
