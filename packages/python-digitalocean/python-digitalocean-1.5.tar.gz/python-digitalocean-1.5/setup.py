bb8f0bec21dec7ee64ce83b7276229702d587bc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:04 +0000

    Add package python-digitalocean/1.5/python-digitalocean-1.5.tar.gz

---
+++ packages/python-digitalocean/python-digitalocean-1.5.tar.gz/python-digitalocean-1.5/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+import os
+try:
+    from setuptools import setup
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup
+
+long_description = """This library provides easy access to Digital Ocean APIs to deploy droplets, images and more."""
+
+if os.path.isfile("README.md"):
+    with open('README.md') as file:
+        long_description = file.read()
+
+setup(
+    name='python-digitalocean',
+    version='1.5',
+    description='digitalocean.com API to manage Droplets and Images',
+    author='Lorenzo Setale ( http://who.is.lorenzo.setale.me/? )',
+    author_email='koalalorenzo@gmail.com',
+    url='https://github.com/koalalorenzo/python-digitalocean',
+    packages=['digitalocean'],
+    install_requires=['requests'],
+    test_suite='digitalocean.tests',
+    license='LGPL v3',
+    long_description=long_description
+)
