a004ecb54d1125faaba6480cc0b173a18c7a32b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:30 +0000

    Add package see/1.1.0/see-1.1.0.tar.gz

---
+++ packages/see/see-1.1.0.tar.gz/see-1.1.0/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup( name='see',
+       version='1.1.0',
+       description='A human-readable alternative to dir',
+       author='Liam Cooke',
+       author_email='hi@liamcooke.com',
+       url='http://inky.github.io/see/',
+       py_modules=['see'],
+       classifiers=[
+           'Development Status :: 4 - Beta',
+           'Environment :: Console',
+           'Intended Audience :: Developers',
+           'License :: Freely Distributable',
+           'License :: OSI Approved :: BSD License',
+           'Natural Language :: English',
+           'Operating System :: OS Independent',
+           'Programming Language :: Python',
+           'Topic :: Software Development',
+       ])
