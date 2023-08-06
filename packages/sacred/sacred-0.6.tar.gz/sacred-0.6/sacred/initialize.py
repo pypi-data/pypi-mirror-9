64908a8b0de9043a98c69cddfab13e97461e5ce6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:13 +0000

    Add package sacred/0.6/sacred-0.6.tar.gz

---
+++ packages/sacred/sacred-0.6.tar.gz/sacred-0.6/sacred/initialize.py
@@ -0,0 +1,290 @@
+#!/usr/bin/env python
+# coding=utf-8
+from __future__ import division, print_function, unicode_literals
+
+import os
+from collections import OrderedDict, defaultdict
+from copy import copy
+
+from sacred.config import (ConfigDict, chain_evaluate_config_scopes, dogmatize,
+                           load_config_file)
+from sacred.config.config_summary import ConfigSummary
+from sacred.host_info import get_host_info
+from sacred.randomness import create_rnd, get_seed
+from sacred.run import Run
+from sacred.utils import (convert_to_nested_dict, create_basic_stream_logger,
+                          get_by_dotted_path, is_prefix, iter_path_splits,
+                          iterate_flattened, set_by_dotted_path)
+
+__sacred__ = True  # marks files that should be filtered from stack traces
+
+
+class Scaffold(object):
+    def __init__(self, config_scopes, subrunners, path, captured_functions,
+                 commands, named_configs, generate_seed):
+        self.config_scopes = config_scopes
+        self.named_configs = named_configs
+        self.subrunners = subrunners
+        self.path = path
+        self.generate_seed = generate_seed
+        self.config_updates = {}
+        self.named_configs_to_use = []
+        self.config = None
+        self.fixture = None  # TODO: rename
+        self.logger = None
+        self.seed = None
+        self.rnd = None
+        self._captured_functions = captured_functions
+        self.commands = commands
+        self.config_mods = None
+        self.summaries = []
+
+    def set_up_seed(self, rnd=None):
+        if self.seed is not None:
+            return
+
+        self.seed = self.config.get('seed') or get_seed(rnd)
+        self.rnd = create_rnd(self.seed)
+
+        if self.generate_seed:
+            self.config['seed'] = self.seed
+
+        if 'seed' in self.config and 'seed' in self.config_mods.added:
+            self.config_mods.modified.add('seed')
+            self.config_mods.added -= {'seed'}
+
+        # Hierarchically set the seed of proper subrunners
+        for subrunner_path, subrunner in reversed(list(
+                self.subrunners.items())):
+            if is_prefix(self.path, subrunner_path):
+                subrunner.set_up_seed(self.rnd)
+
+    def set_up_config(self):
+        if self.config is not None:
+            return self.config
+
+        # gather presets
+        fallback = {}
+        for sr_path, subrunner in self.subrunners.items():
+            if self.path and is_prefix(self.path, sr_path):
+                path = sr_path[len(self.path):].strip('.')
+                set_by_dotted_path(fallback, path, subrunner.config)
+            else:
+                set_by_dotted_path(fallback, sr_path, subrunner.config)
+
+        # dogmatize to make the subrunner configurations read-only
+        const_fallback = dogmatize(fallback)
+        const_fallback.revelation()
+
+        self.config = {}
+
+        # named configs first
+        cfg_list = []
+        for ncfg in self.named_configs_to_use:
+            if os.path.exists(ncfg):
+                cfg_list.append(ConfigDict(load_config_file(ncfg)))
+            else:
+                cfg_list.append(self.named_configs[ncfg])
+
+        self.config_updates, _ = chain_evaluate_config_scopes(
+            cfg_list,
+            fixed=self.config_updates,
+            preset=self.config,
+            fallback=const_fallback)
+
+        # unnamed (default) configs second
+        self.config, self.summaries = chain_evaluate_config_scopes(
+            self.config_scopes,
+            fixed=self.config_updates,
+            preset=self.config,
+            fallback=const_fallback)
+
+        self.get_config_modifications()
+
+    def get_config_modifications(self):
+        self.config_mods = ConfigSummary()
+        for cfg_summary in self.summaries:
+            self.config_mods.update_from(cfg_summary)
+
+    def get_fixture(self):
+        if self.fixture is not None:
+            return self.fixture
+
+        self.fixture = copy(self.config)
+        for sr_path, subrunner in self.subrunners.items():
+            sub_fix = subrunner.get_fixture()
+            sub_path = sr_path
+            if is_prefix(self.path, sub_path):
+                sub_path = sr_path[len(self.path):].strip('.')
+            # Note: This might fail if we allow non-dict fixtures
+            set_by_dotted_path(self.fixture, sub_path, sub_fix)
+        return self.fixture
+
+    def finalize_initialization(self, run):
+        # look at seed again, because it might have changed during the
+        # configuration process
+        if 'seed' in self.config:
+            self.seed = self.config['seed']
+        self.rnd = create_rnd(self.seed)
+
+        for cfunc in self._captured_functions:
+            cfunc.logger = self.logger.getChild(cfunc.__name__)
+            cfunc.config = get_by_dotted_path(self.get_fixture(), cfunc.prefix)
+            seed = get_seed(self.rnd)
+            cfunc.rnd = create_rnd(seed)
+            cfunc.run = run
+
+        self._warn_about_suspicious_changes()
+
+    def _warn_about_suspicious_changes(self):
+        for add in sorted(self.config_mods.added):
+            self.logger.warning('Added new config entry: "%s"' % add)
+
+        for key, (type_old, type_new) in self.config_mods.typechanged.items():
+            if (isinstance(type_old, type(None)) or
+                    (type_old in (int, float) and type_new in (int, float))):
+                continue
+            self.logger.warning(
+                'Changed type of config entry "%s" from %s to %s' %
+                (key, type_old.__name__, type_new.__name__))
+
+        for cfg_summary in self.summaries:
+            for key in cfg_summary.ignored_fallbacks:
+                self.logger.warning(
+                    'Ignored attempt to set value of "%s", because it is an '
+                    'ingredient.' % key
+                )
+
+
+def get_configuration(scaffolding):
+    config = {}
+    for sc_path, scaffold in reversed(list(scaffolding.items())):
+        if sc_path:
+            set_by_dotted_path(config, sc_path, scaffold.config)
+        else:
+            config.update(scaffold.config)
+    return config
+
+
+def distribute_config_updates(scaffolding, config_updates):
+    if config_updates is None:
+        return
+    nested_config_updates = convert_to_nested_dict(config_updates)
+    for path, value in iterate_flattened(nested_config_updates):
+        for prefix, suffix in reversed(list(iter_path_splits(path))):
+            if prefix in scaffolding:
+                set_by_dotted_path(scaffolding[prefix].config_updates, suffix,
+                                   value)
+                break
+                # this is guaranteed to occur for one of the modrunners,
+                # because the exrunner has path ''
+
+
+def distribute_named_configs(scaffolding, named_configs):
+    for ncfg in named_configs:
+        if os.path.exists(ncfg):
+            scaffolding[''].named_configs_to_use.append(ncfg)
+        else:
+            path, _, cfg_name = ncfg.rpartition('.')
+            if path not in scaffolding:
+                raise KeyError('Ingredient for named config "{}" not found'
+                               .format(ncfg))
+            scaffolding[path].named_configs_to_use.append(cfg_name)
+
+
+def initialize_logging(experiment, scaffolding, loglevel=None):
+    if experiment.logger is None:
+        if loglevel:
+            try:
+                loglevel = int(loglevel)
+            except ValueError:
+                pass
+        root_logger = create_basic_stream_logger('', level=loglevel)
+    else:
+        root_logger = experiment.logger
+        if loglevel:
+            root_logger.setLevel(loglevel)
+
+    for sc_path, scaffold in scaffolding.items():
+        if sc_path:
+            scaffold.logger = root_logger.getChild(sc_path)
+        else:
+            scaffold.logger = root_logger
+
+    return root_logger.getChild(experiment.name)
+
+
+def create_scaffolding(experiment):
+    sorted_ingredients = gather_ingredients_topological(experiment)
+    scaffolding = OrderedDict()
+    for ingredient in sorted_ingredients:
+        scaffolding[ingredient] = Scaffold(
+            ingredient.cfgs,
+            subrunners=OrderedDict([(scaffolding[m].path, scaffolding[m])
+                                    for m in ingredient.ingredients]),
+            path=ingredient.path if ingredient != experiment else '',
+            captured_functions=ingredient.captured_functions,
+            commands=ingredient.commands,
+            named_configs=ingredient.named_configs,
+            generate_seed=ingredient.gen_seed)
+    return OrderedDict([(sc.path, sc) for sc in scaffolding.values()])
+
+
+def gather_ingredients_topological(ingredient):
+    sub_ingredients = defaultdict(int)
+    for ingredient, depth in ingredient._traverse_ingredients():
+        sub_ingredients[ingredient] = max(sub_ingredients[ingredient], depth)
+    return sorted(sub_ingredients, key=lambda x: -sub_ingredients[x])
+
+
+def get_config_modifications(scaffolding):
+    config_modifications = ConfigSummary()
+    for sc_path, scaffold in scaffolding.items():
+        config_modifications.update_from(scaffold.config_mods, path=sc_path)
+    return config_modifications
+
+
+def get_command(scaffolding, command_path):
+    path, _, command_name = command_path.rpartition('.')
+    if path not in scaffolding:
+        raise KeyError('Ingredient for command "%s" not found.' % command_path)
+
+    if command_name in scaffolding[path].commands:
+        return scaffolding[path].commands[command_name]
+    else:
+        if path:
+            raise KeyError('Command "%s" not found in ingredient "%s"' %
+                           (command_name, path))
+        else:
+            raise KeyError('Command "%s" not found' % command_name)
+
+
+def create_run(experiment, command_name, config_updates=None, log_level=None,
+               named_configs=()):
+    scaffolding = create_scaffolding(experiment)
+
+    distribute_config_updates(scaffolding, config_updates)
+    distribute_named_configs(scaffolding, named_configs)
+
+    for scaffold in scaffolding.values():
+        scaffold.set_up_config()
+
+    for scaffold in reversed(list(scaffolding.values())):
+        scaffold.set_up_seed()  # partially recursive
+
+    config = get_configuration(scaffolding)
+    config_modifications = get_config_modifications(scaffolding)
+
+    experiment_info = experiment._get_info()
+    host_info = get_host_info()
+    main_function = get_command(scaffolding, command_name)
+
+    logger = initialize_logging(experiment, scaffolding, log_level)
+    run = Run(config, config_modifications, main_function,
+              experiment.observers, logger, experiment_info,
+              host_info)
+
+    for scaffold in scaffolding.values():
+        scaffold.finalize_initialization(run=run)
+
+    return run
---
+++ packages/sacred/sacred-0.6.tar.gz/sacred-0.6/sacred/run.py
@@ -0,0 +1,156 @@
+#!/usr/bin/env python
+# coding=utf-8
+from __future__ import division, print_function, unicode_literals
+
+import datetime
+import os.path
+import sys
+import threading
+import traceback
+
+from sacred.randomness import set_global_seed
+from sacred.utils import tee_output
+
+__sacred__ = True  # marks files that should be filtered from stack traces
+
+
+class Run(object):
+
+    """Represent and manage a single run of an experiment."""
+
+    def __init__(self, config, config_modifications, main_function, observers,
+                 logger, experiment_info, host_info):
+        self.config = config
+        self.main_function = main_function
+        self.config_modifications = config_modifications
+        self._observers = observers
+        self.logger = logger
+        self.experiment_info = experiment_info
+        self.host_info = host_info
+        self.info = {}
+        self._heartbeat = None
+        self.captured_out = None
+        self.start_time = None
+        self.stop_time = None
+        self.elapsed_time = None
+        self.result = None
+
+    def open_resource(self, filename):
+        filename = os.path.abspath(filename)
+        self._emit_resource_added(filename)  # TODO: maybe non-blocking?
+        return open(filename, 'r')  # TODO: How to deal with binary mode?
+
+    def add_artifact(self, filename):
+        filename = os.path.abspath(filename)
+        self._emit_artifact_added(filename)
+
+    def __call__(self, *args):
+        set_global_seed(self.config['seed'])
+        with tee_output() as self.captured_out:
+            self.logger.info('Started')
+            self._emit_started()
+            self._start_heartbeat()
+            try:
+                self.result = self.main_function(*args)
+                self._stop_heartbeat()
+                self._emit_completed(self.result)
+                return self.result
+            except KeyboardInterrupt:
+                self._stop_heartbeat()
+                self._emit_interrupted()
+                raise
+            except:
+                exc_type, exc_value, trace = sys.exc_info()
+                self._stop_heartbeat()
+                self._emit_failed(exc_type, exc_value, trace.tb_next)
+                raise
+
+    def _start_heartbeat(self):
+        self._emit_heatbeat()
+        self._heartbeat = threading.Timer(10, self._start_heartbeat)
+        self._heartbeat.start()
+
+    def _stop_heartbeat(self):
+        if self._heartbeat is None:
+            return
+        self._heartbeat.cancel()
+        self._heartbeat = None
+        self._emit_heatbeat()  # one final beat to flush pending changes
+
+    def _emit_started(self):
+        self.start_time = datetime.datetime.now()
+        for observer in self._observers:
+            try:
+                observer.started_event(
+                    ex_info=self.experiment_info,
+                    host_info=self.host_info,
+                    start_time=self.start_time,
+                    config=self.config)
+            except AttributeError:
+                pass
+
+    def _emit_heatbeat(self):
+        if self.start_time is None or self.stop_time is not None:
+            return
+        beat_time = datetime.datetime.now()
+        for observer in self._observers:
+            try:
+                observer.heartbeat_event(
+                    info=self.info,
+                    captured_out=self.captured_out.getvalue(),
+                    beat_time=beat_time)
+            except AttributeError:
+                pass
+
+    def _stop_time(self):
+        self.stop_time = datetime.datetime.now()
+        self.elapsed_time = datetime.timedelta(
+            seconds=round((self.stop_time - self.start_time).total_seconds()))
+        return self.stop_time
+
+    def _emit_completed(self, result):
+        stop_time = self._stop_time()
+        self.logger.info('Completed after %s' % self.elapsed_time)
+        for observer in self._observers:
+            try:
+                observer.completed_event(
+                    stop_time=stop_time,
+                    result=result)
+            except AttributeError:
+                pass
+
+    def _emit_interrupted(self):
+        interrupt_time = self._stop_time()
+        self.logger.warning("Aborted after %s!" % self.elapsed_time)
+        for observer in self._observers:
+            try:
+                observer.interrupted_event(
+                    interrupt_time=interrupt_time)
+            except AttributeError:
+                pass
+
+    def _emit_failed(self, exc_type, exc_value, trace):
+        fail_time = self._stop_time()
+        self.logger.error("Failed after %s!" % self.elapsed_time)
+        fail_trace = traceback.format_exception(exc_type, exc_value, trace)
+        for observer in self._observers:
+            try:
+                observer.failed_event(
+                    fail_time=fail_time,
+                    fail_trace=fail_trace)
+            except:  # _emit_failed should never throw
+                pass
+
+    def _emit_resource_added(self, filename):
+        for observer in self._observers:
+            try:
+                observer.resource_event(filename=filename)
+            except AttributeError:
+                pass
+
+    def _emit_artifact_added(self, filename):
+        for observer in self._observers:
+            try:
+                observer.artifact_event(filename=filename)
+            except AttributeError:
+                pass
