f5d4e7efc62c4855562178ec2b7a0623e13e803e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:07:40 +0000

    Add package chargebee/1.4.5/chargebee-1.4.5.tar.gz

---
+++ packages/chargebee/chargebee-1.4.5.tar.gz/chargebee-1.4.5/chargebee/models/comment.py
@@ -0,0 +1,25 @@
+import json
+from chargebee.model import Model
+from chargebee import request
+from chargebee import APIError
+
+class Comment(Model):
+
+    fields = ["id", "entity_type", "added_by", "notes", "created_at", "type", "entity_id"]
+
+
+    @staticmethod
+    def create(params, env=None):
+        return request.send('post', request.uri_path("comments"), params, env)
+
+    @staticmethod
+    def retrieve(id, env=None):
+        return request.send('get', request.uri_path("comments",id), None, env)
+
+    @staticmethod
+    def list(params=None, env=None):
+        return request.send('get', request.uri_path("comments"), params, env)
+
+    @staticmethod
+    def delete(id, env=None):
+        return request.send('post', request.uri_path("comments",id,"delete"), None, env)
