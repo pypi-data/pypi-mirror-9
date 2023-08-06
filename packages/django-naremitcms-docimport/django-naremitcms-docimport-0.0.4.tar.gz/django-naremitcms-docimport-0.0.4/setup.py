4bc6b8cb0607b88615986345eae2d82fff72a04d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:07:39 +0000

    Add package django-naremitcms-docimport/0.0.4/django-naremitcms-docimport-0.0.4.tar.gz

---
+++ packages/django-naremitcms-docimport/django-naremitcms-docimport-0.0.4.tar.gz/django-naremitcms-docimport-0.0.4/setup.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name='django-naremitcms-docimport',
+    version='0.0.4',
+    author=u'Jon Combe',
+    author_email='jon@naremit.com',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[],
+    url='http://naremitcms.com',
+    license='BSD licence, see LICENCE file',
+    description='A static text file import tool for NaremitCMS',
+    long_description='A static text file import tool for NaremitCMS',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+    ],
+    zip_safe=False,
+)
