f104417ef19a14538cfe348e75a5af3ae88fd5ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:48 +0000

    Add package django-taggit/0.13.0/django-taggit-0.13.0.tar.gz

---
+++ packages/django-taggit/django-taggit-0.13.0.tar.gz/django-taggit-0.13.0/setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup, find_packages
+
+import taggit
+
+
+with open('README.rst') as f:
+    readme = f.read()
+
+setup(
+    name='django-taggit',
+    version='.'.join(str(i) for i in taggit.VERSION),
+    description='django-taggit is a reusable Django application for simple tagging.',
+    long_description=readme,
+    author='Alex Gaynor',
+    author_email='alex.gaynor@gmail.com',
+    url='http://github.com/alex/django-taggit/tree/master',
+    packages=find_packages(exclude=('tests*',)),
+    package_data = {
+        'taggit': [
+            'locale/*/LC_MESSAGES/*',
+        ],
+    },
+    license='BSD',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Framework :: Django',
+    ],
+    include_package_data=True,
+    zip_safe=False,
+)
