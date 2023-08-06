3de088724dd1cc4d8bb7ff1996ce11573bf1beaa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:11 +0000

    Add package django-parler/1.3/django-parler-1.3.tar.gz

---
+++ packages/django-parler/django-parler-1.3.tar.gz/django-parler-1.3/setup.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+from os import path
+import codecs
+import os
+import re
+import sys
+
+
+# When creating the sdist, make sure the django.mo file also exists:
+if 'sdist' in sys.argv or 'develop' in sys.argv:
+    try:
+        os.chdir('parler')
+        from django.core import management
+        management.call_command('compilemessages')
+    finally:
+        os.chdir('..')
+
+
+def read(*parts):
+    file_path = path.join(path.dirname(__file__), *parts)
+    return codecs.open(file_path, encoding='utf-8').read()
+
+
+def find_version(*parts):
+    version_file = read(*parts)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", version_file, re.M)
+    if version_match:
+        return str(version_match.group(1))
+    raise RuntimeError("Unable to find version string.")
+
+
+setup(
+    name='django-parler',
+    version=find_version('parler', '__init__.py'),
+    license='Apache License, Version 2.0',
+
+    requires=[
+        'Django (>=1.4.2)',
+    ],
+
+    description='Simple Django model translations without nasty hacks, featuring nice admin integration.',
+    long_description=read('README.rst'),
+
+    author='Diederik van der Boor',
+    author_email='opensource@edoburu.nl',
+
+    url='https://github.com/edoburu/django-parler',
+    download_url='https://github.com/edoburu/django-parler/zipball/master',
+
+    packages=find_packages(exclude=('example*',)),
+    include_package_data=True,
+
+    test_suite = 'runtests',
+
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
