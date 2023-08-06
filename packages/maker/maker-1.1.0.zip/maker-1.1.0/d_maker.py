919036c54fd53160780499024cc0c2349e7274ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:08 +0000

    Add package maker/1.1.0/maker-1.1.0.zip

---
+++ packages/maker/maker-1.1.0.zip/maker-1.1.0/d_maker.py
@@ -0,0 +1,16 @@
+"""This is the “d_maker.py" module, and it provides one function called print_lol()
+which prints lists that may or may not include nested lists."""
+
+def print_lol(the_list, level):
+
+        """This function takes a positional argument called “the_list",
+        which is any Python list (of, possibly, nested lists).
+        Each data item in the provided list is (recursively) printed to the screen on its own line"""
+
+        for each_item in the_list:
+                if isinstance(each_item, list):
+                        print_lol(each_item, level+1)
+                else:
+                        for tab_stop in range(level):
+                                print("\t", end='')
+                        print(each_item)
