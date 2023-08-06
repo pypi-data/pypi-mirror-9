155346c8aab0388fc0a1a274b2f168737a7125f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:23 +0000

    Add package vod_metadata/2015.3.2/vod_metadata-2015.3.2-py3-none-any.whl

---
+++ packages/vod_metadata/vod_metadata-2015.3.2-py3-none-any.whl/vod_metadata/__main__.py
@@ -0,0 +1,25 @@
+# VOD metadata file generator
+# Copyright 2014 Bo Bayles (bbayles@gmail.com)
+# See http://github.com/bbayles/vod_metadata for documentation and license
+from __future__ import print_function
+
+from io import open
+import os
+
+from vod_metadata import extensions
+from vod_metadata.md_gen import generate_metadata
+
+
+if __name__ == "__main__":
+    for file_path in os.listdir(os.getcwd()):
+        # Only process movie files
+        file_name, file_ext = os.path.splitext(file_path)
+        if file_ext not in extensions:
+            continue
+        # Create the VodPackage instace
+        print("Processing {}...".format(file_path))
+        vod_package = generate_metadata(file_path)
+        # Write the result
+        s = vod_package.write_xml(rewrite=True)
+        with open(vod_package.xml_path, "wb") as outfile:
+            _ = outfile.write(s)
