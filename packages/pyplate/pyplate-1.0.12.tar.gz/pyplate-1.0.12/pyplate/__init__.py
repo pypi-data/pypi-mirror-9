3deb3452635dc9efa0937eeb23ee90bd02282958
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:29:33 +0000

    Add package pyplate/1.0.12/pyplate-1.0.12.tar.gz

---
+++ packages/pyplate/pyplate-1.0.12.tar.gz/pyplate-1.0.12/pyplate/__init__.py
@@ -0,0 +1,9 @@
+import solve
+import metadata
+import image
+import database
+import pipeline
+import conf
+from _version import __version__
+
+__all__ = ['solve', 'metadata', 'image', 'database', 'pipeline', 'conf']
