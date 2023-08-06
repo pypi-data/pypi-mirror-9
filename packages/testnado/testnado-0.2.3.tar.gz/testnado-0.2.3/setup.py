b5287c8f4ee816ea9d7c4590f732f67aa9fa529b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:42 +0000

    Add package testnado/0.2.3/testnado-0.2.3.tar.gz

---
+++ packages/testnado/testnado-0.2.3.tar.gz/testnado-0.2.3/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+
+with open("VERSION") as version_fp:
+    VERSION = version_fp.read().strip()
+
+setup(
+    name="testnado",
+    version=VERSION,
+    description="Tornado test helpers and Selenium testing with Tornado",
+    author='Josh Marshall',
+    author_email='catchjosh@gmail.com',
+    url="http://github.com/joshmarshall/testnado/",
+    license="http://www.apache.org/licenses/LICENSE-2.0",
+    package_data={"": ["VERSION"]},
+    packages=find_packages(exclude=["tests", "dist"]),
+    install_requires=["tornado", "selenium", "mock"]
+)
