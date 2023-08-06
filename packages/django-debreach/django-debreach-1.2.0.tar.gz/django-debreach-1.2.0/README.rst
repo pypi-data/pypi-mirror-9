634164f8f02e043901e5a0a3478a3082b1e32d7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:17 +0000

    Add package django-debreach/1.2.0/django-debreach-1.2.0.tar.gz

---
+++ packages/django-debreach/django-debreach-1.2.0.tar.gz/django-debreach-1.2.0/README.rst
@@ -0,0 +1,137 @@
+django-debreach
+===============
+
+Basic mitigation against the `BREACH attack <http://breachattack.com/>`_ for 
+Django projects. 
+
+When combined with rate limiting in your web-server, or by using something
+like `django-ratelimit <http://django-ratelimit.readthedocs.org/>`_, the 
+techniques here should provide at least some protection against the BREACH 
+attack.
+
+.. image:: https://travis-ci.org/lpomfrey/django-debreach.png?branch=master
+    :target: https://travis-ci.org/lpomfrey/django-debreach
+    :alt: Build status
+
+.. image:: https://coveralls.io/repos/lpomfrey/django-debreach/badge.png?branch=master
+    :target: https://coveralls.io/r/lpomfrey/django-debreach?branch=master
+    :alt: Coverage
+
+.. image:: https://pypip.in/version/django-debreach/badge.svg
+    :target: https://pypi.python.org/pypi/django-debreach
+    :alt: Latest version
+
+.. image:: https://pypip.in/download/django-debreach/badge.svg
+    :target: https://pypi.python.org/pypi/django-debreach
+    :alt: Downloads
+
+.. image:: https://pypip.in/license/django-debreach/badge.svg
+    :target: https://pypi.python.org/pypi/django-debreach
+    :alt: License
+
+.. image:: https://pypip.in/py_versions/django-debreach/badge.svg
+    :target: https://pypi.python.org/pypi/django-debreach
+    :alt: Supported Python versions
+
+Installation
+------------
+
+Install from PyPI using::
+
+    $ pip install django-debreach
+
+Add to your `INSTALLED_APPS`::
+
+    INSTALLED_APPS = (
+        ...
+        'debreach',
+        ...
+    )
+
+Configuration
+-------------
+
+CSRF token masking
+^^^^^^^^^^^^^^^^^^
+To mask CSRF tokens in the template add the
+``debreach.context_processors.csrf`` context processor to the end of your 
+`TEMPLATE_CONTEXT_PROCESSORS`::
+
+    TEMPLATE_CONTEXT_PROCESSORS = (
+        ...
+        'debreach.context_processors.csrf',
+    )
+
+And add the ``debreach.middleware.CSRFCryptMiddleware`` to your middleware,
+*before* ``django.middleware.csrf.CSRFMiddleware``::
+
+    MIDDLEWARE_CLASSES = (
+        'debreach.middleware.CSRFCryptMiddleware',
+        ...
+        'django.middleware.csrf.CSRFMiddleware',
+        ...
+    )
+
+This works by xor-ing the CSRF token when it is added to the template,
+so that ``{% csrf_token %}`` now produces a hidden field with a value that is 
+``"<random-string>$<actual-csrf-token-xor-ed-with-random-string>"``.
+Then, when the form is POSTed, the middleware xors the CSRF token back into
+it's original form. This ensures that the CSRF content is never the same
+between requests. If you are passing the token using the ``X-CSRFToken``
+header (e.g. using XHR) that header will also be processed in the same way.
+
+Note that values that are unchanged by django-debreach, or rather, don't 
+contain a delimiting ``$``, will be left unmodified. The middleware will
+also not operate on views marked as being exempt from CSRF protection
+using the ``django.views.decorators.csrf.csrf_exempt`` decorator.
+
+CSRF protection using csrf_protect
+""""""""""""""""""""""""""""""""""
+
+If you don't use the CSRF middleware from django but, instead, apply the
+``django.views.decorators.csrf.csrf_protect`` decorator to selected
+views, and don't want to use the ``debreach.middleware.CSRFCryptMiddleware``, 
+then you can use the ``debreach.decorators.csrf_decrypt`` decorator.
+
+To use the ``debreach.decorators.csrf_decrypt`` decorator simply wrap
+your CSRF protected view with the decorator, like so::
+
+    @csrf_protect
+    @csrf_decrypt
+    def view(request, *args, **kwargs):
+        return HttpResponse('')
+
+
+Content length modification
+^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+django-debreach also enables you to counter the BREACH attack by randomising the
+content length of each response. This is acheived by adding a random string of 
+between 12 and 25 characters as a comment to the end of the HTML content. Note
+that this will only be applied to responses with a content type of
+``text/html``.
+
+To enable content length modification for all responses, add the
+``debreach.middleware.RandomCommentMiddleware`` to the *start* of your
+middleware, but *after* the ``GzipMiddleware`` if you are using that.::
+
+    MIDDLEWARE_CLASSES = (
+        'debreach.middleware.RandomCommentMiddleware',
+        ...
+    )
+
+or::
+
+    MIDDLEWARE_CLASSES = (
+        'django.middleware.gzip.GzipMiddleware',
+        'debreach.middleware.RandomCommentMiddleware',
+        ...
+    )
+
+If you wish to disable this feature for selected views, simply apply the
+``debreach.decorators.random_comment_exempt`` decorator to the view.
+
+If you only want to protect a subset of views with content length modification
+then it may be easier to not use the middleware, but to selectively apply the
+``debreach.decorators.append_random_comment`` decorator to the views you want
+protected.
---
+++ packages/django-debreach/django-debreach-1.2.0.tar.gz/django-debreach-1.2.0/debreach/tests.py
@@ -0,0 +1,301 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+import os
+import re
+
+from django.core.exceptions import SuspiciousOperation
+from django.core.urlresolvers import reverse
+from django.http import HttpResponse
+from django.test import TestCase
+from django.test.client import RequestFactory
+from django.utils.unittest import skipUnless
+from django.views.decorators.csrf import csrf_exempt
+
+from debreach.compat import force_text, get_random_string
+from debreach.context_processors import csrf
+from debreach.decorators import append_random_comment, random_comment_exempt
+from debreach.middleware import CSRFCryptMiddleware, RandomCommentMiddleware
+
+try:
+    unichr
+except NameError:
+    pass
+else:
+    chr = unichr
+
+
+def test_view(request):
+    return HttpResponse()
+
+
+class TestCSRFCryptMiddleware(TestCase):
+
+    def test_not_encoded(self):
+        request = RequestFactory().post('/', {'csrfmiddlewaretoken': 'abc123'})
+        middleware = CSRFCryptMiddleware()
+        middleware.process_view(request, test_view, (), {})
+        self.assertEqual(request.POST.get('csrfmiddlewaretoken'), 'abc123')
+
+    def test_encoded(self):
+        request = RequestFactory().post(
+            '/',
+            {'csrfmiddlewaretoken': 'aBcDeF$ACAAdVd1'}
+        )
+        middleware = CSRFCryptMiddleware()
+        middleware.process_view(request, test_view, (), {})
+        self.assertEqual(request.POST.get('csrfmiddlewaretoken'), 'abc123')
+
+    def test_mutable_status(self):
+        request = RequestFactory().post(
+            '/',
+            {'csrfmiddlewaretoken': 'aBcDeF$ACAAdVd1'}
+        )
+        request.POST._mutable = False
+        middleware = CSRFCryptMiddleware()
+        middleware.process_view(request, test_view, (), {})
+        self.assertFalse(request.POST._mutable)
+        request = RequestFactory().post(
+            '/',
+            {'csrfmiddlewaretoken': 'aBcDeF$ACAAdVd1'}
+        )
+        request.POST._mutable = True
+        middleware = CSRFCryptMiddleware()
+        middleware.process_view(request, test_view, (), {})
+        self.assertTrue(request.POST._mutable)
+
+    def test_header_not_encoded(self):
+        request = RequestFactory().post('/', HTTP_X_CSRFTOKEN='abc123')
+        middleware = CSRFCryptMiddleware()
+        middleware.process_view(request, test_view, (), {})
+        self.assertEqual(request.META.get('HTTP_X_CSRFTOKEN'), 'abc123')
+
+    def test_header_encoded(self):
+        request = RequestFactory().post(
+            '/', HTTP_X_CSRFTOKEN='aBcDeF$ACAAdVd1',
+        )
+        middleware = CSRFCryptMiddleware()
+        middleware.process_view(request, test_view, (), {})
+        self.assertEqual(request.META.get('HTTP_X_CSRFTOKEN'), 'abc123')
+
+    def test_tampering(self):
+        request = RequestFactory().post(
+            '/', {'csrfmiddlewaretoken': '123456$abc'})
+        middleware = CSRFCryptMiddleware()
+        with self.assertRaises(SuspiciousOperation):
+            middleware.process_view(request, test_view, (), {})
+
+    def test_header_tampering(self):
+        request = RequestFactory().post('/', HTTP_X_CSRFTOKEN='123456$abc')
+        middleware = CSRFCryptMiddleware()
+        with self.assertRaises(SuspiciousOperation):
+            middleware.process_view(request, test_view, (), {})
+
+    def test_csrf_exempt(self):
+        # This is an odd test. We're testing that, when a view is csrf_exempt,
+        # process_view will bail without performing any processing.
+        request = RequestFactory().post('/', HTTP_X_CSRFTOKEN="aB$AHM")
+        middleware = CSRFCryptMiddleware()
+        middleware.process_view(request, csrf_exempt(test_view), (), {})
+        self.assertEqual("aB$AHM", request.META['HTTP_X_CSRFTOKEN'])
+
+
+class TestRandomCommentMiddleware(TestCase):
+
+    def test_noop_on_wrong_content_type(self):
+        response = HttpResponse('abc', content_type='text/plain')
+        request = RequestFactory().get('/')
+        middleware = RandomCommentMiddleware()
+        response = middleware.process_response(request, response)
+        self.assertEqual(response.content, b'abc')
+
+    def test_html_content_type(self):
+        html = '''<!doctype html>
+<html>
+    <head>
+        <title>Page title</title>
+    </head>
+    <body>
+        <h1>Test</h1>
+        <p>Lorem ipsum</p>
+    </body>
+</html>'''
+        response = HttpResponse(html, content_type='text/html')
+        request = RequestFactory().get('/')
+        middleware = RandomCommentMiddleware()
+        response = middleware.process_response(request, response)
+        self.assertNotEqual(response.content, html)
+
+    def test_unicode_characters(self):
+        html = '''<!doctype html>
+<html>
+    <head>
+        <title>Page title</title>
+    </head>
+    <body>
+        <h1>Test</h1>
+        <p>{0}</p>
+    </body>
+</html>'''.format(''.join(chr(x) for x in range(9999)))
+        response = HttpResponse(html, content_type='text/html')
+        request = RequestFactory().get('/')
+        middleware = RandomCommentMiddleware()
+        response = middleware.process_response(request, response)
+        self.assertNotEqual(force_text(response.content), force_text(html))
+
+    def test_exemption(self):
+        html = '''<html>
+    <head><title>Test</title></head>
+    <body><p>Test body.</p></body>
+</html>'''
+        response = HttpResponse(html)
+        response._random_comment_exempt = True
+        request = RequestFactory().get('/')
+        middleware = RandomCommentMiddleware()
+        response = middleware.process_response(request, response)
+        self.assertEqual(force_text(response.content), html)
+
+    def test_missing_content_type(self):
+        request = RequestFactory().get('/')
+        response = HttpResponse('')
+        del response['Content-Type']
+        middleware = RandomCommentMiddleware()
+        processed_response = middleware.process_response(request, response)
+        self.assertEqual(response, processed_response)
+
+    def test_empty_response_body_ignored(self):
+        request = RequestFactory().get('/')
+        response = HttpResponse('')
+        middleware = RandomCommentMiddleware()
+        processed_response = middleware.process_response(request, response)
+        self.assertEqual(len(processed_response.content), 0)
+
+
+class TestDecorators(TestCase):
+
+    def test_append_random_comment(self):
+        html = '''<html>
+    <head><title>Test</title></head>
+    <body><p>Test body.</p></body>
+</html>'''
+
+        @append_random_comment
+        def test_view(request):
+            return HttpResponse(html)
+
+        request = RequestFactory().get('/')
+        response = test_view(request)
+        self.assertNotEqual(force_text(response.content), html)
+        self.assertIn('<!-- ', force_text(response.content))
+        self.assertIn(' -->', force_text(response.content))
+
+    def test_random_comment_exempt(self):
+        html = '''<html>
+    <head><title>Test</title></head>
+    <body><p>Test body.</p></body>
+</html>'''
+
+        @random_comment_exempt
+        def test_view(request):
+            return HttpResponse(html)
+
+        request = RequestFactory().get('/')
+        response = test_view(request)
+        self.assertTrue(response._random_comment_exempt)
+
+
+class TestContextProcessor(TestCase):
+
+    def test_csrf(self):
+        request = RequestFactory().get('/')
+        request.META['CSRF_COOKIE'] = 'abc123'
+        context = csrf(request)
+        self.assertTrue(force_text(context['csrf_token']))
+        self.assertNotEqual(force_text(context['csrf_token']), 'abc123')
+
+    def test_no_token_csrf(self):
+        request = RequestFactory().get('/')
+        context = csrf(request)
+        self.assertTrue(force_text(context['csrf_token']))
+        self.assertEqual(force_text(context['csrf_token']), 'NOTPROVIDED')
+
+
+@skipUnless(
+    'test_project' in os.environ.get('DJANGO_SETTINGS_MODULE', ''),
+    'Not running in test_project'
+)
+class IntegrationTests(TestCase):
+
+    def test_adds_comment(self):
+        resp = self.client.get(reverse('home'))
+        self.assertFalse(resp.content.endswith(b'</html>'))
+
+    def test_crypt_csrf_token(self):
+        resp = self.client.get(reverse('test_form'))
+        m = re.search(
+            r'value=\'(.*\$.*)\'',
+            force_text(resp.content),
+            re.MULTILINE | re.DOTALL
+        )
+        self.assertEqual(len(m.groups()), 1)
+        token = m.groups()[0].strip()
+        post_resp = self.client.post(
+            reverse('test_form'),
+            {'csrfmiddlewaretoken': token, 'message': 'Some rubbish'}
+        )
+        self.assertRedirects(post_resp, reverse('home'))
+
+    def test_crypt_csrf_header(self):
+        resp = self.client.get(reverse('test_form'))
+        m = re.search(
+            r'value=\'(.*\$.*)\'',
+            force_text(resp.content),
+            re.MULTILINE | re.DOTALL
+        )
+        self.assertEqual(len(m.groups()), 1)
+        token = m.groups()[0].strip()
+        post_resp = self.client.post(
+            reverse('test_form'),
+            {'message': 'Some rubbish'},
+            X_CSRFTOKEN=token,
+        )
+        self.assertRedirects(post_resp, reverse('home'))
+
+    def test_round_trip_loop(self):
+        '''
+        Checks a wide range of input tokens and keys
+        '''
+        for _ in range(1000):
+            request = RequestFactory().get('/')
+            csrf_token = get_random_string(32)
+            request.META['CSRF_COOKIE'] = csrf_token
+            token = force_text(csrf(request)['csrf_token'])
+            request = RequestFactory().post(
+                '/', {'csrfmiddlewaretoken': token})
+            middleware = CSRFCryptMiddleware()
+            middleware.process_view(request, test_view, (), {})
+            self.assertEqual(
+                force_text(request.POST.get('csrfmiddlewaretoken')),
+                force_text(csrf_token)
+            )
+
+    def test_round_trip_loop_header(self):
+        '''
+        Checks a wide range of input tokens and keys
+        '''
+        for _ in range(1000):
+            request = RequestFactory().get('/')
+            csrf_token = get_random_string(32)
+            request.META['CSRF_COOKIE'] = csrf_token
+            token = csrf(request)['csrf_token']
+            request = RequestFactory().post(
+                '/',
+                HTTP_X_CSRFTOKEN=force_text(token),
+                HTTP_X_REQUESTED_WITH='XMLHttpRequest'
+            )
+            middleware = CSRFCryptMiddleware()
+            middleware.process_view(request, test_view, (), {})
+            self.assertEqual(
+                force_text(request.META.get('HTTP_X_CSRFTOKEN')),
+                force_text(csrf_token)
+            )
