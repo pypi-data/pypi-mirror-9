5da7639acf944548df01d13e6c820f8ce10c4638
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:49 +0000

    Add package ufp/1.3.0/ufp-1.3.0.tar.gz

---
+++ packages/ufp/ufp-1.3.0.tar.gz/ufp-1.3.0/ufp/cipher.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+
+from __future__ import unicode_literals, absolute_import, division, print_function
+
+def unpadPkcs5(data):
+	"""
+	PKCS#5 표준 패딩을 제거한다.
+	
+	:param data: 입력 데이터
+	:type data: str, bytes
+	:returns: 패딩이 제거된 데이터
+	"""
+	return data[0:-ord(data[-1])]
+	
