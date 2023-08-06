7a26f7a63cf3bf5c21bbfa93725cb481701bc6cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:23 +0000

    Add package receptive/1.3/receptive-1.3.tar.gz

---
+++ packages/receptive/receptive-1.3.tar.gz/receptive-1.3/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='receptive',
+      version='1.3',
+      description='Receptive integration for Python',
+      url='https://github.com/Receptive/receptive-python',
+      author='Dan Dukeson',
+      author_email='dan@receptive.io',
+      license='MIT',
+      packages=['receptive'],
+      test_suite='nose.collector',
+      tests_require=['nose'],
+      zip_safe=False)
+
