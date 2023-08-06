9a8fdc3f1dd02d25b1bc78b8f7162044fb9af7e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:34 +0000

    Add package django-auth-abakus/0.2.1/django-auth-abakus-0.2.1.tar.gz

---
+++ packages/django-auth-abakus/django-auth-abakus-0.2.1.tar.gz/django-auth-abakus-0.2.1/setup.py
@@ -0,0 +1,37 @@
+import os
+from setuptools import setup, find_packages
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'tests.settings'
+
+setup(
+    name="django-auth-abakus",
+    version='0.2.1',
+    url='http://github.com/webkom/django-auth-abakus',
+    author='Webkom, Abakus Linjeforening',
+    author_email='webkom@abakus.no',
+    description='A django auth module that can be used to to authenticate '
+                'users against the API of abakus.no.',
+    packages=find_packages(exclude='tests'),
+    install_requires=[
+        'requests==2.4.3',
+    ],
+    tests_require=[
+        'django>=1.4',
+        'requests==2.4.3',
+        'responses'
+    ],
+    license='MIT',
+    test_suite='runtests.runtests',
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python",
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Framework :: Django",
+        "Environment :: Web Environment",
+        "Operating System :: OS Independent",
+        "Natural Language :: English",
+    ]
+)
