2dd136d263a886d6dfb7d4a566b6f05f74da9a95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:56 +0000

    Add package apikit/0.2/apikit-0.2.tar.gz

---
+++ packages/apikit/apikit-0.2.tar.gz/apikit-0.2/apikit/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+
+from apikit.pager import Pager
+from apikit.authz import Requirement
+from apikit.jsonify import jsonify
+
+__all__ = ['Pager', 'Requirement', 'jsonify']
