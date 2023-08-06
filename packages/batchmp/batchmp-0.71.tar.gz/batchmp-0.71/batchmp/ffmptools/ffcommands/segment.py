7d30c3eb8b9443a724a39efb7b49701b60acdf50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:57 +0000

    Add package batchmp/0.71/batchmp-0.71.tar.gz

---
+++ packages/batchmp/batchmp-0.71.tar.gz/batchmp-0.71/batchmp/ffmptools/ffcommands/segment.py
@@ -0,0 +1,206 @@
+# coding=utf8
+## Copyright (c) 2014 Arseniy Kuznetsov
+##
+## This program is free software; you can redistribute it and/or
+## modify it under the terms of the GNU General Public License
+## as published by the Free Software Foundation; either version 2
+## of the License, or (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+
+
+""" Batch splitting of media files
+"""
+import shutil, sys, os, math, fnmatch
+from batchmp.commons.utils import temp_dir
+from batchmp.ffmptools.ffrunner import FFMPRunner, FFMPRunnerTask, LogLevel
+from batchmp.commons.taskprocessor import TasksProcessor, TaskResult
+from batchmp.tags.handlers.ffmphandler import FFmpegTagHandler
+from batchmp.tags.handlers.mtghandler import MutagenTagHandler
+from batchmp.ffmptools.ffcommands.cmdopt import FFmpegCommands, FFmpegBitMaskOptions
+from batchmp.ffmptools.ffutils import FFH
+from batchmp.commons.utils import (
+    timed,
+    run_cmd,
+    CmdProcessingError,
+    MiscHelpers
+)
+
+class SegmenterTask(FFMPRunnerTask):
+    ''' Segment TasksProcessor task
+    '''
+    def __init__(self, fpath, target_dir, log_level,
+                            ff_general_options, ff_other_options, preserve_metadata,
+                            reset_timestamps, segment_size_MB, segment_length_secs):
+
+        super().__init__(fpath, target_dir, log_level,
+                                ff_general_options, ff_other_options, preserve_metadata)
+
+        if not self.ff_general_options:
+            self.ff_general_options = FFmpegBitMaskOptions.ff_general_options(
+                                  FFmpegBitMaskOptions.COPY_CODECS | FFmpegBitMaskOptions.MAP_ALL_STREAMS)
+
+            if not self.ff_other_options:
+                self.ff_other_options = self._ff_cmd_exclude_artwork_streams()
+
+        # calculate number of segments and, if needed, the segment length in secs
+        if segment_length_secs:
+            self.segment_length_secs = segment_length_secs
+            self.num_segments = math.ceil(Segmenter._media_duration(self.fpath) / segment_length_secs)
+        elif segment_size_MB:
+            num_segments = Segmenter._media_size_MB(self.fpath) / segment_size_MB
+            self.segment_length_secs = Segmenter._media_duration(self.fpath) / num_segments
+            self.num_segments = math.ceil(num_segments)
+        else:
+            # should not really get there, but just in case
+            raise ValueError('One of the command parameters needs to be specified: '\
+                                                    '<segment_size_MB | segment_length_secs>')
+        self.reset_timestamps = reset_timestamps
+
+    @property
+    def ff_cmd(self):
+        ''' Fragment command builder
+        '''
+        return ''.join((super().ff_cmd,
+                            FFmpegCommands.SEGMENT,
+                            ' {0} {1}'.format(FFmpegCommands.SEGMENT_TIME, self.segment_length_secs),
+                            FFmpegCommands.SEGMENT_RESET_TIMESTAMPS if self.reset_timestamps else ''))
+
+    def execute(self):
+        ''' builds and runs Segment FFmpeg command in a subprocess
+        '''
+        # store tags if needed
+        self._store_tags()
+
+        task_result = TaskResult()
+
+        with temp_dir() as tmp_dir:
+            # compile intermediary output path
+            fn_parts = os.path.splitext(os.path.basename(self.fpath))
+            fname_ext = fn_parts[1].strip().lower()
+            fpath_output = ''.join((fn_parts[0],
+                                   '_%{}d'.format(MiscHelpers.int_num_digits(self.num_segments)),
+                                   fname_ext))
+            fpath_output = os.path.join(tmp_dir, fpath_output)
+
+            # build ffmpeg cmd string
+            p_in = ''.join((self.ff_cmd, ' "{}"'.format(fpath_output)))
+            self._log(p_in, LogLevel.FFMPEG)
+
+            # run ffmpeg command as a subprocess
+            try:
+                _, task_elapsed = run_cmd(p_in)
+                task_result.add_task_step_duration(task_elapsed)
+            except CmdProcessingError as e:
+                task_result.add_task_step_info_msg('A problem while processing media file:\n\t{0}' \
+                                                                    '\nOriginal error message:\n\t{1}' \
+                                                                            .format(self.fpath, e.args[0]))
+            else:
+                # move split files to target directory
+                for segmented_fname in os.listdir(tmp_dir):
+                    if fnmatch.fnmatch(segmented_fname, '*{}'.format(fname_ext)):
+                        segmented_fpath = os.path.join(tmp_dir, segmented_fname)
+
+                        # restore tags if needed
+                        self._restore_tags(segmented_fpath)
+
+                        # move fragmented file to target dir
+                        shutil.move(segmented_fpath, self.target_dir)
+
+        task_result.add_report_msg(self.fpath)
+        return task_result
+
+
+class Segmenter(FFMPRunner):
+    @staticmethod
+    def _media_duration(fpath):
+        handler = MutagenTagHandler() + FFmpegTagHandler()
+        if handler.can_handle(fpath):
+            return handler.tag_holder.length
+        else:
+            return 0.0
+
+    @staticmethod
+    def _media_size_MB(fpath):
+        return os.path.getsize(fpath) / 1000**2
+
+    def segment(self, src_dir,
+                    end_level = sys.maxsize, include = None, exclude = None,
+                    filter_dirs = True, filter_files = True, quiet = False, serial_exec = False,
+                    segment_size_MB = 0.0, segment_length_secs = 0.0,
+                    target_dir = None, log_level = None,
+                    ff_general_options = None, ff_other_options = None,
+                    reset_timestamps = False, preserve_metadata = False):
+        ''' Segment media file by specified size | duration
+        '''
+        cpu_core_time, total_elapsed = self.run(src_dir,
+                                        end_level = end_level,
+                                        include = include, exclude = exclude, quiet = quiet,
+                                        filter_dirs = filter_dirs, filter_files = filter_files,
+                                        segment_size_MB = segment_size_MB,
+                                        segment_length_secs = segment_length_secs,
+                                        serial_exec = serial_exec,
+                                        target_dir = target_dir, log_level = log_level,
+                                        ff_general_options = ff_general_options,
+                                        ff_other_options = ff_other_options,
+                                        reset_timestamps = reset_timestamps,
+                                        preserve_metadata = preserve_metadata)
+        # print run report
+        if not quiet:
+            self.run_report(cpu_core_time, total_elapsed)
+
+    @timed
+    def run(self, src_dir,
+                end_level = sys.maxsize, include = None, exclude = None,
+                filter_dirs = True, filter_files = True, quiet = False, serial_exec = False,
+                segment_size_MB = 0.0, segment_length_secs = 0.0,
+                target_dir = None, log_level = None,
+                ff_general_options = None, ff_other_options = None,
+                reset_timestamps = False, preserve_metadata = False):
+
+        ''' Perform segmentation by size | duration
+        '''
+        cpu_core_time = 0.0
+
+        # validate input values
+        if not segment_size_MB and not segment_length_secs:
+            return cpu_core_time
+
+        if segment_length_secs:
+            # here need to determine media length
+            pass_filter = lambda fpath: self._media_duration(fpath) > segment_length_secs
+        elif segment_size_MB:
+            # simple media selection by size
+            pass_filter = lambda fpath: FFH.supported_media(fpath) and (self._media_size_MB(fpath) > segment_size_MB)
+
+
+        media_files, target_dirs = self._prepare_files(src_dir,
+                                        end_level = end_level,
+                                        include = include, exclude = exclude,
+                                        filter_dirs = filter_dirs, filter_files = filter_files,
+                                        target_dir = target_dir, target_dir_prefix = 'segmented',
+                                        pass_filter = pass_filter)
+
+        if len(media_files) > 0:
+            print('{0} media files to process'.format(len(media_files)))
+
+            # build tasks
+            tasks_params = ((media_file, target_dir_path, log_level,
+                                ff_general_options, ff_other_options, preserve_metadata,
+                                reset_timestamps, segment_size_MB, segment_length_secs)
+                                    for media_file, target_dir_path in zip(media_files, target_dirs))
+            tasks = []
+            for task_param in tasks_params:
+                task = SegmenterTask(*task_param)
+                tasks.append(task)
+
+            cpu_core_time = TasksProcessor().process_tasks(tasks, serial_exec = serial_exec, quiet = quiet)
+        else:
+            print('No media files to process')
+
+        return cpu_core_time
+
+
---
+++ packages/batchmp/batchmp-0.71.tar.gz/batchmp-0.71/batchmp/ffmptools/ffrunner.py
@@ -0,0 +1,164 @@
+# coding=utf8
+## Copyright (c) 2014 Arseniy Kuznetsov
+##
+## This program is free software; you can redistribute it and/or
+## modify it under the terms of the GNU General Public License
+## as published by the Free Software Foundation; either version 2
+## of the License, or (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+
+
+import os, sys, datetime, math
+from enum import IntEnum
+from abc import ABCMeta, abstractmethod
+from batchmp.commons.taskprocessor import Task
+from batchmp.ffmptools.ffutils import FFH, FFmpegNotInstalled
+from batchmp.tags.handlers.mtghandler import MutagenTagHandler
+from batchmp.tags.handlers.ffmphandler import FFmpegTagHandler
+from batchmp.tags.handlers.tagsholder import TagHolder
+from batchmp.fstools.fsutils import UniqueDirNamesChecker
+from batchmp.ffmptools.ffcommands.cmdopt import FFmpegCommands, FFmpegBitMaskOptions
+
+
+class FFMPRunnerTask(Task):
+    ''' Represents an abstract FFMP Runner task
+    '''
+    def __init__(self, fpath, target_dir, log_level,
+                        ff_general_options, ff_other_options, preserve_metadata):
+        self.fpath = fpath
+        self.target_dir = target_dir
+        self.log_level = log_level
+
+        self.ff_general_options = FFmpegBitMaskOptions.ff_general_options(ff_general_options)
+        self.ff_other_options = ff_other_options if ff_other_options else ''
+
+        self.tag_holder = TagHolder() if preserve_metadata else None
+
+    @property
+    def ff_cmd(self):
+        ''' Base FFmpeg command builder
+        '''
+        return ''.join(('ffmpeg',
+                            FFmpegCommands.LOG_LEVEL_ERROR,
+                            ' -i "{}"'.format(self.fpath),
+                            self.ff_general_options,
+                            self.ff_other_options))
+    # Helpers
+    def _store_tags(self):
+        if self.tag_holder:
+            handler = MutagenTagHandler() + FFmpegTagHandler()
+            if handler.can_handle(self.fpath):
+                self.tag_holder.copy_tags(handler.tag_holder)
+
+    def _restore_tags(self, fpath):
+        if self.tag_holder:
+            handler = MutagenTagHandler() + FFmpegTagHandler()
+            if handler.can_handle(fpath):
+                handler.tag_holder.copy_tags(self.tag_holder)
+                handler.save()
+
+    def _log(self, msg, type):
+        if self.log_level and self.log_level >= type:
+            # quick log
+            print(msg)
+
+    # FFmpeg command parts builders
+    def _ff_cmd_exclude_artwork_streams(self):
+        media_entry = FFH.media_file_info_full(self.fpath)
+        exclude_artworks_cmd = ''
+        if media_entry:
+            for artwork_stream in media_entry.artwork_streams:
+                idx = artwork_stream.get('index')
+                if idx is not None:
+                    exclude_artworks_cmd = '{0} {1}'.format(exclude_artworks_cmd,
+                                                            FFmpegCommands.exclude_input_stream(idx))
+        return exclude_artworks_cmd
+
+
+class LogLevel(IntEnum):
+    QUIET = 0
+    FFMPEG = 1
+    VERBOSE = 2
+
+
+class FFMPRunner(metaclass = ABCMeta):
+    ''' Base FFMPRunner
+    '''
+    def __init__(self):
+        if not FFH.ffmpeg_installed():
+            raise FFmpegNotInstalled
+
+    @abstractmethod
+    def run(self, *args, **kwargs):
+        pass
+
+    def run_report(self, cpu_core_time, total_elapsed):
+        ''' Info summary on executed FFMP commands
+        '''
+        ttd = datetime.timedelta(seconds = math.ceil(total_elapsed*100)/100)
+        ctd = datetime.timedelta(seconds = math.ceil(cpu_core_time*100)/100)
+
+        print('Total running time: {}'.format(str(ttd).rstrip('0')))
+        print('Cumulative FFmpeg CPU Cores time: {}'.format(str(ctd).rstrip('0')))
+
+
+    ## Internal helpers
+    @staticmethod
+    def _prepare_files(src_dir, *,
+                        end_level = sys.maxsize,
+                        include = None, exclude = None,
+                        filter_dirs = True, filter_files = True,
+                        target_dir = None, target_dir_prefix = None,
+                        pass_filter = None):
+        ''' Builds a list of matching media files to process,
+            along with their respective target out dirs
+        '''
+        media_files = [fpath for fpath in FFH.media_files(src_dir,
+                                        end_level = end_level,
+                                        include = include, exclude = exclude,
+                                        filter_dirs = filter_dirs, filter_files = filter_files,
+                                        pass_filter = pass_filter)]
+
+        target_dirs = FFMPRunner._setup_target_dirs(src_dir = src_dir,
+                                                target_dir = target_dir, target_dir_prefix = target_dir_prefix,
+                                                fpathes = media_files)
+
+        return media_files, target_dirs
+
+    @staticmethod
+    def _setup_target_dirs(src_dir, *,
+                            target_dir = None, target_dir_prefix = None,
+                            fpathes = None):
+        DEFAULT_TARGET_DIR_PREFIX = 'processed'
+        if target_dir_prefix is None:
+            target_dir_prefix = DEFAULT_TARGET_DIR_PREFIX
+        target_dir_name = '{0}_{1}'.format(os.path.basename(src_dir) if len(fpathes) > 1 else '',
+                                                target_dir_prefix)
+        target_dir_name = UniqueDirNamesChecker(src_dir).unique_name(target_dir_name)
+
+        if target_dir is None:
+            target_dir = os.path.join(os.path.dirname(src_dir), target_dir_name)
+        else:
+            target_dir = os.path.join(target_dir, target_dir_name)
+
+        # target dirs
+        target_dirs = []
+        for fpath in fpathes:
+            relpath = os.path.relpath(os.path.dirname(fpath), src_dir)
+            if relpath.startswith(os.pardir):
+                raise ValueError('File not in specified source directory or its subfolders')
+            elif relpath.endswith('{}'.format(os.path.curdir)):
+                relpath = relpath[:-1]
+
+            target_path = os.path.join(target_dir, relpath)
+            if not os.path.exists(target_path):
+                os.makedirs(target_path)
+            target_dirs.append(target_path)
+
+        return target_dirs
+
+
