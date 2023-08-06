fbf0651dc896f6974aa98c8da3ac7b2821aff9e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:50 +0000

    Add package wigle/0.0.2/wigle-0.0.2.tar.gz

---
+++ packages/wigle/wigle-0.0.2.tar.gz/wigle-0.0.2/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name="wigle",
+    version="0.0.2",
+    author="Stanisław Pitucha",
+    author_email="viraptor@gmail.com",
+    description="Interface to wigle website",
+    license="MIT",
+    install_requires=["requests"],
+    keywords="wigle wifi api search",
+    url='https://github.com/viraptor/wigle',
+    entry_points={'console_scripts': [
+        'wigle_user_info=wigle.cmd:user_info',
+        'wigle_search=wigle.cmd:search',
+        ]},
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: System :: Networking',
+        'Topic :: Utilities',
+        ]
+    )
