3eff0f9cc12b15bc13ed20485bb44d554cce73b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:19 +0000

    Add package django-oidc-provider/0.0.1/django-oidc-provider-0.0.1.tar.gz

---
+++ packages/django-oidc-provider/django-oidc-provider-0.0.1.tar.gz/django-oidc-provider-0.0.1/oidc_provider/lib/endpoints/token.py
@@ -0,0 +1,102 @@
+import urllib
+
+from django.http import JsonResponse
+
+from oidc_provider.lib.errors import *
+from oidc_provider.lib.utils.common import get_issuer
+from oidc_provider.lib.utils.params import *
+from oidc_provider.lib.utils.token import *
+from oidc_provider.models import *
+from oidc_provider import settings
+
+
+class TokenEndpoint(object):
+
+    def __init__(self, request):
+
+        self.request = request
+        self.params = Params()
+        self._extract_params()
+
+    def _extract_params(self):
+
+        query_dict = self.request.POST
+
+        self.params.client_id = query_dict.get('client_id', '')
+        self.params.client_secret = query_dict.get('client_secret', '')
+        self.params.redirect_uri = urllib.unquote(
+            query_dict.get('redirect_uri', ''))
+        self.params.grant_type = query_dict.get('grant_type', '')
+        self.params.code = query_dict.get('code', '')
+        self.params.state = query_dict.get('state', '')
+
+    def validate_params(self):
+        
+        if not (self.params.grant_type == 'authorization_code'):
+            raise TokenError('unsupported_grant_type')
+
+        try:
+            self.client = Client.objects.get(client_id=self.params.client_id)
+
+            if not (self.client.client_secret == self.params.client_secret):
+                raise TokenError('invalid_client')
+
+            if not (self.params.redirect_uri in self.client.redirect_uris):
+                raise TokenError('invalid_client')
+
+            self.code = Code.objects.get(code=self.params.code)
+
+            if not (self.code.client == self.client) and \
+               not self.code.has_expired():
+                raise TokenError('invalid_grant')
+
+        except Client.DoesNotExist:
+            raise TokenError('invalid_client')
+
+        except Code.DoesNotExist:
+            raise TokenError('invalid_grant')
+
+    def create_response_dic(self):
+
+        sub = settings.get('OIDC_IDTOKEN_SUB_GENERATOR')(
+            user=self.code.user)
+
+        id_token_dic = create_id_token(
+            iss=get_issuer(),
+            sub=sub,
+            aud=self.client.client_id,
+            auth_time=self.code.user.last_login)
+
+        token = create_token(
+            user=self.code.user,
+            client=self.code.client,
+            id_token_dic=id_token_dic,
+            scope=self.code.scope)
+
+        # Store the token.
+        token.save()
+
+        # We don't need to store the code anymore.
+        self.code.delete()
+
+        id_token = encode_id_token(id_token_dic, self.client.client_secret)
+
+        dic = {
+            'access_token': token.access_token,
+            'token_type': 'bearer',
+            'expires_in': settings.get('OIDC_TOKEN_EXPIRE'),
+            'id_token': id_token,
+        }
+
+        return dic
+
+    @classmethod
+    def response(cls, dic, status=200):
+        """
+        Create and return a response object.
+        """
+        response = JsonResponse(dic, status=status)
+        response['Cache-Control'] = 'no-store'
+        response['Pragma'] = 'no-cache'
+
+        return response
---
+++ packages/django-oidc-provider/django-oidc-provider-0.0.1.tar.gz/django-oidc-provider-0.0.1/oidc_provider/lib/endpoints/userinfo.py
@@ -0,0 +1,88 @@
+import re
+
+from django.http import HttpResponse
+from django.http import JsonResponse
+
+from oidc_provider.lib.errors import *
+from oidc_provider.lib.claims import *
+from oidc_provider.lib.utils.params import *
+from oidc_provider.models import *
+from oidc_provider import settings
+
+
+class UserInfoEndpoint(object):
+
+    def __init__(self, request):
+
+        self.request = request
+        self.params = Params()
+        self._extract_params()
+
+    def _extract_params(self):
+
+        # TODO: Maybe add other ways of passing access token
+        # http://tools.ietf.org/html/rfc6750#section-2
+        self.params.access_token = self._get_access_token()
+
+    def _get_access_token(self):
+        """
+        Get the access token using Authorization Request Header Field method.
+        See: http://tools.ietf.org/html/rfc6750#section-2.1
+
+        Return a string.
+        """
+        auth_header = self.request.META.get('HTTP_AUTHORIZATION', '')
+
+        if re.compile('^Bearer\s{1}.+$').match(auth_header):
+            access_token = auth_header.split()[1]
+        else:
+            access_token = ''
+
+        return access_token
+
+    def validate_params(self):
+        
+        try:
+            self.token = Token.objects.get(access_token=self.params.access_token)
+
+        except Token.DoesNotExist:
+            raise UserInfoError('invalid_token')
+
+    def create_response_dic(self):
+        """
+        Create a diccionary with all the requested claims about the End-User.
+        See: http://openid.net/specs/openid-connect-core-1_0.html#UserInfoResponse
+
+        Return a diccionary.
+        """
+        dic = {
+            'sub': self.token.id_token.get('sub'),
+        }
+
+        standard_claims = StandardScopeClaims(self.token.user, self.token.scope)
+
+        dic.update(standard_claims.create_response_dic())
+
+        extra_claims = settings.get('OIDC_EXTRA_SCOPE_CLAIMS')(
+            self.token.user, self.token.scope)
+
+        dic.update(extra_claims.create_response_dic())
+
+        return dic
+
+    @classmethod
+    def response(cls, dic):
+
+        response = JsonResponse(dic, status=200)
+        response['Cache-Control'] = 'no-store'
+        response['Pragma'] = 'no-cache'
+
+        return response
+
+    @classmethod
+    def error_response(cls, code, description, status):
+
+        response = HttpResponse(status=status)
+        response['WWW-Authenticate'] = 'error="{0}", error_description="{1}"'.format(code, description)
+
+        return response
