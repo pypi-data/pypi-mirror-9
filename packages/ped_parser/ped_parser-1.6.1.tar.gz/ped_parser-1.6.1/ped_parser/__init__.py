05258d2444b9350e5d26ddbd37fdfd54f638dc8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:04 +0000

    Add package ped_parser/1.6.1/ped_parser-1.6.1.tar.gz

---
+++ packages/ped_parser/ped_parser-1.6.1.tar.gz/ped_parser-1.6.1/ped_parser/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from __future__ import print_function, unicode_literals
+import logging
+from ped_parser.individual import Individual
+from ped_parser.family import Family
+from ped_parser.parser import FamilyParser
+from ped_parser.log import init_log
+
+logger = logging.getLogger(__name__)
+__version__ = '1.6.1'
