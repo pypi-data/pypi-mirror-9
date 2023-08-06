b96a497c858bdc6d4b534f8cef27ef8ea7684ba6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:01 +0000

    Add package dedupe-variable-address/0.0.3/dedupe-variable-address-0.0.3.tar.gz

---
+++ packages/dedupe-variable-address/dedupe-variable-address-0.0.3.tar.gz/dedupe-variable-address-0.0.3/setup.py
@@ -0,0 +1,15 @@
+try:
+    from setuptools import setup, Extension
+except ImportError :
+    raise ImportError("setuptools module required, please go to https://pypi.python.org/pypi/setuptools and follow the instructions for installing setuptools")
+
+
+setup(
+    name='dedupe-variable-address',
+    url='https://github.com/datamade/dedupe-variables-address',
+    version='0.0.3',
+    description='Address variable type for dedupe',
+    packages=['dedupe.variables'],
+    install_requires=['usaddress', 'dedupe', 'future'],
+    license='The MIT License: http://www.opensource.org/licenses/mit-license.php'
+    )
