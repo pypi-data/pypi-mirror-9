2ca1482e0539a50c931b3fdfce351d26efbe0d2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:55 +0000

    Add package rosinstall-generator/0.1.11/rosinstall_generator-0.1.11.tar.gz

---
+++ packages/rosinstall-generator/rosinstall_generator-0.1.11.tar.gz/rosinstall_generator-0.1.11/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+import os
+from setuptools import setup, find_packages
+
+exec(open(os.path.join(os.path.dirname(__file__), 'src', 'rosinstall_generator', '__init__.py')).read())
+
+setup(
+    name='rosinstall_generator',
+    version=__version__,
+    install_requires=['argparse', 'catkin_pkg >= 0.1.28', 'distribute', 'rosdistro >= 0.3.4', 'rospkg', 'PyYAML'],
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    scripts=['bin/rosinstall_generator'],
+    author='Dirk Thomas',
+    author_email='dthomas@osrfoundation.org',
+    maintainer='Dirk Thomas',
+    maintainer_email='dthomas@osrfoundation.org',
+    url='http://wiki.ros.org/rosinstall_generator',
+    download_url='http://download.ros.org/downloads/rosinstall_generator/',
+    keywords=['ROS'],
+    classifiers=['Programming Language :: Python',
+                 'License :: OSI Approved :: BSD License',
+                 'License :: OSI Approved :: MIT License'],
+    description="A tool to generator rosinstall files",
+    long_description="""A tool to generator rosinstall files""",
+    license='BSD'
+)
---
+++ packages/rosinstall-generator/rosinstall_generator-0.1.11.tar.gz/rosinstall_generator-0.1.11/src/rosinstall_generator/distro.py
@@ -0,0 +1,157 @@
+# Software License Agreement (BSD License)
+#
+# Copyright (c) 2013, Open Source Robotics Foundation, Inc.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+#  * Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+#  * Redistributions in binary form must reproduce the above
+#    copyright notice, this list of conditions and the following
+#    disclaimer in the documentation and/or other materials provided
+#    with the distribution.
+#  * Neither the name of Open Source Robotics Foundation, Inc. nor
+#    the names of its contributors may be used to endorse or promote
+#    products derived from this software without specific prior
+#    written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+# COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+import logging
+import os
+import sys
+
+from rosdistro import get_cached_distribution, get_index, get_index_url
+from rosdistro.dependency_walker import DependencyWalker
+from rosdistro.manifest_provider import get_release_tag
+
+
+def get_distro(distro_name):
+    index = get_index(get_index_url())
+    return get_cached_distribution(index, distro_name)
+
+
+def get_package_names(distro):
+    released_names = []
+    unreleased_names = []
+    for pkg_name, pkg in distro.release_packages.items():
+        repo = distro.repositories[pkg.repository_name].release_repository
+        if repo:
+            if repo.version is not None:
+                released_names.append(pkg_name)
+            else:
+                unreleased_names.append(pkg_name)
+    return released_names, unreleased_names
+
+
+# redirect all output to logger
+class CustomLogger(object):
+
+    def __init__(self):
+        self.logger = logging.getLogger('rosinstall_generator.wet')
+        self.linebuf = ''
+
+    def write(self, buf):
+        for line in buf.rstrip().splitlines():
+            self.logger.log(logging.DEBUG, line.rstrip())
+
+
+def get_recursive_dependencies(distro, package_names, excludes=None, limit_depth=None):
+    excludes = set(excludes or [])
+    dependencies = set([])
+    walker = DependencyWalker(distro)
+    # redirect all stderr output to logger
+    stderr = sys.stderr
+    sys.stderr = CustomLogger()
+    try:
+        for pkg_name in package_names:
+            try:
+                dependencies |= walker.get_recursive_depends(pkg_name, ['buildtool', 'build', 'run', 'test'], ros_packages_only=True, ignore_pkgs=dependencies | excludes, limit_depth=limit_depth)
+            except AssertionError as e:
+                raise RuntimeError("Failed to fetch recursive dependencies of package '%s': %s" % (pkg_name, e))
+    finally:
+        sys.stderr = stderr
+    dependencies -= set(package_names)
+    return dependencies
+
+
+def get_recursive_dependencies_on(distro, package_names, excludes=None, limit=None):
+    excludes = set(excludes or [])
+    limit = set(limit or [])
+
+    # to improve performance limit search space if possible
+    if limit:
+        released_names, _ = get_package_names(distro)
+        excludes.update(set(released_names) - limit - set(package_names))
+
+    dependencies = set([])
+    walker = DependencyWalker(distro)
+    # redirect all stderr output to logger
+    stderr = sys.stderr
+    sys.stderr = CustomLogger()
+    try:
+        for pkg_name in package_names:
+            dependencies |= walker.get_recursive_depends_on(pkg_name, ['buildtool', 'build', 'run', 'test'], ignore_pkgs=dependencies | excludes)
+    finally:
+        sys.stderr = stderr
+    dependencies -= set(package_names)
+    return dependencies
+
+
+def generate_rosinstall(distro, package_names, flat=False, tar=False):
+    rosinstall_data = []
+    for pkg_name in package_names:
+        rosinstall_data.extend(_generate_rosinstall_for_package(distro, pkg_name, flat=flat, tar=tar))
+    return rosinstall_data
+
+
+def _generate_rosinstall_for_package(distro, pkg_name, flat=False, tar=False):
+    pkg = distro.release_packages[pkg_name]
+    repo = distro.repositories[pkg.repository_name].release_repository
+    assert repo is not None and repo.version is not None, 'Package "%s" does not have a version"' % pkg_name
+
+    local_name = pkg_name
+    if not flat and repo.package_names != [pkg_name]:
+        local_name = '%s/%s' % (repo.name, local_name)
+    release_tag = get_release_tag(repo, pkg_name)
+    return _generate_rosinstall(local_name, repo.url, release_tag, tar=tar)
+
+
+def _generate_rosinstall(local_name, url, release_tag, tar=False):
+    if tar:
+        # the repository name might be different than repo.name coming from rosdistro
+        repo_name = os.path.basename(url[:-4])
+        suffix = '.git'
+        if not url.endswith(suffix):
+            raise RuntimeError("The repository '%s' must end with '%s': %s" % (repo_name, suffix, url))
+        url = url[:-len(suffix)] + '/archive/{0}.tar.gz'.format(release_tag)
+        data = [{
+            'tar': {
+                'local-name': local_name,
+                'uri': url,
+                'version': '{0}-{1}'.format(repo_name, release_tag.replace('/', '-'))
+            }
+        }]
+    else:
+        data = [{
+            'git': {
+                'local-name': local_name,
+                'uri': url,
+                'version': release_tag
+            }
+        }]
+    return data
