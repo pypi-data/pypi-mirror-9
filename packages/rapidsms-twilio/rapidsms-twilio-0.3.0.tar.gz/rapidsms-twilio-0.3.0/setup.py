4070c899d2d5a1f528ef6d832f46ec1c9d3eba8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:00 +0000

    Add package rapidsms-twilio/0.3.0/rapidsms-twilio-0.3.0.tar.gz

---
+++ packages/rapidsms-twilio/rapidsms-twilio-0.3.0.tar.gz/rapidsms-twilio-0.3.0/setup.py
@@ -0,0 +1,46 @@
+import os
+from setuptools import setup, find_packages
+
+
+def read_file(filename):
+    """Read a file into a string"""
+    path = os.path.abspath(os.path.dirname(__file__))
+    filepath = os.path.join(path, filename)
+    try:
+        return open(filepath).read()
+    except IOError:
+        return ''
+
+
+setup(
+    name='rapidsms-twilio',
+    version=__import__('rtwilio').__version__,
+    author='Caktus Consulting Group',
+    author_email='solutions@caktusgroup.com',
+    packages=find_packages(),
+    include_package_data=True,
+    exclude_package_data={
+        '': ['*.sql', '*.pyc'],
+    },
+    url='http://github.com/caktus/rapidsms-twilio/',
+    license='BSD',
+    description=' '.join(__import__('rtwilio').__doc__.splitlines()).strip(),
+    classifiers=[
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Framework :: Django',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Development Status :: 4 - Beta',
+        'Operating System :: OS Independent',
+    ],
+    long_description=read_file('README.rst'),
+    install_requires=(
+        'rapidsms>=0.13.0',
+        'django>=1.4',
+        'twilio>=3.5,<4.0',
+    ),
+    test_suite="runtests.runtests",
+)
