bcc915e6a8c9615c024a1e236c5d3d78313655f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:41 +0000

    Add package threat-intel/0.0.6/threat_intel-0.0.6.tar.gz

---
+++ packages/threat-intel/threat_intel-0.0.6.tar.gz/threat_intel-0.0.6/setup.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+from setuptools import find_packages
+
+
+setup(
+    name="threat_intel",
+    version='0.0.6',
+    provides=['threat_intel'],
+    author="Yelp Security",
+    url='https://github.com/Yelp/threat_intel',
+    setup_requires='setuptools',
+    license='Copyright 2015 Yelp',
+    author_email="opensource@yelp.com",
+    description="Collection of the API calls for various threat intel feeds.",
+    packages=find_packages(),
+    install_requires=[
+        "grequests==0.2.0",
+        "simplejson==3.6.5",
+    ],
+)
