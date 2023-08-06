373ebb1ce7668ae2379388b2a79619c3672465b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:00 +0000

    Add package logstash_formatter/0.5.11/logstash_formatter-0.5.11.tar.gz

---
+++ packages/logstash_formatter/logstash_formatter-0.5.11.tar.gz/logstash_formatter-0.5.11/setup.py
@@ -0,0 +1,27 @@
+import codecs
+from setuptools import setup, find_packages
+from os import path
+
+def read(*parts):
+    return codecs.open(path.join(path.dirname(__file__), *parts),
+                       encoding="utf-8").read()
+
+setup(name='logstash_formatter',
+      version='0.5.11',
+      description='JSON formatter meant for logstash',
+      long_description=read('README.rst'),
+      url='https://github.com/exoscale/python-logstash-formatter',
+      author='Pierre-Yves Ritschard',
+      author_email='pierre-yves.ritschard@exoscale.ch',
+      license='MIT, see LICENSE file',
+      packages=find_packages(),
+      include_package_data=True,
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: MIT License',
+          'Natural Language :: English',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 3'
+      ],
+      zip_safe=False)
