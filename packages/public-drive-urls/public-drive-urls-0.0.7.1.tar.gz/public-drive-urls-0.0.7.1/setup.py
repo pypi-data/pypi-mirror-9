9032239aca5ee7661137932fe0c98a308f0ca8fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:13 +0000

    Add package public-drive-urls/0.0.7.1/public-drive-urls-0.0.7.1.tar.gz

---
+++ packages/public-drive-urls/public-drive-urls-0.0.7.1.tar.gz/public-drive-urls-0.0.7.1/setup.py
@@ -0,0 +1,15 @@
+
+from setuptools import setup
+
+setup(
+    name="public-drive-urls",
+    version='0.0.7.1',
+    author="Brian Peterson",
+    author_email="bepetersn@gmail.com",
+    description="Find Google Drive download URLs from a file's sharing URL",
+    license="MIT",
+    url='https://github.com/bepetersn/public-drive-urls/',
+    py_modules=['public_drive_urls'],
+    classifiers=[
+    ],
+)
