3c0d4e58683500aa6264797c7b917dcc9f41255a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:14 +0000

    Add package djes/0.0.1a2/djes-0.0.1a2.tar.gz

---
+++ packages/djes/djes-0.0.1a2.tar.gz/djes-0.0.1a2/djes/conf/defaults.py
@@ -0,0 +1,9 @@
+from django.conf import settings as _settings
+
+ES_CONNECTIONS = {
+    "default": {
+        "hosts": "localhost"
+    }
+}
+ES_INDEX = _settings.DATABASES["default"]["NAME"]
+ES_INDEX_SETTINGS = {}
