28527488fdb77a082978095331a2da69fe5188bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:23 +0000

    Add package dogstatsd-python/0.5.3/dogstatsd-python-0.5.3.tar.gz

---
+++ packages/dogstatsd-python/dogstatsd-python-0.5.3.tar.gz/dogstatsd-python-0.5.3/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+import sys
+
+setup(
+    name = "dogstatsd-python",
+    version = "0.5.3",
+    author = "Datadog, Inc.",
+    author_email = "packages@datadoghq.com",
+    description = "Python bindings to Datadog's API and a user-facing command line tool.",
+    py_modules=['statsd'],
+    license = "BSD",
+    keywords = "datadog data statsd metrics",
+    url = "http://www.datadoghq.com"
+)
