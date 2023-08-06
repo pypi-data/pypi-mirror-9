e76ac9d310a7d98de20a50620b6ee8a693946925
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:17 +0000

    Add package todoist-python/0.2.4/todoist-python-0.2.4.tar.gz

---
+++ packages/todoist-python/todoist-python-0.2.4.tar.gz/todoist-python-0.2.4/todoist/managers/user.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from .generic import Manager
+
+
+class UserManager(Manager):
+
+    def update(self, **kwargs):
+        """
+        Updates the user data, and appends the equivalent request to the queue.
+        """
+        self.state['User'].update(kwargs)
+        item = {
+            'type': 'user_update',
+            'timestamp': self.api.generate_timestamp(),
+            'args': kwargs,
+        }
+        self.queue.append(item)
+
+    def sync(self):
+        return self.api.sync(resource_types=['user'])
+
+    def get(self):
+        return self.state['User']
+
+    def get_id(self):
+        return self.state['UserId']
