68b4a3f3c77147c865d8bb7bd5e5d5a3813aca6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:32 +0000

    Add package robotframework-jalali/0.0.0/robotframework-jalali-0.0.0.zip

---
+++ packages/robotframework-jalali/robotframework-jalali-0.0.0.zip/robotframework-jalali-0.0.0/setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+short_description = 'Robot Framework Jalali Date'
+try:
+    description = open('README.rst').read()
+except IOError:
+    description = short_description
+
+
+classifiers = """
+Development Status :: 5 - Production/Stable
+License :: OSI Approved :: MIT License
+Operating System :: OS Independent
+Programming Language :: Python :: 2.7
+Topic :: Software Development :: Testing
+Topic :: Software Development :: Quality Assurance
+""".strip().splitlines()
+
+setup(
+    name='robotframework-jalali',
+    package_dir={'': 'robotframework-jalali'},
+    packages=['jalaliLibrary'],  # this must be the same as the name above
+    version='0.0.0',
+    description=short_description,
+    author='samira esnaashari',
+    author_email='samir.esnaashari@gmail.com',
+    url='https://github.com/samira-esnaashari/robotframework-jalali',
+    download_url='https://pypi.python.org/pypi/robotframework-jalali',
+    keywords=('robotframework testing '
+              'test automation testautomation '
+              'atdd bdd jalali'),  # arbitrary keywords
+    install_requires=[],
+    long_description=description,
+    license='MIT',
+    classifiers=classifiers,
+    platforms='any'
+)
