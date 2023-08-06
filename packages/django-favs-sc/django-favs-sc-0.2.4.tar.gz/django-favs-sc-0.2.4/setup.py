d2cf5364149eb59500900506f56b5cd837bd8759
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:08 +0000

    Add package django-favs-sc/0.2.4/django-favs-sc-0.2.4.tar.gz

---
+++ packages/django-favs-sc/django-favs-sc-0.2.4.tar.gz/django-favs-sc-0.2.4/setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+
+README = open('README.md').readlines()
+
+setup(
+    name='django-favs-sc',
+    version='0.2.4',
+    packages=find_packages(),
+    include_package_data=True,
+    license='MIT License',
+    description=README[2].rstrip('\n'),
+    long_description=''.join(README),
+    url='https://github.com/SpencerCooley/django-favit',
+    author='Spencer Cooley',
+    author_email='contact@spencercooley.com',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        ],
+    install_requires=['simplejson']
+    )
