6ed50f9b55c023ba40762d2aadc019e556747261
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:09:59 +0000

    Add package pip-tools/0.3.6/pip-tools-0.3.6.tar.gz

---
+++ packages/pip-tools/pip-tools-0.3.6.tar.gz/pip-tools-0.3.6/setup.py
@@ -0,0 +1,56 @@
+"""
+pip-tools keeps your pinned dependencies fresh.
+"""
+import sys
+from setuptools import setup
+
+
+def get_dependencies():
+    deps = []
+    if sys.version_info < (2, 7):
+        deps += ['argparse']
+    return deps
+
+
+setup(
+    name='pip-tools',
+    version='0.3.6',
+    url='https://github.com/nvie/pip-tools/',
+    license='BSD',
+    author='Vincent Driessen',
+    author_email='vincent@3rdcloud.com',
+    description=__doc__.strip('\n'),
+    #packages=[],
+    scripts=['bin/pip-review', 'bin/pip-dump'],
+    #include_package_data=True,
+    zip_safe=False,
+    platforms='any',
+    install_requires=get_dependencies(),
+    classifiers=[
+        # As from https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        #'Development Status :: 1 - Planning',
+        #'Development Status :: 2 - Pre-Alpha',
+        #'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
+        #'Development Status :: 5 - Production/Stable',
+        #'Development Status :: 6 - Mature',
+        #'Development Status :: 7 - Inactive',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        #'Programming Language :: Python :: 2.3',
+        #'Programming Language :: Python :: 2.4',
+        #'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        #'Programming Language :: Python :: 3',
+        #'Programming Language :: Python :: 3.0',
+        #'Programming Language :: Python :: 3.1',
+        #'Programming Language :: Python :: 3.2',
+        #'Programming Language :: Python :: 3.3',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Topic :: System :: Systems Administration',
+    ]
+)
