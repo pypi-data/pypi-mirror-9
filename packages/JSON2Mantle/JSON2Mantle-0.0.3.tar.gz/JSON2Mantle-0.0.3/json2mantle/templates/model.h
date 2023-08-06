17dcb87eb3a26b60011342ce83c322dcc668fc24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:36 +0000

    Add package JSON2Mantle/0.0.3/JSON2Mantle-0.0.3.tar.gz

---
+++ packages/JSON2Mantle/JSON2Mantle-0.0.3.tar.gz/JSON2Mantle-0.0.3/json2mantle/templates/model.h
@@ -0,0 +1,15 @@
+//
+//  {{file_name}}.h
+//
+//  Created by JSON2Mantle on {{created_at}}.
+//  Copyright (c) {{year}} {{author}}. All rights reserved.
+//
+
+#import <Mantle.h>
+{{headers}}
+
+@interface {{file_name}} : MTLModel <MTLJSONSerializing>
+
+{{properties}}
+
+@end
