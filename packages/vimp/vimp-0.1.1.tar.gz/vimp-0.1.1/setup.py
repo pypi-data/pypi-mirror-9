26b23ec5a01ed4c969eaead422739d5efe484570
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:05 +0000

    Add package vimp/0.1.1/vimp-0.1.1.tar.gz

---
+++ packages/vimp/vimp-0.1.1.tar.gz/vimp-0.1.1/setup.py
@@ -0,0 +1,22 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name="vimp",
+    packages=["vimp"],
+    version="0.1.1",
+    author="Christian Stigen Larsen",
+    author_email="csl@csl.name",
+    keywords=["vim"],
+    scripts=["bin/vimp"],
+    url="https://github.com/cslarsen/vimp",
+    download_url="https://github.com/cslarsen/vimp/tarball/0.1.1",
+    license="https://www.gnu.org/licenses/lgpl-2.1.html",
+    description="Command-line package manager or vim.",
+    long_description=open("README.rst").read(),
+    zip_safe=True,
+    test_suite="vimp.test",
+    platforms=["unix"],
+)
