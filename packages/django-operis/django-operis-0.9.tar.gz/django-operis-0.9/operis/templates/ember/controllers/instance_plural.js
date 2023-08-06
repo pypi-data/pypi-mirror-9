f1c75b7c585733a65f037c063ce260aaf297aeed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:23:08 +0000

    Add package django-operis/0.9/django-operis-0.9.tar.gz

---
+++ packages/django-operis/django-operis-0.9.tar.gz/django-operis-0.9/operis/templates/ember/controllers/instance_plural.js
@@ -0,0 +1,5 @@
+import Operis{{ model.plural }}Controller from '{{ember_app_name}}/controllers/operis/operis-{{ model.plural_converted }}';
+
+var {{ model.plural }}Controller = Operis{{ model.plural }}Controller.extend({});
+
+export default {{ model.plural }}Controller;  
