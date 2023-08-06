5370046f4ce378c1bb9d4d0c3c5cea8e3dfe8417
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:36:03 +0000

    Add package funniest3D/0.1/funniest3D-0.1.tar.gz

---
+++ packages/funniest3D/funniest3D-0.1.tar.gz/funniest3D-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='funniest3D',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='http://github.com/storborg/funniest3D',
+      author='Flying Circus',
+      author_email='pakpoomton@gmail.com',
+      license='MIT',
+      packages=['funniest3D'],
+      zip_safe=False)
