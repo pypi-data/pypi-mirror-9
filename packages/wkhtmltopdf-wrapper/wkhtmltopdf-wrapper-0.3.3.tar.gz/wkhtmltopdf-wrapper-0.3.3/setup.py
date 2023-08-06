f7a0d377e05bd5294a52f35bcf9b80717c472fac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:52 +0000

    Add package wkhtmltopdf-wrapper/0.3.3/wkhtmltopdf-wrapper-0.3.3.tar.gz

---
+++ packages/wkhtmltopdf-wrapper/wkhtmltopdf-wrapper-0.3.3.tar.gz/wkhtmltopdf-wrapper-0.3.3/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='wkhtmltopdf-wrapper',
+    version='0.3.3',
+    description='A Simpler python wrapper for wkhtmltopdf, inspired by Qoda\'s python-wkhtmltopdf',
+    long_description="%s\n\n%s" % (open('README.rst', 'r').read(), open('AUTHORS.rst', 'r').read()),
+    author='aGuegu',
+    author_email='weihong.guan@gmail.com',
+    license='BSD',
+    url='http://github.com/aguegu/wkhtmltopdf-wrapper',
+    packages=find_packages(),
+    dependency_links=[],
+    install_requires=[],
+    include_package_data=True,
+    keywords = "wkhtmltopdf pdf",
+    classifiers=[
+        "Programming Language :: Python",
+        "License :: OSI Approved :: BSD License",
+        "Development Status :: 4 - Beta",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Developers",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+    ],
+)
