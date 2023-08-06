e7f16fb6d061063b306b5ae1687d61d74d15787f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:29:57 +0000

    Add package Flask-Heroku-Cacheify/1.4/Flask-Heroku-Cacheify-1.4.tar.gz

---
+++ packages/Flask-Heroku-Cacheify/Flask-Heroku-Cacheify-1.4.tar.gz/Flask-Heroku-Cacheify-1.4/flask_cacheify.py
@@ -0,0 +1,54 @@
+from os import environ
+
+from flask.ext.cache import Cache
+
+
+def init_cacheify(app, config=None):
+    """Auto-configure an appropriate cache for your Flask application.
+
+    :param app app: Your Flask application.
+    :param dict config: Your Flask configuration dictionary.
+    """
+    if not (config is None or isinstance(config, dict)):
+        raise ValueError('ERROR: config must be an instance of dict or '
+                'NoneType.')
+
+    config = config if config else app.config
+    config.setdefault('CACHE_TYPE', 'simple')
+
+    # Look for MemCachier.
+    if any([k.startswith('MEMCACHIER_') for k in environ]):
+        config['CACHE_TYPE'] = 'saslmemcached'
+        config.setdefault('CACHE_MEMCACHED_SERVERS',
+                [environ.get('MEMCACHIER_SERVERS')])
+        config.setdefault('CACHE_MEMCACHED_USERNAME',
+                environ.get('MEMCACHIER_USERNAME'))
+        config.setdefault('CACHE_MEMCACHED_PASSWORD',
+                environ.get('MEMCACHIER_PASSWORD'))
+
+    # Look for RedisGreen.
+    elif any([k.startswith('REDISGREEN_') for k in environ]):
+        config['CACHE_TYPE'] = 'redis'
+        config.setdefault('CACHE_REDIS_URL', environ.get('REDISGREEN_URL'))
+
+    # Look for MyRedis.
+    elif any([k.startswith('MYREDIS_') for k in environ]):
+        config['CACHE_TYPE'] = 'redis'
+        config.setdefault('CACHE_REDIS_URL', environ.get('MYREDIS_URL'))
+
+    # Look for Redis Cloud.
+    elif any([k.startswith('REDISCLOUD_') for k in environ]):
+        config['CACHE_TYPE'] = 'redis'
+        config.setdefault('CACHE_REDIS_URL', environ.get('REDISCLOUD_URL'))
+
+    # Look for Redis To Go.
+    elif any([k.startswith('REDISTOGO_') for k in environ]):
+        config['CACHE_TYPE'] = 'redis'
+        config.setdefault('CACHE_REDIS_URL', environ.get('REDISTOGO_URL'))
+
+    # Look for openredis.
+    elif any([k.startswith('OPENREDIS_') for k in environ]):
+        config['CACHE_TYPE'] = 'redis'
+        config.setdefault('CACHE_REDIS_URL', environ.get('OPENREDIS_URL'))
+
+    return Cache(app, config)
---
+++ packages/Flask-Heroku-Cacheify/Flask-Heroku-Cacheify-1.4.tar.gz/Flask-Heroku-Cacheify-1.4/setup.py
@@ -0,0 +1,34 @@
+from os.path import abspath, dirname, join, normpath
+
+from setuptools import setup
+
+
+setup(
+
+    # Basic package information:
+    name = 'Flask-Heroku-Cacheify',
+    version = '1.4',
+    py_modules = ('flask_cacheify', ),
+
+    # Packaging options:
+    zip_safe = False,
+    include_package_data = True,
+
+    # Package dependencies:
+    install_requires = [
+        'Flask-Cache>=0.11.1',
+        'pylibmc>=1.2.3',
+        'redis>=2.7.2'
+    ],
+
+    # Metadata for PyPI:
+    author = 'Randall Degges',
+    author_email = 'rdegges@gmail.com',
+    license = 'UNLICENSE',
+    url = 'https://github.com/rdegges/flask-heroku-cacheify',
+    keywords = 'flask heroku cloud cache memcache memcached redis awesome',
+    description = 'Automatic Flask cache configuration on Heroku.',
+    long_description = open(normpath(join(dirname(abspath(__file__)),
+        'README.md'))).read()
+
+)
