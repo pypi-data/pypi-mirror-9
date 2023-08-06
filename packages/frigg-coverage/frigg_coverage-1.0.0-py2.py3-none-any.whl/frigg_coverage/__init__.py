930b82afbb6520109a7bce9135a488bf63b27970
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:56 +0000

    Add package frigg-coverage/1.0.0/frigg_coverage-1.0.0-py2.py3-none-any.whl

---
+++ packages/frigg-coverage/frigg_coverage-1.0.0-py2.py3-none-any.whl/frigg_coverage/__init__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from .parsers import PARSERS
+
+
+def parse_coverage(coverage_report, parser):
+    """
+    :param coverage_report: A string with the contents of a coverage file
+    :type coverage_report: String
+    :param parser: A string with name of the parser to use
+    :type parser: String
+    :return: Total coverage
+    """
+    if parser in PARSERS:
+        if coverage_report:
+            return PARSERS[parser].parse_coverage_report(coverage_report)
+        return None
+    return NotImplemented
