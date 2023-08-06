66c76ebb51ce520a9a51405b3d0468d15623a93f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:36:14 +0000

    Add package iampharaoh/0.0.2/iampharaoh-0.0.2.tar.gz

---
+++ packages/iampharaoh/iampharaoh-0.0.2.tar.gz/iampharaoh-0.0.2/setup.py
@@ -0,0 +1,35 @@
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.rst')).read()
+
+requires = [
+    'pyramid',
+    ]
+
+setup(name='iampharaoh',
+      version='0.0.2',
+      description='A Pyramid Scaffold to Pony users (django) by Mario Idival',
+      long_description=README,
+      classifiers=[
+        "Programming Language :: Python",
+        "Framework :: Pyramid",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+        ],
+      author='Mario Idival',
+      author_email='marioidival@gmail.com',
+      url="https://github.com/marioidival/iampharaoh",
+      keywords='pyramid scaffold',
+      install_requires=requires,
+      entry_points="""\
+          [pyramid.scaffold]
+          pharaohproject = iampharaoh:ProjectTemplate
+          pharaohapps = iampharaoh:AppsTemplate
+      """,
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      )
