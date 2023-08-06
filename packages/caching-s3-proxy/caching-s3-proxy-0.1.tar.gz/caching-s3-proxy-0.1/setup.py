19e97e2eee34e9cf63a3313fd48cff7bbc15978a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:50 +0000

    Add package caching-s3-proxy/0.1/caching-s3-proxy-0.1.tar.gz

---
+++ packages/caching-s3-proxy/caching-s3-proxy-0.1.tar.gz/caching-s3-proxy-0.1/setup.py
@@ -0,0 +1,33 @@
+import codecs
+import glob
+import os
+from setuptools import setup
+
+
+setup(
+    name='caching-s3-proxy',
+    version='0.1',
+    description=('provides an unauthenticated plain HTTP frontend for'
+                 ' public and private S3 buckets, and caches on the'
+                 ' filesystem using an LRU cache'),
+    author='Rob Helmer',
+    author_email='rhelmer@rhelmer.org',
+    license='MPL',
+    url='https://github.com/rhelmer/caching-s3-proxy',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: Mozilla Public License 1.1 (MPL 1.1)',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet :: WWW/HTTP :: WSGI :: Application',
+        ],
+    keywords=['caching', 'lru', 's3', 'proxy', 'unauthenticated'],
+    install_requires=['boto>=2.36.0', 'wsgiref>=0.1.2'],
+    entry_points={
+        'console_scripts': [
+            'caching-s3-proxy = proxy.run:main'
+            ],
+        },
+    test_suite='nose.collector',
+    zip_safe=False,
+),
