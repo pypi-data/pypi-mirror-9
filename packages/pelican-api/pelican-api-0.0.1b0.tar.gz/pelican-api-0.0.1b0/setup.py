35f283d9bfb5f959a28831c54bb1103d9186cb0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:14 +0000

    Add package pelican-api/0.0.1b0/pelican-api-0.0.1b0.tar.gz

---
+++ packages/pelican-api/pelican-api-0.0.1b0.tar.gz/pelican-api-0.0.1b0/setup.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import sys
+from setuptools import setup, find_packages
+
+
+_ver = sys.version_info
+is_py2 = (_ver[0] == 2)
+is_py3 = (_ver[0] == 3)
+
+dependencies = []
+
+setup(
+    name='pelican-api',
+    description='A static API server running from Pelican.',
+    long_description='A static API server running from Pelican.',
+    version='0.0.1-beta',
+    author='Open Knowledge Foundation',
+    author_email='info@okfn.org',
+    url='http://okfn.org',
+    license='MIT',
+    packages=find_packages(exclude=['tests']),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=dependencies,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+)
