a26a73c2538262452cb5c7ebc65f9704958d7cc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:25 +0000

    Add package django-multipleformwizard/0.2.1/django-multipleformwizard-0.2.1.tar.gz

---
+++ packages/django-multipleformwizard/django-multipleformwizard-0.2.1.tar.gz/django-multipleformwizard-0.2.1/multipleformwizard/__init__.py
@@ -0,0 +1,4 @@
+__version__ = '0.2.1'
+
+from .views import (SessionMultipleFormWizardView, CookieMultipleFormWizardView, NamedUrlSessionMultipleFormWizardView,
+                    NamedUrlCookieMultipleFormWizardView, MultipleFormWizardView, NamedUrlMultipleFormWizardView)
