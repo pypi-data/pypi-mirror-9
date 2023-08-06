e55e966640dae35c97bdebeb727c1130200a8357
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:29:43 +0000

    Add package django-translate/1.0.2/django-translate-1.0.2.tar.gz

---
+++ packages/django-translate/django-translate-1.0.2.tar.gz/django-translate-1.0.2/setup.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+setup(
+    name="django-translate",
+    version="1.0.2",
+    author="Adam Zieliński",
+    author_email="adam@sf2.guru",
+    packages=find_packages(),
+    include_package_data=True,
+    url="https://github.com/adamziel/django_translate",
+
+    license="MIT",
+    description="Non-gettext translations for django.",
+
+    # Dependent packages (distributions)
+    install_requires=[
+        "python_translate"
+    ],
+)
