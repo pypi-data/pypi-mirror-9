88202c3bc187c99a80fe5dd48e38e83f527b7968
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:19 +0000

    Add package xbob.db.replay/1.0.5/xbob.db.replay-1.0.5.tar.gz

---
+++ packages/xbob.db.replay/xbob.db.replay-1.0.5.tar.gz/xbob.db.replay-1.0.5/setup.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+# vim: set fileencoding=utf-8 :
+# Andre Anjos <andre.anjos@idiap.ch>
+# Sex 10 Ago 2012 14:22:33 CEST
+
+from setuptools import setup, find_packages
+
+# The only thing we do in this file is to call the setup() function with all
+# parameters that define our package.
+setup(
+
+    name='xbob.db.replay',
+    version='1.0.5',
+    description='Replay Attack Database Access API for Bob',
+    url='http://pypi.python.org/pypi/xbob.db.replay',
+    license='GPLv3',
+    author='Andre Anjos',
+    author_email='andre.anjos@idiap.ch',
+    long_description=open('README.rst').read(),
+
+    # This line is required for any distutils based packaging.
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+
+    install_requires=[
+      'setuptools',
+      'six',
+      'bob >= 1.1.0',
+      'antispoofing.utils < 2.0.0',
+    ],
+
+    namespace_packages = [
+      'xbob',
+      'xbob.db',
+      ],
+
+    entry_points = {
+      # bob database declaration
+      'bob.db': [
+        'replay = xbob.db.replay.driver:Interface',
+        ],
+
+      # bob unittest declaration
+      'bob.test': [
+        'replay = xbob.db.replay.test:ReplayDatabaseTest',
+        ],
+
+      # antispoofing database declaration
+      'antispoofing.utils.db': [
+        'replay = xbob.db.replay.spoofing:Database',
+        ],
+      },
+
+    classifiers = [
+      'Development Status :: 5 - Production/Stable',
+      'Intended Audience :: Science/Research',
+      'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+      'Natural Language :: English',
+      'Programming Language :: Python',
+      'Programming Language :: Python :: 3',
+      'Topic :: Scientific/Engineering :: Artificial Intelligence',
+      'Topic :: Database :: Front-Ends',
+      ],
+)
---
+++ packages/xbob.db.replay/xbob.db.replay-1.0.5.tar.gz/xbob.db.replay-1.0.5/xbob/db/replay/__init__.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# vim: set fileencoding=utf-8 :
+# Andre Anjos <andre.dos.anjos@gmail.com>
+# Wed 18 May 09:28:44 2011
+
+"""The Replay-Attack Database for face spoofing consists of 1300 video clips of
+photo and video attack attempts to 50 clients, under different lighting
+conditions. This Database was produced at the Idiap Research Institute, in
+Switzerland.
+
+If you use this database in your publication, please cite the following paper
+on your references:
+
+.. code-block:: sh
+
+  @INPROCEEDINGS{Chingovska_BIOSIG-2012,
+    author = {Chingovska, Ivana and Anjos, Andr\\'e and Marcel, S\\'ebastien},
+    keywords = {biometric, Counter-Measures, Local Binary Patterns, Spoofing Attacks},
+    month = september,
+    title = {On the Effectiveness of Local Binary Patterns in Face Anti-spoofing},
+    journal = {IEEE BIOSIG 2012},
+    year = {2012},
+  }
+
+"""
+
+from .query import Database
+from .models import Client, File, Protocol, RealAccess, Attack
+from . import spoofing
+
+__all__ = dir()
