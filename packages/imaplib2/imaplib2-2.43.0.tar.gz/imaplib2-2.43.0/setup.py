7acaab37a8a1f22b6ca3094559a1f412a60cfd41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:12 +0000

    Add package imaplib2/2.43.0/imaplib2-2.43.0.tar.gz

---
+++ packages/imaplib2/imaplib2-2.43.0.tar.gz/imaplib2-2.43.0/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# from distutils.core import setup
+from setuptools import setup, find_packages
+
+setup(
+    name="imaplib2",
+    version="2.43.0",
+    description="A threaded Python IMAP4 client.",
+    author="Piers Lauder",
+    url="http://github.com/bcoe/imaplib2",
+    classifiers=[
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3"
+    ],
+    packages=find_packages(),
+)
