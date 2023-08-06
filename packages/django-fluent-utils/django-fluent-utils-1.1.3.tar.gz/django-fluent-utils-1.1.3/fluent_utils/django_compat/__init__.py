506cae49b9013182bebb99d56d2f1fde95a915df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:09 +0000

    Add package django-fluent-utils/1.1.3/django-fluent-utils-1.1.3.tar.gz

---
+++ packages/django-fluent-utils/django-fluent-utils-1.1.3.tar.gz/django-fluent-utils-1.1.3/fluent_utils/django_compat/__init__.py
@@ -0,0 +1,16 @@
+from .django14 import now, utc, patterns, url, include
+from .django15 import get_user_model, AUTH_USER_MODEL
+from .django16 import transaction_atomic, add_preserved_filters
+from .django17 import is_installed, get_models, get_app_names, get_app_label
+from .django18 import CallableChoiceIterator
+from .python3 import smart_text
+
+__all__ = (
+    'now', 'utc',
+    'patterns', 'url', 'include',
+    'get_user_model', 'AUTH_USER_MODEL',
+    'transaction_atomic',
+    'add_preserved_filters',
+    'is_installed', 'get_models', 'get_app_names', 'get_app_label',
+    'smart_text',
+)
