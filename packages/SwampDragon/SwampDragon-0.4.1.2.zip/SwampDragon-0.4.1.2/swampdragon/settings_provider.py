ccb54129ee052eef36cff8d5a115551ffbce6029
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:55 +0000

    Add package SwampDragon/0.4.1.2/SwampDragon-0.4.1.2.zip

---
+++ packages/SwampDragon/SwampDragon-0.4.1.2.zip/SwampDragon-0.4.1.2/swampdragon/settings_provider.py
@@ -0,0 +1,24 @@
+from tornado.web import RequestHandler
+from swampdragon.default_settings import SwampDragonSettings
+from django.conf import settings as django_settings
+
+
+def get_host():
+    host = django_settings.DRAGON_URL
+    if host.endswith('/'):
+        return host[:-1]
+    return host
+
+
+class SettingsHandler(RequestHandler):
+    def set_default_headers(self):
+        self.set_header("Content-Type", "application/javascript")
+
+    def get(self, *args, **kwargs):
+        data = '''window.swampdragon_settings = {settings};
+window.swampdragon_host = "{host}";
+'''.format(**{
+            'settings': SwampDragonSettings().to_dict(),
+            'host': get_host()
+        })
+        self.write(data)
