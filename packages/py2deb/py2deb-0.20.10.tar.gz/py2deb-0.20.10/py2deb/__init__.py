ebe307785cef5f69cc75a24fd1bed57b522ddf34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:24 +0000

    Add package py2deb/0.20.10/py2deb-0.20.10.tar.gz

---
+++ packages/py2deb/py2deb-0.20.10.tar.gz/py2deb-0.20.10/py2deb/__init__.py
@@ -0,0 +1,18 @@
+# py2deb: Python to Debian package converter.
+#
+# Authors:
+#  - Arjan Verwer
+#  - Peter Odding <peter.odding@paylogic.com>
+# Last Change: March 4, 2015
+# URL: https://py2deb.readthedocs.org
+
+"""
+The top level :py:mod:`py2deb` module contains only a version number.
+
+.. data:: __version__
+
+   The version number of the `pydeb` package (a string).
+"""
+
+# Semi-standard module versioning.
+__version__ = '0.20.10'
