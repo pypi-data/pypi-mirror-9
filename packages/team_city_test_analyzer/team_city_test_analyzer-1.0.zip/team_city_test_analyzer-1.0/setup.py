bc8359ad33e757103a0a29f43e28b780f7b2e27e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:36:03 +0000

    Add package team_city_test_analyzer/1.0/team_city_test_analyzer-1.0.zip

---
+++ packages/team_city_test_analyzer/team_city_test_analyzer-1.0.zip/team_city_test_analyzer-1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='team_city_test_analyzer',
+	version='1.0',
+	description='For use with .csv ouput files from TeamCity',
+	url='http://elevenwireless.com',
+	author='Joe Good',
+	author_email='jgood@elevenwireless.com',
+	license='MIT',
+	packages=['TestAnalyzer'],
+	zip_safe=False)
