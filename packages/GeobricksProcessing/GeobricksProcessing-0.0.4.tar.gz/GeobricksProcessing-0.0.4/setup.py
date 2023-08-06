d650504fbeaa7a5215bba5ec9449999f242d8de6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:52 +0000

    Add package GeobricksProcessing/0.0.4/GeobricksProcessing-0.0.4.tar.gz

---
+++ packages/GeobricksProcessing/GeobricksProcessing-0.0.4.tar.gz/GeobricksProcessing-0.0.4/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+from setuptools import find_packages
+
+setup(
+    name='GeobricksProcessing',
+    version='0.0.4',
+    author='Simone Murzilli; Guido Barbaglia',
+    author_email='geobrickspy@gmail.com',
+    packages=find_packages(),
+    license='LICENSE.txt',
+    long_description=open('README.md').read(),
+    description='Geobricks processing library for raster data.',
+    install_requires=[
+        'watchdog',
+        'flask',
+        'flask-cors',
+        'GeobricksCommon'
+    ],
+    url='http://pypi.python.org/pypi/GeobricksProcessing/',
+    keywords=['geobricks', 'processing', 'raster', 'gis', 'gdal']
+)
---
+++ packages/GeobricksProcessing/GeobricksProcessing-0.0.4.tar.gz/GeobricksProcessing-0.0.4/test/geobricks_processing_test.py
@@ -0,0 +1,52 @@
+import unittest
+from shutil import rmtree
+import os
+from geobricks_processing.core import processing_core
+
+path = "../test_data/burundi_maize_area"
+source_path = [path + "/burundi_maize_area_3857.tif"]
+output_path = path + "/gdalwarp"
+if os.path.isdir(output_path):
+    rmtree(output_path)
+
+processing_gdalwarp = [
+    {
+        "source_path": source_path,
+        "output_path": output_path,
+        "output_file_name": "burundi_maize_area_4326.tif",
+        "band": 1,
+        "process": [
+            {
+                "gdalwarp": {
+                    "opt": {
+                        "-multi": "",
+                        "-overwrite": "",
+                        "-of": "GTiff",
+                        "-s_srs": "EPSG:3857",
+                        "-t_srs": "EPSG:4326"
+                    }
+                }
+            }
+        ]
+    },
+    {
+        "band": 1,
+        "process": [
+            {
+                "gdaladdo": {
+                    "parameters": {
+                        # "--config": "BIGTIFF_OVERVIEW IF_NEEDED"
+                    },
+                    "overviews_levels": "2 4 8 16"
+                }
+            }
+        ]
+    }
+]
+
+
+class GeobricksProcessingTest(unittest.TestCase):
+
+    def test_processing_gdalwarp_gdaladdo(self):
+        outputfiles = processing_core.process_data(processing_gdalwarp)
+        self.assertEqual(len(outputfiles), 1)
