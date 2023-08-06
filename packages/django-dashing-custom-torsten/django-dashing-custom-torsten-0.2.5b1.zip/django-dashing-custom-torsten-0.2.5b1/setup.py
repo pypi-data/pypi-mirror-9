01201e43355ede027a5edf2cbf960b38a617f545
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:06 +0000

    Add package django-dashing-custom-torsten/0.2.5b1/django-dashing-custom-torsten-0.2.5b1.zip

---
+++ packages/django-dashing-custom-torsten/django-dashing-custom-torsten-0.2.5b1.zip/django-dashing-custom-torsten-0.2.5b1/setup.py
@@ -0,0 +1,37 @@
+import os
+from setuptools import setup
+
+README = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-dashing-custom-torsten',
+    version='0.2.5b1',
+    packages=['dashing'],
+    include_package_data=True,
+    license='MIT License',
+    description='A simple Django dashboard app to visualize interesting data about your project. '
+                'Custom for testing by Torsten Feld',
+    long_description=README,
+    url='https://github.com/torstenfeld/django-dashing',
+    author='Torsten Feld (Original: Mauricio Reyes)',
+    author_email='torsten.feld@avira.com (Original: mreyes@talpor.com)',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Software Development :: Localization',
+        'Topic :: Utilities',
+    ],
+    keywords=[],
+)
