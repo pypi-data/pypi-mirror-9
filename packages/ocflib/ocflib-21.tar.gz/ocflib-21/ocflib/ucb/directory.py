16bd36a969d5311f157b00a8bc26f6e362057027
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:19 +0000

    Add package ocflib/21/ocflib-21.tar.gz

---
+++ packages/ocflib/ocflib-21.tar.gz/ocflib-21/ocflib/ucb/directory.py
@@ -0,0 +1,35 @@
+import ocflib.account.search as search
+
+
+def get_calnet_names(uid):
+    """Returns CalNet LDAP entries relating to names"""
+
+    attrs = search.user_attrs_ucb(uid)
+    if attrs:
+        return {key: attrs[key]
+                for key in ('givenName', 'sn', 'displayname') if attrs[key]}
+
+
+def name_by_calnet_uid(uid):
+    """Returns the name of CalNet person, searched by CalNet UID.
+
+    Returns None on faliure.
+    """
+    names = get_calnet_names(uid)
+
+    # the name we want to input into our system is "givenName sn"
+    # displayName is not necessarily equal to what's printed on Cal 1 Cards
+    def get_longest_string(strs):
+        return max(strs, key=len)
+
+    if 'givenName' in names and 'sn' in names:
+        given_name = get_longest_string(names['givenName'])
+        sn = get_longest_string(names['sn'])
+
+        if given_name and sn:
+            return "{} {}".format(given_name, sn)
+    elif 'displayName' in names:
+        display_name = get_longest_string(names["displayName"])
+
+        if display_name:
+            return display_name
