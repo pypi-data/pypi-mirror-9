ef01e4d577adcd9fd6be43a67f0bc2c3f594b424
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:37 +0000

    Add package nusbot/0.3.1/nusbot-0.3.1.tar.gz

---
+++ packages/nusbot/nusbot-0.3.1.tar.gz/nusbot-0.3.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+import nusbot
+
+
+setup(
+    name='nusbot',
+    version=nusbot.__version__,
+    description='adc bot annoucing changes in filelist of hub\'s users',
+    url='https://github.com/kraiz/nusbot',
+    author='Lars Kreisz',
+    author_email='lars.kreisz@gmail.com',
+    license='MIT',
+    packages=['nusbot', 'twisted.plugins'],
+    install_requires=['twisted'],
+    long_description=open('README.rst').read() + '\n\n' + open('CHANGELOG.rst').read(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Information Technology',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Communications :: Chat',
+        'Topic :: Communications :: File Sharing',
+    ],
+)
