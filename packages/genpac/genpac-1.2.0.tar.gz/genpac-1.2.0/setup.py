3b3701d5c8457044f96622b513145a4eb2f6b013
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:10 +0000

    Add package genpac/1.2.0/genpac-1.2.0.tar.gz

---
+++ packages/genpac/genpac-1.2.0.tar.gz/genpac-1.2.0/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+setup(
+    name='genpac',
+    version='1.2.0',
+    description='convert gfwlist to pac, custom rules supported.',
+    long_description=open('README.rst', 'r').read(),
+    author='JinnLynn',
+    author_email='eatfishlin@gmail.com',
+    url='https://github.com/JinnLynn/genpac',
+    packages=['genpac', 'genpac.pysocks'],
+    package_data={
+        'genpac': ['res/*']
+    },
+    entry_points={
+        'console_scripts': 'genpac=genpac:main'
+    },
+    license='MIT',
+    keywords='proxy pac gfwlist gfw',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+    ],
+)
