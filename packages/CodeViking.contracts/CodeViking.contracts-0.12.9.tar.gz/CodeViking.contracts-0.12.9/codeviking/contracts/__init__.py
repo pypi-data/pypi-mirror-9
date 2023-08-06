ae6e4bfa1d77397220c3ad02506dd83e894d6bf0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:46 +0000

    Add package CodeViking.contracts/0.12.9/CodeViking.contracts-0.12.9.tar.gz

---
+++ packages/CodeViking.contracts/CodeViking.contracts-0.12.9.tar.gz/CodeViking.contracts-0.12.9/codeviking/contracts/__init__.py
@@ -0,0 +1,8 @@
+from ._version import __version__
+
+from .argcheckers import Any, Is, Option, Seq, AllOf, Dict, Union, Seq, Eq, \
+    Geq, Gt, IsIterable, Leq, Lt, Neq, Set
+
+from .contracts import check_sig, contracts, precondition, postcondition, \
+    invariant
+
