23e07ff1722be1cdbd9ec8011cbcb6b6c7be3420
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:29:57 +0000

    Add package nefertari_mongodb/0.1.0/nefertari_mongodb-0.1.0.tar.gz

---
+++ packages/nefertari_mongodb/nefertari_mongodb-0.1.0.tar.gz/nefertari_mongodb-0.1.0/setup.py
@@ -0,0 +1,33 @@
+import os
+
+from setuptools import setup, find_packages
+
+install_requires = [
+    'mongoengine',
+    'zope.dottedname',
+    'elasticsearch',
+    'python-dateutil',
+    'pyramid_tm'
+]
+
+setup(
+    name='nefertari_mongodb',
+    version="0.1.0",
+    description='mongodb engine for nefertari',
+    classifiers=[
+        "Programming Language :: Python",
+        "Framework :: Pyramid",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+        "Topic :: Database",
+        "Topic :: Database :: Database Engines/Servers",
+    ],
+    author='',
+    author_email='',
+    url='',
+    keywords='web wsgi bfg pylons pyramid rest mongodb mongoengine',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=install_requires,
+)
