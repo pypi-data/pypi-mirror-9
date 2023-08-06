a430bcc4d0204b2d1b0cb150f225e1fb1353293f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:52 +0000

    Add package bitterGravel/0.0.1/bitterGravel-0.0.1.tar.gz

---
+++ packages/bitterGravel/bitterGravel-0.0.1.tar.gz/bitterGravel-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+    name='bitterGravel',
+    version='0.0.1',
+    author='Michael Imelfort',
+    author_email='mike@mikeimelfort.com',
+    packages=['bittergravel'],
+    scripts=['bin/bittergravel'],
+    url='http://pypi.python.org/pypi/bitterGravel/',
+    license='GPLv3',
+    description='bitterGravel',
+    long_description=open('README.md').read(),
+    install_requires=[],
+)
+
