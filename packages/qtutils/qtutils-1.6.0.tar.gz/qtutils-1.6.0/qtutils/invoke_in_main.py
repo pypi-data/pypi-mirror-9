3ec667efbf899fc1d962b7a9f96659893bff9622
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:50 +0000

    Add package qtutils/1.6.0/qtutils-1.6.0.tar.gz

---
+++ packages/qtutils/qtutils-1.6.0.tar.gz/qtutils-1.6.0/qtutils/invoke_in_main.py
@@ -0,0 +1,153 @@
+#####################################################################
+#                                                                   #
+# invoke_in_main.py                                                 #
+#                                                                   #
+# Copyright 2013, Christopher Billington, Philip Starkey            #
+#                                                                   #
+# This file is part of the qtutils project                          #
+# (see https://bitbucket.org/philipstarkey/qtutils )                #
+# and is licensed under the 2-clause, or 3-clause, BSD License.     #
+# See the license.txt file in the root of the project               #
+# for the full license.                                             #
+#                                                                   #
+#####################################################################
+
+from __future__ import print_function
+import sys
+
+PYTHON2 = sys.version < '3'
+
+if PYTHON2:
+    import Queue
+else:
+    import queue as Queue
+import threading
+import functools
+
+if 'PySide' in sys.modules.copy():
+    from PySide.QtCore import *
+else:
+    from PyQt4.QtCore import *
+
+
+class CallEvent(QEvent):
+    """An event containing a request for a function call."""
+    EVENT_TYPE = QEvent.Type(QEvent.registerEventType())
+    def __init__(self, queue, exceptions_in_main, fn, *args, **kwargs):
+        QEvent.__init__(self, self.EVENT_TYPE)
+        self.fn = fn
+        self.args = args
+        self.kwargs = kwargs
+        self._returnval = queue
+        # Whether to raise exceptions in the main thread or store them
+        # for raising in the calling thread:
+        self._exceptions_in_main = exceptions_in_main
+    
+
+class Caller(QObject):
+    """An event handler which calls the function held within a CallEvent."""
+    def event(self, event):
+        event.accept()
+        exception = None
+        try:
+            result = event.fn(*event.args, **event.kwargs)
+        except Exception:
+            # Store for re-raising the exception in the calling thread:
+            exception = sys.exc_info()
+            result = None
+            if event._exceptions_in_main:
+                # Or, if nobody is listening for this exception,
+                # better raise it here so it doesn't pass
+                # silently:
+                raise
+        finally:
+            event._returnval.put([result,exception])  
+        return True
+         
+caller = Caller()
+
+def inmain(fn, *args, **kwargs):
+    """Execute a function in the main thread. Wait for it to complete
+    and return its return value."""
+    if threading.current_thread().name == 'MainThread':
+        return fn(*args, **kwargs)
+    return get_inmain_result(in_main_later(fn,False,*args,**kwargs))
+    
+def inmain_later(fn, *args, **kwargs):
+    return in_main_later(fn,True,*args,**kwargs)
+    
+def in_main_later(fn, exceptions_in_main, *args, **kwargs):
+    """Asks the mainloop to call a function when it has time. Immediately
+    returns the queue that was sent to the mainloop.  A call to queue.get()
+    will return a list of [result,exception] where exception=[type,value,traceback]
+    of the exception.  Functions are guaranteed to be called in the order
+    they were requested."""        
+    queue = Queue.Queue()
+    QCoreApplication.postEvent(caller, CallEvent(queue, exceptions_in_main, fn, *args, **kwargs))
+    return queue
+    
+def get_inmain_result(queue):
+    result,exception = queue.get()
+    if exception is not None:
+        type, value, traceback = exception
+        if PYTHON2:
+            exec('raise type, value, traceback')
+        else:
+            raise value.with_traceback(traceback)
+    return result
+
+def inthread(f,*args,**kwargs):
+    thread = threading.Thread(target=f, args=args, kwargs=kwargs)
+    thread.daemon=True
+    thread.start()
+    return thread
+    
+def inmain_decorator(wait_for_return=True,exceptions_in_main=True):
+    """ A decorator which sets any function to always run in the main thread
+    If wait_for_return=True, then exceptions_in_main is ignored.
+    """
+    def wrap(fn):
+        """A decorator which sets any function to always run in the main thread."""
+        @functools.wraps(fn)
+        def f(*args,**kwargs):
+            if wait_for_return:
+                return inmain(fn, *args, **kwargs)
+            return in_main_later(fn, exceptions_in_main, *args, **kwargs)  
+        return f
+    return wrap
+    
+    
+if __name__ == '__main__':   
+    import signal
+    
+    def loop(index):
+        if index < 3:
+            thread = inthread(loop, index+1)
+        while True:
+            # print('MyThread-%d: %s'%(index,str(QThread.currentThread())))
+            # print('MyThread-%d: %s'%(index,threading.current_thread().name))
+            #another_function(index)
+            inmain(myFunction,index)
+    
+    def another_function(index):
+        print('in thread-%d, running in thread: %s'%(index,threading.currentThread().name))
+    
+    def myFunction(index):
+        print('from thread-%d, running in thread: %s'%(index,threading.currentThread().name))
+        pass
+        
+    def myFunction2():
+        print('from MainThread, running in thread: %s'%(threading.currentThread().name))
+        QTimer.singleShot(0,x)
+
+    qapplication = QCoreApplication(sys.argv)
+    def sigint_handler(*args):
+        qapplication.quit()
+      
+    signal.signal(signal.SIGINT, sigint_handler)
+
+    thread = inthread(loop, 1)
+    x = lambda: inmain(myFunction2)
+    timer = QTimer.singleShot(0,x)
+    qapplication.exec_()
+
---
+++ packages/qtutils/qtutils-1.6.0.tar.gz/qtutils-1.6.0/setup.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+
+# To upload a version to PyPI, run:
+#
+#    python setup.py sdist upload
+#
+# If the package is not registered with PyPI yet, do so with:
+#
+# python setup.py register
+
+from __future__ import absolute_import, print_function
+from distutils.core import setup
+import sys
+import os
+
+BUILD_PYQT4_ICONS_RESOURCE = True
+BUILD_PYSIDE_ICONS_RESOURCE = True
+
+if 'NO_PYSIDE' in sys.argv:
+    sys.argv.remove('NO_PYSIDE')
+    BUILD_PYSIDE_ICONS_RESOURCE = False
+if 'NO_PYQT4' in sys.argv:
+    BUILD_PYQT4_ICONS_RESOURCE = False
+    sys.argv.remove('NO_PYQT4')
+
+VERSION = '1.6.0'
+
+# Do the build process for icon resource files, this will only do anything
+# if the files are not already present.  The idea is that someone like me
+# will run this during sdist, upload the results to PyPI, and then the
+# files should already be there for those installing via easy_install
+# or pip. So those people will not require pyside-rcc or pyrcc4 on
+# their systems in order to install icon support for both PyQt4 and
+# Pyside. Those installing from an hg clone however will have to have
+# pyside-rcc and pyrcc4 installed for the following to work, or they
+# can disable one of the via the boolean flags at the top of this file.
+print('building qt icon resource files ...')
+sys.path.insert(0,'qtutils/icons')
+import _build
+if BUILD_PYQT4_ICONS_RESOURCE:
+    _build.pyqt4()
+if BUILD_PYSIDE_ICONS_RESOURCE:
+    _build.pyside()
+print('done')
+
+# Auto generate a __version__ package for the package to import
+with open(os.path.join('qtutils', '__version__.py'), 'w') as f:
+    f.write("__version__ = '%s'\n"%VERSION)
+
+setup(name='qtutils',
+      version=VERSION,
+      description='Utilities for providing concurrent access to Qt objects, simplified QSettings storage, and dynamic widget promotion when loading UI files, in Python Qt applications. Also includes the Fugue icon set, by Yusuke Kamiyamane',
+      author='Philip Starkey',
+      author_email='threepineapples@gmail.com',
+      url='https://bitbucket.org/philipstarkey/qtutils',
+      license="2-clause BSD, 3-clause BSD (see LICENSE.TXT for full conditions)",
+      packages=['qtutils', 'qtutils.icons'],
+      package_data={'qtutils.icons':
+                    ['custom/*',
+                     'fugue/*',
+                     'icons.qrc',
+                     'README.txt']}
+     )
