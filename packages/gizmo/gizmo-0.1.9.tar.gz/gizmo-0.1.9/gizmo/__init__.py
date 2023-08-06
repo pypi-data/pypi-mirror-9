86079477108c000f357790867adf0013fd011cc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:42 +0000

    Add package gizmo/0.1.9/gizmo-0.1.9.tar.gz

---
+++ packages/gizmo/gizmo-0.1.9.tar.gz/gizmo-0.1.9/gizmo/__init__.py
@@ -0,0 +1,3 @@
+from .client import AsyncGremlinClient, async, group, chain, chord
+from .exceptions import RequestError, GremlinServerError, SocketError
+__version__ = "0.1.3"
---
+++ packages/gizmo/gizmo-0.1.9.tar.gz/gizmo-0.1.9/setup.py
@@ -0,0 +1,28 @@
+# from distutils.core import setup
+from setuptools import setup
+
+
+setup(
+    name="gizmo",
+    version="0.1.9",
+    url="",
+    license="MIT",
+    author="davebshow",
+    author_email="davebshow@gmail.com",
+    description="Async Python 3 driver for TP3 Gremlin Server",
+    long_description=open("README.txt").read(),
+    packages=["gizmo"],
+    install_requires=[
+        "websockets==2.4"
+    ],
+    test_suite="gizmo.tests",
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3 :: Only'
+    ]
+)
