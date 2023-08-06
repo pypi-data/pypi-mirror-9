0db389fb115c61c510086c9a0f818c61164352f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:18 +0000

    Add package colosseum/0.1.0/colosseum-0.1.0-py2.py3-none-any.whl

---
+++ packages/colosseum/colosseum-0.1.0-py2.py3-none-any.whl/colosseum/constants.py
@@ -0,0 +1,38 @@
+##########################################################################
+# Attribute names
+##########################################################################
+
+# Dimensions
+WIDTH = 'width'
+HEIGHT = 'height'
+
+# Positions
+LEFT = 'left'
+TOP = 'top'
+BOTTOM = 'bottom'
+RIGHT = 'right'
+
+# Flex directions
+COLUMN = 'column'
+ROW = 'row'
+
+##########################################################################
+# CSS Style values
+##########################################################################
+
+# Alignments & Justification values
+AUTO = 'auto'
+FLEX_START = 'flex-start'
+CENTER = 'center'
+FLEX_END = 'flex-end'
+STRETCH = 'stretch'
+SPACE_BETWEEN = 'space-between'
+SPACE_AROUND = 'space-around'
+
+# Position values
+RELATIVE = 'relative'
+ABSOLUTE = 'absolute'
+
+# Wrap values
+NOWRAP = 'nowrap'
+WRAP = 'wrap'
