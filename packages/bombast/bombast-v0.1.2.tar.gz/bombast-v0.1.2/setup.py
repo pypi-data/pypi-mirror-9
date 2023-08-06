e76bd247fce293a19eb6721183c53062b738974c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:27 +0000

    Add package bombast/v0.1.2/bombast-v0.1.2.tar.gz

---
+++ packages/bombast/bombast-v0.1.2.tar.gz/bombast-v0.1.2/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+import bombast
+
+setup(
+    name='bombast',
+    version='v0.1.2',
+    description='An obfuscator for Python source code that manipulates the AST.',
+    url='https://github.com/brianhou/bombast',
+    author='Brian Hou',
+    packages=['bombast'],
+    install_requires=['astunparse'],
+    entry_points={
+        'console_scripts': [
+            'bombast=bombast.__init__:main',
+        ],
+    }
+)
