1afdf4cf56b650366edcc113ee2df1dd943b4c31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:26 +0000

    Add package launchkey-python/1.2.4/launchkey-python-1.2.4.tar.gz

---
+++ packages/launchkey-python/launchkey-python-1.2.4.tar.gz/launchkey-python-1.2.4/setup.py
@@ -0,0 +1,37 @@
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.md')).read()
+CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
+
+requires = [
+    'pycrypto',
+    'requests>=2.5.1',
+    ]
+
+setup(name='launchkey-python',
+      version='1.2.4',
+      description='LaunchKey Python SDK',
+      long_description=README + '\n\n' + CHANGES,
+      classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+        ],
+      author='LaunchKey',
+      author_email='support@launchkey.com',
+      url='https://launchkey.com',
+      keywords='launchkey security authentication',
+      license='MIT',
+      py_modules=[
+          'launchkey',
+      ],
+      zip_safe=False,
+      test_suite='tests',
+      install_requires=requires,
+      tests_require=[
+        'Mocker',
+      ],
+      )
