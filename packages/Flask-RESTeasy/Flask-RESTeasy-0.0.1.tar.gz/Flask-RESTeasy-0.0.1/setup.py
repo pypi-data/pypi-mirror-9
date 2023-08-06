776462d25f777655c044ba0003428b933ed6183d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:40 +0000

    Add package Flask-RESTeasy/0.0.1/Flask-RESTeasy-0.0.1.tar.gz

---
+++ packages/Flask-RESTeasy/Flask-RESTeasy-0.0.1.tar.gz/Flask-RESTeasy-0.0.1/setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+import os
+from setuptools import setup, find_packages
+
+# Get version without import and fresh install race condition.
+for _ in open(os.path.join(os.path.dirname(__file__), 'flask_resteasy.py')).readlines():
+    if _.startswith('__version__'):
+        exec(_.strip(), None)
+        break
+
+requirements = [
+    'Flask>=0.10',
+]
+
+setup(
+    name='Flask-RESTeasy',
+    author='Clinton James',
+    author_email='clinton.james@anuit.com',
+    url='https://www.github.com/jidn/flask-restful/',
+    download_url='https://github.com/jidn/flask-resteasy/tarball/'+__version__,
+    description='Create easy REST APIs with Flask',
+    long_description=open('README.md').read(),
+    version=__version__,
+    keywords=['flask', 'REST'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Framework :: Flask',
+        'Intended Audience :: Developers',
+        'Environment :: Web Environment',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 2.7',
+        # 'Programming Language :: Python :: 3',
+    ],
+    py_modules=['flask_resteasy'],
+    # packages=find_packages(exclude=['tests']),
+    zip_safe=False,
+    include_package_data=True,
+    install_requires=requirements,
+    # Install these with "pip install -e '.[paging]'" or '.[docs]'
+    # extras_require={
+    #     'docs': 'sphinx',
+    # }
+)
