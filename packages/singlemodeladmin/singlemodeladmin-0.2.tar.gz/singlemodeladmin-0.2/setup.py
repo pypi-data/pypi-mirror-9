2168beefec11e756bdfb2c2ed8c5aa4861215954
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:24 +0000

    Add package singlemodeladmin/0.2/singlemodeladmin-0.2.tar.gz

---
+++ packages/singlemodeladmin/singlemodeladmin-0.2.tar.gz/singlemodeladmin-0.2/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='singlemodeladmin',
+    packages=['singlemodeladmin'],
+    version='0.2',
+    description='ModelAdmin for models that are meant to have one record',
+    author='Alexander Meng',
+    author_email='alexbmeng@gmail.com',
+    url='https://github.com/AMeng/django-single-model-admin',
+    keywords=['django', 'model', 'admin'],
+    install_requires=[
+        'Django>=1.4'
+    ]
+)
