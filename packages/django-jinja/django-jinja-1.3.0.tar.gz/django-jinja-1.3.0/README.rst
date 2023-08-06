733da406a8c6b136d93ed066230c0a9cefc08758
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:37 +0000

    Add package django-jinja/1.3.0/django-jinja-1.3.0.tar.gz

---
+++ packages/django-jinja/django-jinja-1.3.0.tar.gz/django-jinja-1.3.0/README.rst
@@ -0,0 +1,29 @@
+django-jinja
+============
+
+Simple and nonobstructive jinja2 integration with Django.
+
+**Now with support for django 1.8**
+
+
+.. image:: https://img.shields.io/travis/niwibe/django-jinja.svg?style=flat
+    :target: https://travis-ci.org/niwibe/django-jinja
+
+.. image:: https://img.shields.io/pypi/v/django-jinja.svg?style=flat
+    :target: https://pypi.python.org/pypi/django-jinja
+
+.. image:: https://img.shields.io/pypi/dm/django-jinja.svg?style=flat
+    :target: https://pypi.python.org/pypi/django-jinja
+
+
+**Documentation:** http://niwibe.github.io/django-jinja/
+
+
+How to install?
+---------------
+
+You can install it with pip:
+
+.. code-block:: shell
+
+    pip install django-jinja
---
+++ packages/django-jinja/django-jinja-1.3.0.tar.gz/django-jinja-1.3.0/setup.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+import sys
+
+
+INSTALL_REQUIRES = [
+    "jinja2 >=2.5",
+    "django >=1.4",
+]
+
+if sys.version_info < (2, 7):
+    INSTALL_REQUIRES.append("importlib")
+
+setup(
+    name = "django-jinja",
+    version = "1.3.0",
+    description = "Jinja2 templating language integrated in Django.",
+    long_description = "",
+    keywords = "django, jinja2",
+    author = "Andrey Antukh",
+    author_email = "niwi@niwi.be",
+    url = "https://github.com/niwibe/django-jinja",
+    license = "BSD",
+    packages = [
+        "django_jinja",
+        "django_jinja.builtins",
+        "django_jinja.management",
+        "django_jinja.management.commands",
+        "django_jinja.contrib",
+        "django_jinja.contrib._pipeline",
+        "django_jinja.contrib._pipeline.templatetags",
+        "django_jinja.contrib._easy_thumbnails",
+        "django_jinja.contrib._easy_thumbnails.templatetags",
+        "django_jinja.contrib._humanize",
+        "django_jinja.contrib._humanize.templatetags",
+        "django_jinja.contrib._subdomains",
+        "django_jinja.contrib._subdomains.templatetags",
+    ],
+
+    install_requires = INSTALL_REQUIRES,
+
+    classifiers = [
+        "Development Status :: 5 - Production/Stable",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Topic :: Internet :: WWW/HTTP",
+    ]
+)
