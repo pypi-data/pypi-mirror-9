4aab0c73334b84b6e11d588e273bf727a905bdcb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:15 +0000

    Add package vectors/0.0.9/vectors-0.0.9.tar.gz

---
+++ packages/vectors/vectors-0.0.9.tar.gz/vectors-0.0.9/setup.py
@@ -0,0 +1,24 @@
+import os
+from setuptools import setup, find_packages
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name="vectors",
+    version="0.0.9",
+    author="Allelos",
+    author_email="p.paliagkas@gmail.com",
+    description="A simple vector toolkit dealing with vectors and points \
+        in the 3-dimensional space",
+    long_description=read('CHANGELOG.rst'),
+    license="The MIT License (MIT)",
+    url="https://github.com/allelos/vectors",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python",
+        "Development Status :: 4 - Beta",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
