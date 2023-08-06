7b3ef679396f7718136c26e21b98c0424411ddfd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:13 +0000

    Add package lazy_python/0.1.5/lazy_python-0.1.5.tar.gz

---
+++ packages/lazy_python/lazy_python-0.1.5.tar.gz/lazy_python-0.1.5/lazy/__init__.py
@@ -0,0 +1,18 @@
+from lazy import data
+from lazy._thunk import thunk, strict
+from lazy._undefined import undefined
+from lazy.bytecode import lazy_function
+from lazy.runtime import run_lazy
+from lazy.transformer import LazyTransformer
+
+__version__ = '0.1.5'
+
+__all__ = [
+    'LazyTransformer',
+    'run_lazy',
+    'lazy_function',
+    'thunk',
+    'data',
+    'undefined',
+    'strict',
+]
---
+++ packages/lazy_python/lazy_python-0.1.5.tar.gz/lazy_python-0.1.5/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+from distutils.core import Extension, setup
+import sys
+
+long_description = ''
+
+if 'upload' in sys.argv:
+    with open('README.rst') as f:
+        long_description = f.read()
+
+setup(
+    name='lazy_python',
+    version='0.1.5',
+    description='Lazy evaluation for python 3',
+    author='Joe Jevnik',
+    author_email='joejev@gmail.com',
+    packages=[
+        'lazy',
+    ],
+    long_description=long_description,
+    license='GPL-2',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Operating System :: POSIX',
+        'Topic :: Software Development :: Pre-processors',
+    ],
+    url='https://github.com/llllllllll/lazy_python',
+    ext_modules=[
+        Extension('lazy._undefined', ['lazy/_undefined.c']),
+        Extension('lazy._thunk', ['lazy/_thunk.c']),
+    ],
+)
