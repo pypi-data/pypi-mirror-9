dbbfe589cd23d0a305394cc321c3b9ad9e7c71dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:07:56 +0000

    Add package caffeine/0.3/caffeine-0.3.tar.gz

---
+++ packages/caffeine/caffeine-0.3.tar.gz/caffeine-0.3/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+  name = 'caffeine',
+  py_modules = ['caffeine'],
+  version = '0.3',
+  description = 'A tool to keep Mac OS X from sleeping',
+  author = 'Jeffrey Newman',
+  author_email = 'jeff@newman.me',
+  url = 'https://github.com/jpn--/caffeine', 
+  download_url = 'https://github.com/jpn--/caffeine', 
+		  license = 'MIT',
+		  classifiers = [
+			'License :: OSI Approved :: MIT License',
+			'Programming Language :: Python :: 3',
+			'Operating System :: MacOS :: MacOS X',
+		  ],
+  install_requires=["pyobjc-core >= 3.0.4",],
+)
