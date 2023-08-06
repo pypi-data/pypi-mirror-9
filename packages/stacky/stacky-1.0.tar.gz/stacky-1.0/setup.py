52fa96b4bf84c1472c2b63d8703980716283a5b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:41 +0000

    Add package stacky/1.0/stacky-1.0.tar.gz

---
+++ packages/stacky/stacky-1.0.tar.gz/stacky-1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='stacky',
+    version='1.0',
+    install_requires=[
+        "nose==1.3.4"
+    ],
+    scripts=[
+        "scripts/istacky",
+    ],
+ )
