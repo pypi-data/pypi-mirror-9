0c7cb14232df0e5084896b8833af737497b1780f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:14 +0000

    Add package natty/0.2.3/natty-0.2.3.tar.gz

---
+++ packages/natty/natty-0.2.3.tar.gz/natty-0.2.3/natty/__init__.py
@@ -0,0 +1,46 @@
+import os
+import imp
+import jpype
+import socket
+import threading
+from dateutil import parser
+
+if jpype.isJVMStarted() is not 1:
+    jars = []
+    for top, dirs, files in os.walk(imp.find_module('natty')[1] + '/data'):
+        for nm in files:
+            jars.append(os.path.join(top, nm))
+    jpype.startJVM(
+        jpype.getDefaultJVMPath(),
+        "-Djava.class.path=%s" % os.pathsep.join(jars)
+    )
+
+socket.setdefaulttimeout(15)
+lock = threading.Lock()
+
+NattyParser = jpype.JClass('com.eadmundo.natty.NattyParser')
+
+
+class DateParser(object):
+
+    def __init__(self, date_string):
+        self.date_groups = None
+        try:
+            # make it thread-safe
+            if threading.activeCount() > 1:
+                if jpype.isThreadAttachedToJVM() is not 1:
+                    jpype.attachThreadToJVM()
+            lock.acquire()
+
+            self.date_groups = NattyParser.parseDateIntoGroups(date_string)
+        finally:
+            lock.release()
+
+    def parse(self, d):
+        return parser.parse(d)
+
+    def result(self):
+        if self.date_groups is not None:
+            return [self.parse(d.toString()) for d in [
+                dg for dg in self.date_groups][0].dates]
+        return None
---
+++ packages/natty/natty-0.2.3.tar.gz/natty-0.2.3/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup
+
+setup(
+    name='natty',
+    version='0.2.3',
+    packages=['natty'],
+    package_data={
+        'natty': [
+            'data/natty-basic-parser-0.2.0.jar'
+        ],
+    },
+    package_dir={'natty': 'natty'},
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'JPype1',
+        'python-dateutil'
+    ],
+    author='Edward Stone',
+    author_email='edwardjstone@yahoo.com',
+    url='https://github.com/eadmundo/python-natty',
+    download_url='https://github.com/eadmundo/python-natty/archive/v0.2.3.tar.gz',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Natural Language :: English',
+    ],
+    keywords='natty',
+    license='MIT',
+    description='Python interface to Natty Date Parser'
+)
