2801e1c5ff21188efd8af5a54f7c2ed75693386a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:10 +0000

    Add package entitymanager/v0.1.2/entitymanager-v0.1.2.zip

---
+++ packages/entitymanager/entitymanager-v0.1.2.zip/entitymanager-v0.1.2/setup.py
@@ -0,0 +1,20 @@
+import os
+from distutils.core import setup
+
+version = 'v0.1.2'
+
+setup(
+    name="entitymanager",
+    packages=['pyentity'],
+    version=version,
+    keywords=["mongo", "mongodb", "pyentity", "motor", "tornado"],
+    description="An simple object-document mapping for mongodb-motor and tornado applications.",
+    author="Leonardo Souza",
+    author_email="leo.desouza@gmail.com",
+    url="https://github.com/leodesouza/pyentity",
+    download_url="https://github.com/leodesouza/pyentity/tree/v0.1.0-beta",
+    classifiers=[
+        "License :: OSI Approved :: Apache Software License",
+    ],
+    license='http://www.apache.org/licenses/LICENSE-2.0',
+)
