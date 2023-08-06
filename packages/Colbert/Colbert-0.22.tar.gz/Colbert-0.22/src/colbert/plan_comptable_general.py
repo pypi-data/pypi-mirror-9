dd8c51e971ad3cc291c654408274b66b1fbfd9a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:52 +0000

    Add package Colbert/0.22/Colbert-0.22.tar.gz

---
+++ packages/Colbert/Colbert-0.22.tar.gz/Colbert-0.22/src/colbert/plan_comptable_general.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+
+from colbert.common import NOM, NUMERO
+
+
+PLAN_COMPTABLE_GENERAL = {
+    'perte': {
+        NUMERO: "129",
+        NOM: u"résultat de l'exercice (perte)",
+    },
+    'benefice': {
+        NUMERO: "120",
+        NOM: u"résultat de l'exercice (bénéfice)",
+    },
+    'regroupement-produits': {
+        NUMERO: "127",
+        NOM: u"Regroupement des comptes de produits",
+    },
+    'regroupement-charges': {
+        NUMERO: "126",
+        NOM: u"Regroupement des comptes de charges",
+    },
+    'tva-ca-factures-a-etablir': {
+        NUMERO: "44587",
+        NOM: u"Taxes sur le CA sur factures à établir",
+    },
+    'tva-collectee': {
+        NUMERO: "44571",
+        NOM: u"TVA collecté",
+    },
+    'tva-deductible': {
+        NUMERO: "44566",
+        NOM: u"TVA déductible sur autres biens et services",
+    },
+    'tva-a-decaisser': {
+        NUMERO: "44551",
+        NOM: u"TVA à décaisser",
+    },
+    'produits-divers-gestion-courante': {
+        NUMERO: "758",
+        NOM: u"Produits divers de gestion courante",
+    },
+    'charges-diverses-gestion-courante': {
+        NUMERO: "658",
+        NOM: u"Charges diverses de gestion courante",
+    },
+}
