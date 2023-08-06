c846fbe4454956ce87018e8c25baa32449ef49e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:19 +0000

    Add package python-consul/0.3.12/python-consul-0.3.12.tar.gz

---
+++ packages/python-consul/python-consul-0.3.12.tar.gz/python-consul-0.3.12/consul/__init__.py
@@ -0,0 +1,9 @@
+__version__ = '0.3.12'
+
+from consul.std import Consul
+
+from consul.base import ConsulException
+from consul.base import ACLPermissionDenied
+from consul.base import ACLDisabled
+from consul.base import NotFound
+from consul.base import Timeout
