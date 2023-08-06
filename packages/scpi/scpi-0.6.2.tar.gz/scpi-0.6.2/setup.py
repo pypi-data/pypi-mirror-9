a4032edc6fe4ae25c7c1b973265294922dd8fcf6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:02 +0000

    Add package scpi/0.6.2/scpi-0.6.2.tar.gz

---
+++ packages/scpi/scpi-0.6.2.tar.gz/scpi-0.6.2/setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+import subprocess
+
+git_version = 'UNKNOWN'
+try:
+    git_version = str(subprocess.check_output(['git', 'rev-parse', '--verify', '--short', 'HEAD'])).strip()
+except subprocess.CalledProcessError,e:
+    print "Got error when trying to read git version: %s" % e
+
+setup(
+    name='scpi',
+    #version='0.6.1dev-%s' % git_version,
+    version='0.6.2',
+    author='Eero "rambo" af Heurlin',
+    author_email='rambo@iki.fi',
+    packages=['scpi',],
+    license='GNU LGPL',
+    long_description=open('README.md').read(),
+    description='Implement SCPI in pure Python',
+    install_requires=[
+        'pyserial>=2.7',
+    ],
+    url='https://github.com/rambo/python-scpi',
+)
+
