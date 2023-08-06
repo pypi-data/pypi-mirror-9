b912bbe1b612a3b5c0898aa19218ba1572cdb10b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:12 +0000

    Add package arcpylogger/1.0.0/arcpylogger-1.0.0.zip

---
+++ packages/arcpylogger/arcpylogger-1.0.0.zip/arcpylogger-1.0.0/arcpylogger/__init__.py
@@ -0,0 +1,9 @@
+__doc__ = """
+Title: ArcPy Logger
+Description: Classes to define ArcPy message stream handlers and a helper function for us noobs.
+Usage:
+"""
+
+from arcpylogger import ArcpyMessageStream
+from arcpylogger import ArcpyMessageHandler
+from arcpyloggerhelper import setup_logging
---
+++ packages/arcpylogger/arcpylogger-1.0.0.zip/arcpylogger-1.0.0/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='arcpylogger',
+    version='1.0.0',
+    packages=['arcpylogger'],
+    url='https://github.com/gisinc/ArcPyLogger',
+    license='MIT',
+    author='cfricke, carnold',
+    author_email='cfricke@gisinc.com, carnold@gisinc.com',
+    maintainer='cfricke, carnold',
+    maintainer_email='cfricke@gisinc.com, carnold@gisinc.com',
+    description='Classes to define ArcPy message stream handlers and a helper function for us noobs.'
+)
