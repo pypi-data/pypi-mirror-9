6480217ed5df07ba93328e88d6355503ad1c554c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:23:00 +0000

    Add package qifqif/0.2.1/qifqif-0.2.1.tar.gz

---
+++ packages/qifqif/qifqif-0.2.1.tar.gz/qifqif-0.2.1/setup.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+# Copyright (c) 2015 Fabrice Laporte - kray.me
+# The MIT License http://www.opensource.org/licenses/mit-license.php
+
+import os
+import sys
+
+from setuptools import setup
+
+
+def publish():
+    """Publish to PyPi"""
+    os.system("python setup.py sdist upload")
+
+if sys.argv[-1] == "publish":
+    publish()
+    sys.exit()
+
+setup(name='qifqif',
+    version='0.2.1',
+    description='QIF file editing tool',
+    long_description=open('README.rst').read(),
+    author='Fabrice Laporte',
+    author_email='kraymer@gmail.com',
+    url='https://github.com/KraYmer/qifqif',
+    license='MIT',
+    platforms='ALL',
+
+    packages=[
+      'qifqif',
+    ],
+
+    entry_points={
+      'console_scripts': [
+          'qifqif = qifqif:main',
+      ],
+    },
+
+    install_requires=[
+        'clint',
+    ],
+
+    classifiers=[
+      'License :: OSI Approved :: MIT License',
+      'Programming Language :: Python',
+      'Environment :: Console',
+      'Topic :: Office/Business :: Financial :: Accounting'
+    ])
