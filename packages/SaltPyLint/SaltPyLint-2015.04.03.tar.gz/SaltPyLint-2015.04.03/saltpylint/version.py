b612d25e85191e6d3c1e918af32a93cb2362af3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:41 +0000

    Add package SaltPyLint/2015.04.03/SaltPyLint-2015.04.03.tar.gz

---
+++ packages/SaltPyLint/SaltPyLint-2015.04.03.tar.gz/SaltPyLint-2015.04.03/saltpylint/version.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+'''
+    saltpylint.version
+    ~~~~~~~~~~~~~~~~~~~
+
+    :codeauthor: :email:`Pedro Algarvio (pedro@algarvio.me)`
+    :copyright: © 2013 by the SaltStack Team, see AUTHORS for more details.
+    :license: Apache 2.0, see LICENSE for more details.
+'''
+
+__version_info__ = (2015, 4, 3)
+__version__ = '{0[0]}.{0[1]:0>2}.{0[2]:0>2}'.format(__version_info__)
