ba86035c87e840b77650d89e12e8f93a36d700a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:29:54 +0000

    Add package serpy/0.0.2/serpy-0.0.2.tar.gz

---
+++ packages/serpy/serpy-0.0.2.tar.gz/serpy-0.0.2/serpy/__init__.py
@@ -0,0 +1,13 @@
+from serpy.fields import (
+    Field, BoolField, IntField, FloatField, MethodField, StrField)
+from serpy.serializer import Serializer
+
+__all__ = [
+    'Serializer',
+    'Field',
+    'BoolField',
+    'IntField',
+    'FloatField',
+    'MethodField',
+    'StrField',
+]
