d8a859f1aea15e99cf705b57ec8f58a9d06c0ac1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:36:28 +0000

    Add package factoryboy-gaendb/0.1.4/factoryboy-gaendb-0.1.4.tar.gz

---
+++ packages/factoryboy-gaendb/factoryboy-gaendb-0.1.4.tar.gz/factoryboy-gaendb-0.1.4/setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+
+setup(
+    name='factoryboy-gaendb',
+    version='0.1.4',
+    packages=[
+        'gaendb',
+    ],
+    license='Apache v2',
+    long_description=open('pypi.rst').read(),
+    description='Factoryboy base factories and helpers for Google App Engine ndb models',
+    author="Anentropic",
+    author_email="ego@anentropic.com",
+    url="https://github.com/anentropic/factoryboy-gaendb",
+    install_requires=[
+        "google-appengine",
+        "factory_boy",
+    ],
+)
