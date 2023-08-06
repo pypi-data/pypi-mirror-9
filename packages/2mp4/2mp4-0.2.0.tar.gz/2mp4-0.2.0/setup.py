010b8e348c521f5c6212918f3a3b343f4a12035d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:44 +0000

    Add package 2mp4/0.2.0/2mp4-0.2.0.tar.gz

---
+++ packages/2mp4/2mp4-0.2.0.tar.gz/2mp4-0.2.0/setup.py
@@ -0,0 +1,56 @@
+from collections import namedtuple
+import subprocess
+
+from setuptools import setup, find_packages
+
+
+classifiers = """\
+Intended Audience :: End Users/Desktop
+License :: OSI Approved :: MIT License
+Programming Language :: Python
+Topic :: Multimedia :: Video :: Conversion
+Operating System :: Unix
+"""
+
+try:
+    p = subprocess.Popen(
+        ['git', 'describe', '--tags'],
+        stderr=subprocess.PIPE,
+        stdout=subprocess.PIPE,
+        shell=False
+    )
+    p.wait()
+except:
+    p = namedtuple('Process', 'returncode')(1)
+
+if p.returncode == 0:
+    version = p.communicate()[0].strip().decode()
+    with open('.version', 'w+') as version_file:
+        version_file.write(version)
+else:
+    with open('.version', 'r') as version_file:
+        version = version_file.readline().strip()
+
+
+setup(
+    name='2mp4',
+    version=version,
+    url='https://github.com/skonakov/2mp4.git',
+    download_url='https://github.com/skonakov/2mp4/tarball/' + version,
+    license='MIT',
+    description='Simple utility to convert your video files into mp4s.',
+    author='Sergey Konakov',
+    author_email='skonakov@gmail.com',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    package_data={'': ['../../.version']},
+    install_requires=['sh', 'progressbar2', 'pymediainfo', 'lxml'],
+    zip_safe=False,
+    entry_points="""\
+    [console_scripts]
+    2mp4 = 2mp4.2mp4:main
+    """,
+    classifiers=filter(None, classifiers.split('\n')),
+    #long_description=read('README.rst') + '\n\n' + read('CHANGES'),
+    #extras_require={'test': []}
+)
