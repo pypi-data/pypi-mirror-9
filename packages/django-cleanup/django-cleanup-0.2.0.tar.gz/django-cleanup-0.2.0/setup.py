2acb8857cea88807d93de02d654973ab2d114b29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:55 +0000

    Add package django-cleanup/0.2.0/django-cleanup-0.2.0.tar.gz

---
+++ packages/django-cleanup/django-cleanup-0.2.0.tar.gz/django-cleanup-0.2.0/setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  Copyright (c) 2012 Ilya Shalyapin
+#
+#  django-cleanup is free software under terms of the MIT License.
+#
+
+import os
+from setuptools import setup, find_packages
+
+
+setup(
+    name     = 'django-cleanup',
+    version  = '0.2.0',
+    packages = ['django_cleanup'],
+    include_package_data=True,
+    requires = ['python (>= 2.5)', 'django (>= 1.3)'],
+    description  = 'Deletes old files.',
+    long_description = open('README.rst').read(),
+    author       = 'Ilya Shalyapin',
+    author_email = 'ishalyapin@gmail.com',
+    url          = 'https://github.com/un1t/django-cleanup',
+    download_url = 'https://github.com/un1t/django-cleanup/tarball/master',
+    license      = 'MIT License',
+    keywords     = 'django',
+    classifiers  = [
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+    ],
+)
