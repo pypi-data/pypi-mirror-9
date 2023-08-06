dd5550c15179583e601cd74036457298f9c42d55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:35 +0000

    Add package pipedrive-py/0.3.5/pipedrive-py-0.3.5.tar.gz

---
+++ packages/pipedrive-py/pipedrive-py-0.3.5.tar.gz/pipedrive-py-0.3.5/pipedrive/pipeline.py
@@ -0,0 +1,35 @@
+# encoding:utf-8
+from schematics.exceptions import ValidationError
+from base import BaseResource, PipedriveAPI, CollectionResponse, dict_to_model
+from schematics.models import Model
+from schematics.types import StringType, IntType
+from types import PipedriveDateTime
+
+
+class Pipeline(Model):
+    """Represents one of the pipelines in the Pipedrive ecosystem"""
+    id = IntType(required=False)
+    name = StringType(required=False)
+    order_nr = IntType(required=False, min_value=0)
+    active = IntType(required=False, choices=(0,1))
+
+
+class PipelineResource(BaseResource):
+    API_ACESSOR_NAME = 'pipeline'
+    LIST_REQ_PATH = '/pipelines'
+    DETAIL_REQ_PATH = '/pipelines/{id}'
+
+    def detail(self, resource_ids):
+        response = self._detail(resource_ids)
+        return Pipeline(raw_input(response.json))
+
+    def create(self, deal):
+        response = self._create(data=deal.to_native())
+        return dict_to_model(response.json()['data'], Pipeline)
+
+    def list(self):
+        return CollectionResponse(self._list(), Pipeline)
+
+
+
+PipedriveAPI.register_resource(PipelineResource)
