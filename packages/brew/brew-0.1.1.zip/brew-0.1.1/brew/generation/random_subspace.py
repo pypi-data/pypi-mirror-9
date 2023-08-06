0246031514bc195b986d94ebc8d9b54b18c8833c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:07 +0000

    Add package brew/0.1.1/brew-0.1.1.zip

---
+++ packages/brew/brew-0.1.1.zip/brew-0.1.1/brew/generation/random_subspace.py
@@ -0,0 +1,52 @@
+from brew.base import Ensemble
+from brew.combination.rules import majority_vote_rule
+from .base import PoolGenerator
+
+import numpy as np
+from sklearn.ensemble import BaggingClassifier
+
+class RandomSubspace(PoolGenerator):
+
+    def __init__(self, base_classifier=None, n_classifiers=100, combination_rule=majority_vote_rule, max_features=0.5):
+        self.base_classifier = base_classifier
+        self.n_classifiers = n_classifiers
+        self.combination_rule = combination_rule
+        self.sk_random_subspace = BaggingClassifier(base_estimator=base_classifier,
+                n_estimators=n_classifiers, max_samples=1.0, max_features=max_features)
+        self.classifiers = None
+        self.ensemble = None
+        
+
+    def fit(self, X, y):
+        self.X = X
+        self.y = y
+
+        self.sk_random_subspace.fit(X, y)
+        self.classifiers = self.sk_random_subspace.estimators_
+        self.ensemble = Ensemble(classifiers=self.classifiers)
+        self.classes_ = set(y)
+
+        return self
+
+
+    def predict(self, X):
+        #TODO usar combinator
+        y = []
+        for i in range(X.shape[0]):
+            d = {}
+            mx = None
+            for idx, classifier in enumerate(self.classifiers):
+                mask = np.zeros(X.shape[1], bool)
+                mask[self.sk_random_subspace.estimators_features_[idx]] = True
+                [out] = classifier.predict(X[i][mask])
+                d[out] = d[out] + 1 if out in d else 1
+                if mx == None or d[mx] < d[out]:
+                    mx = out
+            y = y + [mx]
+        y = map(lambda e: self.sk_random_subspace.classes_[e], y)
+
+        return np.asarray(y)
+            
+                
+                
+        
---
+++ packages/brew/brew-0.1.1.zip/brew-0.1.1/setup.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+
+import os
+import sys
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    sys.exit()
+
+readme = open('README.rst').read()
+doclink = """
+Documentation
+-------------
+
+The full documentation is at http://brew.rtfd.org."""
+history = open('HISTORY.rst').read().replace('.. :changelog:', '')
+
+import brew
+VERSION=brew.__version__
+
+setup(
+    name='brew',
+    version=VERSION,
+    description='BREW: Python Multiple Classifier System API',
+    long_description=readme + '\n\n' + doclink + '\n\n' + history,
+    author='Dayvid Victor <victor.dvro@gmail.com>, Thyago Porpino <thyago.porpino@gmail.com>',
+    author_email='brew-python-devs@googlegroups.com',
+    url='https://github.com/viisar/brew',
+    packages=[
+        'brew',
+        'brew.combination',
+        'brew.generation',
+        'brew.metrics',
+        'brew.metrics.diversity',
+        'brew.preprocessing',
+        'brew.selection',
+        'brew.selection.dynamic'
+    ],
+    package_dir={'brew': 'brew'},
+    include_package_data=True,
+    install_requires=[
+    ],
+    license='MIT',
+    zip_safe=False,
+    keywords='brew',
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Topic :: Scientific/Engineering',
+    ],
+)
