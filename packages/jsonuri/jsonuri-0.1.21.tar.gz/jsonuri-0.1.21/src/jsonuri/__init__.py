4409c8fa710316923fc2ee6bf8655f6f86776411
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:14 +0000

    Add package jsonuri/0.1.21/jsonuri-0.1.21.tar.gz

---
+++ packages/jsonuri/jsonuri-0.1.21.tar.gz/jsonuri-0.1.21/src/jsonuri/__init__.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+
+# Copyright 2015, Guilherme Dinis J
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+__author__ = 'guilherme'
+__copyright__ = "2015, Guilherme Dinis J"
+__email__ = "guilherme@predictry.com"
+__license__ = "Apache License, Version 2.0"
+__package__ = "jsonuri"
+__version__ = "0.1.21"
