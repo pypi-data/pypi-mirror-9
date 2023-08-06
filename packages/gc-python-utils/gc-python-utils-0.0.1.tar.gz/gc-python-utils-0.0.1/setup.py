a3e8b60377928ea6becfdea38fe7f8548d1cd390
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:47 +0000

    Add package gc-python-utils/0.0.1/gc-python-utils-0.0.1.tar.gz

---
+++ packages/gc-python-utils/gc-python-utils-0.0.1.tar.gz/gc-python-utils-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+
+setup(name='gc-python-utils',
+      version='0.0.1',
+      description='GameChanger general python utilities',
+      url='https://github.com/gamechanger/python-utils',
+      author='GameChanger',
+      author_email='jordan.singleton@gamechanger.io',
+      packages=['gc_python_utils'],
+      package_dir={'gc_python_utils': 'gc_python_utils'},
+      zip_safe=False)
