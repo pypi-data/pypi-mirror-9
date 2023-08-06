e2513d4b4a11ec905dd9ad352619fd7519c6dbc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:21 +0000

    Add package aws-saml-login/0.4/aws-saml-login-0.4.tar.gz

---
+++ packages/aws-saml-login/aws-saml-login-0.4.tar.gz/aws-saml-login-0.4/README.rst
@@ -0,0 +1,62 @@
+==============
+AWS SAML Login
+==============
+
+This Python package provides some helper functions to allow programmatic retrieval of temporary AWS credentials from STS_ (Security Token Service) when using federated login with `Shibboleth IDP`_.
+
+The implementation relies on HTML parsing of the Shibboleth redirect page (HTML form) and the AWS role selection page.
+
+This package requires Python 3.4.
+
+Installation
+============
+
+.. code-block:: bash
+
+    $ sudo pip3 install --upgrade aws-saml-login
+
+Usage
+=====
+
+.. code-block:: python
+
+    from aws_saml_login import authenticate, assume_role, write_aws_credentials
+
+    # authenticate against identity provider
+    saml_xml, roles = authenticate('https://shibboleth-idp.example.org', user, password)
+
+    print(roles)
+
+    # just use the first role here, we might display a user dialog to choose one
+    first_role = roles[0]
+
+    provider_arn, role_arn, account_name = first_role
+
+    # get temporary AWS credentials
+    key_id, secret, session_token = assume_role(saml_xml, provider_arn, role_arn)
+
+    # write to ~/.aws/credentials
+    write_aws_credentials('default', key_id, secret, session_token)
+
+    # AWS SDK (e.g. boto) can be used to call AWS endpoints
+
+.. _STS: http://docs.aws.amazon.com/STS/latest/UsingSTS/Welcome.html
+.. _Shibboleth IDP: http://shibboleth.net/products/identity-provider.html
+
+
+License
+=======
+
+Copyright © 2015 Zalando SE
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
---
+++ packages/aws-saml-login/aws-saml-login-0.4.tar.gz/aws-saml-login-0.4/aws_saml_login/saml.py
@@ -0,0 +1,170 @@
+import codecs
+from xml.etree import ElementTree
+import botocore.session
+from bs4 import BeautifulSoup
+import os
+import configparser
+import requests
+
+
+AWS_CREDENTIALS_PATH = '~/.aws/credentials'
+
+
+def write_aws_credentials(profile, key_id, secret, session_token=None):
+    credentials_path = os.path.expanduser(AWS_CREDENTIALS_PATH)
+    os.makedirs(os.path.dirname(credentials_path), exist_ok=True)
+    config = configparser.ConfigParser()
+    if os.path.exists(credentials_path):
+        config.read(credentials_path)
+
+    config[profile] = {}
+    config[profile]['aws_access_key_id'] = key_id
+    config[profile]['aws_secret_access_key'] = secret
+    if session_token:
+        # apparently the different AWS SDKs either use "session_token" or "security_token", so set both
+        config[profile]['aws_session_token'] = session_token
+        config[profile]['aws_security_token'] = session_token
+
+    with open(credentials_path, 'w') as fd:
+        config.write(fd)
+
+
+def get_saml_response(html: str):
+    """
+    Parse SAMLResponse from Shibboleth page
+
+    >>> get_saml_response('<input name="a"/>')
+
+    >>> get_saml_response('<body xmlns="bla"><form><input name="SAMLResponse" value="eG1s"/></form></body>')
+    'xml'
+    """
+    soup = BeautifulSoup(html)
+
+    for elem in soup.find_all('input', attrs={'name': 'SAMLResponse'}):
+        saml_base64 = elem.get('value')
+        xml = codecs.decode(saml_base64.encode('ascii'), 'base64').decode('utf-8')
+        return xml
+
+
+def get_form_action(html: str):
+    '''
+    >>> get_form_action('<body><form action="test"></form></body>')
+    'test'
+    '''
+    soup = BeautifulSoup(html)
+    return soup.find('form').get('action')
+
+
+def get_account_name(role_arn: str, account_names: dict):
+    number = role_arn.split(':')[4]
+    if account_names:
+        return account_names.get(number)
+
+
+def get_roles(saml_xml: str) -> list:
+    """
+    Extract SAML roles from SAML assertion XML
+
+    >>> get_roles('''<xml xmlns="urn:oasis:names:tc:SAML:2.0:assertion"><Assertion>
+    ... <Attribute FriendlyName="Role" Name="https://aws.amazon.com/SAML/Attributes/Role">
+    ... <AttributeValue>arn:aws:iam::911:saml-provider/Shibboleth,arn:aws:iam::911:role/Shibboleth-User</AttributeValue>
+    ... </Attribute>
+    ... </Assertion></xml>''')
+    [('arn:aws:iam::911:saml-provider/Shibboleth', 'arn:aws:iam::911:role/Shibboleth-User')]
+    """
+    tree = ElementTree.fromstring(saml_xml)
+
+    assertion = tree.find('{urn:oasis:names:tc:SAML:2.0:assertion}Assertion')
+
+    roles = []
+    for attribute in assertion.findall('.//{urn:oasis:names:tc:SAML:2.0:assertion}Attribute[@Name]'):
+        if attribute.attrib['Name'] == 'https://aws.amazon.com/SAML/Attributes/Role':
+            for val in attribute.findall('{urn:oasis:names:tc:SAML:2.0:assertion}AttributeValue'):
+                provider_arn, role_arn = val.text.split(',')
+                roles.append((provider_arn, role_arn))
+    return roles
+
+
+def get_account_names(html: str) -> dict:
+    '''
+    Parse account names from AWS page
+
+    >>> get_account_names('')
+    {}
+
+    >>> get_account_names('<div class="saml-account-name">Account: blub  (123) </div>')
+    {'123': 'blub'}
+
+    >>> get_account_names('<div class="saml-account-name">Account: blub  123) </div>')
+    {}
+    '''
+    soup = BeautifulSoup(html)
+
+    accounts = {}
+    for elem in soup.find_all('div', attrs={'class': 'saml-account-name'}):
+        try:
+            name_number = elem.text.split(':', 1)[-1].strip().rstrip(')')
+            name, number = name_number.rsplit('(', 1)
+            name = name.strip()
+            number = number.strip()
+            accounts[number] = name
+        except:
+            # just skip account in case of parsing errors
+            pass
+    return accounts
+
+
+class AuthenticationFailed(Exception):
+    def __init__(self):
+        pass
+
+
+def authenticate(url, user, password):
+    '''Authenticate against the provided Shibboleth Identity Provider'''
+
+    session = requests.Session()
+    response = session.get(url)
+
+    # NOTE: parameters are hardcoded for Shibboleth IDP
+    data = {'j_username': user, 'j_password': password, 'submit': 'Login'}
+    response2 = session.post(response.url, data=data)
+    saml_xml = get_saml_response(response2.text)
+    if not saml_xml:
+        raise AuthenticationFailed()
+
+    url = get_form_action(response2.text)
+    encoded_xml = codecs.encode(saml_xml.encode('utf-8'), 'base64')
+    response3 = session.post(url, data={'SAMLResponse': encoded_xml})
+    account_names = get_account_names(response3.text)
+
+    roles = get_roles(saml_xml)
+
+    roles = [(p_arn, r_arn, get_account_name(r_arn, account_names)) for p_arn, r_arn in roles]
+
+    return saml_xml, roles
+
+
+def assume_role(saml_xml, provider_arn, role_arn):
+    saml_assertion = codecs.encode(saml_xml.encode('utf-8'), 'base64').decode('ascii').replace('\n', '')
+
+    # botocore NEEDS some credentials, but does not care about their actual values
+    os.environ['AWS_ACCESS_KEY_ID'] = 'fake123'
+    os.environ['AWS_SECRET_ACCESS_KEY'] = 'fake123'
+
+    try:
+        session = botocore.session.get_session()
+        sts = session.get_service('sts')
+        operation = sts.get_operation('AssumeRoleWithSAML')
+
+        endpoint = sts.get_endpoint('eu-west-1')
+        endpoint._signature_version = None
+        http_response, response_data = operation.call(endpoint, role_arn=role_arn, principal_arn=provider_arn,
+                                                      SAMLAssertion=saml_assertion)
+    finally:
+        del os.environ['AWS_ACCESS_KEY_ID']
+        del os.environ['AWS_SECRET_ACCESS_KEY']
+
+    key_id = response_data['Credentials']['AccessKeyId']
+    secret = response_data['Credentials']['SecretAccessKey']
+    session_token = response_data['Credentials']['SessionToken']
+    return key_id, secret, session_token
