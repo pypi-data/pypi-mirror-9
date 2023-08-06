b0f2d0918c7413d9ee78732f6d265d5883942136
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:19 +0000

    Add package flasfka/1.1.1/flasfka-1.1.1.tar.gz

---
+++ packages/flasfka/flasfka-1.1.1.tar.gz/flasfka-1.1.1/setup.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+"""
+Push/Pull on Kafka over HTTP
+"""
+
+from setuptools import setup
+from subprocess import check_output as run
+import sys
+import os
+
+
+from flasfka import __version__
+
+
+if os.getenv("TRAVIS") is not None:
+    try:
+        GIT_VERSION = run(["git", "describe"]).decode().strip()
+        if not GIT_VERSION.startswith(__version__):
+            sys.exit("The git tag does not match the version. Please fix.")
+    except subprocess.CalledProcessError:
+        pass
+
+setup(
+    name="flasfka",
+    version=__version__,
+    description=__doc__,
+    long_description=open("README.rst").read(),
+    author="Christophe-Marie Duquesne",
+    author_email="chmd@chmd.fr",
+    url="https://github.com/travel-intelligence/flasfka",
+    download_url="https://github.com/travel-intelligence/flasfka/" +
+                 "archive/%s.tar.gz" % __version__,
+    packages=["flasfka"],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        ],
+    install_requires=[
+        "Flask >= 0.10",
+        "kafka-python >= 0.9.2",
+        ],
+    scripts=["flasfka-serve"],
+    zip_safe=False,
+    include_package_data=True
+)
