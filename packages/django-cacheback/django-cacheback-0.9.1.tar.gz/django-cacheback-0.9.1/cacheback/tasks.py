f42fdf7cdf365309cedeb1c0aa81d70884eeec34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:33 +0000

    Add package django-cacheback/0.9.1/django-cacheback-0.9.1.tar.gz

---
+++ packages/django-cacheback/django-cacheback-0.9.1.tar.gz/django-cacheback-0.9.1/cacheback/tasks.py
@@ -0,0 +1,66 @@
+import time
+
+from celery.task import task
+from celery.utils.log import get_task_logger
+from django.utils import importlib
+
+
+logger = get_task_logger(__name__)
+
+
+@task()
+def refresh_cache(klass_str, obj_args, obj_kwargs, call_args, call_kwargs):
+    """
+    Re-populate cache using the given job class.
+
+    The job class is instantiated with the passed constructor args and the
+    refresh method is called with the passed call args.  That is::
+
+        data = klass(*obj_args, **obj_kwargs).refresh(
+            *call_args, **call_kwargs)
+
+    :klass_str: String repr of class (eg 'apps.twitter.jobs:FetchTweetsJob')
+    :obj_args: Constructor args
+    :obj_kwargs: Constructor kwargs
+    :call_args: Refresh args
+    :call_kwargs: Refresh kwargs
+    """
+    klass = _get_job_class(klass_str)
+    if klass is None:
+        logger.error("Unable to construct %s with args %r and kwargs %r",
+                     klass_str, obj_args, obj_kwargs)
+        return
+
+    logger.info("Using %s with constructor args %r and kwargs %r",
+                klass_str, obj_args, obj_kwargs)
+    logger.info("Calling refresh with args %r and kwargs %r", call_args,
+                call_kwargs)
+    start = time.time()
+    try:
+        klass(*obj_args, **obj_kwargs).refresh(
+            *call_args, **call_kwargs)
+    except Exception as e:
+        logger.error("Error running job: '%s'", e)
+        logger.exception(e)
+    else:
+        duration = time.time() - start
+        logger.info("Refreshed cache in %.6f seconds", duration)
+
+
+def _get_job_class(klass_str):
+    """
+    Return the job class
+    """
+    mod_name, klass_name = klass_str.rsplit('.', 1)
+    try:
+        mod = importlib.import_module(mod_name)
+    except ImportError as e:
+        logger.error("Error importing job module %s: '%s'", mod_name, e)
+        return
+    try:
+        klass = getattr(mod, klass_name)
+    except AttributeError:
+        logger.error("Module '%s' does not define a '%s' class", mod_name,
+                     klass_name)
+        return
+    return klass
---
+++ packages/django-cacheback/django-cacheback-0.9.1.tar.gz/django-cacheback-0.9.1/setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+import os
+from cacheback import __version__
+
+PACKAGE_DIR = os.path.abspath(os.path.dirname(__file__))
+os.chdir(PACKAGE_DIR)
+
+
+setup(name='django-cacheback',
+      version=__version__,
+      url='https://github.com/codeinthehole/django-cacheback',
+      author="David Winterbottom",
+      author_email="david.winterbottom@gmail.com",
+      description=("Caching library for Django that uses Celery "
+                   "to refresh cache items asynchronously"),
+      long_description=open(os.path.join(PACKAGE_DIR, 'README.rst')).read(),
+      license='MIT',
+      packages=find_packages(exclude=["sandbox*", "tests*"]),
+      include_package_data=True,
+      install_requires=[
+          'django>=1.3,<1.7',
+          'django-celery>=3.0',
+          'celery<3.2',
+          'six',
+          ],
+      # See http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=['Environment :: Web Environment',
+                   'Framework :: Django',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved :: BSD License',
+                   'Operating System :: Unix',
+                   'Programming Language :: Python']
+      )
