dd3e2981938ef8413fdeb37a9b86f7f95cce1e87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:22 +0000

    Add package japanese_holiday/0.0.3/japanese_holiday-0.0.3.tar.gz

---
+++ packages/japanese_holiday/japanese_holiday-0.0.3.tar.gz/japanese_holiday-0.0.3/info.py
@@ -0,0 +1,18 @@
+# package information.
+INFO = dict(
+  name        = "japanese_holiday",
+  description = "Get Japanese holiday lib.",
+  author      = "J Yukawa",
+  author_email = "j.yukawa@gmail.com",
+  license     = "MIT",
+  url         = "https://bitbucket.org/vourja/japanese-holiday",
+  classifiers = [
+    "Development Status :: 1 - Planning",
+    "Intended Audience :: Developers",
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 2.7",
+    "License :: OSI Approved :: MIT License"
+  ],
+  keywords='japanese holiday',
+
+)
---
+++ packages/japanese_holiday/japanese_holiday-0.0.3.tar.gz/japanese_holiday-0.0.3/setup.py
@@ -0,0 +1,38 @@
+# -*- coding:utf-8 -*-
+import os
+try:
+    import setuptools
+    from setuptools import setup, find_packages
+except ImportError:
+  print("Please install setuptools.")
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.md')).read()
+CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
+
+requirements = [
+]
+
+tests_require = [
+]
+
+entry_points = {
+    'console_scripts': [
+    ],
+}
+
+import info
+import version
+setup_options = info.INFO
+setup_options['version'] = version.VERSION
+
+
+setup_options.update(dict(
+    install_requires    = open('requirements.txt').read().splitlines(),
+    packages            = ['.'],
+    long_description    = README + '\n\n' + CHANGES,
+    tests_require       = tests_require,
+    entry_points        = entry_points,
+))
+
+setup(**setup_options)
