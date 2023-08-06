533193331c872a9bcb602ef60a2d638a54d447a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:09 +0000

    Add package pyramid_royal/0.8.2/pyramid_royal-0.8.2.tar.gz

---
+++ packages/pyramid_royal/pyramid_royal-0.8.2.tar.gz/pyramid_royal-0.8.2/royal/__init__.py
@@ -0,0 +1,69 @@
+from pyramid.events import NewRequest
+
+from royal.declarative import collection_config, item_config
+from royal.renderer import renderer_adapter
+from royal.resource import Root, Collection, Item
+from royal.deserializer import deserializer_config
+
+
+__all__ = [
+    'Collection',
+    'deserializer_config',
+    'Item',
+    'renderer_adapter',
+    'Root',
+    'collection_config',
+    'item_config',
+]
+
+
+def includeme(config):
+    config.include('royal.renderer')
+    config.include('royal.deserializer')
+    config.include('royal.directives')
+    config.include('royal.resource')
+    config.include('royal.views')
+
+    config.add_subscriber_predicate(
+        'request_methods',
+        RequestMethodEventPredicate
+    )
+
+    config.add_subscriber(
+        override_request_method,
+        NewRequest,
+        request_methods=['POST'],
+    )
+    config.add_request_method(is_resource_nested, 'is_nested')
+    config.commit()
+
+
+def is_resource_nested(request, resource):
+    """Return True if resource is nested in another resource representation.
+
+    It permits to vary a resource representation.
+    """
+    return request.context not in [resource, resource.parent]
+
+
+class RequestMethodEventPredicate(object):
+
+    def __init__(self, methods, config):
+        self.methods = methods
+        self.phash = self.text
+
+    def __call__(self, event):
+        return event.request.method in self.methods
+
+    def text(self):
+        return 'request_method in %s' % self.methods
+
+
+def override_request_method(event):
+    methods = ['PUT', 'DELETE']
+    override = (
+        event.request.headers.get('X-HTTP-Method-Override') or
+        event.request.POST.get('_method', '').upper()
+    )
+    if override in methods:
+        event.request.method = override
---
+++ packages/pyramid_royal/pyramid_royal-0.8.2.tar.gz/pyramid_royal-0.8.2/royal/views.py
@@ -0,0 +1,162 @@
+import logging
+
+from pyramid.settings import asbool
+from pyramid.view import view_defaults, view_config
+from pyramid.httpexceptions import (
+    HTTPCreated,
+    HTTPConflict,
+    HTTPBadRequest,
+    HTTPInternalServerError,
+    HTTPNotFound,
+    HTTPMethodNotAllowed,
+)
+
+
+from royal import exceptions as exc
+from royal import interfaces
+
+log = logging.getLogger(__name__)
+
+
+def includeme(config):
+    config.scan(__name__)
+    in_debug_mode = asbool(config.registry.get('debug', True))
+    if not in_debug_mode:
+        config.add_view(exception, context=Exception, renderer='royal')
+
+
+class BaseView(object):
+
+    def __init__(self, context, request):
+        self.context = context
+        self.request = request
+
+
+@view_defaults(context=interfaces.ICollection, renderer='royal')
+class CollectionView(BaseView):
+
+    @view_config(request_method='GET', permission='index')
+    def index(self):
+        func = self.context.index
+        query_params = self.request.GET.mixed()
+        if hasattr(self.context, 'index_schema'):
+            result = func(self.context.index_schema(query_params))
+        else:
+            result = func(query_params)
+
+        return result
+
+    @view_config(request_method='POST', permission='create')
+    def create(self):
+        func = self.context.create
+
+        params = self.request.deserialized_body
+        if hasattr(self.context, 'create_schema'):
+            params = self.context.create_schema(params)
+
+        item = func(params)
+
+        if hasattr(item, 'url'):
+            self.request.response.headers['Location'] = item.url()
+            self.request.response.status_int = HTTPCreated.code
+
+        return item
+
+
+@view_defaults(context=interfaces.IItem, renderer='royal')
+class ItemView(BaseView):
+
+    @view_config(request_method='GET', permission='show')
+    @view_config(request_method='GET', permission='show',
+                 context=interfaces.IRoot)
+    def show(self):
+        func = self.context.show
+        params = self.request.GET.mixed()
+        return func(params)
+
+    @view_config(request_method='PUT', permission='replace')
+    def put(self):
+        func = self.context.replace
+        params = self.request.deserialized_body
+
+        if hasattr(self.context, 'replace_schema'):
+            params = self.context.replace_schema(params)
+
+        return func(params)
+
+    @view_config(request_method='PATCH', permission='update')
+    def patch(self):
+        func = self.context.update
+        params = self.request.deserialized_body
+
+        if hasattr(self.context, 'update_schema'):
+            params = self.context.update_schema(params)
+
+        func(params)
+        return self.request.response
+
+    @view_config(request_method='POST')
+    def post(self):
+        # XXX should we permit POST on Item?
+        raise exc.MethodNotAllowed(self.context, 'POST')
+
+
+@view_config(context=interfaces.IItem, request_method='DELETE',
+             permission='delete', renderer='royal')
+@view_config(context=interfaces.ICollection, request_method='DELETE',
+             permission='delete', renderer='royal')
+def delete(context, request):
+    context.delete()
+    return {}
+
+
+@view_config(context=exc.MethodNotAllowed, renderer='royal')
+@view_config(context=interfaces.ICollection, renderer='royal')
+@view_config(context=interfaces.IItem, renderer='royal')
+@view_config(context=interfaces.IRoot, renderer='royal')
+def not_allowed(context, request):
+    request.response.status_int = HTTPMethodNotAllowed.code
+    return {
+        'error': 'method_not_allowed',
+        'resource': request.resource_url(context.resource
+                                         if hasattr(context, 'resource')
+                                         else context),
+        'http_method': request.method
+    }
+
+
+@view_config(context=exc.NotFound, renderer='royal')
+def item_not_found(context, request):
+    request.response.status_int = HTTPNotFound.code
+    return {
+        'error': 'not_found',
+        'resource': request.resource_url(context.resource)
+    }
+
+
+@view_config(context=exc.Conflict, renderer='royal')
+def conflict(context, request):
+    request.response.status_int = HTTPConflict.code
+    return {
+        'error': 'already_exists',
+        'resource': request.resource_url(context.resource)
+    }
+
+
+@view_config(context=exc.BadParameter, renderer='royal')
+def bad_parameter(context, request):
+    request.response.status_int = HTTPBadRequest.code
+    return {
+        'error': 'invalid_parameters',
+        'resource': request.resource_url(context.resource),
+        'message': '%s="%s"' % (context.name, context.value),
+    }
+
+
+def exception(context, request):
+    request.response.status_int = HTTPInternalServerError.code
+    return {
+        'error': 'unexpected_error',
+        'message': unicode(context),
+        'error_class': type(context).__name__,
+    }
