f03c8ade6810039a3c854f74f881f3f64bfd684a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:39 +0000

    Add package pyoeis/1.0.5/pyoeis-1.0.5.tar.gz

---
+++ packages/pyoeis/pyoeis-1.0.5.tar.gz/pyoeis-1.0.5/setup.py
@@ -0,0 +1,32 @@
+from distutils.core import setup
+import sys
+
+
+extra = {}
+if sys.version_info >= (3, ):
+    extra['use_2to3'] = True
+
+setup(
+    name='pyoeis',
+    packages=['pyoeis', ],
+    version='1.0.5',
+    author='Dylan Evans',
+    author_email='dylan@physicynicism.com',
+    url='www.physicynicism.com',
+    description='Python interface to the OEIS',
+    long_description='''
+    PyOEIS provides several methods to search the `Online
+    Encyclopedia of Integer Sequences with Python (OEIS)
+    <http://www.oeis.org>`_. It also allows for access
+    the information contained within individual sequence
+    entries, obtained by the querying methods
+    provided.''',
+    classifiers=['Development Status :: 3 - Alpha',
+                 'License :: OSI Approved :: Apache Software License',
+                 'Natural Language :: English',
+                 'Intended Audience :: Developers',
+                 'Topic :: Scientific/Engineering :: Mathematics',
+                 'Operating System :: OS Independent',
+                 'Programming Language :: Python'],
+    **extra
+    )
