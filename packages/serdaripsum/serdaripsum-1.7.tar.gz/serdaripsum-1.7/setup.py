5f597995fe8e37947fab96387a8ff38d92883964
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:49 +0000

    Add package serdaripsum/1.7/serdaripsum-1.7.tar.gz

---
+++ packages/serdaripsum/serdaripsum-1.7.tar.gz/serdaripsum-1.7/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(name='serdaripsum',
+      version='1.7',
+      description='serdar ipsum generator that runs on bash.',
+      url='https://github.com/halilkaya/serdaripsum',
+      author='Halil Kaya',
+      author_email='kayahalil@gmail.com',
+      license='GPLv3',
+      scripts=['serdaripsum.py'],
+      packages=['data'],
+      package_data={
+          'data': ['songs.txt'],
+      },
+      data_files=[
+          ('/opt/serdaripsum/', ['data/songs.txt'])
+      ],
+      install_requires=['setuptools'],
+      zip_safe=False)
