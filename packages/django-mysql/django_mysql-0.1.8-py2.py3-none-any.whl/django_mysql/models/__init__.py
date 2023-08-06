c4bbdbc9e9a00826f76706c157bd4fe39b6391df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:59 +0000

    Add package django-mysql/0.1.8/django_mysql-0.1.8-py2.py3-none-any.whl

---
+++ packages/django-mysql/django_mysql-0.1.8-py2.py3-none-any.whl/django_mysql/models/__init__.py
@@ -0,0 +1,7 @@
+from django_mysql.models.base import Model  # NOQA
+from django_mysql.models.aggregates import *  # NOQA
+from django_mysql.models.query import (  # NOQA
+    ApproximateInt, SmartChunkedIterator, SmartIterator, pt_visual_explain,
+    QuerySet, QuerySetMixin
+)
+from django_mysql.models.fields import *  # NOQA
