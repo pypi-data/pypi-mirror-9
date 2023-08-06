e3bdf1c7e4f88f998bbc120eb62535a4b5a5ef9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:36 +0000

    Add package spinnaker_proxy/2.0.1/spinnaker_proxy-2.0.1.tar.gz

---
+++ packages/spinnaker_proxy/spinnaker_proxy-2.0.1.tar.gz/spinnaker_proxy-2.0.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+import sys
+
+setup(
+    name="spinnaker_proxy",
+    version="2.0.1",
+    packages=[],
+    scripts=["spinnaker_proxy/spinnaker_proxy.py"],
+
+    # Metadata for PyPi
+    author="Jonathan Heathcote",
+    description="A proxy-server for SpiNNaker systems.",
+    license="GPLv2",
+    url="https://github.com/project-rig/spinnaker_proxy",
+
+    # Requirements
+    install_requires=["six"],
+)
