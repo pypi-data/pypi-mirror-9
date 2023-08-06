abcf466583acbb1eed1d1afc22e77cb95adfda0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:15 +0000

    Add package pytest-factoryboy/1.0.1/pytest-factoryboy-1.0.1.tar.gz

---
+++ packages/pytest-factoryboy/pytest-factoryboy-1.0.1.tar.gz/pytest-factoryboy-1.0.1/CHANGES.rst
@@ -0,0 +1,12 @@
+Changelog
+=========
+
+1.0.1
+-----
+
+- use ``inflection`` package to convert camel case to underscore (bubenkoff)
+
+1.0.0
+-----
+
+- initial release
---
+++ packages/pytest-factoryboy/pytest-factoryboy-1.0.1.tar.gz/pytest-factoryboy-1.0.1/setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+"""pytest-factoryboy package config."""
+
+import codecs
+import os
+from setuptools import setup
+
+import pytest_factoryboy
+
+
+dirname = os.path.dirname(__file__)
+
+long_description = (
+    codecs.open(os.path.join(dirname, "README.rst"), encoding="utf-8").read() + "\n" +
+    codecs.open(os.path.join(dirname, "AUTHORS.rst"), encoding="utf-8").read() + "\n" +
+    codecs.open(os.path.join(dirname, "CHANGES.rst"), encoding="utf-8").read()
+)
+
+setup(
+    name="pytest-factoryboy",
+    description="Factory Boy support for pytest.",
+    long_description=long_description,
+    author="Oleg Pidsadnyi, Anatoly Bubenkov and others",
+    license="MIT license",
+    author_email="oleg.pidsadnyi@gmail.com",
+    url="https://github.com/pytest-dev/pytest-factoryboy",
+    version=pytest_factoryboy.__version__,
+    classifiers=[
+        "Development Status :: 6 - Mature",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: MacOS :: MacOS X",
+        "Topic :: Software Development :: Testing",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Utilities",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3"
+    ] + [("Programming Language :: Python :: %s" % x) for x in "2.6 2.7 3.0 3.1 3.2 3.3 3.4".split()],
+    install_requires=[
+        "six",
+        "inflection",
+        "factory_boy",
+    ],
+    # the following makes a plugin available to py.test
+    entry_points={
+        "pytest11": [
+            "pytest-factoryboy = pytest_factoryboy.plugin",
+        ],
+    },
+    tests_require=["tox"],
+    packages=["pytest_factoryboy"],
+    include_package_data=True,
+)
