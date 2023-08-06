a4643d6d19acda0ea819a36b10c40fde12886b53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:41 +0000

    Add package ie/1.0.5/ie-1.0.5.tar.gz

---
+++ packages/ie/ie-1.0.5.tar.gz/ie-1.0.5/ie/checking.py
@@ -0,0 +1,102 @@
+# -*- coding: utf-8 -*-
+"""Module de validation the states number"""
+
+import ie.states.ac import ac    
+import ie.states.al import al
+import ie.states.am import am
+import ie.states.ap import ap
+import ie.states.ba import ba
+import ie.states.ce import ce
+import ie.states.df import df
+import ie.states.es import es
+import ie.states.go import go
+import ie.states.ma import ma
+import ie.states.mt import mt
+import ie.states.ms import ms
+import ie.states.mg import mg
+import ie.states.pa import pa
+import ie.states.pb import pb
+import ie.states.pe import pe
+import ie.states.pr import pr
+import ie.states.pi import pi
+import ie.states.rj import rj
+import ie.states.rn import rn
+import ie.states.rs import rs
+import ie.states.ro import ro
+import ie.states.rr import rr
+import ie.states.sc import sc
+import ie.states.sp import sp
+import ie.states.se import se
+import ie.states.to import to
+
+def start(state_registration_number, state_abbreviation):
+    """
+        This function is like a Facade to another modules that
+        makes their own state validation.
+
+        state_registration_number - string brazilian state registration number
+        state_abbreviation  - state abbreviation
+
+        AC (Acre)
+        AL (Alagoas)
+        AM (Amazonas)
+        AP (Amapá)
+        BA (Bahia)
+        CE (Ceará)
+        DF (Distrito Federal)
+        ES (Espírito Santo')
+        GO (Goias)
+        MA (Maranhão)
+        MG (Minas Gerais)
+        MS (Mato Grosso do Sul)
+        MT (Mato Grosso)
+        PA (Pará)
+        PB (Paraíba)
+        PE (Pernambuco)
+        PI (Piauí)
+        PR (Paraná)
+        RJ (Rio de Janeiro)
+        RN (Rio Grande do Norte)
+        RO (Rondônia)
+        RR (Roraima)
+        RS (Rio Grande do Sul)
+        SC (Santa Catarina)
+        SE (Sergipe)
+        SP (São Paulo)
+        TO (Tocantins)
+    """
+
+    state_abbreviation = state_abbreviation.upper()
+    states_validations = {
+        'AC': "ac.start(" + "\"" + state_registration_number + "\"" + ")",
+        'AL': "al.start(" + "\"" + state_registration_number + "\"" + ")",
+        'AM': "am.start(" + "\"" + state_registration_number + "\"" + ")",
+        'AP': "ap.start(" + "\"" + state_registration_number + "\"" + ")",
+        'BA': "ba.start(" + "\"" + state_registration_number + "\"" + ")",
+        'CE': "ce.start(" + "\"" + state_registration_number + "\"" + ")",
+        'DF': "df.start(" + "\"" + state_registration_number + "\"" + ")",
+        'ES': "es.start(" + "\"" + state_registration_number + "\"" + ")",
+        'GO': "go.start(" + "\"" + state_registration_number + "\"" + ")",
+        'MA': "ma.start(" + "\"" + state_registration_number + "\"" + ")",
+        'MG': "mg.start(" + "\"" + state_registration_number + "\"" + ")",
+        'MS': "ms.start(" + "\"" + state_registration_number + "\"" + ")",
+        'MT': "mt.start(" + "\"" + state_registration_number + "\"" + ")",
+        'PA': "pa.start(" + "\"" + state_registration_number + "\"" + ")",
+        'PB': "pb.start(" + "\"" + state_registration_number + "\"" + ")",
+        'PE': "pe.start(" + "\"" + state_registration_number + "\"" + ")",
+        'PI': "pi.start(" + "\"" + state_registration_number + "\"" + ")",
+        'PR': "pr.start(" + "\"" + state_registration_number + "\"" + ")",
+        'RJ': "rj.start(" + "\"" + state_registration_number + "\"" + ")",
+        'RN': "rn.start(" + "\"" + state_registration_number + "\"" + ")",
+        'RO': "ro.start(" + "\"" + state_registration_number + "\"" + ")",
+        'RR': "rr.start(" + "\"" + state_registration_number + "\"" + ")",
+        'RS': "rs.start(" + "\"" + state_registration_number + "\"" + ")",
+        'SC': "sc.start(" + "\"" + state_registration_number + "\"" + ")",
+        'SE': "se.start(" + "\"" + state_registration_number + "\"" + ")",
+        'SP': "sp.start(" + "\"" + state_registration_number + "\"" + ")",
+        'TO': "to.start(" + "\"" + state_registration_number + "\"" + ")"
+
+    }
+
+    exec('validity = ' + states_validations[state_abbreviation])
+    return validity
