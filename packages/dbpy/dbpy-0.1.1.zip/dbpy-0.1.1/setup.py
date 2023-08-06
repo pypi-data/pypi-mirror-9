b190160c9b0098720ef3f9b2120ccfc4e098828d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:41 +0000

    Add package dbpy/0.1.1/dbpy-0.1.1.zip

---
+++ packages/dbpy/dbpy-0.1.1.zip/dbpy-0.1.1/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+import sys
+from db import __version__
+
+setup(
+    name = 'dbpy',
+    version = __version__,
+    author = "lyanghwy",
+    author_email='lyanghwy@gmail.com',
+    description = "database abstraction layer for pythoneer",
+    license = "GPL",
+    keywords = "datbase abstraction layer for pythoneer(orm, database)",
+    url='https://github.com/thomashuang/dbpy',
+    long_description=open('README.rst').read(),
+    packages=find_packages(exclude=['samples', 'tests*']),
+    zip_safe=False,
+    include_package_data=True,
+    install_requires = ['setuptools',  'MySQL-python'],
+    test_suite='unittests',
+    classifiers=(
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
+        "Natural Language :: English",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Operating System :: OS Independent",
+        "Topic :: Database"
+        )
+    )
