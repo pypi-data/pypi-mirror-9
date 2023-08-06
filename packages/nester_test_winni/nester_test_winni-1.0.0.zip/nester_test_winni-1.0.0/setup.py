e2cc08ef6a19a904a3b3813c2f32884b46ed56ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:06 +0000

    Add package nester_test_winni/1.0.0/nester_test_winni-1.0.0.zip

---
+++ packages/nester_test_winni/nester_test_winni-1.0.0.zip/nester_test_winni-1.0.0/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+	name ='nester_test_winni',
+	version= '1.0.0',
+	py_modules=['nester_test_winni'],
+	author='conny',
+	description='test of list printer',
+	author_email='info@drwienands.com',)
