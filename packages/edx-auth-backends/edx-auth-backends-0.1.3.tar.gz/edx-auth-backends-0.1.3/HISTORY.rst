077f2c79bf9885f21eccedab1e0070dfa677c883
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:59 +0000

    Add package edx-auth-backends/0.1.3/edx-auth-backends-0.1.3.tar.gz

---
+++ packages/edx-auth-backends/edx-auth-backends-0.1.3.tar.gz/edx-auth-backends-0.1.3/HISTORY.rst
@@ -0,0 +1,19 @@
+.. :changelog:
+
+History
+=======
+
+0.1.3 (2015-03-31)
+------------------
+
+- Update required version of Python Social Auth to 0.2.3.
+
+0.1.2 (2015-02-23)
+------------------
+
+- Update required version of Python Social Auth to 0.2.2.
+
+0.1.1 (2015-02-20)
+------------------
+
+- Initial release.
---
+++ packages/edx-auth-backends/edx-auth-backends-0.1.3.tar.gz/edx-auth-backends-0.1.3/setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+from auth_backends._version import __version__
+
+
+with open('README.rst') as a, open('HISTORY.rst') as b, open('AUTHORS') as c:
+    long_description = '{}\n\n{}\n\n{}'.format(a.read(), b.read(), c.read())
+
+setup(
+    name='edx-auth-backends',
+    version=__version__,
+    description='Custom edX authentication backends and pipeline steps',
+    long_description=long_description,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: GNU Affero General Public License v3',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet',
+        'Intended Audience :: Developers',
+        'Environment :: Web Environment',
+    ],
+    keywords='authentication edx',
+    url='https://github.com/edx/auth-backends',
+    author='edX',
+    author_email='oscm@edx.org',
+    license='AGPL',
+    packages=find_packages(exclude=['tests*']),
+    install_requires=[
+        'Django>=1.7',
+        'python-social-auth>=0.2.3',
+    ],
+)
