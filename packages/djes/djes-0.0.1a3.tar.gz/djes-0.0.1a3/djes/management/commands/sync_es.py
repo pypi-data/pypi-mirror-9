aabe80ce8a898f5d8592b065e01f1fa1c2ecd728
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:15 +0000

    Add package djes/0.0.1a3/djes-0.0.1a3.tar.gz

---
+++ packages/djes/djes-0.0.1a3.tar.gz/djes-0.0.1a3/djes/management/commands/sync_es.py
@@ -0,0 +1,94 @@
+from django.core.management.base import BaseCommand
+from elasticsearch import TransportError
+from elasticsearch_dsl.connections import connections
+
+from djes.apps import indexable_registry
+from djes.conf import settings
+from djes.management.commands.bulk_index import bulk_index
+
+
+def get_indexes():
+    indexes = {}
+    for index, models in indexable_registry.indexes.items():
+        indexes[index] = {
+            "mappings": {}
+        }
+
+        if index in settings.ES_INDEX_SETTINGS:
+            indexes[index]["settings"] = settings.ES_INDEX_SETTINGS[index]
+
+        for model in models:
+            indexes[index]["mappings"].update(model.mapping.to_dict())
+
+    return indexes
+
+
+def build_versioned_index(name, version=1, body=None, old_version=None, should_index=False):
+    es = connections.get_connection("default")
+    versioned_index_name = "{0}_{1:0>4}".format(name, version)
+    es.indices.create(index=versioned_index_name, body=body)
+
+    if should_index:
+        bulk_index(es, index=name, version=version)  # Bulk index here...
+
+    actions = [{"add": {"index": versioned_index_name, "alias": name}}]
+    if old_version is not None:
+        old_versioned_index_name = "{0}_{1:0>4}".format(name, old_version)
+        actions.insert(0, {"remove": {"index": old_versioned_index_name, "alias": name}})
+
+    es.indices.update_aliases(body={"actions": actions})
+
+
+def sync_index(name, body, should_index=False):
+    es = connections.get_connection("default")
+
+    if not es.indices.exists_alias(name=name):
+        # We probably haven't synced before, that means we need to create an index, and then alias
+        build_versioned_index(name, body=body, should_index=should_index)
+        return
+
+    # The alias exists, so there's already a version of this index out there
+    alias = es.indices.get_alias(name=name)
+
+    # There will only be one key, let's the name from it
+    index_name = list(alias)[0]
+    if "settings" in body:
+        settings = es.indices.get_settings(index=index_name)[index_name]["settings"]
+
+        # This is a little hard to understand, but it works. This basically checks if `settings`
+        # is a subset of `body["settings"]` (credit: http://stackoverflow.com/q/9323749/931098)
+        if not all(item in settings.items() for item in body["settings"].items()):
+            # Well, it looks like the settings have changed
+            es.indices.put_settings(index=index_name, body=body["settings"])
+
+    server_mappings = es.indices.get_mapping(index=index_name)[index_name]["mappings"]
+
+    for doc_type, mapping_body in body["mappings"].items():
+        if mapping_body != server_mappings.get(doc_type, {}):
+            try:
+                es.indices.put_mapping(index=index_name, doc_type=doc_type, body=mapping_body)
+            except TransportError as e:
+                if "MergeMappingException" in e.error:
+                    # We need to do this the hard way
+                    old_version = int(index_name.split("_")[-1])
+                    new_version = old_version + 1
+                    build_versioned_index(
+                        name,
+                        version=new_version,
+                        body=body,
+                        old_version=old_version
+                    )
+                    break
+                else:
+                    raise e
+
+
+class Command(BaseCommand):
+    help = "Creates ES indices, and ensures that mappings are up to date"
+
+    def handle(self, *args, **options):
+
+        indexes = get_indexes()
+
+        for index, body in indexes.items():
+            sync_index(index, body, should_index=True)
---
+++ packages/djes/djes-0.0.1a3.tar.gz/djes-0.0.1a3/djes/models.py
@@ -0,0 +1,253 @@
+from django.db import models
+from elasticsearch.exceptions import NotFoundError
+from elasticsearch_dsl import Search
+from elasticsearch_dsl.connections import connections
+from elasticsearch_dsl.result import Response
+
+from .apps import indexable_registry
+from .factory import shallow_class_factory
+
+
+class ShallowResponse(Response):
+    def count(self):
+        return self.hits.total
+
+    def __len__(self):
+        return self.hits.total
+
+class FullResponse(Response):
+
+    def count(self):
+        return self.hits.total
+
+    def __len__(self):
+        return self.hits.total
+
+    def _get_result(self, hit):
+        doc_type = hit['_type']
+        _id = int(hit["_id"])
+        return self._in_bulk[doc_type][_id]
+
+    @property
+    def hits(self):
+        if not hasattr(self, "_hits"):
+            # If this is the first call, we need to cache all the hits with bulk queries
+            hits = self._d_["hits"]["hits"]
+
+            doc_type_map = {}
+            for hit in hits:
+                doc_type = hit["_type"]
+                if doc_type not in doc_type_map:
+                    doc_type_map[doc_type] = []
+                doc_type_map[doc_type].append(int(hit["_id"]))
+
+            self._in_bulk = {}
+            for doc_type, ids in doc_type_map.items():
+                cls = indexable_registry.all_models[doc_type]
+                self._in_bulk[doc_type] = cls.objects.in_bulk(ids)
+
+        return super(FullResponse, self).hits
+
+
+class LazySearch(Search):
+    """This extends the base Search object, allowing for Django-like lazy execution"""
+
+    def __len__(self):
+        return self.count()
+
+    def __getitem__(self, n):
+        if isinstance(n, int):
+            return self.execute()[n]
+
+        return super(LazySearch, self).__getitem__(n)
+
+    def full(self):
+        s = self._clone()
+        s._full = True
+        s._fields = ["_id"]
+        return s
+
+    def _clone(self):
+        s = super(LazySearch, self)._clone()
+        s._full = getattr(self, "_full", False)
+        return s
+
+    def execute(self):
+        """
+        Execute the search and return an instance of ``Response`` wrapping all
+        the data.
+        """
+        es = connections.get_connection(self._using)
+
+        if getattr(self, "_full", False) is False:
+            return ShallowResponse(es.search(index=self._index,
+                                                   doc_type=self._doc_type,
+                                                   body=self.to_dict(),
+                                                   **self._params),
+                                         callbacks=self._doc_type_map)
+        else:
+            return FullResponse(es.search(index=self._index,
+                                                doc_type=self._doc_type,
+                                                body=self.to_dict(),
+                                                **self._params))
+
+
+class IndexableManager(models.Manager):
+    """a custom manager class to handle integration of native django models and elasticsearch storage
+    """
+
+    @property
+    def client(self):
+        if not hasattr(self, "_client"):
+            self._client = connections.get_connection("default")
+        return self._client
+    
+
+    def get(self, **kwargs):
+        """gets a specific document from elasticsearch
+
+        :return: the result of the search
+        :rtype: dict
+
+        :keyword id: the id of the model/document
+        :type id: int or str
+
+        :keyword pk: the id of the model/document
+        :type pd: int or str
+        """
+        # get the doc id
+        id = None
+        if "id" in kwargs:
+            id = kwargs["id"]
+            del kwargs["id"]
+        elif "pk" in kwargs:
+            id = kwargs["pk"]
+            del kwargs["pk"]
+        else:
+            raise self.model.DoesNotExist("You must provide an id to find")
+
+        # connect to es and retrieve the document
+        es = connections.get_connection("default")
+
+        doc_type = self.model.mapping.doc_type
+        index = self.model.mapping.index
+        try:
+            doc = es.get(index=index, doc_type=doc_type, id=id, **kwargs)
+        except NotFoundError:
+            message = "Can't find a document for {}, using id {}".format(
+                doc_type, id)
+            raise self.model.DoesNotExist(message)
+
+        # parse and return
+        return self.model.from_es(doc)
+
+    def search(self):
+        model_callbacks = {}
+        indexes = []
+
+        if self.model in indexable_registry.families:
+            # There are child models...
+            for doc_type, cls in indexable_registry.families[self.model].items():
+
+                model_callbacks[doc_type] = cls.from_es
+                if cls.mapping.index not in indexes:
+                    indexes.append(cls.mapping.index)
+        else:
+            # Just this one!
+            model_callbacks[self.model.mapping.doc_type] = self.model.from_es
+            indexes.append(self.model.mapping.index)
+
+        return LazySearch().using(self.client).index(*indexes).doc_type(
+            **model_callbacks)        
+
+    def refresh(self):
+        self.client.indices.refresh(index=self.model.mapping.index)
+
+
+class Indexable(models.Model):
+    """an abstract django model to tie in elasticsearch capabilities
+    """
+
+    class Meta(object):
+        abstract = True
+
+    objects = models.Manager()
+    search_objects = IndexableManager()
+
+    def save(self, index=True, *args, **kwargs):
+        super(Indexable, self).save(*args, **kwargs)
+        if index:
+            self.index()
+
+    def to_dict(self):
+        """converts the django model's fields to an elasticsearch mapping
+
+        :return: an elasticsearch mapping
+        :rtype: elasticsearch_dsl.mapping.Mapping
+        """
+        out = {}
+        for key in self.mapping.properties.properties:
+            # TODO: What if we've mapped the property to a different name? Will we allow that?
+
+            attribute = getattr(self, key)
+
+            field = self.mapping.properties.properties[key]
+
+            # First we check it this is a manager, in which case we have many related objects
+            if isinstance(attribute, models.Manager):
+                if issubclass(attribute.model, Indexable):
+                    out[key] = [obj.to_dict() for obj in attribute.all()]
+                else:
+                    out[key] = list(attribute.values_list("pk", flat=True))
+
+            elif callable(attribute):
+                out[key] = attribute()
+            elif isinstance(attribute, Indexable):
+                out[key] = attribute.to_dict()
+            elif hasattr(field, "to_es"):
+                out[key] = field.to_es(attribute)
+            else:
+                out[key] = attribute
+        return out
+
+    def index(self, refresh=False):
+        """Indexes this object"""
+        es = connections.get_connection("default")
+        es.index(self.mapping.index, self.mapping.doc_type,
+                 id=self.pk,
+                 body=self.to_dict(),
+                 refresh=refresh)
+
+    @classmethod
+    def from_es(cls, hit):
+        doc = hit.copy()
+        klass = shallow_class_factory(cls)
+
+        # We can pass in the entire source, except in the case that we have a many-to-many
+        local_many_to_many_fields = {}
+        for field in cls._meta.local_many_to_many:
+            local_many_to_many_fields[field.get_attname_column()[1]] = field
+
+        to_be_deleted = []
+        for name, value in doc["_source"].items():
+            if name in local_many_to_many_fields:
+                field = local_many_to_many_fields[name]
+                if not hasattr(field.rel.to, "from_es"):
+                    to_be_deleted.append(name)
+
+        for name in to_be_deleted:
+            del doc["_source"][name]
+
+        return klass(**doc["_source"])
+
+    @classmethod
+    def get_base_class(cls):
+        if cls.__bases__:
+            for base in cls.__bases__:
+                if base == Indexable:
+                    return cls
+                elif hasattr(base, "get_base_class"):
+                    base_base = base.get_base_class()
+                    if base_base:
+                        return base_base
+        return None
