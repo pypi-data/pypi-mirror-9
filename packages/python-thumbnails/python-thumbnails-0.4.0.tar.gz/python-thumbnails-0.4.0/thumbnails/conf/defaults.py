f2f60af3157a6a8415bd6abb62cf7937b31729dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:32 +0000

    Add package python-thumbnails/0.4.0/python-thumbnails-0.4.0.tar.gz

---
+++ packages/python-thumbnails/python-thumbnails-0.4.0.tar.gz/python-thumbnails-0.4.0/thumbnails/conf/defaults.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+import os
+
+THUMBNAIL_PATH = os.path.join(os.getcwd(), 'thumbnails-cache')
+THUMBNAIL_URL = '/thumbnails/'
+THUMBNAIL_ENGINE = 'thumbnails.engines.PillowEngine'
+THUMBNAIL_CACHE_BACKEND = 'thumbnails.cache_backends.SimpleCacheBackend'
+THUMBNAIL_CACHE_TIMEOUT = 60 * 60 * 24 * 365
+THUMBNAIL_CACHE_CONNECTION_URI = None
+THUMBNAIL_STORAGE_BACKEND = 'thumbnails.storage_backends.FilesystemStorageBackend'
+
+THUMBNAIL_SCALE_UP = False
+THUMBNAIL_QUALITY = 90
+THUMBNAIL_COLORMODE = 'RGB'
+THUMBNAIL_ALTERNATIVE_RESOLUTIONS = [2]
+
+THUMBNAIL_DUMMY = False
+THUMBNAIL_DUMMY_URL = 'http://puppies.lkng.me/{}x{}'
