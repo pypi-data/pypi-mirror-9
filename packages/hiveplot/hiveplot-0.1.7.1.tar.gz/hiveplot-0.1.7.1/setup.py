3de72db868776bde14c55fbfa44e46b24419d508
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:43 +0000

    Add package hiveplot/0.1.7.1/hiveplot-0.1.7.1.tar.gz

---
+++ packages/hiveplot/hiveplot-0.1.7.1.tar.gz/hiveplot-0.1.7.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup  # Always prefer setuptools over distutils
+
+setup(name='hiveplot',
+	  long_description='A utility for making hive plots in matplotlib.',
+      version='0.1.7.1',
+      py_modules=['hiveplot'],
+      url='https://github.com/ericmjl/hiveplot',
+      author='Eric J. Ma',
+      email='ericmajinglong@gmail.com',
+      maintainer='Eric J. Ma',
+      maintainer_email='ericmajinglong@gmail.com'
+      )
