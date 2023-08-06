0ea22d690b82a89be9b3fd2fef6dcdd8059b5e1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:23:02 +0000

    Add package nesterhyb/1.4.0/nesterhyb-1.4.0.tar.gz

---
+++ packages/nesterhyb/nesterhyb-1.4.0.tar.gz/nesterhyb-1.4.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+        name        = 'nesterhyb',
+        version     = '1.4.0',
+        py_modules  = ['nesterhyb'],
+        author      = 'hybdev',
+        author_email= 'beenyee@163.com',
+        description = 'A simple printer of nested list',
+    )
