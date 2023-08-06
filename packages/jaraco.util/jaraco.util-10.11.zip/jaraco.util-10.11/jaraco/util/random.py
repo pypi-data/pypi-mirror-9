c6c0f2d4b20fd1b859bb009389d95830fcc08002
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:25 +0000

    Add package jaraco.util/10.11/jaraco.util-10.11.zip

---
+++ packages/jaraco.util/jaraco.util-10.11.zip/jaraco.util-10.11/jaraco/util/random.py
@@ -0,0 +1,18 @@
+from __future__ import absolute_import, unicode_literals
+
+import random
+import itertools
+
+import six
+
+def bytes(n):
+	"""
+	Returns n random bytes
+
+	>>> res = list(bytes(5))
+	>>> assert len(res) == 5
+	>>> assert all(len(b) == 1 for b in res)
+	"""
+	bytes = (random.randint(0,255) for i in itertools.count())
+	bytes = itertools.islice(bytes, n)
+	return (six.int2byte(i) for i in bytes)
