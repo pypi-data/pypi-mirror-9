d36cf1b63badde50365427f10a1e7f616be174cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:58 +0000

    Add package merkle-tree/0.1/merkle-tree-0.1.tar.gz

---
+++ packages/merkle-tree/merkle-tree-0.1.tar.gz/merkle-tree-0.1/merkle_tree.py
@@ -0,0 +1,18 @@
+import binascii
+
+from botocore.utils import calculate_tree_hash
+import click
+
+__version__ = '0.1'
+
+@click.command()
+@click.option('--output', type=click.Choice(['ascii', 'binary']),
+              default='ascii', help='output format of Merkle Tree')
+@click.argument('filename', type=click.File('rb'))
+def cli(filename, output):
+    """calculate Merkle Tree
+    """
+    hash_value = calculate_tree_hash(filename)
+    if output == 'binary':
+        hash_value = binascii.unhexlify(hash_value)
+    click.echo(hash_value)
