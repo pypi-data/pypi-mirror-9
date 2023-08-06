568952753446e9478e11f383d4a6174f461a6a22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:07:40 +0000

    Add package django-timestamp-paginator/0.0.1/django-timestamp-paginator-0.0.1.tar.gz

---
+++ packages/django-timestamp-paginator/django-timestamp-paginator-0.0.1.tar.gz/django-timestamp-paginator-0.0.1/django_timestamp_paginator/__init__.py
@@ -0,0 +1,16 @@
+"""
+
+Timestamp Paginator for Django.
+
+Umut Bozkurt - @umutbozkurt
+MIT Licence
+
+"""
+
+
+from django_timestamp_paginator.paginator import TimestampPaginator, Page
+
+__all__ = [
+    'TimestampPaginator',
+    'Page'
+]
