e18747cb72dd94dbf7775a1986fddb298536bf8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:36:14 +0000

    Add package fakeredis/0.6.1/fakeredis-0.6.1.tar.gz

---
+++ packages/fakeredis/fakeredis-0.6.1.tar.gz/fakeredis-0.6.1/setup.py
@@ -0,0 +1,29 @@
+import os
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='fakeredis',
+    version='0.6.1',
+    description="Fake implementation of redis API for testing purposes.",
+    long_description=open(os.path.join(os.path.dirname(__file__),
+                                       'README.rst')).read(),
+    license='BSD',
+    url="https://github.com/jamesls/fakeredis",
+    author='James Saryerwinnie',
+    author_email='js@jamesls.com',
+    py_modules=['fakeredis'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'License :: OSI Approved :: BSD License',
+    ],
+    install_requires=[
+        'redis',
+    ]
+)
+
