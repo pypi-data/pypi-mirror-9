318fa9de0809da307dcf4474ad6d4f5956f719cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:23:26 +0000

    Add package latimes-statestyle/0.1.6/latimes-statestyle-0.1.6.tar.gz

---
+++ packages/latimes-statestyle/latimes-statestyle-0.1.6.tar.gz/latimes-statestyle-0.1.6/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+setup(
+    name='latimes-statestyle',
+    version='0.1.6',
+    description='Standardizes data about U.S. states',
+    author='Ben Welsh',
+    author_email='ben.welsh@latimes.com',
+    packages=find_packages(),
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.1',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
+)
