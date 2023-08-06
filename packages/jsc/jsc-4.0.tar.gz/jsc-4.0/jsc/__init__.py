0d1095808ecb8fbadb28881e68d2460b72d9dfa4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:33 +0000

    Add package jsc/4.0/jsc-4.0.tar.gz

---
+++ packages/jsc/jsc-4.0.tar.gz/jsc-4.0/jsc/__init__.py
@@ -0,0 +1,6 @@
+__version__ = "4.0"
+__description__ = "Jumpstarter Console"
+__long_description__ = """
+Jumpstarter Console - A command line tool for managing your Jumpstarter projects.
+please visit https://github.com/jumpstarter-io/jsc for more information
+"""
---
+++ packages/jsc/jsc-4.0.tar.gz/jsc-4.0/setup.py
@@ -0,0 +1,53 @@
+import os
+import os.path
+import sys
+
+from setuptools import setup, find_packages
+from pip.req import parse_requirements
+import pip.download
+
+from jsc import __version__, __description__, __long_description__
+
+
+def read(fname):
+    path = os.path.join(os.path.dirname(__file__), fname)
+    f = open(path)
+    return f.read()
+
+
+reqs = ['setuptools',
+        'choice',
+        'docopt',
+        'giturlparse.py',
+        'pyparsing',
+        'paramiko',
+        'colorama'
+        ]
+
+
+pyversion = sys.version_info[:2]
+if pyversion < (2, 7) or (3, 0) <= pyversion <= (3, 1):
+    reqs.append('argparse')
+
+setup(
+    name='jsc',
+    version=__version__,
+    packages=find_packages(),
+    author='Jumpstarter',
+    author_email='team@jumpstarter.io',
+    description=__description__,
+    long_description=__long_description__,
+    license='MIT',
+    keywords='jumpstarter js jsc deploy',
+    url="https://github.com/jumpstarter-io/jsc",
+    zip_safe=True,
+    include_package_data=True,
+
+    install_requires=reqs,
+
+    entry_points={
+        'console_scripts': [
+            'jsc = jsc.client:main',
+            ]
+        }
+    )
