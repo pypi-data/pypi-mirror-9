a54aee0190905c3c7ff2bbc1b61e1c109d7e448e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:27 +0000

    Add package django-filepicker/0.2.2/django-filepicker-0.2.2.tar.gz

---
+++ packages/django-filepicker/django-filepicker-0.2.2.tar.gz/django-filepicker-0.2.2/setup.py
@@ -0,0 +1,17 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name='django-filepicker',
+    version='0.2.2',
+    description='Official Filepicker Django Library',
+    author='Filepicker.io',
+    author_email='contact@filepicker.io',
+    url='http://developers.filepicker.io/',
+    packages=['django_filepicker'],
+    install_requires=['django >= 1.3','requests'],
+    license="BSD",
+    zip_safe=False,
+)
