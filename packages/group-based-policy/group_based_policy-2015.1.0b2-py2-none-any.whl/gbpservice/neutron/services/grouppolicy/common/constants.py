b8128875510e832c9bc1993a75af8e25eb238c4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:16 +0000

    Add package group-based-policy/2015.1.0b2/group_based_policy-2015.1.0b2-py2-none-any.whl

---
+++ packages/group-based-policy/group_based_policy-2015.1.0b2-py2-none-any.whl/gbpservice/neutron/services/grouppolicy/common/constants.py
@@ -0,0 +1,29 @@
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+GP_ACTION_ALLOW = 'allow'
+GP_ACTION_REDIRECT = 'redirect'
+
+GP_DIRECTION_IN = 'in'
+GP_DIRECTION_OUT = 'out'
+GP_DIRECTION_BI = 'bi'
+
+GP_NETWORK_SVC_PARAM_TYPE = 'type'
+GP_NETWORK_SVC_PARAM_NAME = 'name'
+GP_NETWORK_SVC_PARAM_VALUE = 'value'
+
+GP_NETWORK_SVC_PARAM_TYPE_IP_SINGLE = 'ip_single'
+GP_NETWORK_SVC_PARAM_TYPE_IP_POOL = 'ip_pool'
+GP_NETWORK_SVC_PARAM_TYPE_STRING = 'string'
+
+GP_NETWORK_SVC_PARAM_VALUE_EXTERNAL_SUBNET = 'external_subnet'
+GP_NETWORK_SVC_PARAM_VALUE_SELF_SUBNET = 'self_subnet'
