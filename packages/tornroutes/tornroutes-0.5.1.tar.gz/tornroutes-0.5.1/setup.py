b2a0d857a70c190336b75f885632d989b034bc79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:45 +0000

    Add package tornroutes/0.5.1/tornroutes-0.5.1.tar.gz

---
+++ packages/tornroutes/tornroutes-0.5.1.tar.gz/tornroutes-0.5.1/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+VERSION = "0.5.1"
+setup(
+    name = 'tornroutes',
+    version = VERSION,
+    description = 'Tornado Web Route Decorator',
+    author = 'Jeremy Kelley',
+    author_email = 'jeremy@33ad.org',
+    url = 'https://github.com/nod/tornroutes',
+    download_url = 'https://github.com/nod/tornroutes/tarball/0.5',
+    license = "http://www.apache.org/licenses/LICENSE-2.0",
+    packages = ['tornroutes'],
+    keywords = ['tornado', 'routes', 'http']
+    )
+
