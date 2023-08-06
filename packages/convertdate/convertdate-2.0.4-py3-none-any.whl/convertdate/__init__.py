23c49146c40fe5e20ce0e1f0caeef39364ee7bfb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:20 +0000

    Add package convertdate/2.0.4/convertdate-2.0.4-py3-none-any.whl

---
+++ packages/convertdate/convertdate-2.0.4-py3-none-any.whl/convertdate/__init__.py
@@ -0,0 +1,34 @@
+# Most of this code is ported from Fourmilab's javascript calendar converter
+# http://www.fourmilab.ch/documents/calendar/
+# which was developed by John Walker
+#
+# The algorithms are believed to be derived from the following source:
+# Meeus, Jean. Astronomical Algorithms . Richmond: Willmann-Bell, 1991. ISBN 0-943396-35-2.
+#    The essential reference for computational positional astronomy.
+#
+
+__version__ = "2.0.3.1"
+
+__all__ = [
+    'holidays', 'astro', 'utils', 'bahai',
+    'french_republican', 'gregorian', 'hebrew',
+    'indian_civil', 'islamic', 'iso',
+    'julian', 'mayan', 'persian', 'mayan',
+    'ordinal',
+]
+
+from . import bahai
+from . import dublin
+from . import french_republican
+from . import gregorian
+from . import hebrew
+from . import holidays
+from . import indian_civil
+from . import islamic
+from . import julian
+from . import julianday
+from . import mayan
+from . import persian
+from . import mayan
+from . import ordinal
+
