c2baf2c169c9b231be2a60e5c8ca28bcd7364952
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:52:59 +0000

    Add package Pimoroni-PiGlow/1.0.1/Pimoroni-PiGlow-1.0.1.tar.gz

---
+++ packages/Pimoroni-PiGlow/Pimoroni-PiGlow-1.0.1.tar.gz/Pimoroni-PiGlow-1.0.1/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+classifiers = ['Development Status :: 4 - Beta',
+               'Operating System :: POSIX :: Linux',
+               'License :: OSI Approved :: MIT License',
+               'Intended Audience :: Developers',
+               'Programming Language :: Python :: 2.6',
+               'Programming Language :: Python :: 2.7',
+               'Programming Language :: Python :: 3',
+               'Topic :: Software Development',
+               'Topic :: System :: Hardware']
+
+setup(name	= 'Pimoroni-PiGlow',
+version 	= '1.0.1',
+author        	= 'Philip Howard',
+author_email	= 'phil@pimoroni.com',
+description	= 'A module to control the PiGlow Raspberry Pi Addon Board',
+long_description= 'A module to control the PiGlow Raspberry Pi Addon Board',
+license	        = 'MIT',
+keywords	= 'Raspberry Pi PiGlow',
+url	        = 'http://www.pimoroni.com',
+classifiers     = classifiers,
+py_modules	= ['piglow'],
+install_requires= ['rpi.gpio >= 0.5.4']
+)
