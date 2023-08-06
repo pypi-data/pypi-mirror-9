e4a730f38efa7c946203bc9492d3a44d4b59a0b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:52 +0000

    Add package helper_uri/0.3/helper_uri-0.3.tar.gz

---
+++ packages/helper_uri/helper_uri-0.3.tar.gz/helper_uri-0.3/setup.py
@@ -0,0 +1,18 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name='helper_uri',
+    version='0.3',
+    url='https://github.com/shuge/helper_uri',
+    license='MIT License',
+    author='Shuge Lee',
+    author_email='shuge.lee@gmail.com',
+    description='URI Helper',
+
+    scripts = [
+        "helper_uri.py",
+    ],
+)
