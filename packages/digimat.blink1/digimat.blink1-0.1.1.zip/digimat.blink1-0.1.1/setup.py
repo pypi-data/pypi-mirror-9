96b27169c8742d64b8aa600b31dabc75f73f9de7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:33 +0000

    Add package digimat.blink1/0.1.1/digimat.blink1-0.1.1.zip

---
+++ packages/digimat.blink1/digimat.blink1-0.1.1.zip/digimat.blink1-0.1.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='digimat.blink1',
+    version='0.1.1',
+    description='Digimat Blink1',
+    namespace_packages=['digimat'],
+    author='Frederic Hess',
+    author_email='fhess@splust.ch',
+    license='PSF',
+    packages=find_packages('src'),
+    package_dir = {'':'src'},
+    install_requires=[
+        'pyusb>=1.0.0b1',
+        'webcolors',
+        'setuptools'
+    ],
+    dependency_links=[
+        ''
+    ],
+    zip_safe=False)
