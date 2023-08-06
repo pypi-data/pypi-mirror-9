f8a5cd6d7789fc9aff81389534e48f7c0b1e948b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:31 +0000

    Add package django-entity-event/0.3.3/django-entity-event-0.3.3.tar.gz

---
+++ packages/django-entity-event/django-entity-event-0.3.3.tar.gz/django-entity-event-0.3.3/setup.py
@@ -0,0 +1,61 @@
+# import multiprocessing to avoid this bug (http://bugs.python.org/issue15881#msg170215)
+import multiprocessing
+assert multiprocessing
+import re
+from setuptools import setup, find_packages
+
+
+def get_version():
+    """
+    Extracts the version number from the version.py file.
+    """
+    VERSION_FILE = 'entity_event/version.py'
+    mo = re.search(r'^__version__ = [\'"]([^\'"]*)[\'"]', open(VERSION_FILE, 'rt').read(), re.M)
+    if mo:
+        return mo.group(1)
+    else:
+        raise RuntimeError('Unable to find version string in {0}.'.format(VERSION_FILE))
+
+
+setup(
+    name='django-entity-event',
+    version=get_version(),
+    description='Newsfeed-style event tracking and subscription management for django-entity.',
+    long_description=open('README.rst').read(),
+    url='https://github.com/ambitioninc/django-entity-event',
+    author='Erik Swanson',
+    author_email='opensource@ambition.com',
+    keywords='',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Framework :: Django',
+        'Framework :: Django :: 1.6',
+        'Framework :: Django :: 1.7',
+    ],
+    license='MIT',
+    install_requires=[
+        'cached-property>=0.1.5',
+        'Django>=1.6',
+        'django-entity>=1.8.2',
+        'jsonfield>=0.9.20',
+        'six'
+    ],
+    tests_require=[
+        'psycopg2',
+        'django-nose',
+        'south>=1.0.2',
+        'mock>=1.0.1',
+        'coverage>=3.7.1',
+        'freezegun',
+        'django-dynamic-fixture'
+    ],
+    test_suite='run_tests.run_tests',
+    include_package_data=True,
+    zip_safe=False,
+)
