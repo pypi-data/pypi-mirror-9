82dda384ebf449b79033f37085c5ee4a518889eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:59 +0000

    Add package nagios-plugin-elasticsearch/1.1.0/nagios-plugin-elasticsearch-1.1.0.tar.gz

---
+++ packages/nagios-plugin-elasticsearch/nagios-plugin-elasticsearch-1.1.0.tar.gz/nagios-plugin-elasticsearch-1.1.0/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+from pip.req import parse_requirements
+
+setup(
+    name="nagios-plugin-elasticsearch",
+    description="An ElasticSearch availability and performance monitoring plugin for Nagios.",
+    version="1.1.0",
+    packages=find_packages(),
+    url="https://github.com/anchor/nagios-plugin-elasticsearch",
+    maintainer="Sharif Olorin",
+    maintainer_email="sio@tesser.org",
+    author="Saj Goonatilleke",
+    author_email="sg@redu.cx",
+    scripts=["check_elasticsearch"],
+    license="MIT",
+    install_requires=[
+      'nagioscheck==0.1.6',
+      'simplejson>=3.4.0',
+      'setuptools>=3.4.0',
+      'pip>=1.4'
+    ],
+    include_package_data=True
+)
