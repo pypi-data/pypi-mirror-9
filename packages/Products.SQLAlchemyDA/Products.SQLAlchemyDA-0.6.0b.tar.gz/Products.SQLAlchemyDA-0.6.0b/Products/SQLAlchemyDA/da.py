8859323f09905cc67f67425b955e67a54dd6e209
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:14 +0000

    Add package Products.SQLAlchemyDA/0.6.0b/Products.SQLAlchemyDA-0.6.0b.tar.gz

---
+++ packages/Products.SQLAlchemyDA/Products.SQLAlchemyDA-0.6.0b.tar.gz/Products.SQLAlchemyDA-0.6.0b/Products/SQLAlchemyDA/da.py
@@ -0,0 +1,424 @@
+"""
+A DA-like integration of SQLAlchemy based on z3c.sqlalchemy
+
+(C) Zope Corporation and Contributors
+Written by Andreas Jung for Haufe Mediengruppe, Freiburg, Germany
+and ZOPYX Ltd. & Co. KG, Tuebingen, Germany
+"""
+
+import os
+import logging
+import random
+import time
+
+from Globals import InitializeClass
+from AccessControl import ClassSecurityInfo
+from AccessControl.Permissions import view_management_screens
+from OFS.SimpleItem import SimpleItem
+from OFS.PropertyManager import PropertyManager
+from Products.PageTemplates.PageTemplateFile import PageTemplateFile
+
+from z3c.sqlalchemy import getSAWrapper, createSAWrapper
+from z3c.sqlalchemy.interfaces import ISQLAlchemyWrapper
+
+
+LOG = logging.getLogger('SQLAlchemyDA')
+
+# maps Python DB-API types to Zope types
+types_mapping = {
+    'DATE': 'd',
+    'TIME': 'd',
+    'DATETIME': 'd',
+    'STRING': 's',
+    'LONGINTEGER': 'i',
+    'INTEGER': 'i',
+    'NUMBER': 'n',
+    'BOOLEAN': 'n',
+    'ROWID': 'i',
+    'BINARY': None,  # ????
+}
+
+
+# Global registry of named SQLAlchemyDA instances
+_da_registry = {}
+
+
+def register_da(name, da_instance):
+    """
+    Register an SQLAlchemy database adapter by name as part of a module
+    level dict.
+
+    Args:
+        name(str): a globally unique name for the given `da_instance`). This
+                   is generally a Zope object id, automatically registered when
+                   instances of `SAWrapper` are initialized.  If this name is
+                   not unique, the most recently registered name will take
+                   effect. This registration API is not designed to be used
+                   with multiple `SAWrapper` instances sharing the same Zope
+                   object ids. An error will not be raised, to prevent problems
+                   for the majority of users who don't make use of this
+                   registration API (and use Acquisition instead).
+        da_instance(`SAWrapper`): a configured instance of
+                                  `Products.SQLAlchemyDA.SAWrapper`
+
+    This might be called early in Zope startup, so this type
+    of registration is necessary instead of a zope.component registration.
+    (The same reason z3c.sqlalchemy uses a module dict for registration)
+
+    Returns:
+        None
+    """
+    _da_registry[name] = da_instance
+
+
+def deregister_da(name):
+    """
+    Remove a named `SAWrapper` instance from the DA registry, if it exists.
+    Either way, the return value is None.
+    """
+    _da_registry.pop(name, None)
+
+
+def lookup_da(name):
+    """
+    Look up and return an `SAWrapper` DA-ish instance registered by name.
+
+    These instances are registered by the `SAWrapper` during initialization
+    of `ZopeWrapper` instances.
+
+    Returns:
+        'SAWrapper' instance.
+    """
+    da = _da_registry.get(name)
+    if not da:
+        raise LookupError("No SAWrapper instance registered under name "
+                          + name)
+    return da
+
+
+def lookup_zope_sa_wrapper(name):
+    """
+    Look up and return a `z3c.sqlalchemy.ZopeWrapper` instance by name.
+
+    This is done by finding the `ZopeWrapper` instance associated with
+    the named `SAWrapper` instance.
+    """
+    da = lookup_da(name)
+    if da:
+        return da._wrapper
+
+
+def clear_da_registry():
+    """
+    Completely empty out the registry of `SAWrapper` instances.
+    """
+    global _da_registry
+
+
+class SAWrapper(SimpleItem, PropertyManager):
+    """ A shim around z3c.sqlalchemy implementing something DA-ish """
+
+    # TODO document any special DA-ish hooks or places where Zope
+    #      automatically makes calls, or at least link to docs on
+    #      what makes it DA-ish. Is there a documented protocol?
+
+    manage_options = ({'label': 'Info', 'action': 'manage_workspace'},) +\
+                     ({'label': 'Test', 'action': 'manage_test'},) + \
+                     PropertyManager.manage_options + \
+                     SimpleItem.manage_options
+    _properties = (
+        {'id': 'dsn', 'type': 'string', 'mode': 'rw', },
+        {'id': 'title', 'type': 'string', 'mode': 'rw'},
+        {'id': 'encoding', 'type': 'string', 'mode': 'rw'},
+        {'id': 'transactional', 'type': 'boolean', 'mode': 'rw'},
+        {'id': 'convert_unicode', 'type': 'boolean', 'mode': 'rw'},
+        {'id': 'quoting_style', 'type': 'selection', 'mode': 'rw',
+                 'select_variable': 'allQuotingStyles'},
+    )
+
+    meta_type = 'SQLAlchemyDA '
+    dsn = ''
+    encoding = 'iso-8859-15'
+    convert_unicode = 0
+    transactional = True
+    quoting_style = 'standard'
+    _isAnSQLConnection = True
+    extra_engine_options = ()
+
+    security = ClassSecurityInfo()
+
+    def __init__(self, id, title=''):
+        self.id = id
+        self.title = title
+        register_da(self.id, self)
+
+    def manage_afterAdd(self, item, container):
+        """ Ensure that a new utility id is assigned after creating
+            or copying an instance.
+        """
+        self._new_utilid()
+        return SimpleItem.manage_afterAdd(self, item, container)
+
+    def _new_utilid(self):
+        """ Assign a new unique utility ID """
+        self.util_id = '%s.%s' % (time.time(), random.random())
+
+    def allQuotingStyles(self):
+        return ('standard', 'no-quote')
+
+    @property
+    def _wrapper(self):
+        if self.dsn:
+            try:
+                return getSAWrapper(self.util_id)
+            except ValueError:
+                return createSAWrapper(
+                            self.dsn,
+                            forZope=True,
+                            transactional=self.transactional,
+                            extension_options={'initial_state': 'invalidated'},
+                            engine_options=self.engine_options,
+                            name=self.util_id)
+        return None
+
+    @property
+    def engine_options(self):
+        engine_options = dict(self.extra_engine_options)
+        engine_options.update(convert_unicode=self.convert_unicode,
+                              encoding=self.encoding)
+        return engine_options
+
+    def add_extra_engine_options(self, engine_options):
+        """ engine_options is a tuple containing additional
+            options for sqlalchemy.create_engine.
+            Say you need to pass some engine options
+            to SQLAlchemy.create_engine::
+            wrapper = SAWrapper(id)
+            wrapper.add_extra_engine_options((('echo', True),
+                                              ('pool_size', 20)))
+        """
+        self.extra_engine_options = engine_options
+
+    security.declareProtected(view_management_screens, 'getInfo')
+    def getInfo(self):
+        """ return a dict with additional information """
+
+        wrapper = self._wrapper
+        if wrapper is not None:
+            d = self._wrapper.__dict__.copy()
+            d['DSN'] = self._wrapper.dsn
+            for k in d.keys()[:]:
+                if k.startswith('_'):
+                    del d[k]
+            return d
+        else:
+            return {}
+
+    def _typesMap(self, proxy):
+        """ Obtain types map from the underlying DB-API. I hope
+            that is portable code.
+        """
+
+        if not hasattr(self, '_v_types_map'):
+            dbapi = self._wrapper.engine.dialect.dbapi
+
+            map = dict()
+            for name in types_mapping.keys():
+                type_obj = getattr(dbapi, name, None)
+                if type_obj:
+                    if hasattr(type_obj, 'values'):
+                        for v in type_obj.values:
+                            map[v] = name
+                    else:
+                        try:
+                            for v in type_obj:
+                                map[v] = name
+                        except TypeError:
+                            # ATT: fix this:->
+                            pass
+
+            self._v_types_map = map
+        return self._v_types_map
+
+    def query(self, query_string, max_rows=None, query_data=None):
+        """ *The* query() method as used by the internal ZSQL
+            machinery.
+        """
+        c = self._wrapper.connection
+        cursor = c.cursor()
+
+        rows = []
+        desc = None
+        nselects = 0
+
+        ts_start = time.time()
+
+        for qs in [x for x in query_string.split('\0') if x]:
+
+            LOG.debug(qs)
+            if query_data:
+                proxy = cursor.execute(qs, query_data)
+            else:
+                proxy = cursor.execute(qs)
+
+            description = cursor.description
+
+            if description is not None:
+                nselects += 1
+
+                if nselects > 1:
+                    raise ValueError("Can't execute multiple SELECTs "
+                                     "within a single query")
+
+                if max_rows:
+                    rows = cursor.fetchmany(max_rows)
+                else:
+                    rows = cursor.fetchall()
+
+                desc = description
+                types_map = self._typesMap(proxy)
+
+        LOG.debug('Execution time: %3.3f seconds' % (time.time() - ts_start))
+
+        if desc is None:
+            return [], None
+
+        items = []
+        for name, type_code, width, internal_size, precision, scale, null_ok in desc:
+
+            items.append(
+                    {'name': name,
+                    'type': types_mapping.get(types_map.get(type_code, None), 's'),
+                    'null': null_ok,
+                    'width': width, })
+
+        return items, rows
+
+    def __call__(self, *args, **kv):
+        return self
+
+    def sql_quote__(self, s):
+        if self.quoting_style == 'standard':
+            if "\'" in s:
+                s = "''".join(s.split("\'"))
+            return "'%s'" % s
+        else:
+            return s
+
+    security.declareProtected(view_management_screens, 'connected')
+    def connected(self):
+        try:
+            return self._wrapper._engine.pool.checkedin() > 0
+        except:
+            return 'n/a'
+
+    security.declareProtected(view_management_screens, 'getPoolSize')
+    def getPoolSize(self):
+        """ """
+        return self._wrapper._engine.pool.size()
+
+    security.declareProtected(view_management_screens, 'getCheckedin')
+    def getCheckedin(self):
+        """ """
+        try:
+            return self._wrapper._engine.pool.checkedin()
+        except:
+            return 'n/a'
+
+    security.declareProtected(view_management_screens, 'manage_start')
+    def manage_start(self, RESPONSE=None):
+        """ start engine """
+        try:
+            self.query('COMMIT')
+            if RESPONSE:
+                msg = 'Database connection opened'
+                RESPONSE.redirect(self.absolute_url() +
+                              '/manage_workspace?manage_tabs_message=%s' % msg)
+        except Exception, e:
+            if RESPONSE:
+                msg = 'Database connection could not be opened (%s)' % e
+                RESPONSE.redirect(self.absolute_url() +
+                              '/manage_workspace?manage_tabs_message=%s' % msg)
+            else:
+                raise
+
+    security.declareProtected(view_management_screens, 'manage_stop')
+    def manage_stop(self, RESPONSE=None):
+        """ close engine """
+        self._wrapper._engine.pool.dispose()
+        if RESPONSE:
+            msg = 'Database connections closed'
+            RESPONSE.redirect(self.absolute_url() +
+                              '/manage_workspace?manage_tabs_message=%s' % msg)
+
+    security.declareProtected(view_management_screens, 'manage_doQuery')
+    def manage_doQuery(self, query):
+        """ perform a query through the ZMI"""
+        return self.query(query)
+
+    security.declareProtected(view_management_screens, 'manage_formatItem')
+    def manage_formatItem(self, s):
+        """ used by query form """
+        if isinstance(s, unicode):
+            return s
+        if isinstance(s, str):
+            return unicode(s, self.encoding, 'ignore')
+        return str(s)
+
+    security.declareProtected(view_management_screens, 'getVersion')
+    def getVersion(self):
+        """ return version.txt """
+        return open(os.path.join(os.path.dirname(__file__),
+                                 'version.txt')).read()
+
+    security.declareProtected(view_management_screens, 'manage_editProperties')
+    def manage_editProperties(self, REQUEST):
+        """ Intercept changed properties in order to perform
+            further actions.
+        """
+        try:
+            # zope 2.10
+            from zope.component import unregisterUtility
+            unregisterUtility(name=self.util_id)
+            deregister_da(name=self.id)
+            self._new_utilid()
+        except ImportError:
+            try:
+                # zope 2.8
+                from zope.component.servicenames import Utilities
+                from zope.app import zapi
+                s = zapi.getGlobalServices().getService(Utilities)
+                s.register((), ISQLAlchemyWrapper, self.util_id, None)
+                self._new_utilid()
+            except:
+                # Zope 2.9 ATT: fix this
+                self._new_utilid()
+
+        return super(SAWrapper, self).manage_editProperties(REQUEST)
+
+    manage_workspace = PageTemplateFile('pt/info', globals(),
+                                        __name__='manage_workspace')
+    manage_test = PageTemplateFile('pt/query', globals(),
+                                        __name__='manage_test')
+
+
+InitializeClass(SAWrapper)
+
+
+def manage_addSAWrapper(self, id, dsn, title, encoding='iso-8859-15',
+                        convert_unicode=0, RESPONSE=None):
+    """ create a new SAWrapper instance """
+    wrapper = SAWrapper(id, title)
+    wrapper.dsn = dsn
+    wrapper.convert_unicode = convert_unicode
+    wrapper.encoding = encoding
+    self._setObject(id, wrapper.__of__(self))
+    if RESPONSE:
+        return RESPONSE.redirect(self._getOb(id).absolute_url()
+                                 + '/manage_workspace')
+    else:
+        return wrapper
+
+manage_addSAWrapperForm = PageTemplateFile('pt/addSAWrapperForm',
+                                           globals(),
+                                           __name__='addSAWrapperForm')
---
+++ packages/Products.SQLAlchemyDA/Products.SQLAlchemyDA-0.6.0b.tar.gz/Products.SQLAlchemyDA-0.6.0b/README.rst
@@ -0,0 +1,158 @@
+Zope ZSQL-SQLAlchemy Integration Wrapper
+========================================
+
+
+About SQLAlchemyDA
+------------------
+
+SQLAlchemyDA is a generic database adapter for Zope ZSQL methods, which are
+an older/legacy SQL templating feature for executing relational database queries
+from with in a Zope to transaction context.
+
+SQLAlchemyDA provides an implementation in the form of a Zope2 "product" which
+wraps `z3c.sqlalchemy <https://pypi.python.org/pypi/zope.sqlalchemy>`_, so that
+database connections are installable as objects in the Zope2 ZMI. Such
+connection objects can be set up to connect to any kind of database backend
+supported by SQLAlchemy using a database URI, such as Postgres, MySQL, Oracle,
+SQLite, MS-SQL, Firebird, Informix. However, some of these database backends
+have not been tested with the SQLAlchemyDA, so your mileage may vary.
+
+In addition to ZSQL support, the SQLAlchemyDA makes it possible to use the
+standard SQLAlchemy API within a Zope2 context and participate in Zope
+transactions.
+
+However, if you do not require ZSQL support, and only wish to call 'normal'
+SQLAlchemy APIs within Zope transactions, this package adds no value. Instead,
+you would be better off trying out `zope.sqlalchemy`, as recommended in the
+`Zope2 book chapter on relational database
+connectivity <http://docs.zope.org/zope2/zope2book/RelationalDatabases.html>`_.
+
+
+Requirements:
+-------------
+
+- Zope 2.10+
+- SQLAlchemy >= 0.5.0 (+ database specific low-level Python drivers)
+- z3c.sqlalchemy >= 1.2.0
+
+
+Installation:
+-------------
+
+- download and install SQLAlchemy as egg or from the sources
+  from Cheeseshop (easy_install sqlalchemy). See
+    
+    http://www.sqlalchemy.org
+
+    for details
+
+- download and install z3c.sqlalchemy as egg or from the sources 
+  from Cheeseshop (easy_install z3c.sqlalchemy). See
+
+    http://cheeseshop.python.org/pypi/z3c.sqlalchemy/
+
+  for details.
+
+- unpack the archive containing SQLAlchemyDA inside the "Products"
+  directory of your Zope instance home.
+
+- after restarting Zope you go to the ZMI and create an instance of
+  "SQLAlchemyDA" (as you would create some DA instance)
+
+- click on the new created SQLAlchemyDA instance within the ZMI
+  and configure your database connection through the "Properties" tab.
+  The connection parameter 'dsn' must be specified as a valid SQLAlchemy DSN 
+  like
+
+         <dbschema>://<username>:<password>@<hostname>/<databasename>
+
+    Example:
+        
+        postgres://admin:123@localhost:5432/TestDB
+
+- ZSQL methods should see the new DA through the selection widget of available
+  database adapters
+
+- NOTE: you must have the low-level Python DB drivers installed in order to 
+  access a particular database. See 
+
+        http://www.sqlalchemy.org/docs/dbengine.html#dbengine_supported
+
+  for details.
+
+
+Configuration of SQLAlchemyDA:
+------------------------------
+
+- 'dsn' - SQLAlchemy compliant Database Set Name (see www.sqlalchemy.org/docs)
+
+- 'transactional' - uncheck this property if you are working with a non-transactional
+   database like older versions of MySQL. Uncheck this property *only* if you see any
+   commit() related error. Otherwise leave this property checked. Changing this
+   property *requires* a Zope restart.
+
+- 'quoting_style' - affects how strings are quoted in SQL. By default 'standard' 
+   quotes strings correctly. Setting the value to 'no-quote' might solve quoting issues
+   with some databases.
+
+
+Using SQLAlchemyDA:
+-------------------
+
+SQLAlchemyDA works as a database adapter as documented within "The Zope Book"
+
+http://www.plope.com/Books/2_7Edition/RelationalDatabases.stx
+
+and can be used like any other DA together with ZSQL methods.
+
+
+Tested with databases:
+----------------------
+
+- Postgres 7.4, 8.0-8.2
+- SQLite 3.3.X
+- Oracle 10g
+- MySQL is *only* supported for MySQL databases with transaction support.
+  (see also z3c/sqlalchemy/README.txt)      
+ 
+
+Known issues:
+-------------
+
+""" Database connection could not be opened ((ProgrammingError) (1064, You
+have an error in your SQL syntax near 'COMMIT .
+"""
+
+This bug might appear with older MySQL versions when opening/closing
+the connections manually through the ZMI. It should not affect the
+functionality of SQLAlchemyDA.
+    
+
+Author
+------
+
+SQLAlchemyDA was written by Andreas Jung for Haufe Mediengruppe, Freiburg,
+Germany and ZOPYX Ltd. & Co. KG, Tuebingen, Germany.
+
+
+License
+-------
+
+SQLAlchemyDA is  licensed under the Zope Public License 2.1. 
+See LICENSE.txt.
+
+
+Contact
+-------
+
+| Sheila Allen
+| E-mail: sallen at zeomega.com
+
+
+Credits
+-------
+
+Parts of the SQLAlchemyDA V 0.3.X development has been sponsored by Wayne
+Volkmuth (renovis.com).
+
+More recent SQLAlchemy support and maintenance sponsored by ZeOmega.com.
