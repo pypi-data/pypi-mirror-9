07247cf004582ed4e251ed611050b328fecf1fc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:36 +0000

    Add package GDM/0.1/GDM-0.1.tar.gz

---
+++ packages/GDM/GDM-0.1.tar.gz/GDM-0.1/gdm/commands.py
@@ -0,0 +1,71 @@
+"""Functions to manage the installation of dependencies."""
+
+import os
+import shutil
+
+from . import common
+from .config import load, install_deps
+
+log = common.logger(__name__)
+
+
+def install(root=None):
+    """Install dependencies for a project."""
+    root = _find_root(root)
+
+    log.info("installing dependencies...")
+    count = install_deps(root)
+    if count == 1:
+        log.info("installed 1 dependency")
+    elif count > 1:
+        log.info("installed %s dependencies", count)
+    else:
+        log.warn("no dependencies installed")
+
+    return count
+
+
+def uninstall(root=None):
+    """Uninstall dependencies for a project."""
+    root = _find_root(root)
+
+    log.info("uninstalling dependencies...")
+    config = load(root)
+    if config:
+        if os.path.exists(config.location):
+            log.debug("deleting '%s'...", config.location)
+            shutil.rmtree(config.location)
+        log.info("dependencies uninstalled")
+        return True
+    else:
+        log.warn("no dependencies to uninstall")
+        return False
+
+
+def _find_root(root, cwd=None):
+    if cwd is None:
+        cwd = os.getcwd()
+
+    if root:
+        root = os.path.abspath(root)
+        log.info("specified root: %s", root)
+    else:
+        path = cwd
+        prev = None
+
+        log.info("searching for root...")
+        while path != prev:
+            log.debug("path: %s", path)
+            if '.git' in os.listdir(path):
+                root = path
+                break
+            prev = path
+            path = os.path.dirname(path)
+
+        if root:
+            log.info("found root: %s", root)
+        else:
+            root = cwd
+            log.warning("no root found, default: %s", root)
+
+    return root
