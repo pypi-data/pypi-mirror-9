f1e19340fc0919921559a3548a4c395e22912d5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:26 +0000

    Add package OrbitalPy/0.5.0/OrbitalPy-0.5.0.tar.gz

---
+++ packages/OrbitalPy/OrbitalPy-0.5.0.tar.gz/OrbitalPy-0.5.0/orbital/__init__.py
@@ -0,0 +1,8 @@
+# encoding: utf-8
+from __future__ import absolute_import, division, print_function
+from orbital.elements import KeplerianElements
+
+__author__ = 'Frazer McLean <frazer@frazermclean.co.uk>'
+__version__ = '0.5.0'
+__license__ = 'MIT'
+__description__ = 'High level orbital mechanics package.'
---
+++ packages/OrbitalPy/OrbitalPy-0.5.0.tar.gz/OrbitalPy-0.5.0/setup.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+import re
+import sys
+
+from setuptools import setup, Command, find_packages
+
+
+INIT_FILE = 'orbital/__init__.py'
+init_data = open(INIT_FILE).read()
+
+metadata = dict(re.findall("__([a-z]+)__ = '([^']+)'", init_data))
+
+AUTHOR_EMAIL = metadata['author']
+VERSION = metadata['version']
+LICENSE = metadata['license']
+DESCRIPTION = metadata['description']
+
+AUTHOR, EMAIL = re.match(r'(.*) <(.*)>', AUTHOR_EMAIL).groups()
+
+requires = ['numpy', 'scipy', 'astropy', 'matplotlib']
+
+
+class PyTest(Command):
+    """Allow 'python setup.py test' to run without first installing pytest"""
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        import subprocess
+        import sys
+        errno = subprocess.call([sys.executable, 'runtests.py'])
+        raise SystemExit(errno)
+
+setup(name='OrbitalPy',
+      version=VERSION,
+      description=DESCRIPTION,
+      long_description=open('README').read(),
+      author=AUTHOR,
+      author_email=EMAIL,
+      url='https://github.com/RazerM/orbital',
+      packages=find_packages(),
+      cmdclass={'test': PyTest},
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Intended Audience :: Science/Research',
+          'Topic :: Scientific/Engineering :: Astronomy',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.0',
+          'Programming Language :: Python :: 3.1',
+          'Programming Language :: Python :: 3.2',
+          'Programming Language :: Python :: 3.3',
+          'Programming Language :: Python :: 3.4'
+      ],
+      license=LICENSE,
+      install_requires=requires)
