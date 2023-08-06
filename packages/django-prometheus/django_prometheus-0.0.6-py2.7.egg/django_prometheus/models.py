ccddb5b4cd2d18d30b2bc55e90d72de5f7f0a411
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:11 +0000

    Add package django-prometheus/0.0.6/django_prometheus-0.0.6-py2.7.egg

---
+++ packages/django-prometheus/django_prometheus-0.0.6-py2.7.egg/django_prometheus/models.py
@@ -0,0 +1,6 @@
+# from prometheus_client import Counter, Histogram
+# from django_prometheus.utils import Time, TimeSince, PowersOf
+# import django_prometheus.exports
+
+class MonitoredModel(object):
+    pass
