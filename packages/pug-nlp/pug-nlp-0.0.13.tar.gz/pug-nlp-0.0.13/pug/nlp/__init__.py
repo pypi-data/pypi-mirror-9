0f4848dcc4fdaca14e8362e3212079b388496732
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:46 +0000

    Add package pug-nlp/0.0.13/pug-nlp-0.0.13.tar.gz

---
+++ packages/pug-nlp/pug-nlp-0.0.13.tar.gz/pug-nlp-0.0.13/pug/nlp/__init__.py
@@ -0,0 +1,19 @@
+# import util
+# import db
+# import charlist
+# import env
+# import http
+# import plot
+# import crypto
+# import words
+
+__all__ = [
+    'util',
+    'charlist', 
+    'db',
+    'env',
+    'http',
+    'plot',
+    'crypto',
+    'words',
+    ]
