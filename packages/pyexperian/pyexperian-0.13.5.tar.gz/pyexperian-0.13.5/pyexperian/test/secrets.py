1acee2354b91feb42f0681d421f3f80d84132a4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:42 +0000

    Add package pyexperian/0.13.5/pyexperian-0.13.5.tar.gz

---
+++ packages/pyexperian/pyexperian-0.13.5.tar.gz/pyexperian-0.13.5/pyexperian/test/secrets.py
@@ -0,0 +1,19 @@
+
+USER_ID = 'ableinc_demo'
+USER_PW = 'qjhLgSVEqoKnhF7'
+EAI = 'BJZAATKP'
+VENDOR_NUMBER = 'BIS079'
+PREAMBLE = ''
+SUB_CODE = '110860'
+DB_HOST = 'BISTESTP'
+TEST_SUB_CODE = 'TBNN0110860'
+ECALS_URL = 'http://www.experian.com/lookupServlet1?lookupServiceName=AccessPoint&lookupServiceVersion=1.0&serviceName=NetConnectDemo&serviceVersion=2.0&responseType=text/plain'
+
+config = {
+    'user_id': USER_ID,
+    'user_pw': USER_PW,
+    'eai': EAI,
+    'vendor_number': VENDOR_NUMBER,
+    'sub_code': SUB_CODE,
+    'db_host': DB_HOST
+}
