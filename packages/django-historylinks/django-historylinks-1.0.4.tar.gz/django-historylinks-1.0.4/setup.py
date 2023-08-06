e128a8cdc4e1d4aeff4c1f696253c0966710a542
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:07:54 +0000

    Add package django-historylinks/1.0.4/django-historylinks-1.0.4.tar.gz

---
+++ packages/django-historylinks/django-historylinks-1.0.4.tar.gz/django-historylinks-1.0.4/setup.py
@@ -0,0 +1,31 @@
+from distutils.core import setup
+
+
+setup(
+    name = "django-historylinks",
+    version = "1.0.4",
+    description = "Automatic SEO-friendly HTTP 301 redirects if the URL of a Django model changes.",
+    long_description = open("README.md").read(),
+    author = "Dave Hall",
+    author_email = "dave@etianen.com",
+    url = "http://github.com/etianen/django-historylinks",
+    zip_safe = False,
+    packages = [
+        "historylinks",
+        "historylinks.management",
+        "historylinks.management.commands",
+        "historylinks.migrations",
+    ],
+    package_dir = {
+        "": "src",
+    },
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Framework :: Django",
+    ],
+)
---
+++ packages/django-historylinks/django-historylinks-1.0.4.tar.gz/django-historylinks-1.0.4/src/historylinks/tests.py
@@ -0,0 +1,56 @@
+from django.db import models
+from django.test import TestCase
+
+import historylinks
+from historylinks.registration import RegistrationError
+
+
+class HistoryLinkTestModel(models.Model):
+    
+    slug = models.SlugField(
+        unique = True,
+    )
+    
+    def get_absolute_url(self):
+        return u"/{slug}/".format(slug=self.slug)
+    
+    class Meta:
+        app_label = "auth"  # Hack: Cannot use an app_label that is under South control, due to http://south.aeracode.org/ticket/520
+
+
+class RegistrationTest(TestCase):
+    
+    def testRegistration(self):
+        # Register the model and test.
+        historylinks.register(HistoryLinkTestModel)
+        self.assertTrue(historylinks.is_registered(HistoryLinkTestModel))
+        self.assertRaises(RegistrationError, lambda: historylinks.register(HistoryLinkTestModel))
+        self.assertTrue(HistoryLinkTestModel in historylinks.get_registered_models())
+        self.assertTrue(isinstance(historylinks.get_adapter(HistoryLinkTestModel), historylinks.HistoryLinkAdapter))
+        # Unregister the model and text.
+        historylinks.unregister(HistoryLinkTestModel)
+        self.assertFalse(historylinks.is_registered(HistoryLinkTestModel))
+        self.assertRaises(RegistrationError, lambda: historylinks.unregister(HistoryLinkTestModel))
+        self.assertTrue(HistoryLinkTestModel not in historylinks.get_registered_models())
+        self.assertRaises(RegistrationError, lambda: isinstance(historylinks.get_adapter(HistoryLinkTestModel)))
+        
+        
+class HistoryLinkRedirectTest(TestCase):
+    
+    def setUp(self):
+        historylinks.register(HistoryLinkTestModel)
+        self.obj = HistoryLinkTestModel.objects.create(slug="foo")
+        self.obj.slug = "bar"
+        self.obj.save()
+        
+    def testRedirectsToNewURL(self):
+        # Try a redirect.
+        response = self.client.get("/foo/")
+        self.assertEqual(response.status_code, 301)
+        self.assertEqual(response["Location"], "http://testserver/bar/")
+        # Try a 404.
+        response = self.client.get("/baz/")
+        self.assertEqual(response.status_code, 404)
+        
+    def tearDown(self):
+        historylinks.unregister(HistoryLinkTestModel)
