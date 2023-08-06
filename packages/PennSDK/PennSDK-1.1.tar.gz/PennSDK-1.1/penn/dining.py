18dac88adb0e01ddb84a6eb3d7af0e032d037466
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:02 +0000

    Add package PennSDK/1.1/PennSDK-1.1.tar.gz

---
+++ packages/PennSDK/PennSDK-1.1.tar.gz/PennSDK-1.1/penn/dining.py
@@ -0,0 +1,90 @@
+"""A module for consuming the Penn Dining API"""
+from os import path
+import requests
+from base import WrapperBase
+import json
+
+
+BASE_URL = "https://esb.isc-seo.upenn.edu/8091/open_data/dining/"
+ENDPOINTS = {
+    'MENUS': BASE_URL + 'menus',
+    'VENUES': BASE_URL + 'venues',
+}
+
+# Normalization for dining menu data
+def normalize_weekly(data):
+    if not "tblMenu" in data["result_data"]["Document"]:
+        data["result_data"]["Document"]["tblMenu"] = []
+    if isinstance(data["result_data"]["Document"]["tblMenu"], dict):
+        data["result_data"]["Document"]["tblMenu"] = [data["result_data"]["Document"]["tblMenu"]]
+    for day in data["result_data"]["Document"]["tblMenu"]:
+        if isinstance(day["tblDayPart"], dict):
+            day["tblDayPart"] = [day["tblDayPart"]]
+        for meal in day["tblDayPart"]:
+            if isinstance(meal["tblStation"], dict):
+                meal["tblStation"] = [meal["tblStation"]]
+            for station in meal["tblStation"]:
+                if isinstance(station["tblItem"], dict):
+                    station["tblItem"] = [station["tblItem"]]
+    return data
+
+
+
+
+class Dining(WrapperBase):
+    """The client for the Registrar. Used to make requests to the API.
+
+    :param bearer: The user code for the API
+    :param token: The password code for the API
+
+    Usage::
+
+      >>> from penn import Dining
+      >>> din = Dining('MY_USERNAME_TOKEN', 'MY_PASSWORD_TOKEN')
+    """
+    def venues(self):
+        """Get a list of all venue objects.
+
+          >>> venues = din.venues()
+        """
+        response = self._request(ENDPOINTS['VENUES'])
+        # Normalize `dateHours` to array
+        for venue in response["result_data"]["document"]["venue"]:
+            if isinstance(venue.get("dateHours"), dict):
+                venue["dateHours"] = [venue["dateHours"]]
+        return response
+
+    def menu_daily(self, building_id):
+        """Get a menu object corresponding to the daily menu for the
+        venue with building_id.
+
+        :param building_id:
+          A string representing the id of a building, e.g. "abc".
+
+
+        >>> commons_today = din.menu_daily("593")
+        """
+        response = self._request(
+            path.join(ENDPOINTS['MENUS'], 'daily', str(building_id))
+        )
+        # Normalize `tblDayPart` and `tblItem` to array
+        meals = response["result_data"]["Document"]["tblMenu"]["tblDayPart"]
+        if isinstance(meals, dict):
+            response["result_data"]["Document"]["tblMenu"]["tblDayPart"] = [meals]
+        for meal in response["result_data"]["Document"]["tblMenu"]["tblDayPart"]:
+            for station in meal["tblStation"]:
+                if isinstance(station["tblItem"], dict):
+                    station["tblItem"] = [station["tblItem"]]
+        return response
+
+    def menu_weekly(self, building_id):
+        """Get an array of menu objects corresponding to the weekly menu for the
+        venue with building_id.
+
+        :param building_id:
+            A string representing the id of a building, e.g. "abc".
+
+        >>> commons_week = din.menu_weekly("593")
+        """
+        response = self._request(path.join(ENDPOINTS['MENUS'], 'weekly', str(building_id)))
+        return normalize_weekly(response)
---
+++ packages/PennSDK/PennSDK-1.1.tar.gz/PennSDK-1.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+
+setup(
+    name='PennSDK',
+    description='Python tools for building Penn-related applications',
+    url='https://github.com/pennappslabs/penn-sdk-python',
+    author='Penn Labs',
+    author_email='pennappslabs@gmail.com',
+    version='1.1',
+    packages=['penn'],
+    license='MIT',
+    package_data = {
+        # If any package contains *.txt or *.rst files, include them:
+        '': ['*.txt', '*.rst'],
+    },
+    long_description=open('./README.rst').read(),
+    install_requires=[
+        'requests==2.4.3',
+        'beautifulsoup4==4.3.2',
+        'html5lib==0.999'
+    ]
+)
