ab88f74279cf45c713db3ed9df787254af7d45e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:37 +0000

    Add package jam/0.0.1/jam-0.0.1.tar.gz

---
+++ packages/jam/jam-0.0.1.tar.gz/jam-0.0.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name='jam',
+    version='0.0.1',
+    description="Jonathan's assorted music utilities",
+    author='Jonathan Marmor',
+    author_email='jmarmor@gmail.com',
+    url='https://github.com/jonathanmarmor/jam',
+    packages=['jam'],
+    long_description="""Jonathan's assorted music utilities.""",
+    classifiers=[
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Programming Language :: Python",
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+    ],
+    keywords='music',
+    license='GPL',
+    install_requires=[
+        'matplotlib',
+    ],
+)
