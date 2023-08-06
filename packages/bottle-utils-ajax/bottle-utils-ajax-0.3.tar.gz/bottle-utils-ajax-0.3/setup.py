314e97672b470624bdf2609f4740a7196e4e4aa5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:17 +0000

    Add package bottle-utils-ajax/0.3/bottle-utils-ajax-0.3.tar.gz

---
+++ packages/bottle-utils-ajax/bottle-utils-ajax-0.3.tar.gz/bottle-utils-ajax-0.3/setup.py
@@ -0,0 +1,35 @@
+import os
+from setuptools import setup, find_packages
+
+import bottle_utils
+
+def read(fname):
+    """ Return content of specified file """
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+VERSION = bottle_utils.__version__
+
+setup(
+    name='bottle-utils-ajax',
+    version=VERSION,
+    author='Outernet Inc',
+    author_email='branko@outernet.is',
+    description=('AJAX utilities for developing apps with Bottle web framework'),
+    license='BSD',
+    keywords='bottle utils ajax',
+    url='http://outernet-project.github.io/bottle-utils/',
+    packages=find_packages(),
+    long_description=read('README.rst'),
+    install_requires=[
+        'bottle==0.12.7',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Topic :: Utilities',
+        'Framework :: Bottle',
+        'Environment :: Web Environment',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+    ],
+)
