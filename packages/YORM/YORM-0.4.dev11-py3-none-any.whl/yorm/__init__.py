7467b9fbb14cac57b562f7eada0e4abac148f258
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:07:46 +0000

    Add package YORM/0.4.dev11/YORM-0.4.dev11-py3-none-any.whl

---
+++ packages/YORM/YORM-0.4.dev11-py3-none-any.whl/yorm/__init__.py
@@ -0,0 +1,23 @@
+"""Package for YORM."""
+
+import sys
+
+__project__ = 'YORM'
+__version__ = '0.4dev11'
+
+VERSION = __project__ + '-' + __version__
+
+PYTHON_VERSION = 3, 3
+
+if not sys.version_info >= PYTHON_VERSION:  # pragma: no cover (manual test)
+    exit("Python {}.{}+ is required.".format(*PYTHON_VERSION))
+
+try:
+    from . import base, converters
+    from .utilities import UUID
+    from .utilities import sync, sync_object, sync_instances, attr
+    from .utilities import update, update_object, update_file
+    from .base.mappable import Mappable
+    from .base.convertible import Convertible
+except ImportError:  # pragma: no cover (manual test)
+    pass
