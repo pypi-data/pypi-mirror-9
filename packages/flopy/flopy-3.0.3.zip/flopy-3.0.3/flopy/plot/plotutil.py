9bfe86faec3601708eccd2dc0d42a58b2594a291
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:20 +0000

    Add package flopy/3.0.3/flopy-3.0.3.zip

---
+++ packages/flopy/flopy-3.0.3.zip/flopy-3.0.3/flopy/plot/plotutil.py
@@ -0,0 +1,359 @@
+"""
+Module to post-process MODFLOW binary output.  The module contains one
+important classes that can be accessed by the user.
+
+*  SwiConcentration (Process Zeta results to concentrations)
+
+"""
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.colors
+
+def rotate(x, y, theta, xorigin=0., yorigin=0.):
+    """
+    Given x and y array-like values calculate the rotation about an
+    arbitrary origin and then return the rotated coordinates.  theta is in
+    radians.
+
+    """
+    xrot = xorigin + np.cos(theta) * (x - xorigin) - np.sin(theta) * (y - yorigin)
+    yrot = yorigin + np.sin(theta) * (x - xorigin) + np.cos(theta) * (y - yorigin)
+    return xrot, yrot
+
+bc_color_dict = {'default': 'black', 'WEL': 'red', 'DRN': 'yellow',
+                 'RIV': 'green', 'GHB': 'cyan'}
+
+class MapPlanView(object):
+    """
+    Class to create a map of the model.
+
+    Parameters
+    ----------
+    ax : matplotlib.pyplot axis
+        The plot axis.  If not provided it, plt.gca() will be used.
+    dis : flopy discretization object
+    layer : int
+        Layer to plot.  Default is 0.
+    xul : float
+        x coordinate for upper left corner
+    yul : float
+        y coordinate for upper left corner
+    rotation : float
+        Angle of grid rotation around the upper left corner.  A positive value
+        indicates clockwise rotation.  Angles are in degrees.
+    extent : tuple of floats
+        (xmin, xmax, ymin, ymax) will be used to specify axes limits.  If None
+        then these will be calculated based on grid, coordinates, and rotation
+    """
+    def __init__(self, ax=None, ml=None, dis=None, layer=0, xul=0., yul=0.,
+                 rotation=0., extent=None):
+        self.ml = ml
+        if dis is None:
+            if ml is None:
+                raise Exception('Cannot find discretization package')
+            else:
+                self.dis = ml.get_package('DIS')
+        else:
+            self.dis = dis
+
+        if ax is None:
+            self.ax = plt.gca()
+        else:
+            self.ax = ax
+        self.layer = layer
+        self.xul = xul
+        self.yul = yul
+        self.rotation = -rotation * np.pi / 180.
+        self.xedge = self.get_xedge_array()
+        self.yedge = self.get_yedge_array()
+        if extent is None:
+            self.extent = self.get_extent()
+        else:
+            self.extent = extent
+        return
+
+    def plot_array(self, a, **kwargs):
+        """
+        Plot the array
+        """
+        # Check array dimension
+        if a.ndim == 3:
+            plotarray = a[self.layer, :, :]
+        elif a.ndim == 2:
+            plotarray = a
+        else:
+            raise Exception('Array must be of dimension 2 or 3')
+
+        xgrid, ygrid = np.meshgrid(self.xedge, self.yedge)
+        xgrid, ygrid = rotate(xgrid, ygrid, self.rotation, 0, self.yedge[0])
+        xgrid += self.xul
+        ygrid += self.yul - self.yedge[0]
+        quadmesh = plt.pcolormesh(xgrid, ygrid, plotarray, **kwargs)
+        return quadmesh
+
+    def plot_ibound(self, ibound, color_noflow='black', color_ch='blue'):
+        # make plot array with 0 = active, 1 = noflow, 2 = constant head
+        plotarray = np.zeros(ibound.shape, dtype=np.int)
+        idx1 = (ibound == 0)
+        idx2 = (ibound < 0)
+        plotarray[idx1] = 1
+        plotarray[idx2] = 2
+        plotarray = np.ma.masked_equal(plotarray, 0)
+        cmap = matplotlib.colors.ListedColormap(['0', color_noflow, color_ch])
+        bounds=[0, 1, 2, 3]
+        norm = matplotlib.colors.BoundaryNorm(bounds, cmap.N)
+        quadmesh = self.plot_array(plotarray, cmap=cmap, norm=norm)
+        return quadmesh
+
+    def plot_grid(self, **kwargs):
+        """
+        Plot the grid.
+
+        Parameters
+        ----------
+            kwargs : axes, colors.  The remaining kwargs are passed into the
+                the LineCollection constructor.
+
+        Returns
+        -------
+            lc : LineCollection
+
+        """
+        if 'axes' in kwargs:
+            ax = kwargs.pop('axes')
+        else:
+            ax = self.ax
+
+        if 'colors' not in kwargs:
+            kwargs['colors'] = '0.5'
+
+        lc = self.get_grid_line_collection(**kwargs)
+        ax.add_collection(lc)
+        ax.set_xlim(self.extent[0], self.extent[1])
+        ax.set_ylim(self.extent[2], self.extent[3])
+        return lc
+
+    def plot_bc(self, ftype=None, package=None, kper=0, color=None, **kwargs):
+        """
+        Plot a boundary locations for a flopy model
+
+        """
+        # Find package to plot
+        if package is not None:
+            p = package
+        elif self.ml is not None:
+            if ftype is None:
+                raise Exception('ftype not specified')
+            p = self.ml.get_package(ftype)
+        else:
+            raise Exception('Cannot find package to plot')
+
+        # Get the list data
+        try:
+            mflist = p.stress_period_data[kper]
+        except:
+            raise Exception('Not a list-style boundary package')
+
+        # Plot the list locations
+        plotarray = np.zeros(self.dis.botm.shape, dtype=np.int)
+        idx = [mflist['k'], mflist['i'], mflist['j']]
+        plotarray[idx] = 1
+        plotarray = np.ma.masked_equal(plotarray, 0)
+        if color is None:
+            if ftype in bc_color_dict:
+                c = bc_color_dict[ftype]
+            else:
+                c = bc_color_dict['default']
+        else:
+            c = color
+        cmap = matplotlib.colors.ListedColormap(['0', c])
+        bounds=[0, 1, 2]
+        norm = matplotlib.colors.BoundaryNorm(bounds, cmap.N)
+        quadmesh = self.plot_array(plotarray, cmap=cmap, norm=norm, **kwargs)
+        return quadmesh
+
+    def get_grid_line_collection(self, **kwargs):
+        """
+        Get a LineCollection of the grid
+        """
+        from matplotlib.collections import LineCollection
+        xmin = self.xedge[0]
+        xmax = self.xedge[-1]
+        ymin = self.yedge[-1]
+        ymax = self.yedge[0]
+        linecol = []
+        # Vertical lines
+        for j in xrange(self.dis.ncol + 1):
+            x0 = self.xedge[j]
+            x1 = x0
+            y0 = ymin
+            y1 = ymax
+            x0r, y0r = rotate(x0, y0, self.rotation, 0, self.yedge[0])
+            x0r += self.xul
+            y0r += self.yul - self.yedge[0]
+            x1r, y1r = rotate(x1, y1, self.rotation, 0, self.yedge[0])
+            x1r += self.xul
+            y1r += self.yul - self.yedge[0]
+            linecol.append(((x0r, y0r), (x1r, y1r)))
+
+        #horizontal lines
+        for i in xrange(self.dis.nrow + 1):
+            x0 = xmin
+            x1 = xmax
+            y0 = self.yedge[i]
+            y1 = y0
+            x0r, y0r = rotate(x0, y0, self.rotation, 0, self.yedge[0])
+            x0r += self.xul
+            y0r += self.yul - self.yedge[0]
+            x1r, y1r = rotate(x1, y1, self.rotation, 0, self.yedge[0])
+            x1r += self.xul
+            y1r += self.yul - self.yedge[0]
+            linecol.append(((x0r, y0r), (x1r, y1r)))
+
+        lc = LineCollection(linecol, **kwargs)
+        return lc
+
+    def get_xedge_array(self):
+        """
+        Return a numpy one-dimensional float array that has the cell edge x
+        coordinates for every cell in the grid.  Array is of size (ncol + 1)
+
+        """
+        xedge = np.concatenate(([0.], np.add.accumulate(self.dis.delr.array)))
+        return xedge
+
+    def get_yedge_array(self):
+        """
+        Return a numpy one-dimensional float array that has the cell edge y
+        coordinates for every cell in the grid.  Array is of size (nrow + 1)
+
+        """
+        length_y = np.add.reduce(self.dis.delc.array)
+        yedge = np.concatenate(([length_y], length_y -
+                             np.add.accumulate(self.dis.delc.array)))
+        return yedge
+
+    def get_extent(self):
+        """
+        Get the extent of the rotated and offset grid
+
+        Return (xmin, xmax, ymin, ymax)
+
+        """
+        x0 = self.xedge[0]
+        x1 = self.xedge[-1]
+        y0 = self.yedge[0]
+        y1 = self.yedge[-1]
+
+        # upper left point
+        x0r, y0r = rotate(x0, y0, self.rotation, 0, self.yedge[0])
+        x0r += self.xul
+        y0r += self.yul - self.yedge[0]
+
+        # upper right point
+        x1r, y1r = rotate(x1, y0, self.rotation, 0, self.yedge[0])
+        x1r += self.xul
+        y1r += self.yul - self.yedge[0]
+
+        # lower right point
+        x2r, y2r = rotate(x1, y1, self.rotation, 0, self.yedge[0])
+        x2r += self.xul
+        y2r += self.yul - self.yedge[0]
+
+        # lower left point
+        x3r, y3r = rotate(x0, y1, self.rotation, 0, self.yedge[0])
+        x3r += self.xul
+        y3r += self.yul - self.yedge[0]
+
+        xmin = min(x0r, x1r, x2r, x3r)
+        xmax = max(x0r, x1r, x2r, x3r)
+        ymin = min(y0r, y1r, y2r, y3r)
+        ymax = max(y0r, y1r, y2r, y3r)
+
+        return (xmin, xmax, ymin, ymax)
+
+
+class SwiConcentration():
+    """
+    The binary_header class is a class to create headers for MODFLOW
+    binary files
+    """
+    def __init__(self, model=None, botm=None, istrat=1, nu=None):
+        if model is None:
+            if isinstance(botm, list):
+                botm = np.array(botm)
+            self.__botm = botm
+            if isinstance(nu, list):
+                nu = np.array(nu)
+            self.__nu = nu
+            self.__istrat = istrat
+            if istrat == 1:
+                self.__nsrf = self.nu.shape - 1
+            else:
+                self.__nsrf = self.nu.shape - 2
+        else:
+            try:
+                dis = model.get_package('DIS')
+            except:
+                sys.stdout.write('Error: DIS package not available.\n')
+            self.__botm = np.zeros((dis.nlay+1, dis.nrow, dis.ncol), np.float)
+            self.__botm[0, :, :] = dis.top.array
+            self.__botm[1:, :, :] = dis.botm.array
+            try:
+                swi = model.get_package('SWI2')
+                self.__nu = swi.nu.array
+                self.__istrat = swi.istrat
+                self.__nsrf = swi.nsrf
+            except:
+                sys.stdout.write('Error: SWI2 package not available...\n')
+        self.__nlay = self.__botm.shape[0] - 1
+        self.__nrow = self.__botm[0, :, :].shape[0]
+        self.__ncol = self.__botm[0, :, :].shape[1]
+        self.__b = self.__botm[0:-1, :, :] - self.__botm[1:, :, :] 
+     
+    def calc_conc(self, zeta, layer=None):
+        """
+        Calculate concentrations for a given time step using passed zeta.
+
+        Parameters
+        ----------
+        zeta : dictionary of numpy arrays
+            Dictionary of zeta results. zeta keys are zero-based zeta surfaces.
+        layer : int
+            Concentration will be calculated for the specified layer.  If layer 
+            is None, then the concentration will be calculated for all layers. 
+            (default is None).
+
+        Returns
+        -------
+        conc : numpy array
+            Calculated concentration.
+
+        Examples
+        --------
+
+        >>> import flopy
+        >>> m = flopy.modflow.Modflow.load('test')
+        >>> c = flopy.plot.SwiConcentration(model=m)
+        >>> conc = c.calc_conc(z, layer=0)
+
+        """
+        conc = np.zeros((self.__nlay, self.__nrow, self.__ncol), np.float)
+        
+        pct = {}
+        for isrf in xrange(self.__nsrf):
+            z = zeta[isrf]
+            pct[isrf] = (self.__botm[:-1, :, :] - z[:, :, :]) / self.__b[:, :, :]
+        for isrf in xrange(self.__nsrf):
+            p = pct[isrf]
+            if self.__istrat == 1:
+                conc[:, :, :] += self.__nu[isrf] * p[:, :, :]
+                if isrf+1 == self.__nsrf:
+                    conc[:, :, :] += self.__nu[isrf+1] * (1. - p[:, :, :])
+            #TODO linear option
+        if layer is None:
+            return conc
+        else:
+            return conc[layer, :, :]
+              
---
+++ packages/flopy/flopy-3.0.3.zip/flopy-3.0.3/flopy/utils/mfreadnam.py
@@ -0,0 +1,140 @@
+"""
+mfreadnam module.  Contains the NamData class. Note that the user can access
+the NamData class as `flopy.modflow.NamData`.
+
+Additional information about the MODFLOW name file can be found at the `Online
+MODFLOW Guide
+<http://water.usgs.gov/ogw/modflow/MODFLOW-2005-Guide/name_file.htm>`_.
+
+"""
+import os
+
+class NamData(object):
+    """
+    MODFLOW Namefile Class.
+
+    Parameters
+    ----------
+    model : model object
+        The model object (of type :class:`flopy.modflow.mf.Modflow`) to which
+        this package will be added.
+    pkgtype : string
+        String identifying the type of MODFLOW package. See the
+        mfnam_packages dictionary keys in the model object for a list
+        of supported packages. This dictionary is also passed in as packages.
+    packages : dictionary
+        Dictionary of package objects as defined in the
+        `mfnam_packages` attribute of :class:`flopy.modflow.mf.Modflow`.
+
+    name : string
+        Filename of the package file identified in the name file
+    handle : file handle
+        File handle referring to the file identified by `name`
+
+    Attributes
+    ----------
+    mxactd : int
+        Maximum number of drains for a stress period.  This is calculated
+        automatically by FloPy based on the information in
+        `stress_period_data`.
+    filehandle : file handle
+        File handle to the package file. Read from `handle`.
+    filename : string
+        Filename of the package file identified in the name file.
+        Read from `name`.
+    filetype : string
+        String identifying the type of MODFLOW package. Read from
+        `pkgtype`.
+    package : string
+        Package type. Only assigned if `pkgtype` is found in the keys
+        of `packages`
+
+    Methods
+    -------
+
+    See Also
+    --------
+
+    Notes
+    -----
+
+    Examples
+    --------
+
+    """
+    def __init__(self, pkgtype, name, handle, packages):
+        self.filehandle = handle
+        self.filename = name
+        self.filetype = pkgtype
+        self.package = None
+        if self.filetype.lower() in packages.keys():
+            self.package = packages[self.filetype.lower()]
+
+# function to test if a string is an integer
+def testint(cval):
+    try:
+        int(cval)
+        return True
+    except:
+        return False
+    
+# function to parse the name file
+def parsenamefile(namfilename, packages, verbose=True):
+    """
+    Function to parse the nam file and return a dictionary with types,
+    names, units and handles
+
+    Parameters
+    ----------
+    namefilename : string
+        Name of the MODFLOW namefile to parse.
+    packages : dictionary
+        Dictionary of package objects as defined in the `mfnam_packages`
+        attribute of :class:`flopy.modflow.mf.Modflow`.
+    verbose : logical
+        Print messages to screen.  Default is True.
+
+    Returns
+    ----------
+    ext_unit_dict : dictionary
+        For each file listed in the name file, a
+        :class:`flopy.utils.mfreadnam.NamData` instance
+        is stored in the ext_unit_dict dictionary keyed by unit number
+    """
+    # add the .nam extension to namfilename if missing
+    if namfilename[-4:].lower() != '.nam':
+        namfilename += '.nam'
+    
+    # initiate the ext_unit_dict dictionary
+    ext_unit_dict = dict()
+
+    if verbose:
+        print 'Parsing the namefile --> {0:s}'.format(namfilename)
+        print 'Setting filehandles:'
+
+    indata = open(namfilename, 'r').readlines()
+    for line in indata:
+        tmp = line.strip().split()
+        if len(tmp) == 0:
+            continue
+        # be sure the line is not a comment
+        if '#' not in tmp[0]:
+            # be sure the second value is an integer
+            if testint(tmp[1]):
+                fname = os.path.join(os.path.dirname(namfilename), tmp[2])
+                # parse the line
+                openmode = 'r'
+                if tmp[0].upper() == 'DATA(BINARY)':
+                    openmode = 'rb'
+                try:
+                    filehandle = open(fname, openmode)
+                except:
+                    if verbose:
+                        print 'could not set filehandle for {0:s}'\
+                            .format(tmp[2])
+                    filehandle = None
+                # populate the dictionary
+                ext_unit_dict[int(tmp[1])] = NamData(tmp[0], fname, filehandle,
+                                                     packages)
+    return ext_unit_dict
+
