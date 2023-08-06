879cc7a9fc7ee6fe1c3a827be114d8ce15752574
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:38 +0000

    Add package STEME/1.9.0/STEME-1.9.0.tar.gz

---
+++ packages/STEME/STEME-1.9.0.tar.gz/STEME-1.9.0/python/stempy/ipython_log.py
@@ -0,0 +1,12 @@
+
+import weblogolib
+
+import weblogolib
+import weblogolib as W
+W.colorscheme.nucleotide
+W.colorscheme.nucleotide.color
+W.colorscheme.nucleotide.color('A')
+W.colorscheme.nucleotide.color('C')
+W.colorscheme.nucleotide.color('G')
+W.colorscheme.nucleotide.color('T')
+
