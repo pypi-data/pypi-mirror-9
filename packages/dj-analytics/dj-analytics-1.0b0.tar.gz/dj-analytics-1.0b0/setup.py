44e0e2ae53c5171287f1dae985665b4f8d3f7efd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:14 +0000

    Add package dj-analytics/1.0b0/dj-analytics-1.0b0.tar.gz

---
+++ packages/dj-analytics/dj-analytics-1.0b0.tar.gz/dj-analytics-1.0b0/setup.py
@@ -0,0 +1,42 @@
+import os
+import sys
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup, find_packages
+from setuptools_utils import minify
+
+version = '1.0b0'
+
+setup(
+    name='dj-analytics',
+    version=version,
+    author='Analyte Health',
+    author_email='tech@analytehealth.com',
+    url='https://github.com/analytehealth/django-analytics',
+    zip_safe=False,
+    download_url='https://github.com/analytehealth/django-analytics/archive/%s.tar.gz' % version,
+    packages=find_packages(exclude=('*.tests',)),
+    package_data={
+        'djanalytics': [
+            'templates/charts/*',
+            'templates/*png',
+            'templates/*js',
+            'templates/*min',
+        ],
+    },
+    description='Django app to capture, track and display site analytics',
+    long_description=open('README.md').read(),
+    install_requires=[
+        'ipaddress',
+        'python-dateutil',
+        'django-graphos==0.0.2a0',
+        'pytz',
+        'user-agents>=0.3.0',
+        'jsmin>=2.0.6',
+    ],
+    cmdclass={'minify': minify},
+)
