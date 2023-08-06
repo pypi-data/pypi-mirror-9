7d14b03ba4a654ea9edae92507f17eb6cdfd795e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:47 +0000

    Add package momap/0.01/momap-0.01.tar.gz

---
+++ packages/momap/momap-0.01.tar.gz/momap-0.01/setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+
+setup(
+    name='momap',
+    version='0.01',
+    package=['momap'],
+
+    author='Zhenbo Li',
+    author_email='litimetal@gmail.com',
+    license='MIT',
+    url='https://github.com/Endle/momap',
+
+    description='Multi Ordered Map for python',
+
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        ]
+)
