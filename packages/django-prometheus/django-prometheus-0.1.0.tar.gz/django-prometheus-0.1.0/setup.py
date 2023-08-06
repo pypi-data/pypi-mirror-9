412c90cf2bec8fe33279d1d1b1ffdf2a2aaf0115
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:40 +0000

    Add package django-prometheus/0.1.0/django-prometheus-0.1.0.tar.gz

---
+++ packages/django-prometheus/django-prometheus-0.1.0.tar.gz/django-prometheus-0.1.0/setup.py
@@ -0,0 +1,39 @@
+import os
+from setuptools import setup
+
+LONG_DESCRIPTION = """Django-Prometheus
+
+This library contains code to expose some monitoring metrics relevant
+to Django internals so they can be monitored by Prometheus.io.
+
+See https://github.com/korfuri/django-prometheus for usage
+instructions.
+"""
+
+setup(
+    name="django-prometheus",
+    version="0.1.0",
+    author="Uriel Corfa",
+    author_email="uriel@corfa.fr",
+    description=(
+        "Django middlewares to monitor your application with Prometheus.io."),
+    license="Apache",
+    keywords="django monitoring prometheus",
+    url="http://github.com/korfuri/django-prometheus",
+    packages=["django_prometheus"],
+    test_suite="tests",
+    long_description=LONG_DESCRIPTION,
+    install_requires=[
+        "prometheus_client>=0.0.8",
+        "pip-prometheus>=1.0.0",
+    ],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Information Technology",
+        "Intended Audience :: System Administrators",
+        "Framework :: Django",
+        "Topic :: System :: Monitoring",
+        "License :: OSI Approved :: Apache Software License",
+    ],
+)
