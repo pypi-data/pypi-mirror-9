7d20806c6a2c57c1d3d9d967942e9a78914ea898
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:03 +0000

    Add package mirakuru/0.3.0/mirakuru-0.3.0-py2.py3-none-any.whl

---
+++ packages/mirakuru/mirakuru-0.3.0-py2.py3-none-any.whl/mirakuru/output.py
@@ -0,0 +1,88 @@
+# Copyright (C) 2014 by Clearcode <http://clearcode.cc>
+# and associates (see AUTHORS).
+
+# This file is part of mirakuru.
+
+# mirakuru is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# mirakuru is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+
+# You should have received a copy of the GNU Lesser General Public License
+# along with mirakuru.  If not, see <http://www.gnu.org/licenses/>.
+"""This executor awaits for appearance of a predefined banner in output."""
+
+import re
+import select
+
+from mirakuru.base import Executor
+
+
+class OutputExecutor(Executor):
+
+    """Executor that awaits for string output being present in output."""
+
+    def __init__(self, command, banner, shell=False, timeout=None, sleep=0.1):
+        """
+        Initialize OutputExecutor executor.
+
+        :param str command: command to run to start service
+        :param str banner: string that has to appear in process output -
+            should compile to regular expression.
+        :param bool shell: see `subprocess.Popen`
+        :param int timeout: time to wait for process to start or stop.
+            if None, wait indefinitely.
+        :param float sleep: how often to check for start/stop condition
+        """
+        super(OutputExecutor, self).__init__(command, shell, timeout, sleep)
+        self._banner = re.compile(banner)
+
+    def start(self):
+        """
+        Start process.
+
+        .. note::
+
+            Process will be considered started, when defined banner will appear
+            in process output.
+        """
+        super(OutputExecutor, self).start()
+
+        # get a polling object
+        self.poll_obj = select.poll()
+
+        # register a file descriptor
+        # POLLIN because we will wait for data to read
+        self.poll_obj.register(self.output(), select.POLLIN)
+
+        try:
+            self.wait_for(self._wait_for_output)
+
+            # unregister the file descriptor and delete the polling object
+            self.poll_obj.unregister(self.output())
+        finally:
+            del self.poll_obj
+
+    def _wait_for_output(self):
+        """
+        Check if output matches banner.
+
+        .. warning::
+            Waiting for I/O completion. It does not work on Windows. Sorry.
+        """
+        # Here we should get an empty list or list with a tuple [(fd, event)]
+        # When we get list with a tuple we can use readline method on
+        # the file descriptor.
+        poll_result = self.poll_obj.poll(0)
+
+        if poll_result:
+            line = self.output().readline()
+            if self._banner.match(line):
+                return True
+
+        return False
---
+++ packages/mirakuru/mirakuru-0.3.0-py2.py3-none-any.whl/mirakuru/tcp.py
@@ -0,0 +1,82 @@
+# Copyright (C) 2014 by Clearcode <http://clearcode.cc>
+# and associates (see AUTHORS).
+
+# This file is part of mirakuru.
+
+# mirakuru is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# mirakuru is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+
+# You should have received a copy of the GNU Lesser General Public License
+# along with mirakuru.  If not, see <http://www.gnu.org/licenses/>.
+"""TCP executor definition."""
+
+import socket
+from mirakuru.base import StartCheckExecutor
+
+
+class TCPExecutor(StartCheckExecutor):
+
+    """
+    TCP-listening process executor.
+
+    Used to start (and wait to actually be running) processes that can accept
+    TCP connections.
+    """
+
+    def __init__(self, command, host, port,
+                 shell=False, timeout=None, sleep=0.1):
+        """
+        Initialize TCPExecutor executor.
+
+        :param str command: command to run to start service
+        :param str host: host under which process is accessible
+        :param int port: port under which process is accessible
+        :param bool shell: see `subprocess.Popen`
+        :param int timeout: time to wait for process to start or stop.
+            if None, wait indefinitely.
+        :param float sleep: how often to check for start/stop condition
+        """
+        super(TCPExecutor, self).__init__(
+            command, shell=shell, timeout=timeout, sleep=sleep
+        )
+        self.host = host
+        """Host name, process is listening on."""
+        self.port = port
+        """Port number, process is listening on."""
+
+    def pre_start_check(self):
+        """
+        Check if process accepts connections.
+
+        .. note::
+
+            Process will be considered started, when it'll be able to accept
+            TCP connections as defined in initializer.
+        """
+        try:
+            sock = socket.socket()
+            sock.connect((self.host, self.port))
+            return True
+        except (socket.error, socket.timeout):
+            return False
+        finally:
+            # close socket manually for sake of pypy
+            sock.close()
+
+    def after_start_check(self):
+        """
+        Check if process accepts connections.
+
+        .. note::
+
+            Process will be considered started, when it'll be able to accept
+            TCP connections as defined in initializer.
+        """
+        return self.pre_start_check()  # we can reuse logic from `pre_start()`
