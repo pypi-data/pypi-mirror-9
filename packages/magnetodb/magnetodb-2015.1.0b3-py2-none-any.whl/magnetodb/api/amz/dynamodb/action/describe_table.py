1726dcaa216d4e14c99a1634ff2efe90bdea87fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:52:55 +0000

    Add package magnetodb/2015.1.0b3/magnetodb-2015.1.0b3-py2-none-any.whl

---
+++ packages/magnetodb/magnetodb-2015.1.0b3-py2-none-any.whl/magnetodb/api/amz/dynamodb/action/describe_table.py
@@ -0,0 +1,83 @@
+# Copyright 2013 Mirantis Inc.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from magnetodb.api.amz.dynamodb import action
+from magnetodb.api.amz.dynamodb import exception as ddb_exception
+from magnetodb.api.amz.dynamodb import parser
+from magnetodb.common import exception
+from magnetodb import storage
+
+
+class DescribeTableDynamoDBAction(action.DynamoDBAction):
+    schema = {
+        "required": [parser.Props.TABLE_NAME],
+        "properties": {
+            parser.Props.TABLE_NAME: parser.Types.TABLE_NAME
+        }
+    }
+
+    def __call__(self):
+
+        table_name = self.action_params.get(parser.Props.TABLE_NAME, None)
+
+        if not table_name:
+            raise ddb_exception.AWSValidationException(
+                message='Table name is not defined'
+            )
+
+        try:
+            table_meta = storage.describe_table(self.context, table_name)
+
+            result = {
+                parser.Props.TABLE: {
+                    parser.Props.ATTRIBUTE_DEFINITIONS: (
+                        parser.Parser.format_attribute_definitions(
+                            table_meta.schema.attribute_type_map
+                        )
+                    ),
+                    parser.Props.CREATION_DATE_TIME: 0,
+                    parser.Props.ITEM_COUNT: 0,
+                    parser.Props.KEY_SCHEMA: (
+                        parser.Parser.format_key_schema(
+                            table_meta.schema.key_attributes
+                        )
+                    ),
+                    parser.Props.PROVISIONED_THROUGHPUT: (
+                        parser.Values.PROVISIONED_THROUGHPUT_DUMMY
+                    ),
+                    parser.Props.TABLE_NAME: table_name,
+                    parser.Props.TABLE_STATUS: (
+                        parser.Parser.format_table_status(table_meta.status)
+                    ),
+                    parser.Props.TABLE_SIZE_BYTES: 0
+                }
+            }
+
+            if table_meta.schema.index_def_map:
+                table_def = result[parser.Props.TABLE]
+                table_def[parser.Props.LOCAL_SECONDARY_INDEXES] = (
+                    parser.Parser.format_local_secondary_indexes(
+                        table_meta.schema.key_attributes[0],
+                        table_meta.schema.index_def_map
+                    )
+                )
+            return result
+
+        except exception.TableNotExistsException:
+            raise ddb_exception.AWSResourceNotFoundException()
+        except ddb_exception.AWSErrorResponseException as e:
+            raise e
+        except Exception:
+            raise ddb_exception.AWSErrorResponseException()
---
+++ packages/magnetodb/magnetodb-2015.1.0b3-py2-none-any.whl/magnetodb/storage/manager/queued_impl.py
@@ -0,0 +1,48 @@
+# Copyright 2014 Symantec Corporation
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import logging
+
+from oslo import messaging
+
+from magnetodb.common import config
+from magnetodb.storage.manager import simple_impl as manager
+
+LOG = logging.getLogger(__name__)
+CONF = config.CONF
+
+
+class QueuedStorageManager(manager.SimpleStorageManager):
+    def __init__(self, storage_driver, table_info_repo,
+                 concurrent_tasks=1000, batch_chunk_size=25,
+                 schema_operation_timeout=300):
+        manager.SimpleStorageManager.__init__(
+            self, storage_driver, table_info_repo,
+            concurrent_tasks, batch_chunk_size,
+            schema_operation_timeout
+        )
+
+        transport = messaging.get_transport(CONF)
+        target = messaging.Target(topic='schema')
+
+        self._rpc_client = messaging.RPCClient(transport, target)
+
+    def _do_create_table(self, context, table_info):
+        self._rpc_client.cast(
+            context.to_dict(), 'create', table_name=table_info.name)
+
+    def _do_delete_table(self, context, table_info):
+        self._rpc_client.cast(
+            context.to_dict(), 'delete', table_name=table_info.name)
