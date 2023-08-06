49577454f6dd18e71835f7adbdef47e667f423e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:08 +0000

    Add package Hatak_Auth/0.2.2.2/Hatak_Auth-0.2.2.2.tar.gz

---
+++ packages/Hatak_Auth/Hatak_Auth-0.2.2.2.tar.gz/Hatak_Auth-0.2.2.2/setup.py
@@ -0,0 +1,29 @@
+# -*- encoding: utf-8 -*-
+from setuptools import setup, find_packages
+
+install_requires = [
+    'hatak>=0.2.7.8',
+    'Hatak_Formskit>=0.2.3.1',
+    'Hatak_Sql>=0.1.11',
+    'Hatak_Jinja2',
+]
+
+if __name__ == '__main__':
+    setup(
+        name='Hatak_Auth',
+        version='0.2.2.2',
+        description='Auth plugin for Hatak.',
+        license='Apache License 2.0',
+        packages=find_packages('src'),
+        package_dir={'': 'src'},
+        namespace_packages=['haplugin'],
+        install_requires=install_requires,
+        include_package_data=True,
+        zip_safe=False,
+        package_data={
+            'haplugin': [
+                'auth/templates/*.jinja2',
+                'auth/templates/forms/*.jinja2',
+            ],
+        }
+    )
