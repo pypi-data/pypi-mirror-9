ffe953093bc125ed84221c0f4f8d2371cda5c4f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:36:24 +0000

    Add package esdrt.theme/1.15.4/esdrt.theme-1.15.4.zip

---
+++ packages/esdrt.theme/esdrt.theme-1.15.4.zip/esdrt.theme-1.15.4/setup.py
@@ -0,0 +1,46 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.15.4'
+
+setup(name='esdrt.theme',
+      version=version,
+      description="Installable theme: esdrt.theme",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        ],
+      keywords='',
+      author='Mikel Santamaria',
+      author_email='msantamaria@bilbomatica.es',
+      url='https://github.com/eea/esdrt.theme/',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['esdrt'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          # -*- Extra requirements: -*-
+          'setuptools',
+          'z3c.jbot',
+          'five.grok',
+          'eea.icons',
+          'esdrt.content',
+          'plone.app.ldap',
+          'python-ldap',
+          'Products.LDAPMultiPlugins',
+          'Products.LDAPUserFolder',
+          'Products.Collage',
+          'eea.controlpanel',
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+
+      [z3c.autoinclude.plugin]
+      target = plone
+      """
+      )
