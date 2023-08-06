8280b6a1a5097122850dc08454defb9a5ecfc963
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:04 +0000

    Add package coinkit/0.7.8/coinkit-0.7.8.tar.gz

---
+++ packages/coinkit/coinkit-0.7.8.tar.gz/coinkit-0.7.8/setup.py
@@ -0,0 +1,36 @@
+"""
+Coinkit
+==============
+
+"""
+
+from setuptools import setup, find_packages
+
+setup(
+    name='coinkit',
+    version='0.7.8',
+    url='https://github.com/onenameio/coinkit',
+    license='MIT',
+    author='Onename (Halfmoon Labs)',
+    author_email='hello@halfmoon.io',
+    description='Tools for Bitcoin & other cryptocurrencies (incl. Namecoin, Litecoin, Dogecoin, etc.).',
+    keywords='bitcoin btc litecoin namecoin dogecoin cryptocurrency',
+    packages=find_packages(),
+    zip_safe=False,
+    install_requires=[
+        'ecdsa==0.11',
+        'utilitybelt>=0.2.1',
+        'requests>=2.4.3',
+        'pybitcointools==1.1.15',
+        'python-bitcoinrpc==0.1'
+    ],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet',
+        'Topic :: Security :: Cryptography',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
