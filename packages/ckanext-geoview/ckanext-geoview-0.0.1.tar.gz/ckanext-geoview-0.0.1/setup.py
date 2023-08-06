207b9c4f57f7c7781a30fbea670e7f46b36c1c90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:22 +0000

    Add package ckanext-geoview/0.0.1/ckanext-geoview-0.0.1.tar.gz

---
+++ packages/ckanext-geoview/ckanext-geoview-0.0.1.tar.gz/ckanext-geoview-0.0.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+version = '0.0.1'
+
+setup(
+    name='ckanext-geoview',
+    version=version,
+    description='CKAN Geospatial ResourceView',
+    long_description=''' ''',
+    classifiers=[],
+    keywords='',
+    author='Philippe Duchesne',
+    author_email='pduchesne@gmail.com',
+    url='http://github.com/ckan/ckanext-geoview',
+    license='MIT',
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    namespace_packages=['ckanext', 'ckanext.geoview'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        # -*- Extra requirements: -*-
+    ],
+    entry_points='''
+    [ckan.plugins]
+    geo_view=ckanext.geoview.plugin:GeoView
+    geo_preview=ckanext.geoview.plugin:GeoView
+    ''',
+)
