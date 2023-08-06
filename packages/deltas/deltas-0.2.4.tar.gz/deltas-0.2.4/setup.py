34bf1345d6eee6cd80f376dcf93ebf4592c2efb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:36:07 +0000

    Add package deltas/0.2.4/deltas-0.2.4.tar.gz

---
+++ packages/deltas/deltas-0.2.4.tar.gz/deltas-0.2.4/setup.py
@@ -0,0 +1,39 @@
+import os
+
+from setuptools import find_packages, setup
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+def requirements(fname):
+    return [line.strip()
+            for line in open(os.path.join(os.path.dirname(__file__), fname))]
+
+setup(
+    name = "deltas",
+    version = "0.2.4",
+    author = "Aaron Halfaker",
+    author_email = "ahalfaker@wikimedia.org",
+    description = "An experimental diff library for generating " + \
+                  "operation deltas that represent the " + \
+                  "difference between two sequences of comparable items.",
+    license = "MIT",
+    url = "https://github.com/halfak/Deltas",
+    packages=find_packages(),
+    long_description = read('README.rst'),
+    install_requires = ['yamlconf', 'hat-trie'],
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Topic :: Text Processing :: Linguistic",
+        "Topic :: Text Processing :: General",
+        "Topic :: Utilities",
+        "Topic :: Scientific/Engineering"
+    ]
+)
