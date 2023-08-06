6ba688fdfc6a8c1b5c4dbd8d6576732d6c76c0ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:45 +0000

    Add package django-uuslug/1.0.4/django-uuslug-1.0.4.tar.gz

---
+++ packages/django-uuslug/django-uuslug-1.0.4.tar.gz/django-uuslug-1.0.4/uuslug/__init__.py
@@ -0,0 +1,7 @@
+from .uuslug import *
+
+__author__ = 'Val Neekman @ Neekware Inc. [@vneekman]'
+__description__ = 'A Python slugify application that also handles Unicode'
+__version__ = '1.0.4'
+
+default_app_config = 'uuslug.apps.AppConfig'
