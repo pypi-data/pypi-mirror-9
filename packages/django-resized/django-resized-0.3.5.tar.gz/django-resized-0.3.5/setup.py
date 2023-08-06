64fc140025196323fbdee89e7220c65dafb24e03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:29 +0000

    Add package django-resized/0.3.5/django-resized-0.3.5.tar.gz

---
+++ packages/django-resized/django-resized-0.3.5.tar.gz/django-resized-0.3.5/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  Copyright (c) 2012 Ilya Shalyapin
+#
+#  django-resized is free software under terms of the MIT License.
+#
+
+import os
+from setuptools import setup, find_packages
+
+
+setup(
+    name     = 'django-resized',
+    version  = '0.3.5',
+    packages = ['django_resized'],
+    requires = ['python (>= 2.7)', 'django (>= 1.7)'],
+    description  = 'Resizes image origin to specified size.',
+    long_description = open('README.rst').read(),
+    author       = 'Ilya Shalyapin',
+    author_email = 'ishalyapin@gmail.com',
+    url          = 'https://github.com/un1t/django-resized',
+    download_url = 'https://github.com/un1t/django-resized/tarball/master',
+    license      = 'MIT License',
+    keywords     = 'django',
+    classifiers  = [
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+    ],
+)
