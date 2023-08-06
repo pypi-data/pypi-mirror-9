1365aed90a524189a6a92507feff84dab7b86340
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:23:03 +0000

    Add package nesterhyb/1.5.0/nesterhyb-1.5.0.tar.gz

---
+++ packages/nesterhyb/nesterhyb-1.5.0.tar.gz/nesterhyb-1.5.0/nesterhyb.py
@@ -0,0 +1,9 @@
+def print_hyb(the_list,indent=False,level=0,fn='sys.stdout'):
+        for each_item in the_list:
+                if isinstance(each_item,list):
+                        print_hyb(each_item,indent,level+1,fn)
+                else:
+                        if indent:                                
+                                for tab_stop in range(level):
+                                        print("\t",end='',file=fn)
+                        print(each_item,file=fn)
