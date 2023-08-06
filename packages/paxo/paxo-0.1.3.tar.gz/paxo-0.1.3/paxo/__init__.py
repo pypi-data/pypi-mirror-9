28e481009cc116d48b22ac7717a37ba5bb7c4459
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:11 +0000

    Add package paxo/0.1.3/paxo-0.1.3.tar.gz

---
+++ packages/paxo/paxo-0.1.3.tar.gz/paxo-0.1.3/paxo/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+paxo: python ♡ terminal
+"""
+
+__title__ = 'paxo'
+__author__ = 'cwoebker'
+__version__ = '0.1.3'
+__license__ = 'BSD'
+__copyright__ = '(c) 2015 Cecil Woebker'
