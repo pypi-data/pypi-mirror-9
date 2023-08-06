ae7957855fee235b296aa93039bedf4ec8388c09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:07:39 +0000

    Add package genpipeline/0.1.1/genpipeline-0.1.1.zip

---
+++ packages/genpipeline/genpipeline-0.1.1.zip/genpipeline-0.1.1/setup.py
@@ -0,0 +1,24 @@
+"""
+Setup for genpipeline
+
+Setup script for building genpipeline package
+"""
+
+from setuptools import find_packages, setup
+
+setup_params = dict(
+    name="genpipeline",
+    description="A simple Python coroutine-based method for creating data processing pipelines",
+    packages=find_packages(),
+    test_suite = "nose.collector",
+    version = "0.1.1",
+    install_requires = ["greenlet>=0.4.0", "sqlalchemy>=0.7.0"],
+    tests_require = ["nose>=1.2.1"],
+    author = "Renshaw Bay",
+    author_email = "technology@renshawbay.com",
+    url="https://github.com/renshawbay/genpipeline",
+    classifiers=["License :: OSI Approved :: MIT License"],
+)
+
+if __name__ == '__main__':
+    setup(**setup_params)
