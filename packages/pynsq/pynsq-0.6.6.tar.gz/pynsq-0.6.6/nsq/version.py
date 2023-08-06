fa25e5130e730b3bc3b436a180ff05bf253d9a5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:25 +0000

    Add package pynsq/0.6.6/pynsq-0.6.6.tar.gz

---
+++ packages/pynsq/pynsq-0.6.6.tar.gz/pynsq-0.6.6/nsq/version.py
@@ -0,0 +1,2 @@
+# also update in setup.py
+__version__ = '0.6.6'
---
+++ packages/pynsq/pynsq-0.6.6.tar.gz/pynsq-0.6.6/setup.py
@@ -0,0 +1,49 @@
+from setuptools import setup
+from setuptools.command.test import test as TestCommand
+import sys
+
+
+class PyTest(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        import pytest
+        errno = pytest.main(self.test_args)
+        sys.exit(errno)
+
+
+# also update in nsq/version.py
+version = '0.6.6'
+
+
+setup(
+    name='pynsq',
+    version=version,
+    description='official Python client library for NSQ',
+    keywords='python nsq',
+    author='Matt Reiferson',
+    author_email='snakes@gmail.com',
+    url='https://github.com/bitly/pynsq',
+    download_url=(
+        'https://s3.amazonaws.com/bitly-downloads/nsq/pynsq-%s.tar.gz' %
+        version
+    ),
+    packages=['nsq'],
+    install_requires=['tornado<4'],  # <4 due to issue #115
+    include_package_data=True,
+    zip_safe=False,
+    tests_require=['pytest', 'mock', 'simplejson',
+                   'python-snappy', 'tornado'],
+    cmdclass={'test': PyTest},
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: Implementation :: CPython',
+    ]
+)
