2843e8eba67ce99f227eb2f01a96880fff6822db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:02 +0000

    Add package MaterialDjango/0.1a4/MaterialDjango-0.1a4-py2-none-any.whl

---
+++ packages/MaterialDjango/MaterialDjango-0.1a4-py2-none-any.whl/materialdjango/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from widgets import PaperTextInput, PaperPasswordInput
+
+
+def mangle_form(form):
+    "Utility to monkeypatch forms into paperinputs, untested"
+    for field in form.fields:
+        if type(field.widget) is forms.widgets.TextInput:
+            field.widget = PaperTextInput()
+            field.label = ''
+        if type(field.widget) is forms.widgets.PasswordInput:
+            field.widget = PaperPasswordInput()
+            field.label = ''
+    return form
