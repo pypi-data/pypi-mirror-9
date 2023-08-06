d00cc0023c35ef3edd2ee081ed0576827335dc11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:14 +0000

    Add package charles87_001/1.3.0/charles87_001-1.3.0.zip

---
+++ packages/charles87_001/charles87_001-1.3.0.zip/charles87_001-1.3.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+	name='charles87_001',
+	version='1.3.0',
+	py_modules=['charles87_001'],
+	author='chlee',
+        author_email='rlacksql@hanmail.net',
+        description='A simple printer of nestes lists'
+        )
+	
