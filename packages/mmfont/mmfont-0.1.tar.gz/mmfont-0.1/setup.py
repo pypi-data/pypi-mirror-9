1bf7241ecb277c9d83c209dbb9eb407b4814218d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:01 +0000

    Add package mmfont/0.1/mmfont-0.1.tar.gz

---
+++ packages/mmfont/mmfont-0.1.tar.gz/mmfont-0.1/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+setup(
+    name = 'mmfont',
+    packages = ['mmfont'],
+    version = '0.1',
+    description = 'Myanmar Font Encoding Converter between Zawgyi 1 and Unicode 5.1 encodings',
+    author = 'Kaung Htet Zaw',
+    author_email = 'emoosx@gmail.com',
+    url = 'https://github.com/emoosx/mmfont',
+    download_url = 'https://github.com/emoosx/mmfont/tarball/0.1',
+    keywords = ['myanmar', 'unicode', 'mm', 'zawgyi', 'mmunicode']
+)
