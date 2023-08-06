51994b2afeb275b86aed8d6b3b6bd79a01fe4506
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:42 +0000

    Add package simplebayes/1.0.4/simplebayes-1.0.4.tar.gz

---
+++ packages/simplebayes/simplebayes-1.0.4.tar.gz/simplebayes-1.0.4/setup.py
@@ -0,0 +1,29 @@
+# coding: utf-8
+import setuptools
+from distutils.core import setup
+
+setup (
+    name = 'simplebayes',
+    version = '1.0.4',
+    url = 'https://github.com/hickeroar/simplebayes',
+    author = 'Ryan Vennell',
+    author_email = 'ryan.vennell@gmail.com',
+    description = 'A memory-based naïve bayesian text classifier.',
+    long_description = open('README.rst', 'r').read(),
+    license = open('LICENSE', 'r').read(),
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Utilities',
+    ],
+    install_requires = [
+        'xxhash',
+    ],
+    packages = ['simplebayes'],
+)
+# python ./setup.py sdist upload
