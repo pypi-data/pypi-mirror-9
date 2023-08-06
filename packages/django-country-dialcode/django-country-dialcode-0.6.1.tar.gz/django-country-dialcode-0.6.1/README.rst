c41708b1366c7239acc68420cb19a2592bf674f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:48 +0000

    Add package django-country-dialcode/0.6.1/django-country-dialcode-0.6.1.tar.gz

---
+++ packages/django-country-dialcode/django-country-dialcode-0.6.1.tar.gz/django-country-dialcode-0.6.1/README.rst
@@ -0,0 +1,68 @@
+=======================
+django-country-dialcode
+=======================
+
+
+This Django application provides Dial Code and Country data to reuse in a django application.
+
+
+Installation
+============
+
+Install Django-Country-Dialcode::
+
+    python setup.py install
+
+
+Settings
+========
+
+in your settings.py file::
+
+INSTALLED_APPS = INSTALLED_APPS + ('country_dialcode',)
+
+
+Usage
+=====
+
+In your models add the following ::
+
+    dialcode = models.ForeignKey(Prefix, verbose_name=_("Destination"), null=True,
+                               blank=True, help_text=_("Select Prefix"))
+
+
+Contributing
+============
+
+If you've found a bug, implemented/improved a feature and think it is useful
+then please consider contributing. Patches, pull requests or just suggestions
+are welcome!
+
+Source code: http://github.com/Star2Billing/django-country-dialcode
+
+
+If you don’t like Github and Git you’re welcome to send regular patches.
+
+Bug tracker: https://github.com/Star2Billing/django-country-dialcode/issues
+
+
+Documentation
+=============
+
+Documentation is available on 'Read the Docs':
+http://django-country-dialcode.readthedocs.org
+
+
+License
+=======
+
+Copyright (c) 2011-2014 Star2Billing S.L. <info@star2billing.com>
+
+Django-country-dialcode is licensed under MIT, see `MIT-LICENSE.txt`.
+
+
+Credit
+======
+
+Django-country-dialcode is a Star2Billing-Sponsored Community Project, for more information visit
+http://www.star2billing.com  or email us at info@star2billing.com
---
+++ packages/django-country-dialcode/django-country-dialcode-0.6.1.tar.gz/django-country-dialcode-0.6.1/country_dialcode/tests/tests.py
@@ -0,0 +1,86 @@
+# -*- coding: utf-8 -*-
+
+from django.core.management import call_command
+from django.test import TestCase
+from country_dialcode.models import Country, Prefix
+
+
+class CountryDialcodeAdminView(TestCase):
+    """Test cases for CountryDialcode Admin Interface."""
+
+    def test_admin_country_view_list(self):
+        """Test Function to check admin country list"""
+        response = self.client.get("/admin/country_dialcode/country/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_admin_country_view_add(self):
+        """Test Function to check admin country add"""
+        response = self.client.get("/admin/country_dialcode/country/add/")
+        self.assertEqual(response.status_code, 200)
+
+        response = self.client.post(
+            '/admin/country_dialcode/country/add/',
+            data={'countrycode': 'ESP',
+                  'iso2': 'ES',
+                  'countryprefix': '34',
+                  'countryname': 'Spain',
+                  }, follow=True)
+        self.assertEqual(response.status_code, 200)
+
+    def test_admin_prefix_view_list(self):
+        """Test Function to check admin prefix list"""
+        response = self.client.get("/admin/country_dialcode/prefix/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_admin_prefix_view_add(self):
+        """Test Function to check admin prefix add"""
+        response = self.client.get("/admin/country_dialcode/prefix/add/")
+        self.assertEqual(response.status_code, 200)
+
+        response = self.client.post(
+            '/admin/country_dialcode/prefix/add/',
+            data={'prefix': '34',
+                  'destination': 'Spain',
+                  'country_id': 198,
+                  'carrier_name': 'xyz',
+                  'prefix_type': 1})
+        self.assertEqual(response.status_code, 200)
+
+
+class CountryDialcodeModel(TestCase):
+    """Test Country, Prefix models"""
+
+    def setUp(self):
+        # Country model
+        self.country = Country(
+            countrycode="ESP",
+            iso2='ES',
+            countryprefix='34',
+            countryname='Spain',
+        )
+        self.country.save()
+        self.assertEqual(self.country.__unicode__(), u'ESP')
+
+        # Prefix model
+        self.prefix = Prefix(
+            prefix=34,
+            destination='Spain',
+            country_id=self.country,
+            carrier_name='xyz',
+            prefix_type=1
+        )
+
+        self.prefix.save()
+        self.assertEqual(self.prefix.__unicode__(), u'34')
+
+    def test_country_prefix_name(self):
+        self.assertEqual(self.country.countryname, "Spain")
+        self.assertEqual(self.prefix.country_name(), "Spain")
+
+    def teardown(self):
+        self.country.delete()
+        self.prefix.delete()
+
+    # def test_mgt_command(self):
+    #     # Test mgt command
+    #     call_command('load_country_dialcode')
