3aa124a90ef81972a387bf95ed9266e1a2038ece
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:25 +0000

    Add package django-email-hijacker/0.3.2/django_email_hijacker-0.3.2-py2.py3-none-any.whl

---
+++ packages/django-email-hijacker/django_email_hijacker-0.3.2-py2.py3-none-any.whl/email_hijacker/backends/hijacker.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+
+from django.core.mail.backends.base import BaseEmailBackend
+
+try:
+    from django.utils.module_loading import import_string
+except ImportError:  # pragma: nocover
+    from django.utils.module_loading import import_by_path as import_string
+
+from ..apps import EmailHijacker
+
+
+class EmailBackend(BaseEmailBackend):
+
+    def __init__(self, *args, **kwargs):
+        super(EmailBackend, self).__init__(*args, **kwargs)
+        self.hijacked = import_string(EmailHijacker.EMAIL_BACKEND)(*args, **kwargs)
+
+    def open(self):
+        self.hijacked.open()
+
+    def close(self):
+        self.hijacked.close()
+
+    def send_messages(self, email_messages):
+        for email in email_messages:
+            email.to = (EmailHijacker.EMAIL_ADDRESS,)
+            email.bcc = ()
+            email.cc = ()
+            email.subject = 'HIJACKED: {}'.format(email.subject)
+        self.hijacked.send_messages(email_messages)
