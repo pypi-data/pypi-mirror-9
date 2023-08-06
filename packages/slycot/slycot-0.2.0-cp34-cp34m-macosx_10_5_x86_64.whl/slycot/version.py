7855269de0af5d0c0e30a4b2d1988f4388fc3bf0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:29:53 +0000

    Add package slycot/0.2.0/slycot-0.2.0-cp34-cp34m-macosx_10_5_x86_64.whl

---
+++ packages/slycot/slycot-0.2.0-cp34-cp34m-macosx_10_5_x86_64.whl/slycot/version.py
@@ -0,0 +1,10 @@
+
+# THIS FILE IS GENERATED FROM SLYCOT SETUP.PY
+short_version = '0.2.0'
+version = '0.2.0'
+full_version = '0.2.0'
+git_revision = '00d5a545345e123dd723141671a5a1fe52629e81'
+release = True
+
+if not release:
+    version = full_version
