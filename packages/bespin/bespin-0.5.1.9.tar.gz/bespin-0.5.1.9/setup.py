3840a545f852c975558ae872b54326ba8318ccd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:27 +0000

    Add package bespin/0.5.1.9/bespin-0.5.1.9.tar.gz

---
+++ packages/bespin/bespin-0.5.1.9.tar.gz/bespin-0.5.1.9/setup.py
@@ -0,0 +1,55 @@
+from setuptools import setup, find_packages
+from bespin import VERSION
+
+setup(
+      name = "bespin"
+    , version = VERSION
+    , packages = ['bespin'] + ['bespin.%s' % pkg for pkg in find_packages('bespin')]
+    , include_package_data = True
+
+    , install_requires =
+      [ "delfick_error==1.6.1"
+      , "option_merge==0.9.1"
+      , "input_algorithms==0.4.3.1"
+
+      , "six"
+      , "pytz"
+      , "humanize"
+      , "argparse"
+      , "requests"
+
+      , "radssh==1.0.1"
+      , "boto==2.36.0"
+      , "pyYaml==3.10"
+      , "rainbow_logging_handler==2.2.2"
+      , "FileChunkIO==1.6"
+      ]
+
+    , extras_require =
+      { "tests":
+        [ "noseOfYeti>=1.5.0"
+        , "nose"
+        , "mock"
+        , "moto"
+
+        # Need to ensure httpretty is not 0.8.7
+        # To prevent an infinite loop in python3 tests
+        , "httpretty==0.8.6"
+        ]
+      }
+
+    , entry_points =
+      { 'console_scripts' :
+        [ 'bespin = bespin.executor:main'
+        ]
+      }
+
+    # metadata for upload to PyPI
+    , url = "https://github.com/realestate-com-au/bespin"
+    , author = "Stephen Moore"
+    , author_email = "stephen.moore@rea-group.com"
+    , description = "Opinionated wrapper around boto that reads yaml"
+    , license = "MIT"
+    , keywords = "cloudformation boto"
+    )
+
