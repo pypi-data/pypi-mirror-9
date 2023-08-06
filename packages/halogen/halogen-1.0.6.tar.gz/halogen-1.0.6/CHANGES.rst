9baba5cc444ce8fbd1b8ebf47685fe91880cf0d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:06 +0000

    Add package halogen/1.0.6/halogen-1.0.6.tar.gz

---
+++ packages/halogen/halogen-1.0.6.tar.gz/halogen-1.0.6/CHANGES.rst
@@ -0,0 +1,23 @@
+Changelog
+=========
+
+1.0.6
+-----
+
+* Respect ValueError in deserialization of the attributes (bubenkoff)
+
+1.0.4
+-----
+
+* Correctly render and document deserialization errors (bubenkoff)
+
+1.0.3
+-----
+
+* Allow Embedded fields to be marked as not required (mattupstate)
+* Field order is preserved in serialized documents (mattupstate)
+
+1.0.0
+-----
+
+* Initial public release
---
+++ packages/halogen/halogen-1.0.6.tar.gz/halogen-1.0.6/halogen/__init__.py
@@ -0,0 +1,24 @@
+"""halogen public API."""
+
+__version__ = '1.0.6'
+
+try:
+    from halogen.schema import Schema, Attr, Link, Curie, Embedded, Accessor
+    from halogen import types
+    from halogen import validators
+    from halogen import exceptions
+
+    __all__ = [
+        "Accessor",
+        "Attr",
+        "Curie",
+        "Embedded",
+        "exceptions",
+        "Link",
+        "Schema",
+        "types",
+        "validators",
+    ]
+except ImportError:
+    # avoid import errors when only __version__ is needed (for setup.py)
+    pass
