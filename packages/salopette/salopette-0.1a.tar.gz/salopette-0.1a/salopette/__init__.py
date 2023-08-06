452965a5332125395ea379df41f736f0b2524127
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:29:46 +0000

    Add package salopette/0.1a/salopette-0.1a.tar.gz

---
+++ packages/salopette/salopette-0.1a.tar.gz/salopette-0.1a/salopette/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__title__ = 'salopette'
+__version__ = '0.1a'
+__author__ = 'Martin Simon <me@martinsimon.me>'
+__repo__ = 'https://github.com/c0ding/salopette'
+__license__ = 'Apache v2.0 License'
