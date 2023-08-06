5f6a4ba37aed1934ed648d03e13dac3fcca54950
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:29 +0000

    Add package container-transform/0.3/container_transform-0.3-py2.py3-none-any.whl

---
+++ packages/container-transform/container_transform-0.3-py2.py3-none-any.whl/container_transform-0.3.dist-info/DESCRIPTION.rst
@@ -0,0 +1,85 @@
+.. image:: https://travis-ci.org/ambitioninc/container-transform.png
+   :target: https://travis-ci.org/ambitioninc/container-transform
+
+.. image:: https://coveralls.io/repos/ambitioninc/container-transform/badge.png?branch=develop
+    :target: https://coveralls.io/r/ambitioninc/container-transform?branch=develop
+.. image:: https://pypip.in/v/container-transform/badge.png
+    :target: https://pypi.python.org/pypi/container-transform/
+    :alt: Latest PyPI version
+
+.. image:: https://pypip.in/d/container-transform/badge.png
+    :target: https://pypi.python.org/pypi/container-transform/
+    :alt: Number of PyPI downloads
+
+
+container-transform
+===================
+container-transform is a small utility to transform various docker container
+formats to one another.
+
+Currently, container-transform can parse and convert ECS task definitions and
+fig configuration files. Any missing required parameters are printed to STDERR.
+
+Quickstart
+----------
+::
+
+    $ cat fig.yml | container-transform  -v
+    [
+        {
+            "memory": 1024,
+            "image": "postgres:9.3",
+            "name": "db",
+            "essential": true
+        },
+        {
+            "memory": 128,
+            "image": "redis:latest",
+            "name": "redis",
+            "essential": true
+        },
+        {
+            "name": "web",
+            "memory": 64,
+            "command": [
+                "uwsgi",
+                "--json",
+                "uwsgi.json"
+            ],
+            "environment": [
+                {
+                    "name": "AWS_ACCESS_KEY_ID",
+                    "value": "AAAAAAAAAAAAAAAAAAAA"
+                },
+                {
+                    "name": "AWS_SECRET_ACCESS_KEY",
+                    "value": "1111111111111111111111111111111111111111"
+                }
+            ],
+            "essential": true
+        }
+    ]
+    Container db is missing required parameter "cpu".
+    Container redis is missing required parameter "cpu".
+    Container web is missing required parameter "image".
+    Container web is missing required parameter "cpu".
+
+Installation
+------------
+To install the latest release, type::
+
+    pip install container-transform
+
+To install the latest code directly from source, type::
+
+    pip install git+git://github.com/ambitioninc/container-transform.git
+
+Documentation
+-------------
+
+Full documentation is available at http://container-transform.readthedocs.org
+
+License
+-------
+MIT License (see LICENSE)
+
---
+++ packages/container-transform/container_transform-0.3-py2.py3-none-any.whl/container_transform/tests/client_tests.py
@@ -0,0 +1,92 @@
+from unittest import TestCase
+import json
+
+
+from click.testing import CliRunner
+
+from container_transform.client import transform
+
+
+class ClientTests(TestCase):
+    """
+    Tests for client
+    """
+    def setUp(self):
+        self.fig_input = (
+            '\n'
+            'web:\n'
+            '  image: me/myapp\n'
+            '  mem_limit: 1024b\n'
+            '\n'
+            'web2:\n'
+            '  build: .\n'
+            '  mem_limit: 1024b\n'
+        )
+
+    def test_prompt_fig_quiet(self):
+        runner = CliRunner()
+        with runner.isolated_filesystem():
+            with open('fig.yml', 'w') as f:
+                f.write(self.fig_input)
+
+            result = runner.invoke(transform, ['fig.yml', '-q'])
+            assert result.exit_code == 0
+
+            data = json.loads(result.output)
+
+            self.assertIn(
+                {
+                    'name': 'web',
+                    'image': 'me/myapp',
+                    'memory': 4,
+                    'essential': True
+                },
+                data,
+            )
+            self.assertIn(
+                {
+                    'name': 'web2',
+                    'memory': 4,
+                    'essential': True
+                },
+                data,
+            )
+
+    def test_prompt_fig_no_quiet(self):
+        runner = CliRunner()
+        with runner.isolated_filesystem():
+            with open('fig.yml', 'w') as f:
+                f.write(self.fig_input)
+
+            result = runner.invoke(transform, ['fig.yml', '--no-verbose'])
+            assert result.exit_code == 0
+
+            data = json.loads(result.output.split('\n')[0])
+
+            messages = set(result.output.split('\n')[1:])
+
+            self.assertEqual(
+                {'Container web2 is missing required parameter "cpu".',
+                 'Container web is missing required parameter "cpu".',
+                 'Container web2 is missing required parameter "image".',
+                 ''},
+                messages
+            )
+
+            self.assertIn(
+                {
+                    'name': 'web',
+                    'image': 'me/myapp',
+                    'memory': 4,
+                    'essential': True
+                },
+                data,
+            )
+            self.assertIn(
+                {
+                    'name': 'web2',
+                    'memory': 4,
+                    'essential': True
+                },
+                data,
+            )
