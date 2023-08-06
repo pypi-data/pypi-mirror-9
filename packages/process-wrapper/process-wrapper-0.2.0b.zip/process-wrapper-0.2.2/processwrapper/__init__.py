9870ce2dcaeba1868887266e3e56ecd8f1218cc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:21 +0000

    Add package process-wrapper/0.2.0/process-wrapper-0.2.0b.zip

---
+++ packages/process-wrapper/process-wrapper-0.2.0b.zip/process-wrapper-0.2.2/processwrapper/__init__.py
@@ -0,0 +1,38 @@
+from .process_wrapper import ProcessWrapper
+from .interfaces.process_utility import DefaultProcessUtility
+
+from contextlib import contextmanager
+
+
+default_process_wrapper = ProcessWrapper(DefaultProcessUtility())
+
+@contextmanager
+def run_process(command):
+    """Runs a command-line program as a context.
+    
+    stdout and stderr are redirected to the subprocess pipe
+
+    On exit, SIGKILL is sent to the parent and all of its
+    child processes.
+
+    Parameters
+    ----------
+    command : string
+       Command-line program with necessary arguments and flags to be run.
+       
+    Tested Capabilities
+    -------------------
+    - Kills parent and child processes on exit
+    - Handles in-context exceptions: It still kills the called process and its children
+    - Process wrapper will do nothing if it encounters a Process Not Found exception for the parent process and raise all others
+    - Tested to work in win32 and linux2 platforms
+
+    Usage
+    -----
+    from processwrapper import run_process
+
+    with run_process('program argument --flag'):
+        do_something()
+    """
+    with default_process_wrapper.run_process(command) as process:
+        yield process
---
+++ packages/process-wrapper/process-wrapper-0.2.0b.zip/process-wrapper-0.2.2/setup.py
@@ -0,0 +1,60 @@
+'''
+Created on Mar 20, 2015
+
+@author: Marc Lopez (marc.rainier.lopez@gmail.com)
+'''
+
+import sys
+from setuptools import setup
+from setuptools.command.test import test as TestCommand
+
+
+class PyTest(TestCommand):
+    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = []
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        import pytest
+        errno = pytest.main(self.pytest_args)
+        sys.exit(errno)
+
+setup(
+    name='process-wrapper',
+    version='0.2.2',
+    author='Marc Lopez',
+    author_email='marc.rainier.lopez@gmail.com',
+    packages=[
+        'processwrapper',
+        'processwrapper.interfaces',
+        ],
+    scripts=[],
+    url='https://github.com/marc-lopez/process-wrapper',
+    platforms=['linux2','win32'],
+    license='LICENSE.txt',
+    description='Context manager for background command-line programs',
+    long_description=open('README.txt').read(),
+    classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Environment :: Console',
+          'Intended Audience :: Developers',
+          'Intended Audience :: System Administrators',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python',
+          'Topic :: Software Development :: Libraries :: Python Modules',
+        ],
+    install_requires=[
+        'psutil>=2.2.1'
+        ],
+    tests_require=[
+        'pytest',
+        ],
+    cmdclass={'test': PyTest},
+    )
