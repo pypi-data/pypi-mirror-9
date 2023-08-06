4e016098d66e13371c066d2e0c7fe3fe059cb012
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:05 +0000

    Add package ApiDaemon/0.02.05/ApiDaemon-0.02.05.tar.gz

---
+++ packages/ApiDaemon/ApiDaemon-0.02.05.tar.gz/ApiDaemon-0.02.05/ApiDaemon/__init__.py
@@ -0,0 +1,9 @@
+from ApiDaemon.exceptions import ApiException
+
+from ApiDaemon.server import Server as ApiServer
+from ApiDaemon.clients.sync_client import SyncClient as ApiSyncClient
+from ApiDaemon.clients.async_client import Client as ApiClient
+
+from ApiDaemon.plugins import VkPlugin
+from ApiDaemon.plugins import LastfmPlugin
+
