4999b8094ba0db6c74adce0002d85fa88aa546fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:04 +0000

    Add package collective.geolocationbehavior/1.3.1/collective.geolocationbehavior-1.3.1.tar.gz

---
+++ packages/collective.geolocationbehavior/collective.geolocationbehavior-1.3.1.tar.gz/collective.geolocationbehavior-1.3.1/collective/geolocationbehavior/geolocation.py
@@ -0,0 +1,16 @@
+from collective.geolocationbehavior import _
+from plone.autoform.interfaces import IFormFieldProvider
+from plone.formwidget.geolocation.geolocation import GeolocationField
+from plone.supermodel import model
+from zope.interface import alsoProvides
+
+
+class IGeolocatable(model.Schema):
+    """Form field for geolocation behavior"""
+    geolocation = GeolocationField(
+        title=_('label_geolocation', default=u'Geolocation'),
+        description=_('help_geolocation',
+                      default=u'Click on the map to select a location, or '
+                              u'use the text input to search by address.'),
+        required=False)
+alsoProvides(IGeolocatable, IFormFieldProvider)
