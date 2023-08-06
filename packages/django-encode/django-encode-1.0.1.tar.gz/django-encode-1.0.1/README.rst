fab707fdaaab473c67b1aaffc017a85ad929c363
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:10 +0000

    Add package django-encode/1.0.1/django-encode-1.0.1.tar.gz

---
+++ packages/django-encode/django-encode-1.0.1.tar.gz/django-encode-1.0.1/README.rst
@@ -0,0 +1,36 @@
+django-encode
+=============
+
+``django-encode`` allows you to encode and transfer media files async.
+
+.. image:: https://img.shields.io/pypi/v/django-encode.svg
+    :target: https://pypi.python.org/pypi/django-encode
+.. image:: https://travis-ci.org/collab-project/django-encode.svg?branch=master
+    :target: https://travis-ci.org/collab-project/django-encode
+.. image:: https://coveralls.io/repos/collab-project/django-encode/badge.svg
+    :target: https://coveralls.io/r/collab-project/django-encode
+.. image:: https://img.shields.io/badge/license-MIT-blue.svg
+    :target: https://raw.githubusercontent.com/collab-project/django-encode/master/LICENSE
+
+Installation
+------------
+
+Use pip_ to install the download and install the package from PyPi_::
+
+  pip install django-encode
+
+Or checkout the source code from Github_::
+
+  git clone https://github.com/collab-project/django-encode.git
+
+
+Documentation
+-------------
+
+Documentation can be found on `readthedocs.org`_.
+
+
+.. _pip: https://pypi.python.org/pypi/pip
+.. _PyPi: https://pypi.python.org/pypi/django-encode
+.. _readthedocs.org: https://django-encode.readthedocs.org/en/latest
+.. _Github: https://github.com/collab-project/django-encode
---
+++ packages/django-encode/django-encode-1.0.1.tar.gz/django-encode-1.0.1/encode/__init__.py
@@ -0,0 +1,68 @@
+# Copyright Collab 2012-2015
+
+"""
+`django-encode` application.
+"""
+
+from __future__ import unicode_literals
+
+AUDIO = "audio"
+VIDEO = "video"
+SNAPSHOT = "snapshot"
+
+#: Accepted file types.
+FILE_TYPES = (
+    (AUDIO, "Audio"),
+    (VIDEO, "Video"),
+    (SNAPSHOT, "Snapshot"),
+)
+
+#: Application version.
+__version__ = (1, 0, 1)
+
+
+def short_version(version=None):
+    """
+    Return short application version. For example: `1.0.0`.
+    """
+    v = version or __version__
+    return '.'.join([str(x) for x in v[:3]])
+
+
+def get_version(version=None):
+    """
+    Return full version nr, inc. rc, beta etc tags.
+
+    For example: `2.0.0a1`
+    :rtype: str
+    """
+    v = version or __version__
+    if len(v) == 4:
+        return '{0}{1}'.format(short_version(v), v[3])
+
+    return short_version(v)
+
+
+#: Full version number.
+version = get_version()
+
+
+class EncodeError(Exception):
+    """
+    Something bad happened while encoding the media.
+    """
+    pass
+
+
+class DecodeError(Exception):
+    """
+    Something bad happened while decoding the media.
+    """
+    pass
+
+
+class UploadError(Exception):
+    """
+    Something bad happened while uploading the encoded media.
+    """
+    pass
