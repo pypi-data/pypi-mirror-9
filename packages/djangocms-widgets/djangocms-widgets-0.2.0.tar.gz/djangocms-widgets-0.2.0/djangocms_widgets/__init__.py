5719d060aeefbf6fc8ac045b3ba452770a6de15d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:33 +0000

    Add package djangocms-widgets/0.2.0/djangocms-widgets-0.2.0.tar.gz

---
+++ packages/djangocms-widgets/djangocms-widgets-0.2.0.tar.gz/djangocms-widgets-0.2.0/djangocms_widgets/__init__.py
@@ -0,0 +1,16 @@
+# coding: utf-8
+
+"""
+    djangocms-widgets
+    ~~~~~~~~~~~~~~~~~
+
+    :copyleft: 2015 by the djangocms-widgets team, see AUTHORS for more details.
+    :created: by JensDiemer.de
+    :license: GNU GPL v3 or above, see LICENSE for more details.
+"""
+
+__version__ = (0,2,0)
+VERSION_STRING = '.'.join(str(part) for part in __version__)
+
+if __name__ == "__main__":
+    print(VERSION_STRING)
