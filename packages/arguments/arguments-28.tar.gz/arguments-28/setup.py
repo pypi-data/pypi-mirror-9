d13e9b834ea1b33a43163cb190bd3f5b7421d30c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:00 +0000

    Add package arguments/28/arguments-28.tar.gz

---
+++ packages/arguments/arguments-28.tar.gz/arguments-28/setup.py
@@ -0,0 +1,28 @@
+# coding=utf-8
+"""
+appinstance
+Active8 (04-03-15)
+license: GNU-GPL2
+"""
+
+from setuptools import setup
+setup(name='arguments',
+      version='28',
+      description='Argument parser based on docopt',
+      url='https://github.com/erikdejonge/arguments',
+      author='Erik de Jonge',
+      author_email='erik@a8.nl',
+      license='GPL',
+      packages=['arguments', 'fallbackdocopt'],
+      zip_safe=True,
+      install_requires=['docopt', 'schema', 'consoleprinter'],
+      classifiers=[
+          "Programming Language :: Python",
+          "Programming Language :: Python :: 3",
+          "Development Status :: 4 - Beta ",
+          "Intended Audience :: Developers",
+          "License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
+          "Operating System :: POSIX",
+          "Topic :: Software Development :: Libraries :: Python Modules",
+          "Topic :: System",
+      ])
