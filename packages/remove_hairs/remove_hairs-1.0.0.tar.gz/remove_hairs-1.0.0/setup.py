f79025645c8f0c3415553e763a8b5a4295b8f1d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:28 +0000

    Add package remove_hairs/1.0.0/remove_hairs-1.0.0.tar.gz

---
+++ packages/remove_hairs/remove_hairs-1.0.0.tar.gz/remove_hairs-1.0.0/setup.py
@@ -0,0 +1,74 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Interpreter version: python 2.7
+#
+# Imports =====================================================================
+from distutils.core import setup
+
+# Variables ===================================================================
+changelog = open('CHANGES.rst').read()
+long_description = "\n\n".join([
+    open('README.rst').read(),
+    changelog
+])
+
+
+# Functions & classes =========================================================
+def allSame(s):
+    return not filter(lambda x: x != s[0], s)
+
+
+def hasDigit(s):
+    return any(map(lambda x: x.isdigit(), s))
+
+
+def getVersion(data):
+    data = data.splitlines()
+    return filter(
+        lambda (x, y):
+            len(x) == len(y) and allSame(y) and hasDigit(x) and "." in x,
+        zip(data, data[1:])
+    )[0][0]
+
+
+# Actual setup definition =====================================================
+setup(
+    name='remove_hairs',
+    version=getVersion(changelog),
+    description='Function form removing characters from both sides of string.',
+    long_description=long_description,
+    url='https://github.com/Bystroushaak/remove_hairs',
+
+    author='Bystroushaak',
+    author_email='bystrousak@kitakitsune.org',
+
+    classifiers=[
+        'Intended Audience :: Developers',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.7',
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development",
+        "Topic :: Text Processing",
+    ],
+    license='MIT',
+
+    packages=["src/remove_hairs"],
+    include_package_data=True,
+    zip_safe=True,
+
+    test_suite='py.test',
+    tests_require=["pytest"],
+    extras_require={
+        "test": [
+            "pytest"
+        ],
+        "docs": [
+            "sphinx",
+            "sphinxcontrib-napoleon",
+        ]
+    },
+)
---
+++ packages/remove_hairs/remove_hairs-1.0.0.tar.gz/remove_hairs-1.0.0/tests/test_remove_hairs.py
@@ -0,0 +1,54 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Interpreter version: python 2.7
+#
+# Imports =====================================================================
+import sys
+
+import pytest
+
+sys.path.append('src/')
+from remove_hairs import remove_hairs
+from remove_hairs import remove_hairs_decorator
+
+
+# Functions & classes =========================================================
+def test_remove_hairs():
+    assert remove_hairs(",a-sd,-/") == "a-sd"
+
+    assert remove_hairs(" - a sd: --", " -") == "a sd:"
+
+    assert remove_hairs(" -  --", " -") == ""
+
+    assert remove_hairs(" -  --", "") == " -  --"
+
+    assert remove_hairs("", "") == ""
+
+    assert remove_hairs("", "-,.") == ""
+
+
+def test_remove_hairs_decorator():
+    @remove_hairs_decorator
+    def x():
+        return ",a-sd,-/"
+
+    assert x() == "a-sd"
+
+    @remove_hairs_decorator
+    def x(a):
+        return ",a-sd,-/"
+
+    assert x(1) == "a-sd"
+
+    @remove_hairs_decorator(hairs=" -")
+    def y():
+        return " - a sd: --"
+
+    assert y() == "a sd:"
+
+    @remove_hairs_decorator(hairs=" -")
+    def y(a):
+        return " - a sd: --"
+
+    assert y(1) == "a sd:"
