55d3b5a1e7b05dc4845193d14efb2df15790c99c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:07:57 +0000

    Add package restmapper/0.0.2/restmapper-0.0.2.tar.gz

---
+++ packages/restmapper/restmapper-0.0.2.tar.gz/restmapper-0.0.2/restmapper/__init__.py
@@ -0,0 +1,21 @@
+import restmapper
+
+from .metadata import (
+    __author__,
+    __copyright__,
+    __email__,
+    __license__,
+    __maintainer__,
+    __version__,
+)
+
+__all__ = [
+    '__author__',
+    '__copyright__',
+    '__email__',
+    '__license__',
+    '__maintainer__',
+    '__version__',
+    'restmapper',
+]
+
---
+++ packages/restmapper/restmapper-0.0.2.tar.gz/restmapper-0.0.2/restmapper/restmapper.py
@@ -0,0 +1,93 @@
+import json
+import requests
+import models
+
+
+class RestMapper(object):
+    def __init__(self, endpoint, parsers={}, callback=None, url_transformer=None, method=requests.get, verify_ssl=True):
+        self.endpoint = endpoint
+        self.parsers = parsers
+        self.callback = callback
+        self.method = method
+        self.verify_ssl = verify_ssl
+        self.url_transformer = url_transformer
+
+    def __call__(self, auth):
+        self.auth = auth
+        return self
+
+    def __getattr__(self, k):
+        if k in ["GET", "POST", "PUT", "PATCH"]:
+            self.method = getattr(requests, k.lower())
+            return self
+        else:
+            return RestMapperCall(self.endpoint, self.method, k, self.auth, self.parsers, self.callback, self.url_transformer, self.verify_ssl)
+
+
+class RestMapperCall(object):
+    def __init__(self, endpoint, method, path, auth, parsers, callback=None, url_transformer=None, verify_ssl=True):
+        self.method = method
+        self.components = [path]
+        self.endpoint = endpoint
+        self.auth = auth
+        self.parsers = parsers
+        self.method = method
+
+        if callback is None:
+            self.callback = lambda response: response
+        else:
+            self.callback = callback
+
+        if url_transformer is None:
+            self.url_transformer = lambda url: url
+        else:
+            self.url_transformer = url_transformer
+
+        self.verify_ssl = verify_ssl
+
+    def __getattr__(self, k):
+        self.components.append(k)
+        return self
+
+    def __getitem__(self, k):
+        self.components.append(k)
+        return self
+
+    def __call__(self, *args, **kwargs):
+        url = "{}{}".format(self.endpoint, "/".join(self.components))
+        url = self.url_transformer(url)
+
+        parse_response = kwargs.get('parse_response', True)
+
+        if 'parse_response' in kwargs:
+            del kwargs['parse_response']
+
+        if len(args) > 0:
+            data = args[0]
+        else:
+            data = None
+
+        response = self.method(
+            url,
+            data=data,
+            params=kwargs,
+            auth=self.auth,
+            verify=self.verify_ssl
+        )
+
+        parse_as = None
+        for component, parser in self.parsers.iteritems():
+            if component in self.components:
+                parse_as = parser
+
+        json_response = response.json()
+
+        self.callback(json_response)
+
+        if parse_response and parse_as is not None:
+            if isinstance(json_response, list):
+                return map(parse_as.parse, json_response)
+            else:
+                return parse_as.parse(json_response)
+        else:
+            return json_response
