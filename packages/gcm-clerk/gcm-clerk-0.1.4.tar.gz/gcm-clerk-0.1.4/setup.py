80238c984e3c62fe94a06a9dae8c67dbec488484
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:23:00 +0000

    Add package gcm-clerk/0.1.4/gcm-clerk-0.1.4.tar.gz

---
+++ packages/gcm-clerk/gcm-clerk-0.1.4.tar.gz/gcm-clerk-0.1.4/setup.py
@@ -0,0 +1,23 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name='gcm-clerk',
+    version='0.1.4',
+    author='Aleksi Hoffman',
+    author_email='aleksi@lekksi.com',
+    url='https://bitbucket.org/aleksihoffman/gcm-clerk',
+    description='Python client for Google Cloud Messaging (GCM)',
+    long_description=open('README.rst').read(),
+    packages=['gcm_clerk'],
+    license="Apache 2.0",
+    keywords='gcm push notification google cloud messaging android',
+    install_requires=['requests'],
+    classifiers=['Development Status :: 4 - Beta',
+                 'Intended Audience :: Developers',
+                 'License :: OSI Approved :: Apache Software License',
+                 'Programming Language :: Python',
+                 'Topic :: Software Development :: Libraries :: Python Modules']
+)
