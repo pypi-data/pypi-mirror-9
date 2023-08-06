2f33ec2a2b3a30af70215d1c8c23f6e70742e4b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:21 +0000

    Add package muffin-peewee/0.0.5/muffin_peewee-0.0.5-py2.py3-none-any.whl

---
+++ packages/muffin-peewee/muffin_peewee-0.0.5-py2.py3-none-any.whl/muffin_peewee/__init__.py
@@ -0,0 +1,15 @@
+"""
+    muffin-peewee description.
+
+"""
+
+# Package information
+# ===================
+
+__version__ = "0.0.5"
+__project__ = "muffin-peewee"
+__author__ = "Kirill Klenov <horneds@gmail.com>"
+__license__ = "MIT"
+
+from .plugin import Plugin          # noqa
+from .models import Model, CModel   # noqa
