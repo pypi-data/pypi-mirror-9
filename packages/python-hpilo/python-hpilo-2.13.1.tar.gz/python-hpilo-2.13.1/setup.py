9d9689f8a4fb47519306bb08690ea5353f435a50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:07 +0000

    Add package python-hpilo/2.13.1/python-hpilo-2.13.1.tar.gz

---
+++ packages/python-hpilo/python-hpilo-2.13.1.tar.gz/python-hpilo-2.13.1/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+
+from distutils.core import setup
+
+setup(name = "python-hpilo",
+      version = "2.13.1",
+      author = "Dennis Kaarsemaker",
+      author_email = "dennis@kaarsemaker.net",
+      url = "http://github.com/seveas/python-hpilo",
+      description = "iLO automation from python or shell",
+      py_modules = ["hpilo", "hpilo_fw"],
+      scripts = ["hpilo_cli"],
+      classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Topic :: System :: Hardware',
+        'Topic :: System :: Systems Administration',
+        'Topic :: System :: Networking',
+      ]
+)
