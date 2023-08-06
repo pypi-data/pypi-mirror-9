2f6e14c97f21ef7419e065e164b04aaa078008c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:36:28 +0000

    Add package django-fast-sync/0.2.5/django-fast-sync-0.2.5.tar.gz

---
+++ packages/django-fast-sync/django-fast-sync-0.2.5.tar.gz/django-fast-sync-0.2.5/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name='django-fast-sync',
+    version='0.2.5',
+    packages=find_packages(),
+    url='https://github.com/bykof/django-fast-sync',
+    license='Apache License',
+    include_package_data=True,
+    author='mbykovski',
+    author_email='mbykovski@seibert-media.net',
+    description='A Django app which helps you to import a lot of data with raw SQL', requires=['django']
+)
