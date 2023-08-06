a76cadf6fd718c955ccd65de949e6b9c460194f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:50 +0000

    Add package pymorm/0.2/pymorm-0.2.tar.gz

---
+++ packages/pymorm/pymorm-0.2.tar.gz/pymorm-0.2/test/test.py
@@ -0,0 +1,34 @@
+__author__ = 'walter'
+
+from pymorm import MongoObject, MongoObjectMeta
+from pymongo import MongoClient
+
+db = MongoClient("mongodb://localhost:27017/pymorm").get_default_database()
+
+
+class Test(MongoObject):
+    __metaclass__ = MongoObjectMeta
+    __collection__ = db.tests
+
+    __defaults__ = {"username": "Test",
+                    "happiness": lambda: "poor"}
+
+    def test_method(self, test):
+        return test
+
+    @property
+    def test_property(self):
+        return "test"
+
+    @classmethod
+    def test_classmethod(cls):
+        return cls.__name__
+
+
+user = Test.add({})
+user2 = Test.add({"username": "Walter"})
+user.happiness = "a lot!"
+print user
+print user2
+user.commit()
+Test.query.remove()
