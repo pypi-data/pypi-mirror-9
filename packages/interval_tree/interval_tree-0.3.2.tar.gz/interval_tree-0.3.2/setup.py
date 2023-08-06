1b1dc0a04a80fa31b1b7fe59814fb7ee1c9392bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:03 +0000

    Add package interval_tree/0.3.2/interval_tree-0.3.2.tar.gz

---
+++ packages/interval_tree/interval_tree-0.3.2.tar.gz/interval_tree-0.3.2/setup.py
@@ -0,0 +1,36 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+    
+# For making things look nice on pypi:
+long_description = 'Python implemetation of the interval tree.'
+
+setup(name='interval_tree',
+    version='0.3.2',
+    description='Python implemetation of the interval tree',
+    author = 'Mans Magnusson',
+    author_email = 'mans.magnusson@scilifelab.se',
+    url = 'http://github.com/moonso/interval_tree',
+    license = 'MIT License',
+    install_requires=[
+        'pytest'
+    ],
+    packages = [
+        'interval_tree'
+    ],
+    keywords = [
+        'interval tree'
+    ],
+    classifiers = [
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Development Status :: 4 - Beta",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Unix",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+    ],
+    long_description = long_description,
+)
