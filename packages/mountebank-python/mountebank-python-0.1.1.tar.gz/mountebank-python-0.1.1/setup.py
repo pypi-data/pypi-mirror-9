d3c6e3c7d8e78b61f915b8d4d65f34c801a8cca4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:19 +0000

    Add package mountebank-python/0.1.1/mountebank-python-0.1.1.tar.gz

---
+++ packages/mountebank-python/mountebank-python-0.1.1.tar.gz/mountebank-python-0.1.1/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+setup(
+    name = 'mountebank-python',
+    packages = ['mountebank-python'],
+    version = '0.1.1',
+    description = 'A thin convenience wrapper for interacting with Mountebank from Python',
+    author = 'Alex Holyoke',
+    author_email = 'aholyoke@uwaterloo.ca',
+    url = 'https://github.com/aholyoke/mountebank-python',
+    download_url = 'https://github.com/aholyoke/mountebank-python/tarball/0.1',
+    keywords = ['testing', 'mountebank'],
+    classifiers = [],
+    install_requires = [
+    'requests>=2.5.3',
+    ]
+)
