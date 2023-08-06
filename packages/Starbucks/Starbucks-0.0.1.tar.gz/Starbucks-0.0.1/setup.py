e812158776d3575c0ce7c61f17cc233401205d37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:00 +0000

    Add package Starbucks/0.0.1/Starbucks-0.0.1.tar.gz

---
+++ packages/Starbucks/Starbucks-0.0.1.tar.gz/Starbucks-0.0.1/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='Starbucks',
+    packages=['starbucks'],
+    version='0.0.1',
+    description='Unoffical Starbucks API.',
+    long_description=open('README.rst').read(),
+    license='BSD License',
+    author='Minsoo Park',
+    author_email='minsoo1003@gmail.com',
+    url='https://github.com/minsoopark/starbucks-py',
+    keywords=['Starbucks'],
+)
