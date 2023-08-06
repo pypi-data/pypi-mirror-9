a899dfb7c9424115a8980e123afc485689da0d88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:24 +0000

    Add package confpy/0.9.2/confpy-0.9.2.tar.gz

---
+++ packages/confpy/confpy-0.9.2.tar.gz/confpy-0.9.2/setup.py
@@ -0,0 +1,28 @@
+"""Setuptools configuration for confpy."""
+
+from setuptools import setup
+from setuptools import find_packages
+
+
+with open('README.rst', 'r') as readmefile:
+
+    README = readmefile.read()
+
+setup(
+    name='confpy',
+    version='0.9.2',
+    url='https://github.com/kevinconway/confpy',
+    description='Config file parsing and option management.',
+    author="Kevin Conway",
+    author_email="kevinjacobconway@gmail.com",
+    long_description=README,
+    license='MIT',
+    packages=find_packages(exclude=['tests', 'build', 'dist', 'docs']),
+    install_requires=[],
+    entry_points={
+        'console_scripts': [
+            'confpy-generate = confpy.cmd:generate_example',
+        ],
+    },
+    include_package_data=True,
+)
