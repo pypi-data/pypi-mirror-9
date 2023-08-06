b0e6e77862cec3aa2c179c11c1368b0bd7ae5cb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:41 +0000

    Add package pybasicbayes/0.1.1/pybasicbayes-0.1.1.tar.gz

---
+++ packages/pybasicbayes/pybasicbayes-0.1.1.tar.gz/pybasicbayes-0.1.1/setup.py
@@ -0,0 +1,32 @@
+from distutils.core import setup
+
+PYBASICBAYES_VERSION = "0.1.1"
+
+# NOTE: cython and moviepy are optional dependencies
+
+setup(name='pybasicbayes',
+      version=PYBASICBAYES_VERSION,
+      description="Basic utilities for Bayesian inference",
+      author='Matthew James Johnson',
+      author_email='mattjj@csail.mit.edu',
+      url="http://github.com/mattjj/pybasicbayes",
+      maintainer='Matthew James Johnson',
+      maintainer_email='mattjj@csail.mit.edu',
+      packages=['pybasicbayes',
+                  'pybasicbayes.internals',
+                  'pybasicbayes.examples',
+                  'pybasicbayes.util',
+                  'pybasicbayes.testing'],
+      platforms='ALL',
+      keywords=['bayesian', 'inference'],
+      install_requires=[
+          "numpy",
+          "scipy",
+          "matplotlib",
+          "nose",
+      ],
+      classifiers=[
+          'Intended Audience :: Science/Research',
+          'Programming Language :: Python',
+      ])
+
