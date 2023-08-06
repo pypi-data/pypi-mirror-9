125410ae7c502f4f828722b6067dbb78d1a42c51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:17 +0000

    Add package remns/0.1.0.dev4/remns-0.1.0.dev4.tar.gz

---
+++ packages/remns/remns-0.1.0.dev4.tar.gz/remns-0.1.0.dev4/remns/endpoints.py
@@ -0,0 +1,42 @@
+from werkzeug.wrappers import Request, BaseResponse as Response
+
+class EndPoint(object):
+    def __init__(self, template_env):
+        self.template_env = template_env
+                
+    def get(self, request):
+        pass
+
+    def post(self, request):
+        pass
+
+    def put(self, request):
+        pass
+
+    def delete(self, request):
+        pass
+
+    def get_response(self, request):
+        verb_map = {
+            'GET' : self.get,
+            'POST': self.post,
+            'PUT' : self.put
+        }
+        return verb_map[request.method](request)
+
+class ViewPost(EndPoint):
+    def __init__(self, template_env, post_service):
+        super(ViewPost, self).__init__(template_env)
+        self.post_service = post_service
+
+    def get(self, request):
+        print "get request!"
+        print request
+        self.response = "getting posts"
+
+    def post(self, request):
+        print "post request!"
+        print request
+        self.response = "postin posts!"
+
+
