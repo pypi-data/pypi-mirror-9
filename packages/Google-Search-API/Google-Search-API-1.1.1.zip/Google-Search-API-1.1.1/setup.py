5158402bcf802178e94c91fb17aa9d5142b66094
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:03 +0000

    Add package Google-Search-API/1.1.1/Google-Search-API-1.1.1.zip

---
+++ packages/Google-Search-API/Google-Search-API-1.1.1.zip/Google-Search-API-1.1.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(name='Google-Search-API',
+      version='1.1.1',
+      url='https://github.com/abenassi/Google-Search-API',
+      description='Search in google',
+      author='Anthony Casagrande, Agustin Benassi',
+      author_email='birdapi@gmail.com, agusbenassi@gmail.com',
+      license='MIT',
+
+      packages=['google'],
+
+      setup_requires=['nose>=1.0'],
+      test_suite='nose.collector',
+      tests_require=['nose', 'nose-cov']
+      )
