41ff6eec7b8b7830aa9214509287d92e69bb6d5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:44 +0000

    Add package pasttle/0.9.3/pasttle-0.9.3.tar.gz

---
+++ packages/pasttle/pasttle-0.9.3.tar.gz/pasttle-0.9.3/setup.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+#
+# -*- mode:python; sh-basic-offset:4; indent-tabs-mode:nil; coding:utf-8 -*-
+# vim:set tabstop=4 softtabstop=4 expandtab shiftwidth=4 fileencoding=utf-8:
+#
+
+import os
+from src import pasttle
+import sys
+from setuptools import setup
+
+extra = {}
+
+readme = os.path.join(os.path.dirname(sys.argv[0]), 'README.rst')
+requirements = os.path.join(os.path.dirname(sys.argv[0]), 'requirements.txt')
+
+install_requires = open(requirements).readlines(),
+
+if sys.version_info >= (3,):
+    extra['use_2to3'] = True
+else:
+    pass
+
+setup(
+    name='pasttle',
+    packages=[
+        'pasttle',
+    ],
+    package_dir={
+        '': 'src',
+    },
+    package_data={
+        'pasttle': [
+            'views/*.html',
+            'views/css/*.css',
+            'views/images/*',
+        ],
+    },
+    version=pasttle.__version__,
+    url='https://github.com/thekad/pasttle',
+    description='Simple pastebin on top of bottle.',
+    author='Jorge Gallegos',
+    author_email='kad@blegh.net',
+    license='MIT',
+    platforms='any',
+    zip_safe=False,
+    entry_points={
+        'console_scripts': [
+            'pasttle-server.py=pasttle.server:main'
+        ],
+    },
+    classifiers=[
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+    keywords=['pastebin', 'web', 'paste', 'bottlepy'],
+    long_description=open(readme).read(),
+    install_requires=install_requires,
+    test_suite='tests.all.test_suites',
+    **extra
+)
---
+++ packages/pasttle/pasttle-0.9.3.tar.gz/pasttle-0.9.3/src/pasttle/model.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+#
+# -*- mode:python; sh-basic-offset:4; indent-tabs-mode:nil; coding:utf-8 -*-
+# vim:set tabstop=4 softtabstop=4 expandtab shiftwidth=4 fileencoding=utf-8:
+#
+
+import hashlib
+import os
+import sqlalchemy
+from sqlalchemy import func
+from sqlalchemy.ext import declarative
+import util
+
+
+# Subclass declarative base for sqla objects
+Base = declarative.declarative_base()
+
+
+class Paste(Base):
+    """
+    Main paste sqlalchemy construct for database storage
+    """
+
+    __tablename__ = 'paste'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)
+    content = sqlalchemy.Column(sqlalchemy.Text, nullable=False)
+    filename = sqlalchemy.Column(sqlalchemy.String(128))
+    password = sqlalchemy.Column(sqlalchemy.String(40))
+    mimetype = sqlalchemy.Column(sqlalchemy.String(64), nullable=False)
+    lexer = sqlalchemy.Column(sqlalchemy.String(64))
+    created = sqlalchemy.Column(
+        sqlalchemy.DateTime, default=func.now(), nullable=False
+    )
+    ip = sqlalchemy.Column(sqlalchemy.LargeBinary(16))
+    parent = sqlalchemy.Column(sqlalchemy.Integer)
+
+    def __init__(
+        self, content, mimetype, filename=None,
+        password=None, is_encrypted=True, ip=None,
+        lexer=None, parent=None
+    ):
+
+        self.content = content
+        self.mimetype = mimetype
+        if filename and filename.strip():
+            self.filename = os.path.basename(filename).strip()[:128]
+        if password:
+            if is_encrypted:
+                self.password = password[:40]
+            else:
+                self.password = hashlib.sha1(password.encode()).hexdigest()
+        self.ip = ip.encode() if ip else None
+        self.lexer = lexer
+        self.parent = parent
+
+    def __repr__(self):
+        return u'<Paste {0} ({1}), protected={2}>'.format(
+            self.filename, self.lexer or self.mimetype, bool(self.password))
+
+
+engine = sqlalchemy.create_engine(
+    util.conf.get(util.cfg_section, 'dsn'), echo=util.is_debug,
+    convert_unicode=True, logging_name='pasttle.db', echo_pool=util.is_debug,
+    pool_recycle=util.pool_recycle
+)
+# Create all metadata on loading, if something blows we need to know asap
+Base.metadata.create_all(engine)
