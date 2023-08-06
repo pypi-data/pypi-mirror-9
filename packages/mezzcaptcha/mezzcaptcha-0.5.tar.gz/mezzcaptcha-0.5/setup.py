231a4952f9ae3014f139f44960564f4e3d22c00d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:35 +0000

    Add package mezzcaptcha/0.5/mezzcaptcha-0.5.tar.gz

---
+++ packages/mezzcaptcha/mezzcaptcha-0.5.tar.gz/mezzcaptcha-0.5/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(name="mezzcaptcha",
+      version="0.5",
+      description="Custom mezzanine forms with captchas",
+      long_description="""\
+Includes a comment form that will display a captcha to guest users
+but will not display a captcha to logged in uers.
+""",
+      author="Paul Hunt",
+      author_email="paul@phookit.com",
+      url="https://github.com/phookit/phookitmezz-captcha",
+      license="BSD",
+      packages=find_packages(),
+      include_package_data=True,
+      install_requires=['django-simple-captcha>=0.4.4',],
+)
+
