dee7d8c4bbaa2d92b94ac36300c3a1ec4bc606ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:29:55 +0000

    Add package seltest/0.1.5/seltest-0.1.5.tar.gz

---
+++ packages/seltest/seltest-0.1.5.tar.gz/seltest-0.1.5/seltest/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+seltest means easy browser-based testing with no overhead.
+"""
+from seltest import Base, BaseMeta
+from helpers import url, waitfor, dontwaitfor, hide
+import seltest
+
+__all__ = ['Base', 'url', 'waitfor', 'dontwaitfor']
+__author__ = 'Isaac Hodes <isaachodes@gmail.com>'
+__version__ = '0.1.5'
+
