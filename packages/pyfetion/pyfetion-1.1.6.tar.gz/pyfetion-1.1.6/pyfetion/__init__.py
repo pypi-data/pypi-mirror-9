1818ed7a3decf3016fd8626647643c6ccb3de5a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:06 +0000

    Add package pyfetion/1.1.6/pyfetion-1.1.6.tar.gz

---
+++ packages/pyfetion/pyfetion-1.1.6.tar.gz/pyfetion-1.1.6/pyfetion/__init__.py
@@ -0,0 +1,6 @@
+# coding=utf-8
+
+from .fetion import sendMessage, sendFetionGroupMessage, \
+        sendFetionGroupsMessage, addFetionFriend, \
+        sendFetionAllGroupsMessage, \
+        sendWeather, sendGroupWeather
