6012bc03827dc80dd9fa93a992da290bb4a55e28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:23:00 +0000

    Add package charles87_002/2.0.0/charles87_002-2.0.0.zip

---
+++ packages/charles87_002/charles87_002-2.0.0.zip/charles87_002-2.0.0/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+setup(
+    name='charles87_002',
+    version='2.0.0',
+    py_module=['charles87_002'],
+    author='chlee',
+    author_email='rlacksql@hanmail.net',
+    description='A multiple data managing program.[new published]')
