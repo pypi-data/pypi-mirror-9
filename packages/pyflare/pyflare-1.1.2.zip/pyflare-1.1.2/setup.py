73edffe8672e2bd4e120927b33db5c6cc484b99b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:13 +0000

    Add package pyflare/1.1.2/pyflare-1.1.2.zip

---
+++ packages/pyflare/pyflare-1.1.2.zip/pyflare-1.1.2/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+long_description = open('README.rst').read()
+
+packages = find_packages(exclude=['tests", "tests.*'])
+
+setup(
+    name='pyflare',
+    version='1.1.2',
+    packages=packages,
+    url='https://github.com/jlinn/pyflare',
+    license='LICENSE.txt',
+    author='Joe Linn',
+    author_email='',
+    description="An adapter for CloudFlare's client API",
+    long_description=long_description,
+    install_requires=['requests'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Development Status :: 5 - Production/Stable',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP'
+    ]
+)
