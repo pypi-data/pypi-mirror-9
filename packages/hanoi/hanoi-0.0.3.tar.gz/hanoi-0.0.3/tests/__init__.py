f2ce902f6d74e4a029c7124cf3e9e21e3f2031a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:07:59 +0000

    Add package hanoi/0.0.3/hanoi-0.0.3.tar.gz

---
+++ packages/hanoi/hanoi-0.0.3.tar.gz/hanoi-0.0.3/tests/__init__.py
@@ -0,0 +1,19 @@
+import unittest
+
+from .api import (
+    FeatureTestCase, RolloutTestCase,
+    RolloutWithRedisTestCase, RolloutWithRedisHighPerfTestCase
+)
+
+from .backend import RedisBackEndTestCase, RedisHighPerfBackEndTestCase
+
+
+def all_tests():
+    suite = unittest.TestSuite()
+    suite.addTest(unittest.makeSuite(FeatureTestCase))
+    suite.addTest(unittest.makeSuite(RolloutTestCase))
+    suite.addTest(unittest.makeSuite(RolloutWithRedisTestCase))
+    suite.addTest(unittest.makeSuite(RolloutWithRedisHighPerfTestCase))
+    suite.addTest(unittest.makeSuite(RedisBackEndTestCase))
+    suite.addTest(unittest.makeSuite(RedisHighPerfBackEndTestCase))
+    return suite
