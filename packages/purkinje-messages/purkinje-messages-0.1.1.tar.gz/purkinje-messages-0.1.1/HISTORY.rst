6f63ee63be954da4d8f35dd7c10ae14a1271b3ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:02 +0000

    Add package purkinje-messages/0.1.1/purkinje-messages-0.1.1.tar.gz

---
+++ packages/purkinje-messages/purkinje-messages-0.1.1.tar.gz/purkinje-messages-0.1.1/HISTORY.rst
@@ -0,0 +1,6 @@
+History
+=======
+
+- Dec 11, 2014: Start of Development
+- Mar 01, 2015: Release of version 0.1.0
+- Mar 01, 2015: Release of version 0.1.1
---
+++ packages/purkinje-messages/purkinje-messages-0.1.1.tar.gz/purkinje-messages-0.1.1/setup.py
@@ -0,0 +1,53 @@
+import sys
+from setuptools import setup
+from setuptools.command.test import test as TestCommand
+
+readme = open('README.rst').read()
+history = open('HISTORY.rst').read()
+
+
+def parse_requirements():
+    with open('requirements.txt') as req:
+        return [x for x in req.readlines() if not x.startswith('-e ')]
+
+
+class Tox(TestCommand):
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        # import here, cause outside the eggs aren't loaded
+        import tox
+        errcode = tox.cmdline(self.test_args)
+        sys.exit(errcode)
+
+setup(name='purkinje-messages',
+      version='0.1.1',
+      description='message definitions for purkinje test runner',
+      long_description=readme + '\n\n' + history,
+      author='Bernhard (Bernd) Biskup',
+      author_email='bbiskup@gmx.de',
+      url='biskup-software.de',
+      package_dir={'purkinje_messages': 'purkinje_messages'},
+      packages=['purkinje_messages'],
+      zip_safe=False,
+      include_package_data=True,
+      install_requires=parse_requirements(),
+      classifiers=[
+          'Development Status :: 2 - Pre-Alpha',
+          'Environment :: Console',
+          'Environment :: Web Environment',
+          'Intended Audience :: Developers',
+          'Topic :: Software Development :: Testing',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 2.7',
+          # 'Programming Language :: Python :: 3.4',
+      ],
+      license='The MIT License (MIT)',
+      keywords='purkinje pytest testrunner websockets',
+      tests_require=['tox'],
+      cmdclass={'test': Tox}
+      )
