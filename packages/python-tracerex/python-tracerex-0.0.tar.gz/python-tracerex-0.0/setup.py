41122e44488cf2f98bd8b206dd923750edb6b7da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:44 +0000

    Add package python-tracerex/0.0/python-tracerex-0.0.tar.gz

---
+++ packages/python-tracerex/python-tracerex-0.0.tar.gz/python-tracerex-0.0/setup.py
@@ -0,0 +1,16 @@
+# encoding: utf8
+
+from setuptools import setup
+
+setup(
+    name='python-tracerex',
+    version="0.0",
+    url='https://pypi.python.org/pypi/hunter/',
+    description='Hunter is a flexible code tracing toolkit.',
+    long_description='''Use `hunter <https://pypi.python.org/pypi/hunter/>`_ instead.''',
+    author='Ionel Cristian M\xc4\x83rie\xc8\x99',
+    platforms=['all'],
+    zip_safe=False,
+    author_email='contact@ionelmc.ro',
+    install_requires=['hunter'],
+)
