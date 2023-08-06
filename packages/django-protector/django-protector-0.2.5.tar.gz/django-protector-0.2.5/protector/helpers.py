d872277a4c1f462d492665c638980b44bc7f6c03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:01 +0000

    Add package django-protector/0.2.5/django-protector-0.2.5.tar.gz

---
+++ packages/django-protector/django-protector-0.2.5.tar.gz/django-protector-0.2.5/protector/helpers.py
@@ -0,0 +1,90 @@
+from django.contrib.auth.models import Permission
+from django.contrib.auth import get_user_model
+from django.contrib.contenttypes.models import ContentType
+from protector.query import Query
+from protector.models import get_permission_owners_query, OwnerToPermission, \
+    NULL_OWNER_TO_PERMISSION_OBJECT_ID, NULL_OWNER_TO_PERMISSION_CTYPE_ID
+
+
+NULL_OBJECT_CONDITION = " op.object_id = {null_id!s} AND op.content_type_id = {null_ctype!s} ".format(
+    null_id=NULL_OWNER_TO_PERMISSION_OBJECT_ID,
+    null_ctype=NULL_OWNER_TO_PERMISSION_CTYPE_ID
+)
+
+
+def get_all_permission_owners(permission, include_superuser=False, include_groups=True, obj=None):
+    query = _get_permissions_query(obj)
+    query.fields.append("gug.user_id AS id")
+    query.conditions.append("op.permission_id = {perm_id!s}")
+    query.params.update({'perm_id': permission.id})
+    table_name = get_user_model()._meta.db_table
+    condition = "{table_name!s}.id IN ({subquery!s})"
+    if include_superuser:
+        condition += " OR {table_name!s}.is_superuser = True"
+    condition = condition.format(
+        table_name=table_name, subquery=query.get_raw_query()
+    )
+    return get_user_model().objects.extra(where=[condition])
+
+
+def get_all_user_permissions(user, obj=None):
+    perm_query = _get_permissions_query(obj)
+    perm_query.fields.append("IFNULL(op.permission_id, gl.permission_id) as perm_id")
+    perm_query.fields.append("gl.permission_id AS gl_perm_id")
+    perm_query.conditions.append("gug.user_id = {user_id!s}".format(user_id=user.id))
+    query = Query(tables=[
+        """
+            {permission_table!s} perm_table LEFT JOIN {content_type_table!s} ctype_table ON
+            perm_table.content_type_id=ctype_table.id INNER JOIN (
+                {permission_owners_query!s}
+            ) perm_query ON perm_query.perm_id = perm_table.id
+        """.format(
+            permission_table=Permission._meta.db_table,
+            content_type_table=ContentType._meta.db_table,
+            permission_owners_query=perm_query.get_raw_query(),
+        )
+    ])
+    query.fields.append("perm_table.id as id")
+    query.fields.append("perm_table.codename as codename")
+    query.fields.append("ctype_table.app_label as app_label")
+    perms = Permission.objects.raw(query.get_raw_query())
+    return set("%s.%s" % (p.app_label, p.codename) for p in perms)
+
+
+def get_permission_owners_of_type_for_object(permission, owner_content_type, content_object):
+    qs = OwnerToPermission.objects.filter(
+        content_type=ContentType.objects.get_for_model(content_object._meta.model),
+        object_id=content_object.pk,
+        owner_content_type=owner_content_type,
+        permission=permission
+    )
+    return owner_content_type.model_class().objects.filter(
+        id__in=qs.values_list('owner_object_id', flat=True)
+    )
+
+
+def _get_permissions_query(obj=None):
+    query = Query(
+        tables=[get_permission_owners_query()]
+    )
+    if obj is None:
+        query.conditions.append(NULL_OBJECT_CONDITION)
+    else:
+        query.params.update({
+            'object_pk': obj.pk, 'ctype_pk': ContentType.objects.get_for_model(obj).pk,
+            'null_object_condition': NULL_OBJECT_CONDITION,
+            'null_object_id': NULL_OWNER_TO_PERMISSION_OBJECT_ID
+        })
+        query.conditions.append(
+            """
+                ({null_object_condition!s})
+                OR (op.content_type_id = {ctype_pk!s} AND
+                    (op.object_id = {object_pk!s} OR op.object_id = {null_object_id!s})
+                )
+                OR (
+                    gl.content_type_id = gug.group_content_type_id AND
+                    gug.group_id = {object_pk!s} AND gug.group_content_type_id = {ctype_pk!s}
+                )
+            """
+        )
+    return query
---
+++ packages/django-protector/django-protector-0.2.5.tar.gz/django-protector-0.2.5/protector/tests.py
@@ -0,0 +1,348 @@
+from django.test import TestCase
+from django.contrib.auth.models import Permission
+from django.contrib.auth import get_user_model
+from django.contrib.contenttypes.models import ContentType
+from django.test.utils import override_settings
+from protector.models import get_user_ctype, GenericGlobalPerm, \
+    get_default_group_ctype, OwnerToPermission, GenericUserToGroup
+from protector.helpers import get_all_permission_owners, get_permission_owners_of_type_for_object
+
+
+TestUser = get_user_model()
+
+
+@override_settings(
+    AUTHENTICATION_BACKENDS=('protector.backends.GenericPermissionBackend',),
+    DISABLE_GENERIC_PERMISSION_CACHE=True
+)
+class GenericObjectRestrictionTest(TestCase):
+
+    def setUp(cls):
+        cls.TestGroup = get_default_group_ctype().model_class()
+        cls.user = TestUser.objects.create(username='test1', email='test@test.com')
+        cls.user2 = TestUser.objects.create(username='test2', email='test2@test.com')
+        cls.user3 = TestUser.objects.create(username='test3', email='test3@test.com')
+        cls.permission = Permission.objects.create(
+            codename='test', content_type=get_user_ctype()
+        )
+        cls.permission2 = Permission.objects.create(
+            codename='test2', content_type=get_user_ctype()
+        )
+        cls.permission_key = get_user_ctype().app_label + '.test'
+        cls.permission2_key = get_user_ctype().app_label + '.test2'
+        cls.group = cls.TestGroup.objects.create(
+            name='test_group'
+        )
+        cls.group2 = cls.TestGroup.objects.create(
+            name='test_group2'
+        )
+        cls.group2.restrict()
+        cls.group2.save()
+
+    def test_object_perm(self):
+        self.assertFalse(
+            self.user.has_perm(self.permission_key, self.user2)
+        )
+        self.user.permissions.add(
+            self.permission, self.user2
+        )
+        self.assertTrue(
+            self.user.has_perm(self.permission_key, self.user2)
+        )
+
+    def test_object_global_perm(self):
+        self.assertFalse(
+            self.user.has_perm(self.permission_key, self.user2)
+        )
+        self.user.permissions.add(self.permission)
+        self.assertTrue(
+            self.user.has_perm(self.permission_key, self.user2)
+        )
+
+    def test_object_group_perm_add_remove(self):
+        self.assertFalse(
+            self.user2.has_perm(self.permission_key, self.user)
+        )
+        self.group.permissions.add(self.permission, self.user)
+        self.group.users.add(self.user2)
+        self.assertTrue(
+            self.user2.has_perm(self.permission_key, self.user)
+        )
+        self.group.permissions.remove(self.permission, self.user)
+        self.assertFalse(
+            self.user2.has_perm(self.permission_key, self.user)
+        )
+
+    def test_group_perm(self):
+        self.user2.groups.add(self.group)
+        self.group.permissions.add(self.permission)
+        self.group.permissions.add(self.permission2)
+        self.assertTrue(
+            self.user2.has_perm(self.permission_key)
+        )
+        self.group.permissions.remove(self.permission)
+        self.assertFalse(
+            self.user2.has_perm(self.permission_key)
+        )
+        self.assertTrue(
+            self.user2.has_perm(self.permission2_key)
+        )
+        self.group.users.remove(self.user2)
+        self.assertFalse(
+            self.user2.has_perm(self.permission2_key)
+        )
+
+    def test_restricted_query_set(self):
+        self.assertEquals(
+            self.TestGroup.objects.count(), 2
+        )
+        self.assertEquals(
+            self.TestGroup.objects.visible().count(), 1
+        )
+        self.assertEquals(
+            self.TestGroup.objects.visible(self.user).count(), 1
+        )
+        self.user.permissions.add(
+            self.TestGroup.get_view_permission()
+        )
+        self.assertEquals(
+            self.TestGroup.objects.visible(self.user).count(), 2
+        )
+
+    def test_restricted_query_set_object_permission(self):
+        self.assertEquals(
+            self.TestGroup.objects.visible(self.user2).count(), 1
+        )
+        self.user2.permissions.add(
+            self.TestGroup.get_view_permission(), self.group2
+        )
+        qset = self.TestGroup.objects.visible(self.user2)
+        self.assertEquals(qset.count(), 2)
+        self.assertEquals(qset.filter(name=self.group2.name).count(), 1)
+
+    def test_user_roles(self):
+        DEFAULT = 1
+        ROLE2 = 2
+        ROLE3 = 4
+        self.assertEquals(
+            self.group.users.count(), 0
+        )
+        self.group.users.add(self.user)
+        self.assertEquals(
+            self.group.users.count(), 1
+        )
+        self.group.users.add(self.user2, roles=DEFAULT+ROLE2)
+
+        self.assertEquals(
+            self.group.users.by_role(roles=DEFAULT).count(), 2
+        )
+        self.assertEquals(
+            self.group.users.by_role(roles=ROLE2).count(), 1
+        )
+        self.assertEquals(
+            self.group.users.by_role(roles=ROLE3).count(), 0
+        )
+        self.group.users.add(self.user3, roles=ROLE3)
+        self.assertEquals(
+            self.group.users.by_role(roles=ROLE2+ROLE3).count(), 2
+        )
+
+    def test_content_type_perm(self):
+        DEFAULT = 1
+        ROLE2 = 2
+        self.group.users.add(self.user2, roles=ROLE2)
+        self.group.users.add(self.user, roles=DEFAULT)
+        GenericGlobalPerm.objects.create(
+            content_type=ContentType.objects.get_for_model(self.TestGroup),
+            roles=ROLE2, permission=self.permission2
+        )
+        self.assertTrue(
+            self.user2.has_perm(self.permission2_key, self.group)
+        )
+        self.assertFalse(
+            self.user2.has_perm(self.permission2_key, self.group2)
+        )
+        self.assertFalse(
+            self.user.has_perm(self.permission2_key, self.group)
+        )
+
+    def test_qset_ctype_perm(self):
+        ROLE2 = 2
+        self.assertEquals(
+            self.TestGroup.objects.visible(self.user2).count(), 1
+        )
+        GenericGlobalPerm.objects.create(
+            content_type=ContentType.objects.get_for_model(self.TestGroup),
+            roles=ROLE2, permission=self.TestGroup.get_view_permission()
+        )
+        self.group.users.add(self.user)
+        self.group2.users.add(self.user2, roles=ROLE2)
+        self.assertEquals(
+            self.TestGroup.objects.visible(self.user2).count(), 2
+        )
+        self.assertEquals(
+            self.TestGroup.objects.visible(self.user).count(), 1
+        )
+
+    def test_all_permission_owners(self):
+        self.user.permissions.add(self.permission)
+        self.assertEquals(
+            get_all_permission_owners(self.permission).count(), 1
+        )
+        self.group.permissions.add(self.permission)
+        self.group.users.add(self.user2)
+        self.assertEquals(
+            get_all_permission_owners(self.permission).count(), 2
+        )
+
+    def test_unrestrict(self):
+        self.group.restrict()
+        self.assertEquals(
+            self.TestGroup.objects.visible(self.user2).count(), 0
+        )
+        self.group.unrestrict()
+        self.assertEquals(
+            self.TestGroup.objects.visible(self.user2).count(), 1
+        )
+
+    def test_ctype_owners(self):
+        owners = get_permission_owners_of_type_for_object(
+            permission=self.TestGroup.get_view_permission(),
+            owner_content_type=ContentType.objects.get_for_model(TestUser),
+            content_object=self.group2
+        )
+        self.assertEquals(
+            owners.count(), 0
+        )
+        self.group2.add_viewer(self.user2)
+        self.assertEquals(
+            owners.count(), 1
+        )
+
+    def test_ctype_permission(self):
+        groups = self.TestGroup.objects.visible(self.user2)
+        self.assertFalse(
+            self.user2.has_perm(self.TestGroup.get_view_permission_name(), self.group2)
+        )
+        self.assertEquals(
+            groups.count(), 1
+        )
+        OwnerToPermission.objects.create(
+            owner=self.user2,
+            content_type=ContentType.objects.get_for_model(self.TestGroup),
+            permission=self.TestGroup.get_view_permission(),
+        )
+        self.assertEquals(
+            groups.count(), 2
+        )
+        self.assertTrue(
+            self.user2.has_perm(self.TestGroup.get_view_permission_name(), self.group2)
+        )
+
+    def test_superuser(self):
+        groups = self.TestGroup.objects.visible(self.user2)
+        self.assertFalse(
+            self.user2.has_perm(self.TestGroup.get_view_permission_name(), self.group2)
+        )
+        self.assertEquals(
+            groups.count(), 1
+        )
+        self.user2.is_superuser = True
+        groups = self.TestGroup.objects.visible(self.user2)
+        self.assertEquals(
+            groups.count(), 2
+        )
+        self.assertTrue(
+            self.user2.has_perm(self.TestGroup.get_view_permission_name(), self.group2)
+        )
+
+    def test_groups_by_ctype(self):
+        DEFAULT = 1
+        ROLE2 = 2
+        self.group2.users.add(self.user2, DEFAULT)
+        self.assertEquals(
+            self.user2.groups.by_ctype(
+                ContentType.objects.get_for_model(self.group2), DEFAULT
+            ).count(), 1
+        )
+        self.assertEquals(
+            self.user2.groups.by_ctype(
+                ContentType.objects.get_for_model(self.group2), ROLE2
+            ).count(), 0
+        )
+
+    def test_user_to_group_by_role(self):
+        utg_qset = GenericUserToGroup.objects.filter(
+            group_id=self.group2.pk,
+            group_content_type=ContentType.objects.get_for_model(self.group2),
+            user=self.user2
+        )
+        DEFAULT = 1
+        ROLE2 = 2
+        ROLE3 = 4
+        self.group2.users.add(self.user2, ROLE2)
+        self.assertEquals(
+            utg_qset.by_role(DEFAULT).count(), 0
+        )
+        self.assertEquals(
+            utg_qset.by_role(ROLE2).count(), 1
+        )
+        self.group2.users.add(self.user2, ROLE3)
+        self.assertEquals(
+            utg_qset.by_role(ROLE2).count(), 1
+        )
+        self.user2.groups.remove(self.group2, ROLE2)
+        self.assertEquals(
+            utg_qset.by_role(ROLE2).count(), 0
+        )
+        self.assertEquals(
+            utg_qset.by_role(ROLE3).count(), 1
+        )
+        self.user2.groups.remove(self.group2, ROLE3)
+        self.user2.groups.remove(self.group2, ROLE3)  # Test DoesNotExist
+        self.assertEquals(
+            utg_qset.by_role(ROLE3).count(), 0
+        )
+
+    def test_permissioned_manager(self):
+        groups = self.TestGroup.by_perm.filter_by_permission(
+            self.user2, self.TestGroup.get_view_permission_name()
+        )
+        self.assertEquals(
+            groups.count(), 0
+        )
+        self.user2.permissions.add(self.group2.get_view_permission(), self.group2)
+        self.assertEquals(
+            groups.count(), 1
+        )
+
+    def test_owner_to_permission_unicode(self):
+        OwnerToPermission.objects.create(
+            owner=self.user2,
+            content_type=ContentType.objects.get_for_model(self.TestGroup),
+            permission=self.TestGroup.get_view_permission(),
+        )
+        otps = [otp.__unicode__() for otp in OwnerToPermission.objects.all()]
+        self.assertEquals(
+            otps[0], u'test_app.testuser.2 Roles 1. Permission view_restricted_objects'
+        )
+
+    def test_has_perms(self):
+        self.user2.permissions.add(self.permission)
+        self.assertFalse(
+            self.user2.has_perms([self.permission_key, self.permission2_key])
+        )
+        self.user2.permissions.add(self.permission2)
+        self.assertTrue(
+            self.user2.has_perms([self.permission_key, self.permission2_key])
+        )
+
+    def test_has_module_perms(self):
+        app_label = get_user_model()._meta.app_label 
+        self.assertFalse(
+            self.user2.has_module_perms(app_label)
+        )
+        self.user2.permissions.add(self.permission)
+        self.assertTrue(
+            self.user2.has_module_perms(app_label)
+        )
