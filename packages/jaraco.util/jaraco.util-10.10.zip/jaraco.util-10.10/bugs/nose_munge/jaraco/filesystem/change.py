8160ab1933f2e606057e883f3da3d30049d797c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:11 +0000

    Add package jaraco.util/10.10/jaraco.util-10.10.zip

---
+++ packages/jaraco.util/jaraco.util-10.10.zip/jaraco.util-10.10/bugs/nose_munge/jaraco/filesystem/change.py
@@ -0,0 +1,8 @@
+from jaraco.util.string import multi_substitution
+
+def call_multisub():
+	r"""
+	>>> call_multisub()
+	''
+	"""
+	return multi_substitution()('')
---
+++ packages/jaraco.util/jaraco.util-10.10.zip/jaraco.util-10.10/jaraco/util/editor.py
@@ -0,0 +1,5 @@
+import warnings
+
+from jaraco.ui.editor import *
+
+warnings.warn("Use jaraco.ui package", DeprecationWarning)
