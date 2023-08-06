e2690bc1cac5d9f3e4ac8e6b90bb937499577241
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:29:54 +0000

    Add package Janome/0.1.2/Janome-0.1.2.tar.gz

---
+++ packages/Janome/Janome-0.1.2.tar.gz/Janome-0.1.2/README.rst
@@ -0,0 +1,52 @@
+========
+janome
+========
+
+Janome is a Japanese morphological analysis engine written in pure Python.
+
+For general documentation: http://mocobeta.github.io/janome/ (for Japanese)
+
+Requirements
+=============
+
+Python 3.4 or above is required.
+
+Install
+========
+
+.. code:: bash
+
+  (venv) $ python setup.py install
+  Finished processing dependencies for Janome==0.1.1
+
+  (venv) $ pip freeze
+  Janome==0.1.1
+
+Run
+====
+
+.. code:: bash
+
+  (venv) $ python
+  >>> from janome.tokenizer import Tokenizer
+  >>> t = Tokenizer()
+  >>> for token in t.tokenize('すもももももももものうち'):
+  ...     print(token)
+  ...
+  すもも 名詞,一般,*,*,*,*,すもも,スモモ,スモモ
+  も    助詞,係助詞,*,*,*,*,も,モ,モ
+  もも  名詞,一般,*,*,*,*,もも,モモ,モモ
+  も    助詞,係助詞,*,*,*,*,も,モ,モ
+  もも  名詞,一般,*,*,*,*,もも,モモ,モモ
+  の    助詞,連体化,*,*,*,*,の,ノ,ノ
+  うち  名詞,非自立,副詞可能,*,*,*,うち,ウチ,ウチ
+
+License
+========
+
+Apache License 2.0
+
+Copyright
+==========
+
+Copyright(C) @moco_beta All rights reserved.
---
+++ packages/Janome/Janome-0.1.2.tar.gz/Janome-0.1.2/setup.py
@@ -0,0 +1,61 @@
+from setuptools import setup
+
+import os
+from zipfile import ZipFile
+import py_compile
+from janome.dic import *
+
+dicdir = 'ipadic'
+
+if not os.path.exists('sysdic') and os.path.exists(os.path.join('ipadic', 'sysdic.zip')):
+    print('Unzip dictionary data...')
+    with ZipFile(os.path.join(dicdir, 'sysdic.zip')) as zf:
+        zf.extractall()
+
+#print('Precompile dictionary data...')
+#py_compile.compile(os.path.join('sysdic', MODULE_FST_DATA))
+#py_compile.compile(os.path.join('sysdic', MODULE_ENTRIES))
+#py_compile.compile(os.path.join('sysdic', MODULE_CONNECTIONS))
+#py_compile.compile(os.path.join('sysdic', MODULE_CHARDEFS))
+#py_compile.compile(os.path.join('sysdic', MODULE_UNKNOWNS))
+
+version = '0.1.2'
+name = 'janome'
+short_description = '`janome` is a package for Japanese Morphological Analysis.'
+long_description = """\
+`janome` is a package for Japanese Morphological Analysis.
+
+Requirements
+------------
+* Python 3.4 or later (not support 2.x)
+
+Features
+--------
+See http://mocobeta.github.io/janome/ (for Japanese)
+
+History
+-------
+0.1.2 (2015-4-8)
+~~~~~~~~~~~~~~~~~~
+* first release
+
+"""
+
+classifiers = [
+    "Development Status :: 3 - Alpha",
+    "License :: OSI Approved :: Apache License 2.0",
+    "Programming Language :: Python",
+    "Topic :: Natural Language :: Japanese",
+    ]
+
+setup(
+    name='Janome',
+    version='0.1.2',
+    description='Japanese morphological analysis engine.',
+    author='Tomoko Uchida',
+    author_email='tomoko.uchida.1111@gmail.com',
+    url='http://mocobeta.github.io/janome/',
+    packages=['janome','sysdic'],
+    py_modules=['janome.dic','janome.fst','janome.lattice','janome.tokenizer']
+)
+
