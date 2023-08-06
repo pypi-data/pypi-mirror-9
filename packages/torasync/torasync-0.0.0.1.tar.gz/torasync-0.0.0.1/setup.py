6fb4dd1d81dea654e530772466312db5d159fb3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:18 +0000

    Add package torasync/0.0.0.1/torasync-0.0.0.1.tar.gz

---
+++ packages/torasync/torasync-0.0.0.1.tar.gz/torasync-0.0.0.1/setup.py
@@ -0,0 +1,17 @@
+#coding=utf8
+__author__ = 'Alexander.Li'
+
+from setuptools import setup
+
+setup(
+    name='torasync',
+    version='0.0.0.1',
+    packages=['torasync'],
+    author='Alexander.Li',
+    author_email='superpowerlee@gmail.com',
+    license='LGPL',
+    install_requires=["tornado>=2.4.1",],
+    description="Run task asynchronously in other processes in easy way",
+    keywords ='tornado asynchronous multiprocess',
+    url="https://github.com/ipconfiger/torasync"
+)
