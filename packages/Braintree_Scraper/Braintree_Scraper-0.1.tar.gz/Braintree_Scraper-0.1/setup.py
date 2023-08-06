ccf2bbe30db0dd19dd0840d2fdb416462aef6e38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:37:46 +0000

    Add package Braintree_Scraper/0.1/Braintree_Scraper-0.1.tar.gz

---
+++ packages/Braintree_Scraper/Braintree_Scraper-0.1.tar.gz/Braintree_Scraper-0.1/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+  name = 'Braintree_Scraper',
+  packages = ['Braintree_Scraper'], # this must be the same as the name above
+  version = '0.1',
+  description = 'This module allows you to easily scrape transactions from braintree',
+  author = 'Dylan Hercher',
+  author_email = 'dylanhercher@gmail.com',
+  url = 'https://github.com/dhercher/Braintree_Scraper', # use the URL to the github repo
+  download_url = 'https://github.com/dhercher/Braintree_Scraper/tarball/0.1', # I'll explain this in a second
+  keywords = ['scraper', 'braintree', 'python'], # arbitrary keywords
+  classifiers = [],
+)
