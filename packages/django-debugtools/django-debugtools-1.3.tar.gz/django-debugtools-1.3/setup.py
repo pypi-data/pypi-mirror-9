ea64f2b920a004ec10f9f193c06b23905b67582a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:02 +0000

    Add package django-debugtools/1.3/django-debugtools-1.3.tar.gz

---
+++ packages/django-debugtools/django-debugtools-1.3.tar.gz/django-debugtools-1.3/setup.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+from os import path
+import codecs
+import os
+import re
+import sys
+
+
+def read(*parts):
+    file_path = path.join(path.dirname(__file__), *parts)
+    return codecs.open(file_path, encoding='utf-8').read()
+
+
+def find_version(*parts):
+    version_file = read(*parts)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", version_file, re.M)
+    if version_match:
+        return str(version_match.group(1))
+    raise RuntimeError("Unable to find version string.")
+
+
+setup(
+    name='django-debugtools',
+    version=find_version('debugtools', '__init__.py'),
+    license='Apache 2.0',
+
+    description='A toolbox of small utilities to assist Django development',
+    long_description=read('README.rst'),
+
+    author='Diederik van der Boor',
+    author_email='opensource@edoburu.nl',
+
+    url='https://github.com/edoburu/django-debugtools',
+    download_url='https://github.com/edoburu/django-debugtools/zipball/master',
+
+    packages=find_packages(exclude=('example*',)),
+    include_package_data=True,
+
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Framework :: Django',
+        'Framework :: Django :: 1.4',
+        'Framework :: Django :: 1.5',
+        'Framework :: Django :: 1.6',
+        'Framework :: Django :: 1.7',
+        'Framework :: Django :: 1.8',
+        'Topic :: Software Development :: Debuggers',
+    ]
+)
