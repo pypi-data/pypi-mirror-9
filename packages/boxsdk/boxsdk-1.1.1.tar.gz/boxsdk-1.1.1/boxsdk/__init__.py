8b911d8ed46923acb6db5ce382a604b122b6d693
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:36:05 +0000

    Add package boxsdk/1.1.1/boxsdk-1.1.1.tar.gz

---
+++ packages/boxsdk/boxsdk-1.1.1.tar.gz/boxsdk-1.1.1/boxsdk/__init__.py
@@ -0,0 +1,7 @@
+# coding: utf-8
+
+from __future__ import unicode_literals
+
+from .auth.oauth2 import OAuth2
+from .client import Client
+from .object import *  # pylint:disable=wildcard-import,redefined-builtin
