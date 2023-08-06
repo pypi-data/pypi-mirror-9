03fdf2d5c80cf8eaee64b3fb342a8ba7184fbe7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:22 +0000

    Add package pip-init/0.2.1/pip-init-0.2.1.tar.gz

---
+++ packages/pip-init/pip-init-0.2.1.tar.gz/pip-init-0.2.1/pip_init/templates.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+from string import Template
+from textwrap import dedent
+
+setup_base_template = Template(dedent("""\
+    from setuptools import setup, find_packages
+
+    try:
+        long_description = open("README.rst").read()
+    except IOError:
+        long_description = ""
+
+    setup(
+    ${setup_lines}\
+    packages=find_packages(),
+        install_requires=[],
+        long_description=long_description,
+    ${classifiers}
+    )
+    """))
+
+setup_line = Template(
+    '    ${name}="${value}",\n'
+)
+
+classifiers_line = Template(
+    '        "${classifier}",\n'
+)
+
+classifiers_template = Template(
+    '    classifiers=[\n'
+    '${classifiers}'
+    '    ]'
+)
