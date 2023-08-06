f137e4081b75a19de905bd23c48800f1ad711b7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:26 +0000

    Add package edgegrid-python/1.0.6/edgegrid-python-1.0.6.tar.gz

---
+++ packages/edgegrid-python/edgegrid-python-1.0.6.tar.gz/edgegrid-python-1.0.6/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+setup(
+    name='edgegrid-python', 
+    version='1.0.6', 
+    description='{OPEN} client authentication protocol for python-requests',
+    author='Jonathan Landis',
+    author_email='jlandis@akamai.com',
+    url='https://github.com/akamai-open/AkamaiOPEN-edgegrid-python',
+    namespace_packages=['akamai'],
+    packages=find_packages(),
+    install_requires = [
+        'requests>=1.2.2'
+    ],
+    license='LICENSE.txt'
+)
