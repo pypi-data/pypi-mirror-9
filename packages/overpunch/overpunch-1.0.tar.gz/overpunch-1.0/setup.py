2050b910bc61a2ce35852a95066a07e5eb19cfae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:23 +0000

    Add package overpunch/1.0/overpunch-1.0.tar.gz

---
+++ packages/overpunch/overpunch-1.0.tar.gz/overpunch-1.0/setup.py
@@ -0,0 +1,30 @@
+# Copyright 2015, Truveris Inc.
+
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup, find_packages
+
+from overpunch import __version__
+
+
+setup(
+    name="overpunch",
+    version=__version__,
+    description="Overpunch Parser/Formatter",
+    author="Truveris Inc.",
+    author_email="dev@truveris.com",
+    url="http://github.com/truveris/overpunch",
+    test_suite="nose.collector",
+    packages=find_packages(exclude=["ez_setup"]),
+    classifiers=[
+        "Intended Audience :: Healthcare Industry",
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 2.7",
+        "Topic :: Text Processing",
+    ],
+)
+
