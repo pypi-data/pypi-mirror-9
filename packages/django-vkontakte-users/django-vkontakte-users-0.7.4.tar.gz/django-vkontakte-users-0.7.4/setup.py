300e3e19c75add4c09457c5165eefd62c59d669f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:19 +0000

    Add package django-vkontakte-users/0.7.4/django-vkontakte-users-0.7.4.tar.gz

---
+++ packages/django-vkontakte-users/django-vkontakte-users-0.7.4.tar.gz/django-vkontakte-users-0.7.4/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='django-vkontakte-users',
+    version=__import__('vkontakte_users').__version__,
+    description='Django implementation for vkontakte API Users',
+    long_description=open('README.md').read(),
+    author='ramusus',
+    author_email='ramusus@gmail.com',
+    url='https://github.com/ramusus/django-vkontakte-users',
+    download_url='http://pypi.python.org/pypi/django-vkontakte-users',
+    license='BSD',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,  # because we're including media that Django needs
+    install_requires=[
+        'python-dateutil==1.5',
+        'django-vkontakte-api>=0.7.0',
+        'django-vkontakte-places>=0.3.0',
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
