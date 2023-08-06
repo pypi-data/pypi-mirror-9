f0b634b7104f9abd0996ae8d5745d8fcadce8525
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:36 +0000

    Add package flylog/0.1.28/flylog-0.1.28.tar.gz

---
+++ packages/flylog/flylog-0.1.28.tar.gz/flylog-0.1.28/flylog/constants.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+AGENT_LOGGER_NAME = 'flylog'
+AGENT_HOST = '127.0.0.1'
+AGENT_PORT = 12000
---
+++ packages/flylog/flylog-0.1.28.tar.gz/flylog-0.1.28/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="flylog",
+    version='0.1.28',
+    zip_safe=False,
+    platforms='any',
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    scripts=['flylog/bin/run_flylog_agent.py'],
+    url="https://github.com/dantezhu/flylog",
+    license="BSD",
+    author="dantezhu",
+    author_email="zny2008@gmail.com",
+    description="make log fly to mail or other",
+)
