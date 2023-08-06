0b6675a1691ff9582bd47fbc76db4f010b8003b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:29 +0000

    Add package mathgraph/0.0.4/mathgraph-0.0.4.tar.gz

---
+++ packages/mathgraph/mathgraph-0.0.4.tar.gz/mathgraph-0.0.4/setup.py
@@ -0,0 +1,28 @@
+import os
+from setuptools import setup, find_packages
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+    
+
+setup(
+    name = "mathgraph",
+    version = "0.0.4",
+    description = "Mathgraph is an open source Python package for calculating topological indices and energy of graphs",
+    long_description = "",
+    url = 'https://pypi.python.org/pypi/mathgraph',
+    license = 'MIT',
+    author = 'Sudharsan Vijayaraghavan',
+    author_email = 'sudvijayr@gmail.com',
+    packages = find_packages(exclude=['tests']),
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python'
+    ],
+    install_requires = ['numpy', 'networkx', 'mathchem']
+
+)
