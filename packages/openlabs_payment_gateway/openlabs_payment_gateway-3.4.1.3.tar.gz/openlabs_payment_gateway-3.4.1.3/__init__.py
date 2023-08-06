c49fe370444432dd5b160ddac406ea99aef2599f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:16 +0000

    Add package openlabs_payment_gateway/3.4.1.3/openlabs_payment_gateway-3.4.1.3.tar.gz

---
+++ packages/openlabs_payment_gateway/openlabs_payment_gateway-3.4.1.3.tar.gz/openlabs_payment_gateway-3.4.1.3/__init__.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+'''
+
+    Payment Gateway
+
+    :copyright: (c) 2013-2014 by Openlabs Technologies & Consulting (P) Ltd.
+    :license: BSD, see LICENSE for more details
+
+'''
+from trytond.pool import Pool
+from .transaction import PaymentTransaction, TransactionLog, PaymentGateway, \
+    PaymentProfile, AddPaymentProfileView, \
+    AddPaymentProfile, Party, TransactionUseCardView, TransactionUseCard, \
+    PaymentGatewayResUser, User
+from .dummy import PaymentGatewayDummy, AddPaymentProfileViewDummy, \
+    AddPaymentProfileDummy, DummyTransaction
+from .manual import PaymentGatewaySelf, ManualSelfTransaction
+
+
+def register():
+    Pool.register(
+        Party,
+        PaymentGateway,
+        PaymentProfile,
+        PaymentTransaction,
+        TransactionLog,
+        AddPaymentProfileView,
+        TransactionUseCardView,
+        # Dummy provider related classes
+        PaymentGatewayDummy,
+        AddPaymentProfileViewDummy,
+        DummyTransaction,
+        # Manualself run payment gateway
+        PaymentGatewaySelf,
+        ManualSelfTransaction,
+        PaymentGatewayResUser,
+        User,
+        module='payment_gateway', type_='model'
+    )
+    Pool.register(
+        AddPaymentProfile,
+        AddPaymentProfileDummy,
+        TransactionUseCard,
+        module='payment_gateway', type_='wizard'
+    )
