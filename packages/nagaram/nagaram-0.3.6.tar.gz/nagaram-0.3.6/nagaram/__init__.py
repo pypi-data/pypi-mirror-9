2c8afa0f5b9b789252477608751b5fc274b13041
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:02 +0000

    Add package nagaram/0.3.6/nagaram-0.3.6.tar.gz

---
+++ packages/nagaram/nagaram-0.3.6.tar.gz/nagaram-0.3.6/nagaram/__init__.py
@@ -0,0 +1,21 @@
+"""Nagaram. Scrabble word anagram finder.
+
+Usage:
+    nagaram [--sowpods] [-l] [-s chars] [-e chars] <letters>
+
+? can be used to represent another tile on the board to play on
+_ can be used to represent blank tiles in your rack (no points)
+
+By default it will use the Tournament Words List. The --sowpods command line
+variable is available to use the SOWPODS words list.
+
+The -l or --length flag if used will print the anagrams sorted by tiles used,
+rather than the default of by Scrabble score.
+
+The -s flag can be used to provide starting characters already on the board,
+similarily the -e flag can be used for ending characters.
+"""
+
+
+__version__ = "0.3.6"
+__release_date__ = "March 5, 2015"
