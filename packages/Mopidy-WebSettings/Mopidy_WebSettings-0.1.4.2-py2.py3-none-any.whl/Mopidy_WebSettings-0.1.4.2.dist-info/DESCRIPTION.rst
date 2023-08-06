f3e8b920687575612bce974cc2b7fda8fb8d00b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:23:19 +0000

    Add package Mopidy-WebSettings/0.1.4.2/Mopidy_WebSettings-0.1.4.2-py2.py3-none-any.whl

---
+++ packages/Mopidy-WebSettings/Mopidy_WebSettings-0.1.4.2-py2.py3-none-any.whl/Mopidy_WebSettings-0.1.4.2.dist-info/DESCRIPTION.rst
@@ -0,0 +1,98 @@
+****************************
+Mopidy-WebSettings
+****************************
+
+.. image:: https://img.shields.io/pypi/v/Mopidy-WebSettings.svg?style=flat
+    :target: https://pypi.python.org/pypi/Mopidy-WebSettings/
+    :alt: Latest PyPI version
+
+.. image:: https://img.shields.io/pypi/dm/Mopidy-WebSettings.svg?style=flat
+    :target: https://pypi.python.org/pypi/Mopidy-WebSettings/
+    :alt: Number of PyPI downloads
+
+.. image:: https://img.shields.io/travis/woutervanwijk/mopidy-websettings/master.png?style=flat
+    :target: https://travis-ci.org/woutervanwijk/mopidy-websettings
+    :alt: Travis CI build status
+
+.. image:: https://img.shields.io/coveralls/woutervanwijk/mopidy-websettings/master.svg?style=flat
+   :target: https://coveralls.io/r/woutervanwijk/mopidy-websettings?branch=master
+   :alt: Test coverage
+
+Mopidy extension for editing settings in a webinterface. Used by Pi MusicBox, but also usable for personal installations or other projects, since MusicBox-only settings are hidden automatically.
+
+
+Installation
+============
+
+Install by running::
+
+    pip install Mopidy-WebSettings
+
+Or, if available, install the Debian/Ubuntu package from `apt.mopidy.com
+<http://apt.mopidy.com/>`_.
+
+
+Configuration
+=============
+
+Before starting Mopidy, you can add configuration for
+Mopidy-WebSettings to your Mopidy configuration file::
+
+    [websettings]
+    enabled = true
+    musicbox = false
+    config_file = /etc/mopidy/mopidy.conf
+
+Make sure the config file is writable by the user under which mopidy is running! And make sure the http extension is working. Go to the ip or url of your mopidy computer and add /settings (e.g. http://musicbox.local/settings or http://192.168.1.10:6680/settings )
+
+For now, the settings are only applied after a reboot, which this extension will try to do. If it doesn't work, a (manual) restart of mopidy is needed. 
+
+Project resources
+=================
+
+- `Source code <https://github.com/woutervanwijk/mopidy-websettings>`_
+- `Issue tracker <https://github.com/woutervanwijk/mopidy-websettings/issues>`_
+- `Development branch tarball <https://github.com/woutervanwijk/mopidy-websettings/archive/master.tar.gz#egg=Mopidy-WebSettings-dev>`_
+
+
+Changelog
+=========
+
+v0.1.4.1
+----------------------------------------
+
+- Small fix for passwords not updated well
+
+v0.1.4
+----------------------------------------
+
+- AudioAddict added (by Nilicule)
+- Passwords mask same size as password
+
+v0.1.3 
+----------------------------------------
+
+- Added YouTube and local support (on or off)
+- Enabled YouTube, SomaFM, Local, Internetarchive, Podcast by default when not in ini-file (the default of the extensions)
+- Settings for enabling streaming services shairport and upmpdcli (for MusicBox only)
+- Writing the config is more reliable
+- Better layout
+
+v0.1.2
+----------------------------------------
+
+- Fixes for writing ini file
+
+
+v0.1.1 
+----------------------------------------
+
+- Fixed missing template files in dist
+- Fixed template
+- Reboot after settings are updated (if mopidy instance has the permission to do that, like on musicbox)
+
+v0.1.0 
+----------------------------------------
+
+- Initial release
+
---
+++ packages/Mopidy-WebSettings/Mopidy_WebSettings-0.1.4.2-py2.py3-none-any.whl/mopidy_websettings/__init__.py
@@ -0,0 +1,170 @@
+from __future__ import unicode_literals
+
+import logging
+import os, sys, subprocess
+
+import tornado.web
+
+from mopidy import config, ext
+
+from configobj import ConfigObj, ConfigObjError
+from validate import Validator
+import jinja2
+
+__version__ = '0.1.4.2'
+
+logger = logging.getLogger(__name__)
+
+spec_file = os.path.join(os.path.dirname(__file__), 'settingsspec.ini')
+template_file = os.path.join(os.path.dirname(__file__), 'index.html')
+#config_file = '/etc/mopidy/mopidy.conf'
+
+#log_file = '/var/log/mopidy/mopidy.log'
+
+password_mask = '*'
+
+def restart_program():
+    """
+    DOES NOT WORK WELL WITH MOPIDY 
+    Hack from https://www.daniweb.com/software-development/python/code/260268/restart-your-python-program
+    to support updating the settings, since mopidy is not able to do that yet
+    Restarts the current program
+    Note: this function does not return. Any cleanup action (like
+    saving data) must be done before calling this function"""
+    
+    python = sys.executable
+    os.execl(python, python, * sys.argv)
+
+class Extension(ext.Extension):
+    dist_name = 'Mopidy-WebSettings'
+    ext_name = 'websettings'
+    version = __version__
+
+    def get_default_config(self):
+        conf_file = os.path.join(os.path.dirname(__file__), 'ext.conf')
+        return config.read(conf_file)
+
+    def get_config_schema(self):
+        schema = super(Extension, self).get_config_schema()
+        schema['musicbox'] = config.String()
+        schema['config_file'] = config.String()
+        return schema
+
+    def setup(self, registry):
+        registry.add('http:app', {
+            #'name': self.ext_name,
+            'name': 'settings',
+            'factory': websettings_app_factory,
+        })
+
+class WebSettingsRequestHandler(tornado.web.RequestHandler):
+
+    def initialize(self, core, config):
+        self.config_file = config.get('websettings')['config_file']
+        self.core = core
+
+    def get(self):
+        templateLoader = jinja2.FileSystemLoader( searchpath = "/" )
+        templateEnv = jinja2.Environment( loader=templateLoader )
+        template = templateEnv.get_template(template_file)
+        error = ''
+        #read config file
+        try:
+            iniconfig = ConfigObj(self.config_file, configspec=spec_file, file_error=True, encoding='utf8')
+        except (ConfigObjError, IOError), e:
+            error = 'Could not load ini file! %s %s %s' % (e, ConfigObjError, IOError)
+        #read values of valid items (in the spec-file)
+        validItems = ConfigObj(spec_file, encoding='utf8')
+        templateVars = {
+            "error": error
+        }
+        #iterate over the valid items to get them into the template
+        for item in validItems:
+            for subitem in validItems[item]:
+                itemName = item + '__' + subitem
+                try:
+                    configValue = iniconfig[item][subitem]
+                    #compare last 8 caracters of subitemname
+                    if subitem[-8:] == 'password' and configValue != '':
+                        configValue = password_mask * len(iniconfig[item][subitem])
+                    templateVars[itemName] = configValue
+                except:
+                    pass
+
+        self.write(template.render ( templateVars ) )
+
+class WebPostRequestHandler(tornado.web.RequestHandler):
+
+    def initialize(self, core, config):
+        self.config_file = config.get('websettings')['config_file']
+        self.core = core
+
+    def post(self):
+        error = ''
+        try:
+            iniconfig = ConfigObj(self.config_file, configspec=spec_file, file_error=True, encoding='utf8')
+        except (ConfigObjError, IOError), e:
+            error = 'Could not load ini file!'
+        if error == '':
+            validItems = ConfigObj(spec_file, encoding='utf8')
+            templateVars = {
+                "error": error
+            }
+            #iterate over the items, so that only valid items are processed
+            for item in validItems:
+                for subitem in validItems[ item ]:
+                    itemName = item + '__' + subitem
+                    argumentItem = self.get_argument(itemName, default='')
+                    if argumentItem:
+                        #don't edit config value if password mask
+                        if subitem[-8:] == 'password':
+                          if argumentItem == (password_mask * len(argumentItem)) or argumentItem == '':
+                              continue
+                        #create section entry if it doesn't exist
+                        try:
+                            iniconfig[item][subitem] = argumentItem
+                        except:
+                            iniconfig[item] = {}
+                            iniconfig[item][subitem] = argumentItem
+            iniconfig.write()
+            error = 'Settings Saved!'
+        message = '<html><body><h1>' + error + '</h1><p>Applying changes (reboot) <br/><a href="/">Back</a><br/></p></body></html>'
+        self.write(message)
+
+        #logger.info ("restart")
+        #restart_program()
+
+        #using two different methods for reboot for different systems
+        logger.info('Rebooting system')
+        os.system("sudo shutdown -r now")
+        os.system("shutdown -r now")
+        #        os.system("/opt/musicbox/startup.sh")
+
+class WebRebootRequestHandler(tornado.web.RequestHandler):
+
+    def initialize(self, core):
+        self.core = core
+
+    def post(self):
+        logger.info('Halting system')
+        os.system("sudo shutdown -r now")
+        os.system("shutdown -r now")
+
+class WebShutdownRequestHandler(tornado.web.RequestHandler):
+
+    def initialize(self, core):
+        self.core = core
+
+    def post(self):
+        logger.info('Halting system')
+        os.system("sudo shutdown -h now")
+        os.system("shutdown -h now")
+
+
+def websettings_app_factory(config, core):
+    return [
+        ('/', WebSettingsRequestHandler, {'core': core, 'config': config}),
+        ('/update', WebPostRequestHandler, {'core': core, 'config': config}),
+        ('/reboot', WebRebootRequestHandler, {'core': core}),
+        ('/shutdown', WebShutdownRequestHandler, {'core': core})
+    ]
