a35aed06bfe8db7a2aa3759ef729eb5ec05b86b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:55 +0000

    Add package tai5_uan5_gian5_gi2_tsu1_liau7_khoo3/3.0.2/tai5_uan5_gian5_gi2_tsu1_liau7_khoo3-3.0.2.zip

---
+++ packages/tai5_uan5_gian5_gi2_tsu1_liau7_khoo3/tai5_uan5_gian5_gi2_tsu1_liau7_khoo3-3.0.2.zip/tai5_uan5_gian5_gi2_tsu1_liau7_khoo3-3.0.2/臺灣言語資料庫/試驗/加資料/加外語資料試驗.py
@@ -0,0 +1,101 @@
+# -*- coding: utf-8 -*-
+from 臺灣言語資料庫.試驗.加資料.加資料試驗 import 加資料試驗
+from 臺灣言語資料庫.資料模型 import 外語表
+from django.core.exceptions import ObjectDoesNotExist
+import json
+
+class 加外語資料試驗(加資料試驗):
+	def setUp(self):
+		super(加外語資料試驗, self).setUp()
+		self.資料表 = 外語表
+		self.詞內容.update({
+			'外語語言':'華語',
+			'外語資料':'漂亮',
+		})
+		self.句內容.update({
+			'外語語言':'英語',
+			'外語資料':'She is beautiful.',
+		})
+	def test_加詞(self):
+		super(加外語資料試驗, self).test_加詞()
+		self.assertEqual(self.資料.外語語言, self.華語)
+		self.assertEqual(self.資料.外語資料, '漂亮')
+	def test_加句(self):
+		super(加外語資料試驗, self).test_加句()
+		self.assertEqual(self.資料.外語語言, self.英語)
+		self.assertEqual(self.資料.外語資料, 'She is beautiful.')
+	def test_外語語言舊編號(self):
+		self.詞內容['外語語言'] = self.華語.pk
+		super(加外語資料試驗, self).test_加詞()
+		self.assertEqual(self.資料.外語語言, self.華語)
+		self.assertEqual(self.資料.外語資料, '漂亮')
+		self.句內容['外語語言'] = self.英語.pk
+		super(加外語資料試驗, self).test_加句()
+		self.assertEqual(self.資料.外語語言, self.英語)
+		self.assertEqual(self.資料.外語資料, 'She is beautiful.')
+	def test_外語語言新字串(self):
+		self.詞內容['外語語言'] = '埔里噶哈巫'
+		super(加外語資料試驗, self).test_加詞()
+		self.assertEqual(self.資料.外語語言.語言腔口, '埔里噶哈巫')
+		self.assertEqual(self.資料.外語資料, '漂亮')
+		self.句內容['外語語言'] = '日語'
+		super(加外語資料試驗, self).test_加句()
+		self.assertEqual(self.資料.外語語言.語言腔口, '日語')
+		self.assertEqual(self.資料.外語資料, 'She is beautiful.')
+	def test_外語語言新編號(self):
+		self.詞內容['外語語言'] = 1115
+		self.assertRaises(ObjectDoesNotExist, super(加外語資料試驗, self).test_加詞)
+		self.assertEqual(self.資料表.objects.all().count(), 0)
+		self.句內容['外語語言'] = 1231
+		self.assertRaises(ObjectDoesNotExist, super(加外語資料試驗, self).test_加句)
+		self.assertEqual(self.資料表.objects.all().count(), 0)
+	def test_外語語言毋是數字佮字串(self):
+		self.詞內容['外語語言'] = [1115]
+		self.assertRaises(TypeError, super(加外語資料試驗, self).test_加詞)
+		self.assertEqual(self.資料表.objects.all().count(), 0)
+		self.句內容['外語語言'] = 1231.23
+		self.assertRaises(TypeError, super(加外語資料試驗, self).test_加句)
+		self.assertEqual(self.資料表.objects.all().count(), 0)
+	def test_無語言(self):
+		self.詞內容.pop('外語語言')
+		self.assertRaises(KeyError, super(加外語資料試驗, self).test_加詞)
+		self.assertEqual(self.資料表.objects.all().count(), 0)
+		self.句內容.pop('外語語言')
+		self.assertRaises(KeyError, super(加外語資料試驗, self).test_加句)
+		self.assertEqual(self.資料表.objects.all().count(), 0)
+	def test_無資料(self):
+		self.詞內容.pop('外語資料')
+		self.assertRaises(KeyError, super(加外語資料試驗, self).test_加詞)
+		self.assertEqual(self.資料表.objects.all().count(), 0)
+		self.句內容.pop('外語資料')
+		self.assertRaises(KeyError, super(加外語資料試驗, self).test_加句)
+		self.assertEqual(self.資料表.objects.all().count(), 0)
+	def test_毋是字串(self):
+		self.詞內容['外語資料'] = 1228
+		self.assertRaises(TypeError, super(加外語資料試驗, self).test_加詞)
+		self.assertEqual(self.資料表.objects.all().count(), 0)
+		self.句內容['外語資料'] = ['沒辦法']
+		self.assertRaises(TypeError, super(加外語資料試驗, self).test_加句)
+		self.assertEqual(self.資料表.objects.all().count(), 0)
+		self.詞內容['外語資料'] = None
+		self.assertRaises(TypeError, super(加外語資料試驗, self).test_加詞)
+		self.assertEqual(self.資料表.objects.all().count(), 0)
+		self.句內容['外語資料'] = {}
+		self.assertRaises(TypeError, super(加外語資料試驗, self).test_加句)
+		self.assertEqual(self.資料表.objects.all().count(), 0)
+	def test_屬性是json字串(self):
+		外語=外語表.加資料(
+			{'著作年': '2014',
+			 '外語資料': '漂亮', 
+			 '種類': '字詞', 
+			 '收錄者': 1, 
+			 '著作所在地': '花蓮', 
+			 '語言腔口': '閩南語', 
+			 '版權': '會使公開', 
+			 '屬性': '{"\\u5b57\\u6578": "2", "\\u8a5e\\u6027": "\\u5f62\\u5bb9\\u8a5e"}', 
+			 '外語語言': '華語', 
+			 '來源': '{"\\u8077\\u696d": "\\u5b78\\u751f", "\\u540d": "\\u963f\\u5aa0"}'
+			})
+		self.assertEqual(外語.屬性.count(), 2)
+		self.assertEqual(json.loads(外語.屬性.get(分類='詞性').性質), '形容詞')
+		self.assertEqual(json.loads(外語.屬性.get(分類='字數').性質), '2')
