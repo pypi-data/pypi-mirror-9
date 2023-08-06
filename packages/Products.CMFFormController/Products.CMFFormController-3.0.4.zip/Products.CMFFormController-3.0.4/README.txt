0715cb0fee0e2c75c173c54a48416acac829eb32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:15 +0000

    Add package Products.CMFFormController/3.0.4/Products.CMFFormController-3.0.4.zip

---
+++ packages/Products.CMFFormController/Products.CMFFormController-3.0.4.zip/Products.CMFFormController-3.0.4/README.txt
@@ -0,0 +1,5 @@
+Overview
+========
+
+CMFFormController replaces the portal_form form validation mechanism from
+Plone. It should work just fine in plain CMF as well.
---
+++ packages/Products.CMFFormController/Products.CMFFormController-3.0.4.zip/Products.CMFFormController-3.0.4/setup.py
@@ -0,0 +1,44 @@
+from setuptools import setup, find_packages
+
+version = '3.0.4'
+
+setup(name='Products.CMFFormController',
+      version=version,
+      description="CMFFormController provides a form validation mechanism for CMF.",
+      long_description=open("README.txt").read() + "\n" +
+                       open("CHANGES.txt").read(),
+      classifiers=[
+        "Framework :: Plone",
+        "Framework :: Plone :: 5.0",
+        "Framework :: Zope2",
+        "License :: OSI Approved :: BSD License",
+        "License :: OSI Approved :: Zope Public License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+      ],
+      keywords='Zope CMF Plone form validation',
+      author='Geoff Davis',
+      author_email='plone-developers@lists.sourceforge.net',
+      url='http://pypi.python.org/pypi/Products.CMFFormController',
+      license='BSD',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['Products'],
+      include_package_data=True,
+      zip_safe=False,
+      extras_require=dict(
+        test=[
+            'plone.app.testing',
+        ]
+      ),
+      install_requires=[
+        'setuptools',
+        'zope.interface',
+        'zope.structuredtext',
+        'zope.tales',
+        'Products.CMFCore',
+        'Products.GenericSetup',
+        'Acquisition',
+        'transaction',
+        'Zope2',
+      ],
+)
