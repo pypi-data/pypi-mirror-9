c1a50720001e46320a46ff2a32e4b57294e74ac4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:07:54 +0000

    Add package inlinesas/0.0.3/inlinesas-0.0.3.tar.gz

---
+++ packages/inlinesas/inlinesas-0.0.3.tar.gz/inlinesas-0.0.3/setup.py
@@ -0,0 +1,16 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distuils.core import setup
+
+setup(
+        name="inlinesas",
+        version='0.0.3',
+        description="Run SAS code from within a Python script.",
+        author="Joe Dougherty",
+        author_email="josepd@wharton.upenn.edu",
+        url="https://wrds-web.wharton.upenn.edu/wrds/",
+        packages=['inlinesas'],
+        keywords = ['SAS', 'etl'],
+        )
+
