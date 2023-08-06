7b818f6a81f9e5defe6038f468481b4c4e3d4181
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:29:57 +0000

    Add package drf-generators/0.1.4/drf-generators-0.1.4.tar.gz

---
+++ packages/drf-generators/drf-generators-0.1.4.tar.gz/drf-generators-0.1.4/drf_generators/generators.py
@@ -0,0 +1,72 @@
+
+from django import template
+import os.path
+
+from drf_generators.templates import *
+from drf_generators.helpers import *
+
+__all__ = ['generate_serializers', 'generate_views', 'generate_urls']
+
+
+def generate_serializers(app_config):
+    name = app_config.name
+    filename = 'serializers.py'
+    message = 'Generating Serializers for %s (%s)' % (name, filename)
+    print(message)
+
+    temp = template.Template(SERIALIZER_FILE_TEMPLATE)
+    models = get_model_names(app_config)
+    details = get_model_details(app_config)
+    context = template.Context({ 'app': name,
+                                 'models': models,
+                                 'details': details})
+
+    filename = 'serializers.py'
+    content = temp.render(context)
+    app = app_config.models_module
+    if write_file(content, filename, app):
+        for serializer in get_serializer_names(models):
+            print('  - %s' % serializer)
+    else:
+        print('Serializer generation cancelled')
+
+
+def generate_views(app_config):
+    app = app_config.models_module
+    name = name = app_config.name
+    filename = 'views.py'
+    message = 'Generating API Views for %s (%s)' % (name, filename)
+    print(message)
+
+    temp = template.Template(VIEW_FILE_TEMPLATE)
+    models = get_model_names(app_config)
+    serializers = get_serializer_names(models)
+    context = template.Context({ 'app': name,
+                                 'serializers': serializers,
+                                 'models': models })
+
+    content = temp.render(context)
+    if write_file(content, filename, app):
+        for view in get_view_names(models):
+            print('  - %s' % view)
+    else:
+        print('View genereration cancelled')
+
+
+def generate_urls(app_config):
+    app = app_config.models_module
+    name = name = app_config.name
+    filename = 'urls.py'
+    message = 'Generating urls for %s (%s)' % (name, filename)
+    print(message)
+
+    temp = template.Template(URL_FILE_TEMPLATE)
+    models = get_model_names(app_config)
+    context = template.Context({ 'app': name,
+                                 'models': models })
+
+    content = temp.render(context)
+    if write_file(content, filename, app):
+        print('  - writing %s' % filename)
+    else:
+        print('Urls genereration cancelled')
---
+++ packages/drf-generators/drf-generators-0.1.4.tar.gz/drf-generators-0.1.4/setup.py
@@ -0,0 +1,41 @@
+import os
+from setuptools import setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+    README = readme.read()
+
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='drf-generators',
+    version='0.1.4',
+
+    description='Generate DRF Serializers, Views, and urls for your API aplication.',
+    long_description=README,
+
+    url='https://github.com/brobin/drf-generators',
+    download_url = 'https://github.com/brobin/drf-generators/archive/0.1.4.zip',
+    author='Tobin Brown',
+    author_email='tobin@brobin.me',
+
+    license='MIT',
+
+    packages=['drf_generators', 'drf_generators.management.commands'],
+    include_package_data=True,
+    install_requires=['Django>=1.7', 'djangorestframework>=3.1.0'],
+
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Internet :: WWW/HTTP',
+    ],
+
+    keywords='API REST framework generate',
+)
