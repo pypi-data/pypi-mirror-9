0472a93ca7f76662c220acfd73196c3c2f186023
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:16 +0000

    Add package nestrick/1.1.1.0/nestrick-1.1.1.0.zip

---
+++ packages/nestrick/nestrick-1.1.1.0.zip/nestrick-1.1.1.0/nestrick.py
@@ -0,0 +1,13 @@
+""" This Module is used to take an input List and print out its contents in
+    nested fashion """
+def print_list (arg_list, tabs = 0):
+    # This function takes in a list argument
+    # and prints out its contents in a nested fashion
+    # recursively.
+    for each_elem in arg_list:
+        if isinstance(each_elem, list):
+            print_list(each_elem, tabs+1)
+        else:
+            for tab_level in range(tabs):
+                print ('\t', end='')
+            print(each_elem)
