46f15c72a7693efba3055d1f39bed19a98eaa497
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:37:41 +0000

    Add package ffn/0.1.7/ffn-0.1.7.tar.gz

---
+++ packages/ffn/ffn-0.1.7.tar.gz/ffn-0.1.7/ffn/__init__.py
@@ -0,0 +1,10 @@
+import core
+import data
+
+from .data import get
+#from .core import year_frac, PerformanceStats, GroupStats, merge
+from .core import *
+
+core.extend_pandas()
+
+__version__ = (0, 1, 7)
---
+++ packages/ffn/ffn-0.1.7.tar.gz/ffn-0.1.7/setup.py
@@ -0,0 +1,44 @@
+import setuptools
+import codecs
+import os
+import re
+
+
+def local_file(filename):
+    return codecs.open(
+        os.path.join(os.path.dirname(__file__), filename), 'r', 'utf-8'
+    )
+
+
+version = re.search(
+    "^__version__ = \((\d+), (\d+), (\d+)\)$",
+    local_file('ffn/__init__.py').read(),
+    re.MULTILINE
+).groups()
+
+
+setuptools.setup(
+    name="ffn",
+    version='.'.join(version),
+    author='Philippe Morissette',
+    author_email='morissette.philippe@gmail.com',
+    description='Financial functions for Python',
+    keywords='python finance quant functions',
+    url='https://github.com/pmorissette/ffn',
+    install_requires=[
+        'decorator',
+        'numpy',
+        'pandas',
+        'tabulate',
+        'matplotlib',
+        'scikit-learn',
+        'scipy'
+    ],
+    packages=['ffn'],
+    long_description=local_file('README.rst').read(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Topic :: Software Development :: Libraries',
+        'Programming Language :: Python'
+    ]
+)
