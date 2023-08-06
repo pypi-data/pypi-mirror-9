119e10c02df27ae3654f74e82529df5b34fe54ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:16 +0000

    Add package cumulus-aws/0.3a1/cumulus-aws-0.3a1.tar.gz

---
+++ packages/cumulus-aws/cumulus-aws-0.3a1.tar.gz/cumulus-aws-0.3a1/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+from ez_setup import use_setuptools
+use_setuptools()
+
+from setuptools import setup, find_packages
+from glob import glob
+
+setup(
+    name="cumulus-aws",
+    version="0.3a1",
+    author="Peter Hall",
+    author_email='cumulus@peterkh.net',
+    license='Apache Software License 2.0',
+    description='Manages AWS Cloudformation stacks across multiple CF'
+                ' templates',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=['PyYAML', 'argparse', 'boto', 'simplejson', 'pystache'],
+    scripts=glob('bin/*'),
+    url="https://github.com/cotdsa/cumulus",
+)
