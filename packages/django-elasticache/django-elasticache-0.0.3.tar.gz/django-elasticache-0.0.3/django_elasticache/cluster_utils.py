4c42257185008f7d7d030928acb5e32dce77e5bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:17 +0000

    Add package django-elasticache/0.0.3/django-elasticache-0.0.3.tar.gz

---
+++ packages/django-elasticache/django-elasticache-0.0.3.tar.gz/django-elasticache-0.0.3/django_elasticache/cluster_utils.py
@@ -0,0 +1,64 @@
+"""
+utils for discovery cluster
+"""
+from distutils.version import StrictVersion
+from django.utils.encoding import smart_text
+import re
+from telnetlib import Telnet
+
+
+class WrongProtocolData(ValueError):
+    """
+    Exception for raising when we get something unexpected
+    in telnet protocol
+    """
+    def __init__(self, cmd, response):
+        super(WrongProtocolData, self).__init__(
+            'Unexpected response {} for command {}'.format(response, cmd))
+
+
+def get_cluster_info(host, port):
+    """
+    return dict with info about nodes in cluster and current version
+    {
+        'nodes': [
+            'IP:port',
+            'IP:port',
+        ],
+        'version': '1.4.4'
+    }
+    """
+    client = Telnet(host, int(port))
+    client.write(b'version\n')
+    res = client.read_until(b'\r\n').strip()
+    version_list = res.split(b' ')
+    if len(version_list) != 2 or version_list[0] != b'VERSION':
+        raise WrongProtocolData('version', res)
+    version = version_list[1]
+    if StrictVersion(smart_text(version)) >= StrictVersion('1.4.14'):
+        cmd = b'config get cluster\n'
+    else:
+        cmd = b'get AmazonElastiCache:cluster\n'
+    client.write(cmd)
+    res = client.read_until(b'\n\r\nEND\r\n')
+    client.close()
+    ls = list(filter(None, re.compile(br'\r?\n').split(res)))
+    if len(ls) != 4:
+        raise WrongProtocolData(cmd, res)
+
+    try:
+        version = int(ls[1])
+    except ValueError:
+        raise WrongProtocolData(cmd, res)
+    nodes = []
+    try:
+        for node in ls[2].split(b' '):
+            host, ip, port = node.split(b'|')
+            nodes.append('{}:{}'.format(smart_text(ip or host),
+                                        smart_text(port)))
+    except ValueError:
+        raise WrongProtocolData(cmd, res)
+    return {
+        'version': version,
+        'nodes': nodes
+    }
