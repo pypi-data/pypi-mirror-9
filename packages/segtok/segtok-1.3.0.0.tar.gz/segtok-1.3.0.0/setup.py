9f466f69f460fe1d8a2dbc172e415736ac2f3a67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:02 +0000

    Add package segtok/1.3.0.0/segtok-1.3.0.0.tar.gz

---
+++ packages/segtok/segtok-1.3.0.0.tar.gz/segtok-1.3.0.0/setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+try:
+    with open('README.rst') as file:
+        long_description = file.read()
+except IOError:
+    long_description = "missing"
+
+
+setup(
+    name='segtok',
+    version='1.3.0.0',
+    url='https://github.com/fnl/segtok',
+    author='Florian Leitner',
+    author_email='florian.leitner@gmail.com',
+    description='sentence segmentation and word tokenization tools',
+    keywords='sentence segmenter splitter word tokenizer token',
+    license='MIT',
+    packages=['segtok'],
+    install_requires=['regex'],  # handles all Unicode categories in Regular Expressions
+    long_description=long_description,
+    entry_points={
+        'console_scripts': [
+            'tokenizer = segtok.tokenizer:main',
+            'segmenter = segtok.segmenter:main',
+        ],
+    },
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Text Processing',
+        'Topic :: Text Processing :: Linguistic',
+    ],
+)
