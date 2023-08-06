c7bebac8ebd50d68bedb17133f89f5225f269206
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:10 +0000

    Add package pyregdom/0.4/pyregdom-0.4.tar.gz

---
+++ packages/pyregdom/pyregdom-0.4.tar.gz/pyregdom-0.4/setup.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+import os
+import sys
+
+import regdom
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    sys.exit()
+
+description      = 'Detect the registered domains for given hostnames based on Mozillas effective TLD listing'
+long_description = description
+if os.path.exists('README.rst'):
+    long_description = open('README.rst').read()
+
+packages = [
+    'regdom'
+]
+
+setup(
+    name='pyregdom',
+    version=regdom.__version__,
+    description='Detect the registered domains for given hostnames based on Mozillas effective TLD listing',
+    long_description=long_description,
+    author='Huy Phan',
+    author_email='dachuy@gmail.com',
+    url='https://github.com/huyphan/pyregdom',
+    packages=packages,
+    package_dir={'regdom': 'regdom'},
+    include_package_data=True,
+    license='Apache 2.0',
+    zip_safe=False,
+    classifiers=(
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7'
+    ),
+)
