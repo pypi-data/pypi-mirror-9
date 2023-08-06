b230f0c4c21832844becdd7c280d4d8ba47bb1fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:39 +0000

    Add package Starbucks/0.3.0/Starbucks-0.3.0.tar.gz

---
+++ packages/Starbucks/Starbucks-0.3.0.tar.gz/Starbucks-0.3.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Starbucks',
+    packages=find_packages(),
+    version='0.3.0',
+    description='Unoffical Starbucks API.',
+    long_description=open('README.rst').read(),
+    license='BSD License',
+    author='Minsoo Park',
+    author_email='minsoo1003@gmail.com',
+    url='https://github.com/minsoopark/starbucks-py',
+    keywords=['Starbucks'],
+    install_requires=[
+        'click >= 3.3', 'requests >= 2.5.3', 'lxml >= 3.0.0'
+    ],
+     entry_points='''
+        [console_scripts]
+        starbucks-card = starbucks.cli:card_info
+        starbucks-star = starbucks.cli:star_info
+    '''
+)
