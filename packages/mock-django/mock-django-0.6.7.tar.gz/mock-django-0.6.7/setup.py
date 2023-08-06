61d6e128218bdd7627cc55d382d3a5d830b68098
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:09 +0000

    Add package mock-django/0.6.7/mock-django-0.6.7.tar.gz

---
+++ packages/mock-django/mock-django-0.6.7.tar.gz/mock-django-0.6.7/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+# Hack to prevent stupid "TypeError: 'NoneType' object is not callable" error
+# in multiprocessing/util.py _exit_function when running `python
+# setup.py test` (see
+# http://www.eby-sarna.com/pipermail/peak/2010-May/003357.html)
+try:
+    import multiprocessing
+except ImportError:
+    pass
+
+setup(
+    name='mock-django',
+    version='0.6.7',
+    description='',
+    license='Apache License 2.0',
+    author='David Cramer',
+    author_email='dcramer@gmail.com',
+    url='http://github.com/dcramer/mock-django',
+    packages=find_packages(),
+    install_requires=[
+        'Django>=1.4',
+        'mock',
+    ],
+    tests_require=[
+        'unittest2',
+        'nose',
+    ],
+    test_suite='runtests.collector',
+    zip_safe=False,
+    include_package_data=True,
+)
