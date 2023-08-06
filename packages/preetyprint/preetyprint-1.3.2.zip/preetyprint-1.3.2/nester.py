e4aaeab6d911fbb8c2ff866719d8a12e001efa3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:29:33 +0000

    Add package preetyprint/1.3.2/preetyprint-1.3.2.zip

---
+++ packages/preetyprint/preetyprint-1.3.2.zip/preetyprint-1.3.2/nester.py
@@ -0,0 +1,15 @@
+"""This is module which include one function preety_print() which
+print nested listwith each item in new line."""
+import sys
+
+def preety_print(the_list,indent=False,level=0,fh=sys.stdout):
+    """Display content of nested list, each item in new line.
+    specify optional indentation and level."""
+    for i in the_list:
+        if isinstance(i,list):
+            preety_print(i,indent,level+1,fh)
+        else:
+            if indent:
+                print("\t"*level,end=' ',file=fh)
+            print(i,file=fh)
+
