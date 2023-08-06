c91267a2ea984d8f45b1bb4f2dcadb8c6a97e894
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:07:46 +0000

    Add package YORM/0.4.dev10/YORM-0.4.dev10-py3-none-any.whl

---
+++ packages/YORM/YORM-0.4.dev10-py3-none-any.whl/yorm/converters/extended.py
@@ -0,0 +1,198 @@
+"""Converter classes for extension types."""
+
+import re
+
+import yaml
+
+from .standard import String, Integer, Float, Boolean
+from .container import Dictionary, List
+
+
+# standard types with None as a default #######################################
+
+
+class NoneString(String):
+
+    """Converter for the `str` type with `None` as default."""
+
+    DEFAULT = None
+
+
+class NoneInteger(Integer):
+
+    """Converter for the `int` type with `None` as default."""
+
+    DEFAULT = None
+
+
+class NoneFloat(Float):
+
+    """Converter for the `float` type with `None` as default."""
+
+    DEFAULT = None
+
+
+class NoneBoolean(Boolean):
+
+    """Converter for the `bool` type with `None` as default."""
+
+    DEFAULT = None
+
+
+# standard types with additional behavior #####################################
+
+
+class _Literal(str):
+
+    """Custom type for strings which should be dumped in the literal style."""
+
+    @staticmethod
+    def representer(dumper, data):
+        """Return a custom dumper that formats `str` in the literal style."""
+        return dumper.represent_scalar('tag:yaml.org,2002:str', data,
+                                       style='|' if data else '')
+
+yaml.add_representer(_Literal, _Literal.representer)
+
+
+class Markdown(String):
+
+    """Converter for a `str` type that contains Markdown."""
+
+    REGEX_MARKDOWN_SPACES = re.compile(r"""
+
+    ([^\n ])  # any character but a newline or space
+
+    (\ ?\n)   # optional space + single newline
+
+    (?!       # none of the following:
+
+      (?:\s)       # whitespace
+      |
+      (?:[-+*]\s)  # unordered list separator + whitespace
+      |
+      (?:\d+\.\s)  # number + period + whitespace
+
+    )
+
+    ([^\n])  # any character but a newline
+
+    """, re.VERBOSE | re.IGNORECASE)
+
+    # based on: http://en.wikipedia.org/wiki/Sentence_boundary_disambiguation
+    REGEX_SENTENCE_BOUNDARIES = re.compile(r"""
+
+    (            # one of the following:
+
+      (?<=[a-z)][.?!])      # lowercase letter + punctuation
+      |
+      (?<=[a-z0-9][.?!]\")  # lowercase letter/number + punctuation + quote
+
+    )
+
+    (\s)          # any whitespace
+
+    (?=\"?[A-Z])  # optional quote + an upppercase letter
+
+    """, re.VERBOSE)
+
+    @classmethod
+    def to_value(cls, obj):
+        """Join non-meaningful line breaks."""
+        value = String.to_value(obj)
+        return Markdown.join(value)
+
+    @staticmethod
+    def join(text):
+        r"""Convert single newlines (ignored by Markdown) to spaces.
+
+        >>> Markdown.join("abc\n123")
+        'abc 123'
+
+        >>> Markdown.join("abc\n\n123")
+        'abc\n\n123'
+
+        >>> Markdown.join("abc \n123")
+        'abc 123'
+
+        """
+        return Markdown.REGEX_MARKDOWN_SPACES.sub(r'\1 \3', text).strip()
+
+    @classmethod
+    def to_data(cls, obj):
+        """Break a string at sentences and dump as a literal string."""
+        value = String.to_value(obj)
+        data = String.to_data(value)
+        split = Markdown.split(data)
+        return _Literal(split)
+
+    @staticmethod
+    def split(text, end='\n'):
+        r"""Replace sentence boundaries with newlines and append a newline.
+
+        :param text: string to line break at sentences
+        :param end: appended to the end of the update text
+
+        >>> Markdown.split("Hello, world!", end='')
+        'Hello, world!'
+
+        >>> Markdown.split("Hello, world! How are you? I'm fine. Good.")
+        "Hello, world!\nHow are you?\nI'm fine.\nGood.\n"
+
+        """
+        stripped = text.strip()
+        if stripped:
+            return Markdown.REGEX_SENTENCE_BOUNDARIES.sub('\n', stripped) + end
+        else:
+            return ''
+
+# container types with additional behavior ####################################
+
+
+class AttributeDictionary(Dictionary):
+
+    """Dictionary converter with keys available as attributes."""
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.__dict__ = self
+
+    @classmethod
+    def default(cls):
+        """Create an uninitialized object with keys as attributes."""
+        if cls is AttributeDictionary:
+            msg = "AttributeDictionary class must be subclassed to use"
+            raise NotImplementedError(msg)
+
+        obj = cls.__new__(cls)
+        obj.__dict__ = obj
+        return obj
+
+
+class SortedList(List):
+
+    """List converter that is sorted on disk."""
+
+    @classmethod
+    def default(cls):
+        """Create an uninitialized object."""
+        if cls is SortedList:
+            msg = "SortedList class must be subclassed to use"
+            raise NotImplementedError(msg)
+        if not cls.item_type:
+            msg = "SortedList subclass must specify item type"
+            raise NotImplementedError(msg)
+
+        return cls.__new__(cls)
+
+    @classmethod
+    def to_data(cls, obj):
+        """Convert all attribute values for optimal dumping to YAML."""
+        value = cls.to_value(obj)
+
+        data = []
+
+        for item in sorted(value):
+            data.append(cls.item_type.to_data(item))
+
+        return data
---
+++ packages/YORM/YORM-0.4.dev10-py3-none-any.whl/yorm/test/test_container.py
@@ -0,0 +1,152 @@
+#!/usr/bin/env python
+# pylint:disable=R0201,C0111
+
+"""Unit tests for the `container` module."""
+
+import pytest
+
+import yorm
+from yorm import common
+from yorm.converters import Dictionary, List
+from yorm.converters import String, Integer
+
+from .samples import *  # pylint: disable=W0401,W0614
+
+
+class TestDictionary:
+
+    """Unit tests for the `Dictionary` container."""
+
+    obj = {'abc': 123}
+
+    data_value = [
+        (obj, obj),
+        (None, {'abc': 0}),
+        ("key=value", {'key': "value", 'abc': 0}),
+        ("key=", {'key': "", 'abc': 0}),
+        ("key", {'key': None, 'abc': 0}),
+    ]
+
+    value_data = [
+        (obj, obj),
+    ]
+
+    def setup_method(self, _):
+        """Reset the class' mapped attributes before each test."""
+        common.ATTRS[SampleDictionary] = {'abc': Integer}
+
+    @pytest.mark.parametrize("data,value", data_value)
+    def test_to_value(self, data, value):
+        """Verify input data is converted to values."""
+        assert value == SampleDictionary.to_value(data)
+
+    @pytest.mark.parametrize("value,data", value_data)
+    def test_to_data(self, value, data):
+        """Verify values are converted to output data."""
+        assert data == SampleDictionary.to_data(value)
+
+    def test_not_implemented(self):
+        """Verify `Dictionary` cannot be used directly."""
+        with pytest.raises(NotImplementedError):
+            Dictionary.to_value(None)
+        with pytest.raises(NotImplementedError):
+            Dictionary.to_data(None)
+
+    def test_dict_as_object(self):
+        """Verify a `Dictionary` can be used as an attribute."""
+        dictionary = SampleDictionaryWithInitialization(1, 2, 3.0)
+        value = {'var1': 1, 'var2': '2'}
+        value2 = dictionary.to_value(dictionary)
+        assert value == value2
+        # keys are not accessible as attributes
+        assert not hasattr(value2, 'var1')
+        assert not hasattr(value2, 'var2')
+        assert not hasattr(value2, 'var3')
+
+
+class TestList:
+
+    """Unit tests for the `List` container."""
+
+    obj = ["a", "b", "c"]
+
+    data_value = [
+        (obj, obj),
+        (None, []),
+        ("a b c", ["a", "b", "c"]),
+        ("a,b,c", ["a", "b", "c"]),
+        ("abc", ["abc"]),
+        ("a\nb\nc", ["a", "b", "c"]),
+        (4.2, ['4.2']),
+    ]
+
+    value_data = [
+        (obj, obj),
+    ]
+
+    @pytest.mark.parametrize("data,value", data_value)
+    def test_to_value(self, data, value):
+        """Verify input data is converted to values."""
+        assert value == StringList.to_value(data)
+
+    @pytest.mark.parametrize("value,data", value_data)
+    def test_to_data(self, value, data):
+        """Verify values are converted to output data."""
+        assert data == StringList.to_data(value)
+
+    def test_item_type(self):
+        """Verify list item type can be determined."""
+        assert String == StringList.item_type
+
+    def test_item_type_none(self):
+        """Verify list item type defaults to None."""
+        assert None is UnknownList.item_type
+
+    def test_not_implemented(self):
+        """Verify `List` cannot be used directly."""
+        with pytest.raises(NotImplementedError):
+            List.to_value(None)
+        with pytest.raises(NotImplementedError):
+            List.to_data(None)
+        with pytest.raises(NotImplementedError):
+            UnknownList.to_value(None)
+        with pytest.raises(NotImplementedError):
+            UnknownList.to_data(None)
+
+
+class TestExtensions:
+
+    """Unit tests for extensions to the container classes."""
+
+    class FindMixin:
+
+        def find(self, value):
+            for value2 in self:
+                if value.lower() == value2.lower():
+                    return value2
+            return None
+
+    @yorm.attr(a=yorm.converters.String)
+    class MyDictionary(Dictionary, FindMixin):
+        pass
+
+    @yorm.attr(all=yorm.converters.String)
+    class MyList(List, FindMixin):
+        pass
+
+    def test_converted_dict_keeps_type(self):
+        my_dict = self.MyDictionary()
+        my_dict['a'] = 1
+        my_dict2 = self.MyDictionary.to_value(my_dict)
+        assert 'a' == my_dict2.find('A')
+        assert None is my_dict2.find('B')
+
+    def test_converted_list_keeps_type(self):
+        my_list = self.MyList()
+        my_list.append('a')
+        my_list2 = self.MyList.to_value(my_list)
+        assert 'a' == my_list2.find('A')
+        assert None is my_list2.find('B')
+
+if __name__ == '__main__':
+    pytest.main()
