838baa08efbce5901a739c57c670416bcca9ad5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:48 +0000

    Add package bu-cascade/0.02/bu_cascade-0.02.tar.gz

---
+++ packages/bu-cascade/bu_cascade-0.02.tar.gz/bu_cascade-0.02/bu_cascade/__init__.py
@@ -0,0 +1,4 @@
+__version__ = 0.02
+
+from .cascade_connector import Cascade
+
---
+++ packages/bu-cascade/bu_cascade-0.02.tar.gz/bu_cascade-0.02/setup.py
@@ -0,0 +1,43 @@
+import os.path
+import re
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+def fpath(name):
+    return os.path.join(os.path.dirname(__file__), name)
+
+
+def read(fname):
+    return open(fpath(fname)).read()
+
+
+def grep(attrname):
+    pattern = r"{0}\W*=\W*'([^']+)'".format(attrname)
+    strval, = re.findall(pattern, file_text)
+    return strval
+
+
+file_text = read(fpath('bu_cascade/__init__.py'))
+
+setup(
+    name='bu_cascade',
+    version=0.02,
+    description='Cascade Server web services integration',
+    long_description='',
+    url='https://github.com/betheluniversity/bu_cascade/',
+    download_url='',
+    author='Eric Jameson',
+    author_email="e-jameson@bethel.edu",
+    license='Apache 2.0',
+    packages=['bu_cascade'],
+    zip_safe=False,
+    install_requires=[
+        'suds'
+    ],
+    test_suite="",
+    classifiers=[]
+)
