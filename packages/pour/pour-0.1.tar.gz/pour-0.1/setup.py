53116189ed7d755ba4dce150d7fc8f92172b37b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:40 +0000

    Add package pour/0.1/pour-0.1.tar.gz

---
+++ packages/pour/pour-0.1.tar.gz/pour-0.1/setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='pour',
+    version='0.1',
+    url='http://www.github.com/keyanp/pour',
+    license='MIT',
+    author='Keyan Pishdadian',
+    author_email='kpishdadian@gmail.com',
+    description='A lightweight Flask app generator.',
+    install_requires=['click'],
+    entry_points={
+        'console_scripts': ['pour = pour.pour:generate']
+    }
+
+)
