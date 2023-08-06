049c777a593fa4eb98771f7c7afa6a401371d703
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:02 +0000

    Add package pyminfraud/1.0.2.dev1/pyminfraud-1.0.2.dev1.tar.gz

---
+++ packages/pyminfraud/pyminfraud-1.0.2.dev1.tar.gz/pyminfraud-1.0.2.dev1/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+import pyminfraud
+
+setuptools.setup(
+	author           = pyminfraud.__author__,
+	author_email     = pyminfraud.__email__,
+	description      = pyminfraud.__description__,
+	keywords         = pyminfraud.__keywords__,
+	license          = pyminfraud.__license__,
+	maintainer       = pyminfraud.__author__,
+	maintainer_email = pyminfraud.__email__,
+	name             = pyminfraud.__project__,
+	packages         = pyminfraud.__packages__,
+	url              = pyminfraud.__url__,
+	version          = pyminfraud.__version__,
+)
