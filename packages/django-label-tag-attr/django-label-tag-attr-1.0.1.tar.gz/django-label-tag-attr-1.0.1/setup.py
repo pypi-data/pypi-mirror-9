61d1abe3ff64a7bd6964c7750fb219d1da8c9ec0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:34 +0000

    Add package django-label-tag-attr/1.0.1/django-label-tag-attr-1.0.1.tar.gz

---
+++ packages/django-label-tag-attr/django-label-tag-attr-1.0.1.tar.gz/django-label-tag-attr-1.0.1/setup.py
@@ -0,0 +1,27 @@
+import os
+from setuptools import setup
+
+__author__ = 'Masashi Shibata <contact@c-bata.link>'
+__version__ = '1.0.1'
+
+BASE_PATH = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(BASE_PATH, 'README.rst')).read()
+CHANGES = open(os.path.join(BASE_PATH, 'CHANGES.rst')).read()
+
+requires = []
+
+setup(
+    name='django-label-tag-attr',
+    version=__version__,
+    author=__author__,
+    author_email='contact@c-bata.link',
+    url='https://github.com/c-bata/django-label-tag-attr',
+    description="Add css classes and html tag attributes "
+                "to django form's label.",
+    long_description=README + '\n\n' + CHANGES,
+    packages=['label_tag_attr'],
+    install_requires=['Django'],
+    keywords=['web', 'django', 'form', 'label_tag_attr'],
+    license='MIT License',
+    include_package_data=True,
+)
