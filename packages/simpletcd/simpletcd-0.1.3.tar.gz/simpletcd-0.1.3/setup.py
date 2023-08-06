90173d4b09dca2474d45713e9e03c00dabd7e947
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:43 +0000

    Add package simpletcd/0.1.3/simpletcd-0.1.3.tar.gz

---
+++ packages/simpletcd/simpletcd-0.1.3.tar.gz/simpletcd-0.1.3/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+setup(
+  name = 'simpletcd',
+  packages = ['simpletcd'],
+  version = '0.1.3',
+  description = 'Simple etcd client make recursive get/put easy',
+  author = 'Liu Xiaohui',
+  author_email = 'herolxh@gmail.com',
+  url = 'https://github.com/oreh/simpletcd',
+  download_url = 'https://github.com/oreh/simpletcd/tarball/0.1.3',
+  keywords = ['client', 'etcd'],
+  classifiers = [],
+  platforms = ['any'],
+  long_description = '',
+  license = 'MIT',
+)
