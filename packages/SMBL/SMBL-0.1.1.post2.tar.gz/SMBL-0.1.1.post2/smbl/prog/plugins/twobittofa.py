b02d3556f1f776b442ffbe1023b1a5b2c723346f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:43 +0000

    Add package SMBL/0.1.1.post2/SMBL-0.1.1.post2.tar.gz

---
+++ packages/SMBL/SMBL-0.1.1.post2.tar.gz/SMBL-0.1.1.post2/smbl/prog/plugins/twobittofa.py
@@ -0,0 +1,37 @@
+import smbl
+import snakemake
+import os
+
+import _program
+
+TWOBITTOFA = _program.get_bin_file_path("twoBitToFa")
+
+
+##########################################
+##########################################
+
+
+class TwoBitToFa(_program.Program):
+	@classmethod
+	def get_installation_files(cls):
+		return [
+				TWOBITTOFA
+			]
+
+	@classmethod
+	def install(cls):
+		if smbl.is_linux() and smbl.is_os_64bit:
+			url="http://hgdownload.cse.ucsc.edu/admin/exe/linux.x86_64/twoBitToFa"
+		elif smbl.is_mac() and smbl.is_os_64bit:
+			url="http://hgdownload.cse.ucsc.edu/admin/exe/macOSX.x86_64/twoBitToFa"
+		elif smbl.is_mac():
+			url="http://hgdownload.cse.ucsc.edu/admin/exe/macOSX.i386/twoBitToFa"
+		else:
+			raise NotImplementedError("For your OS, twoBitToFa is not precompiled")
+
+		cls.download_file(url,"twoBitToFa")
+		cls.install_file("twoBitToFa",TWOBITTOFA)
+
+	@classmethod
+	def supported_platforms(cls):
+		return ["macos","linux"]
