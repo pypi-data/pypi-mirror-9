a949c5e02756f1c49aaf83c179cadb5b0e50549e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:16 +0000

    Add package Transposition-Cipher/1.0.0/Transposition-Cipher-1.0.0.zip

---
+++ packages/Transposition-Cipher/Transposition-Cipher-1.0.0.zip/Transposition-Cipher-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name = 'Transposition-Cipher',
+	version = '1.0.0',
+	py_modules = ['transpositionCipher'],
+	author = 'Telie Chaker',
+	author_email = 'phamthanhviet3112@yahoo.com.vn',
+	url = 'loading...',
+	description = 'Transposition Cipher 4 U!'
+	)
