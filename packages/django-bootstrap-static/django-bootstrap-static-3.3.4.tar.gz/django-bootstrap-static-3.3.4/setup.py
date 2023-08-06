e2afa17ce9a40b8f3599dfa1fcee193858d7c7a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:42 +0000

    Add package django-bootstrap-static/3.3.4/django-bootstrap-static-3.3.4.tar.gz

---
+++ packages/django-bootstrap-static/django-bootstrap-static-3.3.4.tar.gz/django-bootstrap-static-3.3.4/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+version = '3.3.4'
+
+
+setup(name='django-bootstrap-static',
+      version=version,
+      description="A Collection of Bootstrap static files.",
+      long_description=open("README.md", "r").read(),
+      classifiers=[
+          "Development Status :: 5 - Production/Stable",
+          "Environment :: Web Environment",
+          "Intended Audience :: Developers",
+          "Natural Language :: English",
+          "Operating System :: OS Independent",
+          "Framework :: Django",
+          "Programming Language :: Python",
+          "Programming Language :: Python :: 2",
+          "Programming Language :: Python :: 3",
+          "Topic :: Internet :: WWW/HTTP :: Dynamic Content :: CGI Tools/Libraries",
+          "Topic :: Utilities",
+          "License :: OSI Approved :: MIT License",
+          ],
+      keywords='',
+      author='Derek Stegelman',
+      author_email='dstegelman@gmail.com',
+      url='http://github.com/dstegelman/django-bootstrap-static',
+      license='MIT',
+      packages=find_packages(),
+      install_requires = [],
+      include_package_data=True,
+      zip_safe=False,
+    )
