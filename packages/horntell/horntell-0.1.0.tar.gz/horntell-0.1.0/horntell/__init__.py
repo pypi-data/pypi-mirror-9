7aecdcc1d2556bb807f9db63c65f6b06521d78be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:40 +0000

    Add package horntell/0.1.0/horntell-0.1.0.tar.gz

---
+++ packages/horntell/horntell-0.1.0.tar.gz/horntell-0.1.0/horntell/__init__.py
@@ -0,0 +1,13 @@
+key     = None
+secret  = None
+
+base    = 'http://api.horntell.com'
+version = 'v1'
+
+#sdk
+from horntell.app import App
+from horntell.profile import Profile
+from horntell.horn import Horn
+from horntell.campaign import Campaign
+
+from horntell.errors import (Error, InvalidRequestError, AuthenticationError, ForbiddenError, NotFoundError, NetworkError, ServiceError)
