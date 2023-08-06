8cf0d1f7d3ce30ddfa34fe19746c8875fe0a35c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:29:57 +0000

    Add package somebar/0.0.5/somebar-0.0.5.tar.gz

---
+++ packages/somebar/somebar-0.0.5.tar.gz/somebar-0.0.5/setup.py
@@ -0,0 +1,41 @@
+import os
+import sys
+import platform
+from setuptools import setup, find_packages
+from os.path import join, dirname
+
+# copied from electrum project
+data_files = []
+if platform.system() == 'Linux':
+    usr_share = os.path.join(sys.prefix, 'share')
+    data_files += [
+        (os.path.join(usr_share, 'applications/'), ['somebar.desktop']),
+        (os.path.join(usr_share, 'pixmaps/'), ['somebar.xpm']),
+    ]
+
+
+setup(
+    name = 'somebar',
+    scripts = ['somebar'],
+    version = '0.0.5',
+    license = 'MIT License',
+    description = 'Simple taskbar widget that displays color dot or custom icon',
+    author = 'Ivan',
+    author_email = 'ivan.vershigora@gmail.com',
+    url = 'https://github.com/limpbrains/somebar',
+    download_url  ='https://github.com/limpbrains/somebar/tarball/0.0.5',
+    keywords = ['AnyBar', 'somebar', 'taskbar', 'indicator'],
+    packages = find_packages(),
+    classifiers = [
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Operating System :: POSIX :: Linux',
+    ],
+    package_data = {'somebar_icons' : ['*.png']},
+    data_files = data_files,
+    install_requires = [
+        # 'gi'
+    ],
+)
