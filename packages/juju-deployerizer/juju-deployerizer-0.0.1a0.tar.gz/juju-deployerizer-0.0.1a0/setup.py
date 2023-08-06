de1edb201e462f8d3f1a341cac7413a5047e48d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:40 +0000

    Add package juju-deployerizer/0.0.1a0/juju-deployerizer-0.0.1a0.tar.gz

---
+++ packages/juju-deployerizer/juju-deployerizer-0.0.1a0.tar.gz/juju-deployerizer-0.0.1a0/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = 'Jorge Niedbalski R. <jnr@metaklass.org>'
+
+import os
+from setuptools import setup, find_packages
+
+dependencies = ["PyYaml"]
+
+setup(
+    name="juju-deployerizer",
+    version="0.0.1a",
+    author="Jorge Niedbalski R.",
+    include_package_data=True,
+    author_email="jnr@metaklass.org",
+    description="",
+    install_requires=dependencies,
+    packages=find_packages(),
+    test_suite='nose.collector',
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+    ],
+    entry_points="""
+[console_scripts]
+juju-deployerizer = juju_deployerizer.cli:main
+"""
+)
