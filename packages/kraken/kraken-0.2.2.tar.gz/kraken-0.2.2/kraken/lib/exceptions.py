1c052b4a4dcc9b2b31845e3542b858ef5e5a4e2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:10 +0000

    Add package kraken/0.2.2/kraken-0.2.2.tar.gz

---
+++ packages/kraken/kraken-0.2.2.tar.gz/kraken-0.2.2/kraken/lib/exceptions.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+kraken.lib.exceptions
+~~~~~~~~~~~~~~~~~~~~~
+
+All custom exceptions raised by kraken's modules and packages. Packages should
+always define their exceptions here.
+"""
+
+class KrakenRecordException(Exception):
+
+    def __init__(self, message=None):
+        Exception.__init__(self, message)
+
+class KrakenInvalidModelException(Exception):
+
+    def __init__(self, message=None):
+        Exception.__init__(self, message)
+
