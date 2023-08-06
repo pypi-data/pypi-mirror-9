6de37790dcd4cc1f9d3dbb549905d8608cef529c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:25 +0000

    Add package pymorm/0.1/pymorm-0.1.tar.gz

---
+++ packages/pymorm/pymorm-0.1.tar.gz/pymorm-0.1/setup.py
@@ -0,0 +1,44 @@
+__author__ = "walter"
+try:
+    import logging
+    import multiprocessing
+except:
+    pass
+
+import sys
+py_version = sys.version_info[:2]
+
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup, find_packages
+
+dependency_links = []
+
+install_requires = [
+    "pymongo",
+    "bunch",
+]
+
+setup(
+    name='pymorm',
+    version='0.1',
+    description='Really simple pymongo-based ORM',
+    author='Walter Danilo Galante',
+    author_email='walter.galante@proxtome.com',
+    classifiers=["Development Status :: 3 - Alpha",
+                 "License :: OSI Approved :: MIT License",
+                 "Programming Language :: Python",
+                 "Topic :: Database :: Front-Ends",
+                 "Topic :: Software Development :: Libraries :: Python Modules",
+                 "Intended Audience :: Developers"],
+    url="https://github.com/devilicecream/pymorm",
+    packages=find_packages(),
+    keywords='mongodb pymongo simple orm',
+    dependency_links=dependency_links,
+    install_requires=install_requires,
+    entry_points={},
+    zip_safe=True
+)
