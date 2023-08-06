a84602a660dde1c6c4daf21c2c7c52f19f541b62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:37 +0000

    Add package run-exe/0.5/run-exe-0.5.zip

---
+++ packages/run-exe/run-exe-0.5.zip/run-exe-0.5/setup.py
@@ -0,0 +1,23 @@
+import platform
+import sys
+from distutils.core import setup
+
+from path_helpers import path
+
+root_dir = path(__file__).parent.abspath()
+if root_dir not in sys.path:
+    sys.path.insert(0, str(root_dir))
+import version
+
+
+setup(name='run-exe',
+      version=version.getVersion(),
+      description='Run executable file, with option to try as admin on error '
+      'on Windows.',
+      keywords='process windows administrator launch',
+      author='Christian Fobel',
+      author_email='christian@fobel.net',
+      url='https://github.com/cfobel/run-exe',
+      license='GPL',
+      long_description='\n%s\n' % open('README.md', 'rt').read(),
+      packages=['run_exe'])
