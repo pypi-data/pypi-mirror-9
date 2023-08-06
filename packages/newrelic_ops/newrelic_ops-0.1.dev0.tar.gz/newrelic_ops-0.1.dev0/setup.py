500d52f01375c43550d79a8b11d26a8cb7366c71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:57 +0000

    Add package newrelic_ops/0.1.dev0/newrelic_ops-0.1.dev0.tar.gz

---
+++ packages/newrelic_ops/newrelic_ops-0.1.dev0.tar.gz/newrelic_ops-0.1.dev0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='newrelic_ops',
+      version='0.1.dev',
+      description='newrelic manipulation tool',
+      url='https://github.com/Abukamel/newrelic_ops',
+      author='Ahmed Kamel',
+      author_email='k.tricky@gmail.com',
+      license='MIT',
+      packages=['newrelic_ops'],
+      zip_safe=False)
