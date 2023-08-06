284ed4d0c7f92a5799ca3b3ceb4233fc4b01dda7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:01 +0000

    Add package music/0.1.dev9/music-0.1.dev9.tar.gz

---
+++ packages/music/music-0.1.dev9.tar.gz/music-0.1.dev9/music/__init__.py
@@ -0,0 +1,9 @@
+def here():
+    return("there")
+from .tables import *
+from .converters import *
+from .synths import *
+from .utils import *
+from .effects import *
+#from .pieces import *
+from . import pieces
