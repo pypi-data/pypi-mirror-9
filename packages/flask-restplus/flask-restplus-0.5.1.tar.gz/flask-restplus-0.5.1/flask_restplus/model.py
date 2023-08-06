a42325dba9c8d300e53f764c8a61f570fccf9b6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:01 +0000

    Add package flask-restplus/0.5.1/flask-restplus-0.5.1.tar.gz

---
+++ packages/flask-restplus/flask-restplus-0.5.1.tar.gz/flask-restplus-0.5.1/flask_restplus/model.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from collections import MutableMapping
+
+
+class ApiModel(dict, MutableMapping):
+    '''A thin wrapper on dict to store API doc metadata'''
+    def __init__(self, *args, **kwargs):
+        self.__apidoc__ = {}
+        super(ApiModel, self).__init__(*args, **kwargs)
