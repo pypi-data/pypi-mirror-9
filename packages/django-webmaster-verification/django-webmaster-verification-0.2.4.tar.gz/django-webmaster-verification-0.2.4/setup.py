eefb0dd1fd3f4aa1821b1d14c78b0a3d92f71dca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:20 +0000

    Add package django-webmaster-verification/0.2.4/django-webmaster-verification-0.2.4.tar.gz

---
+++ packages/django-webmaster-verification/django-webmaster-verification-0.2.4.tar.gz/django-webmaster-verification-0.2.4/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "django-webmaster-verification",
+    version = "0.2.4",
+    packages = find_packages(),
+
+    author = "Nicolas Kuttler",
+    author_email = "pypi@kuttler.eu",
+    description = "Webmaster tools verification for Django",
+    long_description = open("README.rst").read(),
+    license = "BSD",
+    url = "http://github.com/nkuttler/django-webmaster-verification",
+    include_package_data = True,
+    classifiers = [
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Framework :: Django",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+    ],
+    install_requires = [
+        "Django >= 1.4",
+    ],
+    zip_safe = True,
+)
---
+++ packages/django-webmaster-verification/django-webmaster-verification-0.2.4.tar.gz/django-webmaster-verification-0.2.4/webmaster_verification/urls.py
@@ -0,0 +1,37 @@
+from django.conf.urls import patterns
+from django.conf.urls import url
+
+from .views import GoogleVerificationView
+from .views import BingVerificationView
+from .views import MajesticVerificationView
+from .views import YandexVerificationView
+from .views import AlexaVerificationView
+
+
+urlpatterns = patterns('',
+    url(
+        r'^google(?P<code>[0-9a-f]{16})\.html$',
+        GoogleVerificationView.as_view(),
+        name = 'google_verify'
+    ),
+    url(
+        r'^BingSiteAuth\.xml$',
+        BingVerificationView.as_view(),
+        name = 'bing_verify'
+    ),
+    url(
+        r'^MJ12_(?P<code>[0-9A-F]{32})\.txt$',
+        MajesticVerificationView.as_view(),
+        name = 'majestic_verify'
+    ),
+    url(
+        r'^yandex_(?P<code>[0-9a-f]{16})\.txt$',
+        YandexVerificationView.as_view(),
+        name = 'majestic_verify'
+    ),
+    url(
+        r'^(?P<code>[0-9a-zA-Z]{27})\.html$',
+        AlexaVerificationView.as_view(),
+        name = 'alexa_verify'
+    ),
+)
