df5a289af465fb25710418a60cc247e8f243bd76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:23 +0000

    Add package hickle/1.3.0/hickle-1.3.0.tar.gz

---
+++ packages/hickle/hickle-1.3.0.tar.gz/hickle-1.3.0/setup.py
@@ -0,0 +1,17 @@
+# To increment version
+# git tag x.y.z
+# git push --tags
+# python setup.py sdist upload
+from distutils.core import setup
+setup(name = 'hickle',
+      version = '1.3.0',
+      description = 'Hickle - a HDF5 based version of pickle',
+      author = 'Danny Price',
+      author_email = 'dan@thetelegraphic.com',
+      url = 'http://github.com/telegraphic/hickle',
+      download_url='https://github.com/telegraphic/hickle/archive/1.3.0.tar.gz',
+      platforms = 'Cross platform (Linux, Mac OSX, Windows)',
+      keywords = ['pickle', 'hdf5', 'data storage', 'data export'],
+      py_modules = ['hickle'],
+      install_requires=['numpy', 'h5py']
+      )
