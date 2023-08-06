9eac64123b5b00834f99f63fe8bc614d77657d39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:03 +0000

    Add package Marnadi/0.2.1/Marnadi-0.2.1.tar.gz

---
+++ packages/Marnadi/Marnadi-0.2.1.tar.gz/Marnadi-0.2.1/marnadi/errors.py
@@ -0,0 +1,30 @@
+from marnadi import Header, descriptors
+from marnadi.utils import to_bytes
+
+
+class HttpError(Exception):
+
+    headers = descriptors.Headers(
+        ('Content-Type', Header('text/plain', charset='utf-8')),
+    )
+
+    def __init__(
+        self,
+        status='500 Internal Server Error',
+        data=None,
+        headers=None,
+        error=None,
+        traceback=None,
+    ):
+        self.status = status
+        self.data = data
+        if headers:
+            self.headers.extend(headers)
+        self.error = error
+        self.traceback = traceback
+
+    def __len__(self):
+        return 1
+
+    def __iter__(self):
+        yield to_bytes(self.data)
