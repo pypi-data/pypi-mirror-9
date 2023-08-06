14c9ee69c898bb0d0fc4a23cb24906795213eb27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:11 +0000

    Add package django-throttleandcache/2.1.0/django-throttleandcache-2.1.0.tar.gz

---
+++ packages/django-throttleandcache/django-throttleandcache-2.1.0.tar.gz/django-throttleandcache-2.1.0/throttleandcache/conf.py
@@ -0,0 +1,35 @@
+from appconf import AppConf
+from django.conf import settings
+
+
+def get_dummy_cache():
+    try:
+        from django.core.cache.backends.dummy import DummyCache  # noqa
+    except ImportError:
+        return 'dummy://'
+    else:
+        return 'django.core.cache.backends.dummy.DummyCache'
+
+
+def get_default_cache_alias():
+    # DEFAULT_CACHE_ALIAS doesn't exist in Django<=1.2
+    try:
+        from django.core.cache import DEFAULT_CACHE_ALIAS
+        return DEFAULT_CACHE_ALIAS
+    except ImportError:
+        return 'default'
+
+
+class ThrottleAndCacheConf(AppConf):
+    DEFAULT_CACHE = None
+    MAX_TIMEOUT = 60 * 60 * 24 * 365 * 10
+
+    def configure_default_cache(self, value):
+        if value is not None:
+            return value
+
+        default_cache_alias = get_default_cache_alias()
+        if default_cache_alias in getattr(settings, 'CACHES', {}):
+            return default_cache_alias
+
+        return getattr(settings, 'CACHE_BACKEND', None) or get_dummy_cache()
