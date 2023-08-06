a66fd4c778c90f924dafb43d03a5ef1a15191427
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:51 +0000

    Add package blockscore/4.0.0/blockscore-4.0.0.tar.gz

---
+++ packages/blockscore/blockscore-4.0.0.tar.gz/blockscore-4.0.0/blockscore/__init__.py
@@ -0,0 +1,3 @@
+from blockscore.client import Client
+api_key = None # use blockscore.api_key = 'your_api_key' after importing blockscore
+
---
+++ packages/blockscore/blockscore-4.0.0.tar.gz/blockscore-4.0.0/blockscore/api/__init__.py
@@ -0,0 +1,4 @@
+# Import all the classes into api module
+from blockscore.api import people
+from blockscore.api import question_sets
+from blockscore.api import companies
