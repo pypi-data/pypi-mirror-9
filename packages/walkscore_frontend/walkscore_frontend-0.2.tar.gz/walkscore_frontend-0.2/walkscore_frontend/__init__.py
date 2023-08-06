2d7d3f7e0d56a367aa30ab48cb25bd9500a1eecb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:18 +0000

    Add package walkscore_frontend/0.2/walkscore_frontend-0.2.tar.gz

---
+++ packages/walkscore_frontend/walkscore_frontend-0.2.tar.gz/walkscore_frontend-0.2/walkscore_frontend/__init__.py
@@ -0,0 +1,2 @@
+# Import all of the frontend file
+from walkscore_frontend import *
---
+++ packages/walkscore_frontend/walkscore_frontend-0.2.tar.gz/walkscore_frontend-0.2/walkscore_frontend/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(name='walkscore_frontend',
+    version='0.1',
+    description='Front-end (scrape API) for the Walkscore website',
+    url='http://github.com/evilsoapbox',
+    author='James Malone',
+    author_email='jamalone@gmail.com',
+    license='MIT',
+    packages=['walkscore_frontend'],
+    install_requires=[
+        'beautifulsoup4',
+        'requests'
+    ],
+    test_suite='nose.collector',
+    tests_require=['nose'],
+    zip_safe=False)
