73dc20c6779b8e3778239b73cd9cbb015b1fbcdd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:44 +0000

    Add package redditcurl/0.2.0/redditcurl-0.2.0.tar.gz

---
+++ packages/redditcurl/redditcurl-0.2.0.tar.gz/redditcurl-0.2.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+setup(
+    name="redditcurl",
+    version="0.2.0",
+    description="Download the images you saved on Reddit.",
+    license="GPLv3",
+    author="Kaan Genç",
+    author_email="kaan_genc@outlook.com",
+    url="https://github.com/SeriousBug/redditcurl",
+    download_url="https://github.com/SeriousBug/redditcurl/tarball/0.2.0",
+    install_requires=["praw", "requests", "beautifulsoup4"],
+    keywords=["reddit", "images", "download"],
+    packages=["redditcurl", "redditcurl/websites"],
+    entry_points={
+        "console_scripts": ["redditcurl = redditcurl:__main__"]
+    },
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3 :: Only",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Utilities",
+    ]
+)
