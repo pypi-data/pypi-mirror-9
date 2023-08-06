0b5cbf8fa25beda447ed234ab6f12cc497edaad4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:48 +0000

    Add package SpoofMAC/2.0.4/SpoofMAC-2.0.4.tar.gz

---
+++ packages/SpoofMAC/SpoofMAC-2.0.4.tar.gz/SpoofMAC-2.0.4/setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Easily spoof your MAC address in OS X, Windows & Linux.
+"""
+from setuptools import setup, find_packages
+
+
+def get_version():
+    """
+    Load and return the current package version.
+    """
+    local_results = {}
+    exec(compile(open('spoofmac/version.py').read(), 'spoofmac/version.py', 'exec'), {}, local_results)
+    return local_results['__version__']
+
+
+if __name__ == '__main__':
+    setup(
+        name='SpoofMAC',
+        version=get_version(),
+        description=__doc__,
+        long_description=__doc__,
+        author='Feross Aboukhadijeh',
+        author_email='feross@feross.org',
+        url='http://feross.org/spoofmac/',
+        packages=find_packages(),
+        include_package_data=True,
+        install_requires=[
+            'docopt'
+        ],
+        scripts=[
+            'scripts/spoof-mac.py',
+	    'scripts/spoof-mac'
+        ],
+        license='MIT'
+    )
