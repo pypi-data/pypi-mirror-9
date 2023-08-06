69ebd12068eca5e03f999b606e655be8d5202f0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:41 +0000

    Add package chanjo-report/0.0.12/chanjo_report-0.0.12-py2.py3-none-any.whl

---
+++ packages/chanjo-report/chanjo_report-0.0.12-py2.py3-none-any.whl/chanjo_report/server/extensions.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""Extensions module. Each extension is initialized in the app factory
+located in app.py
+"""
+from __future__ import absolute_import, unicode_literals
+
+from ..miner import Miner
+api = Miner()
