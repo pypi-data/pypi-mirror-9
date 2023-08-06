3fb5c66685b0799c2a3d524b262f4ebbd3454fff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:24 +0000

    Add package Dockets/0.3.4/Dockets-0.3.4.tar.gz

---
+++ packages/Dockets/Dockets-0.3.4.tar.gz/Dockets-0.3.4/dockets/__init__.py
@@ -0,0 +1,28 @@
+__version__ = '0.3.4'
+
+import logging
+from dockets.logging_event_handler import LoggingEventHandler
+
+class NullHandler(logging.Handler):
+    def emit(self, record):
+        pass
+
+logging.getLogger('dockets').addHandler(NullHandler())
+
+
+_global_event_handler_classes = set()
+
+def add_global_event_handler(handler_class):
+    _global_event_handler_classes.add(handler_class)
+
+def clear_global_event_handlers():
+    _global_event_handler_classes.clear()
+
+
+_global_retry_error_classes = set()
+
+def add_global_retry_error_class(error_class):
+    _global_retry_error_classes.add(error_class)
+
+def clear_global_retry_error_classes():
+    _global_retry_error_classes.clear()
