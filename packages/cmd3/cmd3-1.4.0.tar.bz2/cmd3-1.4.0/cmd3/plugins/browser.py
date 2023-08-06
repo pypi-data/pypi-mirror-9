9739ab2a4dfe38a8a3d1a2da340df2319e1d3f71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:54 +0000

    Add package cmd3/1.4.0/cmd3-1.4.0.tar.bz2

---
+++ packages/cmd3/cmd3-1.4.0.tar.bz2/cmd3-1.4.0/cmd3/plugins/browser.py
@@ -0,0 +1,56 @@
+import webbrowser
+import os
+
+from cmd3.shell import command
+
+
+# noinspection PyUnusedLocal
+class browser:
+
+    #
+    # Browser
+    #
+
+    def _expand_filename(self, line):
+        """expands the filename if there is a . as leading path"""
+        # expand .
+        newline = line
+        path = os.getcwd()
+        if newline.startswith("."):
+            newline = newline.replace(".", path, 1)
+        # expand ~
+        newline = os.path.expanduser(newline)
+        return newline
+
+    @command
+    def do_open(self, args, arguments):
+        """
+        ::
+
+            Usage:
+                    open FILENAME
+
+            ARGUMENTS:
+                FILENAME  the file to open in the cwd if . is
+                          specified. If file in in cwd
+                          you must specify it with ./FILENAME
+
+            Opens the given URL in a browser window.
+        """
+        filename = arguments['FILENAME']
+        filename = self._expand_filename(filename)
+        print "open %s" % filename
+
+        if not (filename.startswith("file:") or filename.startswith("http:")):
+            try:
+                with open(filename):
+                    pass
+                filename += "file://"
+            except:
+                print "can not open file %s" % filename
+                return
+
+        try:
+            webbrowser.open("%s" % filename)
+        except:
+            print "can not open browser %s" % filename
