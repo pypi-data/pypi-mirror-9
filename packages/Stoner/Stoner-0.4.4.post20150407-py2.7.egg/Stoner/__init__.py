5d778caedb50911a41679241e4aeefcb73da50d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:01 +0000

    Add package Stoner/0.4.4.post20150407/Stoner-0.4.4.post20150407-py2.7.egg

---
+++ packages/Stoner/Stoner-0.4.4.post20150407-py2.7.egg/Stoner/__init__.py
@@ -0,0 +1,15 @@
+"""Stoner Python Package: Utility classes for simple data analysis scripts.
+
+See http://github.com/~gb119/Stoner-PythonCode for more details."""
+
+__all__=['Core', 'Analysis', 'Plot', 'PlotFormats','Folders','DataFile','Data','DataFolder']
+
+# These fake the old namespace if you do an import Stoner
+
+from .Core import DataFile
+from .Util import Data
+from Stoner.Folders import DataFolder
+
+__version_info__ = ('0', '4', '4')
+__version__ = '.'.join(__version_info__)
+
