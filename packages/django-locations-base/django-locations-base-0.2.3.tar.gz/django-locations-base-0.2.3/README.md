3086a359a8d56d27774e11b9bfb9960d54e79b29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:27 +0000

    Add package django-locations-base/0.2.3/django-locations-base-0.2.3.tar.gz

---
+++ packages/django-locations-base/django-locations-base-0.2.3.tar.gz/django-locations-base-0.2.3/README.md
@@ -0,0 +1,17 @@
+django-locations
+================
+
+A small reusable locations app for Django.  Formally apart of the Hadrian project.
+
+This supports Python 3 **only**
+
+
+Installation 
+------------
+
+    pip install django-locations-base
+    
+    
+Add to installed apps.
+
+More docs coming soon.
---
+++ packages/django-locations-base/django-locations-base-0.2.3.tar.gz/django-locations-base-0.2.3/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+version = '0.2.3'
+
+setup(name='django-locations-base',
+      version=version,
+      description="A simple locations app for Django",
+      long_description=open("README.md", "r").read(),
+      classifiers=[
+          "Development Status :: 5 - Production/Stable",
+          "Environment :: Web Environment",
+          "Intended Audience :: End Users/Desktop",
+          "Natural Language :: English",
+          "Operating System :: OS Independent",
+          "Framework :: Django",
+          "Programming Language :: Python",
+          "Programming Language :: Python :: 3",
+          "Topic :: Internet :: WWW/HTTP :: Dynamic Content :: CGI Tools/Libraries",
+          "Topic :: Utilities",
+          "License :: OSI Approved :: MIT License",
+          ],
+      keywords='',
+      author='Derek Stegelman',
+      author_email='dstegelman@gmail.com',
+      url='http://github.com/dstegelman/django-locations',
+      license='MIT',
+      packages=['locations',
+                'locations.migrations'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=['django-localflavor>=1.1,<2.0', 'geopy>=1.9.1,<2.0']
+    )
