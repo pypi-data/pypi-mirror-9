39093bb44e213b318e81923a1fc4804ee3b240e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:17 +0000

    Add package django-ghostdown/0.5/django-ghostdown-0.5.tar.gz

---
+++ packages/django-ghostdown/django-ghostdown-0.5.tar.gz/django-ghostdown-0.5/ghostdown/conf.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+from django.conf import settings
+from appconf import AppConf
+
+
+__all__ = ['GhostdownConf', 'settings']
+
+
+DEFAULTS = {   # Default setting values
+    'JQUERY_URL': '//code.jquery.com/jquery-1.10.1.min.js',
+    'CODEMIRROR_JS_URL': (
+        '//cdnjs.cloudflare.com/ajax/libs/codemirror/3.20.0/codemirror.min.js'
+    ),
+    'CODEMIRROR_MARKDOWN_JS_URL': (
+        '//cdnjs.cloudflare.com/ajax/libs/codemirror/3.20.0/mode/markdown/'
+        'markdown.min.js'
+    ),
+    'SHOWDOWN_JS_URL': (
+        '//cdnjs.cloudflare.com/ajax/libs/showdown/0.3.1/showdown.min.js'
+    )
+}
+
+
+def _get_setting(key, fallback_key=None):
+    if fallback_key is None:
+        fallback_key = key
+    return getattr(settings, key, DEFAULTS[fallback_key])
+
+
+# Load Appconf
+class GhostdownConf(AppConf):
+    JQUERY_URL = _get_setting('JQUERY_URL')
+    CODEMIRROR_JS_URL = _get_setting('CODEMIRROR_JS_URL')
+    CODEMIRROR_MARKDOWN_JS_URL = _get_setting('CODEMIRROR_MARKDOWN_JS_URL')
+    CODEMIRROR_DEFAULT_OPTIONS = {
+        'mode': 'markdown',
+        'tabMode': 'indent',
+        'lineWrapping': True,
+    }
+    SHOWDOWN_JS_URL = _get_setting('SHOWDOWN_JS_URL')
+    USE_LIVE_PREVIEW = False
+    MARKDOWN_RENDERER = {
+        'path': None,
+        'args': (),
+        'kwargs': {},
+    }
