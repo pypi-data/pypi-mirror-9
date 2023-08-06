ca9d9affe34396cfc9682f2a61048229d5990829
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:46 +0000

    Add package django-skin/0.1.dev0/django-skin-0.1.dev.post2.zip

---
+++ packages/django-skin/django-skin-0.1.dev.post2.zip/django-skin-0.1.dev0/skin/__init__.py
@@ -0,0 +1,15 @@
+__version__ = '0.1'
+
+try:
+    # noinspection PyUnresolvedReferences
+    from django.conf import settings as django_settings
+
+    if 'skin' in django_settings.INSTALLED_APPS:
+        # noinspection PyUnresolvedReferences
+        from skin.conf import settings
+except ImportError:  # pragma: no cover
+    """
+    This exception means that either the application is being built, or is
+    otherwise installed improperly.
+    """
+    pass
