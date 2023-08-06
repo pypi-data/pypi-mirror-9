19ac08e99804394eadcb27766cd79eea49e934a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:30 +0000

    Add package maildump/0.5.1/maildump-0.5.1.tar.gz

---
+++ packages/maildump/maildump-0.5.1.tar.gz/maildump-0.5.1/maildump/static/js/maildump.js
@@ -0,0 +1,215 @@
+(function($) {
+    'use strict';
+
+    $(document).ready(function() {
+        // Misc stuff and initialization
+        $('.resizer').on('mousedown', function(e) {
+            if(e.button != 0) {
+                return;
+            }
+            var $this = $(this);
+            var target = $this.data('sibling') == 'prev' ? $this.prev() : $this.next();
+            e.preventDefault();
+            $(document).on('mousemove.resizer', function(e) {
+                e.preventDefault();
+                target.css('height', e.clientY - target.offset().top);
+            }).on('mouseup.resizer', function(e) {
+                e.preventDefault();
+                $(document).off('.resizer');
+            });
+        });
+
+        $('#disconnected-dialog, #loading-dialog').dialog({
+            autoOpen: false,
+            closeOnEscape: false,
+            dialogClass: 'no-close',
+            draggable: false,
+            modal: true,
+            resizable: false,
+            title: ''
+        });
+
+        // Top nav actions
+        $('nav.app .quit a').on('click', function(e) {
+            e.preventDefault();
+            if(!confirm('Do you really want to terminate the MailDump application?')) {
+                return;
+            }
+            restCall('DELETE', '/');
+        });
+
+        $('nav.app .clear a').on('click', function(e) {
+            e.preventDefault();
+            if (!confirm('Do you really want to delete all messages?')) {
+                return;
+            }
+            restCall('DELETE', '/messages/');
+        });
+
+        if (NotificationUtil.available) {
+            var notificationButton = $('nav.app .notifications a');
+
+            var updateNotificationButton = function updateNotificationButton() {
+                var enabled = $.jStorage.get('notifications') && NotificationUtil.checkPermission();
+                notificationButton.text(enabled ? 'Disable notifications' : 'Enable notifications');
+            };
+
+            notificationButton.parent().show();
+            notificationButton.on('click', function(e) {
+                e.preventDefault();
+                switch (NotificationUtil.checkPermission()) {
+                    case false: // denied
+                        alert('You need to allow notifications via site permissions.');
+                        return;
+                    case true: // allowed
+                        $.jStorage.set('notifications', !$.jStorage.get('notifications'));
+                        updateNotificationButton();
+                        break;
+                    default: // not specified (prompt user)
+                        NotificationUtil.requestPermission(function(perm) {
+                            $.jStorage.set('notifications', !!perm);
+                            updateNotificationButton();
+                        });
+                        break;
+
+                }
+            });
+            updateNotificationButton();
+        }
+
+        $('#search').on('keyup', function() {
+            var term = $(this).val().trim().toLowerCase();
+            Message.applyFilter(term);
+        });
+
+        // Message navigation
+        $('#messages').on('click', '> tr:not(.deleted)', function(e) {
+            var msg;
+            if (e.ctrlKey) {
+                msg = Message.getSelected();
+                if (msg) {
+                    msg.deselect();
+                }
+                if (window.getSelection) {
+                    window.getSelection().removeAllRanges();
+                }
+            }
+            else {
+                msg = Message.get($(this).data('messageId'));
+                if (msg && msg != Message.getSelected()) {
+                    msg.select();
+                    $('#message').show();
+                }
+            }
+        });
+
+        $('.tab.format').on('click', function(e) {
+            e.preventDefault();
+            var msg = Message.getSelected();
+            if (msg) {
+                $('.tab.format.selected').removeClass('selected');
+                $(this).addClass('selected');
+                msg.updateFormat();
+            }
+        });
+
+        $('.action.delete').on('click', function(e) {
+            e.preventDefault();
+            var msg = Message.getSelected();
+            if (msg) {
+                msg.delRemote();
+            }
+        });
+
+        // Load initial message list
+        Message.loadAll();
+
+        // Real-time updates
+        var socket = io.connect(undefined, {
+            'reconnection limit': 10000,
+            'max reconnection attempts': Infinity
+        });
+        var terminating = false;
+        window.onbeforeunload = function() {
+            terminating = true;
+            socket.disconnect();
+            Message.closeNotifications()
+        };
+        socket.on('connect', function() {
+            $('#disconnected-dialog').dialog('close');
+        }).on('reconnect', function() {
+            Message.loadAll();
+        }).on('disconnect', function() {
+            if(terminating) {
+                return;
+            }
+            $('#loading-dialog').dialog('close');
+            $('#disconnected-dialog').dialog('open');
+        }).on('add_message', function(id) {
+            Message.load(id, $.jStorage.get('notifications'));
+        }).on('delete_message', function(id) {
+            var msg = Message.get(id);
+            if(msg) {
+                msg.del();
+            }
+        }).on('delete_messages', function() {
+            Message.deleteAll();
+        });
+
+        // Keyboard shortcuts
+        registerHotkeys({
+            'del': function() {
+                var msg = Message.getSelected();
+                if (msg) {
+                    msg.delRemote();
+                }
+            },
+            'backspace': function(e) {
+                // Backspace causing the iframe to go back even if it's not focused is annoying!
+                e.preventDefault();
+            },
+            'f5': function() {
+                // Chrome bug: http://stackoverflow.com/q/5971710/298479
+                Message.closeNotifications();
+            },
+            'ctrl+f5': function() {
+                // Chrome bug: http://stackoverflow.com/q/5971710/298479
+                Message.closeNotifications();
+            },
+            'ctrl+r': function() {
+                // Chrome bug: http://stackoverflow.com/q/5971710/298479
+                Message.closeNotifications();
+            },
+            'up': function(e) {
+                e.preventDefault();
+                var msg = Message.getSelected();
+                if (!msg) {
+                    $('#messages > tr:last').trigger('click');
+                    return;
+                }
+                msg.dom().prevAll(':visible').first().trigger('click');
+            },
+            'down': function(e) {
+                e.preventDefault();
+                var msg = Message.getSelected();
+                if (!msg) {
+                    $('#messages > tr:first').trigger('click');
+                    return;
+                }
+                msg.dom().nextAll(':visible').first().trigger('click');
+            },
+            'ctrl+up': function(e) {
+                e.preventDefault();
+                $('#messages > tr:first').trigger('click');
+            },
+            'ctrl+down': function(e) {
+                e.preventDefault();
+                $('#messages > tr:last').trigger('click');
+            },
+            '/': function(e) {
+                e.preventDefault();
+                $('#search').focus();
+            }
+        });
+    });
+})(jQuery);
---
+++ packages/maildump/maildump-0.5.1.tar.gz/maildump-0.5.1/maildump/util.py
@@ -0,0 +1,79 @@
+import json
+import pkg_resources
+from datetime import datetime
+from email.header import decode_header as _decode_header
+from email.utils import getaddresses
+from functools import wraps
+
+from pytz import utc
+from flask import current_app
+from webassets.filter.cssprefixer import CSSPrefixer as _CSSPrefixer
+
+
+def _json_default(obj):
+    if isinstance(obj, datetime):
+        return utc.localize(obj).isoformat()
+    raise TypeError(repr(obj) + ' is not JSON serializable')
+
+
+def json_dumps(obj):
+    return json.dumps(obj, default=_json_default, indent=4)
+
+
+def jsonify(*args, **kwargs):
+    return current_app.response_class(json_dumps(dict(*args, **kwargs)), mimetype='application/json')
+
+
+def bool_arg(arg):
+    return arg in ('yes', 'true', '1')
+
+
+def decode_header(value):
+    return ''.join(unicode(decoded, charset or 'utf-8') for decoded, charset in _decode_header(value)).encode('utf-8')
+
+
+def split_addresses(value):
+    return [('{0} <{1}>'.format(name, addr) if name else addr)
+            for name, addr in getaddresses([value])]
+
+
+def rest(f):
+    """Decorator for simple REST endpoints.
+
+    Functions must return one of these values:
+    - a dict to jsonify
+    - nothing for an empty 204 response
+    - a tuple containing a status code and a dict to jsonify
+    """
+
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        ret = f(*args, **kwargs)
+        if ret is None:
+            response = jsonify()
+            response.status_code = 204  # no content
+        elif isinstance(ret, current_app.response_class):
+            response = ret
+        elif isinstance(ret, tuple):
+            # code, result_dict|msg_string
+            if isinstance(ret[1], basestring):
+                response = jsonify(msg=ret[1])
+            else:
+                response = jsonify(**ret[1])
+            response.status_code = ret[0]
+        else:
+            response = jsonify(**ret)
+        return response
+
+    return wrapper
+
+
+def get_version():
+    try:
+        return 'v' + pkg_resources.get_distribution('maildump').version
+    except pkg_resources.DistributionNotFound:
+        return 'dev'
+
+
+class CSSPrefixer(_CSSPrefixer):
+    max_debug_level = None
