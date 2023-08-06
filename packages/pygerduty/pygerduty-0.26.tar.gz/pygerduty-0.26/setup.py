99f28c4796f4093d1fc7d3d1686623a68f208fb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:03 +0000

    Add package pygerduty/0.26/pygerduty-0.26.tar.gz

---
+++ packages/pygerduty/pygerduty-0.26.tar.gz/pygerduty-0.26/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+execfile('pygerduty/version.py')
+
+kwargs = {
+    "name": "pygerduty",
+    "version": str(__version__),
+    "packages": ["pygerduty"],
+    "scripts": ["bin/grab_oncall.py"],
+    "description": "Python Client Library for PagerDuty's REST API",
+    "author": "Gary M. Josack",
+    "maintainer": "Gary M. Josack",
+    "author_email": "gary@dropbox.com",
+    "maintainer_email": "gary@dropbox.com",
+    "license": "MIT",
+    "url": "https://github.com/dropbox/pygerduty",
+    "download_url": "https://github.com/dropbox/pygerduty/archive/master.tar.gz",
+    "classifiers": [
+        "Programming Language :: Python",
+        "Topic :: Software Development",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ]
+}
+
+setup(**kwargs)
