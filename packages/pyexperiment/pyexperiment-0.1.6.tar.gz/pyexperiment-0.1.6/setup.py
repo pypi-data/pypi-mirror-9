db05f3448fa3fa7e490c94dcc89f1b4e8d5abfe7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:20 +0000

    Add package pyexperiment/0.1.6/pyexperiment-0.1.6.tar.gz

---
+++ packages/pyexperiment/pyexperiment-0.1.6.tar.gz/pyexperiment-0.1.6/setup.py
@@ -0,0 +1,44 @@
+"""Setup for pyexperiment
+"""
+from __future__ import print_function
+# from __future__ import unicode_literals
+from __future__ import division
+from __future__ import absolute_import
+
+import os
+from setuptools import setup
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a
+# top level
+# README file and 2) it's easier to type in the README file than to
+# put a raw
+# string in below ...
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name="pyexperiment",
+    version="0.1.6",
+    author="Peter Duerr",
+    author_email="duerrp@gmail.com",
+    description="Framework for easy and clean experiments with python.",
+    license="MIT",
+    keywords="science experiment",
+    url="https://github.com/duerrp/pyexperiment",
+    download_url="https://github.com/duerrp/pyexperiment/tarball/0.1.6",
+    packages=['pyexperiment',
+              'pyexperiment.conf',
+              'pyexperiment.state',
+              'pyexperiment.utils',
+              'pyexperiment.log',
+          ],
+    long_description=read('README'),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
