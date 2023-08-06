5a16ba346e45f634e4291e9ddb7d28f4a4b91613
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:37:26 +0000

    Add package Hydra/2.1/Hydra-2.1.tar.gz

---
+++ packages/Hydra/Hydra-2.1.tar.gz/Hydra-2.1/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+from setuptools.extension import Extension
+from Cython.Distutils import build_ext
+from os.path import join
+
+import os
+
+ext_modules = [Extension("_hydra",
+                         extra_compile_args=['-std=gnu99',
+                                             '-O2',
+                                             '-D_LARGEFILE64_SOURCE'],
+                         sources=["src/_hydra.pyx",
+                                  'src/mmap_writer.c',
+                                  'src/MurmurHash3.c'],
+
+                         # path to .h file(s)
+                         include_dirs=[join(os.getcwd(), 'src')],
+
+                         # path to .a or .so file(s)
+                         library_dirs=[join(os.getcwd(), 'src')])]
+
+setup(name='Hydra',
+      author='Victor Ng',
+      author_email='crankycoder@gmail.com',
+      description='A high performance persistent bloom filter',
+      url="http://github.com/crankycoder/Hydra",
+      version='2.1',
+      license='MIT License',
+      cmdclass={'build_ext': build_ext},
+      zip_safe=False,
+      package_dir={'': 'src'},
+      py_modules=['hydra'],
+      ext_modules=ext_modules)
