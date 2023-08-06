b2a5a50400875bf6c174308606f83154b2306020
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:56 +0000

    Add package kv_progrid/0.1.2/kv_progrid-0.1.2.tar.gz

---
+++ packages/kv_progrid/kv_progrid-0.1.2.tar.gz/kv_progrid-0.1.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name        ='kv_progrid',
+    version     ='0.1.2',
+    description ='Easy to use grid for kivy.',
+    url         ='https://github.com/Cuuuurzel/kivy-pro-grid',
+    author      ='Federico Curzel',
+    author_email='cuuuurzel@gmail.com',
+    packages    =['progrid'],
+    zip_safe    =False,
+#    install_requires=['kivy','material_ui'],
+    include_package_data=True,
+)
