a8dfa3d039b14676ddea16803b3fdab1dd47d428
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:29 +0000

    Add package databaker/0.0.15/databaker-0.0.15.tar.gz

---
+++ packages/databaker/databaker-0.0.15.tar.gz/databaker-0.0.15/databaker/constants.py
@@ -0,0 +1,21 @@
+from xypath import DOWN, UP, LEFT, RIGHT
+import bake
+
+OBS = -9
+DATAMARKER = -8
+STATUNIT = -7
+MEASURETYPE = -6
+UNITMULTIPLIER = -5
+UNITOFMEASURE = -4
+GEOG = -3
+TIME = -2
+TIMEUNIT = -1
+STATPOP = 0
+
+ABOVE = UP
+BELOW = DOWN
+
+DIRECTLY = True
+CLOSEST = False
+
+PARAMS = lambda: bake.Opt.params
