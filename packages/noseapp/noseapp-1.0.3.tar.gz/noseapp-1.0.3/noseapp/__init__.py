1e7fa610bd2a6e3b331f6aa4cd1096ecbe778cb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:39 +0000

    Add package noseapp/1.0.3/noseapp-1.0.3.tar.gz

---
+++ packages/noseapp/noseapp-1.0.3.tar.gz/noseapp-1.0.3/noseapp/__init__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+
+from noseapp.suite import Suite
+from noseapp.app import NoseApp
+from noseapp.case import TestCase
+from noseapp.plugins import AppPlugin
+from noseapp.case import ScreenPlayCase
+
+
+__all__ = (
+    Suite,
+    NoseApp,
+    TestCase,
+    AppPlugin,
+    ScreenPlayCase,
+)
