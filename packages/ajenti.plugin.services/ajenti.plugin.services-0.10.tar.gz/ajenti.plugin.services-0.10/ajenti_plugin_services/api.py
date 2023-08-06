b37b1b38c5fe5fe015f87472c68c693f6e532043
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:06 +0000

    Add package ajenti.plugin.services/0.10/ajenti.plugin.services-0.10.tar.gz

---
+++ packages/ajenti.plugin.services/ajenti.plugin.services-0.10.tar.gz/ajenti.plugin.services-0.10/ajenti_plugin_services/api.py
@@ -0,0 +1,39 @@
+from aj.api import *
+
+
+class Service(object):
+    def __init__(self, manager):
+        self.id = None
+        self.name = None
+        self.manager = manager
+        self.state = None
+        self.running = None
+
+
+class ServiceOperationError(Exception):
+    def __init__(self, inner):
+        self.inner = inner
+
+    def __unicode__(self):
+        return '[ServiceOperationError %s]' % self.inner
+
+
+@interface
+class ServiceManager(object):
+    id = None
+    name = None
+
+    def list(self):
+        raise NotImplementedError
+
+    def get(self, _id):
+        raise NotImplementedError
+
+    def start(self, _id):
+        raise NotImplementedError
+
+    def stop(self, _id):
+        raise NotImplementedError
+
+    def restart(self, _id):
+        raise NotImplementedError
---
+++ packages/ajenti.plugin.services/ajenti.plugin.services-0.10.tar.gz/ajenti.plugin.services-0.10/ajenti_plugin_services/managers/upstart_manager.py
@@ -0,0 +1,69 @@
+from dbus.exceptions import DBusException
+from upstart.system import UpstartSystem, DirectUpstartBus
+from upstart.job import UpstartJob
+
+from aj.api import *
+from aj.plugins.services.api import ServiceManager, Service, ServiceOperationError
+
+
+@component(ServiceManager)
+class UpstartServiceManager(ServiceManager):
+    id = 'upstart'
+    name = 'Upstart'
+
+    @classmethod
+    def __verify__(cls):
+        try:
+            UpstartSystem()
+            return True
+        except:
+            try:
+                UpstartSystem(bus=DirectUpstartBus())
+                return True
+            except:
+                return False
+
+    def __init__(self, context):
+        self.bus = None
+        try:
+            self.upstart = UpstartSystem()
+        except:
+            self.bus = DirectUpstartBus()
+            self.upstart = UpstartSystem(bus=self.bus)
+
+    def __fix_name(self, name):
+        return name.replace('_2d', '-').replace('_2e', '.')
+
+    def list(self):
+        for job_name in self.upstart.get_all_jobs():
+            yield self.get(job_name)
+
+    def get(self, _id):
+        job = UpstartJob(_id, bus=self.bus)
+        svc = Service(self)
+        svc.id = _id
+        svc.name = self.__fix_name(_id)
+        try:
+            svc.state = job.get_status()['state']
+            svc.running = svc.state == 'running'
+        except:
+            svc.running = False
+        return svc
+
+    def start(self, _id):
+        try:
+            UpstartJob(_id).start()
+        except DBusException as e:
+            raise ServiceOperationError(e)
+
+    def stop(self, _id):
+        try:
+            UpstartJob(_id).stop()
+        except DBusException as e:
+            raise ServiceOperationError(e)
+
+    def restart(self, _id):
+        try:
+            UpstartJob(_id).restart()
+        except DBusException as e:
+            raise ServiceOperationError(e)
