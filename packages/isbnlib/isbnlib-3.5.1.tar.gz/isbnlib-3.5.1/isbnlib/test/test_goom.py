f380452257315a557c6555209bf1773fbec0a195
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:36:32 +0000

    Add package isbnlib/3.5.1/isbnlib-3.5.1.tar.gz

---
+++ packages/isbnlib/isbnlib-3.5.1.tar.gz/isbnlib-3.5.1/isbnlib/test/test_goom.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# flake8: noqa
+# pylint: skip-file
+
+"""
+nose tests
+"""
+
+from .. import _goom as goom
+from nose.tools import assert_equals
+
+
+def test_goom():
+    assert_equals(len(repr(goom.query('the old man and the sea'))) > 1000, True)
