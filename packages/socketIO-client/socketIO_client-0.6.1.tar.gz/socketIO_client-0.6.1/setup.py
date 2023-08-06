3a160186d210176b888afc558408f07050fa31bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:19 +0000

    Add package socketIO-client/0.6.1/socketIO_client-0.6.1.tar.gz

---
+++ packages/socketIO-client/socketIO_client-0.6.1.tar.gz/socketIO_client-0.6.1/setup.py
@@ -0,0 +1,39 @@
+from os.path import abspath, dirname, join
+from setuptools import find_packages, setup
+
+
+REQUIREMENTS = [
+    'requests',
+    'six',
+    'websocket-client',
+]
+
+
+HERE = dirname(abspath(__file__))
+DESCRIPTION = '\n\n'.join(open(join(HERE, _)).read() for _ in [
+    'README.rst',
+    'CHANGES.rst',
+])
+setup(
+    name='socketIO_client',
+    version='0.6.1',
+    description='A socket.io client library',
+    long_description=DESCRIPTION,
+    license='MIT',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'License :: OSI Approved :: MIT License',
+    ],
+    keywords='socket.io node.js',
+    author='Roy Hyunjin Han',
+    author_email='rhh@crosscompute.com',
+    url='https://github.com/invisibleroads/socketIO-client',
+    install_requires=REQUIREMENTS,
+    tests_require=[
+        'nose',
+        'coverage',
+    ],
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False)
