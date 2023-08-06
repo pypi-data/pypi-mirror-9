48bc58fd967aaab2dfa9a283544b2700002a616b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:59 +0000

    Add package PyPTables/1.0.2/PyPTables-1.0.2.tar.gz

---
+++ packages/PyPTables/PyPTables-1.0.2.tar.gz/PyPTables-1.0.2/setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+
+setup(
+    name='PyPTables',
+    version='1.0.2',
+    author='Jamie Cockburn',
+    author_email='jamie_cockburn@hotmail.co.uk',
+    packages=['pyptables',
+              'pyptables.test',
+              'pyptables.rules',
+              'pyptables.rules.input',
+              'pyptables.rules.forwarding',
+              ],
+    scripts=[],
+    url='https://github.com/daggaz/python-pyptables',
+    license='LICENSE.txt',
+    description='Python package for generating Linux iptables configurations.',
+    long_description=open('README.rst').read(),
+    install_requires=[
+        "nose",
+        "coverage",
+    ],
+)
