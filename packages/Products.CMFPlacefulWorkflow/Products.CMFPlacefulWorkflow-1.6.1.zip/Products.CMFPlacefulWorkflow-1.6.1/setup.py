1bea0065ae58cbab37d0b4724f5bd665fec81c89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:54 +0000

    Add package Products.CMFPlacefulWorkflow/1.6.1/Products.CMFPlacefulWorkflow-1.6.1.zip

---
+++ packages/Products.CMFPlacefulWorkflow/Products.CMFPlacefulWorkflow-1.6.1.zip/Products.CMFPlacefulWorkflow-1.6.1/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+
+version = '1.6.1'
+
+setup(name='Products.CMFPlacefulWorkflow',
+      version=version,
+      description="Workflow policies for Plone",
+      long_description=open("README.txt").read() + "\n" +
+                       open("CHANGES.txt").read(),
+      classifiers=[
+        "Framework :: Plone",
+        "Framework :: Plone :: 5.0",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        ],
+      keywords='CMF Plone Zope2 workflow',
+      author='Encolpe DEGOUTE',
+      author_email='plone-developers@lists.sourceforge.net',
+      url='http://pypi.python.org/pypi/Products.CMFPlacefulWorkflow',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['Products'],
+      include_package_data=True,
+      zip_safe=False,
+      extras_require=dict(
+          test=[
+              'plone.app.testing',
+              'zope.testing',
+          ]),
+      install_requires=[
+          'setuptools',
+          'zope.component',
+          'zope.interface',
+          'zope.i18nmessageid',
+          'Products.CMFCore',
+          'Products.CMFPlone',
+          'Products.GenericSetup',
+      ],
+      )
