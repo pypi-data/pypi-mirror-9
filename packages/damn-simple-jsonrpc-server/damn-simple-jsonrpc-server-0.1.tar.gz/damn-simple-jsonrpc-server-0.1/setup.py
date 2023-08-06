b4eacd656d880af38bf4fc42efe283518131b1b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:20 +0000

    Add package damn-simple-jsonrpc-server/0.1/damn-simple-jsonrpc-server-0.1.tar.gz

---
+++ packages/damn-simple-jsonrpc-server/damn-simple-jsonrpc-server-0.1.tar.gz/damn-simple-jsonrpc-server-0.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(name='damn-simple-jsonrpc-server',
+      version='0.1',
+      description='Damn simple, framework-agnostic JSON-RPC server',
+      classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Web Environment",
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python :: 2.7",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      author='Marcin Nowak',
+      author_email='marcin.j.nowak@gmail.com',
+      url='https://github.com/marcinn/json-rpc-server',
+      keywords='web json rpc python server',
+      py_modules=['jsonrpcserver'],
+      include_package_data=True,
+      zip_safe=False,
+      )
