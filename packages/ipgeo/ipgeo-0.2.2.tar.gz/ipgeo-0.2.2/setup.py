5f51d0adc830d8a6dcd6602510365286c95888ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:19 +0000

    Add package ipgeo/0.2.2/ipgeo-0.2.2.tar.gz

---
+++ packages/ipgeo/ipgeo-0.2.2.tar.gz/ipgeo-0.2.2/setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+
+setup(
+  name = 'ipgeo',
+  packages = ['ipgeo'],
+  version = '0.2.2',
+  description = 'Geo info retriver for ipv4 address using chinese taobao service',
+  author = 'Ai Chao',
+  author_email = 'aichaoguy@live.com',
+  url = 'https://github.com/aichaoguy/ipgeo',
+  download_url = 'https://github.com/aichaoguy/ipgeo/tarball/0.2.2',
+  keywords = ['ip', 'geo', 'taobao'], # arbitrary keywords
+  classifiers = [
+      "Programming Language :: Python",
+      "Environment :: Other Environment",
+      "Operating System :: OS Independent",
+      "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+)
