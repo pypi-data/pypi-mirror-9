f625f5f217b890e36a52c7b37ac31cb2a3660094
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:02 +0000

    Add package requests-aliyun/0.2.1/requests-aliyun-0.2.1.tar.gz

---
+++ packages/requests-aliyun/requests-aliyun-0.2.1.tar.gz/requests-aliyun-0.2.1/aliyunauth/__init__.py
@@ -0,0 +1,12 @@
+__version__ = "0.2.1"
+__author__ = "SkyLothar"
+__email__ = "allothar@gmail.com"
+__url__ = "http://github.com/skylothar/requests-aliyun"
+
+from .ecs import EcsAuth
+from .oss import OssAuth
+from .rds import RdsAuth
+from .slb import SlbAuth
+
+
+__all__ = ["EcsAuth", "OssAuth", "RdsAuth", "SlbAuth"]
---
+++ packages/requests-aliyun/requests-aliyun-0.2.1.tar.gz/requests-aliyun-0.2.1/setup.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+
+from codecs import open
+
+__version__ = "0.2.1"
+__author__ = "SkyLothar"
+__email__ = "allothar@gmail.com"
+__url__ = "http://github.com/skylothar/requests-aliyun"
+
+
+try:
+    import setuptools
+except ImportError:
+    from distutils.core import setuptools
+
+
+if sys.argv[-1] == "publish":
+    os.system("python setup.py sdist upload")
+    sys.exit()
+
+
+packages = ["aliyunauth"]
+
+
+with open("README.rst", "r", "utf-8") as f:
+    readme = f.read()
+
+with open("requirements.txt", "r", "utf-8") as f:
+    requires = f.read()
+
+with open("tests/requirements.txt", "r", "utf-8") as f:
+    tests_require = f.read()
+
+
+setuptools.setup(
+    name="requests-aliyun",
+    version=__version__,
+    description="authentication for aliyun service",
+    long_description=readme,
+    author=__author__,
+    author_email=__email__,
+    url=__url__,
+    packages=packages,
+    package_data={
+        "": ["LICENSE"]
+    },
+    package_dir={
+        "aliyunauth": "aliyunauth"
+    },
+    include_package_data=True,
+    install_requires=requires,
+    license="Apache 2.0",
+    zip_safe=False,
+    classifiers=(
+        b"Development Status :: 4 - Beta",
+        b"Intended Audience :: Developers",
+        b"Natural Language :: English",
+        b"License :: OSI Approved :: Apache Software License",
+        b"Programming Language :: Python",
+        b"Programming Language :: Python :: 2.7",
+        b"Programming Language :: Python :: 3",
+        b"Programming Language :: Python :: 3.3",
+        b"Programming Language :: Python :: 3.4"
+    ),
+    setup_requires=["nose >= 1.0"],
+    tests_require=tests_require,
+    test_suite="nose.collector"
+)
