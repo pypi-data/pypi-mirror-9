eb2eccf79d6bf88a81a728914ea3804f6094e37b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:24 +0000

    Add package PyPT/0.4.2/PyPT-0.4.2.tar.gz

---
+++ packages/PyPT/PyPT-0.4.2.tar.gz/PyPT-0.4.2/pypt/command/init.py
@@ -0,0 +1,43 @@
+"""
+Usage: pypt init [options]
+
+    -h --help
+    -i --interactive   Interactive mode (without this flag, defaults are used)
+"""
+
+from docopt import docopt
+from ..question.series.init import series
+from .._version import __version__
+from ..templates import get_template
+
+import re
+
+
+def init(argv):
+
+    args = docopt(__doc__, argv=argv)
+
+    if args["--interactive"]:
+
+        print "pypt version {}\n".format(__version__)
+        series.ask()
+
+    else:
+
+        series.process()
+
+    print series.template_vars
+
+    template = get_template("init", extensions=["jinja2.ext.do"])
+
+    setup_content = template.render(series.template_vars)
+
+    setup_content = "\n".join([x for x in setup_content.split("\n")
+                               if x.strip()])
+
+    setup_content = setup_content.replace("\\n", "")
+
+    setup_content = re.sub("(?:\n\s*){3,}", "\n\n", setup_content)
+
+    with open('setup.py', 'w') as setup_file:
+        setup_file.write(setup_content)
---
+++ packages/PyPT/PyPT-0.4.2.tar.gz/PyPT-0.4.2/pypt/question/choice.py
@@ -0,0 +1,69 @@
+from question import Question
+from ..io import get_input
+
+
+class ChoiceQuestion(Question):
+
+    def __init__(self, name, series, description,
+                 choices, follow_ups, default=None, message=None):
+        self.description = description
+
+        if not choices:
+            raise ValueError("choices parameter must be a list of strings")
+
+        if default and default not in choices:
+            raise ValueError(
+                "default ({}) not found in the list of choices ({})".format(
+                    default, ",".join(choices)
+                ))
+
+        for choice in choices:
+            if choice != default and choice not in follow_ups:
+                raise ValueError(
+                    "Non-default choice {} needs a follow-up question".format(
+                        default
+                    ))
+        self.choices = choices
+        self.default = default
+        self.follow_ups = follow_ups
+
+        super(ChoiceQuestion, self).__init__(name, series=series,
+                                             message=message)
+
+    def _get_message(self):
+
+        choice_str = "[{}]".format(",".join(self.choices))
+
+        if self.default:
+            choice_str += "({}): ".format(self.default)
+        else:
+            choice_str += ": "
+
+        return "{}:\n{}\n{}".format(self.name, self.description, choice_str)
+
+    def ask(self):
+
+        while not self.user_input:
+
+            self.user_input = get_input(self.message)
+
+            if not self.user_input and self.default:
+                self.user_input = self.default
+
+            if self.user_input in self.choices and \
+                    self.user_input in self.follow_ups:
+
+                self.follow_ups[self.user_input].ask()
+                break
+            elif self.user_input in self.choices:
+                break
+            else:
+                self.user_input = None
+
+    def process(self):
+        # The only way this should be called directly is to bypass
+        # asking the follow-up questions and just process the default
+        # follow-up.
+
+        if self.default in self.follow_ups:
+            self.follow_ups[self.default].process()
