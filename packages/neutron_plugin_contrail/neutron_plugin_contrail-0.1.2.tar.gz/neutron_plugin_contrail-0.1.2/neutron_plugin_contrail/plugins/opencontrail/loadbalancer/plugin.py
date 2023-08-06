9a7f451200c381f6d9d9c3e3cd89561708e98dc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:12 +0000

    Add package neutron_plugin_contrail/0.1.2/neutron_plugin_contrail-0.1.2.tar.gz

---
+++ packages/neutron_plugin_contrail/neutron_plugin_contrail-0.1.2.tar.gz/neutron_plugin_contrail-0.1.2/neutron_plugin_contrail/plugins/opencontrail/loadbalancer/plugin.py
@@ -0,0 +1,24 @@
+#
+# Copyright (c) 2014 Juniper Networks, Inc. All rights reserved.
+#
+from loadbalancer_db import LoadBalancerPluginDb
+
+class LoadBalancerPlugin(LoadBalancerPluginDb):
+    supported_extension_aliases = ["lbaas"]
+
+    def __init__(self):
+        super(LoadBalancerPlugin, self).__init__()
+
+    def get_plugin_description(self):
+        return "OpenContrail LoadBalancer Service Plugin"
+
+    def _pool_update_provider(self, context, pool):
+        if 'provider' not in pool or pool['provider'].__class__ is object:
+            pool['provider'] = "opencontrail"
+
+    def create_pool(self, context, pool):
+        self._pool_update_provider(context, pool['pool'])
+        p = super(LoadBalancerPlugin, self).create_pool(context, pool)
+	return p
+
+
