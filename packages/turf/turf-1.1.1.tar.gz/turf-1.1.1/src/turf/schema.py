223c6499e63a0864c06fa15fca82f96b066aff7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:44 +0000

    Add package turf/1.1.1/turf-1.1.1.tar.gz

---
+++ packages/turf/turf-1.1.1.tar.gz/turf-1.1.1/src/turf/schema.py
@@ -0,0 +1,7 @@
+"""This module contains pre-defined schema that may be useful"""
+
+#Bugsnag 
+bugsnag_api_key = {"type":"string", "regex":"^[a-f1-9]{32}$"}
+bugsnag_release_stage = {"type":"string"}
+
+
