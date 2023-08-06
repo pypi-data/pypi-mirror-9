48c19cd2071a0b5a0b950fa1ef1d54e9341886de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:02 +0000

    Add package django-fluent-dashboard/0.5/django-fluent-dashboard-0.5.tar.gz

---
+++ packages/django-fluent-dashboard/django-fluent-dashboard-0.5.tar.gz/django-fluent-dashboard-0.5/setup.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+from os import path
+import codecs
+import os
+import re
+import sys
+
+
+# When creating the sdist, make sure the django.mo file also exists:
+if 'sdist' in sys.argv or 'develop' in sys.argv:
+    os.chdir('fluent_dashboard')
+    try:
+        from django.core import management
+        management.call_command('compilemessages', stdout=sys.stderr, verbosity=1)
+    finally:
+        os.chdir('..')
+
+
+def read(*parts):
+    file_path = path.join(path.dirname(__file__), *parts)
+    return codecs.open(file_path, encoding='utf-8').read()
+
+
+def find_version(*parts):
+    version_file = read(*parts)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", version_file, re.M)
+    if version_match:
+        return str(version_match.group(1))
+    raise RuntimeError("Unable to find version string.")
+
+
+setup(
+    name='django-fluent-dashboard',
+    version=find_version('fluent_dashboard', '__init__.py'),
+    license='Apache 2.0',
+
+    install_requires=[
+        'django-admin-tools>=0.5.2',  # 0.5.2 has proper Django 1.5/1.6 support.
+    ],
+    requires=[
+        'Django (>=1.3)',
+    ],
+    extras_require = {
+        'cachestatus': ['dashboardmods>=0.2.2', 'feedparser', 'python-varnish'],
+    },
+
+    description='An improved django-admin-tools dashboard for Django projects',
+    long_description=read('README.rst'),
+
+    author='Diederik van der Boor',
+    author_email='opensource@edoburu.nl',
+
+    url='https://github.com/edoburu/django-fluent-dashboard',
+    download_url='https://github.com/edoburu/django-fluent-dashboard/zipball/master',
+
+    packages=find_packages(),
+    include_package_data=True,
+
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Framework :: Django',
+        'Framework :: Django :: 1.4',
+        'Framework :: Django :: 1.5',
+        'Framework :: Django :: 1.6',
+        'Framework :: Django :: 1.7',
+        'Framework :: Django :: 1.8',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
