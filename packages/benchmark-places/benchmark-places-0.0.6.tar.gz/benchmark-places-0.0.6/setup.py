a293ae0eec6bab1863c899aad037e3a959706cb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:25 +0000

    Add package benchmark-places/0.0.6/benchmark-places-0.0.6.tar.gz

---
+++ packages/benchmark-places/benchmark-places-0.0.6.tar.gz/benchmark-places-0.0.6/setup.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name='benchmark-places',
+    version='0.0.6',
+    author=u'Ken Koontz',
+    author_email='kenneth.koontz@gmail.com',
+    packages=find_packages(),
+    license='MIT licence, see LICENCE.txt',
+    url='https://github.com/usebenchmark/benchmarkplaces.git',
+    description='Place abstraction',
+    long_description=open('README.rst').read(),
+    install_requires = [
+        'rauth',
+        'requests'
+    ],
+    zip_safe=False,
+)
+
+# url='http://bitbucket.org/bruno/django-geoportail',
