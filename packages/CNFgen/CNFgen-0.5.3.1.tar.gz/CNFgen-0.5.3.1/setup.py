ae0576f811c75df6eb595be2177f3cc85af9388b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:19 +0000

    Add package CNFgen/0.5.3.1/CNFgen-0.5.3.1.tar.gz

---
+++ packages/CNFgen/CNFgen-0.5.3.1.tar.gz/CNFgen-0.5.3.1/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+"""
+Setup script for the CNFgen package
+"""
+
+from setuptools import setup
+
+
+setup(name='CNFgen',
+      version='0.5.3.1',
+      description='CNF formula generator',
+      author='Massimo Lauria',
+      author_email='lauria@kth.se',
+      url='https://github.com/MassimoLauria/cnfgen',
+      packages=['cnfformula', 'cnfformula.utils'],
+      license='GPL-3',
+      entry_points={
+          'console_scripts': [
+              'cnfgen=cnfformula.cnfgen:command_line_utility',
+              'cnfshuffle=cnfformula.utils.cnfshuffle:command_line_utility',
+              'cnftransform=cnfformula.utils.dimacstransform:command_line_utility',
+              'kthgraph2pebformula=cnfformula.utils.kthgraph2dimacs:command_line_utility'],
+      },
+      install_requires=['networkx'],
+      # make some tests
+      test_suite='nose.collector',
+      tests_require=['nose']
+     )
