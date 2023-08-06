aee323a058ad7c011192152820fc7f16fc5f88a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:49 +0000

    Add package django-countries/3.3/django-countries-3.3.tar.gz

---
+++ packages/django-countries/django-countries-3.3.tar.gz/django-countries-3.3/setup.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+import codecs
+from setuptools import setup, find_packages
+
+
+def read_files(*filenames):
+    """
+    Output the contents of one or more files to a single concatenated string.
+    """
+    output = []
+    for filename in filenames:
+        f = codecs.open(filename, encoding='utf-8')
+        try:
+            output.append(f.read())
+        finally:
+            f.close()
+    return '\n\n'.join(output)
+
+
+setup(
+    name='django-countries',
+    version='3.3',
+    description='Provides a country field for Django models.',
+    long_description=read_files('README.rst', 'CHANGES.rst'),
+    author='Chris Beaven',
+    author_email='smileychris@gmail.com',
+    url='https://github.com/SmileyChris/django-countries/',
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Framework :: Django',
+    ],
+)
