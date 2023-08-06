4d3f0287a5a64c5de75094c56051cfe448f35a41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:15 +0000

    Add package pytest-gitignore/1.2/pytest-gitignore-1.2.tar.gz

---
+++ packages/pytest-gitignore/pytest-gitignore-1.2.tar.gz/pytest-gitignore-1.2/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+
+setup(
+    name = 'pytest-gitignore',
+    version = '1.2',
+    description = 'py.test plugin to ignore the same files as git',
+    url = 'https://github.com/tgs/pytest-gitignore',
+    long_description = open('README.txt').read(),
+    author = 'Thomas Grenfell Smith',
+    author_email = 'smithtg@ncbi.nlm.nih.gov',
+    py_modules = ['pytest_gitignore'],
+    entry_points = {
+        'pytest11': [
+            'gitignore = pytest_gitignore',
+        ],
+    },
+    classifiers = [
+        'License :: Public Domain',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Testing',
+        'Intended Audience :: Developers',
+        'Development Status :: 4 - Beta',
+        'Topic :: Software Development :: Version Control',
+    ],
+)
