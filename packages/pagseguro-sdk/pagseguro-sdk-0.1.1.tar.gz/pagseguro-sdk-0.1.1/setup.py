41f48d82a52aac96d8d8bd15f26171d5620943d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:41 +0000

    Add package pagseguro-sdk/0.1.1/pagseguro-sdk-0.1.1.tar.gz

---
+++ packages/pagseguro-sdk/pagseguro-sdk-0.1.1.tar.gz/pagseguro-sdk-0.1.1/setup.py
@@ -0,0 +1,37 @@
+#! /usr/bin/env python
+# encoding: utf-8
+
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from distutils.core import setup, find_packages
+
+
+REQUIREMENTS = [i.strip() for i in open("requirements.txt").readlines()]
+
+__version__ = '0.1.1'
+
+setup(
+    name='pagseguro-sdk',
+    version=__version__,
+    description='SDK para utilização do PagSeguro em Python',
+    url='http://github.com/jeanmask/pagseguro-sdk/',
+    author='Jean O. Rodrigues',
+    author_email='github@jean.bz',
+    download_url='https://github.com/jeanmask/pagseguro-sdk/archive/v{0}.tar.gz'.format(__version__),
+    license='MIT',
+    packages=find_packages(exclude=('doc', 'docs', 'example')),
+    install_requires=REQUIREMENTS,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Environment :: Web Environment',
+        'Intended Audience :: End Users/Desktop',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    keywords=['pagseguro', 'sdk', 'python']
+)
