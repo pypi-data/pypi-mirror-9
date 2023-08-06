43392e48b7bd979686e43af108d8a6a91ef323f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:59 +0000

    Add package datanommer.models/0.6.5/datanommer.models-0.6.5.tar.gz

---
+++ packages/datanommer.models/datanommer.models-0.6.5.tar.gz/datanommer.models-0.6.5/datanommer/models/__init__.py
@@ -0,0 +1,405 @@
+# This file is a part of datanommer, a message sink for fedmsg.
+# Copyright (C) 2014, Red Hat, Inc.
+#
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program.  If not, see <http://www.gnu.org/licenses/>.
+from sqlalchemy import create_engine
+from sqlalchemy import (
+    Column,
+    DateTime,
+    Integer,
+    ForeignKey,
+    UnicodeText,
+)
+
+from sqlalchemy.orm import (
+    sessionmaker,
+    scoped_session,
+    relationship,
+    backref,
+)
+
+from sqlalchemy import not_, or_, between
+
+from sqlalchemy.orm import validates
+from sqlalchemy.orm.exc import NoResultFound
+
+from sqlalchemy.schema import Table
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.ext.hybrid import hybrid_property
+
+import pkg_resources
+
+import math
+import datetime
+import fedmsg.encoding
+
+maker = sessionmaker()
+session = scoped_session(maker)
+
+DeclarativeBase = declarative_base()
+DeclarativeBase.query = session.query_property()
+
+import logging
+log = logging.getLogger("datanommer")
+
+_users_seen, _packages_seen = set(), set()
+
+
+def init(uri=None, alembic_ini=None, engine=None, create=False):
+    """ Initialize a connection.  Create tables if requested."""
+
+    if uri and engine:
+        raise ValueError("uri and engine cannot both be specified")
+
+    if uri is None and not engine:
+        uri = 'sqlite:////tmp/datanommer.db'
+        log.warning("No db uri given.  Using %r" % uri)
+
+    if uri and not engine:
+        engine = create_engine(uri)
+
+    # We need to hang our own attribute on the sqlalchemy session to stop
+    # ourselves from initializing twice.  That is only a problem is the code
+    # calling us isn't consistent.
+    if getattr(session, '_datanommer_initialized', None):
+        log.warning("Session already initialized.  Bailing")
+        return
+    session._datanommer_initialized = True
+
+    session.configure(bind=engine)
+    DeclarativeBase.query = session.query_property()
+
+    # Loads the alembic configuration and generates the version table, with
+    # the most recent revision stamped as head
+    if alembic_ini is not None:
+        from alembic.config import Config
+        from alembic import command
+        alembic_cfg = Config(alembic_ini)
+        command.stamp(alembic_cfg, "head")
+
+    if create:
+        DeclarativeBase.metadata.create_all(engine)
+
+
+def add(message):
+    """ Take a dict-like fedmsg message and store it in the table.
+    """
+    timestamp = message['timestamp']
+    try:
+        timestamp = datetime.datetime.utcfromtimestamp(timestamp)
+    except Exception:
+        pass
+
+    obj = Message(
+        i=message['i'],
+        msg_id=message.get('msg_id', None),
+        topic=message['topic'],
+        timestamp=timestamp,
+        certificate=message.get('certificate', None),
+        signature=message.get('signature', None),
+    )
+
+    obj.msg = message['msg']
+
+    session.add(obj)
+
+    usernames = fedmsg.meta.msg2usernames(message)
+    packages = fedmsg.meta.msg2packages(message)
+
+    # If we've never seen one of these users before, then:
+    # 1) make sure they exist in the db (create them if necessary)
+    # 2) mark an in memory cache so we can remember that they exist without
+    #    having to hit the db.
+    for username in usernames:
+        if username not in _users_seen:
+            # Create the user in the DB if necessary
+            User.get_or_create(username)
+            # Then just mark an in memory cache noting that we've seen them.
+            _users_seen.add(username)
+
+    for package in packages:
+        if package not in _packages_seen:
+            Package.get_or_create(package)
+            _packages_seen.add(package)
+
+    session.flush()
+
+    # These two blocks would normally be a simple "obj.users.append(user)" kind
+    # of statement, but here we drop down out of sqlalchemy's ORM and into the
+    # sql abstraction in order to gain a little performance boost.
+    values = [{'username': username, 'msg': obj.id} for username in usernames]
+    if values:
+        session.execute(user_assoc_table.insert(), values)
+
+    values = [{'package': package, 'msg': obj.id} for package in packages]
+    if values:
+        session.execute(pack_assoc_table.insert(), values)
+
+    # TODO -- can we avoid committing every time?
+    session.flush()
+    session.commit()
+
+
+def source_version_default(context):
+    dist = pkg_resources.get_distribution("datanommer.models")
+    return dist.version
+
+
+class BaseMessage(object):
+    id = Column(Integer, primary_key=True)
+    msg_id = Column(UnicodeText, nullable=True, unique=True, default=None, index=True)
+    i = Column(Integer, nullable=False)
+    topic = Column(UnicodeText, nullable=False)
+    timestamp = Column(DateTime, nullable=False, index=True)
+    certificate = Column(UnicodeText)
+    signature = Column(UnicodeText)
+    category = Column(UnicodeText, nullable=False)
+    source_name = Column(UnicodeText, default=u"datanommer")
+    source_version = Column(UnicodeText, default=source_version_default)
+    _msg = Column(UnicodeText, nullable=False)
+
+    @validates('topic')
+    def get_category(self, key, topic):
+        import fedmsg.config
+        import fedmsg.meta
+
+        config = fedmsg.config.load_config([], None)
+        fedmsg.meta.make_processors(**config)
+
+        filters=[]
+        for f in fedmsg.meta.processors:
+            filters.append(f.__name__.lower())
+
+        for f in filters:
+            if '.%s.' % f in topic:
+                self.category = f
+                break
+            else:
+                self.category = 'Unclassified'
+
+        return topic
+
+    @hybrid_property
+    def msg(self):
+        return fedmsg.encoding.loads(self._msg)
+
+    @msg.setter
+    def msg(self, dict_like_msg):
+        self._msg = fedmsg.encoding.dumps(dict_like_msg)
+
+    @classmethod
+    def from_msg_id(cls, msg_id):
+        return cls.query.filter(cls.msg_id == msg_id).first()
+
+    def __json__(self, request=None):
+        return dict(
+            i=self.i,
+            msg_id=self.msg_id,
+            topic=self.topic,
+            timestamp=self.timestamp,
+            certificate=self.certificate,
+            signature=self.signature,
+            msg=self.msg,
+            source_name=self.source_name,
+            source_version=self.source_version,
+        )
+
+user_assoc_table = Table('user_messages', DeclarativeBase.metadata,
+    Column('username', UnicodeText, ForeignKey('user.name')),
+    Column('msg', Integer, ForeignKey('messages.id'))
+)
+
+pack_assoc_table = Table('package_messages', DeclarativeBase.metadata,
+    Column('package', UnicodeText, ForeignKey('package.name')),
+    Column('msg', Integer, ForeignKey('messages.id'))
+)
+
+
+class User(DeclarativeBase):
+    __tablename__ = 'user'
+
+    name = Column(UnicodeText, primary_key=True, index=True)
+
+    @classmethod
+    def get_or_create(cls, name):
+        try:
+            return cls.query.filter_by(name=name).one()
+        except NoResultFound:
+            obj = cls(name=name)
+            session.add(obj)
+            return obj
+
+
+class Package(DeclarativeBase):
+    __tablename__ = 'package'
+
+    name = Column(UnicodeText, primary_key=True, index=True)
+
+    @classmethod
+    def get_or_create(cls, name):
+        try:
+            return cls.query.filter_by(name=name).one()
+        except NoResultFound:
+            obj = cls(name=name)
+            session.add(obj)
+            return obj
+
+
+class Message(DeclarativeBase, BaseMessage):
+    __tablename__ = "messages"
+    users = relationship("User", secondary=user_assoc_table,
+                         backref=backref('messages'))
+    packages = relationship("Package", secondary=pack_assoc_table,
+                            backref=backref('messages'))
+
+    @classmethod
+    def grep(cls, start=None, end=None,
+             page=1, rows_per_page=100,
+             order="asc", msg_id=None,
+             users=None, not_users=None,
+             packages=None, not_packages=None,
+             categories=None, not_categories=None,
+             topics=None, not_topics=None,
+             contains=None,
+             defer=False):
+        """ Flexible query interface for messages.
+
+        Arguments are filters.  start and end should be :mod:`datetime` objs.
+
+        Other filters should be lists of strings.  They are applied in a
+        conjunctive-normal-form (CNF) kind of way
+
+        for example, the following::
+
+          users = ['ralph', 'lmacken']
+          categories = ['bodhi', 'wiki']
+
+        should return messages where
+
+          (user=='ralph' OR user=='lmacken') AND
+          (category=='bodhi' OR category=='wiki')
+
+        Furthermore, you can use a negative version of each argument.
+
+            users = ['ralph']
+            not_categories = ['bodhi', 'wiki']
+
+        should return messages where
+
+            (user == 'ralph') AND
+            NOT (category == 'bodhi' OR category == 'wiki')
+
+        ----
+
+        If the `defer` argument evaluates to True, the query won't actually
+        be executed, but a SQLAlchemy query object returned instead.
+        """
+
+        users = users or []
+        not_users = not_users or []
+        packages = packages or []
+        not_packs = not_packages or []
+        categories = categories or []
+        not_cats = not_categories or []
+        topics = topics or []
+        not_topics = not_topics or []
+        contains = contains or []
+
+        query = Message.query
+
+        # A little argument validation.  We could provide some defaults in
+        # these mixed cases.. but instead we'll just leave it up to our caller.
+        if (start != None and end == None) or (end != None and start == None):
+            raise ValueError("Either both start and end must be specified "
+                             "or neither must be specified")
+
+        if start and end:
+            query = query.filter(between(Message.timestamp, start, end))
+
+        if msg_id:
+            query = query.filter(Message.msg_id == msg_id)
+
+        # Add the four positive filters as necessary
+        if users:
+            query = query.filter(or_(
+                *[Message.users.any(User.name == u) for u in users]
+            ))
+
+        if packages:
+            query = query.filter(or_(
+                *[Message.packages.any(Package.name == p) for p in packages]
+            ))
+
+        if categories:
+            query = query.filter(or_(
+                *[Message.category == category for category in categories]
+            ))
+
+        if topics:
+            query = query.filter(or_(
+                *[Message.topic == topic for topic in topics]
+            ))
+
+        if contains:
+            query = query.filter(or_(
+                *[Message._msg.like('%%%s%%' % contain)
+                  for contain in contains]
+            ))
+
+        # And then the four negative filters as necessary
+        if not_users:
+            query = query.filter(not_(or_(
+                *[Message.users.any(User.name == u) for u in not_users]
+            )))
+
+        if not_packs:
+            query = query.filter(not_(or_(
+                *[Message.packages.any(Package.name == p) for p in not_packs]
+            )))
+
+        if not_cats:
+            query = query.filter(not_(or_(
+                *[Message.category == category for category in not_cats]
+            )))
+
+        if not_topics:
+            query = query.filter(not_(or_(
+                *[Message.topic == topic for topic in not_topics]
+            )))
+
+        # Finally, tag on our pagination arguments
+        total = query.count()
+        query = query.order_by(getattr(Message.timestamp, order)())
+
+        if rows_per_page is None:
+            pages = 1
+        else:
+            pages = int(math.ceil(total / float(rows_per_page)))
+            query = query.offset(rows_per_page * (page - 1)).limit(rows_per_page)
+
+        if defer:
+            return total, page, query
+        else:
+            # Execute!
+            messages = query.all()
+            return total, pages, messages
+
+models = frozenset((
+    v for k, v in locals().items()
+    if (
+        isinstance(v, type) and
+        issubclass(v, BaseMessage) and
+        k is not "BaseMessage"
+    )
+))
---
+++ packages/datanommer.models/datanommer.models-0.6.5.tar.gz/datanommer.models-0.6.5/tests/test_model.py
@@ -0,0 +1,297 @@
+# This file is a part of datanommer, a message sink for fedmsg.
+# Copyright (C) 2014, Red Hat, Inc.
+#
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program.  If not, see <http://www.gnu.org/licenses/>.
+import copy
+import datetime
+import os
+import pprint
+import sqlalchemy
+import sqlalchemy.exc
+import unittest
+import requests
+
+from sqlalchemy.orm import scoped_session
+
+from nose.tools import raises
+from nose.tools import eq_
+
+import datanommer.models
+
+# Set this to false to use faitout
+USE_SQLITE = True  # False
+
+filename = ":memory:"
+
+scm_message = {
+    "i": 1,
+    "timestamp": 1344350850.8867381,
+    "topic": "org.fedoraproject.prod.git.receive.valgrind.master",
+    "msg": {
+        "commit": {
+            "stats": {
+                "files": {
+                    "valgrind.spec": {
+                        "deletions": 2,
+                        "lines": 3,
+                        "insertions": 1
+                    }
+                },
+                "total": {
+                    "deletions": 2,
+                    "files": 1,
+                    "insertions": 1,
+                    "lines": 3
+                }
+            },
+            "name": "Mark Wielaard",
+            "rev": "7a98f80d9b61ce167e4ef8129c81ed9284ecf4e1",
+            "summary": "Clear CFLAGS CXXFLAGS LDFLAGS.",
+            "message": """Clear CFLAGS CXXFLAGS LDFLAGS.
+            This is a bit of a hammer.""",
+            "email": "mjw@redhat.com",
+            "branch": "master",
+            "username": "mjw",
+        }
+    },
+    "signature": "blah",
+    "certificate": "blah",
+}
+
+
+github_message = {
+    "i": 2,
+    "msg": {
+        "compare": "https://github.com/ralphbean/apps.fp.o",
+        "fas_usernames": {},
+        "hook": {
+            "active": True,
+            "config": {
+                "content_type": "json",
+                "secret": "G9KNgSeNb1xkhXFe6ZgnIJkUptGJZ2",
+                "url": "https://apps.fedoraproject.org/github2fedmsg/webhook"
+            },
+            "created_at": "2014-06-18T21:32:43Z",
+            "events": [ "*" ],
+            "id": 2442140,
+            "last_response": {
+                "code": None,
+                "message": None,
+                "status": "unused"
+            },
+            "name": "web",
+            "updated_at": "2014-06-18T21:32:43Z",
+            "url": "https://api.github.com/repos/ralphbean/apps.fp.o/"
+            "hooks/2442140",
+        },
+        "hook_id": 2442140,
+        "zen": "Keep it logically awesome."
+    },
+    "msg_id": "2014-6552feeb-6dd9-4c39-9839-2c35f0a0f498",
+    "source_name": "datanommer",
+    "source_version": "0.6.4",
+    "timestamp": 1403127164.0,
+    "topic": "org.fedoraproject.prod.github.webhook"
+}
+
+
+class TestModels(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        import fedmsg.config
+        import fedmsg.meta
+
+        config = fedmsg.config.load_config([], None)
+
+        if USE_SQLITE:
+            fname = "sqlite:///%s" % filename
+        else:
+            response = requests.get('http://209.132.184.152/faitout/new',
+                                    headers=dict(accept='application/json'))
+            details = response.json()
+            fname = "postgres://{username}:{password}@{host}:{port}/{dbname}"\
+                .format(**details)
+            cls.dbname = details['dbname']
+
+        config['datanommer.sqlalchemy.url'] = fname
+        fedmsg.meta.make_processors(**config)
+
+    @classmethod
+    def tearDownClass(cls):
+        if not USE_SQLITE:
+            requests.get("http://209.132.184.152/faitout/drop/{dbname}"\
+                        .format(dbname=cls.dbname))
+
+    def setUp(self):
+        if USE_SQLITE:
+            fname = "sqlite:///%s" % filename
+        else:
+            response = requests.get('http://209.132.184.152/faitout/new',
+                                    headers=dict(accept='application/json'))
+            details = response.json()
+            import pprint
+            pprint.pprint(details)
+            fname = "postgres://{username}:{password}@{host}:{port}/{dbname}"\
+                .format(**details)
+            self.dbname2 = details['dbname']
+        # We only have to do this so that we can do it over
+        # and over again for each test.
+        datanommer.models.session = scoped_session(datanommer.models.maker)
+        datanommer.models.init(fname, create=True)
+
+
+    def tearDown(self):
+        if USE_SQLITE:
+            engine = datanommer.models.session.get_bind()
+            datanommer.models.DeclarativeBase.metadata.drop_all(engine)
+        else:
+            datanommer.models.session.close()
+            requests.get("http://209.132.184.152/faitout/drop/{dbname}"\
+                        .format(dbname=self.dbname2))
+
+        # These contain objects bound to the old session, so we have to flush.
+        datanommer.models._users_seen = set()
+        datanommer.models._packages_seen = set()
+
+    @raises(KeyError)
+    def test_add_empty(self):
+        datanommer.models.add(dict())
+
+    @raises(KeyError)
+    def test_add_missing_i(self):
+        msg = copy.deepcopy(scm_message)
+        del msg['i']
+        datanommer.models.add(msg)
+
+    @raises(KeyError)
+    def test_add_missing_timestamp(self):
+        msg = copy.deepcopy(scm_message)
+        del msg['timestamp']
+        datanommer.models.add(msg)
+
+    def test_add_many_and_count_statements(self):
+        statements = []
+
+        def track(conn, cursor, statement, param, ctx, many):
+            statements.append(statement)
+
+        engine = datanommer.models.session.get_bind()
+        sqlalchemy.event.listen(engine, "before_cursor_execute", track)
+
+        msg = copy.deepcopy(scm_message)
+
+        # Add it to the db and check how many queries we made
+        datanommer.models.add(msg)
+        eq_(len(statements), 7)
+
+        # Add it again and check again
+        datanommer.models.add(msg)
+        pprint.pprint(statements)
+        eq_(len(statements), 10)
+
+    def test_add_missing_cert(self):
+        msg = copy.deepcopy(scm_message)
+        del msg['certificate']
+        datanommer.models.add(msg)
+
+    def test_add_and_check_for_others(self):
+        # There are no users or packages at the start
+        eq_(datanommer.models.User.query.count(), 0)
+        eq_(datanommer.models.Package.query.count(), 0)
+
+        # Then add a message
+        msg = copy.deepcopy(scm_message)
+        datanommer.models.add(msg)
+
+        # There should now be one of each
+        eq_(datanommer.models.User.query.count(), 1)
+        eq_(datanommer.models.Package.query.count(), 1)
+
+        # If we add it again, there should be no duplicates
+        msg['msg']['msg_id'] = 'foobar2'
+        datanommer.models.add(msg)
+        eq_(datanommer.models.User.query.count(), 1)
+        eq_(datanommer.models.Package.query.count(), 1)
+
+        msg = copy.deepcopy(scm_message)
+        msg['msg']['commit']['username'] = 'ralph'
+        msg['msg']['msg_id'] = 'foobar3'
+        datanommer.models.add(msg)
+        eq_(datanommer.models.User.query.count(), 2)
+        eq_(datanommer.models.Package.query.count(), 1)
+
+    def test_add_nothing(self):
+        eq_(datanommer.models.Message.query.count(), 0)
+
+    def test_add_and_check(self):
+        msg = copy.deepcopy(scm_message)
+        datanommer.models.add(msg)
+        datanommer.models.session.flush()
+        eq_(datanommer.models.Message.query.count(), 1)
+
+    def test_categories(self):
+        msg = copy.deepcopy(scm_message)
+        datanommer.models.add(msg)
+        datanommer.models.session.flush()
+        obj = datanommer.models.Message.query.first()
+        eq_(obj.category, 'git')
+
+    def test_grep_all(self):
+        msg = copy.deepcopy(scm_message)
+        datanommer.models.add(msg)
+        datanommer.models.session.flush()
+        t, p, r = datanommer.models.Message.grep()
+        eq_(t, 1)
+        eq_(p, 1)
+        eq_(len(r), 1)
+        eq_(r[0].msg, scm_message['msg'])
+
+    def test_grep_category(self):
+        msg = copy.deepcopy(scm_message)
+        datanommer.models.add(msg)
+        datanommer.models.session.flush()
+        t, p, r = datanommer.models.Message.grep(categories=['git'])
+        eq_(t, 1)
+        eq_(p, 1)
+        eq_(len(r), 1)
+        eq_(r[0].msg, scm_message['msg'])
+
+    def test_grep_not_category(self):
+        msg = copy.deepcopy(scm_message)
+        datanommer.models.add(msg)
+        datanommer.models.session.flush()
+        t, p, r = datanommer.models.Message.grep(not_categories=['git'])
+        eq_(t, 0)
+        eq_(p, 0)
+        eq_(len(r), 0)
+
+    def test_add_with_close_category(self):
+        msg = copy.deepcopy(github_message)
+        datanommer.models.add(msg)
+        datanommer.models.session.flush()
+        t, p, r = datanommer.models.Message.grep(categories=['github'])
+        eq_(t, 1)
+        eq_(p, 1)
+        eq_(len(r), 1)
+        eq_(r[0].msg_id, '2014-6552feeb-6dd9-4c39-9839-2c35f0a0f498')
+
+    def test_timezone_awareness(self):
+        msg = copy.deepcopy(github_message)
+        datanommer.models.add(msg)
+        datanommer.models.session.flush()
+
+        queried = datanommer.models.Message.query.one()
+
+        t = queried.timestamp
+        eq_(t, datetime.datetime(2014, 6, 18, 21, 32, 44))
