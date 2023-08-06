7ddbf83fc9c4efa9159b0e4e18f31e1090666974
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:46 +0000

    Add package livecodingtv-publisher/0.0.5/livecodingtv-publisher-0.0.5.tar.gz

---
+++ packages/livecodingtv-publisher/livecodingtv-publisher-0.0.5.tar.gz/livecodingtv-publisher-0.0.5/setup.py
@@ -0,0 +1,51 @@
+from setuptools import setup, find_packages
+
+version = "0.0.5"
+
+long_description = ""
+try:
+    long_description=file('README.md').read()
+except Exception:
+    pass
+
+license = ""
+try:
+    license=file('LICENSE').read()
+except Exception:
+    pass
+
+
+setup(
+    name = 'livecodingtv-publisher',
+    version = version,
+    description = 'A livecodingtv streaming tool',
+    author = 'Pablo Saavedra',
+    author_email = 'saavedra.pablo@gmail.com',
+    url = 'http://github.com/psaavedra/livecodingtv-publisher',
+    packages = find_packages(),
+    package_data={
+    },
+    scripts=[
+        "tools/livecodingtv-publisher",
+    ],
+    zip_safe=False,
+    install_requires=[
+    ],
+    data_files=[
+        ('/usr/share/doc/livecodingtv-publisher/',
+            ['cfg/livecodingtv-publisher.cfg.example']
+        ),
+    ],
+
+    download_url= 'https://github.com/psaavedra/livecodingtv-publisher/zipball/master',
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Topic :: System :: Operating System Kernels :: Linux",
+        "Programming Language :: Python",
+        "Topic :: Multimedia :: Video :: Capture",
+    ],
+    long_description=long_description,
+    license=license,
+    keywords = "python libav avconv video streaming livecodingtv",
+)
