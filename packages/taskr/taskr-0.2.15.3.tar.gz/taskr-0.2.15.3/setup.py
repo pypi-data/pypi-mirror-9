2faadf5cd3f258ac26e15d489053e8d86283f769
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:23 +0000

    Add package taskr/0.2.15.3/taskr-0.2.15.3.tar.gz

---
+++ packages/taskr/taskr-0.2.15.3.tar.gz/taskr-0.2.15.3/setup.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+#
+# Copyright 2014-2015 sodastsai
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+from __future__ import unicode_literals, division, absolute_import, print_function
+
+from setuptools import setup, find_packages
+
+setup(name='taskr',
+      version='0.2.15.3',
+      description='Python Command Line Utility',
+      author='sodastsai',
+      author_email='sodas2002@gmail.com',
+      license='Apache License Version 2.0',
+      url='https://github.com/sodastsai/taskr',
+      long_description='''taskr - python command line utility helper''',
+      packages=find_packages(),
+      install_requires=[
+          'six>=1.8.0',
+      ],
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Environment :: Console',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: Apache Software License',
+          'Operating System :: MacOS :: MacOS X',
+          'Operating System :: POSIX :: Linux',
+          'Operating System :: Unix',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.4',
+          'Topic :: Software Development',
+          'Topic :: Utilities',
+      ])
