a88778f94df952797d83736fab40c0b95c06a1be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:07:45 +0000

    Add package buttercms-django/0.2.1/buttercms-django-0.2.1.tar.gz

---
+++ packages/buttercms-django/buttercms-django-0.2.1.tar.gz/buttercms-django-0.2.1/setup.py
@@ -0,0 +1,35 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+with open('README.rst', 'r') as f:
+    readme = f.read()
+
+packages = [
+    'buttercms'
+]
+
+setup(name='buttercms-django',
+        version='0.2.1',
+        description='Company blogs as a service. Built for developers.',
+        long_description=readme,
+        url='https://www.buttercms.com',
+        author='ButterCMS',
+        author_email='jake@buttercms.com',
+        license='MIT',
+        packages=packages,
+        install_requires=['requests'],
+        classifiers=[
+            'Development Status :: 3 - Alpha',
+            'Intended Audience :: Developers',
+            'Environment :: Web Environment',
+            'Operating System :: OS Independent',
+            'Programming Language :: Python',
+            'Framework :: Django',
+            'Topic :: Software Development :: Build Tools',
+            'License :: OSI Approved :: MIT License',
+        ],
+        keywords='django blog service',
+        include_package_data=True,
+        zip_safe=False)
