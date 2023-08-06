7081ae1b73844971e065b534618def58539bd58d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:05 +0000

    Add package django-inline-wrestler/3.2/django-inline-wrestler-3.2.tar.gz

---
+++ packages/django-inline-wrestler/django-inline-wrestler-3.2.tar.gz/django-inline-wrestler-3.2/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+#this is a test
+setup(name = 'django-inline-wrestler',
+      description = 'Wrestle your django inlines with jQuery and Grappelli.',
+      version = '3.2',
+      url = 'https://github.com/ninapavlich/django-inline-wrestler',
+      author = 'Nina Pavlich',
+      author_email='nina@ninalp.com',
+      license = 'BSD',
+      packages=find_packages(),
+      package_data={'': ['*.py','*.css','*.js']},
+      include_package_data=True,
+      install_requires = ['setuptools', 'Django', 'django-grappelli'],
+      classifiers=[
+            'Development Status :: 4 - Beta',
+            'Environment :: Web Environment',
+            'Framework :: Django',
+            'Intended Audience :: Developers',
+            'License :: OSI Approved',
+            'Operating System :: OS Independent',
+            'Programming Language :: Python'
+      ]
+)
