477f2975bd078f4b5e237b4c4e6e7d698549987f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:14 +0000

    Add package xmutant/0.1/xmutant-0.1.tar.gz

---
+++ packages/xmutant/xmutant-0.1.tar.gz/xmutant-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='xmutant',
+      version='0.1',
+      description='Bytecode based mutation testing framework',
+      url='http://github.com/vrthra/xmutant.py',
+      author='Rahul Gopinath',
+      author_email='rahul@gopinath.org',
+      license='GPLv2',
+      packages=['xmutant'],
+      zip_safe=False)
