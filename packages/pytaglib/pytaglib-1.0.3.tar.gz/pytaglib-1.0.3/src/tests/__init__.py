cb7e13c079457b2591ae1927312c6e7c00986d7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:44 +0000

    Add package pytaglib/1.0.3/pytaglib-1.0.3.tar.gz

---
+++ packages/pytaglib/pytaglib-1.0.3.tar.gz/pytaglib-1.0.3/src/tests/__init__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Copyright 2011-2015 Michael Helmling
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 3 as
+# published by the Free Software Foundation
+#
+from contextlib import contextmanager
+import os.path, shutil, tempfile
+
+
+@contextmanager
+def copyTestFile(name):
+    """Make a temporary copy of test data file *name* (without dir) and return its full path. The file
+    is deleted on exit."""
+    orig_file = os.path.join(os.path.dirname(__file__), 'data', name)
+    tempdir = tempfile.mkdtemp()
+    copy_file = os.path.join(tempdir, name)
+    shutil.copy(orig_file, copy_file)
+    try:
+        yield copy_file
+    finally:
+        shutil.rmtree(tempdir)
---
+++ packages/pytaglib/pytaglib-1.0.3.tar.gz/pytaglib-1.0.3/src/tests/test_mp3.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+# Copyright 2011-2015 Michael Helmling
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 3 as
+# published by the Free Software Foundation
+#
+from __future__ import unicode_literals
+import unittest, taglib
+from . import copyTestFile
+
+
+class ID3v2Test(unittest.TestCase):
+    
+    def test_removeFrame1(self):
+        """See https://bugs.kde.org/show_bug.cgi?id=298183
+        
+        Uses the applyID3v2Hack to work on older taglib releases. With taglib>=1.9, should
+        also work without the hack.
+        """
+        with copyTestFile('rare_frames.mp3') as f:
+            tfile = taglib.File(f, True)
+            self.assert_('GENRE' in tfile.tags)
+            self.assertEqual(len(tfile.tags['GENRE']), 1)
+            del tfile.tags['GENRE']
+            tfile.save()
+            del tfile
+            tfile = taglib.File(f)
+            self.assert_('GENRE' not in tfile.tags)
+            
+    def test_removeFrame2(self):
+        """See https://bugs.kde.org/show_bug.cgi?id=298183."""
+        with copyTestFile('r2.mp3') as f:
+            tfile = taglib.File(f, True)
+            self.assert_('TITLE' in tfile.tags)
+            self.assertEqual(len(tfile.tags['TITLE']), 1)
+            del tfile.tags['TITLE']
+            tfile.save()
+            del tfile
+            tfile = taglib.File(f)
+            self.assert_('TITLE' not in tfile.tags)
+
+    def test_id3v1Tov2(self):
+        with copyTestFile('onlyv1.mp3') as f:
+            tfile = taglib.File(f, True)
+            self.assert_('ARTIST' in tfile.tags)
+            self.assertEqual(tfile.tags['ARTIST'][0], 'Bla')
+            tfile.tags['NONID3V1'] = ['omg', 'wtf']
+            ret = tfile.save()
+            self.assertEqual(len(ret), 0)
+            
+            tfile = taglib.File(f)
+            self.assert_('NONID3V1' in tfile.tags)
