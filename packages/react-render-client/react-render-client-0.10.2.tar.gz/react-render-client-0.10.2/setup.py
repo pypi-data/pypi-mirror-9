5b4434cd96dfb4198f838a6f2c23b1d1bdcbd9e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:13 +0000

    Add package react-render-client/0.10.2/react-render-client-0.10.2.tar.gz

---
+++ packages/react-render-client/react-render-client-0.10.2.tar.gz/react-render-client-0.10.2/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+VERSION = '0.10.2'
+
+setup(
+    name='react-render-client',
+    version=VERSION,
+    packages=['react_render'],
+    install_requires=[
+        'django>=1.6',
+        'requests>=2',
+    ],
+    description='Render and bundle React components from a Django application',
+    long_description='Documentation at https://github.com/mic159/react-render',
+    author='Michael Cooper',
+    author_email='mic159@gmail.com',
+    url='https://github.com/mic159/react-render',
+    download_url='https://github.com/mic159/react-render/tarball/{}'.format(VERSION),
+    keywords=['react', 'reactjs', 'django', 'isomorphic', 'templates'],
+    license='MIT',
+    classifiers=[
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development',
+    ]
+)
