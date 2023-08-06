e9a137e637d24fa35311c64f96ad7bcaef2481ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:37 +0000

    Add package pytest-bdd/2.7.0/pytest-bdd-2.7.0.tar.gz

---
+++ packages/pytest-bdd/pytest-bdd-2.7.0.tar.gz/pytest-bdd-2.7.0/pytest_bdd/exceptions.py
@@ -0,0 +1,51 @@
+"""pytest-bdd Exceptions."""
+
+
+class StepError(Exception):
+
+    """Step declaration error."""
+
+
+class ScenarioIsDecoratorOnly(Exception):
+
+    """Scenario can be only used as decorator."""
+
+
+class ScenarioValidationError(Exception):
+
+    """Base class for scenario validation."""
+
+
+class ScenarioNotFound(ScenarioValidationError):
+
+    """Scenario Not Found."""
+
+
+class ScenarioExamplesNotValidError(ScenarioValidationError):
+
+    """Scenario steps argumets do not match declared scenario examples."""
+
+
+class StepTypeError(ScenarioValidationError):
+
+    """Step definition is not of the type expected in the scenario."""
+
+
+class GivenAlreadyUsed(ScenarioValidationError):
+
+    """Fixture that implements the Given has been already used."""
+
+
+class StepDefinitionNotFoundError(Exception):
+
+    """Step definition not found."""
+
+
+class InvalidStepParserError(Exception):
+
+    """Invalid step parser."""
+
+
+class NoScenariosFound(Exception):
+
+    """No scenarios found."""
