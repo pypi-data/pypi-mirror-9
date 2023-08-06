52f209b60b809e20da51d385c3e96df9c33608cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:49 +0000

    Add package rabbitpy/0.26.1/rabbitpy-0.26.1-py2.py3-none-any.whl

---
+++ packages/rabbitpy/rabbitpy-0.26.1-py2.py3-none-any.whl/rabbitpy/__init__.py
@@ -0,0 +1,48 @@
+"""
+rabbitpy, a pythonic RabbitMQ client
+
+"""
+__version__ = '0.26.1'
+version = __version__
+import logging
+import platform
+
+PYPY = platform.python_implementation() == 'PyPy'
+
+try:
+    from logging import NullHandler
+except ImportError:
+    class NullHandler(logging.Handler):
+        """Python 2.6 does not have a NullHandler"""
+        def emit(self, record):
+            """Emit a record
+
+            :param record record: The record to emit
+
+            """
+            pass
+
+logging.getLogger('rabbitpy').addHandler(NullHandler())
+
+from rabbitpy.amqp import AMQP
+from rabbitpy.connection import Connection
+from rabbitpy.channel import Channel
+from rabbitpy.exchange import Exchange
+from rabbitpy.exchange import DirectExchange
+from rabbitpy.exchange import FanoutExchange
+from rabbitpy.exchange import HeadersExchange
+from rabbitpy.exchange import TopicExchange
+from rabbitpy.message import Message
+from rabbitpy.amqp_queue import Queue
+from rabbitpy.tx import Tx
+
+from rabbitpy.simple import consume
+from rabbitpy.simple import get
+from rabbitpy.simple import publish
+from rabbitpy.simple import create_queue
+from rabbitpy.simple import delete_queue
+from rabbitpy.simple import create_direct_exchange
+from rabbitpy.simple import create_fanout_exchange
+from rabbitpy.simple import create_headers_exchange
+from rabbitpy.simple import create_topic_exchange
+from rabbitpy.simple import delete_exchange
