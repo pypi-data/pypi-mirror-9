41f5ecf29a15c5c4ba58e4e6d3b1f4e150cf3e79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:07:55 +0000

    Add package django-advanced-reports/0.9.4/django-advanced-reports-0.9.4.tar.gz

---
+++ packages/django-advanced-reports/django-advanced-reports-0.9.4.tar.gz/django-advanced-reports-0.9.4/advreport_test_project/advreport_test_project/urls.py
@@ -0,0 +1,18 @@
+from django.conf.urls import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+from django.views.generic.base import RedirectView
+
+from advanced_reports.backoffice.examples.backoffice import test_backoffice
+from advreport_test_project.backoffice_definitions import todos_backoffice
+
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    url(r'^$', RedirectView.as_view(url='/todos-backoffice/'), name='home'),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^test-backoffice/', include(test_backoffice.urls)),
+    url(r'^todos-backoffice/', include(todos_backoffice.urls)),
+    url(r'^reports/', include('advanced_reports.urls')),
+)
