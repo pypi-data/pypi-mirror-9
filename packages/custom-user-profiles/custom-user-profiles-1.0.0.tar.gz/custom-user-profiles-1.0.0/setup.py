ac18deae6854d87f8a2820e7c7f38e1bdece1b3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:11 +0000

    Add package custom-user-profiles/1.0.0/custom-user-profiles-1.0.0.tar.gz

---
+++ packages/custom-user-profiles/custom-user-profiles-1.0.0.tar.gz/custom-user-profiles-1.0.0/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# encoding: utf-8
+from setuptools import setup, find_packages
+from pip.req import parse_requirements
+
+install_reqs = parse_requirements("requirements.txt")
+reqs = [str(each.req) for each in install_reqs if each.req]
+
+setup(
+    name             = 'custom-user-profiles',
+    version          = '1.0.0',
+    author           = "Diogo Laginha",
+    author_email     = "diogo.laginha.machado@gmail.com",
+    url              = 'https://github.com/laginha/custom-user-profiles',
+    description      = "Create custom-user profiles easily",
+    packages         = find_packages(where='src'),
+    package_dir      = {'': 'src'},
+    install_requires = reqs,
+    extras_require   = {},
+    zip_safe         = False,
+    license          = 'MIT',
+    classifiers      = [
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Intended Audience :: Developers',
+    ]
+)
---
+++ packages/custom-user-profiles/custom-user-profiles-1.0.0.tar.gz/custom-user-profiles-1.0.0/src/custom_user_profiles/models.py
@@ -0,0 +1,42 @@
+from django.db import models
+from django.conf import settings
+from custom_user.models import AbstractEmailUser
+import re
+
+PATTERN = re.compile(r'^is_(.+)$')
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL)
+    
+    class Meta:
+        abstract = True
+
+
+class CustomUserMixin(object):
+    @property
+    def profile(self):
+        if not hasattr(self, '_profile'):
+            for each in self._meta.get_all_field_names():
+                value = getattr(self, each, None)
+                if value and isinstance(value, Profile):
+                    self._profile = value
+        return self._profile
+	
+    def __getattr__(self, name):
+        match = re.match(PATTERN, name)
+        if match:
+            return self.is_profile(match.group(1))
+        raise AttributeError(name)
+	
+    def is_profile(self, name):
+        return bool(getattr(self, name, None))
+
+
+class AbstractCustomUser(AbstractEmailUser, CustomUserMixin):    
+    class Meta:
+        abstract = True
+        
+class CustomUser(AbstractCustomUser):
+    pass
+    
